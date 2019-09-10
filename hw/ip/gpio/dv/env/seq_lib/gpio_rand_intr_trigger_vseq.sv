9a3c12c31095636edfee57def472fca83c556fd3
Gaurang Chitroda <gaurangg@google.com>
2019-09-10 14:39:07 -0700

    [gpio dv] Scoreboard updates to compare against correct(latched) reg read data
    1. gpio_env_pkg.sv: Updated structure gpio_reg_update_due definition
    2. gpio_scoreboard.sv: rtl provides latched values as register read data
       updated prediction for data_in and intr_state registers to match this
       This update makes scoreboard much more reliable and roust for random
       stimulus.
       Also updated scoreboard for "interrupt test" register functionality.
    3. gpio_intr_with_filter_rand_intr_event_vseq.sv: Delay intr_state read to
       account for rtl delay
    4. gpio_rand_intr_trigger_vseq.sv: Updated to provide more meaningful random
       stimulus that can hit corner cases for intrrupt updates, clearing and
       read back

--- hw/ip/gpio/dv/env/seq_lib/gpio_rand_intr_trigger_vseq.sv
+++ hw/ip/gpio/dv/env/seq_lib/gpio_rand_intr_trigger_vseq.sv
@@ -38,33 +38,45 @@ class gpio_rand_intr_trigger_vseq extends gpio_base_vseq;
       //   (i) toggle gpio_i input at random intervals
       //  (ii) inject random read on gpio "intr_state" register
       begin
-        uint rd_period;
         uint cnt_gpio_tgl;
         bit gpio_tgl_cycle_done;
         `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(cnt_gpio_tgl, cnt_gpio_tgl inside {[5:20]};)
-        `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(rd_period, rd_period inside {[2:20]};)
         fork
           begin : gpio_in_data_drive
             for (uint iter = 0; iter < cnt_gpio_tgl; iter++) begin
               bit [TL_DW-1:0] gpio_i;
               bit [TL_DW-1:0] data_in;
+              int delay_before_gpio_change;
               `DV_CHECK_STD_RANDOMIZE_FATAL(gpio_i)
+              `uvm_info(msg_id, $sformatf("Driving new gpio value 0x%0h", gpio_i), UVM_HIGH)
               cfg.gpio_vif.drive(gpio_i);
-              cfg.clk_rst_vif.wait_clks(1);
-              // read data_in register
-              csr_rd(.ptr(ral.data_in), .value(data_in));
+              `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(delay_before_gpio_change,
+                                                 delay_before_gpio_change inside {[1:5]};)
+              cfg.clk_rst_vif.wait_clks(delay_before_gpio_change);
             end
             gpio_tgl_cycle_done = 1'b1;
           end
           begin : periodic_intr_state_rd_check
             do begin
-              bit [TL_DW-1:0] intr_state;
+              uint rd_period;
+              bit [TL_DW-1:0] reg_rd_data;
+              `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(rd_period, rd_period inside {[2:20]};)
               cfg.clk_rst_vif.wait_clks(rd_period);
-              // read intr_state register
-              csr_rd(.ptr(ral.intr_state), .value(intr_state));
+              `uvm_info(msg_id, $sformatf("Reading intr_state after %0d more clock cycles",
+                                          rd_period), UVM_HIGH)
+              randcase
+                1: begin
+                  csr_rd(.ptr(ral.data_in), .value(reg_rd_data));
+                end
+                9: begin
+                  csr_rd(.ptr(ral.intr_state), .value(reg_rd_data));
+                end
+              endcase
               // Randomly clear random set of interrupt state register bits
               if ($urandom_range(0, 1)) begin
                 `DV_CHECK_RANDOMIZE_FATAL(ral.intr_state)
+                `uvm_info(msg_id, $sformatf("Writing value 0x%0h to intr_state",
+                          ral.intr_state.get()), UVM_HIGH)
                 csr_update(ral.intr_state);
               end
             end
