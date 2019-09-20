88179fc760529329c01916b3504d15af36fed5fd
Miguel Osorio <miguelosorio@google.com>
2019-09-19 23:37:48 -0700

    [CI] Add SW build using make targets
    Add software build targets to Azure pipelines. This may
    eventually change once we add system test coverage to CI.
    
    To run the flow in local mode:
    
    ```console
    $ cd ${REPO_TOP}
    $ ci/run_sw_build.sh
    ```
    
    By default TOOLCHAIN_PATH points to /tools/riscv. If overridden,
    the script will fetch the toolchain if needed before running the
    build, e.g.:
    
    ```console
    $ cd ${REPO_TOP}
    $ TOOLCHAIN_PATH=/tmp/toolchain-path ci/run_sw_build.sh
    ```
    
    Additional ENVARs:
    
    * `REQUEST_UPDATE`: If set to true, the script will attempt to update
      toolchain to the requested `TOOLCHAIN_VERSION` which defaults to
      `latest`.
    
    Other Comments:
    
    * Why a shell script instead of Python? I expect that once we move
    to a new build system, the script will no longer require to hardcode
    the target enumeration, so the bulk of the logic will be gone.
    I am open to rewriting in Python if we prefer consistency.

---
+++ ci/run_sw_build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+set -e
+
+readonly DEFAULT_RISCV_TOOLS=/tools/riscv
+readonly DEFAULT_RISCV_TOOLS_VERSION=latest
+readonly DEFAULT_TOOLCHAIN_REQUEST_UPDATE=false
+
+TOOLCHAIN_PATH="${TOOLCHAIN_PATH:-$DEFAULT_RISCV_TOOLS}"
+TOOLCHAIN_VERSION="${TOOLCHAIN_VERSION:-$DEFAULT_RISCV_TOOLS_VERSION}"
+REQUEST_UPDATE="${REQUEST_UPDATE:-$DEFAULT_TOOLCHAIN_REQUEST_UPDATE}"
+
+if [ ! -d "${TOOLCHAIN_PATH}/bin" ]; then
+  echo "Unable to find toolchain at: ${TOOLCHAIN_PATH}."
+  REQUEST_UPDATE=true
+fi
+
+export RV_TOOLS="${TOOLCHAIN_PATH}/bin"
+echo "Setting toolchain path to RV_TOOLS=${RV_TOOLS}"
+
+if [ "${REQUEST_UPDATE}" = true ]; then
+  echo "Syncing toolchain directory to TOOLCHAIN_VERSION=${TOOLCHAIN_VERSION}"
+  util/get-toolchain.py "--target-dir=${TOOLCHAIN_PATH}" --update --force \
+    "--release-version=${TOOLCHAIN_VERSION}"
+fi
+
+readonly BUILD_TARGETS=("boot_rom"
+  "examples/hello_usbdev"
+  "examples/hello_world"
+  "tests/flash_ctrl"
+  "tests/hmac"
+  "tests/rv_timer"
+)
+
+FAIL_TARGETS=()
+PASS_TARGETS=()
+for target in "${BUILD_TARGETS[@]}"; do
+  echo "Building target ${target}"
+  if make -C sw "SW_DIR=${target}" "SW_BUILD_DIR=build/${target}"; then
+    PASS_TARGETS=("${PASS_TARGETS[@]}" "${target}")
+  else
+    FAIL_TARGETS=("${FAIL_TARGETS[@]}" "${target}")
+  fi
+done
+
+echo "* PASS_TARGETS"
+for target in "${PASS_TARGETS[@]}"; do
+  echo "${target}"
+done
+
+if [ ${#FAIL_TARGETS[@]} -eq 0 ]; then
+  echo "BUILD PASS!"
+else
+  echo "* FAIL_TARGETS"
+  for target in "${FAIL_TARGETS[@]}"; do
+    echo "${target}"
+  done
+  echo "BUILD FAILED!"
+  exit 1
+fi
