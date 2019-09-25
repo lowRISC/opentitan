e589b7920dfabb45c9583f47f5d6b1d230a61b22
Michael Schaffner <msf@google.com>
2019-09-25 16:17:35 -0700

    [reggen] Refactor reggen, first step towards nested multiregs

--- util/reggen/data.py
+++ util/reggen/data.py
@@ -9,7 +9,7 @@ class Field():
     """Field in a register.
 
     Field class contains necessary info to generate RTL code.
-    It has two additional (tool generated) feidls, swrdaccess and swwraccess,
+    It has two additional (tool generated) fields, swrdaccess and swwraccess,
     which represent read and write type. This makes RTL generation code simpler.
     """
     name = ""  # required
@@ -22,6 +22,7 @@ class Field():
     hwaccess = HwAccess.HRO
     hwqe = False
     hwre = False
+    hwext = False
 
     def __init__(self):
         self.name = ""  # required
@@ -34,6 +35,24 @@ class Field():
         self.hwaccess = HwAccess.HRO
         self.hwqe = False
         self.hwre = False
+        self.hwext = False
+
+    def get_n_bits(self, bittype=["q"]):
+        n_bits = 0
+        if "q" in bittype and self.hwaccess in [HwAccess.HRW, HwAccess.HRO]:
+            n_bits += self.msb - self.lsb + 1
+        if "d" in bittype and self.hwaccess in [HwAccess.HRW, HwAccess.HWO]:
+            n_bits += self.msb - self.lsb + 1
+        if "qe" in bittype and self.hwaccess in [HwAccess.HRW, HwAccess.HRO]:
+            n_bits += self.hwqe
+        if "re" in bittype and self.hwaccess in [HwAccess.HRW, HwAccess.HRO]:
+            n_bits += self.hwre
+        if "de" in bittype and self.hwaccess in [HwAccess.HRW, HwAccess.HWO]:
+            n_bits += not self.hwext
+        return n_bits
+
+    def get_fields_flat(self):
+        return [self]
 
 
 class Reg():
@@ -48,8 +67,8 @@ class Reg():
     fields = []
     width = 0  # indicate register size
 
-    def __init__(self):
-        self.name = ""
+    def __init__(self, name=""):
+        self.name = name
         self.offset = 0
         self.hwqe = False
         self.hwre = False
@@ -60,15 +79,85 @@ class Reg():
         self.fields = []
         self.width = 0
 
+    def is_multi_reg(self):
+        """Returns true if this is a multireg"""
+        return False
+
+    def get_n_bits(self, bittype=["q"]):
+        """Returns number of bits in this register (including all multiregs and
+        fields). By default this function counts read data bits (bittype "q"),
+        but other bits such as "d", qe", "re", "de" can be counted as well by
+        specifying them in the bittype list argument.
+        """
+        n_bits = 0
+        for f in self.fields:
+            n_bits += f.get_n_bits(bittype)
+        return n_bits
+
+    def get_fields_flat(self):
+        """Returns a flat list of all the fields in this register"""
+        fields = []
+        for f in self.fields:
+            fields += f.get_fields_flat()
+        return fields
+
+    def get_field_flat(self, linear_idx):
+        """Returns a specific field at a linear index position in
+        the flattened field list"""
+        fields_flat = self.get_fields_flat()
+        return fields_flat[linear_idx]
+
+    def get_n_fields_flat(self):
+        """Returns the number of fields contained in the flat field list"""
+        return len(self.get_fields_flat())
+
+    def get_regs_flat(self):
+        """Returns the a flatt list containing all
+        registers and subregisters"""
+        if isinstance(self.fields[0], Field):
+            return [self]
+        else:
+            regs = []
+            for r in self.fields:
+                regs += r.get_regs_flat()
+            return regs
+
+    def get_reg_flat(self, linear_index):
+        """Returns a specific register at a linear index position in
+        the flattened regiser list"""
+        regs_flat = self.get_regs_flat()
+        return regs_flat[linear_index]
+
+    def get_n_regs_flat(self):
+        """Returns the number of registers contained in
+        the flat register list"""
+        return len(self.get_regs_flat())
+
+    def get_nested_dims(self):
+        """Recursively get dimensions of nested registers (outputs a list)"""
+        dims = []
+        if self.is_multi_reg():
+            dims = self.fields[0].get_nested_dims()
+        return [len(self.fields)] + dims
+
+    def get_nested_params(self):
+        """Recursively get parameters of nested registers (outputs a list)"""
+        params = []
+        if self.is_multi_reg():
+            params = self.fields[0].get_nested_params()
+        return [self.param] + params
+
 
 class MultiReg(Reg):
     param = ""
-    regs = []
 
-    def __init__(self):
-        super.__init__(self)
+    def __init__(self, name):
+        Reg.__init__(self, name)
         self.param = ""
-        self.regs = []
+
+    def is_multi_reg(self):
+        """Returns true if this is a multireg"""
+        return True
 
 
 class Window():
@@ -101,3 +190,25 @@ class Block():
         self.wins = []
         self.blocks = []
         self.params = []
+
+    def get_regs_flat(self):
+        """Returns flattened register list
+        """
+        regs = []
+        for r in self.regs:
+            regs += r.get_regs_flat()
+        return regs
+
+    def get_n_bits(self, bittype=["q"]):
+        """Returns number of bits in this block (including all multiregs and
+        fields). By default this function counts read data bits (bittype "q"),
+        but other bits such as "d", qe", "re", "de" can be counted as well by
+        specifying them in the bittype list argument.
+        """
+        n_bits = 0
+        for r in self.regs:
+            n_bits += r.get_n_bits(bittype)
+        return n_bits
+
+    def get_n_regs_flat(self):
+        return len(self.get_regs_flat())
