a2ffb7160d467f63abe4580991ec88fd98786830
Miguel Osorio <miguelosorio@google.com>
2019-09-26 12:33:38 -0700

    [util/fpga] Update splice_nexysvideo.sh
    Update splice script to work with new Makefile scaffolding.

--- util/fpga/splice_nexysvideo.sh
+++ util/fpga/splice_nexysvideo.sh
@@ -12,17 +12,21 @@
 # Updated bitfile located : at the same place as raw vivado bitfile @
 # $REPO_TOP/build/lowrisc_systems_top_earlgrey_nexysvideo_0.1/synth-vivado/
 #  lowrisc_systems_top_earlgrey_nexysvideo_0.1.splice.bit
+set -e
 
+BUILD_DIR=build-fpga
+TARGET_PREFIX="sw/${BUILD_DIR}/rom"
+FPGA_BUILD_DIR=build/lowrisc_systems_top_earlgrey_nexysvideo_0.1/synth-vivado/
+FPGA_BIT_NAME=lowrisc_systems_top_earlgrey_nexysvideo_0.1
 
-PROGRAM=boot_rom
+make -C sw "SW_BUILD_DIR=${BUILD_DIR}" SW_DIR=boot_rom distclean all
 
-cd sw/boot_rom
-make clean ; make
-srec_cat ${PROGRAM}.bin -binary -offset 0x0 -o ${PROGRAM}.brammem \
+srec_cat ${TARGET_PREFIX}.bin -binary -offset 0x0 -o ${TARGET_PREFIX}.brammem \
   -vmem -Output_Block_Size 4;
-../../util/fpga/addr4x.py -i ${PROGRAM}.brammem -o ${PROGRAM}.mem
-updatemem -force --meminfo ../../util/fpga/bram_load.mmi --data ${PROGRAM}.mem \
-  --bit ../../build/lowrisc_systems_top_earlgrey_nexysvideo_0.1/synth-vivado/\
-lowrisc_systems_top_earlgrey_nexysvideo_0.1.bit  --proc dummy \
-  --out ../../build/lowrisc_systems_top_earlgrey_nexysvideo_0.1/synth-vivado/\
-lowrisc_systems_top_earlgrey_nexysvideo_0.1.splice.bit
+
+util/fpga/addr4x.py -i ${TARGET_PREFIX}.brammem -o ${TARGET_PREFIX}.mem
+
+updatemem -force --meminfo util/fpga/bram_load.mmi \
+  --data ${TARGET_PREFIX}.mem \
+  --bit "${FPGA_BUILD_DIR}/${FPGA_BIT_NAME}.bit"  --proc dummy \
+  --out "${FPGA_BUILD_DIR}/${FPGA_BIT_NAME}.splice.bit"
