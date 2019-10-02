f77f7c42f0e347ee90f88dc2f0a1ec6462f851d4
Weicai Yang <weicai@google.com>
2019-10-02 11:27:17 -0700

    [dv] Use 2 top opts in BUILD_OPTS
    XCelium requires to specify all the tops.
    Default tops are tb ${DUT_TOP}_bind

--- hw/dv/tools/rules.mk
+++ hw/dv/tools/rules.mk
@@ -3,6 +3,8 @@
 ## Licensed under the Apache License, Version 2.0, see LICENSE for details.                       ##
 ## SPDX-License-Identifier: Apache-2.0                                                            ##
 ####################################################################################################
+.DEFAULT_GOAL := all
+
 all: build run
 
 ########################
