0050715dde6b0d20dec2f2baabe797a3c48f8635
Cindy Chen <chencindy@google.com>
2019-10-02 13:59:36 -0700

    [dv/hmac] fix xcelium run time error
    Fix dpi-c error when malloc a memory size of 0

--- hw/ip/hmac/dv/cryptoc_dpi/cryptoc_dpi.c
+++ hw/ip/hmac/dv/cryptoc_dpi/cryptoc_dpi.c
@@ -36,17 +36,22 @@ extern void SHA256_hash_dpi(const svOpenArrayHandle msg, ull_t len,
   unsigned int *arr_ptr;
   ull_t i;
 
-  arr = (unsigned char *)malloc(len * sizeof(unsigned char));
-  arr_ptr = (unsigned int *)svGetArrayPtr(msg);
+  if (len > 0) {
+    arr = (unsigned char *)malloc(len * sizeof(unsigned char));
+    arr_ptr = (unsigned int *)svGetArrayPtr(msg);
 
-  for (i = 0; i < len; i++) {
-    arr[i] = arr_ptr[i];
+    for (i = 0; i < len; i++) {
+      arr[i] = arr_ptr[i];
+    }
+
+    // compute SHA256 hash
+    SHA256_hash(arr, len, hash);
+
+    free(arr);
+  } else {
+    // compute SHA256 hash when msg is empty
+    SHA256_hash(NULL, len, hash);
   }
-
-  // compute SHA256 hash
-  SHA256_hash(arr, len, hash);
-
-  free(arr);
 }
 
 extern void HMAC_SHA_dpi(const svOpenArrayHandle key, ull_t key_len,
@@ -88,23 +93,31 @@ extern void HMAC_SHA256_dpi(const svOpenArrayHandle key, ull_t key_len,
   unsigned int *key_arr_ptr;
   ull_t i;
 
-  msg_arr = (unsigned char *)malloc(msg_len * sizeof(unsigned char));
-  msg_arr_ptr = (unsigned int *)svGetArrayPtr(msg);
+  if (msg_len > 0) {
+    msg_arr = (unsigned char *)malloc(msg_len * sizeof(unsigned char));
+    msg_arr_ptr = (unsigned int *)svGetArrayPtr(msg);
+
+    for (i = 0; i < msg_len; i++) {
+      msg_arr[i] = msg_arr_ptr[i];
+    }
+  }
 
   key_arr = (unsigned char *)malloc(key_len * sizeof(unsigned char));
   key_arr_ptr = (unsigned int *)svGetArrayPtr(key);
-
-  for (i = 0; i < msg_len; i++) {
-    msg_arr[i] = msg_arr_ptr[i];
-  }
 
   for (i = 0; i < key_len; i++) {
     key_arr[i] = key_arr_ptr[i];
   }
 
-  // compute SHA256 hash
-  HMAC_SHA256(key_arr, key_len, msg_arr, msg_len, hmac);
+  if (msg_len > 0) {
+    // compute SHA256 hash
+    HMAC_SHA256(key_arr, key_len, msg_arr, msg_len, hmac);
 
-  free(msg_arr);
+    free(msg_arr);
+  } else {
+    // compute SHA256 hash when msg is empty
+    HMAC_SHA256(key_arr, key_len, NULL, msg_len, hmac);
+  }
+
   free(key_arr);
 }
