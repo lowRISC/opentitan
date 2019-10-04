d839d8180a018acc9d30c58d988bd6ccd8704055
Tobias Wölfel <tobias.woelfel@mailbox.org>
2019-10-04 12:50:12 +0200

    Add missing license headers

--- hw/ip/rv_core_ibex/dv/tests/core_ibex_base_test.sv
+++ hw/ip/rv_core_ibex/dv/tests/core_ibex_base_test.sv
@@ -1,3 +1,7 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 // TODO(taliu) extend this class with the common base test
 class core_ibex_base_test extends uvm_test;
 
