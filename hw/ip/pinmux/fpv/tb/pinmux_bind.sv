5802bd7bf2259573549895b6eee8de307adb933e
Michael Schaffner <msf@google.com>
2019-10-04 10:15:29 -0700

    [pinmux] Introduce symbolic variable to reduce SVA count

--- hw/ip/pinmux/fpv/tb/pinmux_bind.sv
+++ hw/ip/pinmux/fpv/tb/pinmux_bind.sv
@@ -6,6 +6,10 @@
 module pinmux_bind;
 
   bind pinmux pinmux_assert i_pinmux_assert (
+    // symbolic inputs for FPV
+    .periph_sel_i(pinmux_tb.periph_sel_i),
+    .mio_sel_i(pinmux_tb.mio_sel_i),
+    // normal inputs
     .*
   );
 
