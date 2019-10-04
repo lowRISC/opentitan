f1ea5bdbf552311dde87467f1c62322d6738d9f2
Tobias Wölfel <tobias.woelfel@mailbox.org>
2019-10-04 12:50:12 +0200

    Add missing license headers

--- sw/boot_rom/bootstrap.h
+++ sw/boot_rom/bootstrap.h
@@ -1,3 +1,7 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 #ifndef _F_BOOTSTRAP_H__
 #define _F_BOOTSTRAP_H__
 
