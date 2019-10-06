cbdda3feccf02fbc05e86e6b52a8100975fe2c4b
Pirmin Vogel <vogelpi@lowrisc.org>
2019-10-06 14:48:26 +0100

    [aes/rtl] Switch to new reggen version

--- hw/ip/aes/rtl/aes.sv
+++ hw/ip/aes/rtl/aes.sv
@@ -12,7 +12,9 @@ module aes #(
 
   // Bus Interface
   input  tlul_pkg::tl_h2d_t tl_i,
-  output tlul_pkg::tl_d2h_t tl_o
+  output tlul_pkg::tl_d2h_t tl_o,
+
+  input                     devmode_i
 );
 
   import aes_reg_pkg::*;
@@ -26,7 +28,8 @@ module aes #(
     .tl_i,
     .tl_o,
     .reg2hw,
-    .hw2reg
+    .hw2reg,
+    .devmode_i
   );
 
   aes_core #(
--- hw/ip/aes/rtl/aes_core.sv
+++ hw/ip/aes/rtl/aes_core.sv
@@ -68,25 +68,25 @@ module aes_core #(
   logic    [31:0] unused_data_out_q[4];
 
   // Inputs
-  assign key_init[0] = reg2hw.key0.q;
-  assign key_init[1] = reg2hw.key1.q;
-  assign key_init[2] = reg2hw.key2.q;
-  assign key_init[3] = reg2hw.key3.q;
-  assign key_init[4] = reg2hw.key4.q;
-  assign key_init[5] = reg2hw.key5.q;
-  assign key_init[6] = reg2hw.key6.q;
-  assign key_init[7] = reg2hw.key7.q;
+  assign key_init[0] = reg2hw.key[0].q;
+  assign key_init[1] = reg2hw.key[1].q;
+  assign key_init[2] = reg2hw.key[2].q;
+  assign key_init[3] = reg2hw.key[3].q;
+  assign key_init[4] = reg2hw.key[4].q;
+  assign key_init[5] = reg2hw.key[5].q;
+  assign key_init[6] = reg2hw.key[6].q;
+  assign key_init[7] = reg2hw.key[7].q;
 
-  assign key_init_qe = {reg2hw.key7.qe, reg2hw.key6.qe, reg2hw.key5.qe, reg2hw.key4.qe,
-                        reg2hw.key3.qe, reg2hw.key2.qe, reg2hw.key1.qe, reg2hw.key0.qe};
+  assign key_init_qe = {reg2hw.key[7].qe, reg2hw.key[6].qe, reg2hw.key[5].qe, reg2hw.key[4].qe,
+                        reg2hw.key[3].qe, reg2hw.key[2].qe, reg2hw.key[1].qe, reg2hw.key[0].qe};
 
-  assign data_in[0] = reg2hw.data_in0.q;
-  assign data_in[1] = reg2hw.data_in1.q;
-  assign data_in[2] = reg2hw.data_in2.q;
-  assign data_in[3] = reg2hw.data_in3.q;
+  assign data_in[0] = reg2hw.data_in[0].q;
+  assign data_in[1] = reg2hw.data_in[1].q;
+  assign data_in[2] = reg2hw.data_in[2].q;
+  assign data_in[3] = reg2hw.data_in[3].q;
 
-  assign data_in_qe = {reg2hw.data_in3.qe, reg2hw.data_in2.qe,
-                       reg2hw.data_in1.qe, reg2hw.data_in0.qe};
+  assign data_in_qe = {reg2hw.data_in[3].qe, reg2hw.data_in[2].qe,
+                       reg2hw.data_in[1].qe, reg2hw.data_in[0].qe};
 
   always_comb begin : conv_data_in_to_state
     for (int i=0; i<4; i++) begin
@@ -110,15 +110,15 @@ module aes_core #(
     endcase
   end
 
-  assign data_out_re = {reg2hw.data_out3.re, reg2hw.data_out2.re,
-                        reg2hw.data_out1.re, reg2hw.data_out0.re};
+  assign data_out_re = {reg2hw.data_out[3].re, reg2hw.data_out[2].re,
+                        reg2hw.data_out[1].re, reg2hw.data_out[0].re};
 
   // Unused inputs
   // data_out is actually hwo, but we need hrw for hwre
-  assign unused_data_out_q[0] = reg2hw.data_out0.q;
-  assign unused_data_out_q[1] = reg2hw.data_out1.q;
-  assign unused_data_out_q[2] = reg2hw.data_out2.q;
-  assign unused_data_out_q[3] = reg2hw.data_out3.q;
+  assign unused_data_out_q[0] = reg2hw.data_out[0].q;
+  assign unused_data_out_q[1] = reg2hw.data_out[1].q;
+  assign unused_data_out_q[2] = reg2hw.data_out[2].q;
+  assign unused_data_out_q[3] = reg2hw.data_out[3].q;
 
   // State registers
   always_comb begin : state_mux
@@ -347,10 +347,10 @@ module aes_core #(
   );
 
   // Outputs
-  assign hw2reg.data_out0.d = data_out_q[0];
-  assign hw2reg.data_out1.d = data_out_q[1];
-  assign hw2reg.data_out2.d = data_out_q[2];
-  assign hw2reg.data_out3.d = data_out_q[3];
+  assign hw2reg.data_out[0].d = data_out_q[0];
+  assign hw2reg.data_out[1].d = data_out_q[1];
+  assign hw2reg.data_out[2].d = data_out_q[2];
+  assign hw2reg.data_out[3].d = data_out_q[3];
 
   assign hw2reg.ctrl.key_len.d  = {key_len};
   assign hw2reg.ctrl.key_len.de = reg2hw.ctrl.key_len.qe;
