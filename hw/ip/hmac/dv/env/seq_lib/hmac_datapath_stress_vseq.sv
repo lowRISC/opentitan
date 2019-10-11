e17be7efbfd73ce8e200ba6aa5718b4da1171cd7
Cindy Chen <chencindy@google.com>
2019-10-11 15:23:37 -0700

    [dv/hmac] add datapath stress test
    Datapath stress test uses the least amount of message for 2 hash cycles.
    Thus the sequence always enable hmac with msg length = 1 byte.
    Add the datapath stress test to stress_all_vseq

---
+++ hw/ip/hmac/dv/env/seq_lib/hmac_datapath_stress_vseq.sv
@@ -0,0 +1,30 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// datapath_stress sequence will generate all message with size 1 and hmac_en
+// thus will have 2 hashes (one for key, one for msg) with the shortest message required
+// TODO: potentially use DV to check throughput here
+
+class hmac_datapath_stress_vseq extends hmac_sanity_vseq;
+  `uvm_object_utils(hmac_datapath_stress_vseq)
+  `uvm_object_new
+
+  rand int msg_size_base;
+
+  constraint msg_size_base_c {
+    msg_size_base dist {
+        0       :/1,
+        [1:156] :/1
+    };
+  }
+
+  constraint msg_c {
+    msg.size() == 1 + msg_size_base * HMAC_HASH_SIZE;
+  }
+
+  constraint hmac_en_c {
+    hmac_en == 1;
+  }
+
+endclass : hmac_datapath_stress_vseq
--- hw/ip/hmac/dv/env/seq_lib/hmac_vseq_list.sv
+++ hw/ip/hmac/dv/env/seq_lib/hmac_vseq_list.sv
@@ -10,5 +10,6 @@
 `include "hmac_back_pressure_vseq.sv"
 `include "hmac_burst_wr_vseq.sv"
 `include "hmac_common_vseq.sv"
+`include "hmac_datapath_stress_vseq.sv"
 `include "hmac_stress_all_vseq.sv"
 `include "hmac_reset_vseq.sv"
