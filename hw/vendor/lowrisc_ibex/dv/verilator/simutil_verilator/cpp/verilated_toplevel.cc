56ebc204289e5ff4e0c3b62338fe5d2e96e968e8
Timothy Chen <timothytim@google.com>
2019-10-21 11:00:12 -0700

    [ibex, verilator] Update ibex vendor script for verilator re-use
    - Vendor in ibex verilator setup files for top_earlgrey re-use

---
+++ hw/vendor/lowrisc_ibex/dv/verilator/simutil_verilator/cpp/verilated_toplevel.cc
@@ -0,0 +1,12 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "verilated_toplevel.h"
+
+TOPLEVEL_NAME &VerilatedToplevel::dut() {
+  // The static_cast below is generally unsafe, but we know the types involved.
+  // It's safe for these.
+  TOPLEVEL_NAME &dut = static_cast<TOPLEVEL_NAME&>(*this);
+  return dut;
+}
