838daa0c529de6fe6303b9c4e40802ef6d8bec0c
Tim Shepard <tjshep@google.com>
2019-10-25 18:49:29 -0400

    [sw/host/vendor/mpsse] Use util/vendor_hw.py for mpsse
    Add sw/host/vendor/mpsse.vendor.hjson and patches in
    sw/host/vendor/patches/mpsse to use util/vendor_hw.py script to
    replace the previous version of mpsse that was copied in by hand.
    
    Commit message auto-generated by util/vendor_hw.py -c below:
    
    Update mpsse to e729a6148
    
    Update code from subdir trunks/ftdi in upstream repository
    https://chromium.googlesource.com/chromiumos/platform2/ to revision
    e729a61489259d1bf34e6df7f8e284febf02da2e

--- sw/host/vendor/mpsse/mpsse.h
+++ sw/host/vendor/mpsse/mpsse.h
@@ -1,51 +1,62 @@
 /*
- Copyright (C) 2015 The Android Open Source Project
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
- /*
+ *Copyright (C) 2015 The Android Open Source Project
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
+ *
  * This file was copied from https://github.com/devttys0/libmpsse.git (sha1
  * f1a6744b), and modified to suite the Chromium OS project.
  */
+
 #ifndef TRUNKS_FTDI_MPSSE_H_
 #define TRUNKS_FTDI_MPSSE_H_
+
 #include <libftdi1/ftdi.h>
 #include <stdint.h>
+
 #define MPSSE_OK 0
 #define MPSSE_FAIL -1
+
 #define MSB 0x00
 #define LSB 0x08
+
 #define CHUNK_SIZE 65535
 #define SPI_RW_SIZE (63 * 1024)
 #define SPI_TRANSFER_SIZE 512
 #define I2C_TRANSFER_SIZE 64
+
 #define LATENCY_MS 2
 #define TIMEOUT_DIVISOR 1000000
 #define USB_TIMEOUT 120000
 #define SETUP_DELAY 25000
+
 #define BITMODE_RESET 0
 #define BITMODE_MPSSE 2
+
 #define CMD_SIZE 3
 #define MAX_SETUP_COMMANDS 10
 #define SS_TX_COUNT 3
+
 #define LOW 0
 #define HIGH 1
 #define NUM_GPIOL_PINS 4
 #define NUM_GPIO_PINS 12
+
 #define NULL_CONTEXT_ERROR_MSG "NULL MPSSE context pointer!"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 /* FTDI interfaces */
 enum interface {
   IFACE_ANY = INTERFACE_ANY,
@@ -54,6 +65,7 @@ enum interface {
   IFACE_C = INTERFACE_C,
   IFACE_D = INTERFACE_D
 };
+
 /* Common clock rates */
 enum clock_rates {
   ONE_HUNDRED_KHZ = 100000,
@@ -68,6 +80,7 @@ enum clock_rates {
   THIRTY_MHZ = 30000000,
   SIXTY_MHZ = 60000000
 };
+
 /* Supported MPSSE modes */
 enum modes {
   SPI0 = 1,
@@ -78,6 +91,7 @@ enum modes {
   GPIO = 6,
   BITBANG = 7,
 };
+
 enum pins {
   SK = 1,
   DO = 2,
@@ -88,6 +102,7 @@ enum pins {
   GPIO2 = 64,
   GPIO3 = 128
 };
+
 enum gpio_pins {
   GPIOL0 = 0,
   GPIOL1 = 1,
@@ -102,10 +117,13 @@ enum gpio_pins {
   GPIOH6 = 10,
   GPIOH7 = 11
 };
+
 enum i2c_ack { ACK = 0, NACK = 1 };
+
 /* SK/DO/CS and GPIOs are outputs, DI is an input */
 #define DEFAULT_TRIS (SK | DO | CS | GPIO0 | GPIO1 | GPIO2 | GPIO3)
 #define DEFAULT_PORT (SK | CS) /* SK and CS are high, all others low */
+
 enum mpsse_commands {
   INVALID_COMMAND = 0xAB,
   ENABLE_ADAPTIVE_CLOCK = 0x96,
@@ -122,14 +140,17 @@ enum mpsse_commands {
   CLOCK_N8_CYCLES_IO_LOW = 0x9D,
   TRISTATE_IO = 0x9E,
 };
+
 enum low_bits_status { STARTED, STOPPED };
+
 struct vid_pid {
   int vid;
   int pid;
-  const char* description;
+  char* description;
 };
+
 struct mpsse_context {
-  const char* description;
+  char* description;
   struct ftdi_context ftdi;
   enum modes mode;
   enum low_bits_status status;
@@ -153,6 +174,7 @@ struct mpsse_context {
   uint8_t tack;
   uint8_t rack;
 };
+
 struct mpsse_context* MPSSE(enum modes mode, int freq, int endianess);
 struct mpsse_context* Open(int vid,
                            int pid,
@@ -200,17 +222,19 @@ int ReadPins(struct mpsse_context* mpsse);
 int PinState(struct mpsse_context* mpsse, int pin, int state);
 int Tristate(struct mpsse_context* mpsse);
 char Version(void);
+
 #ifdef SWIGPYTHON
 typedef struct swig_string_data {
   int size;
   char* data;
 } swig_string_data;
+
 swig_string_data Read(struct mpsse_context* mpsse, int size);
 swig_string_data Transfer(struct mpsse_context* mpsse, char* data, int size);
 #else
 uint8_t* Read(struct mpsse_context* mpsse, int size);
-uint8_t* Transfer(struct mpsse_context* mpsse,
-                        uint8_t* data, int size);
+uint8_t* Transfer(struct mpsse_context* mpsse, uint8_t* data, int size);
+
 int FastWrite(struct mpsse_context* mpsse, char* data, int size);
 int FastRead(struct mpsse_context* mpsse, char* data, int size);
 int FastTransfer(struct mpsse_context* mpsse,
@@ -221,4 +245,4 @@ int FastTransfer(struct mpsse_context* mpsse,
 #ifdef __cplusplus
 }
 #endif
-#endif  /* TRUNKS_FTDI_MPSSE_H_ */
+#endif /* TRUNKS_FTDI_MPSSE_H_ */
