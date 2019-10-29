92e4298f8c2de268b2420a2c16939cd0784f1bf8
Gaurang Chitroda <gaurangg@google.com>
2019-10-29 14:58:19 -0700

    [gpio/dv] gpio filter stress test with asynchronous driving

--- hw/ip/gpio/dv/env/seq_lib/gpio_vseq_list.sv
+++ hw/ip/gpio/dv/env/seq_lib/gpio_vseq_list.sv
@@ -10,5 +10,6 @@
 `include "gpio_intr_rand_pgm_vseq.sv"
 `include "gpio_rand_intr_trigger_vseq.sv"
 `include "gpio_intr_with_filter_rand_intr_event_vseq.sv"
+`include "gpio_filter_stress_vseq.sv"
 `include "gpio_random_long_reg_writes_reg_reads_vseq.sv"
 `include "gpio_full_random_vseq.sv"
