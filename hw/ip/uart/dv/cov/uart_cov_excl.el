04bb36e0ae1430262b048d400102b0fed43377ac
Weicai Yang <weicai@google.com>
2019-10-31 22:06:11 -0700

    [uart/dv] Update coverage exclusion file as design change

--- hw/ip/uart/dv/cov/uart_cov_excl.el
+++ hw/ip/uart/dv/cov/uart_cov_excl.el
@@ -3,16 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Coverage exclusion file for uart
-CHECKSUM: "1115183444 4229339"
+ANNOTATION: "[UNR] default branch"
+CHECKSUM: "2856317324 3065048585"
 INSTANCE: tb.dut.uart_core
-// this is unreachable condition
-Condition 16 "7100374" "(break_err && (break_st == BRK_CHK)) 1 -1" (2 "10")
-CHECKSUM: "1115183444 3548154843"
-INSTANCE: tb.dut.uart_core
-// these are scanmode ralated branches
-Branch 1 "2534752149" "scanmode_i" (0) "scanmode_i 1"
-Branch 5 "2534752149" "scanmode_i" (0) "scanmode_i 1"
-Branch 1 "2534752149" "scanmode_i" (1) "scanmode_i 0"
-Branch 5 "2534752149" "scanmode_i" (1) "scanmode_i 0"
-// default branch, it's unreachable
-Branch 9 "221332935" "(!rst_ni)" (5) "(!rst_ni) 0,default,-,-"
+Branch 7 "2483258407" "(!rst_ni)" (5) "(!rst_ni) 0,default,-,-"
