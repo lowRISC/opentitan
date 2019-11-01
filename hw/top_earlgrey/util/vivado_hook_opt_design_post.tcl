196dca6a3b7a84fcc5af1ee870080ee6aaf29448
Ram Penugonda <rampenugonda@google.com>
2019-11-01 15:58:24 -0700

    [fpga] Addressing minor nits missed in earlier commit b95a71c
    	modified:   hw/top_earlgrey/util/vivado_hook_opt_design_post.tcl
    
    	modified:   hw/top_earlgrey/util/vivado_hook_opt_design_post.tcl

--- hw/top_earlgrey/util/vivado_hook_opt_design_post.tcl
+++ hw/top_earlgrey/util/vivado_hook_opt_design_post.tcl
@@ -6,11 +6,11 @@
 # 1. hw/top_earlgrey/data/placement.xdc and
 # 2. hw/top_earlgrey/util/vivado_hook_opt_design_post.tcl
 
-send_msg "Designcheck 1-1" INFO "Checking if ROM memory is mapped to BRAM memory."
+send_msg "Designcheck 2-1" INFO "Checking if ROM memory is mapped to BRAM memory."
 
 if {[catch [get_cells -hierarchical -filter { NAME =~  "*rom_rom*dout_o_reg_0" && PRIMITIVE_TYPE =~ BMEM.*.* }]]\
- && [catch [get_cells -hierarchical -filter { NAME =~  "*rom_rom*dout_o_reg_1" && PRIMITIVE_TYPE =~ BMEM.*.* }]] } {
-  send_msg "Designcheck 1-3" INFO "BRAM implementation found for ROM memory."
+&& [catch [get_cells -hierarchical -filter { NAME =~  "*rom_rom*dout_o_reg_1" && PRIMITIVE_TYPE =~ BMEM.*.* }]] } {
+  send_msg "Designcheck 2-2" INFO "BRAM implementation found for ROM memory."
 } else {
-  send_msg "Designcheck 1-2" ERROR "BRAM implementation not found for ROM memory."
+  send_msg "Designcheck 2-3" ERROR "BRAM implementation not found for ROM memory."
 }
