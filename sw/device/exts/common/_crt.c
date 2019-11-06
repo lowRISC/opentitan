03ade34ff554f3acacd4bb462fe800e6aca4ff21
Alistair Francis <alistair.francis@wdc.com>
2019-11-06 11:40:44 -0800

    [sw/device] Remove redundant #include <string.h>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- sw/device/exts/common/_crt.c
+++ sw/device/exts/common/_crt.c
@@ -2,8 +2,6 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include <string.h>
-
 #include "sw/device/lib/common.h"
 #include "sw/device/lib/irq.h"
 
