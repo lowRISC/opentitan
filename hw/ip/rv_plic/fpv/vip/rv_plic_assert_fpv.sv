dd435f08212c1bd9185cc8615a98e4f4182ca65e
Cindy Chen <chencindy@google.com>
2019-11-07 13:21:05 -0800

    [rv_plic/fpv] Optimize the assertion direct path
    Change the method from directly probing design path to bind file
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/ip/rv_plic/fpv/vip/rv_plic_assert_fpv.sv
+++ hw/ip/rv_plic/fpv/vip/rv_plic_assert_fpv.sv
@@ -9,10 +9,18 @@ module rv_plic_assert_fpv import rv_plic_reg_pkg::*; (
   input [NumSrc-1:0] intr_src_i,
   input [NumTarget-1:0] irq_o,
   input [$clog2(NumSrc+1)-1:0] irq_id_o [NumTarget],
-  input [NumTarget-1:0] msip_o
+  input [NumTarget-1:0] msip_o,
+  // probe design signals
+  input [NumSrc-1:0] ip,
+  input [NumSrc-1:0] le,
+  input [NumSrc-1:0] ie [NumTarget],
+  input [NumSrc-1:0] claim,
+  input [NumSrc-1:0] complete,
+  input [1:0]        prio [NumSrc], // TODO: need a way to automatically know the size
+  input [1:0]        threshold [NumTarget]
 );
 
-  logic claim, claimed;
+  logic claim_reg, claimed;
   logic max_priority;
   logic irq;
   logic [$clog2(NumSrc+1)-1:0] i_high_prio;
@@ -28,23 +36,22 @@ module rv_plic_assert_fpv import rv_plic_reg_pkg::*; (
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
-      claim <= 1'b0;
-    end else if (rv_plic.claim[src_sel]) begin
-      claim <= 1'b1;
-    end else if (rv_plic.complete[src_sel]) begin
-      claim <= 1'b0;
+      claim_reg <= 1'b0;
+    end else if (claim[src_sel]) begin
+      claim_reg <= 1'b1;
+    end else if (complete[src_sel]) begin
+      claim_reg <= 1'b0;
     end
   end
 
-  assign claimed = claim || rv_plic.claim[src_sel];
+  assign claimed = claim_reg || claim[src_sel];
 
   always_comb begin
     max_priority = 1'b1;
     for (int i = 0; i < NumSrc; i++) begin
       // conditions that if src_sel has the highest priority with the lowest ID
-      if (i != src_sel && rv_plic.ip[i] && rv_plic.ie[tgt_sel][i] &&
-          (rv_plic.prio[i] > rv_plic.prio[src_sel] ||
-           (rv_plic.prio[i] == rv_plic.prio[src_sel] && i < src_sel))) begin
+      if (i != src_sel && ip[i] && ie[tgt_sel][i] &&
+            (prio[i] > prio[src_sel] || (prio[i] == prio[src_sel] && i < src_sel))) begin
         max_priority = 1'b0;
         break;
       end
@@ -54,41 +61,38 @@ module rv_plic_assert_fpv import rv_plic_reg_pkg::*; (
   always_comb begin
     automatic logic [31:0] max_prio = 0;
     for (int i = NumSrc-1; i >= 0; i--) begin
-      if (rv_plic.ip[i] && rv_plic.ie[tgt_sel][i] && rv_plic.prio[i] >= max_prio) begin
-        max_prio = rv_plic.prio[i];
+      if (ip[i] && ie[tgt_sel][i] && prio[i] >= max_prio) begin
+        max_prio = prio[i];
         i_high_prio = i; // i is the smallest id if have IPs with the same priority
       end
     end
-    if (max_prio > rv_plic.threshold[tgt_sel]) irq = 1'b1;
+    if (max_prio > threshold[tgt_sel]) irq = 1'b1;
     else irq = 1'b0;
   end
 
   // when IP is set, previous cycle should follow edge or level triggered criteria
-  `ASSERT(LevelTriggeredIp_A, $rose(rv_plic.ip[src_sel]) |->
-          $past(rv_plic.le[src_sel]) || $past(intr_src_i[src_sel]), clk_i, !rst_ni)
+  `ASSERT(LevelTriggeredIp_A, $rose(ip[src_sel]) |->
+          $past(le[src_sel]) || $past(intr_src_i[src_sel]), clk_i, !rst_ni)
 
-  `ASSERT(EdgeTriggeredIp_A, $rose(rv_plic.ip[src_sel]) |->
-          !$past(rv_plic.le[src_sel]) || $rose($past(intr_src_i[src_sel])), clk_i, !rst_ni)
+  `ASSERT(EdgeTriggeredIp_A, $rose(ip[src_sel]) |->
+          !$past(le[src_sel]) || $rose($past(intr_src_i[src_sel])), clk_i, !rst_ni)
 
   // when interrupt is trigger, and nothing claimed yet, then next cycle should assert IP.
-  `ASSERT(LevelTriggeredIpWithClaim_A, !rv_plic.le[src_sel] && intr_src_i[src_sel] && !claimed |=>
-          rv_plic.ip[src_sel], clk_i, !rst_ni)
+  `ASSERT(LevelTriggeredIpWithClaim_A, !le[src_sel] && intr_src_i[src_sel] && !claimed |=>
+          ip[src_sel], clk_i, !rst_ni)
 
-  `ASSERT(EdgeTriggeredIpWithClaim_A, rv_plic.le[src_sel] && $rose(intr_src_i[src_sel]) && !claimed |=>
-          rv_plic.ip[src_sel], clk_i, !rst_ni)
+  `ASSERT(EdgeTriggeredIpWithClaim_A, le[src_sel] && $rose(intr_src_i[src_sel]) && !claimed |=>
+          ip[src_sel], clk_i, !rst_ni)
 
   // ip stays stable until claimed, reset to 0 after claimed, and stays 0 until complete
-  `ASSERT(IpStableAfterTriggered_A, rv_plic.ip[src_sel] && !claimed |=>
-          rv_plic.ip[src_sel], clk_i, !rst_ni)
-  `ASSERT(IpClearAfterClaim_A, rv_plic.ip[src_sel] && rv_plic.claim[src_sel] |=>
-          !rv_plic.ip[src_sel], clk_i, !rst_ni)
-  `ASSERT(IpStableAfterClaimed_A, claimed |=> !rv_plic.ip[src_sel], clk_i, !rst_ni)
+  `ASSERT(IpStableAfterTriggered_A, ip[src_sel] && !claimed  |=> ip[src_sel], clk_i, !rst_ni)
+  `ASSERT(IpClearAfterClaim_A, ip[src_sel] && claim[src_sel] |=> !ip[src_sel], clk_i, !rst_ni)
+  `ASSERT(IpStableAfterClaimed_A, claimed |=> !ip[src_sel], clk_i, !rst_ni)
 
   // when ip is set and priority is the largest and above threshold, and interrupt enable is set,
   // assertion irq_o at next cycle
-  `ASSERT(TriggerIrqForwardCheck_A, rv_plic.ip[src_sel] &&
-          rv_plic.prio[src_sel] > rv_plic.threshold[tgt_sel] &&
-          max_priority && rv_plic.ie[tgt_sel][src_sel] |=> irq_o[tgt_sel], clk_i, !rst_ni)
+  `ASSERT(TriggerIrqForwardCheck_A, ip[src_sel] && prio[src_sel] > threshold[tgt_sel] &&
+          max_priority && ie[tgt_sel][src_sel] |=> irq_o[tgt_sel], clk_i, !rst_ni)
 
   `ASSERT(TriggerIrqBackwardCheck_A, $rose(irq_o[tgt_sel]) |->
           $past(irq) && (irq_id_o[tgt_sel] - 1) == $past(i_high_prio), clk_i, !rst_ni)
