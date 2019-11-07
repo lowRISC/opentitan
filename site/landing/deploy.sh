bae27161ee1b8455d3bc83ae98be51b70ab24ed1
Garret Kelly <gdk@google.com>
2019-11-07 14:32:49 -0500

    [site] Add landing page sources
    Add the entirety of the landing page sources and update the deployment
    script to not try to minify the site before deployment.
    
    Signed-off-by: Garret Kelly <gdk@google.com>

--- site/landing/deploy.sh
+++ site/landing/deploy.sh
@@ -21,12 +21,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 rm -rf public
 
-hugo --minify
-hugo deploy --target "${TARGET}" --confirm --verbose --maxDeletes -1
-
-if [ "${TARGET}" = "public" ]; then
-  # TODO: The ability to invalidate a Cloud CDN cache is pending in upstream
-  # hugo.  Once it's landed this can be rolled into the hugo configuration.
-  gcloud compute url-maps invalidate-cdn-cache "opentitan-dot-org" --path="/*" \
-    --async
-fi
+hugo
+hugo deploy --target "${TARGET}" --confirm --verbose --maxDeletes -1 --invalidateCDN
