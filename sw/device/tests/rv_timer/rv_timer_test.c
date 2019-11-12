64c7dad7e1dc1096343960ffda91a39f4b76c639
Alistair Francis <alistair.francis@wdc.com>
2019-11-12 11:30:40 -0800

    [sw/device] Remove redundant #include <stdlib.h>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- sw/device/tests/rv_timer/rv_timer_test.c
+++ sw/device/tests/rv_timer/rv_timer_test.c
@@ -2,8 +2,6 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include <stdlib.h>
-
 #include "sw/device/lib/common.h"
 #include "sw/device/lib/irq.h"
 #include "sw/device/lib/rv_timer.h"
