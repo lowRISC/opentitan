4212e835c4f0ec25f7b2fc72274e5d987a263345
Eunchan Kim <eunchan@opentitan.org>
2019-11-15 14:48:42 -0800

    [reggen] Change register structure for multi register
    As discussed in #270, `multireg` structure is changed.
    This commit is to show the example of reggen API 2.
    
    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>

--- util/reggen/gen_html.py
+++ util/reggen/gen_html.py
@@ -166,8 +166,8 @@ def gen_html_register(outfile, reg, comp, width, rnames, toc, toclvl):
         outfile, "<table class=\"regdef\" id=\"Reg_" + rname.lower() + "\">\n"
         "<tr><th class=\"regdef\" colspan=5>" + comp + "." + rname + " @ + " +
         hex(offset) + "<br>" + desc_expand(reg['desc'], rnames) + "<br>" +
-        "Reset default = " + hex(reg['genresval']) + ", mask " + hex(
-            reg['genresmask']) + regwen_string + "</th></tr>\n")
+        "Reset default = " + hex(reg['genresval']) + ", mask " +
+        hex(reg['genresmask']) + regwen_string + "</th></tr>\n")
     if toc != None:
         toc.append((toclvl, comp + "." + rname, "Reg_" + rname.lower()))
     genout(outfile, "<tr><td colspan=5>")
@@ -214,14 +214,14 @@ def gen_html_register(outfile, reg, comp, width, rnames, toc, toclvl):
         genout(outfile, "<tr><td class=\"regbits\">" + field['bits'] + "</td>")
         genout(outfile, "<td class=\"regperm\">" + field['swaccess'] + "</td>")
         genout(
-            outfile,
-            "<td class=\"regrv\">" + ('x' if field['genresvalx'] else hex(
-                field['genresval'])) + "</td>")
+            outfile, "<td class=\"regrv\">" +
+            ('x' if field['genresvalx'] else hex(field['genresval'])) +
+            "</td>")
         genout(outfile, "<td class=\"regfn\">" + fname + "</td>")
         if 'desc' in field:
             genout(
-                outfile, "<td class=\"regde\">" + desc_expand(
-                    field['desc'], rnames) + "\n")
+                outfile, "<td class=\"regde\">" +
+                desc_expand(field['desc'], rnames) + "\n")
         else:
             genout(outfile, "<td>\n")
 
@@ -258,8 +258,8 @@ def gen_html_window(outfile, win, comp, regwidth, rnames, toc, toclvl):
     offset = win['genoffset']
     genout(
         outfile, '<table class="regdef" id="Reg_' + wname.lower() + '">\n'
-        '<tr><th class="regdef">' + comp + '.' + wname + ' @ + ' + hex(offset)
-        + '<br>' + win['items'] + ' item ' + win['swaccess'] +
+        '<tr><th class="regdef">' + comp + '.' + wname + ' @ + ' +
+        hex(offset) + '<br>' + win['items'] + ' item ' + win['swaccess'] +
         ' window<br>Byte writes are ' +
         ('' if win['genbyte-write'] else '<i>not</i> ') +
         'supported</th></tr>\n')
