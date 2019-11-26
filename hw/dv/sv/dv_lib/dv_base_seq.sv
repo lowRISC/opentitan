284dbeb48a19a46502fceb33e86f2a958c40ca6c
Gaurang Chitroda <gaurangg@google.com>
2019-11-26 14:14:47 -0800

    [dv/spi_agent] Extend spi agent from dv_lib instead of uvm lib
    Signed-off-by: Gaurang Chitroda <gaurangg@google.com>

--- hw/dv/sv/dv_lib/dv_base_seq.sv
+++ hw/dv/sv/dv_lib/dv_base_seq.sv
@@ -2,9 +2,11 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-class dv_base_seq #(type CFG_T       = dv_base_agent_cfg,
-                    type SEQUENCER_T = dv_base_sequencer) extends uvm_sequence;
-  `uvm_object_param_utils(dv_base_seq #(CFG_T, SEQUENCER_T))
+class dv_base_seq #(type REQ         = uvm_sequence_item,
+                    type RSP         = REQ,
+                    type CFG_T       = dv_base_agent_cfg,
+                    type SEQUENCER_T = dv_base_sequencer) extends uvm_sequence#(REQ, RSP);
+  `uvm_object_param_utils(dv_base_seq #(REQ, RSP, CFG_T, SEQUENCER_T))
   `uvm_declare_p_sequencer(SEQUENCER_T)
 
   CFG_T cfg;
--- hw/dv/sv/spi_agent/spi_agent.sv
+++ hw/dv/sv/spi_agent/spi_agent.sv
@@ -2,46 +2,25 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-class spi_agent extends uvm_agent;
+class spi_agent extends dv_base_agent#(
+    .CFG_T          (spi_agent_cfg),
+    .DRIVER_T       (spi_driver),
+    .HOST_DRIVER_T  (spi_host_driver),
+    .DEVICE_DRIVER_T(spi_device_driver),
+    .SEQUENCER_T    (spi_sequencer),
+    .MONITOR_T      (spi_monitor),
+    .COV_T          (spi_agent_cov)
+  );
   `uvm_component_utils(spi_agent)
-
-  spi_agent_cfg  cfg;
-  spi_driver     driver;
-  spi_sequencer  sequencer;
-  spi_monitor    monitor;
 
   `uvm_component_new
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    // get spi_agent_cfg object from uvm_config_db
-    if (!uvm_config_db#(spi_agent_cfg)::get(this, "", "cfg", cfg))
-      `uvm_fatal(`gfn, "failed to get spi_agent_cfg object from uvm_config_db")
 
     // get spi_if handle
     if (!uvm_config_db#(virtual spi_if)::get(this, "", "vif", cfg.vif))
       `uvm_fatal(`gfn, "failed to get spi_if handle from uvm_config_db")
-
-    // create components
-    monitor = spi_monitor::type_id::create("monitor", this);
-    monitor.cfg = cfg;
-    if (cfg.is_active) begin
-      sequencer = spi_sequencer::type_id::create("sequencer", this);
-      sequencer.cfg = cfg;
-      if (cfg.mode == Host) begin
-        driver = spi_host_driver::type_id::create("driver", this);
-      end else begin
-        driver = spi_device_driver::type_id::create("driver", this);
-      end
-      driver.cfg = cfg;
-    end
-  endfunction
-
-  function void connect_phase(uvm_phase phase);
-    super.connect_phase(phase);
-    if (cfg.is_active) begin
-      driver.seq_item_port.connect(sequencer.seq_item_export);
-    end
   endfunction
 
 endclass
