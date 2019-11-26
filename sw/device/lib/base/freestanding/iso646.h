fa12ce0845c7ff61cb22052be72b495d73ee7535
Miguel Young de la Sota <mcyoung@google.com>
2019-11-26 14:53:55 -0600

    [sw] Provide our own freestanding C headers.
    Since these headers implement C11, this change bumps the -std flag
    passed to GCC/Clang to c11.
    
    This change also sets up Meson and Make to ignore all system/compiler
    headers except for those we provide.
    
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

---
+++ sw/device/lib/base/freestanding/iso646.h
@@ -0,0 +1,38 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_BASE_FREESTANDING_ISO646_H_
+#define OPENTITAN_SW_DEVICE_LIB_BASE_FREESTANDING_ISO646_H_
+
+/**
+ * This header implements the iso646.h standard header, as required by C11 S4p6.
+ * See S7.9 of the same for a description.
+ *
+ * This file is provided only for standards compliance reason; including this
+ * file is a style violation.
+ */
+
+// The below macro definitions cause clang-format to freak out somewhat, so we
+// need to turn it off.
+// clang-format off
+
+#define and &&
+#define and_eq &=
+#define bitand &
+
+#define or ||
+#define or_eq |=
+#define bitor |
+
+#define xor ^
+#define xor_eq ^=
+
+#define not !
+#define not_eq !=
+
+#define compl ~
+
+// clang-format on
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_BASE_FREESTANDING_ISO646_H_
---
+++ sw/device/lib/base/freestanding/stdbool.h
@@ -0,0 +1,19 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_BASE_FREESTANDING_STDBOOL_H_
+#define OPENTITAN_SW_DEVICE_LIB_BASE_FREESTANDING_STDBOOL_H_
+
+/**
+ * This header implements the stdbool.h standard header, as required by C11
+ * S4p6. This header is specified in detail in S7.18 of the same.
+ */
+
+#define bool _Bool
+#define true 1
+#define false 0
+
+#define __bool_true_false_are_defined 1
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_BASE_FREESTANDING_STDBOOL_H_
