09fb9921d1e69c96851af855479d5b5f164564c0
Miguel Young de la Sota <mcyoung@google.com>
2019-12-05 16:00:20 -0600

    [sw] Remove Make as a SW build system.
    This change is the culmination of a month or so of work beginning, in
    commit d258b33, to make all building of OpenTitan software happen
    with Meson, rather than Make.
    
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

--- sw/device/examples/hello_world/srcs.mk
+++
@@ -1,5 +0,0 @@
-# Copyright lowRISC contributors.
-# Licensed under the Apache License, Version 2.0, see LICENSE for details.
-# SPDX-License-Identifier: Apache-2.0
-
-SW_SRCS += $(SW_DIR)/hello_world.c
