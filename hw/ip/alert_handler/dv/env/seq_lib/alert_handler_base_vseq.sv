5febc3b5b66389081f6424d850e450c1ff338373
Cindy Chen <chencindy@google.com>
2019-12-17 14:35:01 -0800

    [dv/alert_handler] add sanity test
    connect alert_handler agent on the alert sender side
    enable one alert and let it go through interrupt and escalation
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/ip/alert_handler/dv/env/seq_lib/alert_handler_base_vseq.sv
+++ hw/ip/alert_handler/dv/env/seq_lib/alert_handler_base_vseq.sv
@@ -11,7 +11,7 @@ class alert_handler_base_vseq extends cip_base_vseq #(
   `uvm_object_utils(alert_handler_base_vseq)
 
   // various knobs to enable certain routines
-  bit do_alert_handler_init = 1'b1;
+  bit do_alert_handler_init = 1'b0;
 
   `uvm_object_new
 
@@ -25,8 +25,29 @@ class alert_handler_base_vseq extends cip_base_vseq #(
   endtask
 
   // setup basic alert_handler features
-  virtual task alert_handler_init();
-    // `uvm_error(`gfn, "FIXME")
+  virtual task alert_handler_init(bit [NUM_ALERT_HANDLER_CLASSES-1:0] intr_en = '1,
+                                  bit                                 alert_en = 1'b1,
+                                  bit [TL_DW-1:0]                     alert_class = 'he4,
+                                  bit [TL_DW-1:0]                     classA_ctrl = 'h393d);
+    // TODO: cfg_interrupts does not disable interrupt, need debug
+    //cfg_interrupts(.interrupts(interrupts), .enable(1'b1));
+    csr_wr(.csr(ral.intr_enable), .value(intr_en));
+    ral.alert_en.set(alert_en);
+    ral.alert_class.set(alert_class);
+    ral.classa_ctrl.set(classA_ctrl);
+    csr_update(.csr(ral.alert_en));
+    csr_update(.csr(ral.alert_class));
+    csr_update(.csr(ral.classa_ctrl));
   endtask
 
+  virtual task drive_alert(int alert_index);
+    alert_sender_seq alert_seq;
+    `uvm_create_on(alert_seq, p_sequencer.alert_host_seqr_h[alert_index]);
+    `DV_CHECK_RANDOMIZE_FATAL(alert_seq)
+    `uvm_send(alert_seq)
+  endtask
+
+  virtual task clear_esc();
+    csr_wr(.csr(ral.classa_clr), .value(1));
+  endtask
 endclass : alert_handler_base_vseq
