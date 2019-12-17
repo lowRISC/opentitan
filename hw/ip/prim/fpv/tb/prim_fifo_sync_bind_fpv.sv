8d13db89d765410421402419d0b9713adca05b42
Michael Schaffner <msf@opentitan.org>
2019-12-17 12:32:12 -0800

    [fifo_sync/fpv] Make bind files unique and explicitly override params
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/prim/fpv/tb/prim_fifo_sync_bind_fpv.sv
+++ hw/ip/prim/fpv/tb/prim_fifo_sync_bind_fpv.sv
@@ -5,6 +5,8 @@
 
 module prim_fifo_sync_bind_fpv;
 
+  localparam int unsigned Width = 4;
+
   // need to instantiate by hand since bind statements inside
   // generate blocks are currently not supported
 
@@ -13,9 +15,11 @@ module prim_fifo_sync_bind_fpv;
   ////////////////////
 
   bind i_nopass1 prim_fifo_sync_assert_fpv #(
+    .Width(Width),
     .Pass(1'b0),
-    .Depth(1)
-  ) i_prim_fifo_sync_assert_fpv (
+    .Depth(1),
+    .EnableDataCheck(1'b1)
+  ) i_nopass1_assert_fpv (
     .clk_i,
     .rst_ni,
     .clr_i,
@@ -29,9 +33,11 @@ module prim_fifo_sync_bind_fpv;
   );
 
   bind i_nopass7 prim_fifo_sync_assert_fpv #(
+    .Width(Width),
     .Pass(1'b0),
-    .Depth(7)
-  ) i_prim_fifo_sync_assert_fpv (
+    .Depth(7),
+    .EnableDataCheck(1'b1)
+  ) i_nopass7_assert_fpv (
     .clk_i,
     .rst_ni,
     .clr_i,
@@ -45,9 +51,11 @@ module prim_fifo_sync_bind_fpv;
   );
 
   bind i_nopass8 prim_fifo_sync_assert_fpv #(
+    .Width(Width),
     .Pass(1'b0),
-    .Depth(8)
-  ) i_prim_fifo_sync_assert_fpv (
+    .Depth(8),
+    .EnableDataCheck(1'b1)
+  ) i_nopass8_assert_fpv (
     .clk_i,
     .rst_ni,
     .clr_i,
@@ -61,10 +69,11 @@ module prim_fifo_sync_bind_fpv;
   );
 
   bind i_nopass15 prim_fifo_sync_assert_fpv #(
-    .EnableDataCheck(1'b0),
+    .Width(Width),
     .Pass(1'b0),
-    .Depth(15)
-  ) i_prim_fifo_sync_assert_fpv (
+    .Depth(15),
+    .EnableDataCheck(1'b0)
+  ) i_nopass15_assert_fpv (
     .clk_i,
     .rst_ni,
     .clr_i,
@@ -78,10 +87,11 @@ module prim_fifo_sync_bind_fpv;
   );
 
   bind i_nopass16 prim_fifo_sync_assert_fpv #(
-    .EnableDataCheck(1'b0),
+    .Width(Width),
     .Pass(1'b0),
-    .Depth(16)
-  ) i_prim_fifo_sync_assert_fpv (
+    .Depth(16),
+    .EnableDataCheck(1'b0)
+  ) i_nopass16_assert_fpv (
     .clk_i,
     .rst_ni,
     .clr_i,
@@ -99,8 +109,11 @@ module prim_fifo_sync_bind_fpv;
   ////////////////
 
   bind i_pass0 prim_fifo_sync_assert_fpv #(
-    .Depth(0)
-  ) i_prim_fifo_sync_assert_fpv (
+    .Width(Width),
+    .Pass(1'b1),
+    .Depth(0),
+    .EnableDataCheck(1'b1)
+  ) i_pass0_assert_fpv (
     .clk_i,
     .rst_ni,
     .clr_i,
@@ -114,8 +127,11 @@ module prim_fifo_sync_bind_fpv;
   );
 
   bind i_pass1 prim_fifo_sync_assert_fpv #(
-    .Depth(1)
-  ) i_prim_fifo_sync_assert_fpv (
+    .Width(Width),
+    .Pass(1'b1),
+    .Depth(1),
+    .EnableDataCheck(1'b1)
+  ) i_pass1_assert_fpv (
     .clk_i,
     .rst_ni,
     .clr_i,
@@ -129,8 +145,11 @@ module prim_fifo_sync_bind_fpv;
   );
 
   bind i_pass7 prim_fifo_sync_assert_fpv #(
-    .Depth(7)
-  ) i_prim_fifo_sync_assert_fpv (
+    .Width(Width),
+    .Pass(1'b1),
+    .Depth(7),
+    .EnableDataCheck(1'b1)
+  ) i_pass7_assert_fpv (
     .clk_i,
     .rst_ni,
     .clr_i,
@@ -144,8 +163,11 @@ module prim_fifo_sync_bind_fpv;
   );
 
   bind i_pass8 prim_fifo_sync_assert_fpv #(
-    .Depth(8)
-  ) i_prim_fifo_sync_assert_fpv (
+    .Width(Width),
+    .Pass(1'b1),
+    .Depth(8),
+    .EnableDataCheck(1'b1)
+  ) i_pass8_assert_fpv (
     .clk_i,
     .rst_ni,
     .clr_i,
@@ -159,9 +181,11 @@ module prim_fifo_sync_bind_fpv;
   );
 
   bind i_pass15 prim_fifo_sync_assert_fpv #(
-    .EnableDataCheck(1'b0),
-    .Depth(15)
-  ) i_prim_fifo_sync_assert_fpv (
+    .Width(Width),
+    .Pass(1'b1),
+    .Depth(15),
+    .EnableDataCheck(1'b0)
+  ) i_pass15_assert_fpv (
     .clk_i,
     .rst_ni,
     .clr_i,
@@ -175,9 +199,11 @@ module prim_fifo_sync_bind_fpv;
   );
 
   bind i_pass16 prim_fifo_sync_assert_fpv #(
-    .EnableDataCheck(1'b0),
-    .Depth(16)
-  ) i_prim_fifo_sync_assert_fpv (
+    .Width(Width),
+    .Pass(1'b1),
+    .Depth(16),
+    .EnableDataCheck(1'b0)
+  ) i_pass16_assert_fpv (
     .clk_i,
     .rst_ni,
     .clr_i,
