91aefdcb3effd56f2431c7943b01a90134e34b92
Pirmin Vogel <vogelpi@lowrisc.org>
2019-12-20 17:58:21 +0100

    [aes/rtl] Add Non-LUT-based S-Box
    This commits adapts the design to select either the previous
    LUT-based S-Box or a non-LUT-based Canright S-Box using a parameter.
    
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

---
+++ hw/ip/aes/rtl/aes_sbox.sv
@@ -0,0 +1,29 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// AES SBox
+
+module aes_sbox #(
+  parameter SBoxImpl = "lut"
+) (
+  input  aes_pkg::mode_e mode_i,
+  input  logic [7:0]     data_i,
+  output logic [7:0]     data_o
+);
+
+  if (SBoxImpl == "lut") begin : gen_sbox_lut
+    aes_sbox_lut aes_sbox (
+      .mode_i,
+      .data_i,
+      .data_o
+    );
+  end else if (SBoxImpl == "canright") begin : gen_sbox_canright
+    aes_sbox_canright aes_sbox (
+      .mode_i,
+      .data_i,
+      .data_o
+    );
+  end
+
+endmodule
