7ab51b30b5dffaf65c47e17c130d6c5662fa909f
Stefan Lippuner <lstefan@iis.ee.ethz.ch>
2020-01-08 17:32:33 +0100

    [usbdev] Remove tinyFPGA dependency for usbdev
     - Move used files to usbfs_nb_pe folder, rename to x.sv
     - Remove tinyFPGA folder
    
    Signed-off-by: Stefan Lippuner <lstefan@iis.ee.ethz.ch>

--- hw/ip/usb_fs_nb_pe/rtl/usb_fs_tx.sv
+++ hw/ip/usb_fs_nb_pe/rtl/usb_fs_tx.sv
@@ -1,3 +1,9 @@
+// Copyright lowRISC contributors.
+// Copyright ETH Zurich.
+// Copyright Luke Valenty (TinyFPGA project, https://github.com/tinyfpga/TinyFPGA-Bootloader).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 module usb_fs_tx (
   // A 48MHz clock is required to receive USB data at 12MHz
   // it's simpler to juse use 48MHz everywhere
--- hw/ip/usb_fs_nb_pe/rtl/usb_fs_tx_mux.sv
+++ hw/ip/usb_fs_nb_pe/rtl/usb_fs_tx_mux.sv
@@ -1,3 +1,9 @@
+// Copyright lowRISC contributors.
+// Copyright ETH Zurich.
+// Copyright Luke Valenty (TinyFPGA project, https://github.com/tinyfpga/TinyFPGA-Bootloader).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 module usb_fs_tx_mux (
   // interface to IN Protocol Engine
   input in_tx_pkt_start,
