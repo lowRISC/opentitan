f9fe9b85cc11c87b0363ca10e4f9407813782396
Stefan Lippuner <lstefan@iis.ee.ethz.ch>
2020-01-10 12:30:12 +0100

    [usbdev] Resolve a number of style issues
    Signed-off-by: Stefan Lippuner <lstefan@iis.ee.ethz.ch>

--- hw/ip/usbuart/rtl/rising_edge_detector.sv
+++ hw/ip/usbuart/rtl/rising_edge_detector.sv
@@ -13,7 +13,7 @@ module rising_edge_detector (
   // Store last value
   logic last_q;
   always_ff @(posedge clk_ci or negedge rst_ni) begin : proc_last
-    if(~rst_ni) begin
+    if(!rst_ni) begin
       last_q <= 1'b0;
     end else begin
       last_q <= in_i;
