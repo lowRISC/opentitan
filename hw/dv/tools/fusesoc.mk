559a7ae0032d710733e9dde44c144163d8fb1b87
Michael Schaffner <msf@opentitan.org>
2020-02-24 10:12:38 -0800

    [cleanup] This cleans up header comments in makefiles
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/dv/tools/fusesoc.mk
+++ hw/dv/tools/fusesoc.mk
@@ -1,14 +1,12 @@
-####################################################################################################
-## Copyright lowRISC contributors.                                                                ##
-## Licensed under the Apache License, Version 2.0, see LICENSE for details.                       ##
-## SPDX-License-Identifier: Apache-2.0                                                            ##
-####################################################################################################
-## Make variables specific to FUSESOC tool used for generating the filelist                       ##
-## The following Make variables are to be set in the Test Makefile                                ##
-## FUSESOC_CORE: the top level fusesoc core file developed for the ip/top level testbench         ##
-##               see hw/ip/uart/dv/uart_sim.core as an example                                    ##
-## Rest of the Make variables added here are intermeditiate ones used in the flow                 ##
-####################################################################################################
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+# Make variables specific to FUSESOC tool used for generating the filelist
+# The following Make variables are to be set in the Test Makefile
+# FUSESOC_CORE: the top level fusesoc core file developed for the ip/top level testbench
+#               see hw/ip/uart/dv/uart_sim.core as an example
+# Rest of the Make variables added here are intermeditiate ones used in the flow
+
 # fusesoc tool and options
 SV_FLIST_GEN_TOOL  ?= fusesoc
 SV_FLIST_GEN_OPTS  += --cores-root ${PROJ_ROOT} --cores-root ${RAL_MODEL_DIR} \
