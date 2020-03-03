f35432d19e4902669988c25dde91de80c74aa08f
Cindy Chen <chencindy@google.com>
2020-03-02 16:27:42 -0800

    [dv] add coverage exclusion for hmac and gpio
    Add coverage exclusion into the hjson file for hmac and gpio.
    Add max_outstanding_req for hmac, gpio, and rv_timer.
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/ip/gpio/dv/env/gpio_env_cfg.sv
+++ hw/ip/gpio/dv/env/gpio_env_cfg.sv
@@ -27,6 +27,9 @@ class gpio_env_cfg extends cip_base_env_cfg #(.RAL_T(gpio_reg_block));
     super.initialize(csr_base_addr);
     // set num_interrupts & num_alerts which will be used to create coverage and more
     num_interrupts = ral.intr_state.get_n_used_bits();
+
+    // only support 1 outstanding TL item
+    m_tl_agent_cfg.max_outstanding_req = 1;
   endfunction : initialize
 
 endclass
