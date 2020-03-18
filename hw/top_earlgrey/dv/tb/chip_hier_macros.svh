a39557eb0b8f1bc90461b4bc90f7a22b49a410ec
Michael Schaffner <msf@opentitan.org>
2020-03-17 18:30:21 -0700

    [top/usbdev] Compile error fix in usbdev and naming changes in toplevel
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/top_earlgrey/dv/tb/chip_hier_macros.svh
+++ hw/top_earlgrey/dv/tb/chip_hier_macros.svh
@@ -4,13 +4,13 @@
 
 `define DUT_HIER        dut
 `define CHIP_HIER       `DUT_HIER.top_earlgrey
-`define UART_HIER       `CHIP_HIER.uart
-`define GPIO_HIER       `CHIP_HIER.gpio
-`define SPI_DEVICE_HIER `CHIP_HIER.spi_device
-`define CPU_HIER        `CHIP_HIER.core
+`define UART_HIER       `CHIP_HIER.u_uart
+`define GPIO_HIER       `CHIP_HIER.u_gpio
+`define SPI_DEVICE_HIER `CHIP_HIER.u_spi_device
+`define CPU_HIER        `CHIP_HIER.u_rv_core_ibex
 `define RAM_MAIN_HIER   `CHIP_HIER.u_ram1p_ram_main
 `define ROM_HIER        `CHIP_HIER.u_rom_rom
 `define FLASH_HIER      `CHIP_HIER.u_flash_eflash
-`define USBDEV_HIER     `CHIP_HIER.usbdev
+`define USBDEV_HIER     `CHIP_HIER.u_usbdev
 `define FLASH0_MEM_HIER `FLASH_HIER.gen_flash_banks[0].u_flash.gen_generic.u_impl_generic.u_mem
 `define FLASH1_MEM_HIER `FLASH_HIER.gen_flash_banks[1].u_flash.gen_generic.u_impl_generic.u_mem
