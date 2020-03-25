43796962c5e54ac1bd84484a5aaba1b96344bff4
Silvestrs Timofejevs <silvestrst@lowrisc.org>
2020-03-25 10:14:55 +0000

    [sw, libbase] Create non-volatile bit manipulation library
    This change was suggested by Miguel Young, and aims to create a set of
    bit manipulation routines that can be used across the project.
    
    The functionality added in this change can be used in mmio library,
    which will simply wrap the non-volatile versions. This would make it
    easier to create a set of expectations in the mock MMIO library to
    mimic the mmio library exactly.
    
    There could be other use-cases outside of DIF that could benefit from
    non-volatile bit manipulation API.
    
    Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>

---
+++ sw/device/lib/base/bitfield.c
@@ -0,0 +1,9 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/base/bitfield.h"
+
+// |extern| declarations to give the inline functions in the
+// corresponding header a link location.
+extern void bitfield_set_field32(uint32_t bitfield, bitfield_field32_t field);
