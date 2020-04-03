09e0f1deb5455eb7b756bb9fb185fecea0c8262c
Silvestrs Timofejevs <silvestrst@lowrisc.org>
2020-04-03 08:54:52 +0100

    [sw, libbase] Use helper non-volatile bitfield lib in mmio
    The bit manipulation operations are useful outside of the mmio library,
    as well as inside.
    
    This change aims to avoid code duplication, and make these operations
    uniform between volatile and non-volatile use-cases.
    
    Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>

--- sw/device/lib/base/mmio.c
+++ sw/device/lib/base/mmio.c
@@ -27,7 +27,7 @@ extern void mmio_region_nonatomic_set_mask32(mmio_region_t base,
                                              uint32_t mask_index);
 extern void mmio_region_nonatomic_set_field32(mmio_region_t base,
                                               ptrdiff_t offset,
-                                              mmio_region_field32_t field);
+                                              bitfield_field32_t field);
 extern void mmio_region_nonatomic_clear_bit32(mmio_region_t base,
                                               ptrdiff_t offset,
                                               uint32_t bit_index);
