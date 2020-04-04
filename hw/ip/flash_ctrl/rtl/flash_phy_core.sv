b15ab0755a9cdd86adcb5402dfcbfd8e4f7e062f
Timothy Chen <timothytim@google.com>
2020-04-03 23:21:35 -0700

    [flash] Minor refactoring of prim_flash
    Separate out some of the arbitration logic from prim_flash, as that
    can be open sourced, while contents within prim_flash cannot.
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

---
+++ hw/ip/flash_ctrl/rtl/flash_phy_core.sv
@@ -0,0 +1,143 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Flash Phy Core Module
+//
+//
+// This module wraps every single flash bank and contains most of the region attribute,
+// scramble, ECC, security and arbitration logic.
+// Most of the items are TODO, at the moment only arbitration logic exists.
+
+module flash_phy_core #(
+  parameter int PagesPerBank = 256, // pages per bank
+  parameter int WordsPerPage = 256, // words per page
+  parameter int DataWidth   = 32,   // bits per word
+  parameter bit SkipInit = 1,       // this is an option to reset flash to all F's at reset
+
+  // Derived parameters
+  localparam int PageW = $clog2(PagesPerBank),
+  localparam int WordW = $clog2(WordsPerPage),
+  localparam int AddrW = PageW + WordW
+) (
+  input                        clk_i,
+  input                        rst_ni,
+  input                        host_req_i, // host request - read only
+  input [AddrW-1:0]            host_addr_i,
+  input                        req_i,      // controller request
+  input                        rd_i,
+  input                        prog_i,
+  input                        pg_erase_i,
+  input                        bk_erase_i,
+  input [AddrW-1:0]            addr_i,
+  input [DataWidth-1:0]        prog_data_i,
+  output logic                 host_req_rdy_o,
+  output logic                 host_req_done_o,
+  output logic                 rd_done_o,
+  output logic                 prog_done_o,
+  output logic                 erase_done_o,
+  output logic [DataWidth-1:0] rd_data_o,
+  output logic                 init_busy_o
+);
+
+  import flash_ctrl_pkg::*;
+  import flash_phy_pkg::*;
+
+  // The operation sent to the flash macro
+  flash_phy_op_sel_e op_sel;
+
+  // The flash macro is currently idle
+  logic flash_idle;
+
+  // request signals to flash macro
+  logic [PhyOps-1:0] reqs;
+
+  // ack to phy operations from flash macro
+  logic ack;
+
+  // interface with flash macro
+  logic [AddrW-1:0] muxed_addr;
+
+  // valid request conditions
+  logic op_clr_cond;
+
+  // flash is idle when no op is selected
+  // or when the flash acks
+  assign flash_idle = (op_sel == None) | ack;
+
+  // if current operation is host, only transition to None if no more pending reqs
+  // if current operation is ctrl, transition back whenever operation done
+  assign op_clr_cond = (op_sel == Host) ? flash_idle & !host_req_i :
+                       (op_sel == Ctrl) ? flash_idle : 1'b0;
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      op_sel <= None;
+    end else if (op_clr_cond) begin
+      op_sel <= None;
+    end else if (flash_idle && host_req_i) begin
+      op_sel <= Host;
+    end else if (flash_idle && req_i) begin
+      op_sel <= Ctrl;
+    end
+  end
+
+  assign reqs[PhyRead]    = (req_i & rd_i) | host_req_i;
+  assign reqs[PhyProg]    = !host_req_i & req_i & prog_i;
+  assign reqs[PhyPgErase] = !host_req_i & req_i & pg_erase_i;
+  assign reqs[PhyBkErase] = !host_req_i & req_i & bk_erase_i;
+  assign muxed_addr       = host_req_i ? host_addr_i : addr_i;
+
+  // whenever there is nothing selected, host transactions are always accepted
+  // This also implies host trasnactions have the highest priority
+  assign host_req_rdy_o  = (op_sel == None) | host_req_done_o;
+  assign host_req_done_o = (op_sel == Host) & ack;
+  assign rd_done_o       = (op_sel == Ctrl) & ack & rd_i;
+  assign prog_done_o     = (op_sel == Ctrl) & ack & prog_i;
+  assign erase_done_o    = (op_sel == Ctrl) & ack & (pg_erase_i | bk_erase_i);
+
+  // The actual flash macro wrapper
+  prim_flash #(
+    .PagesPerBank(PagesPerBank),
+    .WordsPerPage(WordsPerPage),
+    .DataWidth(DataWidth),
+    .SkipInit(SkipInit)
+  ) i_flash (
+    .clk_i,
+    .rst_ni,
+    .rd_i(reqs[PhyRead]),
+    .prog_i(reqs[PhyProg]),
+    .pg_erase_i(reqs[PhyPgErase]),
+    .bk_erase_i(reqs[PhyBkErase]),
+    .addr_i(muxed_addr[0 +: PageW + WordW]),
+    .prog_data_i(prog_data_i),
+    .ack_o(ack),
+    .rd_data_o,
+    .init_busy_o // TBD this needs to be looked at later. What init do we need to do,
+                 // and where does it make the most sense?
+  );
+
+  /////////////////////////////////
+  // Assertions
+  /////////////////////////////////
+
+  // requests to flash must always be one hot
+  `ASSERT(OneHotReqs_A, $onehot0(reqs))
+
+  // if op_sel is Ctrl, it must have come from None
+  `ASSERT(CtrlPrevNone_A, $rose(op_sel == Ctrl) |-> $past(op_sel == None))
+
+  // if no operation is ongoing, a host request must always win
+  `ASSERT(HostPriority_A, (op_sel == None) && host_req_i |=> op_sel == Host)
+
+  // if a host request is done, and is immediately followed by another host request
+  // host should retain priority
+  `ASSERT(HostPriorityConseq_A, (op_sel == Host && flash_idle) && host_req_i |=> op_sel == Host)
+
+  // host request should never interrupt an ongoing controller operation
+  `ASSERT(CtrlAtomic_A, op_sel == Ctrl && !flash_idle |=> op_sel == Ctrl)
+
+  // ctrl request do not allow overlapping
+  `ASSERT(CtrlSerial_A, op_sel == Ctrl && flash_idle |=> op_sel == None)
+
+endmodule // flash_phy_core
