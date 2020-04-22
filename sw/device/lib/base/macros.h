8561ea40d57d57fe7ba7b59c682033dbad8ac484
Srikrishna Iyer <sriyer@google.com>
2020-04-22 14:46:43 -0700

    [sw] Fix for DV logging
    SW logging for DV is broken at TOT. See #2026 for details.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- sw/device/lib/base/macros.h
+++ sw/device/lib/base/macros.h
@@ -16,14 +16,18 @@
  * This macro is based off of a well-known preprocessor trick. This
  * StackOverflow post expains the trick in detail:
  * https://stackoverflow.com/questions/2308243/macro-returning-the-number-of-arguments-it-is-given-in-c
+ * TODO #2026: a dummy token is required for this to work correctly.
+ *
+ * @param dummy a dummy token that is required to be passed for the calculation
+ *              to work correctly.
+ * @param ... the variable args list.
  */
-#define GET_NUM_VARIABLE_ARGS(...) PASS_N_VARIABLE_ARGS_(0, ##__VA_ARGS__)
 
 // Implementation details for `GET_NUM_VARIABLE_ARGS()`.
-#define PASS_N_VARIABLE_ARGS_(...)                                             \
-  SHIFT_N_VARIABLE_ARGS_(__VA_ARGS__, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22,  \
-                         21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, \
-                         7, 6, 5, 4, 3, 2, 1, 0)
+#define GET_NUM_VARIABLE_ARGS(dummy, ...)                                      \
+  SHIFT_N_VARIABLE_ARGS_(dummy, ##__VA_ARGS__, 31, 30, 29, 28, 27, 26, 25, 24, \
+                         23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11,   \
+                         10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 #define SHIFT_N_VARIABLE_ARGS_(...) GET_NTH_VARIABLE_ARG_(__VA_ARGS__)
 #define GET_NTH_VARIABLE_ARG_(x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, \
                               x11, x12, x13, x14, x15, x16, x17, x18, x19, \
