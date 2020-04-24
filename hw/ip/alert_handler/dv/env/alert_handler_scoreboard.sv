4564cc56f22b198f20a64821ffdfa435348b4a18
Cindy Chen <chencindy@google.com>
2020-04-24 09:32:36 -0700

    [dv/alert_handler] entropy length adjustment
    Based on the coverage results, to cover the ping for all alerts and
    escalation devices, we need to increase the num_trans and timeout limit
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/ip/alert_handler/dv/env/alert_handler_scoreboard.sv
+++ hw/ip/alert_handler/dv/env/alert_handler_scoreboard.sv
@@ -188,12 +188,12 @@ class alert_handler_scoreboard extends cip_base_scoreboard #(
 
   // check if escalation signal's duration length is correct
   virtual function void check_esc_signal(int cycle_cnt, int esc_sig_i);
-    `uvm_info(`gfn, $sformatf("esc signal_%0d triggered, cycle cnt is %0d", esc_sig_i, cycle_cnt),
-              UVM_LOW)
     if (ral.regen.get_mirrored_value()) begin
       // if ping response enabled, will not check cycle count if esc ping is enabled
       `DV_CHECK_EQ(cycle_cnt, esc_cnter_per_signal[esc_sig_i],
                    $sformatf("check signal_%0d", esc_sig_i))
+      `uvm_info(`gfn, $sformatf("esc signal_%0d triggered, cycle cnt is %0d",
+                                esc_sig_i, cycle_cnt), UVM_LOW)
     end
     esc_cnter_per_signal[esc_sig_i] = 0;
     esc_sig_class[esc_sig_i] = 0;
--- hw/ip/alert_handler/dv/env/seq_lib/alert_handler_entropy_vseq.sv
+++ hw/ip/alert_handler/dv/env/seq_lib/alert_handler_entropy_vseq.sv
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// this sequence triggers escalation by the interrupt timeout
+// this sequence enable entropy by writing 1 to the lock_regen register.
 
 class alert_handler_entropy_vseq extends alert_handler_sanity_vseq;
   `uvm_object_utils(alert_handler_entropy_vseq)
@@ -12,15 +12,22 @@ class alert_handler_entropy_vseq extends alert_handler_sanity_vseq;
   rand bit drive_entropy;
   rand bit lock_regen;
 
-  // TODO: re-evaluate the constraint based on ping coverage data
+  // large number of num_trans to make sure covers all alerts and escalation pings
   constraint num_trans_c {
-    num_trans inside {[4000:8000]};
+    num_trans inside {[4000:100_000]};
+  }
+
+  // increase the possibility to enable more alerts, because alert_handler only sends ping on
+  // enabled alerts
+  constraint enable_one_alert_c {
+    alert_en dist {'b1111 := 9, [0:'b1110] := 1};
   }
 
   function void pre_randomize();
-    this.enable_one_alert_c.constraint_mode(0);
     this.enable_classa_only_c.constraint_mode(0);
     this.sig_int_c.constraint_mode(0);
+    // set verbosity high to avoid printing out too much information
+    verbosity = UVM_HIGH;
   endfunction
 
   virtual task alert_handler_init(bit [NUM_ALERT_HANDLER_CLASSES-1:0] intr_en = '1,
@@ -30,6 +37,11 @@ class alert_handler_entropy_vseq extends alert_handler_sanity_vseq;
                                   bit [TL_DW-1:0]                     loc_alert_class = 'h0);
     super.alert_handler_init(intr_en, alert_en, alert_class, loc_alert_en, loc_alert_class);
     cfg.entropy_vif.drive(drive_entropy);
+    if (ral.regen.get_mirrored_value() == 1) begin
+      `uvm_info(`gfn,
+          $sformatf("init setting: intr_en=%0b, alert_en=%0b, alert_class=%0b, regen=%0b",
+          intr_en, alert_en, alert_class, lock_regen), UVM_LOW)
+    end
     csr_wr(.csr(ral.regen), .value(lock_regen));
   endtask
 
