2c9c8fb00b830b7c3bb360c2cfbfe79f973c4887
Cindy Chen <chencindy@google.com>
2020-05-01 15:49:41 -0700

    [dv/alert_handler] add coverage
    Add coverage on esc signal count
    Set max-outstanding item to 1
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/ip/alert_handler/dv/env/alert_handler_env_cov.sv
+++ hw/ip/alert_handler/dv/env/alert_handler_env_cov.sv
@@ -18,12 +18,25 @@ class alert_handler_env_cov extends cip_base_env_cov #(.CFG_T(alert_handler_env_
       bins saturate_cnt  = {'hffff};
     }
     class_cnt_cross: cross class_index_cp, accum_cnt_cp;
-  endgroup
+  endgroup : accum_cnt_cg
+
+  covergroup esc_sig_length_cg with function sample (int sig_index, int sig_len);
+    esc_sig_index: coverpoint sig_index {
+      bins index[NUM_ESC_SIGNALS] = {[0:NUM_ESC_SIGNALS-1]};
+    }
+    esc_sig_len: coverpoint sig_len {
+      bins len_2 = {2};
+      bins lens_less_than_1000[20] = {[3:1000]};
+      bins lens_more_than_1000     = {[1001:$]};
+    }
+    len_per_esc_sig: cross esc_sig_index, esc_sig_len;
+  endgroup : esc_sig_length_cg
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
     // instantiate all covergroups here
     accum_cnt_cg = new();
+    esc_sig_length_cg = new();
   endfunction : new
 
 endclass
