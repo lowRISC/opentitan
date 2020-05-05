a9d893b40b9711f1bcb44491c34a4446d99ce3a7
Weicai Yang <weicai@google.com>
2020-05-05 13:25:59 -0700

    [dv/xbar] Add multi-clock support
    1. Each host/device uses the clock configured in the xbar*.hjson
    2. All resets are tied to one for now
    
    Signed-off-by: Weicai Yang <weicai@google.com>

--- hw/ip/tlul/generic_dv/tb/xbar_macros.svh
+++ hw/ip/tlul/generic_dv/tb/xbar_macros.svh
@@ -6,7 +6,22 @@
 // TileLink interface connection macros
 // ---------------------------------------------
 
-`define CONNECT_TL_DEVICE_IF(tl_name, path = dut) \
+// TODO, use random clock frequency for now , need to get frequency from hjson
+// reset is from tb.clk_rst_if, disable driving reset in this clk_rst_if
+`define CONNECT_CLK(name) \
+  wire ``name``; \
+  clk_rst_if clk_rst_if_``name``(.clk(``name``), .rst_n(rst_n)); \
+  initial begin \
+    clk_rst_if_``name``.set_active(.drive_rst_n_val(0)); \
+    clk_rst_if_``name``.set_freq_mhz($urandom_range(10, 100)); \
+  end
+
+// TODO, all resets tie together
+`define CONNECT_RST(name) \
+  wire ``name`` = rst_n;
+
+
+`define CONNECT_TL_DEVICE_IF(tl_name, path = dut, clk, rst_n) \
    tl_if ``tl_name``_tl_if(clk, rst_n); \
    initial begin \
      force ``tl_name``_tl_if.h2d = ``path``.tl_``tl_name``_o; \
@@ -15,7 +30,7 @@
                                         ``tl_name``_tl_if); \
    end
 
-`define CONNECT_TL_HOST_IF(tl_name, path = dut) \
+`define CONNECT_TL_HOST_IF(tl_name, path = dut, clk, rst_n) \
    tl_if ``tl_name``_tl_if(clk, rst_n); \
    initial begin \
      force ``tl_name``_tl_if.d2h = ``path``.tl_``tl_name``_o; \
@@ -24,7 +39,7 @@
                                         ``tl_name``_tl_if); \
    end
 
-`define CONNECT_TL_MON_IF(dut_h2d, dut_d2h, tl_name) \
+`define CONNECT_TL_MON_IF(dut_h2d, dut_d2h, tl_name, clk, rst_n) \
    tl_if ``tl_name``_tl_if(clk, rst_n); \
    initial begin \
      force ``tl_name``_tl_if.h2d = dut_h2d; \
