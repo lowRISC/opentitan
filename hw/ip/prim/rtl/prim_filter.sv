7b7e47aa403930ab8566b7ca74ffccaa5fb183ae
Michael Schaffner <msf@opentitan.org>
2020-05-07 11:55:04 -0700

    [style-lint] Fix a couple of nitty style errors
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/prim/rtl/prim_filter.sv
+++ hw/ip/prim/rtl/prim_filter.sv
@@ -10,7 +10,7 @@
 //   new input must be opposite value from stored value for
 //   #Cycles before switching to new value.
 
-module prim_filter #(parameter Cycles = 4) (
+module prim_filter #(parameter int Cycles = 4) (
   input  clk_i,
   input  rst_ni,
   input  enable_i,
