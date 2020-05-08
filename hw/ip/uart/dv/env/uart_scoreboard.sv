a2c20b3cfde538d967a649cf8f5fbc67a0772b78
Weicai Yang <weicai@google.com>
2020-05-07 18:36:43 -0700

    [uart/dv] Fix a corner case tx_watermark mismatch
    Add delay to update tx_watermark after reset txfifo
    
    Signed-off-by: Weicai Yang <weicai@google.com>

--- hw/ip/uart/dv/env/uart_scoreboard.sv
+++ hw/ip/uart/dv/env/uart_scoreboard.sv
@@ -271,8 +271,11 @@ class uart_scoreboard extends cip_base_scoreboard #(.CFG_T(uart_env_cfg),
             end
           end
           // recalculate watermark when RXILVL/TXILVL is updated
-          predict_tx_watermark_intr();
           predict_rx_watermark_intr();
+          fork begin
+            cfg.clk_rst_vif.wait_n_clks(NUM_CLK_DLY_TO_UPDATE_TX_WATERMARK);
+            predict_tx_watermark_intr();
+          end join_none
         end // write && channel == AddrChannel
       end
       "intr_test": begin
