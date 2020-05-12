5b7cbbb4b8d81cf4d0862902260b3278e1dd4030
Philipp Wagner <phw@lowrisc.org>
2020-05-12 13:50:37 +0100

    [prim_ram_2p] Add write mask
    Copy the write mask logic from the single port RAM.
    
    Do not modify the external interface of the prim_ram_2p_*adv modules, as
    it is not clear how the write mask can be applied to writes with
    additional parity/ECC information.
    
    Signed-off-by: Philipp Wagner <phw@lowrisc.org>

--- hw/ip/prim_generic/rtl/prim_generic_ram_2p.sv
+++ hw/ip/prim_generic/rtl/prim_generic_ram_2p.sv
@@ -9,6 +9,7 @@
 module prim_generic_ram_2p #(
   parameter  int Width = 32, // bit
   parameter  int Depth = 128,
+  parameter  int DataBitsPerMask = 1, // Number of data bits per bit of write mask
 
   localparam int Aw    = $clog2(Depth)  // derived parameter
 ) (
@@ -19,6 +20,7 @@ module prim_generic_ram_2p #(
   input                    a_write_i,
   input        [Aw-1:0]    a_addr_i,
   input        [Width-1:0] a_wdata_i,
+  input  logic [Width-1:0] a_wmask_i,
   output logic [Width-1:0] a_rdata_o,
 
 
@@ -26,10 +28,24 @@ module prim_generic_ram_2p #(
   input                    b_write_i,
   input        [Aw-1:0]    b_addr_i,
   input        [Width-1:0] b_wdata_i,
+  input  logic [Width-1:0] b_wmask_i,
   output logic [Width-1:0] b_rdata_o
 );
+  // Width of internal write mask. Note *_wmask_i input into the module is always assumed
+  // to be the full bit mask.
+  localparam int MaskWidth = Width / DataBitsPerMask;
 
-  logic [Width-1:0] mem [Depth];
+  logic [Width-1:0]     mem [Depth];
+  logic [MaskWidth-1:0] a_wmask;
+  logic [MaskWidth-1:0] b_wmask;
+
+  always_comb begin
+    for (int i=0; i < MaskWidth; i = i + 1) begin : create_wmask
+      a_wmask[i] = &a_wmask_i[i*DataBitsPerMask +: DataBitsPerMask];
+      b_wmask[i] = &b_wmask_i[i*DataBitsPerMask +: DataBitsPerMask];
+    end
+  end
+
 
   // Xilinx FPGA specific Dual-port RAM coding style
   // using always instead of always_ff to avoid 'ICPD  - illegal combination of drivers' error
@@ -37,7 +53,12 @@ module prim_generic_ram_2p #(
   always @(posedge clk_a_i) begin
     if (a_req_i) begin
       if (a_write_i) begin
-        mem[a_addr_i] <= a_wdata_i;
+        for (int i=0; i < MaskWidth; i = i + 1) begin
+          if (a_wmask[i]) begin
+            mem[a_addr_i][i*DataBitsPerMask +: DataBitsPerMask] <=
+              a_wdata_i[i*DataBitsPerMask +: DataBitsPerMask];
+          end
+        end
       end
       a_rdata_o <= mem[a_addr_i];
     end
@@ -46,7 +67,12 @@ module prim_generic_ram_2p #(
   always @(posedge clk_b_i) begin
     if (b_req_i) begin
       if (b_write_i) begin
-        mem[b_addr_i] <= b_wdata_i;
+        for (int i=0; i < MaskWidth; i = i + 1) begin
+          if (b_wmask[i]) begin
+            mem[b_addr_i][i*DataBitsPerMask +: DataBitsPerMask] <=
+              b_wdata_i[i*DataBitsPerMask +: DataBitsPerMask];
+          end
+        end
       end
       b_rdata_o <= mem[b_addr_i];
     end
