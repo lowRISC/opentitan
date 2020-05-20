3ad2778afff2ea0aeb4668019504a8731bd9e501
Timothy Chen <timothytim@google.com>
2020-05-19 20:52:45 -0700

    [sensor_ctrl] Add first version of sensor control
    - Minimal module that acts as open source front end for AST
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [sensor_ctrl] sensor_ctrl updates for ast integration
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [sensor_ctrl] update register instance name to enable dv
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [sensor_ctrl] Add missing header
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [sensor_ctrl] minor review updates
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [sensor_ctrl] update sensor_ctrl registers
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

---
+++ hw/top_earlgrey/ip/sensor_ctrl/rtl/sensor_ctrl_pkg.sv
@@ -0,0 +1,18 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+
+package sensor_ctrl_pkg;
+
+  parameter int NumAlerts   = sensor_ctrl_reg_pkg::NumAlerts;
+  parameter int NumIoRails  = sensor_ctrl_reg_pkg::NumIoRails;
+
+  // Ack mode enumerations
+  typedef enum logic {
+    ImmAck = 0,
+    SwAck  = 1
+  } ast_ack_mode_e;
+
+
+endpackage // sensor_ctrl_pkg
