dbce36f2e70233b42c1069e0df5f9b722e1d8420
Cindy Chen <chencindy@google.com>
2020-06-03 19:43:08 -0700

    [dv/alert_handler] fix xcelium warning
    Move hierachy related task/function from alert_esc_interface to drivers
    and monitors, those who actually use the task/function.
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/dv/sv/alert_esc_agent/esc_monitor.sv
+++ hw/dv/sv/alert_esc_agent/esc_monitor.sv
@@ -27,17 +27,17 @@ class esc_monitor extends alert_esc_base_monitor;
 
   virtual task esc_thread(uvm_phase phase);
     alert_esc_seq_item req, req_clone;
-    logic esc_p = cfg.vif.get_esc();
+    logic esc_p = get_esc();
     forever @(cfg.vif.monitor_cb) begin
-      if (!under_reset && !esc_p && cfg.vif.get_esc() === 1'b1) begin
+      if (!under_reset && !esc_p && get_esc() === 1'b1) begin
         phase.raise_objection(this, $sformatf("%s objection raised", `gfn));
         req = alert_esc_seq_item::type_id::create("req");
         req.sig_cycle_cnt++;
-        if (cfg.vif.get_resp_p() == cfg.vif.get_resp_n()) req.esc_handshake_sta = EscIntFail;
+        if (is_sig_int_err()) req.esc_handshake_sta = EscIntFail;
         else req.esc_handshake_sta = EscRespHi;
         @(cfg.vif.monitor_cb);
         // esc_p/n only goes high for a cycle, detect it is a ping signal
-        if (cfg.vif.get_esc() === 1'b0) begin
+        if (get_esc() === 1'b0) begin
           int ping_cnter = 1;
           under_esc_ping = 1;
           req.alert_esc_type = AlertEscPingTrans;
@@ -66,11 +66,11 @@ class esc_monitor extends alert_esc_base_monitor;
           under_esc_ping = 0;
         end
         // when it is not a ping
-        if (cfg.vif.get_esc() === 1'b1) begin
+        if (get_esc() === 1'b1) begin
           req.alert_esc_type = AlertEscSigTrans;
           req.sig_cycle_cnt++;
           check_esc_resp(.req(req), .is_ping(0), .do_wait_clk(1));
-          while (cfg.vif.get_esc() === 1) check_esc_resp(.req(req), .is_ping(0), .do_wait_clk(1));
+          while (get_esc() === 1) check_esc_resp(.req(req), .is_ping(0), .do_wait_clk(1));
           check_esc_resp(.req(req), .is_ping(0), .do_wait_clk(0));
           $cast(req_clone, req.clone());
           req_clone.esc_handshake_sta = EscRespComplete;
@@ -80,16 +80,17 @@ class esc_monitor extends alert_esc_base_monitor;
             req.alert_esc_type.name(), req.esc_handshake_sta.name(), req.timeout), UVM_HIGH)
         phase.drop_objection(this, $sformatf("%s objection dropped", `gfn));
       end
-      esc_p = cfg.vif.get_esc();
+      esc_p = get_esc();
     end
   endtask : esc_thread
 
   virtual task unexpected_resp_thread();
     alert_esc_seq_item req;
     forever @(cfg.vif.monitor_cb) begin
-      while (cfg.vif.get_esc() === 1'b0 && !under_esc_ping && !under_reset) begin
+      while (get_esc() === 1'b0 && !under_esc_ping && !under_reset) begin
         @(cfg.vif.monitor_cb);
-        if (cfg.vif.get_resp_p() === 1'b1 && cfg.vif.get_resp_n() == 1'b0) begin
+        if (cfg.vif.monitor_cb.esc_rx.resp_p === 1'b1 &&
+            cfg.vif.monitor_cb.esc_rx.resp_n === 1'b0) begin
           req = alert_esc_seq_item::type_id::create("req");
           req.alert_esc_type = AlertEscIntFail;
           alert_esc_port.write(req);
@@ -101,7 +102,7 @@ class esc_monitor extends alert_esc_base_monitor;
   virtual task sig_int_fail_thread();
     alert_esc_seq_item req;
     forever @(cfg.vif.monitor_cb) begin
-      if (cfg.vif.get_resp_p() === cfg.vif.get_resp_n() && !under_reset) begin
+      if (is_sig_int_err() && !under_reset) begin
         req = alert_esc_seq_item::type_id::create("req");
         req.alert_esc_type = AlertEscIntFail;
         alert_esc_port.write(req);
@@ -116,7 +117,7 @@ class esc_monitor extends alert_esc_base_monitor;
   // if waited a clk and it is not a ping_response, sig_cyc_cnt is incremented
   virtual task check_esc_resp(alert_esc_seq_item req, bit is_ping, bit do_wait_clk);
     if (req.esc_handshake_sta inside {EscIntFail, EscReceived}) begin
-      if (cfg.vif.get_resp_p() !== 0) begin
+      if (cfg.vif.monitor_cb.esc_rx.resp_p !== 0) begin
         alert_esc_seq_item req_clone;
         $cast(req_clone, req.clone());
         req_clone.esc_handshake_sta = EscIntFail;
@@ -128,14 +129,14 @@ class esc_monitor extends alert_esc_base_monitor;
         req.esc_handshake_sta = EscRespHi;
       end
     end else if (req.esc_handshake_sta == EscRespHi) begin
-      if (cfg.vif.get_resp_p() !== 1) begin
+      if (cfg.vif.monitor_cb.esc_rx.resp_p !== 1) begin
         req.esc_handshake_sta = EscIntFail;
         alert_esc_port.write(req);
       end else begin
         req.esc_handshake_sta = EscRespLo;
       end
     end else if (req.esc_handshake_sta == EscRespLo) begin
-      if (cfg.vif.get_resp_p() !== 0) begin
+      if (cfg.vif.monitor_cb.esc_rx.resp_p !== 0) begin
         req.esc_handshake_sta = EscIntFail;
         alert_esc_port.write(req);
       end else begin
@@ -143,7 +144,7 @@ class esc_monitor extends alert_esc_base_monitor;
         else req.esc_handshake_sta = EscRespHi;
       end
     end else if (req.esc_handshake_sta == EscRespPing0) begin
-      if (cfg.vif.get_resp_p() !== 1) begin
+      if (cfg.vif.monitor_cb.esc_rx.resp_p !== 1) begin
         req.esc_handshake_sta = EscIntFail;
         alert_esc_port.write(req);
       end else begin
@@ -151,7 +152,7 @@ class esc_monitor extends alert_esc_base_monitor;
         if (cfg.probe_vif.get_esc_en() && is_ping) req.esc_handshake_sta = EscRespLo;
       end
     end else if (req.esc_handshake_sta == EscRespPing1) begin
-      if (cfg.vif.get_resp_p() !== 0) begin
+      if (cfg.vif.monitor_cb.esc_rx.resp_p !== 0) begin
         req.esc_handshake_sta = EscIntFail;
         alert_esc_port.write(req);
       end else begin
@@ -160,12 +161,20 @@ class esc_monitor extends alert_esc_base_monitor;
       end
     end
 
-    if (cfg.vif.get_resp_p() == cfg.vif.get_resp_n()) req.esc_handshake_sta = EscIntFail;
+    if (is_sig_int_err()) req.esc_handshake_sta = EscIntFail;
 
     if (do_wait_clk) begin
       @(cfg.vif.monitor_cb);
-      if (cfg.vif.get_esc() === 1 && !is_ping) req.sig_cycle_cnt++;
+      if (get_esc() === 1 && !is_ping) req.sig_cycle_cnt++;
     end
   endtask : check_esc_resp
+
+  virtual function bit get_esc();
+    return cfg.vif.monitor_cb.esc_tx.esc_p && !cfg.vif.monitor_cb.esc_tx.esc_n;
+  endfunction
+
+  virtual function bit is_sig_int_err();
+    return cfg.vif.monitor_cb.esc_rx.resp_p === cfg.vif.monitor_cb.esc_rx.resp_n;
+  endfunction : is_sig_int_err
 
 endclass : esc_monitor
