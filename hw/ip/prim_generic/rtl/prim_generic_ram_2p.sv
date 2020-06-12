dd4da38ce2312ca09fe38387bba985e575d8923b
Michael Schaffner <msf@google.com>
2020-06-11 20:50:20 -0700

    [prim_ram_2p] Fix compile error due to missing ports
    Signed-off-by: Michael Schaffner <msf@google.com>

--- hw/ip/prim_generic/rtl/prim_generic_ram_2p.sv
+++ hw/ip/prim_generic/rtl/prim_generic_ram_2p.sv
@@ -7,10 +7,12 @@
 //   Implementing ECC should be done inside wrapper not this model.
 
 module prim_generic_ram_2p #(
-  parameter  int Width = 32, // bit
-  parameter  int Depth = 128,
+  parameter  int Width           = 32, // bit
+  parameter  int Depth           = 128,
+  parameter  int DataBitsPerMask = 1, // Number of data bits per bit of write mask
+  parameter      MemInitFile     = "", // VMEM file to initialize the memory with
 
-  localparam int Aw    = $clog2(Depth)  // derived parameter
+  localparam int Aw              = $clog2(Depth)  // derived parameter
 ) (
   input clk_a_i,
   input clk_b_i,
@@ -19,6 +21,7 @@ module prim_generic_ram_2p #(
   input                    a_write_i,
   input        [Aw-1:0]    a_addr_i,
   input        [Width-1:0] a_wdata_i,
+  input  logic [Width-1:0] a_wmask_i,
   output logic [Width-1:0] a_rdata_o,
 
 
@@ -26,10 +29,24 @@ module prim_generic_ram_2p #(
   input                    b_write_i,
   input        [Aw-1:0]    b_addr_i,
   input        [Width-1:0] b_wdata_i,
+  input  logic [Width-1:0] b_wmask_i,
   output logic [Width-1:0] b_rdata_o
 );
+  // Width of internal write mask. Note *_wmask_i input into the module is always assumed
+  // to be the full bit mask.
+  localparam int MaskWidth = Width / DataBitsPerMask;
 
-  logic [Width-1:0] mem [Depth];
+  logic [Width-1:0]     mem [Depth];
+  logic [MaskWidth-1:0] a_wmask;
+  logic [MaskWidth-1:0] b_wmask;
+
+  always_comb begin
+    for (int i=0; i < MaskWidth; i = i + 1) begin : create_wmask
+      a_wmask[i] = &a_wmask_i[i*DataBitsPerMask +: DataBitsPerMask];
+      b_wmask[i] = &b_wmask_i[i*DataBitsPerMask +: DataBitsPerMask];
+    end
+  end
+
 
   // Xilinx FPGA specific Dual-port RAM coding style
   // using always instead of always_ff to avoid 'ICPD  - illegal combination of drivers' error
@@ -52,7 +69,6 @@ module prim_generic_ram_2p #(
   always @(posedge clk_b_i) begin
     if (b_req_i) begin
       if (b_write_i) begin
-        mem[b_addr_i] <= b_wdata_i;
         for (int i=0; i < MaskWidth; i = i + 1) begin
           if (b_wmask[i]) begin
             mem[b_addr_i][i*DataBitsPerMask +: DataBitsPerMask] <=
