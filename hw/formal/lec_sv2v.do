4c0047d9e966eebbef7275b8f9c0c099d064357a
Nils Graf <nilsg@google.com>
2020-06-14 18:52:13 -0700

    Update syn_yosys.sh for latest RTL and latest sv2v version. This includes a few hacks (before and after running sv2v tool). With these hacks, LEC passes for all submodules of top_earlgrey.

--- hw/formal/lec_sv2v.do
+++ hw/formal/lec_sv2v.do
@@ -4,47 +4,47 @@
 
 // dofile for LEC script lec_sv2v
 
-//-----------------------------------------------------------------
+//-------------------------------------------------------------------------
 // read in golden (SystemVerilog) and revised (Verilog)
-//-----------------------------------------------------------------
+//-------------------------------------------------------------------------
+set parallel option -threads 8
 
-// black box all instantiated modules (so only top-module is used)
-set undefined cell black_box
+// map all multi-dimensional ports (including structs) onto 1-dim. ports
+set naming rule -mdportflatten
 
-// read golden design
-read design -golden -sv09 \
-  $LEC_DIR/prim_assert.sv \
-  $LEC_DIR/top_pkg.sv \
-  $LEC_DIR/tlul_pkg.sv \
-  $LEC_DIR/*_pkg.sv \
-  $LEC_DIR/$LEC_TOP.sv
+read design -golden -sv09 -f flist_gold -rootonly -root $LEC_TOP
+read design -revised -sys -f flist_rev  -rootonly -root $LEC_TOP
+// TODO: instead of using switch -sys (for old SystemVerilog,
+// older than sv2009) we should use -ve (for Verilog). But
+// currently that doesn't work because sv2v doesn't translate
+// .* port connections. Is that an sv2v bug?
 
-// read revised design
-read design -revised -verilog \
-  $LEC_DIR/$LEC_TOP.v
-
-//-----------------------------------------------------------------
+//-------------------------------------------------------------------------
 // pre-LEC reports
-//-----------------------------------------------------------------
+//-------------------------------------------------------------------------
+report rule check -verbose
 report design data
 report black box
 report module
 
-//-----------------------------------------------------------------
+//-------------------------------------------------------------------------
 // compare
-//-----------------------------------------------------------------
+//-------------------------------------------------------------------------
 set system mode lec
+
+// map key points and I/O ports: because Verilog only supports
+// 1-dimensional I/O ports, convert all multi-dim. ports to 1-dim.
+set mapping method -nets -mem -unreach
 map key points
-set parallel option -threads 4
-analyze datapath -merge -verbose -effort ultra
+report unmapped points
+
 add compare point -all
-set compare effort ultra
-compare -threads 4
+compare -threads 8 -noneq_stop 1
 analyze abort -compare
 
-//-----------------------------------------------------------------
+//-------------------------------------------------------------------------
 // reports
-//-----------------------------------------------------------------
+//-------------------------------------------------------------------------
 report compare data -class nonequivalent -class abort -class notcompared
 report verification -verbose
 report statistics
--- util/syn_yosys.sh
+++ util/syn_yosys.sh
@@ -10,59 +10,139 @@
 # The following tools are required:
 #  - sv2v: SystemVerilog-to-Verilog converter from github.com/zachjs/sv2v
 #  - yosys: synthesis tool from github.com/YosysHQ/yosys
+#  - Cadence Conformal
 #
 # Usage:
-#   syn_yosys.sh 2>&1 | tee syn.std
+#   ./syn_yosys.sh |& tee syn.std
 
 #-------------------------------------------------------------------------
 # use fusesoc to generate files and file list
 #-------------------------------------------------------------------------
-\rm -Rf build
-fusesoc --cores-root .. run --target=sim --setup --build formal > /dev/null 2>&1
+rm -Rf build syn_out
+
+fusesoc --cores-root .. run --tool=icarus --target=syn \
+  --setup "lowrisc:systems:top_earlgrey" > /dev/null 2>&1
 
 # copy all files into directory "syn_out"
-\rm -Rf syn_out
 mkdir syn_out
-cp build/formal_0/src/*/*/*.sv build/formal_0/src/*/*/*/*.sv syn_out
+cp build/*/src/*/*.sv* build/*/src/*/*/*.sv* build/*/src/*/*/*/*.sv* syn_out
+cd syn_out
+
+# copy file list and remove incdir lines
+grep -v incdir ../build/*/*/*.scr > flist_gold
+
+# remove all hierarchical paths
+sed -i 's!.*/!!' flist_gold
+
+# generate revised flist by replacing '.sv' by '.v' and removing packages
+sed 's/.sv/.v/g' flist_gold | grep -v "_pkg.v" > flist_rev
 
 #-------------------------------------------------------------------------
 # convert all RTL files to Verilog
 #-------------------------------------------------------------------------
-cd syn_out
 
+# hack SystemVerilog files to avoid SV2V and/or LEC issues
+# TODO: eventually remove these hacks
+
+# 1) Setting ByteAccess = 0 for tlul_adapter_sram.sv doesn't work. This
+# hack changes the functionality so needs fixing
+sed -i 's/\.ByteAccess(0)/\.ByteAccess(1)/g' *.sv
+sed -i 's/\.ByteAccess  (0)/\.ByteAccess(1)/g' *.sv
+
+# 2) Replace "inside" experessions
+sed -i 's/state_q inside {Read, WaitReadValid}/((state_q == Read) || (state_q == WaitReadValid))/' dmi_jtag.sv
+sed -i 's/k inside {TckIdx, TmsIdx, TrstIdx, SrstIdx, TdiIdx, TdoIdx}/(k==TckIdx)||(k==TmsIdx)||(k==TrstIdx)||(k==SrstIdx)||(k==TdiIdx)||(k==TdoIdx)/' jtag_mux.sv
+sed -i 's/(csr_op_i inside {CSR_OP_WRITE,/((csr_op_i==CSR_OP_WRITE)||(csr_op_i==CSR_OP_SET)||(csr_op_i==CSR_OP_CLEAR));/' ibex_cs_registers.sv
+sed -i 's/    CSR_OP_SET,//' ibex_cs_registers.sv
+sed -i 's/    CSR_OP_CLEAR});//' ibex_cs_registers.sv
+sed -i "s/logic'(tl_i\.a_opcode inside {PutFullData, PutPartialData})/((tl_i\.a_opcode == PutFullData) || (tl_i\.a_opcode == PutFullData))/" tlul_adapter_sram.sv
+
+# 3) Replace "inside" for dm_csrs.sv and dm_mem.sv. This hack changes
+# the functionality so needs fixing
+sed -i 's/) inside/)/g' dm_csrs.sv
+sed -i 's/\[(dm::Data0):DataEnd\]:/dm::Data0:/g' dm_csrs.sv
+sed -i 's/\[(dm::ProgBuf0):ProgBufEnd\]:/dm::ProgBuf0:/g' dm_csrs.sv
+sed -i 's/inside//g' dm_mem.sv
+sed -i 's/\[(dm::DataAddr):DataEndAddr\]:/dm::DataAddr:/' dm_mem.sv
+sed -i 's/\[DataBaseAddr:DataEndAddr\]:/DataBaseAddr:/' dm_mem.sv
+sed -i 's/\[ProgBufBaseAddr:ProgBufEndAddr\]:/ProgBufBaseAddr:/' dm_mem.sv
+sed -i 's/\[AbstractCmdBaseAddr:AbstractCmdEndAddr\]:/AbstractCmdBaseAddr:/' dm_mem.sv
+sed -i 's/\[FlagsBaseAddr:FlagsEndAddr\]:/FlagsBaseAddr:/' dm_mem.sv
+
+printf "\nSV2V VERSION:\n"
+sv2v --version
+
+printf "\nSV2V ERRORS:\n"
 for file in *.sv; do
   module=`basename -s .sv $file`
-  echo $file
-  sv2v --define=VERILATOR --define=SYNTHESIS *_pkg.sv prim_assert.sv $file > ${module}.v
-
-  # TODO: eventually remove below hack. It removes "unsigned" from params
-  # because Yosys doesn't support unsigned parameters
-  sed -i 's/parameter unsigned/parameter/g' ${module}.v
-  sed -i 's/localparam unsigned/localparam/g' ${module}.v
+  sv2v --define=SYNTHESIS *_pkg.sv prim_assert.sv $file > ${module}.v
 done
 
-# remove *pkg.v files (they are empty files and not needed)
+# postprocess generated Verilog to fix some more sv2v issues:
+# TODO: remove below hacks eventually
+sed -i "s/1'sb0/'d0/g" *.v
+sed -i "s/1'sb1/-'sd1/g" *.v
+sed -i 's/parameter ResetValue/parameter [0:0] ResetValue/' prim_flop_2sync.v
+
+# remove *pkg.v files (they are empty files and not needed for Verilog)
 \rm -Rf *_pkg.v
 
 #-------------------------------------------------------------------------
 # run LEC (generarted Verilog vs. original SystemVerilog)
 #-------------------------------------------------------------------------
 printf "\n\nLEC RESULTS:\n"
-cd ../../hw/formal
-for file in ../../util/syn_out/*.v; do
-  module=`basename -s .v $file`
-  lec_sv2v ../../util/syn_out $module > /dev/null 2>&1
+
+# top_earlgrey and all its submodules
+declare -a modules=(
+  "rv_dm"
+  "spi_device"
+  "usbdev"
+  "flash_ctrl"
+  "tlul_adapter_sram"
+  "prim_rom_adv"
+  "prim_ram_1p_adv"
+  "uart"
+  "gpio"
+  "aes"
+  "hmac"
+  "pinmux"
+  "padctrl"
+  "alert_handler"
+  "pwrmgr"
+  "rstmgr"
+  "clkmgr"
+  "nmi_gen"
+  "rv_timer"
+  "rv_plic"
+  "rv_core_ibex"
+  "xbar_main"
+  "xbar_peri"
+  "flash_phy"
+  "top_earlgrey"
+)
+
+# uncomment below two lines if you want to run LEC on all modules
+# for file in *.v; do
+#   export LEC_TOP=`basename -s .v $file`
+
+for module in "${modules[@]}"; do
+  export LEC_TOP="$module"
+
+  # run Conformal LEC
+  lec -xl -nogui -nobanner \
+    -dofile  ../../hw/formal/lec_sv2v.do \
+    -logfile lec_${LEC_TOP}.log \
+    <<< "exit -force" > /dev/null 2>&1
 
   # summarize results
-  result=`grep "Compare Results" lec_${module}.log`
+  result=`grep "Compare Results" lec_${LEC_TOP}.log`
   if [ $? -ne 0 ]; then
     result="CRASH"
   else
     result=`echo $result | awk '{ print $4 }'`
   fi
-  printf "%-25s %s\n" $module $result
+  printf "%-25s %s\n" $LEC_TOP $result
 done
-cd -
 
 #-------------------------------------------------------------------------
 # run yosys
