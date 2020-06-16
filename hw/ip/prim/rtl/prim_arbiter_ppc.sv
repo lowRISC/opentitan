f3a25a80ce42c6bb33d1a31b4f25ddda4dc1002b
Michael Schaffner <msf@google.com>
2020-06-16 15:21:25 -0700

    [prim_arbiter_ppc] Add more FPV fairness checks
    This moves the RoundRobin_A out of the generate loop such that it is
    more generic and can be copied for other RR arbiters as well.
    
    Further, this adds two new FPV checks:
    1) NoStarvation_A: this property checks whether all requests will be
    granted eventually if they do not drop out (i.e. remain asserted until
    granted)
    2) Fairness_A: this property checks for strict fairness, where strict
    fairness is defined as: if N requests are asserted constantly and
    downstream is ready, each of these requests will be granted exactly once
    if we wait for N cycles.
    
    Also, the input request stability assertion can now optionally be
    disabled using the EnReqStabA parameter if needed. The arbiter works
    correctly either way, but this assertion might be useful in bus
    protocols where the requests must remain stable when back pressured.
    
    Signed-off-by: Michael Schaffner <msf@google.com>

--- hw/ip/prim/rtl/prim_arbiter_ppc.sv
+++ hw/ip/prim/rtl/prim_arbiter_ppc.sv
@@ -5,45 +5,51 @@
 // N:1 arbiter module
 //
 // Verilog parameter
-//   N:    Number of request ports
-//   DW:   Data width
+//   N:           Number of request ports
+//   DW:          Data width
+//   DataPort:    Set to 1 to enable the data port. Otherwise that port will be ignored.
+//   EnReqStabA:  Checks whether requests remain asserted until granted
 //
-// This is the original implementation of the arbiter which relies on parallel prefix
-// computing optimization to optimize the request / arbiter tree. Not all synthesis tools
-// may support this.
+// This is the original implementation of the arbiter which relies on parallel prefix computing
+// optimization to optimize the request / arbiter tree. Not all synthesis tools may support this.
 //
-// Note that the currently winning request is held if the data sink is not ready.
-// This behavior is required by some interconnect protocols (AXI, TL). Note that
-// this implies that an asserted request must stay asserted
-// until it has been granted. Note that for PPC, this option cannot
-// be disabled.
+// Note that the currently winning request is held if the data sink is not ready. This behavior is
+// required by some interconnect protocols (AXI, TL). The module contains an assertion that checks
+// this behavior.
+//
+// Also, this module contains a request stability assertion that checks that requests stay asserted
+// until they have been served. This assertion can be optionally disabled by setting EnReqStabA to
+// zero. This is a non-functional parameter and does not affect the designs behavior.
 //
 // See also: prim_arbiter_tree
 
 `include "prim_assert.sv"
 
 module prim_arbiter_ppc #(
-  parameter int unsigned N  = 4,
+  parameter int unsigned N  = 8,
   parameter int unsigned DW = 32,
 
   // Configurations
   // EnDataPort: {0, 1}, if 0, input data will be ignored
-  parameter int EnDataPort = 1,
+  parameter bit EnDataPort = 1,
+
+  // Non-functional parameter to switch on the request stability assertion
+  parameter bit EnReqStabA = 1,
 
   // Derived parameters
-  localparam int unsigned IdxW = $clog2(N)
+  localparam int IdxW = $clog2(N)
 ) (
   input clk_i,
   input rst_ni,
 
-  input        [ N-1:0]        req_i,
-  input        [DW-1:0]        data_i [N],
-  output logic [ N-1:0]        gnt_o,
-  output logic [IdxW-1:0]      idx_o,
+  input        [ N-1:0]    req_i,
+  input        [DW-1:0]    data_i [N],
+  output logic [ N-1:0]    gnt_o,
+  output logic [IdxW-1:0]  idx_o,
 
-  output logic          valid_o,
-  output logic [DW-1:0] data_o,
-  input                 ready_i
+  output logic             valid_o,
+  output logic [DW-1:0]    data_o,
+  input                    ready_i
 );
 
   `ASSERT_INIT(CheckNGreaterZero_A, N > 0)
@@ -118,20 +124,23 @@ module prim_arbiter_ppc #(
         end
       end
     end
-
-    ////////////////
-    // assertions //
-    ////////////////
-    // grant shall be higher index than prev. unless no higher requests exist
-    `ASSERT(RoundRobin_A, valid_o && ready_i && $past(ready_i) && $past(valid_o) &&
-        |(masked_req) |-> idx_o > $past(idx_o))
-
   end
 
   ////////////////
   // assertions //
   ////////////////
 
+  // KNOWN assertions on outputs, except for data as that may be partially X in simulation
+  // e.g. when used on a BUS
+  `ASSERT_KNOWN(ValidKnown_A, valid_o)
+  `ASSERT_KNOWN(GrantKnown_A, gnt_o)
+  `ASSERT_KNOWN(IdxKnown_A, idx_o)
+
+  // grant index shall be higher index than previous index, unless no higher requests exist.
+  `ASSERT(RoundRobin_A,
+      ##1 valid_o && ready_i && $past(ready_i) && $past(valid_o) &&
+      |(req_i & ~((N'(1) << $past(idx_o)+1) - 1)) |->
+      idx_o > $past(idx_o))
   // we can only grant one requestor at a time
   `ASSERT(CheckHotOne_A, $onehot0(gnt_o))
   // A grant implies that the sink is ready
@@ -148,27 +157,69 @@ module prim_arbiter_ppc #(
   `ASSERT(NoReadyValidNoGrant_A, !(ready_i || valid_o) |-> gnt_o == 0)
   // check index / grant correspond
   `ASSERT(IndexIsCorrect_A, ready_i && valid_o |-> gnt_o[idx_o] && req_i[idx_o])
+
+if (EnDataPort) begin: gen_data_port_assertion
   // data flow
   `ASSERT(DataFlow_A, ready_i && valid_o |-> data_o == data_i[idx_o])
-  // KNOWN assertions on outputs, except for data as that may be partially X in simulation
-  // e.g. when used on a BUS
-  `ASSERT_KNOWN(ValidKnown_A, valid_o)
-  `ASSERT_KNOWN(GrantKnown_A, gnt_o)
-  `ASSERT_KNOWN(IdxKnown_A, idx_o)
+end
 
-`ifndef SYNTHESIS
-  // A grant implies a request
-  int unsigned k; // this is a symbolic variable
-  `ASSUME(KStable_M, ##1 $stable(k), clk_i, !rst_ni)
-  `ASSUME(KRange_M, k < N, clk_i, !rst_ni)
-  `ASSERT(GntImpliesReq_A, gnt_o[k] |-> req_i[k])
-
+if (EnReqStabA) begin : gen_lock_assertion
   // requests must stay asserted until they have been granted
-  `ASSUME(ReqStaysHighUntilGranted_M, (|req_i) && !ready_i |=>
+  `ASSUME(ReqStaysHighUntilGranted0_M, (|req_i) && !ready_i |=>
       (req_i & $past(req_i)) == $past(req_i))
   // check that the arbitration decision is held if the sink is not ready
   `ASSERT(LockArbDecision_A, |req_i && !ready_i |=> idx_o == $past(idx_o))
+end
 
+// FPV-only assertions with symbolic variables
+`ifdef FPV_ON
+  // symbolic variables
+  int unsigned k;
+  bit ReadyIsStable;
+  bit ReqsAreStable;
+
+  // constraints for symbolic variables
+  `ASSUME(KStable_M, ##1 $stable(k))
+  `ASSUME(KRange_M, k < N)
+  // this is used enable checking for stable and unstable ready_i and req_i signals in the same run.
+  // the symbolic variables act like a switch that the solver can trun on and off.
+  `ASSUME(ReadyIsStable_M, ##1 $stable(ReadyIsStable))
+  `ASSUME(ReqsAreStable_M, ##1 $stable(ReqsAreStable))
+  `ASSUME(ReadyStable_M, ##1 !ReadyIsStable || $stable(ready_i))
+  `ASSUME(ReqsStable_M, ##1 !ReqsAreStable || $stable(req_i))
+
+  // A grant implies a request
+  `ASSERT(GntImpliesReq_A, gnt_o[k] |-> req_i[k])
+
+  // if request and ready are constantly held at 1, we should eventually get a grant
+  `ASSERT(NoStarvation_A,
+      ReqsAreStable && ReadyIsStable && ready_i && req_i[k] |->
+      strong(##[0:$] gnt_o[k]))
+
+  // if N requests are constantly asserted and ready is constant 1, each request must
+  // be granted exactly once over a time window of N cycles for the arbiter to be fair.
+  for (genvar n = 1; n <= N; n++) begin : gen_fairness
+    integer gnt_cnt;
+    `ASSERT(Fairness_A,
+        ReqsAreStable && ReadyIsStable && ready_i && req_i[k] &&
+        $countones(req_i) == n |->
+        ##n gnt_cnt == $past(gnt_cnt, n) + 1)
+
+    always_ff @(posedge clk_i or negedge rst_ni) begin : p_cnt
+      if (!rst_ni) begin
+        gnt_cnt <= 0;
+      end else begin
+        gnt_cnt <= gnt_cnt + gnt_o[k];
+      end
+    end
+  end
+
+  if (EnReqStabA) begin : gen_lock_assertion_fpv
+    // requests must stay asserted until they have been granted
+    `ASSUME(ReqStaysHighUntilGranted1_M, req_i[k] & !gnt_o[k] |=>
+        req_i[k], clk_i, !rst_ni)
+  end
 `endif
 
-endmodule
+endmodule : prim_arbiter_ppc
+
