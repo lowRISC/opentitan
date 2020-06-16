753a7b0526f8c70b7727793ca2c089e9cdb871a8
Michael Schaffner <msf@google.com>
2020-06-16 15:23:21 -0700

    [prim_arbiter_*] Propagate parameter changes
    This propagates the parameter changes in prim_arbiter_* to all affected
    modules.
    
    Signed-off-by: Michael Schaffner <msf@google.com>

--- hw/ip/prim/fpv/tb/prim_arbiter_ppc_fpv.sv
+++ hw/ip/prim/fpv/tb/prim_arbiter_ppc_fpv.sv
@@ -6,24 +6,31 @@
 // Intended to be used with a formal tool.
 
 module prim_arbiter_ppc_fpv #(
-  parameter int unsigned N = 4,
-  parameter int unsigned DW = 32
+  parameter int unsigned N = 8,
+  parameter int unsigned DW = 32,
+  parameter bit EnDataPort = 1,
+  parameter bit EnReqStabA = 1,
+  localparam int IdxW = $clog2(N)
 ) (
-  input  clk_i,
-  input  rst_ni,
-  input [N-1:0] req_i,
-  input [DW-1:0]data_i [N],
-  output logic[N-1:0] gnt_o,
-  output logic[$clog2(N)-1:0] idx_o,
-  output logic valid_o,
-  output logic[DW-1:0] data_o,
-  input  ready_i
+  input clk_i,
+  input rst_ni,
+
+  input        [ N-1:0]    req_i,
+  input        [DW-1:0]    data_i [N],
+  output logic [ N-1:0]    gnt_o,
+  output logic [IdxW-1:0]  idx_o,
+
+  output logic             valid_o,
+  output logic [DW-1:0]    data_o,
+  input                    ready_i
 );
 
 
   prim_arbiter_ppc #(
     .N(N),
-    .DW(DW)
+    .DW(DW),
+    .EnDataPort(EnDataPort),
+    .EnReqStabA(EnReqStabA)
   ) i_prim_arbiter_ppc (
     .clk_i,
     .rst_ni,
