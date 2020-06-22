9f4f15a7453534a2eb87987fa6437ea8e54bf224
Philipp Wagner <phw@lowrisc.org>
2020-06-22 11:34:00 +0100

    [otbn] Initial OTBN implementation skeleton
    This commit is the initial OTBN toplevel file, containing
    - an 32b wide instruction memory SRAM block (IMEM) of 4 kB,
    - a 256b wide data memory SRAM block (DMEM) of 4 kB,
    - an interrupt, alert, and register making OTBN accessible as
      device from the main bus, allowing Ibex to load IMEM and DMEM, start
      the operation on OTBN, and check its status.
    
    The commit also contains a dummy for the OTBN processing core,
    `otbn_core.sv`. This file doesn't do anything yet.
    
    Signed-off-by: Philipp Wagner <phw@lowrisc.org>

---
+++ hw/ip/otbn/rtl/otbn_core.sv
@@ -0,0 +1,56 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+`include "prim_assert.sv"
+
+/**
+ * OpenTitan Big Number Accelerator (OTBN) Core
+ *
+ * This module is the top-level of the OTBN processing core.
+ */
+module otbn_core
+  import otbn_pkg::*;
+#(
+  // Size of the instruction memory, in bytes
+  parameter int ImemSizeByte = 4096,
+  // Size of the data memory, in bytes
+  parameter int DmemSizeByte = 4096,
+
+  localparam int ImemAddrWidth = prim_util_pkg::vbits(ImemSizeByte),
+  localparam int DmemAddrWidth = prim_util_pkg::vbits(DmemSizeByte)
+)(
+  input  logic  clk_i,
+  input  logic  rst_ni,
+
+  input  logic  start_i, // start the operation
+  output logic  done_o,  // operation done
+
+  input  logic [ImemAddrWidth-1:0] start_addr_i, // start byte address in IMEM
+
+  // Instruction memory (IMEM)
+  output logic                     imem_req_o,
+  output logic                     imem_write_o,
+  output logic [ImemAddrWidth-1:0] imem_addr_o,
+  output logic [31:0]              imem_wdata_o,
+  input  logic [31:0]              imem_rdata_i,
+  input  logic                     imem_rvalid_i,
+  input  logic [1:0]               imem_rerror_i,
+
+  // Data memory (DMEM)
+  output logic                     dmem_req_o,
+  output logic                     dmem_write_o,
+  output logic [DmemAddrWidth-1:0] dmem_addr_o,
+  output logic [WLEN-1:0]          dmem_wdata_o,
+  output logic [WLEN-1:0]          dmem_wmask_o,
+  input  logic [WLEN-1:0]          dmem_rdata_i,
+  input  logic                     dmem_rvalid_i,
+  input  logic [1:0]               dmem_rerror_i
+);
+
+  // TODO: This is probably not the final OTBN implementation.
+
+  assign imem_req_o = 1'b0;
+  assign dmem_req_o = 1'b0;
+  assign done_o = 1'b0;
+endmodule
---
+++ hw/ip/otbn/rtl/otbn_pkg.sv
@@ -0,0 +1,21 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+package otbn_pkg;
+  // Data path width for BN (wide) instructions, in bit.
+  parameter int WLEN = 256;
+
+  // Alerts
+  parameter int                   NumAlerts = 3;
+  parameter logic [NumAlerts-1:0] AlertAsyncOn = {NumAlerts{1'b1}};
+
+  parameter int AlertImemUncorrectable = 0;
+  parameter int AlertDmemUncorrectable = 1;
+  parameter int AlertRegUncorrectable = 2;
+
+  // Error codes
+  typedef enum logic [31:0] {
+    ErrCodeNoError = 32'h 0000_0000
+  } err_code_e;
+
+endpackage
