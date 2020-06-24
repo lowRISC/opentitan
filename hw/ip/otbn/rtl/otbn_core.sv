e2772d930cbb6f75927fbda5d68a3bfca046e00a
Stefan Wallentowitz <stefan.wallentowitz@gi-de.com>
2020-06-24 15:25:10 +0200

    [otbn] Call OTBN python model from simulation
    Use DPI to execute the Python model, the call is
    run-to-completion. The input instructions and data are stored in a
    file, then the simulator run and the result transfered back. This
    works well as there is no interaction in-between at this stage.
    
    Signed-off-by: Stefan Wallentowitz <stefan.wallentowitz@gi-de.com>

--- hw/ip/otbn/rtl/otbn_core.sv
+++ hw/ip/otbn/rtl/otbn_core.sv
@@ -48,9 +48,40 @@ module otbn_core
   input  logic [1:0]               dmem_rerror_i
 );
 
-  // TODO: This is probably not the final OTBN implementation.
+  localparam OTBN_MODEL = 1; // TODO: Instead use package?
 
-  assign imem_req_o = 1'b0;
-  assign dmem_req_o = 1'b0;
-  assign done_o = 1'b0;
+  // TODO: This is probably not the final OTBN implementation.
+  generate
+    if (OTBN_MODEL) begin
+      import "DPI-C" context function int run_model(string imem_scope,
+                                             int    imem_size,
+                                             string dmem_scope,
+                                             int    dmem_size);
+
+      localparam ImemSizeWords = ImemSizeByte / 4;
+      localparam DmemSizeWords = DmemSizeByte / WLEN / 8;
+
+      int count;
+
+      always @(posedge clk_i) begin : model_run
+        if (!rst_ni) begin
+          done_o <= 1'b0;
+        end else begin
+          if (start_i) begin
+            run_model("TOP.top_earlgrey_verilator.top_earlgrey.u_otbn.u_imem.u_mem.gen_generic.u_impl_generic",
+                      ImemSizeWords,
+                      "TOP.top_earlgrey_verilator.top_earlgrey.u_otbn.u_dmem.u_mem.gen_generic.u_impl_generic",
+                      DmemSizeWords);
+            done_o <= 1'b1;
+          end else begin
+            done_o <= 1'b0;
+          end
+        end
+      end
+    end else begin
+      assign imem_req_o = 1'b0;
+      assign dmem_req_o = 1'b0;
+      assign done_o = 1'b0;
+    end
+  endgenerate
 endmodule
