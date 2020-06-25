ac73ba5654dab9fb7c9e8829a8688114fe09055d
Stefan Wallentowitz <stefan.wallentowitz@gi-de.com>
2020-06-25 11:05:29 +0200

    [otbn] Add python simulator
    This adds the first iteration of the python simulator for OTBN. Right
    now it only has RV32I, but it lays the fundamental groundwork.
    
    Before using it, you need to run `pip install -r
    python-requirements.txt` again.
    
    The base model is an external dependency (pypi package) plus a local
    package with the program that performs the required operations. Later
    the local package will have the overloads for the OTBN model.
    
    Signed-off-by: Stefan Wallentowitz <stefan.wallentowitz@gi-de.com>

---
+++ util/otbnsim/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = "otbnsim",
+  packages=find_packages(),
+  install_requires=["riscv-model>=0.4"],
+  entry_points={
+    "console_scripts": [
+        "otbn-python-model = otbnsim.main:main",
+    ],
+  }
+)
