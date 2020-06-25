ac62065b32ef3d30f8a1c20801d7a69f0eb6b091
Timothy Chen <timothytim@google.com>
2020-06-25 13:48:50 -0700

    [flash/top] Generated files and scrambling key connections
    - OTP does not exist yet in master, so the keys are hardwired
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- hw/ip/flash_ctrl/rtl/flash_ctrl_reg_pkg.sv
+++ hw/ip/flash_ctrl/rtl/flash_ctrl_reg_pkg.sv
@@ -105,6 +105,10 @@ package flash_ctrl_reg_pkg;
   } flash_ctrl_reg2hw_addr_reg_t;
 
   typedef struct packed {
+    logic        q;
+  } flash_ctrl_reg2hw_scramble_en_reg_t;
+
+  typedef struct packed {
     struct packed {
       logic        q;
     } en;
@@ -240,11 +244,12 @@ package flash_ctrl_reg_pkg;
   // Register to internal design logic //
   ///////////////////////////////////////
   typedef struct packed {
-    flash_ctrl_reg2hw_intr_state_reg_t intr_state; // [312:307]
-    flash_ctrl_reg2hw_intr_enable_reg_t intr_enable; // [306:301]
-    flash_ctrl_reg2hw_intr_test_reg_t intr_test; // [300:289]
-    flash_ctrl_reg2hw_control_reg_t control; // [288:272]
-    flash_ctrl_reg2hw_addr_reg_t addr; // [271:240]
+    flash_ctrl_reg2hw_intr_state_reg_t intr_state; // [313:308]
+    flash_ctrl_reg2hw_intr_enable_reg_t intr_enable; // [307:302]
+    flash_ctrl_reg2hw_intr_test_reg_t intr_test; // [301:290]
+    flash_ctrl_reg2hw_control_reg_t control; // [289:273]
+    flash_ctrl_reg2hw_addr_reg_t addr; // [272:241]
+    flash_ctrl_reg2hw_scramble_en_reg_t scramble_en; // [240:240]
     flash_ctrl_reg2hw_mp_region_cfg_mreg_t [7:0] mp_region_cfg; // [239:48]
     flash_ctrl_reg2hw_default_region_reg_t default_region; // [47:45]
     flash_ctrl_reg2hw_mp_bank_cfg_mreg_t [1:0] mp_bank_cfg; // [44:43]
@@ -271,28 +276,29 @@ package flash_ctrl_reg_pkg;
   parameter logic [6:0] FLASH_CTRL_CTRL_REGWEN_OFFSET = 7'h c;
   parameter logic [6:0] FLASH_CTRL_CONTROL_OFFSET = 7'h 10;
   parameter logic [6:0] FLASH_CTRL_ADDR_OFFSET = 7'h 14;
-  parameter logic [6:0] FLASH_CTRL_REGION_CFG_REGWEN_OFFSET = 7'h 18;
-  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG0_OFFSET = 7'h 1c;
-  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG1_OFFSET = 7'h 20;
-  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG2_OFFSET = 7'h 24;
-  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG3_OFFSET = 7'h 28;
-  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG4_OFFSET = 7'h 2c;
-  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG5_OFFSET = 7'h 30;
-  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG6_OFFSET = 7'h 34;
-  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG7_OFFSET = 7'h 38;
-  parameter logic [6:0] FLASH_CTRL_DEFAULT_REGION_OFFSET = 7'h 3c;
-  parameter logic [6:0] FLASH_CTRL_BANK_CFG_REGWEN_OFFSET = 7'h 40;
-  parameter logic [6:0] FLASH_CTRL_MP_BANK_CFG_OFFSET = 7'h 44;
-  parameter logic [6:0] FLASH_CTRL_OP_STATUS_OFFSET = 7'h 48;
-  parameter logic [6:0] FLASH_CTRL_STATUS_OFFSET = 7'h 4c;
-  parameter logic [6:0] FLASH_CTRL_SCRATCH_OFFSET = 7'h 50;
-  parameter logic [6:0] FLASH_CTRL_FIFO_LVL_OFFSET = 7'h 54;
-  parameter logic [6:0] FLASH_CTRL_FIFO_RST_OFFSET = 7'h 58;
+  parameter logic [6:0] FLASH_CTRL_SCRAMBLE_EN_OFFSET = 7'h 18;
+  parameter logic [6:0] FLASH_CTRL_REGION_CFG_REGWEN_OFFSET = 7'h 1c;
+  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG0_OFFSET = 7'h 20;
+  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG1_OFFSET = 7'h 24;
+  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG2_OFFSET = 7'h 28;
+  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG3_OFFSET = 7'h 2c;
+  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG4_OFFSET = 7'h 30;
+  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG5_OFFSET = 7'h 34;
+  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG6_OFFSET = 7'h 38;
+  parameter logic [6:0] FLASH_CTRL_MP_REGION_CFG7_OFFSET = 7'h 3c;
+  parameter logic [6:0] FLASH_CTRL_DEFAULT_REGION_OFFSET = 7'h 40;
+  parameter logic [6:0] FLASH_CTRL_BANK_CFG_REGWEN_OFFSET = 7'h 44;
+  parameter logic [6:0] FLASH_CTRL_MP_BANK_CFG_OFFSET = 7'h 48;
+  parameter logic [6:0] FLASH_CTRL_OP_STATUS_OFFSET = 7'h 4c;
+  parameter logic [6:0] FLASH_CTRL_STATUS_OFFSET = 7'h 50;
+  parameter logic [6:0] FLASH_CTRL_SCRATCH_OFFSET = 7'h 54;
+  parameter logic [6:0] FLASH_CTRL_FIFO_LVL_OFFSET = 7'h 58;
+  parameter logic [6:0] FLASH_CTRL_FIFO_RST_OFFSET = 7'h 5c;
 
   // Window parameter
-  parameter logic [6:0] FLASH_CTRL_PROG_FIFO_OFFSET = 7'h 5c;
+  parameter logic [6:0] FLASH_CTRL_PROG_FIFO_OFFSET = 7'h 60;
   parameter logic [6:0] FLASH_CTRL_PROG_FIFO_SIZE   = 7'h 4;
-  parameter logic [6:0] FLASH_CTRL_RD_FIFO_OFFSET = 7'h 60;
+  parameter logic [6:0] FLASH_CTRL_RD_FIFO_OFFSET = 7'h 64;
   parameter logic [6:0] FLASH_CTRL_RD_FIFO_SIZE   = 7'h 4;
 
   // Register Index
@@ -303,6 +309,7 @@ package flash_ctrl_reg_pkg;
     FLASH_CTRL_CTRL_REGWEN,
     FLASH_CTRL_CONTROL,
     FLASH_CTRL_ADDR,
+    FLASH_CTRL_SCRAMBLE_EN,
     FLASH_CTRL_REGION_CFG_REGWEN,
     FLASH_CTRL_MP_REGION_CFG0,
     FLASH_CTRL_MP_REGION_CFG1,
@@ -323,30 +330,31 @@ package flash_ctrl_reg_pkg;
   } flash_ctrl_id_e;
 
   // Register width information to check illegal writes
-  parameter logic [3:0] FLASH_CTRL_PERMIT [23] = '{
+  parameter logic [3:0] FLASH_CTRL_PERMIT [24] = '{
     4'b 0001, // index[ 0] FLASH_CTRL_INTR_STATE
     4'b 0001, // index[ 1] FLASH_CTRL_INTR_ENABLE
     4'b 0001, // index[ 2] FLASH_CTRL_INTR_TEST
     4'b 0001, // index[ 3] FLASH_CTRL_CTRL_REGWEN
     4'b 1111, // index[ 4] FLASH_CTRL_CONTROL
     4'b 1111, // index[ 5] FLASH_CTRL_ADDR
-    4'b 0001, // index[ 6] FLASH_CTRL_REGION_CFG_REGWEN
-    4'b 1111, // index[ 7] FLASH_CTRL_MP_REGION_CFG0
-    4'b 1111, // index[ 8] FLASH_CTRL_MP_REGION_CFG1
-    4'b 1111, // index[ 9] FLASH_CTRL_MP_REGION_CFG2
-    4'b 1111, // index[10] FLASH_CTRL_MP_REGION_CFG3
-    4'b 1111, // index[11] FLASH_CTRL_MP_REGION_CFG4
-    4'b 1111, // index[12] FLASH_CTRL_MP_REGION_CFG5
-    4'b 1111, // index[13] FLASH_CTRL_MP_REGION_CFG6
-    4'b 1111, // index[14] FLASH_CTRL_MP_REGION_CFG7
-    4'b 0001, // index[15] FLASH_CTRL_DEFAULT_REGION
-    4'b 0001, // index[16] FLASH_CTRL_BANK_CFG_REGWEN
-    4'b 0001, // index[17] FLASH_CTRL_MP_BANK_CFG
-    4'b 0001, // index[18] FLASH_CTRL_OP_STATUS
-    4'b 0111, // index[19] FLASH_CTRL_STATUS
-    4'b 1111, // index[20] FLASH_CTRL_SCRATCH
-    4'b 0011, // index[21] FLASH_CTRL_FIFO_LVL
-    4'b 0001  // index[22] FLASH_CTRL_FIFO_RST
+    4'b 0001, // index[ 6] FLASH_CTRL_SCRAMBLE_EN
+    4'b 0001, // index[ 7] FLASH_CTRL_REGION_CFG_REGWEN
+    4'b 1111, // index[ 8] FLASH_CTRL_MP_REGION_CFG0
+    4'b 1111, // index[ 9] FLASH_CTRL_MP_REGION_CFG1
+    4'b 1111, // index[10] FLASH_CTRL_MP_REGION_CFG2
+    4'b 1111, // index[11] FLASH_CTRL_MP_REGION_CFG3
+    4'b 1111, // index[12] FLASH_CTRL_MP_REGION_CFG4
+    4'b 1111, // index[13] FLASH_CTRL_MP_REGION_CFG5
+    4'b 1111, // index[14] FLASH_CTRL_MP_REGION_CFG6
+    4'b 1111, // index[15] FLASH_CTRL_MP_REGION_CFG7
+    4'b 0001, // index[16] FLASH_CTRL_DEFAULT_REGION
+    4'b 0001, // index[17] FLASH_CTRL_BANK_CFG_REGWEN
+    4'b 0001, // index[18] FLASH_CTRL_MP_BANK_CFG
+    4'b 0001, // index[19] FLASH_CTRL_OP_STATUS
+    4'b 0111, // index[20] FLASH_CTRL_STATUS
+    4'b 1111, // index[21] FLASH_CTRL_SCRATCH
+    4'b 0011, // index[22] FLASH_CTRL_FIFO_LVL
+    4'b 0001  // index[23] FLASH_CTRL_FIFO_RST
   };
 endpackage
 
