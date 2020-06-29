280813961a2efde9dfb5d641c3f990ca5122f965
Miguel Young de la Sota <mcyoung@google.com>
2020-06-29 11:29:54 -0400

    [dif] Add dif_warn_unused_result.h
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

---
+++ sw/device/lib/dif/dif_warn_unused_result.h
@@ -0,0 +1,17 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_DIF_DIF_WARN_UNUSED_RESULT_H_
+#define OPENTITAN_SW_DEVICE_LIB_DIF_DIF_WARN_UNUSED_RESULT_H_
+
+/**
+ * Attribute for functions which return errors that must be acknowledged.
+ *
+ * This attribute must be used to mark all DIFs which return an error value of
+ * some kind, to ensure that callers do not accidentally drop the error on the
+ * ground.
+ */
+#define DIF_WARN_UNUSED_RESULT __attribute__((warn_unused_result))
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_DIF_DIF_WARN_UNUSED_RESULT_H_
