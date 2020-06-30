deee609cbeb0049b28a218119ad39bd5e0a21dce
Michael Schaffner <msf@opentitan.org>
2020-06-29 21:37:51 -0700

    [padctrl] Regenerate autogenerated content
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/padctrl/rtl/padctrl_reg_pkg.sv
+++ hw/ip/padctrl/rtl/padctrl_reg_pkg.sv
@@ -9,28 +9,28 @@ package padctrl_reg_pkg;
   // Param list
   parameter int NDioPads = 4;
   parameter int NMioPads = 16;
-  parameter int AttrDw = 8;
+  parameter int AttrDw = 10;
 
   ////////////////////////////
   // Typedefs for registers //
   ////////////////////////////
   typedef struct packed {
-    logic [7:0]  q;
+    logic [9:0] q;
     logic        qe;
   } padctrl_reg2hw_dio_pads_mreg_t;
 
   typedef struct packed {
-    logic [7:0]  q;
+    logic [9:0] q;
     logic        qe;
   } padctrl_reg2hw_mio_pads_mreg_t;
 
 
   typedef struct packed {
-    logic [7:0]  d;
+    logic [9:0] d;
   } padctrl_hw2reg_dio_pads_mreg_t;
 
   typedef struct packed {
-    logic [7:0]  d;
+    logic [9:0] d;
   } padctrl_hw2reg_mio_pads_mreg_t;
 
 
@@ -38,45 +38,54 @@ package padctrl_reg_pkg;
   // Register to internal design logic //
   ///////////////////////////////////////
   typedef struct packed {
-    padctrl_reg2hw_dio_pads_mreg_t [3:0] dio_pads; // [179:144]
-    padctrl_reg2hw_mio_pads_mreg_t [15:0] mio_pads; // [143:0]
+    padctrl_reg2hw_dio_pads_mreg_t [3:0] dio_pads; // [219:176]
+    padctrl_reg2hw_mio_pads_mreg_t [15:0] mio_pads; // [175:0]
   } padctrl_reg2hw_t;
 
   ///////////////////////////////////////
   // Internal design logic to register //
   ///////////////////////////////////////
   typedef struct packed {
-    padctrl_hw2reg_dio_pads_mreg_t [3:0] dio_pads; // [159:128]
-    padctrl_hw2reg_mio_pads_mreg_t [15:0] mio_pads; // [127:0]
+    padctrl_hw2reg_dio_pads_mreg_t [3:0] dio_pads; // [199:160]
+    padctrl_hw2reg_mio_pads_mreg_t [15:0] mio_pads; // [159:0]
   } padctrl_hw2reg_t;
 
   // Register Address
-  parameter logic [4:0] PADCTRL_REGEN_OFFSET = 5'h 0;
-  parameter logic [4:0] PADCTRL_DIO_PADS_OFFSET = 5'h 4;
-  parameter logic [4:0] PADCTRL_MIO_PADS0_OFFSET = 5'h 8;
-  parameter logic [4:0] PADCTRL_MIO_PADS1_OFFSET = 5'h c;
-  parameter logic [4:0] PADCTRL_MIO_PADS2_OFFSET = 5'h 10;
-  parameter logic [4:0] PADCTRL_MIO_PADS3_OFFSET = 5'h 14;
+  parameter logic [5:0] PADCTRL_REGEN_OFFSET = 6'h 0;
+  parameter logic [5:0] PADCTRL_DIO_PADS0_OFFSET = 6'h 4;
+  parameter logic [5:0] PADCTRL_DIO_PADS1_OFFSET = 6'h 8;
+  parameter logic [5:0] PADCTRL_MIO_PADS0_OFFSET = 6'h c;
+  parameter logic [5:0] PADCTRL_MIO_PADS1_OFFSET = 6'h 10;
+  parameter logic [5:0] PADCTRL_MIO_PADS2_OFFSET = 6'h 14;
+  parameter logic [5:0] PADCTRL_MIO_PADS3_OFFSET = 6'h 18;
+  parameter logic [5:0] PADCTRL_MIO_PADS4_OFFSET = 6'h 1c;
+  parameter logic [5:0] PADCTRL_MIO_PADS5_OFFSET = 6'h 20;
 
 
   // Register Index
   typedef enum int {
     PADCTRL_REGEN,
-    PADCTRL_DIO_PADS,
+    PADCTRL_DIO_PADS0,
+    PADCTRL_DIO_PADS1,
     PADCTRL_MIO_PADS0,
     PADCTRL_MIO_PADS1,
     PADCTRL_MIO_PADS2,
-    PADCTRL_MIO_PADS3
+    PADCTRL_MIO_PADS3,
+    PADCTRL_MIO_PADS4,
+    PADCTRL_MIO_PADS5
   } padctrl_id_e;
 
   // Register width information to check illegal writes
-  parameter logic [3:0] PADCTRL_PERMIT [6] = '{
+  parameter logic [3:0] PADCTRL_PERMIT [9] = '{
     4'b 0001, // index[0] PADCTRL_REGEN
-    4'b 1111, // index[1] PADCTRL_DIO_PADS
-    4'b 1111, // index[2] PADCTRL_MIO_PADS0
-    4'b 1111, // index[3] PADCTRL_MIO_PADS1
-    4'b 1111, // index[4] PADCTRL_MIO_PADS2
-    4'b 1111  // index[5] PADCTRL_MIO_PADS3
+    4'b 1111, // index[1] PADCTRL_DIO_PADS0
+    4'b 0011, // index[2] PADCTRL_DIO_PADS1
+    4'b 1111, // index[3] PADCTRL_MIO_PADS0
+    4'b 1111, // index[4] PADCTRL_MIO_PADS1
+    4'b 1111, // index[5] PADCTRL_MIO_PADS2
+    4'b 1111, // index[6] PADCTRL_MIO_PADS3
+    4'b 1111, // index[7] PADCTRL_MIO_PADS4
+    4'b 0011  // index[8] PADCTRL_MIO_PADS5
   };
 endpackage
 
