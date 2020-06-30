d3ee60532a87438f12b65066cd1c0655a14b86b4
Timothy Chen <timothytim@google.com>
2020-06-29 17:08:42 -0700

    [prim] Add a few prim cells needed for clock / resets
    - Reference prim_flops directly from prim_flop_2sync
      - This might be pointless as long term it would probably be better
        to make prim_flop_2sync an abstract cell
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [prim] update prim_flop
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [prim] Change prim_flop_2sync to a primgen cell
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [prim] fix comment
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

---
+++ hw/ip/prim_generic/rtl/prim_generic_flop.sv
@@ -0,0 +1,26 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+`include "prim_assert.sv"
+
+module prim_generic_flop # (
+  parameter int Width      = 1,
+  localparam int WidthSubOne = Width-1,
+  parameter logic [WidthSubOne:0] ResetValue = 0
+) (
+  input clk_i,
+  input rst_ni,
+  input [Width-1:0] d_i,
+  output logic [Width-1:0] q_o
+);
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      q_o <= ResetValue;
+    end else begin
+      q_o <= d_i;
+    end
+  end
+
+endmodule // prim_generic_flop
