45e6e88b12caf695b30b007ff6fb9c01a39312a7
Stefan Wallentowitz <stefan.wallentowitz@gi-de.com>
2020-07-02 20:44:01 +0200

    [otbn] Review fixes to python simulator
    Signed-off-by: Stefan Wallentowitz <stefan.wallentowitz@gi-de.com>

--- util/otbnsim/otbnsim/main.py
+++ util/otbnsim/otbnsim/main.py
@@ -1,3 +1,9 @@
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Execute the simulation
+
 from riscvmodel.sim import Simulator
 from riscvmodel.model import Model
 from riscvmodel.variant import RV32I
@@ -5,25 +11,26 @@ from riscvmodel.code import read_from_binary
 
 import argparse, struct
 
+
 def main():
-  parser = argparse.ArgumentParser()
-  parser.add_argument("imem_words", type=int)
-  parser.add_argument("imem_file")
-  parser.add_argument("dmem_words", type=int)
-  parser.add_argument("dmem_file")
-  parser.add_argument("cycles_file")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("imem_words", type=int)
+    parser.add_argument("imem_file")
+    parser.add_argument("dmem_words", type=int)
+    parser.add_argument("dmem_file")
+    parser.add_argument("cycles_file")
 
-  args = parser.parse_args()
-  sim = Simulator(Model(RV32I))
+    args = parser.parse_args()
+    sim = Simulator(Model(RV32I))
 
-  sim.load_program(read_from_binary(args.imem_file, stoponerror=True))
-  with open(args.dmem_file, "rb") as f:
-    sim.load_data(f.read())
+    sim.load_program(read_from_binary(args.imem_file, stoponerror=True))
+    with open(args.dmem_file, "rb") as f:
+        sim.load_data(f.read())
 
-  cycles = sim.run()
+    cycles = sim.run()
 
-  with open(args.dmem_file, "wb") as f:
-    f.write(sim.dump_data())
+    with open(args.dmem_file, "wb") as f:
+        f.write(sim.dump_data())
 
-  with open(args.cycles_file, "wb") as f:
-    f.write(struct.pack("<L", cycles))
+    with open(args.cycles_file, "wb") as f:
+        f.write(struct.pack("<L", cycles))
