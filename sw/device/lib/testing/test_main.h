17ae3e9b2aa6d446b2b0d1139ebccdca88f3cf6d
Miguel Young de la Sota <mcyoung@google.com>
2020-07-08 16:01:21 -0400

    [tests] Wrap up the test main() function into a library
    This change makes it so the old `main.c` under `sw/device/tests` lives
    alongside its header, `test_main.h`, forming a complete library that
    pulls in all the relevant dependencies. This makes the linking process
    for an on-device test more straightforwrd.
    
    `main()` doesn't actually need to be in one of the main object files, as
    far as our linker scripts care; our CRT requests a `main` symbol, and
    that gets pulled in regardless of where it's defined.
    
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

--- sw/device/lib/testing/test_main.h
+++ sw/device/lib/testing/test_main.h
@@ -5,14 +5,22 @@
 #ifndef OPENTITAN_SW_DEVICE_LIB_TESTING_TEST_MAIN_H_
 #define OPENTITAN_SW_DEVICE_LIB_TESTING_TEST_MAIN_H_
 
+#include <stdbool.h>
+
 /**
- * Runs the SW test.
- *
- * This method is defined externally in a standalone SW test source linked to
- * `main` as a library. It is a fully contained test in itself.
- *
- * @return success or failure of the test in boolean.
+ * @file
+ * @brief Entrypoint definitions for on-device tests
  */
-bool test_main(void);
+
+/**
+ * Entry point for a SW on-device test.
+ *
+ * This function should be defined externally in a standalone SW test, linked
+ * together with this library. This library provides a `main()` function that
+ * does test harness setup and calls `test_main()`.
+ *
+ * @return success or failure of the test as boolean.
+ */
+extern bool test_main(void);
 
 #endif  // OPENTITAN_SW_DEVICE_LIB_TESTING_TEST_MAIN_H_
