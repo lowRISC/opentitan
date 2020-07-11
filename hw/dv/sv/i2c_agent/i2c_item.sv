f141c11cbeab8b493f9df79ac02df50d04aafcdc
Tung Hoang <hoang.tung@wdc.com>
2020-07-11 00:24:49 -0700

    [i2c, dv] Add i2c_fifo_overflow (v2) test for rx_fifo and fmt_fifo
    The main point of this test is to create a sequence and specific
    constraints that deterministicallys trigger overflow interrupts
    which can be predictable and verified by DV
    
    - Update i2c_testplan.hjson to replace rx_fifo_overflow and
      fmt_fifo_overflow tests by a unified fifo_overflow test
    - Add i2c_fifo_overflow_vseq to verify fmt_overflow and rx_overflow interrupt
    - Update i2c_scoreboard to verify dropped data due to fifo overflow
    - Minor refactor i2c_fifo_watermark
    
    Signed-off-by: Tung Hoang <hoang.tung@wdc.com>

--- hw/dv/sv/i2c_agent/i2c_item.sv
+++ hw/dv/sv/i2c_agent/i2c_item.sv
@@ -6,15 +6,18 @@ class i2c_item extends uvm_sequence_item;
 
   // transaction data part
   bit [7:0]                data_q[$];
-  bit [9:0]                addr; // enough to support both 7 & 10-bit target address
+  bit [9:0]                addr;      // enough to support both 7 & 10-bit target address
   int                      tran_id;
-  int                      num_data;
+  int                      num_data;  // valid data
   bus_op_e                 bus_op;
   drv_type_e               drv_type;
   // transaction control part
   bit                      nack;
   bit                      ack;
   bit                      rstart;
+
+  // queue dropped data due to fmt_overflow
+  bit [7:0]                fmt_ovf_data_q[$];
 
   // random flags
   rand bit [7:0]           fbyte;
@@ -31,13 +34,13 @@ class i2c_item extends uvm_sequence_item;
 
   `uvm_object_utils_begin(i2c_item)
     `uvm_field_int(tran_id,                 UVM_DEFAULT)
-    `uvm_field_enum(bus_op_e,    bus_op,    UVM_DEFAULT)
+    `uvm_field_enum(bus_op_e, bus_op,       UVM_DEFAULT)
     `uvm_field_int(addr,                    UVM_DEFAULT)
     `uvm_field_int(num_data,                UVM_DEFAULT)
-    `uvm_field_queue_int(data_q,            UVM_DEFAULT)
     `uvm_field_int(start,                   UVM_DEFAULT)
     `uvm_field_int(stop,                    UVM_DEFAULT)
-    //------
+    `uvm_field_queue_int(data_q,            UVM_DEFAULT)
+    `uvm_field_queue_int(fmt_ovf_data_q,    UVM_DEFAULT | UVM_NOCOMPARE)
     `uvm_field_int(rstart,                  UVM_DEFAULT | UVM_NOPRINT | UVM_NOCOMPARE)
     `uvm_field_int(fbyte,                   UVM_DEFAULT | UVM_NOPRINT | UVM_NOCOMPARE)
     `uvm_field_int(ack,                     UVM_DEFAULT | UVM_NOPRINT | UVM_NOCOMPARE)
@@ -55,6 +58,7 @@ class i2c_item extends uvm_sequence_item;
     addr     = 0;
     drv_type = None;
     data_q.delete();
+    fmt_ovf_data_q.delete();
   endfunction : clear_data
 
   function void clear_flag();
