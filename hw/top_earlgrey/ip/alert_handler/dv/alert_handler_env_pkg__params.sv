1d51845715f090cc6fa1eb089306aae32de7b2c1
Pirmin Vogel <vogelpi@lowrisc.org>
2020-07-13 17:33:42 +0200

    [aes] Add idle hint signal for clkmgr
    This is related to lowRISC/OpenTitan#2647.
    
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

--- hw/top_earlgrey/ip/alert_handler/dv/alert_handler_env_pkg__params.sv
+++ hw/top_earlgrey/ip/alert_handler/dv/alert_handler_env_pkg__params.sv
@@ -6,5 +6,9 @@
 // PLEASE DO NOT HAND-EDIT THIS FILE. IT HAS BEEN AUTO-GENERATED WITH THE FOLLOWING COMMAND:
 // util/topgen.py -t hw/top_earlgrey/doc/top_earlgrey.hjson --alert-handler-only -o hw/top_earlgrey/
 
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
 parameter uint NUM_ALERTS = 4;
 parameter bit [NUM_ALERTS-1:0] ASYNC_ON = 4'b0000;
