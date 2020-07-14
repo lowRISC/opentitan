2f9d8ee0a2ce60d66df99a42a6f769f06814ad73
Philipp Wagner <phw@lowrisc.org>
2020-07-14 11:17:21 +0100

    [otbn] Initial block-level structure
    Initial OTBN block-level structure with modules instantiated for the
    base instruction subset.
    
    All code inside the modules is "dummy code" to make the whole design
    synthesize, but not yet perform any useful functionality.
    
    Fixes #2614
    
    Signed-off-by: Philipp Wagner <phw@lowrisc.org>

---
+++ hw/ip/otbn/rtl/otbn_controller.sv
@@ -0,0 +1,104 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+`include "prim_assert.sv"
+
+/**
+ * OTBN Controller
+ */
+module otbn_controller
+  import otbn_pkg::*;
+#(
+  // Size of the instruction memory, in bytes
+  parameter int ImemSizeByte = 4096,
+  // Size of the data memory, in bytes
+  parameter int DmemSizeByte = 4096,
+
+  localparam int ImemAddrWidth = prim_util_pkg::vbits(ImemSizeByte),
+  localparam int DmemAddrWidth = prim_util_pkg::vbits(DmemSizeByte)
+) (
+  input  logic  clk_i,
+  input  logic  rst_ni,
+
+  input  logic                     start_i, // start the processing at start_addr_i
+  output logic                     done_o,  // processing done, signaled by ECALL
+  input  logic [ImemAddrWidth-1:0] start_addr_i,
+
+  // Next instruction selection (to instruction fetch)
+  output logic                     insn_fetch_req_valid_o,
+  output logic [ImemAddrWidth-1:0] insn_fetch_req_addr_o,
+
+  // Fetched/decoded instruction
+  input  logic                     insn_valid_i,
+  input  logic [ImemAddrWidth-1:0] insn_addr_i,
+  input  insn_op_e                 insn_op_i,
+
+  // Decoded instruction data, matching the "Decoding" section of the specification.
+  input insn_dec_base_t       insn_dec_base_i,
+  input insn_dec_ctrl_t       insn_dec_ctrl_i,
+
+  // Base register file
+  output logic [4:0]   rf_base_wr_addr_o,
+  output logic         rf_base_wr_en_o,
+  output logic [31:0]  rf_base_wr_data_o,
+
+  output logic [4:0]   rf_base_rd_addr_a_o,
+  input  logic [31:0]  rf_base_rd_data_a_i,
+
+  output logic [4:0]   rf_base_rd_addr_b_o,
+  input  logic [31:0]  rf_base_rd_data_b_i,
+
+  // Execution units
+  output alu_base_operation_t alu_base_operation_o,
+  input  logic [31:0]         alu_base_result_i,
+  input  logic                alu_base_comparison_result_i
+);
+
+  // TODO: Using insn_op_i here is easy, potentially should move to dedicated control signals from
+  // the decoder in the future.
+  assign done_o = insn_valid_i && insn_op_i == InsnOpEcall;
+
+  // Next fetch address
+  assign insn_fetch_req_valid_o = 1'b1;
+  always_comb begin
+    if (start_i) begin
+      insn_fetch_req_addr_o = start_addr_i;
+    end else begin
+      insn_fetch_req_addr_o = insn_addr_i + 'd4;
+    end
+    // TODO: Jumps/branches
+  end
+
+  assign rf_base_rd_addr_a_o = insn_dec_base_i.a;
+  assign rf_base_rd_addr_b_o = insn_dec_base_i.b;
+
+  // Base ALU Operand A MUX
+  always_comb begin
+    unique case (insn_dec_ctrl_i.op_a_sel)
+      OpASelRegister:
+        alu_base_operation_o.operand_a = rf_base_rd_data_a_i;
+      default:
+        alu_base_operation_o.operand_a = rf_base_rd_data_a_i;
+    endcase
+  end
+
+  // Base ALU Operand B MUX
+  always_comb begin
+    unique case (insn_dec_ctrl_i.op_b_sel)
+      OpBSelRegister:
+        alu_base_operation_o.operand_b = rf_base_rd_data_b_i;
+      OpBSelImmediate:
+        alu_base_operation_o.operand_b = insn_dec_base_i.i;
+      default:
+        alu_base_operation_o.operand_b = rf_base_rd_data_b_i;
+    endcase
+  end
+
+  assign alu_base_operation_o.op = insn_dec_ctrl_i.alu_op;
+
+  // Register file write MUX
+  // TODO: Switch between CSR and EX writeback.
+  assign rf_base_wr_en_o = 1'b0;
+
+endmodule
--- hw/ip/otbn/rtl/otbn_pkg.sv
+++ hw/ip/otbn/rtl/otbn_pkg.sv
@@ -1,9 +1,33 @@
 // Copyright lowRISC contributors.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
+
+`include "prim_assert.sv"
+
 package otbn_pkg;
+
+  // Global Constants ==============================================================================
+
   // Data path width for BN (wide) instructions, in bits.
   parameter int WLEN = 256;
+
+  // Number of flag groups
+  parameter int NFlagGroups = 2;
+
+  // Width of the GPR index/address
+  parameter int GprAw = 5;
+
+  // Number of General Purpose Registers (GPRs)
+  parameter int NGpr = 2 ** GprAw;
+
+  // Width of the WDR index/address
+  parameter int WdrAw = 5;
+
+  // Number of Wide Data Registers (WDRs)
+  parameter int NWdr = 2 ** WdrAw;
+
+
+  // Toplevel constants ============================================================================
 
   // Alerts
   parameter int                   NumAlerts = 3;
@@ -17,5 +41,98 @@ package otbn_pkg;
   typedef enum logic [31:0] {
     ErrCodeNoError = 32'h 0000_0000
   } err_code_e;
+
+  // Constants =====================================================================================
+
+  typedef enum logic {
+    InsnSubsetBase = 1'b0,  // Base (RV32/Narrow) Instruction Subset
+    InsnSubsetBignum = 1'b1 // Big Number (BN/Wide) Instruction Subset
+  } insn_subset_e;
+
+  // Instructions
+  // TODO: Extend and add an explicit type.
+  typedef enum {
+    InsnOpAdd,
+    InsnOpEcall
+  } insn_op_e;
+
+  typedef enum logic [3:0] {
+    AluOpAdd,
+    AluOpSub,
+
+    AluOpXor,
+    AluOpOr,
+    AluOpAnd,
+    AluOpNot,
+
+    AluOpEq,
+    AluOpNeq
+  } alu_op_e;
+
+  // Operand a source selection
+  typedef enum logic {
+    OpASelRegister  = 1'b0,
+    OpASelImmediate = 1'b1
+  } op_a_sel_e;
+
+  // Operand b source selection
+  typedef enum logic {
+    OpBSelRegister  = 1'b0,
+    OpBSelImmediate = 1'b1
+  } op_b_sel_e;
+
+  // Control and Status Registers (CSRs)
+  parameter int CsrNumWidth = 12;
+  typedef enum logic [CsrNumWidth-1:0] {
+    CsrFlags = 12'h7C0,
+    CsrMod0  = 12'h7D0,
+    CsrMod1  = 12'h7D1,
+    CsrMod2  = 12'h7D2,
+    CsrMod3  = 12'h7D3,
+    CsrMod4  = 12'h7D4,
+    CsrMod5  = 12'h7D5,
+    CsrMod6  = 12'hdD6,
+    CsrMod7  = 12'h7D7,
+    CsrRnd   = 12'hFC0
+  } csr_e;
+
+
+  // Wide Special Purpose Registers (WSRs)
+  parameter int NWsr = 3; // Number of WSRs
+  parameter int WsrNumWidth = $clog2(NWsr);
+  typedef enum logic [WsrNumWidth-1:0] {
+    WsrMod = 'd0,
+    WsrRnd = 'd1,
+    WsrAcc = 'd2
+  } wsr_e;
+  // TODO: Figure out how to add assertions for the enum type width; initial blocks, as produced by
+  // ASSERT_INIT, aren't allowed in packages.
+  //`ASSERT_INIT(WsrESizeMatchesParameter_A, $bits(wsr_e) == WsrNumWidth)
+
+  // Decoded instruction components, with signals matching the "Decoding" section of the
+  // specification.
+  // TODO: The variable names are rather short, especially "i" is confusing. Think about renaming.
+
+  typedef struct packed {
+    logic [4:0]  d;  // Destination register
+    logic [4:0]  a;  // First source register
+    logic [4:0]  b;  // Second source register
+    logic [31:0] i;  // Immediate
+  } insn_dec_base_t;
+
+  // Control signals from decoder to controller: additional information about the decoded
+  // instruction influencing the operation.
+  typedef struct packed {
+    insn_subset_e subset;
+    op_a_sel_e    op_a_sel;
+    op_b_sel_e    op_b_sel;
+    alu_op_e      alu_op;
+  } insn_dec_ctrl_t;
+
+  typedef struct packed {
+    alu_op_e     op;
+    logic [31:0] operand_a;
+    logic [31:0] operand_b;
+  } alu_base_operation_t;
 
 endpackage
