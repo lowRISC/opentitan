7fdfe0dcb5415d30bfc80866c429df5eaa30c8f8
Philipp Wagner <phw@lowrisc.org>
2020-07-14 11:17:21 +0100

    [otbn] Initial block-level structure
    Initial OTBN block-level structure with modules instantiated for the
    base instruction subset.
    
    All code inside the modules is "dummy code" to make the whole design
    synthesize, but not yet perform any useful functionality.
    
    Fixes #2614
    
    Signed-off-by: Philipp Wagner <phw@lowrisc.org>

---
+++ hw/ip/otbn/rtl/otbn_decoder.sv
@@ -0,0 +1,49 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+`include "prim_assert.sv"
+
+/**
+ * OTBN Instruction Decoder
+ */
+module otbn_decoder
+  import otbn_pkg::*;
+(
+  // For assertions only.
+  input  logic                 clk_i,
+  input  logic                 rst_ni,
+
+  // Instruction data to be decoded
+  input  logic [31:0]          insn_fetch_resp_data_i,
+  input  logic                 insn_fetch_resp_valid_i,
+
+  // Decoded instruction
+  output logic                 insn_valid_o,
+  output insn_op_e             insn_op_o,
+
+  // Decoded instruction data, matching the "Decoding" section of the specification.
+  output insn_dec_base_t       insn_dec_base_o,
+
+  // Additional control signals
+  output insn_dec_ctrl_t       insn_dec_ctrl_o
+);
+
+  // Example: ADD
+  assign insn_valid_o = insn_fetch_resp_valid_i;
+  assign insn_op_o = InsnOpAdd;
+
+  assign insn_dec_base_o = '{
+    d: insn_fetch_resp_data_i[11:7],
+    a: insn_fetch_resp_data_i[19:15],
+    b: insn_fetch_resp_data_i[24:20],
+    i: '0
+  };
+
+  assign insn_dec_ctrl_o = '{
+    subset:   InsnSubsetBase,
+    op_a_sel: OpASelRegister,
+    op_b_sel: OpBSelRegister,
+    alu_op: AluOpAdd
+  };
+endmodule
---
+++ hw/ip/otbn/rtl/otbn_instruction_fetch.sv
@@ -0,0 +1,52 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+`include "prim_assert.sv"
+
+/**
+ * OTBN Instruction Fetch Unit
+ *
+ * Fetch an instruction from the instruction memory.
+ */
+module otbn_instruction_fetch
+  import otbn_pkg::*;
+#(
+  parameter int ImemSizeByte = 4096,
+
+  localparam int ImemAddrWidth = prim_util_pkg::vbits(ImemSizeByte)
+) (
+  input logic clk_i,
+  input logic rst_ni,
+
+  // Instruction memory (IMEM) interface. Read-only.
+  output logic                     imem_req_o,
+  output logic [ImemAddrWidth-1:0] imem_addr_o,
+  input  logic [31:0]              imem_rdata_i,
+  input  logic                     imem_rvalid_i,
+  input  logic [1:0]               imem_rerror_i, // Bit1: Uncorrectable, Bit0: Correctable
+
+  // Next instruction selection (to instruction fetch)
+  input  logic                     insn_fetch_req_valid_i,
+  input  logic [ImemAddrWidth-1:0] insn_fetch_req_addr_i,
+
+  // Decoded instruction
+  output logic                     insn_fetch_resp_valid_o,
+  output logic [ImemAddrWidth-1:0] insn_fetch_resp_addr_o,
+  output logic [31:0]              insn_fetch_resp_data_o
+);
+
+  assign imem_req_o = insn_fetch_req_valid_i;
+  assign imem_addr_o = insn_fetch_req_addr_i;
+
+  assign insn_fetch_resp_valid_o = imem_rvalid_i;
+  assign insn_fetch_resp_data_o = imem_rdata_i;
+
+  always_ff @(posedge clk_i) begin
+    insn_fetch_resp_addr_o <= insn_fetch_req_addr_i;
+  end
+
+  // TODO: Need to handle imem_rerror somewhere, which need to be turned into alerts. Could be
+  // handled either here or somewhere more up in the hierarchy.
+
+endmodule
