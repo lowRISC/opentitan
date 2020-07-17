1807b540e1b5fbd08798b5b8f5556f216b1100cf
Philipp Wagner <phw@lowrisc.org>
2020-07-17 09:13:54 +0100

    Remove hand-crafted file lists
    Before fusesoc was widely used we did use `.f` files as database for
    source files. This infrastructure is now unused and has mostly been
    removed, with a couple of exceptions. Remove those as well.
    
    Signed-off-by: Philipp Wagner <phw@lowrisc.org>

--- hw/ip/usbuart/rtl/deps.f
+++
@@ -1,9 +0,0 @@
-${PRJ_DIR}/hw/ip/prim/rtl/prim_assert.sv
-${PRJ_DIR}/hw/ip/tlul/rtl/top_pkg.sv
-${PRJ_DIR}/hw/ip/tlul/rtl/tlul_pkg.sv
-
-${PRJ_DIR}/hw/ip/prim/rtl/prim_fifo_async.sv
-${PRJ_DIR}/hw/ip/prim/rtl/prim_subreg.sv
-${PRJ_DIR}/hw/ip/prim/rtl/prim_subreg_ext.sv
-${PRJ_DIR}/hw/ip/prim/rtl/prim_flop_2sync.sv
-${PRJ_DIR}/hw/ip/prim/rtl/prim_intr_hw.sv
