1fe943e1bb008e02cb1ed1471e30ac07d5934717
Timothy Chen <timothytim@google.com>
2020-07-22 16:59:13 -0700

    [flow] rough setup for running vclp
    - The flow does not completely work yet since vclp licenses are not available
    - setup.tcl should be shared with dc in the end, and not copied over
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

---
+++ hw/power/setup.tcl
@@ -0,0 +1,32 @@
+# Google Confidential
+
+# Technology setup script for TSMC40ULPEF WLM-based synthesis flow
+# See also https://docs.google.com/document/d/1I9R066xZKUuqZxcXhFYtmZW2EC8MBUrTYFDsNT6cNIE
+# for more info on the Earlgrey chip specifications.
+
+########################
+##   LIBRARY PATHS    ##
+########################
+
+source lib-setup.tcl
+
+########################
+##   Assign Config    ##
+########################
+
+# this could eventually be moved into .synopsys_dc.setup
+set_app_var search_path [list .      \
+    ${STDCELL_DB_PATH}               \
+    ${SRAM_DB_PATH}                  \
+    ${PADS_DB_PATH}                  ]
+
+set_app_var target_library [list                                  \
+    "${STD_CELL_VARIANT}_${STDCELL_CORNER}.db"                    \
+    "${PAD_LIB_VARIANT}_${PADS_CORNER}.db" ]
+
+# this appends all ram macros currently wrapped in the prim library
+source ${CONFIG_PATH}/ram-macros-setup.tcl
+
+set_app_var link_library ${target_library}
+
+set LOAD_LIB "${STD_CELL_VARIANT}_${STDCELL_CORNER}"
