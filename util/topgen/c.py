b28a1dcf266444931d82543d4e4912f7672114d5
Sam Elliott <selliott@lowrisc.org>
2020-07-24 23:21:41 +0100

    [topgen] Cleanup PLIC Source/Interrupt Mapping Logic
    Signed-off-by: Sam Elliott <selliott@lowrisc.org>

--- util/topgen/c.py
+++ util/topgen/c.py
@@ -5,11 +5,13 @@
 `top_{name}.h`.
 """
 
+from collections import OrderedDict
+
 from mako.template import Template
 
 
 class Name(object):
-    """We often need to format names in specific ways, this class does so."""
+    """We often need to format names in specific ways; this class does so."""
     def __add__(self, other):
         return Name(self.parts + other.parts)
 
@@ -77,6 +79,8 @@ class CEnum(object):
 
         self.constants.append((full_name, value, docstring))
 
+        return full_name
+
     def add_last_constant(self, docstring=""):
         assert not self.finalized
 
@@ -96,10 +100,113 @@ class CEnum(object):
         return Template(template).render(enum=self)
 
 
+class CArrayMapping(object):
+    def __init__(self, name, output_type_name):
+        self.name = name
+        self.output_type_name = output_type_name
+
+        self.mapping = OrderedDict()
+
+    def add_entry(self, in_name, out_name):
+        self.mapping[in_name] = out_name
+
+    def render_declaration(self):
+        template = (
+            "extern const ${mapping.output_type_name.as_c_type()}\n"
+            "    ${mapping.name.as_snake_case()}[${len(mapping.mapping)}];")
+        return Template(template).render(mapping=self)
+
+    def render_definition(self):
+        template = (
+            "const ${mapping.output_type_name.as_c_type()}\n"
+            "    ${mapping.name.as_snake_case()}[${len(mapping.mapping)}] = {\n"
+            "% for in_name, out_name in mapping.mapping.items():\n"
+            "  [${in_name.as_c_enum()}] = ${out_name.as_c_enum()},\n"
+            "% endfor\n"
+            "};\n")
+        return Template(template).render(mapping=self)
+
+
 class TopGenC(object):
     def __init__(self, top_info):
         self.top = top_info
         self._top_name = Name(["top"]) + Name.from_snake_case(top_info["name"])
+
+        self._init_plic_targets()
+        self._init_plic_mapping()
+
+    def _init_plic_targets(self):
+        enum = CEnum(self._top_name + Name(["plic", "target"]))
+
+        for core_id in range(int(self.top["num_cores"])):
+            enum.add_constant(Name(["ibex", str(core_id)]),
+                              docstring="Ibex Core {}".format(core_id))
+
+        enum.add_last_constant("Final PLIC target")
+
+        self.plic_targets = enum
+
+    def _init_plic_mapping(self):
+        """We eventually want to generate a mapping from interrupt id to the
+        source peripheral.
+
+        In order to do so, we generate two enums, and store the generated names
+        in a dictionary that represents the mapping.
+
+        Plic Interrupt ID 0 corresponds to no interrupt, and so no peripheral,
+        so we encode that in the enum as "unknown".
+
+        The interrupts have to be added in order, with "none" first, to ensure
+        that they get the correct mapping to their PLIC id, which is used for
+        addressing the right registers and bits.
+        """
+        sources = CEnum(self._top_name + Name(["plic", "peripheral"]))
+        interrupts = CEnum(self._top_name + Name(["plic", "irq", "id"]))
+        plic_mapping = CArrayMapping(
+            self._top_name + Name(["plic", "interrupt", "for", "peripheral"]),
+            sources.name)
+
+        unknown_source = sources.add_constant(Name(["unknown"]),
+                                              docstring="Unknown Peripheral")
+        none_irq_id = interrupts.add_constant(Name(["none"]),
+                                              docstring="No Interrupt")
+        plic_mapping.add_entry(none_irq_id, unknown_source)
+
+        # When we generate the `interrupts` enum, the only info we have about
+        # the source is the module name. We'll use `source_name_map` to map a
+        # short module name to the full name object used for the enum constant.
+        source_name_map = {}
+
+        for name in self.top["interrupt_module"]:
+            source_name = sources.add_constant(Name.from_snake_case(name),
+                                               docstring=name)
+            source_name_map[name] = source_name
+
+        sources.add_last_constant("Final PLIC peripheral")
+
+        for intr in self.top["interrupt"]:
+            # Some interrupts are multiple bits wide. Here we deal with that by
+            # adding a bit-index suffix
+            if "width" in intr and int(intr["width"]) != 1:
+                for i in range(int(intr["width"])):
+                    name = Name.from_snake_case(
+                        intr["name"]) + Name([str(i)])
+                    irq_id = interrupts.add_constant(name,
+                                                     docstring="{} {}".format(
+                                                         intr["name"], i))
+                    source_name = source_name_map[intr["module_name"]]
+                    plic_mapping.add_entry(irq_id, source_name)
+            else:
+                name = Name.from_snake_case(intr["name"])
+                irq_id = interrupts.add_constant(name, docstring=intr["name"])
+                source_name = source_name_map[intr["module_name"]]
+                plic_mapping.add_entry(irq_id, source_name)
+
+        interrupts.add_last_constant("The Last Valid Interrupt ID.")
+
+        self.plic_sources = sources
+        self.plic_interrupts = interrupts
+        self.plic_mapping = plic_mapping
 
     def modules(self):
         return [(m["name"],
@@ -112,14 +219,3 @@ class TopGenC(object):
                  MemoryRegion(self._top_name + Name.from_snake_case(m["name"]),
                               m["base_addr"], m["size"]))
                 for m in self.top["memory"]]
-
-    def plic_targets(self):
-        enum = CEnum(self._top_name + Name(["plic", "target"]))
-
-        for core_id in range(int(self.top["num_cores"])):
-            enum.add_constant(Name(["ibex", str(core_id)]),
-                              docstring="Ibex Core {}".format(core_id))
-
-        enum.add_last_constant("Final PLIC target")
-
-        return enum
