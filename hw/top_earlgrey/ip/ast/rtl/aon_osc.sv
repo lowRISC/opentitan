f6bfed00e7c7076ad6d2e9f9cdfa239d7d1b84eb
Arnon Sharlin <arnon.sharlin@opentitan.org>
2020-07-27 21:45:48 +0300

    [top / ast] Adding generic AST files
    modified file headers, and added the ast_reg hjson files
    RTL updates
    
    Also:
    1) Removed defparams + other coding style compliance.
    2) Added translate_off/on pragmas so synthesis won't break
    3) Added *_osc.sv files:
         In DV they generate the clocks.
         In FPGA They should be replaced with fpga PLLs.
    4) updated comments
    
    Signed-off-by: Arnon Sharlin <arnon.sharlin@opentitan.org>

---
+++ hw/top_earlgrey/ip/ast/rtl/aon_osc.sv
@@ -0,0 +1,53 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//############################################################################
+// 
+// *Name: aon_osc
+// *Module Description: AON Clock Oscilator
+//
+//############################################################################
+`timescale 1ns/1ps
+
+module aon_osc #(
+// synopsys translate_off
+   parameter time AON_EN_RDLY = 10us,
+   parameter time AON_EN_FDLY = 100ns
+// synopsys translate_on
+) (
+   input aon_en_i,              // AON Source Clock Enable
+   output logic aon_clk_o,      // AON Clock Output
+   output logic aon_clk_en_o    // AON Clock Enable Output
+);
+
+// synopsys translate_off
+
+localparam time AON_CLK_PERIOD = 5000ns; // 5000ns (200Khz)
+
+logic init_start, clk;
+
+initial begin
+   clk = 1'b0;
+   $display("\nAON Clock Period: %0dns", AON_CLK_PERIOD);
+   init_start = 1'b1; #1;
+   init_start = 1'b0;
+end
+
+always begin
+   #(AON_CLK_PERIOD/2) clk = ~clk;
+end
+
+assign aon_clk_o = clk;
+
+always_ff @( init_start, posedge aon_en_i, negedge aon_en_i ) begin
+    if ( init_start )
+       aon_clk_en_o <= 1'b0;
+    else if ( !init_start && aon_en_i )
+       aon_clk_en_o <= #(AON_EN_RDLY) aon_en_i;
+    else if ( !init_start && !aon_en_i )                  
+       aon_clk_en_o <= #(AON_EN_FDLY) aon_en_i;
+end
+
+// synopsys translate_on
+
+endmodule  // of aon_osc
