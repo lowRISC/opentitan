d4559c3ee64b2f02fd0f5b5189592628f3d8abbc
Cindy Chen <chencindy@google.com>
2020-07-30 09:26:06 -0700

    [fpv/rv_plic] regenerate auto_gen and update according to PR#2989
    This PR reflects the new autogen fpv_csr_assert file in top_level
    rv_plic. Sorry I forget to update this top_level module.
    The change includes:
    1. Regenerate top_level rv_plic fpv_csr_assertions
    2. Change the naming from i_rv_plic to dut
    3. add reg2hw and hw2reg in bind file
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/top_earlgrey/ip/rv_plic/fpv/autogen/rv_plic_bind_fpv.sv
+++ hw/top_earlgrey/ip/rv_plic/fpv/autogen/rv_plic_bind_fpv.sv
@@ -38,8 +38,10 @@ module rv_plic_bind_fpv;
   bind rv_plic rv_plic_csr_assert_fpv rv_plic_csr_assert_fpv (
     .clk_i,
     .rst_ni,
-    .h2d  (tl_i),
-    .d2h  (tl_o)
+    .h2d    (tl_i),
+    .d2h    (tl_o),
+    .reg2hw (reg2hw),
+    .hw2reg (hw2reg)
   );
 
 endmodule : rv_plic_bind_fpv
