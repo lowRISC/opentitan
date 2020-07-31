7b0f7439603ee0bb348618349647edf5adb5cca8
Srikrishna Iyer <sriyer@google.com>
2020-07-31 14:03:00 -0700

    [dv] reorg i2c bind & add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/i2c/dv/sva/i2c_bind.sv
+++ hw/ip/i2c/dv/sva/i2c_bind.sv
@@ -13,5 +13,14 @@ module i2c_bind;
     .d2h  (tl_o)
   );
 
+  import i2c_reg_pkg::*;
+  bind i2c i2c_csr_assert_fpv i2c_csr_assert (
+    .clk_i,
+    .rst_ni,
+    .h2d    (tl_i),
+    .d2h    (tl_o),
+    .reg2hw (reg2hw),
+    .hw2reg (hw2reg)
+  );
 
 endmodule
