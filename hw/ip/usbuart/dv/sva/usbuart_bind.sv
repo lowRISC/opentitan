31d12eb80b5a5f405c78e699edb76cbd11ebc39b
Srikrishna Iyer <sriyer@google.com>
2020-07-31 14:18:23 -0700

    [dv] reorg usbuart bind & add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/usbuart/dv/sva/usbuart_bind.sv
+++ hw/ip/usbuart/dv/sva/usbuart_bind.sv
@@ -13,4 +13,14 @@ module usbuart_bind;
     .d2h  (tl_o)
   );
 
+  import usbuart_reg_pkg::*;
+  bind usbuart usbuart_csr_assert_fpv usbuart_csr_assert (
+    .clk_i,
+    .rst_ni,
+    .h2d    (tl_i),
+    .d2h    (tl_o),
+    .reg2hw (reg2hw),
+    .hw2reg (hw2reg)
+  );
+
 endmodule
