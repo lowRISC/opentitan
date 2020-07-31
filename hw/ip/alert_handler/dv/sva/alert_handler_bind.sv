004375a8fb546f52d6b460e0041329f7cfe3572b
Srikrishna Iyer <sriyer@google.com>
2020-07-31 15:02:36 -0700

    [dv] reorg alert_handler bing & add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/alert_handler/dv/sva/alert_handler_bind.sv
+++ hw/ip/alert_handler/dv/sva/alert_handler_bind.sv
@@ -13,4 +13,14 @@ module alert_handler_bind;
     .d2h  (tl_o)
   );
 
+  // import alert_handler_reg_pkg::*;
+  // bind alert_handler alert_handler_csr_assert_fpv alert_handler_csr_assert (
+  //   .clk_i,
+  //   .rst_ni,
+  //   .h2d    (tl_i),
+  //   .d2h    (tl_o),
+  //   .reg2hw (reg2hw),
+  //   .hw2reg (hw2reg)
+  // );
+
 endmodule
