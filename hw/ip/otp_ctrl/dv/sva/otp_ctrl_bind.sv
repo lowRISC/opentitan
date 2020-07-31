36d4dba1f3103175e969d2bacc6b1a3b4124acdf
Srikrishna Iyer <sriyer@google.com>
2020-07-31 14:16:32 -0700

    [dv] reorg otp_ctrl bind and add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/otp_ctrl/dv/sva/otp_ctrl_bind.sv
+++ hw/ip/otp_ctrl/dv/sva/otp_ctrl_bind.sv
@@ -13,5 +13,14 @@ module otp_ctrl_bind;
     .d2h  (tl_o)
   );
 
+  import otp_ctrl_reg_pkg::*;
+  bind otp_ctrl otp_ctrl_csr_assert_fpv otp_ctrl_csr_assert (
+    .clk_i,
+    .rst_ni,
+    .h2d    (tl_i),
+    .d2h    (tl_o),
+    .reg2hw (reg2hw),
+    .hw2reg (hw2reg)
+  );
 
 endmodule
