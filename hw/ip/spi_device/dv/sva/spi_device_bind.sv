2277ea6018021440e7725521de7ae49159a2aeba
Srikrishna Iyer <sriyer@google.com>
2020-07-31 14:28:57 -0700

    [dv] reorg spi_device bind & add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/spi_device/dv/sva/spi_device_bind.sv
+++ hw/ip/spi_device/dv/sva/spi_device_bind.sv
@@ -13,4 +13,14 @@ module spi_device_bind;
     .d2h  (tl_o)
   );
 
+  // import spi_device_reg_pkg::*;
+  // bind spi_device spi_device_csr_assert_fpv spi_device_csr_assert (
+  //   .clk_i,
+  //   .rst_ni,
+  //   .h2d    (tl_i),
+  //   .d2h    (tl_o),
+  //   .reg2hw (reg2hw),
+  //   .hw2reg (hw2reg)
+  // );
+
 endmodule
