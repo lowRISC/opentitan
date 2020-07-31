21aa122d9c0543abb66ebe292c16d7d8d25524ce
Srikrishna Iyer <sriyer@google.com>
2020-07-31 14:21:30 -0700

    [dv] reorg rv_core_ibex bind
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/rv_core_ibex/dv/sva/rv_core_ibex_bind.sv
+++ hw/ip/rv_core_ibex/dv/sva/rv_core_ibex_bind.sv
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-module core_ibex_bind;
+module rv_core_ibex_bind;
 
   bind rv_core_ibex tlul_assert #(
     .EndpointType("Host")
