4517e44e63583f2c352b9a57b9b6f2ce013d5920
Srikrishna Iyer <sriyer@google.com>
2020-07-31 14:01:28 -0700

    [dv] reorg hmac_bind & add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/hmac/dv/sva/hmac_bind.sv
+++ hw/ip/hmac/dv/sva/hmac_bind.sv
@@ -13,5 +13,14 @@ module hmac_bind;
     .d2h  (tl_o)
   );
 
+  import hmac_reg_pkg::*;
+  bind hmac hmac_csr_assert_fpv hmac_csr_assert (
+    .clk_i,
+    .rst_ni,
+    .h2d    (tl_i),
+    .d2h    (tl_o),
+    .reg2hw (reg2hw),
+    .hw2reg (hw2reg)
+  );
 
 endmodule
