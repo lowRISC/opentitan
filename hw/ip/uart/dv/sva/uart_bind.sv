49da4f42c657bcdbdc3ad63b0961fbb9e4c7d027
Srikrishna Iyer <sriyer@google.com>
2020-07-31 01:17:00 -0700

    [dv] reorg uart_bind & add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/uart/dv/sva/uart_bind.sv
+++ hw/ip/uart/dv/sva/uart_bind.sv
@@ -13,4 +13,14 @@ module uart_bind;
     .d2h  (tl_o)
   );
 
+  import uart_reg_pkg::*;
+  bind uart uart_csr_assert_fpv uart_csr_assert (
+    .clk_i,
+    .rst_ni,
+    .h2d    (tl_i),
+    .d2h    (tl_o),
+    .reg2hw (reg2hw),
+    .hw2reg (hw2reg)
+);
+
 endmodule
