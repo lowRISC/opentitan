a2b79fb3bf31a7a1733d8c6c67660971edca95d5
Srikrishna Iyer <sriyer@google.com>
2020-07-31 14:09:53 -0700

    [dv] reorg pwrmgr bind & add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/pwrmgr/dv/sva/pwrmgr_bind.sv
+++ hw/ip/pwrmgr/dv/sva/pwrmgr_bind.sv
@@ -13,4 +13,14 @@ module pwrmgr_bind;
     .d2h  (tl_o)
   );
 
+  import pwrmgr_reg_pkg::*;
+  bind pwrmgr pwrmgr_csr_assert_fpv pwrmgr_csr_assert (
+    .clk_i,
+    .rst_ni,
+    .h2d    (tl_i),
+    .d2h    (tl_o),
+    .reg2hw (reg2hw),
+    .hw2reg (hw2reg)
+  );
+
 endmodule
