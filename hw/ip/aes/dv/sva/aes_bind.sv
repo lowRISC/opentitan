d26952d856bf5407dc610e8dda5da36ae1293c9c
Srikrishna Iyer <sriyer@google.com>
2020-07-31 13:58:20 -0700

    [dv] reorg aes_bind & add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/aes/dv/sva/aes_bind.sv
+++ hw/ip/aes/dv/sva/aes_bind.sv
@@ -13,4 +13,14 @@ module aes_bind;
     .d2h  (tl_o)
   );
 
+  import aes_reg_pkg::*;
+  bind aes aes_csr_assert_fpv aes_csr_assert (
+    .clk_i,
+    .rst_ni,
+    .h2d    (tl_i),
+    .d2h    (tl_o),
+    .reg2hw (reg2hw),
+    .hw2reg (hw2reg)
+  );
+
 endmodule
