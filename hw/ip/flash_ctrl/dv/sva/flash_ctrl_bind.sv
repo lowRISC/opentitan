428e8d28bcda93b9607d3f1143999cd9692abd2b
Srikrishna Iyer <sriyer@google.com>
2020-07-31 14:27:52 -0700

    [dv] reorg flash_ctrl bind & add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/flash_ctrl/dv/sva/flash_ctrl_bind.sv
+++ hw/ip/flash_ctrl/dv/sva/flash_ctrl_bind.sv
@@ -13,4 +13,14 @@ module flash_ctrl_bind;
     .d2h  (tl_o)
   );
 
+  // import flash_ctrl_reg_pkg::*;
+  // bind flash_ctrl flash_ctrl_csr_assert_fpv flash_ctrl_csr_assert (
+  //   .clk_i,
+  //   .rst_ni,
+  //   .h2d    (tl_i),
+  //   .d2h    (tl_o),
+  //   .reg2hw (reg2hw),
+  //   .hw2reg (hw2reg)
+  // );
+
 endmodule
