ecf4eebe8ef267efc22cfa3e056c07ea1208b4ac
Srikrishna Iyer <sriyer@google.com>
2020-07-31 14:00:12 -0700

    [dv] reorg gpio_bind and add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/gpio/dv/sva/gpio_bind.sv
+++ hw/ip/gpio/dv/sva/gpio_bind.sv
@@ -13,4 +13,14 @@ module gpio_bind;
     .d2h  (tl_o)
   );
 
+  import gpio_reg_pkg::*;
+  bind gpio gpio_csr_assert_fpv gpio_csr_assert (
+    .clk_i,
+    .rst_ni,
+    .h2d    (tl_i),
+    .d2h    (tl_o),
+    .reg2hw (reg2hw),
+    .hw2reg (hw2reg)
+  );
+
 endmodule
