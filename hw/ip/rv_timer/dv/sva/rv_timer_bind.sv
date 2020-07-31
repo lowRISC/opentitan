f654fad82c68b09520cf91b82b3f5efbf4158e0f
Srikrishna Iyer <sriyer@google.com>
2020-07-31 14:05:29 -0700

    [dv] reorg rv_timer bind & add CSR asserts
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/rv_timer/dv/sva/rv_timer_bind.sv
+++ hw/ip/rv_timer/dv/sva/rv_timer_bind.sv
@@ -13,4 +13,14 @@ module rv_timer_bind;
     .d2h  (tl_o)
   );
 
+  import rv_timer_reg_pkg::*;
+  bind rv_timer rv_timer_csr_assert_fpv rv_timer_csr_assert (
+    .clk_i,
+    .rst_ni,
+    .h2d    (tl_i),
+    .d2h    (tl_o),
+    .reg2hw (reg2hw),
+    .hw2reg (hw2reg)
+  );
+
 endmodule
