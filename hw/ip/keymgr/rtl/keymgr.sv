d94f1454464257238aa0e1b2fed122407f59852d
Timothy Chen <timothytim@google.com>
2020-09-09 15:44:35 -0700

    [keymgr] First draft of key manager module
    - Sideload interface currently assumed (decision not finalized)
    - Minimal secure hardneing (multi-bit signals, sparse FSM) in place
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [keymgr] Minor fixes to ensure advance transition can happen
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [keymgr] Minor fixes
    
    - only push to software registers when doing generate
    - check for correct key when generating
    - always over-write existing keys even when in disabled state
    - exclude specific fields from csr tests
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [keymgr] updates per review comments
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [keymgr] minor updates
    
    - address review comments
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

---
+++ hw/ip/keymgr/rtl/keymgr.sv
@@ -0,0 +1,387 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Key manager top level
+//
+
+`include "prim_assert.sv"
+
+module keymgr import keymgr_pkg::*; #(
+  parameter logic AlertAsyncOn = 1'b1
+) (
+  input clk_i,
+  input rst_ni,
+
+  // Bus Interface
+  input  tlul_pkg::tl_h2d_t tl_i,
+  output tlul_pkg::tl_d2h_t tl_o,
+
+  // key interface to crypto modules
+  output hw_key_req_t aes_key_o,
+  output hw_key_req_t hmac_key_o,
+  output hw_key_req_t kmac_key_o,
+
+  // data interface to/from crypto modules
+  output kmac_data_req_t kmac_data_o,
+  input kmac_data_rsp_t kmac_data_i,
+
+  // the following signals should eventually be wrapped into structs from other modules
+  input lc_data_t lc_i,
+  input otp_data_t otp_i,
+  input flash_key_t flash_i,
+
+  // interrupts and alerts
+  output logic intr_op_done_o,
+  output logic intr_err_o,
+  input  prim_alert_pkg::alert_rx_t [keymgr_reg_pkg::NumAlerts-1:0] alert_rx_i,
+  output prim_alert_pkg::alert_tx_t [keymgr_reg_pkg::NumAlerts-1:0] alert_tx_o
+);
+
+  import keymgr_reg_pkg::*;
+
+  `ASSERT_INIT(AdvDataWidth_A, AdvDataWidth <= KDFMaxWidth)
+  `ASSERT_INIT(IdDataWidth_A,  IdDataWidth  <= KDFMaxWidth)
+  `ASSERT_INIT(GenDataWidth_A, GenDataWidth <= KDFMaxWidth)
+  `ASSERT_INIT(OutputKeyDiff_A, HardOutputKey != SoftOutputKey)
+
+  // Register module
+  keymgr_reg2hw_t reg2hw;
+  keymgr_hw2reg_t hw2reg;
+
+  keymgr_reg_top u_reg (
+    .clk_i,
+    .rst_ni,
+    .tl_i,
+    .tl_o,
+    .reg2hw,
+    .hw2reg,
+    .devmode_i  (1'b1) // connect to real devmode signal in the future
+  );
+
+
+  /////////////////////////////////////
+  //  LFSR
+  /////////////////////////////////////
+
+  // A farily large lfsr is used here as entropy in multiple places.
+  // - populate the default working state
+  // - generate random inputs when a bad input is selected
+  //
+  // The first case is sensitive, and thus the working state is constructed
+  // through multiple rounds of the Lfsr
+  // The second case is less sensitive and is applied directly.  If the inputs
+  // have more bits than the lfsr output, the lfsr value is simply replicated
+
+  logic [63:0] lfsr;
+  logic lfsr_en;
+
+  prim_lfsr #(
+    .LfsrDw(64),
+    .StateOutDw(64)
+  ) u_lfsr (
+    .clk_i,
+    .rst_ni,
+    .lfsr_en_i(lfsr_en),
+    .seed_en_i(1'b0),
+    .seed_i('0),
+    .entropy_i('0), // TBD, this should be hooked up to an entropy distribution pkg
+    .state_o(lfsr)
+  );
+
+  /////////////////////////////////////
+  //  Key Manager Control
+  /////////////////////////////////////
+
+  keymgr_stage_e stage_sel;
+  keymgr_gen_out_e key_sel;
+  logic adv_en, id_en, gen_en;
+  logic load_key;
+  logic [Shares-1:0][KeyWidth-1:0] kmac_key;
+  logic op_done;
+  logic data_valid;
+  logic kmac_done;
+  logic kmac_input_invalid;
+  logic kmac_cmd_err;
+  logic kmac_fsm_err;
+  logic [Shares-1:0][KeyWidth-1:0] kmac_data;
+  logic [ErrLastPos-1:0] err_code;
+
+  keymgr_ctrl u_ctrl (
+    .clk_i,
+    .rst_ni,
+    .keymgr_en_i(lc_i.keymgr_en),
+    .prng_en_o(lfsr_en),
+    .entropy_i(lfsr[63:32]),  // TBD, recommend directly interfacing with DRBG for keymgr_ctrl
+    .init_i(reg2hw.control.init.q),
+    .op_i(keymgr_ops_e'(reg2hw.control.operation.q)),
+    .op_start_i(reg2hw.control.start.q),
+    .op_done_o(op_done),
+    .status_o(hw2reg.op_status.d),
+    .error_o(err_code),
+    .data_valid_o(data_valid),
+    .working_state_o(hw2reg.working_state.d),
+    .root_key_i(otp_i.root_key),
+    .hw_sel_o(key_sel),
+    .stage_sel_o(stage_sel),
+    .load_key_o(load_key),
+    .adv_en_o(adv_en),
+    .id_en_o(id_en),
+    .gen_en_o(gen_en),
+    .key_o(kmac_key),
+    .kmac_done_i(kmac_done),
+    .kmac_input_invalid_i(kmac_input_invalid),
+    .kmac_fsm_err_i(kmac_fsm_err),
+    .kmac_cmd_err_i(kmac_cmd_err),
+    .kmac_data_i(kmac_data)
+  );
+
+  assign hw2reg.control.start.d  = '0;
+  assign hw2reg.control.start.de = op_done;
+  assign hw2reg.control.init.d  = '0;
+  assign hw2reg.control.init.de = op_done;
+  assign hw2reg.op_status.de = op_done | reg2hw.control.start.q;
+
+
+  // working state is always visible
+  assign hw2reg.working_state.de = 1'b1;
+
+  // TBD
+  // software q / qe have no impact on cfgen
+  // however they exist due to a reggen script limitation, this should be addressed later
+  keymgr_cfg_en u_cfgen (
+    .clk_i,
+    .rst_ni,
+    .keymgr_en_i(lc_i.keymgr_en),
+    .set_i(op_done),
+    .clr_i(reg2hw.control.start.q),
+    .out_o(hw2reg.cfgen.d)
+  );
+
+  /////////////////////////////////////
+  //  Key Manager Input Construction
+  /////////////////////////////////////
+
+  // The various arrays of inputs for each operation
+  logic [2**StageWidth-1:0][AdvDataWidth-1:0] adv_matrix;
+  logic [2**StageWidth-1:0][IdDataWidth-1:0] id_matrix;
+  logic [GenDataWidth-1:0] gen_in;
+
+  // The max key version for each stage
+  logic [2**StageWidth-1:0][31:0] max_key_versions;
+
+  // Number of times the lfsr output fits into the inputs
+  localparam int AdvLfsrCopies = AdvDataWidth / 32;
+  localparam int IdLfsrCopies = IdDataWidth / 32;
+  localparam int GenLfsrCopies = GenDataWidth / 32;
+
+  // input checking - currently only MAX version, others TBD
+  logic key_version_good;
+
+  // Advance state operation input construction
+  for (genvar i = KeyMgrStages; i < 2**StageWidth; i++) begin : gen_adv_matrix_fill
+    assign adv_matrix[i] = {AdvLfsrCopies{lfsr[31:0]}};
+  end
+
+  // Advance to creator_root_key
+  assign adv_matrix[Creator] = AdvDataWidth'({reg2hw.rom_ext_desc,
+                                              RevisionSecret,
+                                              otp_i.devid,
+                                              lc_i.health_state,
+                                              flash_i.div_key});
+
+  // Advance to owner_intermediate_key
+  assign adv_matrix[OwnerInt] = AdvDataWidth'({reg2hw.software_binding,flash_i.owner_secret});
+
+  // Advance to owner_key
+  assign adv_matrix[Owner]   = AdvDataWidth'(reg2hw.software_binding);
+
+
+  // Generate Identity operation input construction
+  for (genvar i = KeyMgrStages; i < 2**StageWidth; i++) begin : gen_id_matrix_fill
+    assign id_matrix[i] = {IdLfsrCopies{lfsr[31:0]}};
+  end
+
+  assign id_matrix[Creator]  = CreatorIdentityKey;
+  assign id_matrix[OwnerInt] = OwnerIntIdentityKey;
+  assign id_matrix[Owner]    = OwnerIdentityKey;
+
+
+  // Generate output operation input construction
+  logic [KeyWidth-1:0] output_key;
+
+  assign output_key = (key_sel == HwKey) ? HardOutputKey : SoftOutputKey;
+  assign gen_in = (stage_sel == Disable) ? {GenLfsrCopies{lfsr[31:0]}} : {reg2hw.key_version,
+                                                                          reg2hw.salt,
+                                                                          output_key};
+
+  // Advance state operation input construction
+  for (genvar i = KeyMgrStages; i < 2**StageWidth; i++) begin : gen_key_version_fill
+    assign max_key_versions[i] = '0;
+  end
+
+  assign max_key_versions[Creator]  = reg2hw.max_creator_key_ver;
+  assign max_key_versions[OwnerInt] = reg2hw.max_owner_int_key_ver;
+  assign max_key_versions[Owner]    = reg2hw.max_owner_key_ver;
+
+
+  // General module for checking inputs
+  keymgr_input_checks u_checks (
+    .max_key_versions_i(max_key_versions),
+    .stage_sel_i(stage_sel),
+    .key_version_i(reg2hw.key_version),
+    .key_version_good_o(key_version_good)
+  );
+
+
+  /////////////////////////////////////
+  //  KMAC Control
+  /////////////////////////////////////
+
+  logic key_version_err;
+  logic [3:0] invalid_data;
+
+  assign key_version_err = !key_version_good;
+
+  assign invalid_data[OpAdvance]  = '0; // TBD
+  assign invalid_data[OpGenId]    = '0; // TBD
+  assign invalid_data[OpGenSwOut] = key_version_err; // TBD, more to come
+  assign invalid_data[OpGenHwOut] = key_version_err; // TBD, more to come
+
+  keymgr_kmac_if u_kmac_if (
+    .clk_i,
+    .rst_ni,
+    .adv_data_i(adv_matrix[stage_sel]),
+    .id_data_i(id_matrix[stage_sel]),
+    .gen_data_i(gen_in),
+    .inputs_invalid_i(invalid_data),
+    .inputs_invalid_o(kmac_input_invalid),
+    .adv_en_i(adv_en),
+    .id_en_i(id_en),
+    .gen_en_i(gen_en),
+    .done_o(kmac_done),
+    .data_o(kmac_data),
+    .kmac_data_o,
+    .kmac_data_i,
+    .entropy_i(lfsr[31:0]),
+    .fsm_error_o(kmac_fsm_err),
+    .cmd_error_o(kmac_cmd_err)
+  );
+
+
+  /////////////////////////////////////
+  //  Side load key storage
+  /////////////////////////////////////
+  keymgr_key_dest_e dest_sel;
+  logic aes_sel, hmac_sel, kmac_sel;
+
+  assign dest_sel = keymgr_key_dest_e'(reg2hw.control.dest_sel);
+  assign aes_sel  = dest_sel == Aes  & key_sel == HwKey;
+  assign hmac_sel = dest_sel == Hmac & key_sel == HwKey;
+  assign kmac_sel = dest_sel == Kmac & key_sel == HwKey;
+
+  keymgr_sideload_key u_aes_key (
+    .clk_i,
+    .rst_ni,
+    .keymgr_en_i(lc_i.keymgr_en),
+    .set_i(data_valid & aes_sel),
+    .clr_i(lc_i.keymgr_en), // TBD, should add an option for software clear later
+    .entropy_i(lfsr[31:0]), //TBD, recommend directly interfacign with DRBG entropy for keys
+    .key_i(kmac_data),
+    .key_o(aes_key_o)
+  );
+
+  keymgr_sideload_key u_hmac_key (
+    .clk_i,
+    .rst_ni,
+    .keymgr_en_i(lc_i.keymgr_en),
+    .set_i(data_valid & hmac_sel),
+    .clr_i(lc_i.keymgr_en), // TBD, should add an option for software clear later
+    .entropy_i(lfsr[31:0]),
+    .key_i(kmac_data),
+    .key_o(hmac_key_o)
+  );
+
+  keymgr_sideload_key u_kmac_key (
+    .clk_i,
+    .rst_ni,
+    .keymgr_en_i(lc_i.keymgr_en),
+    .set_i(load_key | (data_valid & kmac_sel)),
+    .clr_i(~lc_i.keymgr_en), // TBD, should add an option for software clear later
+    .entropy_i(lfsr[31:0]),
+    .key_i(load_key ? kmac_key : kmac_data),
+    .key_o(kmac_key_o)
+  );
+
+  for (genvar i = 0; i < 8; i++) begin : gen_sw_assigns
+    assign hw2reg.sw_share0_output[i].d  = kmac_data[0][i*32 +: 32];
+    assign hw2reg.sw_share1_output[i].d  = kmac_data[1][i*32 +: 32];
+    assign hw2reg.sw_share0_output[i].de = data_valid & (key_sel == SwKey);
+    assign hw2reg.sw_share1_output[i].de = data_valid & (key_sel == SwKey);
+  end
+
+
+  /////////////////////////////////////
+  //  Alerts and Interrupts
+  /////////////////////////////////////
+
+  prim_intr_hw #(.Width(1)) u_intr_op_done (
+    .event_intr_i           (op_done),
+    .reg2hw_intr_enable_q_i (reg2hw.intr_enable.op_done.q),
+    .reg2hw_intr_test_q_i   (reg2hw.intr_test.op_done.q),
+    .reg2hw_intr_test_qe_i  (reg2hw.intr_test.op_done.qe),
+    .reg2hw_intr_state_q_i  (reg2hw.intr_state.op_done.q),
+    .hw2reg_intr_state_de_o (hw2reg.intr_state.op_done.de),
+    .hw2reg_intr_state_d_o  (hw2reg.intr_state.op_done.d),
+    .intr_o                 (intr_op_done_o)
+  );
+
+  logic [ErrLastPos-1:0] err_code_q;
+  assign hw2reg.err_code.invalid_op.d          = err_code[ErrInvalidOp];
+  assign hw2reg.err_code.invalid_cmd.d         = err_code[ErrInvalidCmd];
+  assign hw2reg.err_code.invalid_kmac_input.d  = err_code[ErrInvalidIn];
+  assign hw2reg.err_code.invalid_kmac_data.d   = err_code[ErrInvalidOut];
+  assign hw2reg.err_code.invalid_op.de         = 1'b1;
+  assign hw2reg.err_code.invalid_cmd.de        = 1'b1;
+  assign hw2reg.err_code.invalid_kmac_input.de = 1'b1;
+  assign hw2reg.err_code.invalid_kmac_data.de  = 1'b1;
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      err_code_q <= '0;
+    end else begin
+      err_code_q <= err_code;
+    end
+  end
+
+  // interrupts are only generated on changes to avoid interrupt storms
+  prim_intr_hw #(.Width(1)) u_err_code (
+    .event_intr_i           (err_code != err_code_q),
+    .reg2hw_intr_enable_q_i (reg2hw.intr_enable.err.q),
+    .reg2hw_intr_test_q_i   (reg2hw.intr_test.err.q),
+    .reg2hw_intr_test_qe_i  (reg2hw.intr_test.err.qe),
+    .reg2hw_intr_state_q_i  (reg2hw.intr_state.err.q),
+    .hw2reg_intr_state_de_o (hw2reg.intr_state.err.de),
+    .hw2reg_intr_state_d_o  (hw2reg.intr_state.err.d),
+    .intr_o                 (intr_err_o)
+  );
+
+  // alerts are generated whenever there is a persisting error status
+  prim_alert_sender #(
+    .AsyncOn(AlertAsyncOn)
+  ) u_err_alert (
+    .clk_i,
+    .rst_ni,
+    .alert_i(|reg2hw.err_code),
+    .alert_rx_i(alert_rx_i),
+    .alert_tx_o(alert_tx_o)
+  );
+
+  /////////////////////////////////////
+  //  Assertions
+  /////////////////////////////////////
+
+  // Only 1 entity should be trying to use the secret kmac key input
+  `ASSERT(KmacKeyLoadExclusive_a, $onehot0({load_key, data_valid & kmac_sel}))
+
+endmodule // keymgr
---
+++ hw/ip/keymgr/rtl/keymgr_ctrl.sv
@@ -0,0 +1,337 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Key manager top level
+//
+
+`include "prim_assert.sv"
+
+module keymgr_ctrl import keymgr_pkg::*;(
+  input clk_i,
+  input rst_ni,
+
+  // lifecycle enforcement
+  input keymgr_en_i,
+
+  // entropy input
+  input [(LfsrWidth/2)-1:0] entropy_i,
+  output logic prng_en_o,
+
+  // Software interface
+  input init_i,
+  input keymgr_ops_e op_i,
+  input op_start_i,
+  output logic op_done_o,
+  output keymgr_op_status_e status_o,
+  output logic [ErrLastPos-1:0] error_o,
+  output logic data_valid_o,
+  output keymgr_working_state_e working_state_o,
+
+  // Data input
+  input  [KeyWidth-1:0] root_key_i,
+  output keymgr_gen_out_e hw_sel_o,
+  output keymgr_stage_e stage_sel_o,
+
+  // KMAC ctrl interface
+  output logic adv_en_o,
+  output logic id_en_o,
+  output logic gen_en_o,
+  output logic [Shares-1:0][KeyWidth-1:0] key_o,
+  output logic load_key_o,
+  input kmac_done_i,
+  input kmac_input_invalid_i, // asserted when selected data fails criteria check
+  input kmac_fsm_err_i, // asserted when kmac fsm reaches unexpected state
+  input kmac_cmd_err_i, // asserted when more than one command given to kmac
+  input [Shares-1:0][KeyWidth-1:0] kmac_data_i
+);
+  localparam int EntropyWidth = LfsrWidth / 2;
+  localparam int EntropyRounds = KeyWidth / EntropyWidth;
+  localparam int CntWidth = $clog2(EntropyRounds + 1);
+
+  keymgr_working_state_e state_q, state_d;
+  logic [Shares-1:0][EntropyRounds-1:0][EntropyWidth-1:0] key_state_q, key_state_d;
+
+  logic [CntWidth-1:0] cnt;
+  logic cnt_en;
+  logic cnt_clr;
+  logic data_valid;
+  logic adv_en_q;
+  logic op_accepted;
+  logic invalid_op;
+
+  // disable is treated like an advanced call
+  logic advance_sel;
+  logic disable_sel;
+  logic gen_id_sel;
+  logic gen_out_sw_sel;
+  logic gen_out_hw_sel;
+  logic gen_out_sel;
+  logic gen_sel;
+
+  // something went wrong with the kmac interface operation
+  logic kmac_op_err;
+
+  assign advance_sel    = op_i == OpAdvance  & keymgr_en_i;
+  assign gen_id_sel     = op_i == OpGenId    & keymgr_en_i;
+  assign gen_out_sw_sel = op_i == OpGenSwOut & keymgr_en_i;
+  assign gen_out_hw_sel = op_i == OpGenHwOut & keymgr_en_i;
+  assign gen_out_sel    = gen_out_sw_sel | gen_out_hw_sel;
+  assign gen_sel        = gen_id_sel | gen_out_sel;
+
+  // disable is selected whenever a normal operation is not, and when
+  // keymgr is disabled
+  assign disable_sel    = !(gen_sel | advance_sel) | !keymgr_en_i;
+
+  assign adv_en_o   = op_accepted & (advance_sel | disable_sel);
+  assign id_en_o    = op_accepted & gen_id_sel;
+  assign gen_en_o   = op_accepted & gen_out_sel;
+  assign load_key_o = adv_en_o & !adv_en_q;
+
+  // check incoming kmac data validity
+  // also check inputs used during compute
+  assign data_valid = valid_data_chk(kmac_data_i[0]) & valid_data_chk(kmac_data_i[1])
+    & !kmac_input_invalid_i;
+
+  // Unlike the key state, the working state can be safely reset.
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      state_q <= StReset;
+      adv_en_q <= 1'b0;
+    end else begin
+      state_q <= state_d;
+      adv_en_q <= adv_en_o;
+    end
+  end
+
+  // prevents unknowns from reaching the outside world.
+  // whatever operation causes the input data select to be disabled should
+  // also not expose the key state
+  assign key_o = disable_sel ? {EntropyRounds * Shares {entropy_i}} : key_state_q;
+
+  // key state is intentionally not reset
+  always_ff @(posedge clk_i) begin
+    key_state_q <= key_state_d;
+  end
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      cnt <= '0;
+    end else if (cnt_clr) begin
+      cnt <= '0;
+    end else if (cnt_en) begin
+      cnt <= cnt + 1'b1;
+    end
+  end
+
+  assign kmac_op_err = kmac_cmd_err_i | kmac_fsm_err_i;
+
+  always_comb begin
+    // persistent data
+    state_d = state_q;
+    key_state_d = key_state_q;
+
+    // locally consumed select signals
+    cnt_en = 1'b0;
+    cnt_clr = 1'b0;
+    op_accepted = 1'b0;
+    invalid_op = 1'b0;
+
+    // data update and select signals
+    hw_sel_o = HwKey;
+    stage_sel_o = Disable;
+
+    // enable prng toggling
+    prng_en_o = 1'b1;
+
+    // TBD
+    // Wait for some more software feedback to see if the states remain so similar.
+    // If yes, we can merge 3 states below with some minor tweaking.
+    unique case (state_q)
+      // This state does not accept any command. Issuing any command
+      // will cause an immediate error
+      StReset: begin
+        // in reset state, don't enable entropy yet, since there are no users.
+        // long term, this should be replaced by a req/ack with csrng
+        prng_en_o = 1'b0;
+        op_done_o = op_start_i;
+        invalid_op = op_start_i;
+
+        // When initialization command is given, begin
+        state_d = (init_i && keymgr_en_i) ? StWipe : StReset;
+      end
+
+      // This state does not accept any command. Issuing any command
+      // will cause an immediate error
+      StWipe: begin
+        op_done_o = op_start_i;
+        invalid_op = op_start_i;
+
+        // populate both shares with the same entropy
+        // This is the default mask
+        if (cnt < EntropyRounds) begin
+          cnt_en = 1'b1;
+          for (int i = 0; i < Shares; i++) begin
+            key_state_d[i][cnt] = entropy_i;
+          end
+        end
+        // when mask population is complete, xor the root_key into the zero share
+        // if in the future the root key is updated to 2 shares, it will direclty overwrite
+        // the values here
+        else begin
+          cnt_clr = 1'b1;
+          key_state_d[0] = key_state_q[0] ^ root_key_i;
+          state_d = StInit;
+        end
+      end
+
+      // Beginning from the Init state, operations are accepted.
+      // Only valid operation is advance state. If invalid command received,
+      // random data is selected for operation and no persistent state is changed.
+      StInit: begin
+        op_done_o = op_start_i & kmac_done_i;
+
+        if (op_start_i || !keymgr_en_i) begin
+          op_accepted = 1'b1;
+          stage_sel_o = !advance_sel ? Disable : Creator;
+        end
+
+        // key state is updated when it is an advance call
+        if (op_done_o && (disable_sel || kmac_op_err)) begin
+          key_state_d = kmac_data_i;
+          state_d = StDisabled;
+        end else if (op_done_o && advance_sel) begin
+          key_state_d = data_valid ? kmac_data_i : key_state_q;
+          state_d = StCreatorRootKey;
+        end else if (op_done_o) begin
+          invalid_op = 1'b1;
+        end
+      end
+
+      // all commands are valid during this stage
+      StCreatorRootKey: begin
+        op_done_o = op_start_i & kmac_done_i;
+
+        // when generating, select creator data input
+        // when advancing, select owner intermediate key as target
+        // when disabling, select random data input
+
+        if (op_start_i || !keymgr_en_i) begin
+          op_accepted = 1'b1;
+          stage_sel_o = disable_sel ? Disable  :
+                        advance_sel ? OwnerInt : Creator;
+          hw_sel_o = gen_out_hw_sel ? HwKey : SwKey;
+        end
+
+        // key state is updated when it is an advance call
+        if (op_done_o && (disable_sel || kmac_op_err)) begin
+          key_state_d = kmac_data_i;
+          state_d = StDisabled;
+        end else if (op_done_o && advance_sel) begin
+          key_state_d = data_valid ? kmac_data_i : key_state_q;
+          state_d = StOwnerIntKey;
+        end
+      end
+
+
+      // all commands are valid during this stage
+      StOwnerIntKey: begin
+        op_done_o = op_start_i & kmac_done_i;
+
+        // when generating, select creator data input
+        // when advancing, select owner intermediate key as target
+        // when disabling, select random data input
+        if (op_start_i || !keymgr_en_i) begin
+          op_accepted = 1'b1;
+          stage_sel_o = disable_sel ? Disable  :
+                        advance_sel ? Owner : OwnerInt;
+          hw_sel_o = gen_out_hw_sel ? HwKey : SwKey;
+        end
+
+        if (op_done_o && (disable_sel || kmac_op_err)) begin
+          key_state_d = kmac_data_i;
+          state_d = StDisabled;
+        end else if (op_done_o && advance_sel) begin
+          key_state_d = data_valid ? kmac_data_i : key_state_q;
+          state_d = StOwnerKey;
+        end
+      end
+
+      // all commands are valid during this stage
+      // however advance goes directly to disabled state
+      StOwnerKey: begin
+        op_done_o = op_start_i & kmac_done_i;
+
+        if (op_start_i || !keymgr_en_i) begin
+          op_accepted = 1'b1;
+          stage_sel_o = disable_sel || advance_sel  ? Disable : Owner;
+          hw_sel_o = gen_out_hw_sel ? HwKey : SwKey;
+        end
+
+        // Calling advanced from ownerKey also leads to disable
+        // Thus data_valid is not checked
+        if (op_done_o && (advance_sel || disable_sel || kmac_op_err)) begin
+          key_state_d = kmac_data_i;
+          state_d = StDisabled;
+        end
+      end
+
+      // Terminal state (StDisabled is included)
+      // However, it will continue to kick off dummy transactions
+      default: begin
+        op_done_o = op_start_i & kmac_done_i;
+
+        // accept any command, but always select fake data
+        op_accepted = op_start_i;
+        stage_sel_o = Disable;
+        hw_sel_o = gen_out_hw_sel ? HwKey : SwKey;
+
+        // During disabled state, wildly update everything
+        key_state_d = op_done_o ? kmac_data_i : key_state_q;
+
+        // Despite accepting all commands, operations are always
+        // considered invalid in disabled state
+        invalid_op = 1'b1;
+      end
+
+    endcase // unique case (state_q)
+  end
+
+
+  // Current working state provided for software read
+  assign working_state_o = state_q;
+
+  // if operation was never accepted (ie a generate was called in StReset / StWipe), then
+  // never update the sw / hw outputs when operation is complete
+  assign data_valid_o = op_done_o & op_accepted & data_valid & gen_sel;
+
+  // data errors are not relevant when operation was not accepted.
+  assign error_o[ErrInvalidOp]  = invalid_op;
+  assign error_o[ErrInvalidCmd] = op_start_i & op_accepted & kmac_op_err;
+  assign error_o[ErrInvalidIn]  = op_done_o & op_accepted & kmac_input_invalid_i;
+  assign error_o[ErrInvalidOut] = op_done_o & op_accepted & ~data_valid;
+
+  always_comb begin
+    status_o = OpIdle;
+    if (op_done_o) begin
+      status_o = |error_o ? OpDoneFail : OpDoneSuccess;
+    end else if (op_start_i) begin
+      status_o = OpWip;
+    end
+  end
+
+
+
+  ///////////////////////////////
+  // Functions
+  ///////////////////////////////
+
+  // unclear what this is supposed to be yet
+  // right now just check to see if it not all 0's and not all 1's
+  function automatic logic valid_data_chk (logic [KeyWidth-1:0] value);
+
+    return |value & ~&value;
+
+  endfunction // byte_mask
+
+endmodule
