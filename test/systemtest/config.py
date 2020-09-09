03aaf320a4b519469668dead50d12ef1c136fb3b
Philipp Wagner <phw@lowrisc.org>
2020-09-09 19:11:44 +0100

    [test] Refactor system tests to use pytest as runner
    System tests, living in `test/systemtest`, test the whole end-to-end
    design of OpenTitan: they initialize the target (e.g. a simulation, an
    FPGA, or later an ASIC), load test software as needed, and check for
    outputs or other expected behavior. To do so, multiple components need
    to work together. For example, a simulation process, components for data
    transfer (such as OpenOCD), components which interact with (simulated or
    real) external interfaces, etc. Pytest provides a very convenient way to
    handle such situations through their fixtures. Our current system tests
    make use of this functionality.
    
    What we haven't been using in the past is the ability of pytest to serve
    as test runner: select tests, collect their output and results, etc.
    (Instead, a shell script was used.)
    
    This PR refactors the system tests to make use of pytest as test runner,
    with one main consequence: Tests are now "opinionated", they don't take
    configuration parameters through command line arguments any more. Tests
    expect build outputs in the distribution directory (`build-bin`) to be
    in the well-known layout for this directory.
    
    Notes:
    * This commit lays the groundwork to add tests running on an FPGA target
      in a similar way as the verilated tests, which should explain some of
      the structuring of the code.
    * The ability to run the debug "compliance test" built into OpenOCD,
      which was present in the `test/systemtest/openocd_verilator_test.py`
      file, has been removed temporarily. This test was unmaintained, and
      will be brought back in revised form at a later time.
    * Equally, the "FPGA test runner" available in
      `test/systemtest/functional_fpga_test.py` has been rarely used (as a
      poll on Slack showed) and is also removed.
      It will be brought back in modified form in a future commit.
    
    Signed-off-by: Philipp Wagner <phw@lowrisc.org>

---
+++ test/systemtest/config.py
@@ -0,0 +1,17 @@
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Self-checking test applications, which return PASS or FAIL after completion.
+TEST_APPS_SELFCHECKING = [
+    "aes_test",
+    "crt_test",
+    "dif_plic_sanitytest",
+    "dif_rv_timer_sanitytest",
+    "dif_uart_sanitytest",
+    "flash_ctrl_test",
+    "pmp_sanitytest_napot",
+    "pmp_sanitytest_tor",
+    "sha256_test",
+    "usbdev_test",
+]
--- test/systemtest/conftest.py
+++ test/systemtest/conftest.py
@@ -4,24 +4,13 @@
 
 import logging
 import os
-import shutil
-import subprocess
 from pathlib import Path
 
 import pytest
-import yaml
+from . import utils
 
+log = logging.getLogger(__name__)
 
-def pytest_addoption(parser):
-    """Test harness configuration options."""
-    parser.addoption("--test_bin", action="store", default="")
-    parser.addoption("--rom_bin", action="store", default="")
-    parser.addoption("--verilator_model", action="store", default="")
-    parser.addoption("--openocd", action="store", default="openocd")
-    parser.addoption("--uart_timeout", action="store", default="60")
-    parser.addoption("--fpga_uart", action="store", default="")
-    parser.addoption("--spiflash", action="store", default="")
-    parser.addoption("--log", action="store", default="")
 
 @pytest.hookimpl(tryfirst=True)
 def pytest_exception_interact(node, call, report):
@@ -29,45 +18,15 @@ def pytest_exception_interact(node, call, report):
     try:
         if not report.failed:
             return
-        if not 'tmp_path' in node.funcargs:
+        if 'tmp_path_factory' not in node.funcargs:
             return
-    except:
+    except Exception:
         return
 
-    tmp_path = str(node.funcargs['tmp_path'])
-    logging.debug("================= DUMP OF ALL TEMPORARY FILES =================")
-
-    for f in os.listdir(tmp_path):
-        f_abs = os.path.join(tmp_path, f)
-        if not os.path.isfile(f_abs):
-            continue
-        logging.debug("vvvvvvvvvvvvvvvvvvvv {} vvvvvvvvvvvvvvvvvvvv".format(f))
-        with open(f_abs, 'r') as fp:
-            for line in fp.readlines():
-                logging.debug(line.rstrip())
-        logging.debug("^^^^^^^^^^^^^^^^^^^^ {} ^^^^^^^^^^^^^^^^^^^^".format(f))
-
+    utils.dump_temp_files(node.funcargs['tmp_path_factory'].getbasetemp())
 
 @pytest.fixture(scope="session")
-def localconf(request):
-    """Host-local configuration."""
-    if os.getenv('OPENTITAN_TEST_LOCALCONF') and os.path.isfile(
-            os.environ['OPENTITAN_TEST_LOCALCONF']):
-        localconf_yaml_file = os.environ['OPENTITAN_TEST_LOCALCONF']
-    else:
-        XDG_CONFIG_HOME = os.getenv(
-            'XDG_CONFIG_HOME', os.path.join(os.environ['HOME'], '.config'))
-        localconf_yaml_file = os.path.join(XDG_CONFIG_HOME, 'opentitan',
-                                           'test-localconf.yaml')
-    logging.getLogger(__name__).info('Reading configuration from ' +
-                                     localconf_yaml_file)
-
-    with open(str(localconf_yaml_file), 'r') as fp:
-        return yaml.load(fp)
-
-
-@pytest.fixture(scope="session")
-def topsrcdir(request):
+def topsrcdir():
     """Return the top-level source directory as Path object."""
     # TODO: Consider making this configurable using a pytest arg.
     path = (Path(os.path.dirname(__file__)) / '..' / '..').resolve()
@@ -76,70 +35,14 @@ def topsrcdir(request):
 
 
 @pytest.fixture(scope="session")
-def sw_test_bin(pytestconfig):
-    """Return path to software test binary."""
-    path = Path(pytestconfig.getoption('test_bin')).resolve()
-    assert path.is_file()
-    return path
+def bin_dir(topsrcdir):
+    """ Return the BIN_DIR (build output directory) """
+    if 'BIN_DIR' in os.environ:
+        bin_dir = Path(os.environ['BIN_DIR'])
+        log.info("Using build outputs from environment variable BIN_DIR={}".format(str(bin_dir)))
+    else:
+        bin_dir = topsrcdir / 'build-bin'
+        log.info("Using build outputs from $REPO_TOP/build-bin ({})".format(str(bin_dir)))
 
-@pytest.fixture(scope="session")
-def rom_bin(pytestconfig):
-    """Return path to boot_rom binary."""
-    path = Path(pytestconfig.getoption('rom_bin')).resolve()
-    assert path.is_file()
-    return path
-
-
-@pytest.fixture(scope="session")
-def sim_top_build(pytestconfig):
-    """Return path to Verilator sim model."""
-    path = Path(pytestconfig.getoption('verilator_model')).resolve()
-    assert path.is_file()
-    return path
-
-
-@pytest.fixture(scope="session")
-def openocd(pytestconfig):
-    """Return path to OpenOCD executable."""
-    path = Path(pytestconfig.getoption('openocd'))
-    # TODO: Require that the OpenOCD executable be passed as a command-line
-    # argument in the future, rather than relying on $PATH lookup.
-    # assert path.is_file()
-    return path
-
-@pytest.fixture(scope="session")
-def uart_timeout(pytestconfig):
-    """Return the timeout in seconds for UART to print PASS."""
-    return int(pytestconfig.getoption('uart_timeout'))
-
-@pytest.fixture(scope="session")
-def fpga_uart(pytestconfig):
-    """Return the path to the UART attached to the FPGA."""
-    path = Path(pytestconfig.getoption('fpga_uart')).resolve()
-    assert path.exists() and not path.is_dir()
-    return path
-
-@pytest.fixture(scope="session")
-def spiflash(pytestconfig):
-    """Return the path to the spiflash executable."""
-    path = Path(pytestconfig.getoption('spiflash')).resolve()
-    assert path.is_file()
-    return path
-
-@pytest.fixture(scope="session")
-def logfile(pytestconfig):
-    """Return path to logfile."""
-    log = pytestconfig.getoption('log')
-    if not log:
-        return
-    # The strict option is only availabe on python 3.6
-    # CI currently uses >=3.5.2
-    # Turns out however even not using resolve doesn't work.
-    # The logging function in 3.5 uses os.path.isabs to check whether
-    # path is absolute and does not accept POSIXPATH objects
-    # path = Path(log).resolve(strict=False)
-    # assert not path.is_dir()
-
-    path = os.path.abspath(log)
-    assert not os.path.isdir(path)
-    return path
+    assert bin_dir.is_dir()
+    return bin_dir
