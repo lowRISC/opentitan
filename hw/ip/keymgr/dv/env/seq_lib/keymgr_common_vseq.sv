91054094b5b13654ab68b797cf6c6b805a442460
Timothy Chen <timothytim@google.com>
2020-09-10 18:12:37 -0700

    [keymgr] Add pre-sanity check to keymgr
    - just exercise keymgr advance / generation mechanisms to make sure things are moving
    - pass csr suite
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [doc] Temporarily remove link to design doc
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [keymgr, dv] Remove unnecessary file
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

---
+++ hw/ip/keymgr/dv/env/seq_lib/keymgr_common_vseq.sv
@@ -0,0 +1,22 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class keymgr_common_vseq extends keymgr_base_vseq;
+  `uvm_object_utils(keymgr_common_vseq)
+
+  constraint num_trans_c {
+    num_trans inside {[1:2]};
+  }
+  `uvm_object_new
+
+  virtual task pre_start();
+    do_keymgr_init = 1'b0;
+    super.pre_start();
+  endtask
+
+  virtual task body();
+    run_common_vseq_wrapper(num_trans);
+  endtask : body
+
+endclass
