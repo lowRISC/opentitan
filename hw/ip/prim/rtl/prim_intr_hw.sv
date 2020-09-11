e4e981ead9960755671f0de865225064bfd9413f
Timothy Chen <timothytim@google.com>
2020-09-11 10:38:21 -0700

    [prim] Add option to register output for interrupts
    - Need to update peripherals to add clk and reset ports
    
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [prim] fix typo
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- hw/ip/prim/rtl/prim_intr_hw.sv
+++ hw/ip/prim/rtl/prim_intr_hw.sv
@@ -7,8 +7,13 @@
 // This module can be instantiated once per interrupt field, or
 // "bussified" with all fields of the interrupt vector.
 
-module prim_intr_hw #(parameter int unsigned Width = 1) (
+module prim_intr_hw # (
+  parameter int unsigned Width = 1,
+  parameter bit FlopOutput = 1
+) (
   // event
+  input  clk_i,
+  input  rst_ni,
   input  [Width-1:0]  event_intr_i,
 
   // register interface
@@ -20,7 +25,7 @@ module prim_intr_hw #(parameter int unsigned Width = 1) (
   output [Width-1:0]  hw2reg_intr_state_d_o,
 
   // outgoing interrupt
-  output [Width-1:0]  intr_o
+  output logic [Width-1:0]  intr_o
 );
 
   logic  [Width-1:0]    new_event;
@@ -30,7 +35,24 @@ module prim_intr_hw #(parameter int unsigned Width = 1) (
   // for scalar interrupts, this resolves to '1' with new event
   // for vector interrupts, new events are OR'd in to existing interrupt state
   assign hw2reg_intr_state_d_o  =  new_event | reg2hw_intr_state_q_i;
-  assign intr_o = reg2hw_intr_state_q_i & reg2hw_intr_enable_q_i;
+
+  if (FlopOutput == 1) begin : gen_flop_intr_output
+    // flop the interrupt output
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+      if (!rst_ni) begin
+        intr_o <= 1'b0;
+      end else begin
+        intr_o <= reg2hw_intr_state_q_i & reg2hw_intr_enable_q_i;
+      end
+    end
+
+  end else begin : gen_intr_passthrough_output
+    logic unused_clk;
+    logic unused_rst_n;
+    assign unused_clk = clk_i;
+    assign unused_rst_n = rst_ni;
+    assign intr_o = reg2hw_intr_state_q_i & reg2hw_intr_enable_q_i;
+  end
+
 
 endmodule
-
