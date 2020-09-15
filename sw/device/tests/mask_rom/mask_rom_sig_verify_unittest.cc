d6d42ffc0154a91a8dbf631f39d8a5b5f7214a7a
Alphan Ulusoy <alphan@google.com>
2020-09-15 10:13:54 -0400

    [mask_rom/sig_verify] Dummy test to compile sig_verify.
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

---
+++ sw/device/tests/mask_rom/mask_rom_sig_verify_unittest.cc
@@ -0,0 +1,19 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/testing/mask_rom_test.h"
+#include "sw/device/mask_rom/sig_verify.h"
+
+namespace mask_rom_test {
+namespace {
+
+using namespace testing;
+using namespace mask_rom_test;
+
+class SigVerifyTest : public MaskRomTest {};
+
+TEST_F(SigVerifyTest, Build) {}
+
+}  // namespace
+}  // namespace mask_rom_test
