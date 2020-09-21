4e17629585a8077d7c81d9ea16409cad673678ea
Michael Schaffner <msf@google.com>
2020-09-21 16:58:35 -0700

    [otp_ctrl] Update to use LFSR permutation feature
    Signed-off-by: Michael Schaffner <msf@google.com>

--- hw/ip/otp_ctrl/rtl/otp_ctrl.sv
+++ hw/ip/otp_ctrl/rtl/otp_ctrl.sv
@@ -12,8 +12,15 @@ module otp_ctrl
   import otp_ctrl_reg_pkg::*;
 #(
   parameter logic [NumAlerts-1:0]  AlertAsyncOn = {NumAlerts{1'b1}},
-  // TODO: need to override this during build time randomization
-  parameter logic [TimerWidth-1:0] LfsrSeed    = TimerWidth'(546532468)
+  // TODO: These constants have to be replaced by the silicon creator before taping out.
+  parameter logic [TimerWidth-1:0]       LfsrSeed     = TimerWidth'(1'b1),
+  parameter logic [TimerWidth-1:0][31:0] LfsrPerm     = {
+    32'd13, 32'd17, 32'd29, 32'd11, 32'd28, 32'd12, 32'd33, 32'd27,
+    32'd05, 32'd39, 32'd31, 32'd21, 32'd15, 32'd01, 32'd24, 32'd37,
+    32'd32, 32'd38, 32'd26, 32'd34, 32'd08, 32'd10, 32'd04, 32'd02,
+    32'd19, 32'd00, 32'd20, 32'd06, 32'd25, 32'd22, 32'd03, 32'd35,
+    32'd16, 32'd14, 32'd23, 32'd07, 32'd30, 32'd09, 32'd18, 32'd36
+  }
 ) (
   input                             clk_i,
   input                             rst_ni,
@@ -260,6 +267,7 @@ module otp_ctrl
 
   otp_ctrl_lfsr_timer #(
     .LfsrSeed(LfsrSeed),
+    .LfsrPerm(LfsrPerm),
     .EntropyWidth(4)
   ) u_otp_ctrl_lfsr_timer (
     .clk_i,
