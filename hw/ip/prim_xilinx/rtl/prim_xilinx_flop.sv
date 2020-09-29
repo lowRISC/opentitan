9ccd775ff2b17aef0ca88b3b6bc6863f3544712b
Michael Schaffner <msf@opentitan.org>
2020-09-29 12:13:07 -0700

    [prim_xilinx_flop] Add a Xilinx version with keep attribute
    This adds a prim_xilinx_flop.sv variant that places a keep attribute on
    the flop output in order to prevent Vivado from optimizing the flop
    away.
    
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

---
+++ hw/ip/prim_xilinx/rtl/prim_xilinx_flop.sv
@@ -0,0 +1,27 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+`include "prim_assert.sv"
+
+module prim_xilinx_flop # (
+  parameter int Width      = 1,
+  localparam int WidthSubOne = Width-1,
+  parameter logic [WidthSubOne:0] ResetValue = 0
+) (
+  input clk_i,
+  input rst_ni,
+  input [Width-1:0] d_i,
+  // Prevent Vivado from optimizing this signal away.
+  (* keep = "true" *) output logic [Width-1:0] q_o
+);
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      q_o <= ResetValue;
+    end else begin
+      q_o <= d_i;
+    end
+  end
+
+endmodule // prim_xilinx_flop
