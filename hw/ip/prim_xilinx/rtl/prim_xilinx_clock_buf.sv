f851850f6e58a00ef292f1cd4278baa035833aed
Pirmin Vogel <vogelpi@lowrisc.org>
2020-10-02 09:41:37 +0200

    [prim] Add clock buffer primitive for Xilinx FPGAs
    This commit adds a Xilinx-specific clock buffer primitive that
    internally uses a BUFG instance. So far, the generic primitive was used
    that did not instantiate a BUFG or similar on FPGA.
    
    This is related to lowRISC/OpenTitan#3634.
    
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

---
+++ hw/ip/prim_xilinx/rtl/prim_xilinx_clock_buf.sv
@@ -0,0 +1,15 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+module prim_xilinx_clock_buf (
+  input clk_i,
+  output logic clk_o
+);
+
+  BUFG bufg_i (
+    .I (clk_i),
+    .O (clk_o)
+  );
+
+endmodule
