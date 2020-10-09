dae0c6519a02a607addb6de78447e8b136df4d2c
Cindy Chen <chencindy@google.com>
2020-10-09 10:00:51 -0700

    [hmac] remove alert from hmac
    Hmac alert is a temp connect to test alert functionality. Hmac does not
    intend to connect to any alerts. Since now we have real alerts connected
    from other modules, we can safely remove hmac alerts from design, dv,
    and top-level.
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/ip/hmac/dv/env/hmac_env_cfg.sv
+++ hw/ip/hmac/dv/env/hmac_env_cfg.sv
@@ -8,7 +8,6 @@ class hmac_env_cfg extends cip_base_env_cfg #(.RAL_T(hmac_reg_block));
   `uvm_object_new
 
   virtual function void initialize(bit [TL_AW-1:0] csr_base_addr = '1);
-    list_of_alerts = {"msg_push_sha_disabled"};
     super.initialize(csr_base_addr);
     // set num_interrupts & num_alerts which will be used to create coverage and more
     num_interrupts = ral.intr_state.get_n_used_bits();
