6b3753defa58ecf686bb758e22a4bcadf24103c2
Srikrishna Iyer <sriyer@google.com>
2020-10-12 02:25:10 -0700

    [dv,sim_sram_if] Minor enhancements to sim_sram_if
    - Added both, tl_d2h and h2d ports as inputs
    - Added a wr_valid signal to qualify writes to the SRAM
    addr region.
    - Spacing fixes in `sim_sram.sv`
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/dv/sv/sim_sram/sim_sram.sv
+++ hw/dv/sv/sim_sram/sim_sram.sv
@@ -10,8 +10,8 @@ module sim_sram #(
   parameter int Depth           = 8,    // 8 locations - can be higher if sim demands it.
   parameter bit ErrOnRead       = 1'b1  // CPU only sinks info.
 ) (
-  input clk_i,
-  input rst_ni,
+  input logic clk_i,
+  input logic rst_ni,
 
   // Incoming TL access.
   input  tlul_pkg::tl_h2d_t tl_in_i,
@@ -43,31 +43,33 @@ module sim_sram #(
   // Separate interface to set the SRAM start address (done by the testbench) & monitor the SRAM
   // access.
   sim_sram_if #(
-      .AddrWidth(AddrWidth)
+    .AddrWidth(AddrWidth)
   ) u_sim_sram_if (
-      .clk_i,
-      .tl_i(tl_socket_h2d[1])
+    .clk_i,
+    .rst_ni,
+    .tl_h2d(tl_socket_h2d[1]),
+    .tl_d2h(tl_socket_d2h[1])
   );
 
   // Split the incoming access into two.
   tlul_socket_1n #(
-      .N        (2),
-      .HReqPass (1'b1),
-      .HRspPass (1'b1),
-      .DReqPass ({2{1'b1}}),
-      .DRspPass ({2{1'b1}}),
-      .HReqDepth(4'h0),
-      .HRspDepth(4'h0),
-      .DReqDepth({2{4'h0}}),
-      .DRspDepth({2{4'h0}})
+    .N        (2),
+    .HReqPass (1'b1),
+    .HRspPass (1'b1),
+    .DReqPass ({2{1'b1}}),
+    .DRspPass ({2{1'b1}}),
+    .HReqDepth(4'h0),
+    .HRspDepth(4'h0),
+    .DReqDepth({2{4'h0}}),
+    .DRspDepth({2{4'h0}})
   ) u_socket (
-      .clk_i,
-      .rst_ni,
-      .tl_h_i      (tl_in_i),
-      .tl_h_o      (tl_in_o),
-      .tl_d_o      (tl_socket_h2d),
-      .tl_d_i      (tl_socket_d2h),
-      .dev_select_i({1'b0, dev_select})
+    .clk_i,
+    .rst_ni,
+    .tl_h_i      (tl_in_i),
+    .tl_h_o      (tl_in_o),
+    .tl_d_o      (tl_socket_h2d),
+    .tl_d_i      (tl_socket_d2h),
+    .dev_select_i({1'b0, dev_select})
   );
 
   // Logic to select SRAM address range.
@@ -91,39 +93,39 @@ module sim_sram #(
     logic                     sram_rvalid;
 
     tlul_adapter_sram #(
-        .SramAw     (SramAddrWidth),
-        .SramDw     (Width),
-        .ErrOnRead  (ErrOnRead),
-        .Outstanding(2)
+      .SramAw     (SramAddrWidth),
+      .SramDw     (Width),
+      .ErrOnRead  (ErrOnRead),
+      .Outstanding(2)
     ) u_tl_adapter_sim_sram (
-        .clk_i,
-        .rst_ni,
-        .tl_i(tl_socket_h2d[1]),
-        .tl_o(tl_socket_d2h[1]),
+      .clk_i,
+      .rst_ni,
+      .tl_i(tl_socket_h2d[1]),
+      .tl_o(tl_socket_d2h[1]),
 
-        .req_o   (sram_req),
-        .gnt_i   (1'b1),
-        .we_o    (sram_we),
-        .addr_o  (sram_addr),
-        .wdata_o (sram_wdata),
-        .wmask_o (sram_wmask),
-        .rdata_i (sram_rdata),
-        .rvalid_i(sram_rvalid),
-        .rerror_i(2'b00)
+      .req_o   (sram_req),
+      .gnt_i   (1'b1),
+      .we_o    (sram_we),
+      .addr_o  (sram_addr),
+      .wdata_o (sram_wdata),
+      .wmask_o (sram_wmask),
+      .rdata_i (sram_rdata),
+      .rvalid_i(sram_rvalid),
+      .rerror_i(2'b00)
     );
 
     prim_ram_1p #(
-        .Width          (Width),
-        .Depth          (Depth),
-        .DataBitsPerMask(WidthBytes)
+      .Width          (Width),
+      .Depth          (Depth),
+      .DataBitsPerMask(WidthBytes)
     ) u_sram (
-        .clk_i,
-        .req_i  (sram_req),
-        .write_i(sram_we),
-        .addr_i (sram_addr),
-        .wdata_i(sram_wdata),
-        .wmask_i(sram_wmask),
-        .rdata_o(sram_rdata)
+      .clk_i,
+      .req_i  (sram_req),
+      .write_i(sram_we),
+      .addr_i (sram_addr),
+      .wdata_i(sram_wdata),
+      .wmask_i(sram_wmask),
+      .rdata_o(sram_rdata)
     );
 
     // Valid data from the SRAM appears 1 cycle after the sram_req.
@@ -139,10 +141,10 @@ module sim_sram #(
   else begin : no_sram
 
     tlul_sink u_tlul_sink (
-        .clk_i,
-        .rst_ni,
-        .tl_i(tl_socket_h2d[1]),
-        .tl_o(tl_socket_d2h[1])
+      .clk_i,
+      .rst_ni,
+      .tl_i(tl_socket_h2d[1]),
+      .tl_o(tl_socket_d2h[1])
     );
 
   end : no_sram
