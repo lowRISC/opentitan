7df6cea447d43becf4885cd1e2036e417db0a567
Steve Nelson <steve.nelson@wdc.com>
2020-10-22 08:45:18 -0700

    [entropy_src/dv] adding push_pull_agents to env
    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>

--- hw/ip/entropy_src/dv/env/entropy_src_env.sv
+++ hw/ip/entropy_src/dv/env/entropy_src_env.sv
@@ -10,15 +10,23 @@ class entropy_src_env extends cip_base_env #(
   );
   `uvm_component_utils(entropy_src_env)
 
-   rng_agent              m_rng_agent;
+   push_pull_agent#(.DataWidth(RNG_DATA_WIDTH))     m_rng_agent;
+   push_pull_agent#(.DataWidth(CSRNG_DATA_WIDTH))   m_csrng_agent;
 
   `uvm_component_new
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
 
-    m_rng_agent = rng_agent::type_id::create("m_rng_agent", this);
-    uvm_config_db#(rng_agent_cfg)::set(this, "m_rng_agent*", "cfg", cfg.m_rng_agent_cfg);
+    m_rng_agent = push_pull_agent#(.DataWidth(RNG_DATA_WIDTH))::type_id::create("m_rng_agent", this);
+    uvm_config_db#(push_pull_agent_cfg#(.DataWidth(RNG_DATA_WIDTH)))::set(this, "m_rng_agent*", "cfg", cfg.m_rng_agent_cfg);
+    cfg.m_rng_agent_cfg.agent_type = push_pull_agent_pkg::PushAgent;
+    cfg.m_rng_agent_cfg.if_mode    = dv_utils_pkg::Host;
+
+    m_csrng_agent = push_pull_agent#(.DataWidth(CSRNG_DATA_WIDTH))::type_id::create("m_csrng_agent", this);
+    uvm_config_db#(push_pull_agent_cfg#(.DataWidth(CSRNG_DATA_WIDTH)))::set(this, "m_csrng_agent*", "cfg", cfg.m_csrng_agent_cfg);
+    cfg.m_csrng_agent_cfg.agent_type = push_pull_agent_pkg::PullAgent;
+    cfg.m_csrng_agent_cfg.if_mode    = dv_utils_pkg::Host;
 
     if (!uvm_config_db#(virtual pins_if)::get(this, "", "efuse_es_sw_reg_en_vif", cfg.efuse_es_sw_reg_en_vif)) begin
       `uvm_fatal(get_full_name(), "failed to get efuse_es_sw_reg_en_vif from uvm_config_db")
--- hw/ip/entropy_src/dv/tb/tb.sv
+++ hw/ip/entropy_src/dv/tb/tb.sv
@@ -6,6 +6,7 @@ module tb;
   // dep packages
   import uvm_pkg::*;
   import dv_utils_pkg::*;
+  import entropy_src_pkg::*;
   import entropy_src_env_pkg::*;
   import entropy_src_test_pkg::*;
 
@@ -14,8 +15,7 @@ module tb;
   `include "dv_macros.svh"
 
   wire clk, rst_n;
-  wire devmode;
-  wire efuse_es_sw_reg_en;
+  wire devmode, fips, efuse_es_sw_reg_en;
   wire [NUM_MAX_INTERRUPTS-1:0] interrupts;
   prim_alert_pkg::alert_rx_t [NUM_ALERTS-1:0] alert_rx;
   prim_alert_pkg::alert_tx_t [NUM_ALERTS-1:0] alert_tx;
@@ -25,12 +25,14 @@ module tb;
 
   // interfaces
   clk_rst_if clk_rst_if(.clk(clk), .rst_n(rst_n));
-  pins_if #(NUM_MAX_INTERRUPTS) intr_if(interrupts);
+  pins_if#(NUM_MAX_INTERRUPTS) intr_if(interrupts);
   alert_esc_if alert_if[NUM_ALERTS](.clk(clk), .rst_n(rst_n));
-  pins_if #(1) devmode_if(devmode);
-  pins_if #(1) efuse_es_sw_reg_en_if(efuse_es_sw_reg_en);
+  pins_if#(1) devmode_if(devmode);
+  pins_if#(1) fips_if(fips);
+  pins_if#(1) efuse_es_sw_reg_en_if(efuse_es_sw_reg_en);
   tl_if tl_if(.clk(clk), .rst_n(rst_n));
-  rng_if rng_if();
+  push_pull_if#(.DataWidth(RNG_DATA_WIDTH)) rng_if(.clk(clk), .rst_n(rst_n));
+  push_pull_if#(.DataWidth(CSRNG_DATA_WIDTH)) csrng_if(.clk(clk), .rst_n(rst_n));
 
   // dut
   entropy_src dut (
@@ -42,11 +44,11 @@ module tb;
 
     .efuse_es_sw_reg_en_i      (efuse_es_sw_reg_en),
 
-    .entropy_src_hw_if_o       (),
-    .entropy_src_hw_if_i       (1'b0),
+    .entropy_src_hw_if_o       ({csrng_if.ack, csrng_if.data, fips}),
+    .entropy_src_hw_if_i       (csrng_if.req),
 
-    .entropy_src_rng_o         (rng_if.enable),
-    .entropy_src_rng_i         ({rng_if.entropy_ok, rng_if.entropy}),
+    .entropy_src_rng_o         (rng_if.ready),
+    .entropy_src_rng_i         ({rng_if.valid, rng_if.data}),
 
     .alert_rx_i                (alert_rx),
     .alert_tx_o                (alert_tx),
@@ -55,9 +57,6 @@ module tb;
     .es_health_test_failed_o   (intr_health_test_failed),
     .es_fifo_err_o             (intr_fifo_err)
   );
-
-  assign rng_if.clk   = clk;
-  assign rng_if.rst_n = rst_n;
 
   assign interrupts[EntropyValid]     = intr_entropy_valid;
   assign interrupts[HealthTestFailed] = intr_health_test_failed;
@@ -80,8 +79,10 @@ module tb;
     uvm_config_db#(intr_vif)::set(null, "*.env", "intr_vif", intr_if);
     uvm_config_db#(devmode_vif)::set(null, "*.env", "devmode_vif", devmode_if);
     uvm_config_db#(virtual pins_if)::set(null, "*.env", "efuse_es_sw_reg_en_vif", efuse_es_sw_reg_en_if);
+    uvm_config_db#(virtual pins_if)::set(null, "*.env", "fips_vif", fips_if);
     uvm_config_db#(virtual tl_if)::set(null, "*.env.m_tl_agent*", "vif", tl_if);
-    uvm_config_db#(virtual rng_if)::set(null, "*.env.m_rng_agent*", "vif", rng_if);
+    uvm_config_db#(virtual push_pull_if#(.DataWidth(RNG_DATA_WIDTH)))::set(null, "*.env.m_rng_agent*", "vif", rng_if);
+    uvm_config_db#(virtual push_pull_if#(.DataWidth(CSRNG_DATA_WIDTH)))::set(null, "*.env.m_csrng_agent*", "vif", csrng_if);
     $timeformat(-12, 0, " ps", 12);
     run_test();
   end
