b0565cc5281c1793b4ec608b78a1024bf09eaa55
Sam Elliott <selliott@lowrisc.org>
2020-10-27 13:12:21 +0000

    [clkmgr] Initial Clock Manager Header
    Signed-off-by: Sam Elliott <selliott@lowrisc.org>

---
+++ sw/device/lib/dif/dif_clkmgr.c
@@ -0,0 +1,9 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/dif/dif_clkmgr.h"
+
+#include "clkmgr_regs.h"  // Generated
+
+#error "Clock Manager DIF Implementation Incomplete"
