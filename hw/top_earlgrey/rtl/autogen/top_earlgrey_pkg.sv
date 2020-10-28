26b8aa1466cc577cd04f82f3ab4551e988bf9950
Michael Schaffner <msf@opentitan.org>
2020-10-27 19:52:23 -0700

    [top] Regenerate files
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/top_earlgrey/rtl/autogen/top_earlgrey_pkg.sv
+++ hw/top_earlgrey/rtl/autogen/top_earlgrey_pkg.sv
@@ -1,6 +1,14 @@
 // Copyright lowRISC contributors.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
+//
+// ------------------- W A R N I N G: A U T O - G E N E R A T E D   C O D E !! -------------------//
+// PLEASE DO NOT HAND-EDIT THIS FILE. IT HAS BEEN AUTO-GENERATED WITH THE FOLLOWING COMMAND:
+//
+// util/topgen.py -t hw/top_earlgrey/data/top_earlgrey.hjson \
+//                --tpl hw/top_earlgrey/data/ \
+//                -o hw/top_earlgrey/ \
+//                --rnd_cnst_seed 4881560218908238235
 
 package top_earlgrey_pkg;
   /**
