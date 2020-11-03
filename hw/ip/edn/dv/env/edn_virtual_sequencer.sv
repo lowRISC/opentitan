958dd09756f040c887f24909fe9cc0b4520d22a6
Steve Nelson <steve.nelson@wdc.com>
2020-11-03 12:15:34 -0800

    [edn/dv] Initial dv environment
    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>

---
+++ hw/ip/edn/dv/env/edn_virtual_sequencer.sv
@@ -0,0 +1,15 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class edn_virtual_sequencer extends cip_base_virtual_sequencer #(
+    .CFG_T(edn_env_cfg),
+    .COV_T(edn_env_cov)
+  );
+  `uvm_component_utils(edn_virtual_sequencer)
+
+  push_pull_sequencer push_pull_sequencer_h;
+
+  `uvm_component_new
+
+endclass
