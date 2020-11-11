9cf7658609d4a03e29a6b4985a61452fb5d01cab
Cindy Chen <chencindy@google.com>
2020-11-11 10:19:45 -0800

    [rv_dm] update naming from sanity to smoke
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/ip/rv_dm/dv/env/seq_lib/rv_dm_smoke_vseq.sv
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_smoke_vseq.sv
@@ -2,9 +2,9 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// basic sanity test vseq
-class rv_dm_sanity_vseq extends rv_dm_base_vseq;
-  `uvm_object_utils(rv_dm_sanity_vseq)
+// smoke test vseq
+class rv_dm_smoke_vseq extends rv_dm_base_vseq;
+  `uvm_object_utils(rv_dm_smoke_vseq)
 
   `uvm_object_new
 
@@ -12,4 +12,4 @@ class rv_dm_sanity_vseq extends rv_dm_base_vseq;
     `uvm_error(`gfn, "FIXME")
   endtask : body
 
-endclass : rv_dm_sanity_vseq
+endclass : rv_dm_smoke_vseq
