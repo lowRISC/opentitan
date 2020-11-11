a53bfc0fd14a66c92c06e90a2c8511f2d4418f9b
Cindy Chen <chencindy@google.com>
2020-11-11 10:15:34 -0800

    [kmac] update wording from sanity to smoke
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/ip/kmac/dv/env/seq_lib/kmac_smoke_vseq.sv
+++ hw/ip/kmac/dv/env/seq_lib/kmac_smoke_vseq.sv
@@ -2,13 +2,13 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// basic sanity test vseq
-class kmac_sanity_vseq extends kmac_base_vseq;
-  `uvm_object_utils(kmac_sanity_vseq)
+// smoke test vseq
+class kmac_smoke_vseq extends kmac_base_vseq;
+  `uvm_object_utils(kmac_smoke_vseq)
 
   `uvm_object_new
 
   task body();
   endtask : body
 
-endclass : kmac_sanity_vseq
+endclass : kmac_smoke_vseq
