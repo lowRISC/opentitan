5bf1b062ad06ea2dac67daec50130f3e442a047c
Michael Schaffner <msf@opentitan.org>
2020-11-10 18:55:21 -0800

    [lc_ctrl] Generate CSRs
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/lc_ctrl/rtl/lc_ctrl_reg_pkg.sv
+++ hw/ip/lc_ctrl/rtl/lc_ctrl_reg_pkg.sv
@@ -6,48 +6,213 @@
 
 package lc_ctrl_reg_pkg;
 
+  // Param list
+  parameter int NumTokenWords = 4;
+  parameter int NumLcStateBits = 4;
+  parameter int NumLcCntBits = 5;
+  parameter int NumAlerts = 2;
+
   ////////////////////////////
   // Typedefs for registers //
   ////////////////////////////
+  typedef struct packed {
+    struct packed {
+      logic        q;
+      logic        qe;
+    } lc_programming_failure;
+    struct packed {
+      logic        q;
+      logic        qe;
+    } lc_state_failure;
+  } lc_ctrl_reg2hw_alert_test_reg_t;
 
   typedef struct packed {
-    logic [1:0]  d;
-    logic        de;
+    struct packed {
+      logic        q;
+    } ready;
+    struct packed {
+      logic        q;
+    } transition_successful;
+    struct packed {
+      logic        q;
+    } transition_error;
+    struct packed {
+      logic        q;
+    } token_error;
+    struct packed {
+      logic        q;
+    } otp_error;
+    struct packed {
+      logic        q;
+    } state_error;
+  } lc_ctrl_reg2hw_status_reg_t;
+
+  typedef struct packed {
+    logic        q;
+    logic        qe;
+  } lc_ctrl_reg2hw_claim_transition_if_reg_t;
+
+  typedef struct packed {
+    logic        q;
+    logic        qe;
+  } lc_ctrl_reg2hw_transition_cmd_reg_t;
+
+  typedef struct packed {
+    logic [31:0] q;
+    logic        qe;
+  } lc_ctrl_reg2hw_transition_token_mreg_t;
+
+  typedef struct packed {
+    logic [3:0]  q;
+    logic        qe;
+  } lc_ctrl_reg2hw_transition_target_reg_t;
+
+  typedef struct packed {
+    logic [3:0]  q;
+  } lc_ctrl_reg2hw_lc_state_reg_t;
+
+  typedef struct packed {
+    logic [4:0]  q;
+  } lc_ctrl_reg2hw_lc_transition_cnt_reg_t;
+
+  typedef struct packed {
+    logic [1:0]  q;
+  } lc_ctrl_reg2hw_lc_id_state_reg_t;
+
+
+  typedef struct packed {
+    struct packed {
+      logic        d;
+    } ready;
+    struct packed {
+      logic        d;
+    } transition_successful;
+    struct packed {
+      logic        d;
+    } transition_error;
+    struct packed {
+      logic        d;
+    } token_error;
+    struct packed {
+      logic        d;
+    } otp_error;
+    struct packed {
+      logic        d;
+    } state_error;
   } lc_ctrl_hw2reg_status_reg_t;
 
   typedef struct packed {
-    logic [1:0]  d;
+    logic        d;
+  } lc_ctrl_hw2reg_claim_transition_if_reg_t;
+
+  typedef struct packed {
+    logic        d;
     logic        de;
-  } lc_ctrl_hw2reg_ctrl_reg_t;
+  } lc_ctrl_hw2reg_transition_regwen_reg_t;
+
+  typedef struct packed {
+    logic        d;
+  } lc_ctrl_hw2reg_transition_cmd_reg_t;
+
+  typedef struct packed {
+    logic [31:0] d;
+  } lc_ctrl_hw2reg_transition_token_mreg_t;
+
+  typedef struct packed {
+    logic [3:0]  d;
+  } lc_ctrl_hw2reg_transition_target_reg_t;
+
+  typedef struct packed {
+    logic [3:0]  d;
+  } lc_ctrl_hw2reg_lc_state_reg_t;
+
+  typedef struct packed {
+    logic [4:0]  d;
+  } lc_ctrl_hw2reg_lc_transition_cnt_reg_t;
+
+  typedef struct packed {
+    logic [1:0]  d;
+  } lc_ctrl_hw2reg_lc_id_state_reg_t;
 
 
   ///////////////////////////////////////
   // Register to internal design logic //
   ///////////////////////////////////////
+  typedef struct packed {
+    lc_ctrl_reg2hw_alert_test_reg_t alert_test; // [161:158]
+    lc_ctrl_reg2hw_status_reg_t status; // [157:152]
+    lc_ctrl_reg2hw_claim_transition_if_reg_t claim_transition_if; // [151:150]
+    lc_ctrl_reg2hw_transition_cmd_reg_t transition_cmd; // [149:148]
+    lc_ctrl_reg2hw_transition_token_mreg_t [3:0] transition_token; // [147:16]
+    lc_ctrl_reg2hw_transition_target_reg_t transition_target; // [15:11]
+    lc_ctrl_reg2hw_lc_state_reg_t lc_state; // [10:7]
+    lc_ctrl_reg2hw_lc_transition_cnt_reg_t lc_transition_cnt; // [6:2]
+    lc_ctrl_reg2hw_lc_id_state_reg_t lc_id_state; // [1:0]
+  } lc_ctrl_reg2hw_t;
 
   ///////////////////////////////////////
   // Internal design logic to register //
   ///////////////////////////////////////
   typedef struct packed {
-    lc_ctrl_hw2reg_status_reg_t status; // [5:6]
-    lc_ctrl_hw2reg_ctrl_reg_t ctrl; // [5:6]
+    lc_ctrl_hw2reg_status_reg_t status; // [152:147]
+    lc_ctrl_hw2reg_claim_transition_if_reg_t claim_transition_if; // [146:145]
+    lc_ctrl_hw2reg_transition_regwen_reg_t transition_regwen; // [144:145]
+    lc_ctrl_hw2reg_transition_cmd_reg_t transition_cmd; // [144:143]
+    lc_ctrl_hw2reg_transition_token_mreg_t [3:0] transition_token; // [142:15]
+    lc_ctrl_hw2reg_transition_target_reg_t transition_target; // [14:10]
+    lc_ctrl_hw2reg_lc_state_reg_t lc_state; // [9:6]
+    lc_ctrl_hw2reg_lc_transition_cnt_reg_t lc_transition_cnt; // [5:1]
+    lc_ctrl_hw2reg_lc_id_state_reg_t lc_id_state; // [0:-1]
   } lc_ctrl_hw2reg_t;
 
   // Register Address
-  parameter logic [2:0] LC_CTRL_STATUS_OFFSET = 3'h 0;
-  parameter logic [2:0] LC_CTRL_CTRL_OFFSET = 3'h 4;
+  parameter logic [5:0] LC_CTRL_ALERT_TEST_OFFSET = 6'h 0;
+  parameter logic [5:0] LC_CTRL_STATUS_OFFSET = 6'h 4;
+  parameter logic [5:0] LC_CTRL_CLAIM_TRANSITION_IF_OFFSET = 6'h 8;
+  parameter logic [5:0] LC_CTRL_TRANSITION_REGWEN_OFFSET = 6'h c;
+  parameter logic [5:0] LC_CTRL_TRANSITION_CMD_OFFSET = 6'h 10;
+  parameter logic [5:0] LC_CTRL_TRANSITION_TOKEN_0_OFFSET = 6'h 14;
+  parameter logic [5:0] LC_CTRL_TRANSITION_TOKEN_1_OFFSET = 6'h 18;
+  parameter logic [5:0] LC_CTRL_TRANSITION_TOKEN_2_OFFSET = 6'h 1c;
+  parameter logic [5:0] LC_CTRL_TRANSITION_TOKEN_3_OFFSET = 6'h 20;
+  parameter logic [5:0] LC_CTRL_TRANSITION_TARGET_OFFSET = 6'h 24;
+  parameter logic [5:0] LC_CTRL_LC_STATE_OFFSET = 6'h 28;
+  parameter logic [5:0] LC_CTRL_LC_TRANSITION_CNT_OFFSET = 6'h 2c;
+  parameter logic [5:0] LC_CTRL_LC_ID_STATE_OFFSET = 6'h 30;
 
 
   // Register Index
   typedef enum int {
+    LC_CTRL_ALERT_TEST,
     LC_CTRL_STATUS,
-    LC_CTRL_CTRL
+    LC_CTRL_CLAIM_TRANSITION_IF,
+    LC_CTRL_TRANSITION_REGWEN,
+    LC_CTRL_TRANSITION_CMD,
+    LC_CTRL_TRANSITION_TOKEN_0,
+    LC_CTRL_TRANSITION_TOKEN_1,
+    LC_CTRL_TRANSITION_TOKEN_2,
+    LC_CTRL_TRANSITION_TOKEN_3,
+    LC_CTRL_TRANSITION_TARGET,
+    LC_CTRL_LC_STATE,
+    LC_CTRL_LC_TRANSITION_CNT,
+    LC_CTRL_LC_ID_STATE
   } lc_ctrl_id_e;
 
   // Register width information to check illegal writes
-  parameter logic [3:0] LC_CTRL_PERMIT [2] = '{
-    4'b 0001, // index[0] LC_CTRL_STATUS
-    4'b 0001  // index[1] LC_CTRL_CTRL
+  parameter logic [3:0] LC_CTRL_PERMIT [13] = '{
+    4'b 0001, // index[ 0] LC_CTRL_ALERT_TEST
+    4'b 0001, // index[ 1] LC_CTRL_STATUS
+    4'b 0001, // index[ 2] LC_CTRL_CLAIM_TRANSITION_IF
+    4'b 0001, // index[ 3] LC_CTRL_TRANSITION_REGWEN
+    4'b 0001, // index[ 4] LC_CTRL_TRANSITION_CMD
+    4'b 1111, // index[ 5] LC_CTRL_TRANSITION_TOKEN_0
+    4'b 1111, // index[ 6] LC_CTRL_TRANSITION_TOKEN_1
+    4'b 1111, // index[ 7] LC_CTRL_TRANSITION_TOKEN_2
+    4'b 1111, // index[ 8] LC_CTRL_TRANSITION_TOKEN_3
+    4'b 0001, // index[ 9] LC_CTRL_TRANSITION_TARGET
+    4'b 0001, // index[10] LC_CTRL_LC_STATE
+    4'b 0001, // index[11] LC_CTRL_LC_TRANSITION_CNT
+    4'b 0001  // index[12] LC_CTRL_LC_ID_STATE
   };
 endpackage
 
--- hw/ip/lc_ctrl/rtl/lc_ctrl_reg_top.sv
+++ hw/ip/lc_ctrl/rtl/lc_ctrl_reg_top.sv
@@ -14,6 +14,7 @@ module lc_ctrl_reg_top (
   input  tlul_pkg::tl_h2d_t tl_i,
   output tlul_pkg::tl_d2h_t tl_o,
   // To HW
+  output lc_ctrl_reg_pkg::lc_ctrl_reg2hw_t reg2hw, // Write
   input  lc_ctrl_reg_pkg::lc_ctrl_hw2reg_t hw2reg, // Read
 
   // Config
@@ -22,7 +23,7 @@ module lc_ctrl_reg_top (
 
   import lc_ctrl_reg_pkg::* ;
 
-  localparam int AW = 3;
+  localparam int AW = 6;
   localparam int DW = 32;
   localparam int DBW = DW/8;                    // Byte Width
 
@@ -70,17 +71,204 @@ module lc_ctrl_reg_top (
   // Define SW related signals
   // Format: <reg>_<field>_{wd|we|qs}
   //        or <reg>_{wd|we|qs} if field == 1 or 0
-  logic [1:0] status_qs;
-  logic [1:0] ctrl_qs;
+  logic alert_test_lc_programming_failure_wd;
+  logic alert_test_lc_programming_failure_we;
+  logic alert_test_lc_state_failure_wd;
+  logic alert_test_lc_state_failure_we;
+  logic status_ready_qs;
+  logic status_ready_re;
+  logic status_transition_successful_qs;
+  logic status_transition_successful_re;
+  logic status_transition_error_qs;
+  logic status_transition_error_re;
+  logic status_token_error_qs;
+  logic status_token_error_re;
+  logic status_otp_error_qs;
+  logic status_otp_error_re;
+  logic status_state_error_qs;
+  logic status_state_error_re;
+  logic claim_transition_if_qs;
+  logic claim_transition_if_wd;
+  logic claim_transition_if_we;
+  logic claim_transition_if_re;
+  logic transition_regwen_qs;
+  logic transition_cmd_wd;
+  logic transition_cmd_we;
+  logic [31:0] transition_token_0_qs;
+  logic [31:0] transition_token_0_wd;
+  logic transition_token_0_we;
+  logic transition_token_0_re;
+  logic [31:0] transition_token_1_qs;
+  logic [31:0] transition_token_1_wd;
+  logic transition_token_1_we;
+  logic transition_token_1_re;
+  logic [31:0] transition_token_2_qs;
+  logic [31:0] transition_token_2_wd;
+  logic transition_token_2_we;
+  logic transition_token_2_re;
+  logic [31:0] transition_token_3_qs;
+  logic [31:0] transition_token_3_wd;
+  logic transition_token_3_we;
+  logic transition_token_3_re;
+  logic [3:0] transition_target_qs;
+  logic [3:0] transition_target_wd;
+  logic transition_target_we;
+  logic transition_target_re;
+  logic [3:0] lc_state_qs;
+  logic lc_state_re;
+  logic [4:0] lc_transition_cnt_qs;
+  logic lc_transition_cnt_re;
+  logic [1:0] lc_id_state_qs;
+  logic lc_id_state_re;
 
   // Register instances
-  // R[status]: V(False)
+  // R[alert_test]: V(True)
+
+  //   F[lc_programming_failure]: 0:0
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_alert_test_lc_programming_failure (
+    .re     (1'b0),
+    .we     (alert_test_lc_programming_failure_we),
+    .wd     (alert_test_lc_programming_failure_wd),
+    .d      ('0),
+    .qre    (),
+    .qe     (reg2hw.alert_test.lc_programming_failure.qe),
+    .q      (reg2hw.alert_test.lc_programming_failure.q ),
+    .qs     ()
+  );
+
+
+  //   F[lc_state_failure]: 1:1
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_alert_test_lc_state_failure (
+    .re     (1'b0),
+    .we     (alert_test_lc_state_failure_we),
+    .wd     (alert_test_lc_state_failure_wd),
+    .d      ('0),
+    .qre    (),
+    .qe     (reg2hw.alert_test.lc_state_failure.qe),
+    .q      (reg2hw.alert_test.lc_state_failure.q ),
+    .qs     ()
+  );
+
+
+  // R[status]: V(True)
+
+  //   F[ready]: 0:0
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_status_ready (
+    .re     (status_ready_re),
+    .we     (1'b0),
+    .wd     ('0),
+    .d      (hw2reg.status.ready.d),
+    .qre    (),
+    .qe     (),
+    .q      (reg2hw.status.ready.q ),
+    .qs     (status_ready_qs)
+  );
+
+
+  //   F[transition_successful]: 1:1
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_status_transition_successful (
+    .re     (status_transition_successful_re),
+    .we     (1'b0),
+    .wd     ('0),
+    .d      (hw2reg.status.transition_successful.d),
+    .qre    (),
+    .qe     (),
+    .q      (reg2hw.status.transition_successful.q ),
+    .qs     (status_transition_successful_qs)
+  );
+
+
+  //   F[transition_error]: 2:2
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_status_transition_error (
+    .re     (status_transition_error_re),
+    .we     (1'b0),
+    .wd     ('0),
+    .d      (hw2reg.status.transition_error.d),
+    .qre    (),
+    .qe     (),
+    .q      (reg2hw.status.transition_error.q ),
+    .qs     (status_transition_error_qs)
+  );
+
+
+  //   F[token_error]: 3:3
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_status_token_error (
+    .re     (status_token_error_re),
+    .we     (1'b0),
+    .wd     ('0),
+    .d      (hw2reg.status.token_error.d),
+    .qre    (),
+    .qe     (),
+    .q      (reg2hw.status.token_error.q ),
+    .qs     (status_token_error_qs)
+  );
+
+
+  //   F[otp_error]: 4:4
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_status_otp_error (
+    .re     (status_otp_error_re),
+    .we     (1'b0),
+    .wd     ('0),
+    .d      (hw2reg.status.otp_error.d),
+    .qre    (),
+    .qe     (),
+    .q      (reg2hw.status.otp_error.q ),
+    .qs     (status_otp_error_qs)
+  );
+
+
+  //   F[state_error]: 5:5
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_status_state_error (
+    .re     (status_state_error_re),
+    .we     (1'b0),
+    .wd     ('0),
+    .d      (hw2reg.status.state_error.d),
+    .qre    (),
+    .qe     (),
+    .q      (reg2hw.status.state_error.q ),
+    .qs     (status_state_error_qs)
+  );
+
+
+  // R[claim_transition_if]: V(True)
+
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_claim_transition_if (
+    .re     (claim_transition_if_re),
+    .we     (claim_transition_if_we),
+    .wd     (claim_transition_if_wd),
+    .d      (hw2reg.claim_transition_if.d),
+    .qre    (),
+    .qe     (reg2hw.claim_transition_if.qe),
+    .q      (reg2hw.claim_transition_if.q ),
+    .qs     (claim_transition_if_qs)
+  );
+
+
+  // R[transition_regwen]: V(False)
 
   prim_subreg #(
-    .DW      (2),
+    .DW      (1),
     .SWACCESS("RO"),
-    .RESVAL  (2'h0)
-  ) u_status (
+    .RESVAL  (1'h1)
+  ) u_transition_regwen (
     .clk_i   (clk_i    ),
     .rst_ni  (rst_ni  ),
 
@@ -88,51 +276,188 @@ module lc_ctrl_reg_top (
     .wd     ('0  ),
 
     // from internal hardware
-    .de     (hw2reg.status.de),
-    .d      (hw2reg.status.d ),
+    .de     (hw2reg.transition_regwen.de),
+    .d      (hw2reg.transition_regwen.d ),
 
     // to internal hardware
     .qe     (),
     .q      (),
 
     // to register interface (read)
-    .qs     (status_qs)
+    .qs     (transition_regwen_qs)
   );
 
 
-  // R[ctrl]: V(False)
+  // R[transition_cmd]: V(True)
 
-  prim_subreg #(
-    .DW      (2),
-    .SWACCESS("RO"),
-    .RESVAL  (2'h0)
-  ) u_ctrl (
-    .clk_i   (clk_i    ),
-    .rst_ni  (rst_ni  ),
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_transition_cmd (
+    .re     (1'b0),
+    // qualified with register enable
+    .we     (transition_cmd_we & transition_regwen_qs),
+    .wd     (transition_cmd_wd),
+    .d      (hw2reg.transition_cmd.d),
+    .qre    (),
+    .qe     (reg2hw.transition_cmd.qe),
+    .q      (reg2hw.transition_cmd.q ),
+    .qs     ()
+  );
 
+
+
+  // Subregister 0 of Multireg transition_token
+  // R[transition_token_0]: V(True)
+
+  prim_subreg_ext #(
+    .DW    (32)
+  ) u_transition_token_0 (
+    .re     (transition_token_0_re),
+    // qualified with register enable
+    .we     (transition_token_0_we & transition_regwen_qs),
+    .wd     (transition_token_0_wd),
+    .d      (hw2reg.transition_token[0].d),
+    .qre    (),
+    .qe     (reg2hw.transition_token[0].qe),
+    .q      (reg2hw.transition_token[0].q ),
+    .qs     (transition_token_0_qs)
+  );
+
+  // Subregister 1 of Multireg transition_token
+  // R[transition_token_1]: V(True)
+
+  prim_subreg_ext #(
+    .DW    (32)
+  ) u_transition_token_1 (
+    .re     (transition_token_1_re),
+    // qualified with register enable
+    .we     (transition_token_1_we & transition_regwen_qs),
+    .wd     (transition_token_1_wd),
+    .d      (hw2reg.transition_token[1].d),
+    .qre    (),
+    .qe     (reg2hw.transition_token[1].qe),
+    .q      (reg2hw.transition_token[1].q ),
+    .qs     (transition_token_1_qs)
+  );
+
+  // Subregister 2 of Multireg transition_token
+  // R[transition_token_2]: V(True)
+
+  prim_subreg_ext #(
+    .DW    (32)
+  ) u_transition_token_2 (
+    .re     (transition_token_2_re),
+    // qualified with register enable
+    .we     (transition_token_2_we & transition_regwen_qs),
+    .wd     (transition_token_2_wd),
+    .d      (hw2reg.transition_token[2].d),
+    .qre    (),
+    .qe     (reg2hw.transition_token[2].qe),
+    .q      (reg2hw.transition_token[2].q ),
+    .qs     (transition_token_2_qs)
+  );
+
+  // Subregister 3 of Multireg transition_token
+  // R[transition_token_3]: V(True)
+
+  prim_subreg_ext #(
+    .DW    (32)
+  ) u_transition_token_3 (
+    .re     (transition_token_3_re),
+    // qualified with register enable
+    .we     (transition_token_3_we & transition_regwen_qs),
+    .wd     (transition_token_3_wd),
+    .d      (hw2reg.transition_token[3].d),
+    .qre    (),
+    .qe     (reg2hw.transition_token[3].qe),
+    .q      (reg2hw.transition_token[3].q ),
+    .qs     (transition_token_3_qs)
+  );
+
+
+  // R[transition_target]: V(True)
+
+  prim_subreg_ext #(
+    .DW    (4)
+  ) u_transition_target (
+    .re     (transition_target_re),
+    // qualified with register enable
+    .we     (transition_target_we & transition_regwen_qs),
+    .wd     (transition_target_wd),
+    .d      (hw2reg.transition_target.d),
+    .qre    (),
+    .qe     (reg2hw.transition_target.qe),
+    .q      (reg2hw.transition_target.q ),
+    .qs     (transition_target_qs)
+  );
+
+
+  // R[lc_state]: V(True)
+
+  prim_subreg_ext #(
+    .DW    (4)
+  ) u_lc_state (
+    .re     (lc_state_re),
     .we     (1'b0),
-    .wd     ('0  ),
-
-    // from internal hardware
-    .de     (hw2reg.ctrl.de),
-    .d      (hw2reg.ctrl.d ),
-
-    // to internal hardware
+    .wd     ('0),
+    .d      (hw2reg.lc_state.d),
+    .qre    (),
     .qe     (),
-    .q      (),
+    .q      (reg2hw.lc_state.q ),
+    .qs     (lc_state_qs)
+  );
 
-    // to register interface (read)
-    .qs     (ctrl_qs)
+
+  // R[lc_transition_cnt]: V(True)
+
+  prim_subreg_ext #(
+    .DW    (5)
+  ) u_lc_transition_cnt (
+    .re     (lc_transition_cnt_re),
+    .we     (1'b0),
+    .wd     ('0),
+    .d      (hw2reg.lc_transition_cnt.d),
+    .qre    (),
+    .qe     (),
+    .q      (reg2hw.lc_transition_cnt.q ),
+    .qs     (lc_transition_cnt_qs)
+  );
+
+
+  // R[lc_id_state]: V(True)
+
+  prim_subreg_ext #(
+    .DW    (2)
+  ) u_lc_id_state (
+    .re     (lc_id_state_re),
+    .we     (1'b0),
+    .wd     ('0),
+    .d      (hw2reg.lc_id_state.d),
+    .qre    (),
+    .qe     (),
+    .q      (reg2hw.lc_id_state.q ),
+    .qs     (lc_id_state_qs)
   );
 
 
 
 
-  logic [1:0] addr_hit;
+  logic [12:0] addr_hit;
   always_comb begin
     addr_hit = '0;
-    addr_hit[0] = (reg_addr == LC_CTRL_STATUS_OFFSET);
-    addr_hit[1] = (reg_addr == LC_CTRL_CTRL_OFFSET);
+    addr_hit[ 0] = (reg_addr == LC_CTRL_ALERT_TEST_OFFSET);
+    addr_hit[ 1] = (reg_addr == LC_CTRL_STATUS_OFFSET);
+    addr_hit[ 2] = (reg_addr == LC_CTRL_CLAIM_TRANSITION_IF_OFFSET);
+    addr_hit[ 3] = (reg_addr == LC_CTRL_TRANSITION_REGWEN_OFFSET);
+    addr_hit[ 4] = (reg_addr == LC_CTRL_TRANSITION_CMD_OFFSET);
+    addr_hit[ 5] = (reg_addr == LC_CTRL_TRANSITION_TOKEN_0_OFFSET);
+    addr_hit[ 6] = (reg_addr == LC_CTRL_TRANSITION_TOKEN_1_OFFSET);
+    addr_hit[ 7] = (reg_addr == LC_CTRL_TRANSITION_TOKEN_2_OFFSET);
+    addr_hit[ 8] = (reg_addr == LC_CTRL_TRANSITION_TOKEN_3_OFFSET);
+    addr_hit[ 9] = (reg_addr == LC_CTRL_TRANSITION_TARGET_OFFSET);
+    addr_hit[10] = (reg_addr == LC_CTRL_LC_STATE_OFFSET);
+    addr_hit[11] = (reg_addr == LC_CTRL_LC_TRANSITION_CNT_OFFSET);
+    addr_hit[12] = (reg_addr == LC_CTRL_LC_ID_STATE_OFFSET);
   end
 
   assign addrmiss = (reg_re || reg_we) ? ~|addr_hit : 1'b0 ;
@@ -140,22 +465,133 @@ module lc_ctrl_reg_top (
   // Check sub-word write is permitted
   always_comb begin
     wr_err = 1'b0;
-    if (addr_hit[0] && reg_we && (LC_CTRL_PERMIT[0] != (LC_CTRL_PERMIT[0] & reg_be))) wr_err = 1'b1 ;
-    if (addr_hit[1] && reg_we && (LC_CTRL_PERMIT[1] != (LC_CTRL_PERMIT[1] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[ 0] && reg_we && (LC_CTRL_PERMIT[ 0] != (LC_CTRL_PERMIT[ 0] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[ 1] && reg_we && (LC_CTRL_PERMIT[ 1] != (LC_CTRL_PERMIT[ 1] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[ 2] && reg_we && (LC_CTRL_PERMIT[ 2] != (LC_CTRL_PERMIT[ 2] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[ 3] && reg_we && (LC_CTRL_PERMIT[ 3] != (LC_CTRL_PERMIT[ 3] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[ 4] && reg_we && (LC_CTRL_PERMIT[ 4] != (LC_CTRL_PERMIT[ 4] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[ 5] && reg_we && (LC_CTRL_PERMIT[ 5] != (LC_CTRL_PERMIT[ 5] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[ 6] && reg_we && (LC_CTRL_PERMIT[ 6] != (LC_CTRL_PERMIT[ 6] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[ 7] && reg_we && (LC_CTRL_PERMIT[ 7] != (LC_CTRL_PERMIT[ 7] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[ 8] && reg_we && (LC_CTRL_PERMIT[ 8] != (LC_CTRL_PERMIT[ 8] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[ 9] && reg_we && (LC_CTRL_PERMIT[ 9] != (LC_CTRL_PERMIT[ 9] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[10] && reg_we && (LC_CTRL_PERMIT[10] != (LC_CTRL_PERMIT[10] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[11] && reg_we && (LC_CTRL_PERMIT[11] != (LC_CTRL_PERMIT[11] & reg_be))) wr_err = 1'b1 ;
+    if (addr_hit[12] && reg_we && (LC_CTRL_PERMIT[12] != (LC_CTRL_PERMIT[12] & reg_be))) wr_err = 1'b1 ;
   end
 
+  assign alert_test_lc_programming_failure_we = addr_hit[0] & reg_we & ~wr_err;
+  assign alert_test_lc_programming_failure_wd = reg_wdata[0];
 
+  assign alert_test_lc_state_failure_we = addr_hit[0] & reg_we & ~wr_err;
+  assign alert_test_lc_state_failure_wd = reg_wdata[1];
+
+  assign status_ready_re = addr_hit[1] && reg_re;
+
+  assign status_transition_successful_re = addr_hit[1] && reg_re;
+
+  assign status_transition_error_re = addr_hit[1] && reg_re;
+
+  assign status_token_error_re = addr_hit[1] && reg_re;
+
+  assign status_otp_error_re = addr_hit[1] && reg_re;
+
+  assign status_state_error_re = addr_hit[1] && reg_re;
+
+  assign claim_transition_if_we = addr_hit[2] & reg_we & ~wr_err;
+  assign claim_transition_if_wd = reg_wdata[0];
+  assign claim_transition_if_re = addr_hit[2] && reg_re;
+
+
+  assign transition_cmd_we = addr_hit[4] & reg_we & ~wr_err;
+  assign transition_cmd_wd = reg_wdata[0];
+
+  assign transition_token_0_we = addr_hit[5] & reg_we & ~wr_err;
+  assign transition_token_0_wd = reg_wdata[31:0];
+  assign transition_token_0_re = addr_hit[5] && reg_re;
+
+  assign transition_token_1_we = addr_hit[6] & reg_we & ~wr_err;
+  assign transition_token_1_wd = reg_wdata[31:0];
+  assign transition_token_1_re = addr_hit[6] && reg_re;
+
+  assign transition_token_2_we = addr_hit[7] & reg_we & ~wr_err;
+  assign transition_token_2_wd = reg_wdata[31:0];
+  assign transition_token_2_re = addr_hit[7] && reg_re;
+
+  assign transition_token_3_we = addr_hit[8] & reg_we & ~wr_err;
+  assign transition_token_3_wd = reg_wdata[31:0];
+  assign transition_token_3_re = addr_hit[8] && reg_re;
+
+  assign transition_target_we = addr_hit[9] & reg_we & ~wr_err;
+  assign transition_target_wd = reg_wdata[3:0];
+  assign transition_target_re = addr_hit[9] && reg_re;
+
+  assign lc_state_re = addr_hit[10] && reg_re;
+
+  assign lc_transition_cnt_re = addr_hit[11] && reg_re;
+
+  assign lc_id_state_re = addr_hit[12] && reg_re;
 
   // Read data return
   always_comb begin
     reg_rdata_next = '0;
     unique case (1'b1)
       addr_hit[0]: begin
-        reg_rdata_next[1:0] = status_qs;
+        reg_rdata_next[0] = '0;
+        reg_rdata_next[1] = '0;
       end
 
       addr_hit[1]: begin
-        reg_rdata_next[1:0] = ctrl_qs;
+        reg_rdata_next[0] = status_ready_qs;
+        reg_rdata_next[1] = status_transition_successful_qs;
+        reg_rdata_next[2] = status_transition_error_qs;
+        reg_rdata_next[3] = status_token_error_qs;
+        reg_rdata_next[4] = status_otp_error_qs;
+        reg_rdata_next[5] = status_state_error_qs;
+      end
+
+      addr_hit[2]: begin
+        reg_rdata_next[0] = claim_transition_if_qs;
+      end
+
+      addr_hit[3]: begin
+        reg_rdata_next[0] = transition_regwen_qs;
+      end
+
+      addr_hit[4]: begin
+        reg_rdata_next[0] = '0;
+      end
+
+      addr_hit[5]: begin
+        reg_rdata_next[31:0] = transition_token_0_qs;
+      end
+
+      addr_hit[6]: begin
+        reg_rdata_next[31:0] = transition_token_1_qs;
+      end
+
+      addr_hit[7]: begin
+        reg_rdata_next[31:0] = transition_token_2_qs;
+      end
+
+      addr_hit[8]: begin
+        reg_rdata_next[31:0] = transition_token_3_qs;
+      end
+
+      addr_hit[9]: begin
+        reg_rdata_next[3:0] = transition_target_qs;
+      end
+
+      addr_hit[10]: begin
+        reg_rdata_next[3:0] = lc_state_qs;
+      end
+
+      addr_hit[11]: begin
+        reg_rdata_next[4:0] = lc_transition_cnt_qs;
+      end
+
+      addr_hit[12]: begin
+        reg_rdata_next[1:0] = lc_id_state_qs;
       end
 
       default: begin
