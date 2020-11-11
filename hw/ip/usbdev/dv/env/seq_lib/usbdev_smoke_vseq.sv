2beffb9b04afb9f857d5eb790438f84e865dfd02
Cindy Chen <chencindy@google.com>
2020-11-11 10:18:13 -0800

    [usb_dev] update wording from sanity to smoke
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/ip/usbdev/dv/env/seq_lib/usbdev_smoke_vseq.sv
+++ hw/ip/usbdev/dv/env/seq_lib/usbdev_smoke_vseq.sv
@@ -2,9 +2,9 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// basic sanity test vseq
-class usbdev_sanity_vseq extends usbdev_base_vseq;
-  `uvm_object_utils(usbdev_sanity_vseq)
+// smoke test vseq
+class usbdev_smoke_vseq extends usbdev_base_vseq;
+  `uvm_object_utils(usbdev_smoke_vseq)
 
   `uvm_object_new
 
@@ -12,4 +12,4 @@ class usbdev_sanity_vseq extends usbdev_base_vseq;
     `uvm_error(`gfn, "FIXME")
   endtask : body
 
-endclass : usbdev_sanity_vseq
+endclass : usbdev_smoke_vseq
