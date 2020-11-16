910cd22aa255a516ee0166be60addee314a35218
Alphan Ulusoy <alphan@google.com>
2020-11-16 15:04:02 -0500

    [sw/dif_keymgr] Key Manager DIF Library Header
    This change adds the header of the key manager DIF library that defines its public interface.
    
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

---
+++ sw/device/lib/dif/dif_keymgr.c
@@ -0,0 +1,7 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/dif/dif_keymgr.h"
+
+#include "keymgr_regs.h"  // Generated.
