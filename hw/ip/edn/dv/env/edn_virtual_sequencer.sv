429c39a197f42522f101529fd118947aa4f67f04
Steve Nelson <steve.nelson@wdc.com>
2020-11-16 10:28:32 -0800

    [edn/dv] dv environment modifications to match latest edn rtl/parameters
    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>

--- hw/ip/edn/dv/env/edn_virtual_sequencer.sv
+++ hw/ip/edn/dv/env/edn_virtual_sequencer.sv
@@ -8,7 +8,7 @@ class edn_virtual_sequencer extends cip_base_virtual_sequencer #(
   );
   `uvm_component_utils(edn_virtual_sequencer)
 
-  push_pull_sequencer push_pull_sequencer_h;
+  push_pull_sequencer#(.DataWidth(ENDPOINT_BUS_WIDTH)) endpoint_sequencer_h[NUM_ENDPOINTS-1:0];
 
   `uvm_component_new
 
