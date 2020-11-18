85d1de176b389a9a575c13435842f93cdba18e11
Weicai Yang <weicai@google.com>
2020-11-17 20:53:29 -0800

    [dv] Use macro for alert connection in tb
    These TBs were added before I merged the PR that created this macro
    And they were not generated by the latest uvmdvgen
    
    Signed-off-by: Weicai Yang <weicai@google.com>

--- hw/ip/entropy_src/dv/tb/tb.sv
+++ hw/ip/entropy_src/dv/tb/tb.sv
@@ -17,8 +17,6 @@ module tb;
   wire clk, rst_n;
   wire devmode, fips, efuse_es_sw_reg_en;
   wire [NUM_MAX_INTERRUPTS-1:0] interrupts;
-  prim_alert_pkg::alert_rx_t [NUM_ALERTS-1:0] alert_rx;
-  prim_alert_pkg::alert_tx_t [NUM_ALERTS-1:0] alert_tx;
   wire intr_entropy_valid;
   wire intr_health_test_failed;
   wire intr_fifo_err;
@@ -26,13 +24,14 @@ module tb;
   // interfaces
   clk_rst_if clk_rst_if(.clk(clk), .rst_n(rst_n));
   pins_if#(NUM_MAX_INTERRUPTS) intr_if(interrupts);
-  alert_esc_if alert_if[NUM_ALERTS](.clk(clk), .rst_n(rst_n));
   pins_if#(1) devmode_if(devmode);
   pins_if#(1) fips_if(fips);
   pins_if#(1) efuse_es_sw_reg_en_if(efuse_es_sw_reg_en);
   tl_if tl_if(.clk(clk), .rst_n(rst_n));
   push_pull_if#(.DataWidth(RNG_DATA_WIDTH)) rng_if(.clk(clk), .rst_n(rst_n));
   push_pull_if#(.DataWidth(CSRNG_DATA_WIDTH)) csrng_if(.clk(clk), .rst_n(rst_n));
+
+  `DV_ALERT_IF_CONNECT
 
   // dut
   entropy_src dut (
@@ -64,15 +63,6 @@ module tb;
   assign interrupts[EntropyValid]     = intr_entropy_valid;
   assign interrupts[HealthTestFailed] = intr_health_test_failed;
   assign interrupts[FifoErr]          = intr_fifo_err;
-
-  for (genvar k = 0; k < NUM_ALERTS; k++) begin : connect_alerts_pins
-    assign alert_rx[k] = alert_if[k].alert_rx;
-    assign alert_if[k].alert_tx = alert_tx[k];
-    initial begin
-      uvm_config_db#(virtual alert_esc_if)::set(null, $sformatf("*.env.m_alert_agent_%0s",
-          LIST_OF_ALERTS[k]), "vif", alert_if[k]);
-    end
-  end
 
   initial begin
     // drive clk and rst_n from clk_if
