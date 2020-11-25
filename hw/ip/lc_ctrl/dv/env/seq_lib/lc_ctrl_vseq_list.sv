58a3d12d4d8e9e659958758f99187ae656d320e9
Cindy Chen <chencindy@google.com>
2020-11-25 14:41:12 -0800

    [dv/lc_ctrl] initial dv testbench for LC
    This PR:
    1. Add a dv testbench for LC using uvmdvgen.
    2. Add pins to trigger LC init.
    3. Add LC testplan in build_doc.py
    4. Add a lc_ctrl_if to drive input of lc
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

---
+++ hw/ip/lc_ctrl/dv/env/seq_lib/lc_ctrl_vseq_list.sv
@@ -0,0 +1,7 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+`include "lc_ctrl_base_vseq.sv"
+`include "lc_ctrl_smoke_vseq.sv"
+`include "lc_ctrl_common_vseq.sv"
