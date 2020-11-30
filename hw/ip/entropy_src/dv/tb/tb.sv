c89e5669a6a81befbbdc4e2b53db22832ead686c
Steve Nelson <steve.nelson@wdc.com>
2020-11-30 15:22:23 -0800

    [entropy_src/dv] parameter names/usage, fips bit, testplan, dv plan
    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>

--- hw/ip/entropy_src/dv/tb/tb.sv
+++ hw/ip/entropy_src/dv/tb/tb.sv
@@ -6,7 +6,6 @@ module tb;
   // dep packages
   import uvm_pkg::*;
   import dv_utils_pkg::*;
-  import entropy_src_pkg::*;
   import entropy_src_env_pkg::*;
   import entropy_src_test_pkg::*;
 
@@ -15,45 +14,46 @@ module tb;
   `include "dv_macros.svh"
 
   wire clk, rst_n;
-  wire devmode, fips, efuse_es_sw_reg_en;
+  wire devmode, efuse_es_sw_reg_en;
   wire [NUM_MAX_INTERRUPTS-1:0] interrupts;
-  wire intr_entropy_valid;
-  wire intr_health_test_failed;
-  wire intr_fifo_err;
+  wire intr_entropy_valid, intr_health_test_failed, intr_fifo_err;
 
   // interfaces
   clk_rst_if clk_rst_if(.clk(clk), .rst_n(rst_n));
   pins_if#(NUM_MAX_INTERRUPTS) intr_if(interrupts);
   pins_if#(1) devmode_if(devmode);
-  pins_if#(1) fips_if(fips);
   pins_if#(1) efuse_es_sw_reg_en_if(efuse_es_sw_reg_en);
   tl_if tl_if(.clk(clk), .rst_n(rst_n));
-  push_pull_if#(.HostDataWidth(RNG_DATA_WIDTH)) rng_if(.clk(clk), .rst_n(rst_n));
-  push_pull_if#(.DeviceDataWidth(CSRNG_DATA_WIDTH)) csrng_if(.clk(clk), .rst_n(rst_n));
+  push_pull_if#(.HostDataWidth(entropy_src_pkg::RNG_BUS_WIDTH))
+    rng_if(.clk(clk), .rst_n(rst_n));
+  push_pull_if#(.HostDataWidth(entropy_src_pkg::FIPS_CSRNG_BUS_WIDTH))
+    csrng_if(.clk(clk), .rst_n(rst_n));
 
   `DV_ALERT_IF_CONNECT
 
   // dut
   entropy_src dut (
-    .clk_i                     (clk        ),
-    .rst_ni                    (rst_n      ),
+    .clk_i                        (clk        ),
+    .rst_ni                       (rst_n      ),
 
-    .tl_i                      (tl_if.h2d  ),
-    .tl_o                      (tl_if.d2h  ),
+    .tl_i                         (tl_if.h2d  ),
+    .tl_o                         (tl_if.d2h  ),
 
-    .efuse_es_sw_reg_en_i      (efuse_es_sw_reg_en),
+    .efuse_es_sw_reg_en_i         (efuse_es_sw_reg_en),
 
-    .entropy_src_hw_if_o       ({csrng_if.ack, csrng_if.d_data, fips}),
-    .entropy_src_hw_if_i       (csrng_if.req),
+    .entropy_src_hw_if_o          ({csrng_if.ack,
+                                    csrng_if.d_data[entropy_src_pkg::CSRNG_BUS_WIDTH-1:0],
+                                    csrng_if.d_data[entropy_src_pkg::CSRNG_BUS_WIDTH]}),
+    .entropy_src_hw_if_i          (csrng_if.req),
 
-    .entropy_src_xht_o         (),
-    .entropy_src_xht_i         ('0),
+    .entropy_src_xht_o            (),
+    .entropy_src_xht_i            ('0),
 
-    .entropy_src_rng_o         (rng_if.ready),
-    .entropy_src_rng_i         ({rng_if.valid, rng_if.h_data}),
+    .entropy_src_rng_o            (rng_if.ready),
+    .entropy_src_rng_i            ({rng_if.valid, rng_if.h_data}),
 
-    .alert_rx_i                (alert_rx),
-    .alert_tx_o                (alert_tx),
+    .alert_rx_i                   (alert_rx),
+    .alert_tx_o                   (alert_tx),
 
     .intr_es_entropy_valid_o      (intr_entropy_valid),
     .intr_es_health_test_failed_o (intr_health_test_failed),
@@ -71,11 +71,13 @@ module tb;
     uvm_config_db#(virtual clk_rst_if)::set(null, "*.env", "clk_rst_vif", clk_rst_if);
     uvm_config_db#(intr_vif)::set(null, "*.env", "intr_vif", intr_if);
     uvm_config_db#(devmode_vif)::set(null, "*.env", "devmode_vif", devmode_if);
-    uvm_config_db#(virtual pins_if)::set(null, "*.env", "efuse_es_sw_reg_en_vif", efuse_es_sw_reg_en_if);
-    uvm_config_db#(virtual pins_if)::set(null, "*.env", "fips_vif", fips_if);
+    uvm_config_db#(virtual pins_if)::set(null, "*.env", "efuse_es_sw_reg_en_vif",
+                                         efuse_es_sw_reg_en_if);
     uvm_config_db#(virtual tl_if)::set(null, "*.env.m_tl_agent*", "vif", tl_if);
-    uvm_config_db#(virtual push_pull_if#(.HostDataWidth(RNG_DATA_WIDTH)))::set(null, "*.env.m_rng_agent*", "vif", rng_if);
-    uvm_config_db#(virtual push_pull_if#(.DeviceDataWidth(CSRNG_DATA_WIDTH)))::set(null, "*.env.m_csrng_agent*", "vif", csrng_if);
+    uvm_config_db#(virtual push_pull_if#(.HostDataWidth(entropy_src_pkg::RNG_BUS_WIDTH)))::set
+                                        (null, "*.env.m_rng_agent*", "vif", rng_if);
+    uvm_config_db#(virtual push_pull_if#(.HostDataWidth(entropy_src_pkg::FIPS_CSRNG_BUS_WIDTH)))::set
+                                        (null, "*.env.m_csrng_agent*", "vif", csrng_if);
     $timeformat(-12, 0, " ps", 12);
     run_test();
   end
