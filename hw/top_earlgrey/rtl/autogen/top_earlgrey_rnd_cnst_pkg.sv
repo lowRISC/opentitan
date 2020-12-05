5e8c35e0e28e07bc4902a4433d9364c316a9690f
Timothy Chen <timothytim@google.com>
2020-12-04 18:08:45 -0800

    [keymgr / top] Auto-generate files
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- hw/top_earlgrey/rtl/autogen/top_earlgrey_rnd_cnst_pkg.sv
+++ hw/top_earlgrey/rtl/autogen/top_earlgrey_rnd_cnst_pkg.sv
@@ -46,6 +46,50 @@ package top_earlgrey_rnd_cnst_pkg;
   };
 
   ////////////////////////////////////////////
+  // keymgr
+  ////////////////////////////////////////////
+  // Compile-time random bits for initial LFSR seed
+  parameter keymgr_pkg::lfsr_seed_t RndCnstKeymgrLfsrSeed = {
+    64'hF4C3471C5DEF7861
+  };
+
+  // Compile-time random permutation for LFSR output
+  parameter keymgr_pkg::lfsr_perm_t RndCnstKeymgrLfsrPerm = {
+    128'h26AC29E186C1F4DC6F959D6ED08DC044,
+    256'hA0F3F1519E8DCA131275DF1E48BBF964AC772E613D0320ADAEBF38552DD822E6
+  };
+
+  // Compile-time random bits for revision seed
+  parameter keymgr_pkg::seed_t RndCnstKeymgrRevisionSeed = {
+    256'h96466A983E5346826A43628219E5A91389B9FE0D3B818E46CE7D846469A3B8E3
+  };
+
+  // Compile-time random bits for creator identity seed
+  parameter keymgr_pkg::seed_t RndCnstKeymgrCreatorIdentitySeed = {
+    256'hDE094CA8F1435F85E0F7489A309CBE57B77F07FF3D7297200D5AB25561AF49C6
+  };
+
+  // Compile-time random bits for owner intermediate identity seed
+  parameter keymgr_pkg::seed_t RndCnstKeymgrOwnerIntIdentitySeed = {
+    256'h12965C7DE10023EC699679EDD5369F11B49BAC9198BD1FF344C5DA2242D290BE
+  };
+
+  // Compile-time random bits for owner identity seed
+  parameter keymgr_pkg::seed_t RndCnstKeymgrOwnerIdentitySeed = {
+    256'h738F30D9006289A1D7D9D0CE1DD7D7C60C06703B494B3FF9FBB73A9BF8C393C
+  };
+
+  // Compile-time random bits for software generation seed
+  parameter keymgr_pkg::seed_t RndCnstKeymgrSoftOutputSeed = {
+    256'h613E5324CBAC660746BCA7E0AE24AF11FE8F673FBA39BB679D58AA91AEB2691C
+  };
+
+  // Compile-time random bits for hardware generation seed
+  parameter keymgr_pkg::seed_t RndCnstKeymgrHardOutputSeed = {
+    256'h205AE5E5FEED81E0CB15451E21FFDF7075A864CB4DAAB803225B91E3B1A7B12
+  };
+
+  ////////////////////////////////////////////
   // alert_handler
   ////////////////////////////////////////////
   // Compile-time random bits for initial LFSR seed
