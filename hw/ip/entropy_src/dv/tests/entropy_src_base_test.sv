574a1fb98d7576d248db6ac67a9ad0dfb23be65a
Steve Nelson <steve.nelson@wdc.com>
2020-12-10 13:41:59 -0800

    [entropy_src/dv] efuse changes
    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>

--- hw/ip/entropy_src/dv/tests/entropy_src_base_test.sv
+++ hw/ip/entropy_src/dv/tests/entropy_src_base_test.sv
@@ -12,15 +12,22 @@ class entropy_src_base_test extends cip_base_test #(
 
    virtual function void build_phase(uvm_phase phase);
      super.build_phase(phase);
+
+     configure_env();
    endfunction // build_phase
 
   // the base class dv_base_test creates the following instances:
   // entropy_src_env_cfg: cfg
   // entropy_src_env:     env
 
-
   // the base class also looks up UVM_TEST_SEQ plusarg to create and run that seq in
   // the run_phase; as such, nothing more needs to be done
+
+  // The following knob settings will serve as the defaults.
+  // Overrides should happen in the specific testcase.
+  virtual function void configure_env();
+    cfg.efuse_es_sw_reg_en_pct = 100;
+  endfunction
 
 endclass : entropy_src_base_test
 
