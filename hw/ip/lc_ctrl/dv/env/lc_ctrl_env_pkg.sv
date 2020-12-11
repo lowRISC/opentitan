3f0c286932d30f736e449d94d3ff02489dc7d251
Cindy Chen <chencindy@google.com>
2020-12-11 11:41:07 -0800

    [dv/lc_ctrl] enhance smoke test
    These PR did a few enhancement for the lc_ctrl smoke test:
    1. Move the constraints to next_valid_lc_state request to
    `lc_ctrl_env_pkg.sv` so we can reuse that in scb. Thanks weicai for the
    help and suggestions. We found for an associative array, index cannot be
    randomized with constraint, so we spilt it into a separate randomize
    function within the sequence.
    
    2. Minor clean up and add some comments.
    
    3. Use all three registers for lc input token instead of one.
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/ip/lc_ctrl/dv/env/lc_ctrl_env_pkg.sv
+++ hw/ip/lc_ctrl/dv/env/lc_ctrl_env_pkg.sv
@@ -20,10 +20,31 @@ package lc_ctrl_env_pkg;
   `include "dv_macros.svh"
 
   // parameters
-  // TODO: add the names of alerts in order
   parameter string LIST_OF_ALERTS[] = {"lc_programming_failure", "lc_state_failure"};
   parameter uint   NUM_ALERTS = 2;
   parameter uint   CLAIM_TRANS_VAL = 'ha5;
+
+  // associative array cannot declare parameter here, so we used const instead
+  const dec_lc_state_e VALID_NEXT_STATES [dec_lc_state_e][$] = '{
+    DecLcStRma:     {DecLcStScrap},
+    DecLcStProdEnd: {DecLcStScrap},
+    DecLcStProd:    {DecLcStScrap, DecLcStRma},
+    DecLcStDev:     {DecLcStScrap, DecLcStRma},
+    DecLcStTestUnlocked3: {DecLcStScrap, DecLcStRma, DecLcStProdEnd, DecLcStProd, DecLcStDev},
+    DecLcStTestUnlocked2: {DecLcStScrap, DecLcStProdEnd, DecLcStProd, DecLcStDev,
+                           DecLcStTestLocked2},
+    DecLcStTestUnlocked1: {DecLcStScrap, DecLcStRma, DecLcStProdEnd, DecLcStProd, DecLcStDev,
+                           DecLcStTestLocked2, DecLcStTestLocked1},
+    DecLcStTestUnlocked0: {DecLcStScrap, DecLcStRma, DecLcStProdEnd, DecLcStProd, DecLcStDev,
+                           DecLcStTestLocked2, DecLcStTestLocked1, DecLcStTestLocked0},
+    DecLcStTestLocked2: {DecLcStScrap, DecLcStProdEnd, DecLcStProd,
+                         DecLcStDev, DecLcStTestUnlocked3},
+    DecLcStTestLocked1: {DecLcStScrap, DecLcStProdEnd, DecLcStProd, DecLcStDev,
+                         DecLcStTestUnlocked3, DecLcStTestUnlocked2},
+    DecLcStTestLocked0: {DecLcStScrap, DecLcStProdEnd, DecLcStProd, DecLcStDev,
+                         DecLcStTestUnlocked3, DecLcStTestUnlocked2, DecLcStTestUnlocked1},
+    DecLcStRaw: {DecLcStScrap, DecLcStTestUnlocked2, DecLcStTestUnlocked1, DecLcStTestUnlocked0}
+  };
 
   // types
   typedef enum bit [1:0] {
@@ -37,6 +58,33 @@ package lc_ctrl_env_pkg;
   typedef virtual lc_ctrl_if              lc_ctrl_vif;
 
   // functions
+  function automatic bit valid_state_for_trans(lc_state_e curr_state);
+    valid_state_for_trans = 0;
+    if (curr_state inside {LcStRma, LcStProdEnd, LcStProd, LcStDev, LcStTestUnlocked3,
+                          LcStTestUnlocked2, LcStTestUnlocked1, LcStTestUnlocked0,
+                          LcStTestLocked2, LcStTestLocked1, LcStTestLocked0, LcStRaw}) begin
+      valid_state_for_trans = 1;
+    end
+  endfunction
+
+  function automatic dec_lc_state_e dec_lc_state(lc_state_e curr_state);
+    case (curr_state)
+      LcStRaw:           return DecLcStRaw;
+      LcStTestUnlocked0: return DecLcStTestUnlocked0;
+      LcStTestLocked0:   return DecLcStTestLocked0;
+      LcStTestUnlocked1: return DecLcStTestUnlocked1;
+      LcStTestLocked1:   return DecLcStTestLocked1;
+      LcStTestUnlocked2: return DecLcStTestUnlocked2;
+      LcStTestLocked2:   return DecLcStTestLocked2;
+      LcStTestUnlocked3: return DecLcStTestUnlocked3;
+      LcStDev:           return DecLcStDev;
+      LcStProd:          return DecLcStProd;
+      LcStProdEnd:       return DecLcStProdEnd;
+      LcStRma:           return DecLcStRma;
+      LcStScrap:         return DecLcStScrap;
+      default: `uvm_fatal("lc_env_pkg", $sformatf("unknown lc_state 0x%0h", curr_state))
+    endcase
+  endfunction
 
   // package sources
   `include "lc_ctrl_env_cfg.sv"
