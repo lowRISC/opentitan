91c2fe4a577d91ac9d81a68bb9144f9484a5e564
Timothy Chen <timothytim@google.com>
2020-12-15 15:45:44 -0800

    [keymgr] DV updates
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    [keymgr] DV connections update
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- hw/ip/keymgr/dv/tb.sv
+++ hw/ip/keymgr/dv/tb.sv
@@ -42,7 +42,7 @@ module tb;
     .lc_keymgr_en_i       (lc_ctrl_pkg::On),
     .lc_keymgr_div_i      (keymgr_if.keymgr_div),
     .otp_key_i            (keymgr_if.otp_key),
-    .otp_i                (keymgr_if.otp),
+    .otp_hw_cfg_i         (keymgr_if.otp_hw_cfg),
     .edn_o                (edn_if.req),
     .edn_i                ({edn_if.ack, edn_if.d_data}),
     .flash_i              (keymgr_if.flash),
