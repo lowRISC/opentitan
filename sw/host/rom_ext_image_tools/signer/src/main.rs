aaf10e16505c6f88545fb9b5a42a59d9e35a201c
Silvestrs Timofejevs <silvestrst@lowrisc.org>
2020-12-16 18:35:16 +0000

    [sw, rom_ext_signer] Add the image manifest parser and configuration
    This change crates a hjson configuration file to initialise empty
    binary image manifest, and a parser to deserialize this configuration
    file.
    
    Complex fields such as "peripheral_lockdown_info" are defined as a
    separate structures, whilst generic (integer) fileds are all parsed as
    a vector (when updating an image these generic fields can be simply
    iterated through).
    
    NOTE: eventually this probably should be generated by the manifest
    generator.
    
    Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>

--- sw/host/rom_ext_image_tools/signer/src/main.rs
+++ sw/host/rom_ext_image_tools/signer/src/main.rs
@@ -2,6 +2,20 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#![deny(warnings)]
+#![deny(unused)]
+#![deny(unsafe_code)]
+
+use std::env;
+use std::path;
+
+use rom_ext_config::parser;
+
 fn main() {
-    println!("Hello World!");
+    let args: Vec<String> = env::args().collect();
+
+    let config_path = path::Path::new(&args[1]);
+
+    // Parse the config.
+    let _config = parser::ParsedConfig::new(&config_path);
 }
