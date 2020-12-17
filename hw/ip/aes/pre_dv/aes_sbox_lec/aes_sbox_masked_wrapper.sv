f319633d233e1ff25e26ed370af8188166f3550b
Pirmin Vogel <vogelpi@lowrisc.org>
2020-12-17 11:24:29 +0100

    [aes] Prepare distribution of PRD for domain-oriented masking (DOM)
    Unlike for the masked Canright S-Boxes, the output masks for SubBytes are
    not known beforehand when using DOM. Instead, the output masks are
    generated from the PRD input inside the individual S-Boxes.
    
    In order to simplify the integration of different masking schemes, this
    commit thus aligns how the masking PRD is distributed when using the masked
    Canright S-Boxes. We no longer separate the masking PRNG output into output
    masks and PRD for SubBytes at the cipher core level but feed the full
    per-S-Box PRNG output into each S-Box. The "generation" of the output mask
    is performed inside the S-Box and the output mask becomes an output port
    also for the masked Canright S-Boxes.
    
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

--- hw/ip/aes/pre_dv/aes_sbox_lec/aes_sbox_masked_wrapper.sv
+++ hw/ip/aes/pre_dv/aes_sbox_lec/aes_sbox_masked_wrapper.sv
@@ -10,25 +10,24 @@ module aes_sbox_masked_wrapper (
   output logic [7:0]        data_o
 );
 
-  logic [7:0] in_data_m, out_data_m;
-  logic [7:0] in_mask, out_mask;
-  logic [9:0] prd_masking;
+  logic  [7:0] in_data_m, out_data_m;
+  logic  [7:0] in_mask, out_mask;
+  logic [17:0] prd_masking;
 
   // The mask inputs are tied to constant values.
   assign in_mask     = 8'hAA;
-  assign out_mask    = 8'h55;
-  assign prd_masking = 10'h2AA;
+  assign prd_masking = 18'h2AAAA;
 
   // Mask input data
   assign in_data_m = data_i ^ in_mask;
 
   aes_sbox_masked aes_sbox_masked (
-    .op_i          ( op_i        ),
-    .data_i        ( in_data_m   ),
-    .in_mask_i     ( in_mask     ),
-    .out_mask_i    ( out_mask    ),
-    .prd_masking_i ( prd_masking ),
-    .data_o        ( out_data_m  )
+    .op_i   ( op_i        ),
+    .data_i ( in_data_m   ),
+    .mask_i ( in_mask     ),
+    .prd_i  ( prd_masking ),
+    .data_o ( out_data_m  ),
+    .mask_o ( out_mask    )
   );
 
   // Unmask output data
