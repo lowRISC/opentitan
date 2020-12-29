5d1141268da1e47623b879210f875dac6f876a80
Timothy Chen <timothytim@google.com>
2020-12-28 16:17:02 -0800

    [top] Auto generate files
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- hw/ip/flash_ctrl/rtl/flash_ctrl.sv
+++ hw/ip/flash_ctrl/rtl/flash_ctrl.sv
@@ -59,9 +59,10 @@ module flash_ctrl import flash_ctrl_pkg::*; #(
   flash_ctrl_reg2hw_t reg2hw;
   flash_ctrl_hw2reg_t hw2reg;
 
-  tlul_pkg::tl_h2d_t tl_fifo_h2d [2];
-  tlul_pkg::tl_d2h_t tl_fifo_d2h [2];
+  tlul_pkg::tl_h2d_t tl_win_h2d [3];
+  tlul_pkg::tl_d2h_t tl_win_d2h [3];
 
+  assign tl_win_d2h[2] = flash_i.tl_flash_p2c;
   // Register module
   flash_ctrl_reg_top u_reg (
     .clk_i,
@@ -70,8 +71,8 @@ module flash_ctrl import flash_ctrl_pkg::*; #(
     .tl_i,
     .tl_o,
 
-    .tl_win_o (tl_fifo_h2d),
-    .tl_win_i (tl_fifo_d2h),
+    .tl_win_o (tl_win_h2d),
+    .tl_win_i (tl_win_d2h),
 
     .reg2hw,
     .hw2reg,
@@ -421,8 +422,8 @@ module flash_ctrl import flash_ctrl_pkg::*; #(
   ) u_to_prog_fifo (
     .clk_i,
     .rst_ni,
-    .tl_i       (tl_fifo_h2d[0]),
-    .tl_o       (tl_fifo_d2h[0]),
+    .tl_i       (tl_win_h2d[0]),
+    .tl_o       (tl_win_d2h[0]),
     .req_o      (sw_wvalid),
     .gnt_i      (sw_wready),
     .we_o       (sw_wen),
@@ -502,8 +503,8 @@ module flash_ctrl import flash_ctrl_pkg::*; #(
   ) u_to_rd_fifo (
     .clk_i,
     .rst_ni,
-    .tl_i       (tl_fifo_h2d[1]),
-    .tl_o       (tl_fifo_d2h[1]),
+    .tl_i       (tl_win_h2d[1]),
+    .tl_o       (tl_win_d2h[1]),
     .req_o      (rd_fifo_ren),
     .gnt_i      (rd_fifo_rvalid),
     .we_o       (),
@@ -755,6 +756,7 @@ module flash_ctrl import flash_ctrl_pkg::*; #(
   assign flash_o.region_cfgs = region_cfgs;
   assign flash_o.addr_key = addr_key;
   assign flash_o.data_key = data_key;
+  assign flash_o.tl_flash_c2p = tl_win_h2d[2];
   assign flash_rd_err = flash_i.rd_err;
   assign flash_rd_data = flash_i.rd_data;
   assign flash_phy_busy = flash_i.init_busy;
