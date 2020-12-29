63c2eaf178c407c32ca8d9fbeaf7cef28d453e64
Cindy Chen <chencindy@google.com>
2020-12-29 14:18:44 -0800

    [dv/otp] add write access error for OTP_ctrl
    Write access error is triggered when OTP write to non-zero addresses.
    This PR adds the sequence to support write access error and add scb
    supports.
    Note that `otp_macro_errors` includes other scenarios. Coming PRs will
    support the rest of the error cases.
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

---
+++ hw/ip/otp_ctrl/dv/env/seq_lib/otp_ctrl_macro_errs_vseq.sv
@@ -0,0 +1,28 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// otp_ctrl_marco_errs_vseq is developed to randomly read/write to any address within OTP:
+// - A writeblank error will be triggered if write to a non-empty address
+// - An access error will be triggered if write to lc partition via DAI interface, or if DAI write
+//   to digest addrs for non-sw partitions
+class otp_ctrl_macro_errs_vseq extends otp_ctrl_smoke_vseq;
+  `uvm_object_utils(otp_ctrl_macro_errs_vseq)
+
+  `uvm_object_new
+
+  constraint num_iterations_c {
+    num_trans  inside {[1:5]};
+    num_dai_op inside {[100:500]};
+  }
+
+  virtual task pre_start();
+    super.pre_start();
+    // TODO: enable this once support
+    // this.partition_index_c.constraint_mode(0);
+    // this.dai_wr_legal_addr_c.constraint_mode(0);
+    this.dai_wr_blank_addr_c.constraint_mode(0);
+    collect_used_addr = 0;
+  endtask
+
+endclass
