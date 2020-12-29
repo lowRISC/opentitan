69cb1f868be7af691d619f517a14aaf021197c83
Cindy Chen <chencindy@google.com>
2020-12-28 21:09:53 -0800

    [dv/alert_esc_agent] add basic support for esc sender driver
    This PR adds basic supports for esc_sender_driver. It only resets the
    signals and avoid fatal errors. Still need more work to drive esc
    sequence.
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/dv/sv/alert_esc_agent/esc_sender_driver.sv
+++ hw/dv/sv/alert_esc_agent/esc_sender_driver.sv
@@ -12,9 +12,30 @@ class esc_sender_driver extends alert_esc_base_driver;
 
   `uvm_component_new
 
+  virtual task reset_signals();
+    do_reset();
+    forever begin
+      @(negedge cfg.vif.rst_n);
+      under_reset = 1;
+      do_reset();
+      @(posedge cfg.vif.rst_n);
+      under_reset = 0;
+    end
+  endtask
+
   virtual task get_and_drive();
     // TODO
+    drive_esc();
   endtask : get_and_drive
 
+  virtual task drive_esc();
+    // TODO
+    wait(!under_reset);
+  endtask
+
+  virtual task do_reset();
+    cfg.vif.esc_tx_int.esc_p <= 1'b0;
+    cfg.vif.esc_tx_int.esc_n <= 1'b1;
+  endtask
 endclass : esc_sender_driver
 
