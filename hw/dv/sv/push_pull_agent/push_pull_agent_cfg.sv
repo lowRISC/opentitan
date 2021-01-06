7fa544b48500f4691da8b5a88db2030b3c6005a1
Udi Jonnalagadda <udij@google.com>
2021-01-06 10:10:21 -0800

    [push_pull_agent] allow user to constrain data
    This PR adds the ability for a user to specify some data value for the
    push_pull_agent to transmit in either Host or Device modes, using a
    setter function in the base sequence and a cfg knob.
    
    Signed-off-by: Udi Jonnalagadda <udij@google.com>

--- hw/dv/sv/push_pull_agent/push_pull_agent_cfg.sv
+++ hw/dv/sv/push_pull_agent/push_pull_agent_cfg.sv
@@ -38,6 +38,19 @@ class push_pull_agent_cfg #(parameter int HostDataWidth = 32,
   // Enable starting the device sequence by default if configured in Device mode.
   bit start_default_device_seq = 1;
 
+  // These data queues allows users to specify data to be driven by the sequence at a higher level.
+  //
+  // To specify some Host data to be sent, `set_h_user_data()` should be called from a higher layer
+  // to push the specified user data into the `h_user_data_q` queue.
+  // The Host sequence will then check if the queue has any data in it, if so it will pop off the
+  // first entry and drive that data value.
+  //
+  // Similarly, `set_d_user_data()` should be called from a higher layer to push some specified
+  // user data into the `d_user_data_q` queue, which will then be popped off and driven by the
+  // Device sequence.
+  local bit [HostDataWidth-1:0]   h_user_data_q[$];
+  local bit [DeviceDataWidth-1:0] d_user_data_q[$];
+
   constraint device_delay_min_c {
     device_delay_min == 0;
   }
@@ -62,6 +75,42 @@ class push_pull_agent_cfg #(parameter int HostDataWidth = 32,
                        1 := 3 };
   }
 
+  // Setter method for the user data queues - must be called externally to place specific user-data
+  // to be sent by the driver.
+  function void add_h_user_data(bit [HostDataWidth-1:0] data);
+    h_user_data_q.push_back(data);
+  endfunction
+
+  // Setter method for the user data queues - must be called externally to place specific user-data
+  // to be sent by the driver.
+  function void add_d_user_data(bit [DeviceDataWidth-1:0] data);
+    d_user_data_q.push_back(data);
+  endfunction
+
+  // Getter method for the user data queues - returns the first data entry.
+  function bit [HostDataWidth-1:0] get_h_user_data();
+    `DV_CHECK_NE_FATAL(has_h_user_data(), 0, "h_user_data_q is empty!")
+    return h_user_data_q.pop_front();
+  endfunction
+
+  // Getter method for the user data queues - returns the first data entry.
+  function bit [DeviceDataWidth-1:0] get_d_user_data();
+    `DV_CHECK_NE_FATAL(has_d_user_data(), 0, "d_user_data_q is empty!")
+    return d_user_data_q.pop_front();
+  endfunction
+
+  // Getter method for the user data queues - must be called externally to check whether there is
+  // any user data in the queues.
+  function bit has_h_user_data();
+    return (h_user_data_q.size() > 0);
+  endfunction
+
+  // Getter method for the user data queues - must be called externally to check whether there is
+  // any user data in the queues.
+  function bit has_d_user_data();
+    return (d_user_data_q.size() > 0);
+  endfunction
+
   `uvm_object_param_utils_begin(push_pull_agent_cfg#(HostDataWidth, DeviceDataWidth))
     `uvm_field_enum(push_pull_agent_e, agent_type, UVM_DEFAULT)
     `uvm_field_int(in_bidirectional_mode,          UVM_DEFAULT)
@@ -71,6 +120,8 @@ class push_pull_agent_cfg #(parameter int HostDataWidth = 32,
     `uvm_field_int(host_delay_max,                 UVM_DEFAULT)
     `uvm_field_int(zero_delays,                    UVM_DEFAULT)
     `uvm_field_int(start_default_device_seq,       UVM_DEFAULT)
+    `uvm_field_queue_int(h_user_data_q,            UVM_DEFAULT)
+    `uvm_field_queue_int(d_user_data_q,            UVM_DEFAULT)
   `uvm_object_utils_end
 
   `uvm_object_new
