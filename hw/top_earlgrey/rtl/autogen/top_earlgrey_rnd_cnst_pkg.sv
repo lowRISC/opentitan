6914ffdc09a9982f82c8463c1d3bf9fe22556d6a
Michael Schaffner <msf@opentitan.org>
2021-01-08 20:12:32 -0800

    [top/otp_ctrl] Remove random constants on the top-level
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/top_earlgrey/rtl/autogen/top_earlgrey_rnd_cnst_pkg.sv
+++ hw/top_earlgrey/rtl/autogen/top_earlgrey_rnd_cnst_pkg.sv
@@ -26,30 +26,6 @@ package top_earlgrey_rnd_cnst_pkg;
     240'h5D294061E29A7C404F4593035A19097666E37072064153623855022D39E0
   };
 
-  // Compile-time random scrambling keys
-  parameter otp_ctrl_pkg::key_array_t RndCnstOtpCtrlKey = {
-    128'h79EE911CE801484BA8373086F9DD4EEE,
-    256'h6FAF88F22BCCD612D1C09F5C02B2C8D1FDB92558E2D9C5D24440722325A93144
-  };
-
-  // Compile-time random digest constant
-  parameter otp_ctrl_pkg::digest_const_array_t RndCnstOtpCtrlDigestConst = {
-    128'hCE7D846469A3B8E35A6BD38295BD2FB3,
-    256'h66B3D62126C75EEAEB93D32F5CBC77463C91917516D51A2FA4400ADC2669E253,
-    256'h0EE2A465FD4DABCBD877AFB6BCFEED7E03A0B091DC41D062DD10CA2D7B93136F
-  };
-
-  // Compile-time random digest IV
-  parameter otp_ctrl_pkg::digest_iv_array_t RndCnstOtpCtrlDigestIV = {
-    64'hB77F07FF3D729720,
-    256'h0D5AB25561AF49C696466A983E5346826A43628219E5A91389B9FE0D3B818E46
-  };
-
-  // Compile-time random value for RAW unlock token.
-  parameter lc_ctrl_pkg::lc_token_t RndCnstOtpCtrlRawUnlockToken = {
-    128'hDE094CA8F1435F85E0F7489A309CBE57
-  };
-
   ////////////////////////////////////////////
   // lc_ctrl
   ////////////////////////////////////////////
