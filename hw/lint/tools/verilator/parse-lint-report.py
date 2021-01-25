9ce4bd9eba8ee99a301ce52f136ca70aa06ccf93
Michael Schaffner <msf@opentitan.org>
2021-01-25 14:42:17 -0800

    [lint] Remove Fusesoc-related message waivers
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/lint/tools/verilator/parse-lint-report.py
+++ hw/lint/tools/verilator/parse-lint-report.py
@@ -45,11 +45,7 @@ def parse_lint_log(str_buffer):
         # this is a workaround until we actually have native Edalize
         # support for JasperGold and "formal" targets
         ("warnings",
-         r"^(?!WARNING: Unknown item formal in section Target)WARNING: .*"
-         # TODO(https://github.com/lowRISC/ibex/issues/1033):
-         # remove once this has been fixed in Edalize or in the corefile.
-         r"^(?!WARNING: Unknown item symbiyosis in section Target)WARNING: .*"
-         ),
+         r"^(?!WARNING: Unknown item formal in section Target)WARNING: .*"),
         ("warnings", r"^%Warning: .* "),
         ("lint_errors", r"^%Error-.*"),
         ("lint_warnings", r"^%Warning-.*"),
