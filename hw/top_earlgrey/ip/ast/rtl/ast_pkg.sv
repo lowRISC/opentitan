e24bae40909493d956cff33b1fec587972ff75fa
Arnon Sharlin <arnon.sharlin@opentitan.org>
2021-02-01 12:53:04 +0200

    [ast] removed ast_wrapper. Still needs work in top level
    Signed-off-by: Arnon Sharlin <arnon.sharlin@opentitan.org>

--- hw/top_earlgrey/ip/ast/rtl/ast_pkg.sv
+++ hw/top_earlgrey/ip/ast/rtl/ast_pkg.sv
@@ -11,6 +11,90 @@
 `define __AST_PKG
 package ast_pkg;
 
+  parameter int NumAlerts       = top_pkg::NUM_AST_ALERTS;
+  parameter int NumIoRails      = top_pkg::NUM_IO_RAILS;
+  parameter int EntropyStreams  = top_pkg::ENTROPY_STREAM;
+  parameter int AdcChannels     = top_pkg::ADC_CHANNELS;
+  parameter int AdcDataWidth    = top_pkg::ADC_DATAW;
+  parameter int UsbCalibWidth   = 16;
+  parameter int Ast2PadOutWidth = 10;
+  parameter int Pad2AstInWidth  = 10;
+
+  typedef struct packed {
+    logic [AdcChannels-1:0] channel_sel;
+    logic pd;
+  } adc_ast_req_t;
+
+  typedef struct packed {
+    logic [AdcDataWidth-1:0] data;
+    logic data_valid;
+  } adc_ast_rsp_t;
+
+  typedef struct packed {
+    logic aon_pok;
+  } ast_rst_t;
+
+  parameter ast_rst_t AST_RST_DEFAULT = '{
+    aon_pok: 1'b1
+  };
+
+  typedef struct packed {
+    logic clk_sys;
+    logic clk_io;
+    logic clk_usb;
+    logic clk_aon;
+  } ast_clks_t;
+
+  typedef struct packed {
+    logic [NumAlerts-1:0] alerts_p;
+    logic [NumAlerts-1:0] alerts_n;
+  } ast_alert_req_t;
+
+  typedef struct packed {
+    logic [NumAlerts-1:0] alerts_ack;
+    logic [NumAlerts-1:0] alerts_trig;
+  } ast_alert_rsp_t;
+
+  typedef struct packed {
+    logic [NumIoRails-1:0] io_pok;
+  } ast_status_t;
+
+  // Ack mode enumerations
+  typedef enum logic {
+    ImmAck = 0,
+    SwAck  = 1
+  } ast_ack_mode_e;
+
+  parameter ast_alert_req_t AST_ALERT_REQ_DEFAULT = '{
+    alerts_p: '0,
+    alerts_n: {NumAlerts{1'b1}}
+  };
+
+  typedef struct packed {
+    logic flash_bist_enable;
+    logic flash_power_down_h;
+    logic flash_power_ready_h;
+  } ast_eflash_t;
+
+  // Alert interface
+  typedef struct packed {
+    logic        p;
+    logic        n;
+  } ast_dif_t;
+
+  // Read-Write Margin interface
+  typedef struct packed {
+    logic       marg_en_a;
+    logic [3:0] marg_a;
+    logic       marg_en_b;
+    logic [3:0] marg_b;
+  } dpm_rm_t;
+
+  typedef struct packed {
+    logic       marg_en;
+    logic [3:0] marg;
+  } spm_rm_t;
+
 `ifndef VERILATOR
 // synopsys translate_off
 /////////////////////////////////
@@ -44,26 +128,6 @@ parameter time RNG_EN_RDLY     = 5us;
 `endif
 // ADC
 parameter int AdcCnvtClks      = 22;
-
-// Alert interface
-typedef struct packed {
-  logic        p;
-  logic        n;
-} ast_dif_t;
-
-// Read-Write Margin interface
-typedef struct packed {
-  logic       marg_en_a;
-  logic [3:0] marg_a;
-  logic       marg_en_b;
-  logic [3:0] marg_b;
-} dpm_rm_t;
-
-typedef struct packed {
-  logic       marg_en;
-  logic [3:0] marg;
-} spm_rm_t;
-
 
 endpackage  // of ast_pkg
 `endif  // of __AST_PKG
