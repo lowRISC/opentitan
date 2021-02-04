cb0a21928f01c380d117f8f1e862797099e3a7d3
Silvestrs Timofejevs <silvestrst@lowrisc.org>
2021-02-04 16:29:33 +0000

    [sw, rom_ext_signer] Add signing and remaining image update API
    - Retrieving exponent is work-in-progress, and at the moment
      dummy hardcoded vector of `0xA5` is used.
    - Retrieving modulus is work-in-progress, and at the moment
      dummy hardcoded vector of `0xA5` is used.
    - Peripheral Lockdown Info encodingis work in progress, and at
      the moment dummy hardcoded vector of `0xA5` is used.
    - It is not clear yet how the device_usage_value is encoded in
      the 256-bit usage_constraints blob, and at the moment dummy
      hardcoded vector of `0xA5` is used.
    
    This change adds image signing functionality through Mundane.
    
    * There was some refactoring done, missing fields
      `extension{0,1,2,3}_offset` have been added.
    
    * `image_length` is no longer passed in the config, as it is
      set automatically in the manifest assembly file.
    
    * `image_timestamp` is no longer passed in the config, and is
      calculated at runtime.
    
    Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>

--- sw/host/rom_ext_image_tools/signer/src/main.rs
+++ sw/host/rom_ext_image_tools/signer/src/main.rs
@@ -7,10 +7,19 @@
 #![deny(unsafe_code)]
 
 use std::env;
+use std::fs;
 use std::path::Path;
 
 use rom_ext_config::parser::ParsedConfig;
 use rom_ext_image::image::RawImage;
+
+use mundane::hash::Sha256;
+use mundane::public::rsa::RsaPkcs1v15;
+use mundane::public::rsa::RsaPrivKey;
+use mundane::public::rsa::RsaSignature;
+use mundane::public::rsa::B3072;
+use mundane::public::DerPrivateKey;
+use mundane::public::Signature;
 
 fn main() {
     let arg: String = env::args().nth(1).expect("Config path is missing");
@@ -21,10 +30,74 @@ fn main() {
     let config = ParsedConfig::new(&config_path);
 
     // Read raw binary.
-    let image_path = Path::new(&config.input_files.image_path);
-    let mut raw_image = RawImage::new(&image_path);
+    let mut image = RawImage::new(&config.input_files.image_path);
 
-    // Modify raw binary.
-    raw_image.update_generic_fields(&config);
-    raw_image.write_file();
+    // Get the private key used for signature generation. It is also used to
+    // extract key public exponent and modulus.
+    let private_key_der =
+        fs::read(&config.input_files.private_key_der_path).expect("Failed to read the image!");
+
+    // Update "signed" manifest fields.
+    image.update_static_fields(&config);
+
+    let exponent = &signature_key_public_exponent_le();
+    image.update_exponent_field(exponent);
+
+    let modulus = &signature_key_modulus_le();
+    image.update_modulus_field(modulus);
+
+    // Convert ASN.1 DER private key into Mundane RsaPrivKey.
+    let private_key =
+        RsaPrivKey::parse_from_der(&private_key_der).expect("Failed to parse private key!");
+
+    // Produce the signature from concatenated system_state_value,
+    // device_usage_value and the portion of the "signed" portion of the image.
+    let image_sign_data = image.data_to_sign();
+    let device_usage_value = &device_usage_value(&config.input_files.usage_constraints_path);
+    let system_state_value = &system_state_value(&config.input_files.system_state_value_path);
+
+    let mut message_to_sign = Vec::<u8>::new();
+    message_to_sign.extend_from_slice(system_state_value);
+    message_to_sign.extend_from_slice(device_usage_value);
+    message_to_sign.extend_from_slice(image_sign_data);
+
+    let signature =
+        RsaSignature::<B3072, RsaPkcs1v15, Sha256>::sign(&private_key, &message_to_sign)
+            .expect("Failed to sign!");
+
+    image.update_signature_field(&signature.bytes());
+
+    // The whole image has been updated and signed, write the result to disk.
+    image.write_file();
+}
+
+/// Generate a dummy signature key public exponent.
+///
+/// Eventually this value will be obtained from the private key.
+fn signature_key_public_exponent_le() -> Vec<u8> {
+    vec![0xA5; 1]
+}
+
+/// Generate a dummy signature key modulus.
+///
+/// Eventually this value will be obtained from the private key.
+fn signature_key_modulus_le() -> Vec<u8> {
+    vec![0xA5; 384]
+}
+
+/// Generates the device usage value.
+///
+/// This value is extrapolated from the ROM_EXT manifest usage_constraints
+/// field, and does not reside in the ROM_EXT manifest directly.
+pub fn device_usage_value(path: &Path) -> Vec<u8> {
+    let _usage_constraints = fs::read(path).expect("Failed to read usage constraints!");
+
+    // TODO: generate the device_usage_value from usage_constraints.
+    //       meanwhile use a "dummy" hard-coded vector.
+    vec![0xA5; 1024]
+}
+
+/// Obtains the system state value from the binary on disk.
+pub fn system_state_value(path: &Path) -> Vec<u8> {
+    fs::read(path).expect("Failed to read system state value!")
 }
