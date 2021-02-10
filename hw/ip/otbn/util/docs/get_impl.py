f6803cbe20ef438cbebe38d50cfa60a3e4348018
Rupert Swarbrick <rswarbrick@lowrisc.org>
2021-02-10 10:45:48 +0000

    [otbn] Initial rewriting support in extracted documentation
    This generates a "pretty" version of GPR/WDR accesses. For example,
    the ADD instruction looked like this before:
    
        val1 = state.gprs.get_reg(self.grs1).read_unsigned()
        val2 = state.gprs.get_reg(self.grs2).read_unsigned()
        result = (val1 + val2) & ((1 << 32) - 1)
        state.gprs.get_reg(self.grd).write_unsigned(result)
    
    and now looks like this:
    
        val1 = GPRs[self.grs1]
        val2 = GPRs[self.grs2]
        result = (val1 + val2) & ((1 << 32) - 1)
        GPRs[self.grd] = result
    
    Signed (2's complement) conversions are shown explicitly. For example,
    here's SRA:
    
        val1 = from_2s_complement(GPRs[self.grs1])
        val2 = GPRs[self.grs2] & 0x1f
        result = val1 >> val2
        GPRs[self.grd] = to_2s_complement(result)
    
    WDRs are also converted. For example, BN.ADD looks like this:
    
        a = WDRs[self.wrs1]
        b = WDRs[self.wrs2]
        b_shifted = logical_byte_shift(b, self.shift_type, self.shift_bytes)
    
        (result, flags) = state.add_with_carry(a, b_shifted, 0)
        WDRs[self.wrd] = result
        state.set_flags(self.flag_group, flags)
    
    This is just an initial rewrite pass. If we go with this, we'll want
    to do things for flags, at least. This is easy enough with the same
    framework.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/otbn/util/docs/get_impl.py
+++ hw/ip/otbn/util/docs/get_impl.py
@@ -5,32 +5,206 @@
 from typing import Dict, Optional, Sequence
 
 import libcst as cst
+from libcst._nodes.internal import CodegenState
 
 
-class ImplVisitor(cst.CSTVisitor):
+class RegRef(cst.Subscript):
+    '''An expression class to represent references to the register file'''
+    gprs = cst.Name('GPRs')
+    wdrs = cst.Name('WDRs')
+
+    is_wide: bool
+    idx: cst.BaseExpression
+
+    def __init__(self, is_wide: bool, idx: cst.BaseExpression):
+        # We want to do a computation when defining the fields in the base
+        # class (value and slice). That means we can't use the automatically
+        # generated init method from dataclass (nor can we use it's
+        # __post_init__ hook: that would happen too late).
+        #
+        # However, we also want to store fields ourselves (is_wide and idx). We
+        # can't just do something like "self.is_wide = is_wide", because that
+        # crashes into the frozen setattr that the base class defines. Instead,
+        # we have to use object.__setattr__ directly.
+        sub_elt = cst.SubscriptElement(slice=cst.Index(value=idx))
+        super().__init__(value=(RegRef.wdrs if is_wide else RegRef.gprs),
+                         slice=[sub_elt])
+        object.__setattr__(self, 'is_wide', is_wide)
+        object.__setattr__(self, 'idx', idx)
+
+    def _visit_and_replace_children(self,
+                                    visitor: cst.CSTVisitorT) -> 'RegRef':
+        new_idx = self.idx.visit(visitor)
+        if isinstance(new_idx, cst.RemovalSentinel):
+            raise ValueError('Cannot remove index of a RegRef')
+        return RegRef(self.is_wide, new_idx)
+
+    def _codegen_impl(self, state: CodegenState) -> None:
+        reg_bank = 'WDRs' if self.is_wide else 'GPRs'
+        state.add_token(reg_bank)
+        state.add_token('[')
+        with state.record_syntactic_position(self):
+            self.idx._codegen(state)
+        state.add_token(']')
+
+
+class ImplTransformer(cst.CSTTransformer):
     '''An AST visitor used to extract documentation from the ISS'''
     def __init__(self) -> None:
         self.impls = {}  # type: Dict[str, Sequence[cst.BaseStatement]]
 
         self.cur_class = None  # type: Optional[str]
 
-    def visit_ClassDef(self, node: cst.ClassDef) -> None:
+    def visit_ClassDef(self, node: cst.ClassDef) -> Optional[bool]:
         assert self.cur_class is None
         self.cur_class = node.name.value
+        return None
 
-    def leave_ClassDef(self, node: cst.ClassDef) -> None:
+    def leave_ClassDef(self,
+                       orig: cst.ClassDef,
+                       updated: cst.ClassDef) -> cst.BaseStatement:
         self.cur_class = None
+        return updated
 
-    def leave_FunctionDef(self, node: cst.FunctionDef) -> None:
+    def leave_FunctionDef(self,
+                          orig: cst.FunctionDef,
+                          updated: cst.FunctionDef) -> cst.BaseStatement:
         if ((self.cur_class is None or
-             node.name.value != 'execute' or
+             updated.name.value != 'execute' or
              self.cur_class in self.impls)):
-            return
+            return updated
 
         # The body of a function definition is always an IndentedBlock. Strip
         # that out to get at the statements inside.
-        assert isinstance(node.body, cst.IndentedBlock)
-        self.impls[self.cur_class] = node.body.body
+        assert isinstance(updated.body, cst.IndentedBlock)
+        self.impls[self.cur_class] = updated.body.body
+        return updated
+
+    @staticmethod
+    def match_get_reg(call: cst.BaseExpression) -> Optional[RegRef]:
+        '''Extract a RegRef from state.gprs.get_reg(foo)
+
+        Returns None if this isn't a match.
+
+        '''
+        if not isinstance(call, cst.Call):
+            return None
+
+        # We expect a single argument (which we take as the index)
+        if len(call.args) != 1:
+            return None
+
+        getreg_idx = call.args[0].value
+
+        # All we need to do still is check that call.func is an
+        # attribute representing state.gprs.get_reg or state.wdrs.get_reg.
+        if ((not isinstance(call.func, cst.Attribute) or
+             call.func.attr.value != 'get_reg')):
+            return None
+
+        state_dot_reg = call.func.value
+        if not isinstance(state_dot_reg, cst.Attribute):
+            return None
+
+        # Finally, state_dot_reg should be either state.gprs or state.wdrs
+        if not (isinstance(state_dot_reg.value, cst.Name) and
+                state_dot_reg.value.value == 'state'):
+            return None
+
+        regfile_name = state_dot_reg.attr.value
+        if regfile_name == 'gprs':
+            is_wide = False
+        elif regfile_name == 'wdrs':
+            is_wide = True
+        else:
+            return None
+
+        return RegRef(is_wide, getreg_idx)
+
+    def leave_Call(self,
+                   orig: cst.Call,
+                   updated: cst.Call) -> cst.BaseExpression:
+        # Detect
+        #
+        #    state.gprs.get_reg(FOO).read_unsigned()
+        #    state.gprs.get_reg(FOO).read_signed()
+        #
+        # and replace it with the expressions
+        #
+        #    GPRs[FOO]
+        #    from_2s_complement(GPRs[FOO])
+        #
+        # respectively.
+
+        # In either case, we expect updated.func to be some long attribute
+        # (representing state.gprs.get_reg(FOO).read_X). For unsigned or
+        # signed, we can check that it is indeed an Attribute and that
+        # updated.args is empty (neither function takes arguments).
+        if updated.args or not isinstance(updated.func, cst.Attribute):
+            return updated
+
+        # Now, check whether we're calling one of the functions we're
+        # interested in.
+        if updated.func.attr.value == 'read_signed':
+            signed = True
+        elif updated.func.attr.value == 'read_unsigned':
+            signed = False
+        else:
+            return updated
+
+        # Check that updated.func.value really does represent something of the
+        # form "state.gprs.get_reg(FOO)".
+        ret = ImplTransformer.match_get_reg(updated.func.value)
+        if ret is None:
+            return updated
+
+        if signed:
+            # If this is a call to read_signed, we want to wrap the returned
+            # value in a call to a fake sign decode function.
+            return cst.Call(func=cst.Name('from_2s_complement'),
+                            args=[cst.Arg(value=ret)])
+        else:
+            return ret
+
+    def leave_Expr(self,
+                   orig: cst.Expr,
+                   updated: cst.Expr) -> cst.BaseSmallStatement:
+        # This is called when leaving statements that are just expressions. We
+        # use it to spot
+        #
+        #   state.gprs.get_reg(foo).write_unsigned(bar)
+        #   state.gprs.get_reg(foo).write_signed(bar)
+        #
+        # and turn it into
+        #
+        #   GPRs[FOO] = bar
+        #   GPRs[FOO] = to_2s_complement(bar)
+
+        if not isinstance(updated.value, cst.Call):
+            return updated
+
+        call = updated.value
+        if len(call.args) != 1 or not isinstance(call.func, cst.Attribute):
+            return updated
+
+        value = call.args[0].value
+
+        if call.func.attr.value == 'write_unsigned':
+            rhs = value
+        elif call.func.attr.value == 'write_signed':
+            rhs = cst.Call(func=cst.Name('to_2s_complement'),
+                           args=[cst.Arg(value=value)])
+        else:
+            return updated
+
+        # We expect call.func.value to be match state.gprs.get_reg(foo).
+        # Extract the RegRef if we can.
+        reg_ref = ImplTransformer.match_get_reg(call.func.value)
+        if reg_ref is None:
+            return updated
+
+        return cst.Assign(targets=[cst.AssignTarget(target=reg_ref)],
+                          value=rhs)
 
 
 def read_implementation(path: str) -> Dict[str, str]:
@@ -45,7 +219,7 @@ def read_implementation(path: str) -> Dict[str, str]:
         node = cst.parse_module(handle.read())
 
     # Extract the function bodies
-    visitor = ImplVisitor()
+    visitor = ImplTransformer()
     node.visit(visitor)
 
     # Render the function bodies
