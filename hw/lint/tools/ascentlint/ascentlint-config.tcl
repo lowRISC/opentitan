2a8d5ba394894ec32faef248d8048e222a49858b
Michael Schaffner <msf@google.com>
2021-02-10 19:49:30 -0800

    [lint] Increase threshold for max number of bits in an array
    We have large regfiles in our design that have a size of 8kbit or more,
    such as the OTBN regfile.
    
    This commit increases the AscentLint-internal threshold for analyzing
    such large arrays to 32kbit in order to get the affected designs lint clean.
    
    Signed-off-by: Michael Schaffner <msf@google.com>

--- hw/lint/tools/ascentlint/ascentlint-config.tcl
+++ hw/lint/tools/ascentlint/ascentlint-config.tcl
@@ -7,3 +7,9 @@ set RI_INSTALL [file dirname [exec which ascentlint]]
 
 # source the policy file containing the lowrisc lint rules
 source "$RI_INSTALL/../Ascent/Lint/lib/policies/lowRISC/LRLR-v1.0.policy"
+
+# increase this from 8k (default) to 32k such that large arrays like
+# regfiles can be analyzed and linted.
+set ri_max_total_range_bits 32768
+set ri_max_single_range_bits 32768
+
