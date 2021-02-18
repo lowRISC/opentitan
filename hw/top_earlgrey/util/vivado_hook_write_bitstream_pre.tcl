d631dec341d52aa99734671c73e62a24097b6888
Pirmin Vogel <vogelpi@lowrisc.org>
2021-02-18 13:30:37 +0100

    [fpga] Add timestamp for bitstream identification via USR_ACCESS reg
    This commit modifies the bitstream generation to add a timestamp
    in the USR_ACCESS register for later identification. This is useful to
    find out which bitstream is currently loaded on a running FPGA.
    
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

--- hw/top_earlgrey/util/vivado_hook_write_bitstream_pre.tcl
+++ hw/top_earlgrey/util/vivado_hook_write_bitstream_pre.tcl
@@ -13,3 +13,6 @@ send_msg "Designcheck 1-2" INFO "Slack is ${slack_ns} ns."
 if [expr {$slack_ns < 0}] {
   send_msg "Designcheck 1-3" ERROR "Timing failed. Slack is ${slack_ns} ns."
 }
+
+# Enable bitstream identification via USR_ACCESS register.
+set_property BITSTREAM.CONFIG.USR_ACCESS TIMESTAMP [current_design]
