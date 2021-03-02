7af57e4c7d961fe7ccac30dea7f12b20b44a6d10
Srikrishna Iyer <sriyer@google.com>
2021-03-02 11:32:44 -0800

    [DV][FCOV] Minor updates to #5414
    - Prefix all macros in `dv_fcov_macros.svh` with `DV_FCOV`
      - Makes it clear where to lookup their definition
    
    - Remove DV_FCOV_SVA - this is already defined as `COVER` in prim_assert
    
    - Move DV_FCOV_EXPR_SEEN to `dv_fcov_macros.svh` since it will be used
    only in DV code.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/dv/sv/dv_utils/dv_fcov_macros.svh
+++ hw/dv/sv/dv_utils/dv_fcov_macros.svh
@@ -2,49 +2,94 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// Include FCOV RTL by default. Exclude it for synthesis and where explicitly requested (by defining
-// EXCLUDE_FCOV).
+// Include FCOV RTL by default. Disable it for synthesis and where explicitly requested (by defining
+// DV_FCOV_DISABLE).
 `ifdef SYNTHESIS
-  `define EXCLUDE_FCOV
+  `define DV_FCOV_DISABLE
 `elsif YOSYS
-  `define EXCLUDE_FCOV
+  `define DV_FCOV_DISABLE
+`endif
+
+// Disable instantiations of FCOV coverpoints or covergroups.
+`ifdef VERILATOR
+  `define DV_FCOV_DISABLE_CP
+`elsif DV_FCOV_DISABLE
+  `define DV_FCOV_DISABLE_CP
+`endif
+
+// Instantiates a covergroup in an interface or module.
+//
+// This macro assumes that a covergroup of the same name as the NAME_ arg is defined in the
+// interface or module. It just adds some extra signals and logic to control the creation of the
+// covergroup instance with ~bit en_<cg_name>~. This defaults to 0. It is ORed with the external
+// COND_ signal. The testbench can modify it at t = 0 based on the test being run.
+// NOTE: This is not meant to be invoked inside a class.
+//
+// NAME_ : Name of the covergroup.
+// COND_ : External condition / expr that controls the creation of the covergroup.
+// ARGS_ : Arguments to covergroup instance, if any. Args MUST BE wrapped in (..).
+`ifndef DV_FCOV_INSTANTIATE_CG
+`ifdef DV_FCOV_DISABLE_CP
+  `define DV_FCOV_INSTANTIATE_CG(NAME_, COND_ = 1'b1, ARGS_ = ())
+`else
+  `define DV_FCOV_INSTANTIATE_CG(NAME_, COND_ = 1'b1, ARGS_ = ()) \
+    bit en_``NAME_ = 1'b0; \
+    NAME_ NAME_``_inst; \
+    initial begin \
+      /* The #1 delay below allows any part of the tb to control the conditions first at t = 0. */ \
+      #1; \
+      if ((en_``NAME_) || (COND_)) begin \
+        $display("%0t: (%0s:%0d) [%m] %0s", $time, `__FILE__, `__LINE__, \
+                 {"Creating covergroup ", `"NAME_`"}); \
+        NAME_``_inst = new``ARGS_; \
+      end \
+    end
+`endif
+`endif
+
+// Creates a coverpoint for an expression where only the expression true case is of interest for
+// coverage (e.g. where the expression indicates an event has occured).
+`ifndef DV_FCOV_EXPR_SEEN
+`ifdef DV_FCOV_DISABLE_CP
+  `define DV_FCOV_EXPR_SEEN(NAME_, EXPR_)
+`else
+  `define DV_FCOV_EXPR_SEEN(NAME_, EXPR_) cp_``NAME_: coverpoint EXPR_ { bins seen = {1}; }
+`endif
 `endif
 
 // Coverage support is not always available but it's useful to include extra fcov signals for
 // linting purposes. They need to be marked as unused to avoid warnings.
-`ifndef FCOV_MARK_UNUSED
-  `define FCOV_MARK_UNUSED(TYPE_, NAME_) \
+`ifndef DV_FCOV_MARK_UNUSED
+  `define DV_FCOV_MARK_UNUSED(TYPE_, NAME_) \
     TYPE_ unused_fcov_``NAME_; \
     assign unused_fcov_``NAME_ = fcov_``NAME_;
 `endif
 
 // Define a signal and expression in the design for capture in functional coverage
-`ifndef FCOV_SIGNAL
-`ifdef EXCLUDE_FCOV
-  // Macro has no effect
-  `define FCOV_SIGNAL(TYPE_, NAME_, EXPR_)
+`ifndef DV_FCOV_SIGNAL
+`ifdef DV_FCOV_DISABLE
+  `define DV_FCOV_SIGNAL(TYPE_, NAME_, EXPR_)
 `else
-  `define FCOV_SIGNAL(TYPE_, NAME_, EXPR_) \
+  `define DV_FCOV_SIGNAL(TYPE_, NAME_, EXPR_) \
     TYPE_ fcov_``NAME_; \
     assign fcov_``NAME_ = EXPR_; \
-    `FCOV_MARK_UNUSED(TYPE_, NAME_)
+    `DV_FCOV_MARK_UNUSED(TYPE_, NAME_)
 `endif
 `endif
 
 // Define a signal and expression in the design for capture in functional coverage depending on
 // design configuration. The input GEN_COND_ must be a constant or parameter.
-`ifndef FCOV_SIGNAL_GEN_IF
-`ifdef EXCLUDE_FCOV
-  // Macro does nothing
-  `define FCOV_SIGNAL_GEN_IF(TYPE_, NAME_, EXPR_, GEN_COND_, DEFAULT_ = '0)
+`ifndef DV_FCOV_SIGNAL_GEN_IF
+`ifdef DV_FCOV_DISABLE
+  `define DV_FCOV_SIGNAL_GEN_IF(TYPE_, NAME_, EXPR_, GEN_COND_, DEFAULT_ = '0)
 `else
-  `define FCOV_SIGNAL_GEN_IF(TYPE_, NAME_, EXPR_, GEN_COND_, DEFAULT_ = '0) \
+  `define DV_FCOV_SIGNAL_GEN_IF(TYPE_, NAME_, EXPR_, GEN_COND_, DEFAULT_ = '0) \
     TYPE_ fcov_``NAME_; \
     if (GEN_COND_) begin : g_fcov_``NAME_ \
       assign fcov_``NAME_ = EXPR_; \
     end else begin : g_no_fcov_``NAME_ \
       assign fcov_``NAME_ = DEFAULT_; \
     end \
-    `FCOV_MARK_UNUSED(TYPE_, NAME_)
+    `DV_FCOV_MARK_UNUSED(TYPE_, NAME_)
 `endif
 `endif
