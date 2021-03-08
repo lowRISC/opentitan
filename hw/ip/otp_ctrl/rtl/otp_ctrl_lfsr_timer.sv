f0c3370c126c608d10e204fa4b8987f0cd58dafd
Michael Schaffner <msf@opentitan.org>
2021-03-08 11:05:04 -0800

    [otp_ctrl] Make sure timeout error is asserted permanently
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/otp_ctrl/rtl/otp_ctrl_lfsr_timer.sv
+++ hw/ip/otp_ctrl/rtl/otp_ctrl_lfsr_timer.sv
@@ -208,7 +208,7 @@ module otp_ctrl_lfsr_timer
     set_all_cnsty_reqs = '0;
 
     // Status signals going to CSRs and error logic.
-    chk_timeout_d = 1'b0;
+    chk_timeout_d = chk_timeout_q;
     chk_pending_o = cnsty_chk_trig_q || integ_chk_trig_q;
     fsm_err_o = 1'b0;
 
