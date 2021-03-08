769ea38f2497d892da731a2d7d04af594e3dc66f
Rupert Swarbrick <rswarbrick@lowrisc.org>
2021-03-08 17:52:52 +0000

    [fpv] Move .core file generation into reggen
    Once blocks may have multiple device interfaces, they might generate
    more than one "FOO_csr_assert_fpv.sv", depending on their exact
    interfaces. The .core file needs to list these files, which is a bit
    difficult to do from csr_assert_gen.py (which doesn't have the reggen
    data).
    
    Rather than re-parse the reggen data (possible, but seems a bit
    silly), move the .core file generation to reggen. Note: this assumes
    that the core file for the block is always called
    "lowrisc:ip:<block>". If this turns out not to be true in general, we
    can either pass it through as another argument to regtool.py
    or (perhaps better) call gen_fpv by loading up reggen directly.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- util/reggen/gen_fpv.py
+++ util/reggen/gen_fpv.py
@@ -7,7 +7,9 @@
 """
 
 import logging as log
+import os.path
 
+import yaml
 from mako import exceptions
 from mako.template import Template
 from pkg_resources import resource_filename
@@ -32,7 +34,10 @@ def gen_fpv(block: IpBlock, outdir):
         filename=resource_filename('reggen', 'fpv_csr.sv.tpl'))
 
     # Generate pkg.sv with block name
-    with open(outdir + "/" + block.name.lower() + "_csr_assert_fpv.sv", 'w') as fout:
+    lblock = block.name.lower()
+    mod_name = lblock + '_csr_assert_fpv'
+    file_name = mod_name + '.sv'
+    with open(os.path.join(outdir, file_name), 'w') as fout:
         try:
             fout.write(
                 fpv_csr_tpl.render(block=block,
@@ -42,5 +47,33 @@ def gen_fpv(block: IpBlock, outdir):
         except:  # noqa: 722
             log.error(exceptions.text_error_template().render())
             return 1
+
+    # Generate a fusesoc core file that points at the files we've just
+    # generated.
+    core_data = {
+        'name': "lowrisc:fpv:{}_csr_assert".format(lblock),
+        'filesets': {
+            'files_dv': {
+                'depend': [
+                    "lowrisc:ip:{}".format(lblock),
+                    "lowrisc:tlul:headers",
+                    "lowrisc:prim:assert",
+                ],
+                'files': [file_name],
+                'file_type': 'systemVerilogSource'
+            },
+        },
+        'targets': {
+            'default': {
+                'filesets': [
+                    'files_dv',
+                ],
+            },
+        },
+    }
+    core_file_path = os.path.join(outdir, lblock + '_csr_assert_fpv.core')
+    with open(core_file_path, 'w') as core_file:
+        core_file.write('CAPI=2:\n')
+        yaml.dump(core_data, core_file, encoding='utf-8')
 
     return 0
