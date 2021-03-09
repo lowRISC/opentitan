77a30c23eebd1faaf51dab3ba8eabd7cf4216e6a
Srikrishna Iyer <sriyer@google.com>
2021-03-08 21:49:17 -0800

    [uvmdvgen] Update DV doc path and terminology
    UVMDVGen generated DV doc appears to not be up to date with the change
    in the terminology (Testplan -> DV plan, DV plan -> DV document, path:
    hw/ip/<ip>/doc/dv_plan/index.md -> hw/ip/<ip>/doc/dv/index.md). This
    update fixes that.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- util/uvmdvgen/gen_agent.py
+++ util/uvmdvgen/gen_agent.py
@@ -16,6 +16,7 @@ def gen_agent(name, has_separate_host_device_driver, root_dir, vendor):
     agent_dir = root_dir + "/" + name + "_agent"
 
     # yapf: disable
+    # flake8: noqa
     # 4-tuple - path, ip name, class name, file ext
     agent_srcs = [(agent_dir,               name + '_', 'if',            '.sv'),
                   (agent_dir,               name + '_', 'item',          '.sv'),
