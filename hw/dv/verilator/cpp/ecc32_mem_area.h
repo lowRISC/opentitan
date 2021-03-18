33688aede8f972d12971564b6cc1960d6931fcc7
Rupert Swarbrick <rswarbrick@lowrisc.org>
2021-03-18 11:53:04 +0000

    [dpi] Make an "ECC32" flavour of MemArea
    This will support memories that use a (39,32)-SECDED code. At the
    moment, it doesn't actually add the check bits (because we don't check
    them in our implementation yet), but it does lay everything out
    correctly.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

---
+++ hw/dv/verilator/cpp/ecc32_mem_area.h
@@ -0,0 +1,36 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_HW_DV_VERILATOR_CPP_ECC32_MEM_AREA_H_
+#define OPENTITAN_HW_DV_VERILATOR_CPP_ECC32_MEM_AREA_H_
+
+#include "mem_area.h"
+
+/**
+ * A memory that implements 32-bit ECC, storing 39 = 32 + 7 bits of physical
+ * data for each 32 bits of logical data.
+ */
+class Ecc32MemArea : public MemArea {
+ public:
+  /**
+   * Constructor
+   *
+   * Create an Ecc32MemArea that will connect to a SystemVerilog memory at
+   * scope. It is size words long. Each memory word is 4 * width_32 bytes wide
+   * in the address space and 39 * width_32 bits wide in the physical memory.
+   */
+  Ecc32MemArea(const std::string &scope, uint32_t size, uint32_t width_32);
+
+  void LoadVmem(const std::string &path) const override;
+
+ private:
+  void WriteBuffer(uint8_t buf[SV_MEM_WIDTH_BYTES],
+                   const std::vector<uint8_t> &data,
+                   size_t start_idx) const override;
+
+  void ReadBuffer(std::vector<uint8_t> &data,
+                  const uint8_t buf[SV_MEM_WIDTH_BYTES]) const override;
+};
+
+#endif  // OPENTITAN_HW_DV_VERILATOR_CPP_ECC32_MEM_AREA_H_
