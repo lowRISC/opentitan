9db70299310472cc3180dd69bd55fb829593069f
Rupert Swarbrick <rswarbrick@lowrisc.org>
2021-03-22 11:19:10 +0000

    [otbn] Add a "multi" vseq to run binaries back-to-back
    This should catch errors where we don't re-initialise enough of the
    design when starting a new operation. It intentionally doesn't do a
    reset, which would hide that sort of thing.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/otbn/dv/uvm/env/seq_lib/otbn_base_vseq.sv
+++ hw/ip/otbn/dv/uvm/env/seq_lib/otbn_base_vseq.sv
@@ -117,4 +117,40 @@ class otbn_base_vseq extends cip_base_vseq #(
     `uvm_info(`gfn, $sformatf("\n\t ----| OTBN finished"), UVM_MEDIUM)
    endtask
 
+  virtual protected function string pick_elf_path();
+    chandle helper;
+    int     num_files;
+    string  elf_path;
+
+    // Check that cfg.otbn_elf_dir was set by the test
+    `DV_CHECK_FATAL(cfg.otbn_elf_dir.len() > 0);
+
+    // Pick an ELF file to use in the test. We have to do this via DPI (because you can't list a
+    // directory in pure SystemVerilog). To do so, we have to construct a helper object, which will
+    // look after memory allocation for the string holding the path.
+    helper = OtbnTestHelperMake(cfg.otbn_elf_dir);
+    `DV_CHECK_FATAL(helper != null)
+
+    // Ask the helper how many files there are. If it returns zero, the directory name is bogus or
+    // the directory is empty.
+    num_files = OtbnTestHelperCountFilesInDir(helper);
+    `DV_CHECK_FATAL(num_files > 0,
+                    $sformatf("No regular files found in directory `%0s'.", cfg.otbn_elf_dir))
+
+    // Pick a file, any file... Note that we pick an index on the SV side so that we use the right
+    // random seed. Then we convert back to a filename with another DPI call. If the result is the
+    // empty string, something went wrong.
+    elf_path = OtbnTestHelperGetFilePath(helper, $urandom_range(num_files - 1));
+    `DV_CHECK_FATAL(elf_path.len() > 0, "Bad index for ELF file")
+
+    // Use sformat in a trivial way to take a copy of the string, so we can safely free helper (and
+    // hence the old elf_path) afterwards.
+    elf_path = $sformatf("%0s", elf_path);
+
+    // Now that we've taken a copy of elf_path, we can safely free the test helper.
+    OtbnTestHelperFree(helper);
+
+    return elf_path;
+  endfunction
+
 endclass : otbn_base_vseq
