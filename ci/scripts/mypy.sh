71a89e96c9851ea250e860b9b694433323fe61c2
Rupert Swarbrick <rswarbrick@lowrisc.org>
2021-03-26 17:19:00 +0000

    [ci] Add a mypy lint check for code that uses it
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

---
+++ ci/scripts/mypy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Run mypy to do Python linting on code that uses it
+
+set -e
+
+dirs_with_lint_makefile=(
+    hw/ip/otbn/dv/rig
+    hw/ip/otbn/dv/otbnsim
+    hw/ip/otbn/util
+)
+
+retcode=0
+for dir in "${dirs_with_lint_makefile[@]}"; do
+    make -C "$dir" lint || {
+        echo -n "##vso[task.logissue type=error]"
+        echo "Failed mypy check in ${dir}."
+        retcode=1
+    }
+done
+
+exit $retcode
