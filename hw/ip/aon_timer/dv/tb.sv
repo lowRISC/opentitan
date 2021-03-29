b5435825b178a501d4ff77b37fc7f875631a937e
Rupert Swarbrick <rswarbrick@lowrisc.org>
2021-03-29 16:16:25 +0100

    [aon_timer, dv] Explicitly remove devmode interface
    The RTL doesn't have this interface at the moment. Remove it from the
    testbench rather than leaving it waving in the wind.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/aon_timer/dv/tb.sv
+++ hw/ip/aon_timer/dv/tb.sv
@@ -14,7 +14,6 @@ module tb;
   `include "dv_macros.svh"
 
   wire clk, rst_n;
-  wire devmode;
   wire [NUM_MAX_INTERRUPTS-1:0] interrupts;
   logic rst_aon_n_gen;
   wire rst_aon_n;
@@ -23,7 +22,6 @@ module tb;
   clk_rst_if clk_rst_if(.clk(clk), .rst_n(rst_n));
   clk_rst_if clk_aon_rst_if(.clk(clk_aon), .rst_n(rst_aon_n));
   pins_if #(NUM_MAX_INTERRUPTS) intr_if(interrupts);
-  pins_if #(1) devmode_if(devmode);
   tl_if tl_if(.clk(clk), .rst_n(rst_n));
 
   // dut
@@ -50,7 +48,6 @@ module tb;
     uvm_config_db#(virtual clk_rst_if)::set(null, "*.env", "clk_rst_vif", clk_rst_if);
     uvm_config_db#(virtual clk_rst_if)::set(null, "*.env", "clk_aon_rst_vif", clk_aon_rst_if);
     uvm_config_db#(intr_vif)::set(null, "*.env", "intr_vif", intr_if);
-    uvm_config_db#(devmode_vif)::set(null, "*.env", "devmode_vif", devmode_if);
     uvm_config_db#(virtual tl_if)::set(null, "*.env.m_tl_agent*", "vif", tl_if);
     $timeformat(-12, 0, " ps", 12);
     run_test();
