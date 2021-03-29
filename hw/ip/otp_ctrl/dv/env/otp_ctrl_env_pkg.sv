3800c6dc1f36b4134bc2b3a9eb0a90889e980f9c
Cindy Chen <chencindy@google.com>
2021-03-29 15:00:04 -0700

    [dv/otp] fix number of edn required for sram
    The num of EDN required for SRAM should be 12 instead of 10.
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/ip/otp_ctrl/dv/env/otp_ctrl_env_pkg.sv
+++ hw/ip/otp_ctrl/dv/env/otp_ctrl_env_pkg.sv
@@ -83,7 +83,7 @@ package otp_ctrl_env_pkg;
   parameter uint SCRAMBLE_KEY_SIZE  = 128;
   parameter uint NUM_ROUND          = 31;
 
-  parameter uint NUM_SRAM_EDN_REQ = 10;
+  parameter uint NUM_SRAM_EDN_REQ = 12;
   parameter uint NUM_OTBN_EDN_REQ = 16;
 
   parameter uint CHK_TIMEOUT_CYC = 40;
