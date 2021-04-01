d2c7fe60ee91c8b59326712e6f33bdeb1f4cae90
Guillermo Maturana <maturana@google.com>
2021-03-31 19:19:18 -0700

    [dv/clkmgr] Creates dv artifacts with uvmdvgen.py.
    Signed-off-by: Guillermo Maturana <maturana@google.com>

---
+++ hw/ip/clkmgr/dv/env/clkmgr_env_cfg.sv
@@ -0,0 +1,26 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class clkmgr_env_cfg extends cip_base_env_cfg #(.RAL_T(clkmgr_reg_block));
+
+  // ext component cfgs
+
+  `uvm_object_utils_begin(clkmgr_env_cfg)
+  `uvm_object_utils_end
+
+  `uvm_object_new
+
+  virtual function void initialize(bit [31:0] csr_base_addr = '1);
+    super.initialize(csr_base_addr);
+
+    // set num_interrupts & num_alerts
+    begin
+      uvm_reg rg = ral.get_reg_by_name("intr_state");
+      if (rg != null) begin
+        num_interrupts = ral.intr_state.get_n_used_bits();
+      end
+    end
+  endfunction
+
+endclass
