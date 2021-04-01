a5e37a41381c773cf994203dcda0a9aa20740b33
Timothy Chen <timothytim@google.com>
2021-03-31 19:56:10 -0700

    [sw] Minor software updates for dif_entropy_src and test
    - This change should now ensure the new seed is used and
      the entropy reads should be different between reads.
    - Lastly, a few auxillary functions are added to poll for specific
      status.  These likely should be replaced long term by something
      more robust
    - unittests are also udpated for the slightly tweaked sequence
    - At the moment, since we disable / re-enable entropy_src, the
      timing differences between platforms (dv vs verilator) causes
      the test to spit out different results.  For the time being,
      favor verilator until #5941 is in.
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- sw/device/tests/dif/dif_entropy_smoketest.c
+++ sw/device/tests/dif/dif_entropy_smoketest.c
@@ -14,9 +14,13 @@
 const test_config_t kTestConfig;
 
 const size_t kEntropyDataNumWords = 12;
+
+// TODO: These values are specific to verilator right now and will not work
+// for DV or FPGA, see #5941
 const uint32_t kExpectedEntropyData[] = {
-    0x65585497, 0x65585497, 0x65585497, 0x65585497, 0x65585497, 0x65585497,
-    0x65585497, 0x65585497, 0x65585497, 0x65585497, 0x65585497, 0x65585497,
+    0x0a3f4df0, 0x4e36fad2, 0xb5f3e0b9, 0x918f5ec0, 0x54ddd680, 0x62f04975,
+    0x700cf2f2, 0x61044bfe, 0x723d6c89, 0xe2d70e19, 0x5f02a234, 0xbf586fcc,
+
 };
 
 bool test_main() {
@@ -25,6 +29,9 @@ bool test_main() {
   };
   dif_entropy_t entropy;
   CHECK(dif_entropy_init(params, &entropy) == kDifEntropyOk);
+
+  // Disable entropy for test purpose, as it has been turned on by ROM
+  CHECK(dif_entropy_disable(&entropy) == kDifEntropyOk);
 
   const dif_entropy_config_t config = {
       .mode = kDifEntropyModeLfsr,
@@ -37,7 +44,8 @@ bool test_main() {
               [kDifEntropyTestMailbox] = false,
               [kDifEntropyTestVendorSpecific] = false,
           },
-      .reset_health_test_registers = true,
+      // this field needs to manually toggled by software.  Disable for now
+      .reset_health_test_registers = false,
       .single_bit_mode = kDifEntropySingleBitModeDisabled,
       .route_to_firmware = true,
       .sample_rate = 2,
@@ -47,7 +55,11 @@ bool test_main() {
 
   uint32_t entropy_data[kEntropyDataNumWords];
   for (uint32_t i = 0; i < kEntropyDataNumWords; ++i) {
-    CHECK(dif_entropy_read(&entropy, &entropy_data[i]) == kDifEntropyOk);
+    // wait for entropy to become available
+    while (dif_entropy_read(&entropy, &entropy_data[i]) != kDifEntropyOk)
+      ;
+    // LOG_INFO("received %x, expectecd %x", entropy_data[i],
+    // kExpectedEntropyData[i]);
     CHECK(entropy_data[i] == kExpectedEntropyData[i]);
   }
 
--- test/systemtest/config.py
+++ test/systemtest/config.py
@@ -74,6 +74,7 @@ TEST_APPS_SELFCHECKING = [
     },
     {
         "name": "dif_entropy_smoketest",
+        "targets": ["sim_verilator"],
     },
     {
         "name": "flash_ctrl_test",
