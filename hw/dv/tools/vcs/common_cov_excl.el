234a4765a009003ede943b439df46b877fe090fa
Rasmus Madsen <rasmus.madsen@wdc.com>
2021-04-12 05:53:36 -0700

    [tools/dv] updated coverage common exclusions for new naming
    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>

--- hw/dv/tools/vcs/common_cov_excl.el
+++ hw/dv/tools/vcs/common_cov_excl.el
@@ -6,7 +6,7 @@
 INSTANCE: tb.dut
 Toggle tl_i.a_user.parity "logic tl_i.a_user.parity[7:0]"
 Toggle tl_i.a_user.parity_en "logic tl_i.a_user.parity_en"
-Toggle tl_i.a_user.rsvd1 "logic tl_i.a_user.rsvd1[6:0]"
+Toggle tl_i.a_user.rsvd1 "logic tl_i.a_user.rsvd[6:0]"
 Toggle tl_o.d_opcode [1] "logic tl_o.d_opcode[2:0]"
 Toggle tl_o.d_opcode [2] "logic tl_o.d_opcode[2:0]"
 Toggle tl_o.d_param "logic tl_o.d_param[2:0]"
