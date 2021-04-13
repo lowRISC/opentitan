c0fb5f6eb0c4960e26d5ef14a26143a23cba2ad2
Cindy Chen <chencindy@google.com>
2021-04-13 12:56:36 -0700

    [dv/otp_ctrl] Add coverage exclusions
    1. Exclude otp_ctrl's FSM unreachable coverages
    2. Exclude alert_sender module coverage
    
    Here we did not perform a full UNR because design only reaches D2. There
    will be some design changes.
    
    Also I discussed with the designer and added a few sequence for
    alert_sender auto sequence (PR #6089). Once this sequence is implemented, we will
    come back and revisit the code coverage. But I believe alert_sender's
    coverage should not prohibit IPs from reaching V2.
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/dv/tools/vcs/common_cov_excl.el
+++ hw/dv/tools/vcs/common_cov_excl.el
@@ -2,10 +2,18 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// Exclude below TL toggle coverage as these pins won't be changed in the comportable IPs
+// Exclude below TL toggle coverage as these pins won't be changed in the comportable IPs.
+
+// Exclude alert_sender as it is fully verified in FPV. Note that user might also need to exclude
+// prim_diff_decode module, but it is not listed here because modules other than prim_alert_sender
+// might instantiate this prim_diff_decode as well.
+
 INSTANCE: tb.dut
 Toggle tl_o.d_opcode [1] "logic tl_o.d_opcode[2:0]"
 Toggle tl_o.d_opcode [2] "logic tl_o.d_opcode[2:0]"
 Toggle tl_o.d_param "logic tl_o.d_param[2:0]"
 Toggle tl_o.d_sink "logic tl_o.d_sink[0:0]"
 Toggle tl_i.a_param "logic tl_i.a_param[2:0]"
+// TODO: revisit after issue #6089 is implemented
+ANNOTATION: "[EXTERNAL] alert_sender is fully verified by FPV testbench."
+MODULE:prim_alert_sender
