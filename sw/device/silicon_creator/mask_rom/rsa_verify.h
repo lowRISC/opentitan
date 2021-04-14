c58dedd179e6d57fe04c056c89dd883a56fdc5f5
Alphan Ulusoy <alphan@google.com>
2021-04-14 18:12:03 -0400

    [mask_rom, sig_verify] Add keys to mask ROM
    This change defines an array of structs for storing keys in mask ROM,
    moves the keys used in tests to this array, and updates the functions in
    rsa_verify.c to use the new types.
    
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

--- sw/device/silicon_creator/mask_rom/rsa_verify.h
+++ sw/device/silicon_creator/mask_rom/rsa_verify.h
@@ -8,78 +8,51 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "sw/device/silicon_creator/mask_rom/sig_verify_keys.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif  // __cplusplus
-
-enum {
-  /**
-   * Number of words of the modulus and signature for RSA-3072.
-   */
-  kRsaNumWords = 96,
-};
-
-/**
- * Exponent to use for signature verification.
- *
- * This determines the e in s^e mod n.
- */
-// TODO(#22): May need to be updated after we decide on a key storage format.
-typedef enum rsa_verify_exponent {
-  /**
-   * e = 3.
-   */
-  kRsaVerifyExponent3,
-  /**
-   * e = 65537.
-   */
-  kRsaVerifyExponent65537,
-} rsa_verify_exponent_t;
 
 // FIXME: Make static and move this comment to the source file. This is here
 // just to be able to add a simple test.
 /**
  * Computes the Montgomery reduction of the product of two integers.
  *
- * Given x, y, m, and m', this function computes x*y*R^-1 mod m, where
- * - x, y, and m are integers with kRsaNumWords base 2^32 digits,
- * - m' = -m^-1 mod b,
- * - R is (2^32)^kRsaNumWords, e.g. 2^3072 for RSA-3072, and
- * - b is 2^32.
+ * Given an RSA public key, x, and y this function computes x*y*R^-1 mod n,
+ * where
+ * - x and y are integers with `kSigVerifyRsaNumWords` base 2^32 digits,
+ * - n is the modulus of the key, and
+ * - R is 2^`kSigVerifyRsaNumBits`, e.g. 2^3072 for RSA-3072.
  *
  * See Handbook of Applied Cryptography, Ch. 14, Alg. 14.36.
  *
- * @param x A `kRsaNumWords` long buffer, little-endian.
- * @param y A `kRsaNumWords` long buffer, little-endian.
- * @param m A `kRsaNumWords` long buffer, little-endian.
- * @param m0_inv Negative of the multiplicative inverse of m modulo b.
- * @param[out] result A `kRsaNumWords` long buffer, little-endian.
+ * @param key An RSA public key.
+ * @param x Buffer that holds `x`, little-endian.
+ * @param y Buffer that holds `y`, little-endian.
+ * @param[out] result Buffer to write the result to, little-endian.
  */
-void mont_mul(const uint32_t *x, const uint32_t *y, const uint32_t *m,
-              uint32_t m0_inv, uint32_t *result);
+void mont_mul(const sigverify_rsa_key_t *key, const sigverify_rsa_buffer_t *x,
+              const sigverify_rsa_buffer_t *y, sigverify_rsa_buffer_t *result);
 
 /**
- * Computes the modular exponentiation of an integer.
+ * Computes the modular exponentiation of an RSA signature.
  *
- * Given sig, e, m, and m', this function computes sig^e mod m using Montgomery
- * multiplication, where
- * - sig and m are integers with kRsaNumWords base b digits,
- * - e is the exponent (3 or 65537),
- * - m' = -m^-1 mod b,
- * - b is 2^32.
+ * Given an RSA public key and sig, this function computes sig^e mod n using
+ * Montgomery multiplication, where
+ * - sig is an RSA signature,
+ * - e and n are the exponent and the modulus of the key, respectively.
  *
- * @param sig A `kRsaNumWords` long buffer, little-endian.
- * @param exponent Exponent to use for signature verification.
- * @param m A `kRsaNumWords` long buffer, little-endian.
- * @param m0_inv Negative of the multiplicative inverse of m modulo b.
- * @param[out] result A `kRsaNumWords` long buffer, little-endian.
+ * @param key An RSA public key.
+ * @param sig Buffer that holds the signature, little-endian.
+ * @param result Buffer to write the result to, little-endian.
  * @return True if successful, false otherwise.
  */
-// TODO(#22): Update this after we decide on a key storage format.
 // FIXME: Error codes are still under discussion, update after we reach a
 // decision.
-bool mod_exp(const uint32_t *sig, rsa_verify_exponent_t e, const uint32_t *m,
-             uint32_t m0_inv, uint32_t *result);
+bool sigverify_mod_exp_ibex(const sigverify_rsa_key_t *key,
+                            const sigverify_rsa_buffer_t *sig,
+                            sigverify_rsa_buffer_t *result);
 
 #ifdef __cplusplus
 }  // extern "C"
---
+++ sw/device/silicon_creator/mask_rom/sig_verify_keys.h
@@ -0,0 +1,85 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_MASK_ROM_SIG_VERIFY_KEYS_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_MASK_ROM_SIG_VERIFY_KEYS_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+enum {
+  /**
+   * Length of an RSA-3072 modulus or signature in bits.
+   */
+  kSigVerifyRsaNumBits = 3072,
+  /**
+   * Length of an RSA-3072 modulus or signature in words.
+   */
+  kSigVerifyRsaNumWords = kSigVerifyRsaNumBits / (sizeof(uint32_t) * 8),
+  /**
+   * Number of RSA public keys.
+   */
+  kSigVerifyNumRsaKeys = 2,
+};
+
+/**
+ * A type that holds `kSigVerifyRsaNumWords` words.
+ *
+ * This can be used for RSA-3072 moduli, signatures, and intermediate values
+ * during modular exponentiation.
+ */
+typedef struct sigverify_rsa_buffer {
+  uint32_t data[kSigVerifyRsaNumWords];
+} sigverify_rsa_buffer_t;
+
+/**
+ * An RSA public key.
+ *
+ * Note: Defined here to be able to use in tests.
+ */
+// TODO(#22): Additional field(s) for key types/domains (or can store along with
+// exponent).
+typedef struct sigverify_rsa_key {
+  /**
+   * Modulus, a `kSigVerifyRsaNumWords` base 2^32 digit integer, little-endian.
+   */
+  sigverify_rsa_buffer_t n;
+  /**
+   * Negative of the multiplicative inverse of n modulo 2^32.
+   */
+  uint32_t n0_inv;
+  /**
+   * Exponent.
+   */
+  uint32_t exponent;
+} sigverify_rsa_key_t;
+
+/**
+ * Public keys for signature verification.
+ *
+ * Note: Declared here to be able to use in tests.
+ */
+extern const sigverify_rsa_key_t kSigVerifyRsaKeys[kSigVerifyNumRsaKeys];
+
+/**
+ * Gets the ID of an RSA public key.
+ *
+ * ID of a key is the least significant byte of its modulus.
+ * Callers must make sure that `key` is valid before calling this function.
+ *
+ * @param key An RSA public key.
+ * @return ID of the key.
+ */
+inline uint32_t sigverify_rsa_key_id_get(const sigverify_rsa_key_t *key) {
+  return key->n.data[0];
+}
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_MASK_ROM_SIG_VERIFY_KEYS_H_
