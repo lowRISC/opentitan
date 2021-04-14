ee2e3820a938637a791e94170ed01cfceca51b85
Rafal Kapuscik <rkapuscik@antmicro.com>
2021-04-14 16:03:31 +0200

    Add formatting job to CI
    Signed-off-by: Rafal Kapuscik <rkapuscik@antmicro.com>

---
+++ ci/scripts/verible-format.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# A wrapper around Verible format, used for CI.
+
+set -e
+
+util/verible-format.sh || {
+  echo -n "##vso[task.logissue type=error]"
+  echo "Verilog format with Verible failed. Run 'util/verible-format.sh' to check and fix all errors."
+  echo "This flow is currently experimental and failures can be ignored."
+  exit 1
+}
