3cf2c9073ca75a1ea65cdde09ea7bcbd5708b61c
Alphan Ulusoy <alphan@google.com>
2021-04-19 08:27:25 -0400

    Improve ROM_EXT image signer CLI and some cleanups
    This change:
    - Removes
      - `config.hjson` along with related code and dependencies,
      - `sw/host/rom_ext_image_tools/signer/dev` folder,
      - Most of the methods from the image struct, essentially making it a
    thin wrapper around a `Vec<[u8]>`,
    - Exposes input image, private key, and output image paths as command
    line arguments (required for signing an image with multiple keys during
    build),
    - Adds `thiserror` and `anyhow` dependencies to remove `expect`s,
    - Modifies the ROM_EXT manifest generator rust template to generate
    structs instead of individual constants, which enables some basic checks
    in `Image::set_manifest_field`.
    
    CLI is pretty basic because the functions in will eventually be
    integrated into the opentitan tool.
    
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

--- sw/host/rom_ext_image_tools/signer/src/main.rs
+++ sw/host/rom_ext_image_tools/signer/src/main.rs
@@ -9,91 +9,186 @@
 use std::env;
 use std::fs;
 use std::path::Path;
+use std::path::PathBuf;
 
-use rom_ext_config::parser::ParsedConfig;
-use rom_ext_image::image::RawImage;
+use rom_ext_image::image::Image;
+use rom_ext_image::manifest;
 
 use mundane::hash::Sha256;
 use mundane::public::rsa::RsaPkcs1v15;
-use mundane::public::rsa::RsaPrivKey;
-use mundane::public::rsa::RsaSignature;
 use mundane::public::rsa::B3072;
 use mundane::public::DerPrivateKey;
 use mundane::public::Signature;
 
-fn main() {
-    let arg: String = env::args().nth(1).expect("Config path is missing");
+use anyhow::bail;
+use anyhow::ensure;
+use anyhow::Context;
+use anyhow::Result;
 
-    let config_path = Path::new(&arg);
+// Type aliases for convenience.
+type ImageSignature =
+    mundane::public::rsa::RsaSignature<B3072, RsaPkcs1v15, Sha256>;
+type PrivateKey = mundane::public::rsa::RsaPrivKey<B3072>;
 
-    // Parse the config.
-    let config = ParsedConfig::new(&config_path);
-
-    // Read raw binary.
-    let mut image = RawImage::new(&config.input_files.image_path);
-
-    // Get the private key used for signature generation. It is also used to
-    // extract key public exponent and modulus.
-    let private_key_der = fs::read(&config.input_files.private_key_der_path)
-        .expect("Failed to read the image!");
-
-    // Update "signed" manifest fields.
-    image.update_static_fields(&config);
-
-    // Convert ASN.1 DER private key into Mundane RsaPrivKey.
-    let private_key = RsaPrivKey::parse_from_der(&private_key_der)
-        .expect("Failed to parse private key!");
-
-    let mut exponent = private_key.public_exponent_be();
-    // Encode public key components in little-endian byte order.
-    exponent.reverse();
-    image.update_exponent_field(&exponent);
-
-    let mut modulus = private_key.public_modulus_be();
-    // Encode public key components in little-endian byte order.
-    modulus.reverse();
-    image.update_modulus_field(&modulus);
-
-    // Produce the signature from concatenated system_state_value,
-    // device_usage_value and the portion of the "signed" portion of the image.
-    image.update_timestamp_field();
-    let image_sign_data = image.data_to_sign();
-    let device_usage_value =
-        &device_usage_value(&config.input_files.usage_constraints_path);
-    let system_state_value =
-        &system_state_value(&config.input_files.system_state_value_path);
-
-    let mut message_to_sign = Vec::<u8>::new();
-    message_to_sign.extend_from_slice(system_state_value);
-    message_to_sign.extend_from_slice(device_usage_value);
-    message_to_sign.extend_from_slice(image_sign_data);
-
-    let signature = RsaSignature::<B3072, RsaPkcs1v15, Sha256>::sign(
-        &private_key,
-        &message_to_sign,
-    )
-    .expect("Failed to sign!");
-
-    image.update_signature_field(&signature.bytes());
-
-    // The whole image has been updated and signed, write the result to disk.
-    image.write_file();
+// TODO: Remove this struct when this functionality is integrated into `opentitantool`.
+struct Args {
+    input: PathBuf,
+    priv_key: PathBuf,
+    output: PathBuf,
 }
 
-/// Generates the device usage value.
-///
-/// This value is extrapolated from the ROM_EXT manifest usage_constraints
-/// field, and does not reside in the ROM_EXT manifest directly.
-pub fn device_usage_value(path: &Path) -> Vec<u8> {
-    let _usage_constraints =
-        fs::read(path).expect("Failed to read usage constraints!");
-
-    // TODO: generate the device_usage_value from usage_constraints.
-    //       meanwhile use a "dummy" hard-coded vector.
-    vec![0xA5; 1024]
+impl Args {
+    pub fn new(args: env::ArgsOs) -> Result<Args> {
+        let args = args.skip(1).collect::<Vec<_>>();
+        match args.as_slice() {
+            [input, priv_key, output] => Ok(Args {
+                    input: PathBuf::from(input),
+                    priv_key: PathBuf::from(priv_key),
+                    output: PathBuf::from(output),
+                }),
+            args => bail!("Expected exactly 3 positional arguments: input, private key, and output, got: {}.", args.len()),
+        }
+    }
 }
 
-/// Obtains the system state value from the binary on disk.
-pub fn system_state_value(path: &Path) -> Vec<u8> {
-    fs::read(path).expect("Failed to read system state value!")
+/// Parses an unsigned big-endian hex string into a little-endian byte vector.
+fn parse_hex_str(hex_str: &str) -> Result<Vec<u8>> {
+    ensure!(
+        hex_str.starts_with("0x")
+            && hex_str.len() > 2
+            && hex_str.len() % 2 == 0,
+        "Invalid hex string: {}",
+        hex_str
+    );
+    let bytes = (2..hex_str.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&hex_str[i..i + 2], 16))
+        .rev()
+        .collect::<Result<_, _>>()?;
+    Ok(bytes)
+}
+
+/// Updates the manifest of an image.
+// TODO: This function must use a public key.
+fn update_image_manifest(
+    image: &mut Image,
+    key: impl AsRef<Path>,
+) -> Result<()> {
+    let key = fs::read(&key).with_context(|| {
+        format!("Failed to read the key from `{}`.", key.as_ref().display())
+    })?;
+    let key =
+        PrivateKey::parse_from_der(&key).context("Failed to parse the key.")?;
+
+    image.set_manifest_field(
+        &manifest::ROM_EXT_SIGNATURE_KEY_PUBLIC_EXPONENT,
+        key.public_exponent_be().iter().rev().cloned(),
+    )?;
+    image.set_manifest_field(
+        &manifest::ROM_EXT_SIGNATURE_KEY_MODULUS,
+        key.public_modulus_be().iter().rev().cloned(),
+    )?;
+    image.set_manifest_field(
+        &manifest::ROM_EXT_MANIFEST_IDENTIFIER,
+        parse_hex_str("0x4552544f")?,
+    )?;
+
+    image.set_manifest_field(
+        &manifest::ROM_EXT_IMAGE_VERSION,
+        // TODO: Use into_iter once it's stabilized (for all usages in this file).
+        std::array::IntoIter::new(0_u32.to_le_bytes()),
+    )?;
+
+    // TODO: Do we need these fields?
+    let extensions = [
+        (
+            &manifest::ROM_EXT_EXTENSION0_OFFSET,
+            &manifest::ROM_EXT_EXTENSION0_CHECKSUM,
+        ),
+        (
+            &manifest::ROM_EXT_EXTENSION1_OFFSET,
+            &manifest::ROM_EXT_EXTENSION1_CHECKSUM,
+        ),
+        (
+            &manifest::ROM_EXT_EXTENSION2_OFFSET,
+            &manifest::ROM_EXT_EXTENSION2_CHECKSUM,
+        ),
+        (
+            &manifest::ROM_EXT_EXTENSION3_OFFSET,
+            &manifest::ROM_EXT_EXTENSION3_CHECKSUM,
+        ),
+    ];
+    for fields in &extensions {
+        image.set_manifest_field(
+            fields.0,
+            std::array::IntoIter::new(0_u32.to_le_bytes()),
+        )?;
+        image.set_manifest_field(
+            fields.1,
+            std::array::IntoIter::new(0_u32.to_le_bytes()),
+        )?;
+    }
+    image.set_manifest_field(
+        &manifest::ROM_EXT_USAGE_CONSTRAINTS,
+        std::array::IntoIter::new(
+            [0; manifest::ROM_EXT_USAGE_CONSTRAINTS.size_bytes],
+        ),
+    )?;
+    image.set_manifest_field(
+        &manifest::ROM_EXT_PERIPHERAL_LOCKDOWN_INFO,
+        std::array::IntoIter::new(
+            [0; manifest::ROM_EXT_PERIPHERAL_LOCKDOWN_INFO.size_bytes],
+        ),
+    )?;
+    image.set_manifest_field(
+        &manifest::ROM_EXT_IMAGE_TIMESTAMP,
+        std::array::IntoIter::new(0_u64.to_le_bytes()),
+    )?;
+
+    Ok(())
+}
+
+/// Calculates the signature for the signed portion of an image.
+fn calculate_image_signature(
+    image: &Image,
+    key: impl AsRef<Path>,
+) -> Result<ImageSignature> {
+    let key = fs::read(&key).with_context(|| {
+        format!("Failed to read key from `{}`.", key.as_ref().display())
+    })?;
+    let key =
+        PrivateKey::parse_from_der(&key).context("Failed to parse the key.")?;
+    let sig = ImageSignature::sign(&key, image.signed_bytes())
+        .context("Failed to sign the image.")?;
+    Ok(sig)
+}
+
+/// Updates the signature of an image.
+fn update_image_signature(
+    image: &mut Image,
+    signature: ImageSignature,
+) -> Result<()> {
+    image.set_manifest_field(
+        &manifest::ROM_EXT_IMAGE_SIGNATURE,
+        signature.bytes().iter().rev().cloned(),
+    )?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args = Args::new(env::args_os())?;
+    let mut image = Image::from(fs::read(&args.input).with_context(|| {
+        format!("Failed to read the image from {}", args.input.display())
+    })?);
+
+    // TODO for a future refactor into opentitantool: These functions probably should belong to a
+    // Signer struct and should move into the image crate.
+    // TODO: This must use the public key.
+    update_image_manifest(&mut image, &args.priv_key)?;
+    let sig = calculate_image_signature(&image, &args.priv_key)?;
+    update_image_signature(&mut image, sig)?;
+    fs::write(&args.output, image).with_context(|| {
+        format!("Failed to write the image to {}", args.output.display())
+    })?;
+    Ok(())
 }
