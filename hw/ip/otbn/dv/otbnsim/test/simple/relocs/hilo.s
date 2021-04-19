72d723eb08b105f9749d64f32d8ea8812613f4ca
Rupert Swarbrick <rswarbrick@lowrisc.org>
2021-04-19 16:46:51 +0100

    [otbn] Add a test for %lo(..) and %hi(..) support
    It turns out that this works "for free" for instructions like
    ADDI (which otbn-as can generate with .insn lines). Add a test to make
    sure nothing comes unstuck.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

---
+++ hw/ip/otbn/dv/otbnsim/test/simple/relocs/hilo.s
@@ -0,0 +1,26 @@
+/* Copyright lowRISC contributors. */
+/* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+/*
+    Code that checks we correctly handle the %lo(...) and %hi(...) relocs
+*/
+
+.data
+foo:
+  .word 0x1234
+bar:
+  .word 0x5678
+
+.text
+  addi  x2, x0, %lo(foo)
+  addi  x3, x0, %lo(bar)
+  lw    x4, 0(x2)
+  lw    x5, 0(x3)
+
+  /* No matter what addresses foo and bar have, we can be pretty
+     certain they're representable in 12 bits (if not, they wouldn't
+     fit in DMEM). */
+  lui   x6, %hi(bar)
+
+  ecall
