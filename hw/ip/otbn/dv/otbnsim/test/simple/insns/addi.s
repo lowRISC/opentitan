97d26bdf8596babda637ef6f0a1b1fc04d5f4e67
Rupert Swarbrick <rswarbrick@lowrisc.org>
2021-04-23 15:35:29 +0100

    [otbn,util] Fix block comment parsing in otbn-as
    For some reason, I'd put effort into passing comments through to the
    destination when translating from OTBN assembly to RISC-V assembly.
    Other aspects of the code get mangled, so this isn't really worth
    doing and, worse, it also made a mess of instruction parsing. So
    something like this:
    
        addi x0, x0, 1 /* foo */
    
    would try to match ADDI args with string "x0, x0, 1 /* foo */" (not
    stripping out the comment).
    
    Stop doing the silly thing! Also, reformat the "addi" ISS test to use
    the syntax that was breaking things. This should avoid things coming
    unstuck in future (without needing to put effort into a specific
    "assembler testsuite").
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/otbn/dv/otbnsim/test/simple/insns/addi.s
+++ hw/ip/otbn/dv/otbnsim/test/simple/insns/addi.s
@@ -2,21 +2,22 @@
 /* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
 /* SPDX-License-Identifier: Apache-2.0 */
 
+/*
+    Simple tests of the addi instruction
+
+    This test also uses a comment syntax that broke the parser in a
+    previous version of otbn-as: if editing it, keep the block
+    comments on the same lines as instructions to keep that test in
+    place.
+
+*/
+
   addi    x10, x0, 1
 
-  /* x2 = 0 + 1 = 1 */
-  addi    x2, x0, 1
-
-  /* x3 = 1 + 1 = 2 */
-  addi    x3, x2, 1
-
-  /* x4 = 0 + (-1) = 0xffffffff */
-  addi    x4, x0, -1
-
-  /* x5 = 2 + (-1) = 1 */
-  addi    x5, x3, -1
-
-  /* x6 = -1 + 10 = 9 */
-  addi    x6, x4, 10
+  addi    x2, x0, 1   /* x2 = 0 + 1 = 1 */
+  addi    x3, x2, 1   /* x3 = 1 + 1 = 2 */
+  addi    x4, x0, -1  /* x4 = 0 + (-1) = 0xffffffff */
+  addi    x5, x3, -1  /* x5 = 2 + (-1) = 1 */
+  addi    x6, x4, 10  /* x6 = -1 + 10 = 9 */
 
   ecall
