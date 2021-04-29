f856905a80e48f5412b2a767dc3867f604f2c454
Udi Jonnalagadda <udij@google.com>
2021-04-28 19:42:07 -0700

    [dv/sram] fix stress tests
    this PR updates the stress_all and stress_all_with_rand_reset tests:
    
    - don't run sram_ctrl_init() for each test sequence, as back to back
      initializations can cause the initialization timing logic in the scb
      to become confused and incorrectly predict the CSR value.
      We only need to run sram_ctrl_init() at the beginning of the
      stress_all vseq.
    
    - explicitly prevent access to alert_test CSR from doing anything,
      preventing failures in stress_all_with_rand_reset
    
    - remove stress_all test from sim_cfg.hjson, as we don't particularly
      need the {variant} naming scheme
    
    Signed-off-by: Udi Jonnalagadda <udij@google.com>

--- hw/ip/sram_ctrl/dv/env/seq_lib/sram_ctrl_stress_all_vseq.sv
+++ hw/ip/sram_ctrl/dv/env/seq_lib/sram_ctrl_stress_all_vseq.sv
@@ -43,6 +43,7 @@ class sram_ctrl_stress_all_vseq extends sram_ctrl_base_vseq;
       sram_vseq.set_sequencer(p_sequencer);
       `DV_CHECK_RANDOMIZE_FATAL(sram_vseq)
       sram_vseq.cfg.cfg_sram_zero_delays(cur_vseq_name == "sram_ctrl_stress_pipeline_vseq");
+      sram_vseq.do_sram_ctrl_init = 0;
       `uvm_info(`gfn, $sformatf("iteration[%0d]: starting %0s", i, cur_vseq_name), UVM_LOW)
       sram_vseq.start(p_sequencer);
     end
