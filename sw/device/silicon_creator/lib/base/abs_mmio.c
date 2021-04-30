d2b656f3e43b2508e0fb2cd3e09ed24a7271100e
Miguel Osorio <miguelosorio@google.com>
2021-04-29 23:21:17 -0700

    [sw/silicon_creator] Introduce the abs_mmio module
    This commit introduces the abs_mmio module, which removes the need for
    `mmio_region_t` inside the mask_rom and switches to addresses encoded in
    `uint32_t`. This commit does not consider DIF changes. ROM drivers can
    now use `base_address + offset` calculations to access registers.
    
    ```cc
    abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CFG_REG_OFFSET, reg);
    ```
    
    Test cases now require the instantiation of the mock mmio handle inside
    the test class, for example:
    
    ```cc
    class HmacTest : public Test {
     protected:
      uint32_t base_ = TOP_EARLGREY_HMAC_BASE_ADDR;
      mask_rom_test::MockAbsMmio mmio_;  // This handle is required.
    };
    ```
    
    Expectations are similar to the dif mock mmio version, with the
    difference that the `mmio_` handle and the `base_` address need to be
    explicitly set inside the macro:
    
    ```cc
     EXPECT_MMIO_WRITE32(mmio_, base_ + HMAC_CFG_REG_OFFSET,
                          {
                              {HMAC_CFG_DIGEST_SWAP_BIT, false},
                              {HMAC_CFG_ENDIAN_SWAP_BIT, true},
                              {HMAC_CFG_SHA_EN_BIT, true},
                              {HMAC_CFG_HMAC_EN_BIT, false},
                          });
    ```
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

---
+++ sw/device/silicon_creator/lib/base/abs_mmio.c
@@ -0,0 +1,12 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/base/abs_mmio.h"
+
+// `extern` declarations to give the inline functions in the corresponding
+// header a link location.
+extern uint32_t abs_mmio_read8(uint32_t addr);
+extern void abs_mmio_write8(uint32_t addr, uint8_t value);
+extern uint32_t abs_mmio_read32(uint32_t addr);
+extern void abs_mmio_write32(uint32_t addr, uint32_t value);
