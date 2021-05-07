fbbfe3ad6d0ff309176026fa9928689d363c82a9
Cindy Chen <chencindy@google.com>
2021-05-07 13:49:44 -0700

    [dv/chip] remove shadow_reg error test
    This PR removes the shadow_reg error test in chip-level because:
    1. Shadow reg errors: update errors and storage errors are fully
    verified in block level testbenchs.
    2. The alert connections will be covered by top-level alert test.
    3. Maintain this extra alert test and backdoor probe errors requires
    additional effort, which I do not think is worth it.
    
    Signed-off-by: Cindy Chen <chencindy@google.com>

--- hw/top_earlgrey/dv/env/seq_lib/chip_vseq_list.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_vseq_list.sv
@@ -5,7 +5,6 @@
 `include "chip_base_vseq.sv"
 `include "chip_stub_cpu_base_vseq.sv"
 `include "chip_common_vseq.sv"
-`include "chip_shadow_reg_errors_vseq.sv"
 `include "chip_sw_base_vseq.sv"
 `include "chip_sw_uart_tx_rx_vseq.sv"
 `include "chip_sw_gpio_smoke_vseq.sv"
