1d80d60c7d6668b4e9c71bd7e2e6ca89d0c81ba7
Srikrishna Iyer <sriyer@google.com>
2021-05-11 12:39:44 -0700

    [sw] Implement prng for random testing
    This adds the ability to generate and test randomized payloads from SW,
    rather than test fixed payloads. Stimulus and hence, verification
    quality is better when HW is programmed and checked against with
    constrained random data. This will be mainly used in DV code where both,
    SW image and the HW can be probed to extract the data and perform
    checks.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

---
+++ sw/device/lib/testing/random.c
@@ -0,0 +1,36 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/testing/random.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "sw/device/lib/testing/check.h"
+
+/**
+ * The polynomial co-efficients used in the 32-bit LFSR implementation.
+ *
+ * This implementation matches the RTL design at `hw/ip/prim/rtl/prim_lfsr.sv`.
+ */
+const uint32_t kLfsrCoefficients = 0x80000057;
+
+uint32_t random_gen32(void) {
+  static uint32_t lfsr = 1;
+  bool lsb = lfsr & 0x1u;
+  lfsr >>= 1;
+  if (lsb) {
+    lfsr ^= kLfsrCoefficients;
+  }
+  return lfsr;
+}
+
+uint32_t random_gen32_range(uint32_t min, uint32_t max) {
+  CHECK(max >= min);
+  uint32_t range = max - min;
+  if (range == 0) {
+    return min;
+  }
+  return min + (random_gen32() % (range + 1));
+}
