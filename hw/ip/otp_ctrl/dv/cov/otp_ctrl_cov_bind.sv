b51e7d3faef8b5c52624c70a005a75e35c83380b
Cindy Chen <chencindy@opentitan.org>
2021-05-20 23:50:15 -0700

    [dv/otp_ctrl] collect some functional coverage through interface
    This PR collects OTP_CTRL I/O coverage via otp_cov_if.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

---
+++ hw/ip/otp_ctrl/dv/cov/otp_ctrl_cov_bind.sv
@@ -0,0 +1,18 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Binds OTP_CTRL functional coverage interaface to the top level OTP_CTRL module.
+module otp_ctrl_cov_bind;
+
+  bind otp_ctrl otp_ctrl_cov_if u_otp_ctrl_cov_if (
+    .clk_i            (clk_i),
+    .pwr_otp_o        (pwr_otp_o),
+    .lc_otp_program_i (lc_otp_program_i),
+    .lc_escalate_en_i (lc_escalate_en_i),
+    .flash_otp_key_i  (flash_otp_key_i),
+    .sram_otp_key_i   (sram_otp_key_i),
+    .otbn_otp_key_i   (otbn_otp_key_i)
+  );
+
+endmodule
