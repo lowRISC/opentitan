22bdcfcc2ede3e148414b92042a8905a227a8c4e
Cindy Chen <chencindy@opentitan.org>
2021-06-03 12:55:02 -0700

    [dv/otp_ctrl] align write_blank_err [PART1]
    The new write_blank_error will write to OTP memory until the
    write_blank_error is triggered. This partially written OTP memories can
    trigger check failures or init failure.
    This PR cleans up otp_dai_lock_vseq and avoid write_blank_err in these
    tests.
    
    For lc_program to issue valid values, this PR reuses the function from
    lc_ctrl dv environment. To avoid duplicated code, this PR moves the
    common pkg functions to a separate file, and packed it with a separate
    core file.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

--- hw/ip/lc_ctrl/dv/env/lc_ctrl_env_pkg.sv
+++ hw/ip/lc_ctrl/dv/env/lc_ctrl_env_pkg.sv
@@ -18,6 +18,7 @@ package lc_ctrl_env_pkg;
   import push_pull_agent_pkg::*;
   import alert_esc_agent_pkg::*;
   import jtag_riscv_agent_pkg::*;
+  import lc_ctrl_dv_utils_pkg::*;
 
   // macro includes
   `include "uvm_macros.svh"
@@ -85,28 +86,6 @@ package lc_ctrl_env_pkg;
     {Off, Off, Off, Off, Off, Off, Off, Off, Off, Off, On}
   };
 
-  // associative array cannot declare parameter here, so we used const instead
-  const dec_lc_state_e VALID_NEXT_STATES [dec_lc_state_e][$] = '{
-    DecLcStRma:     {DecLcStScrap},
-    DecLcStProdEnd: {DecLcStScrap},
-    DecLcStProd:    {DecLcStScrap, DecLcStRma},
-    DecLcStDev:     {DecLcStScrap, DecLcStRma},
-    DecLcStTestUnlocked3: {DecLcStScrap, DecLcStRma, DecLcStProdEnd, DecLcStProd, DecLcStDev},
-    DecLcStTestUnlocked2: {DecLcStScrap, DecLcStProdEnd, DecLcStProd, DecLcStDev,
-                           DecLcStTestLocked2},
-    DecLcStTestUnlocked1: {DecLcStScrap, DecLcStRma, DecLcStProdEnd, DecLcStProd, DecLcStDev,
-                           DecLcStTestLocked2, DecLcStTestLocked1},
-    DecLcStTestUnlocked0: {DecLcStScrap, DecLcStRma, DecLcStProdEnd, DecLcStProd, DecLcStDev,
-                           DecLcStTestLocked2, DecLcStTestLocked1, DecLcStTestLocked0},
-    DecLcStTestLocked2: {DecLcStScrap, DecLcStProdEnd, DecLcStProd,
-                         DecLcStDev, DecLcStTestUnlocked3},
-    DecLcStTestLocked1: {DecLcStScrap, DecLcStProdEnd, DecLcStProd, DecLcStDev,
-                         DecLcStTestUnlocked3, DecLcStTestUnlocked2},
-    DecLcStTestLocked0: {DecLcStScrap, DecLcStProdEnd, DecLcStProd, DecLcStDev,
-                         DecLcStTestUnlocked3, DecLcStTestUnlocked2, DecLcStTestUnlocked1},
-    DecLcStRaw: {DecLcStScrap, DecLcStTestUnlocked2, DecLcStTestUnlocked1, DecLcStTestUnlocked0}
-  };
-
   // types
   typedef enum bit [1:0] {
     LcPwrInitReq,
@@ -126,48 +105,6 @@ package lc_ctrl_env_pkg;
                            LcStTestLocked2, LcStTestLocked1, LcStTestLocked0, LcStRaw}) begin
       valid_state_for_trans = 1;
     end
-  endfunction
-
-  function automatic dec_lc_state_e dec_lc_state(lc_state_e curr_state);
-    case (curr_state)
-      LcStRaw:           return DecLcStRaw;
-      LcStTestUnlocked0: return DecLcStTestUnlocked0;
-      LcStTestLocked0:   return DecLcStTestLocked0;
-      LcStTestUnlocked1: return DecLcStTestUnlocked1;
-      LcStTestLocked1:   return DecLcStTestLocked1;
-      LcStTestUnlocked2: return DecLcStTestUnlocked2;
-      LcStTestLocked2:   return DecLcStTestLocked2;
-      LcStTestUnlocked3: return DecLcStTestUnlocked3;
-      LcStDev:           return DecLcStDev;
-      LcStProd:          return DecLcStProd;
-      LcStProdEnd:       return DecLcStProdEnd;
-      LcStRma:           return DecLcStRma;
-      LcStScrap:         return DecLcStScrap;
-      default: `uvm_fatal("lc_env_pkg", $sformatf("unknown lc_state 0x%0h", curr_state))
-    endcase
-  endfunction
-
-  function automatic int dec_lc_cnt(lc_cnt_e curr_cnt);
-    case (curr_cnt)
-      LcCnt0  : return 0;
-      LcCnt1  : return 1;
-      LcCnt2  : return 2;
-      LcCnt3  : return 3;
-      LcCnt4  : return 4;
-      LcCnt5  : return 5;
-      LcCnt6  : return 6;
-      LcCnt7  : return 7;
-      LcCnt8  : return 8;
-      LcCnt9  : return 9;
-      LcCnt10 : return 10;
-      LcCnt11 : return 11;
-      LcCnt12 : return 12;
-      LcCnt13 : return 13;
-      LcCnt14 : return 14;
-      LcCnt15 : return 15;
-      LcCnt16 : return 16;
-      default: `uvm_fatal("lc_env_pkg", $sformatf("unknown lc_cnt 0x%0h", curr_cnt))
-    endcase
   endfunction
 
   function automatic lc_ctrl_pkg::token_idx_e get_exp_token(dec_lc_state_e curr_state,
