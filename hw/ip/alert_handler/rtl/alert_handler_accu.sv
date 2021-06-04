0d13110cbf0de00882930d17a7f92d05c2a092c6
Michael Schaffner <msf@opentitan.org>
2021-06-04 14:00:38 -0700

    [alert_handler] Duplicate accumulation counters
    These counters are duplicated to harden the design against FI attacks.
    In case the counters have an inconsistent state, the escalation timers
    go into a terminal error state in which all countermeasures are asserted
    simultaneously.
    
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/alert_handler/rtl/alert_handler_accu.sv
+++ hw/ip/alert_handler/rtl/alert_handler_accu.sv
@@ -20,30 +20,52 @@ module alert_handler_accu import alert_pkg::*; (
   input                        class_trig_i, // increments the accu
   input        [AccuCntDw-1:0] thresh_i,     // escalation trigger threshold
   output logic [AccuCntDw-1:0] accu_cnt_o,   // output of current accu value
-  output logic                 accu_trig_o   // escalation trigger output
+  output logic                 accu_trig_o,  // escalation trigger output
+  output logic                 accu_fail_o   // asserted if the tandem accu counters are not equal
 );
 
-  logic trig_gated;
-  logic [AccuCntDw-1:0] accu_d, accu_q;
+  logic trig_gated_unbuf;
+  assign trig_gated_unbuf = class_trig_i & class_en_i;
 
-  assign trig_gated = class_trig_i & class_en_i;
+  // We employ two redundant counters to guard against FI attacks.
+  // If any of the two is glitched and the two counter states do not agree,
+  // the check_fail_o signal is asserted which will move the corresponding escalation
+  // FSM into a terminal error state where all escalation actions will be permanently asserted.
+  logic [1:0][AccuCntDw-1:0] accu_q;
+  for (genvar k = 0; k < 2; k++) begin : gen_double_accu
 
-  assign accu_d = (clr_i)                    ? '0            : // clear
-                  (trig_gated && !(&accu_q)) ? accu_q + 1'b1 : // saturate counter at maximum
-                                               accu_q;
+    logic trig_gated_buf, clr_buf;
+    logic [AccuCntDw-1:0] accu_d;
 
-  assign accu_trig_o = (accu_q >= thresh_i) & trig_gated;
+    // These size_only buffers are instantiated in order to prevent
+    // optimization / merging of the two counters.
+    prim_buf u_prim_buf_clr (
+      .in_i(clr_i),
+      .out_o(clr_buf)
+    );
 
-  assign accu_cnt_o = accu_q;
+    prim_buf u_prim_buf_trig (
+      .in_i(trig_gated_unbuf),
+      .out_o(trig_gated_buf)
+    );
 
-  always_ff @(posedge clk_i or negedge rst_ni) begin : p_regs
-    if (!rst_ni) begin
-      accu_q <= '0;
-    end else begin
-      accu_q <= accu_d;
-    end
+    assign accu_d = (clr_buf)                         ? '0               : // clear
+                    (trig_gated_buf && !(&accu_q[k])) ? accu_q[k] + 1'b1 : // saturate counter
+                                                        accu_q[k];
+
+    prim_flop #(
+      .Width(AccuCntDw)
+    ) u_prim_flop (
+      .clk_i,
+      .rst_ni,
+      .d_i(accu_d),
+      .q_o(accu_q[k])
+    );
   end
 
+  assign accu_cnt_o = accu_q[0];
+  assign accu_trig_o = (accu_q[0] >= thresh_i) & trig_gated_unbuf;
+  assign accu_fail_o = accu_q[0] != accu_q[1];
 
   ////////////////
   // Assertions //
@@ -51,5 +73,7 @@ module alert_handler_accu import alert_pkg::*; (
 
   `ASSERT(DisabledNoTrigFwd_A, !class_en_i |-> !accu_trig_o)
   `ASSERT(DisabledNoTrigBkwd_A, accu_trig_o |-> class_en_i)
+  `ASSERT(CheckFailFwd_A, accu_q[0] != accu_q[1] |-> accu_fail_o)
+  `ASSERT(CheckFailBkwd_A, accu_fail_o |-> accu_q[0] != accu_q[1])
 
 endmodule : alert_handler_accu
