4fa286e02afdfa817287af4a4c7503b61da26b42
Miguel Osorio <miguelosorio@google.com>
2021-06-07 13:36:13 -0700

    [sw/test] Update entropy_src expected values
    Update list of entropy_src expected values in dif_entropy_smoketest.c.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- sw/device/tests/dif/dif_entropy_smoketest.c
+++ sw/device/tests/dif/dif_entropy_smoketest.c
@@ -16,8 +16,8 @@ const test_config_t kTestConfig;
 const size_t kEntropyDataNumWords = 12;
 
 const uint32_t kExpectedEntropyData[] = {
-    0x65585497, 0xac95d5b1, 0xb2741ebf, 0x055cb180, 0x114d19be, 0x9f27b7f7,
-    0x9fbe250d, 0x5ae130f0, 0xf9a679a2, 0x1a4af3e5, 0xa436f52f, 0x613e0635,
+    0xa8f49c0d, 0x148ca619, 0xd1818b93, 0x25f2397d, 0x32955611, 0x0aca4b8e,
+    0xc0956655, 0x80735507, 0x4cf2b852, 0x97e50e09, 0x39649525, 0x6a2795f0,
 };
 
 bool test_main() {
@@ -51,14 +51,15 @@ bool test_main() {
   CHECK(dif_entropy_configure(&entropy, config) == kDifEntropyOk);
 
   uint32_t entropy_data[kEntropyDataNumWords];
+  uint32_t result = 0;
   for (uint32_t i = 0; i < kEntropyDataNumWords; ++i) {
     // wait for entropy to become available
     while (dif_entropy_read(&entropy, &entropy_data[i]) != kDifEntropyOk)
       ;
-    LOG_INFO("received %x, expectecd %x", entropy_data[i],
+    LOG_INFO("received 0x%x, expected 0x%x", entropy_data[i],
              kExpectedEntropyData[i]);
-    CHECK(entropy_data[i] == kExpectedEntropyData[i]);
+    result |= entropy_data[i] ^ kExpectedEntropyData[i];
   }
-
+  CHECK(result == 0);
   return true;
 }
