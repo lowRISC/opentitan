78d9cdad00668df19f025faa979c5b6463d327b8
Michael Munday <mike.munday@lowrisc.org>
2021-06-10 16:07:04 +0100

    [mask_rom, pinmux] Add Pin Multiplexer driver for mask ROM
    Add a simple pinmux driver for the mask ROM to use. Uses the
    `abs_mmio` library for register accesses.
    
    Right now the mask ROM is only using two pins: one for the UART RX
    and one for the UART TX. The number of pins the mask ROM needs to
    configure will grow however so the library has been designed to
    make it trivial to add new pin mappings. The configuration the mask
    ROM needs is static so we do all of the work in a single function:
    `pinmux_init`. Pad attributes may need to be set dynamically so
    those will be handled later.
    
    The driver itself is table driven which makes it easy to see which
    pins are mapped to which peripheral inputs and outputs. In the
    future we may want to revisit this decision to reduce code size.
    
    Signed-off-by: Michael Munday <mike.munday@lowrisc.org>

---
+++ sw/device/silicon_creator/lib/drivers/pinmux.c
@@ -0,0 +1,69 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/drivers/pinmux.h"
+
+#include "sw/device/lib/base/memory.h"
+#include "sw/device/silicon_creator/lib/base/abs_mmio.h"
+
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+#include "pinmux_regs.h"  // Generated.
+
+/**
+ * A peripheral input and MIO pad to link it to.
+ */
+typedef struct pinmux_input {
+  top_earlgrey_pinmux_peripheral_in_t periph;
+  top_earlgrey_pinmux_insel_t pad;
+} pinmux_input_t;
+
+/**
+ * Peripheral input to MIO pad mappings.
+ */
+static const pinmux_input_t kPinmuxInputs[] = {
+    /**
+     * UART
+     */
+    {.periph = kTopEarlgreyPinmuxPeripheralInUart0Rx,
+     .pad = kTopEarlgreyPinmuxInselIoc10},
+};
+
+/**
+ * An MIO pad and a peripheral output to link it to.
+ */
+typedef struct pinmux_output {
+  top_earlgrey_pinmux_mio_out_t pad;
+  top_earlgrey_pinmux_outsel_t periph;
+} pinmux_output_t;
+
+/**
+ * MIO pad to peripheral output mappings.
+ */
+static const pinmux_output_t kPinmuxOutputs[] = {
+    /**
+     * UART
+     */
+    {.pad = kTopEarlgreyPinmuxMioOutIoc11,
+     .periph = kTopEarlgreyPinmuxOutselUart0Tx},
+};
+
+void pinmux_init(void) {
+  // Set the input pad for each specified peripheral input.
+  const uint32_t kInputBase =
+      TOP_EARLGREY_PINMUX_AON_BASE_ADDR + PINMUX_MIO_PERIPH_INSEL_0_REG_OFFSET;
+  for (uint32_t i = 0; i < ARRAYSIZE(kPinmuxInputs); ++i) {
+    uint32_t reg = kPinmuxInputs[i].periph * sizeof(uint32_t);
+    uint32_t val = kPinmuxInputs[i].pad;
+    abs_mmio_write32(kInputBase + reg, val);
+  }
+
+  // Set the peripheral output for each specified output pad.
+  const uint32_t kOutputBase =
+      TOP_EARLGREY_PINMUX_AON_BASE_ADDR + PINMUX_MIO_OUTSEL_0_REG_OFFSET;
+  for (uint32_t i = 0; i < ARRAYSIZE(kPinmuxOutputs); ++i) {
+    uint32_t reg = kPinmuxOutputs[i].pad * sizeof(uint32_t);
+    uint32_t val = kPinmuxOutputs[i].periph;
+    abs_mmio_write32(kOutputBase + reg, val);
+  }
+}
