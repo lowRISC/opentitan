8bb050243ba4c9edf448a99d456ba2bb476dde68
Michael Munday <mike.munday@lowrisc.org>
2021-06-10 16:07:04 +0100

    [mask_rom, pinmux] Add Pin Multiplexer driver for mask ROM
    Add a simple pinmux driver for the mask ROM to use. Uses the
    `abs_mmio` library for register accesses.
    
    Right now the mask ROM is only using two pins: one for the UART RX
    and one for the UART TX. The number of pins the mask ROM needs to
    configure will grow however so the library has been designed to
    make it trivial to add new pin mappings. The configuration the mask
    ROM needs is static so we do all of the work in a single function:
    `pinmux_init`. Pad attributes may need to be set dynamically so
    those will be handled later.
    
    The driver itself is table driven which makes it easy to see which
    pins are mapped to which peripheral inputs and outputs. In the
    future we may want to revisit this decision to reduce code size.
    
    Signed-off-by: Michael Munday <mike.munday@lowrisc.org>

---
+++ sw/device/silicon_creator/lib/drivers/pinmux.h
@@ -0,0 +1,30 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_DRIVERS_PINMUX_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_DRIVERS_PINMUX_H_
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Driver for the Pin Multiplexer (pinmux).
+ *
+ * The pinmux connects peripheral input and output signals to the Padring
+ * MIO pad input and output signals.
+ */
+
+/**
+ * Initialize the pinmux with the configuration required for the mask ROM.
+ */
+void pinmux_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_DRIVERS_PINMUX_H_
---
+++ sw/device/silicon_creator/lib/drivers/pinmux_unittest.cc
@@ -0,0 +1,74 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/drivers/pinmux.h"
+
+#include "gtest/gtest.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/silicon_creator/lib/base/mock_abs_mmio.h"
+
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+#include "pinmux_regs.h"  // Generated.
+
+namespace pinmux_unittest {
+namespace {
+class PinmuxTest : public mask_rom_test::MaskRomTest {
+ protected:
+  uint32_t base_ = TOP_EARLGREY_PINMUX_AON_BASE_ADDR;
+  mask_rom_test::MockAbsMmio mmio_;
+};
+
+class InitTest : public PinmuxTest {
+ protected:
+  /**
+   * Set to track which peripheral inputs have already been configured.
+   */
+  std::set<top_earlgrey_pinmux_peripheral_in_t> configured_in_;
+
+  /**
+   * Register the configuration of the input peripheral in the tracking
+   * set and return its computed address.
+   *
+   * Triggers a test failure if the input has already been registered.
+   */
+  uint32_t RegIn(top_earlgrey_pinmux_peripheral_in_t index) {
+    EXPECT_TRUE(index >= 0 && index < kTopEarlgreyPinmuxPeripheralInLast);
+    EXPECT_TRUE(configured_in_.insert(index).second);
+    return base_ + PINMUX_MIO_PERIPH_INSEL_0_REG_OFFSET +
+           static_cast<uint32_t>(index) * sizeof(uint32_t);
+  }
+
+  /**
+   * Set to track which MIO outputs have already been configured.
+   */
+  std::set<top_earlgrey_pinmux_mio_out_t> configured_out_;
+
+  /**
+   * Register the configuration of the MIO output in the tracking
+   * set and return its computed address.
+   *
+   * Triggers a test failure if the input has already been registered.
+   */
+  uint32_t RegOut(top_earlgrey_pinmux_mio_out_t index) {
+    EXPECT_TRUE(index >= 0 && index < kTopEarlgreyPinmuxMioOutLast);
+    EXPECT_TRUE(configured_out_.insert(index).second);
+    return base_ + PINMUX_MIO_OUTSEL_0_REG_OFFSET +
+           static_cast<uint32_t>(index) * sizeof(uint32_t);
+  };
+};
+
+TEST_F(InitTest, Initialize) {
+  // The inputs that will be configured.
+  EXPECT_ABS_WRITE32(mmio_, RegIn(kTopEarlgreyPinmuxPeripheralInUart0Rx),
+                     kTopEarlgreyPinmuxInselIoc10);
+
+  // The outputs that will be configured.
+  EXPECT_ABS_WRITE32(mmio_, RegOut(kTopEarlgreyPinmuxMioOutIoc11),
+                     kTopEarlgreyPinmuxOutselUart0Tx);
+
+  pinmux_init();
+}
+
+}  // namespace
+}  // namespace pinmux_unittest
