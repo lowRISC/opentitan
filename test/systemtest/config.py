5a774db849658a07a0b1709b0a177c4ec2bee8a8
Chris Frantz <cfrantz@google.com>
2021-06-11 15:39:56 -0700

    [mask_rom] Add alert handler test automation.
    1. Add the alert handler's functest to systemtest and dvsim.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- test/systemtest/config.py
+++ test/systemtest/config.py
@@ -126,4 +126,12 @@ TEST_APPS_SELFCHECKING = [
         "name": "sw_silicon_creator_lib_driver_uart_functest",
         "test_dir": "sw/device/silicon_creator/testing",
     },
+    {
+        "name": "sw_silicon_creator_lib_driver_alert_functest",
+        "test_dir": "sw/device/silicon_creator/testing",
+        # TODO(lowRISC/opentitan#6965) This test resets the chip and appears to
+        # cause a test failure on FPGA boards.  Restrict this test to
+        # verilator for now.
+        "targets": ["sim_verilator"],
+    },
 ]
