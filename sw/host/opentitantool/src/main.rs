b333fad04fd279c762d87719878a459a4e12a927
Chris Frantz <cfrantz@google.com>
2021-06-11 14:17:39 -0700

    Base opentitantool application.
    1. Establish the basic `opentitantool` framework.
    2. Create a procedural macro for deriving `CommandDispatch` for interior
    nodes in the command hierarchy.
    3. Create some sample `hello` commands in `hello.rs`.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ sw/host/opentitantool/src/main.rs
@@ -0,0 +1,44 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::Result;
+use log::LevelFilter;
+use structopt::StructOpt;
+
+mod backend;
+mod command;
+use opentitanlib::app::command::CommandDispatch;
+
+#[derive(Debug, StructOpt, CommandDispatch)]
+enum RootCommandHierarchy {
+    // Flattened because `Greetings` is a subcommand hierarchy.
+    #[structopt(flatten)]
+    Greetings(command::hello::Greetings),
+}
+
+#[derive(Debug, StructOpt)]
+struct Opts {
+    #[structopt(long, default_value = "off")]
+    logging: LevelFilter,
+
+    #[structopt(flatten)]
+    backend_opts: backend::BackendOpts,
+
+    #[structopt(subcommand)]
+    command: RootCommandHierarchy,
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::from_args();
+    env_logger::Builder::from_default_env()
+        .filter(None, opts.logging)
+        .init();
+
+    let mut interface = backend::create(&opts.backend_opts)?;
+
+    if let Some(value) = opts.command.run(&mut *interface)? {
+        println!("{}", serde_json::to_string_pretty(&value)?);
+    }
+    Ok(())
+}
