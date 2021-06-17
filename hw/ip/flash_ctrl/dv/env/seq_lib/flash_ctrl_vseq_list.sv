6d54629cd6e745aa19093daa82dd967dbc48237e
Srikrishna Iyer <sriyer@google.com>
2021-06-17 00:52:43 -0700

    [dv, flash_ctrl] Split flash_ctrl_rand_ops_vseq
    This is a minor enhancement that "renames" the
    `flash_ctrl_rand_ops_vseq` to `flash_ctrl_rand_ops_base_vseq` which is
    not run directly, but extensions of it that configure the randomization
    knobs are. It adds the `flash_ctrl_rand_ops_vseq` which we run as a
    test.
    
    The smoke test is now made to extend from the base rand ops class.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_vseq_list.sv
+++ hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_vseq_list.sv
@@ -4,5 +4,6 @@
 
 `include "flash_ctrl_base_vseq.sv"
 `include "flash_ctrl_common_vseq.sv"
-`include "flash_ctrl_rand_ops_vseq.sv"
+`include "flash_ctrl_rand_ops_base_vseq.sv"
 `include "flash_ctrl_smoke_vseq.sv"
+`include "flash_ctrl_rand_ops_vseq.sv"
