6f9bb43399a34d0d2ded42196b20244088697c3f
Michael Munday <mike.munday@lowrisc.org>
2021-06-17 09:29:37 +0100

    [sw/silicon_creator] Add driver for retention SRAM scrambling
    Adds a simple API to allow silicon creator code to request a new
    scrambling key for the retention SRAM and initialize it.
    
    Signed-off-by: Michael Munday <mike.munday@lowrisc.org>

--- sw/device/silicon_creator/lib/drivers/retention_sram.h
+++ sw/device/silicon_creator/lib/drivers/retention_sram.h
@@ -6,6 +6,7 @@
 #define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_DRIVERS_RETENTION_SRAM_H_
 
 #include "sw/device/lib/base/macros.h"
+#include "sw/device/silicon_creator/lib/error.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -76,6 +77,20 @@ volatile retention_sram_t *retention_sram_get(void);
  * Clear the retention SRAM by setting every word to 0.
  */
 void retention_sram_clear(void);
+
+/**
+ * Start scrambling the retention SRAM.
+ *
+ * Requests a new scrambling key for the retention SRAM. This operation
+ * will wipe all of the data in the retention SRAM. The retention SRAM
+ * will then be initialized to undefined values.
+ *
+ * The scrambling operation takes time and accesses to retention SRAM
+ * will stall until it completes.
+ *
+ * @return An error if a new key cannot be requested.
+ */
+rom_error_t retention_sram_scramble(void);
 
 #ifdef __cplusplus
 }
