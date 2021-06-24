2bdabb120641bf013e65c616cfe6b86b2790f77d
Michael Munday <mike.munday@lowrisc.org>
2021-06-24 13:52:15 +0100

    [sw/silicon_creator] Add minimal ePMP library
    This is a lightweight API for configuring and tracking ePMP control
    register values in RAM. This library does not provide a general
    purpose API for modifying the hardware registers themselves since,
    for efficiency reasons, the mask ROM will be configuring the
    hardware registers directly.
    
    The API contains a function to unlock the address space required for
    reporting test status. All tests booting from the mask ROM will need
    to explicitly unlock the DV address space or attempts to report their
    status will be blocked and result in an access exception.
    
    Signed-off-by: Michael Munday <mike.munday@lowrisc.org>

---
+++ sw/device/silicon_creator/lib/epmp.c
@@ -0,0 +1,60 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/epmp.h"
+
+#include "sw/device/lib/base/csr.h"
+
+/**
+ * Extern declarations of inline functions.
+ */
+extern void epmp_state_configure_tor(epmp_state_t *state, uint32_t entry,
+                                     epmp_region_t region, epmp_perm_t perm);
+extern void epmp_state_configure_na4(epmp_state_t *state, uint32_t entry,
+                                     epmp_region_t region, epmp_perm_t perm);
+extern void epmp_state_configure_napot(epmp_state_t *state, uint32_t entry,
+                                       epmp_region_t region, epmp_perm_t perm);
+
+rom_error_t epmp_state_check(const epmp_state_t *s) {
+  bool result = true;
+#define CHECK_CSR(reg, value) \
+  do {                        \
+    uint32_t csr;             \
+    CSR_READ(reg, &csr);      \
+    result &= csr == (value); \
+  } while (false)
+
+  // Check address registers.
+  CHECK_CSR(CSR_REG_PMPADDR0, s->pmpaddr[0]);
+  CHECK_CSR(CSR_REG_PMPADDR1, s->pmpaddr[1]);
+  CHECK_CSR(CSR_REG_PMPADDR2, s->pmpaddr[2]);
+  CHECK_CSR(CSR_REG_PMPADDR3, s->pmpaddr[3]);
+  CHECK_CSR(CSR_REG_PMPADDR4, s->pmpaddr[4]);
+  CHECK_CSR(CSR_REG_PMPADDR5, s->pmpaddr[5]);
+  CHECK_CSR(CSR_REG_PMPADDR6, s->pmpaddr[6]);
+  CHECK_CSR(CSR_REG_PMPADDR7, s->pmpaddr[7]);
+  CHECK_CSR(CSR_REG_PMPADDR8, s->pmpaddr[8]);
+  CHECK_CSR(CSR_REG_PMPADDR9, s->pmpaddr[9]);
+  CHECK_CSR(CSR_REG_PMPADDR10, s->pmpaddr[10]);
+  CHECK_CSR(CSR_REG_PMPADDR11, s->pmpaddr[11]);
+  CHECK_CSR(CSR_REG_PMPADDR12, s->pmpaddr[12]);
+  CHECK_CSR(CSR_REG_PMPADDR13, s->pmpaddr[13]);
+  CHECK_CSR(CSR_REG_PMPADDR14, s->pmpaddr[14]);
+  CHECK_CSR(CSR_REG_PMPADDR15, s->pmpaddr[15]);
+
+  // Check configuration registers.
+  CHECK_CSR(CSR_REG_PMPCFG0, s->pmpcfg[0]);
+  CHECK_CSR(CSR_REG_PMPCFG1, s->pmpcfg[1]);
+  CHECK_CSR(CSR_REG_PMPCFG2, s->pmpcfg[2]);
+  CHECK_CSR(CSR_REG_PMPCFG3, s->pmpcfg[3]);
+
+  // Check Machine Security Configuration (MSECCFG) register.
+  // High bits are hardcoded to 0.
+  CHECK_CSR(CSR_REG_MSECCFG, s->mseccfg);
+  CHECK_CSR(CSR_REG_MSECCFGH, 0);
+
+#undef CHECK_CSR
+
+  return result ? kErrorOk : kErrorEpmpBadCheck;
+}
---
+++ sw/device/silicon_creator/lib/epmp_defs.h
@@ -0,0 +1,74 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_EPMP_DEFS_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_EPMP_DEFS_H_
+
+/**
+ * Constants for use when interacting with the enhanced Physical Memory
+ * Protection control registers.
+ *
+ * See the Ibex Physical Memory Protection documentation for more
+ * details:
+ *
+ * https://ibex-core.readthedocs.io/en/latest/03_reference/pmp.html
+ *
+ * Note: this file must be usable from assembly, C and C++ files and
+ * should therefore only contain constant definitions.
+ */
+
+/**
+ * Address of the Machine Security Configuration control register.
+ *
+ * For use with CSR operations such as read, write, set and clear.
+ */
+#define EPMP_MSECCFG 0x390
+
+/**
+ * Machine Security Configuration bits.
+ *
+ *   MML = Machine Mode Lockdown
+ *   MMWP = Machine Mode Whitelist Policy
+ *   RLB = Rule Locking Bypass
+ */
+#define EPMP_MSECCFG_MML (1 << 0)
+#define EPMP_MSECCFG_MMWP (1 << 1)
+#define EPMP_MSECCFG_RLB (1 << 2)
+
+/**
+ * PMP configuration (`pmpNcfg`) register fields.
+ *
+ *   8   7       5       3   2   1   0
+ *   +---+-------+-------+---+---+---+
+ *   | L |   0   |   A   | X | W | R |
+ *   +---+-------+-------+---+---+---+
+ *
+ * Key:
+ *
+ *   L - lock
+ *   A - addressing mode (OFF=0, TOR=1, NA4=2, NAPOT=3)
+ *   X - execute
+ *   W - write
+ *   R - read
+ *
+ * Note that there are four 8-bit configuration fields (`pmpNcfg`)
+ * packed into each hardware configuration register (`pmpcfgN`).
+ */
+#define EPMP_CFG_L (1 << 7)
+#define EPMP_CFG_A_OFF (0 << 3)
+#define EPMP_CFG_A_TOR (1 << 3)
+#define EPMP_CFG_A_NA4 (2 << 3)
+#define EPMP_CFG_A_NAPOT (3 << 3)
+#define EPMP_CFG_X (1 << 2)
+#define EPMP_CFG_W (1 << 1)
+#define EPMP_CFG_R (1 << 0)
+
+/**
+ * Common PMP configuration (`pmpNcfg`) permission combinations.
+ */
+#define EPMP_CFG_LR (EPMP_CFG_L | EPMP_CFG_R)
+#define EPMP_CFG_LRX (EPMP_CFG_L | EPMP_CFG_R | EPMP_CFG_X)
+#define EPMP_CFG_LRW (EPMP_CFG_L | EPMP_CFG_R | EPMP_CFG_W)
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_EPMP_DEFS_H_
