c9ed7987350225e962acc0250c2d80ffc1f5803a
Timothy Chen <timothytim@google.com>
2021-06-24 18:09:21 -0700

    [sw] Various updates to complete keymgr_functest
    - lock otp partition
    - reboot device
    - initialize kmac sideload for keymgr derivation
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- sw/device/silicon_creator/lib/drivers/keymgr_functest.c
+++ sw/device/silicon_creator/lib/drivers/keymgr_functest.c
@@ -7,6 +7,10 @@
 
 #include "sw/device/lib/arch/device.h"
 #include "sw/device/lib/base/memory.h"
+#include "sw/device/lib/dif/dif_aon_timer.h"
+#include "sw/device/lib/dif/dif_kmac.h"
+#include "sw/device/lib/dif/dif_otp_ctrl.h"
+#include "sw/device/lib/dif/dif_pwrmgr.h"
 #include "sw/device/lib/flash_ctrl.h"
 #include "sw/device/lib/runtime/hart.h"
 #include "sw/device/lib/runtime/log.h"
@@ -162,6 +166,103 @@ rom_error_t keymgr_rom_ext_test(void) {
   return kErrorOk;
 }
 
+static const dif_pwrmgr_wakeup_reason_t kWakeUpReasonPor = {
+    .types = 0,
+    .request_sources = 0,
+};
+
+/** Check wakeup reason to determine what to do during the test */
+static bool compare_wakeup_reasons(const dif_pwrmgr_wakeup_reason_t *lhs,
+                                   const dif_pwrmgr_wakeup_reason_t *rhs) {
+  return lhs->types == rhs->types &&
+         lhs->request_sources == rhs->request_sources;
+}
+
+static void aon_timer_wakeup_config(dif_aon_timer_t *aon_timer,
+                                    uint32_t wakeup_threshold) {
+  // Make sure that wake-up timer is stopped.
+  CHECK(dif_aon_timer_wakeup_stop(aon_timer) == kDifAonTimerOk);
+
+  // Make sure the wake-up IRQ is cleared to avoid false positive.
+  CHECK(dif_aon_timer_irq_acknowledge(
+            aon_timer, kDifAonTimerIrqWakeupThreshold) == kDifAonTimerOk);
+
+  bool is_pending;
+  CHECK(dif_aon_timer_irq_is_pending(aon_timer, kDifAonTimerIrqWakeupThreshold,
+                                     &is_pending) == kDifAonTimerOk);
+  CHECK(!is_pending);
+
+  CHECK(dif_aon_timer_wakeup_start(aon_timer, wakeup_threshold, 0) ==
+        kDifAonTimerOk);
+}
+
+/**
+ * Busy-wait until the DAI is done with whatever operation it is doing.
+ */
+static dif_otp_ctrl_t otp;
+
+static void wait_for_dai(void) {
+  while (true) {
+    dif_otp_ctrl_status_t status;
+    CHECK(dif_otp_ctrl_get_status(&otp, &status) == kDifOtpCtrlOk);
+    if (bitfield_bit32_read(status.codes, kDifOtpCtrlStatusCodeDaiIdle)) {
+      return;
+    }
+    LOG_INFO("Waiting for DAI...");
+  }
+}
+
+/**
+ * Lock otp secret 2 partition so that on reboot flash seeds can be
+ * automatically loaded
+ */
+static void lock_otp_secret_partition2(void) {
+  // initialize otp
+  mmio_region_t otp_reg =
+      mmio_region_from_addr(TOP_EARLGREY_OTP_CTRL_BASE_ADDR);
+  CHECK(dif_otp_ctrl_init((dif_otp_ctrl_params_t){.base_addr = otp_reg},
+                          &otp) == kDifOtpCtrlOk);
+
+  CHECK(dif_otp_ctrl_dai_digest(&otp, kDifOtpCtrlPartitionSecret2, 0) ==
+        kDifOtpCtrlDaiOk);
+
+  wait_for_dai();
+}
+
+/** Place kmac into sideload mode for correct keymgr operation */
+static void init_kmac_for_keymgr(void) {
+  dif_kmac_t kmac;
+  CHECK(dif_kmac_init((dif_kmac_params_t){.base_addr = mmio_region_from_addr(
+                                              TOP_EARLGREY_KMAC_BASE_ADDR)},
+                      &kmac) == kDifKmacOk);
+
+  // Configure KMAC hardware using software entropy.
+  dif_kmac_config_t config = (dif_kmac_config_t){
+      .sideload = true,
+  };
+  CHECK(dif_kmac_configure(&kmac, config) == kDifKmacOk);
+}
+
+/** Soft reboot device */
+static void soft_reboot(dif_pwrmgr_t *pwrmgr, dif_aon_timer_t *aon_timer) {
+  aon_timer_wakeup_config(aon_timer, 1);
+
+  // Place device into low power and immediately wake
+  dif_pwrmgr_domain_config_t config;
+  config = kDifPwrmgrDomainOptionUsbClockInActivePower;
+
+  CHECK(dif_pwrmgr_set_request_sources(pwrmgr, kDifPwrmgrReqTypeWakeup,
+                                       kDifPwrmgrWakeupRequestSourceFive) ==
+        kDifPwrmgrConfigOk);
+  CHECK(dif_pwrmgr_set_domain_config(pwrmgr, config) == kDifPwrmgrConfigOk);
+  CHECK(dif_pwrmgr_low_power_set_enabled(pwrmgr, kDifPwrmgrToggleEnabled) ==
+        kDifPwrmgrConfigOk);
+
+  // Enter low power mode.
+  LOG_INFO("Entering low power");
+  wait_for_interrupt();
+}
+
 bool test_main(void) {
   rom_error_t result = kErrorOk;
 
@@ -169,9 +270,48 @@ bool test_main(void) {
   lifecycle_state_t lc_state = lifecycle_state_get();
   LOG_INFO("lifecycle state: %s", lifecycle_state_name[lc_state]);
 
-  init_flash();
+  // Initialize pwrmgr
+  dif_pwrmgr_t pwrmgr;
+  CHECK(dif_pwrmgr_init(
+            (dif_pwrmgr_params_t){
+                .base_addr =
+                    mmio_region_from_addr(TOP_EARLGREY_PWRMGR_AON_BASE_ADDR),
+            },
+            &pwrmgr) == kDifPwrmgrOk);
 
-  EXECUTE_TEST(result, keymgr_rom_test);
-  EXECUTE_TEST(result, keymgr_rom_ext_test);
-  return result == kErrorOk;
+  // Initialize aon_timer
+  dif_aon_timer_t aon_timer;
+  dif_aon_timer_params_t params = {
+      .base_addr = mmio_region_from_addr(TOP_EARLGREY_AON_TIMER_AON_BASE_ADDR),
+  };
+  CHECK(dif_aon_timer_init(params, &aon_timer) == kDifAonTimerOk);
+
+  // Get wakeup reason
+  dif_pwrmgr_wakeup_reason_t wakeup_reason;
+  CHECK(dif_pwrmgr_wakeup_reason_get(&pwrmgr, &wakeup_reason) == kDifPwrmgrOk);
+
+  if (compare_wakeup_reasons(&wakeup_reason, &kWakeUpReasonPor)) {
+    LOG_INFO("Powered up for the first time, program flash");
+
+    // Initialize flash
+    init_flash();
+
+    // Lock otp secret partition
+    lock_otp_secret_partition2();
+
+    // reboot device
+    soft_reboot(&pwrmgr, &aon_timer);
+
+  } else {
+    LOG_INFO("Powered up for the second time, actuate keymgr");
+
+    // Initialize kmac for key manager
+    init_kmac_for_keymgr();
+
+    EXECUTE_TEST(result, keymgr_rom_test);
+    EXECUTE_TEST(result, keymgr_rom_ext_test);
+    return result == kErrorOk;
+  }
+
+  return false;
 }
