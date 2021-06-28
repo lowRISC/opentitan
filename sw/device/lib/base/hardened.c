fbf8343fc815a30f0d59b94189f81eb772215e7e
Miguel Young de la Sota <mcyoung@google.com>
2021-06-28 11:31:36 -0400

    [lib] Add launder32()
    This function adds a "value laundering" pessimization that can be used
    in targeted scenarios where the compiler's analysis (typically GVN or
    something like it) would fold away critical check-your-work operations,
    such as double-branches or loop completion checks.
    
    The implementation is the result of careful analysis of existing
    solutions and consultation with lowRISC's toolchain engineers. Usage of
    this "intrinsic" is very subtle and requires careful review and
    documentation.
    
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

---
+++ sw/device/lib/base/hardened.c
@@ -0,0 +1,8 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/base/hardened.h"
+
+// Instantiate inline functions in this TU.
+extern uint32_t launder32(uint32_t);
