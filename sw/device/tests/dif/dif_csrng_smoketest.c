74d21e6ecda3773910bafb33bb92274afefc92a3
Miguel Osorio <miguelosorio@google.com>
2021-07-13 20:10:56 -0700

    [sw/test] Add initial DRBG KAT tests
    Add dif functionality to read the internal DRBG state.
    
    Add instantiate and generate KAT tests using the following no-reseed
    NIST CTR_DRBG test vector:
    
    [AES-256 no df]
    [PredictionResistance = False]
    [EntropyInputLen = 384]
    [NonceLen = 0]
    [PersonalizationStringLen = 0]
    [AdditionalInputLen = 0]
    [ReturnedBitsLen = 512]
    
    COUNT = 0
    EntropyInput = df5d73faa468649edda33b5cca79b0b05600419ccb7a879ddfec9db32ee494e5531b51de16a30f769262474c73bec010
    Nonce =
    PersonalizationString =
    ** INSTANTIATE:
    	Key = 8c52f901632d522774c08fad0eb2c33b98a701a1861aecf3d8a25860941709fd
    	V   = 217b52142105250243c0b2c206b8f59e
    
    AdditionalInput =
    ** GENERATE (FIRST CALL):
    	Key = 72f4af5c93258eb3eeec8c0cacea6c1d1978a4fad44312725f1ac43b167f2d52
    	V   = e86f6d07dfb551cebad80e6bf6830ac4
    
    AdditionalInput =
    ReturnedBits = d1c07cd95af8a7f11012c84ce48bb8cb87189e99d40fccb1771c619bdf82ab2280b1dc2f2581f39164f7ac0c510494b3a43c41b7db17514c87b107ae793e01c5
    ** GENERATE (SECOND CALL):
    	Key = 1a1c6e5f1cccc6974436e5fd3f015bc8e9dc0f90053b73e3c19d4dfd66d1b85a
    	V   = 53c78ac61a0bac9d7d2e92b1e73e3392
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- sw/device/tests/dif/dif_csrng_smoketest.c
+++ sw/device/tests/dif/dif_csrng_smoketest.c
@@ -13,87 +13,131 @@
 
 const test_config_t kTestConfig;
 
-/**
- * FIPS CAVP test vector for CTR DRBG.
- *
- * CAVP test parameters.
- * Cipher: AES-256
- * Derivation Function: No
- * Prediction Resistance: False
- */
-const dif_csrng_seed_material_t kEntropyInput = {
-    .seed_material =
-        {
-            0xe4bc23c5,
-            0x089a19d8,
-            0x6f4119cb,
-            0x3fa08c0a,
-            0x4991e0a1,
-            0xdef17e10,
-            0x1e4c14d9,
-            0xc323460a,
-            0x7c2fb58e,
-            0x0b086c6c,
-            0x57b55f56,
-            0xcae25bad,
-        },
-    .seed_material_len = 12,
+enum {
+  kExpectedOutputLen = 16,
 };
 
 /**
- * Expected CSRNG output.
- */
-const uint32_t kExpectedOutput[] = {
-    0xb2cb8905, 0xc05e5950, 0xca318950, 0x96be29ea, 0x3d5a3b82, 0xb2694955,
-    0x54eb80fe, 0x07de43e1, 0x93b9e7c3, 0xece73b80, 0xe062b1c1, 0xf68202fb,
-    0xb1c52a04, 0x0ea24788, 0x64295282, 0x234aaada,
-};
-const uint32_t kExpectedOutputLen = 16;
-
-/**
- * Wait for the `csrng` instance command interface to be ready to
- * accept commands. Aborts test execution if an error is found.
+ * Wait for the `csrng` instance command interface to be ready to accept
+ * commands. Aborts test execution if an error is found.
  */
 static void wait_for_csrng_cmd_ready(const dif_csrng_t *csrng) {
-  dif_csrng_cmd_status_t cmd_status = {0};
-  while (cmd_status != kDifCsrngCmdStatusReady) {
+  dif_csrng_cmd_status_t cmd_status;
+  do {
     CHECK(dif_csrng_get_cmd_interface_status(csrng, &cmd_status) ==
           kDifCsrngOk);
     CHECK(cmd_status != kDifCsrngCmdStatusError);
-  }
+  } while (cmd_status != kDifCsrngCmdStatusReady);
 }
 
 /**
- * Run CAVP CTR DRBG Counter=0 with `kEntropyInput` deterministic
- * seed material.
+ * Checks the CSRNG internal state against `expected` values.
+ *
+ * @param csrng A CSRNG handle.
+ * @param expected Expected CSRNG internal state.
  */
-void test_ctr_drbg_ctr0(const dif_csrng_t *csrng) {
+static void check_internal_state(const dif_csrng_t *csrng,
+                                 const dif_csrng_internal_state_t *expected) {
+  wait_for_csrng_cmd_ready(csrng);
+  dif_csrng_internal_state_t got;
+  CHECK(dif_csrng_get_internal_state(csrng, kCsrngInternalStateIdSw, &got) ==
+        kDifCsrngOk);
+
+  CHECK(got.instantiated == expected->instantiated);
+  CHECK(got.reseed_counter == expected->reseed_counter);
+  CHECK(got.fips_compliance == expected->fips_compliance);
+
+  bool result = false;
+  CHECK_BUFFER(result, got.v, expected->v, ARRAYSIZE(expected->v));
+  CHECK(result, "CSRNG internal V buffer mismatch.");
+
+  CHECK_BUFFER(result, got.key, expected->key, ARRAYSIZE(expected->key));
+  CHECK(result, "CSRNG internal K buffer mismatch.");
+}
+
+/**
+ * CTR DRBG Known-Answer-Test (KAT) for INSTANTIATE command.
+ */
+static void fips_instantiate_kat(const dif_csrng_t *csrng) {
+  LOG_INFO("Instantiate KAT");
+
+  const dif_csrng_seed_material_t kEntropyInput = {
+      .seed_material = {0x73bec010, 0x9262474c, 0x16a30f76, 0x531b51de,
+                        0x2ee494e5, 0xdfec9db3, 0xcb7a879d, 0x5600419c,
+                        0xca79b0b0, 0xdda33b5c, 0xa468649e, 0xdf5d73fa},
+      .seed_material_len = 12,
+  };
   wait_for_csrng_cmd_ready(csrng);
   CHECK(dif_csrng_instantiate(csrng, kDifCsrngEntropySrcToggleDisable,
                               &kEntropyInput) == kDifCsrngOk);
 
-  wait_for_csrng_cmd_ready(csrng);
-  const dif_csrng_seed_material_t seed_material = {0};
-  CHECK(dif_csrng_reseed(csrng, &seed_material) == kDifCsrngOk);
+  const dif_csrng_internal_state_t kExpectedState = {
+      .reseed_counter = 1,
+      .v = {0x06b8f59e, 0x43c0b2c2, 0x21052502, 0x217b5214},
+      .key = {0x941709fd, 0xd8a25860, 0x861aecf3, 0x98a701a1, 0x0eb2c33b,
+              0x74c08fad, 0x632d5227, 0x8c52f901},
+      .instantiated = true,
+      .fips_compliance = false,
+  };
+  check_internal_state(csrng, &kExpectedState);
+}
 
+/**
+ * Runs CSRNG generate command.
+ *
+ * @param csrng A CSRNG handle.
+ * @param output Output buffer.
+ * @param output_len Number of words of entropy to write to output buffer.
+ */
+static void run_generate_cmd(const dif_csrng_t *csrng, uint32_t *output,
+                             size_t output_len) {
   wait_for_csrng_cmd_ready(csrng);
-  CHECK(dif_csrng_generate_start(csrng, kExpectedOutputLen) == kDifCsrngOk);
+  CHECK(dif_csrng_generate_start(csrng, output_len) == kDifCsrngOk);
 
-  dif_csrng_output_status_t output_status = {0};
-  while (!output_status.valid_data) {
+  dif_csrng_output_status_t output_status;
+  do {
     CHECK(dif_csrng_get_output_status(csrng, &output_status) == kDifCsrngOk);
-  }
+  } while (!output_status.valid_data);
 
-  uint32_t output[16];
-  CHECK(dif_csrng_generate_end(csrng, output, kExpectedOutputLen) ==
-        kDifCsrngOk);
+  CHECK(dif_csrng_generate_end(csrng, output, output_len) == kDifCsrngOk);
+}
 
-  // TODO(#5982): Enable CSRNG SW path without entropy source.
-  for (uint32_t i = 0; i < 16; ++i) {
-    CHECK(output[i] != 0u);
-    LOG_INFO("[%d] got = 0x%x; expected = 0x%x", i, output[i],
-             kExpectedOutput[i]);
-  }
+/**
+ * CTR DRBG Known-Answer-Test (KAT) for GENERATE command.
+ */
+static void fips_generate_kat(const dif_csrng_t *csrng) {
+  LOG_INFO("Generate KAT");
+
+  uint32_t got[kExpectedOutputLen];
+  run_generate_cmd(csrng, got, kExpectedOutputLen);
+  run_generate_cmd(csrng, got, kExpectedOutputLen);
+  const dif_csrng_internal_state_t kExpectedState = {
+      .reseed_counter = 3,
+      .v = {0xe73e3392, 0x7d2e92b1, 0x1a0bac9d, 0x53c78ac6},
+      .key = {0x66d1b85a, 0xc19d4dfd, 0x053b73e3, 0xe9dc0f90, 0x3f015bc8,
+              0x4436e5fd, 0x1cccc697, 0x1a1c6e5f},
+      .instantiated = true,
+      .fips_compliance = false,
+  };
+  check_internal_state(csrng, &kExpectedState);
+
+  const uint32_t kExpectedOutput[kExpectedOutputLen] = {
+      0x793e01c5, 0x87b107ae, 0xdb17514c, 0xa43c41b7, 0x510494b3, 0x64f7ac0c,
+      0x2581f391, 0x80b1dc2f, 0xdf82ab22, 0x771c619b, 0xd40fccb1, 0x87189e99,
+      0xe48bb8cb, 0x1012c84c, 0x5af8a7f1, 0xd1c07cd9};
+
+  bool result = false;
+  CHECK_BUFFER(result, got, kExpectedOutput, kExpectedOutputLen);
+  CHECK(result, "Generate command KAT output mismatch");
+}
+
+/**
+ * Run CTR DRBG Known-Answer-Tests (KATs).
+ */
+void test_ctr_drbg_ctr0(const dif_csrng_t *csrng) {
+  CHECK(dif_csrng_uninstantiate(csrng) == kDifCsrngOk);
+  fips_instantiate_kat(csrng);
+  fips_generate_kat(csrng);
 }
 
 bool test_main() {
