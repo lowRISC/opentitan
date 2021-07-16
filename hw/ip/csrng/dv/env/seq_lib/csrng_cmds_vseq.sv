841d8ec452d0c2cc56ef9fb0f60ca457d8f16430
Steve Nelson <steve.nelson@wdc.com>
2021-07-16 11:21:51 -0700

    [csrng/dv] Generate cmds for multiple hw_apps
    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>

--- hw/ip/csrng/dv/env/seq_lib/csrng_cmds_vseq.sv
+++ hw/ip/csrng/dv/env/seq_lib/csrng_cmds_vseq.sv
@@ -8,10 +8,10 @@ class csrng_cmds_vseq extends csrng_base_vseq;
 
   `uvm_object_new
 
-  rand bit [entropy_src_pkg::FIPS_CSRNG_BUS_WIDTH-1:0]   entropy_val;
-  rand csrng_item                                        cs_item[NUM_HW_APPS];
-  bit [csrng_pkg::CSRNG_CMD_WIDTH-1:0]                   cmd_data, cmd_data_q[$];
-  bit [csrng_pkg::GENBITS_BUS_WIDTH-1:0]                 genbits;
+  bit [entropy_src_pkg::FIPS_CSRNG_BUS_WIDTH-1:0]   entropy_val;
+  csrng_item                                        cs_item, cs_item_clone, cs_item_q[NUM_HW_APPS][$];
+  uint                                              num_cmds;
+  bit [csrng_pkg::GENBITS_BUS_WIDTH-1:0]            genbits;
 
 
   task body();
@@ -21,38 +21,55 @@ class csrng_cmds_vseq extends csrng_base_vseq;
 
     // TODO: Create/start entropy_src device sequence still under development. Will remove/modify
     // as necessary. Also consider making task if it stays.
-    // TODO: csrng_cmds_vseq still under development.
+    // TODO: Enhance to run all cmds in the queues
 
-    // Create and start entropy_src device sequence
+    // Create entropy_src sequence
     m_entropy_src_pull_seq = push_pull_device_seq#(entropy_src_pkg::FIPS_CSRNG_BUS_WIDTH)::type_id::
          create("m_entropy_src_pull_seq");
-    // Create csrng_cmd host sequence
-    m_edn_push_seq = push_pull_host_seq#(csrng_pkg::CSRNG_CMD_WIDTH)::type_id::create("m_edn_push_seq");
+    // Create csrng_cmd host sequences and cs_item
+    for (int i = 0; i < NUM_HW_APPS; i++) begin
+      m_edn_push_seq[i] = push_pull_host_seq#(csrng_pkg::CSRNG_CMD_WIDTH)::type_id::create($sformatf("m_edn_push_seq[%0d]", i));
+      cs_item           = csrng_item::type_id::create("cs_item");
+    end
 
-    for (int i = 0; i < NUM_HW_APPS; i++)
-      cs_item[i] = csrng_item::type_id::create($sformatf("cs_item[%0d]", i));
+    // Generate queues of csrng commands
+    for (int i = 0; i < NUM_HW_APPS; i++) begin
+      // TODO: randomize num_cmds
+      for (int j = 0; j < 1; j++) begin
+        `DV_CHECK_RANDOMIZE_WITH_FATAL(cs_item,
+                                       cs_item.acmd  == csrng_pkg::INS;
+                                       cs_item.flags == 4'h1;
+                                       cs_item.glen  == 'h0;)
+        `downcast(cs_item_clone, cs_item.clone());
+        cs_item_q[i].push_back(cs_item_clone);
+      end
 
+      // Print cs_items
+      foreach (cs_item_q[i][j])
+        `uvm_info(`gfn, $sformatf("cs_item_q[%0d][%0d]: %s", i, j, cs_item_q[i][j].convert2string()), UVM_DEBUG)
+    end
+
+    // Start entropy_src
     fork
-      fork
-        begin
-          // TODO: randomize entropy/fips
-          cfg.m_entropy_src_agent_cfg.add_d_user_data(384'hdeadbeef);
-          cfg.m_entropy_src_agent_cfg.add_d_user_data(384'hbeefdead);
-          m_entropy_src_pull_seq.start(p_sequencer.entropy_src_sequencer_h);
-        end
-      join_none
+      begin
+        // TODO: randomize entropy/fips
+        cfg.m_entropy_src_agent_cfg.add_d_user_data(384'hdeadbeef);
+        cfg.m_entropy_src_agent_cfg.add_d_user_data(384'hbeefdead);
+        m_entropy_src_pull_seq.start(p_sequencer.entropy_src_sequencer_h);
+      end
+    join_none
 
-      // TODO: gen cmds for multiple hw_apps
-      for (int i = 0; i < 1; i++) begin
-        // instantiate cmd
-        // TODO: randomize num_cmds
-        for (int j = 0; j < 10; j++) begin
-          `DV_CHECK_RANDOMIZE_WITH_FATAL(cs_item[i],
-                                         cs_item[i].acmd  == csrng_pkg::INS;
-                                         cs_item[i].flags == 4'h1;
-                                         cs_item[i].glen  == 'h0;)
-          send_cmd_req(i, cs_item[i]);
-        end
+    // Send commands to DUT
+    for (int i = 0; i < NUM_HW_APPS; i++) begin
+      foreach (cs_item_q[i][j])begin
+        send_cmd_req(i, cs_item_q[i][j]);
+      end
+    end
+
+    // Wait for DUT to ack commands
+    for (int i = 0; i < NUM_HW_APPS; i++) begin
+      cfg.m_edn_agent_cfg[i].vif.wait_cmd_ack();
+    end
 
         // TODO: add other commands
         // //generate cmd
@@ -69,8 +86,6 @@ class csrng_cmds_vseq extends csrng_base_vseq;
         // csrng_update(384'hdeadbeef);
 
         // send_cmd_req(.acmd(csrng_pkg::UNI), .clen(4'h0), .flags(4'h0), .glen(19'h0));
-      end
-    join
 
     if (cfg.chk_int_state) begin
       for (int i = 0; i < NUM_HW_APPS; i++)
