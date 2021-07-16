29b7c571d4e2ee1631c001dceff5bd0e18657ad1
Philipp Wagner <phw@lowrisc.org>
2021-07-16 12:06:38 +0100

    [otbn] Indent loop body in assembly
    Comsmetic change: Indent the loop body in assembly code to make it
    easier to read.
    
    Signed-off-by: Philipp Wagner <phw@lowrisc.org>

--- sw/otbn/code-snippets/loop.s
+++ sw/otbn/code-snippets/loop.s
@@ -10,22 +10,22 @@
     li  x3, 0
 
     loop x2, 2
-    addi x3, x3, 2
-    addi x3, x3, -1
+      addi x3, x3, 2
+      addi x3, x3, -1
 
     /* At this point, we've incremented x3 by 2-1 = 1 on each of three
        loop iterations, so x3 should equal 3. */
 
     loopi 5, 1
-    addi x3, x3, -3
+      addi x3, x3, -3
 
     /* Now we've run a loop that decrements x3 by 3 on each of
        five loop iterations, so it should now equal 3-15 = -12. */
 
     loop x2, 3
-    loopi 4, 1
-    addi x3, x3, 2
-    nop
+      loopi 4, 1
+         addi x3, x3, 2
+      nop
 
     /* The nested loop runs 3 * 4 times, incrementing by 2 each
        iteration. So x3 should now equal -12 + 2*12 = 12. */
