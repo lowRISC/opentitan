f23b968f06bce63e1e74100879b71df0fb1d9e09
Alphan Ulusoy <alphan@google.com>
2021-08-08 01:07:52 -0400

    [sw/silicon_creator] Add mock for sigverify_keys_ptrs.h
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

---
+++ sw/device/silicon_creator/mask_rom/mock_sigverify_keys_ptrs.h
@@ -0,0 +1,41 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_MASK_ROM_MOCK_SIGVERIFY_KEYS_PTRS_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_MASK_ROM_MOCK_SIGVERIFY_KEYS_PTRS_H_
+
+#include "sw/device/lib/testing/mask_rom_test.h"
+#include "sw/device/silicon_creator/mask_rom/sigverify_keys_ptrs.h"
+
+namespace mask_rom_test {
+namespace internal {
+
+/**
+ * Mock class for sigverify_keys_ptrs.h
+ */
+class MockSigverifyKeysPtrs : public GlobalMock<MockSigverifyKeysPtrs> {
+ public:
+  MOCK_METHOD(const sigverify_mask_rom_key_t *, RsaKeysPtrGet, ());
+  MOCK_METHOD(size_t, NumRsaKeysGet, ());
+};
+
+}  // namespace internal
+
+using MockSigverifyKeysPtrs =
+    testing::StrictMock<internal::MockSigverifyKeysPtrs>;
+
+extern "C" {
+
+const sigverify_mask_rom_key_t *sigverify_rsa_keys_ptr_get() {
+  return MockSigverifyKeysPtrs::Instance().RsaKeysPtrGet();
+}
+
+size_t sigverify_num_rsa_keys_get() {
+  return MockSigverifyKeysPtrs::Instance().NumRsaKeysGet();
+}
+
+}  // extern "C"
+}  // namespace mask_rom_test
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_MASK_ROM_MOCK_SIGVERIFY_KEYS_PTRS_H_
