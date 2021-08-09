2c3389470e91419bd7bc9d98dc85219e049a1eee
Steve Nelson <steve.nelson@wdc.com>
2021-08-09 11:49:26 -0700

    [entropy_src/dv] Fix for issue #7660
    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>

--- hw/ip/entropy_src/dv/tests/entropy_src_smoke_test.sv
+++ hw/ip/entropy_src/dv/tests/entropy_src_smoke_test.sv
@@ -11,8 +11,10 @@ class entropy_src_smoke_test extends entropy_src_base_test;
     super.configure_env();
 
     // TODO: Enable scoreboard
-    cfg.en_scb             = 0;
-    cfg.route_software_pct = 100;
+    cfg.en_scb                      = 0;
+    cfg.route_software_pct          = 100;
+    // To correctly model ast/rng behavior, back-to-back entropy is not allowed
+    cfg.m_rng_agent_cfg.zero_delays = 0;
 
     `DV_CHECK_RANDOMIZE_FATAL(cfg)
     `uvm_info(`gfn, $sformatf("%s", cfg.convert2string()), UVM_LOW)
