29ff92d97eff1519a74be64970fecc9d07b0ff0f
Rupert Swarbrick <rswarbrick@lowrisc.org>
2021-08-23 12:43:34 +0100

    [otbn,dv] Add loop warping support to otbn_memutil
    This commit teaches otbn_memutil, which handles loading OTBN ELF files
    for Verilator and UVM simulations, to expose the loop warping rules
    that were defined in the file.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/otbn/dv/memutil/otbn_memutil.cc
+++ hw/ip/otbn/dv/memutil/otbn_memutil.cc
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "otbn_memutil.h"
-#include "scrambled_ecc32_mem_area.h"
 
 #include <cassert>
 #include <cstring>
@@ -11,10 +10,13 @@
 #include <iostream>
 #include <libelf.h>
 #include <limits>
+#include <regex>
 #include <sstream>
 #include <stdexcept>
 
+#include "scrambled_ecc32_mem_area.h"
 #include "sv_scoped.h"
+#include "sv_utils.h"
 
 OtbnMemUtil::OtbnMemUtil(const std::string &top_scope)
     : imem_(SVScoped::join_sv_scopes(top_scope, "u_imem"), 4096 / 4, 4 / 4),
@@ -32,13 +34,20 @@ const StagedMem::SegMap &OtbnMemUtil::GetSegs(bool is_imem) const {
   return GetMemoryData(is_imem ? "imem" : "dmem").GetSegs();
 }
 
+uint32_t OtbnMemUtil::GetLoopWarp(uint32_t addr, uint32_t from_cnt) const {
+  auto key = std::make_pair(addr, from_cnt);
+  auto it = loop_warp_.find(key);
+  return (it == loop_warp_.end()) ? from_cnt : it->second;
+}
+
 void OtbnMemUtil::OnElfLoaded(Elf *elf_file) {
   assert(elf_file);
 
-  // Look through the symbol table of elf_file for a symbol called
-  // "_expected_end_addr". If found, use it to set the expected_end_addr_
-  // field.
   expected_end_addr_ = -1;
+  loop_warp_.clear();
+
+  // Look through the symbol table of elf_file for an expected end
+  // address and any loop warping symbols.
   Elf_Scn *scn = nullptr;
   while ((scn = elf_nextscn(elf_file, scn))) {
     Elf32_Shdr *shdr = elf32_getshdr(scn);
@@ -58,13 +67,63 @@ void OtbnMemUtil::OnElfLoaded(Elf *elf_file) {
       if (!sym_name)
         continue;
 
-      if (0 == strcmp(sym_name, "_expected_end_addr")) {
-        // Ahah! We've found the magic symbol!
-        expected_end_addr_ = sym.st_value;
-        break;
-      }
+      OnSymbol(sym_name, sym.st_value);
     }
     break;
+  }
+}
+
+void OtbnMemUtil::OnSymbol(const std::string &name, uint32_t value) {
+  // Expected end address
+  if (name == "_expected_end_addr") {
+    expected_end_addr_ = value;
+  }
+
+  // Loop warping. These symbols are of the form "_loop_warp_FROM_TO_*" where
+  // FROM and TO are decimal loop counts and the value of the symbol is the
+  // address where it should apply. Trailing junk is allowed (to ensure
+  // uniqueness).
+  std::regex lw_re("_loop_warp_([0-9])+_([0-9])+.*");
+  std::smatch lw_match;
+  if (std::regex_match(name, lw_match, lw_re)) {
+    assert(lw_match.size() == 3);
+
+    // Parse the "from" count. We know that the captured group is one or
+    // more decimal digits, so the only question of correctness is whether
+    // it's in range or not. For the "from" count, we know that the loop
+    // counter in the design is only 32 bits so if the value is out of range
+    // then we just ignore it: it can never match anyway.
+    errno = 0;
+    unsigned long from_cnt = strtoul(lw_match[1].str().c_str(), nullptr, 10);
+    bool good_from_cnt =
+        (errno == 0) && (from_cnt <= std::numeric_limits<uint32_t>::max());
+
+    // Parse the "to" count. Again, the only question is whether it's in range
+    // or not. Saturate to the maximum value of a uint32: since the design has a
+    // 32-bit loop counter, this will always jump to the last iteration.
+    unsigned long to_cnt = strtoul(lw_match[2].str().c_str(), nullptr, 10);
+    uint32_t to_cnt32 =
+        std::min(to_cnt, (unsigned long)std::numeric_limits<uint32_t>::max());
+
+    if (good_from_cnt) {
+      AddLoopWarp(value, static_cast<uint32_t>(from_cnt), to_cnt32);
+    }
+  }
+}
+
+void OtbnMemUtil::AddLoopWarp(uint32_t addr, uint32_t from_cnt,
+                              uint32_t to_cnt) {
+  auto key = std::make_pair(addr, from_cnt);
+  auto pr = loop_warp_.insert(std::make_pair(key, to_cnt));
+  if (!pr.second) {
+    // We didn't actually insert anything, which means that there was already a
+    // value stored for this address/from_cnt pair. Since the ordering of ELF
+    // symbols isn't something that the user can control, we don't really have a
+    // sensible behaviour at this point: throw an error.
+    std::ostringstream oss;
+    oss << "Duplicate loop warp symbols for address 0x" << std::hex << addr
+        << " and initial count " << std::dec << from_cnt << ".";
+    throw std::runtime_error(oss.str());
   }
 }
 
@@ -217,4 +276,47 @@ extern "C" svBit OtbnMemUtilGetSegData(
 int OtbnMemUtilGetExpEndAddr(OtbnMemUtil *mem_util) {
   assert(mem_util);
   return mem_util->GetExpEndAddr();
+}
+
+svBit OtbnMemUtilGetLoopWarp(OtbnMemUtil *mem_util,
+                             /* bit [31:0] */ const svBitVecVal *addr,
+                             /* bit [31:0] */ const svBitVecVal *from_cnt,
+                             /* output bit [31:0] */ svBitVecVal *to_cnt) {
+  assert(mem_util);
+  uint32_t addr32 = get_sv_u32(addr);
+  uint32_t from32 = get_sv_u32(from_cnt);
+  uint32_t to32 = mem_util->GetLoopWarp(addr32, from32);
+  set_sv_u32(to_cnt, to32);
+  return to32 != from32;
+}
+
+int OtbnMemUtilGetNumLoopWarps(OtbnMemUtil *mem_util) {
+  assert(mem_util);
+
+  size_t sz = mem_util->GetLoopWarps().size();
+  assert(sz < std::numeric_limits<int>::max());
+
+  return sz;
+}
+
+void OtbnMemUtilGetLoopWarpByIndex(
+    OtbnMemUtil *mem_util, int idx,
+    /* output bit [31:0] */ svBitVecVal *addr,
+    /* output bit [31:0] */ svBitVecVal *from_cnt,
+    /* output bit [31:0] */ svBitVecVal *to_cnt) {
+  assert(mem_util);
+  assert(0 <= idx);
+
+  auto &warps = mem_util->GetLoopWarps();
+  assert(idx < warps.size());
+
+  auto it = std::next(warps.begin(), idx);
+
+  uint32_t addr32 = it->first.first;
+  uint32_t from32 = it->first.second;
+  uint32_t to32 = it->second;
+
+  set_sv_u32(addr, addr32);
+  set_sv_u32(from_cnt, from32);
+  set_sv_u32(to_cnt, to32);
 }
