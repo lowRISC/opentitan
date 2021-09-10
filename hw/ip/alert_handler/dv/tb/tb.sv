2a3e3e24c18e687a5ae4b3b37fa0eee403b968f3
Cindy Chen <chencindy@opentitan.org>
2021-09-10 13:42:59 -0700

    [dv/alert] remove ping corner case in alert handler
    This PR removes ping corner case in alert_handler testbench because they
    are covered in prim_alert or prim_esc, and the related coverage are
    excluded.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

--- hw/ip/alert_handler/dv/tb/tb.sv
+++ hw/ip/alert_handler/dv/tb/tb.sv
@@ -90,9 +90,6 @@ module tb;
   );
 
   initial begin
-    static bit reduce_ping_timer_wait_cycles = 0;
-    void'($value$plusargs("reduce_ping_timer_wait_cycles=%0b", reduce_ping_timer_wait_cycles));
-    if (reduce_ping_timer_wait_cycles) force dut.u_ping_timer.wait_cyc_mask_i = 24'h3FFFF;
     // drive clk and rst_n from clk_if
     clk_rst_if.set_active();
     uvm_config_db#(virtual clk_rst_if)::set(null, "*.env", "clk_rst_vif", clk_rst_if);
