a409d1069b9664bc0a3811cd2c26cf366bd5c0e2
Chris Frantz <cfrantz@google.com>
2021-09-10 17:33:05 -0700

    [opentitantool] Minor refactors for transport interfaces
    1. Define SPI Errors next to the SPI trait definition.
    2. Define GPIO Erros next to the GPIO trait definition.
    3. Express GPIO pins as strings.  Add the backend's default naming scheme
       for its pins.  A future PR will allow change this a bit more to permit
       user-defined pin-mappings.
    4. Add some convenience methods to the GPIO trait to support bootstrap in
       a generic way.
    5. Change the `gpio` command to use positional arguments where appropriate.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/host/opentitanlib/src/io/gpio.rs
+++ sw/host/opentitanlib/src/io/gpio.rs
@@ -3,12 +3,55 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::Result;
+use structopt::clap::arg_enum;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum GpioError {
+    #[error("Invalid GPIO pin name {0}")]
+    InvalidPinName(String),
+    #[error("Invalid GPIO pin number {0}")]
+    InvalidPinNumber(u8),
+    #[error("Invalid GPIO pin direction: {0}")]
+    InvalidPinDirection(u8),
+    #[error("Invalid strap configuration: {0:?}")]
+    InvalidStrapConfig(StrapConfig),
+}
+
+arg_enum! {
+    /// Direction for I/O pins.
+    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    pub enum PinDirection {
+        Input,
+        Output,
+    }
+}
+
+/// Settings for pin straps.  Not all backends support all settings.
+#[derive(Clone, Copy, Debug)]
+pub enum StrapConfig {
+    None,
+    RomBootstrap,
+    Custom(u8),
+}
 
 /// A trait which represents a GPIO interface.
 pub trait Gpio {
     /// Reads the value of the the GPIO pin `id`.
-    fn read(&self, id: u32) -> Result<bool>;
+    fn read(&self, id: &str) -> Result<bool>;
 
     /// Sets the value of the GPIO pin `id` to `value`.
-    fn write(&self, id: u32, value: bool) -> Result<()>;
+    fn write(&self, id: &str, value: bool) -> Result<()>;
+
+    /// Sets the `direction` of GPIO `id` as input or output.
+    fn set_direction(&self, id: &str, direction: PinDirection) -> Result<()>;
+
+    /// Drive the reset pin. The `reset` parameter represents whether or not the caller
+    /// wants to drive the chip into reset, _not_ the logic-level of the reset pin.
+    fn drive_reset(&self, reset: bool) -> Result<()>;
+
+    /// Set the requested strap value to the strapping pins.  Note: not all backends
+    /// support all settings.  An unsupported StrapConfig will result in an
+    /// `InvalidStrapConfig` error.
+    fn set_strap_pins(&self, strap: StrapConfig) -> Result<()>;
 }
--- sw/host/opentitanlib/src/util/mod.rs
+++ sw/host/opentitanlib/src/util/mod.rs
@@ -5,3 +5,19 @@
 pub mod bitfield;
 pub mod file;
 pub mod parse_int;
+
+/// The `collection` macro provides syntax for hash and set literals.
+#[macro_export]
+macro_rules! collection {
+    // map-like
+    ($($k:expr => $v:expr),* $(,)?) => {{
+        use std::iter::{Iterator, IntoIterator};
+        Iterator::collect(IntoIterator::into_iter([$(($k, $v),)*]))
+    }};
+
+    // set-like
+    ($($v:expr),* $(,)?) => {{
+        use std::iter::{Iterator, IntoIterator};
+        Iterator::collect(IntoIterator::into_iter([$($v),*]))
+    }};
+}
