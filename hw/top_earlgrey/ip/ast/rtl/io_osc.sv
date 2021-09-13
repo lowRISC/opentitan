e7ab37f03b83183aebbb6a2ffdbde77fff8911be
Timothy Chen <timothytim@google.com>
2021-09-13 16:07:15 -0700

    PR experiment
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- hw/top_earlgrey/ip/ast/rtl/io_osc.sv
+++ hw/top_earlgrey/ip/ast/rtl/io_osc.sv
@@ -52,7 +52,9 @@ assign io_clk_dly = 1'b1;
 ////////////////////////////////////////
 logic clk, en_osc;
 
-prim_clock_gating u_clk_ckgt (
+prim_clock_gating #(
+  .NoFpgaGate(1'b1)
+) u_clk_ckgt (
   .clk_i ( clk_io_ext_i ),
   .en_i ( en_osc ),
   .test_en_i ( 1'b0 ),
@@ -77,7 +79,9 @@ assign en_osc = en_osc_re || en_osc_fe;  // EN -> 1 || EN -> 0
 
 // Clock Output Buffer
 ////////////////////////////////////////
-prim_clock_buf u_buf (
+prim_clock_buf #(
+  .NoFpgaBuf(1'b1)
+) u_buf (
   .clk_i ( clk ),
   .clk_o ( io_clk_o )
 );
