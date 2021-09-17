2026e33f5c78536cb208289f60cd1e45bee212a3
Cindy Chen <chencindy@opentitan.org>
2021-09-17 10:29:11 -0700

    [dv/csr] Update write exclusion wdata value
    This PR fixes issue #8234. In current code, if a field is excluded from
    CSR write, we mask out the excluded fields to 0. This is under the
    assumption that all fields' reset value is 0, which is not always the
    case.
    So this PR adds a function to find the mirrored value of the excluded
    field from write, and update the wdata value accordingly.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

--- hw/dv/sv/csr_utils/csr_utils_pkg.sv
+++ hw/dv/sv/csr_utils/csr_utils_pkg.sv
@@ -710,6 +710,30 @@ package csr_utils_pkg;
     end
   endfunction
 
+  // Returns the write data value masked with excluded fields.
+  //
+  // Some fields in the CSR may be excluded from writes. In that case, we need to revert those
+  // fields to their mirrored values and write the rest of the fields with the given value.
+  function automatic uvm_reg_data_t get_csr_wdata_with_write_excl(
+      uvm_reg         csr,
+      uvm_reg_data_t  wdata,
+      csr_test_type_e csr_test_type,
+      csr_excl_item   m_csr_excl_item = get_excl_item(csr)
+  );
+    uvm_reg_field flds[$];
+    csr.get_fields(flds);
+
+    foreach (flds[i]) begin
+      if (m_csr_excl_item.is_excl(flds[i], CsrExclWrite, csr_test_type)) begin
+        `uvm_info(msg_id, $sformatf(
+                  "Retain mirrored value 0x%0h for field %0s due to CsrExclWrite exclusion",
+                  `gmv(flds[i]), flds[i].get_full_name()), UVM_MEDIUM)
+        wdata = get_csr_val_with_updated_field(flds[i], wdata, `gmv(flds[i]));
+      end
+    end
+    return wdata;
+  endfunction
+
   function automatic csr_excl_item get_excl_item(uvm_object ptr);
     csr_field_t       csr_or_fld;
     dv_base_reg_block blk;
