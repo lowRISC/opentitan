2b433c21e1580ee829b4256648cfa0c753efcfb5
Timothy Trippel <ttrippel@google.com>
2021-09-24 06:16:12 +0000

    [dif] remove need for DIF unit test header
    Simplify DIF autogen structure by removing need for unit test header.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

--- sw/device/lib/dif/dif_lc_ctrl_unittest.cc
+++ sw/device/lib/dif/dif_lc_ctrl_unittest.cc
@@ -2,11 +2,30 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "sw/device/lib/dif/dif_lc_ctrl_unittest.h"
+#include "sw/device/lib/dif/dif_lc_ctrl.h"
+
+#include <cstring>
+#include <limits>
+#include <utility>
+
+#include "gtest/gtest.h"
+#include "sw/device/lib/base/bitfield.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/base/testing/mock_mmio.h"
+
+#include "lc_ctrl_regs.h"  // Generated.
 
 namespace dif_lc_ctrl_unittest {
 namespace {
 using ::mock_mmio::LeInt;
+using ::mock_mmio::MmioTest;
+using ::mock_mmio::MockDevice;
+using ::testing::Test;
+
+class LcCtrlTest : public Test, public MmioTest {
+ protected:
+  dif_lc_ctrl_t lc_ = {.base_addr = dev().region()};
+};
 
 class InitTest : public LcCtrlTest {};
 
