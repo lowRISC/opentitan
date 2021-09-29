bbfa8069624156c039001558f75fac2b41d45990
Timothy Trippel <ttrippel@google.com>
2021-09-29 19:35:14 +0000

    [dif/otbn] Add autogen'd IRQ DIFs for OTBN.
    This partially addresses #8142, by checking-in auto-generated IRQ DIFs
    and supporting typedefs. These DIFs are not yet integrated into the source
    tree.
    
    This continues the longer term goal of completely auto-generating all
    IRQ DIFs, as described in #8142.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

---
+++ sw/device/lib/dif/autogen/dif_otbn_autogen.h
@@ -0,0 +1,163 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_OTBN_AUTOGEN_H_
+#define OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_OTBN_AUTOGEN_H_
+
+// This file is auto-generated.
+
+/**
+ * @file
+ * @brief <a href="/hw/ip/otbn/doc/">OTBN</a> Device Interface Functions
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "sw/device/lib/base/macros.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/dif/dif_base.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+/**
+ * A handle to otbn.
+ *
+ * This type should be treated as opaque by users.
+ */
+typedef struct dif_otbn {
+  /**
+   * The base address for the otbn hardware registers.
+   */
+  mmio_region_t base_addr;
+} dif_otbn_t;
+
+/**
+ * A otbn interrupt request type.
+ */
+typedef enum dif_otbn_irq {
+  /**
+   * OTBN has completed the operation
+   */
+  kDifOtbnIrqDone = 0,
+} dif_otbn_irq_t;
+
+/**
+ * A snapshot of the state of the interrupts for this IP.
+ *
+ * This is an opaque type, to be used with the `dif_otbn_irq_get_state()`
+ * function.
+ */
+typedef uint32_t dif_otbn_irq_state_snapshot_t;
+
+/**
+ * A snapshot of the enablement state of the interrupts for this IP.
+ *
+ * This is an opaque type, to be used with the
+ * `dif_otbn_irq_disable_all()` and `dif_otbn_irq_restore_all()`
+ * functions.
+ */
+typedef uint32_t dif_otbn_irq_enable_snapshot_t;
+
+/**
+ * Returns whether a particular interrupt is currently pending.
+ *
+ * @param otbn A otbn handle.
+ * @param irq An interrupt request.
+ * @param[out] is_pending Out-param for whether the interrupt is pending.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_otbn_irq_get_state(const dif_otbn_t *otbn,
+                                    dif_otbn_irq_state_snapshot_t *snapshot);
+
+/**
+ * Returns whether a particular interrupt is currently pending.
+ *
+ * @param otbn A otbn handle.
+ * @param irq An interrupt request.
+ * @param[out] is_pending Out-param for whether the interrupt is pending.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_otbn_irq_is_pending(const dif_otbn_t *otbn, dif_otbn_irq_t irq,
+                                     bool *is_pending);
+
+/**
+ * Acknowledges a particular interrupt, indicating to the hardware that it has
+ * been successfully serviced.
+ *
+ * @param otbn A otbn handle.
+ * @param irq An interrupt request.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_otbn_irq_acknowledge(const dif_otbn_t *otbn,
+                                      dif_otbn_irq_t irq);
+
+/**
+ * Checks whether a particular interrupt is currently enabled or disabled.
+ *
+ * @param otbn A otbn handle.
+ * @param irq An interrupt request.
+ * @param[out] state Out-param toggle state of the interrupt.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_otbn_irq_get_enabled(const dif_otbn_t *otbn,
+                                      dif_otbn_irq_t irq, dif_toggle_t *state);
+
+/**
+ * Sets whether a particular interrupt is currently enabled or disabled.
+ *
+ * @param otbn A otbn handle.
+ * @param irq An interrupt request.
+ * @param state The new toggle state for the interrupt.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_otbn_irq_set_enabled(const dif_otbn_t *otbn,
+                                      dif_otbn_irq_t irq, dif_toggle_t state);
+
+/**
+ * Forces a particular interrupt, causing it to be serviced as if hardware had
+ * asserted it.
+ *
+ * @param otbn A otbn handle.
+ * @param irq An interrupt request.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_otbn_irq_force(const dif_otbn_t *otbn, dif_otbn_irq_t irq);
+
+/**
+ * Disables all interrupts, optionally snapshotting all enable states for later
+ * restoration.
+ *
+ * @param otbn A otbn handle.
+ * @param[out] snapshot Out-param for the snapshot; may be `NULL`.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_otbn_irq_disable_all(const dif_otbn_t *otbn,
+                                      dif_otbn_irq_enable_snapshot_t *snapshot);
+
+/**
+ * Restores interrupts from the given (enable) snapshot.
+ *
+ * @param otbn A otbn handle.
+ * @param snapshot A snapshot to restore from.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_otbn_irq_restore_all(
+    const dif_otbn_t *otbn, const dif_otbn_irq_enable_snapshot_t *snapshot);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_OTBN_AUTOGEN_H_
---
+++ sw/device/lib/dif/autogen/dif_otbn_autogen_unittest.cc
@@ -0,0 +1,279 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This file is auto-generated.
+
+#include "sw/device/lib/dif/dif_otbn.h"
+
+#include "gtest/gtest.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/base/testing/mock_mmio.h"
+
+#include "otbn_regs.h"  // Generated.
+
+namespace dif_otbn_autogen_unittest {
+namespace {
+using ::mock_mmio::MmioTest;
+using ::mock_mmio::MockDevice;
+using ::testing::Test;
+
+class OtbnTest : public Test, public MmioTest {
+ protected:
+  dif_otbn_t otbn_ = {.base_addr = dev().region()};
+};
+
+using ::testing::Eq;
+
+class IrqGetStateTest : public OtbnTest {};
+
+TEST_F(IrqGetStateTest, NullArgs) {
+  dif_otbn_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_EQ(dif_otbn_irq_get_state(nullptr, &irq_snapshot), kDifBadArg);
+
+  EXPECT_EQ(dif_otbn_irq_get_state(&otbn_, nullptr), kDifBadArg);
+
+  EXPECT_EQ(dif_otbn_irq_get_state(nullptr, nullptr), kDifBadArg);
+}
+
+TEST_F(IrqGetStateTest, SuccessAllRaised) {
+  dif_otbn_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(OTBN_INTR_STATE_REG_OFFSET,
+                std::numeric_limits<uint32_t>::max());
+  EXPECT_EQ(dif_otbn_irq_get_state(&otbn_, &irq_snapshot), kDifOk);
+  EXPECT_EQ(irq_snapshot, std::numeric_limits<uint32_t>::max());
+}
+
+TEST_F(IrqGetStateTest, SuccessNoneRaised) {
+  dif_otbn_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(OTBN_INTR_STATE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_otbn_irq_get_state(&otbn_, &irq_snapshot), kDifOk);
+  EXPECT_EQ(irq_snapshot, 0);
+}
+
+class IrqIsPendingTest : public OtbnTest {};
+
+TEST_F(IrqIsPendingTest, NullArgs) {
+  bool is_pending;
+
+  EXPECT_EQ(dif_otbn_irq_is_pending(nullptr, kDifOtbnIrqDone, &is_pending),
+            kDifBadArg);
+
+  EXPECT_EQ(dif_otbn_irq_is_pending(&otbn_, kDifOtbnIrqDone, nullptr),
+            kDifBadArg);
+
+  EXPECT_EQ(dif_otbn_irq_is_pending(nullptr, kDifOtbnIrqDone, nullptr),
+            kDifBadArg);
+}
+
+TEST_F(IrqIsPendingTest, BadIrq) {
+  bool is_pending;
+  // All interrupt CSRs are 32 bit so interrupt 32 will be invalid.
+  EXPECT_EQ(dif_otbn_irq_is_pending(&otbn_, (dif_otbn_irq_t)32, &is_pending),
+            kDifBadArg);
+}
+
+TEST_F(IrqIsPendingTest, Success) {
+  bool irq_state;
+
+  // Get the first IRQ state.
+  irq_state = false;
+  EXPECT_READ32(OTBN_INTR_STATE_REG_OFFSET, {{OTBN_INTR_STATE_DONE_BIT, true}});
+  EXPECT_EQ(dif_otbn_irq_is_pending(&otbn_, kDifOtbnIrqDone, &irq_state),
+            kDifOk);
+  EXPECT_TRUE(irq_state);
+
+  // Get the last IRQ state.
+  irq_state = true;
+  EXPECT_READ32(OTBN_INTR_STATE_REG_OFFSET,
+                {{OTBN_INTR_STATE_DONE_BIT, false}});
+  EXPECT_EQ(dif_otbn_irq_is_pending(&otbn_, kDifOtbnIrqDone, &irq_state),
+            kDifOk);
+  EXPECT_FALSE(irq_state);
+}
+
+class IrqAcknowledgeTest : public OtbnTest {};
+
+TEST_F(IrqAcknowledgeTest, NullArgs) {
+  EXPECT_EQ(dif_otbn_irq_acknowledge(nullptr, kDifOtbnIrqDone), kDifBadArg);
+}
+
+TEST_F(IrqAcknowledgeTest, BadIrq) {
+  EXPECT_EQ(dif_otbn_irq_acknowledge(nullptr, (dif_otbn_irq_t)32), kDifBadArg);
+}
+
+TEST_F(IrqAcknowledgeTest, Success) {
+  // Clear the first IRQ state.
+  EXPECT_WRITE32(OTBN_INTR_STATE_REG_OFFSET,
+                 {{OTBN_INTR_STATE_DONE_BIT, true}});
+  EXPECT_EQ(dif_otbn_irq_acknowledge(&otbn_, kDifOtbnIrqDone), kDifOk);
+
+  // Clear the last IRQ state.
+  EXPECT_WRITE32(OTBN_INTR_STATE_REG_OFFSET,
+                 {{OTBN_INTR_STATE_DONE_BIT, true}});
+  EXPECT_EQ(dif_otbn_irq_acknowledge(&otbn_, kDifOtbnIrqDone), kDifOk);
+}
+
+class IrqGetEnabledTest : public OtbnTest {};
+
+TEST_F(IrqGetEnabledTest, NullArgs) {
+  dif_toggle_t irq_state;
+
+  EXPECT_EQ(dif_otbn_irq_get_enabled(nullptr, kDifOtbnIrqDone, &irq_state),
+            kDifBadArg);
+
+  EXPECT_EQ(dif_otbn_irq_get_enabled(&otbn_, kDifOtbnIrqDone, nullptr),
+            kDifBadArg);
+
+  EXPECT_EQ(dif_otbn_irq_get_enabled(nullptr, kDifOtbnIrqDone, nullptr),
+            kDifBadArg);
+}
+
+TEST_F(IrqGetEnabledTest, BadIrq) {
+  dif_toggle_t irq_state;
+
+  EXPECT_EQ(dif_otbn_irq_get_enabled(&otbn_, (dif_otbn_irq_t)32, &irq_state),
+            kDifBadArg);
+}
+
+TEST_F(IrqGetEnabledTest, Success) {
+  dif_toggle_t irq_state;
+
+  // First IRQ is enabled.
+  irq_state = kDifToggleDisabled;
+  EXPECT_READ32(OTBN_INTR_ENABLE_REG_OFFSET,
+                {{OTBN_INTR_ENABLE_DONE_BIT, true}});
+  EXPECT_EQ(dif_otbn_irq_get_enabled(&otbn_, kDifOtbnIrqDone, &irq_state),
+            kDifOk);
+  EXPECT_EQ(irq_state, kDifToggleEnabled);
+
+  // Last IRQ is disabled.
+  irq_state = kDifToggleEnabled;
+  EXPECT_READ32(OTBN_INTR_ENABLE_REG_OFFSET,
+                {{OTBN_INTR_ENABLE_DONE_BIT, false}});
+  EXPECT_EQ(dif_otbn_irq_get_enabled(&otbn_, kDifOtbnIrqDone, &irq_state),
+            kDifOk);
+  EXPECT_EQ(irq_state, kDifToggleDisabled);
+}
+
+class IrqSetEnabledTest : public OtbnTest {};
+
+TEST_F(IrqSetEnabledTest, NullArgs) {
+  dif_toggle_t irq_state = kDifToggleEnabled;
+
+  EXPECT_EQ(dif_otbn_irq_set_enabled(nullptr, kDifOtbnIrqDone, irq_state),
+            kDifBadArg);
+}
+
+TEST_F(IrqSetEnabledTest, BadIrq) {
+  dif_toggle_t irq_state = kDifToggleEnabled;
+
+  EXPECT_EQ(dif_otbn_irq_set_enabled(&otbn_, (dif_otbn_irq_t)32, irq_state),
+            kDifBadArg);
+}
+
+TEST_F(IrqSetEnabledTest, Success) {
+  dif_toggle_t irq_state;
+
+  // Enable first IRQ.
+  irq_state = kDifToggleEnabled;
+  EXPECT_MASK32(OTBN_INTR_ENABLE_REG_OFFSET,
+                {{OTBN_INTR_ENABLE_DONE_BIT, 0x1, true}});
+  EXPECT_EQ(dif_otbn_irq_set_enabled(&otbn_, kDifOtbnIrqDone, irq_state),
+            kDifOk);
+
+  // Disable last IRQ.
+  irq_state = kDifToggleDisabled;
+  EXPECT_MASK32(OTBN_INTR_ENABLE_REG_OFFSET,
+                {{OTBN_INTR_ENABLE_DONE_BIT, 0x1, false}});
+  EXPECT_EQ(dif_otbn_irq_set_enabled(&otbn_, kDifOtbnIrqDone, irq_state),
+            kDifOk);
+}
+
+class IrqForceTest : public OtbnTest {};
+
+TEST_F(IrqForceTest, NullArgs) {
+  EXPECT_EQ(dif_otbn_irq_force(nullptr, kDifOtbnIrqDone), kDifBadArg);
+}
+
+TEST_F(IrqForceTest, BadIrq) {
+  EXPECT_EQ(dif_otbn_irq_force(nullptr, (dif_otbn_irq_t)32), kDifBadArg);
+}
+
+TEST_F(IrqForceTest, Success) {
+  // Force first IRQ.
+  EXPECT_WRITE32(OTBN_INTR_TEST_REG_OFFSET, {{OTBN_INTR_TEST_DONE_BIT, true}});
+  EXPECT_EQ(dif_otbn_irq_force(&otbn_, kDifOtbnIrqDone), kDifOk);
+
+  // Force last IRQ.
+  EXPECT_WRITE32(OTBN_INTR_TEST_REG_OFFSET, {{OTBN_INTR_TEST_DONE_BIT, true}});
+  EXPECT_EQ(dif_otbn_irq_force(&otbn_, kDifOtbnIrqDone), kDifOk);
+}
+
+class IrqDisableAllTest : public OtbnTest {};
+
+TEST_F(IrqDisableAllTest, NullArgs) {
+  dif_otbn_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_EQ(dif_otbn_irq_disable_all(nullptr, &irq_snapshot), kDifBadArg);
+
+  EXPECT_EQ(dif_otbn_irq_disable_all(nullptr, nullptr), kDifBadArg);
+}
+
+TEST_F(IrqDisableAllTest, SuccessNoSnapshot) {
+  EXPECT_WRITE32(OTBN_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_otbn_irq_disable_all(&otbn_, nullptr), kDifOk);
+}
+
+TEST_F(IrqDisableAllTest, SuccessSnapshotAllDisabled) {
+  dif_otbn_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(OTBN_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_WRITE32(OTBN_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_otbn_irq_disable_all(&otbn_, &irq_snapshot), kDifOk);
+  EXPECT_EQ(irq_snapshot, 0);
+}
+
+TEST_F(IrqDisableAllTest, SuccessSnapshotAllEnabled) {
+  dif_otbn_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(OTBN_INTR_ENABLE_REG_OFFSET,
+                std::numeric_limits<uint32_t>::max());
+  EXPECT_WRITE32(OTBN_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_otbn_irq_disable_all(&otbn_, &irq_snapshot), kDifOk);
+  EXPECT_EQ(irq_snapshot, std::numeric_limits<uint32_t>::max());
+}
+
+class IrqRestoreAllTest : public OtbnTest {};
+
+TEST_F(IrqRestoreAllTest, NullArgs) {
+  dif_otbn_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_EQ(dif_otbn_irq_restore_all(nullptr, &irq_snapshot), kDifBadArg);
+
+  EXPECT_EQ(dif_otbn_irq_restore_all(&otbn_, nullptr), kDifBadArg);
+
+  EXPECT_EQ(dif_otbn_irq_restore_all(nullptr, nullptr), kDifBadArg);
+}
+
+TEST_F(IrqRestoreAllTest, SuccessAllEnabled) {
+  dif_otbn_irq_enable_snapshot_t irq_snapshot =
+      std::numeric_limits<uint32_t>::max();
+
+  EXPECT_WRITE32(OTBN_INTR_ENABLE_REG_OFFSET,
+                 std::numeric_limits<uint32_t>::max());
+  EXPECT_EQ(dif_otbn_irq_restore_all(&otbn_, &irq_snapshot), kDifOk);
+}
+
+TEST_F(IrqRestoreAllTest, SuccessAllDisabled) {
+  dif_otbn_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_WRITE32(OTBN_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_otbn_irq_restore_all(&otbn_, &irq_snapshot), kDifOk);
+}
+
+}  // namespace
+}  // namespace dif_otbn_autogen_unittest
