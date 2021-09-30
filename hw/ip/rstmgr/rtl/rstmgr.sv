b62458ece1f72d5c773374b494c2291a90bb9f52
Guillermo Maturana <maturana@google.com>
2021-09-30 12:54:25 -0700

    [rtl/rstmgr] Remove redundant import
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- hw/ip/rstmgr/rtl/rstmgr.sv
+++ hw/ip/rstmgr/rtl/rstmgr.sv
@@ -9,7 +9,10 @@
 `include "prim_assert.sv"
 
 // This top level controller is fairly hardcoded right now, but will be switched to a template
-module rstmgr import rstmgr_pkg::*; (
+module rstmgr
+  import rstmgr_pkg::*;
+  import rstmgr_reg_pkg::*;
+(
   // Primary module clocks
   input clk_i,
   input rst_ni, // this is currently connected to top level reset, but will change once ast is in
@@ -46,8 +49,6 @@ module rstmgr import rstmgr_pkg::*; (
   output rstmgr_out_t resets_o
 
 );
-
-  import rstmgr_reg_pkg::*;
 
   // receive POR and stretch
   // The por is at first stretched and synced on clk_aon
