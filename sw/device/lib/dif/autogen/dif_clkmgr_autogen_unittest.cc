4d65aca4cd14f7a0f06721bfedccb8295789c447
Timothy Trippel <ttrippel@google.com>
2021-09-30 23:25:47 +0000

    [dif/clkmgr] Add autogen'd DIF artifacts for clkmgr.
    This partially addresses #8142, by checking-in auto-generated DIF
    typedefs.
    
    Some things to note:
    - The there are no IRQs associated with the clkmgr IP block, but there
      are **alerts**. In the near future, alert-related DIFs will be
      autogenerated and rely on the supporting autogenerated typedefs in
      this commit.
    - These auto-generated DIF components are **not** yet integrated into
      the source tree.
    
    This continues the longer term goal of completely auto-generating some
    of the DIFs, as described in #8142.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

---
+++ sw/device/lib/dif/autogen/dif_clkmgr_autogen_unittest.cc
@@ -0,0 +1,27 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This file is auto-generated.
+
+#include "sw/device/lib/dif/dif_clkmgr.h"
+
+#include "gtest/gtest.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/base/testing/mock_mmio.h"
+
+#include "clkmgr_regs.h"  // Generated.
+
+namespace dif_clkmgr_autogen_unittest {
+namespace {
+using ::mock_mmio::MmioTest;
+using ::mock_mmio::MockDevice;
+using ::testing::Test;
+
+class ClkmgrTest : public Test, public MmioTest {
+ protected:
+  dif_clkmgr_t clkmgr_ = {.base_addr = dev().region()};
+};
+
+}  // namespace
+}  // namespace dif_clkmgr_autogen_unittest
