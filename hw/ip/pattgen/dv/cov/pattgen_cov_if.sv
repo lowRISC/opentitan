5ccc3c37c73eebebadf9465a60439ccbe11d9f28
alex sapozhnikov <alex.sapozhnikov@wdc.com>
2021-10-02 11:50:05 -0700

    corrected license
    Signed-off-by: alex sapozhnikov <alex.sapozhnikov@wdc.com>

--- hw/ip/pattgen/dv/cov/pattgen_cov_if.sv
+++ hw/ip/pattgen/dv/cov/pattgen_cov_if.sv
@@ -1,7 +1,7 @@
 // Copyright lowRISC contributors.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
-//
+
 // Implements functional coverage for PATTGEN.
 interface pattgen_cov_if (
   input logic clk_i
