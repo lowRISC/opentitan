5549495cc1a28144528e495791102703b9ecdca6
Miguel Young de la Sota <mcyoung@google.com>
2021-10-04 13:15:10 -0400

    [lib] Add constant-time comparisons to hardened.h
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

--- sw/device/lib/base/hardened.c
+++ sw/device/lib/base/hardened.c
@@ -6,6 +6,20 @@
 
 // Instantiate inline functions in this TU.
 extern uint32_t launder32(uint32_t);
-extern uint32_t launderw(uintptr_t);
+extern uintptr_t launderw(uintptr_t);
 extern void barrier32(uint32_t);
 extern void barrierw(uintptr_t);
+
+extern ct_bool32_t ct_sltz32(int32_t);
+extern ct_bool32_t ct_sltu32(uint32_t, uint32_t);
+extern ct_bool32_t ct_sgeu32(uint32_t, uint32_t);
+extern ct_bool32_t ct_seqz32(uint32_t);
+extern ct_bool32_t ct_seq32(uint32_t, uint32_t);
+extern uint32_t ct_cmov32(ct_bool32_t, uint32_t, uint32_t);
+
+extern ct_boolw_t ct_sltzw(intptr_t);
+extern ct_boolw_t ct_sltuw(uintptr_t, uintptr_t);
+extern ct_boolw_t ct_sgeuw(uintptr_t, uintptr_t);
+extern ct_boolw_t ct_seqzw(uintptr_t);
+extern ct_boolw_t ct_seqw(uintptr_t, uintptr_t);
+extern uintptr_t ct_cmovw(ct_boolw_t, uintptr_t, uintptr_t);
