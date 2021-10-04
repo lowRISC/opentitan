25499607b4c2921d1bb7afd10caad664af868969
Timothy Trippel <ttrippel@google.com>
2021-10-04 18:22:27 +0000

    [dif/spi_device] Add autogen'd IRQ DIFs for SPI Device.
    This partially addresses #8142, by checking-in auto-generated IRQ DIFs
    and supporting typedefs. These DIFs are not yet integrated into the source
    tree, but will be in a following commit.
    
    This continues the longer term goal of completely auto-generating all
    IRQ DIFs, as described in #8142.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

---
+++ sw/device/lib/dif/autogen/dif_spi_device_autogen.h
@@ -0,0 +1,194 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_SPI_DEVICE_AUTOGEN_H_
+#define OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_SPI_DEVICE_AUTOGEN_H_
+
+// This file is auto-generated.
+
+/**
+ * @file
+ * @brief <a href="/hw/ip/spi_device/doc/">SPI_DEVICE</a> Device Interface
+ * Functions
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "sw/device/lib/base/macros.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/dif/dif_base.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+/**
+ * A handle to spi_device.
+ *
+ * This type should be treated as opaque by users.
+ */
+typedef struct dif_spi_device {
+  /**
+   * The base address for the spi_device hardware registers.
+   */
+  mmio_region_t base_addr;
+} dif_spi_device_t;
+
+/**
+ * A spi_device interrupt request type.
+ */
+typedef enum dif_spi_device_irq {
+  /**
+   * RX SRAM FIFO Full
+   */
+  kDifSpiDeviceIrqRxf = 0,
+  /**
+   * RX SRAM FIFO is above the level
+   */
+  kDifSpiDeviceIrqRxlvl = 1,
+  /**
+   * TX SRAM FIFO is under the level
+   */
+  kDifSpiDeviceIrqTxlvl = 2,
+  /**
+   * SDI in FwMode has error
+   */
+  kDifSpiDeviceIrqRxerr = 3,
+  /**
+   * RX Async FIFO overflow
+   */
+  kDifSpiDeviceIrqRxoverflow = 4,
+  /**
+   * TX Async FIFO underflow
+   */
+  kDifSpiDeviceIrqTxunderflow = 5,
+  /**
+   * TPM Command/Address buffer available
+   */
+  kDifSpiDeviceIrqTpmCmdaddrNotempty = 6,
+} dif_spi_device_irq_t;
+
+/**
+ * A snapshot of the state of the interrupts for this IP.
+ *
+ * This is an opaque type, to be used with the `dif_spi_device_irq_get_state()`
+ * function.
+ */
+typedef uint32_t dif_spi_device_irq_state_snapshot_t;
+
+/**
+ * A snapshot of the enablement state of the interrupts for this IP.
+ *
+ * This is an opaque type, to be used with the
+ * `dif_spi_device_irq_disable_all()` and `dif_spi_device_irq_restore_all()`
+ * functions.
+ */
+typedef uint32_t dif_spi_device_irq_enable_snapshot_t;
+
+/**
+ * Returns whether a particular interrupt is currently pending.
+ *
+ * @param spi_device A spi_device handle.
+ * @param[out] snapshot Out-param for interrupt state snapshot.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_spi_device_irq_get_state(
+    const dif_spi_device_t *spi_device,
+    dif_spi_device_irq_state_snapshot_t *snapshot);
+
+/**
+ * Returns whether a particular interrupt is currently pending.
+ *
+ * @param spi_device A spi_device handle.
+ * @param irq An interrupt request.
+ * @param[out] is_pending Out-param for whether the interrupt is pending.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_spi_device_irq_is_pending(const dif_spi_device_t *spi_device,
+                                           dif_spi_device_irq_t irq,
+                                           bool *is_pending);
+
+/**
+ * Acknowledges a particular interrupt, indicating to the hardware that it has
+ * been successfully serviced.
+ *
+ * @param spi_device A spi_device handle.
+ * @param irq An interrupt request.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_spi_device_irq_acknowledge(const dif_spi_device_t *spi_device,
+                                            dif_spi_device_irq_t irq);
+
+/**
+ * Checks whether a particular interrupt is currently enabled or disabled.
+ *
+ * @param spi_device A spi_device handle.
+ * @param irq An interrupt request.
+ * @param[out] state Out-param toggle state of the interrupt.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_spi_device_irq_get_enabled(const dif_spi_device_t *spi_device,
+                                            dif_spi_device_irq_t irq,
+                                            dif_toggle_t *state);
+
+/**
+ * Sets whether a particular interrupt is currently enabled or disabled.
+ *
+ * @param spi_device A spi_device handle.
+ * @param irq An interrupt request.
+ * @param state The new toggle state for the interrupt.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_spi_device_irq_set_enabled(const dif_spi_device_t *spi_device,
+                                            dif_spi_device_irq_t irq,
+                                            dif_toggle_t state);
+
+/**
+ * Forces a particular interrupt, causing it to be serviced as if hardware had
+ * asserted it.
+ *
+ * @param spi_device A spi_device handle.
+ * @param irq An interrupt request.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_spi_device_irq_force(const dif_spi_device_t *spi_device,
+                                      dif_spi_device_irq_t irq);
+
+/**
+ * Disables all interrupts, optionally snapshotting all enable states for later
+ * restoration.
+ *
+ * @param spi_device A spi_device handle.
+ * @param[out] snapshot Out-param for the snapshot; may be `NULL`.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_spi_device_irq_disable_all(
+    const dif_spi_device_t *spi_device,
+    dif_spi_device_irq_enable_snapshot_t *snapshot);
+
+/**
+ * Restores interrupts from the given (enable) snapshot.
+ *
+ * @param spi_device A spi_device handle.
+ * @param snapshot A snapshot to restore from.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_spi_device_irq_restore_all(
+    const dif_spi_device_t *spi_device,
+    const dif_spi_device_irq_enable_snapshot_t *snapshot);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_SPI_DEVICE_AUTOGEN_H_
---
+++ sw/device/lib/dif/autogen/dif_spi_device_autogen_unittest.cc
@@ -0,0 +1,316 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This file is auto-generated.
+
+#include "sw/device/lib/dif/dif_spi_device.h"
+
+#include "gtest/gtest.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/base/testing/mock_mmio.h"
+
+#include "spi_device_regs.h"  // Generated.
+
+namespace dif_spi_device_autogen_unittest {
+namespace {
+using ::mock_mmio::MmioTest;
+using ::mock_mmio::MockDevice;
+using ::testing::Test;
+
+class SpiDeviceTest : public Test, public MmioTest {
+ protected:
+  dif_spi_device_t spi_device_ = {.base_addr = dev().region()};
+};
+
+using ::testing::Eq;
+
+class IrqGetStateTest : public SpiDeviceTest {};
+
+TEST_F(IrqGetStateTest, NullArgs) {
+  dif_spi_device_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_EQ(dif_spi_device_irq_get_state(nullptr, &irq_snapshot), kDifBadArg);
+
+  EXPECT_EQ(dif_spi_device_irq_get_state(&spi_device_, nullptr), kDifBadArg);
+
+  EXPECT_EQ(dif_spi_device_irq_get_state(nullptr, nullptr), kDifBadArg);
+}
+
+TEST_F(IrqGetStateTest, SuccessAllRaised) {
+  dif_spi_device_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(SPI_DEVICE_INTR_STATE_REG_OFFSET,
+                std::numeric_limits<uint32_t>::max());
+  EXPECT_EQ(dif_spi_device_irq_get_state(&spi_device_, &irq_snapshot), kDifOk);
+  EXPECT_EQ(irq_snapshot, std::numeric_limits<uint32_t>::max());
+}
+
+TEST_F(IrqGetStateTest, SuccessNoneRaised) {
+  dif_spi_device_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(SPI_DEVICE_INTR_STATE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_spi_device_irq_get_state(&spi_device_, &irq_snapshot), kDifOk);
+  EXPECT_EQ(irq_snapshot, 0);
+}
+
+class IrqIsPendingTest : public SpiDeviceTest {};
+
+TEST_F(IrqIsPendingTest, NullArgs) {
+  bool is_pending;
+
+  EXPECT_EQ(
+      dif_spi_device_irq_is_pending(nullptr, kDifSpiDeviceIrqRxf, &is_pending),
+      kDifBadArg);
+
+  EXPECT_EQ(
+      dif_spi_device_irq_is_pending(&spi_device_, kDifSpiDeviceIrqRxf, nullptr),
+      kDifBadArg);
+
+  EXPECT_EQ(
+      dif_spi_device_irq_is_pending(nullptr, kDifSpiDeviceIrqRxf, nullptr),
+      kDifBadArg);
+}
+
+TEST_F(IrqIsPendingTest, BadIrq) {
+  bool is_pending;
+  // All interrupt CSRs are 32 bit so interrupt 32 will be invalid.
+  EXPECT_EQ(
+      dif_spi_device_irq_is_pending(
+          &spi_device_, static_cast<dif_spi_device_irq_t>(32), &is_pending),
+      kDifBadArg);
+}
+
+TEST_F(IrqIsPendingTest, Success) {
+  bool irq_state;
+
+  // Get the first IRQ state.
+  irq_state = false;
+  EXPECT_READ32(SPI_DEVICE_INTR_STATE_REG_OFFSET,
+                {{SPI_DEVICE_INTR_STATE_RXF_BIT, true}});
+  EXPECT_EQ(dif_spi_device_irq_is_pending(&spi_device_, kDifSpiDeviceIrqRxf,
+                                          &irq_state),
+            kDifOk);
+  EXPECT_TRUE(irq_state);
+
+  // Get the last IRQ state.
+  irq_state = true;
+  EXPECT_READ32(SPI_DEVICE_INTR_STATE_REG_OFFSET,
+                {{SPI_DEVICE_INTR_STATE_TPM_CMDADDR_NOTEMPTY_BIT, false}});
+  EXPECT_EQ(dif_spi_device_irq_is_pending(
+                &spi_device_, kDifSpiDeviceIrqTpmCmdaddrNotempty, &irq_state),
+            kDifOk);
+  EXPECT_FALSE(irq_state);
+}
+
+class IrqAcknowledgeTest : public SpiDeviceTest {};
+
+TEST_F(IrqAcknowledgeTest, NullArgs) {
+  EXPECT_EQ(dif_spi_device_irq_acknowledge(nullptr, kDifSpiDeviceIrqRxf),
+            kDifBadArg);
+}
+
+TEST_F(IrqAcknowledgeTest, BadIrq) {
+  EXPECT_EQ(dif_spi_device_irq_acknowledge(
+                nullptr, static_cast<dif_spi_device_irq_t>(32)),
+            kDifBadArg);
+}
+
+TEST_F(IrqAcknowledgeTest, Success) {
+  // Clear the first IRQ state.
+  EXPECT_WRITE32(SPI_DEVICE_INTR_STATE_REG_OFFSET,
+                 {{SPI_DEVICE_INTR_STATE_RXF_BIT, true}});
+  EXPECT_EQ(dif_spi_device_irq_acknowledge(&spi_device_, kDifSpiDeviceIrqRxf),
+            kDifOk);
+
+  // Clear the last IRQ state.
+  EXPECT_WRITE32(SPI_DEVICE_INTR_STATE_REG_OFFSET,
+                 {{SPI_DEVICE_INTR_STATE_TPM_CMDADDR_NOTEMPTY_BIT, true}});
+  EXPECT_EQ(dif_spi_device_irq_acknowledge(&spi_device_,
+                                           kDifSpiDeviceIrqTpmCmdaddrNotempty),
+            kDifOk);
+}
+
+class IrqGetEnabledTest : public SpiDeviceTest {};
+
+TEST_F(IrqGetEnabledTest, NullArgs) {
+  dif_toggle_t irq_state;
+
+  EXPECT_EQ(
+      dif_spi_device_irq_get_enabled(nullptr, kDifSpiDeviceIrqRxf, &irq_state),
+      kDifBadArg);
+
+  EXPECT_EQ(dif_spi_device_irq_get_enabled(&spi_device_, kDifSpiDeviceIrqRxf,
+                                           nullptr),
+            kDifBadArg);
+
+  EXPECT_EQ(
+      dif_spi_device_irq_get_enabled(nullptr, kDifSpiDeviceIrqRxf, nullptr),
+      kDifBadArg);
+}
+
+TEST_F(IrqGetEnabledTest, BadIrq) {
+  dif_toggle_t irq_state;
+
+  EXPECT_EQ(
+      dif_spi_device_irq_get_enabled(
+          &spi_device_, static_cast<dif_spi_device_irq_t>(32), &irq_state),
+      kDifBadArg);
+}
+
+TEST_F(IrqGetEnabledTest, Success) {
+  dif_toggle_t irq_state;
+
+  // First IRQ is enabled.
+  irq_state = kDifToggleDisabled;
+  EXPECT_READ32(SPI_DEVICE_INTR_ENABLE_REG_OFFSET,
+                {{SPI_DEVICE_INTR_ENABLE_RXF_BIT, true}});
+  EXPECT_EQ(dif_spi_device_irq_get_enabled(&spi_device_, kDifSpiDeviceIrqRxf,
+                                           &irq_state),
+            kDifOk);
+  EXPECT_EQ(irq_state, kDifToggleEnabled);
+
+  // Last IRQ is disabled.
+  irq_state = kDifToggleEnabled;
+  EXPECT_READ32(SPI_DEVICE_INTR_ENABLE_REG_OFFSET,
+                {{SPI_DEVICE_INTR_ENABLE_TPM_CMDADDR_NOTEMPTY_BIT, false}});
+  EXPECT_EQ(dif_spi_device_irq_get_enabled(
+                &spi_device_, kDifSpiDeviceIrqTpmCmdaddrNotempty, &irq_state),
+            kDifOk);
+  EXPECT_EQ(irq_state, kDifToggleDisabled);
+}
+
+class IrqSetEnabledTest : public SpiDeviceTest {};
+
+TEST_F(IrqSetEnabledTest, NullArgs) {
+  dif_toggle_t irq_state = kDifToggleEnabled;
+
+  EXPECT_EQ(
+      dif_spi_device_irq_set_enabled(nullptr, kDifSpiDeviceIrqRxf, irq_state),
+      kDifBadArg);
+}
+
+TEST_F(IrqSetEnabledTest, BadIrq) {
+  dif_toggle_t irq_state = kDifToggleEnabled;
+
+  EXPECT_EQ(dif_spi_device_irq_set_enabled(
+                &spi_device_, static_cast<dif_spi_device_irq_t>(32), irq_state),
+            kDifBadArg);
+}
+
+TEST_F(IrqSetEnabledTest, Success) {
+  dif_toggle_t irq_state;
+
+  // Enable first IRQ.
+  irq_state = kDifToggleEnabled;
+  EXPECT_MASK32(SPI_DEVICE_INTR_ENABLE_REG_OFFSET,
+                {{SPI_DEVICE_INTR_ENABLE_RXF_BIT, 0x1, true}});
+  EXPECT_EQ(dif_spi_device_irq_set_enabled(&spi_device_, kDifSpiDeviceIrqRxf,
+                                           irq_state),
+            kDifOk);
+
+  // Disable last IRQ.
+  irq_state = kDifToggleDisabled;
+  EXPECT_MASK32(
+      SPI_DEVICE_INTR_ENABLE_REG_OFFSET,
+      {{SPI_DEVICE_INTR_ENABLE_TPM_CMDADDR_NOTEMPTY_BIT, 0x1, false}});
+  EXPECT_EQ(dif_spi_device_irq_set_enabled(
+                &spi_device_, kDifSpiDeviceIrqTpmCmdaddrNotempty, irq_state),
+            kDifOk);
+}
+
+class IrqForceTest : public SpiDeviceTest {};
+
+TEST_F(IrqForceTest, NullArgs) {
+  EXPECT_EQ(dif_spi_device_irq_force(nullptr, kDifSpiDeviceIrqRxf), kDifBadArg);
+}
+
+TEST_F(IrqForceTest, BadIrq) {
+  EXPECT_EQ(
+      dif_spi_device_irq_force(nullptr, static_cast<dif_spi_device_irq_t>(32)),
+      kDifBadArg);
+}
+
+TEST_F(IrqForceTest, Success) {
+  // Force first IRQ.
+  EXPECT_WRITE32(SPI_DEVICE_INTR_TEST_REG_OFFSET,
+                 {{SPI_DEVICE_INTR_TEST_RXF_BIT, true}});
+  EXPECT_EQ(dif_spi_device_irq_force(&spi_device_, kDifSpiDeviceIrqRxf),
+            kDifOk);
+
+  // Force last IRQ.
+  EXPECT_WRITE32(SPI_DEVICE_INTR_TEST_REG_OFFSET,
+                 {{SPI_DEVICE_INTR_TEST_TPM_CMDADDR_NOTEMPTY_BIT, true}});
+  EXPECT_EQ(dif_spi_device_irq_force(&spi_device_,
+                                     kDifSpiDeviceIrqTpmCmdaddrNotempty),
+            kDifOk);
+}
+
+class IrqDisableAllTest : public SpiDeviceTest {};
+
+TEST_F(IrqDisableAllTest, NullArgs) {
+  dif_spi_device_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_EQ(dif_spi_device_irq_disable_all(nullptr, &irq_snapshot), kDifBadArg);
+
+  EXPECT_EQ(dif_spi_device_irq_disable_all(nullptr, nullptr), kDifBadArg);
+}
+
+TEST_F(IrqDisableAllTest, SuccessNoSnapshot) {
+  EXPECT_WRITE32(SPI_DEVICE_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_spi_device_irq_disable_all(&spi_device_, nullptr), kDifOk);
+}
+
+TEST_F(IrqDisableAllTest, SuccessSnapshotAllDisabled) {
+  dif_spi_device_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(SPI_DEVICE_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_WRITE32(SPI_DEVICE_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_spi_device_irq_disable_all(&spi_device_, &irq_snapshot),
+            kDifOk);
+  EXPECT_EQ(irq_snapshot, 0);
+}
+
+TEST_F(IrqDisableAllTest, SuccessSnapshotAllEnabled) {
+  dif_spi_device_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(SPI_DEVICE_INTR_ENABLE_REG_OFFSET,
+                std::numeric_limits<uint32_t>::max());
+  EXPECT_WRITE32(SPI_DEVICE_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_spi_device_irq_disable_all(&spi_device_, &irq_snapshot),
+            kDifOk);
+  EXPECT_EQ(irq_snapshot, std::numeric_limits<uint32_t>::max());
+}
+
+class IrqRestoreAllTest : public SpiDeviceTest {};
+
+TEST_F(IrqRestoreAllTest, NullArgs) {
+  dif_spi_device_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_EQ(dif_spi_device_irq_restore_all(nullptr, &irq_snapshot), kDifBadArg);
+
+  EXPECT_EQ(dif_spi_device_irq_restore_all(&spi_device_, nullptr), kDifBadArg);
+
+  EXPECT_EQ(dif_spi_device_irq_restore_all(nullptr, nullptr), kDifBadArg);
+}
+
+TEST_F(IrqRestoreAllTest, SuccessAllEnabled) {
+  dif_spi_device_irq_enable_snapshot_t irq_snapshot =
+      std::numeric_limits<uint32_t>::max();
+
+  EXPECT_WRITE32(SPI_DEVICE_INTR_ENABLE_REG_OFFSET,
+                 std::numeric_limits<uint32_t>::max());
+  EXPECT_EQ(dif_spi_device_irq_restore_all(&spi_device_, &irq_snapshot),
+            kDifOk);
+}
+
+TEST_F(IrqRestoreAllTest, SuccessAllDisabled) {
+  dif_spi_device_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_WRITE32(SPI_DEVICE_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_spi_device_irq_restore_all(&spi_device_, &irq_snapshot),
+            kDifOk);
+}
+
+}  // namespace
+}  // namespace dif_spi_device_autogen_unittest
