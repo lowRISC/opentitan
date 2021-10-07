771337c2f39cbed63c43c5a398d0db0a1c9dc813
Timothy Trippel <ttrippel@google.com>
2021-10-07 06:07:51 +0000

    [dif/usbdev] Add autogen'd IRQ DIFs for usbdev.
    This partially addresses #8142, by checking-in auto-generated IRQ DIFs
    and supporting typedefs. These DIFs are not yet integrated into the source
    tree, but will be in a following commit.
    
    This continues the longer term goal of completely auto-generating all
    IRQ DIFs, as described in #8142.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

---
+++ sw/device/lib/dif/autogen/dif_usbdev_autogen.h
@@ -0,0 +1,242 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_USBDEV_AUTOGEN_H_
+#define OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_USBDEV_AUTOGEN_H_
+
+// This file is auto-generated.
+
+/**
+ * @file
+ * @brief <a href="/hw/ip/usbdev/doc/">USBDEV</a> Device Interface Functions
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "sw/device/lib/base/macros.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/dif/dif_base.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+/**
+ * A handle to usbdev.
+ *
+ * This type should be treated as opaque by users.
+ */
+typedef struct dif_usbdev {
+  /**
+   * The base address for the usbdev hardware registers.
+   */
+  mmio_region_t base_addr;
+} dif_usbdev_t;
+
+/**
+ * A usbdev interrupt request type.
+ */
+typedef enum dif_usbdev_irq {
+  /**
+   * Raised if a packet was received using an OUT or SETUP transaction.
+   */
+  kDifUsbdevIrqPktReceived = 0,
+  /**
+   * Raised if a packet was sent as part of an IN transaction.
+   */
+  kDifUsbdevIrqPktSent = 1,
+  /**
+   * Raised if VBUS is lost thus the link is disconnected.
+   */
+  kDifUsbdevIrqDisconnected = 2,
+  /**
+   * Raised if link is active but SOF was not received from host for 4.096 ms.
+   * The SOF should be every 1 ms.
+   */
+  kDifUsbdevIrqHostLost = 3,
+  /**
+   * Raised if the link is at SE0 longer than 3 us indicating a link reset (host
+   * asserts for min 10 ms, device can react after 2.5 us).
+   */
+  kDifUsbdevIrqLinkReset = 4,
+  /**
+   * Raised if the line has signaled J for longer than 3ms and is therefore in
+   * suspend state.
+   */
+  kDifUsbdevIrqLinkSuspend = 5,
+  /**
+   * Raised when the link becomes active again after being suspended.
+   */
+  kDifUsbdevIrqLinkResume = 6,
+  /**
+   * Raised when a transaction is NACKed because the Available Buffer FIFO for
+   * OUT or SETUP transactions is empty.
+   */
+  kDifUsbdevIrqAvEmpty = 7,
+  /**
+   * Raised when a transaction is NACKed because the Received Buffer FIFO for
+   * OUT or SETUP transactions is full.
+   */
+  kDifUsbdevIrqRxFull = 8,
+  /**
+   * Raised if a write was done to the Available Buffer FIFO when the FIFO was
+   * full.
+   */
+  kDifUsbdevIrqAvOverflow = 9,
+  /**
+   * Raised if a packet to an IN endpoint started to be received but was then
+   * dropped due to an error. After transmitting the IN payload, the USB device
+   * expects a valid ACK handshake packet. This error is raised if either the
+   * packet or CRC is invalid or a different token was received.
+   */
+  kDifUsbdevIrqLinkInErr = 10,
+  /**
+   * Raised if a CRC error occured.
+   */
+  kDifUsbdevIrqRxCrcErr = 11,
+  /**
+   * Raised if an invalid packed identifier (PID) was received.
+   */
+  kDifUsbdevIrqRxPidErr = 12,
+  /**
+   * Raised if an invalid bitstuffing was received.
+   */
+  kDifUsbdevIrqRxBitstuffErr = 13,
+  /**
+   * Raised when the USB frame number is updated with a valid SOF.
+   */
+  kDifUsbdevIrqFrame = 14,
+  /**
+   * Raised if VBUS is applied.
+   */
+  kDifUsbdevIrqConnected = 15,
+  /**
+   * Raised if a packet to an OUT endpoint started to be received but was then
+   * dropped due to an error. This error is raised if either the data toggle,
+   * token, packet or CRC is invalid or if there is no buffer available in the
+   * Received Buffer FIFO.
+   */
+  kDifUsbdevIrqLinkOutErr = 16,
+} dif_usbdev_irq_t;
+
+/**
+ * A snapshot of the state of the interrupts for this IP.
+ *
+ * This is an opaque type, to be used with the `dif_usbdev_irq_get_state()`
+ * function.
+ */
+typedef uint32_t dif_usbdev_irq_state_snapshot_t;
+
+/**
+ * A snapshot of the enablement state of the interrupts for this IP.
+ *
+ * This is an opaque type, to be used with the
+ * `dif_usbdev_irq_disable_all()` and `dif_usbdev_irq_restore_all()`
+ * functions.
+ */
+typedef uint32_t dif_usbdev_irq_enable_snapshot_t;
+
+/**
+ * Returns whether a particular interrupt is currently pending.
+ *
+ * @param usbdev A usbdev handle.
+ * @param[out] snapshot Out-param for interrupt state snapshot.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_usbdev_irq_get_state(
+    const dif_usbdev_t *usbdev, dif_usbdev_irq_state_snapshot_t *snapshot);
+
+/**
+ * Returns whether a particular interrupt is currently pending.
+ *
+ * @param usbdev A usbdev handle.
+ * @param irq An interrupt request.
+ * @param[out] is_pending Out-param for whether the interrupt is pending.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_usbdev_irq_is_pending(const dif_usbdev_t *usbdev,
+                                       dif_usbdev_irq_t irq, bool *is_pending);
+
+/**
+ * Acknowledges a particular interrupt, indicating to the hardware that it has
+ * been successfully serviced.
+ *
+ * @param usbdev A usbdev handle.
+ * @param irq An interrupt request.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_usbdev_irq_acknowledge(const dif_usbdev_t *usbdev,
+                                        dif_usbdev_irq_t irq);
+
+/**
+ * Checks whether a particular interrupt is currently enabled or disabled.
+ *
+ * @param usbdev A usbdev handle.
+ * @param irq An interrupt request.
+ * @param[out] state Out-param toggle state of the interrupt.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_usbdev_irq_get_enabled(const dif_usbdev_t *usbdev,
+                                        dif_usbdev_irq_t irq,
+                                        dif_toggle_t *state);
+
+/**
+ * Sets whether a particular interrupt is currently enabled or disabled.
+ *
+ * @param usbdev A usbdev handle.
+ * @param irq An interrupt request.
+ * @param state The new toggle state for the interrupt.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_usbdev_irq_set_enabled(const dif_usbdev_t *usbdev,
+                                        dif_usbdev_irq_t irq,
+                                        dif_toggle_t state);
+
+/**
+ * Forces a particular interrupt, causing it to be serviced as if hardware had
+ * asserted it.
+ *
+ * @param usbdev A usbdev handle.
+ * @param irq An interrupt request.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_usbdev_irq_force(const dif_usbdev_t *usbdev,
+                                  dif_usbdev_irq_t irq);
+
+/**
+ * Disables all interrupts, optionally snapshotting all enable states for later
+ * restoration.
+ *
+ * @param usbdev A usbdev handle.
+ * @param[out] snapshot Out-param for the snapshot; may be `NULL`.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_usbdev_irq_disable_all(
+    const dif_usbdev_t *usbdev, dif_usbdev_irq_enable_snapshot_t *snapshot);
+
+/**
+ * Restores interrupts from the given (enable) snapshot.
+ *
+ * @param usbdev A usbdev handle.
+ * @param snapshot A snapshot to restore from.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_usbdev_irq_restore_all(
+    const dif_usbdev_t *usbdev,
+    const dif_usbdev_irq_enable_snapshot_t *snapshot);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_USBDEV_AUTOGEN_H_
---
+++ sw/device/lib/dif/autogen/dif_usbdev_autogen_unittest.cc
@@ -0,0 +1,305 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This file is auto-generated.
+
+#include "sw/device/lib/dif/dif_usbdev.h"
+
+#include "gtest/gtest.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/base/testing/mock_mmio.h"
+
+#include "usbdev_regs.h"  // Generated.
+
+namespace dif_usbdev_autogen_unittest {
+namespace {
+using ::mock_mmio::MmioTest;
+using ::mock_mmio::MockDevice;
+using ::testing::Test;
+
+class UsbdevTest : public Test, public MmioTest {
+ protected:
+  dif_usbdev_t usbdev_ = {.base_addr = dev().region()};
+};
+
+using ::testing::Eq;
+
+class IrqGetStateTest : public UsbdevTest {};
+
+TEST_F(IrqGetStateTest, NullArgs) {
+  dif_usbdev_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_EQ(dif_usbdev_irq_get_state(nullptr, &irq_snapshot), kDifBadArg);
+
+  EXPECT_EQ(dif_usbdev_irq_get_state(&usbdev_, nullptr), kDifBadArg);
+
+  EXPECT_EQ(dif_usbdev_irq_get_state(nullptr, nullptr), kDifBadArg);
+}
+
+TEST_F(IrqGetStateTest, SuccessAllRaised) {
+  dif_usbdev_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(USBDEV_INTR_STATE_REG_OFFSET,
+                std::numeric_limits<uint32_t>::max());
+  EXPECT_EQ(dif_usbdev_irq_get_state(&usbdev_, &irq_snapshot), kDifOk);
+  EXPECT_EQ(irq_snapshot, std::numeric_limits<uint32_t>::max());
+}
+
+TEST_F(IrqGetStateTest, SuccessNoneRaised) {
+  dif_usbdev_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(USBDEV_INTR_STATE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_usbdev_irq_get_state(&usbdev_, &irq_snapshot), kDifOk);
+  EXPECT_EQ(irq_snapshot, 0);
+}
+
+class IrqIsPendingTest : public UsbdevTest {};
+
+TEST_F(IrqIsPendingTest, NullArgs) {
+  bool is_pending;
+
+  EXPECT_EQ(
+      dif_usbdev_irq_is_pending(nullptr, kDifUsbdevIrqPktReceived, &is_pending),
+      kDifBadArg);
+
+  EXPECT_EQ(
+      dif_usbdev_irq_is_pending(&usbdev_, kDifUsbdevIrqPktReceived, nullptr),
+      kDifBadArg);
+
+  EXPECT_EQ(
+      dif_usbdev_irq_is_pending(nullptr, kDifUsbdevIrqPktReceived, nullptr),
+      kDifBadArg);
+}
+
+TEST_F(IrqIsPendingTest, BadIrq) {
+  bool is_pending;
+  // All interrupt CSRs are 32 bit so interrupt 32 will be invalid.
+  EXPECT_EQ(dif_usbdev_irq_is_pending(
+                &usbdev_, static_cast<dif_usbdev_irq_t>(32), &is_pending),
+            kDifBadArg);
+}
+
+TEST_F(IrqIsPendingTest, Success) {
+  bool irq_state;
+
+  // Get the first IRQ state.
+  irq_state = false;
+  EXPECT_READ32(USBDEV_INTR_STATE_REG_OFFSET,
+                {{USBDEV_INTR_STATE_PKT_RECEIVED_BIT, true}});
+  EXPECT_EQ(
+      dif_usbdev_irq_is_pending(&usbdev_, kDifUsbdevIrqPktReceived, &irq_state),
+      kDifOk);
+  EXPECT_TRUE(irq_state);
+
+  // Get the last IRQ state.
+  irq_state = true;
+  EXPECT_READ32(USBDEV_INTR_STATE_REG_OFFSET,
+                {{USBDEV_INTR_STATE_LINK_OUT_ERR_BIT, false}});
+  EXPECT_EQ(
+      dif_usbdev_irq_is_pending(&usbdev_, kDifUsbdevIrqLinkOutErr, &irq_state),
+      kDifOk);
+  EXPECT_FALSE(irq_state);
+}
+
+class IrqAcknowledgeTest : public UsbdevTest {};
+
+TEST_F(IrqAcknowledgeTest, NullArgs) {
+  EXPECT_EQ(dif_usbdev_irq_acknowledge(nullptr, kDifUsbdevIrqPktReceived),
+            kDifBadArg);
+}
+
+TEST_F(IrqAcknowledgeTest, BadIrq) {
+  EXPECT_EQ(
+      dif_usbdev_irq_acknowledge(nullptr, static_cast<dif_usbdev_irq_t>(32)),
+      kDifBadArg);
+}
+
+TEST_F(IrqAcknowledgeTest, Success) {
+  // Clear the first IRQ state.
+  EXPECT_WRITE32(USBDEV_INTR_STATE_REG_OFFSET,
+                 {{USBDEV_INTR_STATE_PKT_RECEIVED_BIT, true}});
+  EXPECT_EQ(dif_usbdev_irq_acknowledge(&usbdev_, kDifUsbdevIrqPktReceived),
+            kDifOk);
+
+  // Clear the last IRQ state.
+  EXPECT_WRITE32(USBDEV_INTR_STATE_REG_OFFSET,
+                 {{USBDEV_INTR_STATE_LINK_OUT_ERR_BIT, true}});
+  EXPECT_EQ(dif_usbdev_irq_acknowledge(&usbdev_, kDifUsbdevIrqLinkOutErr),
+            kDifOk);
+}
+
+class IrqGetEnabledTest : public UsbdevTest {};
+
+TEST_F(IrqGetEnabledTest, NullArgs) {
+  dif_toggle_t irq_state;
+
+  EXPECT_EQ(
+      dif_usbdev_irq_get_enabled(nullptr, kDifUsbdevIrqPktReceived, &irq_state),
+      kDifBadArg);
+
+  EXPECT_EQ(
+      dif_usbdev_irq_get_enabled(&usbdev_, kDifUsbdevIrqPktReceived, nullptr),
+      kDifBadArg);
+
+  EXPECT_EQ(
+      dif_usbdev_irq_get_enabled(nullptr, kDifUsbdevIrqPktReceived, nullptr),
+      kDifBadArg);
+}
+
+TEST_F(IrqGetEnabledTest, BadIrq) {
+  dif_toggle_t irq_state;
+
+  EXPECT_EQ(dif_usbdev_irq_get_enabled(
+                &usbdev_, static_cast<dif_usbdev_irq_t>(32), &irq_state),
+            kDifBadArg);
+}
+
+TEST_F(IrqGetEnabledTest, Success) {
+  dif_toggle_t irq_state;
+
+  // First IRQ is enabled.
+  irq_state = kDifToggleDisabled;
+  EXPECT_READ32(USBDEV_INTR_ENABLE_REG_OFFSET,
+                {{USBDEV_INTR_ENABLE_PKT_RECEIVED_BIT, true}});
+  EXPECT_EQ(dif_usbdev_irq_get_enabled(&usbdev_, kDifUsbdevIrqPktReceived,
+                                       &irq_state),
+            kDifOk);
+  EXPECT_EQ(irq_state, kDifToggleEnabled);
+
+  // Last IRQ is disabled.
+  irq_state = kDifToggleEnabled;
+  EXPECT_READ32(USBDEV_INTR_ENABLE_REG_OFFSET,
+                {{USBDEV_INTR_ENABLE_LINK_OUT_ERR_BIT, false}});
+  EXPECT_EQ(
+      dif_usbdev_irq_get_enabled(&usbdev_, kDifUsbdevIrqLinkOutErr, &irq_state),
+      kDifOk);
+  EXPECT_EQ(irq_state, kDifToggleDisabled);
+}
+
+class IrqSetEnabledTest : public UsbdevTest {};
+
+TEST_F(IrqSetEnabledTest, NullArgs) {
+  dif_toggle_t irq_state = kDifToggleEnabled;
+
+  EXPECT_EQ(
+      dif_usbdev_irq_set_enabled(nullptr, kDifUsbdevIrqPktReceived, irq_state),
+      kDifBadArg);
+}
+
+TEST_F(IrqSetEnabledTest, BadIrq) {
+  dif_toggle_t irq_state = kDifToggleEnabled;
+
+  EXPECT_EQ(dif_usbdev_irq_set_enabled(
+                &usbdev_, static_cast<dif_usbdev_irq_t>(32), irq_state),
+            kDifBadArg);
+}
+
+TEST_F(IrqSetEnabledTest, Success) {
+  dif_toggle_t irq_state;
+
+  // Enable first IRQ.
+  irq_state = kDifToggleEnabled;
+  EXPECT_MASK32(USBDEV_INTR_ENABLE_REG_OFFSET,
+                {{USBDEV_INTR_ENABLE_PKT_RECEIVED_BIT, 0x1, true}});
+  EXPECT_EQ(
+      dif_usbdev_irq_set_enabled(&usbdev_, kDifUsbdevIrqPktReceived, irq_state),
+      kDifOk);
+
+  // Disable last IRQ.
+  irq_state = kDifToggleDisabled;
+  EXPECT_MASK32(USBDEV_INTR_ENABLE_REG_OFFSET,
+                {{USBDEV_INTR_ENABLE_LINK_OUT_ERR_BIT, 0x1, false}});
+  EXPECT_EQ(
+      dif_usbdev_irq_set_enabled(&usbdev_, kDifUsbdevIrqLinkOutErr, irq_state),
+      kDifOk);
+}
+
+class IrqForceTest : public UsbdevTest {};
+
+TEST_F(IrqForceTest, NullArgs) {
+  EXPECT_EQ(dif_usbdev_irq_force(nullptr, kDifUsbdevIrqPktReceived),
+            kDifBadArg);
+}
+
+TEST_F(IrqForceTest, BadIrq) {
+  EXPECT_EQ(dif_usbdev_irq_force(nullptr, static_cast<dif_usbdev_irq_t>(32)),
+            kDifBadArg);
+}
+
+TEST_F(IrqForceTest, Success) {
+  // Force first IRQ.
+  EXPECT_WRITE32(USBDEV_INTR_TEST_REG_OFFSET,
+                 {{USBDEV_INTR_TEST_PKT_RECEIVED_BIT, true}});
+  EXPECT_EQ(dif_usbdev_irq_force(&usbdev_, kDifUsbdevIrqPktReceived), kDifOk);
+
+  // Force last IRQ.
+  EXPECT_WRITE32(USBDEV_INTR_TEST_REG_OFFSET,
+                 {{USBDEV_INTR_TEST_LINK_OUT_ERR_BIT, true}});
+  EXPECT_EQ(dif_usbdev_irq_force(&usbdev_, kDifUsbdevIrqLinkOutErr), kDifOk);
+}
+
+class IrqDisableAllTest : public UsbdevTest {};
+
+TEST_F(IrqDisableAllTest, NullArgs) {
+  dif_usbdev_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_EQ(dif_usbdev_irq_disable_all(nullptr, &irq_snapshot), kDifBadArg);
+
+  EXPECT_EQ(dif_usbdev_irq_disable_all(nullptr, nullptr), kDifBadArg);
+}
+
+TEST_F(IrqDisableAllTest, SuccessNoSnapshot) {
+  EXPECT_WRITE32(USBDEV_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_usbdev_irq_disable_all(&usbdev_, nullptr), kDifOk);
+}
+
+TEST_F(IrqDisableAllTest, SuccessSnapshotAllDisabled) {
+  dif_usbdev_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(USBDEV_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_WRITE32(USBDEV_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_usbdev_irq_disable_all(&usbdev_, &irq_snapshot), kDifOk);
+  EXPECT_EQ(irq_snapshot, 0);
+}
+
+TEST_F(IrqDisableAllTest, SuccessSnapshotAllEnabled) {
+  dif_usbdev_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(USBDEV_INTR_ENABLE_REG_OFFSET,
+                std::numeric_limits<uint32_t>::max());
+  EXPECT_WRITE32(USBDEV_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_usbdev_irq_disable_all(&usbdev_, &irq_snapshot), kDifOk);
+  EXPECT_EQ(irq_snapshot, std::numeric_limits<uint32_t>::max());
+}
+
+class IrqRestoreAllTest : public UsbdevTest {};
+
+TEST_F(IrqRestoreAllTest, NullArgs) {
+  dif_usbdev_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_EQ(dif_usbdev_irq_restore_all(nullptr, &irq_snapshot), kDifBadArg);
+
+  EXPECT_EQ(dif_usbdev_irq_restore_all(&usbdev_, nullptr), kDifBadArg);
+
+  EXPECT_EQ(dif_usbdev_irq_restore_all(nullptr, nullptr), kDifBadArg);
+}
+
+TEST_F(IrqRestoreAllTest, SuccessAllEnabled) {
+  dif_usbdev_irq_enable_snapshot_t irq_snapshot =
+      std::numeric_limits<uint32_t>::max();
+
+  EXPECT_WRITE32(USBDEV_INTR_ENABLE_REG_OFFSET,
+                 std::numeric_limits<uint32_t>::max());
+  EXPECT_EQ(dif_usbdev_irq_restore_all(&usbdev_, &irq_snapshot), kDifOk);
+}
+
+TEST_F(IrqRestoreAllTest, SuccessAllDisabled) {
+  dif_usbdev_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_WRITE32(USBDEV_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_EQ(dif_usbdev_irq_restore_all(&usbdev_, &irq_snapshot), kDifOk);
+}
+
+}  // namespace
+}  // namespace dif_usbdev_autogen_unittest
