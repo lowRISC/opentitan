bc9f3cf4179540ebdaa6e8b8c09c4269c1bd5c1e
Timothy Trippel <ttrippel@google.com>
2021-10-12 20:16:02 +0000

    [dif/rv_plic] Add autogen'd DIF artifacts for rv_plic.
    This partially addresses #8142, by checking-in auto-generated DIF
    typedefs.
    
    Some things to note:
    - The there are no IRQs associated with the rv_plic IP block, but there
      are **alerts**. In the near future, alert-related DIFs will be
      autogenerated and rely on the supporting autogenerated typedefs in
      this commit.
    - These auto-generated DIF components are **not** yet integrated into
      the source tree.
    
    This continues the longer term goal of completely auto-generating some
    of the DIFs, as described in #8142.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

---
+++ sw/device/lib/dif/autogen/dif_rv_plic_autogen.c
@@ -0,0 +1,9 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This file is auto-generated.
+
+#include "sw/device/lib/dif/dif_rv_plic.h"
+
+#include "rv_plic_regs.h"  // Generated.
---
+++ sw/device/lib/dif/autogen/dif_rv_plic_autogen_unittest.cc
@@ -0,0 +1,27 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This file is auto-generated.
+
+#include "sw/device/lib/dif/dif_rv_plic.h"
+
+#include "gtest/gtest.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/base/testing/mock_mmio.h"
+
+#include "rv_plic_regs.h"  // Generated.
+
+namespace dif_rv_plic_autogen_unittest {
+namespace {
+using ::mock_mmio::MmioTest;
+using ::mock_mmio::MockDevice;
+using ::testing::Test;
+
+class RvPlicTest : public Test, public MmioTest {
+ protected:
+  dif_rv_plic_t rv_plic_ = {.base_addr = dev().region()};
+};
+
+}  // namespace
+}  // namespace dif_rv_plic_autogen_unittest
