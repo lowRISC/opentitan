a82b763970873fb7c0d87857cb064f348dc64788
Martin Lueker-Boden <martin.lueker-boden@wdc.com>
2021-10-15 14:44:32 -0700

    [ spi_host rtl ] Prepare to move entire SPI_HOST IP the peripheral clock domain
    This is the first of two commits which aim to standardize the CDC strategy for SPI_HOST
    
    In this first commit:
    
    - An explicit tlul_async_fifo is created inside spi_host.sv. This FIFO will eventually move
      to the TLUL fabric or the register interface.  However putting it inside the IP for now
      allows for testing of changes to other blocks without changing the interface, which would
      disrupt DV testing.
    
    - The command_cdc has been removed in favor of a shallow synchronous FIFO in
      spi_host_command_queue.
    
    - spi_host_data_cdc with its async fifos have also been removed in favor
      of spi_host_data_fifos, in which the fifos are synchronous
    
    Pending until next commit:
    
    - Migration of TLUL fifo to some automated system (TLUL fabric? Regtool?)
    - Removal of "Core" clocks & resets
    - Change COMMAND register to hw_ext
    - DV Interface updates to change clocks
    
    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>

---
+++ hw/ip/spi_host/rtl/spi_host_command_queue.sv
@@ -0,0 +1,50 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Queue for SPI_HOST commands
+//
+
+module spi_host_command_queue #(
+  parameter int CmdDepth = 4
+) (
+  input                              clk_i,
+  input                              rst_ni,
+
+  input  spi_host_cmd_pkg::command_t command_i,
+  input                              command_valid_i,
+  output logic                       command_busy_o,
+
+  output spi_host_cmd_pkg::command_t core_command_o,
+  output logic                       core_command_valid_o,
+  input                              core_command_ready_i,
+
+  output logic                       error_busy_o,
+
+  input                              sw_rst_i
+);
+
+  logic command_ready;
+
+  assign command_busy_o = ~command_ready;
+  assign error_busy_o   = command_valid_i & command_busy_o;
+
+  prim_fifo_sync #(
+    .Width(spi_host_cmd_pkg::CmdSize),
+    .Pass(1),
+    .Depth(CmdDepth)
+  ) cmd_fifo (
+    .clk_i,
+    .rst_ni,
+    .clr_i    (sw_rst_i),
+    .wvalid_i (command_valid_i),
+    .wready_o (command_ready),
+    .wdata_i  (command_i),
+    .rvalid_o (core_command_valid_o),
+    .rready_i (core_command_ready_i),
+    .rdata_o  (core_command_o),
+    .full_o   (),
+    .depth_o  ()
+  );
+
+endmodule : spi_host_command_queue
