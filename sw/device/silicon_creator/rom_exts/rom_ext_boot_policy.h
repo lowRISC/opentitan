df7475bd404782598d7332e1c80a161d0256ae1e
Alphan Ulusoy <alphan@google.com>
2021-10-19 14:48:11 -0400

    [sw/silicon_creator] Add initial files for ROM_EXT boot policy
    This change adds skeleton code for ROM_EXT boot policy by mostly copying the
    corresponding files from `sw/device/silicon_creato/mask_rom` to unblock
    booting the first owner boot stage.
    
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

---
+++ sw/device/silicon_creator/rom_exts/rom_ext_boot_policy.h
@@ -0,0 +1,71 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXTS_ROM_EXT_BOOT_POLICY_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXTS_ROM_EXT_BOOT_POLICY_H_
+
+#include "sw/device/silicon_creator/lib/error.h"
+#include "sw/device/silicon_creator/lib/manifest.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+enum {
+  /**
+   * First owner boot stage manifest identifier (ASCII "OTSO").
+   */
+  kRomExtBootPolicyOwnerStageIdentifier = 0x4f53544f,
+};
+
+/**
+ * Type alias for the first owner boot stage entry point.
+ *
+ * The entry point address obtained from the first owner boot stage manifest
+ * must be cast to a pointer to this type before being called.
+ */
+typedef void owner_stage_entry_point(void);
+
+/**
+ * Manifests of first boot owner boot stages in descending order according to
+ * their security versions.
+ *
+ * These boot stages must be verified prior to handing over execution.
+ */
+typedef struct rom_ext_boot_policy_manifests {
+  /**
+   * First owner boot stage manifests in descending order according to
+   * their security versions.
+   */
+  const manifest_t *ordered[2];
+} rom_ext_boot_policy_manifests_t;
+
+/**
+ * Returns the manifests of first owner boot stages that should be attempted to
+ * boot in descending order according to their security versions.
+ *
+ * These boot stages must be verified prior to handing over execution.
+ *
+ * @return Manifests of first owner boot stages in descending order according to
+ * their security versions.
+ */
+rom_ext_boot_policy_manifests_t rom_ext_boot_policy_manifests_get(void);
+
+/**
+ * Checks the fields of a first owner boot stage manifest.
+ *
+ * This function performs bounds checks on the fields of the manifest, checks
+ * that its `identifier` is correct, and its `security_version` is greater than
+ * or equal to the minimum required security version.
+ *
+ * @param manifest A first boot owner boot stage manifest.
+ * @return Result of the operation.
+ */
+rom_error_t rom_ext_boot_policy_manifest_check(const manifest_t *manifest);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXTS_ROM_EXT_BOOT_POLICY_H_
---
+++ sw/device/silicon_creator/rom_exts/rom_ext_boot_policy_ptrs.h
@@ -0,0 +1,57 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXTS_ROM_EXT_BOOT_POLICY_PTRS_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXTS_ROM_EXT_BOOT_POLICY_PTRS_H_
+
+#include "sw/device/silicon_creator/lib/manifest.h"
+
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+static_assert((TOP_EARLGREY_EFLASH_SIZE_BYTES % 2) == 0,
+              "Flash size is not divisible by 2");
+
+#ifndef OT_OFF_TARGET_TEST
+/**
+ * Returns a pointer to the manifest of the first owner boot stage image stored
+ * in flash slot A.
+ *
+ * @return Pointer to the manifest of the first owner boot stage image in slot
+ * A.
+ */
+// TODO(#7893): Should these be volatile?
+inline const manifest_t *rom_ext_boot_policy_manifest_a_get(void) {
+  return (const manifest_t *)(TOP_EARLGREY_EFLASH_BASE_ADDR +
+                              MANIFEST_LENGTH_FIELD_MAX);
+}
+
+/**
+ * Returns a pointer to the manifest of the first owner boot stage image stored
+ * in flash slot B.
+ *
+ * @return Pointer to the manifest of the first owner boot stage image in slot
+ * B.
+ */
+inline const manifest_t *rom_ext_boot_policy_manifest_b_get(void) {
+  return (const manifest_t *)(TOP_EARLGREY_EFLASH_BASE_ADDR +
+                              (TOP_EARLGREY_EFLASH_SIZE_BYTES / 2) +
+                              MANIFEST_LENGTH_FIELD_MAX);
+}
+#else
+/**
+ * Declarations for the functions above that should be defined in tests.
+ */
+const manifest_t *rom_ext_boot_policy_manifest_a_get(void);
+const manifest_t *rom_ext_boot_policy_manifest_b_get(void);
+#endif
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXTS_ROM_EXT_BOOT_POLICY_PTRS_H_
