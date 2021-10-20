7d78ecf2d445ac1c0ebff25c3d95789a4d15db2a
Martin Lueker-Boden <martin.lueker-boden@wdc.com>
2021-10-20 15:02:54 -0700

    [ spi_host ] Split DATA memory window into separate RO and WO windows
    - Creates two new windows TXDATA and RXDATA
    - RXDATA is RO, TXDATA is WO
      - Fixes #5484
    - Signals an error back to TLUL if the RXDATA is written or TXDATA is read
    
    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>

--- hw/ip/spi_host/rtl/spi_host_window.sv
+++ hw/ip/spi_host/rtl/spi_host_window.sv
@@ -8,41 +8,64 @@
 module spi_host_window (
   input  clk_i,
   input  rst_ni,
-  input  tlul_pkg::tl_h2d_t win_i,
-  output tlul_pkg::tl_d2h_t win_o,
+  input  tlul_pkg::tl_h2d_t rx_win_i,
+  output tlul_pkg::tl_d2h_t rx_win_o,
+  input  tlul_pkg::tl_h2d_t tx_win_i,
+  output tlul_pkg::tl_d2h_t tx_win_o,
   output logic [31:0]       tx_data_o,
   output logic [3:0]        tx_be_o,
   output logic              tx_valid_o,
   input        [31:0]       rx_data_i,
-  output                    rx_ready_o
+  output logic              rx_ready_o
 );
 
-  localparam int AW=spi_host_reg_pkg::BlockAw;
-  localparam int DW=32;
+  localparam int AW = spi_host_reg_pkg::BlockAw;
+  localparam int DW = 32;
 
-  logic [AW-1:0] addr;
+  logic rx_we, tx_re;
 
-  // Only support reads/writes to the data fifo window
-  logic win_error;
-  assign win_error = (tx_valid_o || rx_ready_o) &&
-                     (addr != spi_host_reg_pkg::SPI_HOST_DATA_OFFSET);
+  // Only support reads from the data RX fifo window
+  logic  rx_access_error;
+  assign rx_access_error = rx_we;
 
   tlul_adapter_reg #(
-    .RegAw(AW),
-    .RegDw(DW)
-  ) u_adapter (
+    .RegAw (AW),
+    .RegDw (DW)
+  ) u_adapter_rx (
     .clk_i,
     .rst_ni,
-    .tl_i      (win_i),
-    .tl_o      (win_o),
-    .we_o      (tx_valid_o),
-    .re_o      (rx_ready_o),
-    .addr_o    (addr),
-    .wdata_o   (tx_data_o),
-    .be_o      (tx_be_o),
-    .busy_i    ('0),
-    .rdata_i   (rx_data_i),
-    .error_i   (win_error)
+    .tl_i    (rx_win_i),
+    .tl_o    (rx_win_o),
+    .we_o    (rx_we),
+    .re_o    (rx_ready_o),
+    .addr_o  (),
+    .wdata_o (),
+    .be_o    (),
+    .rdata_i (rx_data_i),
+    .error_i (rx_access_error),
+    .busy_i  ('0)
+  );
+
+  // Only support writes to the data TX fifo window
+  logic  tx_access_error;
+  assign tx_access_error = tx_re;
+
+  tlul_adapter_reg #(
+    .RegAw (AW),
+    .RegDw (DW)
+  ) u_adapter_tx (
+    .clk_i,
+    .rst_ni,
+    .tl_i    (tx_win_i),
+    .tl_o    (tx_win_o),
+    .we_o    (tx_valid_o),
+    .re_o    (tx_re),
+    .addr_o  (),
+    .wdata_o (tx_data_o),
+    .be_o    (tx_be_o),
+    .rdata_i ({DW{1'b0}}),
+    .error_i (tx_access_error),
+    .busy_i  ('0)
   );
 
 endmodule : spi_host_window
