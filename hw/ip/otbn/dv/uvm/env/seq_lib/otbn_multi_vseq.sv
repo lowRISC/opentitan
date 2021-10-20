464b736079ba91c6fe52cef12f2772ce576c1302
Rupert Swarbrick <rswarbrick@lowrisc.org>
2021-10-20 17:10:23 +0100

    [otbn,dv] Maybe use IRQ to detect end instead of polling STATUS
    Configuring this goes in two steps: firstly, you have to write to the
    enable register. There's an enable_interrupts_pct knob that sequences
    that care can get from the environment config to spot that it is
    needed.
    
    Then the run_otbn() task in the base class chooses whether to use
    interrupts or polling, based on whether interrupts are enabled (if
    not, it will poll!) and poll_despite_interrupts_pct.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/otbn/dv/uvm/env/seq_lib/otbn_multi_vseq.sv
+++ hw/ip/otbn/dv/uvm/env/seq_lib/otbn_multi_vseq.sv
@@ -26,14 +26,25 @@ class otbn_multi_vseq extends otbn_base_vseq;
 
     for (int i = 0; i < 10; i++) begin
       bit rerun = (i == 0) ? 1'b0 : ($urandom_range(100) <= rerun_pct);
+      bit enable_interrupts = $urandom_range(100) < cfg.enable_interrupts_pct;
+      bit irq_pin_high = cfg.intr_vif.sample_pin(0);
+      bit clear_status = irq_pin_high ? ($urandom_range(100) < cfg.clear_irq_pct) : 1'b0;
 
-      if (rerun) begin
-        `uvm_info(`gfn, $sformatf("Re-using OTBN binary at `%0s'", elf_path), UVM_LOW)
-      end else begin
-        elf_path = pick_elf_path();
-        `uvm_info(`gfn, $sformatf("Loading OTBN binary from `%0s'", elf_path), UVM_LOW)
-        load_elf(elf_path, 1'b0);
-      end
+      fork
+        cfg_interrupts(enable_interrupts);
+        if (clear_status) begin
+          csr_utils_pkg::csr_wr(.ptr(ral.intr_state), .value(32'h1));
+        end
+        begin
+          if (rerun) begin
+            `uvm_info(`gfn, $sformatf("Re-using OTBN binary at `%0s'", elf_path), UVM_LOW)
+          end else begin
+            elf_path = pick_elf_path();
+            `uvm_info(`gfn, $sformatf("Loading OTBN binary from `%0s'", elf_path), UVM_LOW)
+            load_elf(elf_path, 1'b0);
+          end
+        end
+      join
 
       run_otbn();      
     end
