76077a875aae75fc362203a57f781d14fc93d1f4
Timothy Trippel <ttrippel@google.com>
2021-10-20 21:00:03 +0000

    [dif/pattgen] Auto-generate portion of DIFs.
    Currently, the DIF library for the PATTGEN is unimplemented. This begins
    the implementation of this DIF library by first checking in the
    auto-generated DIFs (init and IRQ DIFs).
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

---
+++ sw/device/lib/dif/autogen/dif_pattgen_autogen.c
@@ -0,0 +1,194 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This file is auto-generated.
+
+#include "sw/device/lib/dif/autogen/dif_pattgen_autogen.h"
+
+#include "pattgen_regs.h"  // Generated.
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_init(mmio_region_t base_addr, dif_pattgen_t *pattgen) {
+  if (pattgen == NULL) {
+    return kDifBadArg;
+  }
+
+  pattgen->base_addr = base_addr;
+
+  return kDifOk;
+}
+
+/**
+ * Get the corresponding interrupt register bit offset of the IRQ. If the IP's
+ * HJSON does NOT have a field "no_auto_intr_regs = true", then the
+ * "<ip>_INTR_COMMON_<irq>_BIT" macro can used. Otherwise, special cases will
+ * exist, as templated below.
+ */
+static bool pattgen_get_irq_bit_index(dif_pattgen_irq_t irq,
+                                      bitfield_bit32_index_t *index_out) {
+  switch (irq) {
+    case kDifPattgenIrqDoneCh0:
+      *index_out = PATTGEN_INTR_COMMON_DONE_CH0_BIT;
+      break;
+    case kDifPattgenIrqDoneCh1:
+      *index_out = PATTGEN_INTR_COMMON_DONE_CH1_BIT;
+      break;
+    default:
+      return false;
+  }
+
+  return true;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_get_state(
+    const dif_pattgen_t *pattgen, dif_pattgen_irq_state_snapshot_t *snapshot) {
+  if (pattgen == NULL || snapshot == NULL) {
+    return kDifBadArg;
+  }
+
+  *snapshot =
+      mmio_region_read32(pattgen->base_addr, PATTGEN_INTR_STATE_REG_OFFSET);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_is_pending(const dif_pattgen_t *pattgen,
+                                        dif_pattgen_irq_t irq,
+                                        bool *is_pending) {
+  if (pattgen == NULL || is_pending == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!pattgen_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_state_reg =
+      mmio_region_read32(pattgen->base_addr, PATTGEN_INTR_STATE_REG_OFFSET);
+
+  *is_pending = bitfield_bit32_read(intr_state_reg, index);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_acknowledge(const dif_pattgen_t *pattgen,
+                                         dif_pattgen_irq_t irq) {
+  if (pattgen == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!pattgen_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  // Writing to the register clears the corresponding bits (Write-one clear).
+  uint32_t intr_state_reg = bitfield_bit32_write(0, index, true);
+  mmio_region_write32(pattgen->base_addr, PATTGEN_INTR_STATE_REG_OFFSET,
+                      intr_state_reg);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_force(const dif_pattgen_t *pattgen,
+                                   dif_pattgen_irq_t irq) {
+  if (pattgen == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!pattgen_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_test_reg = bitfield_bit32_write(0, index, true);
+  mmio_region_write32(pattgen->base_addr, PATTGEN_INTR_TEST_REG_OFFSET,
+                      intr_test_reg);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_get_enabled(const dif_pattgen_t *pattgen,
+                                         dif_pattgen_irq_t irq,
+                                         dif_toggle_t *state) {
+  if (pattgen == NULL || state == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!pattgen_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_enable_reg =
+      mmio_region_read32(pattgen->base_addr, PATTGEN_INTR_ENABLE_REG_OFFSET);
+
+  bool is_enabled = bitfield_bit32_read(intr_enable_reg, index);
+  *state = is_enabled ? kDifToggleEnabled : kDifToggleDisabled;
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_set_enabled(const dif_pattgen_t *pattgen,
+                                         dif_pattgen_irq_t irq,
+                                         dif_toggle_t state) {
+  if (pattgen == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!pattgen_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_enable_reg =
+      mmio_region_read32(pattgen->base_addr, PATTGEN_INTR_ENABLE_REG_OFFSET);
+
+  bool enable_bit = (state == kDifToggleEnabled) ? true : false;
+  intr_enable_reg = bitfield_bit32_write(intr_enable_reg, index, enable_bit);
+  mmio_region_write32(pattgen->base_addr, PATTGEN_INTR_ENABLE_REG_OFFSET,
+                      intr_enable_reg);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_disable_all(
+    const dif_pattgen_t *pattgen, dif_pattgen_irq_enable_snapshot_t *snapshot) {
+  if (pattgen == NULL) {
+    return kDifBadArg;
+  }
+
+  // Pass the current interrupt state to the caller, if requested.
+  if (snapshot != NULL) {
+    *snapshot =
+        mmio_region_read32(pattgen->base_addr, PATTGEN_INTR_ENABLE_REG_OFFSET);
+  }
+
+  // Disable all interrupts.
+  mmio_region_write32(pattgen->base_addr, PATTGEN_INTR_ENABLE_REG_OFFSET, 0u);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_restore_all(
+    const dif_pattgen_t *pattgen,
+    const dif_pattgen_irq_enable_snapshot_t *snapshot) {
+  if (pattgen == NULL || snapshot == NULL) {
+    return kDifBadArg;
+  }
+
+  mmio_region_write32(pattgen->base_addr, PATTGEN_INTR_ENABLE_REG_OFFSET,
+                      *snapshot);
+
+  return kDifOk;
+}
---
+++ sw/device/lib/dif/autogen/dif_pattgen_autogen.h
@@ -0,0 +1,183 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_PATTGEN_AUTOGEN_H_
+#define OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_PATTGEN_AUTOGEN_H_
+
+// This file is auto-generated.
+
+/**
+ * @file
+ * @brief <a href="/hw/ip/pattgen/doc/">PATTGEN</a> Device Interface Functions
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "sw/device/lib/base/macros.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/dif/dif_base.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+/**
+ * A handle to pattgen.
+ *
+ * This type should be treated as opaque by users.
+ */
+typedef struct dif_pattgen {
+  /**
+   * The base address for the pattgen hardware registers.
+   */
+  mmio_region_t base_addr;
+} dif_pattgen_t;
+
+/**
+ * Creates a new handle for a(n) pattgen peripheral.
+ *
+ * This function does not actuate the hardware.
+ *
+ * @param base_addr The MMIO base address of the pattgen peripheral.
+ * @param[out] pattgen Out param for the initialized handle.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_init(mmio_region_t base_addr, dif_pattgen_t *pattgen);
+
+/**
+ * A pattgen interrupt request type.
+ */
+typedef enum dif_pattgen_irq {
+  /**
+   * Raise if pattern generation on Channel 0 is complete
+   */
+  kDifPattgenIrqDoneCh0 = 0,
+  /**
+   * Raise if pattern generation on Channel 1 is complete
+   */
+  kDifPattgenIrqDoneCh1 = 1,
+} dif_pattgen_irq_t;
+
+/**
+ * A snapshot of the state of the interrupts for this IP.
+ *
+ * This is an opaque type, to be used with the `dif_pattgen_irq_get_state()`
+ * function.
+ */
+typedef uint32_t dif_pattgen_irq_state_snapshot_t;
+
+/**
+ * Returns whether a particular interrupt is currently pending.
+ *
+ * @param pattgen A pattgen handle.
+ * @param[out] snapshot Out-param for interrupt state snapshot.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_get_state(
+    const dif_pattgen_t *pattgen, dif_pattgen_irq_state_snapshot_t *snapshot);
+
+/**
+ * Returns whether a particular interrupt is currently pending.
+ *
+ * @param pattgen A pattgen handle.
+ * @param irq An interrupt request.
+ * @param[out] is_pending Out-param for whether the interrupt is pending.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_is_pending(const dif_pattgen_t *pattgen,
+                                        dif_pattgen_irq_t irq,
+                                        bool *is_pending);
+
+/**
+ * Acknowledges a particular interrupt, indicating to the hardware that it has
+ * been successfully serviced.
+ *
+ * @param pattgen A pattgen handle.
+ * @param irq An interrupt request.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_acknowledge(const dif_pattgen_t *pattgen,
+                                         dif_pattgen_irq_t irq);
+
+/**
+ * Forces a particular interrupt, causing it to be serviced as if hardware had
+ * asserted it.
+ *
+ * @param pattgen A pattgen handle.
+ * @param irq An interrupt request.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_force(const dif_pattgen_t *pattgen,
+                                   dif_pattgen_irq_t irq);
+
+/**
+ * A snapshot of the enablement state of the interrupts for this IP.
+ *
+ * This is an opaque type, to be used with the
+ * `dif_pattgen_irq_disable_all()` and `dif_pattgen_irq_restore_all()`
+ * functions.
+ */
+typedef uint32_t dif_pattgen_irq_enable_snapshot_t;
+
+/**
+ * Checks whether a particular interrupt is currently enabled or disabled.
+ *
+ * @param pattgen A pattgen handle.
+ * @param irq An interrupt request.
+ * @param[out] state Out-param toggle state of the interrupt.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_get_enabled(const dif_pattgen_t *pattgen,
+                                         dif_pattgen_irq_t irq,
+                                         dif_toggle_t *state);
+
+/**
+ * Sets whether a particular interrupt is currently enabled or disabled.
+ *
+ * @param pattgen A pattgen handle.
+ * @param irq An interrupt request.
+ * @param state The new toggle state for the interrupt.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_set_enabled(const dif_pattgen_t *pattgen,
+                                         dif_pattgen_irq_t irq,
+                                         dif_toggle_t state);
+
+/**
+ * Disables all interrupts, optionally snapshotting all enable states for later
+ * restoration.
+ *
+ * @param pattgen A pattgen handle.
+ * @param[out] snapshot Out-param for the snapshot; may be `NULL`.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_disable_all(
+    const dif_pattgen_t *pattgen, dif_pattgen_irq_enable_snapshot_t *snapshot);
+
+/**
+ * Restores interrupts from the given (enable) snapshot.
+ *
+ * @param pattgen A pattgen handle.
+ * @param snapshot A snapshot to restore from.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_pattgen_irq_restore_all(
+    const dif_pattgen_t *pattgen,
+    const dif_pattgen_irq_enable_snapshot_t *snapshot);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_DIF_AUTOGEN_DIF_PATTGEN_AUTOGEN_H_
