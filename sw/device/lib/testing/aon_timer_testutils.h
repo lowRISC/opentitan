0b7c9ff08778237f7028e4e7cb116b6e0c06c860
Guillermo Maturana <maturana@google.com>
2021-10-20 11:57:09 -0700

    [dv/full_chip] Expand aon_timer testutils
    Add utility function to set a watchdog.
    Change aon_timer smoketest to use the testutils.
    
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- sw/device/lib/testing/aon_timer_testutils.h
+++ sw/device/lib/testing/aon_timer_testutils.h
@@ -10,10 +10,22 @@
 #include "sw/device/lib/dif/dif_aon_timer.h"
 
 /**
- * Set an aon timer for a number of AON clock cycles.
+ * Configure wakeup counter for a number of AON clock cycles.
  * @param cycles The number of AON clock cycles.
  */
 void aon_timer_testutils_wakeup_config(dif_aon_timer_t *aon_timer,
                                        uint32_t cycles);
+
+/**
+ * Configure watchdog counter in number of AON clock cycles.
+ *
+ * The watchdog counter is set without locking it, and configured so it doesn't
+ * pause for low power.
+ * @param bark_cycles The number of AON clock cycles till barking.
+ * @param bite_cycles The number of AON clock cycles till biting.
+ */
+void aon_timer_testutils_watchdog_config(dif_aon_timer_t *aon_timer,
+                                         uint32_t bark_cycles,
+                                         uint32_t bite_cycles);
 
 #endif  // OPENTITAN_SW_DEVICE_LIB_TESTING_AON_TIMER_TESTUTILS_H_
