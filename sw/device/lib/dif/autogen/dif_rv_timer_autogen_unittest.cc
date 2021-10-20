958a965c92b1ee895e44848aef908dacc50de4e0
Timothy Trippel <ttrippel@google.com>
2021-10-20 19:28:49 +0000

    [dif/rv-timer] Fix autogen DIFs dependency architecture.
    This fixes #8755 for the rv_timer, to fix a CI failure, since #8756 was
    merged after #8733, without updating the rv-timer autogen DIFs. (CI
    tests passed for #8756, before #8733 was merged.)
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

--- sw/device/lib/dif/autogen/dif_rv_timer_autogen_unittest.cc
+++ sw/device/lib/dif/autogen/dif_rv_timer_autogen_unittest.cc
@@ -4,7 +4,7 @@
 
 // This file is auto-generated.
 
-#include "sw/device/lib/dif/dif_rv_timer.h"
+#include "sw/device/lib/dif/autogen/dif_rv_timer_autogen.h"
 
 #include "gtest/gtest.h"
 #include "sw/device/lib/base/mmio.h"
