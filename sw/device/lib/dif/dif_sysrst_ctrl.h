b3a05f220729912c0bc555921ffa83eb42bdaa9b
Srikrishna Iyer <sriyer@google.com>
2021-10-23 14:53:47 -0700

    [sw, dif] Add headers for the last remaining IPs
    - Added the DIF header file for the last remaining IPs for which the DIF
    effort has not yet begun. These are adc ctrl, flash ctrl, pattgen, spi
    host and sysrst ctrl.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

---
+++ sw/device/lib/dif/dif_sysrst_ctrl.h
@@ -0,0 +1,26 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_DIF_DIF_SYSRST_CTRL_H_
+#define OPENTITAN_SW_DEVICE_LIB_DIF_DIF_SYSRST_CTRL_H_
+
+/**
+ * @file
+ * @brief <a href="/hw/ip/sysrst_ctrl/doc/">System Reset Controller</a> Device
+ * Interface Functions
+ */
+
+#include "sw/device/lib/dif/autogen/dif_sysrst_ctrl_autogen.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+// TODO: Add DIFs.
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_DIF_DIF_SYSRST_CTRL_H_
