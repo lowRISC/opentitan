613cfc1ac8863a7c6b47cda85068468dca0aba21
Cindy Chen <chencindy@opentitan.org>
2021-10-25 13:52:42 -0700

    [dv/shadow_reg] Clean up shadow reg file
    This PR cleans up shadow_reg vseq by reordering some tasks, and add more
    comments for readability.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

--- hw/dv/sv/cip_lib/seq_lib/cip_base_vseq__shadow_reg_errors.svh
+++ hw/dv/sv/cip_lib/seq_lib/cip_base_vseq__shadow_reg_errors.svh
@@ -2,71 +2,6 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// This function generates a random CSR value different from the input original value.
-// It can generate a rand value, or randomly flip one bit from the original value.
-virtual function bit [BUS_DW-1:0] get_shadow_reg_diff_val(dv_base_reg      csr,
-                                                          bit [BUS_DW-1:0] origin_val);
-  bit [BUS_DW-1:0] reg_mask = csr.get_reg_mask();
-  if ($urandom_range(0, 1)) begin
-    // Generate pure random value, but make sure it is not equal to the origin_val, and does not
-    // inject more than CSR's MSB.
-    `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(get_shadow_reg_diff_val,
-                                       (get_shadow_reg_diff_val & reg_mask) !=
-                                       (origin_val & reg_mask);)
-  end else begin
-    // Only flip one bit from the original.
-    int index;
-    `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(index, reg_mask[index] == 1;)
-    get_shadow_reg_diff_val = origin_val;
-    get_shadow_reg_diff_val[index] = ~get_shadow_reg_diff_val[index];
-  end
-endfunction
-
-// Wrap this shadow register csr_wr task with predict option so extended IPs can override it with
-// their customerized prediction. For example: AES's ctrl_shadow register needs to update `mode`
-// and `key_len` fields before calling the RAL predict function.
-virtual task csr_wr_for_shadow_reg_predict(dv_base_reg csr, uvm_reg_data_t wdata, bit predict = 1);
-  csr_wr(.ptr(csr), .value(wdata), .en_shadow_wr(0), .predict(predict));
-endtask
-
-// Alert triggers as soon as design accept the TLUL transaction, if wait until
-// csr_wr_for_shadow_reg_predict() finishes then check alert, the alert transaction might already
-// finished.
-virtual task shadow_reg_wr(dv_base_reg    csr,
-                           uvm_reg_data_t wdata,
-                           bit            en_shadow_wr = 1,
-                           bit            predict = 1);
-  // Sequence does not know if this write will trigger update error, so set the initial value to
-  // 1 to trigger alert check. Update this value once the write is complete.
-  // If no update_err_alert is expected, exit the alert handshake check.
-  bit exp_update_err_alert = 1;
-  fork
-    begin
-      repeat (en_shadow_wr ? 2 : 1) csr_wr_for_shadow_reg_predict(csr, wdata, predict);
-      exp_update_err_alert = csr.get_shadow_update_err();
-    end
-    begin
-      string alert_name = csr.get_update_err_alert_name();
-      // This logic gates alert_handler testbench because it does not have outgoing alert.
-      if (cfg.m_alert_agent_cfg.exists(alert_name)) begin
-        fork
-          begin
-            `DV_SPINWAIT(while (!cfg.m_alert_agent_cfg[alert_name].vif.get_alert()) begin
-                           cfg.clk_rst_vif.wait_clks(1);
-                         end
-                         cfg.m_alert_agent_cfg[alert_name].vif.wait_ack_complete();,
-                         $sformatf("%0s update_err alert timeout", csr.get_name()))
-          end
-          begin
-            wait (exp_update_err_alert == 0);
-          end
-        join_any
-        disable fork;
-      end
-    end
-  join
-endtask
-
 virtual task run_shadow_reg_errors(int num_times, bit en_csr_rw_seq = 0);
   dv_base_reg shadowed_csrs[$];
 
@@ -151,57 +86,8 @@ virtual task run_shadow_reg_errors(int num_times, bit en_csr_rw_seq = 0);
   end
 endtask
 
-virtual task glitch_shadowed_reset(ref dv_base_reg shadowed_csr[$],
-                                   ref bit ready_to_trigger_csr_rw,
-                                   ref bit has_fatal_alert);
-  string alert_name;
-
-  // If any of the shadowed registers have been written with a different value than its reset
-  // value, glitch reset pins will trigger alerts.
-  foreach (shadowed_csr[i]) begin
-    if (shadowed_csr[i].get_reset() != `gmv(shadowed_csr[i])) begin
-      alert_name = shadowed_csr[i].get_storage_err_alert_name();
-      // Set alert_name manually because alert_handler module does not trigger alerts.
-      if (alert_name == "") alert_name = "fatal_err";
-      predict_shadow_reg_status(.predict_storage_err(1));
-      `uvm_info(`gfn, $sformatf("Expect reset storage error %0s", shadowed_csr[i].get_name()),
-                UVM_HIGH)
-      break;
-    end
-  end
-
-  // Randomly choose to glitch `rst_n` or `shadowed_rst_n` pin.
-  if ($urandom_range(0, 1)) begin
-    ready_to_trigger_csr_rw = 1;
-    `uvm_info(`gfn, "toggle shadow reset pin", UVM_HIGH)
-     cfg.rst_shadowed_vif.drive_shadow_rst_pin(0);
-  end else begin
-    cfg.rst_shadowed_vif.drive_shadow_rst_pin(1);
-    `uvm_info(`gfn, "toggle IP reset pin", UVM_HIGH)
-    dut_init();
-    // Wait until dut reset is done then allow csr_rw sequence to issue.
-    ready_to_trigger_csr_rw = 1;
-  end
-
-  // Check if shadow reset will trigger fatal storage error.
-  // - First check if alert_name exists in alert_agent_cfg because alert_handler IP won't trigger
-  // external alert, it triggers it as a local alert instead.
-  // - Wait for the first alert to trigger then check if the alert is firing continuously, because
-  // `dut_init` is a blocking task and alert is firing once alert_init is done.
-  if (cfg.m_alert_agent_cfg.exists(alert_name) && alert_name != "") begin
-    `DV_SPINWAIT(while (!cfg.m_alert_agent_cfg[alert_name].vif.get_alert())
-                 cfg.clk_rst_vif.wait_clks(1);,
-                 $sformatf("expect fatal alert:%0s to fire after rst_ni glitched", alert_name))
-    check_fatal_alert_nonblocking(alert_name);
-  end
-
-  // Wait for random cycles and reconnect `shadowed_rst_n` with `rst_n`.
-  cfg.clk_rst_vif.wait_clks($urandom_range(50, 200));
-
-  cfg.rst_shadowed_vif.reconnect_shadowed_rst_n_to_rst_n();
-  has_fatal_alert = alert_name != "";
-endtask
-
+// ---------------------- Four individual shadow reg tasks ----------------------------------
+//
 // Write shadow register twice with different value and expect a shadow register update alert.
 virtual task write_and_check_update_error(dv_base_reg shadowed_csr);
   uvm_reg_data_t wdata, err_wdata;
@@ -285,6 +171,129 @@ virtual task poke_and_check_storage_error(dv_base_reg shadowed_csr);
 
   read_check_shadow_reg_status("Poke_and_check_storage_error task");
   cfg.clk_rst_vif.wait_clks($urandom_range(10, 100));
+endtask
+
+// Verifies glitching reset pins can trigger storage error.
+//
+// Randomly choose to glitch rst_n or shadowed_rst_n pin.
+// If any of the shadow register's current value does not match its reset value, then we expect
+// storage error fatal alert to trigger.
+virtual task glitch_shadowed_reset(ref dv_base_reg shadowed_csr[$],
+                                   ref bit ready_to_trigger_csr_rw,
+                                   ref bit has_fatal_alert);
+  string alert_name;
+
+  // If any of the shadowed registers have been written with a different value than its reset
+  // value, glitch reset pins will trigger alerts.
+  foreach (shadowed_csr[i]) begin
+    if (shadowed_csr[i].get_reset() != `gmv(shadowed_csr[i])) begin
+      alert_name = shadowed_csr[i].get_storage_err_alert_name();
+      // Set alert_name manually because alert_handler module does not trigger alerts.
+      if (alert_name == "") alert_name = "fatal_err";
+      predict_shadow_reg_status(.predict_storage_err(1));
+      `uvm_info(`gfn, $sformatf("Expect reset storage error %0s", shadowed_csr[i].get_name()),
+                UVM_HIGH)
+      break;
+    end
+  end
+
+  // Randomly choose to glitch `rst_n` or `shadowed_rst_n` pin.
+  if ($urandom_range(0, 1)) begin
+    ready_to_trigger_csr_rw = 1;
+    `uvm_info(`gfn, "toggle shadow reset pin", UVM_HIGH)
+     cfg.rst_shadowed_vif.drive_shadow_rst_pin(0);
+  end else begin
+    cfg.rst_shadowed_vif.drive_shadow_rst_pin(1);
+    `uvm_info(`gfn, "toggle IP reset pin", UVM_HIGH)
+    dut_init();
+    // Wait until dut reset is done then allow csr_rw sequence to issue.
+    ready_to_trigger_csr_rw = 1;
+  end
+
+  // Check if shadow reset will trigger fatal storage error.
+  // - First check if alert_name exists in alert_agent_cfg because alert_handler IP won't trigger
+  // external alert, it triggers it as a local alert instead.
+  // - Wait for the first alert to trigger then check if the alert is firing continuously, because
+  // `dut_init` is a blocking task and alert is firing once alert_init is done.
+  if (cfg.m_alert_agent_cfg.exists(alert_name) && alert_name != "") begin
+    `DV_SPINWAIT(while (!cfg.m_alert_agent_cfg[alert_name].vif.get_alert())
+                 cfg.clk_rst_vif.wait_clks(1);,
+                 $sformatf("expect fatal alert:%0s to fire after rst_ni glitched", alert_name))
+    check_fatal_alert_nonblocking(alert_name);
+  end
+
+  // Wait for random cycles and reconnect `shadowed_rst_n` with `rst_n`.
+  cfg.clk_rst_vif.wait_clks($urandom_range(50, 200));
+
+  cfg.rst_shadowed_vif.reconnect_shadowed_rst_n_to_rst_n();
+  has_fatal_alert = alert_name != "";
+endtask
+
+// ---------------------- Shadow reg helper functions/tasks -----------------------------------
+//
+// This function generates a random CSR value different from the input original value.
+// It can generate a rand value, or randomly flip one bit from the original value.
+virtual function bit [BUS_DW-1:0] get_shadow_reg_diff_val(dv_base_reg      csr,
+                                                          bit [BUS_DW-1:0] origin_val);
+  bit [BUS_DW-1:0] reg_mask = csr.get_reg_mask();
+  if ($urandom_range(0, 1)) begin
+    // Generate pure random value, but make sure it is not equal to the origin_val, and does not
+    // inject more than CSR's MSB.
+    `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(get_shadow_reg_diff_val,
+                                       (get_shadow_reg_diff_val & reg_mask) !=
+                                       (origin_val & reg_mask);)
+  end else begin
+    // Only flip one bit from the original.
+    int index;
+    `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(index, reg_mask[index] == 1;)
+    get_shadow_reg_diff_val = origin_val;
+    get_shadow_reg_diff_val[index] = ~get_shadow_reg_diff_val[index];
+  end
+endfunction
+
+// Wrap this shadow register csr_wr task with predict option so extended IPs can override it with
+// their customerized prediction. For example: AES's ctrl_shadow register needs to update `mode`
+// and `key_len` fields before calling the RAL predict function.
+virtual task csr_wr_for_shadow_reg_predict(dv_base_reg csr, uvm_reg_data_t wdata, bit predict = 1);
+  csr_wr(.ptr(csr), .value(wdata), .en_shadow_wr(0), .predict(predict));
+endtask
+
+// Alert triggers as soon as design accept the TLUL transaction, if wait until
+// csr_wr_for_shadow_reg_predict() finishes then check alert, the alert transaction might already
+// finished.
+virtual task shadow_reg_wr(dv_base_reg    csr,
+                           uvm_reg_data_t wdata,
+                           bit            en_shadow_wr = 1,
+                           bit            predict = 1);
+  // Sequence does not know if this write will trigger update error, so set the initial value to
+  // 1 to trigger alert check. Update this value once the write is complete.
+  // If no update_err_alert is expected, exit the alert handshake check.
+  bit exp_update_err_alert = 1;
+  fork
+    begin
+      repeat (en_shadow_wr ? 2 : 1) csr_wr_for_shadow_reg_predict(csr, wdata, predict);
+      exp_update_err_alert = csr.get_shadow_update_err();
+    end
+    begin
+      string alert_name = csr.get_update_err_alert_name();
+      // This logic gates alert_handler testbench because it does not have outgoing alert.
+      if (cfg.m_alert_agent_cfg.exists(alert_name)) begin
+        fork
+          begin
+            `DV_SPINWAIT(while (!cfg.m_alert_agent_cfg[alert_name].vif.get_alert()) begin
+                           cfg.clk_rst_vif.wait_clks(1);
+                         end
+                         cfg.m_alert_agent_cfg[alert_name].vif.wait_ack_complete();,
+                         $sformatf("%0s update_err alert timeout", csr.get_name()))
+          end
+          begin
+            wait (exp_update_err_alert == 0);
+          end
+        join_any
+        disable fork;
+      end
+    end
+  join
 endtask
 
 virtual function void predict_shadow_reg_status(bit predict_update_err  = 0,
