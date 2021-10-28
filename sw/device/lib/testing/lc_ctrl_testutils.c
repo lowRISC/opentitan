6e7688ab2760b5cd053862af6f3dcac31bf44979
Silvestrs Timofejevs <silvestrst@lowrisc.org>
2021-10-28 16:06:17 +0100

    [sw, testing] Introduce a Lifecycle Controller test util
    At the moment, only user is SRAM Controller test, however, any further
    shared functionality between tests can go into this module.
    
    Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>

---
+++ sw/device/lib/testing/lc_ctrl_testutils.c
@@ -0,0 +1,32 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/testing/lc_ctrl_testutils.h"
+
+#include <stdbool.h>
+
+#include "sw/device/lib/dif/dif_lc_ctrl.h"
+#include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/testing/check.h"
+
+bool lc_ctrl_testutils_debug_func_enabled(const dif_lc_ctrl_t *lc_ctrl) {
+  dif_lc_ctrl_state_t state;
+  CHECK_DIF_OK(dif_lc_ctrl_get_state(lc_ctrl, &state));
+
+  switch (state) {
+    case kDifLcCtrlStateTestUnlocked0:
+    case kDifLcCtrlStateTestUnlocked1:
+    case kDifLcCtrlStateTestUnlocked2:
+    case kDifLcCtrlStateTestUnlocked3:
+    case kDifLcCtrlStateTestUnlocked4:
+    case kDifLcCtrlStateTestUnlocked5:
+    case kDifLcCtrlStateTestUnlocked6:
+    case kDifLcCtrlStateTestUnlocked7:
+    case kDifLcCtrlStateDev:
+    case kDifLcCtrlStateRma:
+      return true;
+    default:
+      return false;
+  }
+}
