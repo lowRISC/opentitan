39a78813429dd54956243ce76f3bfa6ab00948e7
Timothy Trippel <ttrippel@google.com>
2021-10-29 02:44:25 +0000

    [dif/rom_ctrl] Auto-generate portion of DIFs.
    Currently, the DIF library for the ROM Controller is
    unimplemented. This begins the implementation of this DIF library by
    first checking in the auto-generated DIFs (init and alert DIFs).
    
    This partially addresses #8142.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

---
+++ sw/device/lib/dif/dif_rom_ctrl_unittest.cc
@@ -0,0 +1,24 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/dif/dif_rom_ctrl.h"
+
+#include "gtest/gtest.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/base/testing/mock_mmio.h"
+
+extern "C" {
+#include "rom_ctrl_regs.h"  // Generated.
+}  // extern "C"
+
+namespace dif_rom_ctrl_test {
+namespace {
+using mock_mmio::MmioTest;
+using mock_mmio::MockDevice;
+using testing::Test;
+
+// TODO: Add unit tests.
+
+}  // namespace
+}  // namespace dif_rom_ctrl_test
