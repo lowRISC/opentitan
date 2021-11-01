f2f547ae9a39473c53e2ae56685eb1727924341e
Madhuri.Patel <madhuri.patel@ensilica.com>
2021-11-01 16:07:06 +0000

    [sysrst_ctrl] Add sysrst_ctrl agent
    Signed-off-by: Madhuri.Patel <madhuri.patel@ensilica.com>

---
+++ hw/dv/sv/sysrst_ctrl_agent/README.md
@@ -0,0 +1,3 @@
+# SYSRST_CTRL UVM Agent
+
+SYSRST_CTRL UVM Agent is extended from DV library agent classes.
---
+++ hw/dv/sv/sysrst_ctrl_agent/sysrst_ctrl_if.sv
@@ -0,0 +1,11 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+interface sysrst_ctrl_if ();
+
+  // interface pins
+
+  // debug signals
+
+endinterface
