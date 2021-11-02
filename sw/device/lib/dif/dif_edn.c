85c2c05fbadd16706bd4a0b0235f5950a3cec85f
Douglas Reis <doreis@lowrisc.org>
2021-11-02 16:42:22 +0000

    [edn] Implement function dif_end_stop
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/device/lib/dif/dif_edn.c
+++ sw/device/lib/dif/dif_edn.c
@@ -5,3 +5,14 @@
 #include "sw/device/lib/dif/dif_edn.h"
 
 #include "edn_regs.h"  // Generated
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_edn_stop(const dif_edn_t *edn) {
+  if (edn == NULL) {
+    return kDifBadArg;
+  }
+
+  mmio_region_write32(edn->base_addr, EDN_CTRL_REG_OFFSET, EDN_CTRL_REG_RESVAL);
+
+  return kDifOk;
+}
