9851ec7189e436ac96d78ee24ae09bfe99f1f808
Martin Lueker-Boden <martin.lueker-boden@wdc.com>
2021-11-02 17:32:29 -0700

    [ spi_host ] Improvements to command queue usage
    - Add a CMDQD register to allow FW to monitor the number of segemnts in the current
      command queue.
    - Update documentation to accurately describe the recently added command queue
    - Remove a reference to the Command/Config CDC
    - Properly add CmdDepth as a paramter to spi_host.hjson
    
    Signed-off-by: Martin Lueker-Boden <martin.lueker-boden@wdc.com>

--- hw/ip/spi_host/rtl/spi_host_command_queue.sv
+++ hw/ip/spi_host/rtl/spi_host_command_queue.sv
@@ -19,15 +19,21 @@ module spi_host_command_queue #(
   output logic                       core_command_valid_o,
   input                              core_command_ready_i,
 
+  output logic [3:0]                 qd_o,
+
   output logic                       error_busy_o,
 
   input                              sw_rst_i
 );
 
+  localparam int CmdDepthW = prim_util_pkg::vbits(CmdDepth+1);
+
   logic command_ready;
 
   assign command_busy_o = ~command_ready;
   assign error_busy_o   = command_valid_i & command_busy_o;
+
+  logic [CmdDepthW-1:0] cmd_depth;
 
   prim_fifo_sync #(
     .Width(spi_host_cmd_pkg::CmdSize),
@@ -44,7 +50,9 @@ module spi_host_command_queue #(
     .rready_i (core_command_ready_i),
     .rdata_o  (core_command_o),
     .full_o   (),
-    .depth_o  ()
+    .depth_o  (cmd_depth)
   );
+
+  assign qd_o = 4'(cmd_depth);
 
 endmodule : spi_host_command_queue
