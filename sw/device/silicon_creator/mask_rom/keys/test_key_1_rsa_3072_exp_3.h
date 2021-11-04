7c3a3cf66f98addd676933b8cc26e90cd46fd9a4
Miguel Osorio <miguelosorio@google.com>
2021-11-04 09:24:14 -0700

    [sw/mask_rom] Move keys inside the mask_rom folder
    This comming implement the following changes:
    
    * Move ROM public keys inside the sw/device/silicon_creator/mask_rom/keys folder.
    * Split keys into header files to simplify auditing.
    * Update the key policies to TEST, as the device boots by default in RMA
      mode in the repository.
    * Change the key names from fpga_key to test_key.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

---
+++ sw/device/silicon_creator/mask_rom/keys/test_key_1_rsa_3072_exp_3.h
@@ -0,0 +1,41 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_MASK_ROM_KEYS_TEST_KEY_1_RSA_3072_EXP_3_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_MASK_ROM_KEYS_TEST_KEY_1_RSA_3072_EXP_3_H_
+
+#define TEST_KEY_1_RSA_3072_EXP_3                                       \
+  {                                                                     \
+    .n =                                                                \
+        {{                                                              \
+            0xbd158913, 0xab75ea1a, 0xc04e5292, 0x68f5778a, 0xa71418c7, \
+            0xddc4fc1c, 0xcb09302d, 0xedf3142b, 0x656d7d85, 0xf761d32a, \
+            0x2d334d1b, 0x26c91770, 0x5b9ba5a0, 0x00ac6c05, 0xbabaf1bb, \
+            0xa8299ecc, 0xb4223f99, 0x5b676ad3, 0xcaa786c2, 0x3e2f1785, \
+            0x204b6991, 0x21fa118f, 0x435573ab, 0xa3353ba1, 0x1074c161, \
+            0x2ad5e901, 0x7310247c, 0x1e21b8e9, 0x0cfc7762, 0x0a9139b1, \
+            0xfc655b33, 0x6990faaf, 0xbb88faec, 0x7c7bd6ef, 0x261e4555, \
+            0x6bc3d813, 0x5ce6e18b, 0xdd308629, 0x37d3d54d, 0x65acd84d, \
+            0x97b7e0c3, 0xc0d35caa, 0xb0be177a, 0x09473af3, 0x67f43155, \
+            0x3b2f7661, 0xf9255df2, 0x1b42c84c, 0x355cd607, 0x835e74ca, \
+            0x1d011c4e, 0x46652555, 0x1566f96f, 0x6cffd2f9, 0x204e783e, \
+            0xa178a2eb, 0xe7297a95, 0xd7380039, 0x1a685545, 0x76ed97c9, \
+            0x6bc0b1b7, 0xd9b1338e, 0xa3b23005, 0x6fe7109f, 0x01c232e1, \
+            0x851639c5, 0xe81d338c, 0x25ebe0c4, 0x5b0202cd, 0x3690cb70, \
+            0xad13b664, 0x8bf7833e, 0x6017349c, 0xf6e90b08, 0x953ef3d8, \
+            0x4bc11817, 0xd0f6e840, 0xfe01a954, 0x9b866209, 0xb9653ff8, \
+            0x0d654f5c, 0xff78177c, 0x3688833c, 0x57cc0c30, 0x71965be7, \
+            0xf61fb728, 0xaeac8ca2, 0xbdc9848b, 0x954c529f, 0x9917ac7f, \
+            0x4ba4c007, 0xce2dbf0b, 0xfc7d8504, 0x2712580b, 0xd0293151, \
+            0xa4dbbff3,                                                 \
+        }},                                                             \
+    .n0_inv =                                                           \
+        {                                                               \
+            0x079056e5, 0xe151dae1, 0xd4f9deee, 0xe18c4cab,             \
+            0x868f9abe, 0x8643ed1c, 0x58022be6, 0x8f8972c9,             \
+        },                                                              \
+    .exponent = 3,                                                      \
+  }
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_MASK_ROM_KEYS_TEST_KEY_1_RSA_3072_EXP_3_H_
