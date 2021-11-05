18723bcb32e855b1cf10295cd40099cdb82a00a0
Cindy Chen <chencindy@opentitan.org>
2021-11-05 10:48:14 -0700

    [dv/alert_handler] Add an alert_handler_if
    This PR adds an alert_handler_if drive lpg inputs.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

---
+++ hw/top_earlgrey/ip_autogen/alert_handler/dv/env/alert_handler_if.sv
@@ -0,0 +1,37 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Interface for LPG and crashdump output.
+interface alert_handler_if(input clk, input rst_n);
+  import alert_pkg::*;
+  import prim_mubi_pkg::*;
+
+  mubi4_t [NLpg-1:0] lpg_cg_en;
+  mubi4_t [NLpg-1:0] lpg_rst_en;
+
+  string msg_id = "alert_handler_if";
+
+  function automatic void init();
+    lpg_cg_en = '{default: MuBi4False};
+    lpg_rst_en = '{default: MuBi4False};
+  endfunction
+
+  // TODO: randomize all values outside of the mubi4 enum.
+  function automatic void set_lpg_en(int index);
+    check_lpg_index(index);
+    lpg_cg_en[index] = MuBi4True;
+  endfunction
+
+  function automatic void set_lpg_rst_en(int index);
+    check_lpg_index(index);
+    lpg_rst_en[index] = MuBi4True;
+  endfunction
+
+  function automatic void check_lpg_index(int index);
+    if (index >= NLpg) begin
+      `uvm_fatal(msg_id, $sformatf("Alert_handler has %0d LPGs but attempts to set index %0d",
+                                   NLpg, index))
+    end
+  endfunction
+endinterface
