f4dfd4606e489dec3f6de8a541b2f9df6b0cf950
Timothy Trippel <ttrippel@google.com>
2021-11-05 06:32:48 +0000

    [dif/rom_ctrl] Add DIF header and implementation.
    This completes the header and implementation of all ROM Controller DIFs.
    At this time, the ROM Controller is a very simple IP, so there are only
    three DIFs.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

--- sw/device/lib/dif/dif_rom_ctrl.h
+++ sw/device/lib/dif/dif_rom_ctrl.h
@@ -11,13 +11,82 @@
  * Functions
  */
 
+#include <stdint.h>
+
 #include "sw/device/lib/dif/autogen/dif_rom_ctrl_autogen.h"
+
+#include "rom_ctrl_regs.h"  // Generated.
 
 #ifdef __cplusplus
 extern "C" {
 #endif  // __cplusplus
 
-// TODO: Add DIFs.
+/**
+ * A fatal alert cause error.
+ *
+ * See also: `dif_rom_ctrl_fatal_alert_causes_t`.
+ */
+typedef enum dif_rom_ctrl_fatal_alert_cause {
+  /**
+   * No error has occured.
+   */
+  kDifRomCtrlFatalAlertCauseNoError = 0,
+  /**
+   * Set on a fatal error detected by the ROM checker.
+   */
+  kDifRomCtrlFatalAlertCauseCheckerError =
+      ROM_CTRL_FATAL_ALERT_CAUSE_CHECKER_ERROR_BIT,
+  /**
+   * Set on an integrity error from the register interface.
+   */
+  kDifRomCtrlFatalAlertCauseIntegrityError =
+      ROM_CTRL_FATAL_ALERT_CAUSE_INTEGRITY_ERROR_BIT,
+} dif_rom_ctrl_fatal_alert_cause_t;
+
+/**
+ * A set of fatal alert cause errors.
+ */
+typedef uint32_t dif_rom_ctrl_fatal_alert_causes_t;
+
+/**
+ * A typed representation of the ROM digest.
+ */
+typedef struct dif_rom_ctrl_digest {
+  uint32_t digest[ROM_CTRL_DIGEST_MULTIREG_COUNT];
+} dif_rom_ctrl_digest_t;
+
+/**
+ * Reads the fatal alert cause bits of the ROM Controller.
+ *
+ * @param rom_ctrl A ROM Controller handle.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_rom_ctrl_get_fatal_alert_cause(
+    const dif_rom_ctrl_t *rom_ctrl,
+    dif_rom_ctrl_fatal_alert_causes_t *alert_causes);
+
+/**
+ * Reads the (KMAC computed) ROM digest.
+ *
+ * @param rom_ctrl A ROM Controller handle.
+ * @param digest KMAC digest of ROM.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_rom_ctrl_get_digest(const dif_rom_ctrl_t *rom_ctrl,
+                                     dif_rom_ctrl_digest_t *digest);
+
+/**
+ * Reads the expected ROM digest contained in the top eight words of ROM.
+ *
+ * @param rom_ctrl A ROM Controller handle.
+ * @param expected_digest Expected KMAC digest of ROM.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_rom_ctrl_get_expected_digest(
+    const dif_rom_ctrl_t *rom_ctrl, dif_rom_ctrl_digest_t *expected_digest);
 
 #ifdef __cplusplus
 }  // extern "C"
