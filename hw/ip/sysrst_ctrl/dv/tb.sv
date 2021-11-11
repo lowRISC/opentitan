f4eb62a93a656c933c60159419d9d7f17bc2b17d
Madhuri Patel <madhuri.patel@ensilica.com>
2021-11-11 15:35:55 +0000

    Updated testbench template files to add path to testplan
    Signed-off-by: Madhuri Patel <madhuri.patel@ensilica.com>

---
+++ hw/ip/sysrst_ctrl/dv/tb.sv
@@ -0,0 +1,51 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+module tb;
+  // dep packages
+  import uvm_pkg::*;
+  import dv_utils_pkg::*;
+  import sysrst_ctrl_env_pkg::*;
+  import sysrst_ctrl_test_pkg::*;
+
+  // macro includes
+  `include "uvm_macros.svh"
+  `include "dv_macros.svh"
+
+  wire clk, rst_n;
+  wire devmode;
+  wire [NUM_MAX_INTERRUPTS-1:0] interrupts;
+
+  // interfaces
+  clk_rst_if clk_rst_if(.clk(clk), .rst_n(rst_n));
+  pins_if #(NUM_MAX_INTERRUPTS) intr_if(interrupts);
+  pins_if #(1) devmode_if(devmode);
+  tl_if tl_if(.clk(clk), .rst_n(rst_n));
+
+  `DV_ALERT_IF_CONNECT
+
+  // dut
+  sysrst_ctrl dut (
+    .clk_i                (clk      ),
+    .rst_ni               (rst_n    ),
+
+    .tl_i                 (tl_if.h2d),
+    .tl_o                 (tl_if.d2h),
+    .alert_rx_i           (alert_rx ),
+    .alert_tx_o           (alert_tx )
+    // TODO: add remaining IOs and hook them
+  );
+
+  initial begin
+    // drive clk and rst_n from clk_if
+    clk_rst_if.set_active();
+    uvm_config_db#(virtual clk_rst_if)::set(null, "*.env", "clk_rst_vif", clk_rst_if);
+    uvm_config_db#(intr_vif)::set(null, "*.env", "intr_vif", intr_if);
+    uvm_config_db#(devmode_vif)::set(null, "*.env", "devmode_vif", devmode_if);
+    uvm_config_db#(virtual tl_if)::set(null, "*.env.m_tl_agent*", "vif", tl_if);
+    $timeformat(-12, 0, " ps", 12);
+    run_test();
+  end
+
+endmodule
