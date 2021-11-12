4c501bb4c04157749c438241a5affd016b6a0f13
Timothy Chen <timothytim@google.com>
2021-11-12 13:11:56 -0800

    [ci] re-enable entropy and pwrmgr smoke tests
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- test/systemtest/earlgrey/config.py
+++ test/systemtest/earlgrey/config.py
@@ -65,10 +65,10 @@ TEST_APPS_SELFCHECKING = [
         "name": "rv_plic_smoketest",
         "targets": ["sim_verilator", "fpga_cw310"],
     },
-    # TODO(#6656): AST is not instantiated in chip_earlgrey_verilator.
-    # {
-    #     "name": "pwrmgr_smoketest",
-    # },
+    {
+        "name": "pwrmgr_smoketest",
+        "targets": ["sim_verilator"],
+    },
     {
         "name": "rstmgr_smoketest",
         "targets": ["sim_verilator", "fpga_cw310", "fpga_nexysvideo"],
@@ -90,11 +90,10 @@ TEST_APPS_SELFCHECKING = [
     #    "name": "csrng_smoketest",
     #   "targets": ["sim_verilator", "fpga_cw310"],
     # },
-    # TODO(lowrisc/opentitan#8114):
-    # {
-    #     "name": "entropy_src_smoketest",
-    #     "targets": ["sim_verilator", "fpga_cw310"],
-    # },
+    {
+        "name": "entropy_src_smoketest",
+        "targets": ["sim_verilator", "fpga_cw310"],
+    },
     {
         "name": "kmac_smoketest",
         "targets": ["sim_verilator", "fpga_cw310"],
