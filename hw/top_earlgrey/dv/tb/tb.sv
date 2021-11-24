2c24b85e0c6cb365da3009655b37609f3bddb7f7
Srikrishna Iyer <sriyer@google.com>
2021-11-24 14:23:16 -0800

    [chip, dv] Randomize SRAM
    Fixes private CI.
    Unconditionally initialize the SRAM with garbage data on reset for chip
    level SW based tests.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/top_earlgrey/dv/tb/tb.sv
+++ hw/top_earlgrey/dv/tb/tb.sv
@@ -353,7 +353,7 @@ module tb;
                                       .path  (`DV_STRINGIFY(`RAM_MAIN_MEM_HIER)),
                                       .depth ($size(`RAM_MAIN_MEM_HIER)),
                                       .n_bits($bits(`RAM_MAIN_MEM_HIER)),
-                                      .err_detection_scheme(mem_bkdr_util_pkg::ParityOdd));
+                                      .err_detection_scheme(mem_bkdr_util_pkg::Ecc_39_32));
       `MEM_BKDR_UTIL_FILE_OP(m_mem_bkdr_util[RamMain0], `RAM_MAIN_MEM_HIER)
 
       `uvm_info("tb.sv", "Backdoor init RAM RET", UVM_MEDIUM)
@@ -361,7 +361,7 @@ module tb;
                                      .path  (`DV_STRINGIFY(`RAM_RET_MEM_HIER)),
                                      .depth ($size(`RAM_RET_MEM_HIER)),
                                      .n_bits($bits(`RAM_RET_MEM_HIER)),
-                                     .err_detection_scheme(mem_bkdr_util_pkg::ParityOdd));
+                                     .err_detection_scheme(mem_bkdr_util_pkg::Ecc_39_32));
       `MEM_BKDR_UTIL_FILE_OP(m_mem_bkdr_util[RamRet0], `RAM_RET_MEM_HIER)
 
       `uvm_info("tb.sv", "Backdoor init ROM", UVM_MEDIUM)
