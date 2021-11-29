6e1f93d8985cda97b721a394e84954ea9ac54459
Guillermo Maturana <maturana@google.com>
2021-11-29 09:37:51 -0800

    [dv/rstmgr] Use localparam for MaxSyncDelay
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- hw/ip/rstmgr/rtl/rstmgr_cnsty_chk.sv
+++ hw/ip/rstmgr/rtl/rstmgr_cnsty_chk.sv
@@ -14,9 +14,6 @@
 module rstmgr_cnsty_chk
   import rstmgr_pkg::*;
   import rstmgr_reg_pkg::*;
-#(
-  parameter int MaxSyncDelay = 2
-)
 (
   input clk_i,
   input rst_ni,
@@ -27,6 +24,8 @@ module rstmgr_cnsty_chk
   output logic sw_rst_req_clr_o,
   output logic err_o
 );
+
+  localparam int MaxSyncDelay = 2;
 
   // The "+ 2" here is because the cnt counter that uses this width needs to be able to count up to
   // MaxSyncDelay + 1.
