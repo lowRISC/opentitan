d7c3e7d14263ff486f87a2cf0db8b116d66cfe31
Alphan Ulusoy <alphan@google.com>
2021-12-01 16:26:03 -0500

    [sw/silicon_creator] Add mock_boot_data.h
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

---
+++ sw/device/silicon_creator/lib/mock_boot_data.h
@@ -0,0 +1,41 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_MOCK_BOOT_DATA_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_MOCK_BOOT_DATA_H_
+
+#include "sw/device/silicon_creator/lib/boot_data.h"
+#include "sw/device/silicon_creator/testing/mask_rom_test.h"
+
+namespace mask_rom_test {
+namespace internal {
+
+/**
+ * Mock class for boot_data.
+ */
+class MockBootData : public GlobalMock<MockBootData> {
+ public:
+  MOCK_METHOD(rom_error_t, Read,
+              (lifecycle_state_t lc_state, boot_data_t *boot_data));
+  MOCK_METHOD(rom_error_t, Write, (const boot_data_t *boot_data));
+};
+
+}  // namespace internal
+
+using MockBootData = testing::StrictMock<internal::MockBootData>;
+
+extern "C" {
+
+rom_error_t boot_data_read(lifecycle_state_t lc_state, boot_data_t *boot_data) {
+  return MockBootData::Instance().Read(lc_state, boot_data);
+}
+
+rom_error_t boot_data_write(const boot_data_t *boot_data) {
+  return MockBootData::Instance().Write(boot_data);
+}
+
+}  // extern "C"
+}  // namespace mask_rom_test
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_MOCK_BOOT_DATA_H_
