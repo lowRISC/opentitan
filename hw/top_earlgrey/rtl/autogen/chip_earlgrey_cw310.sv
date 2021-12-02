cbf0c50ed24631c2fb6d297c3309ed722e65796a
Timothy Chen <timothytim@google.com>
2021-12-01 22:37:46 -0800

    [top] remove multiply-driven clock valid signals
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- hw/top_earlgrey/rtl/autogen/chip_earlgrey_cw310.sv
+++ hw/top_earlgrey/rtl/autogen/chip_earlgrey_cw310.sv
@@ -1033,23 +1033,6 @@ module chip_earlgrey_cw310 #(
   //////////////////////
   // Top-level design //
   //////////////////////
-  pwrmgr_pkg::pwr_ast_rsp_t ast_base_pwr;
-  ast_pkg::ast_alert_req_t ast_base_alerts;
-  ast_pkg::ast_status_t ast_base_status;
-
-  assign ast_base_pwr.slow_clk_val = 1'b1;
-  assign ast_base_pwr.core_clk_val = 1'b1;
-  assign ast_base_pwr.io_clk_val   = 1'b1;
-  assign ast_base_pwr.usb_clk_val  = 1'b1;
-  assign ast_base_pwr.main_pok     = 1'b1;
-
-  ast_pkg::ast_dif_t silent_alert = '{
-                                       p: 1'b0,
-                                       n: 1'b1
-                                     };
-
-  assign ast_base_alerts.alerts = {ast_pkg::NumAlerts{silent_alert}};
-  assign ast_base_status.io_pok = {ast_pkg::NumIoRails{1'b1}};
 
   // the rst_ni pin only goes to AST
   // the rest of the logic generates reset based on the 'pok' signal.
