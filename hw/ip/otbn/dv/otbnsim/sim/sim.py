lowRISC/opentitan

commit 0ac448acf7f8daa24b3daa0ea98058d1f8c29165

[otbn, rtl, dv] Add prefetch stage
The prefetch stage fetches instructions a cycle ahead of when they are
required. This is to enable flopped register file inputs to implement
blanking.

The prefetch stage isn't really speculative. It only prefetches where it
can be confident of the address, in situations where it can't
(specifically branches and jumps) it does not prefetch.

Signed-off-by: Greg Chadwick <gac@lowrisc.org>

hw/ip/otbn/dv/otbnsim/sim/sim.py
@@ -109,8 +109,10 @@ class OTBNSim:
         pc_before = self.state.pc
         self.state.commit(sim_stalled=False)
 
-        # Fetch the next instruction
-        self._next_insn = self._fetch(self.state.pc)
+        # Fetch the next instruction unless this instruction had
+        # `has_fetch_stall` set, in which case we inject a single cycle stall.
+        self._next_insn = (None if insn.has_fetch_stall
+                           else self._fetch(self.state.pc))
 
         disasm = insn.disassemble(pc_before)
         if verbose:
@@ -129,7 +131,7 @@ class OTBNSim:
         if not self.state.running():
             return (None, [])
 
-        if self.state.fsm_state == FsmState.PRE_EXEC:
+        if self.state.fsm_state in [FsmState.PRE_EXEC, FsmState.FETCH_WAIT]:
             # Zero INSN_CNT the cycle after we are told to start (and every
             # cycle after that until we start executing instructions, but that
             # doesn't really matter)

hw/ip/otbn/dv/otbnsim/sim/state.py
@@ -24,13 +24,13 @@ class FsmState(IntEnum):
 
     The FSM diagram looks like:
 
-          /---------------------------------------------\
-          |                                             |
-          v                            /->  POST_EXEC --/
-        IDLE  ->  PRE_EXEC  ->  EXEC  <
-                                       \->  LOCKING   --\
-                                                        |
-          /---------------------------------------------/
+          /----------------------------------------------------------\
+          |                                                          |
+          v                                         /->  POST_EXEC --/
+        IDLE  ->  PRE_EXEC  ->  FETCH_WAIT -> EXEC <
+                                                    \->  LOCKING   --\
+                                                                     |
+          /----------------------------------------------------------/
           |
           v
         LOCKED
@@ -39,10 +39,11 @@ class FsmState(IntEnum):
     fatal errors. It matches Status.IDLE. LOCKED represents the state when
     there has been a fatal error. It matches Status.LOCKED.
 
-    PRE_EXEC, EXEC, POST_EXEC and LOCKING correspond to Status.BUSY_EXECUTE.
-    PRE_EXEC is the period after starting OTBN where we're still waiting for an
-    EDN value to seed URND. EXEC is the period where we start fetching and
-    executing instructions.
+    PRE_EXEC, FETCH_WAIT, EXEC, POST_EXEC and LOCKING correspond to
+    Status.BUSY_EXECUTE.  PRE_EXEC is the period after starting OTBN where we're
+    still waiting for an EDN value to seed URND. FETCH_WAIT is the single cycle
+    delay after seeding URND to fill the prefetch stage. EXEC is the period
+    where we start fetching and executing instructions.
 
     POST_EXEC and LOCKING are both used for the single cycle after we finish
     executing where the STATUS register gets updated. The difference between
@@ -56,8 +57,9 @@ class FsmState(IntEnum):
     '''
     IDLE = 0
     PRE_EXEC = 10
-    EXEC = 11
-    POST_EXEC = 12
+    FETCH_WAIT = 11
+    EXEC = 12
+    POST_EXEC = 13
     LOCKING = 13
     LOCKED = 2550
 
@@ -286,7 +288,16 @@ class OTBNState:
             if self.wsrs.URND.running:
                 # This part is strictly for standalone simulation. Otherwise
                 # we would set fsm_state before commit (at urnd_completed)
-                self.fsm_state = FsmState.EXEC
+                self.fsm_state = FsmState.FETCH_WAIT
+
+            return
+
+        # FETCH_WAIT works like PRE_EXEC, but it's only ever a single cycle
+        # wait.
+        if self.fsm_state == FsmState.FETCH_WAIT:
+            self.ext_regs.commit()
+            self.fsm_state = FsmState.EXEC
+
             return
 
         # If we are in POST_EXEC mode, this is the single cycle after the end

hw/ip/otbn/dv/otbnsim/sim/insn.py
@@ -301,6 +301,7 @@ class SW(OTBNInsn):
 class BEQ(OTBNInsn):
     insn = insn_for_mnemonic('beq', 3)
     affects_control = True
+    has_fetch_stall = True
 
     def __init__(self, raw: int, op_vals: Dict[str, int]):
         super().__init__(raw, op_vals)
@@ -326,6 +327,7 @@ class BEQ(OTBNInsn):
 class BNE(OTBNInsn):
     insn = insn_for_mnemonic('bne', 3)
     affects_control = True
+    has_fetch_stall = True
 
     def __init__(self, raw: int, op_vals: Dict[str, int]):
         super().__init__(raw, op_vals)
@@ -352,6 +354,7 @@ class BNE(OTBNInsn):
 class JAL(OTBNInsn):
     insn = insn_for_mnemonic('jal', 2)
     affects_control = True
+    has_fetch_stall = True
 
     def __init__(self, raw: int, op_vals: Dict[str, int]):
         super().__init__(raw, op_vals)
@@ -373,6 +376,7 @@ class JAL(OTBNInsn):
 class JALR(OTBNInsn):
     insn = insn_for_mnemonic('jalr', 3)
     affects_control = True
+    has_fetch_stall = True
 
     def __init__(self, raw: int, op_vals: Dict[str, int]):
         super().__init__(raw, op_vals)

hw/ip/otbn/dv/otbnsim/sim/isa.py
@@ -65,6 +65,10 @@ class OTBNInsn:
     # false by the EmptyInsn subclass)
     has_bits = True
 
+    # A class variable that is true if there will be a cycle of fetch stall
+    # after the instruction executes.
+    has_fetch_stall = False
+
     def __init__(self, raw: int, op_vals: Dict[str, int]):
         self.raw = raw
         self.op_vals = op_vals

hw/ip/otbn/dv/otbnsim/test/stats_test.py
@@ -136,7 +136,7 @@ def test_general_and_loop(tmpdir: py.path.local) -> None:
     stats = _simulate_asm_file(asm_file, tmpdir)
 
     # General statistics
-    assert stats.stall_count == 3
+    assert stats.stall_count == 4
     assert stats.get_insn_count() == 28
     assert stats.insn_histo == {'addi': 22, 'loop': 4, 'loopi': 1, 'ecall': 1}
     assert stats.func_calls == []

hw/ip/otbn/dv/verilator/otbn_top_sim.sv
@@ -34,7 +34,6 @@ module otbn_top_sim (
   logic [ImemAddrWidth-1:0] imem_addr;
   logic [38:0]              imem_rdata;
   logic                     imem_rvalid;
-  logic                     imem_rerror;
 
   // Data memory (DMEM) signals
   logic                     dmem_req;
@@ -76,10 +75,10 @@ module otbn_top_sim (
 
     .imem_req_o                  ( imem_req            ),
     .imem_addr_o                 ( imem_addr           ),
-    .imem_wdata_o                (                     ),
-    .imem_rdata_i                ( imem_rdata[31:0]    ),
+    .imem_rdata_i                ( imem_rdata          ),
     .imem_rvalid_i               ( imem_rvalid         ),
-    .imem_rerror_i               ( imem_rerror         ),
+
+    .insn_fetch_err_o            (                     ),
 
     .dmem_req_o                  ( dmem_req            ),
     .dmem_write_o                ( dmem_write          ),
@@ -109,11 +108,6 @@ module otbn_top_sim (
     .sideload_key_shares_i       ( sideload_key_shares ),
     .sideload_key_shares_valid_i ( 2'b11               )
   );
-
-  // The top bits of IMEM rdata aren't currently used (they will eventually be used for integrity
-  // checks both on the bus and within the core)
-  logic unused_imem_top_rdata;
-  assign unused_imem_top_rdata = &{1'b0, imem_rdata[38:32]};
 
   localparam logic [WLEN-1:0] FixedEdnVal = {{(WLEN / 4){4'h9}}};
 
@@ -264,9 +258,6 @@ module otbn_top_sim (
     .rerror_o     (                         ),
     .cfg_i        ( '0                      )
   );
-
-  // No integrity errors in Verilator testbench
-  assign imem_rerror = 1'b0;
 
   // When OTBN is done let a few more cycles run then finish simulation
   logic [1:0] finish_counter;

hw/ip/otbn/rtl/otbn.sv
@@ -181,9 +181,9 @@ module otbn
   logic imem_req_core;
   logic imem_write_core;
   logic [ImemIndexWidth-1:0] imem_index_core;
-  logic [31:0] imem_rdata_core;
+  logic [38:0] imem_rdata_core;
   logic imem_rvalid_core;
-  logic imem_rerror_core;
+  logic insn_fetch_err;
 
   logic imem_req_bus;
   logic imem_dummy_response_q, imem_dummy_response_d;
@@ -356,7 +356,7 @@ module otbn
   // the currently executed instruction from IMEM through the bus
   // unintentionally.
   assign imem_rdata_bus  = !imem_access_core && !illegal_bus_access_q ? imem_rdata : 39'b0;
-  assign imem_rdata_core = imem_rdata[31:0];
+  assign imem_rdata_core = imem_rdata;
 
   // When an illegal bus access is seen, always return a dummy response the follow cycle.
   assign imem_rvalid_bus  = (~imem_access_core & imem_rvalid) | imem_dummy_response_q;
@@ -365,7 +365,6 @@ module otbn
   // No imem errors reported for bus reads. Integrity is carried through on the bus so integrity
   // checking on TL responses will pick up any errors.
   assign imem_rerror_bus = 1'b0;
-  assign imem_rerror_core = imem_rerror;
 
   // Data Memory (DMEM) ========================================================
 
@@ -662,8 +661,8 @@ module otbn
 
   // FATAL_ALERT_CAUSE register. The .de and .d values are equal for each bit, so that it can only
   // be set, not cleared.
-  assign hw2reg.fatal_alert_cause.imem_intg_violation.de = imem_rerror;
-  assign hw2reg.fatal_alert_cause.imem_intg_violation.d  = imem_rerror;
+  assign hw2reg.fatal_alert_cause.imem_intg_violation.de = insn_fetch_err;
+  assign hw2reg.fatal_alert_cause.imem_intg_violation.d  = insn_fetch_err;
   assign hw2reg.fatal_alert_cause.dmem_intg_violation.de = dmem_rerror;
   assign hw2reg.fatal_alert_cause.dmem_intg_violation.d  = dmem_rerror;
   // TODO: Register file errors
@@ -693,7 +692,7 @@ module otbn
                                   reg2hw.alert_test.recov.qe;
 
   logic [NumAlerts-1:0] alerts;
-  assign alerts[AlertFatal] = imem_rerror          |
+  assign alerts[AlertFatal] = insn_fetch_err       |
                               dmem_rerror          |
                               bus_intg_violation   |
                               illegal_bus_access_d |
@@ -866,7 +865,8 @@ module otbn
       .imem_addr_o                 (imem_addr_core),
       .imem_rdata_i                (imem_rdata_core),
       .imem_rvalid_i               (imem_rvalid_core),
-      .imem_rerror_i               (imem_rerror_core),
+
+      .insn_fetch_err_o            (insn_fetch_err),
 
       .dmem_req_o                  (dmem_req_core),
       .dmem_write_o                (dmem_write_core),
@@ -917,7 +917,8 @@ module otbn
       .imem_addr_o                 (imem_addr_core),
       .imem_rdata_i                (imem_rdata_core),
       .imem_rvalid_i               (imem_rvalid_core),
-      .imem_rerror_i               (imem_rerror_core),
+
+      .insn_fetch_err_o            (insn_fetch_err),
 
       .dmem_req_o                  (dmem_req_core),
       .dmem_write_o                (dmem_write_core),

hw/ip/otbn/rtl/otbn_controller.sv
@@ -31,6 +31,7 @@ module otbn_controller
   // Next instruction selection (to instruction fetch)
   output logic                     insn_fetch_req_valid_o,
   output logic [ImemAddrWidth-1:0] insn_fetch_req_addr_o,
+  output logic                     insn_fetch_resp_clear_o,
   // Error from fetch requested last cycle
   input  logic                     insn_fetch_err_i,
 
@@ -135,7 +136,14 @@ module otbn_controller
   input  logic        lifecycle_escalation_i,
   input  logic        software_errs_fatal_i,
 
-  input  logic [1:0]  sideload_key_shares_valid_i
+  input  logic [1:0]  sideload_key_shares_valid_i,
+
+  // Prefetch stage control
+  output logic                     prefetch_en_o,
+  output logic                     prefetch_loop_active_o,
+  output logic [31:0]              prefetch_loop_iterations_o,
+  output logic [ImemAddrWidth-1:0] prefetch_loop_end_addr_o,
+  output logic [ImemAddrWidth-1:0] prefetch_loop_jump_addr_o
 );
   otbn_state_e state_q, state_d;
 
@@ -146,7 +154,8 @@ module otbn_controller
   logic done_complete;
   logic executing;
 
-  logic insn_fetch_req_valid_raw;
+  logic                     insn_fetch_req_valid_raw;
+  logic [ImemAddrWidth-1:0] insn_fetch_req_addr_last;
 
   logic stall;
   logic ispr_stall;
@@ -274,13 +283,22 @@ module otbn_controller
   assign next_insn_addr_wide = {1'b0, insn_addr_i} + 'd4;
   assign next_insn_addr = next_insn_addr_wide[ImemAddrWidth-1:0];
 
+  // Record address for fetch request so it can be retried when an invalid response is received
+  always_ff @(posedge clk_i) begin
+    if (insn_fetch_req_valid_raw) begin
+      insn_fetch_req_addr_last <= insn_fetch_req_addr_o;
+    end
+  end
+
   always_comb begin
     state_d                  = state_q;
     // `insn_fetch_req_valid_raw` is the value `insn_fetch_req_valid_o` before any errors are
     // considered.
     insn_fetch_req_valid_raw = 1'b0;
     insn_fetch_req_addr_o    = '0;
+    insn_fetch_resp_clear_o  = 1'b1;
     err_bits_en              = 1'b0;
+    prefetch_en_o            = 1'b0;
 
     // TODO: Harden state machine
     // TODO: Jumps/branches
@@ -291,6 +309,7 @@ module otbn_controller
 
           insn_fetch_req_addr_o    = '0;
           insn_fetch_req_valid_raw = 1'b1;
+          prefetch_en_o            = 1'b1;
 
           // Enable error bits to zero them on start
           err_bits_en = 1'b1;
@@ -298,15 +317,21 @@ module otbn_controller
       end
       OtbnStateRun: begin
         insn_fetch_req_valid_raw = 1'b1;
+        prefetch_en_o            = 1'b1;
 
-        if (done_complete) begin
+        if (!insn_valid_i) begin
+          insn_fetch_req_addr_o = insn_fetch_req_addr_last;
+        end else if (done_complete) begin
           state_d                  = OtbnStateHalt;
           insn_fetch_req_valid_raw = 1'b0;
+          prefetch_en_o            = 1'b0;
         end else begin
-          // When stalling refetch the same instruction to keep decode inputs constant
           if (stall) begin
+            // When stalling don't request a new fetch and don't clear response either to keep
+            // current instruction.
             state_d               = OtbnStateStall;
-            insn_fetch_req_addr_o = insn_addr_i;
+            insn_fetch_req_valid_raw = 1'b0;
+            insn_fetch_resp_clear_o  = 1'b0;
           end else begin
             if (branch_taken) begin
               insn_fetch_req_addr_o = branch_target;
@@ -319,13 +344,16 @@ module otbn_controller
         end
       end
       OtbnStateStall: begin
-        insn_fetch_req_valid_raw = 1'b1;
-
+        prefetch_en_o = 1'b1;
         // When stalling refetch the same instruction to keep decode inputs constant
         if (stall) begin
-          state_d               = OtbnStateStall;
-          insn_fetch_req_addr_o = insn_addr_i;
+          state_d                  = OtbnStateStall;
+          //insn_fetch_req_addr_o = insn_addr_i;
+          insn_fetch_req_valid_raw = 1'b0;
+          insn_fetch_resp_clear_o  = 1'b0;
         end else begin
+          insn_fetch_req_valid_raw = 1'b1;
+
           if (loop_jump) begin
             insn_fetch_req_addr_o = loop_jump_addr;
           end else begin
@@ -345,6 +373,9 @@ module otbn_controller
     // On any error immediately halt, either going to OtbnStateLocked or OtbnStateHalt depending on
     // whether it was a fatal error.
     if (err) begin
+      prefetch_en_o           = 1'b0;
+      insn_fetch_resp_clear_o = 1'b1;
+
       if (!secure_wipe_running_i) begin
         // Capture error bits on error unless a secure wipe is in progress
         err_bits_en = 1'b1;
@@ -363,6 +394,8 @@ module otbn_controller
     end
   end
 
+  `ASSERT(InsnAlwaysValidInStall, state_q == OtbnStateStall |-> insn_valid_i)
+
   // Anything that moves us or keeps us in the stall state should cause `stall` to be asserted
   `ASSERT(StallIfNextStateStall, insn_valid_i & (state_d == OtbnStateStall) |-> stall)
 
@@ -379,7 +412,7 @@ module otbn_controller
       end else if (branch_taken) begin
         imem_addr_err = branch_target_overflow;
       end else begin
-        imem_addr_err = next_insn_addr_wide[ImemAddrWidth];
+        imem_addr_err = next_insn_addr_wide[ImemAddrWidth] & insn_valid_i;
       end
     end
   end
@@ -531,7 +564,12 @@ module otbn_controller
     .loop_err_o          (loop_err),
 
     .jump_or_branch_i    (jump_or_branch),
-    .otbn_stall_i        (stall)
+    .otbn_stall_i        (stall),
+
+    .prefetch_loop_active_o,
+    .prefetch_loop_iterations_o,
+    .prefetch_loop_end_addr_o,
+    .prefetch_loop_jump_addr_o
   );
 
   // loop_start_req indicates the instruction wishes to start a loop, loop_start_commit confirms it

commit 71cc4b0337fd84419c3a47d7899bf230e680adc2

[otbn,dv] Correctly model timing for IMEM integrity errors
This was broken by 0ac448a (adding the prefetch stage) because the RTL
now caches IMEM contents for a cycle, meaning that injected IMEM
integrity errors arrive a cycle later.

We also slightly re-jig the timing for where INSN_CNT gets zeroed to
match the new behaviour.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/otbn/dv/otbnsim/sim/sim.py
@@ -154,11 +154,7 @@ class OTBNSim:
             self.state.ext_regs.write('INSN_CNT', 0, True)
             return (None, changes)
 
-        if self.state.fsm_state == FsmState.POST_EXEC:
-            return (None, self._on_stall(verbose, fetch_next=False))
-
-        if self.state.fsm_state == FsmState.LOCKING:
-            self.state.ext_regs.write('INSN_CNT', 0, True)
+        if self.state.fsm_state in [FsmState.POST_EXEC, FsmState.LOCKING]:
             return (None, self._on_stall(verbose, fetch_next=False))
 
         assert self.state.fsm_state == FsmState.EXEC

hw/ip/otbn/dv/otbnsim/sim/state.py
@@ -104,8 +104,13 @@ class OTBNState:
         self.urnd_256b = 4 * [0]
         self.urnd_64b = 0
 
-        # This flag is set to true if we've injected integrity errors, trashing
-        # the whole of IMEM. The next fetch should fail.
+        # To simulate injecting integrity errors, we set a flag to say that
+        # IMEM is no longer readable without getting an error. This can't take
+        # effect instantly because the RTL's prefetch stage (which we don't
+        # model except for matching its timing) has a copy of the next
+        # instruction. Make this a counter, decremented once per cycle. When we
+        # get to zero, we set the flag.
+        self._time_to_imem_invalidation = None  # type: Optional[int]
         self.invalidated_imem = False
 
     def get_next_pc(self) -> int:
@@ -265,6 +270,12 @@ class OTBNState:
         # We shouldn't be running commit() in IDLE or LOCKED mode
         assert self.running()
 
+        if self._time_to_imem_invalidation is not None:
+            self._time_to_imem_invalidation -= 1
+            if self._time_to_imem_invalidation == 0:
+                self.invalidated_imem = True
+                self._time_to_imem_invalidation = None
+
         # Check if we processed the RND data, if so set the register. This is
         # done seperately from the rnd_completed method because in other case
         # we are exiting stall caused by RND waiting by one cycle too early.
@@ -327,8 +338,11 @@ class OTBNState:
         # POST_EXEC to allow one more cycle.
         if self.pending_halt:
             self.ext_regs.commit()
-            self.fsm_state = (FsmState.LOCKING
-                              if self._err_bits >> 16 else FsmState.POST_EXEC)
+            if self._err_bits >> 16:
+                self.ext_regs.write('INSN_CNT', 0, True)
+                self.fsm_state = FsmState.LOCKING
+            else:
+                self.fsm_state = FsmState.POST_EXEC
             return
 
         # As pending_halt wasn't set, there shouldn't be any pending error bits
@@ -498,3 +512,6 @@ class OTBNState:
         '''
         self._err_bits |= err_bits
         self.pending_halt = True
+
+    def invalidate_imem(self) -> None:
+        self._time_to_imem_invalidation = 2

hw/ip/otbn/dv/otbnsim/stepped.py
@@ -291,7 +291,7 @@ def on_edn_rnd_cdc_done(sim: OTBNSim, args: List[str]) -> Optional[OTBNSim]:
 def on_invalidate_imem(sim: OTBNSim, args: List[str]) -> Optional[OTBNSim]:
     check_arg_count('invalidate_imem', 0, args)
 
-    sim.state.invalidated_imem = True
+    sim.state.invalidate_imem()
 
     return None
 