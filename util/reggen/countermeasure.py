0f4f6a8d1d0b4ece635a3c746099199efc7f1d61
Michael Schaffner <msf@opentitan.org>
2021-12-02 16:47:20 -0800

    [reggen] Add tentative list of assets and cm types
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- util/reggen/countermeasure.py
+++ util/reggen/countermeasure.py
@@ -2,25 +2,54 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
+import re
 from typing import Dict, List
 
 from .lib import check_keys, check_str, check_list
 
-# TO BE DEFINED
 CM_ASSETS = [
-    'FOO'
+    'KEY',
+    'DATA_REG',
+    'CONFIG',
+    'LFSR',
+    'CNT',
+    'FSM',
+    'MEM',
+    'CLK',
+    'RST',
+    'BUS',
+    'CTRL_FLOW',
+    'MUX',
+    'CTRL',
+    'CONSTANTS',
+    'LOGIC'
 ]
 
-# TO BE DEFINED
 CM_TYPES = [
-    'BAR'
+    'MUBI',
+    'SPARSE',
+    'DIFF',
+    'REDUN',
+    'SHADOW',
+    'SCRAMBLE',
+    'INTEGRITY',
+    'CONSISTENCY',
+    'DIGEST',
+    'LC_GATED',
+    'LOCAL_ESC',
+    'BKGN_CHK',
+    'GLITCH_DETECT',
+    'SW_UNREADABLE',
+    'SCA',
+    'MASKING'
 ]
 
 
 class CounterMeasure:
     """Object holding details for one countermeasure within an IP block."""
 
-    def __init__(self, asset: str, cm_type: str, desc: str):
+    def __init__(self, instance: str, asset: str, cm_type: str, desc: str):
+        self.instance = instance
         self.asset = asset
         self.cm_type = cm_type
         self.desc = desc
@@ -39,19 +68,27 @@ class CounterMeasure:
         desc = check_str(rd['desc'], f'desc field of {what}')
 
         try:
-            asset, cm_type = name.split('.')
+            # the format is [CM_INST_NAME].<CM_ASSET>.<CM_TYPE>
+            # i.e., the countermeasure instance name is optional.
+            fields = name.split('.')
+            if len(fields) == 3:
+                instance, asset, cm_type = fields
+            else:
+                asset, cm_type = fields
+                instance = ""
         except ValueError:
             raise ValueError(
                 f'Invalid format: {name} ({what}).')
-
+        if not re.fullmatch(r'^([a-zA-Z0-9_]*)', instance):
+            raise ValueError(
+                f'Invalid instance name: {instance} ({what}).')
         if asset not in CM_ASSETS:
             raise ValueError(
                 f'Invalid asset: {asset} ({what}).')
-
         if cm_type not in CM_TYPES:
             raise ValueError(
                 f'Invalid type: {cm_type} ({what}).')
-        return CounterMeasure(asset, cm_type, desc)
+        return CounterMeasure(instance, asset, cm_type, desc)
 
     @staticmethod
     def from_raw_list(what: str,
@@ -77,4 +114,5 @@ class CounterMeasure:
         }
 
     def __str__(self) -> str:
-        return self.asset + '.' + self.cm_type
+        namestr = self.asset + '.' + self.cm_type
+        return self.instance + '.' + namestr if self.instance else namestr
