ae005eb0c2f7ba77ad499f936fcc6e84fb809560
Nikola Miladinovic <nikola.miladinovic@ensilica.com>
2021-12-09 15:34:02 +0000

    [flash_ctrl] Adding second eflash RAL,
    adding test V1 Host Direct Read, adding file (xfile) for override
    
    Xcelium XPROP settings (which are currently not working)
    
    Signed-off-by: Nikola Miladinovic <nikola.miladinovic@ensilica.com>

--- hw/ip/flash_ctrl/dv/env/flash_ctrl_env_pkg.sv
+++ hw/ip/flash_ctrl/dv/env/flash_ctrl_env_pkg.sv
@@ -14,20 +14,20 @@ package flash_ctrl_env_pkg;
   import csr_utils_pkg::*;
   import flash_ctrl_pkg::*;
   import flash_ctrl_core_ral_pkg::*;
+  import flash_ctrl_eflash_ral_pkg::*;
   import mem_bkdr_util_pkg::*;
+  import prim_mubi_pkg::*;
 
   // macro includes
   `include "uvm_macros.svh"
   `include "dv_macros.svh"
 
   // parameters
-  parameter string LIST_OF_ALERTS[] = {
-    "recov_err", "fatal_err"
-  };
+  parameter string LIST_OF_ALERTS[] = {"recov_err", "fatal_err"};
 
   parameter uint NUM_ALERTS = 4;
-  parameter uint FlashNumPages            = flash_ctrl_pkg::NumBanks * flash_ctrl_pkg::PagesPerBank;
-  parameter uint FlashSizeBytes           = FlashNumPages * flash_ctrl_pkg::WordsPerPage *
+  parameter uint FlashNumPages = flash_ctrl_pkg::NumBanks * flash_ctrl_pkg::PagesPerBank;
+  parameter uint FlashSizeBytes         = FlashNumPages * flash_ctrl_pkg::WordsPerPage *
                                             flash_ctrl_pkg::DataWidth / 8;
 
   // Number of bytes in each of the flash pages.
@@ -36,49 +36,49 @@ package flash_ctrl_env_pkg;
   // Num of bytes in each of the flash banks for each of the flash partitions.
   parameter uint BytesPerBank = FlashSizeBytes / flash_ctrl_pkg::NumBanks;
 
-  parameter uint InfoTypeBytes [flash_ctrl_pkg::InfoTypes] = '{
-    flash_ctrl_pkg::InfoTypeSize[0] * BytesPerPage,
-    flash_ctrl_pkg::InfoTypeSize[1] * BytesPerPage,
-    flash_ctrl_pkg::InfoTypeSize[2] * BytesPerPage
+  parameter uint InfoTypeBytes[flash_ctrl_pkg::InfoTypes] = '{
+      flash_ctrl_pkg::InfoTypeSize[0] * BytesPerPage,
+      flash_ctrl_pkg::InfoTypeSize[1] * BytesPerPage,
+      flash_ctrl_pkg::InfoTypeSize[2] * BytesPerPage
   };
 
-  parameter uint FlashNumBusWords         = FlashSizeBytes / top_pkg::TL_DBW;
-  parameter uint FlashNumBusWordsPerBank  = FlashNumBusWords / flash_ctrl_pkg::NumBanks;
-  parameter uint FlashNumBusWordsPerPage  = FlashNumBusWordsPerBank / flash_ctrl_pkg::PagesPerBank;
+  parameter uint FlashNumBusWords = FlashSizeBytes / top_pkg::TL_DBW;
+  parameter uint FlashNumBusWordsPerBank = FlashNumBusWords / flash_ctrl_pkg::NumBanks;
+  parameter uint FlashNumBusWordsPerPage = FlashNumBusWordsPerBank / flash_ctrl_pkg::PagesPerBank;
 
-  parameter uint InfoTypeBusWords [flash_ctrl_pkg::InfoTypes] = '{
-    flash_ctrl_pkg::InfoTypeSize[0] * FlashNumBusWordsPerPage,
-    flash_ctrl_pkg::InfoTypeSize[1] * FlashNumBusWordsPerPage,
-    flash_ctrl_pkg::InfoTypeSize[2] * FlashNumBusWordsPerPage
+  parameter uint InfoTypeBusWords[flash_ctrl_pkg::InfoTypes] = '{
+      flash_ctrl_pkg::InfoTypeSize[0] * FlashNumBusWordsPerPage,
+      flash_ctrl_pkg::InfoTypeSize[1] * FlashNumBusWordsPerPage,
+      flash_ctrl_pkg::InfoTypeSize[2] * FlashNumBusWordsPerPage
   };
 
-  parameter uint FlashBankBytesPerWord    = flash_ctrl_pkg::DataWidth / 8;
+  parameter uint FlashBankBytesPerWord = flash_ctrl_pkg::DataWidth / 8;
 
-  parameter uint FlashDataByteWidth       = $clog2(FlashBankBytesPerWord);
-  parameter uint FlashWordLineWidth       = $clog2(flash_ctrl_pkg::WordsPerPage);
-  parameter uint FlashPageWidth           = $clog2(flash_ctrl_pkg::PagesPerBank);
-  parameter uint FlashBankWidth           = $clog2(flash_ctrl_pkg::NumBanks);
-  parameter uint FlashPgmRes              = flash_ctrl_pkg::BusPgmRes;
-  parameter uint FlashPgmResWidth         = $clog2(FlashPgmRes);
+  parameter uint FlashDataByteWidth = $clog2(FlashBankBytesPerWord);
+  parameter uint FlashWordLineWidth = $clog2(flash_ctrl_pkg::WordsPerPage);
+  parameter uint FlashPageWidth = $clog2(flash_ctrl_pkg::PagesPerBank);
+  parameter uint FlashBankWidth = $clog2(flash_ctrl_pkg::NumBanks);
+  parameter uint FlashPgmRes = flash_ctrl_pkg::BusPgmRes;
+  parameter uint FlashPgmResWidth = $clog2(FlashPgmRes);
 
-  parameter uint FlashMemAddrWordMsbBit   = FlashDataByteWidth - 1;
-  parameter uint FlashMemAddrLineMsbBit   = FlashDataByteWidth + FlashWordLineWidth - 1;
+  parameter uint FlashMemAddrWordMsbBit = FlashDataByteWidth - 1;
+  parameter uint FlashMemAddrLineMsbBit = FlashDataByteWidth + FlashWordLineWidth - 1;
   parameter uint FlashMemAddrPageMsbBit   = FlashDataByteWidth + FlashWordLineWidth +
                                             FlashPageWidth - 1;
   parameter uint FlashMemAddrBankMsbBit   = FlashDataByteWidth + FlashWordLineWidth +
                                             FlashPageWidth + FlashBankWidth - 1;
   // Need to create a design parameter for this
-  parameter uint FlashFullDataWidth       = flash_ctrl_pkg::DataWidth + 4;
+  parameter uint FlashFullDataWidth = flash_ctrl_pkg::DataWidth + 4;
 
   // types
   typedef enum int {
-    FlashCtrlIntrProgEmpty  = 0,
-    FlashCtrlIntrProgLvl    = 1,
-    FlashCtrlIntrRdFull     = 2,
-    FlashCtrlIntrRdLvl      = 3,
-    FlashCtrlIntrOpDone     = 4,
-    FlashCtrlIntrErr        = 5,
-    NumFlashCtrlIntr        = 6
+    FlashCtrlIntrProgEmpty = 0,
+    FlashCtrlIntrProgLvl   = 1,
+    FlashCtrlIntrRdFull    = 2,
+    FlashCtrlIntrRdLvl     = 3,
+    FlashCtrlIntrOpDone    = 4,
+    FlashCtrlIntrErr       = 5,
+    NumFlashCtrlIntr       = 6
   } flash_ctrl_intr_e;
 
   typedef enum {
@@ -90,35 +90,35 @@ package flash_ctrl_env_pkg;
   } flash_mem_init_e;
 
   // Partition select for DV
-  typedef enum logic [flash_ctrl_pkg::InfoTypes:0] { // Data partition and all info partitions
-    FlashPartData         = 0,
-    FlashPartInfo         = 1,
-    FlashPartInfo1        = 2,
-    FlashPartRedundancy   = 4
+  typedef enum logic [flash_ctrl_pkg::InfoTypes:0] {  // Data partition and all info partitions
+    FlashPartData       = 0,
+    FlashPartInfo       = 1,
+    FlashPartInfo1      = 2,
+    FlashPartRedundancy = 4
   } flash_dv_part_e;
 
   typedef struct packed {
-    bit           en;         // enable this region
-    bit           read_en;    // enable reads
-    bit           program_en; // enable write
-    bit           erase_en;   // enable erase
-    uint          num_pages;  // 0:NumPages % start_page
-    uint          start_page; // 0:NumPages-1
+    bit  en;          // enable this region
+    bit  read_en;     // enable reads
+    bit  program_en;  // enable write
+    bit  erase_en;    // enable erase
+    uint num_pages;   // 0:NumPages % start_page
+    uint start_page;  // 0:NumPages-1
   } flash_mp_region_cfg_t;
 
   typedef struct packed {
-    bit           en;         // enable this page
-    bit           read_en;    // enable reads
-    bit           program_en; // enable write
-    bit           erase_en;   // enable erase
+    bit en;          // enable this page
+    bit read_en;     // enable reads
+    bit program_en;  // enable write
+    bit erase_en;    // enable erase
   } flash_bank_mp_info_page_cfg_t;
 
   typedef struct packed {
-    flash_dv_part_e partition;  // data or one of the info partitions
-    flash_erase_e   erase_type; // erase page or the whole bank
-    flash_op_e      op;         // read / program or erase
-    uint            num_words;  // number of words to read or program (TL_DW)
-    bit [TL_AW-1:0] addr;       // starting addr for the op
+    flash_dv_part_e partition;   // data or one of the info partitions
+    flash_erase_e   erase_type;  // erase page or the whole bank
+    flash_op_e      op;          // read / program or erase
+    uint            num_words;   // number of words to read or program (TL_DW)
+    bit [TL_AW-1:0] addr;        // starting addr for the op
   } flash_op_t;
 
   // Data queue for flash transactions
--- hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_base_vseq.sv
+++ hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_base_vseq.sv
@@ -3,18 +3,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 class flash_ctrl_base_vseq extends cip_base_vseq #(
-    .RAL_T               (flash_ctrl_core_reg_block),
-    .CFG_T               (flash_ctrl_env_cfg),
-    .COV_T               (flash_ctrl_env_cov),
-    .VIRTUAL_SEQUENCER_T (flash_ctrl_virtual_sequencer)
-  );
+  .RAL_T              (flash_ctrl_core_reg_block),
+  .CFG_T              (flash_ctrl_env_cfg),
+  .COV_T              (flash_ctrl_env_cov),
+  .VIRTUAL_SEQUENCER_T(flash_ctrl_virtual_sequencer)
+);
   `uvm_object_utils(flash_ctrl_base_vseq)
 
   // flash ctrl configuration settings.
 
-  constraint num_trans_c {
-    num_trans inside {[1:cfg.seq_cfg.max_num_trans]};
-  }
+  constraint num_trans_c {num_trans inside {[1 : cfg.seq_cfg.max_num_trans]};}
 
   `uvm_object_new
 
@@ -70,12 +68,13 @@ class flash_ctrl_base_vseq extends cip_base_vseq #(
     end
     reset_flash();
 
-  endtask // apply_reset
+  endtask  // apply_reset
 
   // Configure the memory protection regions.
   virtual task flash_ctrl_mp_region_cfg(uint index, flash_mp_region_cfg_t region_cfg);
     uvm_reg_data_t data;
     uvm_reg csr;
+    //verilog_format: off
     data =
         get_csr_val_with_updated_field(ral.mp_region_cfg_shadowed[index].en, data,
                                        region_cfg.en) |
@@ -89,6 +88,7 @@ class flash_ctrl_base_vseq extends cip_base_vseq #(
                                        region_cfg.start_page) |
         get_csr_val_with_updated_field(ral.mp_region_cfg_shadowed[index].size, data,
                                        region_cfg.num_pages);
+    //verilog_format: on
     csr_wr(.ptr(ral.mp_region_cfg_shadowed[index]), .value(data));
   endtask
 
@@ -98,8 +98,8 @@ class flash_ctrl_base_vseq extends cip_base_vseq #(
     uvm_reg_data_t data;
 
     data = get_csr_val_with_updated_field(ral.default_region_shadowed.rd_en, data, read_en) |
-           get_csr_val_with_updated_field(ral.default_region_shadowed.prog_en, data, program_en) |
-           get_csr_val_with_updated_field(ral.default_region_shadowed.erase_en, data, erase_en);
+        get_csr_val_with_updated_field(ral.default_region_shadowed.prog_en, data, program_en) |
+        get_csr_val_with_updated_field(ral.default_region_shadowed.erase_en, data, erase_en);
     csr_wr(.ptr(ral.default_region_shadowed), .value(data));
   endtask
 
@@ -116,13 +116,11 @@ class flash_ctrl_base_vseq extends cip_base_vseq #(
       csr_name = $sformatf({csr_name, "_%0d"}, page);
     end
     csr = ral.get_reg_by_name(csr_name);
-    data =
-        get_csr_val_with_updated_field(csr.get_field_by_name("en"), data, page_cfg.en) |
+    data = get_csr_val_with_updated_field(csr.get_field_by_name("en"), data, page_cfg.en) |
         get_csr_val_with_updated_field(csr.get_field_by_name("rd_en"), data, page_cfg.read_en) |
         get_csr_val_with_updated_field(csr.get_field_by_name("prog_en"), data,
                                        page_cfg.program_en) |
-        get_csr_val_with_updated_field(csr.get_field_by_name("erase_en"), data,
-                                       page_cfg.erase_en);
+        get_csr_val_with_updated_field(csr.get_field_by_name("erase_en"), data, page_cfg.erase_en);
     csr_wr(.ptr(csr), .value(data));
   endtask
 
@@ -137,7 +135,7 @@ class flash_ctrl_base_vseq extends cip_base_vseq #(
     uvm_reg_data_t data;
 
     data = get_csr_val_with_updated_field(ral.fifo_lvl.prog, data, program_fifo_intr_level) |
-           get_csr_val_with_updated_field(ral.fifo_lvl.rd, data, read_fifo_intr_level);
+        get_csr_val_with_updated_field(ral.fifo_lvl.rd, data, read_fifo_intr_level);
     csr_wr(.ptr(ral.fifo_lvl), .value(data));
   endtask
 
@@ -147,18 +145,15 @@ class flash_ctrl_base_vseq extends cip_base_vseq #(
   endtask
 
   // Wait for flash_ctrl op to finish.
-  virtual task wait_flash_op_done(bit clear_op_status = 1'b1,
-                                 time timeout_ns = 10_000_000); // Added because mass(bank) erase is longer then default timeout.
+  virtual task wait_flash_op_done(
+      bit clear_op_status = 1'b1, time timeout_ns = 10_000_000
+  );  // Added because mass(bank) erase is longer then default timeout.
     uvm_reg_data_t data;
     bit done;
-    `DV_SPINWAIT(
-      do begin
+    `DV_SPINWAIT(do begin
         csr_rd(.ptr(ral.op_status), .value(data));
         done = get_field_val(ral.op_status.done, data);
-      end while (done == 1'b0);,
-      "wait_flash_op_done timeout occurred!",
-      timeout_ns
-    )
+      end while (done == 1'b0);, "wait_flash_op_done timeout occurred!", timeout_ns)
     if (clear_op_status) begin
       data = get_csr_val_with_updated_field(ral.op_status.done, data, 0);
       csr_wr(.ptr(ral.op_status), .value(data));
@@ -169,13 +164,10 @@ class flash_ctrl_base_vseq extends cip_base_vseq #(
   virtual task wait_flash_op_err(bit clear_op_status = 1'b1);
     uvm_reg_data_t data;
     bit err;
-    `DV_SPINWAIT(
-      do begin
+    `DV_SPINWAIT(do begin
         csr_rd(.ptr(ral.op_status), .value(data));
         err = get_field_val(ral.op_status.err, data);
-      end while (err == 1'b0);,
-      "wait_flash_op_err timeout occurred!"
-    )
+      end while (err == 1'b0);, "wait_flash_op_err timeout occurred!")
     if (clear_op_status) begin
       data = get_csr_val_with_updated_field(ral.op_status.err, data, 0);
       csr_wr(.ptr(ral.op_status), .value(data));
@@ -186,24 +178,18 @@ class flash_ctrl_base_vseq extends cip_base_vseq #(
   virtual task wait_flash_ctrl_prog_fifo_not_full();
     // TODO: if intr enabled, then check interrupt, else check status.
     bit prog_full;
-    `DV_SPINWAIT(
-      do begin
+    `DV_SPINWAIT(do begin
         csr_rd(.ptr(ral.status.prog_full), .value(prog_full));
-      end while (prog_full);,
-      "wait_flash_ctrl_prog_fifo_not_full timeout occurred!"
-    )
+      end while (prog_full);, "wait_flash_ctrl_prog_fifo_not_full timeout occurred!")
   endtask
 
   // Wait for rd fifo to not be empty.
   virtual task wait_flash_ctrl_rd_fifo_not_empty();
     // TODO: if intr enabled, then check interrupt, else check status.
     bit read_empty;
-    `DV_SPINWAIT(
-      do begin
+    `DV_SPINWAIT(do begin
         csr_rd(.ptr(ral.status.rd_empty), .value(read_empty));
-      end while (read_empty);,
-      "wait_flash_ctrl_rd_fifo_not_empty timeout occurred!"
-    )
+      end while (read_empty);, "wait_flash_ctrl_rd_fifo_not_empty timeout occurred!")
   endtask
 
   virtual task flash_ctrl_start_op(flash_op_t flash_op);
@@ -224,8 +210,7 @@ class flash_ctrl_base_vseq extends cip_base_vseq #(
     partition_sel = |flash_op.partition;
     info_sel = flash_op.partition >> 1;
 
-    data =
-        get_csr_val_with_updated_field(ral.control.start, data, 1'b1) |
+    data = get_csr_val_with_updated_field(ral.control.start, data, 1'b1) |
         get_csr_val_with_updated_field(ral.control.op, data, flash_op.op) |
         get_csr_val_with_updated_field(ral.control.erase_sel, data, flash_op.erase_type) |
         get_csr_val_with_updated_field(ral.control.partition_sel, data, partition_sel) |
@@ -262,5 +247,16 @@ class flash_ctrl_base_vseq extends cip_base_vseq #(
     end
   endtask
 
+  // Task to perform a direct Flash read at the specified location
+  virtual task do_direct_read(
+      input bit [TL_AW-1:0] addr, input bit [TL_DBW-1:0] mask = get_rand_contiguous_mask(),
+      input bit blocking = $urandom_range(0, 1), input bit check_rdata = 0,
+      input bit [TL_DW-1:0] exp_rdata = '0, input mubi4_t instr_type = MuBi4False,
+      output logic [TL_DW-1:0] rdata);
+    tl_access(.addr(addr), .write(1'b0), .data(rdata), .mask(mask), .blocking(blocking),
+              .check_exp_data(check_rdata), .exp_data(exp_rdata), .compare_mask(mask),
+              .instr_type(instr_type),
+              .tl_sequencer_h(p_sequencer.tl_sequencer_hs[cfg.flash_ral_name]));
+  endtask
 
 endclass : flash_ctrl_base_vseq
