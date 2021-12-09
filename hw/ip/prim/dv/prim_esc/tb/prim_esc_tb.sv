de998482cbc9da106d40a54af8ea60f2b27ec237
Michael Schaffner <msf@opentitan.org>
2021-12-09 15:17:33 -0800

    [prim_esc_receiver] Switch to standardized prim_count
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/prim/dv/prim_esc/tb/prim_esc_tb.sv
+++ hw/ip/prim/dv/prim_esc/tb/prim_esc_tb.sv
@@ -207,11 +207,11 @@ module prim_esc_tb;
     `DV_SPINWAIT(wait (ping_ok == 1);, , , "Wait for ping_ok timeout");
     main_clk.wait_clks(2);
     ping_req = 0;
-    // If cnt_q[0] and cnt_q[1]'s value do not match, deisgn will set `esc_req_out` signal.
-    force prim_esc_tb.i_esc_receiver.cnt_q[1] = 0;
+    // If duplicate counter values are inconsistent, the design will set the `esc_req_out` signal.
+    force prim_esc_tb.i_esc_receiver.u_prim_count.up_cnt_q[1] = '0;
     `DV_SPINWAIT(wait (esc_req_out == 1);, , , "Wait for esc_req_out timeout");
     if (integ_fail) test_error("Escalation receiver counter unexpected signal integrity error!");
-    release prim_esc_tb.i_esc_receiver.cnt_q[1];
+    release prim_esc_tb.i_esc_receiver.u_prim_count.up_cnt_q[1];
 
     $display("[prim_esc_seq] Escalation couter error sequence finished!");
 
