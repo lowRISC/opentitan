8b2f5f8a112c60200940c6fcbfc313779a41b28c
Jade Philipoom <jadep@google.com>
2021-12-10 11:56:39 +0000

    [otbn,util] Add control-flow machinery to OTBN utils.
    Allows one to analyze, traverse, and print the control-flow graphs for a
    program or subroutine.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

--- hw/ip/otbn/util/shared/section.py
+++ hw/ip/otbn/util/shared/section.py
@@ -3,7 +3,10 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
+from typing import List
+
 from shared.decode import OTBNProgram
+from shared.insn_yaml import Insn
 
 
 class CodeSection:
@@ -15,13 +18,13 @@ class CodeSection:
         self.start = start
         self.end = end
 
-    def get_insn_sequence(self, program: OTBNProgram):
+    def get_insn_sequence(self, program: OTBNProgram) -> List[Insn]:
         return [
             program.get_insn(pc) for pc in range(self.start, self.end + 4, 4)
         ]
 
-    def __contains__(self, pc: int):
-        return self.start <= pc and pc <= self.end
-
-    def __repr__(self):
+    def pretty(self) -> str:
         return '0x{:x}-0x{:x}'.format(self.start, self.end)
+
+    def __contains__(self, pc: int) -> bool:
+        return self.start <= pc and pc <= self.end
