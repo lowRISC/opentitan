e6b36fe0ab10c67433860a25e278275fdb2b92f1
Rupert Swarbrick <rswarbrick@lowrisc.org>
2022-01-04 12:35:17 +0000

    [otbn,dv] Avoid spurious errors from multi-error testcases
    As an example, consider the bnsid-2 testcase. This was causing
    failures in the otbn_multi_err test. The testcase boils down to this
    instruction:
    
       bn.sid x1, 0(x2)
    
    where the call stack is empty (so the read of x1 underflows) and x2
    equals 1 (so the store address is invalid).
    
    The problem is that if the call stack really hasn't ever had any
    values (true if bnsid-2 comes first) then it will respond with X's.
    That's fine, except that this is used as an index into the big-number
    register file, which means that the rdata and associated error bits
    come out as X. What's more, we didn't actually have any intialised
    data in the big-number register file anyway, so we'd have got Xs
    whatever the index was.
    
    This is a problem because (via the fatal_err signal) it causes the
    controller state to go to X and eventually causes us to send an X in
    the imem request signal, which causes an assertion failure.
    
    Avoid the problem by storing zero to the call stack before
    popping it again (which avoids passing X as an index to the big-number
    register file) and then clearing w0.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/otbn/dv/otbnsim/test/simple/multi/bnmovr-1.s
+++ hw/ip/otbn/dv/otbnsim/test/simple/multi/bnmovr-1.s
@@ -3,6 +3,26 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
   Underflow call stack for grs and bad WDR index for grd
+
+  One thing that might go wrong is if that the big-number register file
+  responds with X's to the BN.MOVR instruction that's supposed to trigger the
+  error. This is a little awkward to avoid: firstly, we need a defined
+  big-number register despite underflowing the call stack. Secondly, we need to
+  ensure that this register has known data.
 */
+  /*
+    Ensure that a read from the empty call stack gives zero (as well as a stack
+    underflow error) by clearing all the entries.
+  */
+  loopi 8, 1
+    addi x1, x0, 0
+  loopi 8, 1
+    addi x0, x1, 0
+
+  /*
+    Zero out w0, which will be selected by x1
+  */
+  bn.xor w0, w0, w0
+
   addi    x2, x0, 100
   bn.movr x2, x1
--- hw/ip/otbn/dv/otbnsim/test/simple/multi/bnsid-2.s
+++ hw/ip/otbn/dv/otbnsim/test/simple/multi/bnsid-2.s
@@ -3,6 +3,27 @@
 /* SPDX-License-Identifier: Apache-2.0 */
 /*
   Underflow call stack for grs2 and bad address
+
+  One thing that might go wrong is if that the big-number register file
+  responds with X's to the BN.SID instruction that's supposed to trigger the
+  error. This is a little awkward to avoid: firstly, we need a defined
+  big-number register despite underflowing the call stack. Secondly, we need to
+  ensure that this register has known data.
 */
+
+  /*
+    Ensure that a read from the empty call stack gives zero (as well as a stack
+    underflow error) by clearing all the entries.
+  */
+  loopi 8, 1
+    addi x1, x0, 0
+  loopi 8, 1
+    addi x0, x1, 0
+
+  /*
+    Zero out w0, which will be selected by x1
+  */
+  bn.xor w0, w0, w0
+
   addi   x2, x0, 1
   bn.sid x1, 0(x2)
