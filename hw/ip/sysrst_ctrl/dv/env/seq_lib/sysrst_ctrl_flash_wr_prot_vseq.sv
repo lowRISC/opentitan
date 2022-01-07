bf8ef8681806487b86cafaa51620a544e90f5718
Madhuri Patel <madhuri.patel@ensilica.com>
2022-01-07 12:13:58 +0000

    [sysrst_ctrl,dv] Add flash write prot output test
    Signed-off-by: Madhuri Patel <madhuri.patel@ensilica.com>

---
+++ hw/ip/sysrst_ctrl/dv/env/seq_lib/sysrst_ctrl_flash_wr_prot_vseq.sv
@@ -0,0 +1,67 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This sequence will verify the flash_wp_l_out signal
+// It will be explicitly released from the reset by enabling and
+// disabling the override feature. The output, thus will have a transition from
+// 1->0 and 0->1
+class sysrst_ctrl_flash_wr_prot_vseq extends sysrst_ctrl_base_vseq;
+  `uvm_object_utils(sysrst_ctrl_flash_wr_prot_vseq)
+
+  `uvm_object_new
+
+   rand uvm_reg_data_t en_override_value, set_value, set_allowed;
+
+   constraint num_trans_c {num_trans == 20;}
+
+   task body();
+
+   bit en_override_flash_wp_value, override_val_flash_wp, allowed_flash_wp_0,
+       allowed_flash_wp_1;
+
+    `uvm_info(`gfn, "Starting the body from flash_wr_prot_vseq", UVM_LOW)
+
+    repeat (num_trans) begin
+
+      `DV_CHECK_RANDOMIZE_FATAL(this)
+
+      // Enable the override function
+      csr_wr(ral.pin_out_ctl, en_override_value);
+
+      // Set the pin_override_value
+      csr_wr(ral.pin_out_value, set_value);
+
+      // Allow the output values to override
+      csr_wr(ral.pin_allowed_ctl, set_allowed);
+
+      // It takes 2-3 clock cycles to sync register values
+      cfg.clk_aon_rst_vif.wait_clks(3);
+
+      en_override_flash_wp_value = get_field_val(ral.pin_out_ctl.flash_wp_l, en_override_value);
+
+      override_val_flash_wp = get_field_val(ral.pin_out_value.flash_wp_l, set_value);
+
+      allowed_flash_wp_0 = get_field_val(ral.pin_allowed_ctl.flash_wp_l_0, set_allowed);
+      allowed_flash_wp_1 = get_field_val(ral.pin_allowed_ctl.flash_wp_l_1, set_allowed);
+
+      cfg.clk_aon_rst_vif.wait_clks(1);
+      if (en_override_flash_wp_value == 1) begin
+        if (override_val_flash_wp == 1 && allowed_flash_wp_1 == 1) begin
+          `DV_CHECK_EQ(cfg.vif.flash_wp_l, 1)
+        end else if (override_val_flash_wp == 0 && allowed_flash_wp_0 == 1) begin
+          `DV_CHECK_EQ(cfg.vif.flash_wp_l, 0)
+        end else begin
+          `DV_CHECK_EQ(cfg.vif.flash_wp_l, 0)
+        end
+      end else begin
+        `DV_CHECK_EQ(cfg.vif.flash_wp_l, 0)
+      end
+
+    end
+
+     cfg.clk_aon_rst_vif.wait_clks(20);
+   endtask : body
+
+endclass : sysrst_ctrl_flash_wr_prot_vseq
+
--- hw/ip/sysrst_ctrl/dv/env/sysrst_ctrl_if.sv
+++ hw/ip/sysrst_ctrl/dv/env/sysrst_ctrl_if.sv
@@ -23,10 +23,7 @@ interface sysrst_ctrl_if (
   logic key2_out;
   logic pwrb_out;
   logic z3_wakeup;
-  logic z3_wakeup_in;
-  logic bat_disable_in;
-  logic ac_present_out;
-  logic lid_open_out;
+
 
   // reset value of input signals
   function automatic void reset_signals();
@@ -38,22 +35,20 @@ interface sysrst_ctrl_if (
     lid_open <= 0;
     ec_rst_l_in <= 1;
     flash_wp_l_in <= 1;
-    z3_wakeup_in <= 0;
-    bat_disable_in <= 0;
   endfunction
 
   task automatic randomize_input();
     // VCS doesn't support randomizing logic variable
     // so declare bit variable, randomize it and assigned it to logic
-    bit pwrb, key0, key1, key2, ec_rst_l, flash_wp_l;
-    `DV_CHECK_FATAL(std::randomize(pwrb, key0, key1, key2, ec_rst_l, flash_wp_l), ,
+    bit pwrb, key0, key1, key2, ec_rst_l, flash_wp;
+    `DV_CHECK_FATAL(std::randomize(pwrb, key0, key1, key2, ec_rst_l, flash_wp), ,
        "sysrst_ctrl_if")
     pwrb_in = pwrb;
     key0_in = key0;
     key1_in = key1;
     key2_in = key2;
     ec_rst_l_in = ec_rst_l;
-    flash_wp_l_in = flash_wp_l;
+    flash_wp_l_in = flash_wp;
   endtask
 
 endinterface : sysrst_ctrl_if
