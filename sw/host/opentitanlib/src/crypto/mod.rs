968524e4b9dff05b8b23191890c21ea9a02dd032
Alphan Ulusoy <alphan@google.com>
2022-01-10 16:20:20 -0500

    [opentitantool] Add sha256
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

--- sw/host/opentitanlib/src/crypto/mod.rs
+++ sw/host/opentitanlib/src/crypto/mod.rs
@@ -2,11 +2,4 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-pub mod app;
-pub mod bootstrap;
-pub mod crypto;
-pub mod io;
-pub mod otp;
-pub mod spiflash;
-pub mod transport;
-pub mod util;
+pub mod sha256;
