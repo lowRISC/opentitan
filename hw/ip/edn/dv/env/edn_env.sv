63c36ad34860e43be8b67bda5c8eed1789bd45c3
Steve Nelson <steve.nelson@wdc.com>
2022-01-11 06:00:58 -0800

    [edn/dv] Support multiple endpoints
    Signed-off-by: Steve Nelson <steve.nelson@wdc.com>

--- hw/ip/edn/dv/env/edn_env.sv
+++ hw/ip/edn/dv/env/edn_env.sv
@@ -12,7 +12,7 @@ class edn_env extends cip_base_env #(
 
   csrng_agent   m_csrng_agent;
   push_pull_agent#(.HostDataWidth(edn_pkg::FIPS_ENDPOINT_BUS_WIDTH))
-       m_endpoint_agent [edn_env_pkg::NUM_ENDPOINTS];
+       m_endpoint_agent [MAX_NUM_ENDPOINTS];
 
   `uvm_component_new
 
@@ -24,13 +24,12 @@ class edn_env extends cip_base_env #(
     uvm_config_db#(csrng_agent_cfg)::set(this, "m_csrng_agent*", "cfg", cfg.m_csrng_agent_cfg);
     cfg.m_csrng_agent_cfg.if_mode = dv_utils_pkg::Device;
 
-    for (int i = 0; i < edn_env_pkg::NUM_ENDPOINTS; i++) begin
+    for (int i = 0; i < MAX_NUM_ENDPOINTS; i++) begin
       string endpoint_agent_name = $sformatf("m_endpoint_agent[%0d]", i);
       m_endpoint_agent[i] = push_pull_agent#(.HostDataWidth(edn_pkg::FIPS_ENDPOINT_BUS_WIDTH))::
                             type_id::create(endpoint_agent_name, this);
       uvm_config_db#(push_pull_agent_cfg#(.HostDataWidth(edn_pkg::FIPS_ENDPOINT_BUS_WIDTH)))::
-          set(this, $sformatf("%0s*", endpoint_agent_name),
-                    "cfg", cfg.m_endpoint_agent_cfg[i]);
+          set(this, $sformatf("%0s*", endpoint_agent_name), "cfg", cfg.m_endpoint_agent_cfg[i]);
       cfg.m_endpoint_agent_cfg[i].agent_type = push_pull_agent_pkg::PullAgent;
       cfg.m_endpoint_agent_cfg[i].if_mode    = dv_utils_pkg::Host;
       // TODO: Move these
@@ -44,13 +43,13 @@ class edn_env extends cip_base_env #(
     virtual_sequencer.csrng_sequencer_h = m_csrng_agent.sequencer;
 
     if (cfg.en_scb) begin
-      for (int i = 0; i < edn_env_pkg::NUM_ENDPOINTS; i++) begin
+      for (int i = 0; i < cfg.num_endpoints; i++) begin
         m_endpoint_agent[i].monitor.analysis_port.connect
         (scoreboard.endpoint_fifo[i].analysis_export);
       end
     end
 
-    for (int i = 0; i < edn_env_pkg::NUM_ENDPOINTS; i++) begin
+    for (int i = 0; i < cfg.num_endpoints; i++) begin
       if (cfg.m_endpoint_agent_cfg[i].is_active) begin
         virtual_sequencer.endpoint_sequencer_h[i] = m_endpoint_agent[i].sequencer;
       end
