45c2a7eb949f2d89aa134ab2cfeac3c50eb37e96
Madhuri Patel <madhuri.patel@ensilica.com>
2022-01-11 16:16:14 +0000

    [sysrst_ctrl,dv] Add sysrst_ctrl ultra low power test
    Signed-off-by: Madhuri Patel <madhuri.patel@ensilica.com>

--- hw/ip/sysrst_ctrl/dv/env/sysrst_ctrl_if.sv
+++ hw/ip/sysrst_ctrl/dv/env/sysrst_ctrl_if.sv
@@ -40,13 +40,15 @@ interface sysrst_ctrl_if (
   task automatic randomize_input();
     // VCS doesn't support randomizing logic variable
     // so declare bit variable, randomize it and assigned it to logic
-    bit pwrb, key0, key1, key2, ec_rst_l, flash_wp;
-    `DV_CHECK_FATAL(std::randomize(pwrb, key0, key1, key2, ec_rst_l, flash_wp), ,
+    bit pwrb, key0, key1, key2, ec_rst_l, ac_pst, ld_op, flash_wp;
+    `DV_CHECK_FATAL(std::randomize(pwrb, key0, key1, key2, ec_rst_l, ac_pst, ld_op, flash_wp), ,
        "sysrst_ctrl_if")
     pwrb_in = pwrb;
     key0_in = key0;
     key1_in = key1;
     key2_in = key2;
+    ac_present = ac_pst;
+    lid_open = ld_op;
     ec_rst_l_in = ec_rst_l;
     flash_wp_l_in = flash_wp;
   endtask
