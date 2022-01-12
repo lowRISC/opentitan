c375e1cd5893200da236ad41decab2c71fedce9b
Rupert Swarbrick <rswarbrick@lowrisc.org>
2022-01-12 14:34:46 +0000

    [otbn,dv] Slight tidy-up of state logic in sim.py
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/otbn/dv/otbnsim/sim/sim.py
+++ hw/ip/otbn/dv/otbnsim/sim/sim.py
@@ -132,7 +132,7 @@ class OTBNSim:
         if not self.state.running():
             return (None, [])
 
-        if self.state.fsm_state in [FsmState.PRE_EXEC]:
+        if self.state.fsm_state == FsmState.PRE_EXEC:
             # Zero INSN_CNT the cycle after we are told to start (and every
             # cycle after that until we start executing instructions, but that
             # doesn't really matter)
@@ -143,16 +143,13 @@ class OTBNSim:
             return (None, changes)
 
         # If we are not in PRE_EXEC, then we have a valid URND seed. So we
-        # should step URND nevertheless.
+        # should step URND regardless of whether we're actually executing
+        # instructions.
         self.state.wsrs.URND.commit()
         self.state.wsrs.URND.step()
 
-        if self.state.fsm_state in [FsmState.FETCH_WAIT]:
-            # Zero INSN_CNT the cycle after we are told to start (and every
-            # cycle after that until we start executing instructions, but that
-            # doesn't really matter)
+        if self.state.fsm_state == FsmState.FETCH_WAIT:
             changes = self._on_stall(verbose, fetch_next=False)
-            self.state.ext_regs.write('INSN_CNT', 0, True)
             return (None, changes)
 
         if self.state.fsm_state in [FsmState.POST_EXEC, FsmState.LOCKING]:
