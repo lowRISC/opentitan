831f1a0e1e4aad6bb4eecaabfa15960a843caa86
Michael Schaffner <msf@google.com>
2022-01-13 16:36:36 -0800

    [aes/syn] Update GTECH flow for AES wrapper
    This separate synthesis target for the AES wrapper is only intended to
    produce a mapping of the wrapper alone. The motivation for synthesizing
    AES and the wrapper separately is that we can ensure that no sort of
    optimization takes place across the hierarchy boundary between them (the
    wrapper is TB code whereas the AES is actual synthesizable code).
    
    In order to achieve this we just remove the aes module from the working
    lib after elaboration.
    
    Signed-off-by: Michael Schaffner <msf@google.com>

---
+++ hw/ip/aes/syn/aes_wrap_post_elab_gtech.tcl
@@ -0,0 +1,10 @@
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Post elab script, used in GTECH runs to modify the unmapped netlist before
+# writing it out.
+
+# Remove the AES block itself, since this flow is only intended to produce a
+# GTECH mapping of the wrapper.
+remove_design aes_1_1_4
