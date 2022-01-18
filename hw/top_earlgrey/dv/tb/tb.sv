b51ac17c3ba62806ed746758b0ec61dbb28f0de9
Michael Schaffner <msf@opentitan.org>
2022-01-18 08:44:13 -0800

    [chip/dv] Add missing analog port to TB
    Fix #10116
    
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/top_earlgrey/dv/tb/tb.sv
+++ hw/top_earlgrey/dv/tb/tb.sv
@@ -73,7 +73,7 @@ module tb;
 
   // TODO: Replace with correct interfaces once
   // pinmux/padring and pinout have been updated.
-  wire [24:0] tie_off;
+  wire [25:0] tie_off;
   wire [5:0] spi_host_tie_off;
   wire [1:0] spi_dev_tie_off;
   assign (weak0, weak1) tie_off = '0;
@@ -177,8 +177,10 @@ module tb;
     .FLASH_TEST_MODE0(tie_off[21]),
     .FLASH_TEST_MODE1(tie_off[22]),
     .FLASH_TEST_VOLT(tie_off[23]),
+    // OTP
+    .OTP_EXT_VOLT(tie_off[24]),
     // MISC pad
-    .AST_MISC(tie_off[24])
+    .AST_MISC(tie_off[25])
   );
 
   // connect signals
