ec556cfa91f586afe018b16cc64c8716f7c0768d
Pirmin Vogel <vogelpi@lowrisc.org>
2022-01-21 17:00:34 +0100

    [kmac/pre_sca] Enable formal masking verification of KMAC with Alma
    This commit adds a couple of support files similar to what we have for
    AES to enably formal verification of the masking employed in KMAC
    using the Alma tool. For now, we only support verifying the
    keccak_2share module containing the critical S-Boxes.
    
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

---
+++ hw/ip/kmac/pre_sca/alma/cpp/verilator_tb_keccak_2share.cpp
@@ -0,0 +1,38 @@
+// Copyright lowRISC contributors.
+// Copyright IAIK.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdio.h>
+
+#include "Vcircuit.h"
+#include "testbench.h"
+
+int main(int argc, char **argv) {
+  Verilated::commandArgs(argc, argv);
+  Testbench<Vcircuit> tb;
+  tb.opentrace("tmp.vcd");
+
+  tb.reset();
+
+  // Data signals - we don't really care about the data fed to the module.
+  // The whole tracing is really just about control signals.
+  tb.m_core.rand_i = 0x123456;
+  tb.m_core.s_i = 0x789ABCDE;
+
+  // Control signals
+  tb.m_core.rnd_i = 0;  // Round, just defines which round constant is added
+                        // at the very end.
+  tb.m_core.rand_valid_i = 1;  // Randomness always valid. TODO experiment with
+                               // stalls to test muxing.
+
+  // Phase 1 - Theta, Rho, Pi - Takes 1 cycle.
+  tb.m_core.sel_i = 0;
+  tb.tick();
+  // Phase 2 - Chi, Iota - Takes 2 cycles.
+  tb.m_core.sel_i = 1;
+  tb.tick();
+  tb.tick();
+
+  tb.closetrace();
+}
---
+++ hw/ip/kmac/pre_sca/alma/verify_kmac.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Script to formally verify the masking of KMAC using Alma.
+
+set -e
+
+# Argument parsing
+if [[ "$#" -gt 0 ]]; then
+  TOP_MODULE=$1
+else
+  TOP_MODULE=keccak_2share
+fi
+
+# Select testbench.
+if [[ ${TOP_MODULE} == "keccak_2share" ]]; then
+  TESTBENCH=${TOP_MODULE}
+else
+  echo "TOP_MODULE ${TOP_MODULE} not supported, aborting now"
+  exit 1
+fi
+
+echo "Verifying ${TOP_MODULE} using Alma"
+
+## Parse
+./parse.py --top-module ${TOP_MODULE} \
+--source ${REPO_TOP}/hw/ip/kmac/pre_syn/syn_out/latest/generated/${TOP_MODULE}.alma.v \
+--netlist tmp/circuit.v --log-yosys
+
+# Label
+# Extract WIDTH from label file.
+WIDTH_MIN_1=`cat tmp/labels.txt | grep rand_i | sed 's/^[^0-9]*\([0-9]\+\).*/\1/'`
+WIDTH=$((WIDTH_MIN_1 + 1))
+# Modify label template
+# Change rand_i
+sed -i 's/\(rand_i\s\[[0-9]\+:0\]\s=\s\)unimportant/\1random/g' tmp/labels.txt
+# Generate s_i string
+S_I=$"s_i [$WIDTH_MIN_1:0] = secret $WIDTH_MIN_1:0\ns_i [$((WIDTH*2-1)):$WIDTH] = secret $WIDTH_MIN_1:0"
+# Replace s_i by new string
+sed -i "s/\(s_i\s\[\)\([0-9]\+:0\)\(\]\s=\s\)unimportant/${S_I}/g" tmp/labels.txt
+
+# Trace
+./trace.py --testbench ${REPO_TOP}/hw/ip/kmac/pre_sca/alma/cpp/verilator_tb_${TESTBENCH}.cpp \
+--netlist tmp/circuit.v -o tmp/circuit
+
+# Verify
+./verify.py --json tmp/circuit.json \
+   --label tmp/labels.txt \
+   --top-module ${TOP_MODULE} \
+   --vcd tmp/tmp.vcd \
+   --rst-name rst_ni --rst-phase 0 \
+   --probe-duration once --mode transient \
+   --glitch-behavior loose --cycles 3
