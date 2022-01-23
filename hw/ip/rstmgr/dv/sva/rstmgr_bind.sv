c3e7152c913cd236d5217cddc3f52ca7b941575d
Guillermo Maturana <maturana@google.com>
2022-01-22 16:37:36 -0800

    [dv/rstmgr] Add sw_rst SVA
    Added assertion to check the software controllable reset outputs
    and enables respond to either a parent reset or the CSR controls.
    Minor formatting changes.
    
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- hw/ip/rstmgr/dv/sva/rstmgr_bind.sv
+++ hw/ip/rstmgr/dv/sva/rstmgr_bind.sv
@@ -49,4 +49,32 @@ module rstmgr_bind;
     .expected_cpu_info_attr(($bits(cpu_dump_i) + 31) / 32)
   );
 
+  bind rstmgr rstmgr_sw_rst_sva_if rstmgr_sw_rst_sva_if (
+    .clk_i,
+    .rst_ni,
+    .parent_rst_n(rst_sys_src_n[1]),
+    .enables(reg2hw.sw_rst_regwen),
+    .ctrl_ns(reg2hw.sw_rst_ctrl_n),
+    .rst_ens({
+      rst_en_o.i2c2[1],
+      rst_en_o.i2c1[1],
+      rst_en_o.i2c0[1],
+      rst_en_o.usbif[1],
+      rst_en_o.usb[1],
+      rst_en_o.spi_host1[1],
+      rst_en_o.spi_host0[1],
+      rst_en_o.spi_device[1]
+    }),
+    .rst_ns({
+      resets_o.rst_i2c2_n[1],
+      resets_o.rst_i2c1_n[1],
+      resets_o.rst_i2c0_n[1],
+      resets_o.rst_usbif_n[1],
+      resets_o.rst_usb_n[1],
+      resets_o.rst_spi_host1_n[1],
+      resets_o.rst_spi_host0_n[1],
+      resets_o.rst_spi_device_n[1]
+    })
+  );
+
 endmodule
