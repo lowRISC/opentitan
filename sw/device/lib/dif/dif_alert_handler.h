47faff9f18b6e88f1ead0da7079224e250672535
Timothy Trippel <ttrippel@google.com>
2022-01-24 23:09:56 -0800

    [dif/alert_handler] Add X-macros to escalation/accumulator DIFs.
    This commit adds X-macros to the alert handler DIFs that interface with
    class escalation and accumulator CSRs. This increases the maintaiability
    of these DIFs, since they will not need to be modified if additional
    classes are added.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

--- sw/device/lib/dif/dif_alert_handler.h
+++ sw/device/lib/dif/dif_alert_handler.h
@@ -663,13 +663,13 @@ dif_result_t dif_alert_handler_escalation_clear(
  *
  * @param alert_handler An alert handler handle.
  * @param alert_class The class to get the accumulator for.
- * @param alerts Out-param for the accumulator.
+ * @param num_alerts Out-param for the number of alerts that have accumulated.
  * @return The result of the operation.
  */
 OT_WARN_UNUSED_RESULT
 dif_result_t dif_alert_handler_get_accumulator(
     const dif_alert_handler_t *alert_handler,
-    dif_alert_handler_class_t alert_class, uint16_t *alerts);
+    dif_alert_handler_class_t alert_class, uint16_t *num_alerts);
 
 /**
  * Gets the current value of the "escalation counter".
