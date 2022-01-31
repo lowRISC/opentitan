60d18e275e3ab27736ad4697c3d68378571e1e93
Srikrishna Iyer <sriyer@google.com>
2022-01-31 14:40:19 -0800

    [rv_dm] Add TLUL binds
    This adds TLUL protocol checkers on host and 2 device interfaces. It
    also adds the CSR FPV checker on the regs TL interface.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/rv_dm/dv/sva/rv_dm_bind.sv
+++ hw/ip/rv_dm/dv/sva/rv_dm_bind.sv
@@ -6,18 +6,38 @@ module rv_dm_bind;
 
   bind rv_dm tlul_assert #(
     .EndpointType("Device")
-  ) tlul_assert_device (
+  ) tlul_assert_device_regs (
     .clk_i,
     .rst_ni,
-    .h2d  (tl_i),
-    .d2h  (tl_o)
+    .h2d  (regs_tl_d_i),
+    .d2h  (regs_tl_d_o)
   );
 
-  bind rv_dm rv_dm_csr_assert_fpv rv_dm_csr_assert (
+  bind rv_dm tlul_assert #(
+    .EndpointType("Device")
+  ) tlul_assert_device_rom (
     .clk_i,
     .rst_ni,
-    .h2d    (tl_i),
-    .d2h    (tl_o)
+    .h2d  (rom_tl_d_i),
+    .d2h  (rom_tl_d_o)
   );
+
+  bind rv_dm tlul_assert #(
+    .EndpointType("Host")
+  ) tlul_assert_host_sba (
+    .clk_i,
+    .rst_ni,
+    .h2d  (sba_tl_h_o),
+    .d2h  (sba_tl_h_i)
+  );
+
+  bind rv_dm rv_dm_regs_csr_assert_fpv rv_dm_regs_csr_assert (
+    .clk_i,
+    .rst_ni,
+    .h2d    (regs_tl_d_i),
+    .d2h    (regs_tl_d_o)
+  );
+
+  // TODO: What about 'rv_dm_rom_csr_assert_fpv?
 
 endmodule
