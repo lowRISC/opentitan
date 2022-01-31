c25bf653c8672d5d0a24f30147708c4c078874f2
Srikrishna Iyer <sriyer@google.com>
2022-01-31 14:52:22 -0800

    [rv_dm dv] Remove old testbench components
    This commit removes the old testbench components that are up-to-date in
    the newly added testbench.
    
    There are still several defunct testbench pieces which will be removed
    subsequently, as progress towards V1 is made.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/rv_dm/dv_bkp/probe.tcl
+++
@@ -1,8 +0,0 @@
-# Copyright lowRISC contributors.
-# Licensed under the Apache License, Version 2.0, see LICENSE for details.
-# SPDX-License-Identifier: Apache-2.0
-
-database -shm -open waves.shm -default
-probe -create -shm tb -depth all -all -mem
-run
-exit
