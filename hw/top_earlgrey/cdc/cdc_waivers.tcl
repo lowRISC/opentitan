262651b7db24d593b5ac697b6701da0ed3123628
Eunchan Kim <eunchan@opentitan.org>
2022-02-02 18:31:29 -0800

    [gpio] Waive Reconvergence CDC warning
    As GPIO inputs are not assumed as a bus signal, it is OK to just 2FF
    PAD_IN values.
    
    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>

--- hw/top_earlgrey/cdc/cdc_waivers.tcl
+++ hw/top_earlgrey/cdc/cdc_waivers.tcl
@@ -11,3 +11,13 @@
 #   -comment {}
 # set_rule_status -rule { S_CONF_ENV } -status { Waived } -all_rule_data \
 #   -comment {}
+# Assumes modules defined in run-cdc.tcl
+if {[info exists $modules] == 0} {
+  puts "modules variable is not defined."
+} else {
+  foreach mod $modules {
+    if {[file exists "cdc_waivers.$mod.tcl"]} {
+      source "cdc_waivers.$mod.tcl"
+    }
+  }
+}
