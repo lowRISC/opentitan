c7fa80f546714377a13b6bf41084e2da4d352835
Weicai Yang <weicai@google.com>
2022-02-10 22:46:49 -0800

    [dv] Add mubi_cov for mubi ports
    Signed-off-by: Weicai Yang <weicai@google.com>

---
+++ hw/dv/sv/cip_lib/cip_mubi_cov_if.sv
@@ -0,0 +1,18 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+interface cip_mubi_cov_if #(parameter int Width = 4) (input [Width-1:0] mubi, input rst_ni);
+  import uvm_pkg::*;
+  import dv_base_reg_pkg::*;
+
+  dv_base_mubi_cov mubi_cov;
+  initial begin
+    mubi_cov = dv_base_mubi_cov::type_id::create($sformatf("%m"));
+    mubi_cov.create_cov(Width);
+    forever begin
+      @(mubi or rst_ni);
+      if (rst_ni === 1) mubi_cov.sample(mubi);
+    end
+  end
+endinterface
