9564ca571af7e2b50b5b4f13cd706a46599b57f0
Alphan Ulusoy <alphan@google.com>
2022-02-13 11:53:56 -0500

    [sw/silicon_creator] Watchdog unittest update
    This change adds `ExpectInit(enabled)` to simplify expectations.
    
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

--- sw/device/silicon_creator/lib/drivers/watchdog_unittest.cc
+++ sw/device/silicon_creator/lib/drivers/watchdog_unittest.cc
@@ -21,6 +21,36 @@ using ::testing::Return;
 
 class WatchdogTest : public mask_rom_test::MaskRomTest {
  protected:
+  /**
+   * Sets up expectations for `watchdog_init()`.
+   *
+   * @param enabled Whether watchdog is expected to be enabled.
+   */
+  void ExpectInit(bool enabled) {
+    const uint32_t kBiteThreshold = 0x12345678;
+    EXPECT_CALL(
+        otp_, read32(OTP_CTRL_PARAM_ROM_WATCHDOG_BITE_THRESHOLD_CYCLES_OFFSET))
+        .WillOnce(Return(kBiteThreshold));
+
+    EXPECT_SEC_WRITE32(pwrmgr_ + PWRMGR_RESET_EN_REG_OFFSET,
+                       {
+                           {PWRMGR_RESET_EN_EN_1_BIT, true},
+                       });
+    EXPECT_ABS_WRITE32(pwrmgr_ + PWRMGR_CFG_CDC_SYNC_REG_OFFSET, 1);
+    EXPECT_SEC_WRITE32(wdog_ + AON_TIMER_WDOG_CTRL_REG_OFFSET,
+                       0 << AON_TIMER_WDOG_CTRL_ENABLE_BIT);
+    EXPECT_ABS_WRITE32(wdog_ + AON_TIMER_WDOG_COUNT_REG_OFFSET, 0);
+    EXPECT_ABS_WRITE32(wdog_ + AON_TIMER_WDOG_BARK_THOLD_REG_OFFSET,
+                       std::numeric_limits<uint32_t>::max());
+    EXPECT_SEC_WRITE32(wdog_ + AON_TIMER_WDOG_BITE_THOLD_REG_OFFSET,
+                       kBiteThreshold);
+    EXPECT_SEC_WRITE32(wdog_ + AON_TIMER_WDOG_CTRL_REG_OFFSET,
+                       enabled << AON_TIMER_WDOG_CTRL_ENABLE_BIT);
+    EXPECT_ABS_READ32(wdog_ + AON_TIMER_WDOG_CTRL_REG_OFFSET,
+                      enabled << AON_TIMER_WDOG_CTRL_ENABLE_BIT);
+    EXPECT_ABS_WRITE32(pwrmgr_ + PWRMGR_CFG_CDC_SYNC_REG_OFFSET, 1);
+  }
+
   uint32_t pwrmgr_ = TOP_EARLGREY_PWRMGR_AON_BASE_ADDR;
   uint32_t wdog_ = TOP_EARLGREY_AON_TIMER_AON_BASE_ADDR;
   mask_rom_test::MockAbsMmio abs_;
@@ -32,28 +62,8 @@ TEST_F(WatchdogTest, InitializeNoOtp) {
   constexpr std::array<lifecycle_state_t, 2> kLifecycleStates = {kLcStateTest,
                                                                  kLcStateRma};
   for (const auto lc : kLifecycleStates) {
-    const uint32_t kBiteThreshold = 0x12345678;
-    EXPECT_CALL(
-        otp_, read32(OTP_CTRL_PARAM_ROM_WATCHDOG_BITE_THRESHOLD_CYCLES_OFFSET))
-        .WillOnce(Return(kBiteThreshold));
+    ExpectInit(false);
 
-    EXPECT_SEC_WRITE32(pwrmgr_ + PWRMGR_RESET_EN_REG_OFFSET,
-                       {
-                           {PWRMGR_RESET_EN_EN_1_BIT, true},
-                       });
-    EXPECT_ABS_WRITE32(pwrmgr_ + PWRMGR_CFG_CDC_SYNC_REG_OFFSET, 1);
-    EXPECT_SEC_WRITE32(wdog_ + AON_TIMER_WDOG_CTRL_REG_OFFSET,
-                       0 << AON_TIMER_WDOG_CTRL_ENABLE_BIT);
-    EXPECT_ABS_WRITE32(wdog_ + AON_TIMER_WDOG_COUNT_REG_OFFSET, 0);
-    EXPECT_ABS_WRITE32(wdog_ + AON_TIMER_WDOG_BARK_THOLD_REG_OFFSET,
-                       std::numeric_limits<uint32_t>::max());
-    EXPECT_SEC_WRITE32(wdog_ + AON_TIMER_WDOG_BITE_THOLD_REG_OFFSET,
-                       kBiteThreshold);
-    EXPECT_SEC_WRITE32(wdog_ + AON_TIMER_WDOG_CTRL_REG_OFFSET,
-                       0 << AON_TIMER_WDOG_CTRL_ENABLE_BIT);
-    EXPECT_ABS_READ32(wdog_ + AON_TIMER_WDOG_CTRL_REG_OFFSET,
-                      0 << AON_TIMER_WDOG_CTRL_ENABLE_BIT);
-    EXPECT_ABS_WRITE32(pwrmgr_ + PWRMGR_CFG_CDC_SYNC_REG_OFFSET, 1);
     watchdog_init(lc);
   }
 }
@@ -62,28 +72,8 @@ TEST_F(WatchdogTest, InitializeOtp) {
   constexpr std::array<lifecycle_state_t, 3> kLifecycleStates = {
       kLcStateDev, kLcStateProd, kLcStateProdEnd};
   for (const auto lc : kLifecycleStates) {
-    const uint32_t kBiteThreshold = 0x12345678;
-    EXPECT_CALL(
-        otp_, read32(OTP_CTRL_PARAM_ROM_WATCHDOG_BITE_THRESHOLD_CYCLES_OFFSET))
-        .WillOnce(Return(kBiteThreshold));
+    ExpectInit(true);
 
-    EXPECT_SEC_WRITE32(pwrmgr_ + PWRMGR_RESET_EN_REG_OFFSET,
-                       {
-                           {PWRMGR_RESET_EN_EN_1_BIT, true},
-                       });
-    EXPECT_ABS_WRITE32(pwrmgr_ + PWRMGR_CFG_CDC_SYNC_REG_OFFSET, 1);
-    EXPECT_SEC_WRITE32(wdog_ + AON_TIMER_WDOG_CTRL_REG_OFFSET,
-                       0 << AON_TIMER_WDOG_CTRL_ENABLE_BIT);
-    EXPECT_ABS_WRITE32(wdog_ + AON_TIMER_WDOG_COUNT_REG_OFFSET, 0);
-    EXPECT_ABS_WRITE32(wdog_ + AON_TIMER_WDOG_BARK_THOLD_REG_OFFSET,
-                       std::numeric_limits<uint32_t>::max());
-    EXPECT_SEC_WRITE32(wdog_ + AON_TIMER_WDOG_BITE_THOLD_REG_OFFSET,
-                       kBiteThreshold);
-    EXPECT_SEC_WRITE32(wdog_ + AON_TIMER_WDOG_CTRL_REG_OFFSET,
-                       1 << AON_TIMER_WDOG_CTRL_ENABLE_BIT);
-    EXPECT_ABS_READ32(wdog_ + AON_TIMER_WDOG_CTRL_REG_OFFSET,
-                      1 << AON_TIMER_WDOG_CTRL_ENABLE_BIT);
-    EXPECT_ABS_WRITE32(pwrmgr_ + PWRMGR_CFG_CDC_SYNC_REG_OFFSET, 1);
     watchdog_init(lc);
   }
 }
