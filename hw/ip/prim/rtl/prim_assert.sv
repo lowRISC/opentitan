4da26947794c2cdc10d3961d1b23d90f95a191c8
Pirmin Vogel <vogelpi@lowrisc.org>
2022-02-14 15:13:58 +0100

    [prim] Add new assertion macro for generating static lint errors
    This is suitable warn about Sec parameters taking on non-default values.
    Non-default values for such parameters may be required for pre-silicon
    or FPGA evaluation but we don't want to allow this for tapeout.
    
    This is related to lowRISC/OpenTitan#10721.
    
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

--- hw/ip/prim/rtl/prim_assert.sv
+++ hw/ip/prim/rtl/prim_assert.sv
@@ -33,6 +33,16 @@
          `PRIM_STRINGIFY(__name));                                                       \
 `endif
 
+// This macro is suitable for conditionally triggering lint errors, e.g., if a Sec parameter takes
+// on a non-default value. This may be required for pre-silicon/FPGA evaluation but we don't want
+// to allow this for tapeout.
+`define ASSERT_STATIC_LINT_ERROR(__name, __prop)     \
+  localparam int __name = (__prop) ? 1 : 2;          \
+  always_comb begin                                  \
+    logic unused_assert_static_lint_error;           \
+    unused_assert_static_lint_error = __name'(1'b1); \
+  end
+
 // The basic helper macros are actually defined in "implementation headers". The macros should do
 // the same thing in each case (except for the dummy flavour), but in a way that the respective
 // tools support.
