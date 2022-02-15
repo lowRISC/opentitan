7ff7336b02695bb0961cfeb4e98b103ade4fe692
Srikrishna Iyer <sriyer@google.com>
2022-02-15 10:02:16 -0800

    [rv_dm dv] Drive DUT inputs to known values
    - Initialize the inputs to the DUT with some random values
    - Constrain the inputs in the common seqeunce with amenable values
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/rv_dm/dv/env/seq_lib/rv_dm_common_vseq.sv
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_common_vseq.sv
@@ -4,17 +4,25 @@
 
 class rv_dm_common_vseq extends rv_dm_base_vseq;
   `uvm_object_utils(rv_dm_common_vseq)
+  `uvm_object_new
 
   constraint num_trans_c {
     num_trans inside {[1:2]};
   }
-  `uvm_object_new
+
+  // We set these initial inputs to known values to prevent side effects that may affect these
+  // common tests.
+  constraint lc_hw_debug_en_c {
+    lc_hw_debug_en == lc_ctrl_pkg::On;
+  }
+  constraint scanmode_c {
+    scanmode == prim_mubi_pkg::MuBi4False;
+  }
+  constraint unavailable_c {
+    unavailable == 0;
+  }
 
   virtual task body();
-    // Enable access to debug ROM for the common sequences to work.
-    // TODO (lowrisc/opentitan/#10453): Figure out if this needs to be mdified based on the common
-    // test seq that is run.
-    cfg.rv_dm_vif.lc_hw_debug_en = lc_ctrl_pkg::On;
     run_common_vseq_wrapper(num_trans);
   endtask : body
 
