c4b8d99b71de732a9cfc55a41e5e630885db5371
Kosta Kojdic <kosta.kojdic@ensilica.com>
2022-02-15 09:57:57 +0000

    New SPI Device agent for passthrough connection
    Signed-off-by: Kosta Kojdic <kosta.kojdic@ensilica.com>

--- hw/ip/spi_device/dv/env/spi_device_env_cfg.sv
+++ hw/ip/spi_device/dv/env/spi_device_env_cfg.sv
@@ -4,11 +4,13 @@
 
 class spi_device_env_cfg extends cip_base_env_cfg #(.RAL_T(spi_device_reg_block));
   rand spi_agent_cfg  m_spi_agent_cfg;
+  rand spi_agent_cfg  spi_device_agent_cfg;
   bit [TL_AW-1:0]     sram_start_addr;
   bit [TL_AW-1:0]     sram_end_addr;
 
   `uvm_object_utils_begin(spi_device_env_cfg)
     `uvm_field_object(m_spi_agent_cfg, UVM_DEFAULT)
+    `uvm_field_object(spi_device_agent_cfg, UVM_DEFAULT)
   `uvm_object_utils_end
 
   `uvm_object_new
@@ -18,6 +20,7 @@ class spi_device_env_cfg extends cip_base_env_cfg #(.RAL_T(spi_device_reg_block)
     super.initialize(csr_base_addr);
     // create spi agent config obj
     m_spi_agent_cfg = spi_agent_cfg::type_id::create("m_spi_agent_cfg");
+    spi_device_agent_cfg = spi_agent_cfg::type_id::create("spi_device_agent_cfg");
     // set num_interrupts & num_alerts which will be used to create coverage and more
     num_interrupts = ral.intr_state.get_n_used_bits();
 
