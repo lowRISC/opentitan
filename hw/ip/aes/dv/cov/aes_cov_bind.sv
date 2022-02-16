9a8a8d179bfaf084832b7a8135330f81721965ea
Rasmus Madsen <rasmus.madsen@wdc.com>
2022-02-16 02:16:13 -0800

    [aes/dv] updated func cover
    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>

--- hw/ip/aes/dv/cov/aes_cov_bind.sv
+++ hw/ip/aes/dv/cov/aes_cov_bind.sv
@@ -2,11 +2,12 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 //
-// Binds UART functional coverage interaface to the top level AES module.
+// Binds AES functional coverage interaface to the top level AES module.
 module aes_cov_bind;
 
   bind aes aes_cov_if u_aes_cov_if (
-    .clk_i           (clk_i)
+    .clk_i           (clk_i),
+    .idle_i          (idle_o)
   );
 
 endmodule
