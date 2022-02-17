501a311bb3d53c3cd9b6b08f157a030f26c49e96
Jade Philipoom <jadep@google.com>
2022-02-17 16:43:53 +0000

    [sw/silicon_creator] Move mock_abs_mmio to device/lib.
    Move mock_abs_mmio out of silicon_creator to device/lib/base/testing,
    and update tests and mocks to use it from the new location.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

--- sw/device/silicon_creator/lib/drivers/watchdog_unittest.cc
+++ sw/device/silicon_creator/lib/drivers/watchdog_unittest.cc
@@ -5,7 +5,7 @@
 #include "sw/device/silicon_creator/lib/drivers/watchdog.h"
 
 #include "gtest/gtest.h"
-#include "sw/device/silicon_creator/lib/base/mock_abs_mmio.h"
+#include "sw/device/lib/base/testing/mock_abs_mmio.h"
 #include "sw/device/silicon_creator/lib/base/mock_sec_mmio.h"
 #include "sw/device/silicon_creator/lib/drivers/lifecycle.h"
 #include "sw/device/silicon_creator/lib/drivers/mock_otp.h"
