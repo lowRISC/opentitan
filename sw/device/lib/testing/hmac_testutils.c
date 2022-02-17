c39a50a67dccd11be10812b21e4770fbab9a3fc9
Silvestrs Timofejevs <silvestrst@lowrisc.org>
2022-02-17 17:04:46 +0000

    [hmac_enc, tests] Factor out common functionality into testutils
    The smoketest is written in such way, that large portion of the
    static API can be factored out as testutils helpers.
    
    This is useful in terms of the next commit, which adds the
    2nd part of the hmac_enc_test.
    
    Signed-off-by: Silvestrs Timofejevs <silvestrst@lowrisc.org>

--- sw/device/lib/testing/hmac_testutils.c
+++ sw/device/lib/testing/hmac_testutils.c
@@ -5,6 +5,7 @@
 #include "sw/device/lib/testing/hmac_testutils.h"
 
 #include "sw/device/lib/dif/dif_hmac.h"
+#include "sw/device/lib/runtime/ibex.h"
 #include "sw/device/lib/testing/check.h"
 
 void hmac_testutils_check_message_length(const dif_hmac_t *hmac,
@@ -19,4 +20,56 @@ void hmac_testutils_check_message_length(const dif_hmac_t *hmac,
         "Expected 0x%08x%08x bits but got 0x%08x%08x bits.",
         (uint32_t)(expected_sent_bits >> 32), (uint32_t)expected_sent_bits,
         (uint32_t)(sent_bits >> 32), (uint32_t)sent_bits);
+}
+
+/**
+ * Checks whether the HMAC FIFO is empty.
+ */
+static bool check_fifo_empty(const dif_hmac_t *hmac) {
+  uint32_t fifo_depth;
+  CHECK_DIF_OK(dif_hmac_fifo_count_entries(hmac, &fifo_depth));
+  return fifo_depth == 0;
+}
+void hmac_testutils_fifo_empty_polled(const dif_hmac_t *hmac) {
+  IBEX_SPIN_FOR(check_fifo_empty(hmac), HMAC_TESTUTILS_FIFO_EMPTY_USEC);
+}
+
+static bool check_finished(const dif_hmac_t *hmac,
+                           dif_hmac_digest_t *digest_out) {
+  dif_result_t res = dif_hmac_finish(hmac, digest_out);
+  CHECK(res == kDifOk || res == kDifUnavailable, "HMAC error = %d", res);
+
+  return res == kDifOk;
+}
+void hmac_testutils_finish_polled(const dif_hmac_t *hmac,
+                                  dif_hmac_digest_t *digest_out) {
+  IBEX_SPIN_FOR(check_finished(hmac, digest_out),
+                HMAC_TESTUTILS_FINISH_TIMEOUT_USEC);
+}
+
+void hmac_testutils_finish_and_check_polled(const dif_hmac_t *hmac,
+                                            const dif_hmac_digest_t *expected) {
+  dif_hmac_digest_t digest;
+  hmac_testutils_finish_polled(hmac, &digest);
+  CHECK_BUFFER(digest.digest, expected, ARRAYSIZE(digest.digest));
+}
+
+void hmac_testutils_push_message(const dif_hmac_t *hmac, const char *data,
+                                 size_t len) {
+  const char *dp = data;
+  size_t sent_bytes;
+
+  while (dp - data < len) {
+    dif_result_t res =
+        dif_hmac_fifo_push(hmac, dp, len - (dp - data), &sent_bytes);
+    CHECK(res == kDifOk || res == kDifIpFifoFull, "HMAC error = %d", res);
+
+    // Wait until the FIFO is drained before pushing more data. This helps
+    // to prevent the undesirable back pressure condition.
+    if (res == kDifIpFifoFull) {
+      hmac_testutils_fifo_empty_polled(hmac);
+    }
+
+    dp += sent_bytes;
+  }
 }
--- sw/device/lib/testing/hmac_testutils.h
+++ sw/device/lib/testing/hmac_testutils.h
@@ -8,13 +8,94 @@
 #include <assert.h>
 #include <stdint.h>
 
+#include "sw/device/lib/arch/device.h"
 #include "sw/device/lib/dif/dif_hmac.h"
 
 /**
- * Read and compare the length of the message in the HMAC engine to the length
- * of the message sent in bits.
+ * Timeouts to be used for different HMAC operations.
+ *
+ * All timeouts are calculated against the `kClockFreqCpuHz`, in order
+ * to cover a range of targets. Please see:
+ * https://docs.opentitan.org/hw/ip/hmac/doc/
+ *
+ * 10 cycles are added to the length of the corresponding operation as
+ * described in the documentation. This is to cover any potential
+ * inconsistencies or minor IP changes.
+ *
+ * To avoid cases with sub uS timeout due to very high clock frequency, we
+ * guarantee 1uS minimal timeout by adding it in the end.
+ */
+
+/**
+ * FIFO empty timeout.
+ *
+ * single HMAC block compression takes 80 cycles.
+ */
+#define HMAC_TESTUTILS_FIFO_EMPTY_USEC (80 + 10) * 1000000 / kClockFreqCpuHz + 1
+
+/**
+ * HMAC done timeout.
+ *
+ * Final hash calculation takes 360 cycles, which consists of one block
+ * compression and extra HMAC computation.
+ */
+#define HMAC_TESTUTILS_FINISH_TIMEOUT_USEC \
+  (360 + 10) * 1000000 / kClockFreqCpuHz + 1
+
+/**
+ * Reads and compares the actual sent message length against expected.
+ *
+ * The message length is provided in bits.
+ *
+ * @param hmac An HMAC handle.
+ * @param expected_sent_bits Expected size of hashed data in bits.
  */
 void hmac_testutils_check_message_length(const dif_hmac_t *hmac,
                                          uint64_t expected_sent_bits);
+
+/**
+ * Spins until the HMAC FIFO is empty, or has timed out.
+ *
+ * Internally uses `kHmacTestutilsFifoEmptyTimeoutUsec`.
+ *
+ * @param hmac An HMAC handle.
+ */
+void hmac_testutils_fifo_empty_polled(const dif_hmac_t *hmac);
+
+/**
+ * Spins until the HMAC has finished processing final hash, or timed out.
+ *
+ * Internally uses `kHmacTestutilsFinishTimeoutUsec`.
+ *
+ * @param hmac An HMAC handle.
+ * @param digest_out HMAC final digest.
+ */
+void hmac_testutils_finish_polled(const dif_hmac_t *hmac,
+                                  dif_hmac_digest_t *digest_out);
+
+/**
+ * Spins until HMAC has processed the final hash, and compares the digests.
+ *
+ * Convenience function that combines `hmac_testutils_finish_polled` and
+ * and `CHECK_BUFFER`.
+ *
+ * @param hmac An HMAC handle.
+ * @param expected Expected HMAC final digest.
+ */
+void hmac_testutils_finish_and_check_polled(const dif_hmac_t *hmac,
+                                            const dif_hmac_digest_t *expected);
+
+/**
+ * Loads entire message into the HMAC engine.
+ *
+ * Internally uses `hmac_testutils_fifo_empty_polled` after every push to
+ * avoid the back pressure.
+ *
+ * @param hmac An HMAC handle.
+ * @param data Data to be hashed.
+ * @param len Size of the data to be hashed.
+ */
+void hmac_testutils_push_message(const dif_hmac_t *hmac, const char *data,
+                                 size_t len);
 
 #endif  // OPENTITAN_SW_DEVICE_LIB_TESTING_HMAC_TESTUTILS_H_
