6400ce204641118b278e1410406e3244a426f821
Jes B. Klinke <jbk@chromium.org>
2022-02-18 12:01:26 -0800

    [opentitantool] Proxy transport implementation
    Add a new implementation of the Transport trait, which forwards every
    method invocation via a network protocol to a separate session proxy
    process.
    
    This is to be used either with the session process being a software
    emulator of sorts, or with the session process managing a USB
    connection to a debugger device connected to hardware.
    
    This is one of the steps outlined in issue #10889.  Implementation of
    the session process is in PR #11054.
    
    Signed-off-by: Jes B. Klinke <jbk@chromium.org>
    Change-Id: I6d4cd777a122e3017763704df6632f6f769991fd

---
+++ sw/host/opentitanlib/src/transport/proxy/gpio.rs
@@ -0,0 +1,69 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::rc::Rc;
+
+use crate::bail;
+use crate::io::gpio::{GpioPin, PinMode, PullMode};
+use crate::proxy::protocol::{GpioRequest, GpioResponse, Request, Response};
+use crate::transport::proxy::{Inner, Proxy, Result};
+use super::ProxyError;
+
+pub struct ProxyGpioPin {
+    inner: Rc<Inner>,
+    pinname: String,
+}
+
+impl ProxyGpioPin {
+    pub fn open(proxy: &Proxy, pinname: &str) -> Result<Self> {
+        let result = Self {
+            inner: Rc::clone(&proxy.inner),
+            pinname: pinname.to_string(),
+        };
+        Ok(result)
+    }
+
+    // Convenience method for issuing GPIO commands via proxy protocol. 
+    fn execute_command(&self, command: GpioRequest) -> Result<GpioResponse> {
+        match self.inner.execute_command(Request::Gpio {
+            id: self.pinname.clone(),
+            command,
+        })? {
+            Response::Gpio(resp) => Ok(resp),
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+}
+
+impl GpioPin for ProxyGpioPin {
+    /// Reads the value of the the GPIO pin `id`.
+    fn read(&self) -> Result<bool> {
+        match self.execute_command(GpioRequest::Read)? {
+            GpioResponse::Read { value } => Ok(value),
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+
+    /// Sets the value of the GPIO pin `id` to `value`.
+    fn write(&self, value: bool) -> Result<()> {
+        match self.execute_command(GpioRequest::Write { logic: value })? {
+            GpioResponse::Write => Ok(()),
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+
+    fn set_mode(&self, mode: PinMode) -> Result<()> {
+        match self.execute_command(GpioRequest::SetMode { mode })? {
+            GpioResponse::SetMode => Ok(()),
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+
+    fn set_pull_mode(&self, pull: PullMode) -> Result<()> {
+        match self.execute_command(GpioRequest::SetPullMode { pull })? {
+            GpioResponse::SetPullMode => Ok(()),
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+}
---
+++ sw/host/opentitanlib/src/transport/proxy/uart.rs
@@ -0,0 +1,95 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::rc::Rc;
+use std::time::Duration;
+
+use crate::bail;
+use crate::io::uart::Uart;
+use crate::proxy::protocol::{Request, Response, UartRequest, UartResponse};
+use crate::transport::proxy::{Inner, Proxy, Result};
+use super::ProxyError;
+
+pub struct ProxyUart {
+    inner: Rc<Inner>,
+    instance: String,
+}
+
+impl ProxyUart {
+    pub fn open(proxy: &Proxy, instance: &str) -> Result<Self> {
+        let result = Self {
+            inner: Rc::clone(&proxy.inner),
+            instance: instance.to_string(),
+        };
+        Ok(result)
+    }
+
+    // Convenience method for issuing UART commands via proxy protocol. 
+    fn execute_command(&self, command: UartRequest) -> Result<UartResponse> {
+        match self.inner.execute_command(Request::Uart {
+            id: self.instance.clone(),
+            command,
+        })? {
+            Response::Uart(resp) => Ok(resp),
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+}
+
+impl Uart for ProxyUart {
+    /// Returns the UART baudrate.  May return zero for virtual UARTs.
+    fn get_baudrate(&self) -> Result<u32> {
+        match self.execute_command(UartRequest::GetBaudrate)? {
+            UartResponse::GetBaudrate { rate } => Ok(rate),
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+
+    /// Sets the UART baudrate.  May do nothing for virtual UARTs.
+    fn set_baudrate(&self, rate: u32) -> Result<()> {
+        match self.execute_command(UartRequest::SetBaudrate { rate })? {
+            UartResponse::SetBaudrate => Ok(()),
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+
+    /// Reads UART receive data into `buf`, returning the number of bytes read.
+    /// This function _may_ block.
+    fn read(&self, buf: &mut [u8]) -> Result<usize> {
+        match self.execute_command(UartRequest::Read {
+            timeout_millis: None,
+            len: buf.len() as u32,
+        })? {
+            UartResponse::Read { data } => {
+                buf[..data.len()].clone_from_slice(&data);
+                Ok(data.len())
+            }
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+
+    /// Reads UART receive data into `buf`, returning the number of bytes read.
+    /// The `timeout` may be used to specify a duration to wait for data.
+    /// If timeout expires without any data arriving `Ok(0)` will be returned, never `Err(_)`.
+    fn read_timeout(&self, buf: &mut [u8], timeout: Duration) -> Result<usize> {
+        match self.execute_command(UartRequest::Read {
+            timeout_millis: Some(timeout.as_millis() as u32),
+            len: buf.len() as u32,
+        })? {
+            UartResponse::Read { data } => {
+                buf[..data.len()].clone_from_slice(&data);
+                Ok(data.len())
+            }
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+
+    /// Writes data from `buf` to the UART.
+    fn write(&self, buf: &[u8]) -> Result<()> {
+        match self.execute_command(UartRequest::Write { data: buf.to_vec() })? {
+            UartResponse::Write => Ok(()),
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+}
