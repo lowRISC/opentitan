79b3a677636d0c3d7263c61335ca6548787c46b6
Rasmus Madsen <rasmus.madsen@wdc.com>
2022-02-22 12:23:51 -0800

    [tools/xcelium] updated common coverage exclusions to exclude single bit correctly
    Signed-off-by: Rasmus Madsen <rasmus.madsen@wdc.com>

--- hw/dv/tools/xcelium/common_cov_excl.tcl
+++ hw/dv/tools/xcelium/common_cov_excl.tcl
@@ -6,5 +6,5 @@ exclude -inst $::env(DUT_TOP) -toggle '*tl_i.a_user.rsvd' -comment "\[UNSUPPORTE
 exclude -inst $::env(DUT_TOP) -toggle '*tl_i.a_param' -comment "\[UNSUPPORTED\] Based on our Comportability Spec. Exercising this will result in assertion errors thrown."
 exclude -inst $::env(DUT_TOP) -toggle '*tl_o.d_param' -comment "\[UNR\] Follows tl_i.a_param which is unsupported."
 exclude -inst $::env(DUT_TOP) -toggle '*tl_o.d_sink' -comment "\[UNR\] Based on our Comportability Spec."
-exclude -inst $::env(DUT_TOP) -toggle '*tl_o.d_user.rsp_intg[6]' -comment "\[UNR\] Due to the ECC logics"
+exclude -inst $::env(DUT_TOP) -toggle '*tl_o.d_user.rsp_intg'\[6\] -comment "\[UNR\] Due to the ECC logics"
 exclude -inst $::env(DUT_TOP) -toggle '*alert_rx_*.ping_*' -comment "\[LOW_RISK\] Verified in prim_alert_receiver TB."
