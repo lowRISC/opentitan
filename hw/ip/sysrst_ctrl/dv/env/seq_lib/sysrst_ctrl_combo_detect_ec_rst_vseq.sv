beaeb39363668ec122836cb657786d603b4eeea0
Madhuri Patel <madhuri.patel@ensilica.com>
2022-02-24 18:28:36 +0000

    [sysrst_ctrl,dv] Add sysrst_ctrl auto-block-key-output test
    Signed-off-by: Madhuri Patel <madhuri.patel@ensilica.com>

--- hw/ip/sysrst_ctrl/dv/env/seq_lib/sysrst_ctrl_combo_detect_ec_rst_vseq.sv
+++ hw/ip/sysrst_ctrl/dv/env/seq_lib/sysrst_ctrl_combo_detect_ec_rst_vseq.sv
@@ -2,66 +2,87 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// This sequence will assert the ec_rst_out_l and raise an interrupt when certain combos are detected.
+// This sequence will assert the ec_rst_out_l and raise an interrupt
+// when certain combos are detected.
 class sysrst_ctrl_combo_detect_ec_rst_vseq extends sysrst_ctrl_base_vseq;
   `uvm_object_utils(sysrst_ctrl_combo_detect_ec_rst_vseq)
 
   `uvm_object_new
 
-   task body();
-    uvm_reg_data_t rdata, rdata1;
+  rand int wait_cycles;
+  rand uvm_reg_data_t detect_timer, debounce_timer;
+
+  constraint wait_cycles_c {wait_cycles dist {
+    [15 : 25] :/5,
+    25 :/95
+    };
+  }
+
+  constraint detect_timer_c {detect_timer == 'h5;}
+  constraint debounce_timer_c {debounce_timer == 'h20;}
+
+  task drive_input();
+    // Trigger the input pins
+    cfg.vif.key0_in = 1;
+    cfg.vif.key1_in = 1;
+    cfg.vif.key2_in = 1;
+    cfg.clk_aon_rst_vif.wait_clks($urandom_range(1,10));
+    cfg.vif.key0_in = 0;
+    cfg.vif.key1_in = 0;
+    cfg.vif.key2_in = 0;
+    cfg.clk_aon_rst_vif.wait_clks(wait_cycles);
+    cfg.vif.key0_in = 1;
+    cfg.vif.key1_in = 1;
+    cfg.vif.key2_in = 1;
+  endtask
+
+  task body();
+    uvm_reg_data_t rdata = 0;
 
     `uvm_info(`gfn, "Starting the body from combo detect ec_rst", UVM_LOW)
 
     // Enable the override function and set the override value for ec_rst_l pin
-    ral.pin_out_ctl.ec_rst_l.set(1);
+    ral.pin_out_ctl.ec_rst_l.set(0);
     csr_update(ral.pin_out_ctl);
-
-    ral.pin_out_value.ec_rst_l.set(1);
-    csr_update(ral.pin_out_value);
-
-    ral.pin_allowed_ctl.ec_rst_l_0.set(1);
-    ral.pin_allowed_ctl.ec_rst_l_1.set(1);
-    csr_update(ral.pin_allowed_ctl);
 
     // Enabled key0_in, key1_in, key2_in to trigger the combo
     csr_wr(ral.com_sel_ctl[0], 'h7);
 
     // Set the duration for combo to pressed
-    csr_wr(ral.com_det_ctl[0],'h5) ;
+    csr_wr(ral.com_det_ctl[0], detect_timer);
 
     // Enabled ec_rst_0 action
     csr_wr(ral.com_out_ctl[0], 'h6);
 
     // Set the ec_rst_0 pulse width
-    csr_wr(ral.ec_rst_ctl, 'h5);
+    csr_wr(ral.ec_rst_ctl, 'h10);
 
-    repeat ($urandom_range(1,3)) begin
-      // Trigger the input pins
-      cfg.vif.key0_in = 1;
-      cfg.vif.key1_in = 1;
-      cfg.vif.key2_in = 1;
-      #50us;
-      cfg.vif.key0_in = 0;
-      cfg.vif.key1_in = 0;
-      cfg.vif.key2_in = 0;
-      #50us;
+    // Set the key triggered debounce timer
+    csr_wr(ral.key_intr_debounce_ctl, debounce_timer);
+
+    // It takes 2-3 clock cycles to sync the register values
+    cfg.clk_aon_rst_vif.wait_clks(2);
+
+    drive_input();
+
+    `uvm_info(`gfn, $sformatf("Value of wait_cycles:%0d", wait_cycles), UVM_LOW)
+    if (wait_cycles >= (debounce_timer + detect_timer)) begin
+      // It takes 2-3 clock cycles to sync the interrupt value
+      // Read the combo status register
+      cfg.clk_aon_rst_vif.wait_clks(2);
+      csr_rd_check(.ptr(ral.combo_intr_status), .compare_value(1));
+
+      // Write to clear the interrupt
+      csr_wr(ral.combo_intr_status, 'h1);
+
+      cfg.clk_aon_rst_vif.wait_clks(20);
+      // check if the interrupt is cleared
+      csr_rd_check(.ptr(ral.combo_intr_status), .compare_value(0));
+    end else begin
+      // check there is no interrupt
+      csr_rd_check(.ptr(ral.combo_intr_status), .compare_value(0));
     end
-
-    // Wait for interrupt to raise
-    #100us;
-
-    csr_rd_check(.ptr(ral.combo_intr_status), .compare_value(1));
-    `uvm_info(`gfn, ral.combo_intr_status.sprint(), UVM_MEDIUM)
-
-    // Write to clear the interrupt
-    csr_wr(ral.combo_intr_status, 'h1);
-
-    #100us;
-    // check if the interrupt is cleared
-    csr_rd_check(.ptr(ral.combo_intr_status), .compare_value(0));
-
-    #100us;
+    cfg.clk_aon_rst_vif.wait_clks(20);
   endtask : body
 
 endclass : sysrst_ctrl_combo_detect_ec_rst_vseq
