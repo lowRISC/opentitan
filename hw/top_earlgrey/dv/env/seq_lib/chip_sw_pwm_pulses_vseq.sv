8d4dc9803e2e98d07ce1324fa64a38172188f2c0
Jaedon Kim <jdonjdon@google.com>
2022-02-25 15:03:47 +0000

    [dv,top,pwm] Add initial place holder for pwm test
    add a new c test and sequence for pwm test.
    
    Signed-off-by: Jaedon Kim <jdonjdon@google.com>

---
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_pwm_pulses_vseq.sv
@@ -0,0 +1,15 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class chip_sw_pwm_pulses_vseq extends chip_sw_base_vseq;
+  `uvm_object_utils(chip_sw_pwm_pulses_vseq)
+
+  `uvm_object_new
+
+  virtual task body();
+    super.body();
+    `uvm_error(`gfn, "Not implemented")
+  endtask // body
+
+endclass // chip_sw_pwm_pulses_vseq
