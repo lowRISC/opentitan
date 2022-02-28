1e57c8df2280373b9e682d7077457776f7a253f9
Eitan Shapira <eitanshapira89@gmail.com>
2022-02-28 16:16:15 +0200

    [flash_ctrl/dv] Move callback to apply_reset from dut_init
    Signed-off-by: Eitan Shapira <eitanshapira89@gmail.com>

--- hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_callback_vseq.sv
+++ hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_callback_vseq.sv
@@ -12,7 +12,7 @@ class flash_ctrl_callback_vseq extends cip_base_vseq #(
   `uvm_object_utils(flash_ctrl_callback_vseq)
   `uvm_object_new
 
-  virtual task dut_init_callback();
+  virtual task apply_reset_callback();
     // Do nothing but can be overridden in closed source environment.
   endtask
 
