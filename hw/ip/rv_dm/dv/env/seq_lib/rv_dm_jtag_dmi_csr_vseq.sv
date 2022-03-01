c0fd1007f6b6b0be44638b62bb6281991742332d
Srikrishna Iyer <sriyer@google.com>
2022-02-28 19:17:50 -0800

    [rv_dm dv] Minor enhancement to CSR test seqs
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/rv_dm/dv/env/seq_lib/rv_dm_jtag_dmi_csr_vseq.sv
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_jtag_dmi_csr_vseq.sv
@@ -3,25 +3,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Run the JTAG DMT CSRs through our standard CSR suite via JTAG.
-class rv_dm_jtag_dmi_csr_vseq extends rv_dm_base_vseq;
+class rv_dm_jtag_dmi_csr_vseq extends rv_dm_common_vseq;
   `uvm_object_utils(rv_dm_jtag_dmi_csr_vseq)
   `uvm_object_new
-
-  constraint num_trans_c {
-    num_trans inside {[1:2]};
-  }
-
-  // We set these initial inputs to known values to prevent side effects that may affect these
-  // common tests.
-  constraint lc_hw_debug_en_c {
-    lc_hw_debug_en == lc_ctrl_pkg::On;
-  }
-  constraint scanmode_c {
-    scanmode == prim_mubi_pkg::MuBi4False;
-  }
-  constraint unavailable_c {
-    unavailable == 0;
-  }
 
   virtual task body();
     // If writes to DMI SBA registers triggers an access, then ensure the response is sent.
