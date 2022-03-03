6ced79cda0f85e505715ab708e1d10fb440050be
Maayan Kashani <maayan.kashani@nuvoton.com>
2022-03-03 09:29:25 +0200

    [sw/tests] Adding Meson infrastructure for integration of closed and open source chip level testing.
    Adding one flag called closed_source_dir to Meson
    options file.
    Added closed_source dir under sw/device/tests.
    Added weak hook functions in ottf.c file.
    Added example test called closed_entropy_src_smoketest that can
    overwritten the weak functions in ottf.c file with closed source code
    found under closed_source_dir path.
    Fixes #10852
    
    Signed-off-by: Maayan Kashani <maayan.kashani@nuvoton.com>

--- sw/device/lib/testing/test_framework/ottf.c
+++ sw/device/lib/testing/test_framework/ottf.c
@@ -8,6 +8,7 @@
 #include <stddef.h>
 
 #include "sw/device/lib/arch/device.h"
+#include "sw/device/lib/base/macros.h"
 #include "sw/device/lib/dif/dif_uart.h"
 #include "sw/device/lib/runtime/hart.h"
 #include "sw/device/lib/runtime/log.h"
@@ -54,11 +55,18 @@ static void report_test_status(bool result) {
   test_status_set(result ? kTestStatusPassed : kTestStatusFailed);
 }
 
+OT_WEAK bool closed_source_pre_test_hook(void) { return true; }
+
+OT_WEAK bool closed_source_post_test_hook(void) { return true; }
+
 // A wrapper function is required to enable `test_main()` and test teardown
 // logic to be invoked as a FreeRTOS task. This wrapper can be used by tests
 // that are run on bare-metal.
 static void test_wrapper(void *task_parameters) {
-  bool result = test_main();
+  // Invoke weak symbol that can be overriden in closed source code.
+  bool result = closed_source_pre_test_hook();
+
+  result = result && test_main() && closed_source_post_test_hook();
   report_test_status(result);
 }
 
