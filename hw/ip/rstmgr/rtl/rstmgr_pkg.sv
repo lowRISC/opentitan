2c42df4fb1d8c06cd4f49ba5b1fe6abdd5b40f7c
Guillermo Maturana <maturana@google.com>
2022-03-03 16:42:01 -0800

    [dv/rstmgr] Remove first_reset support in tests
    The RTL recently took out the rst_cpu_n input and associated
    functionality. Fix the reset test to match.
    
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- hw/ip/rstmgr/rtl/rstmgr_pkg.sv
+++ hw/ip/rstmgr/rtl/rstmgr_pkg.sv
@@ -58,13 +58,11 @@ package rstmgr_pkg;
 
   // cpu reset requests and status
   typedef struct packed {
-    logic rst_cpu_n;
     logic ndmreset_req;
   } rstmgr_cpu_t;
 
   // default value for rstmgr_ast_rsp_t (for dangling ports)
   parameter rstmgr_cpu_t RSTMGR_CPU_DEFAULT = '{
-    rst_cpu_n: 1'b1,
     ndmreset_req: '0
   };
 
