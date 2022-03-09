ccffc9806b894c0c6e865af12a5b8c71a1419ae3
Guillermo Maturana <maturana@google.com>
2022-03-08 17:58:35 -0800

    [dv/full_chip] Add clkmgr_external_clk_src_for_sw
    Fix chip external clock frequency: set it to 100 MHz by default, and add
    option to set it to either 100 or 48 MHz via "extclk_freq_mhz" plusargs.
    Create clkmgr_testutils_enable_clock_count_measurement testutils for
    this test.
    
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- sw/device/lib/testing/clkmgr_testutils.c
+++ sw/device/lib/testing/clkmgr_testutils.c
@@ -6,6 +6,9 @@
 
 #include "sw/device/lib/dif/dif_clkmgr.h"
 
+const char *measure_clock_names[kDifClkmgrMeasureClockUsb + 1] = {
+    "io_clk", "io_div2_clk", "io_div4_clk", "main_clk", "usb_clk"};
+
 // `extern` declarations to give the inline functions in the
 // corresponding header a link location.
 
@@ -15,3 +18,15 @@ extern bool clkmgr_testutils_get_trans_clock_status(
 extern void clkmgr_testutils_check_trans_clock_gating(
     dif_clkmgr_t *clkmgr, dif_clkmgr_hintable_clock_t clock,
     bool exp_clock_enabled, uint32_t timeout_usec);
+
+extern void clkmgr_testutils_enable_clock_count_measurement(
+    dif_clkmgr_t *clkmgr, dif_clkmgr_measure_clock_t clock,
+    uint32_t lo_threshold, uint32_t hi_threshold);
+
+void clkmgr_testutils_disable_clock_count_measurements(dif_clkmgr_t *clkmgr) {
+  LOG_INFO("Disabling all clock count measurements");
+  for (int i = 0; i <= kDifClkmgrMeasureClockUsb; ++i) {
+    dif_clkmgr_measure_clock_t clock = (dif_clkmgr_measure_clock_t)i;
+    CHECK_DIF_OK(dif_clkmgr_disable_measure_counts(clkmgr, clock));
+  }
+}
