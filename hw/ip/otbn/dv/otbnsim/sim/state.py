7c2486520585a8aad6d7c96ccde811540c206ed6
Rupert Swarbrick <rswarbrick@lowrisc.org>
2022-03-10 14:32:44 +0000

    [otbn,dv] Still print final instruction on escalation when running
    We run on_lc_escalation() earlier in the cycle than step(), but we
    want to make sure that we still generate a trace item for executing an
    instruction on that cycle (even though it will be cancelled).
    
    This commit adds explicit "pending error bits" to the state to make
    that possible.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/otbn/dv/otbnsim/sim/state.py
+++ hw/ip/otbn/dv/otbnsim/sim/state.py
@@ -125,6 +125,14 @@ class OTBNState:
         # number. Initialise to -1 to catch bugs if we forget to set it.
         self.wipe_cycles = -1
 
+        # If this is nonzero, there's been an error injected from outside. The
+        # Sim.step function will OR these bits into err_bits and stop at the
+        # end of the next cycle. (We don't just call stop_at_end_of_cycle
+        # because this runs earlier in the cycle than step(), which would mean
+        # we wouldn't see the final instruction get executed and then
+        # cancelled).
+        self.injected_err_bits = 0
+
     def get_next_pc(self) -> int:
         if self._pc_next_override is not None:
             return self._pc_next_override
@@ -517,3 +525,9 @@ class OTBNState:
         self.wdrs.wipe()
         self.wsrs.wipe()
         self.csrs.wipe()
+
+    def take_injected_err_bits(self) -> None:
+        '''Apply any injected errors, stopping at the end of the cycle'''
+        if self.injected_err_bits != 0:
+            self.stop_at_end_of_cycle(self.injected_err_bits)
+            self.injected_err_bits = 0
