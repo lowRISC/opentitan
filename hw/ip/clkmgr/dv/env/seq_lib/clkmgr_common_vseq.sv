3bc40dd0833a13fac615683f5de877fa3149fcb9
Guillermo Maturana <maturana@google.com>
2022-03-14 09:50:54 -0700

    [dv/clkmgr] Populate sec_cm testplan
    Add existing and auto-generated tests to sec_cm testplan.
    
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- hw/ip/clkmgr/dv/env/seq_lib/clkmgr_common_vseq.sv
+++ hw/ip/clkmgr/dv/env/seq_lib/clkmgr_common_vseq.sv
@@ -8,6 +8,19 @@ class clkmgr_common_vseq extends clkmgr_base_vseq;
   constraint num_trans_c {num_trans inside {[1 : 2]};}
   `uvm_object_new
 
+  virtual task check_sec_cm_fi_resp(sec_cm_base_if_proxy if_proxy);
+    super.check_sec_cm_fi_resp(if_proxy);
+
+    case (if_proxy.sec_cm_type)
+      SecCmPrimCount: begin
+        csr_rd_check(.ptr(ral.fatal_err_code.idle_cnt), .compare_value(1));
+      end
+      default: begin
+        `uvm_error(`gfn, $sformatf("Unexpected sec_cm_type %0s", if_proxy.sec_cm_type.name))
+      end
+    endcase
+  endtask
+
   virtual task body();
     run_common_vseq_wrapper(num_trans);
   endtask : body
