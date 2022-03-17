9aa5b2c37a4a150e1930b37a256e36a9c977ee42
Cindy Chen <chencindy@opentitan.org>
2022-03-17 15:03:40 -0700

    [dv/otp] Add mubi cov for V2S
    This PR binds mubi coverage for V2S requirements.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

--- hw/ip/otp_ctrl/dv/cov/otp_ctrl_cov_bind.sv
+++ hw/ip/otp_ctrl/dv/cov/otp_ctrl_cov_bind.sv
@@ -14,4 +14,28 @@ module otp_ctrl_cov_bind;
     .otbn_otp_key_i   (otbn_otp_key_i)
   );
 
+  bind otp_ctrl cip_mubi_cov_if #(.Width(4)) u_lc_creator_seed_sw_rw_en_mubi_cov_if (
+    .rst_ni (rst_ni),
+    .mubi   (lc_creator_seed_sw_rw_en_i)
+  );
+
+  bind otp_ctrl cip_mubi_cov_if #(.Width(4)) u_lc_seed_hw_rd_en_mubi_cov_if (
+    .rst_ni (rst_ni),
+    .mubi   (lc_seed_hw_rd_en_i)
+  );
+
+  bind otp_ctrl cip_mubi_cov_if #(.Width(4)) u_lc_dft_en_mubi_cov_if (
+    .rst_ni (rst_ni),
+    .mubi   (lc_dft_en_i)
+  );
+
+  bind otp_ctrl cip_mubi_cov_if #(.Width(4)) u_lc_escalate_en_mubi_cov_if (
+    .rst_ni (rst_ni),
+    .mubi   (lc_escalate_en_i)
+  );
+
+  bind otp_ctrl cip_mubi_cov_if #(.Width(4)) u_lc_check_byp_en_mubi_cov_if (
+    .rst_ni (rst_ni),
+    .mubi   (lc_check_byp_en_i)
+  );
 endmodule
