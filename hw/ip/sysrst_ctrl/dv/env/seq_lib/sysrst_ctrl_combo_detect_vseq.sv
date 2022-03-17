2815497e2f02d1883d6df8e3a589301e108e66b8
Madhuri Patel <madhuri.patel@ensilica.com>
2022-03-17 21:45:41 +0000

    [sysrst_ctrl,dv] Add remaining set of combo detect registers
    Signed-off-by: Madhuri Patel <madhuri.patel@ensilica.com>

--- hw/ip/sysrst_ctrl/dv/env/seq_lib/sysrst_ctrl_combo_detect_vseq.sv
+++ hw/ip/sysrst_ctrl/dv/env/seq_lib/sysrst_ctrl_combo_detect_vseq.sv
@@ -8,15 +8,28 @@ class sysrst_ctrl_combo_detect_vseq extends sysrst_ctrl_base_vseq;
   `uvm_object_utils(sysrst_ctrl_combo_detect_vseq)
 
   `uvm_object_new
-  rand uvm_reg_data_t set_action;
-  rand bit[4:0] trigger_combo;
-  rand uint16_t set_duration, set_pulse_width, set_key_timer;
+  rand uvm_reg_data_t set_action[4];
+  rand bit[4:0] trigger_combo[4];
+  rand uint16_t set_duration[4];
+  rand uint16_t set_pulse_width, set_key_timer;
   rand uint16_t cycles;
 
-  constraint num_trans_c {num_trans == 3;}
+  constraint num_trans_c {num_trans == 2;}
 
   constraint set_duration_c {
-    set_duration dist {
+    set_duration[0] dist {
+      [10:100] :/ 95,
+      [101:$]   :/ 5
+    };
+    set_duration[1] dist {
+      [10:100] :/ 95,
+      [101:$]   :/ 5
+    };
+    set_duration[2] dist {
+      [10:100] :/ 95,
+      [101:$]   :/ 5
+    };
+    set_duration[3] dist {
       [10:100] :/ 95,
       [101:$]   :/ 5
     };
@@ -38,19 +51,31 @@ class sysrst_ctrl_combo_detect_vseq extends sysrst_ctrl_base_vseq;
 
   constraint cycles_c {
     cycles dist {
-      [(set_duration + set_key_timer) - 10 : (set_duration + set_key_timer)] :/ 5,
-      [(set_duration + set_key_timer) : (set_duration + set_key_timer) + 3]   :/ 95
+      [1 : (set_duration[0] + set_key_timer) - 2] :/ 20,
+      [(set_duration[0] + set_key_timer) + 5 : (set_duration[0] + set_key_timer) * 2]   :/ 80
+    };
+    cycles dist {
+      [1 : (set_duration[1] + set_key_timer) - 2] :/ 20,
+      [(set_duration[1] + set_key_timer) + 5 : (set_duration[1] + set_key_timer) * 2]   :/ 80
+    };
+    cycles dist {
+      [1 : (set_duration[2] + set_key_timer) - 2] :/ 20,
+      [(set_duration[2] + set_key_timer) + 5 : (set_duration[2] + set_key_timer) * 2]   :/ 80
+    };
+    cycles dist {
+      [1 : (set_duration[3] + set_key_timer) - 2] :/ 20,
+      [(set_duration[3] + set_key_timer) + 5 : (set_duration[3] + set_key_timer) * 2]   :/ 80
     };
   }
 
-  function bit get_combo_trigger();
-    logic [4:0] in;
-    in[0] = cfg.vif.key0_in;
-    in[1] = cfg.vif.key1_in;
-    in[2] = cfg.vif.key2_in;
-    in[3] = cfg.vif.pwrb_in;
-    in[4] = cfg.vif.ac_present;
-    return  ((in & trigger_combo) == 0) && (trigger_combo != 0);
+  function bit get_combo_trigger(int index);
+      logic [4:0] in;
+      in[0] = cfg.vif.key0_in;
+      in[1] = cfg.vif.key1_in;
+      in[2] = cfg.vif.key2_in;
+      in[3] = cfg.vif.pwrb_in;
+      in[4] = cfg.vif.ac_present;
+      return  ((in & trigger_combo[index]) == 0) && (trigger_combo[index] != 0);
   endfunction
 
   task check_ec_rst_inactive(int max_cycle);
@@ -59,37 +84,34 @@ class sysrst_ctrl_combo_detect_vseq extends sysrst_ctrl_base_vseq;
   endtask
 
   task config_register();
-    // TODO: Add other set of combo registers
-    // Enable input keys to trigger the combo
-    csr_wr(ral.com_sel_ctl[0], trigger_combo);
-    `uvm_info(`gfn, $sformatf("Write data of com_sel_ctl_0 register:0x%0h",
-                               trigger_combo), UVM_LOW)
+    // Select the inputs for the combo
+    foreach (ral.com_sel_ctl[i]) csr_wr(ral.com_sel_ctl[i], trigger_combo[i]);
 
     // Set the duration for combo to pressed
-    csr_wr(ral.com_det_ctl[0], set_duration);
-    `uvm_info(`gfn, $sformatf("Write data of com_det_ctl_0 register:0x%0h",
-                               set_duration), UVM_LOW);
+    foreach (ral.com_det_ctl[i]) csr_wr(ral.com_det_ctl[i], set_duration[i]);
 
     // Set the trigger action
-    csr_wr(ral.com_out_ctl[0], set_action);
-    `uvm_info(`gfn, $sformatf("Write data of com_out_ctl_0 register:0x%0h",
-                               set_action), UVM_LOW);
+    foreach (ral.com_out_ctl[i]) csr_wr(ral.com_out_ctl[i], set_action[i]);
 
     // Set the ec_rst_0 pulse width
     csr_wr(ral.ec_rst_ctl, set_pulse_width);
     `uvm_info(`gfn, $sformatf("Write data of ec_rst_ctl register:0x%0h",
-                               set_pulse_width), UVM_LOW);
+                                 set_pulse_width), UVM_LOW);
 
     // Set the key triggered debounce timer
     csr_wr(ral.key_intr_debounce_ctl, set_key_timer);
     `uvm_info(`gfn, $sformatf("Write data of key_intr_debounce_ctl register:0x%0h",
-                               set_key_timer), UVM_LOW);
+                                 set_key_timer), UVM_LOW);
   endtask
 
   task body();
     uvm_reg_data_t rdata;
-    bit int_act_set, bat_act_set, ec_act_set, rst_act_set;
-    bit triggered;
+    bit [3:0] int_act_set;
+    bit [3:0] bat_act_set;
+    bit [3:0] ec_act_set;
+    bit [3:0] rst_act_set;
+    bit triggered[4];
+    uint16_t [3:0] set_duration;
 
     // Enable interrupt
     ral.intr_enable.set(1);
@@ -102,6 +124,7 @@ class sysrst_ctrl_combo_detect_vseq extends sysrst_ctrl_base_vseq;
     csr_update(ral.pin_out_ctl);
 
     config_register();
+
     monitor_ec_rst_low(set_pulse_width);
 
     // It takes 2-3 clock cycles to sync the register values
@@ -115,10 +138,11 @@ class sysrst_ctrl_combo_detect_vseq extends sysrst_ctrl_base_vseq;
       end
 
       // Wait for debounce + detect timer
-      cfg.clk_aon_rst_vif.wait_clks(cycles+2);
+      cfg.clk_aon_rst_vif.wait_clks(cycles);
 
       // Latch the trigger value before resetting the input pins
-      triggered = get_combo_trigger();
+      foreach (triggered[i]) triggered[i] = get_combo_trigger(i);
+
       // Set the inputs back to inactive to avoid its affect in next iterations
       cfg.vif.pwrb_in = 1;
       cfg.vif.key0_in = 1;
@@ -127,72 +151,76 @@ class sysrst_ctrl_combo_detect_vseq extends sysrst_ctrl_base_vseq;
       cfg.vif.ac_present = 1;
 
       `uvm_info(`gfn, $sformatf("Value of cycles:%0h", cycles), UVM_LOW)
-
-      if (cycles < (set_duration + set_key_timer) || !triggered) begin
-        // Check there is no interrupt if we wait for
-        // cycles < debounce time + detect time
-        cfg.clk_aon_rst_vif.wait_clks(1);
-        check_interrupts(.interrupts(1 << IntrSysrstCtrl), .check_set(0));
-        `DV_CHECK_EQ(cfg.vif.bat_disable, 0);
-        `DV_CHECK_EQ(cfg.vif.sysrst_ctrl_rst_req, 0);
-        check_ec_rst_inactive(set_pulse_width);
-        `DV_CHECK_EQ(cfg.vif.ec_rst_l_out, 1);
-      end else begin
-        // Check if the interrupt has raised
-        // NOTE: The interrupt will only raise if the interrupt
-        // combo action is set
-        int_act_set = get_field_val(ral.com_out_ctl[0].interrupt, set_action);
-        bat_act_set = get_field_val(ral.com_out_ctl[0].bat_disable, set_action);
-        ec_act_set = get_field_val(ral.com_out_ctl[0].ec_rst, set_action);
-        rst_act_set = get_field_val(ral.com_out_ctl[0].rst_req, set_action);
-
-        cfg.clk_aon_rst_vif.wait_clks(1);
-        if (int_act_set == 1) begin
-          check_interrupts(.interrupts(1 << IntrSysrstCtrl), .check_set(1));
-
-          // Write to clear the interrupt
-          csr_wr(ral.combo_intr_status, 'h1);
-
-          cfg.clk_aon_rst_vif.wait_clks(20);
-          // check if the interrupt is cleared
-          csr_rd_check(ral.combo_intr_status, .compare_value(0));
-        end else begin
+      for (int i = 0; i <= 3; i++) begin
+        if (cycles < (set_duration[i] + set_key_timer) || !triggered[i]) begin
+          // Check there is no interrupt if we wait for
+          // cycles < debounce time + detect time
+          cfg.clk_aon_rst_vif.wait_clks(1);
           check_interrupts(.interrupts(1 << IntrSysrstCtrl), .check_set(0));
-        end
-
-        // If bat_disable trigger action is set then check if there is a transition
-        // on cio_bat_disable_o signal
-        if (bat_act_set == 1) begin
-          `DV_CHECK_EQ(cfg.vif.bat_disable, 1);
-        end else begin
           `DV_CHECK_EQ(cfg.vif.bat_disable, 0);
-        end
-
-        // If reset req trigger action is set then check if there is a event
-        // on aon_sysrst_ctrl_rst_req_o pin
-        if (rst_act_set == 1) begin
-          `DV_CHECK_EQ(cfg.vif.sysrst_ctrl_rst_req, 1);
-        end else begin
           `DV_CHECK_EQ(cfg.vif.sysrst_ctrl_rst_req, 0);
-        end
-
-        if (ec_act_set == 1) begin
-          monitor_ec_rst_low(set_pulse_width);
-        end else begin
           check_ec_rst_inactive(set_pulse_width);
-          `DV_CHECK_EQ(cfg.vif.ec_rst_l_out, 0);
-        end
+          `DV_CHECK_EQ(cfg.vif.ec_rst_l_out, 1);
+        end else begin
+          // Check if the interrupt has raised
+          // NOTE: The interrupt will only raise if the interrupt
+          // combo action is set
+          int_act_set[i] = get_field_val(ral.com_out_ctl[i].interrupt, set_action[i]);
+          bat_act_set[i] = get_field_val(ral.com_out_ctl[i].bat_disable, set_action[i]);
+          ec_act_set[i] = get_field_val(ral.com_out_ctl[i].ec_rst, set_action[i]);
+          rst_act_set[i] = get_field_val(ral.com_out_ctl[i].rst_req, set_action[i]);
 
-        if (bat_act_set == 1 || rst_act_set == 1) begin
-         cfg.clk_aon_rst_vif.apply_reset();
-         // apply_reset will set the registers to their default values
-         // wait for sometime and reconfigure the registers for next iteration
-         cfg.clk_aon_rst_vif.wait_clks(10);
-         config_register();
+          cfg.clk_aon_rst_vif.wait_clks(1);
+          if (int_act_set[i]) begin
+            check_interrupts(.interrupts(1 << IntrSysrstCtrl), .check_set(1));
+
+            // Read the status register
+            csr_rd(ral.combo_intr_status, rdata);
+            `DV_CHECK_EQ(rdata[i], 'h1)
+
+            // Write to clear the interrupt
+            csr_wr(ral.combo_intr_status, rdata);
+
+            cfg.clk_aon_rst_vif.wait_clks(20);
+            // check if the interrupt is cleared
+            csr_rd_check(ral.combo_intr_status, .compare_value(0));
+          end else begin
+            check_interrupts(.interrupts(1 << IntrSysrstCtrl), .check_set(0));
+          end
+
+          // If bat_disable trigger action is set then check if there is a transition
+          // on cio_bat_disable_o signal
+          if (bat_act_set[i]) begin
+            `DV_CHECK_EQ(cfg.vif.bat_disable, 1);
+          end else begin
+            `DV_CHECK_EQ(cfg.vif.bat_disable, 0);
+          end
+
+          // If reset req trigger action is set then check if there is a event
+          // on aon_sysrst_ctrl_rst_req_o pin
+          if (rst_act_set[i]) begin
+           `DV_CHECK_EQ(cfg.vif.sysrst_ctrl_rst_req, 1);
+          end else begin
+            `DV_CHECK_EQ(cfg.vif.sysrst_ctrl_rst_req, 0);
+          end
+
+          if (ec_act_set[i]) begin
+            monitor_ec_rst_low(set_pulse_width);
+          end else begin
+            check_ec_rst_inactive(set_pulse_width);
+            `DV_CHECK_EQ(cfg.vif.ec_rst_l_out, 1);
+          end
+
+          if (bat_act_set[i] == 1 || rst_act_set[i] == 1) begin
+           cfg.clk_aon_rst_vif.apply_reset();
+           // apply_reset will set the registers to their default values
+           // wait for sometime and reconfigure the registers for next iteration
+           config_register();
+          end
+          cfg.clk_aon_rst_vif.wait_clks(10);
         end
-        cfg.clk_aon_rst_vif.wait_clks(10);
       end
-    end
+     end
   endtask : body
 
 endclass : sysrst_ctrl_combo_detect_vseq
