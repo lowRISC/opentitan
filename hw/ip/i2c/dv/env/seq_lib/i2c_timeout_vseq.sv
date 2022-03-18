26cd31c428fd84cc23e5f838233d6c135520dcf2
Viswanadha Bazawada <viswanadha.bazawada@ensilica.com>
2022-03-18 09:46:26 +0000

    [i2c/dv] Test Implementation I2C Reset Fmt Rx Fifo and Timeout
    - Added I2C Fifo Reset Test Fmt Rx
    - Added I2C Timeout Test
    - Added sequences i2c_fifo_reset_rx_vseq i2c_fifo_reset_fmt_vseq
    - Added sequence i2c_timeout_vseq
    
    Signed-off-by: Viswanadha Bazawada <viswanadha.bazawada@ensilica.com>

---
+++ hw/ip/i2c/dv/env/seq_lib/i2c_timeout_vseq.sv
@@ -0,0 +1,21 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// timeout test vseq
+// this sequence sets timeout enable and value in timeout control reg
+// runs random i2c transactions agent is configured to pull down clock
+// host will trigger an interrupt stretch timeout when count exceeds the programmed
+// value in timeout control reg
+class i2c_timeout_vseq extends i2c_rx_tx_vseq;
+  `uvm_object_utils(i2c_timeout_vseq)
+  `uvm_object_new
+
+  // increase num_trans to cover all transaction types
+  constraint num_trans_c { num_trans inside {[50 : 100]}; }
+
+  // constraints for i2c timing registers
+  constraint t_timeout_c { t_timeout == cfg.seq_cfg.i2c_max_timing; }
+  constraint e_timeout_c { e_timeout == 1'b1; }
+
+endclass : i2c_timeout_vseq
