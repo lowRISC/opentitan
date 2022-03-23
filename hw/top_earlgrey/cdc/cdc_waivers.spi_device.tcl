750f0a46e0721ad3a015653173a4f08007a01092
Eunchan Kim <eunchan@opentitan.org>
2022-03-23 11:03:17 -0700

    [spi_device] Waiving CDC violations through prim_flop_2sync
    Adding W_CNTL waivers that are passing through prim_flop_2sync.
    
    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>

--- hw/top_earlgrey/cdc/cdc_waivers.spi_device.tcl
+++ hw/top_earlgrey/cdc/cdc_waivers.spi_device.tcl
@@ -12,3 +12,14 @@ set_rule_status -rule {W_RECON_GROUPS} -status {Waived}         \
    -expression {(ControlSignal=~"*u_status_23_to_1_sync.*") &&       \
                 (ReconSignal=~"*u_spid_status.outclk_p2s_byte_*")} \
    -comment {status bits are converged into SPI line. But transmit bit-by-bit. OK to send old and new status bits in a transaction}
+
+set_rule_status -rule {W_CNTL} -status {Waived}   \
+   -expression {(Signal=~"*u_spi_device.rxf_full_q*") && \
+    (ReceivingFlop=~"*u_spi_device.u_sync_rxf*")} \
+   -comment {2FF synchronizer covers the clock domain crossing. RECONV is not waived.}
+
+# TODO: Create more specific Flop instance inside prim_flop_2sync to waive broadly.
+set_rule_status -rule {W_CNTL} -status {Waived} \
+   -expression {(Signal=~"*u_spi_device*_*ptr_gray_q*") && \
+    (ReceivingFlop=~"*u_spi_device*u_sync_1.*")} \
+   -comment {2FF sync for gray code.}
