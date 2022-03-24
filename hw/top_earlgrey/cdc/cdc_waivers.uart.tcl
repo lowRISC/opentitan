2e15ca4c0855eafd2804e334d7b0ac28db780b85
Eunchan Kim <eunchan@opentitan.org>
2022-03-24 15:35:41 -0700

    [uart] Add UART Waiver
    Add W_DATA waiver for the path from tx_out to PINMUX
    
    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>

---
+++ hw/top_earlgrey/cdc/cdc_waivers.uart.tcl
@@ -0,0 +1,14 @@
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Verix CDC waiver file
+# Expression:
+#  ControlSignal==""
+#  ReconSignal==""
+#  MultiClockDomains=="IO_DIV2_CLK::IO_DIV4_CLK"
+
+# PATH violation to PINMUX
+set_rule_status -status {Waived} -rule {W_DATA W_MASYNC} \
+  -expression {(Signal=~"*u_uart_core.tx_out_q*") && (ReceivingFlop=~"IO*")} \
+  -comment {Pinmux is clock invariant}
