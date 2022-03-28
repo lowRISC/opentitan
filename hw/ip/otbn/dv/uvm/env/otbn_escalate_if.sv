08135615d56a92dd13364f68536c013b0ea348f8
Prajwala Puttappa <prajwalaputtappa@lowrisc.org>
2022-03-28 12:47:35 +0100

    [otbn, dv] Generalises otbn_escalate test to allow invalid LC signals
    This commit adds a new function randomize_after_n_clocks in otbn_escalate_if to allow
    setting enable pin to invalid value. This function replaces set_after_n_clocks in
    otbn_escalate_vseq.
    
    Signed-off-by: Prajwala Puttappa <prajwalaputtappa@lowrisc.org>

--- hw/ip/otbn/dv/uvm/env/otbn_escalate_if.sv
+++ hw/ip/otbn/dv/uvm/env/otbn_escalate_if.sv
@@ -15,15 +15,21 @@ interface otbn_escalate_if (
     enable = lc_ctrl_pkg::Off;
   endfunction
 
-  task automatic set_after_n_clocks(int unsigned n);
+  task automatic randomize_after_n_clocks(int unsigned n,
+                                          int t_weight = 2,
+                                          int f_weight = 2,
+                                          int other_weight = 1);
     `DV_SPINWAIT_EXIT(
       begin
         repeat (n) @(posedge clk_i);
-        enable = lc_ctrl_pkg::On;
+          enable = cip_base_pkg::get_rand_lc_tx_val(.t_weight(t_weight),
+                                                    .f_weight(f_weight),
+                                                    .other_weight(other_weight));
       end,
       @(negedge rst_ni);,
       "Not setting enable signal because we've gone into reset",
       "otbn_escalate_if")
   endtask
+
 
 endinterface
