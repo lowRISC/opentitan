ce1e77f88498dd4c0c128cb069cc1317e382245e
Michael Schaffner <msf@opentitan.org>
2022-03-30 10:15:26 -0700

    [rv_dm] Use instantiate dv_dm directly to deduplicate code
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/rv_dm/rtl/rv_dm.sv
+++ hw/ip/rv_dm/rtl/rv_dm.sv
@@ -51,18 +51,40 @@ module rv_dm
   output jtag_pkg::jtag_rsp_t jtag_o
 );
 
+  ///////////////////////////
+  // Parameter Definitions //
+  ///////////////////////////
+
   import prim_mubi_pkg::mubi4_bool_to_mubi;
   import prim_mubi_pkg::mubi4_test_true_strict;
   import lc_ctrl_pkg::lc_tx_test_true_strict;
 
   `ASSERT_INIT(paramCheckNrHarts, NrHarts > 0)
 
+  // static debug hartinfo
+  localparam dm::hartinfo_t DebugHartInfo = '{
+    zero1:      '0,
+    nscratch:   2, // Debug module needs at least two scratch regs
+    zero0:      0,
+    dataaccess: 1'b1, // data registers are memory mapped in the debugger
+    datasize:   dm::DataCount,
+    dataaddr:   dm::DataAddr
+  };
+
+  dm::hartinfo_t [NrHarts-1:0]      hartinfo;
+  for (genvar i = 0; i < NrHarts; i++) begin : gen_dm_hart_ctrl
+    assign hartinfo[i] = DebugHartInfo;
+  end
+
   // Currently only 32 bit busses are supported by our TL-UL IP
   localparam int BusWidth = 32;
   // all harts have contiguous IDs
   localparam logic [NrHarts-1:0] SelectableHarts = {NrHarts{1'b1}};
 
-  // CSR Nodes
+  ///////////////
+  // CSR Nodes //
+  ///////////////
+
   tlul_pkg::tl_h2d_t rom_tl_win_h2d;
   tlul_pkg::tl_d2h_t rom_tl_win_d2h;
   rv_dm_reg_pkg::rv_dm_regs_reg2hw_t regs_reg2hw;
@@ -116,6 +138,14 @@ module rv_dm
     );
   end
 
+  // Decode multibit scanmode enable
+  logic testmode;
+  assign testmode = mubi4_test_true_strict(scanmode_i);
+
+  ///////////////////////
+  // Life Cycle Gating //
+  ///////////////////////
+
   // debug enable gating
   typedef enum logic [3:0] {
     EnFetch,
@@ -139,65 +169,11 @@ module rv_dm
     .lc_en_o(lc_hw_debug_en)
   );
 
-
-  // Debug CSRs
-  dm::hartinfo_t [NrHarts-1:0]      hartinfo;
-  logic [NrHarts-1:0]               halted;
-  // logic [NrHarts-1:0]               running;
-  logic [NrHarts-1:0]               resumeack;
-  logic [NrHarts-1:0]               haltreq;
-  logic [NrHarts-1:0]               resumereq;
-  logic                             clear_resumeack;
-  logic                             cmd_valid;
-  dm::command_t                     cmd;
-
-  logic                             cmderror_valid;
-  dm::cmderr_e                      cmderror;
-  logic                             cmdbusy;
-  logic [dm::ProgBufSize-1:0][31:0] progbuf;
-  logic [dm::DataCount-1:0][31:0]   data_csrs_mem;
-  logic [dm::DataCount-1:0][31:0]   data_mem_csrs;
-  logic                             data_valid;
-  logic [19:0]                      hartsel;
-  // System Bus Access Module
-  logic [BusWidth-1:0]              sbaddress_csrs_sba;
-  logic [BusWidth-1:0]              sbaddress_sba_csrs;
-  logic                             sbaddress_write_valid;
-  logic                             sbreadonaddr;
-  logic                             sbautoincrement;
-  logic [2:0]                       sbaccess;
-  logic                             sbreadondata;
-  logic [BusWidth-1:0]              sbdata_write;
-  logic                             sbdata_read_valid;
-  logic                             sbdata_write_valid;
-  logic [BusWidth-1:0]              sbdata_read;
-  logic                             sbdata_valid;
-  logic                             sbbusy;
-  logic                             sberror_valid;
-  logic [2:0]                       sberror;
-
   dm::dmi_req_t  dmi_req;
   dm::dmi_resp_t dmi_rsp;
   logic dmi_req_valid, dmi_req_ready;
   logic dmi_rsp_valid, dmi_rsp_ready;
   logic dmi_rst_n;
-  logic testmode;
-
-  // Decode multibit scanmode enable
-  assign testmode = mubi4_test_true_strict(scanmode_i);
-
-  // static debug hartinfo
-  localparam dm::hartinfo_t DebugHartInfo = '{
-    zero1:      '0,
-    nscratch:   2, // Debug module needs at least two scratch regs
-    zero0:      0,
-    dataaccess: 1'b1, // data registers are memory mapped in the debugger
-    datasize:   dm::DataCount,
-    dataaddr:   dm::DataAddr
-  };
-  for (genvar i = 0; i < NrHarts; i++) begin : gen_dm_hart_ctrl
-    assign hartinfo[i] = DebugHartInfo;
-  end
 
   logic reset_req_en;
   logic ndmreset_req;
@@ -209,56 +185,9 @@ module rv_dm
   // SEC_CM: DM_EN.CTRL.LC_GATED
   assign dmi_en = lc_tx_test_true_strict(lc_hw_debug_en[EnDmiReq]);
 
-  dm_csrs #(
-    .NrHarts(NrHarts),
-    .BusWidth(BusWidth),
-    .SelectableHarts(SelectableHarts)
-  ) i_dm_csrs (
-    .clk_i                   ( clk_i                 ),
-    .rst_ni                  ( rst_ni                ),
-    .testmode_i              ( testmode              ),
-    .dmi_rst_ni              ( dmi_rst_n             ),
-    .dmi_req_valid_i         ( dmi_req_valid & dmi_en),
-    .dmi_req_ready_o         ( dmi_req_ready         ),
-    .dmi_req_i               ( dmi_req               ),
-    .dmi_resp_valid_o        ( dmi_rsp_valid         ),
-    .dmi_resp_ready_i        ( dmi_rsp_ready & dmi_en),
-    .dmi_resp_o              ( dmi_rsp               ),
-    .ndmreset_o              ( ndmreset_req          ),
-    .dmactive_o              ( dmactive_o            ),
-    .hartsel_o               ( hartsel               ),
-    .hartinfo_i              ( hartinfo              ),
-    .halted_i                ( halted                ),
-    .unavailable_i,
-    .resumeack_i             ( resumeack             ),
-    .haltreq_o               ( haltreq               ),
-    .resumereq_o             ( resumereq             ),
-    .clear_resumeack_o       ( clear_resumeack       ),
-    .cmd_valid_o             ( cmd_valid             ),
-    .cmd_o                   ( cmd                   ),
-    .cmderror_valid_i        ( cmderror_valid        ),
-    .cmderror_i              ( cmderror              ),
-    .cmdbusy_i               ( cmdbusy               ),
-    .progbuf_o               ( progbuf               ),
-    .data_i                  ( data_mem_csrs         ),
-    .data_valid_i            ( data_valid            ),
-    .data_o                  ( data_csrs_mem         ),
-    .sbaddress_o             ( sbaddress_csrs_sba    ),
-    .sbaddress_i             ( sbaddress_sba_csrs    ),
-    .sbaddress_write_valid_o ( sbaddress_write_valid ),
-    .sbreadonaddr_o          ( sbreadonaddr          ),
-    .sbautoincrement_o       ( sbautoincrement       ),
-    .sbaccess_o              ( sbaccess              ),
-    .sbreadondata_o          ( sbreadondata          ),
-    .sbdata_o                ( sbdata_write          ),
-    .sbdata_read_valid_o     ( sbdata_read_valid     ),
-    .sbdata_write_valid_o    ( sbdata_write_valid    ),
-    .sbdata_i                ( sbdata_read           ),
-    .sbdata_valid_i          ( sbdata_valid          ),
-    .sbbusy_i                ( sbbusy                ),
-    .sberror_valid_i         ( sberror_valid         ),
-    .sberror_i               ( sberror               )
-  );
+  /////////////////////////////////////////
+  // System Bus Access Port (TL-UL Host) //
+  /////////////////////////////////////////
 
   logic                   host_req;
   logic   [BusWidth-1:0]  host_add;
@@ -270,39 +199,6 @@ module rv_dm
   logic   [BusWidth-1:0]  host_r_rdata;
   logic                   host_r_err;
   logic                   host_r_other_err;
-
-  dm_sba #(
-    .BusWidth(BusWidth)
-  ) i_dm_sba (
-    .clk_i                   ( clk_i                 ),
-    .rst_ni                  ( rst_ni                ),
-    .master_req_o            ( host_req              ),
-    .master_add_o            ( host_add              ),
-    .master_we_o             ( host_we               ),
-    .master_wdata_o          ( host_wdata            ),
-    .master_be_o             ( host_be               ),
-    .master_gnt_i            ( host_gnt              ),
-    .master_r_valid_i        ( host_r_valid          ),
-    .master_r_err_i          ( host_r_err            ),
-    .master_r_other_err_i    ( host_r_other_err      ),
-    .master_r_rdata_i        ( host_r_rdata          ),
-    .dmactive_i              ( dmactive_o            ),
-    .sbaddress_i             ( sbaddress_csrs_sba    ),
-    .sbaddress_o             ( sbaddress_sba_csrs    ),
-    .sbaddress_write_valid_i ( sbaddress_write_valid ),
-    .sbreadonaddr_i          ( sbreadonaddr          ),
-    .sbautoincrement_i       ( sbautoincrement       ),
-    .sbaccess_i              ( sbaccess              ),
-    .sbreadondata_i          ( sbreadondata          ),
-    .sbdata_i                ( sbdata_write          ),
-    .sbdata_read_valid_i     ( sbdata_read_valid     ),
-    .sbdata_write_valid_i    ( sbdata_write_valid    ),
-    .sbdata_o                ( sbdata_read           ),
-    .sbdata_valid_o          ( sbdata_valid          ),
-    .sbbusy_o                ( sbbusy                ),
-    .sberror_valid_o         ( sberror_valid         ),
-    .sberror_o               ( sberror               )
-  );
 
   // SEC_CM: DM_EN.CTRL.LC_GATED
   tlul_pkg::tl_h2d_t  sba_tl_h_o_int;
@@ -349,69 +245,35 @@ module rv_dm
     .tl_i         (sba_tl_h_i_int)
   );
 
+  //////////////////////////////////////
+  // Debug Memory Port (TL-UL Device) //
+  //////////////////////////////////////
+
   localparam int unsigned AddressWidthWords = BusWidth - $clog2(BusWidth/8);
 
-  logic                         req;
-  logic                         we;
-  logic [BusWidth/8-1:0]        be;
-  logic   [BusWidth-1:0]        wmask;
-  logic   [BusWidth-1:0]        wdata;
-  logic   [BusWidth-1:0]        rdata;
-  logic                         rvalid;
+  logic                         device_req;
+  logic                         device_we;
+  logic [BusWidth/8-1:0]        device_be;
+  logic   [BusWidth-1:0]        device_wmask;
+  logic   [BusWidth-1:0]        device_wdata;
+  logic   [BusWidth-1:0]        device_rdata;
+  logic                         device_rvalid;
 
-  logic [BusWidth-1:0]          addr_b;
-  logic [AddressWidthWords-1:0] addr_w;
+  logic [BusWidth-1:0]          device_addr_b;
+  logic [AddressWidthWords-1:0] device_addr_w;
 
   // Bit-write masks are byte-aligned, so we can reduce them to byte-write enables here.
   for (genvar k = 0; k < BusWidth/8; k++) begin : gen_byte_write
-    assign be[k] = &wmask[8*k +: 8];
+    assign device_be[k] = &device_wmask[8*k +: 8];
   end
 
-  assign addr_b = {addr_w, {$clog2(BusWidth/8){1'b0}}};
+  assign device_addr_b = {device_addr_w, {$clog2(BusWidth/8){1'b0}}};
 
   logic debug_req_en;
   logic debug_req;
   // SEC_CM: DM_EN.CTRL.LC_GATED
   assign debug_req_en = lc_tx_test_true_strict(lc_hw_debug_en[EnDebugReq]);
   assign debug_req_o = debug_req & debug_req_en;
-
-
-  dm_mem #(
-    .NrHarts(NrHarts),
-    .BusWidth(BusWidth),
-    .SelectableHarts(SelectableHarts),
-    // The debug module provides a simplified ROM for systems that map the debug ROM to offset 0x0
-    // on the system bus. In that case, only one scratch register has to be implemented in the core.
-    // However, we require that the DM can be placed at arbitrary offsets in the system, which
-    // requires the generalized debug ROM implementation and two scratch registers. We hence set
-    // this parameter to a non-zero value (inside dm_mem, this just feeds into a comparison with 0).
-    .DmBaseAddress(1)
-  ) i_dm_mem (
-    .clk_i                   ( clk_i                 ),
-    .rst_ni                  ( rst_ni                ),
-    .debug_req_o             ( debug_req             ),
-    .hartsel_i               ( hartsel               ),
-    .haltreq_i               ( haltreq               ),
-    .resumereq_i             ( resumereq             ),
-    .clear_resumeack_i       ( clear_resumeack       ),
-    .halted_o                ( halted                ),
-    .resuming_o              ( resumeack             ),
-    .cmd_valid_i             ( cmd_valid             ),
-    .cmd_i                   ( cmd                   ),
-    .cmderror_valid_o        ( cmderror_valid        ),
-    .cmderror_o              ( cmderror              ),
-    .cmdbusy_o               ( cmdbusy               ),
-    .progbuf_i               ( progbuf               ),
-    .data_i                  ( data_csrs_mem         ),
-    .data_o                  ( data_mem_csrs         ),
-    .data_valid_o            ( data_valid            ),
-    .req_i                   ( req                   ),
-    .we_i                    ( we                    ),
-    .addr_i                  ( addr_b                ),
-    .wdata_i                 ( wdata                 ),
-    .be_i                    ( be                    ),
-    .rdata_o                 ( rdata                 )
-  );
 
   // Gating of JTAG signals
   jtag_pkg::jtag_req_t jtag_in_int;
@@ -501,17 +363,17 @@ module rv_dm
     .rst_ni,
     // SEC_CM: EXEC.CTRL.MUBI
     .en_ifetch_i (en_ifetch),
-    .req_o       (req),
+    .req_o       (device_req),
     .req_type_o  (),
     .gnt_i       (1'b1),
-    .we_o        (we),
-    .addr_o      (addr_w),
-    .wdata_o     (wdata),
-    .wmask_o     (wmask),
+    .we_o        (device_we),
+    .addr_o      (device_addr_w),
+    .wdata_o     (device_wdata),
+    .wmask_o     (device_wmask),
     // SEC_CM: BUS.INTEGRITY
     .intg_error_o(rom_intg_error),
-    .rdata_i     (rdata),
-    .rvalid_i    (rvalid),
+    .rdata_i     (device_rdata),
+    .rvalid_i    (device_rvalid),
     .rerror_i    ('0),
 
     .tl_o        (rom_tl_win_d2h_gated),
@@ -520,11 +382,63 @@ module rv_dm
 
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
-      rvalid <= '0;
+      device_rvalid <= '0;
     end else begin
-      rvalid <= req & ~we;
+      device_rvalid <= device_req & ~device_we;
     end
   end
+
+  ///////////////////////////
+  // Debug Module Instance //
+  ///////////////////////////
+
+  dm_top #(
+    .NrHarts        (NrHarts),
+    .BusWidth       (BusWidth),
+    .SelectableHarts(SelectableHarts),
+    // The debug module provides a simplified ROM for systems that map the debug ROM to offset 0x0
+    // on the system bus. In that case, only one scratch register has to be implemented in the core.
+    // However, we require that the DM can be placed at arbitrary offsets in the system, which
+    // requires the generalized debug ROM implementation and two scratch registers. We hence set
+    // this parameter to a non-zero value (inside dm_mem, this just feeds into a comparison with 0).
+    .DmBaseAddress  (1)
+  ) u_dm_top (
+    .clk_i,
+    .rst_ni,
+    .testmode_i            (testmode              ),
+    .ndmreset_o            (ndmreset_req          ),
+    .dmactive_o,
+    .debug_req_o           (debug_req             ),
+    .unavailable_i,
+    .hartinfo_i            (hartinfo              ),
+    .slave_req_i           (device_req            ),
+    .slave_we_i            (device_we             ),
+    .slave_addr_i          (device_addr_b         ),
+    .slave_be_i            (device_be             ),
+    .slave_wdata_i         (device_wdata          ),
+    .slave_rdata_o         (device_rdata          ),
+    .master_req_o          (host_req              ),
+    .master_add_o          (host_add              ),
+    .master_we_o           (host_we               ),
+    .master_wdata_o        (host_wdata            ),
+    .master_be_o           (host_be               ),
+    .master_gnt_i          (host_gnt              ),
+    .master_r_valid_i      (host_r_valid          ),
+    .master_r_err_i        (host_r_err            ),
+    .master_r_other_err_i  (host_r_other_err      ),
+    .master_r_rdata_i      (host_r_rdata          ),
+    .dmi_rst_ni            (dmi_rst_n             ),
+    .dmi_req_valid_i       (dmi_req_valid & dmi_en),
+    .dmi_req_ready_o       (dmi_req_ready         ),
+    .dmi_req_i             (dmi_req               ),
+    .dmi_resp_valid_o      (dmi_rsp_valid         ),
+    .dmi_resp_ready_i      (dmi_rsp_ready & dmi_en),
+    .dmi_resp_o            (dmi_rsp               )
+  );
+
+  ////////////////
+  // Assertions //
+  ////////////////
 
   `ASSERT_KNOWN(TlRegsDValidKnown_A, regs_tl_d_o.d_valid)
   `ASSERT_KNOWN(TlRegsAReadyKnown_A, regs_tl_d_o.a_ready)
