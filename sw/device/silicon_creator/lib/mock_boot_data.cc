8fef6079b1f10384c435902a5ed4a33daa96ecd5
Miguel Young de la Sota <mcyoung@google.com>
2022-04-01 13:10:33 -0400

    [bazel] Merge :boot_data, :boot_data_intf, and :mock_boot_data
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

---
+++ sw/device/silicon_creator/lib/mock_boot_data.cc
@@ -0,0 +1,21 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/mock_boot_data.h"
+
+namespace mask_rom_test {
+extern "C" {
+rom_error_t boot_data_read(lifecycle_state_t lc_state, boot_data_t *boot_data) {
+  return MockBootData::Instance().Read(lc_state, boot_data);
+}
+
+rom_error_t boot_data_write(const boot_data_t *boot_data) {
+  return MockBootData::Instance().Write(boot_data);
+}
+
+rom_error_t boot_data_digest_is_valid(const boot_data *boot_data) {
+  return MockBootData::Instance().Check(boot_data);
+}
+}  // extern "C"
+}  // namespace mask_rom_test
