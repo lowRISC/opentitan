cdbe5933d3751006b75234bf6e2e51b143076dea
Miguel Young de la Sota <mcyoung@google.com>
2022-04-01 12:30:31 -0400

    [bazel] Merge :otp and :mock_otp
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

---
+++ sw/device/silicon_creator/lib/drivers/mock_otp.cc
@@ -0,0 +1,29 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/drivers/mock_otp.h"
+
+namespace mask_rom_test {
+extern "C" {
+// Note: In the functions below, we use `MockOtp` only for conciseness. The
+// static `Instance()` method returns a reference to the same
+// `internal::MockOtp` instance regardless if we use `MockOtp`, `NiceMockOtp`,
+// or `internal::MockOtp`.
+uint32_t otp_read32(uint32_t address) {
+  return MockOtp::Instance().read32(address);
+}
+
+uint64_t otp_read64(uint32_t address) {
+  return MockOtp::Instance().read64(address);
+}
+
+void otp_read(uint32_t address, uint32_t *data, size_t num_words) {
+  return MockOtp::Instance().read(address, data, num_words);
+}
+
+void otp_creator_sw_cfg_lockdown(void) {
+  MockOtp::Instance().CreatorSwCfgLockdown();
+}
+}  // extern "C"
+}  // namespace mask_rom_test
