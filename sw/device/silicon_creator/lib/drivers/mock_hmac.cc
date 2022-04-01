cde5021bb1417cfd7e8631a28bf43d4c9d6482b3
Miguel Young de la Sota <mcyoung@google.com>
2022-04-01 12:19:04 -0400

    [bazel] Merge :hmac and :mock_hmac
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

---
+++ sw/device/silicon_creator/lib/drivers/mock_hmac.cc
@@ -0,0 +1,19 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/drivers/mock_hmac.h"
+
+namespace mask_rom_test {
+extern "C" {
+void hmac_sha256_init(void) { MockHmac::Instance().sha256_init(); }
+
+void hmac_sha256_update(const void *data, size_t len) {
+  MockHmac::Instance().sha256_update(data, len);
+}
+
+void hmac_sha256_final(hmac_digest_t *digest) {
+  MockHmac::Instance().sha256_final(digest);
+}
+}  // extern "C"
+}  // namespace mask_rom_test
