597bfa86a3603a77db55b89a58e7ecf7a239c3aa
Muqing Liu <muqing.liu@wdc.com>
2022-04-04 10:50:23 -0700

    [edn, dv] Fix assertion fail issue
      - Turn off assertions that fail unexpectedly
    
    Signed-off-by: Muqing Liu <muqing.liu@wdc.com>

--- hw/ip/edn/dv/sva/edn_assert_if.sv
+++ hw/ip/edn/dv/sva/edn_assert_if.sv
@@ -15,6 +15,10 @@
     tb.dut.u_edn_core.u_prim_mubi4_sync_auto_req_mode
 `define PATH5 \
     tb.dut.u_edn_core.u_prim_mubi4_sync_cmd_fifo_rst
+`define PATH6 \
+    tb.dut.u_edn_core.u_edn_main_sm
+`define PATH7 \
+    tb.dut.u_edn_core.gen_ep_blk[0].u_edn_ack_sm_ep
 
 interface edn_assert_if
 (
@@ -23,10 +27,14 @@ interface edn_assert_if
 
   task automatic assert_off ();
     $assertoff(0, `PATH1.CrossCntErrBackward_A);
+    $assertoff(0, `PATH6.u_state_regs_A);
+    $assertoff(0, `PATH7.u_state_regs_A);
   endtask // assert_off
 
   task automatic assert_on ();
     $asserton(0, `PATH1.CrossCntErrBackward_A);
+    $asserton(0, `PATH6.u_state_regs_A);
+    $asserton(0, `PATH7.u_state_regs_A);
   endtask // assert_on
 
   task automatic assert_off_alert ();
