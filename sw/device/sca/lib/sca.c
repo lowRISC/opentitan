d013004c7e32b397289c13f7e82b7fe540bdd59e
Miguel Young de la Sota <mcyoung@google.com>
2022-04-04 14:50:08 -0400

    [bazel] Bazelize the SCA targets
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

--- sw/device/sca/lib/sca.c
+++ sw/device/sca/lib/sca.c
@@ -29,15 +29,6 @@
 #endif
 
 /**
- * Macro for ignoring return values.
- *
- * This is needed because ‘(void)expr;` does not work for gcc.
- */
-#define IGNORE_RESULT(expr) \
-  if (expr) {               \
-  }
-
-/**
  * Bitfield for the trigger source.
  *
  * Bits 9 to 11 are used to select the trigger source. See chiplevel.sv.tpl for
@@ -86,15 +77,15 @@ static void sca_init_uart(void) {
       .parity = kDifUartParityEven,
   };
 
-  IGNORE_RESULT(dif_uart_init(
-      mmio_region_from_addr(TOP_EARLGREY_UART0_BASE_ADDR), &uart0));
-  IGNORE_RESULT(dif_uart_configure(&uart0, uart_config));
+  OT_DISCARD(dif_uart_init(mmio_region_from_addr(TOP_EARLGREY_UART0_BASE_ADDR),
+                           &uart0));
+  OT_DISCARD(dif_uart_configure(&uart0, uart_config));
   base_uart_stdout(&uart0);
 
 #if !OT_IS_ENGLISH_BREAKFAST
-  IGNORE_RESULT(dif_uart_init(
-      mmio_region_from_addr(TOP_EARLGREY_UART1_BASE_ADDR), &uart1));
-  IGNORE_RESULT(dif_uart_configure(&uart1, uart_config));
+  OT_DISCARD(dif_uart_init(mmio_region_from_addr(TOP_EARLGREY_UART1_BASE_ADDR),
+                           &uart1));
+  OT_DISCARD(dif_uart_configure(&uart1, uart_config));
 #endif
 }
 
@@ -104,16 +95,15 @@ static void sca_init_uart(void) {
  * @param trigger Trigger source.
  */
 static void sca_init_gpio(sca_trigger_source_t trigger) {
-  IGNORE_RESULT(
+  OT_DISCARD(
       dif_gpio_init(mmio_region_from_addr(TOP_EARLGREY_GPIO_BASE_ADDR), &gpio));
 
   uint32_t select_mask =
       bitfield_field32_write(0, kTriggerSourceBitfield, UINT32_MAX);
   uint32_t enable_mask = bitfield_bit32_write(0, kTriggerGateBitIndex, true);
-  IGNORE_RESULT(
-      dif_gpio_output_set_enabled_all(&gpio, select_mask | enable_mask));
+  OT_DISCARD(dif_gpio_output_set_enabled_all(&gpio, select_mask | enable_mask));
 
-  IGNORE_RESULT(dif_gpio_write_masked(
+  OT_DISCARD(dif_gpio_write_masked(
       &gpio, select_mask,
       bitfield_field32_write(0, kTriggerSourceBitfield, trigger)));
 }
@@ -122,15 +112,14 @@ static void sca_init_gpio(sca_trigger_source_t trigger) {
  * Initializes the timer peripheral.
  */
 static void sca_init_timer(void) {
-  IGNORE_RESULT(dif_rv_timer_init(
+  OT_DISCARD(dif_rv_timer_init(
       mmio_region_from_addr(TOP_EARLGREY_RV_TIMER_BASE_ADDR), &timer));
-  IGNORE_RESULT(dif_rv_timer_reset(&timer));
+  OT_DISCARD(dif_rv_timer_reset(&timer));
   dif_rv_timer_tick_params_t tick_params;
-  IGNORE_RESULT(dif_rv_timer_approximate_tick_params(
+  OT_DISCARD(dif_rv_timer_approximate_tick_params(
       kClockFreqPeripheralHz, kClockFreqCpuHz, &tick_params));
-  IGNORE_RESULT(
-      dif_rv_timer_set_tick_params(&timer, kRvTimerHart, tick_params));
-  IGNORE_RESULT(dif_rv_timer_irq_set_enabled(
+  OT_DISCARD(dif_rv_timer_set_tick_params(&timer, kRvTimerHart, tick_params));
+  OT_DISCARD(dif_rv_timer_irq_set_enabled(
       &timer, kDifRvTimerIrqTimerExpiredHart0Timer0, kDifToggleEnabled));
   irq_timer_ctrl(true);
   irq_global_ctrl(true);
@@ -141,8 +130,10 @@ static void sca_init_timer(void) {
  */
 static void sca_init_csrng(void) {
 #if !OT_IS_ENGLISH_BREAKFAST
-  IGNORE_RESULT(dif_csrng_init(
-      mmio_region_from_addr(TOP_EARLGREY_CSRNG_BASE_ADDR), &csrng));
+  OT_DISCARD(dif_csrng_init(mmio_region_from_addr(TOP_EARLGREY_CSRNG_BASE_ADDR),
+                            &csrng));
+  OT_DISCARD(dif_csrng_init(mmio_region_from_addr(TOP_EARLGREY_CSRNG_BASE_ADDR),
+                            &csrng));
 #endif
 }
 
@@ -151,10 +142,10 @@ static void sca_init_csrng(void) {
  */
 static void sca_init_edn(void) {
 #if !OT_IS_ENGLISH_BREAKFAST
-  IGNORE_RESULT(
+  OT_DISCARD(
       dif_edn_init(mmio_region_from_addr(TOP_EARLGREY_EDN0_BASE_ADDR), &edn0));
 
-  IGNORE_RESULT(
+  OT_DISCARD(
       dif_edn_init(mmio_region_from_addr(TOP_EARLGREY_EDN1_BASE_ADDR), &edn1));
 #endif
 }
@@ -167,9 +158,9 @@ static void sca_init_edn(void) {
 void ottf_timer_isr(void) {
   // Return values of below functions are ignored to improve capture
   // performance.
-  IGNORE_RESULT(dif_rv_timer_counter_set_enabled(&timer, kRvTimerHart,
-                                                 kDifToggleDisabled));
-  IGNORE_RESULT(dif_rv_timer_irq_acknowledge(
+  OT_DISCARD(dif_rv_timer_counter_set_enabled(&timer, kRvTimerHart,
+                                              kDifToggleDisabled));
+  OT_DISCARD(dif_rv_timer_irq_acknowledge(
       &timer, kDifRvTimerIrqTimerExpiredHart0Timer0));
 }
 
@@ -187,45 +178,45 @@ void ottf_timer_isr(void) {
 void sca_disable_peripherals(sca_peripherals_t disable) {
 #if !OT_IS_ENGLISH_BREAKFAST
   if (disable & kScaPeripheralEdn) {
-    IGNORE_RESULT(dif_edn_stop(&edn0));
-    IGNORE_RESULT(dif_edn_stop(&edn1));
+    OT_DISCARD(dif_edn_stop(&edn0));
+    OT_DISCARD(dif_edn_stop(&edn1));
   }
   if (disable & kScaPeripheralCsrng) {
-    IGNORE_RESULT(dif_csrng_stop(&csrng));
+    OT_DISCARD(dif_csrng_stop(&csrng));
   }
   if (disable & kScaPeripheralEntropy) {
     dif_entropy_src_t entropy;
-    IGNORE_RESULT(dif_entropy_src_init(
+    OT_DISCARD(dif_entropy_src_init(
         mmio_region_from_addr(TOP_EARLGREY_ENTROPY_SRC_BASE_ADDR), &entropy));
-    IGNORE_RESULT(dif_entropy_src_disable(&entropy));
+    OT_DISCARD(dif_entropy_src_disable(&entropy));
   }
 #endif
 
   // Disable HMAC, KMAC, OTBN and USB clocks through CLKMGR DIF.
   dif_clkmgr_t clkmgr;
-  IGNORE_RESULT(dif_clkmgr_init(
+  OT_DISCARD(dif_clkmgr_init(
       mmio_region_from_addr(TOP_EARLGREY_CLKMGR_AON_BASE_ADDR), &clkmgr));
 
   if (disable & kScaPeripheralAes) {
-    IGNORE_RESULT(dif_clkmgr_hintable_clock_set_hint(
+    OT_DISCARD(dif_clkmgr_hintable_clock_set_hint(
         &clkmgr, CLKMGR_CLK_HINTS_CLK_MAIN_AES_HINT_BIT, kDifToggleDisabled));
   }
   if (disable & kScaPeripheralHmac) {
-    IGNORE_RESULT(dif_clkmgr_hintable_clock_set_hint(
+    OT_DISCARD(dif_clkmgr_hintable_clock_set_hint(
         &clkmgr, CLKMGR_CLK_HINTS_CLK_MAIN_HMAC_HINT_BIT, kDifToggleDisabled));
   }
   if (disable & kScaPeripheralUsb) {
-    IGNORE_RESULT(dif_clkmgr_gateable_clock_set_enabled(
+    OT_DISCARD(dif_clkmgr_gateable_clock_set_enabled(
         &clkmgr, CLKMGR_CLK_ENABLES_CLK_USB_PERI_EN_BIT, kDifToggleDisabled));
   }
 
 #if !OT_IS_ENGLISH_BREAKFAST
   if (disable & kScaPeripheralKmac) {
-    IGNORE_RESULT(dif_clkmgr_hintable_clock_set_hint(
+    OT_DISCARD(dif_clkmgr_hintable_clock_set_hint(
         &clkmgr, CLKMGR_CLK_HINTS_CLK_MAIN_KMAC_HINT_BIT, kDifToggleDisabled));
   }
   if (disable & kScaPeripheralOtbn) {
-    IGNORE_RESULT(dif_clkmgr_hintable_clock_set_hint(
+    OT_DISCARD(dif_clkmgr_hintable_clock_set_hint(
         &clkmgr, CLKMGR_CLK_HINTS_CLK_MAIN_OTBN_HINT_BIT, kDifToggleDisabled));
   }
 #endif
@@ -250,11 +241,11 @@ void sca_get_uart(const dif_uart_t **uart_out) {
 }
 
 void sca_set_trigger_high() {
-  IGNORE_RESULT(dif_gpio_write(&gpio, kTriggerGateBitIndex, true));
+  OT_DISCARD(dif_gpio_write(&gpio, kTriggerGateBitIndex, true));
 }
 
 void sca_set_trigger_low() {
-  IGNORE_RESULT(dif_gpio_write(&gpio, kTriggerGateBitIndex, false));
+  OT_DISCARD(dif_gpio_write(&gpio, kTriggerGateBitIndex, false));
 }
 
 void sca_call_and_sleep(sca_callee callee, uint32_t sleep_cycles) {
@@ -262,11 +253,11 @@ void sca_call_and_sleep(sca_callee callee, uint32_t sleep_cycles) {
   uint64_t current_time;
   // Return values of below functions are ignored to improve capture
   // performance.
-  IGNORE_RESULT(dif_rv_timer_counter_read(&timer, kRvTimerHart, &current_time));
-  IGNORE_RESULT(dif_rv_timer_arm(&timer, kRvTimerHart, kRvTimerComparator,
-                                 current_time + sleep_cycles));
-  IGNORE_RESULT(dif_rv_timer_counter_set_enabled(&timer, kRvTimerHart,
-                                                 kDifToggleEnabled));
+  OT_DISCARD(dif_rv_timer_counter_read(&timer, kRvTimerHart, &current_time));
+  OT_DISCARD(dif_rv_timer_arm(&timer, kRvTimerHart, kRvTimerComparator,
+                              current_time + sleep_cycles));
+  OT_DISCARD(dif_rv_timer_counter_set_enabled(&timer, kRvTimerHart,
+                                              kDifToggleEnabled));
 
   callee();
 
