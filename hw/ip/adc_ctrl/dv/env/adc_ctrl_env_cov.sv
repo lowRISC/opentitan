2f63ba8df980639f7e0ae3fc235efb96915ace70
Nigel Scales <nigel.scales@gmail.com>
2022-04-04 11:51:05 +0100

    [adc_ctrl/dv] Enhanced coverage and moved collection to scoreboard
    - Moved coverage collection to scoreboard
    - Added interrupt and wakeup coverage collection
    - Added test mode coverage
    - Updated m_tl_agent_cfg.max_outstanding_req in config object to
    solve issue with tl_agent_pkg::max_outstanding_cg
    - Fixed adc_ctrl_fsm_reset by disabling RTL assertion which was triggering due
    to backdoor preloading of the counters.
    - Importing adc_ctrl_pkg and using adc_ctrl_pkg::fsm_state_e for
    SVA and coverage
    
    Signed-off-by: Nigel Scales <nigel.scales@gmail.com>

--- hw/ip/adc_ctrl/dv/env/adc_ctrl_env_cov.sv
+++ hw/ip/adc_ctrl/dv/env/adc_ctrl_env_cov.sv
@@ -15,31 +15,39 @@ class adc_ctrl_filter_cg_wrapper #(
   // Number of values bins to create
   int NUMBER_VALUES = 10
 );
-  // Sampling event
-  event sample_ev;
-  adc_ctrl_env_cfg cfg;
 
   // Filter configuration one instance per filter
-  covergroup adc_ctrl_filter_cg(int channel, int filter) @(sample_ev);
+  covergroup adc_ctrl_filter_cg(
+      int channel, int filter
+  ) with function sample (
+      adc_ctrl_filter_cfg_t cfg, bit is_interrupt = 0, bit is_wakeup = 0, bit clk_gate = 0
+  );
     option.name = $sformatf("adc_ctrl_filter_cg_%0d_%0d", channel, filter);
     option.per_instance = 1;
-    cond_cp: coverpoint cfg.filter_cfg[channel][filter].cond;
-    min_v_cp: coverpoint cfg.filter_cfg[channel][filter].min_v {
+
+    cond_cp: coverpoint cfg.cond;
+    min_v_cp: coverpoint cfg.min_v {
       bins minimum = {0};
       bins values[NUMBER_VALUES] = {[1 : (2 ** FILTER_WIDTH - 2)]};
       bins maximum = {2 ** FILTER_WIDTH - 1};
     }
-    max_v_cp: coverpoint cfg.filter_cfg[channel][filter].max_v {
+    max_v_cp: coverpoint cfg.max_v {
       bins minimum = {0};
       bins values[NUMBER_VALUES] = {[1 : (2 ** FILTER_WIDTH - 2)]};
       bins maximum = {2 ** FILTER_WIDTH - 1};
     }
-    en_cp: coverpoint cfg.filter_cfg[channel][filter].en;
+    en_cp: coverpoint cfg.en;
+    interrupt_cp: coverpoint is_interrupt;
+    wakeup_cp: coverpoint is_wakeup;
+    clk_gate_cp: coverpoint clk_gate;
+    intr_min_v_cond_xp: cross interrupt_cp, min_v_cp, cond_cp;
+    intr_max_v_cond_xp: cross interrupt_cp, max_v_cp, cond_cp;
+    wakeup_min_v_cond_xp: cross wakeup_cp, min_v_cp, cond_cp;
+    wakeup_max_v_cond_xp: cross wakeup_cp, max_v_cp, cond_cp;
+    wakeup_gated_xp : cross wakeup_cp, clk_gate_cp;
   endgroup
 
-  function new(int channel, int filter, adc_ctrl_env_cfg cfg, event sample_ev);
-    this.sample_ev = sample_ev;
-    this.cfg = cfg;
+  function new(int channel, int filter);
     adc_ctrl_filter_cg = new(channel, filter);
   endfunction
 endclass
@@ -48,29 +56,51 @@ class adc_ctrl_env_cov extends cip_base_env_cov #(
   .CFG_T(adc_ctrl_env_cfg)
 );
   `uvm_component_utils(adc_ctrl_env_cov)
-  `uvm_component_new
 
   // Sample configuration
   event sample_cfg_ev;
 
   // covergroups
 
+  // Modes of operation - sampled when adc_en_ctl is written to start the ADC_CTRL
+  covergroup adc_ctrl_testmode_cg with function sample (adc_ctrl_testmode_e testmode);
+    testmode_cp: coverpoint testmode {
+      bins testmodes[] = {AdcCtrlTestmodeOneShot, AdcCtrlTestmodeNormal, AdcCtrlTestmodeLowpower};
+
+      bins transitions[] = ( AdcCtrlTestmodeOneShot, AdcCtrlTestmodeNormal,
+      AdcCtrlTestmodeLowpower => AdcCtrlTestmodeOneShot, AdcCtrlTestmodeNormal,
+      AdcCtrlTestmodeLowpower );
+    }
+  endgroup
+
+
+  // These are sampled on configuration when adc_en_ctl is written to start the ADC_CTRL
+  // They are also sampled when an interrupt or wakeup event occurs
+  // There is one per filter
   adc_ctrl_filter_cg_wrapper adc_ctrl_filter_cg_wrapper_insts
       [ADC_CTRL_CHANNELS] [ADC_CTRL_NUM_FILTERS];
+
+
+  function new(string name = "adc_ctrl_env_cov", uvm_component parent = null);
+    super.new(name, parent);
+    adc_ctrl_testmode_cg = new();
+  endfunction
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     for (int channel = 0; channel < ADC_CTRL_CHANNELS; channel++) begin
       for (int filter = 0; filter < ADC_CTRL_NUM_FILTERS; filter++) begin
-        adc_ctrl_filter_cg_wrapper_insts[channel][filter] =
-            new(.channel(channel), .filter(filter), .cfg(cfg), .sample_ev(sample_cfg_ev));
+        adc_ctrl_filter_cg_wrapper_insts[channel][filter] = new(.channel(channel), .filter(filter));
       end
     end
   endfunction : build_phase
 
-  // Sample the coverage
-  virtual function void sample_cfg_cov();
-    ->sample_cfg_ev;
+  // Sample filter coverage
+  virtual function void sample_filter_cov(int channel, int filter, adc_ctrl_filter_cfg_t cfg,
+                                          bit is_interrupt = 0, bit is_wakeup = 0,
+                                          bit clk_gate = 0);
+    adc_ctrl_filter_cg_wrapper_insts[channel][filter].adc_ctrl_filter_cg.sample(cfg, is_interrupt,
+                                                                                is_wakeup);
   endfunction
 
 endclass
