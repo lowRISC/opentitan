b660456d60abbefb497e8d801a1a53ddaf3ddc5e
Jaedon Kim <jdonjdon@google.com>
2022-04-05 03:23:23 +0000

    [rstmgr,dv] leaf_rst_shadow test update
     - add normal glitch shadow check.
     - update sec_cm_testplan under top_earlgrey/ip
    
    Signed-off-by: Jaedon Kim <jdonjdon@google.com>

--- hw/ip/rstmgr/dv/env/seq_lib/rstmgr_leaf_rst_shadow_attack_vseq.sv
+++ hw/ip/rstmgr/dv/env/seq_lib/rstmgr_leaf_rst_shadow_attack_vseq.sv
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Description:
 // Test assert glitch to shadow leaf reset module and
-// check if nomal reset module got affected.
+// check if nomal reset module got affected or vice versa
 class rstmgr_leaf_rst_shadow_attack_vseq extends rstmgr_base_vseq;
   `uvm_object_utils(rstmgr_leaf_rst_shadow_attack_vseq)
 
@@ -15,31 +15,29 @@ class rstmgr_leaf_rst_shadow_attack_vseq extends rstmgr_base_vseq;
     for (int i = 0; i < LIST_OF_SHADOW_LEAFS.size(); ++i) begin
       leaf_path = {"tb.dut.", LIST_OF_SHADOW_LEAFS[i]};
       `uvm_info(`gfn, $sformatf("Round %0d %s ", i, leaf_path), UVM_MEDIUM)
-      wait(cfg.rstmgr_vif.resets_o.rst_sys_aon_n == 2'h3);
-      add_glitch_to_shadow(leaf_path);
-      wait_and_check(leaf_path);
-      clean_shadow(leaf_path);
-      cfg.clk_rst_vif.wait_clks(10);
-      apply_reset();
+
+      leaf_rst_attack(leaf_path, {leaf_path, "_shadowed"});
+      leaf_rst_attack({leaf_path, "_shadowed"}, leaf_path);
     end
   endtask // body
 
-  function void add_glitch_to_shadow(string path);
-    string spath = {path, "_shadowed"};
-    string epath = {spath, ".rst_en_o"};
-    string opath = {spath, ".leaf_rst_o"};
+  task leaf_rst_attack(string npath, string gpath);
+      wait(cfg.rstmgr_vif.resets_o.rst_sys_aon_n == 2'h3);
+      add_glitch(gpath);
+      wait_and_check(npath);
+      remove_glitch(gpath);
+
+      cfg.clk_rst_vif.wait_clks(10);
+      apply_reset();
+  endtask // leaf_rst_attach
+
+
+  function void add_glitch(string path);
+    string epath = {path, ".rst_en_o"};
+    string opath = {path, ".leaf_rst_o"};
 
     `DV_CHECK(uvm_hdl_force(epath, prim_mubi_pkg::MuBi4True))
     `DV_CHECK(uvm_hdl_force(opath, 0))
-  endfunction
-
-  function void clean_shadow(string path);
-    string spath = {path, "_shadowed"};
-    string epath = {spath, ".rst_en_o"};
-    string opath = {spath, ".leaf_rst_o"};
-
-    `DV_CHECK(uvm_hdl_release(epath))
-    `DV_CHECK(uvm_hdl_release(opath))
   endfunction
 
   task wait_and_check(string path);
@@ -58,6 +56,15 @@ class rstmgr_leaf_rst_shadow_attack_vseq extends rstmgr_base_vseq;
     `DV_CHECK_EQ_FATAL(leaf_rst, 1,
                        $sformatf("%s value mismatch",opath))
   endtask // wait_and_check
+
+  function void remove_glitch(string path);
+    string spath = {path, "_shadowed"};
+    string epath = {spath, ".rst_en_o"};
+    string opath = {spath, ".leaf_rst_o"};
+
+    `DV_CHECK(uvm_hdl_release(epath))
+    `DV_CHECK(uvm_hdl_release(opath))
+  endfunction
 
   // clean up glitch will create reset consistency error
   // in shadow leaf reset module
