60157fdbd016581cbfffa31d0c9342e4a98a1064
Rupert Swarbrick <rswarbrick@lowrisc.org>
2022-04-11 16:54:20 +0100

    [kmac_app_agent] Support hosts that don't spot constant shares
    This code was originally designed for keymgr, which has a check to
    make sure that no share is all zeros or all ones. The ROM controller
    doesn't have such a check, so we need to tweak the agent slightly.
    
    Without this change, setting err_rsp_pct to 100 in the config might
    still result in a KMAC response whose rsp_error field is zero.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/dv/sv/kmac_app_agent/kmac_app_item.sv
+++ hw/dv/sv/kmac_app_agent/kmac_app_item.sv
@@ -4,10 +4,6 @@
 
 class kmac_app_item extends uvm_sequence_item;
 
-  // below 3 cases will trigger KMAC invalid output error
-  `define CALC_KMAC_DATA_INVALID \
-      (rsp_digest_share0 inside {0, '1} || rsp_digest_share1 inside {0, '1})
-
   // random variables
 
   // request data/mask
@@ -20,27 +16,24 @@ class kmac_app_item extends uvm_sequence_item;
   rand bit [kmac_pkg::AppDigestW-1:0] rsp_digest_share1;
   rand bit                            rsp_error;
 
-  rand bit                is_kmac_rsp_err;
   rand int unsigned       rsp_delay;
-
-  constraint is_kmac_rsp_err_c {
-    (`CALC_KMAC_DATA_INVALID || rsp_error) == is_kmac_rsp_err;
-  }
 
   `uvm_object_utils_begin(kmac_app_item)
     `uvm_field_queue_int(byte_data_q, UVM_DEFAULT)
     `uvm_field_int(rsp_digest_share0, UVM_DEFAULT)
     `uvm_field_int(rsp_digest_share1, UVM_DEFAULT)
     `uvm_field_int(rsp_error,         UVM_DEFAULT)
-    `uvm_field_int(is_kmac_rsp_err,   UVM_DEFAULT)
     `uvm_field_int(rsp_delay,         UVM_DEFAULT)
   `uvm_object_utils_end
 
   `uvm_object_new
 
   virtual function bit get_is_kmac_rsp_data_invalid();
-    return `CALC_KMAC_DATA_INVALID;
+    return is_constant_share(rsp_digest_share0) || is_constant_share(rsp_digest_share1);
   endfunction
 
-  `undef CALC_KMAC_DATA_INVALID
+  static function bit is_constant_share(bit [kmac_pkg::AppDigestW-1:0] share);
+    return share inside {'0, '1};
+  endfunction
+
 endclass
