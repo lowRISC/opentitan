67d724b7d0900ac4f2263ccb68167148fed3bad5
Michael Schaffner <msf@google.com>
2022-04-13 19:08:18 -0700

    [prim_onehot_check] Add option for permissive en_i checks
    A more permissive en_i check is needed when for example the oh_i vector
    is partially masked. E.g. when checking CSR WE strobes, it can happen
    that some of the WE strobes are additionally masked by REGWEN bits.
    In that case there may be situations where the en_i (connected to the
    global write enable bit) is 1, but all  of the oh_i (connected to the
    individual register we bits) elements are 0.
    
    Signed-off-by: Michael Schaffner <msf@google.com>

--- hw/ip/prim/rtl/prim_onehot_check.sv
+++ hw/ip/prim/rtl/prim_onehot_check.sv
@@ -10,9 +10,8 @@
 //
 // 1) EnableCheck: this check performs an OR reduction of the onehot vector, and compares
 //    the result with en_i. If there is a mismatch, an error is generated.
-// 2) AddrCheck: this checks whether the bit at a certain location is equal to en_i.
+// 2) AddrCheck: this checks whether the onehot bit is in the correct position.
 //    It requires an additional address addr_i to be supplied to the module.
-//    This check can only be enabled if EnableCheck is enabled.
 //
 // All checks make use of an explicit binary tree implementation in order to minimize the delay.
 //
@@ -26,7 +25,10 @@ module prim_onehot_check #(
   // If set to 0, the addr_i input will not be used for the check and can be tied off.
   parameter bit          AddrCheck   = 1,
   // If set to 0, the en_i value will not be used for the check and can be tied off.
-  parameter bit          EnableCheck = 1
+  parameter bit          EnableCheck = 1,
+  // If set to 1, the oh_i vector must always be one hot if en_i is set to 1.
+  // If set to 0, the oh_i vector may be 0 if en_i is set to 1 (useful when oh_i can be masked).
+  parameter bit          StrictCheck = 1
 ) (
   // The module is combinational - the clock and reset are only used for assertions.
   input                          clk_i,
@@ -43,14 +45,14 @@ module prim_onehot_check #(
   // Binary tree logic //
   ///////////////////////
 
-  // This only works with 2 or more sources.
-  `ASSERT_INIT(NumSources_A, OneHotWidth >= 2)
-  `ASSERT_INIT(AddrSize_A, OneHotWidth <= 2**AddrWidth)
+  `ASSERT_INIT(NumSources_A, OneHotWidth >= 1)
+  `ASSERT_INIT(AddrWidth_A, AddrWidth >= 1)
+  `ASSERT_INIT(AddrRange_A, OneHotWidth <= 2**AddrWidth)
   `ASSERT_INIT(AddrImpliesEnable_A, AddrCheck && EnableCheck || !AddrCheck)
 
   // Align to powers of 2 for simplicity.
   // A full binary tree with N levels has 2**N + 2**N-1 nodes.
-  localparam int NumLevels = $clog2(OneHotWidth);
+  localparam int NumLevels = AddrWidth;
   logic [2**(NumLevels+1)-2:0] or_tree;
   logic [2**(NumLevels+1)-2:0] and_tree; // Used for the address check
   logic [2**(NumLevels+1)-2:0] err_tree; // Used for the enable check
@@ -95,28 +97,37 @@ module prim_onehot_check #(
     end : gen_level
   end : gen_tree
 
-  // Check whether:
-  // 1) more than 1 bit is set in the vector
-  // 2) whether en_i agrees with (|oh_i)
-  // 3) the bit that is set is actually in the correct position
+  ///////////////////
+  // Onehot Checks //
+  ///////////////////
+
   logic enable_err, addr_err, oh0_err;
+  assign err_o = oh0_err || enable_err || addr_err;
+
+  // Check that no more than 1 bit is set in the vector.
   assign oh0_err = err_tree[0];
+  `ASSERT(Onehot0Check_A, !$onehot0(oh_i) |-> err_o)
+
+  // Check that en_i agrees with (|oh_i).
+  // Note: if StrictCheck 0, the oh_i vector may be all-zero if en_i == 1 (but not vice versa).
   if (EnableCheck) begin : gen_enable_check
-    assign enable_err = or_tree[0] ^ en_i;
-    `ASSERT(EnableCheck_A, ($countones(oh_i) != en_i) |-> err_o)
-    if (AddrCheck) begin : gen_addr_check
-      assign addr_err = and_tree[0] ^ en_i;
-      `ASSERT(AddrCheck_A, oh_i[addr_i] != en_i |-> err_o)
-    end else begin : gen_no_addr_check
-      assign addr_err = 1'b0;
+    if (StrictCheck) begin : gen_strict
+      assign enable_err = or_tree[0] ^ en_i;
+      `ASSERT(EnableCheck_A, (|oh_i) != en_i |-> err_o)
+    end else begin : gen_not_strict
+      assign enable_err = !en_i && or_tree[0];
+      `ASSERT(EnableCheck_A, !en_i && (|oh_i) |-> err_o)
     end
   end else begin : gen_no_enable_check
     assign enable_err = 1'b0;
-    assign addr_err = 1'b0;
   end
 
-  assign err_o = oh0_err || enable_err || addr_err;
-
-  `ASSERT(Onehot0Check_A, !$onehot0(oh_i) |-> err_o)
+  // Check that the set bit is actually in the correct position.
+  if (AddrCheck) begin : gen_addr_check_strict
+    assign addr_err = or_tree[0] ^ and_tree[0];
+    `ASSERT(AddrCheck_A, oh_i[addr_i] != (|oh_i) |-> err_o)
+  end else begin : gen_no_addr_check_strict
+    assign addr_err = 1'b0;
+  end
 
 endmodule : prim_onehot_check
