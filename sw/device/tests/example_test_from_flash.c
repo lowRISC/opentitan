8fb2bb943687d314e4a296ae781eba0fd97a3485
Timothy Trippel <ttrippel@google.com>
2022-04-18 17:33:28 -0700

    [ottf] add non-volatile scratch region
    This adds a region in flash to write/read non-volatile data to
    persistent accross chip resets.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

--- sw/device/tests/example_test_from_flash.c
+++ sw/device/tests/example_test_from_flash.c
@@ -70,6 +70,14 @@ const test_config_t kTestConfig = {
 // void ottf_timer_isr(void) {}
 // void ottf_external_isr(void) {}
 
+/**
+ * Place data in flash that will need to persist across resets by marking with
+ * with the section ".non_volatile_scratch"). Write to this region with the
+ * flash controller DIF. Read from it with `abs_mmio_read32(...)`.
+ */
+// __attribute__((section(".non_volatile_scratch")))
+// const volatile uint32_t non_volatile_data[SIZE_OF_DATA];
+
 bool test_main(void) {
   /**
    * Place test code here.
