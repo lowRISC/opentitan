63dcce536d3b08c86cb1c846669d0b8b133f9b72
Guillermo Maturana <maturana@google.com>
2022-04-19 09:31:40 -0700

    [dv,full_chip,extclk] Fix clkmgr_external_clk_src_for_sw
    Split the previous test in two, one running with 96 MHz ext clk and
    another with 48 MHz. Fix the failures.
    
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- sw/device/tests/clkmgr_external_clk_src_for_sw_impl.c
+++ sw/device/tests/clkmgr_external_clk_src_for_sw_impl.c
@@ -2,14 +2,9 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 //
-// This test enables the external clock via software and ramps down the
-// clock dividers. It checks the expected frequencies via the clock count
+// This runs a test with external clock enabled via software for either fast
+// or slow speed. It checks the expected frequencies via the clock count
 // measurement feature.
-//
-// The variability for USB is larger because that clock is uncalibrated at
-// power-on.
-// TODO(lowrisc/opentitan:#11264): tighten up the USB measurement once
-// this issue is addressed.
 
 #include "sw/device/lib/base/memory.h"
 #include "sw/device/lib/dif/dif_base.h"
@@ -31,9 +26,11 @@ typedef struct expected_count_info {
 } expected_count_info_t;
 
 expected_count_info_t count_infos[kDifClkmgrMeasureClockUsb + 1] = {
-    {479, 1}, {239, 1}, {119, 1}, {499, 1}, {239, 6}};
+    {479, 1}, {239, 1}, {119, 1}, {499, 1}, {239, 1}};
 
-const int extclk_settle_delay_us = 30;
+// Switching to external clocks causes the clocks to be unstable for some time.
+// This is used to delay further action when the switch happens.
+const int extclk_settle_delay_us = 35;
 const int measurement_delay_us = 100;
 
 static void clear_recoverable_errors(const dif_clkmgr_t *clkmgr) {
@@ -51,7 +48,13 @@ static void check_measurement_counts(const dif_clkmgr_t *clkmgr) {
   clear_recoverable_errors(clkmgr);
 }
 
-bool test_main() {
+static bool did_extclk_settle(const dif_clkmgr_t *clkmgr) {
+  bool status;
+  CHECK_DIF_OK(dif_clkmgr_external_clock_is_settled(clkmgr, &status));
+  return status;
+}
+
+bool execute_clkmgr_external_clk_src_for_sw_test(bool fast_ext_clk) {
   dif_clkmgr_t clkmgr;
   dif_clkmgr_recov_err_codes_t err_codes;
 
@@ -76,54 +79,36 @@ bool test_main() {
   check_measurement_counts(&clkmgr);
   clkmgr_testutils_disable_clock_count_measurements(&clkmgr);
 
-  // Configure external clock and low speed: both main and io clocks counts
-  // are the nominal IoDiv2's.
+  // Configure external clock:
+  // - at low speed (48 MHz) both main and io clocks count are the nominal
+  //   IoDiv2's.
+  // - at high speed (96 MHz) the main clock count is the nominal Io's.
   LOG_INFO("Selecting external clock and low speed clocks");
-  CHECK_DIF_OK(dif_clkmgr_external_clock_set_enabled(&clkmgr, true));
+  CHECK_DIF_OK(
+      dif_clkmgr_external_clock_set_enabled(&clkmgr,
+                                            /*is_low_speed=*/!fast_ext_clk));
 
   // Wait a few AON cycles for glitches from the transition to external
   // clock to settle.
-  busy_spin_micros(extclk_settle_delay_us);
+  IBEX_SPIN_FOR(did_extclk_settle(&clkmgr), extclk_settle_delay_us);
 
   // Enable cycle measurements to confirm the frequencies are correct.
   for (int i = 0; i <= kDifClkmgrMeasureClockUsb; ++i) {
     dif_clkmgr_measure_clock_t clock = (dif_clkmgr_measure_clock_t)i;
     expected_count_info_t count_info;
-    if (clock == kDifClkmgrMeasureClockIo ||
-        clock == kDifClkmgrMeasureClockMain) {
-      count_info = count_infos[kDifClkmgrMeasureClockIoDiv2];
+    if (fast_ext_clk) {
+      if (clock == kDifClkmgrMeasureClockMain) {
+        count_info = count_infos[kDifClkmgrMeasureClockIo];
+      } else {
+        count_info = count_infos[clock];
+      }
     } else {
-      count_info = count_infos[clock];
-    }
-    clkmgr_testutils_enable_clock_count_measurement(
-        &clkmgr, clock, count_info.count - count_info.variability,
-        count_info.count + count_info.variability);
-  }
-
-  busy_spin_micros(measurement_delay_us);
-  check_measurement_counts(&clkmgr);
-  clkmgr_testutils_disable_clock_count_measurements(&clkmgr);
-
-  // Configure external clock and high speed: io, io_div2, and main expected
-  // at 96 MHz, and io_div4 at 48 MHz.
-  LOG_INFO("Selecting external clock and high speed clocks");
-  CHECK_DIF_OK(dif_clkmgr_external_clock_set_enabled(&clkmgr, false));
-
-  // Wait a few AON cycles for glitches from the transition to external
-  // high speed to settle.
-  busy_spin_micros(10);
-
-  // Enable cycle measurements to confirm the frequencies are correct.
-  for (int i = 0; i <= kDifClkmgrMeasureClockUsb; ++i) {
-    dif_clkmgr_measure_clock_t clock = (dif_clkmgr_measure_clock_t)i;
-    expected_count_info_t count_info;
-    if (clock == kDifClkmgrMeasureClockMain ||
-        clock == kDifClkmgrMeasureClockIoDiv2) {
-      count_info = count_infos[kDifClkmgrMeasureClockIo];
-    } else if (clock == kDifClkmgrMeasureClockIoDiv4) {
-      count_info = count_infos[kDifClkmgrMeasureClockIoDiv2];
-    } else {
-      count_info = count_infos[clock];
+      if (clock == kDifClkmgrMeasureClockIo ||
+          clock == kDifClkmgrMeasureClockMain) {
+        count_info = count_infos[kDifClkmgrMeasureClockIoDiv2];
+      } else {
+        count_info = count_infos[clock];
+      }
     }
     clkmgr_testutils_enable_clock_count_measurement(
         &clkmgr, clock, count_info.count - count_info.variability,
---
+++ sw/device/tests/clkmgr_external_clk_src_for_sw_slow_test.c
@@ -0,0 +1,14 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// This test enables the external clock running at slow speed. It checks
+// the expected frequencies via the clock count measurement feature.
+
+#include <stdbool.h>
+
+#include "sw/device/tests/clkmgr_external_clk_src_for_sw_impl.h"
+
+bool test_main(void) {
+  return execute_clkmgr_external_clk_src_for_sw_test(/*fast_ext_clk=*/false);
+}
