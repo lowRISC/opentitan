80d8c85ce2c6049bce124143530059eb3ec3c266
Timothy Chen <timothytim@google.com>
2022-04-21 12:17:49 -0700

    [dv/pwrmgr] Corresponding dv changes to power manger stability checks
    - do not re-enable assertion until the system has rebooted. This is
      because the reset request takes some time to propagate after a
      glitch is seen.
    
    - separate the tasks in ast_supply_if to make the above easier.
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_main_power_glitch_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_main_power_glitch_vseq.sv
@@ -15,7 +15,12 @@ class chip_sw_main_power_glitch_vseq extends chip_sw_base_vseq;
 
     // Wait until we reach the SW test state.
     wait(cfg.sw_test_status_vif.sw_test_status == SwTestStatusInTest);
-
     cfg.ast_supply_vif.glitch_vcmain_pok_on_next_trigger(cycles_after_trigger);
+
+    // the above glitch should cause the system to reboot, now wait for reset
+    // before re-enabling assertion
+    wait(cfg.sw_test_status_vif.sw_test_status == SwTestStatusInBootRom);
+    cfg.ast_supply_vif.reenable_vcmain_assertion();
+
   endtask
 endclass
