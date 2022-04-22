bc6e46849614b984e888464dce40a848997e99f0
Alphan Ulusoy <alphan@google.com>
2022-04-22 17:02:43 -0400

    [sw/silicon_creator] Add mock rstmgr driver
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

---
+++ sw/device/silicon_creator/lib/drivers/mock_rstmgr.cc
@@ -0,0 +1,21 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/drivers/mock_rstmgr.h"
+
+namespace mask_rom_test {
+extern "C" {
+uint32_t rstmgr_reason_get(void) { return MockRstmgr::Instance().ReasonGet(); }
+
+void rstmgr_reason_clear(uint32_t reasons) {
+  MockRstmgr::Instance().ReasonClear(reasons);
+}
+
+void rstmgr_alert_info_enable(void) {
+  MockRstmgr::Instance().AlertInfoEnable();
+}
+
+void rstmgr_reset(void) { MockRstmgr::Instance().Reset(); }
+}  // extern "C"
+}  // namespace mask_rom_test
