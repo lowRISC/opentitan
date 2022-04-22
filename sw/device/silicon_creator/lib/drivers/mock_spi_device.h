ac6ba8ef39442c1ec20aa6fe7ea2c644ada1cff0
Alphan Ulusoy <alphan@google.com>
2022-04-22 17:03:40 -0400

    [sw/silicon_creator] Add mock spi_device driver
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

---
+++ sw/device/silicon_creator/lib/drivers/mock_spi_device.h
@@ -0,0 +1,45 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_DRIVERS_MOCK_SPI_DEVICE_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_DRIVERS_MOCK_SPI_DEVICE_H_
+
+#include "sw/device/lib/base/testing/global_mock.h"
+#include "sw/device/silicon_creator/lib/drivers/spi_device.h"
+
+namespace mask_rom_test {
+namespace internal {
+
+/**
+ * Mock class for spi_device.c.
+ */
+class MockSpiDevice : public global_mock::GlobalMock<MockSpiDevice> {
+ public:
+  MOCK_METHOD(void, Init, ());
+  MOCK_METHOD(void, CmdGet, (spi_device_cmd_t *));
+  MOCK_METHOD(void, FlashStatusClear, ());
+};
+
+}  // namespace internal
+
+using MockSpiDevice = testing::StrictMock<internal::MockSpiDevice>;
+
+#ifdef IS_MESON_FOR_MIGRATIONS_ONLY
+extern "C" {
+
+void spi_device_init(void) { MockSpiDevice::Instance().Init(); }
+
+void spi_device_cmd_get(spi_device_cmd_t *cmd) {
+  MockSpiDevice::Instance().CmdGet(cmd);
+}
+
+void spi_device_flash_status_clear(void) {
+  MockSpiDevice::Instance().FlashStatusClear();
+}
+
+}  // extern "C"
+#endif
+}  // namespace mask_rom_test
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_DRIVERS_MOCK_SPI_DEVICE_H_
