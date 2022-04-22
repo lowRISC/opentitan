9aaf88bfeb5cdddb562f35b91389d7283121dc9a
Alphan Ulusoy <alphan@google.com>
2022-04-22 17:03:40 -0400

    [sw/silicon_creator] Add mock spi_device driver
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

---
+++ sw/device/silicon_creator/lib/drivers/mock_spi_device.cc
@@ -0,0 +1,19 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/drivers/mock_spi_device.h"
+
+namespace mask_rom_test {
+extern "C" {
+void spi_device_init(void) { MockSpiDevice::Instance().Init(); }
+
+void spi_device_cmd_get(spi_device_cmd_t *cmd) {
+  MockSpiDevice::Instance().CmdGet(cmd);
+}
+
+void spi_device_flash_status_clear(void) {
+  MockSpiDevice::Instance().FlashStatusClear();
+}
+}  // extern "C"
+}  // namespace mask_rom_test
