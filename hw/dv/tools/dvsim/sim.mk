e4e5b6d459c438df45362a3d30f924b143d9f239
Timothy Trippel <ttrippel@google.com>
2022-04-25 10:40:19 -0700

    [bazel] update dvsim.py to build ROMs with bazel
    This fixes #11562 by update dvsim.py to build ROM images with bazel.
    Note, flash images are still built with meson, and will be transitioned
    to bazel in a future PR.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

--- hw/dv/tools/dvsim/sim.mk
+++ hw/dv/tools/dvsim/sim.mk
@@ -88,8 +88,28 @@ ifneq (${sw_images},)
 			cp ${proj_root}/$$image* $$target_dir/.; \
 		else \
 			echo "Building SW image \"$$image\"."; \
-			target="$$image""_export_${sw_build_device}"; \
-			${LOCK_SW_BUILD_DIR} "ninja -C ${sw_build_dir}/build-out $$target"; \
+			if [[ $$index == "0" ]]; then \
+				target="$$image""_${sw_build_device}"; \
+				image_basename="$$(basename $${target}).scr.39.vmem"; \
+				if [[ -z $${BAZEL_PYTHON_WHEELS_REPO} ]]; then \
+					echo "Building \"//$$(dirname $$image)\" with bazel on network-connected machine."; \
+					./bazelisk.sh build //$$(dirname $$image); \
+					bazel_image_path=$$(find -L $$(./bazelisk.sh info output_path)/ -type f -name "$${image_basename}"); \
+				else \
+					echo "Building \"//$$(dirname $$image)\" with bazel on air-gapped machine."; \
+					bazel build \
+						--distdir=$$BAZEL_DISTDIR \
+						--repository_cache=$$BAZEL_CACHE \
+						//$$(dirname $$image); \
+					bazel_image_path=$$(find -L $$(bazel info output_path)/ -type f -name "$${image_basename}"); \
+				fi; \
+				target_dir="${sw_build_dir}/build-bin/$$(dirname $${target})"; \
+				mkdir -p $$target_dir; \
+				cp $$bazel_image_path $$target_dir/; \
+			else \
+				target="$$image""_export_${sw_build_device}"; \
+				${LOCK_SW_BUILD_DIR} "ninja -C ${sw_build_dir}/build-out $$target"; \
+			fi; \
 		fi; \
 	done;
 endif
@@ -124,13 +144,13 @@ cov_unr_vcs: cov_unr_build
 	@echo "[make]: cov_unr"
 	cd ${sv_flist_gen_dir} && ${cov_unr_run_cmd} ${cov_unr_run_opts}
 
-cov_unr_xcelium: 
+cov_unr_xcelium:
 	@echo "[make]: cov_unr"
 	mkdir -p ${cov_unr_dir}
 	cd ${cov_unr_dir} && ${cov_unr_run_cmd} ${cov_unr_run_opts}
 
 cov_unr_merge:
-	cd ${cov_unr_dir} && ${cov_merge_cmd} -init ${cov_unr_dir}/jgproject/sessionLogs/session_0/unr_imc_coverage_merge.cmd 
+	cd ${cov_unr_dir} && ${cov_merge_cmd} -init ${cov_unr_dir}/jgproject/sessionLogs/session_0/unr_imc_coverage_merge.cmd
 
 ifeq (${SIMULATOR}, xcelium)
   cov_unr: cov_unr_xcelium cov_unr_merge
