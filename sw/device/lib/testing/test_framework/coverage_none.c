502f04fde2287b2e4edcb3c653dc045a0defbbaf
Miguel Young de la Sota <mcyoung@google.com>
2022-04-26 15:50:41 -0400

    [testing] Rename test_coverage.h to coverage.h
    Signed-off-by: Miguel Young de la Sota <mcyoung@google.com>

--- sw/device/lib/testing/test_framework/coverage_none.c
+++ sw/device/lib/testing/test_framework/coverage_none.c
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "sw/device/lib/testing/test_framework/test_coverage.h"
+#include "sw/device/lib/testing/test_framework/coverage.h"
 
 // This NOP function gets linked in when coverage is disabled. See
 // `test_coverage_llvm.c` for its actual definition when coverage is enabled.
