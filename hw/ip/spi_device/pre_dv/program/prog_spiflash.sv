31fb6e444258afa8ab1fc5dd1ef906e2f093a4c8
Eunchan Kim <eunchan@opentitan.org>
2022-05-04 16:27:09 -0700

    test(spi_device): SPI Flash behavioral model
    This commit emulates SPI Flash behavior. It supports JEDEC ID, STATUS,
    READ COMMANDS.
    
    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>

--- hw/ip/spi_device/pre_dv/program/prog_spiflash.sv
+++
@@ -1,9 +0,0 @@
-// Copyright lowRISC contributors.
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-//
-// Behavioral model of SPI Flash
-
-program prog_spiflash;
-
-endprogram : prog_spiflash
