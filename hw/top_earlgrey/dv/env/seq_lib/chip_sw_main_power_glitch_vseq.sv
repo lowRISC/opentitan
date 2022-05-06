fca7dad6e0e41d8a02359de060fd16539f5cf59e
Jaedon Kim <jdonjdon@google.com>
2022-05-06 00:37:44 +0000

    dv,top,pwrmgr] fix pwrmgr_sleep_power_glitch_reset regression failure
    Signed-off-by: Jaedon Kim <jdonjdon@google.com>

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_main_power_glitch_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_main_power_glitch_vseq.sv
@@ -10,9 +10,16 @@ class chip_sw_main_power_glitch_vseq extends chip_sw_base_vseq;
   rand int cycles_after_trigger;
   constraint cycles_after_trigger_c {cycles_after_trigger inside {[0 : 8]};}
 
+  virtual task pre_start();
+    super.pre_start();
+    // disable expected assertion error for power glitch test
+    $assertoff(0,"pwrmgr_ast_sva_if");
+    $assertoff(1,"tb.dut.top_earlgrey.pwrmgr_rstmgr_sva_if.MainPwrRstOff_A");
+    $assertoff(1,"tb.dut.top_earlgrey.pwrmgr_rstmgr_sva_if.MainPwrRstOn_A");
+  endtask
+
   virtual task body();
     super.body();
-
     // Wait until we reach the SW test state.
     wait(cfg.sw_test_status_vif.sw_test_status == SwTestStatusInTest);
     cfg.ast_supply_vif.glitch_vcmain_pok_on_next_trigger(cycles_after_trigger);
@@ -21,6 +28,5 @@ class chip_sw_main_power_glitch_vseq extends chip_sw_base_vseq;
     // before re-enabling assertion
     wait(cfg.sw_test_status_vif.sw_test_status == SwTestStatusInBootRom);
     cfg.ast_supply_vif.reenable_vcmain_assertion();
-
   endtask
 endclass
