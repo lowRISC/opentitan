d10f31152e65f33c6501e9f0b188310333b9dfa6
Miles Dai <milesdai@google.com>
2022-05-10 10:48:17 -0400

    [ci] Move verible checks to slow-lint
    Signed-off-by: Miles Dai <milesdai@google.com>

--- ci/jobs/slow-lint.sh
+++ ci/jobs/slow-lint.sh
@@ -16,3 +16,12 @@ bazel run buildifier_check
 
 echo "### Check vendored directories are up-to-date"
 ci/scripts/check-vendoring.sh
+
+echo -e "\n### Style-Lint RTL Verilog source files with Verible"
+ci/scripts/verible-lint.sh rtl
+
+echo -e "\n### Style-Lint DV Verilog source files with Verible"
+ci/scripts/verible-lint.sh dv
+
+echo -e "\n### Style-Lint FPV Verilog source files with Verible"
+ci/scripts/verible-lint.sh fpv
