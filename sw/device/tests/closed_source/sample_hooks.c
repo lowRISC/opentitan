4e59e75a3ebb676cdc27e9889f5e954732ceff62
Chris Frantz <cfrantz@google.com>
2022-05-12 14:20:17 -0700

    [bazel] Add infrastructure for linking in external test hooks
    1. Define a `hooks_setup` repo that can point either to a dummy `test_hooks`
       repository or to a manufacturer-provided repository.  The `hooks_setup`
       repo writes out a configuration file (`repos.bzl`) which points to the
       manufacturer supplied repository.
    2. Add a dummy `test_hooks` repository with do-nothing hooks.  This repo
       serves as an example to manufacturers and as a source of empty hooks
       so tests which need hooks can always have a `@test_hooks//...` label
       in their `deps` and will be supplied with empty functions when external
       test hooks are not in use.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ sw/device/tests/closed_source/sample_hooks.c
@@ -0,0 +1,23 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stddef.h>
+
+#include "sw/device/lib/runtime/log.h"
+
+bool closed_source_pre_test_hook(void) {
+  // LOG_INFO("open_source_pre_test_hook");
+
+  // Perform whatever pre-test setup needs to be done.
+  // Return true if successful, false if unsuccessful.
+  return true;
+}
+
+bool closed_source_post_test_hook(void) {
+  // LOG_INFO("open_source_post_test_hook");
+
+  // Perform whatever post-test teardown needs to be done.
+  // Return true if successful, false if unsuccessful.
+  return true;
+}
