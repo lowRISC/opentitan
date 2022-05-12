03da2f961907aaef83b12e755bf6fb3e62312459
Cindy Chen <chencindy@opentitan.org>
2022-05-12 13:43:17 -0700

    [dv/chip] Move a checking to common testutils file
    This PR moves the `check_lc_state_transition_count` task to the common
    `lc_ctrl_testuitls` file so that it can be used by other tests.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

--- sw/device/lib/testing/lc_ctrl_testutils.c
+++ sw/device/lib/testing/lc_ctrl_testutils.c
@@ -30,3 +30,14 @@ bool lc_ctrl_testutils_debug_func_enabled(const dif_lc_ctrl_t *lc_ctrl) {
       return false;
   }
 }
+
+void lc_ctrl_testutils_check_transition_count(const dif_lc_ctrl_t *lc_ctrl,
+                                              uint8_t exp_lc_count) {
+  LOG_INFO("Read LC count and check with expect_val=%0d", exp_lc_count);
+  uint8_t lc_count;
+  CHECK(dif_lc_ctrl_get_attempts(lc_ctrl, &lc_count) == kDifOk,
+        "Read lc_count register failed!");
+  CHECK(lc_count == exp_lc_count,
+        "LC_count error, expected %0d but actual count is %0d", exp_lc_count,
+        lc_count);
+}
