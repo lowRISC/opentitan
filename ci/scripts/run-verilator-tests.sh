43acab4628b83feeea9c213c5559f955794684d4
Miles Dai <milesdai@google.com>
2022-05-12 17:21:39 -0400

    [ci] Fix bazel query bug to run a larger set of tests
    Signed-off-by: Miles Dai <milesdai@google.com>

--- ci/scripts/run-verilator-tests.sh
+++ ci/scripts/run-verilator-tests.sh
@@ -6,16 +6,16 @@
 set -e
 
 # Increase the test_timeout due to slow performance on CI
-ci/bazelisk.sh test \
+
+./bazelisk.sh query "filter('sim_verilator\_.*\_smoketest', tests(//sw/device/...))" | \
+xargs ci/bazelisk.sh test \
     --build_tests_only=true \
     --test_timeout=2400,2400,3600,-1 \
     --local_test_jobs=4 \
     --local_cpu_resources=4 \
-    --test_timeout_filters=short,moderate \
     --test_tag_filters=verilator,-failing_verilator,-broken \
     --test_output=errors \
     --//hw:verilator_options=--threads,1 \
-    $(bazel query 'filter("sim_verilator\_.*\_smoketest", tests(//sw/device/...))-attr("tags", "failing_verilator", tests(//sw/device/...))') \
     //sw/device/tests:sim_verilator_crt_test \
     //sw/device/tests:sim_verilator_otbn_randomness_test \
     //sw/device/tests:sim_verilator_otbn_irq_test \
@@ -28,4 +28,4 @@ ci/bazelisk.sh test \
     //sw/device/silicon_creator/lib/drivers:sim_verilator_retention_sram_functest \
     //sw/device/silicon_creator/lib/drivers:sim_verilator_alert_functest \
     //sw/device/silicon_creator/lib/drivers:sim_verilator_watchdog_functest \
-    //sw/device/silicon_creator/lib:sim_verilator_irq_asm_functest \
+    //sw/device/silicon_creator/lib:sim_verilator_irq_asm_functest
