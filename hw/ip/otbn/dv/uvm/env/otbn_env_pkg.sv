6e19fb16471841353ab77d73b6d92f9b56d4ca34
Rupert Swarbrick <rswarbrick@lowrisc.org>
2022-05-13 17:14:22 +0100

    [otbn,dv] Don't connect up "idle_o" output in TB
    The checking for this signal is handled by the otbn_idle_checker
    instance which is bound in to the otbn module. The connections that
    we'd wired up here didn't actually do anything, and in fact were
    wrong! This signal turned into a mubi4_t back in March (6a18e34) and
    the only result was an Xcelium warning about mismatching port widths.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/otbn/dv/uvm/env/otbn_env_pkg.sv
+++ hw/ip/otbn/dv/uvm/env/otbn_env_pkg.sv
@@ -52,7 +52,6 @@ package otbn_env_pkg;
   parameter int KEY_RSP_DATA_SIZE = $bits(otp_ctrl_pkg::otbn_otp_key_rsp_t);
 
   // typedefs
-  typedef virtual pins_if #(1)     idle_vif;
   typedef virtual otbn_escalate_if escalate_vif;
   typedef logic [TL_AIW-1:0]       tl_source_t;
   typedef key_sideload_agent#(keymgr_pkg::otbn_key_req_t) otbn_sideload_agent;
