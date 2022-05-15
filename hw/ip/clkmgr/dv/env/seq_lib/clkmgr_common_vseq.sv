8d4533f0b7e8ebf5318163258dd1b640716f3293
Jaedon Kim <jdonjdon@google.com>
2022-05-15 17:53:19 +0000

    [dv,clkmgr] clkmgr regression fix
    - add more delay for shadow registers in slow clock domain
    
    Signed-off-by: Jaedon Kim <jdonjdon@google.com>

--- hw/ip/clkmgr/dv/env/seq_lib/clkmgr_common_vseq.sv
+++ hw/ip/clkmgr/dv/env/seq_lib/clkmgr_common_vseq.sv
@@ -8,6 +8,16 @@ class clkmgr_common_vseq extends clkmgr_base_vseq;
   constraint num_trans_c {num_trans inside {[1 : 2]};}
   `uvm_object_new
 
+  virtual task pre_start();
+    csr_excl_item csr_excl = ral.get_excl_item();
+    super.pre_start();
+
+    // Remove rw1c type from same_csr_outstanding
+    if (common_seq_type == "same_csr_outstanding") begin
+      csr_excl.add_excl("clkmgr_reg_block.recov_err_code", CsrExclWrite);
+    end
+  endtask
+
   virtual task body();
     run_common_vseq_wrapper(num_trans);
   endtask : body
@@ -47,5 +57,46 @@ class clkmgr_common_vseq extends clkmgr_base_vseq;
     // csr test.
     cfg.clkmgr_vif.idle_i = {NUM_TRANS{MuBi4False}};
   endtask : initialize_on_start
+
+  // override shadow_reg_errors task
+  // to cover shadow regs under clock div2, div4
+  task poke_and_check_storage_error(dv_base_reg shadowed_csr);
+    uvm_reg_data_t  err_val, origin_val, rand_val;
+    bkdr_reg_path_e kind;
+    int             shadow_reg_width = shadowed_csr.get_msb_pos() + 1;
+    string          alert_name = shadowed_csr.get_storage_err_alert_name();
+
+    `DV_CHECK_STD_RANDOMIZE_WITH_FATAL(
+                                       kind, kind inside {BkdrRegPathRtl, BkdrRegPathRtlShadow};)
+    csr_peek(.ptr(shadowed_csr), .value(origin_val), .kind(kind));
+    err_val = get_shadow_reg_diff_val(shadowed_csr, origin_val);
+
+    csr_poke(.ptr(shadowed_csr), .value(err_val), .kind(kind), .predict(1));
+    predict_shadow_reg_status(.predict_storage_err(shadowed_csr.get_shadow_storage_err()));
+    `uvm_info(`gfn, $sformatf("backdoor write %s through %s with value 0x%0h",
+                              shadowed_csr.`gfn, kind.name, err_val), UVM_HIGH);
+
+    // This non-blocking task checks if the alert is continuously firing until reset is issued.
+    if (cfg.m_alert_agent_cfg.exists(alert_name)) begin
+      // increase clock delay for shadow regs under clk div2, div4
+      cfg.clk_rst_vif.wait_clks(10);
+      check_fatal_alert_nonblocking(alert_name);
+    end
+
+    // Wait random clock cycles and ensure the fatal alert is continuously firing.
+    cfg.clk_rst_vif.wait_clks($urandom_range(10, 100));
+
+    // Check if CSR write is blocked.
+    `DV_CHECK_STD_RANDOMIZE_FATAL(rand_val);
+    shadow_reg_wr(.csr(shadowed_csr), .wdata(rand_val), .en_shadow_wr(1));
+    csr_rd_check(.ptr(shadowed_csr), .compare_vs_ral(1));
+
+    // Backdoor write back to original value and ensure the fatal alert is continuously firing.
+    csr_poke(.ptr(shadowed_csr), .value(origin_val), .kind(kind), .predict(1));
+
+    read_check_shadow_reg_status("Poke_and_check_storage_error task");
+    cfg.clk_rst_vif.wait_clks($urandom_range(10, 100));
+  endtask
+
 
 endclass
