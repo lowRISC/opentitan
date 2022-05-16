272b5bca48ff99cada9e045e00c41ce293387432
Chris Frantz <cfrantz@google.com>
2022-05-16 10:18:23 -0700

    [bazel, ci] Create a CI configuration for the FPGA board.
    1. Allow overriding the UART setup in opentitantool.  serialport-rs
       wants to use udev to associate uarts with the CW310 board, but udev
       is not available in the CI's docker container.
    2. Add a bazel `define` for the CI environment.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/host/opentitanlib/src/backend/cw310.rs
+++ sw/host/opentitanlib/src/backend/cw310.rs
@@ -2,16 +2,33 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::transport::cw310::CW310;
-use crate::transport::Transport;
 use anyhow::Result;
+use structopt::StructOpt;
 
 use crate::backend::BackendOpts;
+use crate::transport::cw310::CW310;
+use crate::transport::Transport;
+
+#[derive(Debug, StructOpt)]
+pub struct Cw310Opts {
+    #[structopt(
+        long,
+        default_value,
+        help = "Comma-separated list of CW310 UARTs for non-udev environments. List the console uart first."
+    )]
+    pub cw310_uarts: String,
+}
 
 pub fn create(args: &BackendOpts) -> Result<Box<dyn Transport>> {
+    let uarts = args
+        .cw310_opts
+        .cw310_uarts
+        .split(',')
+        .collect::<Vec<&str>>();
     Ok(Box::new(CW310::new(
         args.usb_vid,
         args.usb_vid,
         args.usb_serial.as_deref(),
+        &uarts,
     )?))
 }
