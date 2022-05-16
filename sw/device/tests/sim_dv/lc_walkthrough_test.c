256f09bd6cfc255df5636ec75230392536bdc2f7
Cindy Chen <chencindy@opentitan.org>
2022-05-16 15:49:26 -0700

    [dv/chip] LC_walkthrough test [PART3]
    Adding a transition to RMA state using the RMA token.
    This PR adds the following transitions:
    1). RAW -> TESTUNLOCK0 -> PROD -> RMA
    2). RAW -> TESTUNLOCK0 -> DEV -> RMA
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

--- sw/device/tests/sim_dv/lc_walkthrough_test.c
+++ sw/device/tests/sim_dv/lc_walkthrough_test.c
@@ -6,6 +6,7 @@
 #include "sw/device/lib/base/mmio.h"
 #include "sw/device/lib/dif/dif_lc_ctrl.h"
 #include "sw/device/lib/dif/dif_otp_ctrl.h"
+#include "sw/device/lib/dif/dif_rstmgr.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/lc_ctrl_testutils.h"
 #include "sw/device/lib/testing/otp_ctrl_testutils.h"
@@ -17,11 +18,12 @@
 
 static dif_lc_ctrl_t lc;
 static dif_otp_ctrl_t otp;
+static dif_rstmgr_t rstmgr;
 
 /**
- * Track LC state transition tokens.
+ * Track LC state transition tokens and destination state.
  *
- * These tokens will be further randomized and overridden by the testbench.
+ * These variables will be further randomized and overridden by the testbench.
  */
 
 static volatile const uint8_t kDestState;
@@ -44,19 +46,106 @@ static volatile const uint8_t kOtpUnlockToken[LC_TOKEN_SIZE] = {
     0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
 };
 
+// LC rma token value for LC state transition.
+static volatile const uint8_t kLcRmaToken[LC_TOKEN_SIZE] = {
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+};
+
+// LC rma token value in OTP secret2 partition.
+static volatile const uint8_t kOtpRmaToken[LC_TOKEN_SIZE] = {
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+};
+
+static void lock_otp_secret0_partition() {
+  uint64_t otp_unlock_token_0 = 0;
+  uint64_t otp_unlock_token_1 = 0;
+  for (int i = 0; i < LC_TOKEN_SIZE; i++) {
+    if (i < LC_TOKEN_SIZE / 2) {
+      otp_unlock_token_0 |= (uint64_t)kOtpUnlockToken[i] << (i * 8);
+    } else {
+      otp_unlock_token_1 |= (uint64_t)kOtpUnlockToken[i]
+                            << ((i - LC_TOKEN_SIZE / 2) * 8);
+    }
+  }
+
+  uint64_t otp_exit_token_0 = 0;
+  uint64_t otp_exit_token_1 = 0;
+  for (int i = 0; i < LC_TOKEN_SIZE; i++) {
+    if (i < LC_TOKEN_SIZE / 2) {
+      otp_exit_token_0 |= (uint64_t)kOtpExitToken[i] << (i * 8);
+    } else {
+      otp_exit_token_1 |= (uint64_t)kOtpExitToken[i]
+                          << ((i - LC_TOKEN_SIZE / 2) * 8);
+    }
+  }
+
+  CHECK_DIF_OK(dif_otp_ctrl_dai_program64(&otp, kDifOtpCtrlPartitionSecret0,
+                                          /*address=*/0x0,
+                                          /*value=*/otp_unlock_token_0));
+  otp_ctrl_testutils_wait_for_dai(&otp);
+  CHECK_DIF_OK(dif_otp_ctrl_dai_program64(&otp, kDifOtpCtrlPartitionSecret0,
+                                          /*address=*/0x8,
+                                          /*value=*/otp_unlock_token_1));
+  otp_ctrl_testutils_wait_for_dai(&otp);
+  CHECK_DIF_OK(dif_otp_ctrl_dai_program64(&otp, kDifOtpCtrlPartitionSecret0,
+                                          /*address=*/0x10,
+                                          /*value=*/otp_exit_token_0));
+  otp_ctrl_testutils_wait_for_dai(&otp);
+  CHECK_DIF_OK(dif_otp_ctrl_dai_program64(&otp, kDifOtpCtrlPartitionSecret0,
+                                          /*address=*/0x18,
+                                          /*value=*/otp_exit_token_1));
+  otp_ctrl_testutils_wait_for_dai(&otp);
+
+  CHECK_DIF_OK(dif_otp_ctrl_dai_digest(&otp, kDifOtpCtrlPartitionSecret0,
+                                       /*digest=*/0));
+  otp_ctrl_testutils_wait_for_dai(&otp);
+}
+
+static void lock_otp_secret2_partition() {
+  // Write LC RMA tokens to OTP secret2 partition.
+  uint64_t otp_rma_token_0 = 0;
+  uint64_t otp_rma_token_1 = 0;
+  for (int i = 0; i < LC_TOKEN_SIZE; i++) {
+    if (i < LC_TOKEN_SIZE / 2) {
+      otp_rma_token_0 |= (uint64_t)kOtpRmaToken[i] << (i * 8);
+    } else {
+      otp_rma_token_1 |= (uint64_t)kOtpRmaToken[i]
+                         << ((i - LC_TOKEN_SIZE / 2) * 8);
+    }
+  }
+  CHECK_DIF_OK(dif_otp_ctrl_dai_program64(&otp, kDifOtpCtrlPartitionSecret2,
+                                          /*address=*/0x0,
+                                          /*value=*/otp_rma_token_0));
+  otp_ctrl_testutils_wait_for_dai(&otp);
+  CHECK_DIF_OK(dif_otp_ctrl_dai_program64(&otp, kDifOtpCtrlPartitionSecret2,
+                                          /*address=*/0x8,
+                                          /*value=*/otp_rma_token_1));
+  otp_ctrl_testutils_wait_for_dai(&otp);
+
+  CHECK_DIF_OK(dif_otp_ctrl_dai_digest(&otp, kDifOtpCtrlPartitionSecret2,
+                                       /*digest=*/0));
+  otp_ctrl_testutils_wait_for_dai(&otp);
+}
+
 /**
- * Walkthrough LC state: RAW -> TestUnlock0 -> Destination state.
+ * Walkthrough LC state: RAW -> TestUnlock0 -> Destination -> RMA (if
+ * applicable).
  *
  * 1). Preload OTP RAW image file.
  * 2). DV sequence drives JTAG interface to write RawUnlockToken and
  * transfers LC state to TestUnlock0 state.
  * 3). In TestUnlock0 state, SW programs OTP secret0 partition to write
  * ExitToken and TestUnlockToken.
+ * Also programs OTP secret2 partition to write RmaToken.
  * 4). DV sequence issues reset to lock OTP secret0 partition.
  * 5). SW requests a LC state transfer to ProdEnd state with the correct
  * TestLockToken.
  * 6). DV sequence issues reset and SW check if LC transfers to the destination
  * state.
+ * 7). If the destination state can be transferred to RMA state, will issue
+ * another LC state transfer to RMA state.
  *
  * Note that the destination state is a runtime input from the testbench.
  */
@@ -71,6 +160,9 @@ bool test_main(void) {
       mmio_region_from_addr(TOP_EARLGREY_OTP_CTRL_CORE_BASE_ADDR);
   CHECK_DIF_OK(dif_otp_ctrl_init(otp_reg, &otp));
 
+  CHECK_DIF_OK(dif_rstmgr_init(
+      mmio_region_from_addr(TOP_EARLGREY_RSTMGR_AON_BASE_ADDR), &rstmgr));
+
   LOG_INFO("Read and check LC state and count.");
   dif_lc_ctrl_state_t curr_state;
   CHECK_DIF_OK(dif_lc_ctrl_get_state(&lc, &curr_state));
@@ -82,59 +174,20 @@ bool test_main(void) {
         &otp, kDifOtpCtrlPartitionSecret0, &secret0_locked));
 
     if (!secret0_locked) {
-      // Write LC tokens to OTP secret0 partition.
-      uint64_t otp_unlock_token_0 = 0;
-      uint64_t otp_unlock_token_1 = 0;
-      for (int i = 0; i < LC_TOKEN_SIZE; i++) {
-        if (i < LC_TOKEN_SIZE / 2) {
-          otp_unlock_token_0 |= (uint64_t)kOtpUnlockToken[i] << (i * 8);
-        } else {
-          otp_unlock_token_1 |= (uint64_t)kOtpUnlockToken[i]
-                                << ((i - LC_TOKEN_SIZE / 2) * 8);
-        }
-      }
-
-      uint64_t otp_exit_token_0 = 0;
-      uint64_t otp_exit_token_1 = 0;
-      for (int i = 0; i < LC_TOKEN_SIZE; i++) {
-        if (i < LC_TOKEN_SIZE / 2) {
-          otp_exit_token_0 |= (uint64_t)kOtpExitToken[i] << (i * 8);
-        } else {
-          otp_exit_token_1 |= (uint64_t)kOtpExitToken[i]
-                              << ((i - LC_TOKEN_SIZE / 2) * 8);
-        }
-      }
-
-      CHECK_DIF_OK(dif_otp_ctrl_dai_program64(&otp, kDifOtpCtrlPartitionSecret0,
-                                              /*address=*/0x0,
-                                              /*value=*/otp_unlock_token_0));
-      otp_ctrl_testutils_wait_for_dai(&otp);
-
-      CHECK_DIF_OK(dif_otp_ctrl_dai_program64(&otp, kDifOtpCtrlPartitionSecret0,
-                                              /*address=*/0x8,
-                                              /*value=*/otp_unlock_token_1));
-      otp_ctrl_testutils_wait_for_dai(&otp);
-
-      CHECK_DIF_OK(dif_otp_ctrl_dai_program64(&otp, kDifOtpCtrlPartitionSecret0,
-                                              /*address=*/0x10,
-                                              /*value=*/otp_exit_token_0));
-      otp_ctrl_testutils_wait_for_dai(&otp);
-
-      CHECK_DIF_OK(dif_otp_ctrl_dai_program64(&otp, kDifOtpCtrlPartitionSecret0,
-                                              /*address=*/0x18,
-                                              /*value=*/otp_exit_token_1));
-      otp_ctrl_testutils_wait_for_dai(&otp);
-
-      CHECK_DIF_OK(dif_otp_ctrl_dai_digest(&otp, kDifOtpCtrlPartitionSecret0,
-                                           /*digest=*/0));
-      otp_ctrl_testutils_wait_for_dai(&otp);
-
-      LOG_INFO("Wrote and locked OTP secret0 partition!");
+      LOG_INFO("In TestUnlocked0 state. Write and lock OTP secret0 partition.");
+      lock_otp_secret0_partition();
+      LOG_INFO("Written and locked OTP secret0 partition!");
+      CHECK_DIF_OK(dif_rstmgr_software_device_reset(&rstmgr));
       wait_for_interrupt();
+      // Print out LcRmaToken to avoid SW compile error saying kLcRmaToken is
+      // unused in certain state trasition tests.
+      LOG_INFO("LC RMA token start with %0h", kLcRmaToken[0]);
       // Unreachable
       return false;
     } else {
-      // Issue a LC state transfer to destination state.
+      LOG_INFO(
+          "In TestUnlocked0 state. Issue LC state transfer to destination "
+          "state.");
       dif_lc_ctrl_token_t token;
       for (int i = 0; i < LC_TOKEN_SIZE; i++) {
         if (kDestState != kDifLcCtrlStateRma) {
@@ -158,10 +211,59 @@ bool test_main(void) {
       // Unreachable
       return false;
     }
+  } else if (curr_state == kDestState) {
+    bool secret2_locked;
+    CHECK_DIF_OK(dif_otp_ctrl_is_digest_computed(
+        &otp, kDifOtpCtrlPartitionSecret2, &secret2_locked));
+
+    if (!secret2_locked) {
+      LOG_INFO(
+          "In destination state. Check LC count and lock secret2 partition.");
+      CHECK(curr_state == kDestState);
+      lc_ctrl_testutils_check_transition_count(&lc, 2);
+
+      lock_otp_secret2_partition();
+      LOG_INFO("Written and locked OTP secret2 partition in next power cycle!");
+      CHECK_DIF_OK(dif_rstmgr_software_device_reset(&rstmgr));
+      wait_for_interrupt();
+      return false;
+
+    } else {
+      if (kDestState == kDifLcCtrlStateRma ||
+          kDestState == kDifLcCtrlStateProdEnd) {
+        lc_ctrl_testutils_check_transition_count(&lc, 2);
+        LOG_INFO("LC transition done!");
+        return true;
+      } else {
+        // Check LC counts and transit to RMA state.
+        LOG_INFO(
+            "In destination state. Check LC count and transfer to RMA "
+            "state.");
+        lc_ctrl_testutils_check_transition_count(&lc, 2);
+
+        // Issue a LC state transfer to destination state.
+        dif_lc_ctrl_token_t token;
+        for (int i = 0; i < LC_TOKEN_SIZE; i++) {
+          token.data[i] = kLcRmaToken[i];
+        }
+        CHECK_DIF_OK(dif_lc_ctrl_mutex_try_acquire(&lc));
+        dif_lc_ctrl_settings_t settings;
+        // TODO: randomize using external or internal clock.
+        settings.clock_select = kDifLcCtrlExternalClockEn;
+        CHECK_DIF_OK(
+            dif_lc_ctrl_transition(&lc, kDifLcCtrlStateRma, &token, &settings),
+            "LC_transition failed!");
+
+        LOG_INFO("Waiting for LC RMA transtition done and reboot.");
+        wait_for_interrupt();
+        // unreachable
+        return false;
+      }
+    }
   } else {
-    // Check LC enters ProdEnd state.
-    CHECK(curr_state == kDestState);
-    lc_ctrl_testutils_check_transition_count(&lc, 2);
+    LOG_INFO("In RMA state. Check LC count and exit.");
+    CHECK(curr_state == kDifLcCtrlStateRma);
+    lc_ctrl_testutils_check_transition_count(&lc, 3);
     return true;
   }
 }
