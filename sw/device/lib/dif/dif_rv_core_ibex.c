6372f683248c32b7c0a673e5184b539c52121661
Douglas Reis <doreis@lowrisc.org>
2022-05-18 10:56:01 +0100

    [dif, rv_core_ibex] Add address translation and unittest
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

---
+++ sw/device/lib/dif/dif_rv_core_ibex.c
@@ -0,0 +1,163 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/dif/dif_rv_core_ibex.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "sw/device/lib/base/memory.h"
+#include "sw/device/lib/base/mmio.h"
+
+// #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+#include "rv_core_ibex_regs.h"
+
+typedef struct ibex_addr_translation_regs {
+  uint32_t ibus_maching;
+  uint32_t ibus_remap;
+  uint32_t ibus_en;
+  uint32_t ibus_lock;
+  uint32_t dbus_maching;
+  uint32_t dbus_remap;
+  uint32_t dbus_en;
+  uint32_t dbus_lock;
+} ibex_addr_translation_regs_t;
+
+static const ibex_addr_translation_regs_t
+    kRegsMap[kDifRvCoreIbexAddrTranslationSlotCount] = {
+        [kDifRvCoreIbexAddrTranslationSlot_0] =
+            {
+                .ibus_maching = RV_CORE_IBEX_IBUS_ADDR_MATCHING_0_REG_OFFSET,
+                .ibus_remap = RV_CORE_IBEX_IBUS_REMAP_ADDR_0_REG_OFFSET,
+                .ibus_en = RV_CORE_IBEX_IBUS_ADDR_EN_0_REG_OFFSET,
+                .ibus_lock = RV_CORE_IBEX_IBUS_REGWEN_0_REG_OFFSET,
+                .dbus_maching = RV_CORE_IBEX_DBUS_ADDR_MATCHING_0_REG_OFFSET,
+                .dbus_remap = RV_CORE_IBEX_DBUS_REMAP_ADDR_0_REG_OFFSET,
+                .dbus_en = RV_CORE_IBEX_DBUS_ADDR_EN_0_REG_OFFSET,
+                .dbus_lock = RV_CORE_IBEX_DBUS_REGWEN_0_REG_OFFSET,
+            },
+        [kDifRvCoreIbexAddrTranslationSlot_1] =
+            {
+                .ibus_maching = RV_CORE_IBEX_IBUS_ADDR_MATCHING_1_REG_OFFSET,
+                .ibus_remap = RV_CORE_IBEX_IBUS_REMAP_ADDR_1_REG_OFFSET,
+                .ibus_en = RV_CORE_IBEX_IBUS_ADDR_EN_1_REG_OFFSET,
+                .ibus_lock = RV_CORE_IBEX_IBUS_REGWEN_1_REG_OFFSET,
+                .dbus_maching = RV_CORE_IBEX_DBUS_ADDR_MATCHING_1_REG_OFFSET,
+                .dbus_remap = RV_CORE_IBEX_DBUS_REMAP_ADDR_1_REG_OFFSET,
+                .dbus_en = RV_CORE_IBEX_DBUS_ADDR_EN_1_REG_OFFSET,
+                .dbus_lock = RV_CORE_IBEX_DBUS_REGWEN_1_REG_OFFSET,
+            },
+};
+
+/**
+ * Convert the region address and size into a natural power of two address.
+ *
+ * @param addr Region start address.
+ * @param size region size.
+ * @return Napot address
+ */
+static uint32_t to_napot(uint32_t addr, size_t size) {
+  return addr | (size - 1) >> 1;
+}
+
+/**
+ * Split a natural power of two address into a start address and size.
+ *
+ * @param napot Address formated in NAPOT.
+ * @param size  Pointer to receive the region size.
+ * @return The region start address.
+ */
+static uint32_t from_napot(uint32_t napot, size_t *size) {
+  for (size_t i = 1; i < sizeof(uint32_t) * 8; ++i) {
+    uint32_t ref = (1u << i) - 1;
+    if ((napot & ref) == ref >> 1) {
+      *size = 1u << i;
+      break;
+    }
+  }
+  return napot & ~((*size - 1) >> 1);
+}
+
+dif_result_t dif_rv_core_ibex_configure_addr_translation(
+    const dif_rv_core_ibex_t *rv_core_ibex,
+    dif_rv_core_ibex_addr_translation_slot_t slot,
+    dif_rv_core_ibex_addr_translation_region_t region) {
+  if (rv_core_ibex == NULL || slot >= kDifRvCoreIbexAddrTranslationSlotCount) {
+    return kDifBadArg;
+  }
+
+  if (!bitfield_is_power_of_two32(region.dbus.size) ||
+      !bitfield_is_power_of_two32(region.ibus.size)) {
+    return kDifBadArg;
+  }
+
+  const ibex_addr_translation_regs_t regs = kRegsMap[slot];
+
+  if (mmio_region_read32(rv_core_ibex->base_addr, regs.dbus_lock) == 0 ||
+      mmio_region_read32(rv_core_ibex->base_addr, regs.ibus_lock) == 0) {
+    return kDifLocked;
+  }
+
+  uint32_t mask = to_napot(region.ibus.matching_addr, region.ibus.size);
+  mmio_region_write32(rv_core_ibex->base_addr, regs.ibus_maching, mask);
+  mmio_region_write32(rv_core_ibex->base_addr, regs.ibus_remap,
+                      region.ibus.remap_addr);
+  mmio_region_write32(rv_core_ibex->base_addr, regs.ibus_en, 1);
+
+  mask = to_napot(region.dbus.matching_addr, region.dbus.size);
+  mmio_region_write32(rv_core_ibex->base_addr, regs.dbus_maching, mask);
+  mmio_region_write32(rv_core_ibex->base_addr, regs.dbus_remap,
+                      region.dbus.remap_addr);
+  mmio_region_write32(rv_core_ibex->base_addr, regs.dbus_en, 1);
+
+  return kDifOk;
+}
+
+dif_result_t dif_rv_core_ibex_read_addr_translation(
+    const dif_rv_core_ibex_t *rv_core_ibex,
+    dif_rv_core_ibex_addr_translation_slot_t slot,
+    dif_rv_core_ibex_addr_translation_region_t *region) {
+  if (rv_core_ibex == NULL || region == NULL ||
+      slot >= kDifRvCoreIbexAddrTranslationSlotCount) {
+    return kDifBadArg;
+  }
+
+  const ibex_addr_translation_regs_t regs = kRegsMap[slot];
+
+  uint32_t reg = mmio_region_read32(rv_core_ibex->base_addr, regs.ibus_maching);
+  region->ibus.matching_addr = from_napot(reg, &region->ibus.size);
+
+  region->ibus.remap_addr =
+      mmio_region_read32(rv_core_ibex->base_addr, regs.ibus_remap);
+
+  reg = mmio_region_read32(rv_core_ibex->base_addr, regs.dbus_maching);
+  region->dbus.matching_addr = from_napot(reg, &region->dbus.size);
+
+  region->dbus.remap_addr =
+      mmio_region_read32(rv_core_ibex->base_addr, regs.dbus_remap);
+
+  return kDifOk;
+}
+
+dif_result_t dif_rv_core_ibex_lock_addr_translation(
+    const dif_rv_core_ibex_t *rv_core_ibex,
+    dif_rv_core_ibex_addr_translation_slot_t slot) {
+  if (rv_core_ibex == NULL || slot >= kDifRvCoreIbexAddrTranslationSlotCount) {
+    return kDifBadArg;
+  }
+
+  const ibex_addr_translation_regs_t regs = kRegsMap[slot];
+
+  // Only locks in case it is not locked already.
+  if (mmio_region_read32(rv_core_ibex->base_addr, regs.dbus_lock) == 1) {
+    mmio_region_write32(rv_core_ibex->base_addr, regs.dbus_lock, 0);
+  }
+
+  if (mmio_region_read32(rv_core_ibex->base_addr, regs.ibus_lock) == 1) {
+    mmio_region_write32(rv_core_ibex->base_addr, regs.ibus_lock, 0);
+  }
+
+  return kDifOk;
+}
---
+++ sw/device/lib/dif/dif_rv_core_ibex.h
@@ -0,0 +1,122 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_DIF_DIF_RV_CORE_IBEX_H_
+#define OPENTITAN_SW_DEVICE_LIB_DIF_DIF_RV_CORE_IBEX_H_
+
+#include <stdint.h>
+
+#include "sw/device/lib/base/macros.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/dif/dif_base.h"
+
+#include "sw/device/lib/dif/autogen/dif_rv_core_ibex_autogen.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+/**
+ * Address translation slot.
+ */
+typedef enum dif_rv_core_ibex_addr_translation_slot {
+  kDifRvCoreIbexAddrTranslationSlot_0,
+  kDifRvCoreIbexAddrTranslationSlot_1,
+  kDifRvCoreIbexAddrTranslationSlotCount,
+} dif_rv_core_ibex_addr_translation_slot_t;
+
+/**
+ * Address tranlation matching region.
+ *
+ * The value programmed is done at power-of-2 alignment. For example, if the
+ * intended matching region is 0x8000_0000 to 0x8000_FFFF, the value
+ * programmed is 0x8000_7FFF.
+ *
+ * The value programmed can be determined from the translation granule. Assume
+ * the user wishes to translate a specific 64KB block to a different address:
+ * 64KB has a hex value of 0x10000. Subtract 1 from this value and then right
+ * shift by one to obtain 0x7FFF. This value is then logically OR'd with the
+ * upper address bits that would select which 64KB to translate.
+ */
+typedef struct dif_rv_core_ibex_addr_translation_pair {
+  /**
+   * Matching address (Virtual address).
+   * When an incoming transaction matches the matching
+   * region, it is redirected to the new address. If a transaction does not
+   * match, then it is directly passed through.
+   */
+  uintptr_t matching_addr;
+
+  /**
+   * Remap address (Physical address).
+   * The region where the matched transtaction will be
+   * redirected to.
+   */
+  uintptr_t remap_addr;
+
+  /**
+   * Address region size.
+   */
+  size_t size;
+} dif_rv_core_ibex_addr_translation_pair_t;
+
+/**
+ * Addresses translation region.
+ */
+typedef struct dif_rv_core_ibex_addr_translation_region {
+  /**
+   * Region representing the instruction bus.
+   */
+  dif_rv_core_ibex_addr_translation_pair_t ibus;
+
+  /**
+   * Region representing the data bus.
+   */
+  dif_rv_core_ibex_addr_translation_pair_t dbus;
+} dif_rv_core_ibex_addr_translation_region_t;
+
+/**
+ * Configure the instruction and data bus in the address translation `slot`.
+ *
+ * @param rv_core_ibex Handle.
+ * @param slot   Slot to be used.
+ * @param region Dbus and Ibus addresses.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_rv_core_ibex_configure_addr_translation(
+    const dif_rv_core_ibex_t *rv_core_ibex,
+    dif_rv_core_ibex_addr_translation_slot_t slot,
+    dif_rv_core_ibex_addr_translation_region_t region);
+
+/**
+ *
+ * @param rv_core_ibex Handle.
+ * @param slot Slot to be read.
+ * @param[out] region Pointer to receive the Dbus and Ibus addresses.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_rv_core_ibex_read_addr_translation(
+    const dif_rv_core_ibex_t *rv_core_ibex,
+    dif_rv_core_ibex_addr_translation_slot_t slot,
+    dif_rv_core_ibex_addr_translation_region_t *region);
+
+/**
+ * Lock the `slot` registers. Once locked it can no longer be unlocked until the
+ * next system reset.
+ *
+ * @param rv_core_ibex Handle.
+ * @param slot Slot to be locked.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_rv_core_ibex_lock_addr_translation(
+    const dif_rv_core_ibex_t *rv_core_ibex,
+    dif_rv_core_ibex_addr_translation_slot_t slot);
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_DIF_DIF_RV_CORE_IBEX_H_
