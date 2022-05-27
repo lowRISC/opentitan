1f08131660c53bad6e34f9c29cfe42aa8e1da7ea
Eunchan Kim <eunchan@opentitan.org>
2022-05-27 09:42:50 -0700

    chore(cdc): Pads Attribute
    Ignore pad attribute signals starting from IO_DIV4_CLK. They are assumed
    as static.
    
    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>

--- hw/top_earlgrey/cdc/cdc_waivers.tcl
+++ hw/top_earlgrey/cdc/cdc_waivers.tcl
@@ -68,3 +68,10 @@ set_rule_status -rule {W_MASYNC} -status {Waived}        \
 set_rule_status -rule {W_ASYNC_RST_FLOPS} -status {Waived} \
   -expression {(DrivingSignal=~"*.u_pinmux_strap_sampling.tap_strap_q*")} \
   -comment {Tester should ensure no jtag transactions when tap_strap is sampled}
+
+set_rule_status -rule {W_CNTL} -status {Waived} \
+  -expression {(Signal=~"*u_pinmux_aon.dio_pad_attr_*")} \
+  -comment {PAD Attributes are static signals.}
+set_rule_status -rule {W_DATA} -status {Waived} \
+  -expression {(Signal=~"*u_pinmux_aon.dio_pad_attr_*")} \
+  -comment {PAD Attributes are static signals.}
