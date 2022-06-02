a7a06bcb3d39820ae961b361ed277e9c77b9ae46
Jon Flatley <jflat@google.com>
2022-06-02 17:29:00 -0400

    [opentitantool/signer] Remove rom_ext_image_signer
    Now that opentitan tool supports image signing there is no need for the
    dedicated signer tool.
    
    Configure bazel to use opentitantool for signing and remove the old
    signer tool.
    
    Signed-off-by: Jon Flatley <jflat@google.com>

--- sw/host/rom_ext_image_tools/signer/image/src/lib.rs
+++
@@ -1,6 +0,0 @@
-// Copyright lowRISC contributors.
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-
-pub mod image;
-pub mod manifest;
