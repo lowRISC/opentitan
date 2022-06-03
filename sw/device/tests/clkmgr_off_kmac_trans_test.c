4a62f12f3cbf832c5a1b3d476ccbeece16c022cc
Guillermo Maturana <maturana@google.com>
2022-06-02 23:46:40 -0700

    [dw,chip_sw] Add clkmgr_off_trans tests
    Signed-off-by: Guillermo Maturana <maturana@google.com>

---
+++ sw/device/tests/clkmgr_off_kmac_trans_test.c
@@ -0,0 +1,13 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This tests that disabling the KMAC clock causes CSR accesses to it to fail.
+
+#include "sw/device/tests/clkmgr_off_trans_impl.h"
+
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+
+bool test_main() {
+  return execute_off_trans_test(kTopEarlgreyHintableClocksMainKmac);
+}
