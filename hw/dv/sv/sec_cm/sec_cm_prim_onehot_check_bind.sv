6d211c008ce67c0ec0849d6c8724dc0146cf490b
Weicai Yang <weicai@google.com>
2022-06-03 17:02:02 -0700

    [dv] Add common test for reg write enable check
    Address #12113
    - Inject a fault in the prim_onehot_check to trigger reg integrity error
    - Check status reg and fatal alert
    
    Only add it for sram_ctrl. Will create a separate PR to enable this for
    all other blocks
    
    Signed-off-by: Weicai Yang <weicai@google.com>

---
+++ hw/dv/sv/sec_cm/sec_cm_prim_onehot_check_bind.sv
@@ -0,0 +1,12 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+module sec_cm_prim_onehot_check_bind();
+  bind prim_onehot_check prim_onehot_check_if #(.AddrWidth(AddrWidth),
+                                                .OneHotWidth(OneHotWidth),
+                                                .AddrCheck(AddrCheck),
+                                                .EnableCheck(EnableCheck),
+                                                .StrictCheck(StrictCheck)
+                                              ) u_prim_onehot_check_if (.*);
+endmodule
