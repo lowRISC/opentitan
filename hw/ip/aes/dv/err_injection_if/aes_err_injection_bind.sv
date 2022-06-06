f0aa9fae18f68c0a643f7fca9e3f1075c0639f84
Cindy Chen <chencindy@opentitan.org>
2022-06-06 11:41:34 -0700

    [fpv/aes] Support SEC_CM tests
    FPV re-uses sva core files, and currently due to the fault injection, it
    includes UVM files and causes a compile error.
    In this PR, the proposed solution is to put fault injection to a
    separate core file.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

---
+++ hw/ip/aes/dv/err_injection_if/aes_err_injection_bind.sv
@@ -0,0 +1,41 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+module aes_err_injection_bind;
+
+  // bind fault inject if to control_fsm
+  bind aes_control_fsm signal_force
+    #(.Signal("aes_ctrl_cs"),
+      .IfName("aes_fi_vif"),
+      .SignalWidth(aes_env_pkg::StateWidth)
+     )
+  u_fi
+    (
+     .clk          (clk_i),
+     .rst_ni       (rst_ni)
+    );
+
+  // bind fault inject if to cipher fsm
+  bind aes_cipher_control_fsm signal_force
+    #(.Signal("aes_cipher_ctrl_cs"),
+      .IfName("aes_cipher_fi_vif"),
+      .SignalWidth(aes_env_pkg::StateWidth)
+     )
+  u_cipher_fi
+    (
+     .clk          (clk_i),
+     .rst_ni       (rst_ni)
+    );
+
+  // bind fault inject if to round counter
+  bind aes_ctr_fsm signal_force
+    #(.Signal("aes_ctr_cs"),
+      .IfName("aes_ctr_fi_vif"),
+      .SignalWidth(aes_env_pkg::StateWidth)
+     )
+  u_ctr_fi
+    (
+     .clk          (clk_i),
+     .rst_ni       (rst_ni)
+    );
+endmodule
