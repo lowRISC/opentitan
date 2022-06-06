29e7e143a5580cdb411c8b1d0696b10ca10d4a54
Alphan Ulusoy <alphan@google.com>
2022-06-06 13:58:36 -0400

    [sw/silicon_creator] Add OTP words to control default boot_data behavior
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

--- hw/ip/otp_ctrl/rtl/otp_ctrl_part_pkg.sv
+++ hw/ip/otp_ctrl/rtl/otp_ctrl_part_pkg.sv
@@ -322,7 +322,10 @@ package otp_ctrl_part_pkg;
     }),
     6400'({
       64'h7D7EA64D850E128D,
-      4736'h0, // unallocated space
+      4640'h0, // unallocated space
+      32'h0,
+      32'h0,
+      32'h0,
       32'h0,
       32'h0,
       32'h0,
