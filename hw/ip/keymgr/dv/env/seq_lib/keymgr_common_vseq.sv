d957c8654e10f029263a1bf10776eb0c7c98f536
Weicai Yang <weicai@google.com>
2022-06-07 15:00:30 -0700

    [dv] Enable reg_wr_check test for all blocks
    Signed-off-by: Weicai Yang <weicai@google.com>

--- hw/ip/keymgr/dv/env/seq_lib/keymgr_common_vseq.sv
+++ hw/ip/keymgr/dv/env/seq_lib/keymgr_common_vseq.sv
@@ -138,7 +138,7 @@ class keymgr_common_vseq extends keymgr_base_vseq;
           $assertoff(0, "tb.dut.KmacDataKnownO_A");
         end
       end
-      SecCmPrimSparseFsmFlop: begin
+      SecCmPrimSparseFsmFlop, SecCmPrimOnehot: begin
         // No need to disable any assertion
       end
       default: `uvm_fatal(`gfn, $sformatf("unexpected sec_cm_type %s", if_proxy.sec_cm_type.name))
