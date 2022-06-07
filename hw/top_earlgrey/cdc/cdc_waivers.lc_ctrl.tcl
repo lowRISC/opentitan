e0f729dade5552cb395b437a973f20bf387590da
Eunchan Kim <eunchan@opentitan.org>
2022-06-07 10:48:34 -0700

    chore(cdc): Waive LC sender output
    LC sender outputs (e.g. cpu_en) are one-time change after getting OTP
    LifeCycle State value. It keeps the value until changing the LC state or
    reset. It is guaranteed to be received on the other side.
    
    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>

--- hw/top_earlgrey/cdc/cdc_waivers.lc_ctrl.tcl
+++ hw/top_earlgrey/cdc/cdc_waivers.lc_ctrl.tcl
@@ -14,3 +14,6 @@
 set_rule_status -rule {W_FANOUT} -status {Waived} \
   -expression {(Driver =~ "*u_lc_ctrl*.u_prim_lc_sender_escalate_en*")} \
   -comment {No Reconvergence issue. Each IP handles Escalate En individually}
+
+# lc_sender output (to be changed once in a power up)
+set_rule_status -rule {W_CNTL} -status {Waived} -expression {((Signal=~"*u_lc_ctrl.*.u_prim_lc_sender_*_en.gen_flops*") || (Signal=~"*u_lc_ctrl.*.u_prim_lc_sender_*rma_req*.gen_flops*")) && (ReceivingFlop=~"*u_prim_flop_2sync*")} -comment {LC EN is one-time change during a power up. Slow to fast clock error can be ignored}
