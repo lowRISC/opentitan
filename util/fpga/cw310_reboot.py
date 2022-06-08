aa5cd4a5176907624636235132d50d65399ac46e
Drew Macrae <drewmacrae@google.com>
2022-06-07 22:24:25 -0400

    [ci] reset the cw310 at the start of a test to isolate from previous
    This requires the CW310 to be running firmware version 0.40.1 and will
    makes the content of
    https://github.com/newaetech/chipwhisperer/pull/410 be available in
    chipwhisperer to enable it to trigger a reset
    
    Signed-off-by: Drew Macrae <drewmacrae@google.com>

---
+++ util/fpga/cw310_reboot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+import chipwhisperer as cw
+
+
+def main():
+    target = cw.target(None, cw.targets.CW310)
+    print("Found CW310 with FW Version: " + target.fw_version_str)
+    target.reset_sam3u()
+
+
+if __name__ == "__main__":
+    main()
