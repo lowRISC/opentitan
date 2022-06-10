9f9c19d1fbeaa722a6ad0978b192ce709b6c7613
Alphan Ulusoy <alphan@google.com>
2022-06-10 08:52:02 -0400

    [sw/silicon_creator] Refactor consistency checks to mask_rom_pre_boot_check()
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

--- sw/device/silicon_creator/mask_rom/mask_rom.c
+++ sw/device/silicon_creator/mask_rom/mask_rom.c
@@ -48,14 +48,24 @@
  * Each counter is indexed by Name. The Initial Value is used to initialize the
  * counters with unique values with a good hamming distance. The values are
  * restricted to 11-bit to be able use immediate load instructions.
+
+ * Encoding generated with
+ * $ ./util/design/sparse-fsm-encode.py -d 6 -m 6 -n 11 \
+ *     -s 1630646358 --language=c
+ *
+ * Minimum Hamming distance: 6
+ * Maximum Hamming distance: 8
+ * Minimum Hamming weight: 5
+ * Maximum Hamming weight: 8
  */
 // clang-format off
 #define ROM_CFI_FUNC_COUNTERS_TABLE(X) \
-  X(kCfiRomMain,    0xf05) \
-  X(kCfiRomInit,    0x042) \
-  X(kCfiRomVerify,  0x89d) \
-  X(kCfiRomTryBoot, 0x4c7) \
-  X(kCfiRomBoot,    0x0fb) \
+  X(kCfiRomMain,         0x14b) \
+  X(kCfiRomInit,         0x7dc) \
+  X(kCfiRomVerify,       0x5a7) \
+  X(kCfiRomTryBoot,      0x235) \
+  X(kCfiRomPreBootCheck, 0x43a) \
+  X(kCfiRomBoot,         0x2e2)
 // clang-format on
 
 // Define counters and constant values required by the CFI counter macros.
@@ -219,6 +229,36 @@ static inline  uintptr_t rom_ext_vma_get(const manifest_t *manifest, uintptr_t l
 }
 
 /**
+ * Performs consistency checks before booting a ROM_EXT.
+ *
+ * All of the checks in this function are expected to pass and any failures
+ * result in shutdown.
+ */
+static void mask_rom_pre_boot_check(void) {
+  CFI_FUNC_COUNTER_INCREMENT(rom_counters, kCfiRomPreBootCheck, 1);
+
+  // Check cached life cycle state against the value reported by hardware.
+  lifecycle_state_t lc_state_check = lifecycle_state_get();
+  if (launder32(lc_state_check) != lc_state) {
+    HARDENED_UNREACHABLE();
+  }
+  HARDENED_CHECK_EQ(lc_state_check, lc_state);
+  CFI_FUNC_COUNTER_INCREMENT(rom_counters, kCfiRomPreBootCheck, 2);
+
+  // Check cached boot data.
+  rom_error_t boot_data_ok = boot_data_check(&boot_data);
+  if (launder32(boot_data_ok) != kErrorOk) {
+    HARDENED_UNREACHABLE();
+  }
+  HARDENED_CHECK_EQ(boot_data_ok, kErrorOk);
+  CFI_FUNC_COUNTER_INCREMENT(rom_counters, kCfiRomPreBootCheck, 3);
+
+  sec_mmio_check_values(rnd_uint32());
+  sec_mmio_check_counters(/*expected_check_count=*/4);
+  CFI_FUNC_COUNTER_INCREMENT(rom_counters, kCfiRomPreBootCheck, 4);
+}
+
+/**
  * Boots a ROM_EXT.
  *
  * Note: This function should not return under normal conditions. Any returns
@@ -236,16 +276,6 @@ static rom_error_t mask_rom_boot(const manifest_t *manifest,
   keymgr_creator_max_ver_set(manifest->max_key_version);
   SEC_MMIO_WRITE_INCREMENT(kKeymgrSecMmioSwBindingSet +
                            kKeymgrSecMmioCreatorMaxVerSet);
-
-  // Check cached life cycle state against the value reported by hardware.
-  lifecycle_state_t lc_state_check = lifecycle_state_get();
-  if (launder32(lc_state_check) != lc_state) {
-    return kErrorMaskRomBootFailed;
-  }
-  HARDENED_CHECK_EQ(lc_state_check, lc_state);
-
-  // Check cached boot data.
-  HARDENED_RETURN_IF_ERROR(boot_data_check(&boot_data));
 
   sec_mmio_check_counters(/*expected_check_count=*/2);
 
@@ -284,15 +314,20 @@ static rom_error_t mask_rom_boot(const manifest_t *manifest,
   mask_rom_epmp_unlock_rom_ext_rx(&epmp, text_region);
   HARDENED_RETURN_IF_ERROR(epmp_state_check(&epmp));
 
+  CFI_FUNC_COUNTER_PREPCALL(rom_counters, kCfiRomBoot, 2, kCfiRomPreBootCheck);
+  mask_rom_pre_boot_check();
+  CFI_FUNC_COUNTER_INCREMENT(rom_counters, kCfiRomBoot, 4);
+  CFI_FUNC_COUNTER_CHECK(rom_counters, kCfiRomPreBootCheck, 5);
+
   // Enable execution of code from flash if signature is verified.
   flash_ctrl_exec_set(flash_exec);
   SEC_MMIO_WRITE_INCREMENT(kFlashCtrlSecMmioExecSet);
 
   sec_mmio_check_values(rnd_uint32());
-  sec_mmio_check_counters(/*expected_check_count=*/4);
+  sec_mmio_check_counters(/*expected_check_count=*/5);
 
   // Jump to ROM_EXT entry point.
-  CFI_FUNC_COUNTER_INCREMENT(rom_counters, kCfiRomBoot, 2);
+  CFI_FUNC_COUNTER_INCREMENT(rom_counters, kCfiRomBoot, 5);
   ((rom_ext_entry_point *)entry_point)();
 
   return kErrorMaskRomBootFailed;
