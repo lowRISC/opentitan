55a458ab439d424ee90d1bd8080eacc65df53553
Drew Macrae <drewmacrae@google.com>
2022-06-10 22:35:57 -0400

    [ci] enable all cw310 tests not marked as broken
    Signed-off-by: Drew Macrae <drewmacrae@google.com>

--- ci/scripts/run-fpga-cw310-tests.sh
+++ ci/scripts/run-fpga-cw310-tests.sh
@@ -23,50 +23,12 @@ export BITSTREAM="--offline --list ${SHA}"
 # in case we've crashed the UART handler on the CW310's SAM3U
 trap 'python3 ./util/fpga/cw310_reboot.py' EXIT
 
-# TODO: remove the --notest_keep_going and --nokeep_going flag after the CW310
-# tests are reliable (#13044)
 ci/bazelisk.sh test \
     --define DISABLE_VERILATOR_BUILD=true \
-    --notest_keep_going \
     --nokeep_going \
     --test_tag_filters=cw310,-broken \
     --test_output=all \
     --build_tests_only \
     --define cw310=lowrisc \
-    //sw/device/tests:aes_smoketest_fpga_cw310 \
-    //sw/device/tests:aon_timer_smoketest_fpga_cw310 \
-    //sw/device/tests:clkmgr_smoketest_fpga_cw310 \
-    //sw/device/tests:crt_test_fpga_cw310 \
-    //sw/device/tests:flash_ctrl_test_fpga_cw310 \
-    //sw/device/tests:gpio_smoketest_fpga_cw310 \
-    //sw/device/tests:kmac_smoketest_fpga_cw310 \
-    //sw/device/tests:kmac_mode_cshake_test_fpga_cw310 \
-    //sw/device/tests:kmac_mode_kmac_test_fpga_cw310 \
-    //sw/device/tests:otbn_randomness_test_fpga_cw310 \
-    //sw/device/tests:otbn_irq_test_fpga_cw310 \
-    //sw/device/tests:otbn_rsa_test_fpga_cw310 \
-    //sw/device/tests:otbn_ecdsa_op_irq_test_fpga_cw310 \
-    //sw/device/tests:otp_ctrl_smoketest_fpga_cw310 \
-    //sw/device/tests:pwrmgr_smoketest_fpga_cw310 \
-    //sw/device/tests:rstmgr_smoketest_fpga_cw310 \
-    //sw/device/tests:rv_plic_smoketest_fpga_cw310 \
-    //sw/device/tests:rv_timer_smoketest_fpga_cw310 \
-    //sw/device/tests:sram_ctrl_smoketest_fpga_cw310 \
-    //sw/device/tests:uart_smoketest_fpga_cw310 \
-    //sw/device/silicon_creator/lib:boot_data_functest_fpga_cw310 \
-    //sw/device/silicon_creator/lib/drivers:hmac_functest_fpga_cw310 \
-    //sw/device/silicon_creator/lib/drivers:retention_sram_functest_fpga_cw310 \
-    //sw/device/silicon_creator/lib/drivers:uart_functest_fpga_cw310 \
-    //sw/device/silicon_creator/lib/drivers:watchdog_functest_fpga_cw310 \
-    //sw/device/silicon_creator/mask_rom:e2e_bootup_no_rom_ext_signature_fpga_cw310 \
-    //sw/device/silicon_creator/lib/sigverify:sigverify_functest_fpga_cw310
-
-    # Note that some tests were included in the original systemtest but are
-    # failing when run under bazel and so are excluded from this run. These
-    # tests are:
-    # - pmp_smoketest_napot
-    # - pmp_smoktetest_tor
-    # TODO: use the following query instead of the list above after the CW310
-    # UART is reliable (#13044)
-    # $(./bazelisk.sh query 'rdeps(//...,@bitstreams//:bitstream_test_rom)') \
-    # All the tests that depend on the test rom
+    $(./bazelisk.sh query 'rdeps(//...,@bitstreams//:bitstream_test_rom)') \
+    $(./bazelisk.sh query 'rdeps(//...,@bitstreams//:bitstream_mask_rom)')
