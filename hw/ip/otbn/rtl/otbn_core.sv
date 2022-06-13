f8d6762e8a77622de926a2a915fca6610ff4a866
Andreas Kurth <adk@lowrisc.org>
2022-06-13 20:46:48 +0200

    [otbn,rtl] Assert wdata known only if writing
    Prior to this commit, the `DmemWdataOKnown_A` assertion would trigger
    also on read requests (i.e., when `dmem_req_o` is set but `dmem_write_o`
    is not).  For read requests, however, no data is written and thus the
    write data value does not have to be known.  This caused spurious
    failures of this assertion.
    
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- hw/ip/otbn/rtl/otbn_core.sv
+++ hw/ip/otbn/rtl/otbn_core.sv
@@ -873,7 +873,7 @@ module otbn_core
   `ASSERT_KNOWN(DmemReqOKnown_A, dmem_req_o)
   `ASSERT_KNOWN_IF(DmemWriteOKnown_A, dmem_write_o, dmem_req_o)
   `ASSERT_KNOWN_IF(DmemAddrOKnown_A, dmem_addr_o, dmem_req_o)
-  `ASSERT_KNOWN_IF(DmemWdataOKnown_A, dmem_wdata_o, dmem_req_o)
+  `ASSERT_KNOWN_IF(DmemWdataOKnown_A, dmem_wdata_o, dmem_req_o & dmem_write_o)
   `ASSERT_KNOWN_IF(DmemRmaskOKnown_A, dmem_rmask_o, dmem_req_o)
   `ASSERT_KNOWN(EdnRndReqOKnown_A, edn_rnd_req_o)
   `ASSERT_KNOWN(EdnUrndReqOKnown_A, edn_urnd_req_o)
