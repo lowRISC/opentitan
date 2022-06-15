89d481f1d745d410de26f2fd3bddef9a80403943
Guillermo Maturana <maturana@google.com>
2022-06-15 13:47:25 -0700

    [dv,rstmgr] Fix some sec_cm test failures
    Update the list of rstmgr_leaf_rst instances for design changes.
    Add some messages in check failures for ease of diagnostics.
    Formatting changes for verible.
    
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- hw/ip/rstmgr/dv/env/rstmgr_env_pkg.sv
+++ hw/ip/rstmgr/dv/env/rstmgr_env_pkg.sv
@@ -34,37 +34,44 @@ package rstmgr_env_pkg;
   parameter string LIST_OF_ALERTS[] = {"fatal_fault", "fatal_cnsty_fault"};
   parameter uint NUM_ALERTS = 2;
 
-  // leaf reset modules for top_earlgrey
-  parameter string LIST_OF_LEAFS[] = {"u_daon_por",
-                                      "u_daon_por_io",
-                                      "u_daon_por_io_div2",
-                                      "u_daon_por_usb",
-                                      "u_daon_sys_aon",
-                                      "u_daon_sys_io_div4",
-                                      "u_d0_lc",
-                                      "u_d0_lc_shadowed",
-                                      "u_d0_lc_io_div4",
-                                      "u_d0_lc_io_div4_shadowed",
-                                      "u_daon_lc_aon",
-                                      "u_daon_lc_io_div4",
-                                      "u_daon_lc_io_div4_shadowed",
-                                      "u_d0_sys",
-                                      "u_d0_sys_shadowed",
-                                      "u_d0_sys_io_div4",
-                                      "u_d0_sys_aon",
-                                      "u_d0_spi_device",
-                                      "u_d0_spi_host0",
-                                      "u_d0_spi_host1",
-                                      "u_d0_usb",
-                                      "u_d0_i2c0",
-                                      "u_d0_i2c1",
-                                      "u_d0_i2c2"};
+  // Instances of rstmgr_leaf_rst modules in top_earlgrey's rstmgr.
+  parameter string LIST_OF_LEAFS[] = {
+    "u_daon_por",
+    "u_daon_por_io",
+    "u_daon_por_io_div2",
+    // The leaf below doesn't implement
+    // consistency checks.
+    // "u_daon_por_io_div4",
+    "u_daon_por_usb",
+    "u_d0_lc",
+    "u_d0_lc_shadowed",
+    "u_daon_lc_io_div4",
+    "u_d0_lc_io_div4",
+    "u_daon_lc_io_div4_shadowed",
+    "u_d0_lc_io_div4_shadowed",
+    "u_daon_lc_aon",
+    "u_d0_sys",
+    "u_d0_sys_shadowed",
+    "u_daon_sys_io_div4",
+    "u_d0_sys_io_div4",
+    "u_daon_sys_aon",
+    "u_d0_sys_aon",
+    "u_d0_spi_device",
+    "u_d0_spi_host0",
+    "u_d0_spi_host1",
+    "u_d0_usb",
+    "u_d0_i2c0",
+    "u_d0_i2c1",
+    "u_d0_i2c2"
+  };
 
-  // leaf reset which has shadow pair
-  parameter string LIST_OF_SHADOW_LEAFS[] = {"u_d0_lc_io_div4",
-                                             "u_daon_lc_io_div4",
-                                             "u_daon_por_io_div4",
-                                             "u_d0_sys"};
+  // Instances of rstmgr_leaf_rst modules which have a shadow pair.
+  parameter string LIST_OF_SHADOW_LEAFS[] = {
+    "u_d0_lc",
+    "u_daon_lc_io_div4",
+    "u_d0_lc_io_div4",
+    "u_d0_sys"
+  };
 
   // types
   typedef logic [NumSwResets-1:0] sw_rst_t;
