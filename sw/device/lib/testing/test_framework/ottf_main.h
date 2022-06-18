7ce10f7df06654df1cbcd7de00ac14693d02fe89
Alphan Ulusoy <alphan@google.com>
2022-06-17 20:48:36 -0400

    [ottf] Add ottf_test_config.h
    This commit moves the definitions of `test_config_t` and
    `OTTF_DEFINE_TEST_CONFIG` to `ottf_test_config.h` so that they can be
    used in tests that don't depend on OTTF.
    
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

--- sw/device/lib/testing/test_framework/ottf_main.h
+++ sw/device/lib/testing/test_framework/ottf_main.h
@@ -10,86 +10,12 @@
 // This private header is included here so that OTTF users can include a single
 // header in their test application (the `ottf_main.h` header).
 #include "sw/device/lib/testing/test_framework/FreeRTOSConfig.h"
+#include "sw/device/lib/testing/test_framework/ottf_test_config.h"
 
 /**
  * @file
  * @brief Entrypoint definitions for on-device tests
  */
-
-/**
- * Configuration variables for an on-device test.
- *
- * This type represents configuration values for an on-device test, which allow
- * tests to configure the behavior of the OpenTitan Test Framework (OTTF).
- *
- * WARNING: DO NOT REARRANGE THE MEMBERS IN THIS STRUCT. THE FIRST MEMBER IS
- * ACCESSED BY OTTF ISR ASSEMBLY WHICH EXPECTS A SPECIFIC CONFIGURATION.
- *
- * However, new fields can be safely added to this struct without affecting any
- * tests; the "default" value of all fields should be zero (or NULL, or
- * equivalent).
- *
- * See `kTestConfig`.
- */
-typedef struct test_config {
-  /**
-   * If true, `test_main()` is run as a FreeRTOS task, enabling test writers to
-   * to spawn additional (concurrent) FreeRTOS tasks within the `test_main()`
-   * execution context.
-   *
-   * If false, `test_main()` is executed on bare-metal, and cannot spawn
-   * additional concurrent execution contexts. This is useful for tests that do
-   * not require concurrency, and seek to minimize simulation runtime.
-   */
-  bool enable_concurrency;
-
-  /**
-   * Indicates that `test_main()` does something non-trivial to the UART
-   * device. Setting this to true will make `test_main()` guard against this
-   * by resetting the UART device before printing debug information.
-   */
-  bool can_clobber_uart;
-
-  /**
-   * Name of the file in which `kTestConfig` is defined. Most of the time, this
-   * will be the file that defines `test_main()`.
-   */
-  const char *file;
-} test_config_t;
-
-/**
- * Helper macro for defining the `kTestConfig` symbol in test files.
- *
- * While the `kTestConfig` struct can be defined directly, tests should prefer
- * this macro since it handles the definition of the `file` field.
- *
- * A test with default options should use:
- *
- *   OTTF_DEFINE_TEST_CONFIG();
- *
- * A test that wants to enable concurrency and can also clobber UART should use:
- *
- *   OTTF_DEFINE_TEST_CONFIG(.enable_concurrency = true,
- *                           .can_clobber_uart = true, );
- *
- */
-#define OTTF_DEFINE_TEST_CONFIG(...) \
-  const test_config_t kTestConfig = {.file = __FILE__, __VA_ARGS__};
-
-/**
- * Global test configuration.
- *
- * This symbol should be defined externally in a standalone SW test. For most
- * tests, this will just look like the following:
- *
- *   OTTF_DEFINE_TEST_CONFIG();
- *
- * The zero values of all of the fields will behave like sane defaults.
- *
- * This value needs to be provided as a global so that the initialization code
- * that runs before `test_main()` is executed can take note of it.
- */
-extern const test_config_t kTestConfig;
 
 /**
  * Entry point for a SW on-device (or chip-level) test.
