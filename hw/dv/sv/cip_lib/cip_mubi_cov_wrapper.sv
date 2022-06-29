a354b3c909d6b169d74da67c577654032da4b3a2
Cindy Chen <chencindy@opentitan.org>
2022-06-29 09:27:15 -0700

    [dv/cip_mubi_cov] Add a wrapper to support binding an mubi array
    In alert_handler, lpg groups are an array of mubi inputs. To collect
    coverage on mubi types, we usually bind the mubi input with
    cip_mubi_cov.if. But binding an interface won't work for genvar for
    loops. To avoid hardcoding 22 signals, I added a cip_mubi_cov_wrapper
    module that uses a parameter to input number of mubi signals.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

---
+++ hw/dv/sv/cip_lib/cip_mubi_cov_wrapper.sv
@@ -0,0 +1,18 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This is a wrapper module for IPs to bind an array of mubi types port.
+module cip_mubi_cov_wrapper
+#(
+  parameter int NumMubis = 1,
+  parameter int Width    = 4  // parameter for cip_mubi_cov_if module
+) (
+  input [NumMubis-1:0][Width-1:0] mubis,
+  input rst_ni
+);
+
+  for (genvar k = 0; k < NumMubis; k++) begin : gen_mubi_cov_if
+    cip_mubi_cov_if#(Width) mubi_cov_if(.mubi(mubis[k]), .rst_ni(rst_ni));
+  end
+endmodule
