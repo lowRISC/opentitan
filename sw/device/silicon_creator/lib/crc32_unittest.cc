a1d2957984b18d6e3f49103d3817bd0a79e3f8cc
Alphan Ulusoy <alphan@google.com>
2022-06-30 09:04:07 -0400

    [sw/silicon_creator] Add crc32_add32() and crc32_add()
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

--- sw/device/silicon_creator/lib/crc32_unittest.cc
+++ sw/device/silicon_creator/lib/crc32_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "sw/device/silicon_creator/lib/crc32.h"
 
+#include <cstring>
 #include <stdint.h>
 
 #include "gtest/gtest.h"
@@ -32,6 +33,10 @@ INSTANTIATE_TEST_SUITE_P(AllCases, CrcTest,
                              TestParams{
                                  "The quick brown fox jumps over the lazy dog",
                                  0x414fa339,
+                             },
+                             TestParams{
+                                 "\xfe\xca\xfe\xca\x02\xb0\xad\x1b",
+                                 0x9508ac14,
                              }));
 
 TEST_P(CrcTest, Crc32) {
@@ -39,13 +44,47 @@ TEST_P(CrcTest, Crc32) {
             GetParam().exp_crc);
 }
 
-TEST_P(CrcTest, Crc32WithContext) {
+TEST_P(CrcTest, Crc32Add) {
+  uint32_t ctx;
+  crc32_init(&ctx);
+  crc32_add(&ctx, GetParam().input.data(), GetParam().input.length());
+
+  EXPECT_EQ(crc32_finish(&ctx), GetParam().exp_crc);
+}
+
+TEST_F(CrcTest, Misaligned) {
+  constexpr uint32_t kExpCrc = 0x414fa339;
+  alignas(uint32_t) char input[] =
+      ">The quick brown fox jumps over the lazy dog";
+
+  EXPECT_EQ(crc32(&input[1], std::strlen(input) - 1), kExpCrc);
+
+  uint32_t ctx;
+  crc32_init(&ctx);
+  crc32_add(&ctx, &input[1], std::strlen(input) - 1);
+
+  EXPECT_EQ(crc32_finish(&ctx), kExpCrc);
+}
+
+TEST_P(CrcTest, Crc32Add8) {
   uint32_t ctx;
   crc32_init(&ctx);
   for (auto val : GetParam().input) {
-    crc32_add(&ctx, val);
+    crc32_add8(&ctx, val);
   }
   EXPECT_EQ(crc32_finish(&ctx), GetParam().exp_crc);
+}
+
+TEST_F(CrcTest, Crc32Add32) {
+  uint32_t ctx;
+  crc32_init(&ctx);
+  constexpr uint32_t kExpCrc = 0x9508ac14;
+
+  crc32_add32(&ctx, 0xcafecafe);
+  crc32_finish(&ctx);
+  crc32_add32(&ctx, 0x1badb002);
+
+  EXPECT_EQ(crc32_finish(&ctx), kExpCrc);
 }
 
 }  // namespace
