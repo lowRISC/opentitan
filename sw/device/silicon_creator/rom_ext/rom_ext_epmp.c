579d874310f844219c81f3c2f67ad78a770057ad
Alphan Ulusoy <alphan@google.com>
2022-07-01 17:08:02 -0400

    [sw/silicon_creator] ROM_EXT ePMP updates
    This commit fixes some minor nits in `rom_ext_epmp.c` and removes the ePMP
    state check from `rom_ext.c` since we don't initialize the in-memory
    copy of the ePMP state yet. See #13507.
    
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

--- sw/device/silicon_creator/rom_ext/rom_ext_epmp.c
+++ sw/device/silicon_creator/rom_ext/rom_ext_epmp.c
@@ -10,23 +10,23 @@
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
 void rom_ext_epmp_unlock_owner_stage_rx(epmp_state_t *state,
-                                        epmp_region_t image) {
+                                        epmp_region_t region) {
   const int kEntry = 8;
-  epmp_state_configure_tor(state, kEntry, image, kEpmpPermLockedReadExecute);
+  epmp_state_configure_tor(state, kEntry, region, kEpmpPermLockedReadExecute);
 
   // Update the hardware configuration (CSRs).
   //
-  // Entry is hardcoded as 1. Make sure to modify hardcoded values if changing
+  // Entry is hardcoded as 8. Make sure to modify hardcoded values if changing
   // kEntry.
   //
   // The `pmp1cfg` configuration is the second field in `pmpcfg0`.
   //
   //            32          24          16           8           0
   //             +-----------+-----------+-----------+-----------+
-  // `pmpcfg2` = | `pmp11cfg` | `pmp10cfg` | `pmp9cfg` | `pmp8cfg` |
+  // `pmpcfg2` = | `pmp11cfg | `pmp10cfg`| `pmp9cfg` | `pmp8cfg` |
   //             +-----------+-----------+-----------+-----------+
-  CSR_WRITE(CSR_REG_PMPADDR7, ((uint32_t)image.start) >> 2);
-  CSR_WRITE(CSR_REG_PMPADDR8, ((uint32_t)image.end) >> 2);
+  CSR_WRITE(CSR_REG_PMPADDR7, ((uint32_t)region.start) >> 2);
+  CSR_WRITE(CSR_REG_PMPADDR8, ((uint32_t)region.end) >> 2);
   CSR_CLEAR_BITS(CSR_REG_PMPCFG2, 0xff);
   CSR_SET_BITS(CSR_REG_PMPCFG2, (kEpmpModeTor | kEpmpPermLockedReadExecute));
 }
@@ -38,18 +38,18 @@ void rom_ext_epmp_unlock_owner_stage_r(epmp_state_t *state,
 
   // Update the hardware configuration (CSRs).
   //
-  // Entry is hardcoded as 6. Make sure to modify hardcoded values if changing
+  // Entry is hardcoded as 9. Make sure to modify hardcoded values if changing
   // kEntry.
   //
   // The `pmp6cfg` configuration is the second field in `pmpcfg1`.
   //
   //            32          24          16           8           0
   //             +-----------+-----------+-----------+-----------+
-  // `pmpcfg2` = | `pmp11cfg` | `pmp10cfg` | `pmp9cfg` | `pmp8cfg` |
+  // `pmpcfg2` = | `pmp11cfg`| `pmp10cfg`| `pmp9cfg` | `pmp8cfg` |
   //             +-----------+-----------+-----------+-----------+
   CSR_WRITE(CSR_REG_PMPADDR9,
             (uint32_t)region.start >> 2 |
-            ((uint32_t)region.end - (uint32_t)region.start - 1) >> 3);
+                ((uint32_t)region.end - (uint32_t)region.start - 1) >> 3);
   CSR_CLEAR_BITS(CSR_REG_PMPCFG2, 0xff << 8);
   CSR_SET_BITS(CSR_REG_PMPCFG2,
                ((kEpmpModeNapot | kEpmpPermLockedReadOnly) << 8));
