d11487a8ba47dfea02d69c24a3686b11078ed4bf
Alphan Ulusoy <alphan@google.com>
2022-07-01 16:58:45 -0400

    [sw/silicon_owner] Simplify bare_metal example
    This commit removes several unnecessary dependencies and uses
    `rom_print` library instead of `runtime/print`.
    
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

--- sw/device/silicon_owner/bare_metal/bare_metal_example.c
+++ sw/device/silicon_owner/bare_metal/bare_metal_example.c
@@ -2,39 +2,18 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "sw/device/lib/arch/device.h"
-#include "sw/device/lib/base/csr.h"
-#include "sw/device/lib/base/memory.h"
-#include "sw/device/lib/base/stdasm.h"
-#include "sw/device/lib/runtime/hart.h"
-#include "sw/device/lib/runtime/print.h"
-#include "sw/device/silicon_creator/lib/drivers/pinmux.h"
-#include "sw/device/silicon_creator/lib/drivers/uart.h"
 #include "sw/device/silicon_creator/lib/manifest_def.h"
+#include "sw/device/silicon_creator/lib/rom_print.h"
 
-#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"  // Generated.
-
-void bare_metal_init(void) {
-  pinmux_init();
-  uart_init(kUartNCOValue);
-  base_set_stdout((buffer_sink_t){
-      .data = NULL,
-      .sink = uart_sink,
-  });
-}
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
 void bare_metal_main(void) {
-  bare_metal_init();
-
-  // The PASS string is scanned by system test scripts. See
-  // test/systemtest/earlgrey/test_fpga_cw310.py for more details.
-  base_printf("Bare metal PASS!\r\n");
-
+  rom_printf("Bare metal PASS!\r\n");
   while (true) {
   }
 }
 
-void interrupt_handler(void) {}
+void interrupt_handler(void) { rom_printf("Interrupt!\r\n"); }
 
 // We only need a single handler for all interrupts, but we want to
 // keep distinct symbols to make writing tests easier.
