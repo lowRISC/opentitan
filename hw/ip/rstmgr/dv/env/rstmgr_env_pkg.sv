e0b11ad373bf7f0e2738ef86a58e19499b9dd4d1
Guillermo Maturana <maturana@google.com>
2022-07-13 12:21:49 -0700

    [dv/rstrmgr] Update list of rstmgr_leaf_rst
    Update list per design change, and suggest a way to generate the list to
    ease updates. The incorrect list was causing all rstmgr_leaf_rst_cnsty
    tests to fail.
    
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- hw/ip/rstmgr/dv/env/rstmgr_env_pkg.sv
+++ hw/ip/rstmgr/dv/env/rstmgr_env_pkg.sv
@@ -34,45 +34,48 @@ package rstmgr_env_pkg;
   parameter string LIST_OF_ALERTS[] = {"fatal_fault", "fatal_cnsty_fault"};
   parameter uint NUM_ALERTS = 2;
 
-  // Instances of rstmgr_leaf_rst modules in top_earlgrey's rstmgr.
+  // Sorted instances of rstmgr_leaf_rst modules in top_earlgrey's rstmgr.
+  // This can be generated from the source using
+  //   grep -A 5 rstmgr_leaf_rst <path to rstmgr.sv> | \
+  //     egrep '^[ ]+\) u_' | sed 's/[ )(]//g' | sort | \
+  //     sed 's/\(.*\)/    \"\1\",/'
   parameter string LIST_OF_LEAFS[] = {
-    "u_daon_por_io",
-    // The leaf below doesn't implement consistency checks.
-    // "u_daon_por_io_div4",
-    "u_daon_lc",
+    "u_d0_i2c0",
+    "u_d0_i2c1",
+    "u_d0_i2c2",
     "u_d0_lc",
-    "u_daon_lc_shadowed",
-    "u_d0_lc_shadowed",
+    "u_d0_lc_io_div4",
+    "u_d0_lc_io_div4_shadowed",
+    "u_d0_spi_device",
+    "u_d0_spi_host0",
+    "u_d0_spi_host1",
+    "u_d0_sys",
+    "u_d0_sys_io",
+    "u_d0_sys_io_div2",
+    "u_d0_sys_io_div4",
+    "u_d0_sys_shadowed",
+    "u_d0_sys_usb",
+    "u_d0_usb",
+    "u_d0_usb_aon",
+    "u_daon_lc",
     "u_daon_lc_aon",
     "u_daon_lc_io",
     "u_daon_lc_io_div2",
     "u_daon_lc_io_div4",
-    "u_d0_lc_io_div4",
     "u_daon_lc_io_div4_shadowed",
-    "u_d0_lc_io_div4_shadowed",
     "u_daon_lc_usb",
-    "u_d0_sys",
-    "u_d0_sys_shadowed",
-    "u_daon_sys_io_div4",
-    "u_d0_sys_io_div4",
+    "u_daon_por_io",
+    // The leaf below doesn't implement consistency checks.
+    // "u_daon_por_io_div4",
     "u_daon_sys_aon",
-    "u_d0_spi_device",
-    "u_d0_spi_host0",
-    "u_d0_spi_host1",
-    "u_d0_usb",
-    "u_d0_usb_aon",
-    "u_d0_i2c0",
-    "u_d0_i2c1",
-    "u_d0_i2c2"
+    "u_daon_sys_io_div4"
   };
 
   // Instances of rstmgr_leaf_rst modules which have a shadow pair.
   parameter string LIST_OF_SHADOW_LEAFS[] = {
-    "u_daon_lc",
-    "u_d0_lc",
-    "u_daon_lc_io_div4",
     "u_d0_lc_io_div4",
-    "u_d0_sys"
+    "u_d0_sys",
+    "u_daon_lc_io_div4"
   };
 
   // types
