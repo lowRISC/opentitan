b293eda48c0b3e662ac7544ed35a0fcd182d529d
Douglas Reis <doreis@lowrisc.org>
2022-07-13 16:57:24 +0100

    [test, aes] Use the `hw/ip/aes/model` keys in the idle test
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/device/tests/aes_idle_test.c
+++ sw/device/tests/aes_idle_test.c
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "hw/ip/aes/model/aes_modes.h"
 #include "sw/device/lib/base/memory.h"
 #include "sw/device/lib/base/mmio.h"
 #include "sw/device/lib/dif/dif_aes.h"
@@ -15,31 +16,7 @@
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
-// The following plaintext, key and ciphertext are extracted from Appendix C of
-// the Advanced Encryption Standard (AES) FIPS Publication 197 available at
-// https://www.nist.gov/publications/advanced-encryption-standard-aes
-
 #define TIMEOUT (1000 * 1000)
-
-static const uint32_t kPlainText[] = {
-    0x33221100,
-    0x77665544,
-    0xbbaa9988,
-    0xffeeddcc,
-};
-
-static const uint8_t kKey[] = {
-    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a,
-    0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
-    0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
-};
-
-static const uint32_t kCipherTextGold[] = {
-    0xcab7a28e,
-    0xbf456751,
-    0x9049fcea,
-    0x8960494b,
-};
 
 // The mask share, used to mask kKey. Note that the masking should not be done
 // manually. Software is expected to get the key in two shares right from the
@@ -87,15 +64,15 @@ bool test_main(void) {
 
   // Mask the key. Note that this should not be done manually. Software is
   // expected to get the key in two shares right from the beginning.
-  uint8_t key_share0[ARRAYSIZE(kKey)];
-  for (int i = 0; i < ARRAYSIZE(kKey); ++i) {
-    key_share0[i] = kKey[i] ^ kKeyShare1[i];
+  uint8_t key_share0[sizeof(kAesModesKey256)];
+  for (int i = 0; i < sizeof(kAesModesKey256); ++i) {
+    key_share0[i] = kAesModesKey256[i] ^ kKeyShare1[i];
   }
 
   // "Convert" key share byte arrays to `dif_aes_key_share_t`.
   dif_aes_key_share_t key;
-  memcpy(key.share0, key_share0, sizeof(kKey));
-  memcpy(key.share1, kKeyShare1, sizeof(kKey));
+  memcpy(key.share0, key_share0, sizeof(key.share0));
+  memcpy(key.share1, kKeyShare1, sizeof(key.share1));
 
   // Setup ECB encryption transaction.
   dif_aes_transaction_t transaction = {
@@ -125,7 +102,7 @@ bool test_main(void) {
 
   // "Convert" plain data byte arrays to `dif_aes_data_t`.
   dif_aes_data_t in_data_plain;
-  memcpy(in_data_plain.data, kPlainText, sizeof(kPlainText));
+  memcpy(in_data_plain.data, kAesModesPlainText, sizeof(in_data_plain.data));
 
   // Load the plain text to trigger the encryption operation.
   AES_TESTUTILS_WAIT_FOR_STATUS(&aes, kDifAesStatusInputReady, true, TIMEOUT);
@@ -152,14 +129,13 @@ bool test_main(void) {
   // correctness.
   CLKMGR_TESTUTILS_SET_AND_CHECK_CLOCK_HINT(
       clkmgr, kAesClock, kDifToggleEnabled, kDifToggleEnabled);
-  dif_aes_data_t out_data_cipher;
-  CHECK_DIF_OK(dif_aes_read_output(&aes, &out_data_cipher));
+  dif_aes_data_t out_data;
+  CHECK_DIF_OK(dif_aes_read_output(&aes, &out_data));
 
   // Finish the ECB encryption transaction.
   CHECK_DIF_OK(dif_aes_end(&aes));
-
-  CHECK_ARRAYS_EQ(out_data_cipher.data, kCipherTextGold,
-                  ARRAYSIZE(kCipherTextGold));
+  CHECK_ARRAYS_EQ((uint8_t *)out_data.data, kAesModesCipherTextEcb256,
+                  sizeof(out_data.data));
 
   return true;
 }
