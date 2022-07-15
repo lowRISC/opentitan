1be6176260a1abec3c55f3278e61b546a34135d0
Timothy Chen <timothytim@google.com>
2022-07-15 15:03:46 -0700

    [flash_ctrl] fixes for hanging transactions
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- hw/ip/flash_ctrl/rtl/flash_phy_rd_buffers.sv
+++ hw/ip/flash_ctrl/rtl/flash_phy_rd_buffers.sv
@@ -25,6 +25,7 @@ module flash_phy_rd_buffers import flash_phy_pkg::*; (
   input en_i,
   input alloc_i,
   input update_i,
+  input err_i,
   input wipe_i,
   input [BankAddrW-1:0] addr_i,
   input part_i,
@@ -40,18 +41,23 @@ module flash_phy_rd_buffers import flash_phy_pkg::*; (
       out_o.part <= flash_ctrl_pkg::FlashPartData;
       out_o.info_sel <= '0;
       out_o.attr <= Invalid;
+      out_o.err <= '0;
     end else if (!en_i && out_o.attr != Invalid) begin
       out_o.attr <= Invalid;
+      out_o.err <= '0;
     end else if (wipe_i && en_i) begin
       out_o.attr <= Invalid;
+      out_o.err <= '0;
     end else if (alloc_i && en_i) begin
       out_o.addr <= addr_i;
       out_o.part <= part_i;
       out_o.info_sel <= info_sel_i;
       out_o.attr <= Wip;
+      out_o.err <= '0;
     end else if (update_i && en_i) begin
       out_o.data <= data_i;
       out_o.attr <= Valid;
+      out_o.err <= err_i;
     end
   end
 
