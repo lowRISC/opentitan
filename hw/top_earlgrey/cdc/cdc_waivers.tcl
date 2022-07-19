6a8a9ecb00b3fb80db817decbd877583f76e98fb
Joshua Park <jeoong@google.com>
2022-07-19 15:27:12 -0700

    [cdc] misc. waivers
    W_ASYNC_RST_FLOPS :
     -spi_device tpm and dmi jtag
    W_RECON_GROUPS :
     -Signals driven by AST
     -Recon in RV PLIC
    
    Signed-off-by: Joshua Park <jeoong@google.com>

--- hw/top_earlgrey/cdc/cdc_waivers.tcl
+++ hw/top_earlgrey/cdc/cdc_waivers.tcl
@@ -50,6 +50,83 @@ set_rule_status -rule {W_GLITCH} -status {Waived}          \
     (GlitchOutput=~"*u_sync_1*")} \
   -comment {Waive PADs input goes into synchronizer}
 
+# Driving from PAD RESET to SPI TPM
+set_rule_status -rule {W_ASYNC_RST_FLOPS} -status {Waived}                           \
+  -expression {(DrivingSignal=~"IO*") && (ReceivingFlop=~"*u_spi_device.u_spi_tpm.*")} \
+  -comment {PAD RESET driving to SPI TPM}
+
+set_rule_status -rule {W_ASYNC_RST_FLOPS} -status {Waived}                           \
+  -expression {(DrivingSignal=~"SPI_DEV_CS_L*") && (ReceivingFlop=~"*u_spi_device.u_spi_tpm.*")} \
+  -comment {PAD RESET driving to SPI TPM}
+
+set_rule_status -rule {W_ASYNC_RST_FLOPS} -status {Waived}                           \
+  -expression {(DrivingSignal=~"top_earlgrey.u_pinmux_aon*") && (ReceivingFlop=~"*u_spi_device.u_spi_tpm.*")} \
+  -comment {PAD RESET driving to SPI TPM}
+
+set_rule_status -rule {W_ASYNC_RST_FLOPS} -status {Waived}                           \
+  -expression {(DrivingSignal=~"top_earlgrey.u_rstmgr_aon*") && (ReceivingFlop=~"*u_spi_device.u_spi_tpm.*")} \
+  -comment {PAD RESET driving to SPI TPM}
+
+# Driving from PAD PIN MUX RESET to DMI JTAG
+set_rule_status -rule {W_ASYNC_RST_FLOPS} -status {Waived}                           \
+  -expression {(DrivingSignal=~"top_earlgrey.u_pinmux_aon*") && (ReceivingFlop=~"*u_dmi_jtag.*")} \
+  -comment {PAD RESET driving to dmi jtag}
+
+# dual port memory to SPI
+set_rule_status -rule {W_MASYNC} -status {Waived}                           \
+  -expression {(Driver =~ "top_earlgrey.u_spi_device.u_memory_2p.u_mem.gen_generic.u_impl_generic.b_rdata_o*") && (ReceivingFlop=~"top_earlgrey.u_spi_device.u_fwmode.*")} \
+  -comment {Dual port memory read port to SPI}
+
+# retention regs (#13811)
+set_rule_status -rule {W_MASYNC} -status {Waived}                           \
+  -expression {(Driver =~ "SPI_DEV_*") && (ReceivingFlop=~"top_earlgrey.u_pinmux_aon.dio_oe_retreg_q*")} \
+  -comment {retention regs}
+
+# retention regs (#13811)
+set_rule_status -rule {W_MASYNC} -status {Waived}                           \
+  -expression {(Driver =~ "SPI_HOST_*") && (ReceivingFlop=~"top_earlgrey.u_pinmux_aon.dio_oe_retreg_q*")} \
+  -comment {retention regs}
+
+# rspfifo to normal_fifo in tlul xbar_main
+set_rule_status -rule {W_MASYNC} -status {Waived}                           \
+  -expression {(Driver =~ "top_earlgrey.u_xbar_main.u_asf_*.rspfifo.storage*") && (ReceivingFlop=~"top_earlgrey.u_xbar_main.u_s1n_57.fifo_h.rspfifo.gen_normal_fifo.storage*")} \
+  -comment {input rspfifo rd_port is in the same domain as device_fifo}
+
+# tlul xbar_main rspfifo to ibex
+set_rule_status -rule {W_MASYNC} -status {Waived}                           \
+  -expression {(Driver =~ "top_earlgrey.u_xbar_main.u_asf_*.rspfifo.storage*") && (ReceivingFlop=~"top_earlgrey.u_rv_core_ibex.u_core.gen_regfile_ff*")} \
+  -comment {tlul xbar_main rspfifo to ibex}
+
+# tlul xbar_main rspfifo to ibex
+set_rule_status -rule {W_MASYNC} -status {Waived}                           \
+  -expression {(Driver =~ "top_earlgrey.u_xbar_main.u_asf_*.rspfifo.storage*") && (ReceivingFlop=~"top_earlgrey.u_rv_core_ibex.u_prim_lc_sender.gen_flops*")} \
+  -comment {tlul xbar_main rspfifo to ibex}
+
+# tlul xbar_main rspfifo to spi_device
+set_rule_status -rule {W_MASYNC} -status {Waived}                           \
+  -expression {(Driver =~ "top_earlgrey.u_xbar_main.u_asf_*.rspfifo.storage*") && (ReceivingFlop=~"top_earlgrey.u_spi_device.u_memory_2p.u_mem.gen_generic.u_impl_generic.a_*")} \
+  -comment {tlul xbar_main rspfifo to spi_device}
+
+# tlul xbar_main rspfifo to usb device
+set_rule_status -rule {W_MASYNC} -status {Waived}                           \
+  -expression {(Driver =~ "top_earlgrey.u_xbar_main.u_asf_*.rspfifo.storage*") && (ReceivingFlop=~"top_earlgrey.u_usbdev.u_memory_2p.i_prim_ram_2p_async_adv.u_mem.gen_generic.u_impl_generic.a_*")} \
+  -comment {tlul xbar_main rspfifo to usb device}
+
+# AST signals reconverged
+set_rule_status -rule {W_RECON_GROUPS} -status {Waived}                           \
+  -expression {(ControlSignal =~ "u_ast.u_ast_clks_byp.*u_impl_generic.q_o*") && (ReconSignal=~"top_earlgrey.*.u_reg*")} \
+  -comment {Reconverged signals coming from AST}
+
+# RV PLIC signals reconverged
+set_rule_status -rule {W_RECON_GROUPS} -status {Waived}                           \
+  -expression {(ControlSignal =~ "*u_rv_plic.*") && (ReconSignal=~"*u_rv_plic.u_gateway.ia*")} \
+  -comment {Reconverged signals in RV PLIC}
+
+# Misc RV PLIC signals reconverged
+set_rule_status -rule {W_RECON_GROUPS} -status {Waived}                           \
+  -expression {(ControlSignal =~ "*u_sync_1.gen_generic.u_impl_generic.q_o*") && (ReconSignal=~"*u_rv_plic.u_gateway.ia*")} \
+  -comment {Reconverged signals in RV PLIC}
+
 # PADs attribute to multiple IPs
 # Assume the attributes are not used when IPs are active
 set_rule_status -rule {W_FANOUT} -status {Waived}           \
@@ -77,6 +154,7 @@ set_rule_status -rule {W_CNTL} -status {Waived}                              \
   -comment {Gray Pointer sync}
 
 ## Waive pinmux attr: static
+# TODO(#13806): set_stable_value has not been added because it's overlapped with this existing waiver
 set_rule_status -rule {W_MASYNC} -status {Waived}        \
   -expression {(Driver=~"*u_pinmux_aon.dio_pad_attr_*")} \
   -comment {PAD Attributes are static signals.}
