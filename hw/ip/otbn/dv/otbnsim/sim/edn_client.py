d5d8dc925f453c1ccf3fc6caa192c21f94a1c2a6
Andreas Kurth <adk@lowrisc.org>
2022-07-19 10:03:07 +0200

    [otbn,dv] Fix reset during EDN request (again)
    This commit reapplies 40dea7f638 (#13294), which had been partially
    undone in 3c7192597e (part of #13391).
    
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- hw/ip/otbn/dv/otbnsim/sim/edn_client.py
+++ hw/ip/otbn/dv/otbnsim/sim/edn_client.py
@@ -69,11 +69,14 @@ class EdnClient:
         self._retry = False
 
     def take_word(self, word: int, fips_err: bool) -> None:
-        '''Take a 32-bit data word that we've been waiting for'''
+        '''
+        Take a 32-bit data word, which we requested.
+
+        If there is a reset in between an EDN transaction, request flag will
+        drop. In that case, incoming word will be ignored. Otherwise, append
+        it to the internal `_acc` list.
+        '''
         assert 0 <= word < (1 << 32)
-        assert self._acc is not None
-        assert len(self._acc) < ACC_LEN
-        assert self._cdc_counter is None
 
         if self._acc is None:
             return
