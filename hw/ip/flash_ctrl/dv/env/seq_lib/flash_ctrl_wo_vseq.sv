7cc3138ff632bc507aa51a64c385ae82bc56734f
Jaedon Kim <jdonjdon@google.com>
2022-07-20 16:52:44 +0000

    4byte write read
    Signed-off-by: Jaedon Kim <jdonjdon@google.com>

--- hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_wo_vseq.sv
+++ hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_wo_vseq.sv
@@ -10,13 +10,17 @@ class flash_ctrl_wo_vseq extends flash_ctrl_otf_base_vseq;
   virtual task body();
     flash_op_t ctrl;
     int num, bank;
-    flash_ctrl_default_region_cfg(,,,MuBi4True,MuBi4True);
+    flash_ctrl_default_region_cfg();//,,,MuBi4True,MuBi4True);
 
     ctrl.partition  = FlashPartData;
-    repeat(100) begin
-      num = $urandom_range(1, 32);
-      bank = $urandom_range(0, 1);
-      prog_flash(ctrl, bank, num);
-    end
+//    repeat(100) begin
+//      num = $urandom_range(1, 32);
+//      bank = $urandom_range(0, 1);
+//      prog_flash(ctrl, bank, num);
+//    end
+    repeat(2) prog_flash(ctrl, 0, 1, 1);
+
+    ctrl.otf_addr = 0;
+    read_flash(ctrl, 0, 1, 2);
   endtask
 endclass // flash_ctrl_wo_vseq
