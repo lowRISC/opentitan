16a4555cd71d2926bd43975fe7c7199792c7aa56
Weicai Yang <weicai@google.com>
2022-07-20 13:01:26 -0700

    [spi_device/dv] Add read status intercept sequence
    1. Test 3 read status intercept and compare returned data in scb
    2. Add spi_device_pass_all_vseq which enables all passthrough features
    in one seq
    3. deleted quad/dual_vseq as they don't need to be tested in a dedicated test
    
    Signed-off-by: Weicai Yang <weicai@google.com>

---
+++ hw/ip/spi_device/dv/env/seq_lib/spi_device_pass_all_vseq.sv
@@ -0,0 +1,36 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// Enable all passthrough related features during init, and then randomly send valid commands
+class spi_device_pass_all_vseq extends spi_device_pass_base_vseq;
+  `uvm_object_utils(spi_device_pass_all_vseq)
+  `uvm_object_new
+
+  int write_flash_status_pct = 30;
+  virtual task body();
+    bit [7:0] op;
+    uint payload_size;
+
+    allow_addr_swap    = 1;
+    allow_payload_swap = 1;
+    allow_intercept    = 1;
+
+    for (int i = 0; i < num_trans; ++i) begin
+      `uvm_info(`gfn, $sformatf("running iteration %0d/%0d", i, num_trans), UVM_LOW)
+      spi_device_flash_pass_init(PassthroughMode);
+
+      for (int j = 0; j < 20; ++j) begin
+        if ($urandom_range(0, 99) < write_flash_status_pct) random_write_flash_status();
+
+        op = get_rand_opcode();
+        `uvm_info(`gfn, $sformatf("Testing op_num %0d/20, op = 0x%0h", j, op), UVM_MEDIUM)
+        payload_size = get_rand_payload_size();
+
+        spi_host_xfer_flash_item(op, payload_size);
+        cfg.clk_rst_vif.wait_clks(10);
+      end
+    end
+
+  endtask : body
+endclass : spi_device_pass_all_vseq
