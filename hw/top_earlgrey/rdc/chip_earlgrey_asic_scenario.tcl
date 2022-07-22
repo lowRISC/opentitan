8094cecb9ee0e212314216296fafe0b4720660f9
Eunchan Kim <eunchan@opentitan.org>
2022-07-22 11:45:10 -0700

    feat(rdc): ENV file and Reset Scenario
    This commit tweaks the script to read ENV file for additional
    constraints based on the recommended method from Real Intent.
    
    In addition to above, this commit adds a reset scenario template.
    
    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>

---
+++ hw/top_earlgrey/rdc/chip_earlgrey_asic_scenario.tcl
@@ -0,0 +1,11 @@
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Meridian RDC Reset Scenario
+
+###################
+# Reset Scenarios #
+###################
+
+# TODO: SPI_CSB asserted(release from reset) only when SYS_RST_N is not asserted
