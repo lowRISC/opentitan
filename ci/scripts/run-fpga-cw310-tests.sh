e1a8aad36efbc7eaebea7f1788dadf40bccbce90
Dan McArdle <dmcardle@google.com>
2022-07-22 12:02:58 -0400

    [bazel] Use cached MMI when splicing bitstreams
    Using cached MMI files, we can quickly splice locally-built Mask and
    Test ROM images into a cached bitstream, avoiding a multi-hour bitstream
    build. Note that the splicing is performed locally, and thus requires
    Vivado's `updatemem` command to be installed.
    
    To splice a local ROM into a GCP-cached bitstream, specify this Bazel
    flag: `--define bitstream=gcp_splice` when building
    //hw/bitstream:mask_rom, //hw/bitstream:test_rom, or any target that
    depends on one of them.
    
    Issue #13603
    Issue #10867
    
    Signed-off-by: Dan McArdle <dmcardle@google.com>

--- ci/scripts/run-fpga-cw310-tests.sh
+++ ci/scripts/run-fpga-cw310-tests.sh
@@ -7,30 +7,38 @@ set -x
 set -e
 
 . util/build_consts.sh
-readonly SHA=$(git rev-parse HEAD)
-readonly BIT_CACHE_DIR="${HOME}/.cache/opentitan-bitstreams/cache/${SHA}"
-readonly BIT_SRC_PREFIX="${BIN_DIR}/hw/top_earlgrey/lowrisc_systems_chip_earlgrey_cw310_0.1.bit"
-readonly BIT_DST_PREFIX="${BIT_CACHE_DIR}/lowrisc_systems_chip_earlgrey_cw310_0.1.bit"
 
-mkdir -p ${BIT_CACHE_DIR}
-for suffix in orig splice; do
-  cp "${BIT_SRC_PREFIX}.${suffix}" "${BIT_DST_PREFIX}.${suffix}"
-done
-echo -n ${SHA} > ${BIT_CACHE_DIR}/../../latest.txt
+SHA=$(git rev-parse HEAD)
+readonly SHA
+
+# Copy bitstreams and related files into the cache directory so Bazel will have
+# the corresponding targets in the @bitstreams workspace.
+#
+# TODO(#13807) Update this when we change the naming scheme.
+readonly BIT_CACHE_DIR="${HOME}/.cache/opentitan-bitstreams/cache/${SHA}"
+readonly BIT_SRC_DIR="${BIN_DIR}/hw/top_earlgrey"
+readonly BIT_NAME_PREFIX="lowrisc_systems_chip_earlgrey_cw310_0.1.bit"
+mkdir -p "${BIT_CACHE_DIR}"
+cp "${BIT_SRC_DIR}/${BIT_NAME_PREFIX}.orig" \
+    "${BIT_SRC_DIR}/${BIT_NAME_PREFIX}.splice" \
+    "${BIT_SRC_DIR}/otp.mmi"  \
+    "${BIT_SRC_DIR}/rom.mmi" \
+    "${BIT_CACHE_DIR}"
+
+echo -n "$SHA" > "${BIT_CACHE_DIR}/../../latest.txt"
 export BITSTREAM="--offline --list ${SHA}"
 
 # We will lose serial access when we reboot, but if tests fail we should reboot
 # in case we've crashed the UART handler on the CW310's SAM3U
 trap 'python3 ./util/fpga/cw310_reboot.py' EXIT
 
-ci/bazelisk.sh test \
-    --define DISABLE_VERILATOR_BUILD=true \
-    --nokeep_going \
-    --test_tag_filters=cw310,-broken \
-    --test_timeout_filters=short,moderate \
-    --test_output=all \
-    --build_tests_only \
-    --define cw310=lowrisc \
-    --flaky_test_attempts=2 \
-    $(./bazelisk.sh query 'rdeps(//...,@bitstreams//:bitstream_test_rom)') \
-    $(./bazelisk.sh query 'rdeps(//...,@bitstreams//:bitstream_mask_rom)')
+./bazelisk.sh query 'rdeps(//..., @bitstreams//...)' |
+    xargs ci/bazelisk.sh test \
+        --define DISABLE_VERILATOR_BUILD=true \
+        --nokeep_going \
+        --test_tag_filters=cw310,-broken,-manual \
+        --test_timeout_filters=short,moderate \
+        --test_output=all \
+        --build_tests_only \
+        --define cw310=lowrisc \
+        --flaky_test_attempts=2
