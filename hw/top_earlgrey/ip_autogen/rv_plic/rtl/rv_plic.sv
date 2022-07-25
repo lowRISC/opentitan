9a396db0aebc2315759fdec5b3a402f320d3d877
Eunchan Kim <eunchan@opentitan.org>
2022-07-25 09:12:09 -0700

    fix(rv_plic): Typo
    Typo `alerts[0]`. It should be `alerts[i]`.
    
    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>

--- hw/top_earlgrey/ip_autogen/rv_plic/rtl/rv_plic.sv
+++ hw/top_earlgrey/ip_autogen/rv_plic/rtl/rv_plic.sv
@@ -391,7 +391,7 @@ module rv_plic import rv_plic_reg_pkg::*; #(
       .clk_i,
       .rst_ni,
       .alert_test_i  ( alert_test[i] ),
-      .alert_req_i   ( alerts[0]     ),
+      .alert_req_i   ( alerts[i]     ),
       .alert_ack_o   (               ),
       .alert_state_o (               ),
       .alert_rx_i    ( alert_rx_i[i] ),
