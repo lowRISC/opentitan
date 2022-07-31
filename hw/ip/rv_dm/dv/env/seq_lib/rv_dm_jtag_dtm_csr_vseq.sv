62e1aab08f7442c07a6b02509bc55ad91f07b8fc
Srikrishna Iyer <sriyer@google.com>
2022-07-30 19:02:51 -0700

    [rv_dm dv] Fix DTM RAL CSR failures
    There are 2 fixes - write exclusion on `dtmcs.dmihardreset` field
    and write of 0 to `dmi.op` field before CSR suite tests commence.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/rv_dm/dv/env/seq_lib/rv_dm_jtag_dtm_csr_vseq.sv
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_jtag_dtm_csr_vseq.sv
@@ -8,6 +8,9 @@ class rv_dm_jtag_dtm_csr_vseq extends rv_dm_common_vseq;
   `uvm_object_new
 
   virtual task body();
+    // Clear the jtag_dtm_ral.dmi.op field before starting the CSR suite of tests to ensure its
+    // mirrored value reflects 0 (nop) instead of the value from previous accesses.
+    csr_wr(.ptr(jtag_dtm_ral.dmi.op), .value(0), .blocking(1), .predict(1));
     run_csr_vseq_wrapper(.num_times(num_trans), .models({jtag_dtm_ral}));
   endtask : body
 
