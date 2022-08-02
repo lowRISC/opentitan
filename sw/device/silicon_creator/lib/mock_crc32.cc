8da1b490335786870b025d87d61aff73a855e33a
Alphan Ulusoy <alphan@google.com>
2022-08-02 13:57:35 -0400

    [sw/silicon_creator] Add mock_crc32
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

---
+++ sw/device/silicon_creator/lib/mock_crc32.cc
@@ -0,0 +1,33 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/mock_crc32.h"
+
+namespace mask_rom_test {
+extern "C" {
+
+void crc32_init(uint32_t *ctx) { MockCrc32::Instance().Init(ctx); }
+
+void crc32_add8(uint32_t *ctx, uint8_t byte) {
+  MockCrc32::Instance().Add8(ctx, byte);
+}
+
+void crc32_add32(uint32_t *ctx, uint32_t word) {
+  MockCrc32::Instance().Add32(ctx, word);
+}
+
+void crc32_add(uint32_t *ctx, const void *buf, size_t len) {
+  MockCrc32::Instance().Add(ctx, buf, len);
+}
+
+uint32_t crc32_finish(const uint32_t *ctx) {
+  return MockCrc32::Instance().Finish(ctx);
+}
+
+uint32_t crc32(const void *buf, size_t len) {
+  return MockCrc32::Instance().Crc32(buf, len);
+}
+
+}  // extern "C"
+}  // namespace mask_rom_test
---
+++ sw/device/silicon_creator/lib/mock_crc32.h
@@ -0,0 +1,33 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_MOCK_CRC32_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_MOCK_CRC32_H_
+
+#include "sw/device/lib/base/global_mock.h"
+#include "sw/device/silicon_creator/lib/crc32.h"
+
+namespace mask_rom_test {
+namespace internal {
+
+/**
+ * Mock class for crc32.
+ */
+class MockCrc32 : public global_mock::GlobalMock<MockCrc32> {
+ public:
+  MOCK_METHOD(void, Init, (uint32_t *));
+  MOCK_METHOD(void, Add8, (uint32_t *, uint8_t));
+  MOCK_METHOD(void, Add32, (uint32_t *, uint32_t));
+  MOCK_METHOD(void, Add, (uint32_t *, const void *, size_t));
+  MOCK_METHOD(uint32_t, Finish, (const uint32_t *));
+  MOCK_METHOD(uint32_t, Crc32, (const void *, size_t));
+};
+
+}  // namespace internal
+
+using MockCrc32 = testing::StrictMock<internal::MockCrc32>;
+
+}  // namespace mask_rom_test
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_MOCK_CRC32_H_
