b0f2f65dc29fc771d74c331d5ae5fedb0e2db77f
Srikrishna Iyer <sriyer@google.com>
2022-08-02 01:30:56 -0700

    [rv_dm, dv] Fixes for sberror=2,7 support
    Previously, if the SBA TL access returned an error response,
    it would have not impact (i.e. it would not show up) in the
    DMI SBCS status register, and the debugger would have no way
    of knowing an erroneous response occurred. Likewise, if the
    SBA TL response had a data integrity error, it would also not
    show up.
    
    The recent changes to the PULP debug module added support for
    indicating device error response as sberror=2 and also allowed
    TL device intg error to be indicated as other error (sberror=7).
    
    This commit updates the sba_access_utils_pkg (monitor and the
    utility tasks) and the RV_DM testbench (sequences and the
    scoreboard with fixes needed to support this design change.
    
    The sba access monitor can only predict sberror=3,4. It now
    wires the actual sberror value seen when polling the sbcs for
    status into the predicted SBA access item that is written to the
    analysis port. The rv_dm scoreboard which retrieves the item
    checks the correctness of sberror=2,7 based on the monitored
    TL transaction.
    
    The sba access util tasks have minor flow related enhancements
    to ensure the accesses to SBA registers in the DMI space are
    sequenced corrected to properly facilitate a SBA TL access.
    
    The rv_dm sba test sequences have similar fixes to properly
    sequence events to avoid unexpected bahaviors. The injection of
    SBA TL device error response and SBA TL device intg error is
    now disbled in the base sequence class, which now does only
    does clean accesses. These are constrained-randomized in the
    extended vseq classes instead, since they have a real impact
    on the behavior of the PULP debug module's SBA logic.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/dv/sv/jtag_dmi_agent/sba_access_item.sv
+++ hw/dv/sv/jtag_dmi_agent/sba_access_item.sv
@@ -7,50 +7,43 @@ class sba_access_item extends uvm_sequence_item;
   rand bus_op_e           bus_op;
   rand sba_access_size_e  size;
   rand logic [BUS_AW-1:0] addr;
-  rand logic [BUS_DW-1:0] wdata;
+  rand logic [BUS_DW-1:0] wdata[$];
 
   // Special stimulus configuration knobs.
   rand logic readonaddr;
   rand logic readondata;
-  rand logic autoincrement;
+
+  // Have the design autoincrement the address.
+  //
+  // If set to 0, the autoincrement feature is disabled and a single access is made. If set to N (N
+  // > 0), N + 1 accesses are made by the sba_access_utils_pkg::sba_acess() task. This is only used
+  // during the generation of stimulus. It is not used by the sba_access_monitor.
+  uint autoincrement;
 
   // Response side signals.
-  rand logic [BUS_DW-1:0] rdata;
-  rand  sba_access_err_e  is_err;
-  rand logic              is_busy_err;
-  rand logic              timed_out;
+  logic [BUS_DW-1:0] rdata[$];
+  sba_access_err_e   is_err;
+  logic              is_busy_err;
+  logic              timed_out;
+
+  constraint wdata_size_c {
+    wdata.size() == (autoincrement + 1);
+  }
 
   `uvm_object_utils_begin(sba_access_item)
     `uvm_field_enum(bus_op_e, bus_op,         UVM_DEFAULT)
     `uvm_field_enum(sba_access_size_e, size,  UVM_DEFAULT)
     `uvm_field_int (addr,                     UVM_DEFAULT)
-    `uvm_field_int (wdata,                    UVM_DEFAULT)
+    `uvm_field_queue_int(wdata,               UVM_DEFAULT)
     `uvm_field_int (readonaddr,               UVM_DEFAULT)
     `uvm_field_int (readondata,               UVM_DEFAULT)
     `uvm_field_int (autoincrement,            UVM_DEFAULT)
-    `uvm_field_int (rdata,                    UVM_DEFAULT)
+    `uvm_field_queue_int(rdata,               UVM_DEFAULT)
     `uvm_field_int (is_busy_err,              UVM_DEFAULT)
     `uvm_field_enum(sba_access_err_e, is_err, UVM_DEFAULT)
     `uvm_field_int (timed_out,                UVM_DEFAULT)
   `uvm_object_utils_end
 
   `uvm_object_new
-
-  virtual function void disable_req_randomization();
-    bus_op.rand_mode(0);
-    addr.rand_mode(0);
-    size.rand_mode(0);
-    wdata.rand_mode(0);
-    readonaddr.rand_mode(0);
-    readondata.rand_mode(0);
-    autoincrement.rand_mode(0);
-  endfunction
-
-  virtual function void disable_rsp_randomization();
-    rdata.rand_mode(0);
-    is_err.rand_mode(0);
-    is_busy_err.rand_mode(0);
-    timed_out.rand_mode(0);
-  endfunction
 
 endclass
--- hw/dv/sv/jtag_dmi_agent/sba_access_utils_pkg.sv
+++ hw/dv/sv/jtag_dmi_agent/sba_access_utils_pkg.sv
@@ -50,7 +50,6 @@ package sba_access_utils_pkg;
                             input bit sba_access_err_clear = 1'b1);
 
     uvm_reg_data_t rdata, wdata;
-    logic is_busy;
 
     // Update sbcs for the new transaction.
     wdata = jtag_dmi_ral.sbcs.get_mirrored_value();
@@ -64,45 +63,94 @@ package sba_access_utils_pkg;
       wdata = get_csr_val_with_updated_field(jtag_dmi_ral.sbcs.sbreadondata, wdata, 0);
     end
     wdata = get_csr_val_with_updated_field(jtag_dmi_ral.sbcs.sbautoincrement, wdata,
-                                           req.autoincrement);
+                                           (req.autoincrement > 0));
     if (wdata != jtag_dmi_ral.sbcs.sbaccess.get_mirrored_value()) begin
       csr_wr(.ptr(jtag_dmi_ral.sbcs), .value(wdata), .predict(1));
     end
 
+    // Update the sbaddress0.
     csr_wr(.ptr(jtag_dmi_ral.sbaddress0), .value(req.addr), .predict(1));
+
+    // These steps are run in several branches in the following code. Macroize them.
+`define BUSYWAIT_AND_EXIT_ON_ERR                                                          \
+  sba_access_busy_wait_and_clear_error(.jtag_dmi_ral(jtag_dmi_ral), .cfg(cfg), .req(req), \
+      .sba_access_err_clear(sba_access_err_clear));                                       \
+                                                                                          \
+  // We don't know what to do if any of the following is true - return to the caller:     \
+  //                                                                                      \
+  // - The request timed out or returned is_busy_err                                      \
+  // - The access reported non-0 sberror and sba_access_err_clear is set to 0             \
+  // - Request was malformed (An SBA access is not made in the case)                      \
+  // - The access returned SbaErrOther error response (The DUT may need special handling) \
+  if (req.timed_out ||                                                                    \
+      req.is_busy_err ||                                                                  \
+      (req.is_err != SbaErrNone && !sba_access_err_clear) ||                              \
+      req.is_err inside {SbaErrBadAlignment, SbaErrBadSize, SbaErrOther}) begin           \
+    return;                                                                               \
+  end                                                                                     \
+
+    // Write / read sbdata, wait for transaction to complete.
     if (req.bus_op == BusOpRead) begin
-      // Writing to addr with readonaddr would have already trigger an SBA read.
-      if (req.readondata && !req.readonaddr) begin
-        csr_rd(.ptr(jtag_dmi_ral.sbdata0), .value(rdata));
-      end
-      if (!req.readonaddr && !req.readondata) begin
-        `uvm_info(MsgId, {"readonaddr and readondata are not set. ",
-                          "Read request will not be triggered. Returning."}, UVM_MEDIUM)
-        return;
-      end
+      case ({req.readondata, req.readonaddr})
+        2'b00: begin
+          `uvm_info(MsgId, {"readonaddr and readondata are not set. ",
+                            "Read request will not be triggered. Returning."}, UVM_MEDIUM)
+        end
+        2'b01: begin
+          // SBA read already triggered on write to sbaddress0. Read sbdata0 to fetch the response
+          // data. If autoincrement is set, then return to the caller - it is not a valid usecase.
+          `BUSYWAIT_AND_EXIT_ON_ERR
+          if (!cfg.in_reset) begin
+            csr_rd(.ptr(jtag_dmi_ral.sbdata0), .value(rdata));
+            req.rdata[0] = rdata;
+          end
+        end
+        2'b10, 2'b11: begin
+          if (!req.readonaddr) begin
+            // The first read to sbdata returns stale data. Discard it.
+            if (!cfg.in_reset) begin
+              csr_rd(.ptr(jtag_dmi_ral.sbdata0), .value(rdata));
+            end
+          end
+          // The previous step triggered an SBA read. Wait for it to complete.
+          `BUSYWAIT_AND_EXIT_ON_ERR
+
+          // Read sbdata req.autoincrement+1 number of times.
+          //
+          // Randomly also inject reads to sbaddress0 to verify address is correctly incremented
+          // (done externally by the scoreboard).
+          for (int i = 0; i < req.autoincrement + 1; i++) begin
+            if (!cfg.in_reset) begin
+              csr_rd(.ptr(jtag_dmi_ral.sbdata0), .value(rdata));
+              req.rdata[i] = rdata;
+            end
+            `BUSYWAIT_AND_EXIT_ON_ERR
+            if (i > 0 && !cfg.in_reset && !$urandom_range(0, 3)) begin  //25%
+              csr_rd(.ptr(jtag_dmi_ral.sbaddress0), .value(rdata));
+            end
+          end
+        end
+        default: begin
+          `uvm_fatal(MsgId, "Unreachable!")
+        end
+      endcase
     end else begin
-      csr_wr(.ptr(jtag_dmi_ral.sbdata0), .value(req.wdata), .predict(1));
-    end
-
-    // Wait for the access to complete.
-    sba_access_busy_wait(jtag_dmi_ral, cfg, req, is_busy);
-
-    // If the access returns with an error, then the request was not made - return back to the
-    // caller.
-    if (req.is_busy_err || req.is_err != SbaErrNone) begin
-      if (!cfg.in_reset && sba_access_err_clear) begin
-        sba_access_error_clear(jtag_dmi_ral, req);
+      // Write sbdata req.autoincrement+1 number of times.
+      //
+      // Randomly also inject reads to sbaddress0 to verify address is correctly incremented
+      // (done externally by the scoreboard).
+      for (int i = 0; i < req.autoincrement + 1; i++) begin
+        if (!cfg.in_reset) begin
+          csr_wr(.ptr(jtag_dmi_ral.sbdata0), .value(req.wdata[i]), .predict(1));
+        end
+        `BUSYWAIT_AND_EXIT_ON_ERR
+        if (i > 0 && !cfg.in_reset && !$urandom_range(0, 3)) begin  //25%
+          csr_rd(.ptr(jtag_dmi_ral.sbaddress0), .value(rdata));
+        end
       end
-      return;
     end
 
-    // Return the data on reads.
-    if (req.bus_op == BusOpRead && !cfg.in_reset && !is_busy) begin
-      csr_rd(.ptr(jtag_dmi_ral.sbdata0), .value(req.rdata));
-    end
-
-    // If readondata is set, the read above will trigger another SBA read, which needs to be handled
-    // by the caller.
+`undef BUSYWAIT_AND_EXIT_ON_ERR
   endtask
 
   // Read the SBA access status.
@@ -114,15 +162,17 @@ package sba_access_utils_pkg;
     is_busy = get_field_val(jtag_dmi_ral.sbcs.sbbusy, data);
     req.is_busy_err = get_field_val(jtag_dmi_ral.sbcs.sbbusyerror, data);
     req.is_err = sba_access_err_e'(get_field_val(jtag_dmi_ral.sbcs.sberror, data));
-    `uvm_info(MsgId, $sformatf("SBA req status: %0s", req.sprint(uvm_default_line_printer)),
-              UVM_HIGH)
+    if (!is_busy) begin
+      `uvm_info(MsgId, $sformatf("SBA req completed: %0s", req.sprint(uvm_default_line_printer)),
+                UVM_HIGH)
+    end
   endtask
 
   // Reads sbcs register to poll and wait for access to complete.
   task automatic sba_access_busy_wait(input jtag_dmi_reg_block jtag_dmi_ral,
                                       input jtag_agent_cfg cfg,
-                                      input sba_access_item req,
-                                      output logic is_busy);
+                                      input sba_access_item req);
+    logic is_busy;
     `DV_SPINWAIT_EXIT(
       do begin
         sba_access_status(jtag_dmi_ral, req, is_busy);
@@ -148,16 +198,30 @@ package sba_access_utils_pkg;
   // Clear SBA access busy error and access error sticky bits if they are set.
   //
   // Note that the req argument will continue to reflect the error bits.
-  task automatic sba_access_error_clear(jtag_dmi_reg_block jtag_dmi_ral, sba_access_item req);
+  task automatic sba_access_error_clear(jtag_dmi_reg_block jtag_dmi_ral, sba_access_item req,
+                                        bit clear_sbbusyerror = 1'b1, bit clear_sberror = 1'b1);
     uvm_reg_data_t data = jtag_dmi_ral.sbcs.get_mirrored_value();
-    if (req.is_busy_err) begin
+    if (clear_sbbusyerror && req.is_busy_err) begin
       data = get_csr_val_with_updated_field(jtag_dmi_ral.sbcs.sbbusyerror, data, 1);
     end
-    if (req.is_err != SbaErrNone) begin
+    if (clear_sberror && req.is_err != SbaErrNone) begin
       data = get_csr_val_with_updated_field(jtag_dmi_ral.sbcs.sberror, data, 1);
     end
     csr_wr(.ptr(jtag_dmi_ral.sbcs), .value(data), .predict(1));
   endtask
+
+  // Wrapper task for busy wait followed by clearing of sberror if an error was reported.
+  task automatic sba_access_busy_wait_and_clear_error(input jtag_dmi_reg_block jtag_dmi_ral,
+                                                      input jtag_agent_cfg cfg,
+                                                      input sba_access_item req,
+                                                      input bit sba_access_err_clear);
+    sba_access_busy_wait(jtag_dmi_ral, cfg, req);
+    // Only clear sberror - let the caller handle sbbusyerror.
+    if (!cfg.in_reset && sba_access_err_clear && req.is_err != SbaErrNone) begin
+      sba_access_error_clear(.jtag_dmi_ral(jtag_dmi_ral), .req(req), .clear_sbbusyerror(0));
+    end
+  endtask
+
 
   `include "sba_access_item.sv"
   `include "sba_access_monitor.sv"
