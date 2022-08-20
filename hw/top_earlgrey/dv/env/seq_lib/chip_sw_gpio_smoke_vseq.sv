9693adfcb1d0c4321ef21ae11bac3c736dc53b9a
Srikrishna Iyer <sriyer@google.com>
2022-08-20 09:22:29 -0700

    [chip dv] Allow SW symbol not found in elf
    This commit updates the common uitl method which
    searches for the SW symbol in the elf file to add
    `does_not_exist_ok` argument. It is set by default
    to 0.
    
    If set to 1 and if the symbol is not found in the SW
    image (elf), then it simply returns. Otherwise, if
    the symbol is expected to exist in the elf and is not
    found, a fatal error is thrown.
    
    The advantage of doing this is we can unconditionally
    override symbols in common {lib, testutil etc} code
    that may or may not be a dependency of the test, rather
    than have to know which test will be dependent on the
    said lib code and condition the symbol override based
    on that.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_gpio_smoke_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_gpio_smoke_vseq.sv
@@ -22,8 +22,9 @@ class chip_sw_gpio_smoke_vseq extends chip_sw_base_vseq;
   function void pre_randomize();
     int addr;
     // Find how many bytes the SW allocate for SW_SYM_GPIO_VALS, then convert to word size.
-    sw_symbol_get_addr_size({p_sequencer.cfg.sw_images[SwTypeTest], ".elf"},
-                            SW_SYM_GPIO_VALS, addr, num_gpio_vals);
+    void'(sw_symbol_get_addr_size(.elf_file({p_sequencer.cfg.sw_images[SwTypeTest], ".elf"}),
+                                  .symbol(SW_SYM_GPIO_VALS), .does_not_exist_ok(0), .addr(addr),
+                                  .size(num_gpio_vals)));
     num_gpio_vals /= 4;
   endfunction
 
