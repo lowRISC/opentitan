1770fba9940e78785a0542c10f5d4e44f3b595ea
Eunchan Kim <eunchan@opentitan.org>
2022-08-22 13:27:02 -0700

    feat(rdc): Add SPI_DEVICE RX,TXFIFO reset signals to waivers
    RX/TX FIFOS SW reset signals are recognized as combo logic by the RDC
    tool. The reason is the outputs are also used to return data to SW. The
    actual reset lines are directly connected to the registers' output (q).
    
    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>

---
+++ hw/top_earlgrey/rdc/rdc_waivers.spi_device.tcl
@@ -0,0 +1,14 @@
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Meridian RDC Waivers for SPI_DEVICE
+#
+
+# W_RST_COMBO_LOGIC
+# rst_rx/txfifo signals are reset signals but also returns data to SW. it is
+# recognized as combo reset by tool but reset line is OK.
+# TODO: Add buffer to separate other logics from reset
+set_rule_status -rule {W_RST_COMBO_LOGIC} -status {Waived}  \
+  -expression {(SourceReset=~"*.u_spi_device.u_reg.u_control_rst_*xfifo.q*")} \
+  -comment {rst_rx,txfifo are reset signals directly connected to the registers.q}
