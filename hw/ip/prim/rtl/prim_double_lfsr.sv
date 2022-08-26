8c0cbaec09b09e20bee9e0013dd5623b312a9629
Srikrishna Iyer <sriyer@google.com>
2022-08-25 22:33:44 -0700

    [prim_lfsr] Enable randomization of initial seed
    This commit enables the randomization of the initial seed in the
    RTL itself, within an ifdef SYNTHESIS block.
    
    The prim_lfsr testbench required some associated fixes to get it
    working again.
    
    The prim_double_lfsr also required changes to ensure both internal
    LFSRs start off with the same seed value.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/ip/prim/rtl/prim_double_lfsr.sv
+++ hw/ip/prim/rtl/prim_double_lfsr.sv
@@ -84,6 +84,20 @@ module prim_double_lfsr #(
     );
   end
 
+`ifndef SYNTHESIS
+`ifndef VERILATOR
+  // Ensure both LFSRs start off with the same default seed. if randomized in simulations.
+  initial begin
+    wait (!$isunknown(gen_double_lfsr[0].u_prim_lfsr.DefaultSeedLocal));
+    wait (!$isunknown(gen_double_lfsr[1].u_prim_lfsr.DefaultSeedLocal));
+    gen_double_lfsr[1].u_prim_lfsr.DefaultSeedLocal =
+        gen_double_lfsr[0].u_prim_lfsr.DefaultSeedLocal;
+    $display("%m: Updated gen_double_lfsr[1].u_prim_lfsr.DefaultSeedLocal = 0x%0h",
+        gen_double_lfsr[1].u_prim_lfsr.DefaultSeedLocal);
+  end
+`endif
+`endif
+
   // Output the state from the first LFSR
   assign state_o = lfsr_state[0][StateOutDw-1:0];
   assign err_o = lfsr_state[0] != lfsr_state[1];
