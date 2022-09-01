28697b666891ec5fb1537415c3a69b2521254501
Jade Philipoom <jadep@google.com>
2022-09-01 13:53:35 +0200

    [crypto] Create templates for cryptolib API files.
    In preparation for splitting the API into multiple files, this commit
    creates small stubs for each header so that the following diffs will be
    easier to read.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

---
+++ sw/device/lib/crypto/include/ecc.h
@@ -0,0 +1,23 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_CRYPTO_INCLUDE_ECC_H_
+#define OPENTITAN_SW_DEVICE_LIB_CRYPTO_INCLUDE_ECC_H_
+
+/**
+ * @file
+ * @brief Elliptic curve operations for OpenTitan cryptography library.
+ *
+ * Includes ECDSA, ECDH, Ed25519, and X25519.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_CRYPTO_INCLUDE_ECC_H_
---
+++ sw/device/lib/crypto/include/rsa.h
@@ -0,0 +1,21 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_CRYPTO_INCLUDE_RSA_H_
+#define OPENTITAN_SW_DEVICE_LIB_CRYPTO_INCLUDE_RSA_H_
+
+/**
+ * @file
+ * @brief RSA signature operations for the OpenTitan cryptography library.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_CRYPTO_INCLUDE_RSA_H_
