109d72e2af408540b2170f334910def9224cc90b
Eli Kim <eli@opentitan.org>
2022-09-02 10:59:55 -0700

    feat(chip): Add chip_sw_sleep_pin_mio_dio_val test
    This commit scaffolds the `chip_sw_sleep_pin_mio_dio_val` test. It is
    currently intentionally failed (sw returns false).
    
    Following commits will implement the actual test code.
    
    Signed-off-by: Eli Kim <eli@opentitan.org>

---
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_sleep_pin_mio_dio_val_vseq.sv
@@ -0,0 +1,23 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class chip_sw_sleep_pin_mio_dio_val_vseq extends chip_sw_base_vseq;
+  `uvm_object_utils(chip_sw_sleep_pin_mio_dio_val_vseq)
+
+  `uvm_object_new
+
+  virtual task cpu_init();
+    super.cpu_init();
+  endtask : cpu_init
+
+  virtual task body();
+    super.body();
+
+  endtask : body
+
+  virtual task post_start();
+    super.post_start();
+  endtask : post_start
+
+endclass : chip_sw_sleep_pin_mio_dio_val_vseq
---
+++ sw/device/tests/sim_dv/sleep_pin_mio_dio_val_test.c
@@ -0,0 +1,20 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/dif/dif_pinmux.h"
+#include "sw/device/lib/dif/dif_pwrmgr.h"
+#include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/testing/pwrmgr_testutils.h"
+#include "sw/device/lib/testing/rand_testutils.h"
+#include "sw/device/lib/testing/test_framework/check.h"
+#include "sw/device/lib/testing/test_framework/ottf_main.h"
+
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+
+OTTF_DEFINE_TEST_CONFIG();
+
+bool test_main(void) {
+    return false;
+}
