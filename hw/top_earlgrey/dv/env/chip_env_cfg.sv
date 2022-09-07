ba36aec46d32d1a153cb15210b71cf38c94b785c
Alexander Williams <awill@google.com>
2022-09-07 15:52:37 -0700

    [top-test] Change DV bootstrap task to use SPI flash
    Add sequence to use SPI flash opcodes for bootstrapping.
    
    Signed-off-by: Alexander Williams <awill@google.com>

--- hw/top_earlgrey/dv/env/chip_env_cfg.sv
+++ hw/top_earlgrey/dv/env/chip_env_cfg.sv
@@ -56,7 +56,6 @@ class chip_env_cfg #(type RAL_T = chip_ral_pkg::chip_reg_block) extends cip_base
   // - .elf:          embedded executable
   // - .32.vmem:      mem image with 32-bit word size (for boot ROM)
   // - .64.vmem:      mem image with 64-bit word size (for sw_test / flash load)
-  // - .frames.vmem:  mem image converted with spiflash frames (for tests with boostrap enabled)
   // - .rodata.txt:   dump of RO sections of the SW
   // - .logs.txt:     dump of SW logs
   //
