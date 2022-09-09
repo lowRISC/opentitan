e4df7570ebfcca57170c024e2738d30cb5383650
Srikrishna Iyer <sriyer@google.com>
2022-09-09 14:25:37 -0700

    [dv, pwm] Remove pwn_en from agent
    This commit removes the pwn_en signal from the pwm
    monitor / interface since it is tied off at the
    block itself.
    
    Instead, an assertion check is added to pwm testbench.
    
    This is done to support chip level which only
    exposes the PWM output at the chip IOs.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/dv/sv/pwm_monitor/pwm_if.sv
+++ hw/dv/sv/pwm_monitor/pwm_if.sv
@@ -2,16 +2,15 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-interface pwm_if;
-  // core clock
-  logic clk;
-  logic rst_n;
+interface pwm_if (
+  input logic clk,
+  input logic rst_n
+);
 
   logic pwm;
-  logic pwm_en;
-
 
   clocking cb @(posedge clk);
-    input pwm, pwm_en;
+    input pwm;
   endclocking
+
 endinterface : pwm_if
