9551168bd163b64cec249ffbfcae3c6c471c7acd
Timothy Trippel <ttrippel@google.com>
2022-09-13 23:37:07 -0700

    [bazel,ottf] enable running OTTF tests at BL0
    This fixes #10712 by enabling OTTF-launched tests to run at BL0.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

---
+++ sw/device/silicon_owner/bare_metal/empty_test.c
@@ -0,0 +1,11 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdbool.h>
+
+#include "sw/device/lib/testing/test_framework/ottf_main.h"
+
+OTTF_DEFINE_TEST_CONFIG();
+
+bool test_main(void) { return true; }
