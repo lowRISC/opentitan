5ce32015e48b99a6b0c918a0298f0488e7cc14e8
Eli Kim <eli@opentitan.org>
2022-09-21 14:22:10 -0700

    feat(chip): Set LC State to PROD for sleep_pin_wake
    To not be disrupted by JTAG strap, sleep test now sets the LC State to
    PROD at the beginning.
    
    Signed-off-by: Eli Kim <eli@opentitan.org>

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_sleep_pin_wake_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_sleep_pin_wake_vseq.sv
@@ -122,6 +122,12 @@ class chip_sw_sleep_pin_wake_vseq extends chip_sw_base_vseq;
     };
   }
 
+  virtual task dut_init(string reset_kind = "HARD");
+    super.dut_init(reset_kind);
+
+    cfg.mem_bkdr_util_h[Otp].otp_write_lc_partition_state(LcStProd);
+  endtask : dut_init
+
   virtual task cpu_init();
     bit [7:0] byte_arr [];
 
