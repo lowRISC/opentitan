4cae521adffffe848846221951a98ac9ece75611
Jacob Levy <jacob.levy@nuvoton.com>
2022-09-22 16:53:47 +0300

    [ast] Update AST files header with auto-generated information
    Signed-off-by: Jacob Levy <jacob.levy@nuvoton.com>

--- hw/top_earlgrey/ip/ast/rtl/ast_dft.sv
+++ hw/top_earlgrey/ip/ast/rtl/ast_dft.sv
@@ -1,6 +1,10 @@
 // Copyright lowRISC contributors.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
+//
+// -------- W A R N I N G: A U T O - G E N E R A T E D  C O D E !! -------- //
+// PLEASE DO NOT HAND-EDIT THIS FILE. IT HAS BEEN AUTO-GENERATED.
+//
 //############################################################################
 // *Name: ast_dft
 // *Module Description: AST DFT
