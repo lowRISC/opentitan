43ad5a53274c5d62c544140ff02a369c5d3d3144
Guillermo Maturana <maturana@google.com>
2022-09-28 17:58:10 -0700

    [dv/sv] Fix comment typos and improve format
    This only changes comments.
    
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- hw/dv/sv/tl_agent/tl_reg_adapter.sv
+++ hw/dv/sv/tl_agent/tl_reg_adapter.sv
@@ -28,7 +28,7 @@ class tl_reg_adapter #(type ITEM_T = tl_seq_item) extends uvm_reg_adapter;
     ITEM_T bus_req;
     bus_req = ITEM_T::type_id::create("bus_req");
 
-    // TODO: add a knob to contrl the randomization in case TLUL implementation changes and does
+    // TODO: add a knob to control the randomization in case TLUL implementation changes and does
     // not support partial read/write
     // randomize CSR partial or full read
     // for partial read DUT (except memory) always return the entire 4 bytes bus data
