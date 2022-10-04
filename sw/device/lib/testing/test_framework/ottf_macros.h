921476b69cf308ea46e084e4736d3dea07e212fd
Timothy Trippel <ttrippel@google.com>
2022-10-03 21:48:37 -0700

    [tests] add example top-level concurrency test
    This adds an example top-level concurrency test to showcase how to use
    the OTTF to launch, and context switch between several concurrent FreeRTOS
    tasks.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

--- sw/device/lib/testing/test_framework/ottf_macros.h
+++ sw/device/lib/testing/test_framework/ottf_macros.h
@@ -9,5 +9,9 @@
 #define OTTF_NV_SCRATCH _non_volatile_scratch_start
 #define OTTF_HALF_WORD_SIZE (OTTF_WORD_SIZE / 2)
 #define OTTF_CONTEXT_SIZE (OTTF_WORD_SIZE * 30)
+#define OTTF_TASK_DELETE_SELF_OR_DIE \
+  ottf_task_delete_self();           \
+  abort();                           \
+  CHECK(false);
 
 #endif  // OPENTITAN_SW_DEVICE_LIB_TESTING_TEST_FRAMEWORK_OTTF_MACROS_H_
