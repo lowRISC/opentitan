81ffedecdd645d09411432016b0cd5a572974521
Srikrishna Iyer <sriyer@google.com>
2022-10-07 11:33:48 -0700

    [sw,testutils] Update pinmux testutils for GPIO
    THis commit adds data structures and methods to program
    the pinmux to connect specific chip IOs to the GPIO
    peripheral for both, input and output directions.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- sw/device/lib/testing/pinmux_testutils.c
+++ sw/device/lib/testing/pinmux_testutils.c
@@ -4,8 +4,10 @@
 
 #include "sw/device/lib/testing/pinmux_testutils.h"
 
+#include "sw/device/lib/arch/device.h"
 #include "sw/device/lib/base/macros.h"
 #include "sw/device/lib/dif/dif_base.h"
+#include "sw/device/lib/dif/dif_gpio.h"
 #include "sw/device/lib/dif/dif_pinmux.h"
 #include "sw/device/lib/runtime/hart.h"
 #include "sw/device/lib/testing/test_framework/check.h"
@@ -55,4 +57,59 @@ void pinmux_testutils_init(dif_pinmux_t *pinmux) {
   // Configure USBDEV SENSE outputs to be high-Z (IOC7)
   CHECK_DIF_OK(dif_pinmux_output_select(pinmux, kTopEarlgreyPinmuxMioOutIoc7,
                                         kTopEarlgreyPinmuxOutselConstantHighZ));
+}
+
+// Mapping of Chip IOs to the GPIO peripheral.
+//
+// Depending on the simulation platform, there may be a limitation to how chip
+// IOs are allocated to the GPIO peripheral, even for testing. The DV testbench
+// does not have this limitation, and is able to allocate as many chip IOs as
+// the number of GPIOs supported by the peripheral. At this time, these pin
+// assignments matches DV (see `hw/top_earlgrey/dv/env/chip_if.sv`).
+//
+// The pinout spreadsheet allocates fewer pins to GPIOs than what the GPIO IP
+// supports. This oversubscription is intentional to maximize testing.
+const dif_pinmux_index_t kPinmuxTestutilsGpioInselPins[kDifGpioNumPins] = {
+    kTopEarlgreyPinmuxInselIoa0,  kTopEarlgreyPinmuxInselIoa1,
+    kTopEarlgreyPinmuxInselIoa2,  kTopEarlgreyPinmuxInselIoa3,
+    kTopEarlgreyPinmuxInselIoa4,  kTopEarlgreyPinmuxInselIoa5,
+    kTopEarlgreyPinmuxInselIoa6,  kTopEarlgreyPinmuxInselIoa7,
+    kTopEarlgreyPinmuxInselIoa8,  kTopEarlgreyPinmuxInselIob6,
+    kTopEarlgreyPinmuxInselIob7,  kTopEarlgreyPinmuxInselIob8,
+    kTopEarlgreyPinmuxInselIob9,  kTopEarlgreyPinmuxInselIob10,
+    kTopEarlgreyPinmuxInselIob11, kTopEarlgreyPinmuxInselIob12,
+    kTopEarlgreyPinmuxInselIoc9,  kTopEarlgreyPinmuxInselIoc10,
+    kTopEarlgreyPinmuxInselIoc11, kTopEarlgreyPinmuxInselIoc12,
+    kTopEarlgreyPinmuxInselIor0,  kTopEarlgreyPinmuxInselIor1,
+    kTopEarlgreyPinmuxInselIor2,  kTopEarlgreyPinmuxInselIor3,
+    kTopEarlgreyPinmuxInselIor4,  kTopEarlgreyPinmuxInselIor5,
+    kTopEarlgreyPinmuxInselIor6,  kTopEarlgreyPinmuxInselIor7,
+    kTopEarlgreyPinmuxInselIor10, kTopEarlgreyPinmuxInselIor11,
+    kTopEarlgreyPinmuxInselIor12, kTopEarlgreyPinmuxInselIor13};
+
+const dif_pinmux_index_t kPinmuxTestutilsGpioMioOutPins[kDifGpioNumPins] = {
+    kTopEarlgreyPinmuxMioOutIoa0,  kTopEarlgreyPinmuxMioOutIoa1,
+    kTopEarlgreyPinmuxMioOutIoa2,  kTopEarlgreyPinmuxMioOutIoa3,
+    kTopEarlgreyPinmuxMioOutIoa4,  kTopEarlgreyPinmuxMioOutIoa5,
+    kTopEarlgreyPinmuxMioOutIoa6,  kTopEarlgreyPinmuxMioOutIoa7,
+    kTopEarlgreyPinmuxMioOutIoa8,  kTopEarlgreyPinmuxMioOutIob6,
+    kTopEarlgreyPinmuxMioOutIob7,  kTopEarlgreyPinmuxMioOutIob8,
+    kTopEarlgreyPinmuxMioOutIob9,  kTopEarlgreyPinmuxMioOutIob10,
+    kTopEarlgreyPinmuxMioOutIob11, kTopEarlgreyPinmuxMioOutIob12,
+    kTopEarlgreyPinmuxMioOutIoc9,  kTopEarlgreyPinmuxMioOutIoc10,
+    kTopEarlgreyPinmuxMioOutIoc11, kTopEarlgreyPinmuxMioOutIoc12,
+    kTopEarlgreyPinmuxMioOutIor0,  kTopEarlgreyPinmuxMioOutIor1,
+    kTopEarlgreyPinmuxMioOutIor2,  kTopEarlgreyPinmuxMioOutIor3,
+    kTopEarlgreyPinmuxMioOutIor4,  kTopEarlgreyPinmuxMioOutIor5,
+    kTopEarlgreyPinmuxMioOutIor6,  kTopEarlgreyPinmuxMioOutIor7,
+    kTopEarlgreyPinmuxMioOutIor10, kTopEarlgreyPinmuxMioOutIor11,
+    kTopEarlgreyPinmuxMioOutIor12, kTopEarlgreyPinmuxMioOutIor13};
+
+uint32_t pinmux_testutils_get_testable_gpios_mask(void) {
+  if (kDeviceType == kDeviceFpgaCw310) {
+    // Only IOA2 to IOA8 are available for use as GPIOs.
+    return 0x1fc;
+  } else {
+    return 0xffffffff;
+  }
 }
