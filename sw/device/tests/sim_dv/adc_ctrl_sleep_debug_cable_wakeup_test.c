b0542bcf233383bad316c069106b0ba741c25098
Timothy Chen <timothytim@google.com>
2022-10-07 19:34:35 -0700

    [top/dv] Tweak existing test to satisfy testpoint
    - The two adc_ctrl tests are extremely similar, except one
      calls for an interrupt and the other does not.
    - Enhance the test to check for interrupts to satisfy both
      requirements.
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- sw/device/tests/sim_dv/adc_ctrl_sleep_debug_cable_wakeup_test.c
+++ sw/device/tests/sim_dv/adc_ctrl_sleep_debug_cable_wakeup_test.c
@@ -6,6 +6,9 @@
 #include "sw/device/lib/dif/dif_adc_ctrl.h"
 #include "sw/device/lib/dif/dif_pwrmgr.h"
 #include "sw/device/lib/dif/dif_rstmgr.h"
+#include "sw/device/lib/dif/dif_rv_plic.h"
+#include "sw/device/lib/runtime/hart.h"
+#include "sw/device/lib/runtime/irq.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/pwrmgr_testutils.h"
 #include "sw/device/lib/testing/rstmgr_testutils.h"
@@ -13,6 +16,7 @@
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+#include "sw/device/lib/testing/autogen/isr_testutils.h"
 
 OTTF_DEFINE_TEST_CONFIG();
 
@@ -49,8 +53,49 @@ static void configure_adc_ctrl(const dif_adc_ctrl_t *adc_ctrl) {
                     .wake_up_time_aon_cycles = kWakeUpTimeAonCycles}));
 }
 
+static dif_adc_ctrl_t adc_ctrl;
+static dif_rv_plic_t plic;
+static volatile bool interrupt_expected = false;
+static volatile bool interrupt_serviced = false;
+
+void ottf_external_isr(void) {
+  plic_isr_ctx_t plic_ctx = {.rv_plic = &plic,
+                             .hart_id = kTopEarlgreyPlicTargetIbex0};
+
+  adc_ctrl_isr_ctx_t adc_ctrl_ctx = {
+      .adc_ctrl = &adc_ctrl,
+      .plic_adc_ctrl_start_irq_id = kTopEarlgreyPlicIrqIdAdcCtrlAonMatchDone,
+      .expected_irq = 0,
+      .is_only_irq = true};
+
+  top_earlgrey_plic_peripheral_t peripheral;
+  dif_adc_ctrl_irq_t adc_ctrl_irq;
+  isr_testutils_adc_ctrl_isr(plic_ctx, adc_ctrl_ctx, &peripheral,
+                             &adc_ctrl_irq);
+
+  CHECK(peripheral == kTopEarlgreyPlicPeripheralAdcCtrlAon);
+  CHECK(adc_ctrl_irq == kDifAdcCtrlIrqMatchDone);
+  interrupt_serviced = true;
+}
+
+static void en_plic_irqs(dif_rv_plic_t *plic) {
+  top_earlgrey_plic_irq_id_t plic_irqs[] = {
+      kTopEarlgreyPlicIrqIdAdcCtrlAonMatchDone};
+
+  for (uint32_t i = 0; i < ARRAYSIZE(plic_irqs); ++i) {
+    CHECK_DIF_OK(dif_rv_plic_irq_set_enabled(
+        plic, plic_irqs[i], kTopEarlgreyPlicTargetIbex0, kDifToggleEnabled));
+
+    // Assign a default priority
+    CHECK_DIF_OK(dif_rv_plic_irq_set_priority(plic, plic_irqs[i], 0x1));
+  }
+
+  // Enable the external IRQ at Ibex.
+  irq_global_ctrl(true);
+  irq_external_ctrl(true);
+}
+
 bool test_main(void) {
-  dif_adc_ctrl_t adc_ctrl;
   dif_pwrmgr_t pwrmgr;
   dif_rstmgr_t rstmgr;
 
@@ -60,6 +105,12 @@ bool test_main(void) {
       mmio_region_from_addr(TOP_EARLGREY_PWRMGR_AON_BASE_ADDR), &pwrmgr));
   CHECK_DIF_OK(dif_rstmgr_init(
       mmio_region_from_addr(TOP_EARLGREY_RSTMGR_AON_BASE_ADDR), &rstmgr));
+  CHECK_DIF_OK(dif_rv_plic_init(
+      mmio_region_from_addr(TOP_EARLGREY_RV_PLIC_BASE_ADDR), &plic));
+
+  // Enable adc interrupts.
+  CHECK_DIF_OK(dif_adc_ctrl_irq_set_enabled(&adc_ctrl, kDifAdcCtrlIrqMatchDone,
+                                            kDifToggleEnabled));
 
   uint16_t channel0_filter0_max =
       (kChannel0MaxHighByte << 8) | kChannel0MaxLowByte;
@@ -73,6 +124,8 @@ bool test_main(void) {
   // Assuming the chip hasn't slept yet, wakeup reason should be empty.
   if (pwrmgr_testutils_is_wakeup_reason(&pwrmgr, 0)) {
     LOG_INFO("POR reset.");
+    interrupt_expected = false;
+    en_plic_irqs(&plic);
 
     CHECK(rstmgr_testutils_is_reset_info(&rstmgr, kDifRstmgrResetInfoPor));
 
@@ -83,7 +136,7 @@ bool test_main(void) {
     CHECK_DIF_OK(dif_adc_ctrl_configure_filter(
         &adc_ctrl, kDifAdcCtrlChannel0,
         (dif_adc_ctrl_filter_config_t){.filter = kDifAdcCtrlFilter0,
-                                       .generate_irq_on_match = false,
+                                       .generate_irq_on_match = true,
                                        .generate_wakeup_on_match = true,
                                        .in_range = true,
                                        .max_voltage = channel0_filter0_max,
@@ -92,7 +145,7 @@ bool test_main(void) {
     CHECK_DIF_OK(dif_adc_ctrl_configure_filter(
         &adc_ctrl, kDifAdcCtrlChannel1,
         (dif_adc_ctrl_filter_config_t){.filter = kDifAdcCtrlFilter0,
-                                       .generate_irq_on_match = false,
+                                       .generate_irq_on_match = true,
                                        .generate_wakeup_on_match = true,
                                        .in_range = true,
                                        .max_voltage = channel1_filter0_max,
@@ -118,6 +171,9 @@ bool test_main(void) {
   } else if (pwrmgr_testutils_is_wakeup_reason(
                  &pwrmgr, kDifPwrmgrWakeupRequestSourceTwo)) {
     LOG_INFO("Wakeup reset.");
+    interrupt_expected = true;
+    en_plic_irqs(&plic);
+
     CHECK(rstmgr_testutils_is_reset_info(&rstmgr,
                                          kDifRstmgrResetInfoLowPowerExit));
     uint16_t adc_value;
@@ -131,6 +187,10 @@ bool test_main(void) {
     CHECK(channel1_filter0_min <= adc_value &&
           adc_value <= channel1_filter0_max);
 
+    // Interrupt should have been serviced.
+    CHECK(interrupt_serviced);
+    return true;
+
   } else {
     dif_pwrmgr_wakeup_reason_t wakeup_reason;
     CHECK_DIF_OK(dif_pwrmgr_wakeup_reason_get(&pwrmgr, &wakeup_reason));
@@ -138,5 +198,5 @@ bool test_main(void) {
               wakeup_reason.types, wakeup_reason.request_sources);
     return false;
   }
-  return true;
+  return false;
 }
