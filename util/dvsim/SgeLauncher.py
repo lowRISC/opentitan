6b08f0d9c3241ce7b9e099910511a16d7dc311f8
Sharon Topaz <sharon.topaz@nuvoton.com>
2022-10-12 11:58:35 +0300

    Sungrid input from command file instead of command line
    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    
    lint issues
    
    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    
    fix white spaces
    
    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    
    fix white spaces
    
    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>
    
    remove redandant str
    
    Signed-off-by: Sharon Topaz <sharon.topaz@nuvoton.com>

--- util/dvsim/SgeLauncher.py
+++ util/dvsim/SgeLauncher.py
@@ -80,6 +80,7 @@ class SgeLauncher(Launcher):
             sgeJob.args.b = 'y'  # This is a binary file
             sgeJob.args.o = self.deploy.get_log_path() + '.sge'
             cmd = str(sgeJob.execute(mode='echo'))
+            print('INFO: SGE command line : "' + str(cmd) + '"')
             # ---------------
             self.process = subprocess.Popen(shlex.split(cmd),
                                             bufsize=4096,
