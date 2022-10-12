8908d157d6ac4688c35ff7068dc3d253f0d1fc6a
Eli Kim <eli@opentitan.org>
2022-10-12 16:23:44 -0700

    feat(prim): Add Status Interrupt type to `prim_intr_hw`
    This commit defines a new interrput type and handling logic inside
    `prim_intr_hw`. The `Status` type is a persistent interrupt, which
    remains high until the cause is processed.
    
    fix(intr): Combinational output
    
    `prim_intr_hw` now generates combinational output for Status type
    interrupts. Previously, `prim_intr_hw` relied on INTR_STATE.q to raise
    the interrupt.
    
    The previous sequence created unexpected error on `plic_all_irqs` test
    as the IS received in Ibex is delayed a couple of Ibex cycles (due to
    clock ratio diff). It causes the test code to store the premature
    `_serviced` variable into a register, which makes the test checker
    failed.
    
    The test code should be race-condition free (will be addressed later).
    However, this commit revises `prim_intr_hw` for quick fix.
    
    Signed-off-by: Eli Kim <eli@opentitan.org>

--- hw/ip/prim/rtl/prim_intr_hw.sv
+++ hw/ip/prim/rtl/prim_intr_hw.sv
@@ -9,7 +9,19 @@
 
 module prim_intr_hw # (
   parameter int unsigned Width = 1,
-  parameter bit FlopOutput = 1
+  parameter bit FlopOutput = 1,
+
+  // IntrT parameter is to hint the logic for the interrupt type. Module
+  // supports two interrupt types, *Status* and *Event*.
+  //
+  // The differences between those two types are:
+  // - Status is persistent. Until the root cause is alleviated, the interrupt
+  //   keeps asserting.
+  // - Event remains high for a relatively short period time without SW
+  //   intervention. One distinct example is an error (error could be status
+  //   though). If a certain error condition is captured, HW logic may create a
+  //   pulse. In this case the interrupt is assumed as an Event interrupt.
+  parameter IntrT = "Event" // Event or Status
 ) (
   // event
   input  clk_i,
@@ -28,13 +40,39 @@ module prim_intr_hw # (
   output logic [Width-1:0]  intr_o
 );
 
-  logic  [Width-1:0]    new_event;
-  assign new_event =
-             (({Width{reg2hw_intr_test_qe_i}} & reg2hw_intr_test_q_i) | event_intr_i);
-  assign hw2reg_intr_state_de_o = |new_event;
-  // for scalar interrupts, this resolves to '1' with new event
-  // for vector interrupts, new events are OR'd in to existing interrupt state
-  assign hw2reg_intr_state_d_o  =  new_event | reg2hw_intr_state_q_i;
+  logic [Width-1:0] status; // incl. test
+
+  if (IntrT == "Event") begin : g_intr_event
+    logic  [Width-1:0]    new_event;
+    assign new_event =
+               (({Width{reg2hw_intr_test_qe_i}} & reg2hw_intr_test_q_i) | event_intr_i);
+    assign hw2reg_intr_state_de_o = |new_event;
+    // for scalar interrupts, this resolves to '1' with new event
+    // for vector interrupts, new events are OR'd in to existing interrupt state
+    assign hw2reg_intr_state_d_o  =  new_event | reg2hw_intr_state_q_i;
+
+    assign status = reg2hw_intr_state_q_i ;
+  end : g_intr_event
+  else if (IntrT == "Status") begin : g_intr_status
+    logic [Width-1:0] test_q; // Storing test. Cleared by SW
+
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+      if (!rst_ni) test_q <= '0;
+      else if (reg2hw_intr_test_qe_i) test_q <= reg2hw_intr_test_q_i;
+    end
+
+    // TODO: In Status type, INTR_STATE is better to be external type and RO.
+    assign hw2reg_intr_state_de_o = 1'b 1; // always represent the status
+    assign hw2reg_intr_state_d_o  = event_intr_i | test_q;
+
+    assign status = event_intr_i | test_q;
+
+    // To make the timing same to event type, status signal does not use CSR.q,
+    // rather the input of the CSR.
+    logic unused_reg2hw;
+    assign unused_reg2hw = ^reg2hw_intr_state_q_i;
+  end : g_intr_status
+
 
   if (FlopOutput == 1) begin : gen_flop_intr_output
     // flop the interrupt output
@@ -42,7 +80,7 @@ module prim_intr_hw # (
       if (!rst_ni) begin
         intr_o <= '0;
       end else begin
-        intr_o <= reg2hw_intr_state_q_i & reg2hw_intr_enable_q_i;
+        intr_o <= status & reg2hw_intr_enable_q_i;
       end
     end
 
@@ -54,5 +92,6 @@ module prim_intr_hw # (
     assign intr_o = reg2hw_intr_state_q_i & reg2hw_intr_enable_q_i;
   end
 
+  `ASSERT_INIT(IntrTKind_A, IntrT inside {"Event", "Status"})
 
 endmodule
