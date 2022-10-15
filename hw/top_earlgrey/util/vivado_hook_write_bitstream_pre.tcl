d9a65f685a8729c626e836b53115c48f54115998
Dan McArdle <dmcardle@google.com>
2022-10-15 18:10:42 -0400

    [fpga] Fix OTP-specific behavior in MMI file generator
    I discovered a few problems with the OTP-specific MMI files we have been
    generating prior to this commit.
    
    (1) The file's bitlanes were overlapping. For instance, the first
    bitlane claimed to cover bits 0-3 (inclusive) and the second one claimed
    to cover bits 1-4. This is expressly not allowed by Vivado's updatemem
    manual, so I knew something was wrong. By poking around the Vivado GUI,
    I realized that OTP bitlanes should each cover a single bit. I'm not
    sure whether that logic is correct or necessary for the ROM case, so I
    left it alone.
    
    (2) The MMI file claimed that BRAMS are the type "RAMB32" when the OTP
    BRAMs are actually RAMB18E1. It's not as simple as just saying "RAMB18"
    in the MMI file. Ultimately, I got it working after bumping the MMI's
    minor version from 0 to 1 and configuring an environment variable before
    calling updatemem.
    
    (3) The bitlane "Placement" attributes were all wrong because we were
    trying to trim the "RAMB36_" prefix from strings that actually began
    with "RAMB18_".
    
    (4) The AddressSpace and and DataWidth tags had inaccurate values.
    Apparently there is a significant amount of padding between OTP words in
    the OTP BRAMs. In fact, each word is followed by 15 zeroes.
    
    Separately, I also modified the TCL script to dump the OTP BRAMs'
    INIT_XX strings to a text file. Ultimately, examining these INIT_XX
    strings helped me crack the way that Vivado lays out data in the
    BRAMs (more detail in a later commit). However, examining the INIT_XX
    strings in the Vivado GUI required too much clicking; there are 64
    INIT_XX strings per OTP BRAM, and there are 22 BRAMs. This is purely a
    debugging aid, but it only takes a few seconds to generate.
    
    Signed-off-by: Dan McArdle <dmcardle@google.com>

--- hw/top_earlgrey/util/vivado_hook_write_bitstream_pre.tcl
+++ hw/top_earlgrey/util/vivado_hook_write_bitstream_pre.tcl
@@ -17,7 +17,7 @@ set_property BITSTREAM.CONFIG.USR_ACCESS TIMESTAMP [current_design]
 
 # Dump MMI for Boot ROM.
 
-proc generate_mmi {filename brams designtask_count} {
+proc generate_mmi {filename brams addr_space_name mem_type loc_prefix designtask_count} {
     send_msg "${designtask_count}-1" INFO "Dumping MMI to ${filename}"
 
     set workroot [file dirname [info script]]
@@ -32,9 +32,10 @@ proc generate_mmi {filename brams designtask_count} {
         if {$slice_begin eq {} || $slice_end eq {}} {
             send_msg "${designtask_count}-2" ERROR "Extraction of ${filename} information failed."
         }
+
         # The scrambled Boot ROM is actually 39 bits wide but the updatemem tool segfaults
         # for slice sizes not divisible by 8.
-        if {[expr {($slice_end - $slice_begin + 1) < 8}]} {
+        if {$filename eq "rom.mmi" && [expr {($slice_end - $slice_begin + 1) < 8}]} {
             set slice_end [expr {$slice_begin + 7}]
         }
         set addr_begin [get_property ram_addr_begin [get_cells $inst]]
@@ -42,32 +43,40 @@ proc generate_mmi {filename brams designtask_count} {
         if {$addr_begin eq {} || $addr_end eq {}} {
             send_msg "${designtask_count}-3" ERROR "Extraction of ${filename} MMI information failed."
         }
+
         # Calculate total number of bits.
         set space [expr {$space + ($addr_end - $addr_begin + 1) * ($slice_end - $slice_begin + 1)}]
+    }
+
+    if {$filename eq "otp.mmi"} {
+        set space [expr {$space * 16}]
     }
 
     # Generate the MMI.
     set space [expr {($space / 8) - 1}]
     puts $fileout "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-    puts $fileout "<MemInfo Version=\"1\" Minor=\"0\">"
+    puts $fileout "<MemInfo Version=\"1\" Minor=\"1\">"
     puts $fileout "  <Processor Endianness=\"Little\" InstPath=\"dummy\">"
-    puts $fileout "  <AddressSpace Name=\"rom\" Begin=\"0\" End=\"$space\">"
+    puts $fileout "  <AddressSpace Name=\"$addr_space_name\" Begin=\"0\" End=\"$space\">"
     puts $fileout "      <BusBlock>"
 
     set part [get_property PART [current_design]]
     foreach inst [lsort -dictionary $brams] {
         set loc [get_property LOC [get_cells $inst]]
-        set loc [string trimleft $loc RAMB36_]
+        set loc [string trimleft $loc $loc_prefix]
         set slice_begin [get_property ram_slice_begin [get_cells $inst]]
         set slice_end [get_property ram_slice_end [get_cells $inst]]
         # The scrambled Boot ROM is actually 39 bits wide but the updatemem tool segfaults
         # for slice sizes not divisible by 4.
-        if {[expr {($slice_end - $slice_begin + 1) < 4}]} {
+        if {$filename eq "rom.mmi" && [expr {($slice_end - $slice_begin + 1) < 4}]} {
             set slice_end [expr {$slice_begin + 3}]
         }
         set addr_begin [get_property ram_addr_begin [get_cells $inst]]
         set addr_end [get_property ram_addr_end [get_cells $inst]]
-        puts $fileout "        <BitLane MemType=\"RAMB32\" Placement=\"$loc\">"
+        if {$filename eq "otp.mmi"} {
+            set addr_end [expr {($addr_end + 1) * 16 - 1}]
+        }
+        puts $fileout "        <BitLane MemType=\"$mem_type\" Placement=\"$loc\">"
         puts $fileout "          <DataWidth MSB=\"$slice_end\" LSB=\"$slice_begin\"/>"
         puts $fileout "          <AddressRange Begin=\"$addr_begin\" End=\"$addr_end\"/>"
         puts $fileout "          <Parity ON=\"false\" NumBits=\"0\"/>"
@@ -84,8 +93,40 @@ proc generate_mmi {filename brams designtask_count} {
     send_msg "${designtask_count}-4" INFO "MMI dumped to ${filepath}"
 }
 
+proc dump_init_strings {filename brams designtask_count} {
+    # For each OTP BRAM, dump all the INIT_XX strings.
+    send_msg "${designtask_count}-1" INFO "Dumping INIT_XX strings to ${filename}"
+
+    set workroot [file dirname [info script]]
+    set filepath "${workroot}/${filename}"
+    set fileout [open $filepath "w"]
+
+    foreach inst [lsort -dictionary $brams] {
+        set bram [get_cells $inst]
+
+        set loc [get_property LOC $bram]
+        puts $fileout "LOC: $loc"
+
+        set init_count 0
+        while 1 {
+            set key [format "INIT_%.2X" $init_count]
+            if { [llength [list_property $bram $key]] eq 0 } {
+                break
+            }
+            set val [get_property $key $bram]
+            puts $fileout "$key $val"
+            incr init_count
+        }
+
+        puts $fileout ""
+    }
+    close $fileout
+    send_msg "${designtask_count}-4" INFO "INIT_XX strings dumped to ${filepath}"
+}
+
 set brams [split [get_cells -hierarchical -filter { PRIMITIVE_TYPE =~ BMEM.bram.* && NAME =~ *u_rom_ctrl*}] " "]
-generate_mmi "rom.mmi" $brams 1
+generate_mmi "rom.mmi" $brams "rom" "RAMB36" "RAMB36_" 1
 
 set brams [split [get_cells -hierarchical -filter { PRIMITIVE_TYPE =~ BMEM.bram.* && NAME =~ *u_otp_ctrl*}] " "]
-generate_mmi "otp.mmi" $brams 2
+generate_mmi "otp.mmi" $brams "otp" "RAMB18" "RAMB18_" 2
+dump_init_strings "otp_init_strings.txt" $brams 3
