b003b78a2cf2f74bdbeb1f660163ee61623cad50
Alphan Ulusoy <alphan@google.com>
2022-10-20 09:57:30 -0400

    [sw/silicon_creator] Add OTP items for RMA check in early boot
    This commit adds the `CREATOR_SW_CONFIG_RMA_SPIN_EN` and
    `CREATOR_SW_CONFIG_RMA_SPIN_CYCLES` OTP items for derisking RMA entry in
    PROD.
    
    Signed-off-by: Alphan Ulusoy <alphan@google.com>

--- hw/ip/otp_ctrl/rtl/otp_ctrl_part_pkg.sv
+++ hw/ip/otp_ctrl/rtl/otp_ctrl_part_pkg.sv
@@ -326,7 +326,9 @@ package otp_ctrl_part_pkg;
     }),
     6400'({
       64'h7D7EA64D850E128D,
-      4576'h0, // unallocated space
+      4512'h0, // unallocated space
+      32'h0,
+      32'h0,
       32'h0,
       32'h0,
       32'h0,
