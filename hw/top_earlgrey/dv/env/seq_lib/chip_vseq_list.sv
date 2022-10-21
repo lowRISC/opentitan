f32d443bfb327ad5de74e0d8ed6c3929b8f5f956
Hugo McNally <hugo.mcnally@gmail.com>
2022-10-21 12:40:57 +0100

    [test/top] RV_DM access after wakeup test
    - Added a test that covers `chip_rv_dm_access_after_wakeup`
      in the earlgrey testplan.
    
    - Slight tweak to the jtag_if to delay activity by one cycle after
      reset. Without doing, so the jtag_dmi_agent collides with the
      reset and always attempt to skip the first transaction.
    
    Co-authored-by: Timothy Chen <timothytim@google.com>
    Signed-off-by: Hugo McNally <hugo.mcnally@gmail.com>

--- hw/top_earlgrey/dv/env/seq_lib/chip_vseq_list.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_vseq_list.sv
@@ -52,6 +52,7 @@
 `include "chip_sw_keymgr_sideload_kmac_vseq.sv"
 `include "chip_sw_ast_clk_outputs_vseq.sv"
 `include "chip_sw_sensor_ctrl_status_intr_vseq.sv"
+`include "chip_sw_rv_dm_access_after_wakeup_vseq.sv"
 `include "chip_sw_pwrmgr_deep_sleep_all_wake_ups_vseq.sv"
 `include "chip_sw_adc_ctrl_sleep_debug_cable_wakeup_vseq.sv"
 `include "chip_tap_straps_vseq.sv"
