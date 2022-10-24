ffab90e6a8123980561696ac528fc162f9801893
Jade Philipoom <jadep@google.com>
2022-10-24 17:26:00 +0200

    [rom] Fix pinmux indexing and set attributes first.
    The pinmux initialization previously used the pinmux input selector
    value as the pad index, which results in an off-by-two error since the
    input selector is (pad index + 2).
    
    Also, the pinmux documentation specifies that pad attributes should be
    set first, before configuring the pinmux matrix, so this change moves
    attribute-setting to precede IO configuration.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

--- sw/device/silicon_creator/lib/drivers/pinmux.c
+++ sw/device/silicon_creator/lib/drivers/pinmux.c
@@ -25,7 +25,8 @@ enum {
  */
 typedef struct pinmux_input {
   top_earlgrey_pinmux_peripheral_in_t periph;
-  top_earlgrey_pinmux_insel_t pad;
+  top_earlgrey_pinmux_insel_t pad_insel;
+  top_earlgrey_muxed_pads_t pad;
 } pinmux_input_t;
 
 /**
@@ -41,7 +42,8 @@ typedef struct pinmux_output {
  */
 static const pinmux_input_t kInputUart0 = {
     .periph = kTopEarlgreyPinmuxPeripheralInUart0Rx,
-    .pad = kTopEarlgreyPinmuxInselIoc3,
+    .pad_insel = kTopEarlgreyPinmuxInselIoc3,
+    .pad = kTopEarlgreyMuxedPadsIoc3,
 };
 
 /**
@@ -58,15 +60,18 @@ static const pinmux_output_t kOutputUart0 = {
  */
 static const pinmux_input_t kInputSwStrap0 = {
     .periph = kTopEarlgreyPinmuxPeripheralInGpioGpio22,
-    .pad = kTopEarlgreyPinmuxInselIoc0,
+    .pad_insel = kTopEarlgreyPinmuxInselIoc0,
+    .pad = kTopEarlgreyMuxedPadsIoc0,
 };
 static const pinmux_input_t kInputSwStrap1 = {
     .periph = kTopEarlgreyPinmuxPeripheralInGpioGpio23,
-    .pad = kTopEarlgreyPinmuxInselIoc1,
+    .pad_insel = kTopEarlgreyPinmuxInselIoc1,
+    .pad = kTopEarlgreyMuxedPadsIoc1,
 };
 static const pinmux_input_t kInputSwStrap2 = {
     .periph = kTopEarlgreyPinmuxPeripheralInGpioGpio24,
-    .pad = kTopEarlgreyPinmuxInselIoc2,
+    .pad_insel = kTopEarlgreyPinmuxInselIoc2,
+    .pad = kTopEarlgreyMuxedPadsIoc2,
 };
 
 /**
@@ -77,7 +82,7 @@ static const pinmux_input_t kInputSwStrap2 = {
 static void configure_input(pinmux_input_t input) {
   abs_mmio_write32(kBase + PINMUX_MIO_PERIPH_INSEL_0_REG_OFFSET +
                        input.periph * sizeof(uint32_t),
-                   input.pad);
+                   input.pad_insel);
 }
 
 /**
@@ -85,24 +90,11 @@ static void configure_input(pinmux_input_t input) {
  *
  * @param pad A MIO pad.
  */
-static void enable_pull_down(top_earlgrey_pinmux_insel_t pad) {
+static void enable_pull_down(top_earlgrey_muxed_pads_t pad) {
   uint32_t reg =
       bitfield_bit32_write(0, PINMUX_MIO_PAD_ATTR_0_PULL_EN_0_BIT, true);
   abs_mmio_write32(
       kBase + PINMUX_MIO_PAD_ATTR_0_REG_OFFSET + pad * sizeof(uint32_t), reg);
-}
-
-/**
- * Configures the given pin as input and enables the internal pull-down.
- *
- * We enable internal pull-downs since we expect strong pull-ups on the strap
- * pins.
- *
- * @param input A peripheral input and MIO pad to link it to.
- */
-static void configure_strap_pin(pinmux_input_t input) {
-  configure_input(input);
-  enable_pull_down(input.pad);
 }
 
 /**
@@ -121,9 +113,14 @@ void pinmux_init(void) {
       otp_read32(OTP_CTRL_PARAM_OWNER_SW_CFG_ROM_BOOTSTRAP_EN_OFFSET);
   if (launder32(bootstrap_en) == kHardenedBoolTrue) {
     HARDENED_CHECK_EQ(bootstrap_en, kHardenedBoolTrue);
-    configure_strap_pin(kInputSwStrap0);
-    configure_strap_pin(kInputSwStrap1);
-    configure_strap_pin(kInputSwStrap2);
+    // Note: attributes should be configured before the pinmux matrix to avoid
+    // "undesired electrical behavior and/or contention at the pads".
+    enable_pull_down(kInputSwStrap0.pad);
+    enable_pull_down(kInputSwStrap1.pad);
+    enable_pull_down(kInputSwStrap2.pad);
+    configure_input(kInputSwStrap0);
+    configure_input(kInputSwStrap1);
+    configure_input(kInputSwStrap2);
   }
 
   configure_input(kInputUart0);
