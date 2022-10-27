a3ab7906e9c319d53ff1f8901ad486bc219cfb7e
Timothy Trippel <ttrippel@google.com>
2022-10-26 17:35:58 -0700

    [bazel] update rust toolchain `iso_date` and prefetch
    This updates the rust toolchain `iso_date` to a version for which a
    SHA256 string exists in `rules_rust` so it can be cached in the
    repository cache (needed for airgapped builds). Additionally, this
    prefetches the rust toolchain repositories for airgapped builds.
    
    This fixes #15640.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

--- util/prep-bazel-airgapped-build.sh
+++ util/prep-bazel-airgapped-build.sh
@@ -151,7 +151,9 @@ if [[ ${AIRGAPPED_DIR_CONTENTS} == "ALL" || \
     @python3_toolchains//... \
     @remotejdk11_linux//... \
     @riscv-compliance//... \
-    @rust_linux_x86_64__x86_64-unknown-linux-gnu_tools//...
+    @rust_linux_x86_64__x86_64-unknown-linux-gnu_tools//... \
+    @rust_opentitan_rv32imc__riscv32imc-unknown-none-elf//... \
+    @rust_opentitan_rv32imc__riscv32imc-unknown-none-elf_tools//...
   cp -R "$(${BAZELISK} info output_base)"/external/${BAZEL_PYTHON_WHEEL_REPO} \
     ${BAZEL_AIRGAPPED_DIR}/
   # We don't need all bitstreams in the cache, we just need the latest one so
