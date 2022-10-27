c062fd809486043d12e10a80b97ef22aae3dee71
Timothy Trippel <ttrippel@google.com>
2022-10-26 17:42:36 -0700

    [bazel] increase airgapped build test set
    This increases the targets that are built in an airgapped environment in
    CI to provide better coverage at catching future breakages.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

--- ci/scripts/test-airgapped-build.sh
+++ ci/scripts/test-airgapped-build.sh
@@ -19,13 +19,14 @@ sudo ip netns add airgapped
 sudo ip netns exec airgapped ip addr add 127.0.0.1/8 dev lo
 sudo ip netns exec airgapped ip link set dev lo up
 
-# Enter the network namespace and perform a build test.
+# Enter the network namespace and perform several builds.
 sudo ip netns exec airgapped sudo -u "$USER" bash -c \
   "export BAZEL_BITSTREAMS_CACHE=$(pwd)/bazel-airgapped/bitstreams-cache;
-  export BITSTREAM=--offline;
+  export BITSTREAM=\"--offline latest\";
   export BAZEL_PYTHON_WHEELS_REPO=$(pwd)/bazel-airgapped/ot_python_wheels;
   bazel-airgapped/bazel build \
-    --distdir=bazel-airgapped/bazel-distdir \
-    --repository_cache=bazel-airgapped/bazel-cache \
-    //sw/device/tests:uart_smoketest_signed_sim_dv;"
+    --distdir=$(pwd)/bazel-airgapped/bazel-distdir \
+    --repository_cache=$(pwd)/bazel-airgapped/bazel-cache \
+    --define DISABLE_VERILATOR_BUILD=true \
+    //sw/device/silicon_creator/..."
 exit 0
