0d3e153ffe3ceb795ed9584ef31b571da3637e59
Jon Flatley <jflat@google.com>
2022-10-31 16:22:02 -0400

    [opentitanlib] Cleanup unused OTP features
    OTP support in opentitantool is moving away from end to end vmem
    generation. Instead, opentitantool will serve as a front end utility for
    generating some software values (such as for alert_handler
    configuration) and for providing an way to specifying OTP values using
    named constants.
    
    Signed-off-by: Jon Flatley <jflat@google.com>

--- sw/host/opentitanlib/src/otp/mod.rs
+++ sw/host/opentitanlib/src/otp/mod.rs
@@ -7,33 +7,5 @@ pub mod alert_handler_regs;
 pub mod lc_state;
 // TODO(lowRISC/opentitan#15443): Fix this lint.
 #[allow(clippy::module_inception)]
-pub mod otp;
 pub mod otp_img;
-pub mod otp_mmap;
-pub mod vmem_serialize;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::testdata;
-    use anyhow::Result;
-
-    #[test]
-    fn test_vmem_serialize() -> Result<()> {
-        let mut otp_mmap = otp_mmap::OtpMap::new(&testdata!("otp_ctrl_mmap.hjson"))?;
-        let mut otp_img = otp_img::OtpImg::new(&testdata!("otp_ctrl_img_dev.hjson"))?;
-        let lc_state = lc_state::LcSecded::new(&testdata!("lc_ctrl_state.hjson"))?;
-        let vmem = otp_mmap.make_vmem(&mut otp_img)?;
-        let keys = otp_mmap.generate_keys(&otp_img);
-        let result = vmem.generate(keys, &lc_state)?;
-        let expected = std::fs::read_to_string(testdata!("output.vmem"))?;
-        let expected = expected
-            .split('\n')
-            .filter(|s| !s.is_empty())
-            .collect::<Vec<&str>>();
-
-        assert_eq!(result, expected);
-
-        Ok(())
-    }
-}
