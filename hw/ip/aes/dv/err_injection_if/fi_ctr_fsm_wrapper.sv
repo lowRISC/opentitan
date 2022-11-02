84ff3679456f51c48fdf5113f86d91aaa002a096
Andreas Kurth <adk@lowrisc.org>
2022-11-02 14:41:27 +0000

    [aes,dv] Define port types of `fi_ctr_fsm_wrapper`
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- hw/ip/aes/dv/err_injection_if/fi_ctr_fsm_wrapper.sv
+++ hw/ip/aes/dv/err_injection_if/fi_ctr_fsm_wrapper.sv
@@ -10,10 +10,10 @@ module fi_ctr_fsm_wrapper
   import uvm_pkg::*;
   import aes_env_pkg::*;
   #( parameter string IfName = "vif"
-  )
-  ( input clk_i,
-    input rst_ni
-    );
+  ) (
+    input logic clk_i,
+    input logic rst_ni
+  );
 
   // declare interface
   fi_ctr_fsm_if  fi_if (.*);
