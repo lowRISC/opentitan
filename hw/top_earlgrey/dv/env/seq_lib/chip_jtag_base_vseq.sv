1f26f6f55b4780c66f5b86aec9996684e2a11a3c
Timothy Chen <timothytim@google.com>
2022-11-03 22:40:05 -0700

    [pwrmgr] fixes for reset domain change
    Signed-off-by: Timothy Chen <timothytim@google.com>
    
    Temporary dv fix until strap_sampling / rv_dm enhancements land
    
    Signed-off-by: Timothy Chen <timothytim@google.com>

--- hw/top_earlgrey/dv/env/seq_lib/chip_jtag_base_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_jtag_base_vseq.sv
@@ -31,12 +31,30 @@ class chip_jtag_base_vseq extends chip_sw_base_vseq;
     csr_wr(.ptr(jtag_dmi_ral.dmcontrol.dmactive), .value(1), .blocking(1), .predict(1));
     cfg.clk_rst_vif.wait_clks(5);
     csr_wr(.ptr(jtag_dmi_ral.dmcontrol.ndmreset), .value(1));
-    cfg.clk_rst_vif.wait_clks(5);
+
+    // spin wait on reset completion
+    csr_spinwait(.ptr(ral.lc_ctrl.status.ready), .exp_data(0), .backdoor(1),
+                 .spinwait_delay_ns(1000));
+    `uvm_info(`gfn, $sformatf("saw lc ready"),
+              UVM_LOW)
+
+    csr_spinwait(.ptr(ral.lc_ctrl.status.ready), .exp_data(1), .backdoor(1),
+                 .spinwait_delay_ns(1000), .timeout_ns(5000000));
+    `uvm_info(`gfn, $sformatf("saw lc ready again"),
+              UVM_LOW)
   endtask
 
    task ndm_reset_off();
-      csr_wr(.ptr(jtag_dmi_ral.dmcontrol.ndmreset), .value(0));
-      cfg.clk_rst_vif.wait_clks(5);
+     // Temporary workaround until pinmux/rv_dm fixes are in
+    `uvm_info(`gfn, $sformatf("issue reset"),
+              UVM_LOW)
+     cfg.m_jtag_riscv_agent_cfg.m_jtag_agent_cfg.vif.do_trst_n(2);
+    `uvm_info(`gfn, $sformatf("reset done"),
+              UVM_LOW)
+     cfg.m_jtag_riscv_agent_cfg.m_jtag_agent_cfg.in_reset = 1'b0;
+
+     csr_wr(.ptr(jtag_dmi_ral.dmcontrol.ndmreset), .value(0));
+     cfg.clk_rst_vif.wait_clks(5);
    endtask
 
   virtual task apply_reset(string kind = "HARD");
