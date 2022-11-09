d7f5d506bd2a30ea62c57286ebae2a9a8e2c0391
Chris Frantz <cfrantz@google.com>
2022-11-09 10:02:49 -0800

    [testutil] Add sw_strap reading functions
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/device/lib/testing/pinmux_testutils.h
+++ sw/device/lib/testing/pinmux_testutils.h
@@ -11,6 +11,8 @@
 #include "sw/device/lib/dif/dif_gpio.h"
 #include "sw/device/lib/dif/dif_pinmux.h"
 
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+
 /**
  * Default pinmux initialization.
  *
@@ -37,5 +39,38 @@ extern const dif_pinmux_index_t kPinmuxTestutilsGpioMioOutPins[kDifGpioNumPins];
  * subset of GPIOs depending on the IO allocation limitations.
  */
 uint32_t pinmux_testutils_get_testable_gpios_mask(void);
+
+/**
+ * Reads a pin as a four-valued strapping pin.
+ *
+ * The value returned will be in the range [0..3] and represent the following
+ * values:
+ *   0: Strong 0
+ *   1: Weak 0
+ *   2: Weak 1
+ *   3: Strong 1
+ *
+ * @param pinmux A pinmux handle.
+ * @param gpio A GPIO handle.
+ * @param io The GPIO pin to read.
+ * @param pad The pinmux pad to which the GPIO pin is connected.
+ * @return A result in the range [0..3].
+ */
+uint32_t pinmux_testutils_read_strap_pin(dif_pinmux_t *pinmux, dif_gpio_t *gpio,
+                                         dif_gpio_pin_t io,
+                                         top_earlgrey_muxed_pads_t pad);
+
+/**
+ * Reads the OpenTitan sw strap pins for the strap configuration value.
+ *
+ * The value returned is the combination of 2-bit values returned from
+ * [IOC2, IOC1, IOC0] and ranges from [0..63].
+ * This function assumes the Earlgrey configuration where the external
+ * pads IOC{0,1,2} are used as software straps and that the pinmux is
+ * configured to map those pads to GPIOs 22, 23 and 24.
+ *
+ * @return A result in the range [0..63].
+ */
+uint32_t pinmux_testutils_read_straps(dif_pinmux_t *pinmux, dif_gpio_t *gpio);
 
 #endif  // OPENTITAN_SW_DEVICE_LIB_TESTING_PINMUX_TESTUTILS_H_
