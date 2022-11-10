61bcfcdedb5f24eb8e159db55901f8a37a78ce1a
Miguel Osorio <miguelosorio@google.com>
2022-11-09 17:27:56 -0800

    [otp/rom] Add entropy_src health OTP settings.
    This change adds OTP settings for the entropy_src health tests. These
    settings must be loaded into OTP before enabling ROM execution otherwise
    the entropy source will trigger a health test alert.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- hw/ip/otp_ctrl/rtl/otp_ctrl_part_pkg.sv
+++ hw/ip/otp_ctrl/rtl/otp_ctrl_part_pkg.sv
@@ -326,7 +326,18 @@ package otp_ctrl_part_pkg;
     }),
     6400'({
       64'h7D7EA64D850E128D,
-      4512'h0, // unallocated space
+      4160'h0, // unallocated space
+      32'h0,
+      32'h0,
+      32'h0,
+      32'h0,
+      32'h0,
+      32'h0,
+      32'h0,
+      32'h0,
+      32'h0,
+      32'h0,
+      32'h0,
       32'h0,
       32'h0,
       32'h0,
