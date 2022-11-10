1576948fab40cf5204bfe21a8da7cacc06417f40
Andreas Kurth <adk@lowrisc.org>
2022-11-10 07:54:15 +0000

    [top/dv] Invalidate icache multiple times in `rv_core_ibex_icache_invalidate` test
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- sw/device/tests/rv_core_ibex_icache_invalidate_test.c
+++ sw/device/tests/rv_core_ibex_icache_invalidate_test.c
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "sw/device/lib/base/csr.h"
 #include "sw/device/lib/runtime/hart.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 
@@ -9,8 +10,18 @@
 OTTF_DEFINE_TEST_CONFIG();
 
 bool test_main(void) {
-  // Invalidate instruction cache.
-  icache_invalidate();
+  // Invalidate instruction cache 7 times.  (The number of iterations has to be
+  // kept in sync with the vseq).
+  for (unsigned i = 0; i < 7; i++) {
+    icache_invalidate();
+
+    // Wait for the icache scramble key to become valid before requesting the
+    // next invalidation.
+    uint32_t cpuctrlsts;
+    do {
+      CSR_READ(CSR_REG_CPUCTRL, &cpuctrlsts);
+    } while ((cpuctrlsts & (1 << 8)) == 0);
+  }
 
   return true;
 }
