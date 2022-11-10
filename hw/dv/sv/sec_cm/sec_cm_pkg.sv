9eef9774e15b13f91820740ed0b51085640eb657
Srikrishna Iyer <sriyer@google.com>
2022-11-09 18:14:09 -0800

    [dv sec_cm_pkg] Improve find_sec_cm_if_proxy function
    Add support fort regex lookup, which was listed in the comment as
    TODO. Required for this (and a few other) tests.
    
    Signed-off-by: Srikrishna Iyer <sriyer@google.com>

--- hw/dv/sv/sec_cm/sec_cm_pkg.sv
+++ hw/dv/sv/sec_cm/sec_cm_pkg.sv
@@ -30,13 +30,22 @@ package sec_cm_pkg;
   //
   // This function matches the first instance whose `path` matches the input argument `path`.
   // A fatal error is thrown if there is no instance in the queue that matches the path.
-  // TODO: Allow partial path matching with `uvm_re_match`.
-  function automatic sec_cm_base_if_proxy find_sec_cm_if_proxy(string path);
+  // The argument `is_regex` indicates that the `path` is not a full path, but a regular expression.
+  function automatic sec_cm_base_if_proxy find_sec_cm_if_proxy(string path, bit is_regex = 0);
     sec_cm_base_if_proxy proxies[$];
-    // Search singleton queue of proxies in sec_cm_pkg
-    proxies = sec_cm_pkg::sec_cm_if_proxy_q.find_first() with (item.path == path);
-    if (proxies.size > 0) return proxies[0];
+    if (is_regex) begin
+      proxies = sec_cm_pkg::sec_cm_if_proxy_q.find_first() with (!uvm_re_match(path, item.path));
+    end else begin
+      proxies = sec_cm_pkg::sec_cm_if_proxy_q.find_first() with (item.path == path);
+    end
+    if (proxies.size()) begin
+      `uvm_info(msg_id, $sformatf({"find_sec_cm_if_proxy: found proxy instance for path %s: ",
+                                   "type = %0d, full path = %0s"}, path, proxies[0].sec_cm_type,
+                                  proxies[0].path), UVM_MEDIUM)
+      return proxies[0];
+    end
     else `uvm_fatal(msg_id, $sformatf("find_sec_cm_if_proxy: no proxy with path %s", path))
+    return null;
   endfunction
 
 endpackage
