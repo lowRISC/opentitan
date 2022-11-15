2bd45746474988f55021bf5078502f5f411d12c6
Timothy Trippel <ttrippel@google.com>
2022-11-14 23:19:52 -0800

    [rom_e2e,dv] increase timeouts for `sigverify_always` tests
    The test that operate in the test_unlocked0 LC state tests need longer
    timeouts.
    
    Signed-off-by: Timothy Trippel <ttrippel@google.com>

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_shutdown_output_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_shutdown_output_vseq.sv
@@ -59,7 +59,7 @@ class chip_sw_rom_e2e_shutdown_output_vseq extends chip_sw_base_vseq;
 
     // Wait until we receive the expected boot fault message length of bytes over UART0.
     `DV_WAIT(uart_tx_data_q.size() == exp_msg.len(),
-      "Timeout waiting for UART FIFO to fill.", 40_000_000)
+      "Timeout waiting for UART FIFO to fill.", 200_000_000)
     `uvm_info(`gfn, "Checking the UART TX data matches expected boot fault msg ...", UVM_LOW)
     foreach (uart_tx_data_q[i]) begin
       `DV_CHECK_EQ(uart_tx_data_q[i], exp_msg[i],
