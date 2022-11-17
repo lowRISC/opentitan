81785325ec6bfcb64d9e0143d46511f67eab548a
Guillermo Maturana <maturana@google.com>
2022-11-17 01:09:23 -0800

    [dv/rstmgr_cnsty_chk] Reach coverage for dedicated environment
    The dedicated test environment needs to reach above 90% coverage on all
    metrics.
    Change the test strategy to scan events past each other.
    Inject sparse fsm faults and check they fire.
    
    Fixes #9314
    
    Signed-off-by: Guillermo Maturana <maturana@google.com>

--- hw/ip/rstmgr/dv/rstmgr_cnsty_chk/tb.sv
+++ hw/ip/rstmgr/dv/rstmgr_cnsty_chk/tb.sv
@@ -4,11 +4,23 @@
 //
 // Testbench module for rstmgr_cnsty_chk.
 //
-// Checks that errors are flagged for the following cases.
-// - Child reset going active with inactive reset requests.
-// - Child reset going inactive with active reset requests.
+// The test runs pairs of events separated by a variable number of cycles in order to determine
+// when errors are triggered.
 //
-// NB: The checks are triggered by the reference clock.
+// Consider events E1 and E2: it can send E1 from N cycles before E2 up to M cycles after it and
+// any number of cycles in between.
+//
+// The event pairs considered are:
+// - Parent reset active, child reset active
+// - Sw reset active, child reset active
+// - Parent reset inactive, child reset inactive
+// - Sw reset inactive, child reset inactive
+//
+// There are two clocks involved, and it tests either one running faster then the other.
+//
+// In order to improve coverage it also injects sparse fsm errors.
+
+// Interface driving the dut.
 interface rstmgr_cnsty_chk_if;
   logic child_rst_ni;
   logic parent_rst_ni;
@@ -18,11 +30,14 @@ interface rstmgr_cnsty_chk_if;
   logic fsm_err_o;
 endinterface
 
+// Class generating stimulus.
 class reset_class;
-
   parameter time ClkPeriod = 10_000;
-  parameter time ChildFastClkPeriod = 6_000;
-  parameter time ChildSlowClkPeriod = 25_000;
+  parameter time ChildFastClkPeriod = 6_543;
+  parameter time ChildSlowClkPeriod = 25_678;
+
+  parameter int ScanSweepCycles = 40;
+  parameter int IterationsPerDelta = 16;
 
   import uvm_pkg::*;
 
@@ -51,27 +66,25 @@ class reset_class;
   virtual rstmgr_cnsty_chk_if reset_vif;
 
   logic error = 0;
-  int cycles_to_check = 7;
+  int cycles_to_check = 8;
+
+  bit parent_rst_n;
+  bit sw_reset;
+
+  int cycles_to_child_reset;
+  int cycles_to_child_release;
+  int cycles_to_parent_reset;
+  int cycles_to_parent_release;
+  int cycles_to_sw_reset;
+  int cycles_to_sw_release;
 
   rand int cycles_reset_width;
   rand int cycles_child_reset_width;
   rand int cycles_in_apply_resets;
-  rand int cycles_to_child_reset;
-  rand int cycles_to_child_release;
-  rand int cycles_to_parent_reset;
-  rand int cycles_to_parent_release;
-  rand int cycles_to_sw_reset;
-  rand int cycles_to_sw_release;
 
   constraint cycles_reset_width_c {cycles_reset_width inside {[2 : 10]};}
   constraint cycles_child_reset_width_c {cycles_child_reset_width inside {[2 : 10]};}
   constraint cycles_in_apply_resets_c {cycles_in_apply_resets inside {[5 : 25]};}
-  constraint cycles_to_child_reset_c {cycles_to_child_reset inside {[3 : 8]};}
-  constraint cycles_to_child_release_c {cycles_to_child_release inside {[3 : 6]};}
-  constraint cycles_to_parent_reset_c {cycles_to_parent_reset inside {[2 : 8]};}
-  constraint cycles_to_parent_release_c {cycles_to_parent_release inside {[3 : 6]};}
-  constraint cycles_to_sw_reset_c {cycles_to_sw_reset inside {[2 : 8]};}
-  constraint cycles_to_sw_release_c {cycles_to_sw_release inside {[3 : 6]};}
 
   function new(virtual clk_rst_if clk_vif, virtual clk_rst_if child_clk_vif,
                virtual rstmgr_cnsty_chk_if rst_vif);
@@ -103,6 +116,7 @@ class reset_class;
   endtask
 
   task apply_resets();
+    `uvm_info(`gfn, "Start apply_resets", UVM_MEDIUM)
     fork
       clk_rst_vif.apply_reset(.reset_width_clks(cycles_reset_width));
       child_clk_rst_vif.apply_reset(.reset_width_clks(cycles_child_reset_width));
@@ -111,8 +125,14 @@ class reset_class;
         clk_rst_vif.wait_clks(cycles_in_apply_resets);
         reset_vif.parent_rst_ni = 1'b1;
       end
+      begin
+        reset_vif.child_rst_ni = 1'b0;
+        child_clk_rst_vif.wait_clks(cycles_in_apply_resets);
+        reset_vif.child_rst_ni = 1'b1;
+      end
     join
-    clk_rst_vif.wait_clks(4);
+    clk_rst_vif.wait_clks(20);
+    `uvm_info(`gfn, "End apply_resets", UVM_MEDIUM)
   endtask
 
   task set_quiescent();
@@ -123,68 +143,189 @@ class reset_class;
   endtask
 
   task set_parent_reset(logic value, int cycles);
-    clk_rst_vif.wait_clks(cycles_to_parent_reset);
-    `uvm_info(`gfn, $sformatf("Setting parent_rst_ni=%b", value), UVM_MEDIUM)
+    if (reset_vif.parent_rst_ni == value) return;
+    `uvm_info(`gfn, $sformatf("Setting parent_rst_ni=%b after %0d cycles", value, cycles), UVM_HIGH)
+    clk_rst_vif.wait_clks(cycles);
     reset_vif.parent_rst_ni = value;
   endtask
 
   task set_sw_reset(logic value, int cycles);
-    clk_rst_vif.wait_clks(cycles_to_sw_reset);
-    `uvm_info(`gfn, $sformatf("Setting sw_rst_req_i=%b", value), UVM_MEDIUM)
+    if (reset_vif.sw_rst_req_i == value) return;
+    `uvm_info(`gfn, $sformatf("Setting sw_rst_req_i=%b after %0d cycles", value, cycles), UVM_HIGH)
+    clk_rst_vif.wait_clks(cycles);
     reset_vif.sw_rst_req_i = value;
   endtask
 
   task set_child_reset(logic value, int cycles);
+    if (reset_vif.child_rst_ni == value) return;
+    `uvm_info(`gfn, $sformatf("Setting child_rst_ni=%b after %0d cycles", value, cycles), UVM_HIGH)
     clk_rst_vif.wait_clks(cycles);
-    `uvm_info(`gfn, $sformatf("Setting child_rst_ni=%b", value), UVM_MEDIUM)
     reset_vif.child_rst_ni = value;
   endtask
 
-  task send_unexpected_child_resets();
-    `uvm_info(`gfn, "check unexpected active child reset", UVM_MEDIUM)
-    set_child_reset(.value(0), .cycles(cycles_to_child_reset));
-    clk_rst_vif.wait_clks(cycles_to_check);
-    `DV_CHECK_EQ(reset_vif.err_o, 1'b1, "expected error for unexpected active child reset")
-    clk_rst_vif.wait_clks(cycles_to_child_release);
-    reset_vif.child_rst_ni = 1'b1;
-  endtask
-
-  task send_unexpected_child_release();
-    `uvm_info(`gfn, "check unexpected inactive child reset", UVM_MEDIUM)
+  task reset_start();
     fork
-      set_parent_reset(.value(0), .cycles(cycles_to_parent_reset));
-      set_sw_reset(.value(1), .cycles(cycles_to_sw_reset));
+      set_parent_reset(.value(parent_rst_n), .cycles(cycles_to_parent_reset));
+      set_sw_reset(.value(sw_reset), .cycles(cycles_to_sw_reset));
       set_child_reset(.value(0), .cycles(cycles_to_child_reset));
     join
-    clk_rst_vif.wait_clks(cycles_to_child_release);
-    reset_vif.child_rst_ni = 1'b1;
-    clk_rst_vif.wait_clks(cycles_to_check);
-    `uvm_info(`gfn, "Checking err_o output", UVM_MEDIUM)
-    `DV_CHECK_EQ(reset_vif.err_o, 1'b1, "expected error for unexpected inactive child reset")
   endtask
 
-  task unexpected_child_activity();
-    `uvm_info(`gfn, "checking unexpected child activity", UVM_LOW)
-    for (int i = 0; i < 20; ++i) begin
+  task reset_end();
+    fork
+      set_parent_reset(.value(1), .cycles(cycles_to_parent_release));
+      set_sw_reset(.value(0), .cycles(cycles_to_sw_release));
+      set_child_reset(.value(1), .cycles(cycles_to_child_release));
+    join
+  endtask
+
+  // Run a number of reset scenarios with some given cycle delays to allow CDC cycle fluctuations.
+  task run_iterations(input string description, input int delta_cycles, output int error_count);
+    error_count = 0;
+    for (int i = 0; i < IterationsPerDelta; ++i) begin
       set_quiescent();
-      `DV_CHECK_RANDOMIZE_FATAL(this);
-
-      send_unexpected_child_resets();
-      // Expect an error, so reset the dut.
+      reset_start();
+      clk_rst_vif.wait_clks(20);
+      reset_end();
+      clk_rst_vif.wait_clks(cycles_to_check);
+      if (reset_vif.err_o) begin
+        ++error_count;
+      end
+      `uvm_info(`gfn, $sformatf(
+                "Scan %0s with cycles delta %0d error %b",
+                description,
+                delta_cycles,
+                reset_vif.err_o
+                ), UVM_MEDIUM)
+      // May get error, so reset.
+      set_quiescent();
       apply_resets();
+    end
+    `uvm_info(`gfn, $sformatf("error_count %0d", error_count), UVM_MEDIUM)
+  endtask
 
-      send_unexpected_child_release();
-      apply_resets();
+  // Run a parent reset to child reset.
+  task scan_parent_rst();
+    `uvm_info(`gfn, "scanning parent resets", UVM_LOW)
+    sw_reset = 0;
+    parent_rst_n = 0;
+    cycles_to_parent_release = 4;
+    cycles_to_child_release = 5;
+    cycles_to_child_reset = ScanSweepCycles / 2;
+    for (
+        cycles_to_parent_reset = 0;
+        cycles_to_parent_reset < ScanSweepCycles;
+        ++cycles_to_parent_reset
+    ) begin
+      int error_count = 0;
+      int delta_cycles = cycles_to_parent_reset - cycles_to_child_reset;
+      `uvm_info(`gfn, $sformatf("Sending parent reset %0d cycles from child", delta_cycles),
+                UVM_MEDIUM)
+      run_iterations("parent reset", delta_cycles, error_count);
+      `uvm_info(`gfn, $sformatf(
+                "Scan parent reset with cycles delta %0d error %0d", delta_cycles, error_count),
+                UVM_LOW)
+      `DV_CHECK(((delta_cycles <= -4) || (delta_cycles >= 4)) || (error_count == 0))
+      `DV_CHECK(((delta_cycles >= -5) && (delta_cycles <= 5)) || (error_count > 0))
     end
   endtask
 
+  task scan_parent_release();
+    `uvm_info(`gfn, "scanning parent release", UVM_LOW)
+    sw_reset = 0;
+    parent_rst_n = 0;
+    cycles_to_parent_reset = 5;
+    cycles_to_child_reset = 5;
+    cycles_to_child_release = ScanSweepCycles / 2;
+    for (
+        cycles_to_parent_release = 0;
+        cycles_to_parent_release < ScanSweepCycles;
+        ++cycles_to_parent_release
+    ) begin
+      int error_count = 0;
+      int delta_cycles = cycles_to_parent_release - cycles_to_child_release;
+      `uvm_info(`gfn, $sformatf("Sending parent release %0d cycles from child", delta_cycles),
+                UVM_MEDIUM)
+      run_iterations("parent release", delta_cycles, error_count);
+      `uvm_info(`gfn, $sformatf(
+                "Scan parent release with cycles delta %0d error %0d", delta_cycles, error_count),
+                UVM_LOW)
+      `DV_CHECK((delta_cycles >= 0) || (error_count == 0))
+      `DV_CHECK((delta_cycles < 1) || (error_count > 0))
+    end
+  endtask
+
+  task scan_sw_rst();
+    `uvm_info(`gfn, "scanning sw resets", UVM_LOW)
+    sw_reset = 1;
+    parent_rst_n = 1;
+    cycles_to_sw_release = 4;
+    cycles_to_child_release = 5;
+    cycles_to_child_reset = ScanSweepCycles / 2;
+    for (cycles_to_sw_reset = 0; cycles_to_sw_reset < ScanSweepCycles; ++cycles_to_sw_reset) begin
+      int error_count = 0;
+      int delta_cycles = cycles_to_sw_reset - cycles_to_child_reset;
+      `uvm_info(`gfn, $sformatf("Sending sw reset %0d cycles from child", delta_cycles), UVM_HIGH)
+      run_iterations("sw reset", delta_cycles, error_count);
+      `uvm_info(`gfn, $sformatf(
+                "Scan sw reset with cycles delta %0d error %0d", delta_cycles, error_count),
+                UVM_LOW)
+      `DV_CHECK((delta_cycles >= 3) || (error_count == 0))
+      `DV_CHECK((delta_cycles <= 3) || (error_count > 0))
+    end
+  endtask
+
+  task scan_sw_release();
+    `uvm_info(`gfn, "scanning sw releases", UVM_LOW)
+    sw_reset = 1;
+    parent_rst_n = 1;
+    cycles_to_sw_reset = 4;
+    cycles_to_child_reset = 5;
+    cycles_to_child_release = ScanSweepCycles / 2;
+    for (
+        cycles_to_sw_release = 0; cycles_to_sw_release < ScanSweepCycles; ++cycles_to_sw_release
+    ) begin
+      int error_count = 0;
+      int delta_cycles = cycles_to_sw_release - cycles_to_child_release;
+      `uvm_info(`gfn, $sformatf("Sending sw release %0d cycles from child", delta_cycles), UVM_HIGH)
+      run_iterations("sw release", delta_cycles, error_count);
+      `uvm_info(`gfn, $sformatf(
+                "Scan sw release with cycles delta %0d error %0d", delta_cycles, error_count),
+                UVM_LOW)
+      `DV_CHECK((delta_cycles >= 4) || (error_count == 0))
+      `DV_CHECK((delta_cycles <= 4) || (error_count > 0))
+    end
+  endtask
+
+  task inject_fsm_errors();
+    sec_cm_pkg::sec_cm_base_if_proxy if_proxy = sec_cm_pkg::find_sec_cm_if_proxy(
+        "tb.dut.u_state_regs", 0
+    );
+    `DV_CHECK(!reset_vif.fsm_err_o)
+    repeat (10) begin
+      clk_rst_vif.wait_clks(5);
+      if_proxy.inject_fault();
+      clk_rst_vif.wait_clks(5);
+      if_proxy.restore_fault();
+      `DV_CHECK(reset_vif.fsm_err_o)
+      apply_resets();
+      `DV_CHECK(!reset_vif.fsm_err_o)
+    end
+    clk_rst_vif.wait_clks(5);
+  endtask
+
   task body();
+    foreach (sec_cm_pkg::sec_cm_if_proxy_q[i]) begin
+      `uvm_info(`gfn, $sformatf("Path of proxy: %0s", sec_cm_pkg::sec_cm_if_proxy_q[i].path),
+                UVM_MEDIUM)
+    end
     clk_rst_vif.set_period_ps(ClkPeriod);
     clk_rst_vif.set_active();
     child_clk_rst_vif.set_period_ps(ChildFastClkPeriod);
     child_clk_rst_vif.set_active();
     `DV_CHECK_RANDOMIZE_FATAL(this);
 
+    `uvm_info(`gfn, "Past set active", UVM_MEDIUM)
     set_quiescent();
     apply_resets();
 
@@ -192,13 +333,49 @@ class reset_class;
     set_child_period(ChildClkFaster);
     clk_rst_vif.wait_clks(20);
 
-    unexpected_child_activity();
+    set_quiescent();
+    apply_resets();
+    scan_parent_rst();
+
+    set_quiescent();
+    apply_resets();
+    scan_parent_release();
+
+    set_quiescent();
+    apply_resets();
+    scan_sw_rst();
+
+    set_quiescent();
+    apply_resets();
+    scan_sw_release();
+
+    set_quiescent();
+    apply_resets();
 
     // Run with child clock slower than reference.
     set_child_period(ChildClkSlower);
     clk_rst_vif.wait_clks(20);
 
-    unexpected_child_activity();
+    set_quiescent();
+    apply_resets();
+    scan_parent_rst();
+
+    set_quiescent();
+    apply_resets();
+    scan_parent_release();
+
+    set_quiescent();
+    apply_resets();
+    scan_sw_rst();
+
+    set_quiescent();
+    apply_resets();
+    scan_sw_release();
+
+    // And inject sparse fsm errors.
+    set_quiescent();
+    apply_resets();
+    inject_fsm_errors();
   endtask
 endclass
 
@@ -213,17 +390,32 @@ module tb;
   wire child_clk_i;
   wire unused_child_rst_ni;
 
+  bind prim_sparse_fsm_flop prim_sparse_fsm_flop_if #(
+    .Width(Width),
+    .CustomForceName(CustomForceName)
+  ) prim_sparse_fsm_flop_if (.*);
+
   clk_rst_if clk_rst_if (
     .clk  (clk_i),
     .rst_n(rst_ni)
   );
   clk_rst_if child_clk_rst_if (
     .clk  (child_clk_i),
-    .rst_n(unused_child_rst_ni)
+    .rst_n(child_rst_ni)
   );
   rstmgr_cnsty_chk_if rstmgr_cnsty_chk_if ();
 
-  logic error = 0;
+  logic sw_rst_req_q;
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      sw_rst_req_q <= '0;
+    end else if (sw_rst_req_q && rstmgr_cnsty_chk_if.sw_rst_req_clr_o) begin
+      sw_rst_req_q <= '0;
+    end else if (!sw_rst_req_q && rstmgr_cnsty_chk_if.sw_rst_req_i &&
+                 !rstmgr_cnsty_chk_if.sw_rst_req_clr_o) begin
+      sw_rst_req_q <= 1'b1;
+    end
+  end
 
   rstmgr_cnsty_chk dut (
     .clk_i(clk_i),
@@ -231,7 +423,7 @@ module tb;
     .child_clk_i(child_clk_i),
     .child_rst_ni(rstmgr_cnsty_chk_if.child_rst_ni),
     .parent_rst_ni(rstmgr_cnsty_chk_if.parent_rst_ni),
-    .sw_rst_req_i(rstmgr_cnsty_chk_if.sw_rst_req_i),
+    .sw_rst_req_i(rstmgr_cnsty_chk_if.sw_rst_req_i | sw_rst_req_q),
     .sw_rst_req_clr_o(rstmgr_cnsty_chk_if.sw_rst_req_clr_o),
     .err_o(rstmgr_cnsty_chk_if.err_o),
     .fsm_err_o(rstmgr_cnsty_chk_if.fsm_err_o)
