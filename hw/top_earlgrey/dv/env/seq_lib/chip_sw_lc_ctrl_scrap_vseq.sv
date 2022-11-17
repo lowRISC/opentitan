f2029a1c988f64547d24aaaa618327c69228496e
Dror Kabely <dror.kabely@opentitan.org>
2022-11-17 15:36:03 +0200

    [dv/top_earlgrey] added regression for chip_sw_lc_ctrl_scrap test
    Signed-off-by: Dror Kabely <dror.kabely@opentitan.org>

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_lc_ctrl_scrap_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_lc_ctrl_scrap_vseq.sv
@@ -1,94 +1,74 @@
 // Copyright lowRISC contributors.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
-class chip_sw_lc_ctrl_raw_to_scrap_vseq extends chip_sw_base_vseq;
-  `uvm_object_utils(chip_sw_lc_ctrl_raw_to_scrap_vseq)
+class chip_sw_lc_ctrl_scrap_vseq extends chip_sw_base_vseq;
+  import lc_ctrl_state_pkg::dec_lc_state_e;
+  import lc_ctrl_state_pkg::lc_state_e;
+  import lc_ctrl_dv_utils_pkg::encode_lc_state;
+
+  `uvm_object_utils(chip_sw_lc_ctrl_scrap_vseq)
 
   `uvm_object_new
 
-  protected bit _end_of_test = 1'b0;
+  rand dec_lc_state_e src_state;
+  constraint valid_src_state_c {
+    !(src_state inside {DecLcStScrap, DecLcStInvalid, DecLcStPostTrans, DecLcStEscalate});
+  }
+
+  function void post_randomize();
+    super.post_randomize();
+    // source state is randomized, but if a plusarg is supplied, assign the given value
+    `DV_GET_ENUM_PLUSARG(lc_ctrl_state_pkg::dec_lc_state_e, src_state, src_dec_state)
+    `uvm_info(`gfn, $sformatf("Source state is %0s", src_state.name), UVM_MEDIUM)
+
+    // a guard to check we didn't screw up
+    `DV_CHECK_FATAL(src_state != DecLcStScrap)
+  endfunction : post_randomize
 
   virtual task pre_start();
     cfg.chip_vif.tap_straps_if.drive(JtagTapLc);
     super.pre_start();
-  endtask
+  endtask : pre_start
 
   virtual function void backdoor_override_otp();
     // Override the LC partition to TestLocked1 state.
-    cfg.mem_bkdr_util_h[Otp].otp_write_lc_partition_state(LcStRaw);
-  endfunction
+    cfg.mem_bkdr_util_h[Otp].otp_write_lc_partition_state(encode_lc_state(src_state));
+  endfunction : backdoor_override_otp
 
   virtual task dut_init(string reset_kind = "HARD");
-    // setting 'expect_fatal_alert' = 1 will trigger a dut_init at the end of the test
-    // which will change the SW logger from pass/fail to reset, which will result in a uvm error
-    // when entering the cleanup phases of the sequence
-    // so if we're at the end of the sequence, don't perform dut_init
-    if (_end_of_test) begin
-      return;
-    end
-
     super.dut_init(reset_kind);
     backdoor_override_otp();
-  endtask
+  endtask : dut_init
 
   virtual task body();
+    bit [BUS_DW-1:0] state;
     super.body();
 
-    `uvm_info(`gfn, $sformatf("Will run for %d iterations", num_trans), UVM_MEDIUM)
-    for (int trans_i = 1; trans_i <= num_trans; trans_i++) begin
-      // sw_symbol_backdoor_overwrite takes an array as the input.
-      bit [7:0] trans_i_array[] = {trans_i};
-      bit [TL_DW-1:0] ext_clock_en;
-      bit [BUS_DW-1:0] state;
+    // Wait for LC_CTRL post initialization
+    wait_lc_ready(.allow_err(1));
 
-      if (trans_i > 1) begin
-        `uvm_info(`gfn, $sformatf("Applying reset and otp override for trans %d", trans_i),
-                  UVM_MEDIUM)
-        apply_reset();
-        backdoor_override_otp();
-      end
+    // perform a LC state transition to SCRAP
+    jtag_lc_state_transition(src_state, DecLcStScrap);
 
-      wait_lc_ready(.allow_err(1));
+    // LC state transition requires a chip reset.
+    `uvm_info(`gfn, $sformatf("Applying reset after lc transition from %s state", src_state.name),
+              UVM_MEDIUM)
+    apply_reset();
 
-      jtag_lc_state_transition(DecLcStRaw, DecLcStScrap);
+    wait_lc_initialized(.allow_err(1));
 
-      // LC state transition requires a chip reset.
-      `uvm_info(`gfn, $sformatf("Applying reset after lc transition for trans %d", trans_i),
-                UVM_MEDIUM)
-      apply_reset();
-
-      // acquire access for JTAG to LC CTRL
-       wait_lc_initialized(.allow_err(1));
-
-      // check LC state is indeed in SCRAP
-      jtag_riscv_agent_pkg::jtag_read_csr(ral.lc_ctrl.lc_state.get_offset(),
-                                          p_sequencer.jtag_sequencer_h,
-                                          state);
-
-      `DV_CHECK_EQ(state, {6{DecLcStScrap}})
-    end
+    // check LC state is indeed in SCRAP
+    jtag_riscv_agent_pkg::jtag_read_csr(ral.lc_ctrl.lc_state.get_offset(),
+                                        p_sequencer.jtag_sequencer_h, state);
+    `DV_CHECK_EQ(state, {DecLcStateNumRep{DecLcStScrap}})
   endtask : body
 
   task post_start();
-  	// once in SCRAP mode, the CPU can't execute any SW code, so
-  	// we don't expect the SW to be done
-  	// so we shutdown the device, and then override the test exit from here
+    // once in SCRAP mode, the CPU can't execute any SW code, so
+    // we don't expect the SW to be done
+    // so we shutdown the device, and then override the test exit from here
     override_test_status_and_finish(.passed(1));
-
-    // changing into SCRAP mode fires an alert from OTP controller
-    // but as we expect this, don't check for fatal alerts
-    expect_fatal_alerts = 1'b1;
-
-    // cip_base_vseq::post_start calls `dut_init` when expect_fatal_alerts == 1
-    //
-    // this will change the SW logger status from pass/failed to reset, and will
-    // cause false `uvm_error when checking on the logger status
-    // when the vseq is in its closing stage
-    //
-    // _end_of_test == 1 will prevent this.dut_init from actually performing a reset
-    // and thus will bypass the problem
-    _end_of_test = 1'b1;
 
     super.post_start();
   endtask : post_start
-endclass : chip_sw_lc_ctrl_raw_to_scrap_vseq
+endclass : chip_sw_lc_ctrl_scrap_vseq
