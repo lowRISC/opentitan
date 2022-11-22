e75d935d568be1ef40166564d8dd28128dc061e8
Alexander Williams <awill@google.com>
2022-11-22 10:04:55 -0800

    [dv] Fix up errors in chip_sw_power_sleep_load
    Add missing run mode for chip_sw_power_sleep_load. The missing
    configuration prevented successful simulation, as required
    dependencies (like OTP generation) were not produced.
    
    Add randomization of the sleep parameters, and test that the plusargs
    are provided before attempting to get their value.
    
    Signed-off-by: Alexander Williams <awill@google.com>

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_power_sleep_load_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_power_sleep_load_vseq.sv
@@ -9,58 +9,74 @@ class chip_sw_power_sleep_load_vseq extends chip_sw_base_vseq;
   int duty_cycle[NUM_PWM_CHANNELS] = '{6,11,27,8,17,7};
   uint pkt_cnt[NUM_PWM_CHANNELS]  = '{default : 0};
   localparam uint CLOCK_PERIOD = 32;
-  bit junk;
 
   `uvm_object_new
 
+  virtual task cpu_init;
+    bit core_clk_off;
+    bit io_clk_off;
+    bit usb_slp_clk_off;
+    bit usb_act_clk_off;
+    bit deep_sleep_on;
+    {core_clk_off, io_clk_off, usb_slp_clk_off, usb_act_clk_off, deep_sleep_on} = $urandom();
+
+    super.cpu_init();
+
+    void'($value$plusargs("SW_CORE_CLK_OFF=%d", core_clk_off));
+    void'($value$plusargs("SW_IO_CLK_OFF=%d", io_clk_off));
+    void'($value$plusargs("SW_USB_SLP_CLK_OFF=%d", usb_slp_clk_off));
+    void'($value$plusargs("SW_USB_ACT_CLK_OFF=%d", usb_act_clk_off));
+    void'($value$plusargs("SW_DEEP_SLEEP_ON=%d", deep_sleep_on));
+
+    sw_symbol_bkdr_overwr_scalar("kCoreClkOff", core_clk_off);
+    sw_symbol_bkdr_overwr_scalar("kIoClkOff", io_clk_off);
+    sw_symbol_bkdr_overwr_scalar("kUsbSlpOff", usb_slp_clk_off);
+    sw_symbol_bkdr_overwr_scalar("kUsbActOff", usb_act_clk_off);
+    sw_symbol_bkdr_overwr_scalar("kDeepSleep", deep_sleep_on);
+  endtask
+
   virtual task body();
     super.body();
-
-    if ($value$plusargs("SW_CORE_CLK_OFF", junk)) sw_symbol_bkdr_overwr_scalar("kCoreClkOff", 1);
-    if ($value$plusargs("SW_IO_CLK_OFF", junk)  ) sw_symbol_bkdr_overwr_scalar("kIoClkOff", 1);
-    if ($value$plusargs("SW_USB_SLP_CLK_OFF", junk)) sw_symbol_bkdr_overwr_scalar("kUsbSlpOff", 1);
-    if ($value$plusargs("SW_USB_ACT_CLK_OFF", junk)) sw_symbol_bkdr_overwr_scalar("kUsbActOff", 1);
-    if ($value$plusargs("SW_DEEP_SLEEP_ON", junk)) sw_symbol_bkdr_overwr_scalar("kDeepSleep", 1);
 
     fork
 
       begin : GPIO_CHECK
         // Check IOA2 = 0
-        `DV_SPINWAIT(wait(cfg.sw_logger_vif.printed_log == "SW_INFO: GPIO active");,
-                     "timeout waiting for S/W sync GPIO1",
-                     cfg.sw_test_timeout_ns)
-        `DV_SPINWAIT(wait(cfg.chip_vif.gpios_if.pins[2] == '0);,
-                     $sformatf("Timed out waiting for IOA2 == %0h", 0),
-                     timeout_ns)
+        `DV_WAIT(cfg.sw_logger_vif.printed_log == "GPIO active",
+                 "timeout waiting for SW sync GPIO (1)",
+                 cfg.sw_test_timeout_ns)
+        `DV_WAIT(cfg.chip_vif.gpios_if.pins[2] == '0,
+                 $sformatf("Timed out waiting for IOA2 == %0h", 0),
+                 timeout_ns)
         // Check IOA2 = 1
-        `DV_SPINWAIT(wait(cfg.sw_logger_vif.printed_log == "SW_INFO: all H/W is active");,
-                     "timeout waiting for S/W sync GPIO2",
-                     cfg.sw_test_timeout_ns)
-        `DV_SPINWAIT(wait(cfg.chip_vif.gpios_if.pins[2] == '1);,
-                     $sformatf("Timed out waiting for IOA2 == %0h", 1),
-                     timeout_ns)
+        `DV_WAIT(cfg.sw_logger_vif.printed_log == "all HW is active",
+                 "timeout waiting for SW sync GPIO (2)",
+                 cfg.sw_test_timeout_ns)
+        `DV_WAIT(cfg.chip_vif.gpios_if.pins[2] == '1,
+                 $sformatf("Timed out waiting for IOA2 == %0h", 1),
+                 timeout_ns)
         // Check IOA2 = 0
-        `DV_SPINWAIT(wait(cfg.sw_logger_vif.printed_log == "SW_INFO: Prepare to exit");,
-                     "timeout waiting for S/W sync GPIO3",
-                     cfg.sw_test_timeout_ns)
-        `DV_SPINWAIT(wait(cfg.chip_vif.gpios_if.pins[2] == '0);,
-                     $sformatf("Timed out waiting for IOA2 == %0h", 0),
-                     timeout_ns)
+        `DV_WAIT(cfg.sw_logger_vif.printed_log == "Prepare to exit",
+                 "timeout waiting for SW sync GPIO (3)",
+                 cfg.sw_test_timeout_ns)
+        `DV_WAIT(cfg.chip_vif.gpios_if.pins[2] == '0,
+                 $sformatf("Timed out waiting for IOA2 == %0h", 0),
+                 timeout_ns)
       end : GPIO_CHECK
 
       begin : PWM_CHECK
-        `DV_SPINWAIT(wait(cfg.sw_logger_vif.printed_log == "SW_INFO: PWM active");,
-                     "timeout waiting for S/W sync PWM1",
-                     cfg.sw_test_timeout_ns)
+        `DV_WAIT(cfg.sw_logger_vif.printed_log == "PWM active",
+                 "timeout waiting for SW sync PWM (1)",
+                 cfg.sw_test_timeout_ns)
         foreach (cfg.m_pwm_monitor_cfg[i]) begin
           cfg.m_pwm_monitor_cfg[i].active = 1;
         end
         fork
           collect_pwm_data();   // this is infinite task
         join_none
-        `DV_SPINWAIT(wait(cfg.sw_logger_vif.printed_log == "SW_INFO: Prepare to exit");,
-                     "timeout waiting for S/W PWM2",
-                     cfg.sw_test_timeout_ns)
+        `DV_WAIT(cfg.sw_logger_vif.printed_log == "Prepare to exit",
+                 "timeout waiting for SW PWM (2)",
+                 cfg.sw_test_timeout_ns)
         foreach (cfg.m_pwm_monitor_cfg[i]) begin
           cfg.m_pwm_monitor_cfg[i].active = 0;
         end
