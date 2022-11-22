lowRISC/opentitan

commit 1f7eda046e9b72b4358698e433a010198f59187d

[kmac/rtl] Add key sharing to unmasked KMAC implementation
This enables SW to set the KMAC key in two shares even when
parameter EnMasking = 0. The key is computed by XORing of the two
shares. This feature was already possible for masked implementation
(EnMasking =1), but according to specifications it should also be
enabled for the unmasked version.

Signed-off-by: Vladimir Rozic <vrozic@lowrisc.org>

hw/ip/kmac/dv/env/kmac_scoreboard.sv
@@ -1441,11 +1441,7 @@ class kmac_scoreboard extends cip_base_scoreboard #(
           exp_keys = (sideload_en || (in_kmac_app && app_mode == AppKeymgr)) ?
                       get_keymgr_keys : keys;
           for (int i = 0; i < key_word_len; i++) begin
-            if (cfg.enable_masking) begin
-              unmasked_key.push_back(exp_keys[0][i] ^ exp_keys[1][i]);
-            end else begin
-              unmasked_key.push_back(exp_keys[0][i]);
-            end
+            unmasked_key.push_back(exp_keys[0][i] ^ exp_keys[1][i]);
             `uvm_info(`gfn, $sformatf("unmasked_key[%0d] = 0x%0x", i, unmasked_key[i]), UVM_HIGH)
           end
 

hw/ip/kmac/rtl/kmac.sv
@@ -245,6 +245,7 @@ module kmac
 
 
   // Secret Key signals
+  logic [MaxKeyLen-1:0] sw_key_data_reg [2];
   logic [MaxKeyLen-1:0] sw_key_data [Share];
   key_len_e             sw_key_len;
   logic [MaxKeyLen-1:0] key_data [Share];
@@ -467,31 +468,24 @@ module kmac
   // SEC_CM: SW_KEY.KEY.MASKING
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
-      sw_key_data[0] <= '0;
+      sw_key_data_reg[0] <= '0;
+      sw_key_data_reg[1] <= '0;
     end else if (engine_stable) begin
       for (int j = 0 ; j < MaxKeyLen/32 ; j++) begin
         if (reg2hw.key_share0[j].qe) begin
-          sw_key_data[0][32*j+:32] <= reg2hw.key_share0[j].q;
+          sw_key_data_reg[0][32*j+:32] <= reg2hw.key_share0[j].q;
+        end
+        if (reg2hw.key_share1[j].qe) begin
+          sw_key_data_reg[1][32*j+:32] <= reg2hw.key_share1[j].q;
         end
       end // for j
     end // else if engine_stable
   end // always_ff
 
   if (EnMasking) begin : gen_key_masked
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-      if (!rst_ni) begin
-        sw_key_data[1] <= '0;
-      end else if (engine_stable) begin
-        for (int i = 0 ; i < MaxKeyLen/32 ; i++) begin
-          if (reg2hw.key_share1[i].qe) begin
-            sw_key_data[1][32*i+:32] <= reg2hw.key_share1[i].q;
-          end
-        end // for i
-      end // else if engine_stable
-    end // always_ff
-  end else begin : gen_unused_key_share1
-    logic unused_keyshare1;
-    assign unused_keyshare1 = ^reg2hw.key_share1;
+    assign sw_key_data = sw_key_data_reg;
+  end else begin : gen_key_unmasked
+    assign sw_key_data[0] = sw_key_data_reg[0] ^ sw_key_data_reg[1];
   end
 
   assign sw_key_len = key_len_e'(reg2hw.key_len.q);

commit ded4e7a4200a81ec18f9636f10b5542f1490af23

[dv/kmac] Fix unmasked regression err
This PR fixes kmac unmasked regression error from PR #14178.
The RTL changes the key masking for register input only, but the DV
changes impacts the sideload key as well.
So this PR adds a logic to gate key masking if it is sideload key.

Signed-off-by: Cindy Chen <chencindy@opentitan.org>

hw/ip/kmac/dv/env/kmac_scoreboard.sv
@@ -1441,7 +1441,12 @@ class kmac_scoreboard extends cip_base_scoreboard #(
           exp_keys = (sideload_en || (in_kmac_app && app_mode == AppKeymgr)) ?
                       get_keymgr_keys : keys;
           for (int i = 0; i < key_word_len; i++) begin
-            unmasked_key.push_back(exp_keys[0][i] ^ exp_keys[1][i]);
+            if (sideload_en || (in_kmac_app && app_mode == AppKeymgr)) begin
+              // Sideload key from keymgr is already masked in keymgr.
+              unmasked_key.push_back(exp_keys[0][i]);
+            end else begin
+              unmasked_key.push_back(exp_keys[0][i] ^ exp_keys[1][i]);
+            end
             `uvm_info(`gfn, $sformatf("unmasked_key[%0d] = 0x%0x", i, unmasked_key[i]), UVM_HIGH)
           end
 