946be57b6da06f40e966b7c4600b64cc65aedfe3
Jacob Levy <jacob.levy@nuvoton.com>
2022-11-23 14:00:58 +0200

    [ast] Update OS ADC model to check channels connectivity
    Signed-off-by: Jacob Levy <jacob.levy@nuvoton.com>

--- hw/top_earlgrey/ip/ast/rtl/adc_ana.sv
+++ hw/top_earlgrey/ip/ast/rtl/adc_ana.sv
@@ -16,20 +16,30 @@ module adc_ana (
 `ifndef SYNTHESIS
 // Behavioral Model
 ////////////////////////////////////////
-ast_pkg::awire_t vref;
-ast_pkg::awire_t adc_vi0, adc_vi1;
+real vref = 2.3;
+real adc_vi0_hook = 1.0;
+real adc_vi1_hook = 1.0;
+real adc_vi0, adc_vi1;
 
-assign vref = 2.3;
+`ifdef ANALOGSIM
 assign adc_vi0 = adc_a0_ai;
 assign adc_vi1 = adc_a1_ai;
+`else
+assign adc_vi0 = adc_a0_ai ? adc_vi0_hook : 0.0;
+assign adc_vi1 = adc_a1_ai ? adc_vi1_hook : 0.0;
+`endif
 assign adc_d_ch0_o = $rtoi((adc_vi0/vref) * $itor(10'h3ff));
 assign adc_d_ch1_o = $rtoi((adc_vi1/vref) * $itor(10'h3ff));
 `else  // of SYNTHESIS
 // FPGA/VERILATOR
 ////////////////////////////////////////
+logic [10-1:0] adc_d_vi0_hook, adc_d_vi1_hook;
 
-assign adc_d_ch0_o = {9'h018, adc_a0_ai};  // 0.111V
-assign adc_d_ch1_o = {9'h10f, adc_a1_ai};  // 1.222V
+assign adc_d_vi0_hook = 10'h155;
+assign adc_d_vi1_hook = 10'h2AA;
+
+assign adc_d_ch0_o = adc_a0_ai ? adc_d_vi0_hook : 10'h000;
+assign adc_d_ch1_o = adc_a1_ai ? adc_d_vi1_hook : 10'h000;
 `endif
 
 endmodule : adc_ana
