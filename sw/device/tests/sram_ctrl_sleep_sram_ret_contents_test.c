7717f2ca9f06f88dfe9546aa298da96aa44a51e1
Douglas Reis <doreis@lowrisc.org>
2022-11-24 11:29:08 +0000

    [dv, sram_ctrl] `sleep_sram_ret_contents` add the normal sleep case
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/device/tests/sram_ctrl_sleep_sram_ret_contents_test.c
+++ sw/device/tests/sram_ctrl_sleep_sram_ret_contents_test.c
@@ -6,16 +6,19 @@
 #include "sw/device/lib/dif/dif_aon_timer.h"
 #include "sw/device/lib/dif/dif_pwrmgr.h"
 #include "sw/device/lib/dif/dif_rstmgr.h"
+#include "sw/device/lib/runtime/irq.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/aon_timer_testutils.h"
 #include "sw/device/lib/testing/pwrmgr_testutils.h"
 #include "sw/device/lib/testing/rstmgr_testutils.h"
+#include "sw/device/lib/testing/rv_plic_testutils.h"
 #include "sw/device/lib/testing/sram_ctrl_testutils.h"
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 #include "sw/device/silicon_creator/lib/drivers/retention_sram.h"
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+#include "sw/device/lib/testing/autogen/isr_testutils.h"
 
 enum {
   /**
@@ -32,6 +35,16 @@ enum {
 };
 
 OTTF_DEFINE_TEST_CONFIG();
+
+static dif_rv_plic_t rv_plic;
+static dif_pwrmgr_t pwrmgr;
+static plic_isr_ctx_t plic_ctx = {.rv_plic = &rv_plic,
+                                  .hart_id = kTopEarlgreyPlicTargetIbex0};
+static pwrmgr_isr_ctx_t pwrmgr_isr_ctx = {
+    .pwrmgr = &pwrmgr,
+    .plic_pwrmgr_start_irq_id = kTopEarlgreyPlicIrqIdPwrmgrAonWakeup,
+    .expected_irq = kDifPwrmgrIrqWakeup,
+    .is_only_irq = true};
 
 // Random data to read/write to/from retention SRAM.
 const uint32_t kTestData[kTestBufferSizeWords] = {
@@ -64,27 +77,45 @@ static void retention_sram_check(check_config_t config) {
 }
 
 /**
- * Override internal IRQ interrupt service routine to count
- * the number of integrity exceptions.
+ * Override internal interrupt handler to handle the ECC errors when reading the
+ * scrambled memory.
  */
 void ottf_internal_isr(void) {}
 
+/**
+ * Override external interrupt handler to handle the normal sleep IRQ.
+ */
+void ottf_external_isr(void) {
+  dif_pwrmgr_irq_t irq_id;
+  top_earlgrey_plic_peripheral_t peripheral;
+
+  isr_testutils_pwrmgr_isr(plic_ctx, pwrmgr_isr_ctx, &peripheral, &irq_id);
+
+  // Check that both the peripheral and the irq id are correct.
+  CHECK(peripheral == kTopEarlgreyPlicPeripheralPwrmgrAon,
+        "IRQ peripheral: %d is incorrect", peripheral);
+  CHECK(irq_id == kDifPwrmgrIrqWakeup, "IRQ ID: %d is incorrect", irq_id);
+}
+
 bool test_main(void) {
-  dif_pwrmgr_t pwrmgr;
   dif_rstmgr_t rstmgr;
   dif_aon_timer_t aon_timer;
   dif_sram_ctrl_t ret_sram;
 
-  CHECK_DIF_OK(dif_pwrmgr_init(
-      mmio_region_from_addr(TOP_EARLGREY_PWRMGR_AON_BASE_ADDR), &pwrmgr));
-  CHECK_DIF_OK(dif_rstmgr_init(
-      mmio_region_from_addr(TOP_EARLGREY_RSTMGR_AON_BASE_ADDR), &rstmgr));
-  CHECK_DIF_OK(dif_aon_timer_init(
-      mmio_region_from_addr(TOP_EARLGREY_AON_TIMER_AON_BASE_ADDR), &aon_timer));
+  // Enable global and external IRQ at Ibex.
+  irq_global_ctrl(true);
+  irq_external_ctrl(true);
 
-  CHECK_DIF_OK(dif_sram_ctrl_init(
-      mmio_region_from_addr(TOP_EARLGREY_SRAM_CTRL_RET_AON_REGS_BASE_ADDR),
-      &ret_sram));
+  mmio_region_t addr = mmio_region_from_addr(TOP_EARLGREY_PWRMGR_AON_BASE_ADDR);
+  CHECK_DIF_OK(dif_pwrmgr_init(addr, &pwrmgr));
+  addr = mmio_region_from_addr(TOP_EARLGREY_RSTMGR_AON_BASE_ADDR);
+  CHECK_DIF_OK(dif_rstmgr_init(addr, &rstmgr));
+  addr = mmio_region_from_addr(TOP_EARLGREY_AON_TIMER_AON_BASE_ADDR);
+  CHECK_DIF_OK(dif_aon_timer_init(addr, &aon_timer));
+  addr = mmio_region_from_addr(TOP_EARLGREY_SRAM_CTRL_RET_AON_REGS_BASE_ADDR);
+  CHECK_DIF_OK(dif_sram_ctrl_init(addr, &ret_sram));
+  addr = mmio_region_from_addr(TOP_EARLGREY_RV_PLIC_BASE_ADDR);
+  CHECK_DIF_OK(dif_rv_plic_init(addr, &rv_plic));
 
   dif_rstmgr_reset_info_bitfield_t rstmgr_reset_info;
   rstmgr_reset_info = rstmgr_testutils_reason_get();
@@ -100,6 +131,27 @@ bool test_main(void) {
     // Write data to retention SRAM and readback (to test basic functionality.)
     retention_sram_check((check_config_t){.do_write = true, .is_equal = true});
 
+    aon_timer_testutils_wakeup_config(&aon_timer, 100);
+    // Enable all the AON interrupts used in this test.
+    rv_plic_testutils_irq_range_enable(&rv_plic, kTopEarlgreyPlicTargetIbex0,
+                                       kTopEarlgreyPlicIrqIdPwrmgrAonWakeup,
+                                       kTopEarlgreyPlicIrqIdPwrmgrAonWakeup);
+    CHECK_DIF_OK(dif_pwrmgr_irq_set_enabled(&pwrmgr, 0, kDifToggleEnabled));
+
+    // Normal sleep.
+    pwrmgr_testutils_enable_low_power(
+        &pwrmgr, /*wakeups=*/kDifPwrmgrWakeupRequestSourceFive,
+        /*domain_config=*/kDifPwrmgrDomainOptionCoreClockInLowPower |
+            kDifPwrmgrDomainOptionUsbClockInActivePower |
+            kDifPwrmgrDomainOptionMainPowerInLowPower);
+    // Enter low power mode.
+    LOG_INFO("Issue WFI to enter normal sleep");
+    wait_for_interrupt();
+    LOG_INFO("Awaked");
+    // Check that the previously written retention SRAM data can still
+    // be read successfully.
+    retention_sram_check((check_config_t){.do_write = false, .is_equal = true});
+
     // Prepare rstmgr for a reset.
     rstmgr_testutils_pre_reset(&rstmgr);
     aon_timer_testutils_wakeup_config(&aon_timer, 20);
@@ -108,7 +160,7 @@ bool test_main(void) {
                                       kDifPwrmgrWakeupRequestSourceFive, 0);
 
     // Enter low power mode.
-    LOG_INFO("Issue WFI to enter sleep");
+    LOG_INFO("Issue WFI to enter deep sleep");
     wait_for_interrupt();
   } else if (rstmgr_reset_info & kDifRstmgrResetInfoLowPowerExit) {
     if (!(rstmgr_reset_info & kDifRstmgrResetInfoWatchdog)) {
@@ -153,6 +205,8 @@ bool test_main(void) {
 
   // Turn off the AON timer hardware completely before exiting.
   aon_timer_testutils_shutdown(&aon_timer);
-  CHECK_DIF_OK(dif_rstmgr_software_device_reset(&rstmgr));
+  if (kDeviceType == kDeviceSimDV) {
+    CHECK_DIF_OK(dif_rstmgr_software_device_reset(&rstmgr));
+  }
   return true;
 }
