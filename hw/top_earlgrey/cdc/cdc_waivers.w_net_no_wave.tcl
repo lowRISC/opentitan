f6d1ab04dba46a965ed02d162d0230a515d819e6
Joshua Park <jeoong@google.com>
2022-11-28 11:01:23 -0800

    [CDC] Resolved a deferred item in #16478
    - This issue was reported as a CDC violation in #16478
    - But, we confirmed it's not a CDC issue because prim_mubi4_sync handles it accordingly
    
    Signed-off-by: Joshua Park <jeoong@google.com>

--- hw/top_earlgrey/cdc/cdc_waivers.w_net_no_wave.tcl
+++ hw/top_earlgrey/cdc/cdc_waivers.w_net_no_wave.tcl
@@ -4,5 +4,5 @@
 #
 # Verix CDC waiver file
 
-set_rule_status -rule {S_NET_NO_WAVE} -expression {(Signal =~ "ast_init_done[3:1]") && (DriverType =~ "Undriven") && (FanoutClockDomain == "IO_DIV4_CLK")} -status {Deferred} -comment {Deferred to Post-M2 while waiting for Nuvoton to clarify : https://github.com/lowRISC/opentitan/issues/16478}
+set_rule_status -rule {S_NET_NO_WAVE} -expression {(Signal =~ "ast_init_done[3:1]") && (DriverType =~ "Undriven") && (FanoutClockDomain == "IO_DIV4_CLK")} -status {Waived} -comment {prim_mubi4_sync was not recognized by CDC tool}
 set_rule_status -rule {S_NET_NO_WAVE} -expression {(Signal =~ "top_earlgrey.*.u_memory_2p*u_mem.gen_generic.u_impl_generic.*_rdata_o*")} -status {Waived} -comment {Signals from 2p memory}
