d54525609f1040c9f0de32b4bda781dade2e9129
Weicai Yang <weicai@google.com>
2022-12-02 15:09:33 -0800

    [chip, dv] Fix chip_sw_sleep_sram_ret_contents
    Found a couple issues in this test.
    1. According to the testplan below, we need to test data survives after any low power
    exit, so we shouldn't do scramble as the new request key isn't preserved after deep sleep or
    reset.
    > Ensure that the data within the retention SRAM survives ALL low power entry-exit
    variations.
    2. Change to check the data is the same as before after deep sleep exit.
    3. No need to issue a reset at the end of the C test. It was needed because we requested new
    scramble key, after wakeup from deep sleep, the key is reseted and reading uninitialized ret
    sram caused intergrity errors and a fatal alert. The fatal alert prevents sim from finishing unless
    we reset it.
    
    Signed-off-by: Weicai Yang <weicai@google.com>

--- sw/device/tests/sram_ctrl_sleep_sram_ret_contents_test.c
+++ sw/device/tests/sram_ctrl_sleep_sram_ret_contents_test.c
@@ -4,11 +4,13 @@
 
 #include "sw/device/lib/base/mmio.h"
 #include "sw/device/lib/dif/dif_aon_timer.h"
+#include "sw/device/lib/dif/dif_flash_ctrl.h"
 #include "sw/device/lib/dif/dif_pwrmgr.h"
 #include "sw/device/lib/dif/dif_rstmgr.h"
 #include "sw/device/lib/runtime/irq.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/aon_timer_testutils.h"
+#include "sw/device/lib/testing/flash_ctrl_testutils.h"
 #include "sw/device/lib/testing/pwrmgr_testutils.h"
 #include "sw/device/lib/testing/rstmgr_testutils.h"
 #include "sw/device/lib/testing/rv_plic_testutils.h"
@@ -37,7 +39,9 @@ enum {
 OTTF_DEFINE_TEST_CONFIG();
 
 static dif_rv_plic_t rv_plic;
+static dif_aon_timer_t aon_timer;
 static dif_pwrmgr_t pwrmgr;
+static dif_rstmgr_t rstmgr;
 static plic_isr_ctx_t plic_ctx = {.rv_plic = &rv_plic,
                                   .hart_id = kTopEarlgreyPlicTargetIbex0};
 static pwrmgr_isr_ctx_t pwrmgr_isr_ctx = {
@@ -57,6 +61,11 @@ typedef struct {
   bool do_write;
   bool is_equal;
 } check_config_t;
+
+// this is preserved across resets. Flash default value is all 1s,
+// can be written to 0 without an erase, so name this `non` scramble.
+// flash doesn't support byte write, hence define it to a 32 bit int.
+OT_SECTION(".non_volatile_scratch") uint32_t ret_non_scrambled;
 
 static void retention_sram_check(check_config_t config) {
   if (config.do_write) {
@@ -91,15 +100,76 @@ void ottf_external_isr(void) {
 
   isr_testutils_pwrmgr_isr(plic_ctx, pwrmgr_isr_ctx, &peripheral, &irq_id);
 
+  LOG_INFO("Receive irq in normal sleep");
   // Check that both the peripheral and the irq id are correct.
   CHECK(peripheral == kTopEarlgreyPlicPeripheralPwrmgrAon,
         "IRQ peripheral: %d is incorrect", peripheral);
   CHECK(irq_id == kDifPwrmgrIrqWakeup, "IRQ ID: %d is incorrect", irq_id);
 }
 
+// test these 2 cases:
+// normal sleep, no scrambling -> data preserved
+// normal sleep, with scrambling -> data preserved
+void test_ret_sram_in_normal_sleep(void) {
+  // Write data to retention SRAM and readback (to test basic functionality.)
+  retention_sram_check((check_config_t){.do_write = true, .is_equal = true});
+
+  // set up wakeup timer
+  aon_timer_testutils_wakeup_config(&aon_timer, 20);
+  // Enable all the AON interrupts used in this test.
+  rv_plic_testutils_irq_range_enable(&rv_plic, kTopEarlgreyPlicTargetIbex0,
+                                     kTopEarlgreyPlicIrqIdPwrmgrAonWakeup,
+                                     kTopEarlgreyPlicIrqIdPwrmgrAonWakeup);
+  CHECK_DIF_OK(dif_pwrmgr_irq_set_enabled(&pwrmgr, 0, kDifToggleEnabled));
+
+  // Normal sleep.
+  pwrmgr_testutils_enable_low_power(
+      &pwrmgr, /*wakeups=*/kDifPwrmgrWakeupRequestSourceFive,
+      /*domain_config=*/kDifPwrmgrDomainOptionCoreClockInLowPower |
+          kDifPwrmgrDomainOptionUsbClockInActivePower |
+          kDifPwrmgrDomainOptionMainPowerInLowPower);
+  // Enter low power mode.
+  LOG_INFO("Issue WFI to enter normal sleep");
+  wait_for_interrupt();
+  // data preserved
+  retention_sram_check((check_config_t){.do_write = false, .is_equal = true});
+}
+
+// test these 2 cases:
+// deep sleep, no scrambling -> data preserved
+// deep sleep, with scrambling -> data preserved
+void enter_deep_sleep() {
+  // Prepare rstmgr for a reset.
+  rstmgr_testutils_pre_reset(&rstmgr);
+  // set up wakeup timer
+  aon_timer_testutils_wakeup_config(&aon_timer, 20);
+  // Deep sleep.
+  pwrmgr_testutils_enable_low_power(&pwrmgr, kDifPwrmgrWakeupRequestSourceFive,
+                                    0);
+
+  // Enter low power mode.
+  LOG_INFO("Issue WFI to enter deep sleep");
+  wait_for_interrupt();
+  CHECK(false, "Should have a reset to CPU before this line");
+}
+
+void set_up_reset_request(void) {
+  // Prepare rstmgr for a reset.
+  rstmgr_testutils_pre_reset(&rstmgr);
+  CHECK_DIF_OK(dif_pwrmgr_set_request_sources(&pwrmgr, kDifPwrmgrReqTypeReset,
+                                              kDifPwrmgrResetRequestSourceTwo,
+                                              kDifToggleEnabled));
+
+  CHECK_DIF_OK(dif_aon_timer_wakeup_stop(&aon_timer));
+
+  // Enter low power mode.
+  aon_timer_testutils_watchdog_config(&aon_timer, UINT32_MAX, 20, false);
+  LOG_INFO("wait for reset");
+  wait_for_interrupt();
+  CHECK(false, "Should have a reset to CPU and ret_sram before this line");
+}
+
 bool test_main(void) {
-  dif_rstmgr_t rstmgr;
-  dif_aon_timer_t aon_timer;
   dif_sram_ctrl_t ret_sram;
 
   // Enable global and external IRQ at Ibex.
@@ -125,77 +195,66 @@ bool test_main(void) {
   if (rstmgr_reset_info == kDifRstmgrResetInfoPor) {
     LOG_INFO("POR reset");
 
-    LOG_INFO("Scrambling...");
-    sram_ctrl_testutils_scramble(&ret_sram);
+    test_ret_sram_in_normal_sleep();
 
-    // Write data to retention SRAM and readback (to test basic functionality.)
-    retention_sram_check((check_config_t){.do_write = true, .is_equal = true});
+    enter_deep_sleep();
+  } else if (rstmgr_reset_info & kDifRstmgrResetInfoLowPowerExit) {
+    // This branch will be entered twice.
+    // In the first time, ret_non_scrambled is True. In the 2nd time, it's
+    // false.
+    LOG_INFO("wake up from deep sleep with ret_non_scrambled: %x",
+             ret_non_scrambled);
 
-    aon_timer_testutils_wakeup_config(&aon_timer, 100);
-    // Enable all the AON interrupts used in this test.
-    rv_plic_testutils_irq_range_enable(&rv_plic, kTopEarlgreyPlicTargetIbex0,
-                                       kTopEarlgreyPlicIrqIdPwrmgrAonWakeup,
-                                       kTopEarlgreyPlicIrqIdPwrmgrAonWakeup);
-    CHECK_DIF_OK(dif_pwrmgr_irq_set_enabled(&pwrmgr, 0, kDifToggleEnabled));
-
-    // Normal sleep.
-    pwrmgr_testutils_enable_low_power(
-        &pwrmgr, /*wakeups=*/kDifPwrmgrWakeupRequestSourceFive,
-        /*domain_config=*/kDifPwrmgrDomainOptionCoreClockInLowPower |
-            kDifPwrmgrDomainOptionUsbClockInActivePower |
-            kDifPwrmgrDomainOptionMainPowerInLowPower);
-    // Enter low power mode.
-    LOG_INFO("Issue WFI to enter normal sleep");
-    wait_for_interrupt();
-    LOG_INFO("Awaked");
-    // Check that the previously written retention SRAM data can still
-    // be read successfully.
+    CHECK(pwrmgr_testutils_is_wakeup_reason(&pwrmgr,
+                                            kDifPwrmgrWakeupRequestSourceFive));
+    // data preserved
     retention_sram_check((check_config_t){.do_write = false, .is_equal = true});
 
-    // Prepare rstmgr for a reset.
-    rstmgr_testutils_pre_reset(&rstmgr);
-    aon_timer_testutils_wakeup_config(&aon_timer, 20);
-    // Deep sleep.
-    pwrmgr_testutils_enable_low_power(&pwrmgr,
-                                      kDifPwrmgrWakeupRequestSourceFive, 0);
-
-    // Enter low power mode.
-    LOG_INFO("Issue WFI to enter deep sleep");
-    wait_for_interrupt();
-  } else if (rstmgr_reset_info & kDifRstmgrResetInfoLowPowerExit) {
-    if (!(rstmgr_reset_info & kDifRstmgrResetInfoWatchdog)) {
-      LOG_INFO("Wakeup reset");
-
-      CHECK(pwrmgr_testutils_is_wakeup_reason(
-          &pwrmgr, kDifPwrmgrWakeupRequestSourceFive));
-
-      LOG_INFO("Aon timer wakeup detected");
-
-      // Check that the previously written retention SRAM data can still
-      // be read successfully.
+    set_up_reset_request();
+  } else if (rstmgr_reset_info & kDifRstmgrResetInfoWatchdog) {
+    // This branch will be entered twice.
+    // In the first time, ret_non_scrambled is True. In the 2nd time, it's
+    // false.
+    LOG_INFO("watchdog reset with ret_non_scrambled: %x", ret_non_scrambled);
+    if (ret_non_scrambled) {
+      // reset due to a reset request, no scrambling -> data preserved
       retention_sram_check(
           (check_config_t){.do_write = false, .is_equal = true});
 
-      // Prepare rstmgr for a reset.
-      rstmgr_testutils_pre_reset(&rstmgr);
-      // Deep sleep.
-      pwrmgr_testutils_enable_low_power(&pwrmgr,
-                                        kDifPwrmgrWakeupRequestSourceTwo, 0);
-      // Enable watchdog timer.
-      CHECK_DIF_OK(dif_pwrmgr_set_request_sources(
-          &pwrmgr, kDifPwrmgrReqTypeReset, kDifPwrmgrResetRequestSourceTwo,
-          kDifToggleEnabled));
+      LOG_INFO("Start to test ret_sram with scramble enabled");
 
-      CHECK_DIF_OK(dif_aon_timer_wakeup_stop(&aon_timer));
+      dif_flash_ctrl_state_t flash_ctrl_state;
+      CHECK_DIF_OK(dif_flash_ctrl_init_state(
+          &flash_ctrl_state,
+          mmio_region_from_addr(TOP_EARLGREY_FLASH_CTRL_CORE_BASE_ADDR)));
+      flash_ctrl_testutils_default_region_access(&flash_ctrl_state,
+                                                 /*rd_en=*/true,
+                                                 /*prog_en=*/true,
+                                                 /*erase_en=*/true,
+                                                 /*scramble_en=*/false,
+                                                 /*ecc_en=*/false,
+                                                 /*he_en=*/false);
+      // write ret_non_scrambled to 0
+      const uint32_t new_data = 0;
+      CHECK(flash_ctrl_testutils_write(
+                &flash_ctrl_state,
+                (uint32_t)&ret_non_scrambled -
+                    TOP_EARLGREY_FLASH_CTRL_MEM_BASE_ADDR,
+                /*partition_id*/ 0, &new_data, kDifFlashCtrlPartitionTypeData,
+                /*word_count*/ 1),
+            "Flash write failed");
+      // wipe data otherwise, the data may still be read after reset, since it's
+      // written with the default key/nounce.
+      LOG_INFO("Wiping ret_sram...");
+      sram_ctrl_testutils_wipe(&ret_sram);
+      LOG_INFO("Scrambling ret_sram...");
+      sram_ctrl_testutils_scramble(&ret_sram);
 
-      // Enter low power mode.
-      LOG_INFO("Issue WFI to enter sleep");
-      aon_timer_testutils_watchdog_config(&aon_timer, UINT32_MAX, 20, false);
-      wait_for_interrupt();
+      test_ret_sram_in_normal_sleep();
+
+      enter_deep_sleep();
     } else {
-      LOG_INFO("Watchdog reset");
-      // Check that the previously written retention SRAM data cannot be read
-      // successfully.
+      // reset due to a reset request, with scrambling -> data NOT preserved
       retention_sram_check(
           (check_config_t){.do_write = false, .is_equal = false});
     }
@@ -205,8 +264,5 @@ bool test_main(void) {
 
   // Turn off the AON timer hardware completely before exiting.
   aon_timer_testutils_shutdown(&aon_timer);
-  if (kDeviceType == kDeviceSimDV) {
-    CHECK_DIF_OK(dif_rstmgr_software_device_reset(&rstmgr));
-  }
   return true;
 }
