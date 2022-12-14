cda0ec52aa0d9ae8a65d16aab83b626993664392
Marno van der Maas <mvdmaas+git@lowrisc.org>
2022-12-14 17:32:44 +0000

    [lint,prim_generic] Turn off unused Verilator lint in clock buf
    Signed-off-by: Marno van der Maas <mvdmaas+git@lowrisc.org>

--- hw/ip/prim_generic/rtl/prim_generic_clock_buf.sv
+++ hw/ip/prim_generic/rtl/prim_generic_clock_buf.sv
@@ -5,8 +5,12 @@
 `include "prim_assert.sv"
 
 module prim_generic_clock_buf #(
+  // Turning off these verilator lints because keeping these parameters makes it consistent with
+  // the IP in hw/ip/prim_xilinx/rtl/ .
+  /* verilator lint_off UNUSED */
   parameter bit NoFpgaBuf = 1'b0, // serves no function in generic
   parameter bit RegionSel = 1'b0  // serves no function in generic
+  /* verilator lint_on UNUSED */
 ) (
   input clk_i,
   output logic clk_o
