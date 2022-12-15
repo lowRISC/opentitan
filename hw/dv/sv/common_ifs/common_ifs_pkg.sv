dfe3d69633a30eb9c8bfe4c752675cb3cbad0647
Weicai Yang <weicai@google.com>
2022-12-14 16:12:08 -0800

    [dv] Resolve/clean up more TODOs
    1. make an eunum for rst_scheme as suggested in the TODO
    2. clean up the other TODOs which needs no further action
    3. fix core file dependency - common_ifs isn't used in dv_utils, but needed for dv_lib
    Signed-off-by: Weicai Yang <weicai@google.com>

---
+++ hw/dv/sv/common_ifs/common_ifs_pkg.sv
@@ -0,0 +1,15 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+package common_ifs_pkg;
+  // dep packages
+  import uvm_pkg::*;
+
+  // Enum representing reset scheme
+  typedef enum bit [1:0] {
+    RstAssertSyncDeassertSync,
+    RstAssertAsyncDeassertSync,
+    RstAssertAsyncDeassertASync
+  } rst_scheme_e;
+endpackage
