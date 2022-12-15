fae6bf055c6fd8e8f7d3fe140d3e9f111de7acb7
Jade Philipoom <jadep@google.com>
2022-12-15 15:32:51 +0100

    [silicon_creator] Consolidate OTBN driver.
    Similar to a previous refactor for cryptolib: combine the previous two
    layers of OTBN driver into one and remove the context object.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

--- sw/device/silicon_creator/lib/drivers/otbn.c
+++ sw/device/silicon_creator/lib/drivers/otbn.c
@@ -17,34 +17,16 @@
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 #include "otbn_regs.h"  // Generated.
 
-#define ASSERT_ERR_BIT_MATCH(enum_val, autogen_val) \
-  static_assert(enum_val == 1 << (autogen_val),     \
-                "OTBN register bit doesn't match autogen value.");
-
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsBadDataAddr, OTBN_ERR_BITS_BAD_DATA_ADDR_BIT);
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsBadInsnAddr, OTBN_ERR_BITS_BAD_INSN_ADDR_BIT);
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsCallStack, OTBN_ERR_BITS_CALL_STACK_BIT);
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsIllegalInsn, OTBN_ERR_BITS_ILLEGAL_INSN_BIT);
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsLoop, OTBN_ERR_BITS_LOOP_BIT);
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsImemIntgViolation,
-                     OTBN_ERR_BITS_IMEM_INTG_VIOLATION_BIT);
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsDmemIntgViolation,
-                     OTBN_ERR_BITS_DMEM_INTG_VIOLATION_BIT);
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsRegIntgViolation,
-                     OTBN_ERR_BITS_REG_INTG_VIOLATION_BIT);
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsBusIntgViolation,
-                     OTBN_ERR_BITS_BUS_INTG_VIOLATION_BIT);
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsIllegalBusAccess,
-                     OTBN_ERR_BITS_ILLEGAL_BUS_ACCESS_BIT);
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsLifecycleEscalation,
-                     OTBN_ERR_BITS_LIFECYCLE_ESCALATION_BIT);
-ASSERT_ERR_BIT_MATCH(kOtbnErrBitsFatalSoftware,
-                     OTBN_ERR_BITS_FATAL_SOFTWARE_BIT);
-
-const size_t kOtbnDMemSizeBytes = OTBN_DMEM_SIZE_BYTES;
-const size_t kOtbnIMemSizeBytes = OTBN_IMEM_SIZE_BYTES;
-
-enum { kBase = TOP_EARLGREY_OTBN_BASE_ADDR };
+enum {
+  /**
+   * Base address for OTBN.
+   */
+  kBase = TOP_EARLGREY_OTBN_BASE_ADDR,
+  /**
+   * Highest index of OTBN error bits.
+   */
+  kOtbnErrBitsLast = OTBN_ERR_BITS_FATAL_SOFTWARE_BIT,
+};
 
 /**
  * Ensures that `offset_bytes` and `len` are valid for a given `mem_size`.
@@ -100,6 +82,34 @@ static void otbn_write(uint32_t dest_addr, const uint32_t *src,
   HARDENED_CHECK_EQ(iter_cnt, num_words);
 }
 
+static rom_error_t otbn_imem_write(size_t num_words, const uint32_t *src,
+                                   otbn_addr_t dest) {
+  HARDENED_RETURN_IF_ERROR(
+      check_offset_len(dest, num_words, OTBN_IMEM_SIZE_BYTES));
+  otbn_write(kBase + OTBN_IMEM_REG_OFFSET + dest, src, num_words);
+  return kErrorOk;
+}
+
+rom_error_t otbn_dmem_write(size_t num_words, const uint32_t *src,
+                            otbn_addr_t dest) {
+  HARDENED_RETURN_IF_ERROR(
+      check_offset_len(dest, num_words, OTBN_DMEM_SIZE_BYTES));
+  otbn_write(kBase + OTBN_DMEM_REG_OFFSET + dest, src, num_words);
+  return kErrorOk;
+}
+
+rom_error_t otbn_dmem_read(size_t num_words, otbn_addr_t src, uint32_t *dest) {
+  HARDENED_RETURN_IF_ERROR(
+      check_offset_len(src, num_words, OTBN_DMEM_SIZE_BYTES));
+  size_t i = 0;
+  for (; launder32(i) < num_words; ++i) {
+    dest[i] = abs_mmio_read32(kBase + OTBN_DMEM_REG_OFFSET + src +
+                              i * sizeof(uint32_t));
+  }
+  HARDENED_CHECK_EQ(i, num_words);
+  return kErrorOk;
+}
+
 /**
  * Helper function for running an OTBN command.
  *
@@ -112,26 +122,26 @@ static void otbn_write(uint32_t dest_addr, const uint32_t *src,
 static rom_error_t otbn_cmd_run(otbn_cmd_t cmd, rom_error_t error) {
   enum {
     kIntrStateDone = (1 << OTBN_INTR_COMMON_DONE_BIT),
-    // Use a bit index that doesn't overlap with `otbn_err_bits_t`.
+    // Use a bit index that doesn't overlap with error bits.
     kResDoneBit = 31,
   };
   static_assert((UINT32_C(1) << kResDoneBit) > kOtbnErrBitsLast,
-                "kResDoneBit must not overlap with `otbn_err_bits_t`");
+                "kResDoneBit must not overlap with OTBN error bits");
 
   abs_mmio_write32(kBase + OTBN_INTR_STATE_REG_OFFSET, kIntrStateDone);
   abs_mmio_write32(kBase + OTBN_CMD_REG_OFFSET, cmd);
 
   rom_error_t res = kErrorOk ^ (UINT32_C(1) << kResDoneBit);
   uint32_t reg = 0;
-  while (launder32(reg) != kIntrStateDone) {
+  do {
     reg = abs_mmio_read32(kBase + OTBN_INTR_STATE_REG_OFFSET);
     res ^= bitfield_bit32_read(reg, OTBN_INTR_COMMON_DONE_BIT) << kResDoneBit;
-  }
+  } while (launder32(reg) != kIntrStateDone);
   HARDENED_CHECK_EQ(reg, kIntrStateDone);
   abs_mmio_write32(kBase + OTBN_INTR_STATE_REG_OFFSET, kIntrStateDone);
 
-  otbn_err_bits_t err_bits;
-  otbn_err_bits_get(&err_bits);
+  // Error bits register should be 0 (no errors).
+  uint32_t err_bits = abs_mmio_read32(kBase + OTBN_ERR_BITS_REG_OFFSET);
   res ^= err_bits;
 
   // Status should be kOtbnStatusIdle; OTBN can also issue a done interrupt
@@ -139,8 +149,10 @@ static rom_error_t otbn_cmd_run(otbn_cmd_t cmd, rom_error_t error) {
   // the status here.
   uint32_t status = abs_mmio_read32(kBase + OTBN_STATUS_REG_OFFSET);
 
-  if (launder32(res) == kErrorOk && launder32(status) == kOtbnStatusIdle) {
+  if (launder32(res) == kErrorOk && launder32(err_bits) == 0 &&
+      launder32(status) == kOtbnStatusIdle) {
     HARDENED_CHECK_EQ(res, kErrorOk);
+    HARDENED_CHECK_EQ(err_bits, 0);
     HARDENED_CHECK_EQ(abs_mmio_read32(kBase + OTBN_STATUS_REG_OFFSET),
                       kOtbnStatusIdle);
     return res;
@@ -149,12 +161,16 @@ static rom_error_t otbn_cmd_run(otbn_cmd_t cmd, rom_error_t error) {
 }
 
 rom_error_t otbn_execute(void) {
-  otbn_set_ctrl_software_errs_fatal(true);
-  return otbn_cmd_run(kOtbnCmdExecute, kErrorOtbnExecutionFailed);
-}
+  // If OTBN is busy, wait for it to be done.
+  HARDENED_RETURN_IF_ERROR(otbn_busy_wait_for_done());
 
-void otbn_err_bits_get(otbn_err_bits_t *err_bits) {
-  *err_bits = abs_mmio_read32(kBase + OTBN_ERR_BITS_REG_OFFSET);
+  // Set software errors to fatal before running the program. Note: the CTRL
+  // register has only this one setting, so we have no need to read the
+  // previous value.
+  sec_mmio_write32(kBase + OTBN_CTRL_REG_OFFSET,
+                   1 << OTBN_CTRL_SOFTWARE_ERRS_FATAL_BIT);
+
+  return otbn_cmd_run(kOtbnCmdExecute, kErrorOtbnExecutionFailed);
 }
 
 uint32_t otbn_instruction_count_get(void) {
@@ -165,58 +181,51 @@ rom_error_t otbn_imem_sec_wipe(void) {
   return otbn_cmd_run(kOtbnCmdSecWipeImem, kErrorOtbnSecWipeImemFailed);
 }
 
-rom_error_t otbn_imem_write(uint32_t offset_bytes, const uint32_t *src,
-                            size_t num_words) {
-  HARDENED_RETURN_IF_ERROR(
-      check_offset_len(offset_bytes, num_words, kOtbnIMemSizeBytes));
-  otbn_write(kBase + OTBN_IMEM_REG_OFFSET + offset_bytes, src, num_words);
-  return kErrorOk;
-}
-
 rom_error_t otbn_dmem_sec_wipe(void) {
   return otbn_cmd_run(kOtbnCmdSecWipeDmem, kErrorOtbnSecWipeDmemFailed);
 }
 
-rom_error_t otbn_dmem_write(uint32_t offset_bytes, const uint32_t *src,
-                            size_t num_words) {
+/**
+ * Checks if the OTBN application's IMEM and DMEM address parameters are valid.
+ *
+ * IMEM and DMEM ranges must not be "backwards" in memory, with the end address
+ * coming before the start address, and the IMEM range must additionally be
+ * non-empty.
+ *
+ * @param app the OTBN application to check
+ * @return OK if the addresses are valid, otherwise `kErrorOtbnInvalidArgument`.
+ */
+static rom_error_t check_app_address_ranges(const otbn_app_t app) {
+  if (app.imem_end > app.imem_start &&
+      app.dmem_data_end >= app.dmem_data_start) {
+    HARDENED_CHECK_GT(app.imem_end, app.imem_start);
+    HARDENED_CHECK_GE(app.dmem_data_end, app.dmem_data_start);
+    return kErrorOk;
+  }
+  return kErrorOtbnInvalidArgument;
+}
+
+rom_error_t otbn_load_app(const otbn_app_t app) {
+  HARDENED_RETURN_IF_ERROR(check_app_address_ranges(app));
+
+  // If OTBN is busy, wait for it to be done.
+  HARDENED_RETURN_IF_ERROR(otbn_busy_wait_for_done());
+
+  // Wipe memories.
+  HARDENED_RETURN_IF_ERROR(otbn_dmem_sec_wipe());
+  HARDENED_RETURN_IF_ERROR(otbn_imem_sec_wipe());
+
+  const size_t imem_num_words = app.imem_end - app.imem_start;
+  const size_t data_num_words = app.dmem_data_end - app.dmem_data_start;
+
+  // IMEM always starts at 0.
+  otbn_addr_t imem_start_addr = 0;
   HARDENED_RETURN_IF_ERROR(
-      check_offset_len(offset_bytes, num_words, kOtbnDMemSizeBytes));
-  otbn_write(kBase + OTBN_DMEM_REG_OFFSET + offset_bytes, src, num_words);
+      otbn_imem_write(imem_num_words, app.imem_start, imem_start_addr));
+
+  if (data_num_words > 0) {
+    HARDENED_RETURN_IF_ERROR(otbn_dmem_write(
+        data_num_words, app.dmem_data_start, app.dmem_data_start_addr));
+  }
   return kErrorOk;
-}
-
-rom_error_t otbn_dmem_read(uint32_t offset_bytes, uint32_t *dest,
-                           size_t num_words) {
-  HARDENED_RETURN_IF_ERROR(
-      check_offset_len(offset_bytes, num_words, kOtbnDMemSizeBytes));
-
-  size_t i = 0;
-  for (; launder32(i) < num_words; ++i) {
-    dest[i] = abs_mmio_read32(kBase + OTBN_DMEM_REG_OFFSET + offset_bytes +
-                              i * sizeof(uint32_t));
-  }
-  HARDENED_CHECK_EQ(i, num_words);
-
-  return kErrorOk;
-}
-
-void otbn_zero_dmem(void) {
-  size_t i = 0;
-  for (; launder32(i) < kOtbnDMemSizeBytes; i += sizeof(uint32_t)) {
-    abs_mmio_write32(kBase + OTBN_DMEM_REG_OFFSET + i, 0u);
-  }
-  HARDENED_CHECK_EQ(i, kOtbnDMemSizeBytes);
-}
-
-void otbn_set_ctrl_software_errs_fatal(bool enable) {
-  // Only one bit in the CTRL register so no need to read current value.
-  uint32_t new_ctrl;
-
-  if (enable) {
-    new_ctrl = 1;
-  } else {
-    new_ctrl = 0;
-  }
-
-  sec_mmio_write32(kBase + OTBN_CTRL_REG_OFFSET, new_ctrl);
 }
--- sw/device/silicon_creator/lib/sigverify/mod_exp_otbn.c
+++ sw/device/silicon_creator/lib/sigverify/mod_exp_otbn.c
@@ -8,7 +8,6 @@
 #include "sw/device/lib/base/memory.h"
 #include "sw/device/silicon_creator/lib/drivers/otbn.h"
 #include "sw/device/silicon_creator/lib/error.h"
-#include "sw/device/silicon_creator/lib/otbn_util.h"
 #include "sw/device/silicon_creator/lib/sigverify/rsa_key.h"
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
@@ -38,55 +37,48 @@ static const otbn_addr_t kOtbnVarRsaM0Inv =
 /**
  * Copies a 3072-bit number from the CPU memory to OTBN data memory.
  *
- * @param otbn The OTBN context object.
  * @param src Source of the data to copy.
  * @param dst Address of the destination in OTBN's data memory.
  * @return The result of the operation.
  */
-static rom_error_t write_rsa_3072_int_to_otbn(otbn_t *otbn,
-                                              const sigverify_rsa_buffer_t *src,
+static rom_error_t write_rsa_3072_int_to_otbn(const sigverify_rsa_buffer_t *src,
                                               otbn_addr_t dst) {
-  return otbn_copy_data_to_otbn(otbn, kSigVerifyRsaNumWords, src->data, dst);
+  return otbn_dmem_write(kSigVerifyRsaNumWords, src->data, dst);
 }
 
 /**
  * Copies a 3072-bit number from OTBN data memory to CPU memory.
  *
- * @param otbn The OTBN context object.
  * @param src The address in OTBN data memory to copy from.
  * @param dst The destination of the copied data in main memory (preallocated).
  * @return The result of the operation.
  */
-static rom_error_t read_rsa_3072_int_from_otbn(otbn_t *otbn,
-                                               const otbn_addr_t src,
+static rom_error_t read_rsa_3072_int_from_otbn(const otbn_addr_t src,
                                                sigverify_rsa_buffer_t *dst) {
-  return otbn_copy_data_from_otbn(otbn, kSigVerifyRsaNumWords, src, dst->data);
+  return otbn_dmem_read(kSigVerifyRsaNumWords, src, dst->data);
 }
 
 rom_error_t run_otbn_rsa_3072_modexp(
     const sigverify_rsa_key_t *public_key,
     const sigverify_rsa_buffer_t *signature,
     sigverify_rsa_buffer_t *recovered_message) {
-  otbn_t otbn;
-
-  // Initialize OTBN and load the RSA app.
-  otbn_init(&otbn);
-  HARDENED_RETURN_IF_ERROR(otbn_load_app(&otbn, kOtbnAppRsa));
+  // Load the RSA app.
+  HARDENED_RETURN_IF_ERROR(otbn_load_app(kOtbnAppRsa));
 
   // Set the modulus (n).
   HARDENED_RETURN_IF_ERROR(
-      write_rsa_3072_int_to_otbn(&otbn, &public_key->n, kOtbnVarRsaInMod));
+      write_rsa_3072_int_to_otbn(&public_key->n, kOtbnVarRsaInMod));
 
   // Set the signature.
   HARDENED_RETURN_IF_ERROR(
-      write_rsa_3072_int_to_otbn(&otbn, signature, kOtbnVarRsaInBuf));
+      write_rsa_3072_int_to_otbn(signature, kOtbnVarRsaInBuf));
 
   // Set the precomputed constant m0_inv.
-  HARDENED_RETURN_IF_ERROR(otbn_copy_data_to_otbn(
-      &otbn, kOtbnWideWordNumWords, public_key->n0_inv, kOtbnVarRsaM0Inv));
+  HARDENED_RETURN_IF_ERROR(otbn_dmem_write(
+      kOtbnWideWordNumWords, public_key->n0_inv, kOtbnVarRsaM0Inv));
 
   // Start the OTBN routine.
-  HARDENED_RETURN_IF_ERROR(otbn_execute_app(&otbn));
+  HARDENED_RETURN_IF_ERROR(otbn_execute());
 
   // Check that the instruction count falls within the expected range. If the
   // instruction count falls outside this range, it indicates that there was a
@@ -100,8 +92,7 @@ rom_error_t run_otbn_rsa_3072_modexp(
   HARDENED_CHECK_LE(count, kModExpOtbnInsnCountMax);
 
   // Read recovered message out of OTBN dmem.
-  return read_rsa_3072_int_from_otbn(&otbn, kOtbnVarRsaOutBuf,
-                                     recovered_message);
+  return read_rsa_3072_int_from_otbn(kOtbnVarRsaOutBuf, recovered_message);
 }
 
 rom_error_t sigverify_mod_exp_otbn(const sigverify_rsa_key_t *key,
