c3607d4e82cdca7ce0a79d0005228684f4fd5ecb
Jaedon Kim <jdonjdon@google.com>
2022-12-15 19:32:43 +0000

    [i2c,dv] target performance test
    Signed-off-by: Jaedon Kim <jdonjdon@google.com>

---
+++ hw/ip/i2c/dv/env/seq_lib/i2c_target_perf_vseq.sv
@@ -0,0 +1,28 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// targetmode performance test vseq
+class i2c_target_perf_vseq extends i2c_target_smoke_vseq;
+  `uvm_object_utils(i2c_target_perf_vseq)
+  `uvm_object_new
+
+  // Fast timing values programmed to registers
+  // See constraint minimum in i2c_target_smoke_vseq
+  constraint timing_val_c {
+    t_r       == 1;
+    t_f       == 1;
+    thd_sta   == 1;
+    tsu_sto   == 1;
+    tsu_dat   == 1;
+    thd_dat   == 1;
+    t_timeout == 1;
+    e_timeout == 1;
+    tsu_sta   == 1;
+
+    thigh     == 3;
+    tlow      == 8;
+    t_buf     == 1;
+  }
+
+endclass
