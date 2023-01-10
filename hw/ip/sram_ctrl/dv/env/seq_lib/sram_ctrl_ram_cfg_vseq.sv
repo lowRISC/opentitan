dd98f529729edf9a0f1ca96396b8f4981fd01ec1
Weicai Yang <weicai@google.com>
2023-01-10 14:02:03 -0800

    [sram_ctrl/dv] Add a simple connectivity test for sram_ctrl.cfg_i
    Test it's connected correctly to prim_mem_1p
    
    Signed-off-by: Weicai Yang <weicai@google.com>

---
+++ hw/ip/sram_ctrl/dv/env/seq_lib/sram_ctrl_ram_cfg_vseq.sv
@@ -0,0 +1,25 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// Randomly set dut.cfg_i and check this value is propagated to prim_generic_ram_1p.
+class sram_ctrl_ram_cfg_vseq extends sram_ctrl_base_vseq;
+
+  `uvm_object_utils(sram_ctrl_ram_cfg_vseq)
+  `uvm_object_new
+
+  virtual task body();
+    prim_ram_1p_pkg::ram_1p_cfg_t src_ram_cfg, dst_ram_cfg;
+    string src_path = "tb.dut.cfg_i";
+    string dst_path =
+        "tb.dut.u_prim_ram_1p_scr.u_prim_ram_1p_adv.u_mem.gen_generic.u_impl_generic.cfg_i";
+
+    repeat (100) begin
+      `DV_CHECK_STD_RANDOMIZE_FATAL(src_ram_cfg)
+      `DV_CHECK(uvm_hdl_deposit(src_path, src_ram_cfg))
+      #($urandom_range(1, 100) * 1ns);
+      `DV_CHECK(uvm_hdl_read(dst_path, dst_ram_cfg))
+      `DV_CHECK_CASE_EQ(src_ram_cfg, dst_ram_cfg)
+    end
+  endtask
+endclass
