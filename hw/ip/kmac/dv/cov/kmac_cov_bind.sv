9a76d798153e9dc4166a4914e1372c1fac549725
Cindy Chen <chencindy@opentitan.org>
2023-01-31 16:25:53 -0800

    [dv/kmac] Fix nightly regression error
    This PR fix issue #17094
    
    The strong liveness assertions in sha3pad module are written to ensure a kmac request/transaction
    must finish. However, in chip level testbench, it is not necessary to wait for all kmac
    transactions to finish. A recent update in VCS will flag these unfinished strong assertions as
    failures.
    So the solution here is to move these assertions to block level testbench, using an interface with
    binding method. This gives user flexibility to choose whether they want to enable these strong
    assertions.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

--- hw/ip/kmac/dv/cov/kmac_cov_bind.sv
+++ hw/ip/kmac/dv/cov/kmac_cov_bind.sv
@@ -16,9 +16,18 @@ module kmac_cov_bind;
     .mubi   (sha3_done)
   );
 
-    bind kmac cip_mubi_cov_if #(.Width(4)) kmac_sha3_absorb_mubi_cov_if (
+  bind kmac cip_mubi_cov_if #(.Width(4)) kmac_sha3_absorb_mubi_cov_if (
     .rst_ni (rst_ni),
     .mubi   (sha3_absorbed)
+  );
+
+  bind kmac sha3pad_assert_if #(.EnMasking(u_sha3.u_pad.EnMasking)) sha3pad_assert_cov_if (
+    .clk_i (clk_i),
+    .rst_ni (rst_ni),
+    .process_i (u_sha3.u_pad.process_i),
+    .keccak_complete_i (u_sha3.u_pad.keccak_complete_i),
+    .keccak_run_o (u_sha3.u_pad.keccak_run_o),
+    .lc_escalate_en_i (u_sha3.u_pad.lc_escalate_en_i)
   );
 
 endmodule
