6bfca7e1fac5a9e4829a17de2b894c494cb9c31c
Eitan Shapira <eitanshapira89@gmail.com>
2023-02-06 17:09:01 +0200

    [dv/util/sungrid] Update rand_ops & smoke partitions randomizations
    Signed-off-by: Eitan Shapira <eitanshapira89@gmail.com>

--- hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_smoke_vseq.sv
+++ hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_smoke_vseq.sv
@@ -22,6 +22,11 @@ class flash_ctrl_smoke_vseq extends flash_ctrl_rand_ops_base_vseq;
     // Don't enable any memory protection.
     cfg.seq_cfg.num_en_mp_regions = 0;
 
+    cfg.seq_cfg.set_partition_pc(.sel_data_part_pc(100),
+                                 .sel_info_part_pc(0),
+                                 .sel_info1_part_pc(0),
+                                 .sel_info2_part_pc(0));
+
     // Don't enable access to any of information partitions.
     foreach (cfg.seq_cfg.mp_info_page_en_pc[i, j]) begin
       cfg.seq_cfg.mp_info_page_en_pc[i][j] = 0;
