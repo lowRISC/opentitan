bc50ba849e059619e3e77dd2296d3de37f646f75
Hugo McNally <hugo.mcnally@gmail.com>
2023-02-12 22:29:09 +0000

    [doc] Created wavejson preprocessor
    This preprocessor surrounds wavejson code-blocks
    with script tags of type WaveDrom,
    so that the wavejson is rendered by WaveDrom.
    
    Signed-off-by: Hugo McNally <hugo.mcnally@gmail.com>

---
+++ util/mdbook/wavejson/wavejson.js
@@ -0,0 +1,4 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+window.addEventListener('load', WaveDrom.ProcessAll)
