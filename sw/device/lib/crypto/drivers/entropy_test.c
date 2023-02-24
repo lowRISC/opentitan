5cbddc36d85756fbe86f2a615717881292405c83
Miguel Osorio <miguelosorio@google.com>
2023-02-23 17:07:25 -0800

    [cryptolib] Add entropy complex init functions
    Adds logic to configure the entropy_src, csrng, edn0 and edn1 blocks in
    continuous mode with FIPS mode enabled. The entire initialization
    sequence is provided in a single API.
    
    This change does not integrate KAT tests to the complex initialization
    sequence. These will be added in a follow up commit.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- sw/device/lib/crypto/drivers/entropy_test.c
+++ sw/device/lib/crypto/drivers/entropy_test.c
@@ -1,18 +1,38 @@
 // Copyright lowRISC contributors.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
+#include "sw/device/lib/crypto/drivers/entropy.h"
+
 #include "sw/device/lib/base/memory.h"
 #include "sw/device/lib/base/status.h"
 #include "sw/device/lib/crypto/drivers/entropy_kat.h"
+#include "sw/device/lib/dif/dif_otbn.h"
 #include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
+#include "sw/device/tests/otbn_randomness_impl.h"
+
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
 OTTF_DEFINE_TEST_CONFIG();
+
+static void entropy_complex_init_test(void) {
+  CHECK_STATUS_OK(entropy_complex_init());
+
+  // The following test requests entropy from both EDN0 and EDN1.
+  dif_otbn_t otbn;
+  CHECK_DIF_OK(
+      dif_otbn_init(mmio_region_from_addr(TOP_EARLGREY_OTBN_BASE_ADDR), &otbn));
+  otbn_randomness_test_start(&otbn);
+  CHECK(otbn_randomness_test_end(&otbn, /*skip_otbn_don_check=*/false));
+}
+
 bool test_main(void) {
   status_t result = entropy_csrng_kat();
   if (!status_ok(result)) {
     LOG_ERROR("entropy_csrng_kat: %r\n", result);
     return false;
   }
+  entropy_complex_init_test();
   return true;
 }
