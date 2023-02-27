aae313a91f791688a814ab59939f855fe161f258
Dror Kabely <dror.kabely@opentitan.org>
2023-02-27 21:22:57 +0200

    [chip/dv] added OTP images for additional 5 LC states
    Signed-off-by: Dror Kabely <dror.kabely@opentitan.org>

--- hw/top_earlgrey/dv/env/chip_env_cfg.sv
+++ hw/top_earlgrey/dv/env/chip_env_cfg.sv
@@ -198,6 +198,10 @@ class chip_env_cfg #(type RAL_T = chip_ral_pkg::chip_reg_block) extends cip_base
     otp_images[OtpTypeLcStProd] = "otp_ctrl_img_prod.vmem";
     otp_images[OtpTypeLcStRma] = "otp_ctrl_img_rma.vmem";
     otp_images[OtpTypeLcStTestUnlocked0] = "otp_ctrl_img_test_unlocked0.vmem";
+    otp_images[OtpTypeLcStTestUnlocked1] = "otp_ctrl_img_test_unlocked1.vmem";
+    otp_images[OtpTypeLcStTestUnlocked2] = "otp_ctrl_img_test_unlocked2.vmem";
+    otp_images[OtpTypeLcStTestLocked0] = "otp_ctrl_img_test_locked0.vmem";
+    otp_images[OtpTypeLcStTestLocked1] = "otp_ctrl_img_test_locked1.vmem";
     otp_images[OtpTypeCustom] = "";
 
     `DV_CHECK_LE_FATAL(num_ram_main_tiles, 16)
