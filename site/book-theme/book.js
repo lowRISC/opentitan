05d92da2a55069d0bdbf0f9e4354fbc897896b5f
Harry Callahan <hcallahan@lowrisc.org>
2023-03-09 13:24:26 +0000

    Add missing copyright / attributions for mdbook-theme code
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>

--- site/book-theme/book.js
+++ site/book-theme/book.js
@@ -1,3 +1,10 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// SPDX-FileCopyrightText: 2023 'mdBook contributers' (https://github.com/rust-lang/mdBook)
+// SPDX-License-Identifier: MPL-2.0
+
 "use strict";
 
 // Fix back button cache problem
