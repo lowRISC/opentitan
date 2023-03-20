a578836c1ec27c57a2658ad8639cf95c77eb9727
Harry Callahan <hcallahan@lowrisc.org>
2023-03-20 08:38:53 +0000

    Small refactor of util/autogen_md.py, adding verbose descriptions
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>

--- util/autogen_md.py
+++ util/autogen_md.py
@@ -2,8 +2,15 @@
 # Copyright lowRISC contributors.
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
-"""Go through all markdown files and autogenerate content based on commands
-recorded in the file.
+"""Mechanism to auto-generate documentation based on inline shell commands.
+
+This is a generic, repeatable method of inserting generated/rendered content
+into a markdown file. The populated .md file can then be committed into the
+repository, so the in-repo representation matches the generated content.
+
+CI tooling can automatically re-generate the files if other changes to the
+repository would change the generated content. Linting or pre-commit checks
+could keep such-documenation always up-to-date.
 """
 
 import sys
@@ -11,25 +18,42 @@ from pathlib import Path
 import re
 import subprocess
 import argparse
-import os
-from typing import Callable
 
-START_MARKER_PATTERN = re.compile(r"\n<!--\s*BEGIN\s*AUTOGEN\s*(?P<cmd>.+)\s*-->\n")
-END_MARKER_PATTERN = re.compile(r"\n<!--\s*END\s*AUTOGEN\s*-->\n")
+import logging
+logger = logging.getLogger(__name__)
 
 
-def apply_to_all_files(path: Path, fn: Callable[[Path], None]):
-    for child in path.iterdir():
-        if child.is_file():
-            fn(child)
-        if child.is_dir():
-            apply_to_all_files(child, fn)
+# START and END marker tags use the HTML-style comments '<!-- -->'
+START_MARKER_PATTERN = re.compile(
+    r"""^<!--             \s*
+        BEGIN \s* AUTOGEN \s*
+        (?P<cmd>.+)       \s* # The command to generate content
+        -->$""",
+    flags=(re.M | re.X))
+END_MARKER_PATTERN = re.compile(
+    r"""^<!--             \s*
+        END \s* AUTOGEN   \s*
+        -->$""",
+    flags=(re.M | re.X))
 
 
 def autogen_rewrite_md(filepath: Path, dry_run: bool):
-    # only consider .md files
-    if not filepath.name.endswith(".md"):
-        return
+    '''Find all matching blocks in a file, and re-generate their contents.
+
+    START_MARKER_PATTERN and END_MARKER_PATTERN define the delimiters.
+    For example, AUTOGEN blocks are defined within a file as follows...
+    """
+    <!-- BEGIN AUTOGEN python3 util/selfdoc.py reggen -->
+                       `----------------------------`
+                                    `cmd`
+
+    ...generated_content...
+
+
+    <!-- END AUTOGEN -->
+    """
+
+    '''
     content = filepath.read_text()
     modified = False
     pos = 0
@@ -37,50 +61,67 @@ def autogen_rewrite_md(filepath: Path, dry_run: bool):
         # search next start marker
         match_start = START_MARKER_PATTERN.search(content, pos)
         if match_start is None:
-            # no more replacements to do
-            break
+            break  # no more replacements to do
+        match_start_linum = 1 + content[0:match_start.start(0)].count("\n")
         cmd = match_start.group('cmd').strip()
+
         # search end marker after the start marker
         match_end = END_MARKER_PATTERN.search(content, pos)
         if match_end is None:
-            sys.exit(f"Error in {filepath}: start marker with command '{cmd}' without end marker")
+            sys.exit(f"Error in {filepath}: start marker on "
+                     f"L{match_start_linum} with command '{cmd}' is missing a"
+                     "matching end marker!")
 
-        print(f"In {filepath}: running '{cmd}'")
+        logger.info(f" {filepath}:L{match_start_linum} : Running cmd '{cmd}'")
         if dry_run:
             # don't run
             pos = match_end.end(0)
             continue
+
+        # Run the found-command in a subshell
         res = subprocess.run(cmd, shell=True, capture_output=True)
         if res.stderr:
-            print(
-                f"The command '{cmd}' output "
-                f"the following error messages:\n{res.stderr.decode()}"
-            )
+            logger.info(f"At L{match_start_linum} the command '{cmd}' output"
+                        " the following error messages:\n"
+                        f" {res.stderr.decode()}")
         if res.returncode != 0:
-            sys.exit(f"The command '{cmd}' had a non-zero return code.")
+            sys.exit(f"At L{match_start_linum} the command '{cmd}' had a"
+                     " non-zero return code.")
 
         new_content = res.stdout.decode()
         # replace content
-        content = content[0:match_start.end(0)] + new_content + content[match_end.start(0):]
+        content = \
+            content[0:match_start.end(0)] + \
+            '\n' + new_content + '\n' + \
+            content[match_end.start(0):]
         modified = True
         # resume after end line, adjust for new content size
-        pos = match_end.end(0) + len(new_content) - (match_end.start(0) - match_start.end(0))
+        pos = \
+            match_end.end(0) + \
+            len(new_content) - \
+            (match_end.start(0) - match_start.end(0))
+
     # write back
     if modified:
         filepath.write_text(content)
 
 
-def main(args: argparse.Namespace):
-    this_file_path = Path(__file__)
-    repo_root = this_file_path.parent.parent.resolve()
-    os.chdir(repo_root)
-    apply_to_all_files(repo_root, lambda x: autogen_rewrite_md(x, args.dry_run))
+def main():
+    '''Run AUTOGEN for all markdown files in the OpenTitan project.'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dry-run",
+                        help="Print commands but do not execute",
+                        action="store_true")
+    args = parser.parse_args()
+
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)  # root -> most-permissive
+    logger.setLevel(logging.INFO)
+
+    repo_root = Path(__file__).parents[1].resolve()
+    for f in repo_root.rglob('**/*.md'):  # Recursively find all .md files
+        autogen_rewrite_md(f, args.dry_run)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--dry-run",
-                        help = "print commands to execute but do not execute them",
-                        action="store_true")
-    args = parser.parse_args()
-    main(args)
+    main()
