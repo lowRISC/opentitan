79cde63c3f3ec3069920cadb745e9e9d3e3d0546
Douglas Reis <doreis@lowrisc.org>
2023-03-20 13:32:18 +0000

    [aes] Remove unused testutils_entropy_src header
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/device/tests/aes_entropy_test.c
+++ sw/device/tests/aes_entropy_test.c
@@ -8,7 +8,6 @@
 #include "sw/device/lib/dif/dif_aes.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/aes_testutils.h"
-#include "sw/device/lib/testing/entropy_testutils.h"
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 
