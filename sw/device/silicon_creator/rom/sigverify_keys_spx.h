d67d24a55590a2e94b39fceccf59e326595b128a
Alphan Ulusoy <alphan@opentitan.org>
2023-03-24 05:41:26 -0400

    [sw/silicon_creator] Add sigverify_keys_spx.h
    Signed-off-by: Alphan Ulusoy <alphan@opentitan.org>

---
+++ sw/device/silicon_creator/rom/sigverify_keys_spx.h
@@ -0,0 +1,70 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_SIGVERIFY_KEYS_SPX_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_SIGVERIFY_KEYS_SPX_H_
+
+#include <stdint.h>
+
+#include "sw/device/silicon_creator/lib/drivers/lifecycle.h"
+#include "sw/device/silicon_creator/lib/error.h"
+#include "sw/device/silicon_creator/lib/sigverify/spx_key.h"
+#include "sw/device/silicon_creator/rom/sigverify_keys.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+/**
+ * An SPX public key stored in ROM.
+ */
+typedef struct sigverify_rom_spx_key {
+  /**
+   * An SPX public key.
+   */
+  sigverify_spx_key_t key;
+  /**
+   * Type of the key.
+   */
+  sigverify_key_type_t key_type;
+} sigverify_rom_spx_key_t;
+
+/**
+ * Number of SPX public keys.
+ */
+extern const size_t kSigverifySpxKeysCnt;
+
+/**
+ * Step size to use when checking SPX public keys.
+ *
+ * This must be coprime with and less than `kSigverifyNumSpxKeys`.
+ * Note: Step size is not applicable when `kSigverifyNumSpxKeys` is 1.
+ */
+extern const size_t kSigverifySpxKeysStep;
+
+/**
+ * Public keys for signature verification.
+ */
+extern const sigverify_rom_spx_key_t kSigverifySpxKeys[];
+
+/**
+ * Returns the key with the given ID.
+ *
+ * This function returns the key only if it can be used in the given life cycle
+ * state and is valid in OTP. OTP check is performed only if the device is in a
+ * non-test operational state (PROD, PROD_END, DEV, RMA).
+ *
+ * @param key_id A key ID.
+ * @param lc_state Life cycle state of the device.
+ * @param key Key with the given ID, valid only if it exists.
+ * @return Result of the operation.
+ */
+rom_error_t sigverify_spx_key_get(uint32_t key_id, lifecycle_state_t lc_state,
+                                  const sigverify_spx_key_t **key);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_SIGVERIFY_KEYS_SPX_H_
