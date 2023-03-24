c204dc9af9a520edb517f4294a3b0f0623ad3006
Alphan Ulusoy <alphan@opentitan.org>
2023-03-24 05:50:45 -0400

    [sw/silicon_creator] Add fake SPX keys
    Signed-off-by: Alphan Ulusoy <alphan@opentitan.org>

---
+++ sw/device/silicon_creator/rom/keys/fake/spx/test_key_0_spx.h
@@ -0,0 +1,21 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_KEYS_FAKE_SPX_TEST_KEY_0_SPX_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_KEYS_FAKE_SPX_TEST_KEY_0_SPX_H_
+
+#define TEST_KEY_0_SPX \
+  {                    \
+    .data = {          \
+      0x8adc0de0,      \
+      0x00000000,      \
+      0x00000000,      \
+      0x00000000,      \
+      0x00000000,      \
+      0x00000000,      \
+      0x00000000,      \
+      0x00000000,      \
+    }                  \
+  }
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_KEYS_FAKE_SPX_TEST_KEY_0_SPX_H_
