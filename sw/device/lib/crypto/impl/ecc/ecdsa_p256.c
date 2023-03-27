9493212f566713bf9ce2330973ecfdbb751c8334
Jade Philipoom <jadep@google.com>
2023-03-27 18:45:57 +0200

    [crypto] Rename lib/crypto/impl/ecdsa_p256 to lib/crypto/impl/ecdsa.
    This is a minor fixup that will ultimately result in slightly less
    clutter in the impl/ directory, allowing ecdsa/ to contain all ECDSA
    implementations for different curves rather than separating different
    curves into different directories.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

--- sw/device/lib/crypto/impl/ecc/ecdsa_p256.c
+++ sw/device/lib/crypto/impl/ecc/ecdsa_p256.c
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "sw/device/lib/crypto/impl/ecdsa_p256/ecdsa_p256.h"
+#include "sw/device/lib/crypto/impl/ecc/ecdsa_p256.h"
 
 #include "sw/device/lib/base/hardened.h"
 #include "sw/device/lib/crypto/drivers/otbn.h"
