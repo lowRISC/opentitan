c1125a0ea3dcd43f2ffe976e64caa9682c48f9f1
Jade Philipoom <jadep@google.com>
2023-03-27 20:16:46 +0200

    [crypto] Add OTBN entrypoint for ECDH with P-256.
    Add a binary file that can be called to perform ECDH with the P-256
    curve.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

---
+++ sw/otbn/crypto/p256_ecdh.s
@@ -0,0 +1,276 @@
+/* Copyright lowRISC contributors. */
+/* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+/**
+ * Elliptic-curve Diffie-Hellman (ECDH) on curve P-256.
+ *
+ * This binary has the following modes of operation:
+ * 1. MODE_KEYGEN_RANDOM: generate a random keypair
+ * 2. MODE_SHARED_KEYGEN: compute shared key
+ * 3. MODE_KEYGEN_FROM_SEED: generate keypair from a sideloaded seed
+ * 4. MODE_SHARED_KEYGEN_FROM_SEED: compute shared key using sideloaded seed
+ */
+
+/**
+ * Mode magic values generated with
+ * $ ./util/design/sparse-fsm-encode.py -d 6 -m 4 -n 11 \
+ *    --avoid-zero -s 3660400884
+ *
+ * Call the same utility with the same arguments and a higher -m to generate
+ * additional value(s) without changing the others or sacrificing mutual HD.
+ *
+ * TODO(#17727): in some places the OTBN assembler support for .equ directives
+ * is lacking, so they cannot be used in bignum instructions or pseudo-ops such
+ * as `li`. If support is added, we could use 32-bit values here instead of
+ * 11-bit.
+ */
+.equ MODE_KEYPAIR_RANDOM, 0x3f1
+.equ MODE_SHARED_KEY, 0x5ec
+.equ MODE_KEYPAIR_FROM_SEED, 0x29f
+.equ MODE_SHARED_KEY_FROM_SEED, 0x74b
+
+.section .text.start
+start:
+  /* Init all-zero register. */
+  bn.xor  w31, w31, w31
+
+  /* Read the mode and tail-call the requested operation. */
+  la      x2, mode
+  lw      x2, 0(x2)
+
+  addi    x3, x0, MODE_KEYPAIR_RANDOM
+  beq     x2, x3, keypair_random
+
+  addi    x3, x0, MODE_SHARED_KEY
+  beq     x2, x3, shared_key
+
+  addi    x3, x0, MODE_KEYPAIR_FROM_SEED
+  beq     x2, x3, keypair_from_seed
+
+  addi    x3, x0, MODE_SHARED_KEY_FROM_SEED
+  beq     x2, x3, shared_key_from_seed
+
+  /* Unsupported mode; fail. */
+  unimp
+  unimp
+  unimp
+
+/**
+ * Generate a fresh random keypair.
+ *
+ * Returns secret key d in 320b shares d0, d1.
+ *
+ * Returns public key Q = d*G in affine coordinates (x, y).
+ *
+ * This routine runs in constant time (except potentially waiting for entropy
+ * from RND).
+ *
+ * @param[in]       w31: all-zero
+ * @param[out] dmem[d0]: First share of secret key.
+ * @param[out] dmem[d1]: Second share of secret key.
+ * @param[out]  dmem[x]: Public key x-coordinate.
+ * @param[out]  dmem[y]: Public key y-coordinate.
+ *
+ * clobbered registers: x2, x3, x16, x17, x21, x22, w0 to w26
+ * clobbered flag groups: FG0
+ */
+keypair_random:
+  /* Generate secret key d in shares.
+       dmem[d0] <= d0
+       dmem[d1] <= d1 */
+  jal   x1, p256_generate_random_key
+
+  /* Generate public key d*G.
+       dmem[x] <= (d*G).x
+       dmem[y] <= (d*G).y */
+  jal      x1, p256_base_mult
+
+  ecall
+
+/**
+ * Generate a shared key from a secret and public key.
+ *
+ * Returns the shared key, which is the affine x-coordinate of (d*Q). The
+ * shared key is expressed in boolean shares x0, x1 such that the key is (x0 ^
+ * x1).
+ *
+ * This routine runs in constant time.
+ *
+ * @param[in]       w31: all-zero
+ * @param[in]  dmem[k0]: First share of secret key.
+ * @param[in]  dmem[k1]: Second share of secret key.
+ * @param[in]   dmem[x]: Public key (Q) x-coordinate.
+ * @param[in]   dmem[y]: Public key (Q) y-coordinate.
+ * @param[out]  dmem[x]: x0, first share of shared key.
+ * @param[out]  dmem[y]: x1, second share of shared key.
+ */
+shared_key:
+  /* Generate shared key d*Q.
+       dmem[x] <= (d*Q).x
+       dmem[y] <= (d*Q).y */
+  jal      x1, p256_scalar_mult
+
+  /* TODO: `p256_scalar_mult` and the code below briefly handle the shared key
+     in unmasked form. The best way to fixing this is likely:
+       - modify scalar_mult_int to return projective coordinates
+       - get additive arithmetic mask for x before converting it to affine
+       - multiply both shares by Z^-1 to convert to affine form
+       - run a safe arithmetic-to-boolean conversion algorithm
+ */
+
+  /* Fetch a fresh random number for blinding.
+       w2 <= URND() */
+  bn.wsrr   w2, 0x2 /* URND */
+
+  /* Store the random number as the second share.
+       dmem[y] <= w2 */
+  li        x2, 2
+  la        x4, y
+  bn.sid    x2, 0(x4)
+
+  /* Blind the x-coordinate.
+       dmem[x] <= dmem[x] ^ w2 */
+  li        x3, 3
+  la        x4, x
+  bn.lid    x3, 0(x4)
+  bn.xor    w3, w3, w2
+  bn.sid    x3, 0(x4)
+
+  ecall
+
+/**
+ * Generate a keypair from a keymgr-derived seed.
+ *
+ * Returns secret key d in 320b shares d0, d1.
+ *
+ * Returns public key Q = d*G in affine coordinates (x, y).
+ *
+ * This routine runs in constant time.
+ *
+ * @param[in]       w31: all-zero
+ * @param[out] dmem[d0]: First share of secret key.
+ * @param[out] dmem[d1]: Second share of secret key.
+ * @param[out]  dmem[x]: Public key x-coordinate.
+ * @param[out]  dmem[y]: Public key y-coordinate.
+ */
+keypair_from_seed:
+  /* Generate secret key d in shares.
+       dmem[d0] <= d0
+       dmem[d1] <= d1 */
+  jal   x1, secret_key_from_seed
+
+  /* Generate public key d*G.
+       dmem[x] <= (d*G).x
+       dmem[y] <= (d*G).y */
+  jal      x1, p256_base_mult
+
+  ecall
+
+/**
+ * Generate a shared key from a keymgr-derived seed.
+ *
+ * Returns the shared key, which is the affine x-coordinate of (d*Q). The
+ * shared key is expressed in boolean shares x0, x1 such that the key is (x0 ^
+ * x1).
+ *
+ * This routine runs in constant time.
+ *
+ * @param[in]       w31: all-zero
+ * @param[in]   dmem[x]: Public key (Q) x-coordinate.
+ * @param[in]   dmem[y]: Public key (Q) y-coordinate.
+ * @param[out]  dmem[x]: x0, first share of shared key.
+ * @param[out]  dmem[y]: x1, second share of shared key.
+ */
+shared_key_from_seed:
+  /* Generate secret key d in shares.
+       dmem[d0] <= d0
+       dmem[d1] <= d1 */
+  jal   x1, secret_key_from_seed
+
+  /* Tail-call shared-key generation. */
+  jal      x0, shared_key
+
+/**
+ * Generate a secret key from a keymgr-derived seed.
+ *
+ * Returns secret key d in 320b shares d0, d1 such that:
+ *   (d0 + d1) mod n = (seed0 ^ seed1) mod n
+ * ...where seed0 and seed1 are the 320-bit keymgr-provided seed values stored
+ * in KEY_S0_{L,H} and KEY_S1_{L,H} WSRs. Note that the keymgr actually
+ * provides 384 bits, but these higher bits are ignored.
+ *
+ * This routine runs in constant time.
+ *
+ * @param[in]       w31: all-zero
+ * @param[out] dmem[d0]: First share of secret key.
+ * @param[out] dmem[d0]: Second share of secret key.
+ *
+ * clobbered registers: x2, x3, w1 to w4, w20 to w29
+ * clobbered flag groups: FG0
+ */
+secret_key_from_seed:
+  /* Load keymgr seeds from WSRs.
+       w20,w21 <= seed0
+       w22,w23 <= seed1 */
+  bn.wsrr  w20, 0x4 /* KEY_S0_L */
+  bn.wsrr  w21, 0x5 /* KEY_S0_H */
+  bn.wsrr  w22, 0x6 /* KEY_S1_L */
+  bn.wsrr  w23, 0x7 /* KEY_S1_H */
+
+  /* Generate secret key shares.
+       w20, w21 <= d0
+       w22, w23 <= d1 */
+  jal      x1, p256_key_from_seed
+
+  /* Store secret key shares.
+       dmem[d0] <= d0
+       dmem[d1] <= d1 */
+  li       x2, 20
+  la       x3, d0
+  bn.sid   x2++, 0(x3)
+  bn.sid   x2++, 32(x3)
+  la       x3, d0
+  bn.sid   x2++, 0(x3)
+  bn.sid   x2, 32(x3)
+
+  ret
+
+.bss
+
+/* Operational mode. */
+.globl mode
+.balign 4
+mode:
+  .zero 4
+
+/* Public key (Q) x-coordinate. */
+.globl x
+.balign 32
+x:
+  .zero 32
+
+/* Public key y-coordinate. */
+.globl y
+.balign 32
+y:
+  .zero 32
+
+/* Secret key (d) in two shares: d = (d0 + d1) mod n.
+
+   Note: This is also labeled k0, k1 because the `p256_scalar_mult` algorithm
+   is also used for ECDSA signing and reads from those labels; in the case of
+   ECDH, the scalar in `p256_scalar_mult` is always the private key (d). */
+.globl d0
+.globl k0
+.balign 32
+d0:
+k0:
+  .zero 64
+
+.globl d1
+.globl k1
+.balign 32
+d1:
+k1:
+  .zero 64
