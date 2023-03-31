0635986e1a6d851748ae74b087c818fe4398e44e
Cindy Chen <chencindy@opentitan.org>
2023-03-31 13:59:01 -0700

    [dv/otp] Fix two OTP dv TODOs
    1). Icebox an otp todo and documented in issue #17770
    2). Implement the testbench to randomly drive OTP lc_dft_en signal in
      test_access vseq.
    
    Signed-off-by: Cindy Chen <chencindy@opentitan.org>

--- hw/ip/otp_ctrl/dv/env/seq_lib/otp_ctrl_test_access_vseq.sv
+++ hw/ip/otp_ctrl/dv/env/seq_lib/otp_ctrl_test_access_vseq.sv
@@ -2,8 +2,6 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// TODO: add support to randomly turn on and off lc_dft_en to fully verify the `prim_lc_gate`
-// module.
 class otp_ctrl_test_access_vseq extends otp_ctrl_dai_lock_vseq;
   `uvm_object_utils(otp_ctrl_test_access_vseq)
 
@@ -20,4 +18,15 @@ class otp_ctrl_test_access_vseq extends otp_ctrl_dai_lock_vseq;
     cfg.clk_rst_vif.wait_clks(3);
   endtask
 
+  virtual task rand_drive_dft_en();
+    super.rand_drive_dft_en();
+    // 25% chance drive lc_dft_en to a random value.
+    if ($urandom_range(0, 3) == 3) begin
+      cfg.otp_ctrl_vif.drive_lc_dft_en(get_rand_lc_tx_val(
+                                       .t_weight(1), .f_weight(1), .other_weight(1)));
+      // Once turn on lc_dft_en regiser, will need some time to update the state register
+      // two clock cycles for lc_async mode, one clock cycle for driving dft_en.
+      cfg.clk_rst_vif.wait_clks(3);
+    end
+  endtask
 endclass
