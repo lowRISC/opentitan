4c1f41d85921f46ae667414eed13ee3bab2ce027
Douglas Reis <doreis@lowrisc.org>
2023-04-03 16:43:54 +0100

    [sram_ctrl, testutils] Refactor the scramble function to return status_t
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/device/lib/testing/sram_ctrl_testutils.h
+++ sw/device/lib/testing/sram_ctrl_testutils.h
@@ -8,6 +8,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "sw/device/lib/base/status.h"
 #include "sw/device/lib/dif/dif_sram_ctrl.h"
 
 /**
@@ -31,8 +32,10 @@ void sram_ctrl_testutils_write(uintptr_t address,
  * has not finished in approximately 850 cycles, a timeout assertion occurs.
  * The SRAM documentation stated that the scrambling operation takes around
  * 800 cycles, so another 50 are added just to be on a safe side.
+ *
+ * @return The result of the operation.
  */
-void sram_ctrl_testutils_scramble(const dif_sram_ctrl_t *sram_ctrl);
+status_t sram_ctrl_testutils_scramble(const dif_sram_ctrl_t *sram_ctrl);
 
 /**
  * Triggers the SRAM wipe operation and waits for it to finish.
