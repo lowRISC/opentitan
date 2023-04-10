9a3341edd3e4b43eafc4901ec73a14f1d8a129bd
Miguel Osorio <miguelosorio@google.com>
2023-04-10 09:36:51 -0700

    [manuf] Individualize - HW_CFG
    Adds support to write the HW_CFG partition.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

---
+++ sw/device/silicon_creator/manuf/lib/individualize.c
@@ -0,0 +1,226 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+#include "sw/device/silicon_creator/manuf/lib/individualize.h"
+
+#include "sw/device/lib/base/bitfield.h"
+#include "sw/device/lib/base/multibits.h"
+#include "sw/device/lib/base/status.h"
+#include "sw/device/lib/crypto/drivers/entropy.h"
+#include "sw/device/lib/dif/dif_flash_ctrl.h"
+#include "sw/device/lib/dif/dif_lc_ctrl.h"
+#include "sw/device/lib/dif/dif_otp_ctrl.h"
+#include "sw/device/lib/testing/flash_ctrl_testutils.h"
+#include "sw/device/lib/testing/lc_ctrl_testutils.h"
+#include "sw/device/lib/testing/otp_ctrl_testutils.h"
+
+#include "otp_ctrl_regs.h"
+
+enum {
+  kHwCfgDeviceIdOffset =
+      OTP_CTRL_PARAM_DEVICE_ID_OFFSET - OTP_CTRL_PARAM_HW_CFG_OFFSET,
+  kHwCfgDeviceIdWordCount = OTP_CTRL_PARAM_DEVICE_ID_SIZE / sizeof(uint32_t),
+
+  kHwCfgManufStateOffset =
+      OTP_CTRL_PARAM_MANUF_STATE_OFFSET - OTP_CTRL_PARAM_HW_CFG_OFFSET,
+  kHwCfgManufStateWordCount =
+      OTP_CTRL_PARAM_MANUF_STATE_SIZE / sizeof(uint32_t),
+
+  /**
+   * Offset to the EN_SRAM_IFETCH OTP bits relative to the start of the HW_CFG
+   * partition.
+   */
+  kHwCfgEnSramIfetchOffset =
+      OTP_CTRL_PARAM_EN_SRAM_IFETCH_OFFSET - OTP_CTRL_PARAM_HW_CFG_OFFSET,
+
+  /**
+   * DeviceID info flash location. This assumes the device ID is written to
+   * flash during CP testing. This test case is provided as a reference
+   * implementation. Manufactures may modify the way DeviceId generated and
+   * written into OTP.
+   */
+  kFlashInfoDeviceIdPartitionId = 0,
+  kFlashInfoDeviceIdBankId = 0,
+  kFlashInfoDeviceIdPageId = 0,
+  kFlashInfoDeviceIdByteAddress = 0,
+  kFlashInfoDeviceIdWordCount = kHwCfgDeviceIdWordCount,
+
+  /**
+   * ManufState info flash location. This assumes the manuf state is written to
+   * flash before invoking this test library.
+   */
+  kFlashInfoManufStatePartitionId = 0,
+  kFlashInfoManufStateBankId = 0,
+  kFlashInfoManufStatePageId = 0,
+  kFlashInfoManufStateByteAddress = kHwCfgDeviceIdWordCount * sizeof(uint32_t),
+  kFlashInfoManufStateWordCount = kHwCfgManufStateWordCount,
+};
+
+typedef struct hw_cfg_settings {
+  /**
+   * Enable / disable execute from SRAM CSR switch.
+   */
+  multi_bit_bool_t en_sram_ifetch;
+  /**
+   * This input efuse is used to enable access to the NIST internal state per
+   * instance.
+   */
+  multi_bit_bool_t en_csrng_sw_app_read;
+  /**
+   * This input efuse is used to enable access to the ENTROPY_DATA register
+   * directly.
+   */
+  multi_bit_bool_t en_entropy_src_fw_read;
+  /**
+   * This input efuse is used to enable access to the firmware override FIFO and
+   * other related functions.
+   */
+  multi_bit_bool_t en_entropy_src_fw_over;
+} hw_cfg_settings_t;
+
+// Changing any of the following values may result in unexpected device
+// behavior.
+//
+// - en_sram_ifetch: required to enable SRAM code execution during
+//   manufacturing.
+// - en_csrng_sw_app_read: required to be able to extract output from CSRNG.
+// - en_entropy_src_fw_read and en_entropy_src_fw_over: Required to implement
+//   entropy_src conditioner KAT.
+const hw_cfg_settings_t kHwCfgSettings = {
+    .en_sram_ifetch = kMultiBitBool8True,
+    .en_csrng_sw_app_read = kMultiBitBool8True,
+    .en_entropy_src_fw_read = kMultiBitBool8True,
+    .en_entropy_src_fw_over = kMultiBitBool8True,
+};
+
+/**
+ * Configures digital logic settings in the HW_CFG partition.
+ *
+ * @param otp OTP controller instance.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+static status_t hw_cfg_enable_knobs_set(const dif_otp_ctrl_t *otp) {
+#define HW_CFG_EN_OFFSET(m, i) ((bitfield_field32_t){.mask = m, .index = i})
+  static const bitfield_field32_t kSramFetch = HW_CFG_EN_OFFSET(0xff, 0);
+  static const bitfield_field32_t kCsrngAppRead = HW_CFG_EN_OFFSET(0xff, 1);
+  static const bitfield_field32_t kEntropySrcFwRd = HW_CFG_EN_OFFSET(0xff, 2);
+  static const bitfield_field32_t kEntropySrcFwOvr = HW_CFG_EN_OFFSET(0xff, 3);
+#undef HW_CFG_EN_OFFSET
+
+  uint32_t val =
+      bitfield_field32_write(0, kSramFetch, kHwCfgSettings.en_sram_ifetch);
+  val = bitfield_field32_write(val, kCsrngAppRead,
+                               kHwCfgSettings.en_csrng_sw_app_read);
+  val = bitfield_field32_write(val, kEntropySrcFwRd,
+                               kHwCfgSettings.en_entropy_src_fw_read);
+  val = bitfield_field32_write(val, kEntropySrcFwOvr,
+                               kHwCfgSettings.en_entropy_src_fw_over);
+
+  TRY(otp_ctrl_testutils_dai_write32(otp, kDifOtpCtrlPartitionHwCfg,
+                                     kHwCfgEnSramIfetchOffset, &val,
+                                     /*len=*/1));
+  return OK_STATUS();
+}
+
+/**
+ * Reads info flash page.
+ *
+ * Note: This function assumes that the data stored in info flash is unscrambled
+ * and ECC enabled.
+ *
+ * @param flash_state Flash controller instance.
+ * @param relative_address Byte address relative to the start of the page.
+ * @param partition_id Info flash partition Id.
+ * @param page_id Page Id.
+ * @param[out] data_out Output buffer.
+ * @param word_count Number of words to read from flash and write to `data_out`.
+ * @return The result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+static status_t flash_info_read(dif_flash_ctrl_state_t *flash_state,
+                                uint32_t relative_address,
+                                uint32_t partition_id, uint32_t page_id,
+                                uint32_t *data_out, uint32_t word_count) {
+  uint32_t byte_address = 0;
+
+  // Enable read access and calculate the `byte_address` with respect to the
+  // flash bank.
+  TRY(flash_ctrl_testutils_info_region_setup_properties(
+      flash_state, page_id, /*bank=*/0, partition_id,
+      (dif_flash_ctrl_region_properties_t){
+          .ecc_en = kMultiBitBool4True,
+          .high_endurance_en = kMultiBitBool4False,
+          .erase_en = kMultiBitBool4False,
+          .prog_en = kMultiBitBool4False,
+          .rd_en = kMultiBitBool4True,
+          .scramble_en = kMultiBitBool4False},
+      &byte_address));
+
+  TRY(flash_ctrl_testutils_read(flash_state, byte_address, partition_id,
+                                data_out, kDifFlashCtrlPartitionTypeInfo,
+                                word_count,
+                                /*delay=*/0));
+
+  // Disable all access after done.
+  TRY(flash_ctrl_testutils_info_region_setup_properties(
+      flash_state, page_id, /*bank=*/0, partition_id,
+      (dif_flash_ctrl_region_properties_t){
+          .ecc_en = kMultiBitBool4True,
+          .high_endurance_en = kMultiBitBool4False,
+          .erase_en = kMultiBitBool4False,
+          .prog_en = kMultiBitBool4False,
+          .rd_en = kMultiBitBool4False,
+          .scramble_en = kMultiBitBool4False},
+      &byte_address));
+
+  return OK_STATUS();
+}
+
+status_t individualize_dev_hw_cfg_start(dif_flash_ctrl_state_t *flash_state,
+                                        const dif_lc_ctrl_t *lc_ctrl,
+                                        const dif_otp_ctrl_t *otp) {
+  TRY(lc_ctrl_testutils_operational_state_check(lc_ctrl));
+
+  bool is_locked;
+  TRY(dif_otp_ctrl_is_digest_computed(otp, kDifOtpCtrlPartitionHwCfg,
+                                      &is_locked));
+  if (is_locked) {
+    return OK_STATUS();
+  }
+
+  // Configure byte-sized hardware enable knobs.
+  TRY(hw_cfg_enable_knobs_set(otp));
+
+  // Configure DeviceID
+  uint32_t device_id[kFlashInfoDeviceIdWordCount];
+  TRY(flash_info_read(flash_state, kFlashInfoDeviceIdByteAddress,
+                      kFlashInfoDeviceIdPartitionId, kFlashInfoDeviceIdPageId,
+                      device_id, kFlashInfoDeviceIdWordCount));
+  TRY(otp_ctrl_testutils_dai_write32(otp, kDifOtpCtrlPartitionHwCfg,
+                                     kHwCfgDeviceIdOffset, device_id,
+                                     kHwCfgDeviceIdWordCount));
+
+  // Configure ManufState
+  uint32_t manuf_state[kFlashInfoManufStateWordCount];
+  TRY(flash_info_read(flash_state, kFlashInfoManufStateByteAddress,
+                      kFlashInfoManufStatePartitionId,
+                      kFlashInfoManufStatePageId, manuf_state,
+                      kFlashInfoManufStateWordCount));
+  TRY(otp_ctrl_testutils_dai_write32(otp, kDifOtpCtrlPartitionHwCfg,
+                                     kHwCfgManufStateOffset, manuf_state,
+                                     kHwCfgManufStateWordCount));
+
+  TRY(otp_ctrl_testutils_lock_partition(otp, kDifOtpCtrlPartitionHwCfg,
+                                        /*digest=*/0));
+  return OK_STATUS();
+}
+
+status_t individualize_dev_hw_cfg_end(const dif_otp_ctrl_t *otp) {
+  // TODO: Add DeviceId by comparing OTP flash value against the value reported
+  // by lc_ctrl. Consider erasing the data from the flash info pages.
+  bool is_locked;
+  TRY(dif_otp_ctrl_is_digest_computed(otp, kDifOtpCtrlPartitionHwCfg,
+                                      &is_locked));
+  return is_locked ? OK_STATUS() : INTERNAL();
+}
---
+++ sw/device/silicon_creator/manuf/lib/individualize_functest.c
@@ -0,0 +1,67 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/base/status.h"
+#include "sw/device/lib/dif/dif_flash_ctrl.h"
+#include "sw/device/lib/dif/dif_lc_ctrl.h"
+#include "sw/device/lib/dif/dif_otp_ctrl.h"
+#include "sw/device/lib/dif/dif_rstmgr.h"
+#include "sw/device/lib/testing/rstmgr_testutils.h"
+#include "sw/device/lib/testing/test_framework/check.h"
+#include "sw/device/lib/testing/test_framework/ottf_main.h"
+#include "sw/device/silicon_creator/manuf/lib/individualize.h"
+
+#include "flash_ctrl_regs.h"  // Generated
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+#include "lc_ctrl_regs.h"   // Generated
+#include "otp_ctrl_regs.h"  // Generated
+
+OTTF_DEFINE_TEST_CONFIG();
+
+/**
+ * DIF Handles.
+ *
+ * Keep this list sorted in alphabetical order.
+ */
+static dif_flash_ctrl_state_t flash_state;
+static dif_lc_ctrl_t lc_ctrl;
+static dif_otp_ctrl_t otp_ctrl;
+static dif_rstmgr_t rstmgr;
+
+/**
+ * Initializes all DIF handles used in this module.
+ */
+static status_t peripheral_handles_init(void) {
+  TRY(dif_flash_ctrl_init_state(
+      &flash_state,
+      mmio_region_from_addr(TOP_EARLGREY_FLASH_CTRL_CORE_BASE_ADDR)));
+  TRY(dif_lc_ctrl_init(mmio_region_from_addr(TOP_EARLGREY_LC_CTRL_BASE_ADDR),
+                       &lc_ctrl));
+  TRY(dif_otp_ctrl_init(
+      mmio_region_from_addr(TOP_EARLGREY_OTP_CTRL_CORE_BASE_ADDR), &otp_ctrl));
+  TRY(dif_rstmgr_init(mmio_region_from_addr(TOP_EARLGREY_RSTMGR_AON_BASE_ADDR),
+                      &rstmgr));
+  return OK_STATUS();
+}
+
+bool test_main(void) {
+  CHECK_STATUS_OK(peripheral_handles_init());
+
+  dif_rstmgr_reset_info_bitfield_t info = rstmgr_testutils_reason_get();
+  if (info & kDifRstmgrResetInfoPor) {
+    CHECK_STATUS_OK(
+        individualize_dev_hw_cfg_start(&flash_state, &lc_ctrl, &otp_ctrl));
+
+    // Issue and wait for reset.
+    rstmgr_testutils_reason_clear();
+    CHECK_DIF_OK(dif_rstmgr_software_device_reset(&rstmgr));
+    wait_for_interrupt();
+  } else if (info == kDifRstmgrResetInfoSw) {
+    CHECK_STATUS_OK(individualize_dev_hw_cfg_end(&otp_ctrl));
+  } else {
+    LOG_FATAL("Unexpected reset reason: %08x", info);
+  }
+
+  return true;
+}
