44a0a17da5f89d0a38eeca00d020c1d5b3f3573a
Alphan Ulusoy <alphan@opentitan.org>
2023-04-10 12:08:25 -0400

    [sw/silicon_creator] Add rsa_key.c for extern declaration
    Signed-off-by: Alphan Ulusoy <alphan@opentitan.org>

---
+++ sw/device/silicon_creator/lib/sigverify/rsa_key.c
@@ -0,0 +1,7 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+#include "sw/device/silicon_creator/lib/sigverify/rsa_key.h"
+
+// `extern` declarations for `inline` functions in the header.
+extern uint32_t sigverify_rsa_key_id_get(const sigverify_rsa_buffer_t *modulus);
