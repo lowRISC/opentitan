7a802b9d7774d9897606ea28c5f54f28c7bfd233
Alphan Ulusoy <alphan@opentitan.org>
2023-04-11 06:38:14 -0400

    [sw/silicon_creator] Move RSA signature verification to rsa_verify
    Signed-off-by: Alphan Ulusoy <alphan@opentitan.org>

--- sw/device/silicon_creator/lib/sigverify/sigverify.h
+++ sw/device/silicon_creator/lib/sigverify/sigverify.h
@@ -8,11 +8,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include "sw/device/silicon_creator/lib/drivers/hmac.h"
-#include "sw/device/silicon_creator/lib/drivers/lifecycle.h"
-#include "sw/device/silicon_creator/lib/error.h"
 #include "sw/device/silicon_creator/lib/manifest.h"
-#include "sw/device/silicon_creator/lib/sigverify/rsa_key.h"
+#include "sw/device/silicon_creator/lib/sigverify/rsa_verify.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -28,24 +25,6 @@ enum {
   kSigverifyFlashExec = 0xa26a38f7,
 };
 
-/**
- * Verifies an RSASSA-PKCS1-v1_5 signature.
- *
- * The actual implementation that is used (software or OTBN) is determined by
- * the life cycle state of the device and the OTP value.
- *
- * @param signature Signature to be verified.
- * @param key Signer's RSA public key.
- * @param act_digest Actual digest of the message being verified.
- * @param lc_state Life cycle state of the device.
- * @param[out] flash_exec Value to write to the flash_ctrl EXEC register.
- * @return Result of the operation.
- */
-rom_error_t sigverify_rsa_verify(const sigverify_rsa_buffer_t *signature,
-                                 const sigverify_rsa_key_t *key,
-                                 const hmac_digest_t *act_digest,
-                                 lifecycle_state_t lc_state,
-                                 uint32_t *flash_exec);
 
 /**
  * Gets the usage constraints struct that is used for verifying a ROM_EXT.
