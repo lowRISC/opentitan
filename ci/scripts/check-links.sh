d98e7ad1c589e2d0d595476f6b93a6623e9fcb89
Hugo McNally <hugo.mcnally@gmail.com>
2023-04-11 10:53:48 +0100

    [ci] Added an offline link check.
    Signed-off-by: Hugo McNally <hugo.mcnally@gmail.com>

---
+++ ci/scripts/check-links.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Checks links between files.
+
+set -e
+
+# Remove file containing invalid UTF-8
+rm -f sw/vendor/eembc_coremark/docs/html/index/General2.html
+
+# Run Offline Link Check
+lychee hw/ sw/ doc/ util/ \
+    --offline --no-progress \
+    --exclude-path sw/vendor \
+    --exclude-path util/i2csvg/smbus/SMBus.md \
+    --exclude-path hw/ip_templates/ \
+    --exclude-path hw/dv/doc/dv_doc_template.md \
+    --exclude-path doc/rust_for_c_devs.md \
+    --exclude-path hw/top_earlgrey/ip/pinmux/doc/autogen/targets.md \
+    || {
+      echo -n "##vso[task.logissue type=error]"
+      echo "Link Check failed."
+      exit 1
+    }
