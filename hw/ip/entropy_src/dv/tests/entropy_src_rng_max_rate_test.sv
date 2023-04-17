4310155e2da2e9594b3129308f57fe1eb7414bcf
Pirmin Vogel <vogelpi@lowrisc.org>
2023-04-17 16:31:03 +0000

    [entropy_src, dv] Tune entropy_src_rng_max_rate config for coverage
    This commit tunes the configuration of the max rate test to improve
    functional coverage. This is achieved 1) by lowering the average time
    between random re-configurations, doing more configs with FIPS mode
    enabled, and fewer runs with ES_TYPE set to true (SHA3 conditioner off),
    and 2) by increasing the number of runs.
    
    This helps to push the functional coverage for the cr_config cross
    of the csrng_hw_cg covergroup from roughly 70% to 96%. The remaining
    
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

---
+++ hw/ip/entropy_src/dv/tests/entropy_src_rng_max_rate_test.sv
@@ -0,0 +1,36 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class entropy_src_rng_max_rate_test extends entropy_src_rng_test;
+
+  `uvm_component_utils(entropy_src_rng_max_rate_test)
+  `uvm_component_new
+
+  function void configure_env();
+    super.configure_env();
+
+    // RNG failures and CSR-driven alert events won't happen in this test.
+    cfg.hard_mtbf                = -1;
+    cfg.soft_mtbf                = -1;
+    cfg.mean_rand_csr_alert_time = -1;
+    // We are interested in reconfiguring the DUT frequently to try many configurations for
+    // coverage. To this end, this test configures the RNG agent to produce entropy at the
+    // highest possible rate. At this rate, the initial boot-time/non-FIPS seed is produced
+    // after roughly 7us (rng_bit_enable = 0) or 30us (rng_bit_enable = 1) on average. The
+    // first FIPS seed is then available after roughly 80us and further seeds after 40us
+    // (rng_bit_enable = 0).
+    cfg.mean_rand_reconfig_time = 500us;
+    // This test automatically switches the DUT from non-FIPS to FIPS mode once a seed
+    // is observed. However, as we reconfigure the DUT very frequently, this automatic
+    // switch might not happen.
+    cfg.dut_cfg.fips_enable_pct = 60;
+    // We want to hit all rng_bit_enable settings (off, bit 0 - 3) w/ and w/o bypass and
+    // fips_enable.
+    cfg.dut_cfg.rng_bit_enable_pct = 80;
+    cfg.dut_cfg.type_bypass_pct = 50;
+    `DV_CHECK_RANDOMIZE_FATAL(cfg)
+
+    `uvm_info(`gfn, $sformatf("%s", cfg.convert2string()), UVM_LOW)
+  endfunction
+endclass : entropy_src_rng_max_rate_test
