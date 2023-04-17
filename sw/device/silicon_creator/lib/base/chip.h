44fa493bd84a0eaa0f82a48979fb878568c9a4f1
Alphan Ulusoy <alphan@opentitan.org>
2023-04-17 07:21:57 -0400

    [sw/silicon_creator] Add spx_key field to the manifest
    Signed-off-by: Alphan Ulusoy <alphan@opentitan.org>

--- sw/device/silicon_creator/lib/base/chip.h
+++ sw/device/silicon_creator/lib/base/chip.h
@@ -13,7 +13,7 @@
 /**
  * Manifest size for boot stages stored in flash (in bytes).
  */
-#define CHIP_MANIFEST_SIZE 8752
+#define CHIP_MANIFEST_SIZE 8784
 
 /**
  * ROM_EXT manifest identifier (ASCII "OTRE").
