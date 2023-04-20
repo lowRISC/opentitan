29b3bd6fd2cdd0198bd874588053ee82eb040b4c
Chris Frantz <cfrantz@google.com>
2023-04-20 15:53:09 -0700

    [hsmtool] Additional utility functions
    1. Define `hsmtool`-level errors.
    2. Implement some helper functions for finding HSM objects:
       `search_spec`, `object_exists` and `no_object_exits`.
    3. Implmeent some helpers for file IO.
    4. Implement some helpers for handling input data to be signed:
       - `PlainText` data is hashed and PKCS1v1.5 padded.
       - `Sh256Hash` data is already hashed, but gets PKCS1v1.5 padded.
       - `Raw` data gets no transformation before signing.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ sw/host/hsmtool/src/error.rs
@@ -0,0 +1,28 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::util::attribute::AttributeError;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum HsmError {
+    #[error("Token {0:?} not found")]
+    TokenNotFound(String),
+    #[error("Unknown user {0:?}.  Expecting one of {{so, user}}")]
+    UnknownUser(String),
+    #[error("Key error:  {0}")]
+    KeyError(String),
+    #[error("Object already exists id={0:?} label={1:?}")]
+    ObjectExists(String, String),
+    #[error("Bad hash size: expected {0} bytes, but got {1} bytes")]
+    HashSizeError(usize, usize),
+    #[error("No search criteria.  Specify an id or label")]
+    NoSearchCriteria,
+    #[error("Session required")]
+    SessionRequired,
+    #[error("Unsupported: {0}")]
+    Unsupported(String),
+    #[error(transparent)]
+    AttributeError(#[from] AttributeError),
+}
