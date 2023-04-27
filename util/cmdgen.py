7a5ff8f0cd3b9bc9a24b4bdabfb40453516b9ae9
Hugo McNally <hugo.mcnally@gmail.com>
2023-04-27 20:51:08 +0100

    [cmdgen] Now takes globs as arguments.
    Signed-off-by: Hugo McNally <hugo.mcnally@gmail.com>

--- util/cmdgen.py
+++ util/cmdgen.py
@@ -5,12 +5,22 @@
 """Mechanism to auto-generate documentation based on inline shell commands.
 
 This is a generic, repeatable method of inserting generated/rendered content
-into a markdown file. The populated .md file can then be committed into the
+into documentation files. The populated files can then be committed into the
 repository, so the in-repo representation matches the generated content.
 
-CI tooling can automatically re-generate the files if other changes to the
-repository would change the generated content. Linting or pre-commit checks
-could keep such-documenation always up-to-date.
+CMDGEN blocks are declared within a file as follows:
+```
+<!-- BEGIN CMDGEN util/selfdoc.py reggen -->
+                   `--------------------`
+                            `cmd`
+
+... generated_content ...
+
+<!-- END CMDGEN -->
+```
+
+CI tooling can automatically check that files are out of date due to
+changes to the repository from which the documentation is generated.
 """
 
 import sys
@@ -22,8 +32,9 @@ import argparse
 import logging
 logger = logging.getLogger(__name__)
 
+REPO_ROOT = Path(__file__).parents[1].resolve()
 
-# START and END marker tags use the HTML-style comments '<!-- -->'
+# The CMDGEN block delimiters:
 START_MARKER_PATTERN = re.compile(
     r"""^<!--            \s*
         BEGIN \s* CMDGEN \s*
@@ -37,25 +48,23 @@ END_MARKER_PATTERN = re.compile(
     flags=(re.M | re.X))
 
 
-def cmdgen_rewrite_md(filepath: Path, dry_run: bool):
-    '''Find all matching blocks in a file, and re-generate their contents.
+def cmdgen_rewrite_md(filepath: Path, dry_run: bool, update: bool) -> bool:
+    '''Find all CMDGEN blocks in a file and check their content is up to date.
 
-    START_MARKER_PATTERN and END_MARKER_PATTERN define the delimiters.
-    For example, CMDGEN blocks are defined within a file as follows...
-    """
-    <!-- BEGIN CMDGEN util/selfdoc.py reggen -->
-                       `--------------------`
-                                `cmd`
+    See this module's docstring to see how these blocks are declared.
 
-    ...generated_content...
+    Args:
+        filepath (Path): The file to check.
+        dry_run (bool): If set, commands will be printed, but not executed.
+        update (bool): If set, when content is out of date it will be updated.
 
-
-    <!-- END CMDGEN -->
-    """
-
+    Returns:
+        bool: If neither dry_run or update are set, will return True if an update is required.
     '''
-    content = filepath.read_text()
+    rel_path = filepath.relative_to(REPO_ROOT)
     modified = False
+    needs_updating = False
+    content = filepath.read_text()
     pos = 0
     while True:
         # search next start marker
@@ -68,12 +77,13 @@ def cmdgen_rewrite_md(filepath: Path, dry_run: bool):
         # search end marker after the start marker
         match_end = END_MARKER_PATTERN.search(content, pos)
         if match_end is None:
-            sys.exit(f"Error in {filepath}: start marker on "
-                     f"L{match_start_linum} with command '{cmd}' is missing a"
-                     "matching end marker!")
+            sys.exit(
+                f"Error in {rel_path}: start marker on L{match_start_linum} "
+                "missing a matching end marker!"
+            )
 
-        logger.info(f" {filepath}:L{match_start_linum} : Running cmd '{cmd}'")
         if dry_run:
+            logger.info(f"{rel_path}:L{match_start_linum}: `{cmd}`")
             # don't run
             pos = match_end.end(0)
             continue
@@ -81,47 +91,73 @@ def cmdgen_rewrite_md(filepath: Path, dry_run: bool):
         # Run the found-command in a subshell
         res = subprocess.run(cmd, shell=True, capture_output=True)
         if res.stderr:
-            logger.info(f"At L{match_start_linum} the command '{cmd}' output"
-                        " the following error messages:\n"
-                        f" {res.stderr.decode()}")
+            logger.info(
+                f"{rel_path}:L{match_start_linum}: `{cmd}` "
+                f"output the following error messages:\n{res.stderr.decode()}"
+            )
         if res.returncode != 0:
-            sys.exit(f"At L{match_start_linum} the command '{cmd}' had a"
-                     " non-zero return code.")
+            sys.exit(
+                f"{rel_path}:L{match_start_linum}: `{cmd}` "
+                "had a non-zero return code of {res.returncode}."
+            )
 
-        new_content = res.stdout.decode()
-        # replace content
-        content = \
-            content[0:match_start.end(0)] + \
-            '\n' + new_content + '\n' + \
-            content[match_end.start(0):]
-        modified = True
-        # resume after end line, adjust for new content size
-        pos = \
-            match_end.end(0) + \
-            len(new_content) - \
-            (match_end.start(0) - match_start.end(0))
+        pos = match_end.end(0)
+
+        new_content = '\n' + res.stdout.decode() + '\n'
+        old_content = content[match_start.end(0):match_end.start(0)]
+        if new_content != old_content:
+            if update:
+                logger.info(
+                    f"{rel_path}:L{match_start_linum}: Updating generated content."
+                )
+                # replace content
+                content = \
+                    content[0:match_start.end(0)] + \
+                    new_content + \
+                    content[match_end.start(0):]
+                modified = True
+                # update position to account for new content size
+                pos += len(new_content) - len(old_content)
+            else:
+                logger.info(
+                    f"{rel_path}:L{match_start_linum}: Generated content needs updating."
+                )
+                needs_updating = True
 
     # write back
     if modified:
         filepath.write_text(content)
 
+    return needs_updating
 
-def main():
-    '''Run CMDGEN for all markdown files in the OpenTitan project.'''
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--dry-run",
-                        help="Print commands but do not execute",
-                        action="store_true")
+
+def main() -> int:
+    '''Either check or update all CMDGEN blocks found in the given files.'''
+    parser = argparse.ArgumentParser(prog="cmdgen")
+    parser.add_argument(
+        "globs", nargs="+", type=str, metavar="file",
+        help="File(s) to check with paths relative to the repository root, "
+             "these can be given as glob patterns.",
+    )
+    parser.add_argument(
+        "--dry-run", help="Print commands but do not execute.", action="store_true",
+    )
+    parser.add_argument(
+        "-u", "--update", help="Update out of date content.", action="store_true",
+    )
     args = parser.parse_args()
 
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)  # root -> most-permissive
     logger.setLevel(logging.INFO)
 
-    repo_root = Path(__file__).parents[1].resolve()
-    for f in repo_root.rglob('**/*.md'):  # Recursively find all .md files
-        cmdgen_rewrite_md(f, args.dry_run)
+    needs_updating = False
+    for glob in args.globs:
+        for file in REPO_ROOT.glob(glob):
+            needs_updating |= cmdgen_rewrite_md(file, args.dry_run, args.update)
+
+    return 1 if needs_updating else 0
 
 
 if __name__ == '__main__':
-    main()
+    exit(main())
