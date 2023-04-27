93d2f9ef21e4ef69f885f424586d5dcee237eee9
Tim Trippel <ttrippel@google.com>
2023-04-26 22:44:05 -0700

    [manuf] parse exported ujson provisioning data on host
    This updates the provisioning_functest rust test harness to parse the
    provisioning data exported from the device over the console.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/host/tests/manuf/provisioning/src/main.rs
+++ sw/host/tests/manuf/provisioning/src/main.rs
@@ -4,14 +4,17 @@
 
 use std::time::Duration;
 
-use anyhow::{anyhow, Result};
-use regex::Regex;
+use anyhow::Result;
 use structopt::StructOpt;
 
 use opentitanlib::app::TransportWrapper;
 use opentitanlib::execute_test;
 use opentitanlib::test_utils::init::InitializeTest;
-use opentitanlib::uart::console::{ExitStatus, UartConsole};
+use opentitanlib::test_utils::rpc::UartRecv;
+use opentitanlib::uart::console::UartConsole;
+
+mod provisioning_data;
+use provisioning_data::ManufProvisioning;
 
 #[derive(Debug, StructOpt)]
 struct Opts {
@@ -27,51 +30,24 @@ struct Opts {
 }
 
 fn provisioning(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    // Get UART, set flow control, and wait for for test to start running.
     let uart = transport.uart("console")?;
-    let mut console = UartConsole {
-        timeout: Some(opts.timeout),
-        exit_success: Some(Regex::new(r"PASS.*\n")?),
-        exit_failure: Some(Regex::new(r"(FAIL|FAULT).*\n")?),
-        newline: true,
-        ..Default::default()
-    };
-    let mut stdout = std::io::stdout();
-    let result = console.interact(&*uart, None, Some(&mut stdout))?;
-    match result {
-        ExitStatus::None | ExitStatus::CtrlC => Ok(()),
-        ExitStatus::Timeout => {
-            if console.exit_success.is_some() {
-                Err(anyhow!("Console timeout exceeded"))
-            } else {
-                Ok(())
-            }
-        }
-        ExitStatus::ExitSuccess => {
-            log::info!(
-                "ExitSuccess({:?})",
-                console.captures(result).unwrap().get(0).unwrap().as_str()
-            );
-            Ok(())
-        }
-        ExitStatus::ExitFailure => {
-            log::info!(
-                "ExitFailure({:?})",
-                console.captures(result).unwrap().get(0).unwrap().as_str()
-            );
-            Err(anyhow!("Matched exit_failure expression"))
-        }
-    }
+    uart.set_flow_control(true)?;
+    let _ = UartConsole::wait_for(&*uart, r"Running [^\r\n]*", opts.timeout)?;
+
+    // Wait for exported data to be transimitted over the console.
+    let export_data = ManufProvisioning::recv(&*uart, opts.timeout, false)?;
+    log::info!("{:#x?}", export_data);
+
+    // TODO: parse RMA unlock token, decrypt it, and try to issue an LC transition.
+    Ok(())
 }
 
 fn main() -> Result<()> {
     let opts = Opts::from_args();
     opts.init.init_logging();
+
     let transport = opts.init.init_target()?;
-    let uart = transport.uart("console")?;
-    uart.set_flow_control(true)?;
-    let _ = UartConsole::wait_for(&*uart, r"Running [^\r\n]*", opts.timeout)?;
-
     execute_test!(provisioning, &opts, &transport);
-
     Ok(())
 }
