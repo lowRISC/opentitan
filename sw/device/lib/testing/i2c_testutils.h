ae08db2b433b8a7594526926cc3ddca8809d19ea
Douglas Reis <doreis@lowrisc.org>
2023-04-28 11:01:12 +0100

    [i2c, testutils] Enforce return checking with OT_WARN_UNUSED_RESULT
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/device/lib/testing/i2c_testutils.h
+++ sw/device/lib/testing/i2c_testutils.h
@@ -21,6 +21,7 @@
  * @param skip_stop Skip the stop bit as this may be chained with a read.
  * @return The result of the operation.
  */
+OT_WARN_UNUSED_RESULT
 status_t i2c_testutils_wr(const dif_i2c_t *i2c, uint8_t addr,
                           uint8_t byte_count, const uint8_t *data,
                           bool skip_stop);
@@ -33,6 +34,7 @@ status_t i2c_testutils_wr(const dif_i2c_t *i2c, uint8_t addr,
  * @param byte_count The number of bytes to be read.
  * @return The result of the operation.
  */
+OT_WARN_UNUSED_RESULT
 status_t i2c_testutils_rd(const dif_i2c_t *i2c, uint8_t addr,
                           uint8_t byte_count);
 
@@ -44,6 +46,7 @@ status_t i2c_testutils_rd(const dif_i2c_t *i2c, uint8_t addr,
  * @return kOk(dir) Where dir indicates the direction of transaction is signaled
  * as Read(1) and not Write(0), or an error.
  */
+OT_WARN_UNUSED_RESULT
 status_t i2c_testutils_target_check_start(const dif_i2c_t *i2c, uint8_t *addr);
 
 /**
@@ -57,6 +60,7 @@ status_t i2c_testutils_target_check_start(const dif_i2c_t *i2c, uint8_t *addr);
  * the transaction should continue and the contents of cont_byte are valid, or
  * an error.
  */
+OT_WARN_UNUSED_RESULT
 status_t i2c_testutils_target_check_end(const dif_i2c_t *i2c,
                                         uint8_t *cont_byte);
 
@@ -68,6 +72,7 @@ status_t i2c_testutils_target_check_end(const dif_i2c_t *i2c,
  * @param data Array of data bytes to be sent.
  * @return The result of the operation.
  */
+OT_WARN_UNUSED_RESULT
 status_t i2c_testutils_target_rd(const dif_i2c_t *i2c, uint8_t byte_count,
                                  const uint8_t *data);
 
@@ -82,6 +87,7 @@ status_t i2c_testutils_target_rd(const dif_i2c_t *i2c, uint8_t byte_count,
  * the transaction should continue and the contents of cont_byte are valid, or
  * an error.
  */
+OT_WARN_UNUSED_RESULT
 status_t i2c_testutils_target_check_rd(const dif_i2c_t *i2c, uint8_t *addr,
                                        uint8_t *cont_byte);
 
@@ -92,6 +98,7 @@ status_t i2c_testutils_target_check_rd(const dif_i2c_t *i2c, uint8_t *addr,
  * @param byte_count The number of bytes to be written.
  * @return The result of the operation.
  */
+OT_WARN_UNUSED_RESULT
 status_t i2c_testutils_target_wr(const dif_i2c_t *i2c, uint8_t byte_count);
 
 /**
@@ -107,6 +114,7 @@ status_t i2c_testutils_target_wr(const dif_i2c_t *i2c, uint8_t byte_count);
  * the transaction should continue and the contents of cont_byte are valid, or
  * an error.
  */
+OT_WARN_UNUSED_RESULT
 status_t i2c_testutils_target_check_wr(const dif_i2c_t *i2c, uint8_t byte_count,
                                        uint8_t *addr, uint8_t *bytes,
                                        uint8_t *cont_byte);
@@ -118,6 +126,7 @@ status_t i2c_testutils_target_check_wr(const dif_i2c_t *i2c, uint8_t byte_count,
  * @param kI2cIdx The i2c identifier.
  * @return The result of the operation.
  */
+OT_WARN_UNUSED_RESULT
 status_t i2c_testutils_connect_i2c_to_pinmux_pins(const dif_pinmux_t *pinmux,
                                                   uint8_t kI2cIdx);
 
