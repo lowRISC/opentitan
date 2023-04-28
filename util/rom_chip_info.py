e4996168bebb1e55279c9e05bea785839819da94
Dan McArdle <dmcardle@opentitan.org>
2023-04-28 12:47:52 -0400

    [util] Refactor rom_chip_info.py and add tests
    Signed-off-by: Dan McArdle <dmcardle@opentitan.org>

--- util/rom_chip_info.py
+++ util/rom_chip_info.py
@@ -2,13 +2,11 @@
 # Copyright lowRISC contributors.
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
-r"""Generates chip_info.h for ROM build
-"""
+"""Generates chip_info.c for ROM build."""
 
 import argparse
 import hashlib
 import logging as log
-import sys
 from pathlib import Path
 
 
@@ -21,6 +19,7 @@ def generate_chip_info_c_source(scm_revision: int) -> str:
 
     SHA1_DIGEST_SIZE_BITS = hashlib.sha1().digest_size * 8
     assert scm_revision.bit_length() <= SHA1_DIGEST_SIZE_BITS
+    assert scm_revision >= 0
 
     # Truncate the SCM revision to the top N bits.
     SCM_REVISION_SIZE_BITS = 64
@@ -42,46 +41,48 @@ const chip_info_t kChipInfo __attribute__((section(".chip_info"))) = {{
 """
 
 
+def read_version_file(path: Path) -> int:
+    """Interprets the contents of `path` as a big-endian hex literal."""
+
+    with open(path, "rt") as f:
+        version = f.read().strip()
+    return int(version, base=16)
+
+
+def write_source_file(outdir: Path, contents: str) -> Path:
+    """Creates chip_info.c in `outdir`. Returns the path to the new file."""
+
+    source_out_path = outdir / "chip_info.c"
+
+    outdir.mkdir(parents=True, exist_ok=True)
+    with source_out_path.open(mode='w', encoding='utf-8') as fout:
+        fout.write(contents)
+
+    return source_out_path
+
+
 def main():
+    log.basicConfig(format="%(levelname)s: %(message)s")
+
     parser = argparse.ArgumentParser(prog="rom_chip_info")
     parser.add_argument('--outdir',
                         '-o',
                         required=True,
+                        type=Path,
                         help='Output Directory')
-    parser.add_argument('--ot_version',
-                        required=False,
-                        help='OpenTitan Version')
     parser.add_argument('--ot_version_file',
-                        required=False,
+                        required=True,
+                        type=Path,
                         help='Path to a file with the OpenTitan Version')
 
-    log.basicConfig(format="%(levelname)s: %(message)s")
     args = parser.parse_args()
 
-    if not args.outdir:
-        log.error("Missing --outdir.")
-        raise SystemExit(sys.exc_info()[1])
-
-    if args.ot_version:
-        version = args.ot_version
-    elif args.ot_version_file:
-        version = open(args.ot_version_file, "rt").read().strip()
-    else:
-        log.error(
-            "Missing ot_version, provide --ot_version or --ot_version_file.")
-        raise SystemExit(sys.exc_info()[1])
-    version = int(version, base=16)
+    version = read_version_file(args.ot_version_file)
     log.info("Version: %x" % (version, ))
 
-    outdir = Path(args.outdir)
-
-    outdir.mkdir(parents=True, exist_ok=True)
-    source_out_path = outdir / "chip_info.c"
-
-    output = generate_chip_info_c_source(version)
-
-    with source_out_path.open(mode='w', encoding='UTF-8') as fout:
-        fout.write(output)
+    generated_source = generate_chip_info_c_source(version)
+    out_path = write_source_file(args.outdir, generated_source)
+    log.info("Generated new source file: %s" % (out_path))
 
 
 if __name__ == "__main__":
