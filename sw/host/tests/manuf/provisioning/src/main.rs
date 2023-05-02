2a738da77f8055b6d0d6eaeded43793836812aad
Tim Trippel <ttrippel@google.com>
2023-05-02 14:54:21 -0700

    [manuf] load RMA unlock token EC keys in provisioning test
    This loads the EC keys used to generate the shared secret (i.e., AES
    key) used to encrypt the RMA unlock token when exporting it from the
    device during provisioning. We make use of the `elliptic-curve` crate
    from the RustCrypto project.
    
    This partially addresses #17393.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/host/tests/manuf/provisioning/src/main.rs
+++ sw/host/tests/manuf/provisioning/src/main.rs
@@ -2,9 +2,13 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+use std::path::PathBuf;
 use std::time::Duration;
 
 use anyhow::Result;
+use elliptic_curve::pkcs8::DecodePrivateKey;
+use elliptic_curve::{PublicKey, SecretKey};
+use p256::NistP256;
 use structopt::StructOpt;
 
 use opentitanlib::app::TransportWrapper;
@@ -27,6 +31,9 @@ struct Opts {
         help = "Console receive timeout",
     )]
     timeout: Duration,
+
+    #[structopt(long, help = "HSM generated ECDH private key DER file.")]
+    hsm_ecdh_sk: PathBuf,
 }
 
 fn provisioning(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
@@ -39,7 +46,25 @@ fn provisioning(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
     let export_data = ManufProvisioning::recv(&*uart, opts.timeout, false)?;
     log::info!("{:#x?}", export_data);
 
-    // TODO: parse RMA unlock token, decrypt it, and try to issue an LC transition.
+    // Load HSM-generated EC private key.
+    let _hsm_sk = SecretKey::<NistP256>::read_pkcs8_der_file(&opts.hsm_ecdh_sk)?;
+
+    // Load device-generated EC public key.
+    let mut device_pk_sec1_bytes = Vec::new();
+    for key_word in &export_data.wrapped_rma_unlock_token.ecc_pk_device_y {
+        device_pk_sec1_bytes.extend(&key_word.to_le_bytes());
+    }
+    for key_word in &export_data.wrapped_rma_unlock_token.ecc_pk_device_x {
+        device_pk_sec1_bytes.extend(&key_word.to_le_bytes());
+    }
+    device_pk_sec1_bytes.push(0x04); // This indicates the EC public key is not compressed.
+    device_pk_sec1_bytes.reverse();
+    let _device_pk = PublicKey::<NistP256>::from_sec1_bytes(&device_pk_sec1_bytes)?;
+
+    // TODO(#17393): Peform ECDH to generate shared secrete (AES) key.
+    // TODO(#17393): Decrypt RMA unlock token.
+    // TODO(#17393): Try to issue an LC transition to RMA to verify unlock token.
+
     Ok(())
 }
 
