c9f404dc1e2072009b046917ce2b92e7efc47b6c
Jade Philipoom <jadep@google.com>
2023-05-02 19:40:29 +0200

    [crypto] Create a unified header file for the cryptolib.
    This will hide the internal structure and allow callers to import only
    one thing.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

---
+++ sw/device/lib/crypto/otcrypto.h
@@ -0,0 +1,31 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_CRYPTO_OTCRYPTO_H_
+#define OPENTITAN_SW_DEVICE_LIB_CRYPTO_OTCRYPTO_H_
+
+#include "sw/device/lib/crypto/include/aes.h"
+#include "sw/device/lib/crypto/include/datatypes.h"
+#include "sw/device/lib/crypto/include/drbg.h"
+#include "sw/device/lib/crypto/include/ecc.h"
+#include "sw/device/lib/crypto/include/hash.h"
+#include "sw/device/lib/crypto/include/kdf.h"
+#include "sw/device/lib/crypto/include/key_transport.h"
+#include "sw/device/lib/crypto/include/mac.h"
+#include "sw/device/lib/crypto/include/rsa.h"
+
+/**
+ * @file
+ * @brief Unified header file that includes the full crypto library.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_CRYPTO_OTCRYPTO_H_
