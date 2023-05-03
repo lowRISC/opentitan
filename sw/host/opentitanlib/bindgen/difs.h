f3534a71e2107dd03f632d2c61f5b409687d8830
James Wainwright <james.wainwright@lowrisc.org>
2023-05-03 10:50:22 +0100

    [opentitanlib] Add bindgen for otp_ctrl registers
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- sw/host/opentitanlib/bindgen/difs.h
+++ sw/host/opentitanlib/bindgen/difs.h
@@ -9,7 +9,8 @@
 #include "sw/device/lib/dif/dif_lc_ctrl.h"
 #include "sw/device/lib/dif/dif_rstmgr.h"
 
-#include "clkmgr_regs.h"   // Generated.
-#include "lc_ctrl_regs.h"  // Generated.
+#include "clkmgr_regs.h"    // Generated.
+#include "lc_ctrl_regs.h"   // Generated.
+#include "otp_ctrl_regs.h"  // Generated.
 
 #endif  // OPENTITAN_SW_HOST_OPENTITANLIB_BINDGEN_DIFS_H_
