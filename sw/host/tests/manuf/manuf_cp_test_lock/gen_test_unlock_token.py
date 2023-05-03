f84ed7d81d79f13a5a637f6b506c411f457199b7
James Wainwright <james.wainwright@lowrisc.org>
2023-05-03 10:06:53 +0100

    [manuf] Add test for manuf_cp_test_lock
    Co-authored-by: Tim Trippel <ttrippel@google.com>
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

---
+++ sw/host/tests/manuf/manuf_cp_test_lock/gen_test_unlock_token.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+from Crypto.Hash import cSHAKE128
+
+TOKEN_LEN = 16
+TOKEN_PREIMAGE = b'\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA'
+
+if __name__ == '__main__':
+    hash_obj = cSHAKE128.new(data=TOKEN_PREIMAGE, custom=b'LC_CTRL')
+    digest_bytes = hash_obj.read(TOKEN_LEN)
+    digest_int = int.from_bytes(digest_bytes, byteorder='little')
+
+    preimage_literal = '[' + ','.join(hex(byte)
+                                      for byte in TOKEN_PREIMAGE) + ']'
+    literal = f'0x{digest_int:032x}'
+    print(f'preimage literal: {preimage_literal}')
+    print(f'postimage literal: {literal}')
