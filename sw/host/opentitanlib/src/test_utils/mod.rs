589878c31e12aae0af5cf66e8a6d30aa5e1fc855
James Wainwright <james.wainwright@lowrisc.org>
2023-05-03 14:14:49 +0100

    [opentitanlib] Add utility for reading and writing OTP partitions
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- sw/host/opentitanlib/src/test_utils/mod.rs
+++ sw/host/opentitanlib/src/test_utils/mod.rs
@@ -11,6 +11,7 @@ pub mod init;
 pub mod lc_transition;
 pub mod load_bitstream;
 pub mod load_sram_program;
+pub mod otp_ctrl;
 // The "english breakfast" variant of the chip doesn't have the same
 // set of IO and pinmux constants as the "earlgrey" chip.
 #[cfg(not(feature = "english_breakfast"))]
