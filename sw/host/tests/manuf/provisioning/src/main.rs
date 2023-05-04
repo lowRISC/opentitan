fc861e1bf742ebc741059cd3694c9b78ee720214
Tim Trippel <ttrippel@google.com>
2023-05-03 19:16:57 -0700

    [manuf] decrypt RMA unlock token on host
    This decrypts the RMA unlock token generated during provisioning on the
    host using the AES rust crate.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/host/tests/manuf/provisioning/src/main.rs
+++ sw/host/tests/manuf/provisioning/src/main.rs
@@ -5,7 +5,10 @@
 use std::path::PathBuf;
 use std::time::Duration;
 
+use aes::cipher::{generic_array::GenericArray, BlockDecrypt, KeyInit};
+use aes::Aes256Dec;
 use anyhow::Result;
+use elliptic_curve::ecdh::diffie_hellman;
 use elliptic_curve::pkcs8::DecodePrivateKey;
 use elliptic_curve::{PublicKey, SecretKey};
 use p256::NistP256;
@@ -44,10 +47,9 @@ fn provisioning(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
 
     // Wait for exported data to be transimitted over the console.
     let export_data = ManufProvisioning::recv(&*uart, opts.timeout, false)?;
-    log::info!("{:#x?}", export_data);
 
     // Load HSM-generated EC private key.
-    let _hsm_sk = SecretKey::<NistP256>::read_pkcs8_der_file(&opts.hsm_ecdh_sk)?;
+    let hsm_sk = SecretKey::<NistP256>::read_pkcs8_der_file(&opts.hsm_ecdh_sk)?;
 
     // Load device-generated EC public key.
     let mut device_pk_sec1_bytes = Vec::new();
@@ -59,10 +61,30 @@ fn provisioning(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
     }
     device_pk_sec1_bytes.push(0x04); // This indicates the EC public key is not compressed.
     device_pk_sec1_bytes.reverse();
-    let _device_pk = PublicKey::<NistP256>::from_sec1_bytes(&device_pk_sec1_bytes)?;
+    let device_pk = PublicKey::<NistP256>::from_sec1_bytes(&device_pk_sec1_bytes)?;
 
-    // TODO(#17393): Peform ECDH to generate shared secrete (AES) key.
-    // TODO(#17393): Decrypt RMA unlock token.
+    // Peform ECDH to generate shared secret (AES) key.
+    let ecdh_shared_secret = diffie_hellman(hsm_sk.to_nonzero_scalar(), device_pk.as_affine());
+    let mut aes_key_bytes = Vec::from(ecdh_shared_secret.raw_secret_bytes().as_slice());
+    aes_key_bytes.reverse();
+    let aes_key = GenericArray::from_slice(aes_key_bytes.as_slice());
+
+    // Load encrypted RMA unlock token into a GenericArray.
+    let mut ciphertext = Vec::new();
+    for ciphertext_word in export_data.wrapped_rma_unlock_token.data {
+        ciphertext.extend(&ciphertext_word.to_le_bytes());
+    }
+    let mut plaintext = GenericArray::from_mut_slice(ciphertext.as_mut_slice());
+
+    // Decrypt the RMA unlock token, and convert it to a fixed array of 32-bit words.
+    let aes_ecb_cipher = Aes256Dec::new(&aes_key);
+    aes_ecb_cipher.decrypt_block(&mut plaintext);
+    let mut rma_unlock_token = [0u32; 4];
+    for (i, word) in rma_unlock_token.iter_mut().enumerate() {
+        *word = u32::from_le_bytes(plaintext[(i * 4)..((i * 4) + 4)].try_into().unwrap());
+    }
+    log::info!("Decrypted Token: {:#x?}", rma_unlock_token);
+
     // TODO(#17393): Try to issue an LC transition to RMA to verify unlock token.
 
     Ok(())
