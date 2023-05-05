d42903d1ad6032c7dd745e2147279098f6133db2
Tim Trippel <ttrippel@google.com>
2023-05-05 14:02:10 -0700

    [manuf] fix bugs in `manuf_cp_yield_test`
    There were several bugs in the `manuf_cp_yield_test`, that were causing
    it to fail consistently in CI, including:
    1. the external clock enablement is failing due to clkmgr not ACKing the
       switch (see #18403),
    2. the convention for writing host tests with opentitanlib was not
       followed (convention is to use the `execute_test!` macro so test code
       can be reused between tests if needed,
    3. logging was not initialized, so we couldn't tell what was happening
       in the test, and
    4. we did not check the lc_ctrl status was INITIALIZED before reading
       the LC state over the system bus (causing additional test flakiness
       on top of the JTAG flakiness that already exists).
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/host/tests/manuf/manuf_cp_yield_test/src/main.rs
+++ sw/host/tests/manuf/manuf_cp_yield_test/src/main.rs
@@ -8,79 +8,83 @@
 //! Both interfaces are used to check they're accessible. This is only possible
 //! in some lifecycle states: `TEST_UNLOCKED0` is used in this case.
 
-use anyhow::Context;
+use std::time::Duration;
+
+use anyhow::{Context, Result};
 use structopt::StructOpt;
 
-use opentitanlib::dif::lc_ctrl::{DifLcCtrlState, LcCtrlReg};
+use opentitanlib::app::TransportWrapper;
+use opentitanlib::dif::lc_ctrl::{DifLcCtrlState, LcCtrlReg, LcCtrlStatus};
+use opentitanlib::execute_test;
 use opentitanlib::io::jtag::{JtagParams, JtagTap};
-use opentitanlib::test_utils::extclk::{ClockSpeed, ExternalClock};
+//use opentitanlib::test_utils::extclk::{ClockSpeed, ExternalClock};
 use opentitanlib::test_utils::init::InitializeTest;
+use opentitanlib::test_utils::lc_transition::wait_for_status;
 use top_earlgrey::top_earlgrey_memory;
 
 #[derive(Debug, StructOpt)]
 struct Opts {
     #[structopt(flatten)]
     init: InitializeTest,
+
     #[structopt(flatten)]
     jtag: JtagParams,
 }
 
-fn main() -> anyhow::Result<()> {
-    let opts = Opts::from_args();
-    opts.init.init_target()?;
-
-    let transport = opts.init.init_target()?;
-
-    // Begin with the TAP straps set for the CPU.
+fn manuf_cp_yield_test(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    // Reset the chip, select the CPU TAP, and connect to it.
     transport
         .pin_strapping("PINMUX_TAP_RISCV")?
         .apply()
         .context("failed to apply RISCV TAP strapping")?;
-    transport
-        .reset_target(opts.init.bootstrap.options.reset_delay, true)
-        .context("failed to reset")?;
-
+    transport.reset_target(opts.init.bootstrap.options.reset_delay, true)?;
     let jtag = transport.jtag(&opts.jtag)?;
-
-    // Connect to the CPU over JTAG.
     jtag.connect(JtagTap::RiscvTap)
         .context("failed to connect to RISCV TAP over JTAG")?;
 
-    ExternalClock::enable(&*jtag, ClockSpeed::High).context("failed to enable external clock")?;
+    // TODO(#18403): switching to external clock does not succeed. Clkmgr fails to ACK switch.
+    //ExternalClock::enable(&*jtag, ClockSpeed::High).context("failed to enable external clock")?;
 
     // Read and write memory to verify connection.
 
-    // Check the lifecycle state via its memory map.
-    let mut reset_info_buf = [0];
+    // Check we can read the MMIO system memory region by reading the LC state.
+    // We must wait for the lc_ctrl to initialize before the LC state is exposed.
+    wait_for_status(&jtag, Duration::from_secs(3), LcCtrlStatus::INITIALIZED)?;
+    let mut encoded_lc_state = [0u32];
     jtag.read_memory32(
         top_earlgrey_memory::TOP_EARLGREY_LC_CTRL_BASE_ADDR as u32 + LcCtrlReg::LcState as u32,
-        &mut reset_info_buf,
+        &mut encoded_lc_state,
     )?;
-
     assert_eq!(
-        reset_info_buf[0],
+        encoded_lc_state[0],
         DifLcCtrlState::TestUnlocked0.redundant_encoding()
     );
 
-    // Without resetting, changing TAP strapping to the LC and reconnect JTAG.
-
+    // Without resetting, change TAP strapping to the LC and reconnect.
     jtag.disconnect().context("failed to disconnect JTAG")?;
-
     transport
         .pin_strapping("PINMUX_TAP_LC")?
         .apply()
         .context("failed to apply LC TAP strapping")?;
-
     jtag.connect(JtagTap::LcTap)
         .context("failed to connect to LC TAP over JTAG")?;
 
-    // Read and write registers to verify connection.
-
-    // Check the LC state is `TEST_UNLOCKED0`.
+    // Read and write LC state register to verify connection.
     let state = jtag.read_lc_ctrl_reg(&LcCtrlReg::LcState)?;
     assert_eq!(state, DifLcCtrlState::TestUnlocked0.redundant_encoding());
 
     jtag.disconnect().context("failed to disconnect JTAG")?;
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::from_args();
+    opts.init.init_logging();
+    opts.init.init_target()?;
+    let transport = opts.init.init_target()?;
+
+    execute_test!(manuf_cp_yield_test, &opts, &transport);
 
     Ok(())
 }
