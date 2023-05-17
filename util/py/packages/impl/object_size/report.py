a01f69c7ac96bbc4ebe94508d0f0901d7fc9da6c
Alphan Ulusoy <alphan@opentitan.org>
2023-05-17 07:02:00 -0400

    [py/scripts/object_size] Add section report
    Signed-off-by: Alphan Ulusoy <alphan@opentitan.org>

--- util/py/packages/impl/object_size/report.py
+++ util/py/packages/impl/object_size/report.py
@@ -6,12 +6,14 @@ import functools
 from pathlib import Path
 
 from rich.console import Console
-from rich.padding import Padding
 from rich.progress import BarColumn, Progress, TaskProgressColumn, TextColumn
+from rich.rule import Rule
+from rich.table import Column, Padding, Table
 
 from util.py.packages.impl.object_size.elf import parse_elf_file
 from util.py.packages.impl.object_size.memory import parse_memory_file
-from util.py.packages.impl.object_size.types import Size
+from util.py.packages.impl.object_size.types import (Address, Alignment,
+                                                     Memory, Section, Size)
 
 
 def print_utilization_report(memories) -> None:
@@ -27,15 +29,43 @@ def print_utilization_report(memories) -> None:
         rprint(Padding(bar.get_renderable(), (0, 0, 0, 4)))
 
 
+def my_table(*args, **kwargs):
+    return Table(
+        *args, **{
+            "highlight": True,
+            "row_styles": ["on bright_black", ""],
+            **kwargs
+        })
+
+
+def print_section_report(sections: list[Section]) -> None:
+    table = my_table(
+        Column("Name", justify="right", style="bold yellow", no_wrap=True),
+        Column("Memory", justify="center"),
+        Column("Size", justify="right"),
+        Column("Address", justify="left"),
+        Column("Alignment", justify="right"),
+        Column("Type", justify="center"),
+        Column("Flags", justify="right"),
+        title="[bold]Sections ASCENDING by VMA:[/bold]",
+    )
+
+    for s in sorted(sections.values(), key=lambda s: s.vma):
+        table.add_row(s.name, ', '.join(s.memories), Size.__str__(s),
+                      Address.__str__(s), Alignment.__str__(s), s.type_,
+                      s.flags)
+    rprint(Padding(table, (2, 0, 0, 2)))
 def print_report(path: Path, force_color: bool = False) -> None:
     global rprint
     rprint = Console(force_terminal=force_color).print
+    rprint(Padding(Rule(f"[bold]{path.name}[/bold]"), (1, 0, 0, 2)))
     rprint(Padding("[bold]Full path:[/bold]", (1, 0, 0, 2)))
     rprint(Padding(f"{path}", (0, 0, 0, 4)))
     rprint(Padding("[bold]File size:[/bold]", (1, 0, 0, 2)))
     rprint(Padding(f"{Size(path.stat().st_size)}", (0, 0, 0, 4)))
     if (path.suffixes[-1] == ".elf"):
         memories = parse_memory_file()
-        parse_elf_file(path, memories)
+        sections = parse_elf_file(path, memories)
         print_utilization_report(memories)
+        print_section_report(sections)
     rprint(Padding("", (2, 0, 0, 0)))
