94eb0df12dd39f584fad521240365edc00ae9eb9
Hugo McNally <hugo.mcnally@gmail.com>
2023-05-24 16:27:27 +0100

    [ujson] Added crc32 checks to the example unittest
    Signed-off-by: Hugo McNally <hugo.mcnally@gmail.com>

--- sw/device/lib/ujson/example_test.cc
+++ sw/device/lib/ujson/example_test.cc
@@ -21,6 +21,7 @@ TEST(Derive, FooSerialize) {
   EXPECT_TRUE(status_ok(ujson_serialize_foo(&uj, &foo)));
   EXPECT_EQ(ss.Sink(),
             R"json({"foo":-5,"bar":150000,"message":"Kilroy was here"})json");
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x1e31c20e);
 }
 
 TEST(Derive, FooDeserialize) {
@@ -31,6 +32,7 @@ TEST(Derive, FooDeserialize) {
   ujson_t uj = ss.UJson();
   EXPECT_TRUE(status_ok(ujson_deserialize_foo(&uj, &foo)));
   EXPECT_EQ(memcmp(&foo, &expected, sizeof(foo)), 0);
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x1e31c20e);
 }
 
 TEST(Derive, FooDeserializeNoFoo) {
@@ -40,6 +42,7 @@ TEST(Derive, FooDeserializeNoFoo) {
   ujson_t uj = ss.UJson();
   EXPECT_TRUE(status_ok(ujson_deserialize_foo(&uj, &foo)));
   EXPECT_EQ(memcmp(&foo, &expected, sizeof(foo)), 0);
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x431acc08);
 }
 
 TEST(Derive, FooDeserializeNoMessage) {
@@ -52,6 +55,7 @@ TEST(Derive, FooDeserializeNoMessage) {
   ujson_t uj = ss.UJson();
   EXPECT_TRUE(status_ok(ujson_deserialize_foo(&uj, &foo)));
   EXPECT_EQ(memcmp(&foo, &expected, sizeof(foo)), 0);
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0xded4ce6);
 }
 
 TEST(Derive, FooDeserializeMessageToLong) {
@@ -62,6 +66,7 @@ TEST(Derive, FooDeserializeMessageToLong) {
   ujson_t uj = ss.UJson();
   EXPECT_TRUE(status_ok(ujson_deserialize_foo(&uj, &foo)));
   EXPECT_EQ(memcmp(&foo, &expected, sizeof(foo)), 0);
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0xfaebc314);
 }
 
 TEST(Derive, FooDeserializeBogusKey) {
@@ -80,6 +85,7 @@ TEST(Derive, RectSerialize) {
   EXPECT_EQ(
       ss.Sink(),
       R"json({"top_left":{"x":10,"y":10},"bottom_right":{"x":60,"y":40}})json");
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x4b9a0fb1);
 }
 
 TEST(Derive, RectDeserialize) {
@@ -90,6 +96,7 @@ TEST(Derive, RectDeserialize) {
   ujson_t uj = ss.UJson();
   EXPECT_TRUE(status_ok(ujson_deserialize_rect(&uj, &r)));
   EXPECT_EQ(memcmp(&r, &expected, sizeof(r)), 0);
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0xb11e9dea);
 }
 
 TEST(Derive, MatrixSerialize) {
@@ -101,6 +108,7 @@ TEST(Derive, MatrixSerialize) {
   EXPECT_TRUE(status_ok(ujson_serialize_matrix(&uj, &m)));
   EXPECT_EQ(ss.Sink(),
             R"json({"k":[[0,1,2,3,4],[5,6,7,8,9],[-1,-2,-3,-4,-5]]})json");
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x927a936);
 }
 
 TEST(Derive, MatrixDeserialize) {
@@ -111,6 +119,7 @@ TEST(Derive, MatrixDeserialize) {
   SourceSink ss(R"json({"k":[[0,1],[2, 3, 4, 5],[-1]]})json");
   ujson_t uj = ss.UJson();
   EXPECT_TRUE(status_ok(ujson_deserialize_matrix(&uj, &m)));
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0xca69b105);
   ujson_serialize_matrix(&uj, &m);
   std::cout << ss.Sink() << "\n\n";
   EXPECT_EQ(memcmp(&m, &expected, sizeof(m)), 0);
@@ -122,16 +131,21 @@ TEST(Derive, DirectionSerialize) {
   ujson_t uj = ss.UJson();
   EXPECT_TRUE(status_ok(ujson_serialize_direction(&uj, &d)));
   EXPECT_EQ(ss.Sink(), R"json("East")json");
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x20ab69de);
 
   ss.Reset();
+  ujson_crc32_reset(&uj);
   d = kDirectionSouth;
   EXPECT_TRUE(status_ok(ujson_serialize_direction(&uj, &d)));
   EXPECT_EQ(ss.Sink(), R"json("South")json");
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x236b5d67);
 
   ss.Reset();
+  ujson_crc32_reset(&uj);
   d = static_cast<direction>(120);
   EXPECT_TRUE(status_ok(ujson_serialize_direction(&uj, &d)));
   EXPECT_EQ(ss.Sink(), R"json({"IntValue":120})json");
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x63cb5f57);
 }
 
 TEST(Derive, DirectionDeserialize) {
@@ -140,14 +154,19 @@ TEST(Derive, DirectionDeserialize) {
   ujson_t uj = ss.UJson();
   EXPECT_TRUE(status_ok(ujson_deserialize_direction(&uj, &d)));
   EXPECT_EQ(d, kDirectionWest);
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0xb5e93a6b);
 
   ss.Reset(R"json("North")json");
+  ujson_crc32_reset(&uj);
   EXPECT_TRUE(status_ok(ujson_deserialize_direction(&uj, &d)));
   EXPECT_EQ(d, kDirectionNorth);
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x1f4749b);
 
   ss.Reset(R"json({"IntValue":35})json");
+  ujson_crc32_reset(&uj);
   EXPECT_TRUE(status_ok(ujson_deserialize_direction(&uj, &d)));
   EXPECT_EQ(d, static_cast<direction>(35));
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x86f181c2);
 }
 
 TEST(Derive, FuzzyBoolSerialize) {
@@ -156,16 +175,21 @@ TEST(Derive, FuzzyBoolSerialize) {
   ujson_t uj = ss.UJson();
   EXPECT_TRUE(status_ok(ujson_serialize_fuzzy_bool(&uj, &d)));
   EXPECT_EQ(ss.Sink(), R"json("True")json");
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x68d3703f);
 
   ss.Reset();
+  ujson_crc32_reset(&uj);
   d = kFuzzyBoolFalse;
   EXPECT_TRUE(status_ok(ujson_serialize_fuzzy_bool(&uj, &d)));
   EXPECT_EQ(ss.Sink(), R"json("False")json");
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x52b000f3);
 
   ss.Reset();
+  ujson_crc32_reset(&uj);
   d = static_cast<fuzzy_bool>(75);
   EXPECT_TRUE(status_ok(ujson_serialize_fuzzy_bool(&uj, &d)));
   EXPECT_EQ(ss.Sink(), R"json(75)json");
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x876d76e8);
 }
 
 TEST(Derive, FuzzyBoolDeserialize) {
@@ -174,14 +198,19 @@ TEST(Derive, FuzzyBoolDeserialize) {
   ujson_t uj = ss.UJson();
   EXPECT_TRUE(status_ok(ujson_deserialize_fuzzy_bool(&uj, &d)));
   EXPECT_EQ(d, kFuzzyBoolFalse);
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x52b000f3);
 
+  ujson_crc32_reset(&uj);
   ss.Reset(R"json("True")json");
   EXPECT_TRUE(status_ok(ujson_deserialize_fuzzy_bool(&uj, &d)));
   EXPECT_EQ(d, kFuzzyBoolTrue);
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0x68d3703f);
 
+  ujson_crc32_reset(&uj);
   ss.Reset(R"json(35)json");
   EXPECT_TRUE(status_ok(ujson_deserialize_fuzzy_bool(&uj, &d)));
   EXPECT_EQ(d, static_cast<fuzzy_bool>(35));
+  EXPECT_EQ(ujson_crc32_finish(&uj), 0xe301b3ec);
 }
 
 }  // namespace
