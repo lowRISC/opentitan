a01d654cbf683e656ee329bab9e24593ae41e619
Harry Callahan <hcallahan@lowrisc.org>
2023-05-25 14:14:43 +0100

    [site] Add pagetoc js/css sources from upstream
    Symlink pagetoc & theme files to doc/guides/getting-started
     - This is to get around an issue where using a relative path to the
       additional files causes a strange breakage I don't understand. The files
       are deleted in-place, but no error is generated.
    
    Put the pagetoc in a wrapper div so we can use a flexbox
    
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>

---
+++ site/book-theme/pagetoc.js
@@ -0,0 +1,56 @@
+/* Copyright lowRISC contributors. */
+/* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+// Original Author
+// https://github.com/JorelAli/mdBook-pagetoc
+
+// Un-active everything when you click it
+Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el) {
+    el.addEventHandler("click", function() {
+        Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el) {
+            el.classList.remove("active");
+        });
+        el.classList.add("active");
+    });
+});
+
+var updateFunction = function() {
+
+    var id;
+    var elements = document.getElementsByClassName("header");
+    Array.prototype.forEach.call(elements, function(el) {
+        if (window.pageYOffset >= el.offsetTop) {
+            id = el;
+        }
+    });
+
+    Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el) {
+        el.classList.remove("active");
+    });
+    if (!id) return;
+    Array.prototype.forEach.call(document.getElementsByClassName("pagetoc")[0].children, function(el) {
+        if (id.href.localeCompare(el.href) == 0) {
+            el.classList.add("active");
+        }
+    });
+};
+
+// Populate sidebar on load
+window.addEventListener('load', function() {
+    var pagetoc = document.getElementsByClassName("pagetoc")[0];
+    var elements = document.getElementsByClassName("header");
+    Array.prototype.forEach.call(elements, function (el) {
+        var link = document.createElement("a");
+        link.appendChild(document.createTextNode(el.text));
+        link.href = el.href;
+        link.classList.add("pagetoc-" + el.parentElement.tagName);
+        pagetoc.appendChild(link);
+      });
+    updateFunction.call();
+});
+
+
+
+// Handle active elements on scroll
+window.addEventListener("scroll", updateFunction);
