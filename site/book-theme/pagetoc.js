cda3acf847a6bd45d796010e100f20f5c2d2d7dc
Harry Callahan <hcallahan@lowrisc.org>
2023-05-25 18:14:28 +0100

    [site] Add title element to pagetoc
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>

--- site/book-theme/pagetoc.js
+++ site/book-theme/pagetoc.js
@@ -36,21 +36,39 @@ var updateFunction = function() {
     });
 };
 
-// Populate sidebar on load
-window.addEventListener('load', function() {
-    var pagetoc = document.getElementsByClassName("pagetoc")[0];
-    var elements = document.getElementsByClassName("header");
-    Array.prototype.forEach.call(elements, function (el) {
+/* create_pagetoc_structure()
+ * Dynamically create a tree of <a> elements for each heading in
+ * the content body.
+ * Add the created structure within the 'pagetoc' element. */
+var create_pagetoc_structure = function(el_pagetoc) {
+    // Search the page for all <H*> elements
+    let headerElements = Array.from(document.getElementsByClassName("header"));
+
+    // Add title div to ToC
+    let title = document.createElement("div");
+    title.appendChild(document.createTextNode("Table of Contents"));
+    title.setAttribute("id", "pagetoc-title");
+    el_pagetoc.appendChild(title);
+
+    Array.prototype.forEach.call(headerElements, function (el) {
         var link = document.createElement("a");
         link.appendChild(document.createTextNode(el.text));
         link.href = el.href;
         link.classList.add("pagetoc-" + el.parentElement.tagName);
-        pagetoc.appendChild(link);
+        el_pagetoc.appendChild(link);
       });
     updateFunction.call();
-});
+};
 
 
 
 // Handle active elements on scroll
 window.addEventListener("scroll", updateFunction);
+
+/* Populate the pagetoc sidebar on load
+ * - Create a tree structure of elements within the pagetoc nav item
+ * - Update the overflow height behaviour when the rendered size is known. */
+window.addEventListener('load', function() {
+    let pagetoc = document.getElementsByClassName("pagetoc")[0];
+    create_pagetoc_structure(pagetoc);
+});
