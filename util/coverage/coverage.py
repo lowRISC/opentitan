0ce643b96a230435c3eea4d27ddd267e9c88ae4c
Alphan Ulusoy <alphan@opentitan.org>
2023-05-27 17:27:54 -0400

    [coverage] Add artifacts-relpath command
    This commit refactors existing code to expose artifacts-relpath command
    that prints the subdir <TIMESTAMP>-<HASH> to be used by helper scripts.
    
    Signed-off-by: Alphan Ulusoy <alphan@opentitan.org>

--- util/coverage/coverage.py
+++ util/coverage/coverage.py
@@ -11,22 +11,14 @@ Typical usage:
     util/coverage/coverage_off_target.py --print-text-report
 """
 
-from pathlib import (
-    Path,
-    PurePath,
-)
+from pathlib import Path, PurePath
 from typing import Dict
 
 import typer
 
-from common import (
-    CoverageParams,
-    CoverageType,
-    LogLevel,
-    measure_coverage,
-)
-from unittest_coverage import PARAMS as UNITTEST_PARAMS
+from common import CoverageParams, CoverageType, LogLevel, artifacts_relpath, measure_coverage
 from functest_coverage import PARAMS as FUNCTEST_PARAMS
+from unittest_coverage import PARAMS as UNITTEST_PARAMS
 
 app = typer.Typer(pretty_exceptions_enable=False, add_completion=False)
 
@@ -34,6 +26,13 @@ PARAMS: Dict[str, CoverageParams] = {
     CoverageType.UNITTEST: UNITTEST_PARAMS,
     CoverageType.FUNCTEST: FUNCTEST_PARAMS,
 }
+
+
+@app.command("artifacts-relpath")
+def artifacts_relpath_cmd() -> None:
+    """Prints the relative path for coverage artifacts.
+    """
+    print(artifacts_relpath())
 
 
 @app.command()
@@ -46,8 +45,8 @@ def measure(
     """Measures coverage of silicon creator code."""
     measure_coverage(
         log_level=log_level,
-        out_root_dir=out_root_dir,
-        out_sub_dir=PurePath(coverage_type.value),
+        out_dir=out_root_dir / artifacts_relpath() /
+        PurePath(coverage_type.value),
         print_text_report=print_text_report,
         **PARAMS[coverage_type]._asdict(),
     )
