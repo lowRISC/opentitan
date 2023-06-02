a59af840491469d948b56cd4d00ac8654536415b
Alphan Ulusoy <alphan@opentitan.org>
2023-06-02 14:50:57 -0400

    [sw/silicon_creator] Add 4 more _fake_ real RSA keys
    Signed-off-by: Alphan Ulusoy <alphan@opentitan.org>

---
+++ sw/device/silicon_creator/rom/keys/real/rsa/prod_key_2_rsa_3072_exp_f4.h
@@ -0,0 +1,39 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_KEYS_REAL_RSA_PROD_KEY_2_RSA_3072_EXP_F4_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_KEYS_REAL_RSA_PROD_KEY_2_RSA_3072_EXP_F4_H_
+
+#define PROD_KEY_2_RSA_3072_EXP_F4                                      \
+  {                                                                     \
+    .n =                                                                \
+        {{                                                              \
+            0x57a2fc91, 0x54d8a239, 0x3c8f8b47, 0x90bbf37c, 0xc9d86586, \
+            0x26b1cf31, 0xfa9400ac, 0x55b8b9e9, 0x747180b6, 0x1af9c537, \
+            0x63e046fd, 0xf406e427, 0x936bd3e5, 0x174684be, 0xc36fa56a, \
+            0x20f122d8, 0xa4224a7f, 0x3c05825d, 0x1ad191bf, 0x3ac4af2e, \
+            0x2475bda7, 0x368b9fdc, 0xcad9fbe0, 0xfbd4a4ac, 0x4fdc28ee, \
+            0xd2ccdeb8, 0x286982d4, 0xda282480, 0x4d0d14b1, 0x74b0cc88, \
+            0xca734170, 0x88d192c7, 0x4e44efae, 0x5334a0ec, 0x9e77f25a, \
+            0xd9802730, 0x494295dc, 0x4cc9009b, 0xcd61632e, 0xed781145, \
+            0xb5897309, 0xd46d9c8c, 0xd1a16079, 0x08ef201c, 0xa956d327, \
+            0x8a426a2e, 0xe7901017, 0x9901fc0c, 0x452198b5, 0x5bc9045d, \
+            0xab5b517a, 0x410880cb, 0xde87e857, 0x477f932d, 0x9021809d, \
+            0x50dbf04f, 0xa054f16c, 0xb7ae20ff, 0x6e5273d5, 0xd2c8f9cf, \
+            0x7558640c, 0xe54cf5de, 0x9eb034f5, 0xd2fee2a9, 0x1661a91f, \
+            0xbaea81d9, 0x4cc76063, 0xcba1fc46, 0x88516cf1, 0x9c3e7b69, \
+            0xfb1c33e6, 0x73efc6e8, 0x54138527, 0x34caa450, 0x1d9c7f1f, \
+            0x364b30ba, 0x8213fa38, 0x932030c8, 0xef43a7c0, 0xa34e6e82, \
+            0xa6d12908, 0xa87acb3b, 0x8f06d7ef, 0xe7888932, 0xc0b6bb17, \
+            0xbaa200f5, 0x47a5e990, 0x17294f11, 0x1dc618de, 0xa2f155f3, \
+            0x9272c82c, 0x5a26ff02, 0xe47fc8f6, 0xf9c608d8, 0xf05c3626, \
+            0xc7feb360,                                                 \
+        }},                                                             \
+    .n0_inv = {                                                         \
+        0x7d87bb8f, 0x64fa16ce, 0xb5452c9a, 0x939b883c,                 \
+        0x1cc1da2e, 0x61ea7f94, 0x3a4908ad, 0x36ea9850,                 \
+    },                                                                  \
+  }
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_KEYS_REAL_RSA_PROD_KEY_2_RSA_3072_EXP_F4_H_
