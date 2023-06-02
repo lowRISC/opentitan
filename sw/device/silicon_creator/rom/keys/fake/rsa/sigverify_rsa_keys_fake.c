d8aad8838989125a28dd7a6dcb9e6751c6206b37
Alphan Ulusoy <alphan@opentitan.org>
2023-06-02 14:38:43 -0400

    [sw/silicon_creator] Add 4 more fake RSA keys
    This commit adds 4 more fake RSA keys so that the total number matches
    the number of keys we plan to include in production ROM.
    
    Signed-off-by: Alphan Ulusoy <alphan@opentitan.org>

--- sw/device/silicon_creator/rom/keys/fake/rsa/sigverify_rsa_keys_fake.c
+++ sw/device/silicon_creator/rom/keys/fake/rsa/sigverify_rsa_keys_fake.c
@@ -4,8 +4,12 @@
 
 #include "sw/device/lib/base/macros.h"
 #include "sw/device/silicon_creator/rom/keys/fake/rsa/dev_key_0_rsa_3072_exp_f4.h"
+#include "sw/device/silicon_creator/rom/keys/fake/rsa/dev_key_1_rsa_3072_exp_f4.h"
 #include "sw/device/silicon_creator/rom/keys/fake/rsa/prod_key_0_rsa_3072_exp_f4.h"
+#include "sw/device/silicon_creator/rom/keys/fake/rsa/prod_key_1_rsa_3072_exp_f4.h"
+#include "sw/device/silicon_creator/rom/keys/fake/rsa/prod_key_2_rsa_3072_exp_f4.h"
 #include "sw/device/silicon_creator/rom/keys/fake/rsa/test_key_0_rsa_3072_exp_f4.h"
+#include "sw/device/silicon_creator/rom/keys/fake/rsa/test_key_1_rsa_3072_exp_f4.h"
 #include "sw/device/silicon_creator/rom/sigverify_keys_rsa.h"
 
 #include "otp_ctrl_regs.h"
@@ -14,7 +18,7 @@
  * Number of RSA public keys.
  */
 enum {
-  kSigverifyRsaKeysCnt_ = 3,
+  kSigverifyRsaKeysCnt_ = 7,
 };
 const size_t kSigverifyRsaKeysCnt = kSigverifyRsaKeysCnt_;
 
@@ -24,7 +28,7 @@ const size_t kSigverifyRsaKeysCnt = kSigverifyRsaKeysCnt_;
  * This must be coprime with and less than `kSigverifyNumRsaKeys`.
  * Note: Step size is not applicable when `kSigverifyNumRsaKeys` is 1.
  */
-const size_t kSigverifyRsaKeysStep = 1;
+const size_t kSigverifyRsaKeysStep = 5;
 
 /**
  * Fake public keys for signature verification in tests.
@@ -43,7 +47,21 @@ const sigverify_rom_rsa_key_t kSigverifyRsaKeys[kSigverifyRsaKeysCnt_] = {
     {
         .entry =
             {
+                .key = TEST_KEY_1_RSA_3072_EXP_F4,
+                .key_type = kSigverifyKeyTypeTest,
+            },
+    },
+    {
+        .entry =
+            {
                 .key = DEV_KEY_0_RSA_3072_EXP_F4,
+                .key_type = kSigverifyKeyTypeDev,
+            },
+    },
+    {
+        .entry =
+            {
+                .key = DEV_KEY_1_RSA_3072_EXP_F4,
                 .key_type = kSigverifyKeyTypeDev,
             },
     },
@@ -54,9 +72,23 @@ const sigverify_rom_rsa_key_t kSigverifyRsaKeys[kSigverifyRsaKeysCnt_] = {
                 .key_type = kSigverifyKeyTypeProd,
             },
     },
+    {
+        .entry =
+            {
+                .key = PROD_KEY_1_RSA_3072_EXP_F4,
+                .key_type = kSigverifyKeyTypeProd,
+            },
+    },
+    {
+        .entry =
+            {
+                .key = PROD_KEY_2_RSA_3072_EXP_F4,
+                .key_type = kSigverifyKeyTypeProd,
+            },
+    },
 };
 
 static_assert(OTP_CTRL_PARAM_CREATOR_SW_CFG_SIGVERIFY_RSA_KEY_EN_SIZE >=
                   kSigverifyRsaKeysCnt_,
               "CREATOR_SW_CFG_SIGVERIFY_RSA_KEY_EN OTP item must be at least "
-              "`kSigVerifyNumKeysFake` bytes.");
+              "`kSigVerifyRsaKeysCnt` bytes.");
