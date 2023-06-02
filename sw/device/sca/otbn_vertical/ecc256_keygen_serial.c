b5808ae0c1c7c19a8e324d1eba7f6d5e4111b381
Vladimir Rozic <vrozic@lowrisc.org>
2023-06-02 17:48:56 +0100

    [sca] Add batch capture for ECC256 keygen
    Adds batch capture for ECC256 keygen TVLA fixed-vs-random key test.
    
    Signed-off-by: Vladimir Rozic <vrozic@lowrisc.org>

--- sw/device/sca/otbn_vertical/ecc256_keygen_serial.c
+++ sw/device/sca/otbn_vertical/ecc256_keygen_serial.c
@@ -123,6 +123,21 @@ uint32_t ecc256_seed[kEcc256SeedNumWords] = {
     0x22b79d5f, 0x1176f31d, 0xb5ac3a51, 0x99a082d7, 0x484eb366,
 };
 
+uint32_t ecc256_C[kEcc256SeedNumWords] = {
+    0x016064e9, 0x11e3f4d6, 0xac3a6fa7, 0xaba11a1b, 0x8f9271d1,
+    0x22b79d5f, 0x1176f31d, 0xb5ac3a51, 0x99a082d7, 0x484eb366,
+};
+
+uint32_t random_number[kEcc256CoordNumWords] = {
+    0x016064e9, 0x11e3f4d6, 0xac3a6fa7, 0xaba11a1b,
+    0x22b79d5f, 0x1176f31d, 0xb5ac3a51, 0x99a082d7,
+};
+
+uint32_t ecc256_fixed_number[kEcc256CoordNumWords] = {
+    0x04030201, 0x00000000, 0x00000000, 0x00000000,
+    0x00000000, 0x00000000, 0x00000000, 0x00000000,
+};
+
 void ecc256_en_masks(const uint8_t *enable, size_t enable_len) {
   SS_CHECK(enable_len == 1);
   if (*enable) {
@@ -132,6 +147,14 @@ void ecc256_en_masks(const uint8_t *enable, size_t enable_len) {
   }
 }
 
+/**
+ * Simple serial 'x' (set seed) command handler.
+ *
+ * The seed must be `kEcc256SeedNumBytes` bytes long.
+ *
+ * @param seed Value for seed share.
+ * @param seed_len Length of seed share.
+ */
 void ecc256_set_seed(const uint8_t *seed, size_t seed_len) {
   SS_CHECK(seed_len == kEcc256SeedNumBytes);
   memcpy(ecc256_seed, seed, seed_len);
@@ -139,6 +162,19 @@ void ecc256_set_seed(const uint8_t *seed, size_t seed_len) {
   // generate the fixed seed.
   uint32_t temp[kEcc256SeedNumWords];
   prng_rand_bytes((unsigned char *)temp, kEcc256SeedNumBytes);
+}
+
+/**
+ * Simple serial 'c' (set constant) command handler.
+ *
+ * The constant must be `kEcc256SeedNumBytes` bytes long.
+ *
+ * @param C Value of the C constant.
+ * @param len Length of the C constant.
+ */
+void ecc256_set_c(const uint8_t *C, size_t len) {
+  SS_CHECK(len == kEcc256SeedNumBytes);
+  memcpy(ecc256_C, C, len);
 }
 
 /**
@@ -173,7 +209,7 @@ static void p256_run_keygen(uint32_t mode, const uint32_t *share0,
   sca_set_trigger_low();
 }
 
-void ecc256_ecdsa_secret_keygen_batch(const uint8_t *data, size_t data_len) {
+void ecc256_ecdsa_keygen_fvsr_seed_batch(const uint8_t *data, size_t data_len) {
   uint32_t num_traces = 0;
   uint32_t batch_digest[kEcc256SeedNumWords];
   uint8_t dummy[kEcc256SeedNumBytes];
@@ -229,6 +265,103 @@ void ecc256_ecdsa_secret_keygen_batch(const uint8_t *data, size_t data_len) {
     }
   }
 
+  // Send the batch digest to the host for verification.
+  simple_serial_send_packet('r', (uint8_t *)batch_digest,
+                            kEcc256SeedNumWords * 4);
+}
+
+/**
+ * Adds two integers storred in byte arrays.
+ *
+ * Adds the integer stored in source array to the integer stored in
+ * destination aray.
+ * The user needs to ensure that dest_len isenough to store the result
+ * without overflow.
+ *
+ * @param[in] dest  Location of the first input array and the result.
+ * @param[in] source  Location of the second input array.
+ * @param[in] dest_len   Length od the dest array in bytes.
+ * @param[in] source_len   Length of the source array in bytes.
+ */
+static void add_arrays(uint8_t *dest, uint8_t *source, size_t dest_len,
+                       size_t source_len) {
+  uint16_t temp = 0;
+
+  for (size_t i = 0; i < source_len; i++) {
+    temp += (uint16_t)source[i] + dest[i];
+    dest[i] = (uint8_t)(temp & 0x00FF);
+    temp >>= 8;
+  }
+
+  for (size_t i = source_len; i < dest_len; i++) {
+    temp += (uint16_t)dest[i];
+    dest[i] = (uint8_t)(temp & 0x00FF);
+    temp >>= 8;
+  }
+}
+
+void ecc256_ecdsa_keygen_fvsr_key_batch(const uint8_t *data, size_t data_len) {
+  uint32_t num_traces = 0;
+  uint32_t batch_digest[kEcc256SeedNumWords];
+  uint8_t dummy[kEcc256SeedNumBytes];
+  SS_CHECK(data_len == sizeof(num_traces));
+  num_traces = read_32(data);
+
+  if (num_traces > kNumBatchOpsMax) {
+    LOG_ERROR("Too many traces for one batch.");
+    return;
+  }
+
+  // zero the batch digest
+  for (uint32_t j = 0; j < kEcc256SeedNumWords; ++j) {
+    batch_digest[j] = 0;
+  }
+
+  for (uint32_t i = 0; i < num_traces; ++i) {
+    if (run_fixed) {
+      memcpy(batch_share0[i], ecc256_seed, kEcc256SeedNumBytes);
+    } else {
+      // Here change to random_number + C
+      // It is necessary to set the C first
+      memcpy(batch_share0[i], ecc256_C, kEcc256SeedNumBytes);
+      prng_rand_bytes((unsigned char *)random_number, kEcc256CoordNumBytes);
+      add_arrays((unsigned char *)batch_share0[i],
+                 (unsigned char *)random_number, kEcc256SeedNumBytes,
+                 kEcc256CoordNumBytes);
+    }
+    if (en_masks) {
+      prng_rand_bytes((unsigned char *)batch_share1[i], kEcc256SeedNumBytes);
+    } else {
+      for (uint32_t j = 0; j < kEcc256SeedNumWords; ++j) {
+        batch_share1[i][j] = 0;
+      }
+    }
+    for (uint32_t j = 0; j < kEcc256SeedNumWords; ++j) {
+      batch_share0[i][j] ^= batch_share1[i][j];
+    }
+    // Another PRNG run to determine 'run_fixed' for the next cycle.
+    prng_rand_bytes(dummy, kEcc256SeedNumBytes);
+
+    run_fixed = dummy[0] & 0x1;
+  }
+
+  for (uint32_t i = 0; i < num_traces; ++i) {
+    p256_run_keygen(kEcc256ModePrivateKeyOnly, batch_share0[i],
+                    batch_share1[i]);
+
+    // Read results.
+    SS_CHECK_STATUS_OK(
+        otbn_dmem_read(kEcc256SeedNumWords, kOtbnVarD0, d0_batch));
+    SS_CHECK_STATUS_OK(
+        otbn_dmem_read(kEcc256SeedNumWords, kOtbnVarD1, d1_batch));
+
+    // The correctness of each batch is verified by computing and sending
+    // the batch digest. This digest is computed by XORing all d0 shares of
+    // the batch.
+    for (uint32_t j = 0; j < kEcc256SeedNumWords; ++j) {
+      batch_digest[j] ^= d0_batch[j];
+    }
+  }
   // Send the batch digest to the host for verification.
   simple_serial_send_packet('r', (uint8_t *)batch_digest,
                             kEcc256SeedNumWords * 4);
