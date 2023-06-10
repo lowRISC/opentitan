9fc3a57c753085d48f55f32b1b4f50369049fd5c
Harry Callahan <hcallahan@lowrisc.org>
2023-06-10 16:15:08 +0100

    [spi_host,dv] Refactor status_stall_vseq and scb, add more comments
    This commit is cleanups and refactoring to make the test and scoreboard more
    linear. It also adds a number of comments as I went through learning how it
    works, and refactors names to be more meaningful.
    
    The status_stall_vseq test has been a source of failures for a long time. This
    commit refactors it in an attempt to make it easier to debug, and tries to
    address some of the outstanding issues.
    Problems experienced by this test have been 1) timeouts, due to requiring lots of
    stimulus to be generated to fill the RXFIFO and induce a rxstall condition, and
    2) the scoreboard not checking all of the expected/observed transaction items
    due to this test exercising a different codepath in the scoreboard by using
    cfg.tx_stall_check.
    
    - Increase timeout for spi_host_status_stall test
    - Schedule a smoketest sequence after the main test to check recovery, and make
      use of read_rx_fifo() after this to ensure the scb is up-to-date.
    
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>

--- hw/ip/spi_host/dv/env/seq_lib/spi_host_status_stall_vseq.sv
+++ hw/ip/spi_host/dv/env/seq_lib/spi_host_status_stall_vseq.sv
@@ -2,9 +2,13 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// error_cmd test vseq
-// test tries to capture error interrupt when cmd invalid condition appears
-// cmd invalid is created when cmd sent and host isnt ready
+// Force the DUT to stall on both a rx and a tx transaction.
+// This occurs when the RXFIFO and TXFIFO are full/empty respectively, and we
+// try to continue a transaction.
+// First create read transactions, without emptying the fifo, until we see rxfull
+// and rxstall.
+// Then, create a write transaction without adding data to the txfifo, and check
+// for txempty and txstall.
 class spi_host_status_stall_vseq extends spi_host_tx_rx_vseq;
   `uvm_object_utils(spi_host_status_stall_vseq)
   `uvm_object_new
@@ -13,23 +17,31 @@ class spi_host_status_stall_vseq extends spi_host_tx_rx_vseq;
   spi_host_command_t command_q[$];
   spi_segment_item segment_q[$];
 
+  virtual task pre_start();
+    // Force more segments so we can fill the RXFIFO faster.
+    cfg.seq_cfg.host_spi_min_num_seg = 4;
+    cfg.seq_cfg.host_spi_max_num_seg = 8;
+    super.pre_start();
+  endtask : pre_start;
+
   virtual task body();
     spi_host_command_t command_snd;
-    spi_segment_item segment_snd;
     bit [7:0] read_q[$];
 
+    // Generate read transactions without accesing RXFIFO until we get an rxstall
     begin : isolation_fork
-      fork
-        start_agent_reactive_seqs();
-      join_none
-
+      // Start the agent sequences to provide (random) response data to our reads.
+      fork start_agent_reactive_seqs(); join_none
       begin
         program_spi_host_regs();
         while (rxqd < SPI_HOST_RX_DEPTH) begin
-          spi_rd_trans();
-          if (rxqd == SPI_HOST_RX_DEPTH) begin
-            break;
-          end else begin
+          wait_ready_for_command();
+          generate_stall_transaction();
+          rd_trans(transaction);
+          if (rxqd == SPI_HOST_RX_DEPTH) break;
+          else begin
+            // If we have not yet filled the RXFIFO, confirm that the DUT does
+            // not indicate rxfull or rxstall.
             check_event(ral.status.rxfull, 0, 0);
             csr_rd_check(.ptr(ral.status.rxstall), .compare_value(1'b0));
           end
@@ -37,82 +49,83 @@ class spi_host_status_stall_vseq extends spi_host_tx_rx_vseq;
       end
       disable fork;
     end : isolation_fork
+    // Check that rxfull and rxstall events are set now that the
+    // RXQD has indicated we are full.
     csr_spinwait(.ptr(ral.status.rxfull), .exp_data(1'b1));
     csr_spinwait(.ptr(ral.status.rxstall), .exp_data(1'b1));
-    for (int i = 0; i < SPI_HOST_RX_DEPTH; i++) begin
-      access_data_fifo(read_q, RxFifo);
-    end
-    // send transaction to check tx stall
-    cfg.tx_stall_check = 1'b1;
-    wr_trans();
-    command_snd = command_q[0];
-    spi_wr_trans_cmd(command_snd);
-    csr_spinwait(.ptr(ral.status.txstall), .exp_data(1'b1));
-    segment_snd = segment_q[0];
-    spi_wr_trans_seg(segment_snd);
-    csr_spinwait(.ptr(ral.status.active), .exp_data(1'b0));
-    csr_spinwait(.ptr(ral.status.txqd), .exp_data(0));
-    cfg.clk_rst_vif.wait_clks(100);
+    // Check complete, clear all data from the RXFIFO, allowing the txn to complete.
     read_rx_fifo();
+
+    // Now, send a new write transaction, without any data in TXFIFO, to check txstall
+    begin : create_txstall_txempty
+      spi_segment_item segment_snd;
+      cfg.tx_stall_check = 1'b1;
+
+      generate_stall_transaction(.readonly(0));
+      $cast(segment_snd, transaction.segments[$].clone());
+      wait_ready_for_command();
+      program_command_reg(segment_snd.command_reg);
+      // Now wait until the DUT asserts it's stall output.
+      csr_spinwait(.ptr(ral.status.txstall), .exp_data(1'b1));
+      // Now put some data into the TXFIFO, allowing the command to complete.
+      access_data_fifo(segment_snd.spi_data, TxFifo);
+      csr_spinwait(.ptr(ral.status.cmdqd), .exp_data(0));
+      csr_spinwait(.ptr(ral.status.active), .exp_data(0));
+      cfg.tx_stall_check = 1'b0;
+    end : create_txstall_txempty
+
+    begin : test_recovery_with_smoketest
+      uvm_sequence seq = create_seq_by_name("spi_host_smoke_vseq");
+      seq.set_sequencer(p_sequencer);
+      cfg.seq_cfg.host_spi_min_trans = 1;
+      cfg.seq_cfg.host_spi_max_trans = 2;
+      `DV_CHECK_RANDOMIZE_FATAL(seq)
+      seq.start(p_sequencer);
+    end
+
+    cfg.clk_rst_vif.wait_clks(1000);
+    csr_spinwait(.ptr(ral.status.active), .exp_data(0));
+    // If needed, read out any remaining RXFIFO data for the scb before ending.
+    begin : clear_rxfifo
+      spi_host_status_t status;
+      csr_rd(.ptr(ral.status), .value(status));
+      if (status.rx_qd != 0) read_rx_fifo();
+    end
+    cfg.clk_rst_vif.wait_clks(1000);
 
   endtask : body
 
-  virtual task spi_rd_trans(bit wait_ready = 1'b1);
-    if (wait_ready) wait_ready_for_command();
-    generate_transaction_stall();
-    rd_trans(transaction, wait_ready);
-  endtask
-
+  // Drive all segments of the read transaction. Return when we run out of
+  // segments, or the RXQD indicates we are full.
   virtual task rd_trans(spi_transaction_item trans, bit wait_ready = 1'b1);
     spi_segment_item segment;
     while ((trans.segments.size() > 0)  && (!(rxqd == SPI_HOST_RX_DEPTH))) begin
-      // wait on DUT ready
-      segment = trans.segments.pop_back();
       if (wait_ready) wait_ready_for_command();
-      // lock fifo to this seq
-      spi_host_atomic.get(1);
-      // write data to fifo
-      if (segment.command_reg.direction != RxOnly) begin
-        access_data_fifo(segment.spi_data, TxFifo);
-      end
-      program_command_reg(segment.command_reg);
-      spi_host_atomic.put(1);
+
+      // Lock fifo to this task, write any data to TXFIFO if required, write COMMAND csr starts txn.
+      spi_host_atomic.get(1); begin : locked_fifo_access
+        segment = trans.segments.pop_back();
+        if (segment.command_reg.direction != RxOnly) begin
+          access_data_fifo(segment.spi_data, TxFifo);
+        end
+        program_command_reg(segment.command_reg);
+      end spi_host_atomic.put(1);
+
       csr_rd(.ptr(ral.status.rxqd), .value(rxqd));
     end
   endtask : rd_trans
 
-  virtual task generate_transaction_stall(bit readonly = 1'b1);
+  // Randomize the transaction item to allow us to create stalls.
+  virtual task generate_stall_transaction(bit readonly = 1'b1);
     transaction_init();
     if (readonly) begin
       transaction.tx_only_weight = 0;
       `DV_CHECK_RANDOMIZE_WITH_FATAL(transaction,
-         num_segments == 1;cmd == ReadStd;)
+         cmd == ReadStd;)
     end else begin
       `DV_CHECK_RANDOMIZE_WITH_FATAL(transaction,
-         num_segments == 1;cmd == WriteStd;)
+         cmd == WriteStd;)
     end
-  endtask
-
-  virtual task wr_trans();
-    spi_segment_item segment;
-    generate_transaction_stall(.readonly(0));
-    while (transaction.segments.size() > 0) begin
-      segment = transaction.segments.pop_back();
-      wait_ready_for_command();
-      command_q.push_back(segment.command_reg);
-      segment_q.push_back(segment);
-    end
-  endtask : wr_trans
-
-  virtual task spi_wr_trans_cmd(spi_host_command_t command_snd);
-    wait_ready_for_command();
-    program_command_reg(command_snd);
-  endtask
-
-  virtual task spi_wr_trans_seg(spi_segment_item segment_snd);
-    if (segment_snd.command_reg.direction != RxOnly) begin
-      access_data_fifo(segment_snd.spi_data, TxFifo);
-    end
-  endtask
+  endtask : generate_stall_transaction
 
 endclass : spi_host_status_stall_vseq
