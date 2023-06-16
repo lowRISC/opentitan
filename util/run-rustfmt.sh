66f931b072d38e59efd2b7d6f4d9a4988056d673
Gary Guo <gary.guo@lowrisc.org>
2023-06-16 14:17:08 +0100

    [ci] remove out-of-Bazel Rust installation in CI
    There is currently a discrepancy between Rust version in
    tool_requirements.py and CI (1.60) and in Bazel (1.67).
    
    Previously the out-of-Bazel Rust toolchain is used only for rustfmt,
    and now it is also managed by Bazel, so we should be able to remove
    this.
    
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

--- util/run-rustfmt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Copyright lowRISC contributors.
-# Licensed under the Apache License, Version 2.0, see LICENSE for details.
-# SPDX-License-Identifier: Apache-2.0
-
-find sw hw \
-    -name '*.rs' \
-    -exec rustfmt --check {} \;
-
