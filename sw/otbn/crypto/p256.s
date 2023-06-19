34963086f1eb21275d9b6258ec6cda724f4b1671
Moritz Wettermann <moritz.wettermann@gi-de.com>
2023-06-19 11:26:44 +0200

    [sw/otbn/crypto] Add arithmetic to boolean masking for P256 ECDH
    This commit modifies the p256_scalar_mult function so that it returns
    an arithmetically masked result. The arithmetic masking is done before
    converting back to affine coordinates. For this the internal
    scalar_mult_int function is changed to return projective coordinates.
    The transformation to affine coordinates now needs to be done outside
    scalar_mult_int.
    
    This commit also adds code to convert from arithmetic masking to
    boolean masking. The conversion is implemented following Gaubin's
    algorithm to prevent side-channel leakage. The conversion is
    implemented in a 257-bit domain to enable carry bit handling,
    which is necessary when the arithmetic masking is performed
    within a mod p domain.
    
    Both described changes are then put together to generate the P256
    ECDH shared session key.
    
    Further, this commit also adds the relevant tests.
    
    Signed-off-by: Moritz Wettermann <moritz.wettermann@gi-de.com>

--- sw/otbn/crypto/p256.s
+++ sw/otbn/crypto/p256.s
@@ -20,6 +20,8 @@
 .globl proj_add
 .globl mod_inv
 .globl mod_mul_320x128
+.globl arithmetic_to_boolean
+.globl arithmetic_to_boolean_mod
 
 .text
 
@@ -665,6 +667,7 @@ proj_add:
  * @param[in]  MOD: p, modulus of the finite field of P-256
  * @param[out]  w11: x_a, x-coordinate of curve point (affine)
  * @param[out]  w12: y_a, y-coordinate of curve point (affine)
+ * @param[out]  w14: z^-1, modular inverse of the projective z-coordinate
  *
  * clobbered registers: w10 to w19, w24, w25
  * clobbered flag groups: FG0
@@ -1066,8 +1069,9 @@ proj_double:
  * @param[in]  w27: b, curve domain parameter
  * @param[in]  w31: all-zero
  * @param[in]  MOD: p, modulus, 2^256 > p > 2^255.
- * @param[out]  w11: x_r, affine x-coordinate of resulting point
- * @param[out]  w12: y_r, affine y-coordinate of resulting point
+ * @param[out]  w8: x, x-coordinate of curve point (projective)
+ * @param[out]  w9: y, y-coordinate of curve point (projective)
+ * @param[out]  w10: z, z-coordinate of curve point (projective)
  *
  * Flags: When leaving this subroutine, the M, L and Z flags of FG0 depend on
  *        the computed affine y-coordinate.
@@ -1225,10 +1229,6 @@ scalar_mult_int:
   bn.cmp    w10, w31
   jal       x1, trigger_fault_if_fg0_z
 
-  /* convert back to affine coordinates
-     R = (x_a, y_a) = (w11, w12) */
-  jal       x1, proj_to_affine
-
   ret
 
 
@@ -1314,11 +1314,15 @@ p256_sign:
   la        x3, p256_u_n
   bn.lid    x2, 0(x3)
 
-  /* scalar multiplication with base point
-     (x_1, y_1) = (w11, w12) <= k*G = w0*(dmem[p256_gx], dmem[p256_gy]) */
+  /* scalar multiplication with base point (projective)
+     (x_1, y_1, z_1) = (w8, w9, w10) <= k*G = w0*(dmem[p256_gx], dmem[p256_gy]) */
   la        x21, p256_gx
   la        x22, p256_gy
   jal       x1, scalar_mult_int
+
+  /* Convert masked result back to affine coordinates.
+     R = (x_a, y_a) = (w11, w12) */
+  jal       x1, proj_to_affine
 
   /* setup modulus n (curve order) and Barrett constant
      MOD <= w29 <= n = dmem[p256_n]; w28 <= u_n = dmem[p256_u_n]  */
@@ -1514,10 +1518,14 @@ p256_base_mult:
   bn.lid    x2, 0(x16)
 
   /* call internal scalar multiplication routine
-     R = (x_a, y_a) = (w11, w12) <= d*P = (w0 + w1)*P */
+     R = (x_p, y_p, z_p) = (w8, w9, w10) <= d*P = (w0 + w1)*P */
   la        x21, p256_gx
   la        x22, p256_gy
   jal       x1, scalar_mult_int
+
+  /* Convert masked result back to affine coordinates.
+     R = (x_a, y_a) = (w11, w12) */
+  jal       x1, proj_to_affine
 
   /* store result (affine coordinates) in dmem
      dmem[x] <= x_a = w11
@@ -1907,9 +1915,10 @@ p256_verify:
 /**
  * Externally callable wrapper for P-256 scalar point multiplication
  *
- * returns R = k*P = k*(x_p, y_p, z_p)
- *         with R, P being valid P-256 curve points in projective form,
- *              k being a 256 bit scalar.
+ * returns R = k*P = k*(x_a, y_a)
+ *         with R, P being valid P-256 curve points in affine form,
+ *         k being a 256 bit scalar. The x coordinate of R is
+ *         arithmetically masked.
  *
  * This routine assumes that the scalar k is provided in two shares, k0 and k1,
  * where:
@@ -1920,8 +1929,10 @@ p256_verify:
  *
  * @param[in]      dmem[k0]:  first share of scalar k (256 bits)
  * @param[in]      dmem[k1]:  second share of scalar k (256 bits)
- * @param[in,out]  dmem[x]:   affine x-coordinate in dmem
- * @param[in,out]  dmem[y]:   affine y-coordinate in dmem
+ * @param[in]      dmem[x]:   affine x-coordinate in dmem
+ * @param[in]      dmem[y]:   affine y-coordinate in dmem
+ * @param[out]     dmem[x]:   affine arithmetically masked x-coordinate in dmem
+ * @param[out]     dmem[y]:   arithmetic mask for x coordinate in dmem
  *
  * Flags: When leaving this subroutine, the M, L and Z flags of FG0 depend on
  *        the computed affine y-coordinate.
@@ -1942,7 +1953,7 @@ p256_scalar_mult:
   li        x2, 1
   bn.lid    x2, 0(x16)
 
-  /* Load second share of secret key d from dmem.
+  /* Load second share of secret key k from dmem.
        w2,w3 = dmem[k1] */
   la        x16, k1
   li        x2, 2
@@ -1950,17 +1961,78 @@ p256_scalar_mult:
   li        x2, 3
   bn.lid    x2, 0(x16)
 
-  /* call internal scalar multiplication routine
-     R = (x_a, y_a) = (w11, w12) <= k*P = w0*P */
+  /* Call internal scalar multiplication routine.
+     Returns point in projective coordinates.
+     R = (x, y, z) = (w8, w9, w10) <= k*P = w0*P */
   la        x21, x
   la        x22, y
   jal       x1, scalar_mult_int
 
-  /* store result (affine coordinates) in dmem
-     dmem[x] <= x_a = w11
-     dmem[y] <= y_a = w12 */
+  /* Arithmetic masking:
+   1. Generate a random mask
+   2. Subtract masks from projective x coordinate
+      (x, y, z) -> ((x - m) mod p,
+                     y,
+                     z)
+   3. Convert masked curve point back to affine
+      form.
+   4. Multiply mask with z^-1 for use in
+      affine space. */
+
+  /* Fetch a fresh random number as mask.
+       w2 <= URND() */
+  bn.wsrr   w2, 0x2 /* URND */
+
+  /* Subtract random mask from x coordinate of
+     projective point.
+     The addition has to be done within the underlying
+     finite field -> mod p.
+     w8 = (w8 - w2) mod p */
+  bn.subm    w8, w8, w2
+
+  /* Convert masked result back to affine coordinates.
+     R = (x_a, y_a) = (w11, w12) */
+  jal       x1, proj_to_affine
+
+  /* Store result (masked affine x-coordinate) in DMEM.
+     Y-coordinate not needed, will be overwritten with
+     mask value below.
+     dmem[x] <= x_a = w11 */
   li        x2, 11
-  bn.sid    x2++, 0(x21)
+  bn.sid    x2, 0(x21)
+
+  /* Get modular inverse z^-1 of projective z coordinate
+     and multiply the random masks with z^-1 to
+     also convert them into affine space. */
+
+  /* Load barrett constant as input for
+     mod_mul_256x256.
+     w28 = dmem[p256_u_p] */
+  li        x2, 28
+  la        x4, p256_u_p
+  bn.lid    x2, 0(x4)
+
+  /* Get field modulus p.
+     w29 <= MOD() */
+  bn.wsrr   w29, 0x00 /* MOD */
+
+  /* Move z^-1 and x coordinate mask to
+     mod_mul_256x256 input WDRs.
+     z^-1 is still stored in w14 from previous
+     proj_to_affine call.
+     w25 <= w14 = z^-1
+     w24 <= w2 = m_x */
+  bn.mov    w25, w14
+  bn.mov    w24, w2
+
+  /* Compute modular multiplication of m_x and z^-1.
+     w19 = w24 * w25 mod w29 = m_x * z^-1 mod p */
+  jal       x1, mod_mul_256x256
+
+  /* Store "affine" mask to DMEM. Use the y-coordinate
+     to save memory (not needed afterwards)
+     dmem[y] <= w19 = m_x * z^-1 mod p */
+  li        x2, 19
   bn.sid    x2, 0(x22)
 
   ret
@@ -2271,6 +2343,233 @@ boolean_to_arithmetic:
   /* remove fresh mask */
   bn.xor    w20, w28, w20
   bn.xor    w21, w29, w21
+
+  ret
+
+/**
+ * Converts arithmetic shares mod p to boolean shares.
+ *
+ * Calls the 257-bit A2B function twice, first using unmodified 256-bit shares
+ * in reduced form, and then using modified 257-bit shares in unreduced form.
+ *
+ * It then checks if the MSB (carry bit) is true or false, to decide
+ * which of the two A2B results is used. This detects and handles an
+ * underflow during the subtraction of arithmetic masking.
+ *
+ * The logic behind the carry bit handling is as follows:
+ * If x >= r, then  A = (x - r) mod p = x - r exactly.
+ * So when we add 2^257 - p and then add A and x, we get
+ * (2^257 - p + x - r + r) mod 2^257 = 2^257 - p + x.
+ * In this case, the high bit is always true since p - x <= p < 2^256,
+ * so we choose the A2B conversion without the 2^257 - p added.
+ * On the other hand, if x < r, then A = (x - r) mod p = x - r + p.
+ * When we add 2^257 - p and then add A and x, we get
+ * or (2^257 - p + x - r + p + r) mod 2^257 = (2^257 + x) mod 2^257 = x.
+ * In this case, the high bit is always false since x < p < 2^256, so we
+ * choose this second A2B conversion.
+ *
+ * This routine runs in constant time.
+ *
+ * Flags: Flags have no meaning beyond the scope of this subroutine.
+ *
+ * @param[in]  w31: all-zero wide data register
+ * @param[in]  w19: mask r
+ * @param[in]  w11: arithmetically masked value A, such that x = A + r
+ * @param[out] w20: boolean masked value x', such that x = x' ^ r
+ *
+ * clobbered registers: w1 to w6, w11, w12, w18, w20 to w27, and w29
+ * clobbered flag groups: FG0
+ */
+arithmetic_to_boolean_mod:
+  /* First step: calculate A2B from reduced values. */
+
+  /* Save inputs for second A2B execution.
+     w24 <= w19 = r
+     w25 <= w11 = A */
+  bn.mov    w24, w19
+  bn.mov    w25, w11
+
+  /* Expand inputs r and A (w19 and w11) to 257-bit values [w19,w18]
+     and [w12,w11] and prepare input for 257-bit A2B function.
+     w18 <= w19
+     w19 <= w31
+     w11 <= w11 -> obsolete
+     w12 <= w31 */
+  bn.mov    w18, w19
+  bn.mov    w19, w31
+  bn.mov    w12, w31
+
+  /* Call 257-bit A2B function.
+     [w21,w20] <= x' */
+  jal       x1, arithmetic_to_boolean
+
+  /* Save intermediate result of reduced inputs.
+     w26 <= w20 = x' (lower part)
+     w27 <= w21 = x' (upper part) */
+  bn.mov    w26, w20
+  bn.mov    w27, w21
+
+  /* Second step: calculate A2B from unreduced values. */
+
+  /* Restore and expand inputs r and A (w19 and w11) to 257-bit
+     values [w19,w18] and [w12,w11] and prepare input for
+     257-bit A2B function.
+     w18 <= w24
+     w19 <= w31
+     w11 <= w25
+     w12 <= w31 */
+  bn.mov    w18, w24
+  bn.mov    w19, w31
+  bn.mov    w11, w25
+  bn.mov    w12, w31
+
+  /* Get field modulus p.
+     w29 <= MOD() */
+  bn.wsrr   w29, 0x00 /* MOD */
+
+  /* Convert input A ([w12,w11]) to an unreduced value
+     in the 2^257 domain. For this add (2^257 - p) to A.
+     [w12,w11] <= [w12,w11] + 2^257 - w29 = A + 2^257 - p
+     w12 <= w12 + 0x2 = A + 2^257
+            -> equal to addition of 2^257
+               (w11 doesn't need to be touched)
+     [w12,w11] <= [w12,w11] - w29 = (A + 2^257) - p */
+  bn.addi   w12, w12, 0x2
+  bn.sub    w11, w11, w29
+  bn.subb   w12, w12, w31
+
+  /* Call 257-bit A2B function.
+     [w21,w20] <= x' */
+  jal       x1, arithmetic_to_boolean
+
+  /* Restore initial mask input of w19 for consistency
+     in calling functions.
+     w19 <= w24 */
+  bn.mov    w19, w24
+
+  /* Check MSB (carry bit) of second A2B result for true or false. */
+  bn.cmp    w21, w31 /* w21 can only be 0x1 or 0x0 */
+
+  /* Return the unreduced A2B computation (second result),
+     if zero flag is set, otherwise return the reduced
+     A2B computation (first result). */
+  bn.sel    w20, w20, w26, FG0.Z
+
+  ret
+
+/**
+ * Convert arithmetic shares to boolean ones using Goubin's algorithm.
+ *
+ * We use Goubin's boolean-to-arithmetic masking algorithm to switch from
+ * an arithmetic masking scheme to a boolean one without ever unmasking the
+ * seed. See Algorithm 2 here:
+ * https://link.springer.com/content/pdf/10.1007/3-540-44709-1_2.pdf
+ *
+ * This implementation expands the algorithm to 257 bits for carry bit
+ * handling. The carry bit can be used to detect and handle an
+ * underflow during the subtraction of arithmetic masking.
+ *
+ * This routine runs in constant time.
+ *
+ * Flags: Flags have no meaning beyond the scope of this subroutine.
+ *
+ * @param[in]  w31: all-zero wide data register
+ * @param[in]  w18: lower part of mask r
+ * @param[in]  w19: upper part of mask r
+ * @param[in]  w11: lower part of arithmetically masked value A,
+ *                  such that x = A + r
+ * @param[in]  w12: upper part of arithmetically masked value A,
+ *                  such that x = A + r
+ * @param[out] w20: lower part of boolean masked value x',
+ *                  such that x = x' ^ r
+ * @param[out] w21: upper part of boolean masked value x',
+ *                  such that x = x' ^ r
+ *
+ * clobbered registers: w1 - w6, w11, w12, and w18 - w21
+ * clobbered flag groups: FG0
+ */
+arithmetic_to_boolean:
+  /* Initialize inputs: in case of randomness in upper part of inputs
+     truncate to 257 bits. */
+  bn.rshi   w19, w19, w31 >> 1
+  bn.rshi   w19, w31, w19 >> 255
+  bn.rshi   w12, w12, w31 >> 1
+  bn.rshi   w12, w31, w12 >> 255
+
+  /* Fetch 257 bits of randomness.
+     [w2,w1] = gamma    <= URND */
+  bn.wsrr   w1, 2
+  bn.wsrr   w2, 2
+  bn.rshi   w2, w31, w2 >> 255
+
+  /* Double gamma and truncate to 257 bits.
+     [w4,w3] = T        <= 2 * [w2,w1] = 2 * gamma */
+  bn.add    w3, w1, w1
+  bn.addc   w4, w2, w2
+  bn.rshi   w4, w4, w31 >> 1
+  bn.rshi   w4, w31, w4 >> 255
+
+  /* [w21,w20] = x'     <= [w2,w1] ^ [w19,w18] = gamma ^ r */
+  bn.xor    w20, w1, w18
+  bn.xor    w21, w2, w19
+
+  /* [w6,w5] = omega    <= [w2,w1] & [w21,w20] = gamma & x' */
+  bn.and    w5, w1, w20
+  bn.and    w6, w2, w21
+
+  /* [w21,w20] = x'     <= [w4,w3] ^ [w12,w11] = T ^ A */
+  bn.xor    w20, w3, w11
+  bn.xor    w21, w4, w12
+
+  /* [w2,w1] = gamma    <= [w2,w1] ^ [w21,w20] = gamma ^ x' */
+  bn.xor    w1, w1, w20
+  bn.xor    w2, w2, w21
+
+  /* [w2,w1] = gamma    <= [w2,w1] & [w19,w18] = gamma & r */
+  bn.and    w1, w1, w18
+  bn.and    w2, w2, w19
+
+  /* [w6,w5] = omega    <= [w6,w5] ^ [w2,w1] = omega ^ gamma */
+  bn.xor    w5, w5, w1
+  bn.xor    w6, w6, w2
+
+  /* [w2,w1] = gamma    <= [w4,w3] & [w12,w11] = T & A */
+  bn.and    w1, w3, w11
+  bn.and    w2, w4, w12
+
+  /* [w6,w5] = omega    <= [w6,w5] ^ [w2,w1] = omega ^ gamma */
+  bn.xor    w5, w5, w1
+  bn.xor    w6, w6, w2
+
+  /* Loop for k = 1 to K - 1 = 257 - 1 */
+  loopi     256, 12
+
+    /* [w2,w1] = gamma  <= [w4,w3] & [w19,w18] = T & r */
+    bn.and     w1, w3, w18
+    bn.and     w2, w4, w19
+
+    /* [w2,w1] = gamma  <= [w2,w1] ^ [w6,w5] = gamma ^ omega */
+    bn.xor     w1, w1, w5
+    bn.xor     w2, w2, w6
+
+    /* [w4,w3] = T      <= [w4,w3] & [w12,w11] = T & A */
+    bn.and     w3, w3, w11
+    bn.and     w4, w4, w12
+
+    /* [w2,w1] = gamma  <= [w2,w1] ^ [w4,w3] = gamma ^ T */
+    bn.xor     w1, w1, w3
+    bn.xor     w2, w2, w4
+
+    /* Double gamma and truncate to 257 bits.
+       [w4,w3] = T      <= 2 * [w2,w1] = 2 * gamma */
+    bn.add    w3, w1, w1
+    bn.addc   w4, w2, w2
+    bn.rshi   w4, w4, w31 >> 1
+    bn.rshi   w4, w31, w4 >> 255
+
+  /* [w21,w20] = x'     <= [w21,w20] ^ [w4,w3] = x' ^ T */
+  bn.xor    w20, w20, w3
+  bn.xor    w21, w21, w4
 
   ret
 
