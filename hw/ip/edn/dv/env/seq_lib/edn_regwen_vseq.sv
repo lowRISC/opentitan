235ee3775176997bbc059d8087f0f1305da09454
Andreas Kurth <adk@lowrisc.org>
2023-06-22 21:21:45 +0000

    [edn/dv] Link to newly created issue for `regwen` testing
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- hw/ip/edn/dv/env/seq_lib/edn_regwen_vseq.sv
+++ hw/ip/edn/dv/env/seq_lib/edn_regwen_vseq.sv
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// TODO: add CSR supports in scb so we can apply randomize `set_regwen` in any test.
+// TODO(#19028): add CSR supports in scb so we can apply randomize `set_regwen` in any test.
 // Currently this randomization will fail in stress_all tests.
 class edn_regwen_vseq extends edn_smoke_vseq;
   `uvm_object_utils(edn_regwen_vseq)
