d1cc876410525060eced668861cf7619f7f7cbe5
Douglas Reis <doreis@lowrisc.org>
2023-06-26 11:38:55 +0100

    [spi_host, testutils] Add a function setup  pinmux
     This commit add a function to connect spi_host1 to the BoB via
     pinmux.
    
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/device/lib/testing/spi_host_testutils.c
+++ sw/device/lib/testing/spi_host_testutils.c
@@ -4,7 +4,10 @@
 
 #include "sw/device/lib/testing/spi_host_testutils.h"
 
+#include "sw/device/lib/dif/dif_pinmux.h"
 #include "sw/device/lib/testing/test_framework/check.h"
+
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
 // `extern` declarations to give the inline functions in the
 // corresponding header a link location.
@@ -18,5 +21,37 @@ status_t spi_host_testutils_flush(dif_spi_host_t *spi_host) {
     TRY(dif_spi_host_fifo_read(spi_host, &dummy, sizeof(dummy)));
     TRY(dif_spi_host_get_status(spi_host, &status));
   }
+  return OK_STATUS();
+}
+
+status_t spi_host1_pinmux_connect_to_bob(const dif_pinmux_t *pinmux,
+                                         dif_pinmux_index_t csb_outsel) {
+  // CSB.
+  TRY(dif_pinmux_output_select(pinmux, csb_outsel,
+                               kTopEarlgreyPinmuxOutselSpiHost1Csb));
+  // SCLK.
+  TRY(dif_pinmux_output_select(pinmux, kTopEarlgreyPinmuxMioOutIoa3,
+                               kTopEarlgreyPinmuxOutselSpiHost1Sck));
+  // SD0.
+  TRY(dif_pinmux_input_select(pinmux, kTopEarlgreyPinmuxPeripheralInSpiHost1Sd0,
+                              kTopEarlgreyPinmuxInselIoa5));
+  TRY(dif_pinmux_output_select(pinmux, kTopEarlgreyPinmuxMioOutIoa5,
+                               kTopEarlgreyPinmuxOutselSpiHost1Sd0));
+
+  // SD1.
+  TRY(dif_pinmux_input_select(pinmux, kTopEarlgreyPinmuxPeripheralInSpiHost1Sd1,
+                              kTopEarlgreyPinmuxInselIoa4));
+  TRY(dif_pinmux_output_select(pinmux, kTopEarlgreyPinmuxMioOutIoa4,
+                               kTopEarlgreyPinmuxOutselSpiHost1Sd1));
+  // SD2.
+  TRY(dif_pinmux_input_select(pinmux, kTopEarlgreyPinmuxPeripheralInSpiHost1Sd2,
+                              kTopEarlgreyPinmuxInselIoa8));
+  TRY(dif_pinmux_output_select(pinmux, kTopEarlgreyPinmuxMioOutIoa8,
+                               kTopEarlgreyPinmuxOutselSpiHost1Sd2));
+  // SD3.
+  TRY(dif_pinmux_input_select(pinmux, kTopEarlgreyPinmuxPeripheralInSpiHost1Sd3,
+                              kTopEarlgreyPinmuxInselIoa7));
+  TRY(dif_pinmux_output_select(pinmux, kTopEarlgreyPinmuxMioOutIoa7,
+                               kTopEarlgreyPinmuxOutselSpiHost1Sd3));
   return OK_STATUS();
 }
