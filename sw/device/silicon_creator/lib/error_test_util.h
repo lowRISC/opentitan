73d972a8557bab8220bb90b506d7d8490023c99f
Dan McArdle <dmcardle@opentitan.org>
2023-06-26 21:48:44 +0000

    [test] Extract rom_error_t -> string map, add custom printer
    Signed-off-by: Dan McArdle <dmcardle@opentitan.org>

---
+++ sw/device/silicon_creator/lib/error_test_util.h
@@ -0,0 +1,55 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_ERROR_TEST_UTIL_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_ERROR_TEST_UTIL_H_
+
+#include <iostream>
+#include <map>
+#include <ostream>
+#include <stdint.h>
+#include <string>
+
+#include "sw/device/silicon_creator/lib/error.h"
+
+const std::map<std::string, uint32_t> &GetErrorMap() {
+#define STRINGIFY(x) #x
+#define ERROR_MAP_INIT(name, value) \
+  { STRINGIFY(name), value }
+  static std::map<std::string, uint32_t> errors = {
+      DEFINE_ERRORS(ERROR_MAP_INIT),
+  };
+  return errors;
+#undef ERROR_MAP_INIT
+#undef STRINGIFY
+}
+
+const std::map<uint32_t, std::string> &GetErrorToStringMap() {
+#define STRINGIFY(x) #x
+#define ERROR_MAP_INIT(name, value) \
+  { value, STRINGIFY(name) }
+  static std::map<uint32_t, std::string> errors = {
+      DEFINE_ERRORS(ERROR_MAP_INIT),
+  };
+  return errors;
+#undef ERROR_MAP_INIT
+#undef STRINGIFY
+}
+
+/**
+ * A custom printer for `rom_error_t` in unit tests.
+ */
+void PrintTo(const rom_error_t &err, std::ostream *os) {
+  const std::map<uint32_t, std::string> &error_to_string_map =
+      GetErrorToStringMap();
+  // Print the symbolic name if it is known.
+  if (error_to_string_map.count(err)) {
+    *os << error_to_string_map.at(err);
+  } else {
+    *os << "Unrecognized rom_error_t value";
+  }
+  *os << " (0x" << std::hex << err << ")";
+}
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_ERROR_TEST_UTIL_H_
