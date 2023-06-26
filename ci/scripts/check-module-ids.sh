1df18c40e7fd8077d60eefcee3dbce13e2243f50
Amaury Pouly <amaury.pouly@lowrisc.org>
2023-06-26 18:15:59 +0100

    [bazel] Add an aspect to check for colliding module IDs
    We want to be able to run `opentitantool status lint` on every ELF
    file that depends on //sw/device/lib/base:status. The approach here
    is to use a bazel aspect to run an action for every ELF file. One
    difficulty is that we have no easy way to specifying the list of
    targets that we want to run the aspect on, and we want to avoid
    modifying existing rules or list all targets by hand. A compromise
    is to use a shell script to query the list of targets and then
    manually specify an aspect on bazel's CLI when building them.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

---
+++ ci/scripts/check-module-ids.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Checks that all module IDs are unique.
+
+set -e
+
+# List of all targets that need to be checked: we only consider targets that
+# produce a binary file and that depend on the status library. Since every test
+# is usually built for real targets and dv, we only check for one real target to
+# save time.
+query_elfs='
+    filter(
+        ".*prog_fpga_cw310.*",
+        kind(
+            cc_binary,
+            rdeps(
+                //sw/device/...,
+                //sw/device/lib/base:status
+            )
+        )
+    )'
+target_file=$(mktemp)
+./bazelisk.sh query "$query_elfs" >"$target_file"
+# We now ask bazel to build all targets but we also add the module ID checker aspect
+# and we query the corresponding output group to force bazel to run the checks.
+./ci/bazelisk.sh build \
+    --config=riscv32 \
+    --aspects=rules/quality.bzl%modid_check_aspect \
+    --output_groups=modid_check \
+    --target_pattern_file="$target_file"
