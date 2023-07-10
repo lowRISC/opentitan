12d287ea2176dbed56f78ffcff5ed6bdf584615e
Michael Schaffner <msf@opentitan.org>
2023-07-10 06:15:57 -0700

    [prim_subreg] Fix RC access corner case
    Implement fix as proposed in #5416.
    
    Fix #5416
    
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/prim/rtl/prim_subreg.sv
+++ hw/ip/prim/rtl/prim_subreg.sv
@@ -61,6 +61,13 @@ module prim_subreg
   // feed back out for consolidation
   assign ds = wr_en ? wr_data : qs;
   assign qe = wr_en;
-  assign qs = q;
+
+  if (SwAccess == SwAccessRC) begin : gen_rc
+    // In case of a SW RC colliding with a HW write, SW gets the value written by HW
+    // but the register is cleared to 0. See #5416 for a discussion.
+    assign qs = de && we ? d : q;
+  end else begin : gen_no_rc
+    assign qs = q;
+  end
 
 endmodule
