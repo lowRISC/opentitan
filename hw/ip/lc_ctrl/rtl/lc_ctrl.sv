47f3ec2479a37581c20207d8e45e8a2e15ac02af
Robert Schilling <rschilling@rivosinc.com>
2023-07-12 16:45:42 +0000

    [hw,tlul] Make a_user.rsvd width configurable
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/lc_ctrl/rtl/lc_ctrl.sv
+++ hw/ip/lc_ctrl/rtl/lc_ctrl.sv
@@ -249,6 +249,7 @@ module lc_ctrl
     .wdata_i      ( dmi_req.data                           ),
     .wdata_intg_i ('0                                      ),
     .be_i         ( {top_pkg::TL_DBW{1'b1}}                ),
+    .user_rsvd_i  ('0                                      ),
     .instr_type_i ( prim_mubi_pkg::MuBi4False              ),
     .valid_o      ( dmi_resp_valid                         ),
     .rdata_o      ( dmi_resp.data                          ),
