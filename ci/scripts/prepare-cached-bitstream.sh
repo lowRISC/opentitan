1e0ce67c575f3d06f0d27bb73c4cf170bf94cf5d
Andreas Kurth <adk@lowrisc.org>
2023-07-18 08:44:12 +0000

    [ci] Add arg with top name to `prepare-cached-bitstream.sh`
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- ci/scripts/prepare-cached-bitstream.sh
+++ ci/scripts/prepare-cached-bitstream.sh
@@ -3,19 +3,29 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-# This script currently assumes it is used for chip_earlgrey_cw310.
-
 set -ex
+
+if test "$#" -ne 1; then
+  echo "Usage: $0 <top name>" >&2
+  echo "Example: $0 earlgrey" >&2
+  exit 1
+fi
+readonly TOP_NAME="$1"
 
 . util/build_consts.sh
 
-readonly TOPLEVEL=top_earlgrey
-readonly TOPLEVEL_BIN_DIR="${BIN_DIR}/hw/${TOPLEVEL}"
-readonly TARGETS=(
-  @bitstreams//:chip_earlgrey_cw310_bitstream
-  @bitstreams//:chip_earlgrey_cw310_rom_mmi
-  @bitstreams//:chip_earlgrey_cw310_otp_mmi
+readonly TOPLEVEL_BIN_DIR="${BIN_DIR}/hw/top_${TOP_NAME}"
+readonly TARGET_PREFIX=@bitstreams//:chip_
+readonly TARGET_SUFFIXES=(
+  _cw310_bitstream
+  _cw310_rom_mmi
+  _cw310_otp_mmi
 )
+TARGETS=()
+for suffix in "${TARGET_SUFFIXES[@]}"; do
+  TARGETS+=("${TARGET_PREFIX}${TOP_NAME}${suffix}")
+done
+readonly TARGETS
 readonly BAZEL_OPTS=(
   "--define"
   "bitstream=gcp_splice"
