bdeffa6454c597dbdfd0eab8ca43b22574b6aae0
Moritz Wettermann <moritz.wettermann@gi-de.com>
2023-07-19 09:56:38 +0200

    [sw/crypto] Hardening of P-384 implementation
    This commit includes the following changes:
    - The internal scalar multiplication function now requires the scalar
    value to be handed over as additive (modulo n) shares.
    - Therefore, scalar k and private key d are now stored in shares in DMEM
    - The shares are expanded by 64 bits to 448-bit values. This is a
    side-channel protection measurement.
    - Scalar and base point multiplication functions as well as the signing
    function are adapted to the mentioned scalar/key handling.
    - For signing the shares are masked multiplicatively to not reveal the
    scalar/key value via side-channel leakage during signature computation.
    - For multiplicative masking a new 448x128-bit multiplication function
    is added, as well as a modular reduction function (modulo n).
    - The mentioned changes increase code size quite significantly. Because
    of this, (internal) scalar and base point multiplication are separated
    into own source code files to prevent IMEM overflow.
    
    Signed-off-by: Moritz Wettermann <moritz.wettermann@gi-de.com>

--- sw/otbn/crypto/tests/p384_ecdsa_sign_test.s
+++ sw/otbn/crypto/tests/p384_ecdsa_sign_test.s
@@ -15,29 +15,29 @@
 
 p384_ecdsa_sign_test:
 
-  /* set dmem pointer to nonce k */
-  la       x2, nonce_k
-  la       x3, dptr_k
+  /* set dmem pointer to point to 1st scalar share k0 */
+  la       x2, k0
+  la       x3, dptr_k0
   sw       x2, 0(x3)
 
-  /* set dmem pointer to point to blinding parameter */
-  la       x2, blinding_param
-  la       x3, dptr_rnd
+  /* set dmem pointer to point to 2nd scalar share k1 */
+  la       x2, k1
+  la       x3, dptr_k1
+  sw       x2, 0(x3)
+
+  /* set dmem pointer to point to 1st scalar share d0 (private key) */
+  la       x2, d0
+  la       x3, dptr_d0
+  sw       x2, 0(x3)
+
+  /* set dmem pointer to point to 2nd scalar share d1 (private key) */
+  la       x2, d1
+  la       x3, dptr_d1
   sw       x2, 0(x3)
 
   /* set dmem pointer to point to message */
   la       x2, msg
   la       x3, dptr_msg
-  sw       x2, 0(x3)
-
-  /* set dmem pointer to point to nonce k */
-  la       x2, nonce_k
-  la       x3, dptr_k
-  sw       x2, 0(x3)
-
-  /* set dmem pointer to point to private key d */
-  la       x2, priv_key_d
-  la       x3, dptr_d
   sw       x2, 0(x3)
 
   /* set dmem pointer to point to signature */
@@ -66,7 +66,43 @@ p384_ecdsa_sign_test:
 
 .data
 
-/* nonce k */
+/* 1st scalar share k0 (448-bit) */
+k0:
+  .word 0x5c832a51
+  .word 0x3eb17c27
+  .word 0x9a0c1b76
+  .word 0x6e001281
+  .word 0x4de8344e
+  .word 0x5b7d3b0f
+  .word 0x96d2f9e0
+  .word 0x1e9d19e7
+  .word 0x16f5c1ee
+  .word 0x800a4c94
+  .word 0xe14cd8df
+  .word 0xadb9ce1b
+  .word 0x8677a5f2
+  .word 0x32f9e2b0
+  .zero 8
+
+/* 2nd scalar share k1 (448-bit) */
+k1:
+  .word 0xe50b5e8e
+  .word 0x776ad076
+  .word 0x60d31f0e
+  .word 0x3521b5e8
+  .word 0x7bf0f8d5
+  .word 0xe08231d6
+  .word 0x7042f3bb
+  .word 0x4cb12f81
+  .word 0x82a3d7ab
+  .word 0x198f4d05
+  .word 0xb84cc0ba
+  .word 0xebdfcb7d
+  .word 0x9ec9b42f
+  .word 0x9e5dc598
+  .zero 8
+
+/* nonce k = k0 + k1 mod n (n: curve order) */
 nonce_k:
   .word 0x99999999
   .word 0x99999999
@@ -82,20 +118,56 @@ nonce_k:
   .word 0x99999999
   .zero 16
 
-/* blinding parameter rnd */
- blinding_param:
-  .word 0xa82c85b0
-  .word 0x163ce1c8
-  .word 0x32518fd7
-  .word 0xf8a428cd
-  .word 0xf5b9d867
-  .word 0x00906f5f
-  .word 0x7387b4f2
-  .word 0xa2d3da7a
-  .word 0xebe0a647
-  .word 0xfb2ef7ca
-  .word 0x74249432
-  .word 0x230e5ff6
+/* 1st private key share d0 (448-bit) */
+d0:
+  .word 0x5c832a51
+  .word 0x3eb17c27
+  .word 0x9a0c1b76
+  .word 0x6e001281
+  .word 0x4de8344e
+  .word 0x5b7d3b0f
+  .word 0x96d2f9e0
+  .word 0x1e9d19e7
+  .word 0x16f5c1ee
+  .word 0x800a4c94
+  .word 0xe14cd8df
+  .word 0xadb9ce1b
+  .word 0x8677a5f2
+  .word 0x32f9e2b0
+  .zero 8
+
+/* 2nd private key share d1 (448-bit) */
+d1:
+  .word 0x33eae098
+  .word 0xd31b18d5
+  .word 0x507568cd
+  .word 0xab8fb14d
+  .word 0x9ef51898
+  .word 0x44676e61
+  .word 0x9cb814d9
+  .word 0x4ad22b6e
+  .word 0x8930f243
+  .word 0xb706d682
+  .word 0xa9da1611
+  .word 0x13e7014a
+  .word 0x9ec9b430
+  .word 0x9e5dc598
+  .zero 8
+
+/* private key d = d0 + d1 mod n (n: curve order) */
+priv_key_d:
+  .word 0xe8791ba3
+  .word 0xf549e1f7
+  .word 0x893be358
+  .word 0x100794fe
+  .word 0xbc9db95d
+  .word 0xfd7ed624
+  .word 0xc60ebab6
+  .word 0x97ba9586
+  .word 0xa026b431
+  .word 0x37112316
+  .word 0x8b26eef1
+  .word 0xc1a0cf66
   .zero 16
 
 /* message */
@@ -112,22 +184,6 @@ msg:
   .word 0x55555555
   .word 0x55555555
   .word 0x55555555
-  .zero 16
-
-/* private key d */
-priv_key_d:
-  .word 0xe8791ba3
-  .word 0xf549e1f7
-  .word 0x893be358
-  .word 0x100794fe
-  .word 0xbc9db95d
-  .word 0xfd7ed624
-  .word 0xc60ebab6
-  .word 0x97ba9586
-  .word 0xa026b431
-  .word 0x37112316
-  .word 0x8b26eef1
-  .word 0xc1a0cf66
   .zero 16
 
 /* signature R */
