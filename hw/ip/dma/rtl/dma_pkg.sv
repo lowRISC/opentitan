4ea358f0e328278545ca1e262245dd1b1e0d53ed
Robert Schilling <rschilling@rivosinc.com>
2023-07-21 05:52:48 -0700

    [hw,dma,rtl] Factor out system port to own package
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/dma/rtl/dma_pkg.sv
+++ hw/ip/dma/rtl/dma_pkg.sv
@@ -27,4 +27,26 @@ package dma_pkg;
     SysOpcMsgP2P          = 3'd7
   } sys_opc_e;
 
+  // System port request interface
+  typedef struct packed {
+    logic     [SYS_NUM_REQ_CH-1:0]                         vld_vec;
+    logic     [SYS_NUM_REQ_CH-1:0][SYS_ADDR_WIDTH-1:0]     metadata_vec;
+    sys_opc_e [SYS_NUM_REQ_CH-1:0]                         opcode_vec;
+    logic     [SYS_NUM_REQ_CH-1:0][SYS_METADATA_WIDTH-1:0] iova_vec;
+    logic     [SYS_NUM_REQ_CH-1:0][SYS_RACL_WIDTH-1:0]     racl_vec;
+    logic     [(SYS_DATA_BYTEWIDTH*8)-1:0]                 write_data;
+    logic     [SYS_DATA_BYTEWIDTH-1:0]                     write_be;
+    logic     [SYS_DATA_BYTEWIDTH-1:0]                     read_be;
+  } sys_req_t;
+
+  // System port response interface
+  typedef struct packed {
+    logic [SYS_NUM_REQ_CH-1:0]         req_grant_vec_i;
+    logic                              read_data_vld_i;
+    logic [(SYS_DATA_BYTEWIDTH*8)-1:0] read_data_i;
+    logic [SYS_METADATA_WIDTH-1:0]     read_metadata_i;
+    logic                              error_vld_i;
+    logic [SYS_NUM_ERROR_TYPES-1:0]    error_vec_i;
+  } sys_rsp_t;
+
 endpackage
