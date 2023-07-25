f61bdd50fcb71bceb2b7a726bd1aec1db3381186
Jon Flatley <jflat@opentitan.org>
2023-07-25 16:07:20 -0400

    [sw/silicon_creator] Add primary bl0 slot messages
    Add the ROM_EXT boot service request and response messages for Set
    Primary BL0 Slot.
    
    Signed-off-by: Jon Flatley <jflat@opentitan.org>

---
+++ sw/device/silicon_creator/lib/boot_svc/boot_svc_primary_bl0_slot.c
@@ -0,0 +1,23 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/boot_svc/boot_svc_primary_bl0_slot.h"
+
+#include "sw/device/silicon_creator/lib/error.h"
+
+void boot_svc_primary_bl0_slot_req_init(uint32_t primary_bl0_slot,
+                                        boot_svc_primary_bl0_slot_req_t *msg) {
+  msg->primary_bl0_slot = primary_bl0_slot;
+  boot_svc_header_finalize(kBootSvcPrimaryBl0SlotReqType,
+                           sizeof(boot_svc_primary_bl0_slot_req_t),
+                           &msg->header);
+}
+
+void boot_svc_primary_bl0_slot_res_init(rom_error_t status,
+                                        boot_svc_primary_bl0_slot_res_t *msg) {
+  msg->status = status;
+  boot_svc_header_finalize(kBootSvcPrimaryBl0SlotResType,
+                           sizeof(boot_svc_primary_bl0_slot_res_t),
+                           &msg->header);
+}
