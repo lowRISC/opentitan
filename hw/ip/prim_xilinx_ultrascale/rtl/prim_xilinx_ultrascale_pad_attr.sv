e2289739ac1be5b62e33dc30f7349ee9d4ad3af5
Alexander Williams <awill@opentitan.org>
2023-07-26 14:53:11 -0700

    [prim] Add prim library for Ultrascale technology
    Add a library specific to the Xilinx / AMD Ultrascale FPGAs. The
    clocking architecture is quite different for Ultrascale vs. 7 series,
    and the available primitives are not the same. Thus, it needed its own
    set of primitives.
    
    Signed-off-by: Alexander Williams <awill@opentitan.org>

---
+++ hw/ip/prim_xilinx_ultrascale/rtl/prim_xilinx_ultrascale_pad_attr.sv
@@ -0,0 +1,54 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+
+`include "prim_assert.sv"
+
+module prim_xilinx_ultrascale_pad_attr
+  import prim_pad_wrapper_pkg::*;
+#(
+  // This parameter is ignored in this Xilinx variant.
+  parameter pad_type_e PadType = BidirStd
+) (
+  output pad_attr_t attr_warl_o
+);
+
+  // Currently supported pad attributes of the Xilinx pad library.
+  //
+  // Input-only:
+  //
+  // - inversion
+  //
+  // Bidirectional:
+  //
+  // - inversion
+  // - virtual open drain
+  //
+  if (PadType == InputStd) begin : gen_input_only_warl
+    always_comb begin : p_attr
+      attr_warl_o = '0;
+      attr_warl_o.invert = 1'b1;
+    end
+  end else if (PadType == BidirStd ||
+               PadType == BidirTol ||
+               PadType == BidirOd) begin : gen_bidir_warl
+    always_comb begin : p_attr
+      attr_warl_o = '0;
+      attr_warl_o.invert = 1'b1;
+      attr_warl_o.virt_od_en = 1'b1;
+    end
+  end else if (PadType == AnalogIn0) begin : gen_analog0_warl
+    // The analog pad type is basically just a feedthrough,
+    // and does hence not support any of the attributes.
+    always_comb begin : p_attr
+      attr_warl_o = '0;
+    end
+  end else begin : gen_invalid_config
+    // this should throw link warnings in elaboration
+    assert_static_in_generate_config_not_available
+        assert_static_in_generate_config_not_available();
+  end
+
+
+endmodule : prim_xilinx_ultrascale_pad_attr
