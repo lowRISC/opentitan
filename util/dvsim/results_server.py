7d228f49200309fedb625feb458b5a556357349f
Rupert Swarbrick <rswarbrick@lowrisc.org>
2023-07-27 15:36:16 +0100

    [dvsim] Wrap logic to move "latest" directory into results_server.py
    This shouldn't change behaviour, but it hopefully makes the code in
    FlowCfg.py simpler to follow (by dint of it being moved somewhere
    else!)
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- util/dvsim/results_server.py
+++ util/dvsim/results_server.py
@@ -8,9 +8,11 @@ Code for a wrapper class which represents the "results server".
 This is hosted with Google cloud.
 """
 
+import datetime
+import logging as log
 import subprocess
 from shutil import which
-from typing import List
+from typing import List, Optional
 
 
 class NoGCPError(Exception):
@@ -51,3 +53,51 @@ class ResultsServer:
         # Get the list of files by splitting into lines, then dropping the
         # empty line at the end.
         return process.stdout.split('\n')[:-1]
+
+    def get_creation_time(self, path: str) -> Optional[datetime.datetime]:
+        """Get the creation time at path as a datetime.
+
+        If the file does not exist (or we can't see the creation time for some
+        reason), returns None.
+        """
+        bucket_pfx = 'gs://' + self.bucket_name
+        try:
+            process = subprocess.run(['gsutil', 'ls', '-l', bucket_pfx + '/' + path],
+                                     capture_output=True,
+                                     universal_newlines=True,
+                                     check=True)
+        except subprocess.CalledProcessError:
+            log.error("Failed to run ls -l over GCP on {}".format(path))
+            return None
+
+        # With gsutil, ls -l on a file prints out something like the following:
+        #
+        #     35079  2023-07-27T13:26:04Z  gs://rjs-ot-scratch/path/to/my.file
+        #
+        # Grab the second word on the first (only) line and parse it into a
+        # datetime object. Recent versions of Python (3.11+) parse this format
+        # with fromisoformat but we can't do that with the minimum version we
+        # support.
+        timestamp = process.stdout.split()[1]
+        try:
+            return datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S%z')
+        except ValueError:
+            log.error("Could not parse creation time ({}) from GCP"
+                      .format(timestamp))
+            return None
+
+    def mv(self, from_path: str, to_path: str) -> None:
+        """Use gsutil mv to move a file/directory."""
+        try:
+            subprocess.run(['gsutil', 'mv',
+                            self._path_in_bucket(from_path),
+                            self._path_in_bucket(to_path)],
+                           check=True)
+        except subprocess.CalledProcessError:
+            # If we failed to move the file, print an error message but also
+            # fail with an error: we might not want anything downstream to keep
+            # going if it assumes some precious object has been moved to a
+            # place of safety!
+            log.error('Failed to use gsutil to move {} to {}'
+                      .format(from_path, to_path))
+            raise
