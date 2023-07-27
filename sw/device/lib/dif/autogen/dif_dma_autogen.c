4a45c716ce0d932ee68b03e6ce75ec8667ef5361
Daniel Beitel <dbeitel@rivosinc.com>
2023-07-26 17:47:51 -0700

    [darjeeling/dif] Restructure dif library to support multiple devices
    Move dif directory into new structure
    Separate device-specific difs into separate ip directories
    Update bazel build files for new structure
    Update header directory structure
    Fixes to get unit tests to pass
    Update documentation
    Update headers and references to dif library
    Fix header guards
    Move dif base library into ip directory
    
    Signed-off-by: Daniel Beitel <dbeitel@rivosinc.com>

---
+++ sw/device/lib/dif/autogen/dif_dma_autogen.c
@@ -0,0 +1,256 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// THIS FILE HAS BEEN GENERATED, DO NOT EDIT MANUALLY. COMMAND:
+// util/make_new_dif.py --mode=regen --only=autogen
+
+#include "sw/device/lib/dif/autogen/dif_dma_autogen.h"
+
+#include <stdint.h>
+
+#include "sw/device/lib/dif/dif_base.h"
+
+#include "dma_regs.h"  // Generated.
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_init(mmio_region_t base_addr, dif_dma_t *dma) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  dma->base_addr = base_addr;
+
+  return kDifOk;
+}
+
+dif_result_t dif_dma_alert_force(const dif_dma_t *dma, dif_dma_alert_t alert) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t alert_idx;
+  switch (alert) {
+    case kDifDmaAlertFatalFault:
+      alert_idx = DMA_ALERT_TEST_FATAL_FAULT_BIT;
+      break;
+    default:
+      return kDifBadArg;
+  }
+
+  uint32_t alert_test_reg = bitfield_bit32_write(0, alert_idx, true);
+  mmio_region_write32(dma->base_addr, (ptrdiff_t)DMA_ALERT_TEST_REG_OFFSET,
+                      alert_test_reg);
+
+  return kDifOk;
+}
+
+/**
+ * Get the corresponding interrupt register bit offset of the IRQ.
+ */
+static bool dma_get_irq_bit_index(dif_dma_irq_t irq,
+                                  bitfield_bit32_index_t *index_out) {
+  switch (irq) {
+    case kDifDmaIrqDmaDone:
+      *index_out = DMA_INTR_COMMON_DMA_DONE_BIT;
+      break;
+    case kDifDmaIrqDmaError:
+      *index_out = DMA_INTR_COMMON_DMA_ERROR_BIT;
+      break;
+    default:
+      return false;
+  }
+
+  return true;
+}
+
+static dif_irq_type_t irq_types[] = {
+    kDifIrqTypeEvent,
+    kDifIrqTypeEvent,
+};
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_irq_get_type(const dif_dma_t *dma, dif_dma_irq_t irq,
+                                  dif_irq_type_t *type) {
+  if (dma == NULL || type == NULL || irq == kDifDmaIrqDmaError + 1) {
+    return kDifBadArg;
+  }
+
+  *type = irq_types[irq];
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_irq_get_state(const dif_dma_t *dma,
+                                   dif_dma_irq_state_snapshot_t *snapshot) {
+  if (dma == NULL || snapshot == NULL) {
+    return kDifBadArg;
+  }
+
+  *snapshot =
+      mmio_region_read32(dma->base_addr, (ptrdiff_t)DMA_INTR_STATE_REG_OFFSET);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_irq_acknowledge_state(
+    const dif_dma_t *dma, dif_dma_irq_state_snapshot_t snapshot) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  mmio_region_write32(dma->base_addr, (ptrdiff_t)DMA_INTR_STATE_REG_OFFSET,
+                      snapshot);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_irq_is_pending(const dif_dma_t *dma, dif_dma_irq_t irq,
+                                    bool *is_pending) {
+  if (dma == NULL || is_pending == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!dma_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_state_reg =
+      mmio_region_read32(dma->base_addr, (ptrdiff_t)DMA_INTR_STATE_REG_OFFSET);
+
+  *is_pending = bitfield_bit32_read(intr_state_reg, index);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_irq_acknowledge_all(const dif_dma_t *dma) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  // Writing to the register clears the corresponding bits (Write-one clear).
+  mmio_region_write32(dma->base_addr, (ptrdiff_t)DMA_INTR_STATE_REG_OFFSET,
+                      UINT32_MAX);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_irq_acknowledge(const dif_dma_t *dma, dif_dma_irq_t irq) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!dma_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  // Writing to the register clears the corresponding bits (Write-one clear).
+  uint32_t intr_state_reg = bitfield_bit32_write(0, index, true);
+  mmio_region_write32(dma->base_addr, (ptrdiff_t)DMA_INTR_STATE_REG_OFFSET,
+                      intr_state_reg);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_irq_force(const dif_dma_t *dma, dif_dma_irq_t irq,
+                               const bool val) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!dma_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_test_reg = bitfield_bit32_write(0, index, val);
+  mmio_region_write32(dma->base_addr, (ptrdiff_t)DMA_INTR_TEST_REG_OFFSET,
+                      intr_test_reg);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_irq_get_enabled(const dif_dma_t *dma, dif_dma_irq_t irq,
+                                     dif_toggle_t *state) {
+  if (dma == NULL || state == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!dma_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_enable_reg =
+      mmio_region_read32(dma->base_addr, (ptrdiff_t)DMA_INTR_ENABLE_REG_OFFSET);
+
+  bool is_enabled = bitfield_bit32_read(intr_enable_reg, index);
+  *state = is_enabled ? kDifToggleEnabled : kDifToggleDisabled;
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_irq_set_enabled(const dif_dma_t *dma, dif_dma_irq_t irq,
+                                     dif_toggle_t state) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!dma_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_enable_reg =
+      mmio_region_read32(dma->base_addr, (ptrdiff_t)DMA_INTR_ENABLE_REG_OFFSET);
+
+  bool enable_bit = (state == kDifToggleEnabled) ? true : false;
+  intr_enable_reg = bitfield_bit32_write(intr_enable_reg, index, enable_bit);
+  mmio_region_write32(dma->base_addr, (ptrdiff_t)DMA_INTR_ENABLE_REG_OFFSET,
+                      intr_enable_reg);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_irq_disable_all(const dif_dma_t *dma,
+                                     dif_dma_irq_enable_snapshot_t *snapshot) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  // Pass the current interrupt state to the caller, if requested.
+  if (snapshot != NULL) {
+    *snapshot = mmio_region_read32(dma->base_addr,
+                                   (ptrdiff_t)DMA_INTR_ENABLE_REG_OFFSET);
+  }
+
+  // Disable all interrupts.
+  mmio_region_write32(dma->base_addr, (ptrdiff_t)DMA_INTR_ENABLE_REG_OFFSET,
+                      0u);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_dma_irq_restore_all(
+    const dif_dma_t *dma, const dif_dma_irq_enable_snapshot_t *snapshot) {
+  if (dma == NULL || snapshot == NULL) {
+    return kDifBadArg;
+  }
+
+  mmio_region_write32(dma->base_addr, (ptrdiff_t)DMA_INTR_ENABLE_REG_OFFSET,
+                      *snapshot);
+
+  return kDifOk;
+}
---
+++ sw/device/lib/dif/dif_dma.c
@@ -0,0 +1,169 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/dif/dif_dma.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "sw/device/lib/base/memory.h"
+#include "sw/device/lib/base/mmio.h"
+
+#include "dma_regs.h"  // Generated.
+
+static_assert(kDifDmaOpentitanInternalBus ==
+                  DMA_ADDRESS_SPACE_ID_DESTINATION_ASID_VALUE_OT_ADDR,
+              "Address Space ID mismatches with value defined in HW");
+static_assert(kDifDmaSoCControlRegisterBus ==
+                  DMA_ADDRESS_SPACE_ID_DESTINATION_ASID_VALUE_SOC_ADDR,
+              "Address Space ID mismatches with value defined in HW");
+static_assert(kDifDmaSoCSystemBus ==
+                  DMA_ADDRESS_SPACE_ID_DESTINATION_ASID_VALUE_SYS_ADDR_,
+              "Address Space ID mismatches with value defined in HW");
+static_assert(kDifDmaOpentitanExternalFlash ==
+                  DMA_ADDRESS_SPACE_ID_DESTINATION_ASID_VALUE_FLASH_ADDR,
+              "Address Space ID mismatches with value defined in HW");
+
+dif_result_t dif_dma_configure(const dif_dma_t *dma,
+                               dif_dma_transaction_t transaction) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  mmio_region_write32(dma->base_addr, DMA_SOURCE_ADDRESS_LO_REG_OFFSET,
+                      transaction.source.address & UINT32_MAX);
+  mmio_region_write32(dma->base_addr, DMA_SOURCE_ADDRESS_HI_REG_OFFSET,
+                      transaction.source.address >> (sizeof(uint32_t) * 8));
+
+  mmio_region_write32(dma->base_addr, DMA_DESTINATION_ADDRESS_LO_REG_OFFSET,
+                      transaction.destination.address & UINT32_MAX);
+  mmio_region_write32(
+      dma->base_addr, DMA_DESTINATION_ADDRESS_HI_REG_OFFSET,
+      transaction.destination.address >> (sizeof(uint32_t) * 8));
+
+  uint32_t reg = 0;
+  reg = bitfield_field32_write(reg, DMA_ADDRESS_SPACE_ID_SOURCE_ASID_FIELD,
+                               transaction.source.asid);
+  reg = bitfield_field32_write(reg, DMA_ADDRESS_SPACE_ID_DESTINATION_ASID_FIELD,
+                               transaction.destination.asid);
+  mmio_region_write32(dma->base_addr, DMA_ADDRESS_SPACE_ID_REG_OFFSET, reg);
+
+  mmio_region_write32(dma->base_addr, DMA_TOTAL_DATA_SIZE_REG_OFFSET,
+                      transaction.size);
+  mmio_region_write32(dma->base_addr, DMA_TRANSFER_WIDTH_REG_OFFSET,
+                      transaction.width);
+
+  return kDifOk;
+}
+
+dif_result_t dif_dma_handshake_enable(const dif_dma_t *dma,
+                                      dif_dma_handshake_t handshake) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  uint32_t reg = mmio_region_read32(dma->base_addr, DMA_CONTROL_REG_OFFSET);
+  reg = bitfield_bit32_write(reg, DMA_CONTROL_HARDWARE_HANDSHAKE_ENABLE_BIT,
+                             true);
+  reg = bitfield_bit32_write(reg, DMA_CONTROL_FIFO_AUTO_INCREMENT_ENABLE_BIT,
+                             handshake.fifo_auto_increment);
+  reg = bitfield_bit32_write(
+      reg, DMA_CONTROL_MEMORY_BUFFER_AUTO_INCREMENT_ENABLE_BIT,
+      handshake.memory_auto_increment);
+  reg = bitfield_bit32_write(reg, DMA_CONTROL_DATA_DIRECTION_BIT,
+                             handshake.direction_from_mem_to_fifo);
+  mmio_region_write32(dma->base_addr, DMA_CONTROL_REG_OFFSET, reg);
+  return kDifOk;
+}
+
+dif_result_t dif_dma_handshake_disable(const dif_dma_t *dma) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  uint32_t reg = mmio_region_read32(dma->base_addr, DMA_CONTROL_REG_OFFSET);
+  reg = bitfield_bit32_write(reg, DMA_CONTROL_HARDWARE_HANDSHAKE_ENABLE_BIT,
+                             false);
+  mmio_region_write32(dma->base_addr, DMA_CONTROL_REG_OFFSET, reg);
+  return kDifOk;
+}
+
+dif_result_t dif_dma_start(const dif_dma_t *dma,
+                           dif_dma_transaction_opcode_t opcode) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  uint32_t reg = mmio_region_read32(dma->base_addr, DMA_CONTROL_REG_OFFSET);
+  reg = bitfield_field32_write(reg, DMA_CONTROL_OPCODE_FIELD, opcode);
+  reg = bitfield_bit32_write(reg, DMA_CONTROL_GO_BIT, 1);
+  mmio_region_write32(dma->base_addr, DMA_CONTROL_REG_OFFSET, reg);
+  return kDifOk;
+}
+
+dif_result_t dif_dma_abort(const dif_dma_t *dma) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  uint32_t reg = mmio_region_read32(dma->base_addr, DMA_CONTROL_REG_OFFSET);
+  reg = bitfield_bit32_write(reg, DMA_CONTROL_ABORT_BIT, 1);
+  mmio_region_write32(dma->base_addr, DMA_CONTROL_REG_OFFSET, reg);
+  return kDifOk;
+}
+
+dif_result_t dif_dma_memory_range_set(const dif_dma_t *dma, uint32_t address,
+                                      size_t size) {
+  if (dma == NULL || size == 0) {
+    return kDifBadArg;
+  }
+
+  mmio_region_write32(dma->base_addr, DMA_ENABLED_MEMORY_RANGE_BASE_REG_OFFSET,
+                      address);
+  // The limit address is inclusive so we subtract one.
+  uint32_t end_addr = address + size - 1;
+  mmio_region_write32(dma->base_addr, DMA_ENABLED_MEMORY_RANGE_LIMIT_REG_OFFSET,
+                      end_addr);
+
+  return kDifOk;
+}
+
+dif_result_t dif_dma_memory_range_get(const dif_dma_t *dma, uint32_t *address,
+                                      size_t *size) {
+  if (dma == NULL || size == NULL || address == NULL) {
+    return kDifBadArg;
+  }
+
+  *address = mmio_region_read32(dma->base_addr,
+                                DMA_ENABLED_MEMORY_RANGE_BASE_REG_OFFSET);
+
+  // The limit address is inclusive so we add one.
+  *size = mmio_region_read32(dma->base_addr,
+                             DMA_ENABLED_MEMORY_RANGE_LIMIT_REG_OFFSET) -
+          *address + 1;
+
+  return kDifOk;
+}
+
+dif_result_t dif_dma_memory_range_lock(const dif_dma_t *dma) {
+  if (dma == NULL) {
+    return kDifBadArg;
+  }
+
+  mmio_region_write32(dma->base_addr, DMA_RANGE_UNLOCK_REGWEN_REG_OFFSET,
+                      kMultiBitBool4False);
+  return kDifOk;
+}
+
+dif_result_t dif_dma_is_memory_range_locked(const dif_dma_t *dma,
+                                            bool *is_locked) {
+  if (dma == NULL || is_locked == NULL) {
+    return kDifBadArg;
+  }
+
+  *is_locked =
+      kMultiBitBool4False ==
+      mmio_region_read32(dma->base_addr, DMA_RANGE_UNLOCK_REGWEN_REG_OFFSET);
+  return kDifOk;
+}
