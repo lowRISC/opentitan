849cc124c9a54f262394e6a80b637205e6f54d31
Raviteja Chatta <crteja@lowrisc.org>
2023-07-27 13:23:29 +0100

    [hw,dma,dv] Replace assert with DV macros
    Replace immediate assert statements with macros in dv_macros.svh
    
    Signed-off-by: Raviteja Chatta <crteja@lowrisc.org>
    Co-authored-by: Andreas Kurth <adk@lowrisc.org>

--- hw/ip/dma/dv/env/dma_pull_seq.sv
+++ hw/ip/dma/dv/env/dma_pull_seq.sv
@@ -26,19 +26,17 @@ class dma_pull_seq extends tl_device_seq;
   virtual function void randomize_rsp(REQ rsp);
     rsp.disable_a_chan_randomization();
     if (d_error_pct > 0) rsp.no_d_error_c.constraint_mode(0);
-    if (!(rsp.randomize() with
-           {rsp.d_valid_delay inside {[min_rsp_delay : max_rsp_delay]};
-            if (rsp.a_opcode == tlul_pkg::Get) {
-              rsp.d_opcode == tlul_pkg::AccessAckData;
-            } else {
-              rsp.d_opcode == tlul_pkg::AccessAck;
-            }
-            rsp.d_size == rsp.a_size;
-            rsp.d_source == rsp.a_source;
-            d_error dist {0 :/ (100 - d_error_pct), 1 :/ d_error_pct};
-        })) begin
-      `uvm_fatal(`gfn, "Cannot randomize rsp")
-    end
+    `DV_CHECK_RANDOMIZE_WITH_FATAL(rsp,
+      rsp.d_valid_delay inside {[min_rsp_delay : max_rsp_delay]};
+      if (rsp.a_opcode == tlul_pkg::Get) {
+        rsp.d_opcode == tlul_pkg::AccessAckData;
+      } else {
+        rsp.d_opcode == tlul_pkg::AccessAck;
+      }
+      rsp.d_size == rsp.a_size;
+      rsp.d_source == rsp.a_source;
+      d_error dist {0 :/ (100 - d_error_pct), 1 :/ d_error_pct};
+    )
     `uvm_info("dma_pull_seq",
               $sformatf("[check][d_chan] : a_address=0x%08h d_valid_delay=%0d",
                         rsp.a_addr, rsp.d_valid_delay),
