d7b76a6e354a360228a26339d47d923713e846e3
Alexander Williams <awill@opentitan.org>
2023-07-28 23:03:17 -0700

    [ci] Add script to get the build ID for cached bitstreams
    The old method of checking the base directory no longer works. Get the
    build ID from the manifest directly.
    
    Signed-off-by: Alexander Williams <awill@opentitan.org>

--- ci/scripts/get-bitstream-strategy.sh
+++ ci/scripts/get-bitstream-strategy.sh
@@ -6,11 +6,11 @@
 set -e
 
 usage_string="
-Usage: get-bitstream-strategy.sh <cached-bitstream-target> [pathspec]...
+Usage: get-bitstream-strategy.sh <cached-bitstream-design> [pathspec]...
 
-    cached-bitsream-target The bazel target for the cached bitstream. This script
-                           will retrieve the most recent image from a commit in this
-                           this branch's history.
+    cached-bitstream-design The design name for the cached bitstream. This script
+                            will retrieve the most recent image from a commit in
+                            this branch's history.
     pathspec A git pathspec (multiple instances permitted) indicating which files
                    trigger building the bitstream. Please use only exclude pathspecs
                    to cause new, unconsidered paths to trigger the build.
@@ -24,7 +24,7 @@ fi
 
 . util/build_consts.sh
 
-bitstream_target=${*:1:1}
+bitstream_design=${*:1:1}
 
 if [ $# -gt 1 ]; then
   excluded_files=( "${@:2}" )
@@ -33,22 +33,31 @@ else
 fi
 
 # Retrieve the most recent bitstream at or before HEAD.
-BITSTREAM="--refresh HEAD" ./bazelisk.sh build ${bitstream_target}
+BITSTREAM="--refresh HEAD" ./bazelisk.sh build @bitstreams//:manifest
 
-# The directory containing the bitstream is named after the git hash.
-bitstream_file=$(ci/scripts/target-location.sh ${bitstream_target})
-bitstream_commit=$(basename "$(dirname ${bitstream_file})")
+# Find the bitstream commit from the manifest file.
+manifest_file=$(ci/scripts/target-location.sh @bitstreams//:manifest)
 
-echo "Checking for changes against pre-built bitstream from ${bitstream_commit}"
-echo "Files changed:"
-git diff --stat --name-only ${bitstream_commit}
-echo
-echo "Changed files after exclusions applied:"
-# Use the cached bitstream if no changed files remain.
-if git diff --exit-code --stat --name-only ${bitstream_commit} -- "${excluded_files[@]}"; then
-  bitstream_strategy=cached
-else
+bitstream_commit=$(./bazelisk.sh run //util/py/scripts:get_bitstream_build_id \
+  -- --manifest ${manifest_file} \
+  --schema ${REPO_TOP}/rules/scripts/bitstreams_manifest.schema.json \
+  --design ${bitstream_design})
+
+if [ -z "${bitstream_commit}" ]; then
+  echo "Design ${bitstream_design} not found in the cache"
   bitstream_strategy=build
+else
+  echo "Checking for changes against pre-built bitstream from ${bitstream_commit}"
+  echo "Files changed:"
+  git diff --stat --name-only ${bitstream_commit}
+  echo
+  echo "Changed files after exclusions applied:"
+  # Use the cached bitstream if no changed files remain.
+  if git diff --exit-code --stat --name-only ${bitstream_commit} -- "${excluded_files[@]}"; then
+    bitstream_strategy=cached
+  else
+    bitstream_strategy=build
+  fi
 fi
 
 echo
