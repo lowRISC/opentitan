fb9c07fdec1d80056026e4048f5ab846e1cd2b0e
Douglas Reis <doreis@lowrisc.org>
2023-07-31 12:09:56 +0100

    [dma, dif] Unittest the `dif_dma_configure` function
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/device/lib/dif/dif_dma_unittest.cc
+++ sw/device/lib/dif/dif_dma_unittest.cc
@@ -34,7 +34,7 @@ class ConfigureTest
     : public DmaTestInitialized,
       public testing::WithParamInterface<dif_dma_transaction_t> {};
 
-TEST_P(ConfigureTest, ReadSuccess) {
+TEST_P(ConfigureTest, Success) {
   dif_dma_transaction_t transaction = GetParam();
   EXPECT_WRITE32(
       DMA_SOURCE_ADDRESS_LO_REG_OFFSET,
@@ -47,13 +47,13 @@ TEST_P(ConfigureTest, ReadSuccess) {
   EXPECT_WRITE32(DMA_DESTINATION_ADDRESS_HI_REG_OFFSET,
                  transaction.destination.address >> 32);
 
-  EXPECT_WRITE32(DMA_ADDRESS_SPACE_ID_REG_OFFSET,
-                 {
-                     {DMA_ADDRESS_SPACE_ID_SOURCE_ASID_OFFSET,
-                      transaction.source.space_id},
-                     {DMA_ADDRESS_SPACE_ID_DESTINATION_ASID_OFFSET,
-                      transaction.destination.space_id},
-                 });
+  EXPECT_WRITE32(
+      DMA_ADDRESS_SPACE_ID_REG_OFFSET,
+      {
+          {DMA_ADDRESS_SPACE_ID_SOURCE_ASID_OFFSET, transaction.source.asid},
+          {DMA_ADDRESS_SPACE_ID_DESTINATION_ASID_OFFSET,
+           transaction.destination.asid},
+      });
 
   EXPECT_WRITE32(DMA_TOTAL_DATA_SIZE_REG_OFFSET, transaction.size);
   EXPECT_WRITE32(DMA_TRANSFER_WIDTH_REG_OFFSET, transaction.width);
@@ -61,16 +61,69 @@ TEST_P(ConfigureTest, ReadSuccess) {
   EXPECT_DIF_OK(dif_dma_configure(&dma_, transaction));
 }
 
-INSTANTIATE_TEST_SUITE_P(
-    ConfigureTest, ConfigureTest,
-    testing::ValuesIn(std::vector<dif_dma_transaction_t>{{
-        {.source = {.address = 0x80004000,
-                    .space_id = kDifDmaOpentitanInternalBus},
-         .destination = {.address = 0x70004000,
-                         .space_id = kDifDmaOpentitanInternalBus},
-         .size = 0x1000,
-         .width = kDifDmaTransWidth1Byte},
-    }}));
+INSTANTIATE_TEST_SUITE_P(ConfigureTest, ConfigureTest,
+                         testing::ValuesIn(std::vector<dif_dma_transaction_t>{{
+                             // Test 0
+                             {
+                                 .source =
+                                     {
+                                         .address = 0xB05BA84B,
+                                         .asid = kDifDmaOpentitanInternalBus,
+                                     },
+                                 .destination =
+                                     {
+                                         .address = 0x721F400F,
+                                         .asid = kDifDmaOpentitanInternalBus,
+                                     },
+                                 .size = 0x1,
+                                 .width = kDifDmaTransWidth1Byte,
+                             },
+                             // Test 1
+                             {
+                                 .source =
+                                     {
+                                         .address = 0x34FCA80BC5C5CA67,
+                                         .asid = kDifDmaOpentitanExternalFlash,
+                                     },
+                                 .destination =
+                                     {
+                                         .address = 0xD0CF2C50,
+                                         .asid = kDifDmaSoCControlRegisterBus,
+                                     },
+                                 .size = 0x2,
+                                 .width = kDifDmaTransWidth2Bytes,
+                             },
+                             // Test 2
+                             {
+                                 .source =
+                                     {
+                                         .address = 0x05BA857F8D9C0838,
+                                         .asid = kDifDmaOpentitanExternalFlash,
+                                     },
+                                 .destination =
+                                     {
+                                         .address = 0x32CD872A12225CCE,
+                                         .asid = kDifDmaSoCSystemBus,
+                                     },
+                                 .size = 0x4,
+                                 .width = kDifDmaTransWidth4Bytes,
+                             },
+                             // Test 3
+                             {
+                                 .source =
+                                     {
+                                         .address = 0xBFED148856E0555E,
+                                         .asid = kDifDmaSoCSystemBus,
+                                     },
+                                 .destination =
+                                     {
+                                         .address = 0x9ECFA11919F684D7,
+                                         .asid = kDifDmaOpentitanExternalFlash,
+                                     },
+                                 .size = std::numeric_limits<uint32_t>::max(),
+                                 .width = kDifDmaTransWidth4Bytes,
+                             },
+                         }}));
 
 TEST_F(ConfigureTest, BadArg) {
   dif_dma_transaction_t transaction;
