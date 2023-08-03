b375de93467564fe394842e7687c0adbfa1304f2
Jaedon Kim <jdonjdon@opentitan.org>
2023-08-03 01:05:01 +0000

    [flash_ctrl,dv] more tb cleanup for reset event
    following tests need scoreboard cleanup during intermittent reset event
    
    Signed-off-by: Jaedon Kim <jdonjdon@opentitan.org>

--- hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_derr_detect_vseq.sv
+++ hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_derr_detect_vseq.sv
@@ -19,42 +19,46 @@ class flash_ctrl_derr_detect_vseq extends flash_ctrl_otf_base_vseq;
     cfg.otf_scb_h.stop = 0;
     cfg.clk_rst_vif.wait_clks(5);
 
-    fork
-      begin
-        repeat(40) begin
-          `DV_CHECK_RANDOMIZE_FATAL(this)
-          ctrl = rand_op;
-          bank = rand_op.addr[OTFBankId];
-          if (ctrl.partition == FlashPartData) begin
-            num = ctrl_num;
-          end else begin
-            num = ctrl_info_num;
+    fork begin : isolation_fork
+      fork
+        begin
+          repeat(40) begin
+            `DV_CHECK_RANDOMIZE_FATAL(this)
+            ctrl = rand_op;
+            bank = rand_op.addr[OTFBankId];
+            if (ctrl.partition == FlashPartData) begin
+              num = ctrl_num;
+            end else begin
+              num = ctrl_info_num;
+            end
+            // If the partition that selected configured as read-only, skip the program
+            sync_otf_wr_ro_part();
+            randcase
+              cfg.otf_wr_pct:prog_flash(ctrl, bank, num, fractions);
+              1:read_flash(ctrl, bank, num, fractions);
+            endcase
           end
-          // If the partition that selected configured as read-only, skip the program
-          sync_otf_wr_ro_part();
-          randcase
-            cfg.otf_wr_pct:prog_flash(ctrl, bank, num, fractions);
-            1:read_flash(ctrl, bank, num, fractions);
-          endcase
         end
-      end
-      begin
-        for (int i = 0; i < 4; ++i) begin
-          fork
-            send_rand_host_rd();
-          join_none
-          #0;
+        begin
+          for (int i = 0; i < 4; ++i) begin
+            fork
+              send_rand_host_rd();
+            join_none
+            #0;
+          end
+          csr_utils_pkg::wait_no_outstanding_access();
         end
-        csr_utils_pkg::wait_no_outstanding_access();
-      end
-      begin
-        while (cfg.scb_h.alert_count["fatal_err"] == 0) begin
-          cfg.clk_rst_vif.wait_clks(1);
+        begin
+          while (cfg.scb_h.alert_count["fatal_err"] == 0) begin
+            cfg.clk_rst_vif.wait_clks(1);
+          end
+          dut_init();
         end
-        dut_init();
-      end
-    join_any
-    disable fork;
+      join_any
+      disable fork;
+    end // block: isolation_fork
+    join
+
     // Add drain time
     #10us;
     if (cfg.derr_created[0] + cfg.derr_created[1] > 0) begin
