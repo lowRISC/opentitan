34567f57d8e86f91fd0e804effdc2059cf333df5
Jaedon Kim <jdonjdon@opentitan.org>
2023-08-04 21:17:09 +0000

    [flash_ctrl,dv] fix lcmgr_intg test error
    Refine force point using posedge.
    
    Signed-off-by: Jaedon Kim <jdonjdon@opentitan.org>

--- hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_lcmgr_intg_vseq.sv
+++ hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_lcmgr_intg_vseq.sv
@@ -16,22 +16,25 @@ class flash_ctrl_lcmgr_intg_vseq extends flash_ctrl_err_base_vseq;
 
    // fatal_std_err
    task run_error_event();
-      int wait_timeout_ns = 50_000;
-      string path = "tb.dut.u_flash_hw_if.rdata_i[38:32]";
+     int wait_timeout_ns = 50_000;
+     string path = "tb.dut.u_flash_hw_if.rdata_i[38:32]";
+     int    err_intg = $urandom_range(0, 127);
 
-      `DV_SPINWAIT(wait(cfg.flash_ctrl_vif.hw_rvalid == 1);,
-                   , wait_timeout_ns)
-      cfg.scb_h.expected_alert["fatal_std_err"].expected = 1;
-      cfg.scb_h.expected_alert["fatal_std_err"].max_delay = 2000;
-      cfg.scb_h.exp_alert_contd["fatal_std_err"] = 10000;
+     `DV_SPINWAIT(wait(cfg.flash_ctrl_vif.hw_rvalid == 1);,
+                  , wait_timeout_ns)
+     cfg.scb_h.expected_alert["fatal_std_err"].expected = 1;
+     cfg.scb_h.expected_alert["fatal_std_err"].max_delay = 2000;
+     cfg.scb_h.exp_alert_contd["fatal_std_err"] = 10000;
 
-      $assertoff(0, "tb.dut.u_flash_mp.NoReqWhenErr_A");
-      `DV_CHECK(uvm_hdl_force(path, $urandom))
-      // Make sure this is not too long.
-      // If this is too long, it will causes other fatal errors.
-      cfg.clk_rst_vif.wait_clks(5);
-      `DV_CHECK(uvm_hdl_release(path))
-      collect_err_cov_status(ral.std_fault_status);
-      csr_rd_check(.ptr(ral.std_fault_status.lcmgr_intg_err), .compare_value(1));
+     $assertoff(0, "tb.dut.u_flash_mp.NoReqWhenErr_A");
+     #0;
+     @(posedge cfg.flash_ctrl_vif.hw_rvalid);
+     `DV_CHECK(uvm_hdl_force(path, err_intg))
+     // Make sure this is not too long.
+     // If this is too long, it will causes other fatal errors.
+     cfg.clk_rst_vif.wait_clks(5);
+     `DV_CHECK(uvm_hdl_release(path))
+     collect_err_cov_status(ral.std_fault_status);
+     csr_rd_check(.ptr(ral.std_fault_status.lcmgr_intg_err), .compare_value(1));
    endtask
 endclass // flash_ctrl_lcmgr_intg_vseq
