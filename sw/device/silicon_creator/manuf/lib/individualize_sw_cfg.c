e86a92dbb6fc2ff7787476dfb3ed1d049ab607c0
Tim Trippel <ttrippel@google.com>
2023-08-08 14:17:26 -0700

    [manuf] remove unneeded `otp_img` lib
    Since this lib only contained a single function for writing OTP
    key-value pairs to a partition, this was moved in the only lib that
    invokes it: the `individualize_sw_cfg` lib.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/device/silicon_creator/manuf/lib/individualize_sw_cfg.c
+++ sw/device/silicon_creator/manuf/lib/individualize_sw_cfg.c
@@ -5,8 +5,46 @@
 #include "sw/device/silicon_creator/manuf/lib/individualize_sw_cfg.h"
 
 #include "sw/device/lib/dif/dif_otp_ctrl.h"
-#include "sw/device/silicon_creator/manuf/lib/otp_img.h"
+#include "sw/device/lib/testing/otp_ctrl_testutils.h"
 #include "sw/device/silicon_creator/manuf/lib/otp_img_types.h"
+
+/**
+ * Writes OTP values to target OTP `partition`.
+ *
+ * The `kv` array is preferrably generated using the build infrastructure. See
+ * individualize_preop.c and its build target for an example.
+ *
+ * @param otp OTP Controller instance.
+ * @param partition Target OTP partition.
+ * @param kv OTP Array of OTP key values. See `otp_kv_t` documentation for more
+ * details.
+ * @param len Length of the `kv` array.
+ * @return OT_WARN_UNUSED_RESULT
+ */
+OT_WARN_UNUSED_RESULT
+static status_t otp_img_write(const dif_otp_ctrl_t *otp,
+                              dif_otp_ctrl_partition_t partition,
+                              const otp_kv_t *kv, size_t len) {
+  for (size_t i = 0; i < len; ++i) {
+    uint32_t offset;
+    TRY(dif_otp_ctrl_relative_address(partition, kv[i].offset, &offset));
+    switch (kv[i].type) {
+      case kOptValTypeUint32Buff:
+        TRY(otp_ctrl_testutils_dai_write32(otp, partition, offset,
+                                           kv[i].value32, kv[i].num_values));
+        break;
+      case kOptValTypeUint64Buff:
+        TRY(otp_ctrl_testutils_dai_write64(otp, partition, offset,
+                                           kv[i].value64, kv[i].num_values));
+        break;
+      case kOptValTypeUint64Rnd:
+        return UNIMPLEMENTED();
+      default:
+        return INTERNAL();
+    }
+  }
+  return OK_STATUS();
+}
 
 status_t manuf_individualize_device_sw_cfg(const dif_otp_ctrl_t *otp_ctrl) {
   TRY(otp_img_write(otp_ctrl, kDifOtpCtrlPartitionCreatorSwCfg,
