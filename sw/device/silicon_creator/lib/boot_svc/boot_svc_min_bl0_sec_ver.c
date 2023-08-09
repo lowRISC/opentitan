6e0f3c6ec519f83d8ce6dd66634cc67d69668bdb
Jon Flatley <jflat@opentitan.org>
2023-08-09 15:36:32 -0400

    [sw/silicon_creator] Prevent setting lower BL0 minimum security versions
    Signed-off-by: Jon Flatley <jflat@opentitan.org>

--- sw/device/silicon_creator/lib/boot_svc/boot_svc_min_bl0_sec_ver.c
+++ sw/device/silicon_creator/lib/boot_svc/boot_svc_min_bl0_sec_ver.c
@@ -6,16 +6,18 @@
 
 #include "sw/device/silicon_creator/lib/error.h"
 
-void boot_svc_min_bl0_sec_ver_req_init(uint32_t min_sec_version,
+void boot_svc_min_bl0_sec_ver_req_init(uint32_t min_bl0_sec_ver,
                                        boot_svc_min_bl0_sec_ver_req_t *msg) {
-  msg->min_bl0_sec_ver = min_sec_version;
+  msg->min_bl0_sec_ver = min_bl0_sec_ver;
   boot_svc_header_finalize(kBootSvcMinBl0SecVerReqType,
                            sizeof(boot_svc_min_bl0_sec_ver_req_t),
                            &msg->header);
 }
 
-void boot_svc_min_bl0_sec_ver_res_init(rom_error_t status,
+void boot_svc_min_bl0_sec_ver_res_init(uint32_t min_bl0_sec_ver,
+                                       rom_error_t status,
                                        boot_svc_min_bl0_sec_ver_res_t *msg) {
+  msg->min_bl0_sec_ver = min_bl0_sec_ver;
   msg->status = status;
   boot_svc_header_finalize(kBootSvcMinBl0SecVerResType,
                            sizeof(boot_svc_min_bl0_sec_ver_res_t),
