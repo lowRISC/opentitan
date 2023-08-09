0eaa70fbb08dcb88b483d2c6f5bb75db75c78a64
Musawir Hussain <Musawir745@gmail.com>
2023-08-09 16:39:18 +0500

    [rv_dm] remove extra displays
    Signed-off-by: Musawir Hussain <Musawir745@gmail.com>

--- hw/ip/rv_dm/dv/env/seq_lib/rv_dm_mem_tl_access_halted_vseq.sv
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_mem_tl_access_halted_vseq.sv
@@ -1,4 +1,4 @@
-// HALTEDD test vseq
+// HALTED test vseq
 class rv_dm_mem_tl_access_halted_vseq extends rv_dm_base_vseq;
   `uvm_object_utils(rv_dm_mem_tl_access_halted_vseq)
 
@@ -16,12 +16,10 @@ class rv_dm_mem_tl_access_halted_vseq extends rv_dm_base_vseq;
     repeat ($urandom_range(1, 10)) begin
       rw_data = $urandom_range(0, 1);
       // Verify that writing to HALTED results in anyhalted and allhalted to be set.
-      $display("data: %0h", rw_data);
       csr_wr(.ptr(jtag_dmi_ral.dmcontrol.haltreq), .value(1));
       cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
       csr_wr(.ptr(tl_mem_ral.halted), .value(rw_data));
       cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
-      //csr_wr(.ptr(tl_mem_ral.halted), .value(1));
       csr_rd(.ptr(jtag_dmi_ral.dmstatus), .value(rw_data));
       `DV_CHECK_EQ(1, get_field_val(jtag_dmi_ral.dmstatus.anyhalted, rw_data))      
       `DV_CHECK_EQ(1, get_field_val(jtag_dmi_ral.dmstatus.allhalted, rw_data))
