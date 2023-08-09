fd427f218fb80c985d5563aa828308888cd147f0
Musawir Hussain <Musawir745@gmail.com>
2023-08-09 16:38:01 +0500

    [rv_dm] Halted test
    This test verify the halted status of the hart by writing into the debug memory register called HALTED through mem_tl_if interface.
    
    Signed-off-by: Musawir Hussain <Musawir745@gmail.com>

---
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_mem_tl_access_halted_vseq.sv
@@ -0,0 +1,32 @@
+// HALTEDD test vseq
+class rv_dm_mem_tl_access_halted_vseq extends rv_dm_base_vseq;
+  `uvm_object_utils(rv_dm_mem_tl_access_halted_vseq)
+
+  `uvm_object_new
+
+  constraint lc_hw_debug_en_c {
+    lc_hw_debug_en == lc_ctrl_pkg::On;
+  }
+  constraint scanmode_c {
+    scanmode == prim_mubi_pkg::MuBi4False;
+  }
+  
+  task body();   
+    uvm_reg_data_t rw_data;
+    repeat ($urandom_range(1, 10)) begin
+      rw_data = $urandom_range(0, 1);
+      // Verify that writing to HALTED results in anyhalted and allhalted to be set.
+      $display("data: %0h", rw_data);
+      csr_wr(.ptr(jtag_dmi_ral.dmcontrol.haltreq), .value(1));
+      cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
+      csr_wr(.ptr(tl_mem_ral.halted), .value(rw_data));
+      cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
+      //csr_wr(.ptr(tl_mem_ral.halted), .value(1));
+      csr_rd(.ptr(jtag_dmi_ral.dmstatus), .value(rw_data));
+      `DV_CHECK_EQ(1, get_field_val(jtag_dmi_ral.dmstatus.anyhalted, rw_data))      
+      `DV_CHECK_EQ(1, get_field_val(jtag_dmi_ral.dmstatus.allhalted, rw_data))
+      cfg.clk_rst_vif.wait_clks($urandom_range(1, 10));
+    end  
+  endtask : body
+  
+endclass : rv_dm_mem_tl_access_halted_vseq
