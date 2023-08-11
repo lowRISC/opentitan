36e503f174e8eea6b0b08d54001c11ce289d585e
Michael Schaffner <msf@opentitan.org>
2023-08-10 18:28:53 -0700

    [tlul] Implement tlul_adapter_dmi
    Implement a TL-UL adapter that translates to the custom DMI
    protocol of the PULP RISC-V debug module.
    
    This is based off the tlul_adapter_reg, which has similar
    functionality.
    
    We may get rid of this module in the future in case
    we refactor the dm_csrs module to speak TL-UL natively.
    
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/tlul/rtl/tlul_adapter_dmi.sv
+++ hw/ip/tlul/rtl/tlul_adapter_dmi.sv
@@ -1,169 +1,121 @@
 // Copyright lowRISC contributors (OpenTitan project).
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
+//
+// Tile-Link UL adapter for RISC-V debug module from the PULP project.
+//
+// Note that this module can only handle one outstanding request at a time.
 
 `include "prim_assert.sv"
 
-/**
- * Tile-Link UL adapter for Register interface
- *
- * ICEBOX(#15822): Note that due to some modules with special needs (like
- * the vendored-in RV_DM), this module has been extended so that it
- * supports use cases outside of the generated reg_top module. This makes
- * this adapter and its parameterization options a bit heavy.
- *
- * We should in the future come back to this and refactor / align the
- * module and its parameterization needs.
- */
-
-module tlul_adapter_reg
+module tlul_adapter_dmi
   import tlul_pkg::*;
-  import prim_mubi_pkg::mubi4_t;
 #(
   parameter  bit CmdIntgCheck      = 0,  // 1: Enable command integrity check
   parameter  bit EnableRspIntgGen  = 0,  // 1: Generate response integrity
   parameter  bit EnableDataIntgGen = 0,  // 1: Generate response data integrity
-  parameter  int RegAw             = 8,  // Width of register address
-  parameter  int RegDw             = 32, // Shall be matched with TL_DW
-  parameter  int AccessLatency     = 0,  // 0: same cycle, 1: next cycle
-  localparam int RegBw             = RegDw/8
+  // This is fixed by the dm_pkg.
+  // Unfortunately there only exists a request struct type, hence these
+  // values have to be hardcoded here. Two SVAs are added further below to make
+  // sure these are aligned with the widths in dm_pkg.
+  localparam int DmiAw             = 7,  // Width of register address
+  localparam int DmiDw             = 32  // Shall be matched with TL_DW
 ) (
   input clk_i,
   input rst_ni,
 
   // TL-UL interface
-  input  tl_h2d_t tl_i,
-  output tl_d2h_t tl_o,
+  input  tl_h2d_t tl_h2d_i,
+  output tl_d2h_t tl_d2h_o,
 
   // control interface
-  input  mubi4_t  en_ifetch_i,
   output logic    intg_error_o,
 
-  // Register interface
-  output logic             re_o,
-  output logic             we_o,
-  output logic [RegAw-1:0] addr_o,
-  output logic [RegDw-1:0] wdata_o,
-  output logic [RegBw-1:0] be_o,
-  input                    busy_i,
-  // The following two signals are expected
-  // to be returned in AccessLatency cycles.
-  input        [RegDw-1:0] rdata_i,
-  // This can be a write or read error.
-  input                    error_i
+  output logic          dmi_req_valid_o,
+  input  logic          dmi_req_ready_i,
+  output dm::dmi_req_t  dmi_req_o,
+
+  input  logic          dmi_resp_valid_i,
+  output logic          dmi_resp_ready_o,
+  input  dm::dmi_resp_t dmi_resp_i
 );
 
-  `ASSERT_INIT(AllowedLatency_A, AccessLatency inside {0, 1})
+  localparam int IdW = $bits(tl_h2d_i.a_source);
+  localparam int SzW = $bits(tl_h2d_i.a_size);
 
-  localparam int IW  = $bits(tl_i.a_source);
-  localparam int SZW = $bits(tl_i.a_size);
-
-  logic outstanding_q;    // Indicates current request is pending
-  logic a_ack, d_ack;
-
-  logic [RegDw-1:0] rdata, rdata_q;
-  logic             error_q, error, err_internal, instr_error, intg_error;
-
-  logic addr_align_err;     // Size and alignment
-  logic malformed_meta_err; // User signal format error or unsupported
-  logic tl_err;             // Common TL-UL error checker
-
-  logic [IW-1:0]  reqid_q;
-  logic [SZW-1:0] reqsz_q;
-  tl_d_op_e       rspop_q;
-
-  logic rd_req, wr_req;
-
-  assign a_ack   = tl_i.a_valid & tl_o.a_ready;
-  assign d_ack   = tl_o.d_valid & tl_i.d_ready;
-  // Request signal
-  assign wr_req  = a_ack & ((tl_i.a_opcode == PutFullData) | (tl_i.a_opcode == PutPartialData));
-  assign rd_req  = a_ack & (tl_i.a_opcode == Get);
-
-  assign we_o    = wr_req & ~err_internal;
-  assign re_o    = rd_req & ~err_internal;
-  assign wdata_o = tl_i.a_data;
-  assign be_o    = tl_i.a_mask;
-
-  if (RegAw <= 2) begin : gen_only_one_reg
-    assign addr_o  = '0;
-  end else begin : gen_more_regs
-    assign addr_o  = {tl_i.a_address[RegAw-1:2], 2'b00}; // generate always word-align
-  end
-
+  logic a_ack, d_ack;  // Indicates whether the corresponding valid/ready handshake completes.
+  logic outstanding_q; // Indicates whether there is a pending request.
+  assign a_ack   = tl_h2d_i.a_valid & tl_d2h_o.a_ready;
+  assign d_ack   = tl_d2h_o.d_valid & tl_h2d_i.d_ready;
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni)    outstanding_q <= 1'b0;
     else if (a_ack) outstanding_q <= 1'b1;
     else if (d_ack) outstanding_q <= 1'b0;
   end
 
+  // Decode Requests
+  logic rd_req, wr_req;
+  assign wr_req  = tl_h2d_i.a_valid && ((tl_h2d_i.a_opcode == PutFullData) ||
+                                            (tl_h2d_i.a_opcode == PutPartialData));
+  assign rd_req  = tl_h2d_i.a_valid && (tl_h2d_i.a_opcode == Get);
+
+  // If we detect an error, the response will be returned immediately in the next cycle
+  // and we do not send a request out to the DMI.
+  logic error_d, error_q;
+  assign dmi_req_valid_o = (wr_req || rd_req) && !error_d;
+  // The DMI response can be accepted if there is a pending request without error.
+  assign dmi_resp_ready_o = outstanding_q && !error_q;
+
+  // We expect a word-aligned address here, otherwise an error is returned (see further below).
+  assign dmi_req_o.addr = tl_h2d_i.a_address[DmiAw+2-1:2];
+  assign dmi_req_o.data = tl_h2d_i.a_data;
+  assign dmi_req_o.op   = (wr_req) ? dm::DTM_WRITE :
+                          (rd_req) ? dm::DTM_READ  : dm::DTM_NOP;
+
+  logic [IdW-1:0] reqid_q;
+  logic [SzW-1:0] reqsz_q;
+  tl_d_op_e       rspop_q;
+  logic wr_req_q, rd_req_q;
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
-      reqid_q <= '0;
-      reqsz_q <= '0;
-      rspop_q <= AccessAck;
+      reqid_q  <= '0;
+      reqsz_q  <= '0;
+      rspop_q  <= AccessAck;
+      error_q  <= 1'b0;
+      wr_req_q <= 1'b0;
+      rd_req_q <= 1'b0;
     end else if (a_ack) begin
-      reqid_q <= tl_i.a_source;
-      reqsz_q <= tl_i.a_size;
+      reqid_q <= tl_h2d_i.a_source;
+      reqsz_q <= tl_h2d_i.a_size;
       // Return AccessAckData regardless of error
-      rspop_q <= (rd_req) ? AccessAckData : AccessAck ;
+      rspop_q <= (rd_req) ? AccessAckData : AccessAck;
+      rd_req_q <= rd_req;
+      wr_req_q <= wr_req;
+      error_q  <= error_d;
     end
   end
 
-  if (AccessLatency == 1) begin : gen_access_latency1
-    logic wr_req_q, rd_req_q;
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-      if (!rst_ni) begin
-        rdata_q  <= '0;
-        error_q  <= 1'b0;
-        wr_req_q <= 1'b0;
-        rd_req_q <= 1'b0;
-      end else begin
-        rd_req_q <= rd_req;
-        wr_req_q <= wr_req;
-        // Addressing phase
-        if (a_ack) begin
-          error_q <= err_internal;
-        // Response phase
-        end else begin
-          error_q <= error;
-          rdata_q <= rdata;
-        end
-      end
-    end
-    assign rdata = (error_i || error_q || wr_req_q) ? '1      :
-                   (rd_req_q)                       ? rdata_i :
-                                                      rdata_q; // backpressure case
-    assign error = (rd_req_q || wr_req_q) ? (error_q || error_i) :
-                                            error_q; // backpressure case
-  end else begin : gen_access_latency0
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-      if (!rst_ni) begin
-        rdata_q <= '0;
-        error_q <= 1'b0;
-      end else if (a_ack) begin
-        rdata_q <= (error_i || err_internal || wr_req) ? '1 : rdata_i;
-        error_q <= error_i || err_internal;
-      end
-    end
-    assign rdata = rdata_q;
-    assign error = error_q;
-  end
+  // Send back a bus error if the dm_csrs module responded with an error.
+  logic error_resp;
+  assign error_resp = error_q || (dmi_resp_i.resp == dm::DTM_ERR);
 
-  tlul_pkg::tl_d2h_t tl_o_pre;
-  assign tl_o_pre = '{
-    // busy is selected based on address
-    // thus if there is no valid transaction, we should ignore busy
-    a_ready:  ~(outstanding_q | tl_i.a_valid & busy_i),
-    d_valid:  outstanding_q,
+  tlul_pkg::tl_d2h_t tl_d2h_o_pre;
+  assign tl_d2h_o_pre = '{
+    // We are ready to accept a request if there is no outstanding one and if the DMI is ready.
+    a_ready:  dmi_req_ready_i && !outstanding_q,
+    // A data response is valid if the DMI response is valid and we've got an outstanding request.
+    // If the request resulted in an internal error, the error response is returned without a DMI
+    // response, since the request to the DMI has been suppressed by the error.
+    d_valid:  (dmi_resp_valid_i || error_q) && outstanding_q,
     d_opcode: rspop_q,
     d_param:  '0,
     d_size:   reqsz_q,
     d_source: reqid_q,
     d_sink:   '0,
-    d_data:   rdata,
+    // Blank data upon error in the same way as other TL-UL adapters would in OpenTitan.
+    d_data:   (rd_req_q && !(error_resp || wr_req_q)) ? dmi_resp_i.data : {DmiDw{1'b1}},
     d_user:   '0,
-    d_error:  error
+    d_error:  error_resp
   };
 
   // outgoing integrity generation
@@ -171,14 +123,15 @@ module tlul_adapter_reg
     .EnableRspIntgGen(EnableRspIntgGen),
     .EnableDataIntgGen(EnableDataIntgGen)
   ) u_rsp_intg_gen (
-    .tl_i(tl_o_pre),
-    .tl_o(tl_o)
+    .tl_i(tl_d2h_o_pre),
+    .tl_o(tl_d2h_o)
   );
 
+  logic intg_error;
   if (CmdIntgCheck) begin : gen_cmd_intg_check
     logic intg_error_q;
     tlul_cmd_intg_chk u_cmd_intg_chk (
-      .tl_i(tl_i),
+      .tl_i(tl_h2d_i),
       .err_o(intg_error)
     );
     // permanently latch integrity error until reset
@@ -199,39 +152,39 @@ module tlul_adapter_reg
   // Error Handling //
   ////////////////////
 
-  // An instruction type transaction is only valid if en_ifetch is enabled
-  // If the instruction type is completely invalid, also considered an instruction error
-  assign instr_error = prim_mubi_pkg::mubi4_test_invalid(tl_i.a_user.instr_type) |
-                       (prim_mubi_pkg::mubi4_test_true_strict(tl_i.a_user.instr_type) &
-                        prim_mubi_pkg::mubi4_test_false_loose(en_ifetch_i));
+  logic addr_oob_err;       // Address out of bounds error.
+  logic addr_align_err;     // Size and alignment
+  logic malformed_meta_err; // User signal format error or unsupported
+  logic tl_err;             // Common TL-UL error checker
+  assign error_d = addr_oob_err ||
+                   addr_align_err ||
+                   malformed_meta_err ||
+                   tl_err ||
+                   intg_error;
 
-  assign err_internal = addr_align_err | malformed_meta_err | tl_err | instr_error | intg_error;
-
-  // Don't allow unsupported values.
-  assign malformed_meta_err = tl_a_user_chk(tl_i.a_user);
+  // Error if the address is larger than what can be represented with DmiAw
+  assign addr_oob_err = (wr_req || rd_req) && |(tl_h2d_i.a_address >> (2 + DmiAw));
 
   // addr_align_err
   //    Raised if addr isn't aligned with the size
   //    Read size error is checked in tlul_assert.sv
   //    Here is it added due to the limitation of register interface.
-  always_comb begin
-    if (wr_req) begin
-      // Only word-align is accepted based on comportability spec
-      addr_align_err = |tl_i.a_address[1:0];
-    end else begin
-      // No request
-      addr_align_err = 1'b0;
-    end
-  end
+  //    Only word-align is accepted based on comportability spec.
+  assign addr_align_err = (wr_req) && |tl_h2d_i.a_address[1:0];
+
+  // Don't allow unsupported values.
+  assign malformed_meta_err = tl_a_user_chk(tl_h2d_i.a_user);
 
   // tl_err : separate checker
   tlul_err u_err (
     .clk_i,
     .rst_ni,
-    .tl_i,
+    .tl_i(tl_h2d_i),
     .err_o (tl_err)
   );
 
-  `ASSERT_INIT(MatchedWidthAssert, RegDw == top_pkg::TL_DW)
+  `ASSERT_INIT(DwMatchedWithTl_A, DmiDw == top_pkg::TL_DW)
+  `ASSERT_INIT(DwMatchedWithDm_A, DmiDw == $bits(dmi_req_o.data))
+  `ASSERT_INIT(AwIsMatchedWithDm, DmiAw == $bits(dmi_req_o.addr))
 
-endmodule
+endmodule : tlul_adapter_dmi
