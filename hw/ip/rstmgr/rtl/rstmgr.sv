3936d5bf30055f1f48e15f7d1e0ff93c43e6caef
Emmanuel Blot <eblot@rivosinc.com>
2023-08-11 16:42:19 +0200

    [hw,rstmgr] fix typo
    Signed-off-by: Emmanuel Blot <eblot@rivosinc.com>

--- hw/ip/rstmgr/rtl/rstmgr.sv
+++ hw/ip/rstmgr/rtl/rstmgr.sv
@@ -614,7 +614,7 @@ module rstmgr
   end
 
   // once dump is captured, no more information is captured until
-  // re-eanbled by software.
+  // re-enabled by software.
   assign hw2reg.alert_info_ctrl.en.d  = 1'b0;
   assign hw2reg.alert_info_ctrl.en.de = dump_capture;
 
