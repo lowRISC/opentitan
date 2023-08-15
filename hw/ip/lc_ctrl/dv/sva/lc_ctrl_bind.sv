2e9eb6d94b0ae63d8343e5ad1b54bd3126b3acd2
Michael Schaffner <msf@opentitan.org>
2023-08-15 00:04:42 -0700

    [lc_ctrl/dv] Update DV environment
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/lc_ctrl/dv/sva/lc_ctrl_bind.sv
+++ hw/ip/lc_ctrl/dv/sva/lc_ctrl_bind.sv
@@ -6,11 +6,20 @@ module lc_ctrl_bind;
 
   bind lc_ctrl tlul_assert #(
     .EndpointType("Device")
-  ) tlul_assert_device (
+  ) tlul_assert_device_regs (
     .clk_i,
     .rst_ni,
     .h2d  (tl_i),
     .d2h  (tl_o)
+  );
+
+  bind lc_ctrl tlul_assert #(
+    .EndpointType("Device")
+  ) tlul_assert_device_dmi (
+    .clk_i,
+    .rst_ni,
+    .h2d  (dmi_tl_h2d_i),
+    .d2h  (dmi_tl_d2h_o)
   );
 
   bind lc_ctrl lc_ctrl_csr_assert_fpv lc_ctrl_csr_assert (
