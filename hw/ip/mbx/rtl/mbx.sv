1c6c11741494698b466088c79f32bc9039534a5c
Robert Schilling <rschilling@rivosinc.com>
2023-08-17 13:15:42 -0700

    [hw,mbx,rtl] Implementation of the host interface
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/mbx/rtl/mbx.sv
+++ hw/ip/mbx/rtl/mbx.sv
@@ -6,19 +6,93 @@
 
 module mbx
   import tlul_pkg::*;
-(
-  input  logic                  clk_i,
-  input  logic                  rst_ni,
-  output logic                  irq_o,
+  import mbx_reg_pkg::*;
+#(
+  parameter logic [NumAlerts-1:0] AlertAsyncOn = {NumAlerts{1'b1}},
+  parameter int unsigned CfgSramAddrWidth      = 32,
+  parameter int unsigned CfgSramDataWidth      = 32
+) (
+  input  logic                                       clk_i,
+  input  logic                                      rst_ni,
+  output logic                                      intr_mbx_ready_o,
+  // Alerts
+  input  prim_alert_pkg::alert_rx_t [NumAlerts-1:0] alert_rx_i,
+  output prim_alert_pkg::alert_tx_t [NumAlerts-1:0] alert_tx_o,
   // Device port facing OT-host
-  input   tlul_pkg::tl_h2d_t    tl_host_i,
-  output  tlul_pkg::tl_d2h_t    tl_host_o,
+  input   tlul_pkg::tl_h2d_t                        tl_host_i,
+  output  tlul_pkg::tl_d2h_t                        tl_host_o,
   // Device port facing CTN Xbar
-  input   tlul_pkg::tl_h2d_t    tl_sys_i,
-  output  tlul_pkg::tl_d2h_t    tl_sys_o,
+  input   tlul_pkg::tl_h2d_t                        tl_sys_i,
+  output  tlul_pkg::tl_d2h_t                        tl_sys_o,
   // Host port to access private SRAM
-  input   tlul_pkg::tl_d2h_t    tl_sram_i,
-  output  tlul_pkg::tl_h2d_t    tl_sram_o
+  input   tlul_pkg::tl_d2h_t                        tl_sram_i,
+  output  tlul_pkg::tl_h2d_t                        tl_sram_o
 );
+  logic [CfgSramAddrWidth-1:0] ib_write_ptr;
+  logic [CfgSramAddrWidth-1:0] ob_read_ptr;
+
+  // External write signals for control and status register
+  logic hostif_set_control_abort;
+  logic hostif_clear_status_busy;
+  logic hostif_set_status_error, hostif_clear_status_error;
+  logic hostif_set_status_async_msg_status;
+  // External read signals for control and status register
+  logic hostif_status_busy, hostif_status_error;
+  logic hostif_status_async_msg_status, hostif_status_ready;
+
+  logic [CfgSramAddrWidth-1:0] ibmbx_hostif_sram_write_ptr;
+  logic [CfgSramDataWidth-1:0] ibmbx_write_data;
+  logic [CfgSramAddrWidth-1:0] obmbx_hostif_sram_read_ptr;
+  logic [CfgSramDataWidth-1:0] obmbx_hostif_sram_read_resp;
+
+  logic hostif_address_range_valid;
+  logic [CfgSramAddrWidth-1:0] hostif_ib_base, hostif_ib_limit;
+  logic [CfgSramAddrWidth-1:0] hostif_ob_base, hostif_ob_limit;
+
+  logic hostif_write_ob_object_size, hostif_read_ob_object_size;
+  logic [11:0] hostif_ob_object_size_wdata, hostif_ob_object_size_rdata;
+
+  logic sysif_write_control_abort;
+
+  mbx_hostif #(
+    .AlertAsyncOn    ( AlertAsyncOn     ),
+    .CfgSramAddrWidth( CfgSramAddrWidth )
+  ) u_hostif (
+    .clk_i                               ( clk_i                              ),
+    .rst_ni                              ( rst_ni                             ),
+    // Device port to the host side
+    .tl_host_i                           ( tl_host_i                          ),
+    .tl_host_o                           ( tl_host_o                          ),
+    .alert_rx_i                          ( alert_rx_i                         ),
+    .alert_tx_o                          ( alert_tx_o                         ),
+    // Access to the control register
+    .hostif_set_control_abort_o          ( hostif_set_control_abort           ),
+    // Access to the status register
+    .hostif_clear_status_busy_o          ( hostif_clear_status_busy           ),
+    .hostif_set_status_error_o           ( hostif_set_status_error            ),
+    .hostif_clear_status_error_o         ( hostif_clear_status_error          ),
+    .hostif_set_status_async_msg_status_o( hostif_set_status_async_msg_status ),
+    .hostif_status_busy_i                ( hostif_status_busy                 ),
+    .hostif_status_error_i               ( hostif_status_error                ),
+    .hostif_status_async_msg_status_i    ( hostif_status_async_msg_status     ),
+    .hostif_status_ready_i               ( hostif_status_ready                ),
+    // Access to the IB/OB RD/WR Pointers
+    .hostif_ib_write_ptr_i               ( ibmbx_hostif_sram_write_ptr        ),
+    .hostif_ob_read_ptr_i                ( obmbx_hostif_sram_read_ptr         ),
+    // Access to the memory region registers
+    .hostif_address_range_valid_o        ( hostif_address_range_valid         ),
+    .hostif_ib_base_o                    ( hostif_ib_base                     ),
+    .hostif_ib_limit_o                   ( hostif_ib_limit                    ),
+    .hostif_ob_base_o                    ( hostif_ob_base                     ),
+    .hostif_ob_limit_o                   ( hostif_ob_limit                    ),
+    // Read/Write access for the OB DW Count register
+    .hostif_write_ob_object_size_o      ( hostif_write_ob_object_size         ),
+    .hostif_ob_object_size_o            ( hostif_ob_object_size_wdata         ),
+    .hostif_ob_object_size_i            ( hostif_ob_object_size_rdata         ),
+    .hostif_read_ob_object_size_i       ( hostif_read_ob_object_size          ),
+    // Control inputs coming from the system registers interface
+    .sysif_write_control_abort_i         ( sysif_write_control_abort          )
+  );
+
 
 endmodule
--- hw/ip/mbx/rtl/mbx_reg_pkg.sv
+++ hw/ip/mbx/rtl/mbx_reg_pkg.sv
@@ -6,8 +6,11 @@
 
 package mbx_reg_pkg;
 
+  // Param list
+  parameter int NumAlerts = 1;
+
   // Address widths within the block
-  parameter int HostAw = 4;
+  parameter int HostAw = 6;
   parameter int SysAw = 1;
 
   ///////////////////////////////////////////////
@@ -28,43 +31,213 @@ package mbx_reg_pkg;
   } mbx_reg2hw_intr_test_reg_t;
 
   typedef struct packed {
+    logic        q;
+    logic        qe;
+  } mbx_reg2hw_alert_test_reg_t;
+
+  typedef struct packed {
+    logic        q;
+    logic        qe;
+  } mbx_reg2hw_control_reg_t;
+
+  typedef struct packed {
+    struct packed {
+      logic        q;
+      logic        qe;
+    } busy;
+    struct packed {
+      logic        q;
+      logic        qe;
+    } error;
+    struct packed {
+      logic        q;
+      logic        qe;
+    } async_msg_status;
+    struct packed {
+      logic        q;
+      logic        qe;
+    } ready;
+  } mbx_reg2hw_status_reg_t;
+
+  typedef struct packed {
+    logic        q;
+  } mbx_reg2hw_address_range_regwen_reg_t;
+
+  typedef struct packed {
+    logic        q;
+  } mbx_reg2hw_address_range_valid_reg_t;
+
+  typedef struct packed {
+    logic [29:0] q;
+    logic        qe;
+  } mbx_reg2hw_inbound_base_address_reg_t;
+
+  typedef struct packed {
+    logic [29:0] q;
+    logic        qe;
+  } mbx_reg2hw_inbound_limit_address_reg_t;
+
+  typedef struct packed {
+    logic [29:0] q;
+    logic        qe;
+  } mbx_reg2hw_inbound_write_ptr_reg_t;
+
+  typedef struct packed {
+    logic [29:0] q;
+    logic        qe;
+  } mbx_reg2hw_outbound_base_address_reg_t;
+
+  typedef struct packed {
+    logic [29:0] q;
+    logic        qe;
+  } mbx_reg2hw_outbound_limit_address_reg_t;
+
+  typedef struct packed {
+    logic [29:0] q;
+    logic        qe;
+  } mbx_reg2hw_outbound_read_ptr_reg_t;
+
+  typedef struct packed {
+    logic [10:0] q;
+    logic        qe;
+  } mbx_reg2hw_outbound_object_size_reg_t;
+
+  typedef struct packed {
     logic        d;
     logic        de;
   } mbx_hw2reg_intr_state_reg_t;
 
+  typedef struct packed {
+    logic        d;
+  } mbx_hw2reg_control_reg_t;
+
+  typedef struct packed {
+    struct packed {
+      logic        d;
+    } busy;
+    struct packed {
+      logic        d;
+    } error;
+    struct packed {
+      logic        d;
+    } async_msg_status;
+    struct packed {
+      logic        d;
+    } ready;
+  } mbx_hw2reg_status_reg_t;
+
+  typedef struct packed {
+    logic [29:0] d;
+  } mbx_hw2reg_inbound_write_ptr_reg_t;
+
+  typedef struct packed {
+    logic [29:0] d;
+  } mbx_hw2reg_outbound_read_ptr_reg_t;
+
+  typedef struct packed {
+    logic [10:0] d;
+    logic        de;
+  } mbx_hw2reg_outbound_object_size_reg_t;
+
   // Register -> HW type for host interface
   typedef struct packed {
-    mbx_reg2hw_intr_state_reg_t intr_state; // [3:3]
-    mbx_reg2hw_intr_enable_reg_t intr_enable; // [2:2]
-    mbx_reg2hw_intr_test_reg_t intr_test; // [1:0]
+    mbx_reg2hw_intr_state_reg_t intr_state; // [215:215]
+    mbx_reg2hw_intr_enable_reg_t intr_enable; // [214:214]
+    mbx_reg2hw_intr_test_reg_t intr_test; // [213:212]
+    mbx_reg2hw_alert_test_reg_t alert_test; // [211:210]
+    mbx_reg2hw_control_reg_t control; // [209:208]
+    mbx_reg2hw_status_reg_t status; // [207:200]
+    mbx_reg2hw_address_range_regwen_reg_t address_range_regwen; // [199:199]
+    mbx_reg2hw_address_range_valid_reg_t address_range_valid; // [198:198]
+    mbx_reg2hw_inbound_base_address_reg_t inbound_base_address; // [197:167]
+    mbx_reg2hw_inbound_limit_address_reg_t inbound_limit_address; // [166:136]
+    mbx_reg2hw_inbound_write_ptr_reg_t inbound_write_ptr; // [135:105]
+    mbx_reg2hw_outbound_base_address_reg_t outbound_base_address; // [104:74]
+    mbx_reg2hw_outbound_limit_address_reg_t outbound_limit_address; // [73:43]
+    mbx_reg2hw_outbound_read_ptr_reg_t outbound_read_ptr; // [42:12]
+    mbx_reg2hw_outbound_object_size_reg_t outbound_object_size; // [11:0]
   } mbx_host_reg2hw_t;
 
   // HW -> register type for host interface
   typedef struct packed {
-    mbx_hw2reg_intr_state_reg_t intr_state; // [1:0]
+    mbx_hw2reg_intr_state_reg_t intr_state; // [78:77]
+    mbx_hw2reg_control_reg_t control; // [76:76]
+    mbx_hw2reg_status_reg_t status; // [75:72]
+    mbx_hw2reg_inbound_write_ptr_reg_t inbound_write_ptr; // [71:42]
+    mbx_hw2reg_outbound_read_ptr_reg_t outbound_read_ptr; // [41:12]
+    mbx_hw2reg_outbound_object_size_reg_t outbound_object_size; // [11:0]
   } mbx_host_hw2reg_t;
 
   // Register offsets for host interface
-  parameter logic [HostAw-1:0] MBX_INTR_STATE_OFFSET = 4'h 0;
-  parameter logic [HostAw-1:0] MBX_INTR_ENABLE_OFFSET = 4'h 4;
-  parameter logic [HostAw-1:0] MBX_INTR_TEST_OFFSET = 4'h 8;
+  parameter logic [HostAw-1:0] MBX_INTR_STATE_OFFSET = 6'h 0;
+  parameter logic [HostAw-1:0] MBX_INTR_ENABLE_OFFSET = 6'h 4;
+  parameter logic [HostAw-1:0] MBX_INTR_TEST_OFFSET = 6'h 8;
+  parameter logic [HostAw-1:0] MBX_ALERT_TEST_OFFSET = 6'h c;
+  parameter logic [HostAw-1:0] MBX_CONTROL_OFFSET = 6'h 10;
+  parameter logic [HostAw-1:0] MBX_STATUS_OFFSET = 6'h 14;
+  parameter logic [HostAw-1:0] MBX_ADDRESS_RANGE_REGWEN_OFFSET = 6'h 18;
+  parameter logic [HostAw-1:0] MBX_ADDRESS_RANGE_VALID_OFFSET = 6'h 1c;
+  parameter logic [HostAw-1:0] MBX_INBOUND_BASE_ADDRESS_OFFSET = 6'h 20;
+  parameter logic [HostAw-1:0] MBX_INBOUND_LIMIT_ADDRESS_OFFSET = 6'h 24;
+  parameter logic [HostAw-1:0] MBX_INBOUND_WRITE_PTR_OFFSET = 6'h 28;
+  parameter logic [HostAw-1:0] MBX_OUTBOUND_BASE_ADDRESS_OFFSET = 6'h 2c;
+  parameter logic [HostAw-1:0] MBX_OUTBOUND_LIMIT_ADDRESS_OFFSET = 6'h 30;
+  parameter logic [HostAw-1:0] MBX_OUTBOUND_READ_PTR_OFFSET = 6'h 34;
+  parameter logic [HostAw-1:0] MBX_OUTBOUND_OBJECT_SIZE_OFFSET = 6'h 38;
 
   // Reset values for hwext registers and their fields for host interface
   parameter logic [0:0] MBX_INTR_TEST_RESVAL = 1'h 0;
   parameter logic [0:0] MBX_INTR_TEST_MBX_READY_RESVAL = 1'h 0;
+  parameter logic [0:0] MBX_ALERT_TEST_RESVAL = 1'h 0;
+  parameter logic [0:0] MBX_ALERT_TEST_FATAL_FAULT_RESVAL = 1'h 0;
+  parameter logic [0:0] MBX_CONTROL_RESVAL = 1'h 0;
+  parameter logic [0:0] MBX_CONTROL_ABORT_RESVAL = 1'h 0;
+  parameter logic [31:0] MBX_STATUS_RESVAL = 32'h 1;
+  parameter logic [0:0] MBX_STATUS_BUSY_RESVAL = 1'h 1;
+  parameter logic [0:0] MBX_STATUS_ERROR_RESVAL = 1'h 0;
+  parameter logic [0:0] MBX_STATUS_ASYNC_MSG_STATUS_RESVAL = 1'h 0;
+  parameter logic [0:0] MBX_STATUS_READY_RESVAL = 1'h 0;
+  parameter logic [31:0] MBX_INBOUND_WRITE_PTR_RESVAL = 32'h 0;
+  parameter logic [29:0] MBX_INBOUND_WRITE_PTR_INBOUND_READ_PTR_RESVAL = 30'h 0;
+  parameter logic [31:0] MBX_OUTBOUND_READ_PTR_RESVAL = 32'h 0;
+  parameter logic [29:0] MBX_OUTBOUND_READ_PTR_OUTBOUND_WRITE_PTR_RESVAL = 30'h 0;
 
   // Register index for host interface
   typedef enum int {
     MBX_INTR_STATE,
     MBX_INTR_ENABLE,
-    MBX_INTR_TEST
+    MBX_INTR_TEST,
+    MBX_ALERT_TEST,
+    MBX_CONTROL,
+    MBX_STATUS,
+    MBX_ADDRESS_RANGE_REGWEN,
+    MBX_ADDRESS_RANGE_VALID,
+    MBX_INBOUND_BASE_ADDRESS,
+    MBX_INBOUND_LIMIT_ADDRESS,
+    MBX_INBOUND_WRITE_PTR,
+    MBX_OUTBOUND_BASE_ADDRESS,
+    MBX_OUTBOUND_LIMIT_ADDRESS,
+    MBX_OUTBOUND_READ_PTR,
+    MBX_OUTBOUND_OBJECT_SIZE
   } mbx_host_id_e;
 
   // Register width information to check illegal writes for host interface
-  parameter logic [3:0] MBX_HOST_PERMIT [3] = '{
-    4'b 0001, // index[0] MBX_INTR_STATE
-    4'b 0001, // index[1] MBX_INTR_ENABLE
-    4'b 0001  // index[2] MBX_INTR_TEST
+  parameter logic [3:0] MBX_HOST_PERMIT [15] = '{
+    4'b 0001, // index[ 0] MBX_INTR_STATE
+    4'b 0001, // index[ 1] MBX_INTR_ENABLE
+    4'b 0001, // index[ 2] MBX_INTR_TEST
+    4'b 0001, // index[ 3] MBX_ALERT_TEST
+    4'b 0001, // index[ 4] MBX_CONTROL
+    4'b 1111, // index[ 5] MBX_STATUS
+    4'b 0001, // index[ 6] MBX_ADDRESS_RANGE_REGWEN
+    4'b 0001, // index[ 7] MBX_ADDRESS_RANGE_VALID
+    4'b 1111, // index[ 8] MBX_INBOUND_BASE_ADDRESS
+    4'b 1111, // index[ 9] MBX_INBOUND_LIMIT_ADDRESS
+    4'b 1111, // index[10] MBX_INBOUND_WRITE_PTR
+    4'b 1111, // index[11] MBX_OUTBOUND_BASE_ADDRESS
+    4'b 1111, // index[12] MBX_OUTBOUND_LIMIT_ADDRESS
+    4'b 1111, // index[13] MBX_OUTBOUND_READ_PTR
+    4'b 0011  // index[14] MBX_OUTBOUND_OBJECT_SIZE
   };
 
 endpackage
