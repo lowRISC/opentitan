7d3bea030e536b05e2d65213e0a91be68ce1bc87
Chris Frantz <cfrantz@google.com>
2023-08-21 11:09:07 -0700

    [tock] Create a basic `tock` test
    1. Create a basic test that runs under tock.  Tests that run with the
       default `opentitantool` test harness should emit `PASS` or `FAIL` to
       indicate their status.
    2. Create a build setting that can select between the upstream and
       downstream tock kernel.  Defaults to the downstream kernel.
       ```
       bazel test \
           --//sw/device/silicon_owner/tock/tests:kernel=<upstream|downstream> \
           <label-of-the-test>
       ```
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ sw/device/silicon_owner/tock/tests/basic/src/basic.rs
@@ -0,0 +1,19 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#![no_main]
+#![no_std]
+use core::fmt::Write;
+use libtock::console::Console;
+use libtock::runtime::{set_main, stack_size};
+
+set_main!(main);
+stack_size!(0x200);
+
+fn main() {
+    write!(Console::writer(), "Hello world!\r\n").unwrap();
+    // opentitan_functest's default test harness looks for `PASS` or `FAIL` in
+    // the test output to determine the test result.
+    write!(Console::writer(), "PASS!\r\n").unwrap();
+}
