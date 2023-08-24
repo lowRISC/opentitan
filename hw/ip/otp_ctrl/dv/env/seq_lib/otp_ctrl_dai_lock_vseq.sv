74021db9ee72e39bd8ca860980e497926f314e35
Michael Schaffner <msf@opentitan.org>
2023-08-23 18:03:48 -0700

    [otp_ctrl] Fix OOB error in DAI
    We did not hit this corner case before since the OTP array
    was fully populated. The new parameterization is not yet
    fully complete and may not be fully populated, hence we
    should add an OOB check.
    
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/otp_ctrl/dv/env/seq_lib/otp_ctrl_dai_lock_vseq.sv
+++ hw/ip/otp_ctrl/dv/env/seq_lib/otp_ctrl_dai_lock_vseq.sv
@@ -33,12 +33,12 @@ class otp_ctrl_dai_lock_vseq extends otp_ctrl_smoke_vseq;
     if (part_idx == Secret0Idx)      dai_addr inside `PART_ADDR_RANGE(Secret0Idx);
     if (part_idx == Secret1Idx)      dai_addr inside `PART_ADDR_RANGE(Secret1Idx);
     if (part_idx == Secret2Idx)      dai_addr inside `PART_ADDR_RANGE(Secret2Idx);
-    if (part_idx == LifeCycleIdx && write_unused_addr) {
+    if (part_idx == LifeCycleIdx) {
       if (write_unused_addr) {
         // Dai address input is only 11 bits wide.
         dai_addr inside {[PartInfo[LifeCycleIdx].offset : {11{1'b1}}]};
       } else {
-        dai_addr inside {[PartInfo[LifeCycleIdx].offset : '1]};
+        dai_addr inside `PART_ADDR_RANGE(LifeCycleIdx);
       }
     }
     solve part_idx before dai_addr;
