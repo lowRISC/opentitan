dea64dfab9a8cf1a642b7beb09dc1c2a0e6ac437
Michael Schaffner <msf@opentitan.org>
2023-08-24 19:35:19 -0700

    [prim] Add two-port memory implementation
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/ip/prim_generic/rtl/prim_generic_ram_1r1w.sv
+++ hw/ip/prim_generic/rtl/prim_generic_ram_1r1w.sv
@@ -2,11 +2,11 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 //
-// Synchronous dual-port SRAM register model
+// Synchronous two-port SRAM register model
 //   This module is for simulation and small size SRAM.
 //   Implementing ECC should be done inside wrapper not this model.
 `include "prim_assert.sv"
-module prim_generic_ram_2p import prim_ram_2p_pkg::*; #(
+module prim_generic_ram_1r1w import prim_ram_2p_pkg::*; #(
   parameter  int Width           = 32, // bit
   parameter  int Depth           = 128,
   parameter  int DataBitsPerMask = 1, // Number of data bits per bit of write mask
@@ -17,19 +17,15 @@ module prim_generic_ram_2p import prim_ram_2p_pkg::*; #(
   input clk_a_i,
   input clk_b_i,
 
+  // Port A can only write
   input                    a_req_i,
-  input                    a_write_i,
   input        [Aw-1:0]    a_addr_i,
   input        [Width-1:0] a_wdata_i,
   input  logic [Width-1:0] a_wmask_i,
-  output logic [Width-1:0] a_rdata_o,
 
-
+  // Port B can only read
   input                    b_req_i,
-  input                    b_write_i,
   input        [Aw-1:0]    b_addr_i,
-  input        [Width-1:0] b_wdata_i,
-  input  logic [Width-1:0] b_wmask_i,
   output logic [Width-1:0] b_rdata_o,
 
   input ram_2p_cfg_t       cfg_i
@@ -40,7 +36,7 @@ module prim_generic_ram_2p import prim_ram_2p_pkg::*; #(
 // these are going to be simulated using plain RTL models in netlist simulations. This can be done
 // by analyzing and elaborating the design, and then removing the memory submodules before writing
 // out the verilog netlist. However, memory arrays can take a long time to elaborate, and in case
-// of dual port rams they can even trigger elab errors due to multiple processes writing to the
+// of two port rams they can even trigger elab errors due to multiple processes writing to the
 // same memory variable concurrently. To this end, we exclude the entire logic in this module in
 // these runs with the following macro.
 `ifndef SYNTHESIS_MEMORY_BLACK_BOXING
@@ -54,51 +50,32 @@ module prim_generic_ram_2p import prim_ram_2p_pkg::*; #(
 
   logic [Width-1:0]     mem [Depth];
   logic [MaskWidth-1:0] a_wmask;
-  logic [MaskWidth-1:0] b_wmask;
-
   for (genvar k = 0; k < MaskWidth; k++) begin : gen_wmask
     assign a_wmask[k] = &a_wmask_i[k*DataBitsPerMask +: DataBitsPerMask];
-    assign b_wmask[k] = &b_wmask_i[k*DataBitsPerMask +: DataBitsPerMask];
 
     // Ensure that all mask bits within a group have the same value for a write
     `ASSERT(MaskCheckPortA_A, a_req_i && a_write_i |->
         a_wmask_i[k*DataBitsPerMask +: DataBitsPerMask] inside {{DataBitsPerMask{1'b1}}, '0},
         clk_a_i, '0)
-    `ASSERT(MaskCheckPortB_A, b_req_i && b_write_i |->
-        b_wmask_i[k*DataBitsPerMask +: DataBitsPerMask] inside {{DataBitsPerMask{1'b1}}, '0},
-        clk_b_i, '0)
   end
 
-  // Xilinx FPGA specific Dual-port RAM coding style
+  // Xilinx FPGA specific Two-port RAM coding style
   // using always instead of always_ff to avoid 'ICPD  - illegal combination of drivers' error
   // thrown due to 'mem' being driven by two always processes below
   always @(posedge clk_a_i) begin
     if (a_req_i) begin
-      if (a_write_i) begin
-        for (int i=0; i < MaskWidth; i = i + 1) begin
-          if (a_wmask[i]) begin
-            mem[a_addr_i][i*DataBitsPerMask +: DataBitsPerMask] <=
-              a_wdata_i[i*DataBitsPerMask +: DataBitsPerMask];
-          end
+      for (int i=0; i < MaskWidth; i = i + 1) begin
+        if (a_wmask[i]) begin
+          mem[a_addr_i][i*DataBitsPerMask +: DataBitsPerMask] <=
+            a_wdata_i[i*DataBitsPerMask +: DataBitsPerMask];
         end
-      end else begin
-        a_rdata_o <= mem[a_addr_i];
       end
     end
   end
 
   always @(posedge clk_b_i) begin
     if (b_req_i) begin
-      if (b_write_i) begin
-        for (int i=0; i < MaskWidth; i = i + 1) begin
-          if (b_wmask[i]) begin
-            mem[b_addr_i][i*DataBitsPerMask +: DataBitsPerMask] <=
-              b_wdata_i[i*DataBitsPerMask +: DataBitsPerMask];
-          end
-        end
-      end else begin
-        b_rdata_o <= mem[b_addr_i];
-      end
+      b_rdata_o <= mem[b_addr_i];
     end
   end
 
