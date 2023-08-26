48fb9777f16bd9fbc36952c09498a2f62fe5762d
Andreas Kurth <adk@lowrisc.org>
2023-08-26 05:27:52 +0000

    [sw/soc_proxy] Add scaffold of DIF
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

---
+++ sw/ip/soc_proxy/dif/dif_soc_proxy.h
@@ -0,0 +1,10 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_IP_SOC_PROXY_DIF_DIF_SOC_PROXY_H_
+#define OPENTITAN_SW_IP_SOC_PROXY_DIF_DIF_SOC_PROXY_H_
+
+#include "sw/ip/soc_proxy/dif/autogen/dif_soc_proxy_autogen.h"
+
+#endif  // OPENTITAN_SW_IP_SOC_PROXY_DIF_DIF_SOC_PROXY_H_
