83ae752b9701087721311de70ee2d28d76d020aa
Tim Trippel <ttrippel@google.com>
2023-08-31 14:51:39 -0700

    [manuf] add composible command interface to CP provisioning binary
    With the ability to inject commands via the ujson framework to the
    device side, the host test harness can piece together commands to
    restrict the provisioning actions taken on the device side. This will
    enable initial bring-up or failed parts to be incremetally provisioned.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

---
+++ sw/device/lib/testing/json/provisioning_command.h
@@ -0,0 +1,29 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_TESTING_JSON_PROVISIONING_COMMAND_H_
+#define OPENTITAN_SW_DEVICE_LIB_TESTING_JSON_PROVISIONING_COMMAND_H_
+
+#include "sw/device/lib/ujson/ujson_derive.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// clang-format off
+
+#define ENUM_CP_PROVISIONING_COMMAND(_, value) \
+    value(_, EraseAndWriteAll) \
+    value(_, FlashInfoEraseDeviceIdAndManufState) \
+    value(_, FlashInfoWriteDeviceIdAndManufState) \
+    value(_, FlashInfoEraseWaferAuthSecret) \
+    value(_, FlashInfoWriteWaferAuthSecret) \
+    value(_, OtpSecret0WriteAndLock) \
+    value(_, Done)
+UJSON_SERDE_ENUM(CpProvisioningCommand, cp_provisioning_command_t, ENUM_CP_PROVISIONING_COMMAND);
+
+// clang-format on
+#ifdef __cplusplus
+}
+#endif
+#endif  // OPENTITAN_SW_DEVICE_LIB_TESTING_JSON_PROVISIONING_COMMAND_H_
---
+++ sw/host/tests/manuf/cp_provision_init/src/provisioning_command.rs
@@ -0,0 +1,6 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// Bring in the auto-generated sources.
+include!(env!("provisioning_command"));
