6585d934a0539531f932415a0a1f190062602c5c
Musawir Hussain <Musawir745@gmail.com>
2023-09-02 16:16:21 +0500

    [RV_DM] Updated HALTED and RESUMING test
    Fixing HALTED and RESUMING register issues in halted and resuming test.
    
    Signed-off-by: Musawir Hussain <Musawir745@gmail.com>

--- hw/ip/rv_dm/dv/env/seq_lib/rv_dm_mem_tl_access_resuming_vseq.sv
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_mem_tl_access_resuming_vseq.sv
@@ -22,11 +22,13 @@ class rv_dm_mem_tl_access_resuming_vseq extends rv_dm_base_vseq;
       wdata = $urandom_range(0, 1);
       // Verify that writing to RESUMING results in anyresumeack and allresumeack to be set,
       //when resumereq bit is set it will clear the anyresumeack and allresumeack.
-      csr_wr(.ptr(tl_mem_ral.halted), .value(wdata));
+      csr_wr(.ptr(tl_mem_ral.halted), .value(0));
+      csr_rd(.ptr(jtag_dmi_ral.dmstatus), .value(rdata));
+      `DV_CHECK_EQ(1, get_field_val(jtag_dmi_ral.dmstatus.allhalted, rdata))
+      cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
+      csr_wr(.ptr(tl_mem_ral.resuming), .value(0));
+      cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
       csr_wr(.ptr(jtag_dmi_ral.dmcontrol.resumereq), .value(wdata));
-      cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
-      csr_wr(.ptr(tl_mem_ral.resuming), .value(wdata));
-      cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
       csr_rd(.ptr(jtag_dmi_ral.dmstatus), .value(rdata));
       `DV_CHECK_EQ(!wdata, get_field_val(jtag_dmi_ral.dmstatus.anyresumeack, rdata))
       `DV_CHECK_EQ(!wdata, get_field_val(jtag_dmi_ral.dmstatus.allresumeack, rdata))
