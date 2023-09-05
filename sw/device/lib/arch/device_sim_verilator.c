a42c29b3e4e07ce301be7d4c45b39cc2750c47bc
Tim Trippel <ttrippel@google.com>
2023-09-05 16:34:32 -0700

    [test_rom] align clk jitter enablement between test ROM and ROM
    The test ROM now reads the jittery clk enablement field from OTP to
    perfrom the enablement, same as the ROM. This fixes #19580.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/device/lib/arch/device_sim_verilator.c
+++ sw/device/lib/arch/device_sim_verilator.c
@@ -52,6 +52,4 @@ const uintptr_t kDeviceTestStatusAddress = 0x411f0080;
 
 const uintptr_t kDeviceLogBypassUartAddress = 0;
 
-const bool kJitterEnabled = false;
-
 void device_fpga_version_print(void) {}
