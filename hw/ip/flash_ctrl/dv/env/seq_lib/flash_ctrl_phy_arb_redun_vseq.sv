54c0437d8f602b5110c81683d72ea158568aec70
Jaedon Kim <jdonjdon@opentitan.org>
2023-09-06 18:02:45 +0000

    [flash_ctrl,dv] fix cs phy_arb_redun test
    override constraint to reduce program duration
    for frequent error injection
    
    Signed-off-by: Jaedon Kim <jdonjdon@opentitan.org>

--- hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_phy_arb_redun_vseq.sv
+++ hw/ip/flash_ctrl/dv/env/seq_lib/flash_ctrl_phy_arb_redun_vseq.sv
@@ -6,12 +6,16 @@ class flash_ctrl_phy_arb_redun_vseq extends flash_ctrl_err_base_vseq;
   `uvm_object_utils(flash_ctrl_phy_arb_redun_vseq)
   `uvm_object_new
 
+  constraint ctrl_num_c {
+    ctrl_num dist { CTRL_TRANS_MIN := 2, [2:16] :/ 1};
+  }
+
   task run_error_event();
     int delay;
     string path = {"tb.dut.u_eflash.gen_flash_cores[0].",
                    "u_core.u_host_arb.gen_input_bufs[1].u_req_buf.out_o[1:0]"};
     cfg.scb_h.expected_alert["fatal_err"].expected = 1;
-    cfg.scb_h.expected_alert["fatal_err"].max_delay = 2000;
+    cfg.scb_h.expected_alert["fatal_err"].max_delay = cfg.seq_cfg.long_fatal_err_delay;
     cfg.scb_h.exp_alert_contd["fatal_err"] = 10000;
 
     // unit 100 ns;
