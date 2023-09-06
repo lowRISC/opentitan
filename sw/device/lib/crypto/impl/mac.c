0590b2c62bc97fe11099fc8db576c5b90cc9c075
Jade Philipoom <jadep@google.com>
2023-09-06 17:49:10 +0200

    [crypto] Add support for HMAC with larger SHA2s.
    Adds HMAC-SHA384 and HMAC-SHA512.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

--- sw/device/lib/crypto/impl/mac.c
+++ sw/device/lib/crypto/impl/mac.c
@@ -9,93 +9,12 @@
 #include "sw/device/lib/crypto/impl/integrity.h"
 #include "sw/device/lib/crypto/impl/keyblob.h"
 #include "sw/device/lib/crypto/impl/sha2/sha256.h"
+#include "sw/device/lib/crypto/impl/sha2/sha512.h"
 #include "sw/device/lib/crypto/impl/status.h"
+#include "sw/device/lib/crypto/include/hash.h"
 
 // Module ID for status codes.
 #define MODULE_ID MAKE_MODULE_ID('m', 'a', 'c')
-
-/**
- * Ensure that the hmac context is large enough.
- *
- * The HMAC context should hold one SHA-256 message block and a SHA-256 state
- * object.
- */
-static_assert(
-    sizeof(hmac_context_t) == kSha256MessageBlockBytes + sizeof(sha256_state_t),
-    "hmac_context_t must be the same size as a key block and a SHA-256 state");
-/**
- * Ensure that the SHA-256 state struct is suitable for `hardened_memcpy()`.
- */
-static_assert(sizeof(sha256_state_t) % sizeof(uint32_t) == 0,
-              "Size of sha256_state_t must be a multiple of the word size for "
-              "`hardened_memcpy()`");
-
-enum {
-  /**
-   * Word-offset of the key block within the HMAC context object.
-   */
-  kHmacContextKeyBlockWordOffset = 0,
-  /**
-   * Word-offset of the SHA-256 state within the HMAC context object.
-   */
-  kHmacContextSha256StateWordOffset = kSha256MessageBlockWords,
-};
-
-/**
- * Save a SHA-256 state to an HMAC context.
- *
- * @param[out] ctx HMAC context to copy to.
- * @param state SHA-256 state object.
- * @param k0 Key block.
- */
-static void sha256_state_save(hmac_context_t *restrict ctx,
-                              const sha256_state_t *restrict state) {
-  // As per the `hardened_memcpy()` documentation, it
-  // is OK to cast to `uint32_t *` here as long as `state` is word-aligned,
-  // which it must be because all its fields are.
-  uint32_t *dest = ctx->data + kHmacContextSha256StateWordOffset;
-  hardened_memcpy(dest, (uint32_t *)state,
-                  sizeof(sha256_state_t) / sizeof(uint32_t));
-}
-
-/**
- * Restore a SHA-256 state from an HMAC context.
- *
- * @param ctx HMAC context to restore from.
- * @param[out] state Destination SHA-256 state object.
- */
-static void sha256_state_restore(const hmac_context_t *restrict ctx,
-                                 sha256_state_t *restrict state) {
-  // As per the `hardened_memcpy()` documentation, it is OK to cast to
-  // `uint32_t *` here as long as `state` is word-aligned, which it must be
-  // because all its fields are.
-  const uint32_t *src = ctx->data + kHmacContextSha256StateWordOffset;
-  hardened_memcpy((uint32_t *)state, src,
-                  sizeof(sha256_state_t) / sizeof(uint32_t));
-}
-/**
- * Save the key block to an HMAC context.
- *
- * @param[out] ctx HMAC context to copy to.
- * @param k0 Key block to save.
- */
-static void key_block_save(hmac_context_t *restrict ctx,
-                           const uint32_t *restrict k0) {
-  uint32_t *dest = ctx->data + kHmacContextKeyBlockWordOffset;
-  hardened_memcpy(dest, k0, kSha256MessageBlockWords);
-}
-
-/**
- * Restore a key block from an HMAC context.
- *
- * @param ctx HMAC context to restore from.
- * @param[out] k0 Destination key block buffer.
- */
-static void key_block_restore(const hmac_context_t *restrict ctx,
-                              uint32_t *restrict k0) {
-  const uint32_t *src = ctx->data + kHmacContextKeyBlockWordOffset;
-  hardened_memcpy(k0, src, kSha256MessageBlockWords);
-}
 
 crypto_status_t otcrypto_mac_keygen(crypto_blinded_key_t *key) {
   // TODO: Implement KMAC sideloaded key generation once we have a keymgr
@@ -107,10 +26,10 @@ crypto_status_t otcrypto_mac_keygen(crypto_blinded_key_t *key) {
 OT_WARN_UNUSED_RESULT
 crypto_status_t otcrypto_hmac(const crypto_blinded_key_t *key,
                               crypto_const_byte_buf_t input_message,
-                              crypto_word32_buf_t *tag) {
+                              hash_mode_t hash_mode, crypto_word32_buf_t *tag) {
   // Compute HMAC using the streaming API.
   hmac_context_t ctx;
-  HARDENED_TRY(otcrypto_hmac_init(&ctx, key));
+  HARDENED_TRY(otcrypto_hmac_init(&ctx, key, hash_mode));
   HARDENED_TRY(otcrypto_hmac_update(&ctx, input_message));
   return otcrypto_hmac_final(&ctx, tag);
 }
@@ -194,20 +113,54 @@ crypto_status_t otcrypto_kmac(const crypto_blinded_key_t *key,
 }
 
 crypto_status_t otcrypto_hmac_init(hmac_context_t *ctx,
-                                   const crypto_blinded_key_t *key) {
+                                   const crypto_blinded_key_t *key,
+                                   hash_mode_t hash_mode) {
   if (ctx == NULL || key == NULL || key->keyblob == NULL) {
     return OTCRYPTO_BAD_ARGS;
   }
-
   if (key->config.hw_backed != kHardenedBoolFalse) {
-    // TODO(#15590): Add support for sideloaded keys.
+    // TODO(#15590): Add support for sideloaded keys via a custom OTBN program.
+    return OTCRYPTO_NOT_IMPLEMENTED;
+  }
+  if (key->config.security_level != kSecurityLevelLow) {
+    // TODO: Harden SHA2 implementations.
     return OTCRYPTO_NOT_IMPLEMENTED;
   }
 
-  // Key mode must be HMAC-SHA256.
-  if (key->config.key_mode != kKeyModeHmacSha256) {
-    return OTCRYPTO_BAD_ARGS;
+  // Ensure the key is for HMAC and the hash function matches, and remember the
+  // digest and message block sizes.
+  size_t digest_words = 0;
+  size_t message_block_words = 0;
+  switch (key->config.key_mode) {
+    case kKeyModeHmacSha256:
+      if (hash_mode != kHashModeSha256) {
+        return OTCRYPTO_BAD_ARGS;
+      }
+      digest_words = kSha256DigestWords;
+      message_block_words = kSha256MessageBlockWords;
+      break;
+    case kKeyModeHmacSha384:
+      if (hash_mode != kHashModeSha384) {
+        return OTCRYPTO_BAD_ARGS;
+      }
+      digest_words = kSha384DigestWords;
+      // Since SHA-512 and SHA-384 have the same core, they use the same
+      // message block size.
+      message_block_words = kSha512MessageBlockWords;
+      break;
+    case kKeyModeHmacSha512:
+      if (hash_mode != kHashModeSha512) {
+        return OTCRYPTO_BAD_ARGS;
+      }
+      digest_words = kSha512DigestWords;
+      message_block_words = kSha512MessageBlockWords;
+      break;
+    default:
+      return OTCRYPTO_BAD_ARGS;
   }
+  HARDENED_CHECK_NE(digest_words, 0);
+  HARDENED_CHECK_NE(message_block_words, 0);
+  HARDENED_CHECK_LE(digest_words, message_block_words);
 
   // Check the integrity of the key.
   if (integrity_blinded_key_check(key) != kHardenedBoolTrue) {
@@ -219,55 +172,83 @@ crypto_status_t otcrypto_hmac_init(hmac_context_t *ctx,
   uint32_t *share1;
   HARDENED_TRY(keyblob_to_shares(key, &share0, &share1));
 
-  // TODO: Once we have hardened SHA-256, do not unmask the key here.
+  // TODO: Once we have hardened SHA2, do not unmask the key here.
   uint32_t unmasked_key[keyblob_share_num_words(key->config)];
   for (size_t i = 0; i < keyblob_share_num_words(key->config); i++) {
     unmasked_key[i] = share0[i] ^ share1[i];
   }
 
   // Initialize the key block, K0. See FIPS 198-1, section 4.
-  uint32_t k0[kSha256MessageBlockWords] = {0};
-  if (key->config.key_length <= kSha256MessageBlockBytes) {
-    // If the key fits into the SHA-256 block size, we just need to copy it
+  uint32_t k0[message_block_words];
+  memset(k0, 0, sizeof(k0));
+  if (key->config.key_length < (digest_words * sizeof(uint32_t))) {
+    // Key is too short.
+    return OTCRYPTO_BAD_ARGS;
+  } else if (key->config.key_length <= message_block_words * sizeof(uint32_t)) {
+    // If the key fits into the message block size, we just need to copy it
     // into the first part of K0.
     hardened_memcpy(k0, unmasked_key, ARRAYSIZE(unmasked_key));
+    // If the key size isn't a multiple of the word size, zero the last few
+    // bytes.
+    size_t offset = key->config.key_length % sizeof(uint32_t);
+    if (offset != 0) {
+      unsigned char *k0_bytes = (unsigned char *)k0;
+      size_t num_zero_bytes = sizeof(uint32_t) - offset;
+      unsigned char *dest = k0_bytes + (sizeof(unmasked_key) - num_zero_bytes);
+      memset(dest, 0, num_zero_bytes);
+    }
   } else {
-    // If the key is longer than the SHA-256 block size, we need to hash it
-    // and write the digest into the start of K0.
-    HARDENED_TRY(
-        sha256((unsigned char *)unmasked_key, key->config.key_length, k0));
+    // If the key is longer than the hash function block size, we need to hash
+    // it and write the digest into the start of K0.
+    crypto_word32_buf_t key_digest = {
+        .len = digest_words,
+        .data = k0,
+    };
+    HARDENED_TRY(otcrypto_hash(
+        (crypto_const_byte_buf_t){
+            .len = key->config.key_length,
+            .data = (unsigned char *)unmasked_key,
+        },
+        hash_mode, &key_digest));
   }
 
-  // Compute SHA256(K0 ^ ipad).
-  uint32_t inner_block[kSha256MessageBlockWords];
-  for (size_t i = 0; i < kSha256MessageBlockWords; i++) {
+  // Compute (K0 ^ ipad).
+  uint32_t inner_block[message_block_words];
+  for (size_t i = 0; i < message_block_words; i++) {
     inner_block[i] = k0[i] ^ 0x36363636;
   }
 
-  // Start computing SHA256(K0 ^ ipad) || message).
-  sha256_state_t sha256_ctx;
-  sha256_init(&sha256_ctx);
-  HARDENED_TRY(sha256_update(&sha256_ctx, (unsigned char *)inner_block,
-                             sizeof(inner_block)));
+  // Compute (K0 ^ opad).
+  uint32_t outer_block[message_block_words];
+  for (size_t i = 0; i < message_block_words; i++) {
+    outer_block[i] = k0[i] ^ 0x5c5c5c5c;
+  }
 
-  // Save the key block and SHA-256 state.
-  key_block_save(ctx, k0);
-  sha256_state_save(ctx, &sha256_ctx);
+  // Start computing inner hash = H(K0 ^ ipad) || message).
+  HARDENED_TRY(otcrypto_hash_init(&ctx->inner, hash_mode));
+  HARDENED_TRY(otcrypto_hash_update(
+      &ctx->inner,
+      (crypto_const_byte_buf_t){.len = sizeof(inner_block),
+                                .data = (unsigned char *)inner_block}));
+
+  // Start computing outer hash = H(K0 ^ opad || inner).
+  HARDENED_TRY(otcrypto_hash_init(&ctx->outer, hash_mode));
+  HARDENED_TRY(otcrypto_hash_update(
+      &ctx->outer,
+      (crypto_const_byte_buf_t){.len = sizeof(outer_block),
+                                .data = (unsigned char *)outer_block}));
 
   return OTCRYPTO_OK;
 }
 
 crypto_status_t otcrypto_hmac_update(hmac_context_t *const ctx,
                                      crypto_const_byte_buf_t input_message) {
-  if (ctx == NULL || (input_message.data == NULL && input_message.len != 0)) {
+  if (ctx == NULL) {
     return OTCRYPTO_BAD_ARGS;
   }
 
-  sha256_state_t sha256_ctx;
-  sha256_state_restore(ctx, &sha256_ctx);
-  HARDENED_TRY(
-      sha256_update(&sha256_ctx, input_message.data, input_message.len));
-  sha256_state_save(ctx, &sha256_ctx);
+  // Append the message to the inner block.
+  HARDENED_TRY(otcrypto_hash_update(&ctx->inner, input_message));
   return OTCRYPTO_OK;
 }
 
@@ -277,36 +258,17 @@ crypto_status_t otcrypto_hmac_final(hmac_context_t *const ctx,
     return OTCRYPTO_BAD_ARGS;
   }
 
-  if (tag->len != kSha256DigestWords) {
-    return OTCRYPTO_BAD_ARGS;
-  }
+  // Finalize the computation of the inner hash = H(K0 ^ ipad || message) and
+  // store it in `tag` temporarily.
+  HARDENED_TRY(otcrypto_hash_final(&ctx->inner, tag));
 
-  // Restore the SHA-256 state from the context.
-  sha256_state_t sha256_ctx;
-  sha256_state_restore(ctx, &sha256_ctx);
-
-  // Finalize the computation of SHA256(K0 ^ ipad || message).
-  uint32_t inner_digest[kSha256DigestWords];
-  HARDENED_TRY(sha256_final(&sha256_ctx, inner_digest));
-
-  // Restore the key block K0 from the context.
-  uint32_t k0[kSha256MessageBlockWords];
-  key_block_restore(ctx, k0);
-
-  // Compute K0 ^ opad.
-  uint32_t outer_block[kSha256MessageBlockWords];
-  for (size_t i = 0; i < kSha256MessageBlockWords; i++) {
-    outer_block[i] = k0[i] ^ 0x5c5c5c5c;
-  }
-
-  // Start a new hash computation to get the final tag: SHA256(K0 ^ opad ||
-  // SHA256(K0 ^ ipad || message))
-  sha256_init(&sha256_ctx);
-  HARDENED_TRY(sha256_update(&sha256_ctx, (unsigned char *)outer_block,
-                             sizeof(outer_block)));
-  HARDENED_TRY(sha256_update(&sha256_ctx, (unsigned char *)inner_digest,
-                             sizeof(inner_digest)));
-  HARDENED_TRY(sha256_final(&sha256_ctx, tag->data));
+  // Finalize the computation of the outer hash
+  //    = H(K0 ^ opad || H(K0 ^ ipad || message)).
+  HARDENED_TRY(otcrypto_hash_update(
+      &ctx->outer,
+      (crypto_const_byte_buf_t){.len = sizeof(uint32_t) * tag->len,
+                                .data = (unsigned char *)tag->data}));
+  HARDENED_TRY(otcrypto_hash_final(&ctx->outer, tag));
 
   return OTCRYPTO_OK;
 }
