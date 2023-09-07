b764dc2c0e6172bb37608057579076ea03fdd26b
Andreas Kurth <adk@lowrisc.org>
2023-09-07 13:39:52 +0000

    [top_darjeeling,soc_proxy] Implement external alerts
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- hw/top_darjeeling/ip/soc_proxy/rtl/soc_proxy.sv
+++ hw/top_darjeeling/ip/soc_proxy/rtl/soc_proxy.sv
@@ -24,7 +24,7 @@ module soc_proxy
   input  prim_alert_pkg::alert_rx_t [NumAlerts-1:0] alert_rx_i,
   output prim_alert_pkg::alert_tx_t [NumAlerts-1:0] alert_tx_o,
 
-  output logic [3:0] intr_external_o,
+  output logic [NumExternalIrqs-1:0] intr_external_o,
 
   output logic wkup_internal_req_o,
   output logic wkup_external_req_o,
@@ -34,14 +34,14 @@ module soc_proxy
   output tlul_pkg::tl_h2d_t ctn_tl_h2d_o,
   input  tlul_pkg::tl_d2h_t ctn_tl_d2h_i,
 
-  input  soc_alert_req_t [3:0] soc_fatal_alert_i,
-  output soc_alert_rsp_t [3:0] soc_fatal_alert_o,
-  input  soc_alert_req_t [3:0] soc_recov_alert_i,
-  output soc_alert_rsp_t [3:0] soc_recov_alert_o,
+  input  soc_alert_req_t [NumFatalExternalAlerts-1:0] soc_fatal_alert_i,
+  output soc_alert_rsp_t [NumFatalExternalAlerts-1:0] soc_fatal_alert_o,
+  input  soc_alert_req_t [NumRecovExternalAlerts-1:0] soc_recov_alert_i,
+  output soc_alert_rsp_t [NumRecovExternalAlerts-1:0] soc_recov_alert_o,
 
   input  logic soc_wkup_async_i,
 
-  input  logic [3:0] soc_intr_async_i
+  input  logic [NumExternalIrqs-1:0] soc_intr_async_i
 );
 
   // Feed CTN TL-UL ports through.
@@ -50,47 +50,173 @@ module soc_proxy
 
   // Tie off unimplemented outputs temporarily.
   assign dma_lsio_trigger_o = '0;
-  assign intr_external_o = '0;
   assign wkup_internal_req_o = 1'b0;
   assign wkup_external_req_o = 1'b0;
-  for (genvar i = 0; i < 4; i++) begin : gen_tieoff_soc_alert_rsp
-    assign soc_fatal_alert_o[i] = SOC_ALERT_RSP_DEFAULT;
-    assign soc_recov_alert_o[i] = SOC_ALERT_RSP_DEFAULT;
-  end
 
   // Register node
   soc_proxy_core_reg2hw_t reg2hw;
-  // soc_proxy_core_hw2reg_t hw2reg;
-  logic intg_err;
+  soc_proxy_core_hw2reg_t hw2reg;
+  logic reg_top_intg_err;
   soc_proxy_core_reg_top u_reg (
     .clk_i,
     .rst_ni,
     .tl_i       (core_tl_i),
     .tl_o       (core_tl_o),
     .reg2hw,
-    .intg_err_o (intg_err)
+    .hw2reg,
+    .intg_err_o (reg_top_intg_err)
   );
 
   // Alert test
   logic [NumAlerts-1:0] alert_test;
-  assign alert_test[0] = reg2hw.alert_test.qe & reg2hw.alert_test.q;
+  assign alert_test[FatalAlertIntg] = reg2hw.alert_test.fatal_alert_intg.qe &
+                                      reg2hw.alert_test.fatal_alert_intg.q;
+  assign alert_test[FatalAlertExternal0] = reg2hw.alert_test.fatal_alert_external_0.qe &
+                                           reg2hw.alert_test.fatal_alert_external_0.q;
+  assign alert_test[FatalAlertExternal1] = reg2hw.alert_test.fatal_alert_external_1.qe &
+                                           reg2hw.alert_test.fatal_alert_external_1.q;
+  assign alert_test[FatalAlertExternal2] = reg2hw.alert_test.fatal_alert_external_2.qe &
+                                           reg2hw.alert_test.fatal_alert_external_2.q;
+  assign alert_test[FatalAlertExternal3] = reg2hw.alert_test.fatal_alert_external_3.qe &
+                                           reg2hw.alert_test.fatal_alert_external_3.q;
+  assign alert_test[RecovAlertExternal0] = reg2hw.alert_test.recov_alert_external_0.qe &
+                                           reg2hw.alert_test.recov_alert_external_0.q;
+  assign alert_test[RecovAlertExternal1] = reg2hw.alert_test.recov_alert_external_1.qe &
+                                           reg2hw.alert_test.recov_alert_external_1.q;
+  assign alert_test[RecovAlertExternal2] = reg2hw.alert_test.recov_alert_external_2.qe &
+                                           reg2hw.alert_test.recov_alert_external_2.q;
+  assign alert_test[RecovAlertExternal3] = reg2hw.alert_test.recov_alert_external_3.qe &
+                                           reg2hw.alert_test.recov_alert_external_3.q;
 
-  // Alert sender
+  // Handle fatal external alert requests
+  logic [NumFatalExternalAlerts-1:0] soc_fatal_alert_p, soc_fatal_alert_n, fatal_alert_external;
+  for (genvar i = 0; i < NumFatalExternalAlerts; i++) begin : gen_fatal_alert_handling
+    // Buffer/anchor incoming signals to prevent optimization
+    prim_sec_anchor_buf #(
+      .Width(2)
+    ) u_prim_sec_anchor_buf (
+      .in_i ({soc_fatal_alert_i[i].alert_p, soc_fatal_alert_i[i].alert_n}),
+      .out_o({soc_fatal_alert_p[i], soc_fatal_alert_n[i]})
+    );
+    // Treat any positive value on `alert_p` and any negative value on `alert_n` as alert.
+    assign fatal_alert_external[i] = soc_fatal_alert_p[i] | ~soc_fatal_alert_n[i];
+    // Acknowledge handled alerts.
+    assign soc_fatal_alert_o[i] = '{
+      ack_p: soc_fatal_alert_p[i],
+      ack_n: soc_fatal_alert_n[i]
+    };
+  end
+
+  // Handle recoverable external alert requests
+  logic [NumRecovExternalAlerts-1:0] soc_recov_alert_p, soc_recov_alert_n, recov_alert_external;
+  for (genvar i = 0; i < NumRecovExternalAlerts; i++) begin : gen_recov_alert_handling
+    // Buffer/anchor incoming signals to prevent undesired optimizations
+    prim_sec_anchor_buf #(
+      .Width(2)
+    ) u_prim_sec_anchor_buf (
+      .in_i ({soc_recov_alert_i[i].alert_p, soc_recov_alert_i[i].alert_n}),
+      .out_o({soc_recov_alert_p[i], soc_recov_alert_n[i]})
+    );
+    // Treat any positive value on `alert_p` and any negative value on `alert_n` as alert.
+    assign recov_alert_external[i] = soc_recov_alert_p[i] | ~soc_recov_alert_n[i];
+    // Acknowledge alert based on request.
+    assign soc_recov_alert_o[i] = '{
+      ack_p: soc_recov_alert_p[i],
+      ack_n: soc_recov_alert_n[i]
+    };
+  end
+
+  // Aggregate integrity alerts
+  logic intg_err;
+  assign intg_err = reg_top_intg_err;
+
+  // Alert sender for integrity alerts
   prim_alert_sender #(
-    .AsyncOn(AlertAsyncOn[0]),
+    .AsyncOn(AlertAsyncOn[FatalAlertIntg]),
     .IsFatal(1)
-  ) u_prim_fatal_alert_sender (
+  ) u_prim_fatal_alert_intg_sender (
     .clk_i,
     .rst_ni,
-    .alert_test_i(alert_test[0]),
+    .alert_test_i(alert_test[FatalAlertIntg]),
     .alert_req_i(intg_err),
     .alert_ack_o(),
     .alert_state_o(),
-    .alert_rx_i(alert_rx_i[0]),
-    .alert_tx_o(alert_tx_o[0])
+    .alert_rx_i(alert_rx_i[FatalAlertIntg]),
+    .alert_tx_o(alert_tx_o[FatalAlertIntg])
+  );
+
+  // Alert senders for fatal external alerts
+  for (genvar i = 0; i < NumFatalExternalAlerts; i++) begin : gen_fatal_alert_sender
+    prim_alert_sender #(
+      .AsyncOn(AlertAsyncOn[FatalAlertExternal0 + i]),
+      .IsFatal(1'b1)
+    ) u_prim_alert_sender (
+      .clk_i,
+      .rst_ni,
+      .alert_test_i(alert_test[FatalAlertExternal0 + i]),
+      .alert_req_i(fatal_alert_external[i]),
+      .alert_ack_o(),
+      .alert_state_o(),
+      .alert_rx_i(alert_rx_i[FatalAlertExternal0 + i]),
+      .alert_tx_o(alert_tx_o[FatalAlertExternal0 + i])
+    );
+  end
+
+  // Alert senders for recoverable external alerts
+  for (genvar i = 0; i < NumRecovExternalAlerts; i++) begin : gen_recov_alert_sender
+    prim_alert_sender #(
+      .AsyncOn(AlertAsyncOn[RecovAlertExternal0 + i]),
+      .IsFatal(1'b0)
+    ) u_prim_alert_sender (
+      .clk_i,
+      .rst_ni,
+      .alert_test_i(alert_test[RecovAlertExternal0 + i]),
+      .alert_req_i(recov_alert_external[i]),
+      .alert_ack_o(),
+      .alert_state_o(),
+      .alert_rx_i(alert_rx_i[RecovAlertExternal0 + i]),
+      .alert_tx_o(alert_tx_o[RecovAlertExternal0 + i])
+    );
+  end
+
+  // Synchronize external interrupt signals
+  logic [NumExternalIrqs-1:0] soc_intr;
+  for (genvar i = 0; i < NumExternalIrqs; i++) begin : gen_sync_external_irqs
+    prim_flop_2sync #(
+      .Width(1)
+    ) u_prim_flop_2sync (
+      .clk_i,
+      .rst_ni,
+      .d_i(soc_intr_async_i[i]),
+      .q_o(soc_intr[i])
+    );
+  end
+
+  // Handle external interrupts
+  prim_intr_hw #(
+    .Width(NumExternalIrqs)
+  ) u_prim_intr_hw (
+    .clk_i,
+    .rst_ni,
+    .event_intr_i           (soc_intr),
+    .reg2hw_intr_enable_q_i (reg2hw.intr_enable.q),
+    .reg2hw_intr_test_q_i   (reg2hw.intr_test.q),
+    .reg2hw_intr_test_qe_i  (reg2hw.intr_test.qe),
+    .reg2hw_intr_state_q_i  (reg2hw.intr_state.q),
+    .hw2reg_intr_state_de_o (hw2reg.intr_state.de),
+    .hw2reg_intr_state_d_o  (hw2reg.intr_state.d),
+    .intr_o                 (intr_external_o)
   );
 
   // Assertions
-  `ASSERT_PRIM_REG_WE_ONEHOT_ERROR_TRIGGER_ALERT(RegWeOnehotCheck_A, u_reg, alert_tx_o[0])
+  `ASSERT_PRIM_REG_WE_ONEHOT_ERROR_TRIGGER_ALERT(RegWeOnehotCheck_A,
+                                                 u_reg,
+                                                 alert_tx_o[FatalAlertIntg])
+
+  // Assert that there's one index for each alert defined in the Hjson.
+  `ASSERT_INIT(AlertSourcesComplete_A, NumAlertSources == NumAlerts)
+
+  // Assert that the number of internal and external alerts sum up to the total number of alerts.
+  `ASSERT_INIT(AlertNumsSumCorrect_A, NumInternalAlerts + NumExternalAlerts == NumAlertSources)
 
 endmodule
--- hw/top_darjeeling/ip/soc_proxy/rtl/soc_proxy_core_reg_top.sv
+++ hw/top_darjeeling/ip/soc_proxy/rtl/soc_proxy_core_reg_top.sv
@@ -13,6 +13,7 @@ module soc_proxy_core_reg_top (
   output tlul_pkg::tl_d2h_t tl_o,
   // To HW
   output soc_proxy_reg_pkg::soc_proxy_core_reg2hw_t reg2hw, // Write
+  input  soc_proxy_reg_pkg::soc_proxy_core_hw2reg_t hw2reg, // Read
 
   // Integrity check errors
   output logic intg_err_o
@@ -20,7 +21,7 @@ module soc_proxy_core_reg_top (
 
   import soc_proxy_reg_pkg::* ;
 
-  localparam int AW = 2;
+  localparam int AW = 4;
   localparam int DW = 32;
   localparam int DBW = DW/8;                    // Byte Width
 
@@ -51,9 +52,9 @@ module soc_proxy_core_reg_top (
 
   // also check for spurious write enables
   logic reg_we_err;
-  logic [0:0] reg_we_check;
+  logic [3:0] reg_we_check;
   prim_reg_we_check #(
-    .OneHotWidth(1)
+    .OneHotWidth(4)
   ) u_prim_reg_we_check (
     .clk_i(clk_i),
     .rst_ni(rst_ni),
@@ -120,35 +121,259 @@ module soc_proxy_core_reg_top (
   // Define SW related signals
   // Format: <reg>_<field>_{wd|we|qs}
   //        or <reg>_{wd|we|qs} if field == 1 or 0
+  logic intr_state_we;
+  logic [3:0] intr_state_qs;
+  logic [3:0] intr_state_wd;
+  logic intr_enable_we;
+  logic [3:0] intr_enable_qs;
+  logic [3:0] intr_enable_wd;
+  logic intr_test_we;
+  logic [3:0] intr_test_wd;
   logic alert_test_we;
-  logic alert_test_wd;
+  logic alert_test_fatal_alert_intg_wd;
+  logic alert_test_fatal_alert_external_0_wd;
+  logic alert_test_fatal_alert_external_1_wd;
+  logic alert_test_fatal_alert_external_2_wd;
+  logic alert_test_fatal_alert_external_3_wd;
+  logic alert_test_recov_alert_external_0_wd;
+  logic alert_test_recov_alert_external_1_wd;
+  logic alert_test_recov_alert_external_2_wd;
+  logic alert_test_recov_alert_external_3_wd;
 
   // Register instances
-  // R[alert_test]: V(True)
-  logic alert_test_qe;
-  logic [0:0] alert_test_flds_we;
-  assign alert_test_qe = &alert_test_flds_we;
+  // R[intr_state]: V(False)
+  prim_subreg #(
+    .DW      (4),
+    .SwAccess(prim_subreg_pkg::SwAccessW1C),
+    .RESVAL  (4'h0),
+    .Mubi    (1'b0)
+  ) u_intr_state (
+    .clk_i   (clk_i),
+    .rst_ni  (rst_ni),
+
+    // from register interface
+    .we     (intr_state_we),
+    .wd     (intr_state_wd),
+
+    // from internal hardware
+    .de     (hw2reg.intr_state.de),
+    .d      (hw2reg.intr_state.d),
+
+    // to internal hardware
+    .qe     (),
+    .q      (reg2hw.intr_state.q),
+    .ds     (),
+
+    // to register interface (read)
+    .qs     (intr_state_qs)
+  );
+
+
+  // R[intr_enable]: V(False)
+  prim_subreg #(
+    .DW      (4),
+    .SwAccess(prim_subreg_pkg::SwAccessRW),
+    .RESVAL  (4'h0),
+    .Mubi    (1'b0)
+  ) u_intr_enable (
+    .clk_i   (clk_i),
+    .rst_ni  (rst_ni),
+
+    // from register interface
+    .we     (intr_enable_we),
+    .wd     (intr_enable_wd),
+
+    // from internal hardware
+    .de     (1'b0),
+    .d      ('0),
+
+    // to internal hardware
+    .qe     (),
+    .q      (reg2hw.intr_enable.q),
+    .ds     (),
+
+    // to register interface (read)
+    .qs     (intr_enable_qs)
+  );
+
+
+  // R[intr_test]: V(True)
+  logic intr_test_qe;
+  logic [0:0] intr_test_flds_we;
+  assign intr_test_qe = &intr_test_flds_we;
   prim_subreg_ext #(
-    .DW    (1)
-  ) u_alert_test (
+    .DW    (4)
+  ) u_intr_test (
     .re     (1'b0),
-    .we     (alert_test_we),
-    .wd     (alert_test_wd),
+    .we     (intr_test_we),
+    .wd     (intr_test_wd),
     .d      ('0),
     .qre    (),
-    .qe     (alert_test_flds_we[0]),
-    .q      (reg2hw.alert_test.q),
+    .qe     (intr_test_flds_we[0]),
+    .q      (reg2hw.intr_test.q),
     .ds     (),
     .qs     ()
   );
-  assign reg2hw.alert_test.qe = alert_test_qe;
+  assign reg2hw.intr_test.qe = intr_test_qe;
+
+
+  // R[alert_test]: V(True)
+  logic alert_test_qe;
+  logic [8:0] alert_test_flds_we;
+  assign alert_test_qe = &alert_test_flds_we;
+  //   F[fatal_alert_intg]: 0:0
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_alert_test_fatal_alert_intg (
+    .re     (1'b0),
+    .we     (alert_test_we),
+    .wd     (alert_test_fatal_alert_intg_wd),
+    .d      ('0),
+    .qre    (),
+    .qe     (alert_test_flds_we[0]),
+    .q      (reg2hw.alert_test.fatal_alert_intg.q),
+    .ds     (),
+    .qs     ()
+  );
+  assign reg2hw.alert_test.fatal_alert_intg.qe = alert_test_qe;
+
+  //   F[fatal_alert_external_0]: 1:1
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_alert_test_fatal_alert_external_0 (
+    .re     (1'b0),
+    .we     (alert_test_we),
+    .wd     (alert_test_fatal_alert_external_0_wd),
+    .d      ('0),
+    .qre    (),
+    .qe     (alert_test_flds_we[1]),
+    .q      (reg2hw.alert_test.fatal_alert_external_0.q),
+    .ds     (),
+    .qs     ()
+  );
+  assign reg2hw.alert_test.fatal_alert_external_0.qe = alert_test_qe;
+
+  //   F[fatal_alert_external_1]: 2:2
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_alert_test_fatal_alert_external_1 (
+    .re     (1'b0),
+    .we     (alert_test_we),
+    .wd     (alert_test_fatal_alert_external_1_wd),
+    .d      ('0),
+    .qre    (),
+    .qe     (alert_test_flds_we[2]),
+    .q      (reg2hw.alert_test.fatal_alert_external_1.q),
+    .ds     (),
+    .qs     ()
+  );
+  assign reg2hw.alert_test.fatal_alert_external_1.qe = alert_test_qe;
+
+  //   F[fatal_alert_external_2]: 3:3
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_alert_test_fatal_alert_external_2 (
+    .re     (1'b0),
+    .we     (alert_test_we),
+    .wd     (alert_test_fatal_alert_external_2_wd),
+    .d      ('0),
+    .qre    (),
+    .qe     (alert_test_flds_we[3]),
+    .q      (reg2hw.alert_test.fatal_alert_external_2.q),
+    .ds     (),
+    .qs     ()
+  );
+  assign reg2hw.alert_test.fatal_alert_external_2.qe = alert_test_qe;
+
+  //   F[fatal_alert_external_3]: 4:4
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_alert_test_fatal_alert_external_3 (
+    .re     (1'b0),
+    .we     (alert_test_we),
+    .wd     (alert_test_fatal_alert_external_3_wd),
+    .d      ('0),
+    .qre    (),
+    .qe     (alert_test_flds_we[4]),
+    .q      (reg2hw.alert_test.fatal_alert_external_3.q),
+    .ds     (),
+    .qs     ()
+  );
+  assign reg2hw.alert_test.fatal_alert_external_3.qe = alert_test_qe;
+
+  //   F[recov_alert_external_0]: 5:5
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_alert_test_recov_alert_external_0 (
+    .re     (1'b0),
+    .we     (alert_test_we),
+    .wd     (alert_test_recov_alert_external_0_wd),
+    .d      ('0),
+    .qre    (),
+    .qe     (alert_test_flds_we[5]),
+    .q      (reg2hw.alert_test.recov_alert_external_0.q),
+    .ds     (),
+    .qs     ()
+  );
+  assign reg2hw.alert_test.recov_alert_external_0.qe = alert_test_qe;
+
+  //   F[recov_alert_external_1]: 6:6
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_alert_test_recov_alert_external_1 (
+    .re     (1'b0),
+    .we     (alert_test_we),
+    .wd     (alert_test_recov_alert_external_1_wd),
+    .d      ('0),
+    .qre    (),
+    .qe     (alert_test_flds_we[6]),
+    .q      (reg2hw.alert_test.recov_alert_external_1.q),
+    .ds     (),
+    .qs     ()
+  );
+  assign reg2hw.alert_test.recov_alert_external_1.qe = alert_test_qe;
+
+  //   F[recov_alert_external_2]: 7:7
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_alert_test_recov_alert_external_2 (
+    .re     (1'b0),
+    .we     (alert_test_we),
+    .wd     (alert_test_recov_alert_external_2_wd),
+    .d      ('0),
+    .qre    (),
+    .qe     (alert_test_flds_we[7]),
+    .q      (reg2hw.alert_test.recov_alert_external_2.q),
+    .ds     (),
+    .qs     ()
+  );
+  assign reg2hw.alert_test.recov_alert_external_2.qe = alert_test_qe;
+
+  //   F[recov_alert_external_3]: 8:8
+  prim_subreg_ext #(
+    .DW    (1)
+  ) u_alert_test_recov_alert_external_3 (
+    .re     (1'b0),
+    .we     (alert_test_we),
+    .wd     (alert_test_recov_alert_external_3_wd),
+    .d      ('0),
+    .qre    (),
+    .qe     (alert_test_flds_we[8]),
+    .q      (reg2hw.alert_test.recov_alert_external_3.q),
+    .ds     (),
+    .qs     ()
+  );
+  assign reg2hw.alert_test.recov_alert_external_3.qe = alert_test_qe;
 
 
 
-  logic [0:0] addr_hit;
+  logic [3:0] addr_hit;
   always_comb begin
     addr_hit = '0;
-    addr_hit[0] = (reg_addr == SOC_PROXY_ALERT_TEST_OFFSET);
+    addr_hit[0] = (reg_addr == SOC_PROXY_INTR_STATE_OFFSET);
+    addr_hit[1] = (reg_addr == SOC_PROXY_INTR_ENABLE_OFFSET);
+    addr_hit[2] = (reg_addr == SOC_PROXY_INTR_TEST_OFFSET);
+    addr_hit[3] = (reg_addr == SOC_PROXY_ALERT_TEST_OFFSET);
   end
 
   assign addrmiss = (reg_re || reg_we) ? ~|addr_hit : 1'b0 ;
@@ -156,18 +381,49 @@ module soc_proxy_core_reg_top (
   // Check sub-word write is permitted
   always_comb begin
     wr_err = (reg_we &
-              ((addr_hit[0] & (|(SOC_PROXY_CORE_PERMIT[0] & ~reg_be)))));
+              ((addr_hit[0] & (|(SOC_PROXY_CORE_PERMIT[0] & ~reg_be))) |
+               (addr_hit[1] & (|(SOC_PROXY_CORE_PERMIT[1] & ~reg_be))) |
+               (addr_hit[2] & (|(SOC_PROXY_CORE_PERMIT[2] & ~reg_be))) |
+               (addr_hit[3] & (|(SOC_PROXY_CORE_PERMIT[3] & ~reg_be)))));
   end
 
   // Generate write-enables
-  assign alert_test_we = addr_hit[0] & reg_we & !reg_error;
+  assign intr_state_we = addr_hit[0] & reg_we & !reg_error;
 
-  assign alert_test_wd = reg_wdata[0];
+  assign intr_state_wd = reg_wdata[3:0];
+  assign intr_enable_we = addr_hit[1] & reg_we & !reg_error;
+
+  assign intr_enable_wd = reg_wdata[3:0];
+  assign intr_test_we = addr_hit[2] & reg_we & !reg_error;
+
+  assign intr_test_wd = reg_wdata[3:0];
+  assign alert_test_we = addr_hit[3] & reg_we & !reg_error;
+
+  assign alert_test_fatal_alert_intg_wd = reg_wdata[0];
+
+  assign alert_test_fatal_alert_external_0_wd = reg_wdata[1];
+
+  assign alert_test_fatal_alert_external_1_wd = reg_wdata[2];
+
+  assign alert_test_fatal_alert_external_2_wd = reg_wdata[3];
+
+  assign alert_test_fatal_alert_external_3_wd = reg_wdata[4];
+
+  assign alert_test_recov_alert_external_0_wd = reg_wdata[5];
+
+  assign alert_test_recov_alert_external_1_wd = reg_wdata[6];
+
+  assign alert_test_recov_alert_external_2_wd = reg_wdata[7];
+
+  assign alert_test_recov_alert_external_3_wd = reg_wdata[8];
 
   // Assign write-enables to checker logic vector.
   always_comb begin
     reg_we_check = '0;
-    reg_we_check[0] = alert_test_we;
+    reg_we_check[0] = intr_state_we;
+    reg_we_check[1] = intr_enable_we;
+    reg_we_check[2] = intr_test_we;
+    reg_we_check[3] = alert_test_we;
   end
 
   // Read data return
@@ -175,7 +431,27 @@ module soc_proxy_core_reg_top (
     reg_rdata_next = '0;
     unique case (1'b1)
       addr_hit[0]: begin
+        reg_rdata_next[3:0] = intr_state_qs;
+      end
+
+      addr_hit[1]: begin
+        reg_rdata_next[3:0] = intr_enable_qs;
+      end
+
+      addr_hit[2]: begin
+        reg_rdata_next[3:0] = '0;
+      end
+
+      addr_hit[3]: begin
         reg_rdata_next[0] = '0;
+        reg_rdata_next[1] = '0;
+        reg_rdata_next[2] = '0;
+        reg_rdata_next[3] = '0;
+        reg_rdata_next[4] = '0;
+        reg_rdata_next[5] = '0;
+        reg_rdata_next[6] = '0;
+        reg_rdata_next[7] = '0;
+        reg_rdata_next[8] = '0;
       end
 
       default: begin
