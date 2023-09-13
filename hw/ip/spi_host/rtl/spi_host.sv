c7f42b98ce02d286286ce52461e7a9c79f4b907d
Andreas Kurth <adk@lowrisc.org>
2023-09-13 10:16:48 +0000

    [top_darjeeling,spi_host] Implement LSIO trigger
    In this minimal implementation, SPI Host asserts `lsio_trigger_o` iff
    the TX FIFO fill level is smaller than the configured watermark or the
    RX FIFO fill level is greater than or equal to the configured watermark,
    independent of IRQ enablement.
    
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- hw/ip/spi_host/rtl/spi_host.sv
+++ hw/ip/spi_host/rtl/spi_host.sv
@@ -37,6 +37,8 @@ module spi_host
   input  spi_device_pkg::passthrough_req_t passthrough_i,
   output spi_device_pkg::passthrough_rsp_t passthrough_o,
 
+  output logic             lsio_trigger_o,
+
   output logic             intr_error_o,
   output logic             intr_spi_event_o
 );
