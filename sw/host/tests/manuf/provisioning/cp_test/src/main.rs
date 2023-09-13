d5dc58c229e6551d5d91bef8736590b5efe6b8ee
Tim Trippel <ttrippel@google.com>
2023-09-13 16:02:25 -0700

    [manuf] update CP provisioning test to read values over JTAG
    Rather than loading a flash program to read / check values provisioned
    in flash / OTP during CP, this changes the test harness to read said
    values over JTAG to enable running this test on real silicon without
    needing to bootstrap something into flash, which requires provisioning
    more OTP fields (i.e. CREATOR_SW_CFG partition) to allow the ROM to boot
    the flash image.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/host/tests/manuf/provisioning/cp_test/src/main.rs
+++ sw/host/tests/manuf/provisioning/cp_test/src/main.rs
@@ -3,13 +3,18 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::Result;
+use arrayvec::ArrayVec;
 use clap::Parser;
+use rand::RngCore;
 
 use cp_lib::{
     hex_string_to_u32_arrayvec, reset_and_lock, run_sram_cp_provision, unlock_raw,
     ManufCpProvisioningData, Opts,
 };
 use opentitanlib::app::TransportWrapper;
+use opentitanlib::dif::lc_ctrl::{DifLcCtrlState, LcCtrlReg};
+use opentitanlib::io::jtag::JtagTap;
+use opentitanlib::test_utils::lc_transition;
 
 fn cp_provision(
     opts: &Opts,
@@ -22,29 +27,108 @@ fn cp_provision(
     Ok(())
 }
 
+fn test_unlock(
+    opts: &Opts,
+    transport: &TransportWrapper,
+    provisioning_data: &ManufCpProvisioningData,
+) -> Result<()> {
+    // Connect to LC TAP.
+    transport.pin_strapping("PINMUX_TAP_LC")?.apply()?;
+    transport.reset_target(opts.init.bootstrap.options.reset_delay, true)?;
+    let jtag = opts.init.jtag_params.create(transport)?;
+    jtag.connect(JtagTap::LcTap)?;
+
+    // Check that LC state is currently `TEST_LOCKED0`.
+    let state = jtag.read_lc_ctrl_reg(&LcCtrlReg::LcState)?;
+    assert_eq!(state, DifLcCtrlState::TestLocked0.redundant_encoding());
+
+    // ROM execution is not yet enabled in the OTP so we can safely reconnect to the LC TAP after
+    // the transition without risking the chip resetting.
+    lc_transition::trigger_lc_transition(
+        transport,
+        jtag.clone(),
+        DifLcCtrlState::TestUnlocked1,
+        Some(
+            provisioning_data
+                .test_unlock_token
+                .clone()
+                .into_inner()
+                .unwrap(),
+        ),
+        /*use_external_clk=*/ true,
+        opts.init.bootstrap.options.reset_delay,
+        /*reconnect_jtag_tap=*/ Some(JtagTap::LcTap),
+    )?;
+
+    // Check that LC state has transitioned to `TestUnlocked1`.
+    let state = jtag.read_lc_ctrl_reg(&LcCtrlReg::LcState)?;
+    assert_eq!(state, DifLcCtrlState::TestUnlocked1.redundant_encoding());
+
+    jtag.disconnect()?;
+    transport.pin_strapping("PINMUX_TAP_LC")?.remove()?;
+
+    Ok(())
+}
+
+fn check_cp_provisioning(
+    opts: &Opts,
+    transport: &TransportWrapper,
+    provisioning_data: &ManufCpProvisioningData,
+) -> Result<()> {
+    // Set the TAP straps for the CPU and reset.
+    transport.pin_strapping("PINMUX_TAP_RISCV")?.apply()?;
+    transport.reset_target(opts.init.bootstrap.options.reset_delay, true)?;
+
+    // Connect to the RISCV TAP via JTAG.
+    let jtag = opts.init.jtag_params.create(transport)?;
+    jtag.connect(JtagTap::RiscvTap)?;
+
+    // TODO: Read and check device_id field from flash info page 0.
+    // TODO: Read and check manuf_state field from flash info page 0.
+    // TODO: Read and check wafer_auth_secret field from flash info page 3.
+    // TODO: Read and check test exit token field from OTP secret0 partition.
+    // TODO: Read and check test unlock token field from OTP secret0 partition.
+
+    jtag.disconnect()?;
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let opts = Opts::parse();
     opts.init.init_logging();
     let transport = opts.init.init_target()?;
 
+    // Generate random provisioning values for testing.
+    let mut device_id = ArrayVec::<u32, 8>::new();
+    let mut manuf_state = ArrayVec::<u32, 8>::new();
+    let mut wafer_auth_secret = ArrayVec::<u32, 8>::new();
+    let mut test_exit_token = ArrayVec::<u32, 4>::new();
+    let mut test_unlock_token = ArrayVec::<u32, 4>::new();
+    for i in 0..8 {
+        if i < 4 {
+            test_exit_token.push(rand::thread_rng().next_u32());
+            test_unlock_token.push(rand::thread_rng().next_u32());
+        }
+        device_id.push(rand::thread_rng().next_u32());
+        manuf_state.push(rand::thread_rng().next_u32());
+        wafer_auth_secret.push(rand::thread_rng().next_u32());
+    }
+    println!("{:?}", device_id);
+
+    // Provision values into the chip.
     let provisioning_data = ManufCpProvisioningData {
         device_id: hex_string_to_u32_arrayvec::<8>(opts.provisioning_data.device_id.as_str())?,
         manuf_state: hex_string_to_u32_arrayvec::<8>(opts.provisioning_data.manuf_state.as_str())?,
-        wafer_auth_secret: hex_string_to_u32_arrayvec::<8>(
-            opts.provisioning_data.wafer_auth_secret.as_str(),
-        )?,
-        test_unlock_token: hex_string_to_u32_arrayvec::<4>(
-            opts.provisioning_data.test_unlock_token.as_str(),
-        )?,
-        test_exit_token: hex_string_to_u32_arrayvec::<4>(
-            opts.provisioning_data.test_exit_token.as_str(),
-        )?,
+        wafer_auth_secret: wafer_auth_secret,
+        test_unlock_token: test_unlock_token,
+        test_exit_token: test_exit_token,
     };
-
     cp_provision(&opts, &transport, &provisioning_data)?;
 
-    // TODO(#19573): transition to PROD.
-    // TODO(#19573): bootstrap flash binary to check OTP / flash info fields provisioned during CP.
+    // Transition to TEST_UNLOCKED1 and check provisioning operations over JTAG.
+    test_unlock(&opts, &transport, &provisioning_data)?;
+    check_cp_provisioning(&opts, &transport, &provisioning_data)?;
 
     Ok(())
 }
