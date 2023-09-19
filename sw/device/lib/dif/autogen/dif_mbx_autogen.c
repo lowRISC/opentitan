d38fc055a4a8912957b9aa58b1ef67f1e539360f
Andreas Kurth <adk@lowrisc.org>
2023-09-19 13:35:35 +0000

    [sw/mbx] Autogenerate (parts of) DIF
    Command executed:
    ```
    util/make_new_dif.py \
        --mode new \
        --topcfg  hw/top_darjeeling/data/top_darjeeling.hjson \
        --ip-name-snake mbx \
        --ip-name-long 'Mailbox'
    ```
    
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

---
+++ sw/device/lib/dif/autogen/dif_mbx_autogen.c
@@ -0,0 +1,263 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// THIS FILE HAS BEEN GENERATED, DO NOT EDIT MANUALLY. COMMAND:
+// util/make_new_dif.py --mode=regen --only=autogen
+
+#include "sw/device/lib/dif/autogen/dif_mbx_autogen.h"
+
+#include <stdint.h>
+
+#include "sw/device/lib/dif/dif_base.h"
+
+#include "mbx_regs.h"  // Generated.
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_init(mmio_region_t base_addr, dif_mbx_t *mbx) {
+  if (mbx == NULL) {
+    return kDifBadArg;
+  }
+
+  mbx->base_addr = base_addr;
+
+  return kDifOk;
+}
+
+dif_result_t dif_mbx_alert_force(const dif_mbx_t *mbx, dif_mbx_alert_t alert) {
+  if (mbx == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t alert_idx;
+  switch (alert) {
+    case kDifMbxAlertFatalFault:
+      alert_idx = MBX_ALERT_TEST_FATAL_FAULT_BIT;
+      break;
+    case kDifMbxAlertRecovFault:
+      alert_idx = MBX_ALERT_TEST_RECOV_FAULT_BIT;
+      break;
+    default:
+      return kDifBadArg;
+  }
+
+  uint32_t alert_test_reg = bitfield_bit32_write(0, alert_idx, true);
+  mmio_region_write32(mbx->base_addr, (ptrdiff_t)MBX_ALERT_TEST_REG_OFFSET,
+                      alert_test_reg);
+
+  return kDifOk;
+}
+
+/**
+ * Get the corresponding interrupt register bit offset of the IRQ.
+ */
+static bool mbx_get_irq_bit_index(dif_mbx_irq_t irq,
+                                  bitfield_bit32_index_t *index_out) {
+  switch (irq) {
+    case kDifMbxIrqMbxReady:
+      *index_out = MBX_INTR_COMMON_MBX_READY_BIT;
+      break;
+    case kDifMbxIrqMbxAbort:
+      *index_out = MBX_INTR_COMMON_MBX_ABORT_BIT;
+      break;
+    case kDifMbxIrqMbxError:
+      *index_out = MBX_INTR_COMMON_MBX_ERROR_BIT;
+      break;
+    default:
+      return false;
+  }
+
+  return true;
+}
+
+static dif_irq_type_t irq_types[] = {
+    kDifIrqTypeEvent,
+    kDifIrqTypeEvent,
+    kDifIrqTypeEvent,
+};
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_irq_get_type(const dif_mbx_t *mbx, dif_mbx_irq_t irq,
+                                  dif_irq_type_t *type) {
+  if (mbx == NULL || type == NULL || irq == kDifMbxIrqMbxError + 1) {
+    return kDifBadArg;
+  }
+
+  *type = irq_types[irq];
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_irq_get_state(const dif_mbx_t *mbx,
+                                   dif_mbx_irq_state_snapshot_t *snapshot) {
+  if (mbx == NULL || snapshot == NULL) {
+    return kDifBadArg;
+  }
+
+  *snapshot =
+      mmio_region_read32(mbx->base_addr, (ptrdiff_t)MBX_INTR_STATE_REG_OFFSET);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_irq_acknowledge_state(
+    const dif_mbx_t *mbx, dif_mbx_irq_state_snapshot_t snapshot) {
+  if (mbx == NULL) {
+    return kDifBadArg;
+  }
+
+  mmio_region_write32(mbx->base_addr, (ptrdiff_t)MBX_INTR_STATE_REG_OFFSET,
+                      snapshot);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_irq_is_pending(const dif_mbx_t *mbx, dif_mbx_irq_t irq,
+                                    bool *is_pending) {
+  if (mbx == NULL || is_pending == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!mbx_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_state_reg =
+      mmio_region_read32(mbx->base_addr, (ptrdiff_t)MBX_INTR_STATE_REG_OFFSET);
+
+  *is_pending = bitfield_bit32_read(intr_state_reg, index);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_irq_acknowledge_all(const dif_mbx_t *mbx) {
+  if (mbx == NULL) {
+    return kDifBadArg;
+  }
+
+  // Writing to the register clears the corresponding bits (Write-one clear).
+  mmio_region_write32(mbx->base_addr, (ptrdiff_t)MBX_INTR_STATE_REG_OFFSET,
+                      UINT32_MAX);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_irq_acknowledge(const dif_mbx_t *mbx, dif_mbx_irq_t irq) {
+  if (mbx == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!mbx_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  // Writing to the register clears the corresponding bits (Write-one clear).
+  uint32_t intr_state_reg = bitfield_bit32_write(0, index, true);
+  mmio_region_write32(mbx->base_addr, (ptrdiff_t)MBX_INTR_STATE_REG_OFFSET,
+                      intr_state_reg);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_irq_force(const dif_mbx_t *mbx, dif_mbx_irq_t irq,
+                               const bool val) {
+  if (mbx == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!mbx_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_test_reg = bitfield_bit32_write(0, index, val);
+  mmio_region_write32(mbx->base_addr, (ptrdiff_t)MBX_INTR_TEST_REG_OFFSET,
+                      intr_test_reg);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_irq_get_enabled(const dif_mbx_t *mbx, dif_mbx_irq_t irq,
+                                     dif_toggle_t *state) {
+  if (mbx == NULL || state == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!mbx_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_enable_reg =
+      mmio_region_read32(mbx->base_addr, (ptrdiff_t)MBX_INTR_ENABLE_REG_OFFSET);
+
+  bool is_enabled = bitfield_bit32_read(intr_enable_reg, index);
+  *state = is_enabled ? kDifToggleEnabled : kDifToggleDisabled;
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_irq_set_enabled(const dif_mbx_t *mbx, dif_mbx_irq_t irq,
+                                     dif_toggle_t state) {
+  if (mbx == NULL) {
+    return kDifBadArg;
+  }
+
+  bitfield_bit32_index_t index;
+  if (!mbx_get_irq_bit_index(irq, &index)) {
+    return kDifBadArg;
+  }
+
+  uint32_t intr_enable_reg =
+      mmio_region_read32(mbx->base_addr, (ptrdiff_t)MBX_INTR_ENABLE_REG_OFFSET);
+
+  bool enable_bit = (state == kDifToggleEnabled) ? true : false;
+  intr_enable_reg = bitfield_bit32_write(intr_enable_reg, index, enable_bit);
+  mmio_region_write32(mbx->base_addr, (ptrdiff_t)MBX_INTR_ENABLE_REG_OFFSET,
+                      intr_enable_reg);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_irq_disable_all(const dif_mbx_t *mbx,
+                                     dif_mbx_irq_enable_snapshot_t *snapshot) {
+  if (mbx == NULL) {
+    return kDifBadArg;
+  }
+
+  // Pass the current interrupt state to the caller, if requested.
+  if (snapshot != NULL) {
+    *snapshot = mmio_region_read32(mbx->base_addr,
+                                   (ptrdiff_t)MBX_INTR_ENABLE_REG_OFFSET);
+  }
+
+  // Disable all interrupts.
+  mmio_region_write32(mbx->base_addr, (ptrdiff_t)MBX_INTR_ENABLE_REG_OFFSET,
+                      0u);
+
+  return kDifOk;
+}
+
+OT_WARN_UNUSED_RESULT
+dif_result_t dif_mbx_irq_restore_all(
+    const dif_mbx_t *mbx, const dif_mbx_irq_enable_snapshot_t *snapshot) {
+  if (mbx == NULL || snapshot == NULL) {
+    return kDifBadArg;
+  }
+
+  mmio_region_write32(mbx->base_addr, (ptrdiff_t)MBX_INTR_ENABLE_REG_OFFSET,
+                      *snapshot);
+
+  return kDifOk;
+}
---
+++ sw/device/lib/dif/autogen/dif_mbx_autogen_unittest.cc
@@ -0,0 +1,379 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// THIS FILE HAS BEEN GENERATED, DO NOT EDIT MANUALLY. COMMAND:
+// util/make_new_dif.py --mode=regen --only=autogen
+
+#include "sw/device/lib/dif/autogen/dif_mbx_autogen.h"
+
+#include "gtest/gtest.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/base/mock_mmio.h"
+#include "sw/device/lib/dif/dif_test_base.h"
+
+#include "mbx_regs.h"  // Generated.
+
+namespace dif_mbx_autogen_unittest {
+namespace {
+using ::mock_mmio::MmioTest;
+using ::mock_mmio::MockDevice;
+using ::testing::Eq;
+using ::testing::Test;
+
+class MbxTest : public Test, public MmioTest {
+ protected:
+  dif_mbx_t mbx_ = {.base_addr = dev().region()};
+};
+
+class InitTest : public MbxTest {};
+
+TEST_F(InitTest, NullArgs) {
+  EXPECT_DIF_BADARG(dif_mbx_init(dev().region(), nullptr));
+}
+
+TEST_F(InitTest, Success) {
+  EXPECT_DIF_OK(dif_mbx_init(dev().region(), &mbx_));
+}
+
+class AlertForceTest : public MbxTest {};
+
+TEST_F(AlertForceTest, NullArgs) {
+  EXPECT_DIF_BADARG(dif_mbx_alert_force(nullptr, kDifMbxAlertFatalFault));
+}
+
+TEST_F(AlertForceTest, BadAlert) {
+  EXPECT_DIF_BADARG(
+      dif_mbx_alert_force(nullptr, static_cast<dif_mbx_alert_t>(32)));
+}
+
+TEST_F(AlertForceTest, Success) {
+  // Force first alert.
+  EXPECT_WRITE32(MBX_ALERT_TEST_REG_OFFSET,
+                 {{MBX_ALERT_TEST_FATAL_FAULT_BIT, true}});
+  EXPECT_DIF_OK(dif_mbx_alert_force(&mbx_, kDifMbxAlertFatalFault));
+
+  // Force last alert.
+  EXPECT_WRITE32(MBX_ALERT_TEST_REG_OFFSET,
+                 {{MBX_ALERT_TEST_RECOV_FAULT_BIT, true}});
+  EXPECT_DIF_OK(dif_mbx_alert_force(&mbx_, kDifMbxAlertRecovFault));
+}
+
+class IrqGetTypeTest : public MbxTest {};
+
+TEST_F(IrqGetTypeTest, NullArgs) {
+  dif_irq_type_t type;
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_get_type(nullptr, kDifMbxIrqMbxReady, &type));
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_get_type(&mbx_, kDifMbxIrqMbxReady, nullptr));
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_get_type(nullptr, kDifMbxIrqMbxReady, nullptr));
+}
+
+TEST_F(IrqGetTypeTest, BadIrq) {
+  dif_irq_type_t type;
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_get_type(
+      &mbx_, static_cast<dif_mbx_irq_t>(kDifMbxIrqMbxError + 1), &type));
+}
+
+TEST_F(IrqGetTypeTest, Success) {
+  dif_irq_type_t type;
+
+  EXPECT_DIF_OK(dif_mbx_irq_get_type(&mbx_, kDifMbxIrqMbxReady, &type));
+  EXPECT_EQ(type, kDifIrqTypeEvent);
+}
+
+class IrqGetStateTest : public MbxTest {};
+
+TEST_F(IrqGetStateTest, NullArgs) {
+  dif_mbx_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_get_state(nullptr, &irq_snapshot));
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_get_state(&mbx_, nullptr));
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_get_state(nullptr, nullptr));
+}
+
+TEST_F(IrqGetStateTest, SuccessAllRaised) {
+  dif_mbx_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(MBX_INTR_STATE_REG_OFFSET,
+                std::numeric_limits<uint32_t>::max());
+  EXPECT_DIF_OK(dif_mbx_irq_get_state(&mbx_, &irq_snapshot));
+  EXPECT_EQ(irq_snapshot, std::numeric_limits<uint32_t>::max());
+}
+
+TEST_F(IrqGetStateTest, SuccessNoneRaised) {
+  dif_mbx_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(MBX_INTR_STATE_REG_OFFSET, 0);
+  EXPECT_DIF_OK(dif_mbx_irq_get_state(&mbx_, &irq_snapshot));
+  EXPECT_EQ(irq_snapshot, 0);
+}
+
+class IrqIsPendingTest : public MbxTest {};
+
+TEST_F(IrqIsPendingTest, NullArgs) {
+  bool is_pending;
+
+  EXPECT_DIF_BADARG(
+      dif_mbx_irq_is_pending(nullptr, kDifMbxIrqMbxReady, &is_pending));
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_is_pending(&mbx_, kDifMbxIrqMbxReady, nullptr));
+
+  EXPECT_DIF_BADARG(
+      dif_mbx_irq_is_pending(nullptr, kDifMbxIrqMbxReady, nullptr));
+}
+
+TEST_F(IrqIsPendingTest, BadIrq) {
+  bool is_pending;
+  // All interrupt CSRs are 32 bit so interrupt 32 will be invalid.
+  EXPECT_DIF_BADARG(dif_mbx_irq_is_pending(
+      &mbx_, static_cast<dif_mbx_irq_t>(32), &is_pending));
+}
+
+TEST_F(IrqIsPendingTest, Success) {
+  bool irq_state;
+
+  // Get the first IRQ state.
+  irq_state = false;
+  EXPECT_READ32(MBX_INTR_STATE_REG_OFFSET,
+                {{MBX_INTR_STATE_MBX_READY_BIT, true}});
+  EXPECT_DIF_OK(dif_mbx_irq_is_pending(&mbx_, kDifMbxIrqMbxReady, &irq_state));
+  EXPECT_TRUE(irq_state);
+
+  // Get the last IRQ state.
+  irq_state = true;
+  EXPECT_READ32(MBX_INTR_STATE_REG_OFFSET,
+                {{MBX_INTR_STATE_MBX_ERROR_BIT, false}});
+  EXPECT_DIF_OK(dif_mbx_irq_is_pending(&mbx_, kDifMbxIrqMbxError, &irq_state));
+  EXPECT_FALSE(irq_state);
+}
+
+class AcknowledgeStateTest : public MbxTest {};
+
+TEST_F(AcknowledgeStateTest, NullArgs) {
+  dif_mbx_irq_state_snapshot_t irq_snapshot = 0;
+  EXPECT_DIF_BADARG(dif_mbx_irq_acknowledge_state(nullptr, irq_snapshot));
+}
+
+TEST_F(AcknowledgeStateTest, AckSnapshot) {
+  constexpr uint32_t num_irqs = 3;
+  constexpr uint32_t irq_mask = (uint64_t{1} << num_irqs) - 1;
+  dif_mbx_irq_state_snapshot_t irq_snapshot = 1;
+
+  // Test a few snapshots.
+  for (size_t i = 0; i < num_irqs; ++i) {
+    irq_snapshot = ~irq_snapshot & irq_mask;
+    irq_snapshot |= (1u << i);
+    EXPECT_WRITE32(MBX_INTR_STATE_REG_OFFSET, irq_snapshot);
+    EXPECT_DIF_OK(dif_mbx_irq_acknowledge_state(&mbx_, irq_snapshot));
+  }
+}
+
+TEST_F(AcknowledgeStateTest, SuccessNoneRaised) {
+  dif_mbx_irq_state_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(MBX_INTR_STATE_REG_OFFSET, 0);
+  EXPECT_DIF_OK(dif_mbx_irq_get_state(&mbx_, &irq_snapshot));
+  EXPECT_EQ(irq_snapshot, 0);
+}
+
+class AcknowledgeAllTest : public MbxTest {};
+
+TEST_F(AcknowledgeAllTest, NullArgs) {
+  EXPECT_DIF_BADARG(dif_mbx_irq_acknowledge_all(nullptr));
+}
+
+TEST_F(AcknowledgeAllTest, Success) {
+  EXPECT_WRITE32(MBX_INTR_STATE_REG_OFFSET,
+                 std::numeric_limits<uint32_t>::max());
+
+  EXPECT_DIF_OK(dif_mbx_irq_acknowledge_all(&mbx_));
+}
+
+class IrqAcknowledgeTest : public MbxTest {};
+
+TEST_F(IrqAcknowledgeTest, NullArgs) {
+  EXPECT_DIF_BADARG(dif_mbx_irq_acknowledge(nullptr, kDifMbxIrqMbxReady));
+}
+
+TEST_F(IrqAcknowledgeTest, BadIrq) {
+  EXPECT_DIF_BADARG(
+      dif_mbx_irq_acknowledge(nullptr, static_cast<dif_mbx_irq_t>(32)));
+}
+
+TEST_F(IrqAcknowledgeTest, Success) {
+  // Clear the first IRQ state.
+  EXPECT_WRITE32(MBX_INTR_STATE_REG_OFFSET,
+                 {{MBX_INTR_STATE_MBX_READY_BIT, true}});
+  EXPECT_DIF_OK(dif_mbx_irq_acknowledge(&mbx_, kDifMbxIrqMbxReady));
+
+  // Clear the last IRQ state.
+  EXPECT_WRITE32(MBX_INTR_STATE_REG_OFFSET,
+                 {{MBX_INTR_STATE_MBX_ERROR_BIT, true}});
+  EXPECT_DIF_OK(dif_mbx_irq_acknowledge(&mbx_, kDifMbxIrqMbxError));
+}
+
+class IrqForceTest : public MbxTest {};
+
+TEST_F(IrqForceTest, NullArgs) {
+  EXPECT_DIF_BADARG(dif_mbx_irq_force(nullptr, kDifMbxIrqMbxReady, true));
+}
+
+TEST_F(IrqForceTest, BadIrq) {
+  EXPECT_DIF_BADARG(
+      dif_mbx_irq_force(nullptr, static_cast<dif_mbx_irq_t>(32), true));
+}
+
+TEST_F(IrqForceTest, Success) {
+  // Force first IRQ.
+  EXPECT_WRITE32(MBX_INTR_TEST_REG_OFFSET,
+                 {{MBX_INTR_TEST_MBX_READY_BIT, true}});
+  EXPECT_DIF_OK(dif_mbx_irq_force(&mbx_, kDifMbxIrqMbxReady, true));
+
+  // Force last IRQ.
+  EXPECT_WRITE32(MBX_INTR_TEST_REG_OFFSET,
+                 {{MBX_INTR_TEST_MBX_ERROR_BIT, true}});
+  EXPECT_DIF_OK(dif_mbx_irq_force(&mbx_, kDifMbxIrqMbxError, true));
+}
+
+class IrqGetEnabledTest : public MbxTest {};
+
+TEST_F(IrqGetEnabledTest, NullArgs) {
+  dif_toggle_t irq_state;
+
+  EXPECT_DIF_BADARG(
+      dif_mbx_irq_get_enabled(nullptr, kDifMbxIrqMbxReady, &irq_state));
+
+  EXPECT_DIF_BADARG(
+      dif_mbx_irq_get_enabled(&mbx_, kDifMbxIrqMbxReady, nullptr));
+
+  EXPECT_DIF_BADARG(
+      dif_mbx_irq_get_enabled(nullptr, kDifMbxIrqMbxReady, nullptr));
+}
+
+TEST_F(IrqGetEnabledTest, BadIrq) {
+  dif_toggle_t irq_state;
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_get_enabled(
+      &mbx_, static_cast<dif_mbx_irq_t>(32), &irq_state));
+}
+
+TEST_F(IrqGetEnabledTest, Success) {
+  dif_toggle_t irq_state;
+
+  // First IRQ is enabled.
+  irq_state = kDifToggleDisabled;
+  EXPECT_READ32(MBX_INTR_ENABLE_REG_OFFSET,
+                {{MBX_INTR_ENABLE_MBX_READY_BIT, true}});
+  EXPECT_DIF_OK(dif_mbx_irq_get_enabled(&mbx_, kDifMbxIrqMbxReady, &irq_state));
+  EXPECT_EQ(irq_state, kDifToggleEnabled);
+
+  // Last IRQ is disabled.
+  irq_state = kDifToggleEnabled;
+  EXPECT_READ32(MBX_INTR_ENABLE_REG_OFFSET,
+                {{MBX_INTR_ENABLE_MBX_ERROR_BIT, false}});
+  EXPECT_DIF_OK(dif_mbx_irq_get_enabled(&mbx_, kDifMbxIrqMbxError, &irq_state));
+  EXPECT_EQ(irq_state, kDifToggleDisabled);
+}
+
+class IrqSetEnabledTest : public MbxTest {};
+
+TEST_F(IrqSetEnabledTest, NullArgs) {
+  dif_toggle_t irq_state = kDifToggleEnabled;
+
+  EXPECT_DIF_BADARG(
+      dif_mbx_irq_set_enabled(nullptr, kDifMbxIrqMbxReady, irq_state));
+}
+
+TEST_F(IrqSetEnabledTest, BadIrq) {
+  dif_toggle_t irq_state = kDifToggleEnabled;
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_set_enabled(
+      &mbx_, static_cast<dif_mbx_irq_t>(32), irq_state));
+}
+
+TEST_F(IrqSetEnabledTest, Success) {
+  dif_toggle_t irq_state;
+
+  // Enable first IRQ.
+  irq_state = kDifToggleEnabled;
+  EXPECT_MASK32(MBX_INTR_ENABLE_REG_OFFSET,
+                {{MBX_INTR_ENABLE_MBX_READY_BIT, 0x1, true}});
+  EXPECT_DIF_OK(dif_mbx_irq_set_enabled(&mbx_, kDifMbxIrqMbxReady, irq_state));
+
+  // Disable last IRQ.
+  irq_state = kDifToggleDisabled;
+  EXPECT_MASK32(MBX_INTR_ENABLE_REG_OFFSET,
+                {{MBX_INTR_ENABLE_MBX_ERROR_BIT, 0x1, false}});
+  EXPECT_DIF_OK(dif_mbx_irq_set_enabled(&mbx_, kDifMbxIrqMbxError, irq_state));
+}
+
+class IrqDisableAllTest : public MbxTest {};
+
+TEST_F(IrqDisableAllTest, NullArgs) {
+  dif_mbx_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_disable_all(nullptr, &irq_snapshot));
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_disable_all(nullptr, nullptr));
+}
+
+TEST_F(IrqDisableAllTest, SuccessNoSnapshot) {
+  EXPECT_WRITE32(MBX_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_DIF_OK(dif_mbx_irq_disable_all(&mbx_, nullptr));
+}
+
+TEST_F(IrqDisableAllTest, SuccessSnapshotAllDisabled) {
+  dif_mbx_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(MBX_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_WRITE32(MBX_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_DIF_OK(dif_mbx_irq_disable_all(&mbx_, &irq_snapshot));
+  EXPECT_EQ(irq_snapshot, 0);
+}
+
+TEST_F(IrqDisableAllTest, SuccessSnapshotAllEnabled) {
+  dif_mbx_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_READ32(MBX_INTR_ENABLE_REG_OFFSET,
+                std::numeric_limits<uint32_t>::max());
+  EXPECT_WRITE32(MBX_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_DIF_OK(dif_mbx_irq_disable_all(&mbx_, &irq_snapshot));
+  EXPECT_EQ(irq_snapshot, std::numeric_limits<uint32_t>::max());
+}
+
+class IrqRestoreAllTest : public MbxTest {};
+
+TEST_F(IrqRestoreAllTest, NullArgs) {
+  dif_mbx_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_restore_all(nullptr, &irq_snapshot));
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_restore_all(&mbx_, nullptr));
+
+  EXPECT_DIF_BADARG(dif_mbx_irq_restore_all(nullptr, nullptr));
+}
+
+TEST_F(IrqRestoreAllTest, SuccessAllEnabled) {
+  dif_mbx_irq_enable_snapshot_t irq_snapshot =
+      std::numeric_limits<uint32_t>::max();
+
+  EXPECT_WRITE32(MBX_INTR_ENABLE_REG_OFFSET,
+                 std::numeric_limits<uint32_t>::max());
+  EXPECT_DIF_OK(dif_mbx_irq_restore_all(&mbx_, &irq_snapshot));
+}
+
+TEST_F(IrqRestoreAllTest, SuccessAllDisabled) {
+  dif_mbx_irq_enable_snapshot_t irq_snapshot = 0;
+
+  EXPECT_WRITE32(MBX_INTR_ENABLE_REG_OFFSET, 0);
+  EXPECT_DIF_OK(dif_mbx_irq_restore_all(&mbx_, &irq_snapshot));
+}
+
+}  // namespace
+}  // namespace dif_mbx_autogen_unittest
