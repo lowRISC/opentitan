9ee2729fa294c808dd7f7e7afedf8c63d0c573f5
Andreas Kurth <adk@lowrisc.org>
2023-09-19 13:43:37 +0000

    [sw/mbx] Add scaffold of DIF
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

---
+++ sw/ip/mbx/dif/dif_mbx.h
@@ -0,0 +1,10 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_IP_MBX_DIF_DIF_MBX_H_
+#define OPENTITAN_SW_IP_MBX_DIF_DIF_MBX_H_
+
+#include "sw/ip/mbx/dif/autogen/dif_mbx_autogen.h"
+
+#endif  // OPENTITAN_SW_IP_MBX_DIF_DIF_MBX_H_
