02fbb9c72f711eb53d135be8b41f0e012d95786d
Guillermo Maturana <maturana@opentitan.org>
2023-09-19 22:54:21 +0000

    [rstmgr,gen] Undo vectorization of rstmgr_sw_rst_sva_if
    Make this assertion handle one bit a time to ease generation.
    
    Signed-off-by: Guillermo Maturana <maturana@opentitan.org>

--- hw/ip/rstmgr/dv/sva/rstmgr_bind.sv
+++ hw/ip/rstmgr/dv/sva/rstmgr_bind.sv
@@ -33,41 +33,69 @@ module rstmgr_bind;
     .expected_alert_info_attr(($bits(alert_dump_i) + 31) / 32),
     .expected_cpu_info_attr(($bits(cpu_dump_i) + 31) / 32)
   );
-
-  bind rstmgr rstmgr_sw_rst_sva_if rstmgr_sw_rst_sva_if (
-    .clk_i({
-      clk_io_div4_i,
-      clk_io_div4_i,
-      clk_io_div4_i,
-      clk_aon_i,
-      clk_usb_i,
-      clk_io_div2_i,
-      clk_io_i,
-      clk_io_div4_i
-    }),
+  bind rstmgr rstmgr_sw_rst_sva_if rstmgr_sw_rst_0_sva_if (
+    .clk_i(clk_io_div4_i),
     .rst_ni,
     .parent_rst_n(rst_sys_src_n[1]),
-    .ctrl_ns(reg2hw.sw_rst_ctrl_n),
-    .rst_ens({
-      rst_en_o.i2c2[1] == prim_mubi_pkg::MuBi4True,
-      rst_en_o.i2c1[1] == prim_mubi_pkg::MuBi4True,
-      rst_en_o.i2c0[1] == prim_mubi_pkg::MuBi4True,
-      rst_en_o.usb_aon[1] == prim_mubi_pkg::MuBi4True,
-      rst_en_o.usb[1] == prim_mubi_pkg::MuBi4True,
-      rst_en_o.spi_host1[1] == prim_mubi_pkg::MuBi4True,
-      rst_en_o.spi_host0[1] == prim_mubi_pkg::MuBi4True,
-      rst_en_o.spi_device[1] == prim_mubi_pkg::MuBi4True
-    }),
-    .rst_ns({
-      resets_o.rst_i2c2_n[1],
-      resets_o.rst_i2c1_n[1],
-      resets_o.rst_i2c0_n[1],
-      resets_o.rst_usb_aon_n[1],
-      resets_o.rst_usb_n[1],
-      resets_o.rst_spi_host1_n[1],
-      resets_o.rst_spi_host0_n[1],
-      resets_o.rst_spi_device_n[1]
-    })
+    .ctrl_n(reg2hw.sw_rst_ctrl_n[0]),
+    .rst_en(rst_en_o.spi_device[1] == prim_mubi_pkg::MuBi4True),
+    .rst_n(resets_o.rst_spi_device_n[1])
+  );
+  bind rstmgr rstmgr_sw_rst_sva_if rstmgr_sw_rst_1_sva_if (
+    .clk_i(clk_io_i),
+    .rst_ni,
+    .parent_rst_n(rst_sys_src_n[1]),
+    .ctrl_n(reg2hw.sw_rst_ctrl_n[1]),
+    .rst_en(rst_en_o.spi_host0[1] == prim_mubi_pkg::MuBi4True),
+    .rst_n(resets_o.rst_spi_host0_n[1])
+  );
+  bind rstmgr rstmgr_sw_rst_sva_if rstmgr_sw_rst_2_sva_if (
+    .clk_i(clk_io_div2_i),
+    .rst_ni,
+    .parent_rst_n(rst_sys_src_n[1]),
+    .ctrl_n(reg2hw.sw_rst_ctrl_n[2]),
+    .rst_en(rst_en_o.spi_host1[1] == prim_mubi_pkg::MuBi4True),
+    .rst_n(resets_o.rst_spi_host1_n[1])
+  );
+  bind rstmgr rstmgr_sw_rst_sva_if rstmgr_sw_rst_3_sva_if (
+    .clk_i(clk_usb_i),
+    .rst_ni,
+    .parent_rst_n(rst_sys_src_n[1]),
+    .ctrl_n(reg2hw.sw_rst_ctrl_n[3]),
+    .rst_en(rst_en_o.usb[1] == prim_mubi_pkg::MuBi4True),
+    .rst_n(resets_o.rst_usb_n[1])
+  );
+  bind rstmgr rstmgr_sw_rst_sva_if rstmgr_sw_rst_4_sva_if (
+    .clk_i(clk_aon_i),
+    .rst_ni,
+    .parent_rst_n(rst_sys_src_n[1]),
+    .ctrl_n(reg2hw.sw_rst_ctrl_n[4]),
+    .rst_en(rst_en_o.usb_aon[1] == prim_mubi_pkg::MuBi4True),
+    .rst_n(resets_o.rst_usb_aon_n[1])
+  );
+  bind rstmgr rstmgr_sw_rst_sva_if rstmgr_sw_rst_5_sva_if (
+    .clk_i(clk_io_div4_i),
+    .rst_ni,
+    .parent_rst_n(rst_sys_src_n[1]),
+    .ctrl_n(reg2hw.sw_rst_ctrl_n[5]),
+    .rst_en(rst_en_o.i2c0[1] == prim_mubi_pkg::MuBi4True),
+    .rst_n(resets_o.rst_i2c0_n[1])
+  );
+  bind rstmgr rstmgr_sw_rst_sva_if rstmgr_sw_rst_6_sva_if (
+    .clk_i(clk_io_div4_i),
+    .rst_ni,
+    .parent_rst_n(rst_sys_src_n[1]),
+    .ctrl_n(reg2hw.sw_rst_ctrl_n[6]),
+    .rst_en(rst_en_o.i2c1[1] == prim_mubi_pkg::MuBi4True),
+    .rst_n(resets_o.rst_i2c1_n[1])
+  );
+  bind rstmgr rstmgr_sw_rst_sva_if rstmgr_sw_rst_7_sva_if (
+    .clk_i(clk_io_div4_i),
+    .rst_ni,
+    .parent_rst_n(rst_sys_src_n[1]),
+    .ctrl_n(reg2hw.sw_rst_ctrl_n[7]),
+    .rst_en(rst_en_o.i2c2[1] == prim_mubi_pkg::MuBi4True),
+    .rst_n(resets_o.rst_i2c2_n[1])
   );
 `endif
 endmodule
--- hw/ip/rstmgr/dv/sva/rstmgr_sw_rst_sva_if.sv
+++ hw/ip/rstmgr/dv/sva/rstmgr_sw_rst_sva_if.sv
@@ -2,43 +2,43 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// This has assertions that check the output resets read-only value of the alert and cpu_info_attr.
+// This assertions check a software controlled reset and its corresponding reset enable.
+// The reset enable here means the output that goes to the alert handler indicating whether
+// the reset is active.
 interface rstmgr_sw_rst_sva_if (
-  input logic [rstmgr_reg_pkg::NumSwResets-1:0] clk_i,
-  input logic                                   rst_ni,
-  input logic                                   parent_rst_n,
-  input logic [rstmgr_reg_pkg::NumSwResets-1:0] ctrl_ns,
-  input logic [rstmgr_reg_pkg::NumSwResets-1:0] rst_ens,
-  input logic [rstmgr_reg_pkg::NumSwResets-1:0] rst_ns
+  input logic clk_i,
+  input logic rst_ni,
+  input logic parent_rst_n,
+  input logic ctrl_n,
+  input logic rst_en,
+  input logic rst_n
 );
   parameter int RiseMin = 2;
   parameter int RiseMax = 12;
 
   bit disable_sva;
 
-  for (genvar i = 0; i < rstmgr_reg_pkg::NumSwResets; ++i) begin : gen_assertions
-    logic rst_cause;
-    always_comb rst_cause = !parent_rst_n || !ctrl_ns[i];
+  logic rst_cause;
+  always_comb rst_cause = !parent_rst_n || !ctrl_n;
 
-    sequence CauseReadyOn_S;
-      $rose(
-          rst_cause
-      ) ##1 rst_cause [* RiseMin];
-    endsequence
+  sequence CauseReadyOn_S;
+    $rose(
+        rst_cause
+    ) ##1 rst_cause [* RiseMin];
+  endsequence
 
-    sequence CauseReadyOff_S;
-      $fell(
-          rst_cause
-      ) ##1 !rst_cause [* RiseMin];
-    endsequence
+  sequence CauseReadyOff_S;
+    $fell(
+        rst_cause
+    ) ##1 !rst_cause [* RiseMin];
+  endsequence
 
-    `ASSERT(RstNOn_A, CauseReadyOn_S |=> ##[0:RiseMax-RiseMin] !rst_cause || !rst_ns[i], clk_i[i],
-            !rst_ni || disable_sva)
-    `ASSERT(RstNOff_A, CauseReadyOff_S |=> ##[0:RiseMax-RiseMin] rst_cause || rst_ns[i], clk_i[i],
-            !rst_ni || disable_sva)
-    `ASSERT(RstEnOn_A, CauseReadyOn_S |=> ##[0:RiseMax-RiseMin] !rst_cause || rst_ens[i], clk_i[i],
-            !rst_ni || disable_sva)
-    `ASSERT(RstEnOff_A, CauseReadyOff_S |=> ##[0:RiseMax-RiseMin] rst_cause || !rst_ens[i],
-            clk_i[i], !rst_ni || disable_sva)
-  end
+  `ASSERT(RstNOn_A, CauseReadyOn_S |=> ##[0:RiseMax-RiseMin] !rst_cause || !rst_n, clk_i,
+          !rst_ni || disable_sva)
+  `ASSERT(RstNOff_A, CauseReadyOff_S |=> ##[0:RiseMax-RiseMin] rst_cause || rst_n, clk_i,
+          !rst_ni || disable_sva)
+  `ASSERT(RstEnOn_A, CauseReadyOn_S |=> ##[0:RiseMax-RiseMin] !rst_cause || rst_en, clk_i,
+          !rst_ni || disable_sva)
+  `ASSERT(RstEnOff_A, CauseReadyOff_S |=> ##[0:RiseMax-RiseMin] rst_cause || !rst_en, clk_i,
+          !rst_ni || disable_sva)
 endinterface
