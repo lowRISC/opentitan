91bec64af964731b5b10480ce176cfe1f42c7770
Andreas Kurth <adk@lowrisc.org>
2023-09-19 13:43:37 +0000

    [sw/mbx] Add scaffold of DIF
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

---
+++ sw/device/lib/dif/dif_mbx.h
@@ -0,0 +1,10 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_DIF_DIF_MBX_H_
+#define OPENTITAN_SW_DEVICE_LIB_DIF_DIF_MBX_H_
+
+#include "sw/device/lib/dif/autogen/dif_mbx_autogen.h"
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_DIF_DIF_MBX_H_
