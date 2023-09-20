a7c0dfbb363338694b60729f40f26b6a427f791e
Michael Schaffner <msf@opentitan.org>
2023-09-19 20:44:00 -0700

    [top_darjeeling] Increase CTN SRAM pipelining
    This should help with timing pressure on FPGA targets.
    The CTN SRAM access latency thus becomes 3 cycles.
    
    The latency should not adversely affect performance, since
    the flash_ctrl also has ~4 cycles latency.
    
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/top_darjeeling/rtl/autogen/chip_darjeeling_cw310.sv
+++ hw/top_darjeeling/rtl/autogen/chip_darjeeling_cw310.sv
@@ -1141,9 +1141,8 @@ module chip_darjeeling_cw310 #(
     .DataBitsPerMask(CtnSramDw),
     .EnableECC(0),
     .EnableParity(0),
-    // TODO: may want to evaluate whether this pipeline stage can be removed.
     .EnableInputPipeline(1),
-    .EnableOutputPipeline(0)
+    .EnableOutputPipeline(1)
   ) u_prim_ram_1p_adv_ctn (
     .clk_i    (clkmgr_aon_clocks.clk_main_infra),
     .rst_ni   (rstmgr_aon_resets.rst_lc_n[rstmgr_pkg::Domain0Sel]),
