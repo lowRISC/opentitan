ee53b9e4086a5a5dd6a52935549d2dde963696cf
Michael Schaffner <msf@opentitan.org>
2023-09-22 15:33:55 -0700

    [keymgr_dpe/dif] Generate new DIF skeleton
    THis is generated using the make_new_dif script.
    
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

---
+++ sw/ip/keymgr_dpe/dif/dif_keymgr_dpe.h
@@ -0,0 +1,26 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_IP_KEYMGR_DPE_DIF_DIF_KEYMGR_DPE_H_
+#define OPENTITAN_SW_IP_KEYMGR_DPE_DIF_DIF_KEYMGR_DPE_H_
+
+/**
+ * @file
+ * @brief <a href="/hw/ip/keymgr_dpe/doc/">Key Manager DPE</a> Device Interface
+ * Functions
+ */
+
+#include <stdint.h>
+
+#include "sw/ip/keymgr_dpe/dif/autogen/dif_keymgr_dpe_autogen.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
+
+#endif  // OPENTITAN_SW_IP_KEYMGR_DPE_DIF_DIF_KEYMGR_DPE_H_
