430cab2341829b24e5d7f1cb13e894262c5d0739
Jade Philipoom <jadep@google.com>
2023-09-22 11:46:31 +0200

    [crypto] Add streaming AES test and update to use `status_t`.
    The new streaming tests will help ensure that the IV is being properly
    updated, so that it's possible for the caller to repeatedly call the
    one-shot AES function and simulate a streaming interface.
    
    Also updates the AES tests to produce more debuggable `status_t`
    printouts.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

--- sw/device/tests/crypto/aes_functest.c
+++ sw/device/tests/crypto/aes_functest.c
@@ -11,11 +11,17 @@
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 #include "sw/device/tests/crypto/aes_testvectors.h"
 
+// Module ID for status codes.
+#define MODULE_ID MAKE_MODULE_ID('t', 's', 't')
+
 // Random mask for testing.
 static const uint32_t kKeyMask[8] = {
     0x1b81540c, 0x220733c9, 0x8bf85383, 0x05ab50b4,
     0x8acdcb7e, 0x15e76440, 0x8459b2ce, 0xdc2110cc,
 };
+
+// Global pointer to the current test.
+static const aes_test_t *test = NULL;
 
 enum {
   kAesBlockBytes = 128 / 8,
@@ -59,14 +65,22 @@ static crypto_key_config_t make_key_config(const aes_test_t *test) {
   };
 }
 
-static void encrypt_decrypt_test(const aes_test_t *test) {
+/**
+ * Run AES encryption for the given test vector.
+ *
+ * If `streaming` is true, we will process the plaintext in small chunks to
+ * ensure that the IV is updating correctly.
+ *
+ * @param test Test vector to run.
+ * @param streaming Whether to run in streaming mode.
+ */
+static status_t run_encrypt(const aes_test_t *test, bool streaming) {
   // Determine the key configuration.
   crypto_key_config_t config = make_key_config(test);
 
   // Construct blinded key from the key and testing mask.
   uint32_t keyblob[keyblob_num_words(config)];
-  CHECK_STATUS_OK(
-      keyblob_from_key_and_mask(test->key, kKeyMask, config, keyblob));
+  TRY(keyblob_from_key_and_mask(test->key, kKeyMask, config, keyblob));
   crypto_blinded_key_t key = {
       .config = config,
       .keyblob_length = sizeof(keyblob),
@@ -74,7 +88,7 @@ static void encrypt_decrypt_test(const aes_test_t *test) {
   };
   key.checksum = integrity_blinded_checksum(&key);
 
-  // Construct non-constant version of IV buffer.
+  // Construct a buffer to hold the IV.
   uint32_t iv_data[kAesBlockWords];
   memcpy(iv_data, test->iv, kAesBlockBytes);
   crypto_word32_buf_t iv = {
@@ -82,62 +96,170 @@ static void encrypt_decrypt_test(const aes_test_t *test) {
       .len = kAesBlockWords,
   };
 
-  // Construct plaintext buffer.
-  crypto_const_byte_buf_t plaintext = {
-      .data = (const unsigned char *)test->plaintext,
-      .len = test->plaintext_len,
+  // Calculate the size of the padded plaintext.
+  size_t padded_len_bytes;
+  TRY(otcrypto_aes_padded_plaintext_length(test->plaintext_len, test->padding,
+                                           &padded_len_bytes));
+
+  // Create buffer for ciphertext.
+  uint32_t ciphertext_data[padded_len_bytes / sizeof(uint32_t)];
+
+  // If in streaming mode, encrypt one block at a time with null padding until
+  // there is at most 1 block of input remaining.
+  size_t plaintext_len = test->plaintext_len;
+  size_t ciphertext_len = sizeof(ciphertext_data);
+  const unsigned char *plaintext = (const unsigned char *)test->plaintext;
+  unsigned char *ciphertext = (unsigned char *)ciphertext_data;
+  if (streaming) {
+    while (plaintext_len > kAesBlockBytes) {
+      crypto_const_byte_buf_t plaintext_block = {.data = plaintext,
+                                                 .len = kAesBlockBytes};
+      crypto_byte_buf_t ciphertext_block = {.data = ciphertext,
+                                            .len = kAesBlockBytes};
+      TRY(otcrypto_aes(&key, iv, test->mode, kAesOperationEncrypt,
+                       plaintext_block, kAesPaddingNull, ciphertext_block));
+      plaintext += kAesBlockBytes;
+      ciphertext += kAesBlockBytes;
+      plaintext_len -= kAesBlockBytes;
+      ciphertext_len -= kAesBlockBytes;
+    }
+  }
+
+  // Encrypt the remaining input in one shot with the requested padding.
+  crypto_const_byte_buf_t plaintext_buf = {.data = plaintext,
+                                           .len = plaintext_len};
+  crypto_byte_buf_t ciphertext_buf = {.data = ciphertext,
+                                      .len = ciphertext_len};
+  TRY(otcrypto_aes(&key, iv, test->mode, kAesOperationEncrypt, plaintext_buf,
+                   test->padding, ciphertext_buf));
+
+  TRY_CHECK_ARRAYS_EQ(ciphertext_data, test->exp_ciphertext,
+                      ARRAYSIZE(ciphertext_data));
+  return OK_STATUS();
+}
+
+/**
+ * Run AES decryption for the given test vector.
+ *
+ * If `streaming` is true, we will process the ciphertext in small chunks to
+ * ensure that the IV is updating correctly.
+ *
+ * @param test Test vector to run.
+ * @param streaming Whether to run in streaming mode.
+ */
+static status_t run_decrypt(const aes_test_t *test, bool streaming) {
+  // Determine the key configuration.
+  crypto_key_config_t config = make_key_config(test);
+
+  // Construct blinded key from the key and testing mask.
+  uint32_t keyblob[keyblob_num_words(config)];
+  TRY(keyblob_from_key_and_mask(test->key, kKeyMask, config, keyblob));
+  crypto_blinded_key_t key = {
+      .config = config,
+      .keyblob_length = sizeof(keyblob),
+      .keyblob = keyblob,
+  };
+  key.checksum = integrity_blinded_checksum(&key);
+
+  // Construct a buffer to hold the IV.
+  uint32_t iv_data[kAesBlockWords];
+  memcpy(iv_data, test->iv, kAesBlockBytes);
+  crypto_word32_buf_t iv = {
+      .data = iv_data,
+      .len = kAesBlockWords,
   };
 
   // Calculate the size of the padded plaintext.
   size_t padded_len_bytes;
-  CHECK_STATUS_OK(otcrypto_aes_padded_plaintext_length(
-      test->plaintext_len, test->padding, &padded_len_bytes));
-  CHECK(padded_len_bytes % sizeof(uint32_t) == 0);
-
-  // Create buffer for ciphertext.
-  uint32_t ciphertext_data[padded_len_bytes / sizeof(uint32_t)];
-  crypto_byte_buf_t ciphertext = {
-      .data = (unsigned char *)ciphertext_data,
-      .len = padded_len_bytes,
-  };
-
-  // Run encryption and check the result.
-  CHECK_STATUS_OK(otcrypto_aes(&key, iv, test->mode, kAesOperationEncrypt,
-                               plaintext, test->padding, ciphertext));
-
-  CHECK_ARRAYS_EQ(ciphertext_data, test->exp_ciphertext,
-                  ARRAYSIZE(ciphertext_data));
-
-  // Create a const buffer for the ciphertext.
-  crypto_const_byte_buf_t const_ciphertext = {
-      .data = (const unsigned char *)ciphertext_data,
-      .len = padded_len_bytes,
-  };
+  TRY(otcrypto_aes_padded_plaintext_length(test->plaintext_len, test->padding,
+                                           &padded_len_bytes));
 
   // Construct a buffer for the recovered plaintext.
-  uint32_t recovered_plaintext_data[ARRAYSIZE(ciphertext_data)];
-  crypto_byte_buf_t recovered_plaintext = {
-      .data = (unsigned char *)recovered_plaintext_data,
-      .len = padded_len_bytes,
-  };
+  TRY_CHECK(padded_len_bytes % sizeof(uint32_t) == 0);
+  size_t padded_len_words = padded_len_bytes / sizeof(uint32_t);
+  uint32_t recovered_plaintext_data[padded_len_words];
+  memset(recovered_plaintext_data, 0, sizeof(recovered_plaintext_data));
 
-  // Run decryption and check the result (not including padding).
-  CHECK_STATUS_OK(otcrypto_aes(&key, iv, test->mode, kAesOperationDecrypt,
-                               const_ciphertext, test->padding,
-                               recovered_plaintext));
+  // If in streaming mode, decrypt one block at a time with null padding until
+  // there is only 1 block of input remaining.
+  size_t len = sizeof(recovered_plaintext_data);
+  const unsigned char *ciphertext = (const unsigned char *)test->exp_ciphertext;
+  unsigned char *recovered_plaintext =
+      (unsigned char *)recovered_plaintext_data;
+  if (streaming) {
+    while (len > kAesBlockBytes) {
+      crypto_const_byte_buf_t ciphertext_block = {.data = ciphertext,
+                                                  .len = kAesBlockBytes};
+      crypto_byte_buf_t recovered_plaintext_block = {
+          .data = recovered_plaintext, .len = kAesBlockBytes};
+      TRY(otcrypto_aes(&key, iv, test->mode, kAesOperationDecrypt,
+                       ciphertext_block, kAesPaddingNull,
+                       recovered_plaintext_block));
+      ciphertext += kAesBlockBytes;
+      recovered_plaintext += kAesBlockBytes;
+      len -= kAesBlockBytes;
+    }
+    // Expect that the length is now exactly one block, since the ciphertext
+    // length should be a multiple of the block size.
+    TRY_CHECK(len == kAesBlockBytes);
+  }
 
-  CHECK_ARRAYS_EQ((unsigned char *)recovered_plaintext_data,
-                  (unsigned char *)test->plaintext, test->plaintext_len);
+  // Decrypt the remaining input in one shot.
+  crypto_const_byte_buf_t ciphertext_buf = {.data = ciphertext, .len = len};
+  crypto_byte_buf_t recovered_plaintext_buf = {.data = recovered_plaintext,
+                                               .len = len};
+  TRY(otcrypto_aes(&key, iv, test->mode, kAesOperationDecrypt, ciphertext_buf,
+                   test->padding, recovered_plaintext_buf));
+
+  // Check the result (not including padding).
+  TRY_CHECK_ARRAYS_EQ((unsigned char *)recovered_plaintext_data,
+                      (unsigned char *)test->plaintext, test->plaintext_len);
+
+  return OK_STATUS();
+}
+
+/**
+ * Test one-shot AES encryption.
+ */
+static status_t encrypt_test(void) {
+  return run_encrypt(test, /*streaming=*/false);
+}
+
+/**
+ * Test one-shot AES decryption.
+ */
+static status_t decrypt_test(void) {
+  return run_decrypt(test, /*streaming=*/false);
+}
+
+/**
+ * Test streaming AES encryption.
+ */
+static status_t encrypt_streaming_test(void) {
+  return run_encrypt(test, /*streaming=*/true);
+}
+
+/**
+ * Test streaming AES decryption.
+ */
+static status_t decrypt_streaming_test(void) {
+  return run_decrypt(test, /*streaming=*/true);
 }
 
 OTTF_DEFINE_TEST_CONFIG();
 
 bool test_main(void) {
+  status_t result = OK_STATUS();
+
   for (size_t i = 0; i < ARRAYSIZE(kAesTests); i++) {
     LOG_INFO("Starting AES test %d of %d...", i + 1, ARRAYSIZE(kAesTests));
-    encrypt_decrypt_test(&kAesTests[i]);
+    test = &kAesTests[i];
+    EXECUTE_TEST(result, encrypt_test);
+    EXECUTE_TEST(result, decrypt_test);
+    EXECUTE_TEST(result, encrypt_streaming_test);
+    EXECUTE_TEST(result, decrypt_streaming_test);
     LOG_INFO("Finished AES test %d.", i + 1);
   }
 
-  return true;
+  return status_ok(result);
 }
