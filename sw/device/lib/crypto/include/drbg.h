a4196cf13c21262b5be252f2c4e5cae57f188426
Jade Philipoom <jadep@google.com>
2023-09-25 12:38:10 +0200

    [crypto] Update DRBG interface.
    Make usage of `const` a little more consistent and provide clearer
    guidance about personalization string lengths.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

--- sw/device/lib/crypto/include/drbg.h
+++ sw/device/lib/crypto/include/drbg.h
@@ -19,30 +19,34 @@ extern "C" {
 /**
  * Instantiates the DRBG system.
  *
- * Initializes the DRBG and the context for DRBG. Gets the required
- * entropy input automatically from the entropy source.
+ * Initializes the DRBG and the context for DRBG. Gets the required entropy
+ * input automatically from the entropy source.
+ *
+ * The personalization string may empty, and may be up to 48 bytes long; any
+ * longer will result in an error.
  *
  * @param perso_string Pointer to personalization bitstring.
  * @return Result of the DRBG instantiate operation.
  */
-crypto_status_t otcrypto_drbg_instantiate(crypto_byte_buf_t perso_string);
+crypto_status_t otcrypto_drbg_instantiate(crypto_const_byte_buf_t perso_string);
 
 /**
  * Reseeds the DRBG with fresh entropy.
  *
- * Reseeds the DRBG with fresh entropy that is automatically fetched
- * from the entropy source and updates the working state parameters.
+ * Reseeds the DRBG with fresh entropy that is automatically fetched from the
+ * entropy source and updates the working state parameters.
  *
  * @param additional_input Pointer to the additional input for DRBG.
  * @return Result of the DRBG reseed operation.
  */
-crypto_status_t otcrypto_drbg_reseed(crypto_byte_buf_t additional_input);
+crypto_status_t otcrypto_drbg_reseed(crypto_const_byte_buf_t additional_input);
 
 /**
  * Instantiates the DRBG system.
  *
- * Initializes DRBG and the DRBG context. Gets the required entropy
- * input from the user through the `entropy` parameter.
+ * Initializes DRBG and the DRBG context. Gets the required entropy input from
+ * the user through the `entropy` parameter. Calling this function breaks FIPS
+ * compliance until the DRBG is uninstantiated.
  *
  * The entropy input must be exactly 384 bits long (48 bytes). The
  * personalization string must not be longer than the entropy input, and may be
@@ -53,45 +57,63 @@ crypto_status_t otcrypto_drbg_reseed(crypto_byte_buf_t additional_input);
  * @return Result of the DRBG manual instantiation.
  */
 crypto_status_t otcrypto_drbg_manual_instantiate(
-    crypto_byte_buf_t entropy, crypto_byte_buf_t perso_string);
+    crypto_const_byte_buf_t entropy, crypto_const_byte_buf_t perso_string);
 
 /**
  * Reseeds the DRBG with fresh entropy.
  *
- * Reseeds the DRBG with entropy input from the user through `entropy`
- * parameter and updates the working state parameters.
+ * Reseeds the DRBG with entropy input from the user through the `entropy`
+ * parameter and updates the working state parameters. Calling this function
+ * breaks FIPS compliance until the DRBG is uninstantiated.
  *
  * @param entropy Pointer to the user defined entropy value.
  * @param additional_input Pointer to the additional input for DRBG.
  * @return Result of the manual DRBG reseed operation.
  */
-crypto_status_t otcrypto_drbg_manual_reseed(crypto_byte_buf_t entropy,
-                                            crypto_byte_buf_t additional_input);
+crypto_status_t otcrypto_drbg_manual_reseed(
+    crypto_const_byte_buf_t entropy, crypto_const_byte_buf_t additional_input);
 
 /**
  * DRBG function for generating random bits.
  *
- * Used to generate pseudo random bits after DRBG instantiation or
- * DRBG reseeding.
+ * This function checks the hardware flags for FIPS compatibility of the
+ * generated bits, so it will fail after `otcrypto_drbg_manual_instantiate` or
+ * `otcrypto_drbg_manual_reseed`.
  *
- * The caller should allocate space for the `drbg_output` buffer,
- * (of length `output_len`), and set the length of expected
- * output in the `len` field of `drbg_output`. If the user-set length
- * and the output length does not match, an error message will be
- * returned.
+ * The caller should allocate space for the `drbg_output` buffer and set the
+ * length of expected output in the `len` field.
  *
- * The output is generated in 16-byte blocks; if `output_len` is not a multiple
- * of 16, some output from the hardware will be discarded. This detail may be
- * important for known-answer tests.
+ * The output is generated in 16-byte blocks; if `drbg_output->len` is not a
+ * multiple of 4, some output from the hardware will be discarded. This detail
+ * may be important for known-answer tests.
  *
  * @param additional_input Pointer to the additional data.
- * @param output_len Required len of pseudorandom output, in bytes.
  * @param[out] drbg_output Pointer to the generated pseudo random bits.
  * @return Result of the DRBG generate operation.
  */
-crypto_status_t otcrypto_drbg_generate(crypto_byte_buf_t additional_input,
-                                       size_t output_len,
-                                       crypto_byte_buf_t *drbg_output);
+crypto_status_t otcrypto_drbg_generate(crypto_const_byte_buf_t additional_input,
+                                       crypto_word32_buf_t *drbg_output);
+
+/**
+ * DRBG function for generating random bits.
+ *
+ * This function does NOT check the hardware flags for FIPS compatibility of the
+ * generated bits, so it may be called after `otcrypto_drbg_manual_instantiate`
+ * or `otcrypto_drbg_manual_reseed`.
+ *
+ * The caller should allocate space for the `drbg_output` buffer and set the
+ * length of expected output in the `len` field.
+ *
+ * The output is generated in 16-byte blocks; if `drbg_output->len` is not a
+ * multiple of 4, some output from the hardware will be discarded. This detail
+ * may be important for known-answer tests.
+ *
+ * @param additional_input Pointer to the additional data.
+ * @param[out] drbg_output Pointer to the generated pseudo random bits.
+ * @return Result of the DRBG generate operation.
+ */
+crypto_status_t otcrypto_drbg_manual_generate(
+    crypto_const_byte_buf_t additional_input, crypto_word32_buf_t *drbg_output);
 
 /**
  * Uninstantiates DRBG and clears the context.
