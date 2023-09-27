86207c7e966e407a49a94a750f345249c56e75ae
Andreas Kurth <adk@lowrisc.org>
2023-09-27 18:28:04 +0000

    [top_darjeeling] Autogen after adding external IRQs
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- hw/top_darjeeling/ip/soc_proxy/rtl/soc_proxy_reg_pkg.sv
+++ hw/top_darjeeling/ip/soc_proxy/rtl/soc_proxy_reg_pkg.sv
@@ -7,7 +7,7 @@
 package soc_proxy_reg_pkg;
 
   // Param list
-  parameter int unsigned NumExternalIrqs = 8;
+  parameter int unsigned NumExternalIrqs = 32;
   parameter int NumAlerts = 17;
 
   // Address widths within the block
@@ -19,15 +19,15 @@ package soc_proxy_reg_pkg;
   ///////////////////////////////////////////////
 
   typedef struct packed {
-    logic [7:0]  q;
+    logic [31:0] q;
   } soc_proxy_reg2hw_intr_state_reg_t;
 
   typedef struct packed {
-    logic [7:0]  q;
+    logic [31:0] q;
   } soc_proxy_reg2hw_intr_enable_reg_t;
 
   typedef struct packed {
-    logic [7:0]  q;
+    logic [31:0] q;
     logic        qe;
   } soc_proxy_reg2hw_intr_test_reg_t;
 
@@ -103,21 +103,21 @@ package soc_proxy_reg_pkg;
   } soc_proxy_reg2hw_alert_test_reg_t;
 
   typedef struct packed {
-    logic [7:0]  d;
+    logic [31:0] d;
     logic        de;
   } soc_proxy_hw2reg_intr_state_reg_t;
 
   // Register -> HW type for core interface
   typedef struct packed {
-    soc_proxy_reg2hw_intr_state_reg_t intr_state; // [58:51]
-    soc_proxy_reg2hw_intr_enable_reg_t intr_enable; // [50:43]
-    soc_proxy_reg2hw_intr_test_reg_t intr_test; // [42:34]
+    soc_proxy_reg2hw_intr_state_reg_t intr_state; // [130:99]
+    soc_proxy_reg2hw_intr_enable_reg_t intr_enable; // [98:67]
+    soc_proxy_reg2hw_intr_test_reg_t intr_test; // [66:34]
     soc_proxy_reg2hw_alert_test_reg_t alert_test; // [33:0]
   } soc_proxy_core_reg2hw_t;
 
   // HW -> register type for core interface
   typedef struct packed {
-    soc_proxy_hw2reg_intr_state_reg_t intr_state; // [8:0]
+    soc_proxy_hw2reg_intr_state_reg_t intr_state; // [32:0]
   } soc_proxy_core_hw2reg_t;
 
   // Register offsets for core interface
@@ -127,8 +127,8 @@ package soc_proxy_reg_pkg;
   parameter logic [CoreAw-1:0] SOC_PROXY_ALERT_TEST_OFFSET = 4'h c;
 
   // Reset values for hwext registers and their fields for core interface
-  parameter logic [7:0] SOC_PROXY_INTR_TEST_RESVAL = 8'h 0;
-  parameter logic [7:0] SOC_PROXY_INTR_TEST_EXTERNAL_RESVAL = 8'h 0;
+  parameter logic [31:0] SOC_PROXY_INTR_TEST_RESVAL = 32'h 0;
+  parameter logic [31:0] SOC_PROXY_INTR_TEST_EXTERNAL_RESVAL = 32'h 0;
   parameter logic [16:0] SOC_PROXY_ALERT_TEST_RESVAL = 17'h 0;
   parameter logic [0:0] SOC_PROXY_ALERT_TEST_FATAL_ALERT_INTG_RESVAL = 1'h 0;
   parameter logic [0:0] SOC_PROXY_ALERT_TEST_FATAL_ALERT_EXTERNAL_0_RESVAL = 1'h 0;
@@ -158,9 +158,9 @@ package soc_proxy_reg_pkg;
 
   // Register width information to check illegal writes for core interface
   parameter logic [3:0] SOC_PROXY_CORE_PERMIT [4] = '{
-    4'b 0001, // index[0] SOC_PROXY_INTR_STATE
-    4'b 0001, // index[1] SOC_PROXY_INTR_ENABLE
-    4'b 0001, // index[2] SOC_PROXY_INTR_TEST
+    4'b 1111, // index[0] SOC_PROXY_INTR_STATE
+    4'b 1111, // index[1] SOC_PROXY_INTR_ENABLE
+    4'b 1111, // index[2] SOC_PROXY_INTR_TEST
     4'b 0111  // index[3] SOC_PROXY_ALERT_TEST
   };
 
