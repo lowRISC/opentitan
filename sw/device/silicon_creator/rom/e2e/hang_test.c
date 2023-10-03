b3bc41a6b448bc4afcfa0656986237ed52d1bd12
Amaury Pouly <amaury.pouly@lowrisc.org>
2023-10-03 16:26:12 +0000

    [e2e] Fix rom_e2e_shutdown_watchdog
    The test does not make sense: it asks the watchdog to shutdown after
    2 seconds but the waits only 1 second before returning. Therefore the
    shutdown nevers happens while waiting and the tests fails. This commit
    fixes the test by setting the watchdog bite threshold at 1 second and
    change the test to wait 2 seconds. This way, and with some margin,
    it will always reset during the wait.
    
    I also change the flash program to display the value of the watchdog
    count after the loop. The idea is for the test to verify that in
    certain LC states, the watchdog was actually disabled. Ideally, a
    further test should be added to verify that in PROD, if we wait
    only half a second, the value will be printed before the shutdown
    and show the expected value.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/silicon_creator/rom/e2e/hang_test.c
+++ sw/device/silicon_creator/rom/e2e/hang_test.c
@@ -4,16 +4,28 @@
 
 #include <stdbool.h>
 
+#include "sw/device/lib/dif/dif_aon_timer.h"
 #include "sw/device/lib/runtime/ibex.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+
 OTTF_DEFINE_TEST_CONFIG();
 
 bool test_main(void) {
+  dif_aon_timer_t aon_timer;
+  CHECK_DIF_OK(dif_aon_timer_init(
+      mmio_region_from_addr(TOP_EARLGREY_AON_TIMER_AON_BASE_ADDR), &aon_timer));
+  /* Pet the watchdog to have make sure that we start from a known value */
+  CHECK_DIF_OK(dif_aon_timer_watchdog_pet(&aon_timer));
   ibex_timeout_t timeout = ibex_timeout_init(HANG_SECS * 1000000);
   while (!ibex_timeout_check(&timeout)) {
   }
+  /* Get the watchdog count value */
+  uint32_t count;
+  CHECK_DIF_OK(dif_aon_timer_watchdog_get_count(&aon_timer, &count));
   LOG_INFO("Returning after %d seconds", HANG_SECS);
+  LOG_INFO("Watchdog count: %d", count);
   return true;
 }
