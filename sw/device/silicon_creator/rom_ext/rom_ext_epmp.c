3cc2097650a611c911fd46c9ea9f01ae81aa8510
Chris Frantz <cfrantz@google.com>
2023-10-05 17:03:16 -0700

    [rom_ext] Use different ePMP entries for owner code
    Our ePMP implementation resolves conflicting regions by selecting the
    lowest matching entry.  Because the entire flash region is specified
    in entry 5 as read-only, placing owner code in entries 7-8 as
    read-execute fails to allow code execution.
    
    Since we're in the ROM_EXT, we no longer need the ROM regions 0-1.
    Reuse those regions to enable owner code execution.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/device/silicon_creator/rom_ext/rom_ext_epmp.c
+++ sw/device/silicon_creator/rom_ext/rom_ext_epmp.c
@@ -10,45 +10,47 @@
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
 void rom_ext_epmp_unlock_owner_stage_rx(epmp_region_t region) {
-  const int kEntry = 8;
+  const int kEntry = 1;
   epmp_state_configure_tor(kEntry, region, kEpmpPermLockedReadExecute);
 
   // Update the hardware configuration (CSRs).
   //
-  // Entry is hardcoded as 8. Make sure to modify hardcoded values if changing
+  // Entry is hardcoded as 1. Make sure to modify hardcoded values if changing
   // kEntry.
   //
   // The `pmp1cfg` configuration is the second field in `pmpcfg0`.
   //
   //            32          24          16           8           0
   //             +-----------+-----------+-----------+-----------+
-  // `pmpcfg2` = | `pmp11cfg | `pmp10cfg`| `pmp9cfg` | `pmp8cfg` |
+  // `pmpcfg0` = |  `pmp3cfg |  `pmp2cfg`| `pmp1cfg` | `pmp0cfg` |
   //             +-----------+-----------+-----------+-----------+
-  CSR_WRITE(CSR_REG_PMPADDR7, ((uint32_t)region.start) >> 2);
-  CSR_WRITE(CSR_REG_PMPADDR8, ((uint32_t)region.end) >> 2);
-  CSR_CLEAR_BITS(CSR_REG_PMPCFG2, 0xff);
-  CSR_SET_BITS(CSR_REG_PMPCFG2, (kEpmpModeTor | kEpmpPermLockedReadExecute));
+
+  CSR_WRITE(CSR_REG_PMPADDR0, ((uint32_t)region.start) >> 2);
+  CSR_WRITE(CSR_REG_PMPADDR1, ((uint32_t)region.end) >> 2);
+  CSR_CLEAR_BITS(CSR_REG_PMPCFG0, 0xff << 8);
+  CSR_SET_BITS(CSR_REG_PMPCFG0, (kEpmpModeTor | kEpmpPermLockedReadExecute)
+                                    << 8);
 }
 
 void rom_ext_epmp_unlock_owner_stage_r(epmp_region_t region) {
-  const int kEntry = 9;
+  const int kEntry = 2;
   epmp_state_configure_napot(kEntry, region, kEpmpPermLockedReadOnly);
 
   // Update the hardware configuration (CSRs).
   //
-  // Entry is hardcoded as 9. Make sure to modify hardcoded values if changing
+  // Entry is hardcoded as 2. Make sure to modify hardcoded values if changing
   // kEntry.
   //
-  // The `pmp6cfg` configuration is the second field in `pmpcfg1`.
+  // The `pmp2cfg` configuration is the third field in `pmpcfg0`.
   //
   //            32          24          16           8           0
   //             +-----------+-----------+-----------+-----------+
-  // `pmpcfg2` = | `pmp11cfg`| `pmp10cfg`| `pmp9cfg` | `pmp8cfg` |
+  // `pmpcfg0` = |  `pmp3cfg |  `pmp2cfg`| `pmp1cfg` | `pmp0cfg` |
   //             +-----------+-----------+-----------+-----------+
-  CSR_WRITE(CSR_REG_PMPADDR9,
+  CSR_WRITE(CSR_REG_PMPADDR2,
             (uint32_t)region.start >> 2 |
                 ((uint32_t)region.end - (uint32_t)region.start - 1) >> 3);
-  CSR_CLEAR_BITS(CSR_REG_PMPCFG2, 0xff << 8);
-  CSR_SET_BITS(CSR_REG_PMPCFG2,
-               ((kEpmpModeNapot | kEpmpPermLockedReadOnly) << 8));
+  CSR_CLEAR_BITS(CSR_REG_PMPCFG0, 0xff << 16);
+  CSR_SET_BITS(CSR_REG_PMPCFG0,
+               ((kEpmpModeNapot | kEpmpPermLockedReadOnly) << 16));
 }
