7e9d2e5e8168e6cd7e7018f9c681284b57b651d1
Tim Trippel <ttrippel@google.com>
2023-10-12 14:35:12 -0700

    [manuf] add utility to compute SHA256 of SW OTP partitions
    This adds a utility to compute digests of SW OTP partitions so they can
    be locked properly after they are configured.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/device/silicon_creator/manuf/lib/util.c
+++ sw/device/silicon_creator/manuf/lib/util.c
@@ -7,8 +7,19 @@
 #include <stdint.h>
 
 #include "sw/device/lib/base/status.h"
+#include "sw/device/lib/crypto/impl/sha2/sha256.h"
 #include "sw/device/lib/crypto/include/datatypes.h"
 #include "sw/device/lib/crypto/include/hash.h"
+#include "sw/device/lib/dif/dif_otp_ctrl.h"
+#include "sw/device/lib/testing/otp_ctrl_testutils.h"
+
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+#include "otp_ctrl_regs.h"  // Generated.
+
+static_assert(
+    OTP_CTRL_PARAM_VENDOR_TEST_SIZE % sizeof(OTP_CTRL_PARAM_VENDOR_TEST_SIZE) ==
+        0,
+    "OTP Vendor Test partition should be an integer multiple of 32-bit words.");
 
 status_t manuf_util_hash_lc_transition_token(const uint32_t *raw_token,
                                              size_t token_size_bytes,
@@ -45,6 +56,51 @@ status_t manuf_util_hash_lc_transition_token(const uint32_t *raw_token,
   TRY(otcrypto_xof_cshake(input, kXofCshakeModeCshake128, function_name_string,
                           customization_string, token_size_bytes, &output));
   memcpy(hashed_token, token_data, sizeof(token_data));
+
+  return OK_STATUS();
+}
+
+status_t manuf_util_hash_otp_partition(const dif_otp_ctrl_t *otp_ctrl,
+                                       dif_otp_ctrl_partition_t partition,
+                                       crypto_word32_buf_t *output) {
+  if (otp_ctrl == NULL || output == NULL || output->len != kSha256DigestWords) {
+    return INVALID_ARGUMENT();
+  }
+
+  switch (partition) {
+    case kDifOtpCtrlPartitionVendorTest: {
+      uint32_t vendor_test_32bit_array[OTP_CTRL_PARAM_VENDOR_TEST_SIZE /
+                                       sizeof(uint32_t)];
+
+      TRY(otp_ctrl_testutils_dai_read32_array(
+          otp_ctrl, kDifOtpCtrlPartitionVendorTest, 0, vendor_test_32bit_array,
+          OTP_CTRL_PARAM_VENDOR_TEST_SIZE / sizeof(uint32_t)));
+      crypto_const_byte_buf_t input = {
+          .data = (unsigned char *)vendor_test_32bit_array,
+          .len = OTP_CTRL_PARAM_VENDOR_TEST_SIZE,
+      };
+      TRY(otcrypto_hash(input, kHashModeSha256, output));
+    } break;
+    case kDifOtpCtrlPartitionCreatorSwCfg: {
+      crypto_const_byte_buf_t input = {
+          .data = (unsigned char *)(TOP_EARLGREY_OTP_CTRL_CORE_BASE_ADDR +
+                                    OTP_CTRL_SW_CFG_WINDOW_REG_OFFSET),
+          .len = OTP_CTRL_PARAM_CREATOR_SW_CFG_SIZE,
+      };
+      TRY(otcrypto_hash(input, kHashModeSha256, output));
+    } break;
+    case kDifOtpCtrlPartitionOwnerSwCfg: {
+      crypto_const_byte_buf_t input = {
+          .data = (unsigned char *)(TOP_EARLGREY_OTP_CTRL_CORE_BASE_ADDR +
+                                    OTP_CTRL_SW_CFG_WINDOW_REG_OFFSET +
+                                    OTP_CTRL_PARAM_CREATOR_SW_CFG_SIZE),
+          .len = OTP_CTRL_PARAM_OWNER_SW_CFG_SIZE,
+      };
+      TRY(otcrypto_hash(input, kHashModeSha256, output));
+    } break;
+    default:
+      return INVALID_ARGUMENT();
+  }
 
   return OK_STATUS();
 }
--- sw/device/silicon_creator/manuf/lib/util.h
+++ sw/device/silicon_creator/manuf/lib/util.h
@@ -8,6 +8,8 @@
 #include <stdint.h>
 
 #include "sw/device/lib/base/status.h"
+#include "sw/device/lib/crypto/include/datatypes.h"
+#include "sw/device/lib/dif/dif_otp_ctrl.h"
 
 /**
  * Hashes a lifecycle transition token to prepare it to be written to OTP.
@@ -29,5 +31,27 @@ OT_WARN_UNUSED_RESULT
 status_t manuf_util_hash_lc_transition_token(const uint32_t *raw_token,
                                              size_t token_size_bytes,
                                              uint64_t *hashed_token);
+
+/**
+ * Computes a SHA256 digest of the specified OTP partition.
+ *
+ * Acceptable OTP partitions are:
+ * - VendorTest
+ * - CreatorSwCfg
+ * - OwnerSwCfg
+ *
+ * For the *SwCfg partitions, the entire hash can be written to the UDS
+ * (Creator) certificate DiceTcbInfo extension (specifically the `fwids` field),
+ * and the least-significant 64-bits can be written to the corresponding OTP
+ * *_SW_CFG partition digest CSRs.
+ *
+ * @param partition The OTP partition to use.
+ * @param[out] output The output hash.
+ * @return Result of the hash operation.
+ */
+OT_WARN_UNUSED_RESULT
+status_t manuf_util_hash_otp_partition(const dif_otp_ctrl_t *otp_ctrl,
+                                       dif_otp_ctrl_partition_t partition,
+                                       crypto_word32_buf_t *hash);
 
 #endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_MANUF_LIB_UTIL_H_
