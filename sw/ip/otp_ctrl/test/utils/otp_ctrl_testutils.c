0a03fa0f21fb2cafd4b0d46246ae1ea7afaef0fa
Michael Schaffner <msf@opentitan.org>
2023-10-12 19:28:59 -0700

    [otp_ctrl_testutils] Add NVM counter functions
    This adds helper functions in otp_ctrl_testutils that allow
    implementing a strike counter inside the NVM counter OTP
    partition.
    
    The functions implement read & decode, encode & write,
    as well as increment functions that can be used in
    associated tests.
    
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- sw/ip/otp_ctrl/test/utils/otp_ctrl_testutils.c
+++ sw/ip/otp_ctrl/test/utils/otp_ctrl_testutils.c
@@ -162,3 +162,138 @@ status_t otp_ctrl_testutils_dai_write64(const dif_otp_ctrl_t *otp,
   }
   return OK_STATUS();
 }
+
+static const uint32_t kNvmCntWords =
+    OTP_CTRL_PARAM_EXT_NVM_ANTIREPLAY_FRESHNESS_CNT_SIZE / sizeof(uint32_t) / 2;
+static const uint32_t kNvmMaxCnt = kNvmCntWords * 32;
+
+/**
+ * Decodes a thermometer encoded 32bit word and returns the count.
+ *
+ * The faulty_bits argument is zero if the thermometer
+ * encoding is correct. In case it is incorrect,
+ * the bit position where the thermometer encoding has
+ * a "hole" is set to 1.
+ *
+ * examples:
+ *
+ * raw_data: 0x0000FFFF returns: 15 faulty_bits: 0x00000000
+ * raw_data: 0x3FFFFFFF returns: 29 faulty_bits: 0x00000000
+ * raw_data: 0x100FFFFF returns: 28 faulty_bits: 0x0FF00000
+ *
+ * This function assumes that at least one bit is set in the word.
+ */
+static uint32_t thermometer_decode(uint32_t raw_data, uint32_t *faulty_bits) {
+  uint32_t num_lz = (uint32_t)__builtin_clz(raw_data);
+  *faulty_bits = 0x7FFFFFFF;
+  *faulty_bits >>= num_lz - 1;
+  *faulty_bits &= raw_data ^ (*faulty_bits);
+  return 32 - num_lz;
+}
+
+status_t otp_ctrl_read_nvm_cnt(const dif_otp_ctrl_t *otp, uint32_t *val,
+                               otp_ctrl_nvm_cnt_error_t *err) {
+  uint32_t cnt[2];
+  *err = kOtpCtrlNvmCntErrOk;
+  *val = 0;
+  for (uint32_t k = 0; k < kNvmCntWords; k++) {
+    // This uses the fast memory-mapped read access path instead of the DAI.
+    TRY(dif_otp_ctrl_read_blocking(otp, kDifOtpCtrlPartitionExtNvm,
+                                   k * sizeof(uint64_t), cnt, 2));
+    // Check whether the redundant copies agree.
+    if (cnt[0] != cnt[1]) {
+      // If there was no previous corruption, this may still be correctable.
+      if (*err != kOtpCtrlNvmCntErrDataLoss) {
+        *err = kOtpCtrlNvmCntErrCorrected;
+      }
+      LOG_INFO("Potentially correctable encoding error in word %d: 0x%x | 0x%x",
+               k, cnt[0], cnt[1]);
+      cnt[0] |= cnt[1];  // Redundancy scheme uses bitwise OR.
+    }
+    // If the word is zero, we don't have to do anything here.
+    if (cnt[0] != 0) {
+      // If this word is nonzero, all previous words must have been all-ones.
+      if (*val != (k * 32)) {
+        LOG_INFO("Zero value expected in word %d: 0x%x", k, cnt[0]);
+        *err = kOtpCtrlNvmCntErrDataLoss;
+        // Set the val to the expected value so that decoding can continue
+        // even with corrupt encodings.
+        *val = k * 32;
+      }
+      // If this word is all-ones, we just add 32 and continue.
+      if (cnt[0] == 0xFFFFFFFF) {
+        *val += 32;
+        // If the word is neither zero nor all-ones, we can't take shortcuts.
+      } else {
+        uint32_t faulty_bits;
+        *val += thermometer_decode(cnt[0], &faulty_bits);
+        if (faulty_bits != 0) {
+          LOG_INFO(
+              "Corrupt data word %d: 0x%x (corrupt bits 0x%x), total count %d",
+              k, cnt[0], faulty_bits, *val);
+          *err = kOtpCtrlNvmCntErrDataLoss;
+        }
+      }
+    }
+  }
+  return OK_STATUS();
+}
+
+status_t otp_ctrl_prog_nvm_cnt_word_copies(const dif_otp_ctrl_t *otp,
+                                           uint32_t start_address,
+                                           uint32_t word, uint32_t len) {
+  TRY(otp_ctrl_testutils_wait_for_dai(otp));
+  for (uint32_t k = 0; k < len; k++) {
+    TRY(dif_otp_ctrl_dai_program32(otp, kDifOtpCtrlPartitionExtNvm,
+                                   start_address, word));
+    TRY(otp_ctrl_testutils_wait_for_dai(otp));
+    TRY(otp_ctrl_dai_write_error_check(otp));
+    start_address += sizeof(uint32_t);
+  }
+  return OK_STATUS();
+}
+
+status_t otp_ctrl_prog_nvm_cnt(const dif_otp_ctrl_t *otp, uint32_t val,
+                               bool last_word_only,
+                               otp_ctrl_nvm_cnt_error_t *err) {
+  *err = kOtpCtrlNvmCntErrOk;
+
+  if (val > kNvmMaxCnt) {
+    LOG_INFO("%d exceeds the maximum NVM count %d", val, kNvmMaxCnt);
+    *err = kOtpCtrlNvmCntErrSaturated;
+    return OK_STATUS();
+  }
+
+  uint32_t num_full_words = val / 32;
+  uint32_t last_word_val = val - num_full_words * 32;
+
+  // In case we only touch the last word and the counter value is a
+  // word-aligned all-ones sequence, we just program all-ones to the last
+  // word. Otherwise all words up to that last word are programmed.
+  if (last_word_only && (last_word_val == 0) && (num_full_words > 0)) {
+    TRY(otp_ctrl_prog_nvm_cnt_word_copies(
+        otp, (num_full_words - 1) * sizeof(uint64_t), 0xFFFFFFFF, 2));
+  } else {
+    TRY(otp_ctrl_prog_nvm_cnt_word_copies(otp, 0, 0xFFFFFFFF,
+                                          num_full_words * 2));
+  }
+
+  // Write remaining bits within the last word.
+  if (last_word_val != 0) {
+    TRY(otp_ctrl_prog_nvm_cnt_word_copies(
+        otp, num_full_words * sizeof(uint64_t), ((1 << last_word_val) - 1), 2));
+  }
+  return OK_STATUS();
+}
+
+status_t otp_ctrl_inc_nvm_cnt(const dif_otp_ctrl_t *otp, uint32_t *val,
+                              otp_ctrl_nvm_cnt_error_t *err) {
+  TRY(otp_ctrl_read_nvm_cnt(otp, val, err));
+  if (*err == kOtpCtrlNvmCntErrOk) {
+    TRY(otp_ctrl_prog_nvm_cnt(otp, (*val) + 1, true, err));
+    if (*err == kOtpCtrlNvmCntErrOk) {
+      (*val)++;
+    }
+  }
+  return OK_STATUS();
+}
--- sw/ip/otp_ctrl/test/utils/otp_ctrl_testutils.h
+++ sw/ip/otp_ctrl/test/utils/otp_ctrl_testutils.h
@@ -8,6 +8,13 @@
 #include "sw/ip/otp_ctrl/dif/dif_otp_ctrl.h"
 #include "sw/lib/sw/device/base/status.h"
 
+#include "otp_ctrl_regs.h"  // Generated.
+
+// Header Extern Guard (so header can be used from C and C++)
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
 /**
  * Enum that encodes the expectation for the check_dai_access_error function.
  */
@@ -113,5 +120,120 @@ status_t otp_ctrl_testutils_dai_write64(const dif_otp_ctrl_t *otp,
                                         dif_otp_ctrl_partition_t partition,
                                         uint32_t start_address,
                                         const uint64_t *buffer, size_t len);
+
+/**
+ * NVM Counter Encoding Error types
+ *
+ * kOtpCtrlNvmCntErrOk        : encoding is consistent
+ * kOtpCtrlNvmCntErrCorrected : lossless correction has occurred
+ * kOtpCtrlNvmCntErrDataLoss  : the counter encoding is corrupt / lossy
+ * kOtpCtrlNvmCntErrSaturated : the counter has saturated and can't be
+ * incremented anymore
+ */
+typedef enum {
+  kOtpCtrlNvmCntErrOk,
+  kOtpCtrlNvmCntErrCorrected,
+  kOtpCtrlNvmCntErrDataLoss,
+  kOtpCtrlNvmCntErrSaturated
+} otp_ctrl_nvm_cnt_error_t;
+
+/**
+ * Reads and decodes the NVM counter
+ *
+ * This reads and decodes the NVM counter on the fly so that memory
+ * requirements are not dependent on how large the counter partition in OTP
+ * is. I.e., instead of reading both redundant counter copies from OTP first,
+ * this function only reads out two 32bit words of each counter copy at a
+ * time. The two counter copies are word-level interleaved for convenience.
+ *
+ * The counter encoding uses "thermometer" or "strike" encoding, meaning that
+ * each bit that is set to 1 represents a counter increment (starting from
+ * bit offset 0), e.g.:
+ *
+ * raw data: 0x00000001, count: 1
+ * raw data: 0x0000000F, count: 4
+ * raw data: 0x0000FFFF, count: 16
+ *
+ * The decoding always detects the highest bit that is set to 1 in the entire
+ * counter array - but additional checks ensure that the encoding is
+ * consistent (the two counter copies must match, and all bits below the
+ * highest bit need to be set to ones, whereas all other bits above the
+ * highest bit need to be set to ).
+ *
+ * @param otp otp_ctrl instance.
+ * @param val the decoded counter value.
+ * @param err kOtpCtrlNvmCntErrOk        if the encoding is ok
+ *            kOtpCtrlNvmCntErrCorrected if a lossless correction occurred
+ *            kOtpCtrlNvmCntErrDataLoss  if the encoding is corrupt / lossy
+ * @return OK_STATUS on success.
+ */
+OT_WARN_UNUSED_RESULT
+status_t otp_ctrl_read_nvm_cnt(const dif_otp_ctrl_t *otp, uint32_t *val,
+                               otp_ctrl_nvm_cnt_error_t *err);
+
+/**
+ * Programs a 32bit value into consecutive 32bit locations.
+ *
+ * For writing a redundant 32bit pair, set len to 2.
+ *
+ * @param otp otp_ctrl instance.
+ * @param start_addr the relative address inside the counter partition.
+ * @param word the 32bit value to program.
+ * @param len number of copies to program into consecutive word locations.
+ * @return OK_STATUS on success.
+ */
+OT_WARN_UNUSED_RESULT
+status_t otp_ctrl_prog_nvm_cnt_word_copies(const dif_otp_ctrl_t *otp,
+                                           uint32_t start_addr, uint32_t word,
+                                           uint32_t len);
+
+/**
+ * Encodes and writes the NVM counter
+ *
+ * This encodes the count value into thermometer encoding and writes it to
+ * OTP. Similarly to otp_ctrl_read_nvm_cnt, operations are done while
+ * traversing the NVM counter partition in OTP so that memory requirements do
+ * not depend on how large that partition is.
+ *
+ * @param otp otp_ctrl instance.
+ * @param val the decoded counter value to program
+ * @param last_word_only Set to false to program all non-zero blocks.
+ *                        Set to true to only write the block
+ *                        affected by the MSB of the current counter state.
+ *                        This is a useful option if val has only been
+ * incremented by 1, since it completes much faster for larger counts.
+ * @param err kOtpCtrlNvmCntErrOk        if the encoding is ok
+ *            kOtpCtrlNvmCntErrSaturated the counter cannot be incremented
+ * anymore
+ * @return OK_STATUS on success.
+ */
+OT_WARN_UNUSED_RESULT
+status_t otp_ctrl_prog_nvm_cnt(const dif_otp_ctrl_t *otp, uint32_t val,
+                               bool last_word_only,
+                               otp_ctrl_nvm_cnt_error_t *err);
+
+/**
+ * Increments the NVM counter by 1
+ *
+ * This is a short-hand combination of the otp_ctrl_read_nvm_cnt() and
+ * otp_ctrl_prog_nvm_cnt() functions. It returns the current counter value
+ * stored in OTP.
+ *
+ * @param otp otp_ctrl instance.
+ * @param val the current counter value stored in OTP.
+ * @param err kOtpCtrlNvmCntErrOk        if the encoding is ok
+ *            kOtpCtrlNvmCntErrCorrected if a lossless correction occurred
+ *            kOtpCtrlNvmCntErrDataLoss  if the encoding is corrupt / lossy
+ *            kOtpCtrlNvmCntErrSaturated the counter cannot be incremented
+ * anymore
+ * @return OK_STATUS on success.
+ */
+OT_WARN_UNUSED_RESULT
+status_t otp_ctrl_inc_nvm_cnt(const dif_otp_ctrl_t *otp, uint32_t *val,
+                              otp_ctrl_nvm_cnt_error_t *err);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif  // __cplusplus
 
 #endif  // OPENTITAN_SW_IP_OTP_CTRL_TEST_UTILS_OTP_CTRL_TESTUTILS_H_
