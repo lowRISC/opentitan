cd93fe39dfeda117184fe0ec1f00e039c029086c
Guillermo Maturana <maturana@opentitan.org>
2023-10-18 19:21:55 +0000

    [sw,testutils] Add support for a scratch buffer in retention sram
    Add a scratch buffer in retention sram, needed for the all_escalation_resets
    test.
    
    Addresses #20066
    
    Signed-off-by: Guillermo Maturana <maturana@opentitan.org>

--- sw/device/lib/testing/ret_sram_testutils.c
+++ sw/device/lib/testing/ret_sram_testutils.c
@@ -15,15 +15,20 @@
 
 // All the memory used by these utilities should be placed within the the
 // owner portion of retention SRAM, as defined in retention_sram.h.
-enum {
-  kNumberOfCounters = 4,
-};
 
+/**
+ * This represents the memory used by these utilities.
+ */
 typedef struct testing_utilities {
   /**
    * An array of counters.
    */
-  uint32_t counters[kNumberOfCounters];
+  uint32_t counters[kRetSramTestutilsNumberOfCounters];
+
+  /**
+   * A scratch array.
+   */
+  uint32_t scratch[kRetSramTestutilsScratchSizeAsInts];
 } testing_utilities_t;
 
 enum { kOffsetOfTestutils = offsetof(retention_sram_t, owner) };
@@ -40,21 +45,21 @@ testing_utilities_t *testing_utilities =
                             kOffsetOfTestutils);
 
 status_t ret_sram_testutils_counter_clear(size_t counter) {
-  TRY_CHECK(counter < kNumberOfCounters);
+  TRY_CHECK(counter < kRetSramTestutilsNumberOfCounters);
   testing_utilities->counters[counter] = 0;
   return OK_STATUS();
 }
 
 status_t ret_sram_testutils_counter_get(size_t counter, uint32_t *value) {
   TRY_CHECK(value != NULL);
-  TRY_CHECK(counter < kNumberOfCounters);
+  TRY_CHECK(counter < kRetSramTestutilsNumberOfCounters);
   *value = testing_utilities->counters[counter];
   return OK_STATUS();
 }
 
 status_t ret_sram_testutils_counter_increment(size_t counter) {
   uint32_t value;
-  TRY_CHECK(counter < kNumberOfCounters);
+  TRY_CHECK(counter < kRetSramTestutilsNumberOfCounters);
   value = testing_utilities->counters[counter];
   TRY_CHECK(value < UINT32_MAX);
   testing_utilities->counters[counter]++;
@@ -62,7 +67,27 @@ status_t ret_sram_testutils_counter_increment(size_t counter) {
 }
 
 status_t ret_sram_testutils_counter_set(size_t counter, uint32_t value) {
-  TRY_CHECK(counter < kNumberOfCounters);
+  TRY_CHECK(counter < kRetSramTestutilsNumberOfCounters);
   testing_utilities->counters[counter] = value;
+  return OK_STATUS();
+}
+
+status_t ret_sram_testutils_scratch_read(size_t offset, size_t size,
+                                         uint32_t *dest) {
+  TRY_CHECK(dest != NULL);
+  TRY_CHECK(offset + size <= kRetSramTestutilsScratchSizeAsInts);
+  for (size_t i = 0; i < size; ++i) {
+    dest[i] = testing_utilities->scratch[offset + i];
+  }
+  return OK_STATUS();
+}
+
+status_t ret_sram_testutils_scratch_write(size_t offset, size_t size,
+                                          uint32_t *src) {
+  TRY_CHECK(src != NULL);
+  TRY_CHECK(offset + size <= kRetSramTestutilsScratchSizeAsInts);
+  for (size_t i = 0; i < size; ++i) {
+    testing_utilities->scratch[offset + i] = src[i];
+  }
   return OK_STATUS();
 }
