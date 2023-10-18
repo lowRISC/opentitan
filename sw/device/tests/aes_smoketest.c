6dbf674824e495d933ee98d5621db112c134b3cb
Hakim Filali <hfilali@lowrisc.org>
2023-10-18 11:54:19 +0000

    [chip-test/sival] Add parts of aes smoke test to aes testutils
    This commit adds the main parts of aes smoke test to aes testutils.
    This enables the EDN tests to utilize the same code whenever we need
    to consume entropy through the AES block.
    
    Signed-off-by: Hakim Filali <hfilali@lowrisc.org>

--- sw/device/tests/aes_smoketest.c
+++ sw/device/tests/aes_smoketest.c
@@ -15,30 +15,9 @@
 
 #define TIMEOUT (1000 * 1000)
 
-// The mask share, used to mask kKey. Note that the masking should not be done
-// manually. Software is expected to get the key in two shares right from the
-// beginning.
-static const uint8_t kKeyShare1[] = {
-    0x0f, 0x1f, 0x2f, 0x3F, 0x4f, 0x5f, 0x6f, 0x7f, 0x8f, 0x9f, 0xaf,
-    0xbf, 0xcf, 0xdf, 0xef, 0xff, 0x0a, 0x1a, 0x2a, 0x3a, 0x4a, 0x5a,
-    0x6a, 0x7a, 0x8a, 0x9a, 0xaa, 0xba, 0xca, 0xda, 0xea, 0xfa,
-};
-
 OTTF_DEFINE_TEST_CONFIG();
 
 status_t execute_test(dif_aes_t *aes) {
-  // Mask the key. Note that this should not be done manually. Software is
-  // expected to get the key in two shares right from the beginning.
-  uint8_t key_share0[sizeof(kAesModesKey256)];
-  for (int i = 0; i < sizeof(kAesModesKey256); ++i) {
-    key_share0[i] = kAesModesKey256[i] ^ kKeyShare1[i];
-  }
-
-  // "Convert" key share byte arrays to `dif_aes_key_share_t`.
-  dif_aes_key_share_t key;
-  memcpy(key.share0, key_share0, sizeof(key.share0));
-  memcpy(key.share1, kKeyShare1, sizeof(key.share1));
-
   // Setup ECB encryption transaction.
   dif_aes_transaction_t transaction = {
       .operation = kDifAesOperationEncrypt,
@@ -50,47 +29,12 @@ status_t execute_test(dif_aes_t *aes) {
       .reseed_on_key_change = false,
       .ctrl_aux_lock = false,
   };
-  CHECK_DIF_OK(dif_aes_start(aes, &transaction, &key, NULL));
 
-  // "Convert" plain data byte arrays to `dif_aes_data_t`.
-  dif_aes_data_t in_data_plain;
-  memcpy(in_data_plain.data, kAesModesPlainText, sizeof(in_data_plain.data));
-
-  // Load the plain text to trigger the encryption operation.
-  AES_TESTUTILS_WAIT_FOR_STATUS(aes, kDifAesStatusInputReady, true, TIMEOUT);
-  CHECK_DIF_OK(dif_aes_load_data(aes, in_data_plain));
-
-  // Read out the produced cipher text.
-  dif_aes_data_t out_data;
+  CHECK_STATUS_OK(aes_testutils_setup_encryption(transaction, aes));
 
   AES_TESTUTILS_WAIT_FOR_STATUS(aes, kDifAesStatusOutputValid, true, TIMEOUT);
 
-  CHECK_DIF_OK(dif_aes_read_output(aes, &out_data));
-
-  // Finish the ECB encryption transaction.
-  CHECK_DIF_OK(dif_aes_end(aes));
-
-  CHECK_ARRAYS_EQ((uint8_t *)out_data.data, kAesModesCipherTextEcb256,
-                  sizeof(out_data.data));
-
-  // Setup ECB decryption transaction.
-  transaction.operation = kDifAesOperationDecrypt;
-  CHECK_DIF_OK(dif_aes_start(aes, &transaction, &key, NULL));
-
-  // Load the previously produced cipher text to start the decryption operation.
-  AES_TESTUTILS_WAIT_FOR_STATUS(aes, kDifAesStatusInputReady, true, TIMEOUT);
-  CHECK_DIF_OK(dif_aes_load_data(aes, out_data));
-
-  // Read out the produced plain text.
-  AES_TESTUTILS_WAIT_FOR_STATUS(aes, kDifAesStatusOutputValid, true, TIMEOUT);
-  CHECK_DIF_OK(dif_aes_read_output(aes, &out_data));
-
-  // Finish the ECB encryption transaction.
-  CHECK_DIF_OK(dif_aes_end(aes));
-
-  CHECK_ARRAYS_EQ((uint8_t *)out_data.data, kAesModesPlainText,
-                  sizeof(out_data.data));
-  return OK_STATUS();
+  return aes_testutils_decrypt_ciphertext(transaction, aes);
 }
 
 bool test_main(void) {
