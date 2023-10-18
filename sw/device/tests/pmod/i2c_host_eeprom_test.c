a3c43d4db04d57942426c444d429fd03226b564e
Douglas Reis <doreis@lowrisc.org>
2023-10-18 11:43:14 +0100

    [i2c] Refactor the pmod eeprom test to accept different i2c instances
     This will be useful for sival tests, where we need to test all i2c
     instances.
    
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/device/tests/pmod/i2c_host_eeprom_test.c
+++ sw/device/tests/pmod/i2c_host_eeprom_test.c
@@ -33,18 +33,16 @@ enum {
   kAckPollTimeoutMicros = 80000,
 };
 
-static dif_rv_core_ibex_t rv_core_ibex;
-static dif_pinmux_t pinmux;
-static dif_i2c_t i2c;
-
-static status_t write_byte(const uint8_t addr[2], uint8_t byte) {
+static status_t write_byte(dif_i2c_t *i2c, const uint8_t addr[2],
+                           uint8_t byte) {
   uint8_t data[3] = {addr[0], addr[1], byte};
-  return i2c_testutils_write(&i2c, kDeviceAddr, sizeof(data), data, false);
+  return i2c_testutils_write(i2c, kDeviceAddr, sizeof(data), data, false);
 }
 
-static status_t read_byte(const uint8_t addr[2], uint8_t *byte) {
-  TRY(i2c_testutils_write(&i2c, kDeviceAddr, 2, addr, true));
-  return i2c_testutils_read(&i2c, kDeviceAddr, 1, byte, kDefaultTimeoutMicros);
+static status_t read_byte(dif_i2c_t *i2c, const uint8_t addr[2],
+                          uint8_t *byte) {
+  TRY(i2c_testutils_write(i2c, kDeviceAddr, 2, addr, true));
+  return i2c_testutils_read(i2c, kDeviceAddr, 1, byte, kDefaultTimeoutMicros);
 }
 
 /**
@@ -54,30 +52,30 @@ static status_t read_byte(const uint8_t addr[2], uint8_t *byte) {
  * microsecond timeout. The EEPROM device will not acknowledge the device
  * address until it is finished writing.
  */
-static status_t poll_while_busy(void) {
-  return i2c_testutils_read(&i2c, kDeviceAddr, 0, NULL, kAckPollTimeoutMicros);
+static status_t poll_while_busy(dif_i2c_t *i2c) {
+  return i2c_testutils_read(i2c, kDeviceAddr, 0, NULL, kAckPollTimeoutMicros);
 }
 
-static status_t write_read_random(void) {
+static status_t write_read_random(dif_i2c_t *i2c) {
   // Write a byte to some random address.
   const uint8_t kAddr[2] = {0x03, 0x21};
-  TRY(write_byte(kAddr, 0xAB));
+  TRY(write_byte(i2c, kAddr, 0xAB));
 
   // Wait for the write to finish.
-  TRY(poll_while_busy());
+  TRY(poll_while_busy(i2c));
 
   // Read back the data at that address.
   uint8_t read_data = 0x00;
-  TRY(read_byte(kAddr, &read_data));
+  TRY(read_byte(i2c, kAddr, &read_data));
 
   // Check the written byte matches what we wrote.
   TRY_CHECK(read_data == 0xAB, "Unexpected value: 0x%02x", read_data);
 
   // Erase the value we just wrote to prevent the success state persisting to
   // subsequent runs of the test.
-  TRY(write_byte(kAddr, 0xFF));
-  TRY(poll_while_busy());
-  TRY(read_byte(kAddr, &read_data));
+  TRY(write_byte(i2c, kAddr, 0xFF));
+  TRY(poll_while_busy(i2c));
+  TRY(read_byte(i2c, kAddr, &read_data));
 
   // Check the over-written byte matches the new value.
   TRY_CHECK(read_data == 0xFF, "Unexpected value: 0x%02x", read_data);
@@ -85,19 +83,19 @@ static status_t write_read_random(void) {
   return OK_STATUS();
 }
 
-static status_t write_read_page(void) {
+static status_t write_read_page(dif_i2c_t *i2c) {
   // Write multiple bytes to the 9th page (of 64 bytes each).
   const uint8_t kAddr[2] = {0x02, 0x01};
   uint8_t data[5] = {kAddr[0], kAddr[1], 0xAB, 0xCD, 0xEF};
-  TRY(i2c_testutils_write(&i2c, kDeviceAddr, sizeof(data), data, false));
+  TRY(i2c_testutils_write(i2c, kDeviceAddr, sizeof(data), data, false));
 
   // Wait for the write to finish.
-  TRY(poll_while_busy());
+  TRY(poll_while_busy(i2c));
 
   // Read back the data at that address.
   uint8_t read_data[3] = {0x00, 0x00, 0x00};
-  TRY(i2c_testutils_write(&i2c, kDeviceAddr, sizeof(kAddr), kAddr, true));
-  TRY(i2c_testutils_read(&i2c, kDeviceAddr, sizeof(read_data), read_data,
+  TRY(i2c_testutils_write(i2c, kDeviceAddr, sizeof(kAddr), kAddr, true));
+  TRY(i2c_testutils_read(i2c, kDeviceAddr, sizeof(read_data), read_data,
                          kDefaultTimeoutMicros));
 
   // Check the written bytes match what we wrote.
@@ -106,10 +104,10 @@ static status_t write_read_page(void) {
   // Erase the values we just wrote to prevent the success state persisting to
   // subsequent runs of the test.
   data[2] = data[3] = data[4] = 0xFF;
-  TRY(i2c_testutils_write(&i2c, kDeviceAddr, sizeof(data), data, false));
-  TRY(poll_while_busy());
-  TRY(i2c_testutils_write(&i2c, kDeviceAddr, sizeof(kAddr), kAddr, true));
-  TRY(i2c_testutils_read(&i2c, kDeviceAddr, sizeof(read_data), read_data,
+  TRY(i2c_testutils_write(i2c, kDeviceAddr, sizeof(data), data, false));
+  TRY(poll_while_busy(i2c));
+  TRY(i2c_testutils_write(i2c, kDeviceAddr, sizeof(kAddr), kAddr, true));
+  TRY(i2c_testutils_read(i2c, kDeviceAddr, sizeof(read_data), read_data,
                          kDefaultTimeoutMicros));
 
   // Check the over-written bytes match the new erased value.
@@ -118,37 +116,37 @@ static status_t write_read_page(void) {
   return OK_STATUS();
 }
 
-static status_t test_init(void) {
-  mmio_region_t base_addr =
-      mmio_region_from_addr(TOP_EARLGREY_RV_CORE_IBEX_CFG_BASE_ADDR);
+static status_t i2c_configure(dif_i2c_t *i2c, dif_pinmux_t *pinmux,
+                          uintptr_t i2c_base_addr) {
+  mmio_region_t base_addr;
 
-  TRY(dif_rv_core_ibex_init(base_addr, &rv_core_ibex));
+  base_addr = mmio_region_from_addr(i2c_base_addr);
+  TRY(dif_i2c_init(base_addr, i2c));
 
-  base_addr = mmio_region_from_addr(TOP_EARLGREY_I2C2_BASE_ADDR);
-  TRY(dif_i2c_init(base_addr, &i2c));
+  TRY(i2c_testutils_select_pinmux(pinmux, 2));
 
-  base_addr = mmio_region_from_addr(TOP_EARLGREY_PINMUX_AON_BASE_ADDR);
-  TRY(dif_pinmux_init(base_addr, &pinmux));
-
-  TRY(i2c_testutils_select_pinmux(&pinmux, 2));
-
-  TRY(dif_i2c_host_set_enabled(&i2c, kDifToggleEnabled));
+  TRY(dif_i2c_host_set_enabled(i2c, kDifToggleEnabled));
 
   return OK_STATUS();
 }
 
 bool test_main(void) {
-  status_t test_result;
-  CHECK_STATUS_OK(test_init());
+  dif_pinmux_t pinmux;
+  dif_i2c_t i2c;
+
+  CHECK_DIF_OK(dif_pinmux_init(
+      mmio_region_from_addr(TOP_EARLGREY_PINMUX_AON_BASE_ADDR), &pinmux));
+
+  CHECK_STATUS_OK(i2c_configure(&i2c, &pinmux, TOP_EARLGREY_I2C2_BASE_ADDR));
 
   dif_i2c_speed_t speeds[] = {kDifI2cSpeedStandard, kDifI2cSpeedFast,
                               kDifI2cSpeedFastPlus};
 
-  test_result = OK_STATUS();
+  status_t test_result = OK_STATUS();
   for (size_t i = 0; i < ARRAYSIZE(speeds); ++i) {
     CHECK_STATUS_OK(i2c_testutils_set_speed(&i2c, speeds[i]));
-    EXECUTE_TEST(test_result, write_read_random);
-    EXECUTE_TEST(test_result, write_read_page);
+    EXECUTE_TEST(test_result, write_read_random, &i2c);
+    EXECUTE_TEST(test_result, write_read_page, &i2c);
   }
 
   return status_ok(test_result);
