576a6f3a46516d3f57fbb7aab2f539a3f37a74d1
Hakim Filali <hfilali@lowrisc.org>
2023-10-18 12:15:45 +0000

    [chip-test/sival] Add edn testutils library
    This commit adds a edn testutils library which for now includes
    the random command generation for auto mode. So far this has only
    been used in the csrng_edn_concurrency_test but the same code will
    be reused in the sw_edn_sw_mode test. Therefore, it might make sense
    to add it to the edn testutils library.
    
    Signed-off-by: Hakim Filali <hfilali@lowrisc.org>

--- sw/device/tests/csrng_edn_concurrency_test.c
+++ sw/device/tests/csrng_edn_concurrency_test.c
@@ -11,6 +11,7 @@
 #include "sw/device/lib/runtime/irq.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/csrng_testutils.h"
+#include "sw/device/lib/testing/edn_testutils.h"
 #include "sw/device/lib/testing/entropy_testutils.h"
 #include "sw/device/lib/testing/otbn_testutils.h"
 #include "sw/device/lib/testing/rand_testutils.h"
@@ -126,57 +127,6 @@ static void init_peripherals(void) {
 }
 
 /**
- * Returns randomized seed material.
- */
-static dif_edn_seed_material_t edn_seed_material_build(void) {
-  dif_edn_seed_material_t seed;
-  seed.len =
-      rand_testutils_gen32_range(/*min=*/0, kDifEntropySeedMaterialMaxWordLen);
-  for (size_t i = 0; i < seed.len; ++i) {
-    seed.data[i] = rand_testutils_gen32();
-  }
-  return seed;
-}
-
-/**
- * Returns a randomized EDN auto mode configuration.
- */
-static dif_edn_auto_params_t edn_auto_params_build(void) {
-  dif_edn_seed_material_t seed0 = edn_seed_material_build();
-  dif_edn_seed_material_t seed1 = edn_seed_material_build();
-  return (dif_edn_auto_params_t){
-      .instantiate_cmd =
-          {
-              .cmd = csrng_cmd_header_build(kCsrngAppCmdInstantiate,
-                                            kDifCsrngEntropySrcToggleEnable,
-                                            seed0.len,
-                                            /*generate_len=*/0),
-              .seed_material = seed0,
-          },
-      .reseed_cmd =
-          {
-              .cmd = csrng_cmd_header_build(kCsrngAppCmdReseed,
-                                            kDifCsrngEntropySrcToggleEnable,
-                                            seed1.len,
-                                            /*generate_len=*/0),
-              .seed_material = seed1,
-          },
-      .generate_cmd =
-          {
-              .cmd = csrng_cmd_header_build(kCsrngAppCmdGenerate,
-                                            kDifCsrngEntropySrcToggleEnable,
-                                            /*cmd_len=*/0,
-                                            /*generate_len=*/1),
-              .seed_material =
-                  {
-                      .len = 0,
-                  },
-          },
-      .reseed_interval = 4,
-  };
-}
-
-/**
  * Helper function used to set the task done flag.
  *
  * Issues a `ottf_task_yield()` to ensure control is handed over to pending
@@ -272,8 +222,8 @@ static void entropy_config(void) {
   CHECK_STATUS_OK(entropy_testutils_auto_mode_init());
 
   LOG_INFO("Generating EDN params");
-  dif_edn_auto_params_t edn_params0 = edn_auto_params_build();
-  dif_edn_auto_params_t edn_params1 = edn_auto_params_build();
+  dif_edn_auto_params_t edn_params0 = edn_testutils_auto_params_build();
+  dif_edn_auto_params_t edn_params1 = edn_testutils_auto_params_build();
 
   task_iter_count_max[kTestTaskIdOtbn] =
       rand_testutils_gen32_range(/*min=*/1, kTestParamNumOtbnIterationsMax);
