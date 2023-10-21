5a49fd05996fa5c300eeaf2187fd9b21e41cfbf6
Guillermo Maturana <maturana@opentitan.org>
2023-10-21 00:35:51 +0000

    [sival,pwrmgr] Rework the various sleep_all_reset tests
    Create a library to share the C code.
    Remove some redundant test cases.
    Avoid memory overwrite, instead have the CPU sequence the reset cases.
    Adjust SV sequence so it can deal with all three tests.
    
    Addresses #20133
    Addresses #20134
    
    Signed-off-by: Guillermo Maturana <maturana@opentitan.org>

--- hw/top_earlgrey/dv/env/seq_lib/chip_vseq_list.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_vseq_list.sv
@@ -24,7 +24,6 @@
 `include "chip_sw_main_power_glitch_vseq.sv"
 `include "chip_sw_random_power_glitch_vseq.sv"
 `include "chip_sw_sysrst_ctrl_vseq.sv"
-`include "chip_sw_random_sleep_all_reset_vseq.sv"
 `include "chip_sw_deep_sleep_all_reset_vseq.sv"
 `include "chip_sw_sleep_por_reset_vseq.sv"
 `include "chip_sw_uart_tx_rx_vseq.sv"
