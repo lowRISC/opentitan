9ceac559d91afae402b2d8932bd34fb4c45f58d0
Tim Trippel <ttrippel@google.com>
2023-10-24 12:01:24 -0700

    [manuf] remove unused ECC key file
    Now that the ECC public key is derived from the private key inside the
    rust test harness, we no longer need the public key files.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/device/silicon_creator/manuf/keys/fake/fake_keys.c
+++
@@ -1,9 +0,0 @@
-// Copyright lowRISC contributors.
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-
-#include "sw/device/lib/crypto/include/ecc.h"
-#include "sw/device/silicon_creator/manuf/keys/fake/rma_unlock_token_export_key.pk_hsm.h"
-
-const ecc_public_key_t kRmaUnlockTokenExportKeyPkHsm =
-    RMA_UNLOCK_TOKEN_EXPORT_KEY_PK_HSM;
