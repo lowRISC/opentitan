a3a95d1c7484ccb1f67af9cc8fac11c61f94cbec
Chris Frantz <cfrantz@google.com>
2023-10-26 08:12:42 -0700

    [boot_svc] Do not use rnd in the empty message
    The empty message cannot be initialized with random data using the
    silicon_creator `rnd` driver.  That driver reads OTP from SW_CREATOR_CFG
    and that partition is locked by the ROM_EXT.  Attempting to use the
    `rnd` driver from the owner stage will result in a cpu fault.
    
    Since the point of the empty message is to overwrite any existing
    content in the boot_svc retention RAM, writing zeros is sufficient.
    
    1. Rename the `rand_data` field to `payload`.
    2. Do not initialize the payload with random data.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/device/silicon_creator/lib/boot_svc/boot_svc_empty_unittest.cc
+++ sw/device/silicon_creator/lib/boot_svc/boot_svc_empty_unittest.cc
@@ -10,7 +10,6 @@
 
 #include "gtest/gtest.h"
 #include "sw/device/silicon_creator/lib/boot_svc/mock_boot_svc_header.h"
-#include "sw/device/silicon_creator/lib/drivers/mock_rnd.h"
 #include "sw/device/silicon_creator/testing/rom_test.h"
 
 bool operator==(boot_svc_empty_t lhs, boot_svc_empty_t rhs) {
@@ -24,23 +23,19 @@ using ::testing::Return;
 
 class BootSvcEmptyTest : public rom_test::RomTest {
  protected:
-  rom_test::MockRnd rnd_;
   rom_test::MockBootSvcHeader boot_svc_header_;
 };
 
 TEST_F(BootSvcEmptyTest, Init) {
-  std::array<uint32_t, kBootSvcEmptyPayloadWordCount> rand_data;
-  std::iota(rand_data.begin(), rand_data.end(), 0xcafe0000);
-  for (auto v : rand_data) {
-    EXPECT_CALL(rnd_, Uint32).WillOnce(Return(v));
-  }
+  std::array<uint32_t, kBootSvcEmptyPayloadWordCount> payload{0};
+
   boot_svc_empty_t msg{};
   EXPECT_CALL(boot_svc_header_,
               Finalize(kBootSvcEmptyType, sizeof(msg), &msg.header));
 
   boot_svc_empty_init(&msg);
 
-  EXPECT_THAT(msg.rand_data, ElementsAreArray(rand_data));
+  EXPECT_THAT(msg.payload, ElementsAreArray(payload));
 }
 
 }  // namespace
