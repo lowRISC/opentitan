4a452f0a2c355757895f0719093e7ae0e0ad8d67
Bobby Reyes <bobby@rivosinc.com>
2023-10-29 19:07:27 -0700

    [keymgr_dpe, dv] re-names file and classes
    Re-names class names and file refernces from
    keymgr to keymgr_dpe
    
    Signed-off-by: Bobby Reyes <bobby@rivosinc.com>

--- hw/ip/keymgr_dpe/dv/cov/keymgr_dpe_cov_bind.sv
+++ hw/ip/keymgr_dpe/dv/cov/keymgr_dpe_cov_bind.sv
@@ -2,8 +2,8 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 //
-// Binds functional coverage interaface to the top level keymgr module.
-module keymgr_cov_bind;
+// Binds functional coverage interaface to the top level keymgr_dpe module.
+module keymgr_dpe_cov_bind;
 
   bind keymgr cip_lc_tx_cov_if u_lc_escalate_en_cov_if (
     .rst_ni (rst_ni),
