3321fa01837047c7de0ab93f1e68b12827d99aab
Bobby Reyes <bobby@rivosinc.com>
2023-11-02 08:34:17 -0700

    [keymgr_dpe, dv] modify keymgr_dpe_smoke_vseq
    Changes the behavior of the keymgr_dpe_smoke_vseq,
    it now does first advance call, followed by further advance calls
    and generates hw/sw keys. Also improves the keymgr_dpe_operations task
    called in the smoke test by adding debug print and randomize default values.
    
    Signed-off-by: Bobby Reyes <bobby@rivosinc.com>

--- hw/ip/keymgr_dpe/dv/env/seq_lib/keymgr_dpe_smoke_vseq.sv
+++ hw/ip/keymgr_dpe/dv/env/seq_lib/keymgr_dpe_smoke_vseq.sv
@@ -7,19 +7,31 @@ class keymgr_dpe_smoke_vseq extends keymgr_dpe_base_vseq;
   `uvm_object_utils(keymgr_dpe_smoke_vseq)
   `uvm_object_new
 
+  rand int num_ops;
+
   // limit to SW operations
   constraint gen_operation_c {
-    gen_operation inside {keymgr_dpe_pkg::OpDpeGenSwOut};
+    gen_operation inside {
+      keymgr_dpe_pkg::OpDpeGenHwOut,
+      keymgr_dpe_pkg::OpDpeGenSwOut
+    };
+  }
+  constraint num_ops_c {
+    num_ops inside {[2:3]};
   }
 
   task body();
-    keymgr_dpe_pkg::keymgr_dpe_exposed_working_state_e state;
-    `uvm_info(`gfn, "Key manager dpe seq start", UVM_HIGH)
-    // Advance from StReset to last state StDisabled and advance one extra time,
-    // then it should stay at StDisabled
-    // In each state check SW/HW output
-    repeat (state.num() + 1) begin
-      keymgr_dpe_operations(.advance_state(1), .num_gen_op(1), .clr_output(1));
+    `uvm_info(`gfn, "Key Manager DPE Smoke Start", UVM_HIGH)
+    /* 1. Advance from StWorkDpeReset to StWorkDpeAvailable state (latching OTP)
+         - Check OTP key
+         - Check dst slot values
+       2. Make further advance calls 
+         - Generate SW/HW identity's 
+         - Check SW/HW output
+         - Check dst slot values
+    */
+    repeat (2) begin
+      keymgr_dpe_operations(.advance_state(1), .num_gen_op(0), .clr_output(1));
     end
 
   endtask : body
