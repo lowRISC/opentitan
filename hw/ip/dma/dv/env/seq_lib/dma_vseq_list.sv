2a2d8db4d63ebd4d94957f602c607605ded41e98
Adrian Lees <a.lees@lowrisc.org>
2023-11-06 18:44:38 +0000

    [dv,dma] Simple smoke test with all transfer types
    Introduce a simple smoke test that combines memory-to-memory
    transfers and hardware handshaking transfers.
    
    Signed-off-by: Adrian Lees <a.lees@lowrisc.org>

--- hw/ip/dma/dv/env/seq_lib/dma_vseq_list.sv
+++ hw/ip/dma/dv/env/seq_lib/dma_vseq_list.sv
@@ -5,6 +5,7 @@
 `include "dma_base_vseq.sv"
 `include "dma_common_vseq.sv"
 `include "dma_generic_vseq.sv"
+`include "dma_generic_smoke_vseq.sv"
 `include "dma_memory_vseq.sv"
 `include "dma_memory_smoke_vseq.sv"
 `include "dma_handshake_vseq.sv"
