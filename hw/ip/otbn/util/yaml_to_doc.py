625a69f2d651e2b1d8ff0bb884c9d0bfbf1de142
Rupert Swarbrick <rswarbrick@lowrisc.org>
2023-11-13 22:50:51 +0000

    [otbn]: Add ISR support to otbn_as and otbn_objdump
    Most of the work in this patch is loading up the lists of CSRs and
    WSRs and then passing them through the code that translates between
    source code and an executable binary.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/otbn/util/yaml_to_doc.py
+++ hw/ip/otbn/util/yaml_to_doc.py
@@ -391,7 +391,7 @@ def main() -> int:
     args = parser.parse_args()
 
     try:
-        insns = load_file(args.yaml_file)
+        insns = load_file(args.yaml_file, None)
         impls = read_implementation(args.py_file)
     except RuntimeError as err:
         print(err, file=sys.stderr)
