6079d720188a5046d798d2974506b21acd42c29a
Loïc Lefort <loic@rivosinc.com>
2023-11-20 17:53:49 +0100

    [sw/device/silicon_creator] improve ibex driver to support more than 2 remapper slots
    Signed-off-by: Loïc Lefort <loic@rivosinc.com>

--- sw/device/silicon_creator/lib/drivers/ibex_unittest.cc
+++ sw/device/silicon_creator/lib/drivers/ibex_unittest.cc
@@ -23,10 +23,15 @@ class AddressTranslationTest : public rom_test::RomTest {
   rom_test::MockSecMmio sec_;
 };
 
-TEST_F(AddressTranslationTest, Slot0Sucess) {
+TEST_F(AddressTranslationTest, SlotCount) {
+  EXPECT_EQ(ibex_addr_remap_slots(), RV_CORE_IBEX_PARAM_NUM_REGIONS);
+}
+
+TEST_F(AddressTranslationTest, Slot0Success) {
   uint32_t matching_addr = 0x9000000;
   uint32_t remap_addr = 0x2000000;
   uint32_t size = 0x8000;
+
   EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_IBUS_ADDR_MATCHING_0_REG_OFFSET,
                      0x9003fff);
   EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_DBUS_ADDR_MATCHING_0_REG_OFFSET,
@@ -40,27 +45,63 @@ TEST_F(AddressTranslationTest, Slot0Sucess) {
   EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_IBUS_ADDR_EN_0_REG_OFFSET, 1);
   EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_DBUS_ADDR_EN_0_REG_OFFSET, 1);
 
-  ibex_addr_remap_0_set(matching_addr, remap_addr, size);
+  EXPECT_EQ(ibex_addr_remap_set(0, matching_addr, remap_addr, size), kErrorOk);
 }
 
-TEST_F(AddressTranslationTest, Slot1Sucess) {
+TEST_F(AddressTranslationTest, SlotMaxSuccess) {
   uint32_t matching_addr = 0xB040000;
   uint32_t remap_addr = 0x6000000;
   uint32_t size = 0x80000;
-  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_IBUS_ADDR_MATCHING_1_REG_OFFSET,
-                     0xb07ffff);
-  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_DBUS_ADDR_MATCHING_1_REG_OFFSET,
-                     0xb07ffff);
+  size_t slot = ibex_addr_remap_slots() - 1;
+  uint32_t offset = slot * 4;
 
-  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_IBUS_REMAP_ADDR_1_REG_OFFSET,
+  EXPECT_SEC_WRITE32(
+      base_ + RV_CORE_IBEX_IBUS_ADDR_MATCHING_0_REG_OFFSET + offset, 0xb07ffff);
+  EXPECT_SEC_WRITE32(
+      base_ + RV_CORE_IBEX_DBUS_ADDR_MATCHING_0_REG_OFFSET + offset, 0xb07ffff);
+
+  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_IBUS_REMAP_ADDR_0_REG_OFFSET + offset,
                      remap_addr);
-  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_DBUS_REMAP_ADDR_1_REG_OFFSET,
+  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_DBUS_REMAP_ADDR_0_REG_OFFSET + offset,
                      remap_addr);
 
-  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_IBUS_ADDR_EN_1_REG_OFFSET, 1);
-  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_DBUS_ADDR_EN_1_REG_OFFSET, 1);
+  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_IBUS_ADDR_EN_0_REG_OFFSET + offset,
+                     1);
+  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_DBUS_ADDR_EN_0_REG_OFFSET + offset,
+                     1);
 
-  ibex_addr_remap_1_set(matching_addr, remap_addr, size);
+  EXPECT_EQ(ibex_addr_remap_set(slot, matching_addr, remap_addr, size),
+            kErrorOk);
 }
+
+TEST_F(AddressTranslationTest, SlotInvalidFail) {
+  uint32_t slot = ibex_addr_remap_slots();
+
+  EXPECT_EQ(ibex_addr_remap_set(slot, 0, 0, 0), kErrorIbexBadRemapSlot);
+}
+
+TEST_F(AddressTranslationTest, Slot0LockSuccess) {
+  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_IBUS_REGWEN_0_REG_OFFSET, 1);
+  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_DBUS_REGWEN_0_REG_OFFSET, 1);
+
+  EXPECT_EQ(ibex_addr_remap_lock(0), kErrorOk);
+}
+
+TEST_F(AddressTranslationTest, SlotMaxLockSuccess) {
+  size_t slot = ibex_addr_remap_slots() - 1;
+  uint32_t offset = slot * 4;
+
+  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_IBUS_REGWEN_0_REG_OFFSET + offset, 1);
+  EXPECT_SEC_WRITE32(base_ + RV_CORE_IBEX_DBUS_REGWEN_0_REG_OFFSET + offset, 1);
+
+  EXPECT_EQ(ibex_addr_remap_lock(slot), kErrorOk);
+}
+
+TEST_F(AddressTranslationTest, SlotInvalidLockFail) {
+  uint32_t slot = ibex_addr_remap_slots();
+
+  EXPECT_EQ(ibex_addr_remap_lock(slot), kErrorIbexBadRemapSlot);
+}
+
 }  // namespace
 }  // namespace ibex_unittest
