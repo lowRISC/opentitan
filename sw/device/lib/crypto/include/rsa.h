4f5b7d46e2b46c885df9e11d17ef838f409a26aa
Jade Philipoom <jadep@google.com>
2023-11-23 11:43:15 +0100

    [crypto] Adjust RSA interface to remove specialized structs.
    Use normal key and word-buffer structs for RSA keys and signatures
    instead of specialized multi-level structs. This is more convenient for
    the caller and also will make it easier to adjust the representation of
    RSA datatypes under the hood.
    
    Signed-off-by: Jade Philipoom <jadep@google.com>

--- sw/device/lib/crypto/include/rsa.h
+++ sw/device/lib/crypto/include/rsa.h
@@ -31,38 +31,51 @@ typedef enum rsa_padding {
 } rsa_padding_t;
 
 /**
- * Struct to handle the RSA private exponent and modulus.
- */
-typedef struct rsa_private_key {
-  // Unblinded key struct with RSA modulus.
-  crypto_unblinded_key_t n;
-  // Blinded key struct with RSA private exponent.
-  crypto_blinded_key_t d;
-} rsa_private_key_t;
-
-/**
  * Enum to define possible lengths of RSA (public) keys.
  *
  * Values are hardened.
  */
-typedef enum rsa_key_size {
-  // 2048-bit RSA key.
-  kRsaKeySize2048 = 0x5d1,
-  // 3072-bit RSA key.
-  kRsaKeySize3072 = 0xc35,
-  // 4096-bit RSA key.
-  kRsaKeySize4096 = 0x8da,
-} rsa_key_size_t;
+typedef enum rsa_size {
+  // 2048-bit RSA.
+  kRsaSize2048 = 0x5d1,
+  // 3072-bit RSA.
+  kRsaSize3072 = 0xc35,
+  // 4096-bit RSA.
+  kRsaSize4096 = 0x8da,
+} rsa_size_t;
 
 /**
- * Struct to handle the RSA public exponent and modulus.
+ * Get the length of an RSA public key.
+ *
+ * The internal representation of RSA public keys is implementation-specific;
+ * this function will return the expected key length for a given RSA size.
+ *
+ * @param size RSA size parameter.
+ * @param[out] key_length Key length in bytes.
+ * @return Result of the operation.
  */
-typedef struct rsa_public_key {
-  // Unblinded key struct with RSA modulus.
-  crypto_unblinded_key_t n;
-  // Blinded key struct with RSA public exponent.
-  crypto_unblinded_key_t e;
-} rsa_public_key_t;
+crypto_status_t otcrypto_rsa_public_key_length(rsa_size_t size,
+                                               size_t *key_length);
+
+/**
+ * Get the length of an RSA private key.
+ *
+ * The internal representation of RSA private keys is implementation-specific;
+ * this function will return the expected key and keyblob length for a given
+ * RSA size. The `key_length` output parameter represents the unmasked form of
+ * the private key and is intended for the `key_length` field of
+ * `crypto_key_config_t`, while the `keyblob_length` parameter represents the
+ * blinded form and is intended for the `keyblob_length` field of
+ * `crypto_unblinded_key_t`.
+ *
+ * @param size RSA size parameter.
+ * @param[out] key_length Unblinded key length in bytes.
+ * @param[out] keyblob_length Blinded keyblob length in bytes.
+ * @return Result of the operation.
+ */
+crypto_status_t otcrypto_rsa_private_key_length(rsa_size_t size,
+                                                size_t *key_length,
+                                                size_t *keyblob_length);
 
 /**
  * Performs the RSA key generation.
@@ -70,49 +83,79 @@ typedef struct rsa_public_key {
  * Computes RSA private key (d) and RSA public key exponent (e) and
  * modulus (n).
  *
- * The caller should allocate and partially populate all blinded and unblinded
- * key structs underneath `rsa_private_key` and `rsa_public_key`.
+ * The caller should allocate space for the public key and set the `key` and
+ * `key_length` fields accordingly. Use `otcrypto_rsa_public_key_length` to get
+ * the expected length.
  *
- * For unblinded keys, this means setting the key mode, allocating a buffer for
- * the key material, and recording the length of the allocated buffer in
- * `key_length`. If the buffer size does not match expectations, this function
- * will return an error. RSA public key exponents should always have 32 bits (4
- * bytes) allocated for them.
- *
- * For blinded key structs, the caller should fully populate the key
- * configuration and allocate space for the keyblob. As for unblinded keys, the
- * caller should record the allocated buffer length and this function will
- * return an error if the keyblob length does not match expectations. The
- * keyblob should be twice the length of the key.
+ * The caller should fully populate the blinded key configuration and allocate
+ * space for the keyblob, setting `config.key_length` and `keyblob_length`
+ * accordingly. Use `otcrypto_rsa_private_key_length` to get the expected
+ * lengths.
  *
  * The value in the `checksum` field of key structs is not checked here and
  * will be populated by the key generation function.
  *
- * @param required_key_len Requested key length.
- * @param[out] rsa_public_key Pointer to RSA public exponent struct.
- * @param[out] rsa_private_key Pointer to RSA private exponent struct.
+ * @param size RSA size parameter.
+ * @param[out] public_key Pointer to public key struct.
+ * @param[out] private_key Pointer to blinded private key struct.
  * @return Result of the RSA key generation.
  */
-crypto_status_t otcrypto_rsa_keygen(rsa_key_size_t required_key_len,
-                                    rsa_public_key_t *rsa_public_key,
-                                    rsa_private_key_t *rsa_private_key);
+crypto_status_t otcrypto_rsa_keygen(rsa_size_t size,
+                                    crypto_unblinded_key_t *public_key,
+                                    crypto_blinded_key_t *private_key);
+
+/**
+ * Constructs an RSA public key from the modulus and public exponent.
+ *
+ * The caller should allocate space for the public key and set the `key` and
+ * `key_length` fields accordingly. Use `otcrypto_rsa_public_key_length` to get
+ * the expected length.
+ *
+ * @param size RSA size parameter.
+ * @param modulus RSA modulus (n).
+ * @param exponent RSA public exponent (e).
+ * @param[out] public_key Destination public key struct.
+ * @return Result of the RSA key construction.
+ */
+crypto_status_t otcrypto_rsa_public_key_construct(
+    rsa_size_t size, crypto_const_word32_buf_t modulus, uint32_t exponent,
+    crypto_unblinded_key_t *public_key);
+
+/**
+ * Constructs an RSA private key from the modulus and public/private exponents.
+ *
+ * The caller should allocate space for the private key and set the `keyblob`,
+ * `keyblob_length`, and `key_length` fields accordingly. Use
+ * `otcrypto_rsa_private_key_length` to get the expected length.
+ *
+ * @param size RSA size parameter.
+ * @param modulus RSA modulus (n).
+ * @param exponent RSA public exponent (e).
+ * @param d_share0 First share of the RSA private exponent d.
+ * @param d_share1 Second share of the RSA private exponent d.
+ * @param[out] public_key Destination public key struct.
+ * @return Result of the RSA key construction.
+ */
+crypto_status_t otcrypto_rsa_private_key_from_exponents(
+    rsa_size_t size, crypto_const_word32_buf_t modulus, uint32_t e,
+    crypto_const_word32_buf_t d_share0, crypto_const_word32_buf_t d_share1,
+    crypto_blinded_key_t *private_key);
 
 /**
  * Computes the digital signature on the input message data.
  *
- * The caller should allocate space for the `signature` buffer,
- * (expected length same as modulus length from `rsa_private_key`),
+ * The caller should allocate space for the `signature` buffer
  * and set the length of expected output in the `len` field of
  * `signature`. If the user-set length and the output length does not
  * match, an error message will be returned.
  *
- * @param rsa_private_key Pointer to RSA private exponent struct.
+ * @param private_key Pointer to blinded private key struct.
  * @param message_digest Message digest to be signed (pre-hashed).
  * @param padding_mode Padding scheme to be used for the data.
  * @param[out] signature Pointer to the generated signature struct.
  * @return The result of the RSA signature generation.
  */
-crypto_status_t otcrypto_rsa_sign(const rsa_private_key_t *rsa_private_key,
+crypto_status_t otcrypto_rsa_sign(const crypto_blinded_key_t *private_key,
                                   const hash_digest_t *message_digest,
                                   rsa_padding_t padding_mode,
                                   crypto_word32_buf_t *signature);
@@ -120,18 +163,18 @@ crypto_status_t otcrypto_rsa_sign(const rsa_private_key_t *rsa_private_key,
 /**
  * Verifies the authenticity of the input signature.
  *
- * The generated signature is compared against the input signature and
- * PASS / FAIL is returned.
+ * An "OK" status code does not mean that the signature passed verification;
+ * the caller must check both the returned status and `verification_result`
+ * before trusting the signature.
  *
- * @param rsa_public_key Pointer to RSA public exponent struct.
+ * @param public_key Pointer to public key struct.
  * @param message_digest Message digest to be verified (pre-hashed).
  * @param padding_mode Padding scheme to be used for the data.
  * @param signature Pointer to the input signature to be verified.
- * @param[out] verification_result Result of signature verification
- * (Pass/Fail).
+ * @param[out] verification_result Result of signature verification.
  * @return Result of the RSA verify operation.
  */
-crypto_status_t otcrypto_rsa_verify(const rsa_public_key_t *rsa_public_key,
+crypto_status_t otcrypto_rsa_verify(const crypto_unblinded_key_t *public_key,
                                     const hash_digest_t *message_digest,
                                     rsa_padding_t padding_mode,
                                     crypto_const_word32_buf_t signature,
@@ -147,26 +190,23 @@ crypto_status_t otcrypto_rsa_verify(const rsa_public_key_t *rsa_public_key,
  * started, or`kCryptoStatusInternalError` if the operation cannot be
  * started.
  *
- * @param required_key_len Requested key length.
+ * @param size RSA size parameter.
  * @return Result of async RSA keygen start operation.
  */
-crypto_status_t otcrypto_rsa_keygen_async_start(
-    rsa_key_size_t required_key_len);
+crypto_status_t otcrypto_rsa_keygen_async_start(rsa_size_t size);
 
 /**
  * Finalizes the asynchronous RSA key generation function.
  *
- * Returns `kCryptoStatusOK` and copies the RSA private key (d), RSA
- * public key exponent (e) and modulus (n) if the OTBN status is done,
- * or `kCryptoStatusAsyncIncomplete` if the OTBN is busy or
- * `kCryptoStatusInternalError` if there is an error.
+ * See `otcrypto_rsa_keygen` for details on the requirements for `public_key`
+ * and `private_key`.
  *
- * @param[out] rsa_public_key Pointer to RSA public exponent struct.
- * @param[out] rsa_private_key Pointer to RSA private exponent struct.
+ * @param[out] public_key Pointer to public key struct.
+ * @param[out] private_key Pointer to blinded private key struct.
  * @return Result of asynchronous RSA keygen finalize operation.
  */
 crypto_status_t otcrypto_rsa_keygen_async_finalize(
-    rsa_public_key_t *rsa_public_key, rsa_private_key_t *rsa_private_key);
+    crypto_unblinded_key_t *public_key, crypto_blinded_key_t *private_key);
 
 /**
  * Starts the asynchronous digital signature generation function.
@@ -178,27 +218,19 @@ crypto_status_t otcrypto_rsa_keygen_async_finalize(
  * started, or`kCryptoStatusInternalError` if the operation cannot be
  * started.
  *
- * @param rsa_private_key Pointer to RSA private exponent struct.
+ * @param private_key Pointer to blinded private key struct.
  * @param message_digest Message digest to be signed (pre-hashed).
  * @param padding_mode Padding scheme to be used for the data.
  * @return Result of async RSA sign start operation.
  */
 crypto_status_t otcrypto_rsa_sign_async_start(
-    const rsa_private_key_t *rsa_private_key,
+    const crypto_blinded_key_t *private_key,
     const hash_digest_t *message_digest, rsa_padding_t padding_mode);
 
 /**
  * Finalizes the asynchronous digital signature generation function.
  *
- * Returns `kCryptoStatusOK` and copies the signature if the OTBN
- * status is done, or `kCryptoStatusAsyncIncomplete` if the OTBN is
- * busy or `kCryptoStatusInternalError` if there is an error.
- *
- * The caller should allocate space for the `signature` buffer,
- * (expected length same as modulus length from `rsa_private_key`),
- * and set the length of expected output in the `len` field of
- * `signature`. If the user-set length and the output length does not
- * match, an error message will be returned.
+ * See `otcrypto_rsa_sign` for details on the requirements for `signature`.
  *
  * @param[out] signature Pointer to generated signature struct.
  * @return Result of async RSA sign finalize operation.
@@ -212,27 +244,24 @@ crypto_status_t otcrypto_rsa_sign_async_finalize(
  * Initializes OTBN and starts the OTBN routine to recover the message
  * from the input signature.
  *
- * @param rsa_public_key Pointer to RSA public exponent struct.
+ * @param public_key Pointer to public key struct.
  * @param signature Pointer to the input signature to be verified.
  * @return Result of async RSA verify start operation.
  */
 crypto_status_t otcrypto_rsa_verify_async_start(
-    const rsa_public_key_t *rsa_public_key,
+    const crypto_unblinded_key_t *public_key,
     crypto_const_word32_buf_t signature);
 
 /**
  * Finalizes the asynchronous signature verification function.
  *
- * Returns `kCryptoStatusOK` and populates the `verification result`
- * if the OTBN status is done, or `kCryptoStatusAsyncIncomplete` if
- * OTBN is busy or `kCryptoStatusInternalError` if there is an error.
- * The (hash of) recovered message is compared against the input
- * message and a PASS or FAIL is returned.
+ * An "OK" status code does not mean that the signature passed verification;
+ * the caller must check both the returned status and `verification_result`
+ * before trusting the signature.
  *
  * @param message_digest Message digest to be verified (pre-hashed).
  * @param padding_mode Padding scheme to be used for the data.
- * @param[out] verification_result Result of signature verification
- * (Pass/Fail).
+ * @param[out] verification_result Result of signature verification.
  * @return Result of async RSA verify finalize operation.
  */
 crypto_status_t otcrypto_rsa_verify_async_finalize(
