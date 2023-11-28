8ea6ca61e841026cba75e4868f815f0c6f279013
Pascal Nasahl <nasahlpa@lowrisc.org>
2023-11-28 17:32:17 +0000

    [fi] Add Ibex FI command handlers
    This PR adds first FI command handlers for Ibex penetration tests to the
    codebase.
    
    The host code is located in the ot-sca repository
    ([PR#224](https://github.com/lowRISC/ot-sca/pull/224)).
    
    @milesdai please let me know whether I can keep these fils here or
    where to move them.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

---
+++ sw/device/tests/crypto/cryptotest/firmware/ibex_fi.h
@@ -0,0 +1,15 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_TESTS_CRYPTO_CRYPTOTEST_FIRMWARE_IBEX_FI_H_
+#define OPENTITAN_SW_DEVICE_TESTS_CRYPTO_CRYPTOTEST_FIRMWARE_IBEX_FI_H_
+
+#include "sw/device/lib/base/status.h"
+#include "sw/device/lib/ujson/ujson.h"
+
+status_t handle_ibex_fi_char_unrolled_reg_op_loop(ujson_t *uj);
+status_t handle_ibex_fi_init_trigger(ujson_t *uj);
+status_t handle_ibex_fi(ujson_t *uj);
+
+#endif  // OPENTITAN_SW_DEVICE_TESTS_CRYPTO_CRYPTOTEST_FIRMWARE_IBEX_FI_H_
---
+++ sw/device/tests/crypto/cryptotest/json/ibex_fi_commands.h
@@ -0,0 +1,29 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_TESTS_CRYPTO_CRYPTOTEST_JSON_IBEX_FI_COMMANDS_H_
+#define OPENTITAN_SW_DEVICE_TESTS_CRYPTO_CRYPTOTEST_JSON_IBEX_FI_COMMANDS_H_
+#include "sw/device/lib/ujson/ujson_derive.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// clang-format off
+
+#define IBEXFI_SUBCOMMAND(_, value) \
+    value(_, InitTrigger) \
+    value(_, CharUnrolledRegOpLoop)
+UJSON_SERDE_ENUM(IbexFiSubcommand, ibex_fi_subcommand_t, IBEXFI_SUBCOMMAND);
+
+#define IBEXFI_LOOP_COUNTER_OUTPUT(field, string) \
+    field(loop_counter, uint32_t) \
+    field(err_status, uint32_t)
+UJSON_SERDE_STRUCT(IbexFiLoopCounterOutput, ibex_fi_loop_counter_t, IBEXFI_LOOP_COUNTER_OUTPUT);
+
+// clang-format on
+
+#ifdef __cplusplus
+}
+#endif
+#endif  // OPENTITAN_SW_DEVICE_TESTS_CRYPTO_CRYPTOTEST_JSON_IBEX_FI_COMMANDS_H_
