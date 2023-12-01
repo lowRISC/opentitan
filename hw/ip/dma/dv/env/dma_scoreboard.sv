c4264b85b35ce22ba6463a7ef19b727e3ab36f73
Andreas Kurth <adk@lowrisc.org>
2023-12-01 13:16:48 +0000

    [dma/dv] Make name of range REGWEN in DV consistent with CSR
    Analogous motivation as for previous commit.
    
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- hw/ip/dma/dv/env/dma_scoreboard.sv
+++ hw/ip/dma/dv/env/dma_scoreboard.sv
@@ -846,21 +846,21 @@ class dma_scoreboard extends cip_base_scoreboard #(
                                   dma_config.dst_asid.name()), UVM_HIGH)
       end
       "enabled_memory_range_base": begin
-        if (dma_config.mem_range_lock == MuBi4True) begin
+        if (dma_config.range_regwen == MuBi4True) begin
           dma_config.mem_range_base = item.a_data;
           `uvm_info(`gfn, $sformatf("Got enabled_memory_range_base = %0x",
                                     dma_config.mem_range_base), UVM_HIGH)
         end
       end
       "enabled_memory_range_limit": begin
-        if (dma_config.mem_range_lock == MuBi4True) begin
+        if (dma_config.range_regwen == MuBi4True) begin
           dma_config.mem_range_limit = item.a_data;
           `uvm_info(`gfn, $sformatf("Got enabled_memory_range_limit = %0x",
                                     dma_config.mem_range_limit), UVM_HIGH)
         end
       end
       "range_valid": begin
-        if (dma_config.mem_range_lock == MuBi4True) begin
+        if (dma_config.range_regwen == MuBi4True) begin
           dma_config.mem_range_valid = `gmv(ral.range_valid.range_valid);
           `uvm_info(`gfn, $sformatf("Got mem_range_valid = %x",
                                     dma_config.mem_range_valid), UVM_HIGH)
@@ -868,9 +868,9 @@ class dma_scoreboard extends cip_base_scoreboard #(
       end
       "range_regwen": begin
         // Get mirrored field value and cast to associated enum in dma_config
-        dma_config.mem_range_lock = mubi4_t'(`gmv(ral.range_regwen.regwen));
+        dma_config.range_regwen = mubi4_t'(`gmv(ral.range_regwen.regwen));
         `uvm_info(`gfn, $sformatf("Got range register lock = %s",
-                                  dma_config.mem_range_lock.name()), UVM_HIGH)
+                                  dma_config.range_regwen.name()), UVM_HIGH)
       end
       "total_data_size": begin
         dma_config.total_data_size = item.a_data;
