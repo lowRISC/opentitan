548c9677c0180867f901b4275d4faa5483bdaa8c
Miguel Osorio <miguelosorio@google.com>
2023-12-01 08:56:21 -0800

    [dif] Remove flash dependencies from keymgr_dpe_testutils
    keymgr_dpe does not require flash initialization.
    
    Other changes:
    
    1. Update dif:keymgr_dpe and keymgr_dpe_testutils targets to be able to
       build them with Bazel.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- sw/ip/keymgr_dpe/test/utils/keymgr_dpe_testutils.c
+++ sw/ip/keymgr_dpe/test/utils/keymgr_dpe_testutils.c
@@ -6,9 +6,6 @@
 
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/silicon_creator/lib/drivers/retention_sram.h"
-#include "sw/ip/flash_ctrl/dif/dif_flash_ctrl.h"
-#include "sw/ip/flash_ctrl/driver/flash_ctrl.h"
-#include "sw/ip/flash_ctrl/test/utils/flash_ctrl_testutils.h"
 #include "sw/ip/keymgr_dpe/dif/dif_keymgr_dpe.h"
 #include "sw/ip/keymgr_dpe/driver/keymgr_dpe.h"
 #include "sw/ip/otp_ctrl/dif/dif_otp_ctrl.h"
@@ -21,58 +18,7 @@
 #include "sw/lib/sw/device/runtime/log.h"
 #include "sw/lib/sw/device/silicon_creator/base/chip.h"
 
-enum {
-  /** Flash Secret partition ID. */
-  kFlashInfoPartitionId = 0,
-
-  /** Secret partition flash bank ID. */
-  kFlashInfoBankId = 0,
-
-  /** Creator Secret flash info page ID. */
-  kFlashInfoPageIdCreatorSecret = 1,
-
-  /** Owner Secret flash info page ID. */
-  kFlashInfoPageIdOwnerSecret = 2,
-};
-
-static status_t write_info_page(dif_flash_ctrl_state_t *flash, uint32_t page_id,
-                                const keymgr_dpe_testutils_secret_t *data,
-                                bool scramble) {
-  uint32_t address = 0;
-  if (scramble) {
-    TRY(flash_ctrl_testutils_info_region_scrambled_setup(
-        flash, page_id, kFlashInfoBankId, kFlashInfoPartitionId, &address));
-  } else {
-    TRY(flash_ctrl_testutils_info_region_setup(
-        flash, page_id, kFlashInfoBankId, kFlashInfoPartitionId, &address));
-  }
-
-  TRY(flash_ctrl_testutils_erase_and_write_page(
-      flash, address, kFlashInfoPartitionId, data->value,
-      kDifFlashCtrlPartitionTypeInfo, ARRAYSIZE(data->value)));
-
-  keymgr_dpe_testutils_secret_t readback_data;
-  TRY(flash_ctrl_testutils_read(
-      flash, address, kFlashInfoPartitionId, readback_data.value,
-      kDifFlashCtrlPartitionTypeInfo, ARRAYSIZE(readback_data.value), 0));
-  TRY_CHECK(memcmp(data->value, readback_data.value, sizeof(data->value)) == 0);
-  return OK_STATUS();
-}
-
-status_t keymgr_dpe_testutils_flash_init(
-    dif_flash_ctrl_state_t *flash,
-    const keymgr_dpe_testutils_secret_t *creator_secret,
-    const keymgr_dpe_testutils_secret_t *owner_secret) {
-  // Initialize flash secrets.
-  write_info_page(flash, kFlashInfoPageIdCreatorSecret, creator_secret,
-                  /*scramble=*/true);
-  write_info_page(flash, kFlashInfoPageIdOwnerSecret, owner_secret,
-                  /*scramble=*/true);
-  return OK_STATUS();
-}
-
 status_t keymgr_dpe_testutils_startup(dif_keymgr_dpe_t *keymgr_dpe) {
-  dif_flash_ctrl_state_t flash;
   dif_rstmgr_t rstmgr;
   dif_rstmgr_reset_info_bitfield_t info;
 
@@ -81,15 +27,7 @@ status_t keymgr_dpe_testutils_startup(dif_keymgr_dpe_t *keymgr_dpe) {
 
   // POR reset.
   if (info == kDifRstmgrResetInfoPor) {
-    LOG_INFO("Powered up for the first time, program flash");
-
-    TRY(dif_flash_ctrl_init_state(
-        &flash, mmio_region_from_addr(kFlashCtrlCoreBaseAddr[0])));
-
-    TRY(keymgr_dpe_testutils_flash_init(&flash, &kCreatorSecret,
-                                        &kOwnerSecret));
-
-    // Lock otp secret partition.
+    LOG_INFO("Powered up for the first time, lock SECRET2 partition");
     dif_otp_ctrl_t otp;
     TRY(dif_otp_ctrl_init(mmio_region_from_addr(kOtpCtrlCoreBaseAddr[0]),
                           &otp));
