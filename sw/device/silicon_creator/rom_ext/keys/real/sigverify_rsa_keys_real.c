0c073c13c804f07c4930733edcd197a61bcd3540
Chris Frantz <cfrantz@google.com>
2023-12-04 23:17:13 -0800

    [sival] Add SiVAL ROM_EXT keys
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/device/silicon_creator/rom_ext/keys/real/sigverify_rsa_keys_real.c
+++ sw/device/silicon_creator/rom_ext/keys/real/sigverify_rsa_keys_real.c
@@ -2,4 +2,35 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#error "Not implemented."
+#include "sw/device/lib/base/macros.h"
+#include "sw/device/silicon_creator/rom_ext/keys/real/earlgrey_z0_sival_1.h"
+#include "sw/device/silicon_creator/rom_ext/sigverify_keys.h"
+
+/**
+ * Number of RSA public keys.
+ */
+enum {
+  kSigverifyRsaKeysCnt_ = 1,
+};
+const size_t kSigverifyRsaKeysCnt = kSigverifyRsaKeysCnt_;
+
+/**
+ * Step size to use when checking RSA public keys.
+ *
+ * This must be coprime with and less than `kSigverifyNumRsaKeys`.
+ * Note: Step size is not applicable when `kSigverifyNumRsaKeys` is 1.
+ */
+const size_t kSigverifyRsaKeysStep = 1;
+
+/**
+ * Fake public keys for signature verification in tests.
+ *
+ * Please see sw/device/silicon_creator/rom/keys/README.md for more
+ * details.
+ */
+const sigverify_rom_ext_key_t kSigverifyRsaKeys[kSigverifyRsaKeysCnt_] = {
+    {
+        .key = EARLGREY_Z0_SIVAL_1,
+        .key_type = kSigverifyKeyTypeFirmwareProd,
+    },
+};
