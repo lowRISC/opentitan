7b11241a0d3311115aefacf845a84a6bf3b1f6ca
Amaury Pouly <amaury.pouly@lowrisc.org>
2023-12-07 16:52:01 +0000

    [ot_certs] add command to generate a certificate
    The certificate must specify all values, or some substitution data
    must be provided.

--- sw/host/opentitantool/src/command/certificate.rs
+++ sw/host/opentitantool/src/command/certificate.rs
@@ -17,10 +17,22 @@ use ot_certs::{codegen, x509};
 
 fn load_template(path: &PathBuf) -> Result<Template> {
     // Load template.
-    let template_content = fs::read_to_string(path)
-        .with_context(|| format!("Could not load the template file {}", path.display()))?;
-    Template::from_hjson_str(&template_content)
-        .with_context(|| format!("Failed to parse template file {}", path.display()))
+    let template_content = fs::read_to_string(path).with_context(|| {
+        format!(
+            "Could not load the template file {}",
+            path.display()
+        )
+    })?;
+    Template::from_hjson_str(&template_content).with_context(|| {
+        format!("Failed to parse template file {}", path.display())
+    })
+}
+
+fn load_subst(path: &PathBuf) -> Result<SubstData> {
+    let data_content = fs::read_to_string(path)
+        .with_context(|| format!("Could not load the data file {}", path.display()))?;
+    SubstData::from_json(&data_content)
+        .with_context(|| format!("Failed to parse data file {}", path.display()))
 }
 
 /// Commands for interacting with certificates.
@@ -122,6 +134,9 @@ pub struct GenCertCommand {
     /// Certificate format
     #[arg(long, value_enum, default_value_t = CertFormat::X509)]
     cert_format: CertFormat,
+    /// Optional substitution data.
+    #[arg(long)]
+    subst: Option<PathBuf>,
     /// Output file.
     output: PathBuf,
 }
@@ -134,19 +149,26 @@ impl CommandDispatch for GenCertCommand {
     ) -> Result<Option<Box<dyn Annotate>>> {
         // Load template.
         let template = load_template(&self.template)?;
+        // Load data.
+        let data = self.subst.as_ref().map(load_subst).transpose()?;
+        // Warn user if there is no substitution data and variables.
+        if !template.variables.is_empty() && data.is_none() {
+            bail!("the template contains variable so you must specify some substition data using --subst")
+        }
+        // Substitute
+        let template = if let Some(data) = data {
+            template.subst(&data)?
+        }
+        else {
+            template
+        };
         // Check there are no remaining variables.
         if !template.variables.is_empty() {
-            let rem_vars = template
-                .variables
-                .keys()
-                .cloned()
-                .collect::<Vec<_>>()
-                .join(", ");
+            let rem_vars = template.variables.keys().cloned().collect::<Vec<_>>().join(", ");
             bail!("the substition data does not cover the following variables: {rem_vars}")
         }
         // Generate
-        let der =
-            x509::generate_certificate(&template).context("could not generate X509 certificate")?;
+        let der = x509::generate_certificate(&template).context("could not generate X509 certificate")?;
         // Output
         let mut file = File::create(&self.output)?;
         file.write_all(&der)?;
@@ -182,7 +204,7 @@ pub struct SubstCommand {
     /// Filename of the input hjson template.
     template: PathBuf,
     /// Filename to the substitution data.
-    data: PathBuf,
+    subst: PathBuf,
     /// Filename of the output hjson template.
     #[arg(long)]
     output: Option<PathBuf>,
@@ -195,20 +217,9 @@ impl CommandDispatch for SubstCommand {
         _transport: &TransportWrapper,
     ) -> Result<Option<Box<dyn Annotate>>> {
         // Load template.
-        let template_content = fs::read_to_string(&self.template).with_context(|| {
-            format!(
-                "Could not load the template file {}",
-                self.template.display()
-            )
-        })?;
-        let template = Template::from_hjson_str(&template_content).with_context(|| {
-            format!("Failed to parse template file {}", self.template.display())
-        })?;
+        let template = load_template(&self.template)?;
         // Load data.
-        let data_content = fs::read_to_string(&self.data)
-            .with_context(|| format!("Could not load the data file {}", self.template.display()))?;
-        let data = SubstData::from_json(&data_content)
-            .with_context(|| format!("Failed to parse data file {}", self.data.display()))?;
+        let data = load_subst(&self.subst)?;
         // Substitute
         let template = template.subst(&data)?;
         // Output
