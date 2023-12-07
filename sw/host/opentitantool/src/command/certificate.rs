87ab8885c84af9f79a2925a8542c86f6bee944dc
Amaury Pouly <amaury.pouly@lowrisc.org>
2023-12-07 23:28:36 +0000

    [ottols] Add a command to generate a certificate from a template
    This command only works for a template that specifies all variables,
    like the example.hjson template.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/host/opentitantool/src/command/certificate.rs
+++ sw/host/opentitantool/src/command/certificate.rs
@@ -2,20 +2,32 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use clap::{Args, Subcommand, ValueEnum};
 use serde_annotate::Annotate;
 use std::any::Any;
-use std::fs;
+use std::fs::{self, File};
+use std::io::Write;
 use std::path::PathBuf;
 
 use opentitanlib::app::command::CommandDispatch;
 use opentitanlib::app::TransportWrapper;
-use ot_certs::{template, x509};
+use ot_certs::template::Template;
+use ot_certs::x509;
+
+fn load_template(path: &PathBuf) -> Result<Template> {
+    // Load template.
+    let template_content = fs::read_to_string(path)
+        .with_context(|| format!("Could not load the template file {}", path.display()))?;
+    Template::from_hjson_str(&template_content)
+        .with_context(|| format!("Failed to parse template file {}", path.display()))
+}
 
 /// Commands for interacting with certificates.
 #[derive(Debug, Subcommand, CommandDispatch)]
 pub enum CertificateCommand {
+    /// Generate a binary certificate
+    Generate(GenCertCommand),
     /// Generate a certificate template.
     GenTemplate(GenTplCommand),
     /// Parse a certificate.
@@ -46,17 +58,49 @@ impl CommandDispatch for GenTplCommand {
         _context: &dyn Any,
         _transport: &TransportWrapper,
     ) -> Result<Option<Box<dyn Annotate>>> {
-        let template_content = fs::read_to_string(&self.template).with_context(|| {
-            format!(
-                "Could not load the template file {}",
-                self.template.display()
-            )
-        })?;
-        let template =
-            template::Template::from_hjson_str(&template_content).with_context(|| {
-                format!("Failed to parse template file {}", self.template.display())
-            })?;
+        let template = load_template(&self.template)?;
         Ok(Some(Box::new(template)))
+    }
+}
+
+/// Generate a certificate.
+#[derive(Debug, Args)]
+pub struct GenCertCommand {
+    /// Filename of the template.
+    template: PathBuf,
+    /// Certificate format
+    #[arg(long, value_enum, default_value_t = CertFormat::X509)]
+    cert_format: CertFormat,
+    /// Output file.
+    output: PathBuf,
+}
+
+impl CommandDispatch for GenCertCommand {
+    fn run(
+        &self,
+        _context: &dyn Any,
+        _transport: &TransportWrapper,
+    ) -> Result<Option<Box<dyn Annotate>>> {
+        // Load template.
+        let template = load_template(&self.template)?;
+        // Check there are no remaining variables.
+        if !template.variables.is_empty() {
+            let rem_vars = template
+                .variables
+                .keys()
+                .cloned()
+                .collect::<Vec<_>>()
+                .join(", ");
+            bail!("the substition data does not cover the following variables: {rem_vars}")
+        }
+        // Generate
+        let der =
+            x509::generate_certificate(&template).context("could not generate X509 certificate")?;
+        // Output
+        let mut file = File::create(&self.output)?;
+        file.write_all(&der)?;
+
+        Ok(None)
     }
 }
 
