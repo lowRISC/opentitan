e37f208745eaed47e04effa4197a970f77558b17
Amaury Pouly <amaury.pouly@lowrisc.org>
2023-12-08 14:12:48 +0000

    [ot_certs] Add DER implementation of the ASN1 builder trait
    This implementation can produce the DER representation of an ASN1
    document. It only supports literal values and will error out if
    a variable is provided. Unittest are provided to make sure that
    the output is correct. Most of the tests comes from, or are
    variations of, examples in the X.690 specification.
    
    This implementation serves two purposes: illustrate how to
    implement the builder trait and provide a rust refernce to compare
    against. This will be useful when testing the upcoming codegen
    implementation.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/host/ot_certs/src/asn1/mod.rs
+++ sw/host/ot_certs/src/asn1/mod.rs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+use anyhow::{Context, Result};
+
 pub mod builder;
 pub mod der;
 
@@ -39,5 +41,64 @@ impl Oid {
         match self {
             Oid::Custom(oid) => oid,
         }
+    }
+
+    /// Return the DER encoding of the OID as a list of bytes.
+    pub fn to_der(&self) -> Result<Vec<u8>> {
+        let mut components = self
+            .oid()
+            .split('.')
+            .map(|s| {
+                s.parse::<u32>()
+                    .with_context(|| format!("invalid OID component '{s}'"))
+            })
+            .collect::<Result<Vec<_>>>()?;
+        // From X.690 spec, section 8.19:
+        // The number of subidentifiers (N) shall be one less than the number of object identifier components
+        // in the object identifier value being encoded. The numerical value of the first subidentifier is derived
+        // from the values of the first two object identifier components in the object identifier value being encoded,
+        // using the formula: (X*40) + Y where X is the value of the first object identifier component and Y is the value
+        // of the second object identifier component.
+        components.reverse();
+        let first = components
+            .pop()
+            .context("cannot call push_oid with an empty OID")?;
+        let second = components
+            .pop()
+            .context("cannot call push_oid with a single-component OID")?;
+        components.push(40 * first + second);
+        components.reverse();
+        let mut bytes = Vec::<u8>::new();
+        for comp in components {
+            // The contents octets shall be an (ordered) list of encodings of subidentifiers (see 8.19.3 and 8.19.4) concatenated
+            // together. Each subidentifier is represented as a series of (one or more) octets. Bit 8 of each octet indicates whether it is the last in the
+            // series: bit 8 of the last octet is zero; bit 8 of each preceding octet is one. Bits 7 to 1 of the octets in the series collectively
+            // encode the subidentifier. Conceptually, these groups of bits are concatenated to form an unsigned binary number whose most
+            // significant bit is bit 7 of the first octet and whose least significant bit is bit 1 of the last octet. The subidentifier shall be
+            // encoded in the fewest possible octets, that is, the leading octet of the subidentifier shall not have the value 8016
+
+            // Compute the length that we need: each byte stores 7 bits so this is the
+            // the log in base 128 of the number.
+            let mut copy_comp = comp;
+            let mut length = 0;
+            while copy_comp > 0 {
+                length += 1;
+                copy_comp >>= 7;
+            }
+            if length == 0 {
+                length = 1;
+            }
+            // Create the bytes
+            for i in (0..length).rev() {
+                // Extract 7-bit chunk of the number.
+                let mut byte = ((comp >> (7 * i)) & 0x7f) as u8;
+                // Add continuation marker.
+                if i != 0 {
+                    byte |= 0x80;
+                }
+                bytes.push(byte);
+            }
+        }
+        Ok(bytes)
     }
 }
