354607830875ef254c34b42245d5e2e7ac6569f1
Emanuele Parisi emanuele.parisi@unibo.it <emanuele.parisi@unibo.it>
2023-12-11 16:31:53 +0100

    Add AES-CBC benchmarks

--- sw/tests/opentitan/titanssl_sram_hmac_dif/titanssl_sram_hmac_dif.c
+++ sw/tests/opentitan/titanssl_sram_hmac_dif/titanssl_sram_hmac_dif.c
@@ -2,16 +2,19 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+
 #include "sw/device/lib/arch/device.h"
 #include "sw/device/lib/base/mmio.h"
 #include "sw/device/lib/base/bitfield.h"
 #include "sw/device/lib/base/memory.h"
 #include "sw/device/lib/dif/dif_hmac.h"
+#include "sw/device/lib/dif/dif_aes.h"  
 #include "sw/device/silicon_creator/rom/uart.h"
 #include "sw/device/silicon_creator/rom/string_lib.h"
 
 #include "hmac_regs.h"  // Generated.
-#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+#include "aes_regs.h"  // Generated.
 
 
 #define TARGET_SYNTHESIS
@@ -27,6 +30,7 @@ typedef struct
 static titanssl_buffer_t titanssl_data_src;
 static titanssl_buffer_t titanssl_data_dst;
 static titanssl_buffer_t titanssl_data_key;
+static titanssl_buffer_t titanssl_data_iv;
 
 
 /* ============================================================================
@@ -45,8 +49,9 @@ static titanssl_buffer_t titanssl_data_key;
 #define TITANSSL_BENCHMARK_PAYLOAD_65536 0
 
 // Configure cryptographic operation.
-#define TITANSSL_BENCHMARK_SHA256 1
+#define TITANSSL_BENCHMARK_SHA256 0
 #define TITANSSL_BENCHMARK_HMAC   0
+#define TITANSSL_BENCHMARK_AES    1
 
 /* ============================================================================
  * Benchmark automatic configuration
@@ -56,10 +61,12 @@ static titanssl_buffer_t titanssl_data_key;
 #define TITANSSL_DATA_SRC 0x80720000
 #define TITANSSL_DATA_DST 0x80740000
 #define TITANSSL_DATA_KEY 0xe0006000
+#define TITANSSL_DATA_IV  0xe0006100
 #elif TITANSSL_BENCHMARK_L1
 #define TITANSSL_DATA_SRC 0xe0002000
 #define TITANSSL_DATA_DST 0xe0004000
 #define TITANSSL_DATA_KEY 0xe0006000
+#define TITANSSL_DATA_IV  0xe0006100
 #else
 #error "Wrong benchmark memory configuration"
 #endif
@@ -73,13 +80,26 @@ static titanssl_buffer_t titanssl_data_key;
 #endif
 
 #if TITANSSL_BENCHMARK_SHA256
+
 #define TITANSSL_OUTPUT_SIZE 32
 #define TITANSSL_KEY_SIZE 0
+#define TITANSSL_IV_SIZE 0
 #define titanssl_benchmark titanssl_benchmark_sha256
+
 #elif TITANSSL_BENCHMARK_HMAC
+
 #define TITANSSL_OUTPUT_SIZE 32
 #define TITANSSL_KEY_SIZE 32
+#define TITANSSL_IV_SIZE 0
 #define titanssl_benchmark titanssl_benchmark_hmac
+
+#elif TITANSSL_BENCHMARK_AES
+
+#define TITANSSL_OUTPUT_SIZE ((TITANSSL_PAYLOAD_SIZE+0xF) & ~0xF)
+#define TITANSSL_KEY_SIZE 32
+#define TITANSSL_IV_SIZE 16
+#define titanssl_benchmark titanssl_benchmark_aes
+
 #else
 #error "Wrong benchmark operation configuration"
 #endif
@@ -88,13 +108,29 @@ static titanssl_buffer_t titanssl_data_key;
  * Benchmark implementation
  * ========================================================================= */
 
+void initialize_edn()
+{
+    uint32_t *p;
+
+    p = (uint32_t*)0xc1160024;
+    *p = 0x00909099;
+    p = (uint32_t*)0xc1160020;
+    *p = 0x00000006;
+    p = (uint32_t*)0xc1150014;
+    *p = 0x00000666;
+    p = (uint32_t*)0xc1170014;
+    *p = 0x00009966;
+}
+
 void initialize_memory(
         uint8_t *src_data,
         size_t src_n,
         uint8_t *dst_data,
         size_t dst_n,
         uint8_t *key_data,
-        size_t key_n)
+        size_t key_n,
+        uint8_t *iv_data,
+        size_t iv_n)
 {
     titanssl_data_src.data = src_data;
     titanssl_data_src.n = src_n;
@@ -116,12 +152,20 @@ void initialize_memory(
     {
         titanssl_data_key.data[i] = 0x0;
     }
+
+    titanssl_data_iv.data = iv_data;
+    titanssl_data_iv.n = iv_n;
+    for (size_t i=0; i<iv_n; i++)
+    {
+        titanssl_data_iv.data[i] = 0x0;
+    }
 }
 
 void titanssl_benchmark_sha256(
         titanssl_buffer_t *const src,
         titanssl_buffer_t *const dst,
-        titanssl_buffer_t *const key)
+        titanssl_buffer_t *const key,
+        titanssl_buffer_t *const iv)
 {
 	dif_hmac_t hmac;
     dif_hmac_transaction_t cfg;
@@ -132,10 +176,8 @@ void titanssl_benchmark_sha256(
 		mmio_region_from_addr(TOP_EARLGREY_HMAC_BASE_ADDR),
 		&hmac
 	);
-    cfg = {
-        .digest_endianness = kDifHmacEndiannessLittle,
-        .message_endianness = kDifHmacEndiannessLittle,
-    };
+    cfg.digest_endianness = kDifHmacEndiannessLittle;
+    cfg.message_endianness = kDifHmacEndiannessLittle;
    	res = dif_hmac_mode_sha256_start(
 		&hmac,
 		cfg
@@ -203,7 +245,8 @@ void titanssl_benchmark_sha256(
 void titanssl_benchmark_hmac(
         titanssl_buffer_t *const src,
         titanssl_buffer_t *const dst,
-        titanssl_buffer_t *const key)
+        titanssl_buffer_t *const key,
+        titanssl_buffer_t *const iv)
 {
 	dif_hmac_t hmac;
     dif_hmac_transaction_t cfg;
@@ -214,14 +257,12 @@ void titanssl_benchmark_hmac(
 		mmio_region_from_addr(TOP_EARLGREY_HMAC_BASE_ADDR),
 		&hmac
 	);
-    cfg = {
-        .digest_endianness = kDifHmacEndiannessLittle,
-        .message_endianness = kDifHmacEndiannessLittle,
-    };
-   	res = dif_hmac_mode_hmac(
+    cfg.digest_endianness = kDifHmacEndiannessLittle;
+    cfg.message_endianness = kDifHmacEndiannessLittle;
+   	res = dif_hmac_mode_hmac_start(
 		&hmac,
-        key.data,
-		kHmacTransactionConfig
+        key->data,
+		cfg
 	);
 
 	// Compute SHA256
@@ -283,6 +324,109 @@ void titanssl_benchmark_hmac(
 #endif
 }
 
+void titanssl_benchmark_aes(
+        titanssl_buffer_t *const src,
+        titanssl_buffer_t *const dst,
+        titanssl_buffer_t *const key,
+        titanssl_buffer_t *const iv)
+{
+    dif_aes_key_share_t round_key;
+    dif_aes_iv_t round_iv;
+    dif_aes_t aes;
+    dif_aes_transaction_t cfg;
+    dif_result_t res;
+
+	// Initialize AES IP in CBC mode
+    cfg.operation = kDifAesOperationEncrypt;
+    cfg.mode = kDifAesModeCbc;
+    cfg.key_len = kDifAesKey256;
+    cfg.key_provider = kDifAesKeySoftwareProvided;
+    cfg.mask_reseeding = kDifAesReseedPer64Block;
+    cfg.manual_operation = kDifAesManualOperationAuto;
+    cfg.reseed_on_key_change = false;
+    cfg.ctrl_aux_lock = false;
+    for (size_t i=0; i<sizeof(round_key.share0); i++)
+    {
+        round_key.share0[i] = key->data[i];
+        round_key.share1[i] = key->data[i];
+    }
+    for (size_t i=0; i<sizeof(round_iv.iv); i++)
+    {
+        round_iv.iv[i] = iv->data[i];
+    }
+    res = dif_aes_init(
+        mmio_region_from_addr(TOP_EARLGREY_AES_BASE_ADDR), 
+        &aes
+    );
+    res = dif_aes_reset(&aes);
+    res = dif_aes_start(
+        &aes, 
+        &cfg, 
+        &round_key, 
+        &round_iv
+    );
+
+    // Compute AES
+    const uint8_t *kDataSrc = src->data;
+    const uint8_t *kDataDst = dst->data;
+    size_t kDataSize = src->n;
+    uint8_t *dpSrc = src->data;
+    uint8_t *dpDst = dst->data;
+
+    mmio_region_write32(aes.base_addr, AES_DATA_IN_0_REG_OFFSET, ((uint32_t*)dpSrc)[0]);
+    mmio_region_write32(aes.base_addr, AES_DATA_IN_1_REG_OFFSET, ((uint32_t*)dpSrc)[1]);
+    mmio_region_write32(aes.base_addr, AES_DATA_IN_2_REG_OFFSET, ((uint32_t*)dpSrc)[2]);
+    mmio_region_write32(aes.base_addr, AES_DATA_IN_3_REG_OFFSET, ((uint32_t*)dpSrc)[3]);
+    while(!mmio_region_get_bit32(
+        aes.base_addr, 
+        AES_STATUS_REG_OFFSET,
+        AES_STATUS_INPUT_READY_BIT
+    ));
+    dpSrc += 16;
+    
+    while (dpSrc - kDataSrc < kDataSize) {
+        mmio_region_write32(aes.base_addr, AES_DATA_IN_0_REG_OFFSET, ((uint32_t*)dpSrc)[0]);
+        mmio_region_write32(aes.base_addr, AES_DATA_IN_1_REG_OFFSET, ((uint32_t*)dpSrc)[1]);
+        mmio_region_write32(aes.base_addr, AES_DATA_IN_2_REG_OFFSET, ((uint32_t*)dpSrc)[2]);
+        mmio_region_write32(aes.base_addr, AES_DATA_IN_3_REG_OFFSET, ((uint32_t*)dpSrc)[3]);
+
+        while(!mmio_region_get_bit32(
+            aes.base_addr, 
+            AES_STATUS_REG_OFFSET,
+            AES_STATUS_OUTPUT_VALID_BIT
+        ));
+        *(uint32_t*)(dpDst+0x0) = mmio_region_read32(aes.base_addr, AES_DATA_OUT_0_REG_OFFSET);
+        *(uint32_t*)(dpDst+0x4) = mmio_region_read32(aes.base_addr, AES_DATA_OUT_1_REG_OFFSET);
+        *(uint32_t*)(dpDst+0x8) = mmio_region_read32(aes.base_addr, AES_DATA_OUT_2_REG_OFFSET);
+        *(uint32_t*)(dpDst+0xc) = mmio_region_read32(aes.base_addr, AES_DATA_OUT_3_REG_OFFSET);
+        dpDst += 16;
+        dpSrc += 16;
+    }
+    while(!mmio_region_get_bit32(
+        aes.base_addr, 
+        AES_STATUS_REG_OFFSET,
+        AES_STATUS_OUTPUT_VALID_BIT
+    ));
+    *(uint32_t*)(dpDst+0x0) = mmio_region_read32(aes.base_addr, AES_DATA_OUT_0_REG_OFFSET);
+    *(uint32_t*)(dpDst+0x4) = mmio_region_read32(aes.base_addr, AES_DATA_OUT_1_REG_OFFSET);
+    *(uint32_t*)(dpDst+0x8) = mmio_region_read32(aes.base_addr, AES_DATA_OUT_2_REG_OFFSET);
+    *(uint32_t*)(dpDst+0xc) = mmio_region_read32(aes.base_addr, AES_DATA_OUT_3_REG_OFFSET);
+
+    res = dif_aes_end(&aes);
+
+    // Print the message digest, if we are in debug mode.
+#if TITANSSL_BENCHMARK_DEBUG
+	for (int i=0; i<titanssl_data_dst.n; i++)
+	{
+		printf(
+            "0x%08x\r\n",
+            ((uint32_t*)(dst->data))[i]
+        );
+        uart_wait_tx_done();
+	}
+#endif
+}
+
 int main(
         int argc, 
         char **argv)
@@ -299,18 +443,22 @@ int main(
         (test_freq/baud_rate)>>4
     );
 
+    initialize_edn();
     initialize_memory(
         (uint8_t*)TITANSSL_DATA_SRC,
         TITANSSL_PAYLOAD_SIZE,
         (uint8_t*)TITANSSL_DATA_DST,
         TITANSSL_OUTPUT_SIZE,
         (uint8_t*)TITANSSL_DATA_KEY,
-        TITANSSL_KEY_SIZE
+        TITANSSL_KEY_SIZE,
+        (uint8_t*)TITANSSL_DATA_IV,
+        TITANSSL_IV_SIZE
     );
     titanssl_benchmark(
         &titanssl_data_src,
         &titanssl_data_dst,
-        &titanssl_data_key
+        &titanssl_data_key,
+        &titanssl_data_iv
     );
 
 	return 0;
