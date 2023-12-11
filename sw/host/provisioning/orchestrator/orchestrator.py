83cc6ed00619576646ace884a9826b715b96bbb7
Miles Dai <milesdai@opentitan.org>
2023-12-11 09:00:04 +0000

    [manuf] Add local device registry and provisioning orchestrator
    Signed-off-by: Miles Dai <milesdai@opentitan.org>

---
+++ sw/host/provisioning/orchestrator/orchestrator.py
@@ -0,0 +1,233 @@
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+import argparse
+import binascii
+import datetime
+import logging
+import os
+import shlex
+import sqlite3
+import struct
+import subprocess
+import sys
+
+from ot_device import OTDevice
+from util import (format_hex, confirm, bytes_to_int, parse_hexstring_to_int,
+                  parse_str_to_ascii_bytes, OT_SQL_TABLE_NAME)
+
+
+def setup_db(db_filename, log_archive_root):
+    """Set up the provisioning database.
+
+    This function should be idempotent.
+
+    Returns:
+    db_conn - a database connecion
+    """
+    db_conn = sqlite3.connect(db_filename)
+    cur = db_conn.cursor()
+    cur.execute(f"""
+                CREATE TABLE IF NOT EXISTS {OT_SQL_TABLE_NAME} (
+                    device_id STRING NOT NULL PRIMARY KEY,
+                    test_unlock_token STRING,
+                    test_exit_token STRING,
+                    enc_rma_unlock STRING,
+                    device_ecc_pub_key_x STRING,
+                    device_ecc_pub_key_y STRING,
+                    host_ecc_priv_keyfile STRING,
+                    uds_cert_pub_key_x STRING,
+                    uds_cert_pub_key_y STRING,
+                    lc_state STRING,
+                    sku STRING,
+                    commit_hash STRING,
+                    timestamp STRING)
+    """)
+
+    # Create the diretory that holds the logs.
+    os.makedirs(log_archive_root, exist_ok=True)
+
+    return db_conn
+
+
+def make_device_id(si_creator_id, product_id, chip_revision, package_id,
+                   serial_num):
+    """Create the full 256-bit device ID
+
+    Each parameter should be written as a little-endian formatted hex string
+
+    Arguments:
+    si_creator_id (2 bytes): Silicon Creator identifier. Assigned by the OpenTitan project
+    product_id (2 bytes): Product identifier. Assigned by the Silicon Creator
+    chip_revision (2 bytes): [Device UID] ASCII chip revision identifier
+    package_id (2 bytes): [Device UID] ASCII package identifier
+    serial_num (2 bytes): [Device UID] uint16_t serial number
+    """
+
+    device_uid = struct.pack("<HHHH", serial_num, bytes_to_int(package_id), 0,
+                             bytes_to_int(chip_revision))
+    logging.debug(f"[Device ID] device_uid: {device_uid}")
+    logging.debug(
+        f"[Device ID] device_uid: {format_hex(bytes_to_int(device_uid), width=16)}"
+    )
+
+    # HW origin contains:
+    # - 16 bits SiCreator ID
+    # - 16 bits Product ID
+    # - 64 bits Device UID
+    hw_origin = struct.pack("<HH8s", si_creator_id, product_id, device_uid)
+    logging.debug(
+        f"[Device ID] hw_origin: {format_hex(bytes_to_int(hw_origin), width=24)}"
+    )
+
+    crc = binascii.crc32(hw_origin)
+    full_hw_origin = struct.pack("<12sI", hw_origin, crc)
+    logging.debug(
+        f"[Device ID] full_hw_origin: {format_hex(bytes_to_int(full_hw_origin), width=32)}"
+    )
+
+    # Repeat the hw origin information in the sku_specific_info section
+    sku_specific_info = full_hw_origin
+
+    # Assemble full device ID
+    device_id_bytes = struct.pack("<16s16s", full_hw_origin, sku_specific_info)
+    device_id = bytes_to_int(device_id_bytes)
+    logging.debug(f"[Device ID] device_id: {format_hex(device_id, width=64)}")
+
+    return device_id
+
+
+def main():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        handlers=[
+            logging.FileHandler("orchestrator.log.txt"),
+            logging.StreamHandler(sys.stdout)
+        ],
+    )
+
+    parser = argparse.ArgumentParser(
+        prog="OT ES Provisioning Orchestrator",
+        description=
+        "This tool runs the CP and FT programs and records the results.",
+    )
+    parser.add_argument("--si_creator_id",
+                        required=True,
+                        type=parse_hexstring_to_int)
+    parser.add_argument("--product_id",
+                        required=True,
+                        type=parse_hexstring_to_int)
+    parser.add_argument("--serial_num",
+                        required=True,
+                        type=parse_hexstring_to_int)
+    parser.add_argument("--package_id",
+                        required=True,
+                        type=parse_str_to_ascii_bytes)
+    parser.add_argument("--chip_revision",
+                        required=True,
+                        type=parse_str_to_ascii_bytes)
+    parser.add_argument("--test_unlock_token",
+                        required=True,
+                        type=parse_hexstring_to_int)
+    parser.add_argument("--test_exit_token",
+                        required=True,
+                        type=parse_hexstring_to_int)
+    parser.add_argument("--ecc_priv_keyfile", required=True)
+    parser.add_argument("--target_lc_state",
+                        help="Target mission mode LC state",
+                        choices=["dev", "prod"],
+                        required=True)
+    parser.add_argument("--sku",
+                        help="SKU",
+                        choices=["sival", "sival_bringup"],
+                        required=True)
+    parser.add_argument("--fpga_test",
+                        action="store_true",
+                        help="Run on FPGA targets",
+                        default=False)
+    parser.add_argument("--non_interactive",
+                        action="store_true",
+                        help="Skip all non-required confirmations",
+                        default=False)
+    parser.add_argument("--db_file",
+                        default="earlgrey_z1.db",
+                        help="Database file")
+    parser.add_argument("--log_archive_root",
+                        default="logs",
+                        help="Root directory to store log files under.")
+    args = parser.parse_args()
+
+    db_conn = setup_db(args.db_file, args.log_archive_root)
+
+    device_id = make_device_id(args.si_creator_id, args.product_id,
+                               args.chip_revision, args.package_id,
+                               args.serial_num)
+
+    # Check if the device ID is present in the DB
+    logging.debug("Checking for duplicate Device ID in DB...")
+    device_id_str = format_hex(device_id, width=64)
+    res = db_conn.cursor().execute(
+        f"SELECT device_id FROM {OT_SQL_TABLE_NAME} WHERE device_id='{device_id_str}'"
+    )
+    existing_devices = res.fetchall()
+    if len(existing_devices) > 0:
+        logging.warning(
+            f"Device ID {device_id_str} already exists in the database." +
+            " Database write will fail if you contine."
+        )
+        confirm()
+    else:
+        logging.debug("Did not find duplicate Device ID in DB. Continuing")
+
+    # Generate some metadata
+    commit_hash = subprocess.run(shlex.split("git rev-parse HEAD"),
+                                 capture_output=True,
+                                 text=True).stdout.strip()
+    ecc_keyfile_basename = os.path.basename(args.ecc_priv_keyfile)
+
+    print(
+        "\nPlease confirm the following values (> indicates generated values):"
+    )
+    print(f"""
+          [DEVICE ID]
+          si_creator_id: {format_hex(args.si_creator_id, width = 4)}
+          product_id: {format_hex(args.product_id, width = 4)}
+          serial_num: {format_hex(args.serial_num, width = 2)}
+          chip_revision: {args.chip_revision}
+          package_id: {args.package_id}
+          > device_id: {format_hex(device_id, width=64)}
+
+          [TOKENS]
+          test_unlock_token: {format_hex(args.test_unlock_token, width=32)}
+          test_exit_token: {format_hex(args.test_exit_token, width=32)}
+
+          [ECC PRIVATE KEYFILE]
+          keyfile path: {args.ecc_priv_keyfile}
+          > keyfile basename (stored in DB): {ecc_keyfile_basename}
+
+          [OTHER]
+          target_lc_state: {args.target_lc_state}
+          sku: {args.sku}
+          > commit hash: {commit_hash}
+          """)
+
+    if not args.non_interactive:
+        confirm()
+
+    timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
+
+    chip = OTDevice(device_id, args.test_unlock_token, args.test_exit_token,
+                    args.target_lc_state, args.sku, args.fpga_test,
+                    args.log_archive_root)
+    chip.cp_provision(require_confirmation=not args.non_interactive)
+    chip.ft_provision(args.ecc_priv_keyfile,
+                      require_confirmation=not args.non_interactive)
+    chip.parse_logs()
+    chip.record_device(db_conn, commit_hash, timestamp, ecc_keyfile_basename)
+
+    logging.info("Provisioning complete")
+
+
+if __name__ == "__main__":
+    main()
---
+++ sw/host/provisioning/orchestrator/ot_device.py
@@ -0,0 +1,244 @@
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+import json
+import logging
+import os
+import re
+import sqlite3
+
+from util import format_hex, confirm, run, OT_SQL_TABLE_NAME
+
+
+class OTDevice:
+
+    def __init__(self, device_id, test_unlock_token, test_exit_token,
+                 target_lc_state, sku, fpga_test,
+                 log_archive_root):
+        """Class representing an Opentitan Device being provisioned.
+
+        Arguments:
+        device_id -- Device ID defined by the Silicon Creator and specified in
+            https://opentitan.org/book/doc/security/specs/identities_and_root_keys/#device-identifier
+        test_unlock_token -- Test-Unlock token
+        test_exit_token -- Test-exit token
+        target_lc_state -- final lifecycle state of the device after provisioning.
+            Must be dev or prod.
+        sku -- OTP image to use. Must be sival or sival_bringup.
+        fpga_test -- Boolean indicating if the FPGA test flow should be executed.
+        log_archive_root -- Root directory to store log files under.
+        """
+        self.test_unlock_token = test_unlock_token
+        self.test_exit_token = test_exit_token
+        self.target_lc_state = target_lc_state
+        self.device_id = device_id
+        self.sku = sku
+        self.fpga_test = fpga_test
+
+        # Strip the 0x from the front of the directory name
+        self.log_dir = f"{log_archive_root}/{format_hex(self.device_id, width=64)[2:]}"
+        if os.path.exists(self.log_dir):
+            logging.warning(
+                f"Log file {self.log_dir} already exists. Continue to overwrite"
+            )
+            confirm()
+
+        os.makedirs(self.log_dir, exist_ok=True)
+
+    def cp_provision(self, require_confirmation=True):
+        """Run the CP provisioning Bazel target."""
+        logging.info("Running CP Provisioning")
+
+        elf_path = "sw/device/silicon_creator/manuf/skus/earlgrey_a0/sival_bringup/{}"
+        if self.fpga_test:
+            platform_flags = """--interface=cw310 \
+--clear-bitstream --bitstream=hw/bitstream/rom_with_fake_keys_otp_raw.bit \
+--openocd=third_party/openocd/build_openocd/bin/openocd \
+--openocd-adapter-config=external/openocd/tcl/interface/ftdi/olimex-arm-usb-tiny-h.cfg"""
+            elf = elf_path.format(
+                "sram_cp_provision_fpga_cw310_rom_with_fake_keys.elf")
+        else:
+            platform_flags = """--interface=hyper310 \
+--openocd=third_party/openocd/build_openocd/bin/openocd \
+--openocd-adapter-config=external/openocd/tcl/interface/cmsis-dap.cfg"""
+            elf = elf_path.format("sram_cp_provision_silicon_creator.elf")
+
+        # Assemble bazel command
+        cmd = f"""bazel run //sw/host/tests/manuf/provisioning/cp:cp -- \
+--rcfile= --logging=info \
+{platform_flags} \
+--elf={elf} \
+--device-id="{format_hex(self.device_id, width=64)}" \
+--test-unlock-token="{format_hex(self.test_unlock_token, width=32)}" \
+--test-exit-token="{format_hex(self.test_exit_token, width=32)}" \
+--manuf-state=0x00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000 \
+--wafer-auth-secret=0x00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000"""
+
+        logging.info(f"Running command: {cmd}")
+        if require_confirmation:
+            confirm()
+
+        res = run(cmd, f"{self.log_dir}/cp_out.log.txt",
+                  f"{self.log_dir}/cp_err.log.txt")
+
+        if res.returncode != 0:
+            logging.warning(
+                f"cp_provisioning returned with non-zero exit code: {res.returncode}." +
+                " Logs have been written but no database entry has been made yet."
+            )
+            confirm()
+        else:
+            logging.info("CP completed successfully")
+
+    def ft_provision(self, ecc_priv_keyfile, require_confirmation=True):
+        """Run the FT provisioning Bazel target."""
+        logging.info("Running FT Provisioning")
+
+        sram_ft_indiv_elf_path = "sw/device/silicon_creator/manuf/skus/earlgrey_a0/sival_bringup/sram_ft_individualize_{}_{}.elf"  # noqa: E501
+        perso_bin_path = "sw/device/silicon_creator/manuf/skus/earlgrey_a0/sival_bringup/{}"
+
+        if self.fpga_test:
+            elf = sram_ft_indiv_elf_path.format(
+                self.sku, "fpga_cw310_rom_with_fake_keys")
+
+            bootstrap = perso_bin_path.format(
+                "ft_personalize_1_fpga_cw310_rom_with_fake_keys.prod_key_0.signed.bin"
+            )
+            bootstrap2 = perso_bin_path.format(
+                "ft_personalize_2_fpga_cw310_rom_with_fake_keys.prod_key_0.signed.bin"
+            )
+            bootstrap3 = perso_bin_path.format(
+                "ft_personalize_3_fpga_cw310_rom_with_fake_keys.prod_key_0.signed.bin"
+            )
+
+            platform_bazel_flags = ""
+            platform_harness_flags = """--interface=cw310 --clear-bitstream \
+--bitstream=sw/host/tests/manuf/provisioning/ft/ft_test_bitstream.bit \
+--openocd=third_party/openocd/build_openocd/bin/openocd \
+--openocd-adapter-config=external/openocd/tcl/interface/ftdi/olimex-arm-usb-tiny-h.cfg"""
+
+        else:
+            elf = sram_ft_indiv_elf_path.format(self.sku, "silicon_creator")
+
+            bootstrap = perso_bin_path.format(
+                f"ft_personalize_1_silicon_creator.earlgrey_a0_{self.target_lc_state}_0.signed.bin"
+            )
+            bootstrap2 = perso_bin_path.format(
+                f"ft_personalize_2_silicon_creator.earlgrey_a0_{self.target_lc_state}_0.signed.bin"
+            )
+            bootstrap3 = perso_bin_path.format(
+                f"ft_personalize_3_silicon_creator.earlgrey_a0_{self.target_lc_state}_0.signed.bin"
+            )
+
+            platform_bazel_flags = "--//signing:token=//signing/tokens:nitrokey"
+            platform_harness_flags = """--interface=hyper310 \
+--openocd=third_party/openocd/build_openocd/bin/openocd \
+--openocd-adapter-config=external/openocd/tcl/interface/cmsis-dap.cfg"""
+
+        cmd = f"""bazel run //sw/host/tests/manuf/provisioning/ft:ft {platform_bazel_flags} -- \
+--rcfile= --logging=info \
+{platform_harness_flags} \
+--elf={elf} \
+--bootstrap={bootstrap} \
+--second-bootstrap={bootstrap2} \
+--third-bootstrap={bootstrap3}  \
+--device-id="{format_hex(self.device_id, 64)}" \
+--test-unlock-token="{format_hex(self.test_unlock_token, width=32)}" \
+--test-exit-token="{format_hex(self.test_exit_token, width=32)}" \
+--target-mission-mode-lc-state="{self.target_lc_state}" \
+--host-ecc-sk={ecc_priv_keyfile} \
+--rom-ext-measurement=0x00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000  \
+--owner-manifest-measurement=0x00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000  \
+--owner-measurement=0x00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000"""  # noqa: E501
+
+        logging.info(f"Running command: {cmd}")
+        if require_confirmation:
+            confirm()
+        res = run(cmd, f"{self.log_dir}/ft_out.log.txt",
+                  f"{self.log_dir}/ft_err.log.txt")
+
+        if res.returncode != 0:
+            logging.warning(
+                f"ft_provisioning returned with non-zero return code: {res.returncode}." +
+                " Logs have been written but no database entry has been made yet."
+            )
+            confirm()
+        else:
+            logging.info("FT completed successfully")
+
+    def parse_logs(self):
+        logging.info("Extracting values from logs...")
+        # Extract output from logs
+        with open(f"{self.log_dir}/ft_out.log.txt", "r") as f:
+            stdout = f.read()
+
+        rma_msg_matches = re.findall(
+            r"RESP_OK:(\{\"wrapped_rma_unlock_token\":.*) CRC:.*", stdout)
+        if len(rma_msg_matches) != 1:
+            logging.error(
+                f"Expected 1 RMA unlock token message, found these messages: {rma_msg_matches}"
+            )
+
+        # TODO: UDS certs cannot currently be provisioned in ft_personalize_3. Write a zero for now.
+        # uds_cert_msg_matches = re.findall("RESP_OK:(\{\"uds_certificate\":.*) CRC:.*", stdout)
+        # if len(uds_cert_msg_matches) != 1:
+        #     logging.error(f"Expected 1 UDS certificate message, found these messages: {uds_cert_msg_matches}")  # noqa: E501
+
+        logging.debug("Found RESP_OK messages")
+
+        # First match is the RMA unlock token payload
+        rma_payload = json.loads(rma_msg_matches[0])
+        wrapped_key = rma_payload["wrapped_rma_unlock_token"]["data"]
+        device_pk = rma_payload["wrapped_rma_unlock_token"]["device_pk"]
+        # TODO: parse these keys
+        self.enc_rma_unlock = str(wrapped_key)
+        self.device_ecc_pub_key_x = str(device_pk["x"])
+        self.device_ecc_pub_key_y = str(device_pk["y"])
+
+        # Second match is the certificate payload
+        # cert_payload = json.loads(uds_cert_msg_matches[0])
+        # Note: FT stage currently outputs the public key of the certficate
+        # rather than the cert itself
+        # uds_cert_pub_key = cert_payload["uds_certificate"]
+        # self.uds_cert_pub_key_x = str(uds_cert_pub_key["x"])
+        # self.uds_cert_pub_key_y = str(uds_cert_pub_key["y"])
+        self.uds_cert_pub_key_x = "0"
+        self.uds_cert_pub_key_y = "0"
+
+    def record_device(self, db_conn, commit_hash, timestamp,
+                      ecc_keyfile_basename):
+        logging.info("Writing outputs to DB")
+        table_values = [
+            format_hex(self.device_id, width=64),
+            format_hex(self.test_unlock_token, width=32),
+            format_hex(self.test_exit_token, width=32),
+            self.enc_rma_unlock,
+            self.device_ecc_pub_key_x,
+            self.device_ecc_pub_key_y,
+            ecc_keyfile_basename,
+            self.uds_cert_pub_key_x,
+            self.uds_cert_pub_key_y,
+            self.target_lc_state,
+            self.sku,
+            commit_hash,
+            timestamp,
+        ]
+        sql_cmd = f"INSERT INTO {OT_SQL_TABLE_NAME} VALUES ("
+        for i in table_values:
+            sql_cmd += f"\"{i}\","
+        # Remove last comma
+        sql_cmd = sql_cmd[:-1]
+        sql_cmd += ")"
+
+        logging.debug(f"Executing SQL command: {sql_cmd}")
+        try:
+            db_conn.cursor().execute(sql_cmd)
+            db_conn.commit(
+            )  # INSERT statement implicitly opens a SQL transaction that must be committed
+        except sqlite3.IntegrityError:
+            logging.warning(f"SQL command failed executing: {sql_cmd}")
+
+        # Write out metadata to filesystem backup
+        with open(f"{self.log_dir}/metadata.log.txt", "w") as f:
+            f.write(commit_hash)
+            f.write(timestamp)
