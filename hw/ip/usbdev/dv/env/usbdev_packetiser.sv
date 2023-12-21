1d0cd3f41657070d4c64241a1a7066524d0d46c6
Saad Anees <saad.impare@gmail.com>
2023-12-21 18:13:53 +0500

    [usbdev] Predictor of usbdev
    This request contains classes of usbdev predictor with little ammendment
    
    Signed-off-by: Saad Anees <saad.impare@gmail.com>

--- hw/ip/usbdev/dv/env/usbdev_packetiser.sv
+++ hw/ip/usbdev/dv/env/usbdev_packetiser.sv
@@ -12,30 +12,38 @@ class usbdev_packetiser extends uvm_object;
   bit data_pkt_arr[];
   bit handshake_pkt_arr[];
 
-  // Handles to usb20_item and classes derived from usb20_item
-  usb20_item     m_usb20_item;
-  token_pkt      m_tpkt;
-  data_pkt       m_dpkt;
-  handshake_pkt  m_hpkt;
+  // Handles to classes derived from usb20_item
+  token_pkt     m_tpkt;
+  data_pkt      m_dpkt;
+  handshake_pkt m_hpkt;
 
   function new(string name = "usbdev_packetiser");
     super.new(name);
-    m_usb20_item = new("usb20_item");
-    m_tpkt       = new("token_pkt");
-    m_dpkt       = new("data_pkt");
-    m_hpkt       = new("handshake_pkt");
+    m_tpkt = new("token_pkt");
+    m_dpkt = new("data_pkt");
+    m_hpkt = new("handshake_pkt");
   endfunction
 
   // pack_pkt task
   // -------------------------------
-  task pack_pkt();
+  task pack_pkt(usb20_item m_usb20_item);
     if (m_usb20_item.m_pkt_type == PktTypeToken) begin
+      m_tpkt.m_pid_type = pid_type_e'({<<4{m_tpkt.m_pid_type}});
+      m_tpkt.m_pid_type = pid_type_e'({<<{m_tpkt.m_pid_type}});
+      m_tpkt.address = {<<{m_tpkt.address}};
+      m_tpkt.endpoint = {<<{m_tpkt.endpoint}};
+      m_tpkt.crc5 = {<<{m_tpkt.crc5}};
       m_tpkt.pack(token_pkt_arr);
     end
     else if (m_usb20_item.m_pkt_type == PktTypeData) begin
+      m_dpkt.m_pid_type = pid_type_e'({<<4{m_dpkt.m_pid_type}});
+      m_dpkt.m_pid_type = pid_type_e'({<<{m_dpkt.m_pid_type}});
+      m_dpkt.crc16 = {<<{m_dpkt.crc16}};
       m_dpkt.pack(data_pkt_arr);
     end
     else if (m_usb20_item.m_pkt_type == PktTypeHandshake) begin
+      m_hpkt.m_pid_type = pid_type_e'({<<4{m_hpkt.m_pid_type}});
+      m_hpkt.m_pid_type = pid_type_e'({<<{m_hpkt.m_pid_type}});
       m_hpkt.pack(handshake_pkt_arr);
     end
     else;
--- hw/ip/usbdev/dv/env/usbdev_pkt_manager.sv
+++ hw/ip/usbdev/dv/env/usbdev_pkt_manager.sv
@@ -21,12 +21,12 @@ class usbdev_pkt_manager extends uvm_object;
        expected_pkt = exp_q.pop_back();
     end
     else
-      `uvm_info(get_type_name(),"QUEUE IS EMPTY",UVM_LOW);
+      `uvm_info(get_type_name(), "QUEUE IS EMPTY", UVM_LOW);
   endtask
 
    // push_packet task to push the expected packet
   // -------------------------------
-  task push_packet(bit exp_pkt []);
+  task push_packet(bit exp_pkt[]);
     exp_q.push_front(exp_pkt);
   endtask
 endclass
@@ -34,41 +34,35 @@ endclass
 // Token_packet class
 // -------------------------------
 class token_packet extends usbdev_pkt_manager;
-  logic [7:0]  sync;
-  logic [7:0]  pid;
-  logic [6:0]  addr;
-  logic [3:0]  endp;
-  logic [4:0]  crc5;
-  logic [2:0]  eop;
+  logic [7:0] pid;
+  logic [6:0] addr;
+  logic [3:0] endp;
+  logic [4:0] crc5;
   bit pack_token[];
 
-  `uvm_object_utils_begin (token_packet)
-    `uvm_field_int(sync, UVM_ALL_ON)
-    `uvm_field_int(pid, UVM_ALL_ON)
+  `uvm_object_utils_begin(token_packet)
+    `uvm_field_int(pid,  UVM_ALL_ON)
     `uvm_field_int(addr, UVM_ALL_ON)
     `uvm_field_int(endp, UVM_ALL_ON)
     `uvm_field_int(crc5, UVM_ALL_ON)
-    `uvm_field_int(eop, UVM_ALL_ON)
   `uvm_object_utils_end
 
   function new(string name = "token_packet");
     super.new(name);
-    this.sync = 0;
     this.pid  = 0;
     this.addr = 0;
     this.endp = 0;
     this.crc5 = 0;
-    this.eop  = 0;
   endfunction
 
   // send_token_packet task : To get the fields of token packet and pack them and then send that
   // packet into expected queue
   // -------------------------------
-  task send_token_packet(bit[7:0] tpid,bit[6:0] taddress,bit[3:0] tendpoint);
+  task send_token_packet(bit [7:0] tpid, bit [6:0] taddress, bit [3:0] tendpoint);
     bit [4:0] crc;
     bit [10:0] data;
     bit [4:0] polynomial = 5'b0_0101;
-    data = {tendpoint,taddress};
+    data = {tendpoint, taddress};
 
     // Calculate crc5
     crc = 5'b1_1111;
@@ -83,7 +77,6 @@ class token_packet extends usbdev_pkt_manager;
     crc = ~crc;
 
     // Bit streaming to send LSB first...
-    this.sync = 8'b0000_0001;
     this.pid  = {<<4{tpid}};
     this.pid  = {<<{this.pid}};
     taddress  = {<<{taddress}};
@@ -91,7 +84,6 @@ class token_packet extends usbdev_pkt_manager;
     tendpoint = {<<{tendpoint}};
     this.endp = tendpoint;
     this.crc5 = crc;
-    this.eop  = 3'b001;
     // Pack the token packet
     this.pack(pack_token);
     super.push_packet(pack_token);
@@ -101,39 +93,33 @@ endclass
 // data_packet class
 // -------------------------------
 class data_packet extends usbdev_pkt_manager;
-  logic [7:0]   sync;
-  logic [7:0]   pid;
-  logic data    [];
-  logic [15:0]  crc16;
-  logic [2:0]   eop;
-  bit pack_data [];
+  logic [7:0] pid;
+  logic data[];
+  logic [15:0] crc16;
+  bit pack_data[];
 
-  `uvm_object_utils_begin (data_packet)
-    `uvm_field_int(sync, UVM_ALL_ON)
-    `uvm_field_int(pid, UVM_ALL_ON)
+  `uvm_object_utils_begin(data_packet)
+    `uvm_field_int(pid,        UVM_ALL_ON)
     `uvm_field_array_int(data, UVM_ALL_ON)
-    `uvm_field_int(crc16, UVM_ALL_ON)
-    `uvm_field_int(eop, UVM_ALL_ON)
+    `uvm_field_int(crc16,      UVM_ALL_ON)
   `uvm_object_utils_end
 
   function new(string name = "data_packet");
     super.new(name);
-    this.sync = 0;
     this.pid  = 0;
     foreach(data[i])
       this.data[i] = 0;
     this.crc16 = 0;
-    this.eop = 0;
   endfunction
 
   // send_data_packet task : To get the fields of data packet and pack them and then send that
   // packet into expected queue
   // -------------------------------
-  task send_data_packet(bit[7:0] dpid,bit ddata []);
+  task send_data_packet(bit [7:0] dpid, bit ddata[]);
     bit [15:0] crc;
     bit [15:0] polynomial = 16'h8005;
 
-    // store the data bit by bit in dynamic array
+    // Store the data bit by bit in dynamic array
     foreach(ddata[i]) begin
       this.data = new[this.data.size() + 1](this.data);
       this.data[this.data.size() - 1] = ddata[i];
@@ -155,11 +141,12 @@ class data_packet extends usbdev_pkt_manager;
     end
     crc = ~crc;
 
-    this.sync  = 8'b0000_0001;
-    this.pid   = {<<4{dpid}};
-    this.pid  = {<<{this.pid}};
-    this.crc16 = crc;
-    this.eop   = 3'b001;
+    this.pid = {<<4{dpid}};
+    this.pid = {<<{this.pid}};
+    this.data = {<<8{this.data}};
+    this.data = {<<{this.data}};
+    this.crc16 = {<<{crc}};
+
     // Pack the data packet
     this.pack(pack_data);
     super.push_packet(pack_data);
@@ -170,31 +157,23 @@ endclass
 // hand_shake_packet class
 // -------------------------------
 class hand_shake_packet extends usbdev_pkt_manager;
-  logic [7:0] sync;
   logic [7:0] pid;
-  logic [2:0] eop;
   bit pack_handshake[];
 
-  `uvm_object_utils_begin (hand_shake_packet)
-    `uvm_field_int(sync,UVM_ALL_ON)
-    `uvm_field_int(pid,UVM_ALL_ON)
-    `uvm_field_int(eop,UVM_ALL_ON)
+  `uvm_object_utils_begin(hand_shake_packet)
+    `uvm_field_int(pid, UVM_ALL_ON)
   `uvm_object_utils_end
 
   function new(string name = "hand_shake_packet");
     super.new(name);
-    this.sync = 0;
     this.pid  = 0;
-    this.eop  = 0;
   endfunction
 
   // send_handshake_packet task : To get the field of data packet and pack them and then send that
   // packet into expected queue
-  task send_handshake_packet(bit[7:0] hpid);
-    this.sync  = 8'b0000_0001;
-    this.pid   = {<<4{hpid}};
-    this.pid  = {<<{this.pid}};
-    this.eop   = 3'b001;
+  task send_handshake_packet(bit [7:0] hpid);
+    this.pid  = hpid;
+
     // Pack the handshake packet
     this.pack(pack_handshake);
     super.push_packet(pack_handshake);
