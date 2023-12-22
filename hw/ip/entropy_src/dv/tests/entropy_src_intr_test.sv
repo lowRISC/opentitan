50ecc0222fab15267ab85f34a634651a09fe9cfe
Hakim Filali <hfilali@lowrisc.org>
2023-12-22 13:20:25 +0000

    [es/dv] Add interrupt test
    This commit adds the new interrupt test, which uses the existing
    interrupt vseq.
    
    Signed-off-by: Hakim Filali <hfilali@lowrisc.org>

---
+++ hw/ip/entropy_src/dv/tests/entropy_src_intr_test.sv
@@ -0,0 +1,22 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class entropy_src_intr_test extends entropy_src_base_test;
+
+  `uvm_component_utils(entropy_src_intr_test)
+  `uvm_component_new
+
+  function void configure_env();
+    super.configure_env();
+
+    cfg.en_scb                     = 1;
+    cfg.dut_cfg.fips_window_size   = 2048;
+    cfg.dut_cfg.bypass_window_size = 384;
+    cfg.dut_cfg.default_ht_thresholds_pct = 100;
+
+    `DV_CHECK_RANDOMIZE_FATAL(cfg)
+
+    `uvm_info(`gfn, $sformatf("%s", cfg.convert2string()), UVM_LOW)
+  endfunction
+endclass : entropy_src_intr_test
