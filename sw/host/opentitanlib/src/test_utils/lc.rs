283143dbef835030f62952913103f35ade28dcd5
Miguel Osorio <miguelosorio@google.com>
2023-12-21 19:45:39 -0800

    [ottool] Apply rom_bootstrap before connecting to lc tap.
    Added to read_lc_state to make the call stable when ROM execution is
    enabled.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- sw/host/opentitanlib/src/test_utils/lc.rs
+++ sw/host/opentitanlib/src/test_utils/lc.rs
@@ -16,10 +16,15 @@ pub fn read_lc_state(
     reset_delay: Duration,
 ) -> Result<DifLcCtrlState> {
     transport.pin_strapping("PINMUX_TAP_LC")?.apply()?;
+
+    // Apply bootstrap pin to be able to connect to JTAG when ROM execution is
+    // enabled.
+    transport.pin_strapping("ROM_BOOTSTRAP")?.apply()?;
     transport.reset_target(reset_delay, true)?;
     let mut jtag = jtag_params.create(transport)?.connect(JtagTap::LcTap)?;
     let raw_lc_state = jtag.read_lc_ctrl_reg(&LcCtrlReg::LcState)?;
     jtag.disconnect()?;
     transport.pin_strapping("PINMUX_TAP_LC")?.remove()?;
+    transport.pin_strapping("ROM_BOOTSTRAP")?.remove()?;
     DifLcCtrlState::from_redundant_encoding(raw_lc_state)
 }
