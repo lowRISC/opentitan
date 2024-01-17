6bb50073bec9cc7d2960f59aa21a634a701ebd1b
Chris Frantz <cfrantz@google.com>
2024-01-17 12:59:34 -0800

    [rom_ext] Update the ROM_EXT ePMP configuration
    To provide owner applications with a more flexible memory protection
    configuration, I have re-arranged the ePMP settings.
    
    Notably:
    - The lockouts are moved to regions 0 & 1 to have the highest priority.
    - The full FLASH/MMIO/RAM windows have been moved to the end to allow
      finer grained controls to be applied in the middle entries.
    - The ROM_EXT region is moved and unlocked so owners can reclaim the
      ROM_EXT entries for their own use.
    - The owner code regions are left unlocked so owners can select their
      own preferred arrangement.
    
    We jump to owner code with MML=0, allowing the owner to reconfigure the
    unlocked entries.  The owner should set MML=1 if they need machine/user
    mode isolation.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/device/silicon_creator/rom_ext/e2e/lockdown/epmp_rlb_lockdown.c
+++ sw/device/silicon_creator/rom_ext/e2e/lockdown/epmp_rlb_lockdown.c
@@ -5,6 +5,7 @@
 #include "sw/device/lib/base/csr.h"
 #include "sw/device/lib/base/status.h"
 #include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 #include "sw/device/silicon_creator/lib/dbg_print.h"
 
@@ -13,13 +14,16 @@
 OTTF_DEFINE_TEST_CONFIG();
 
 bool test_main(void) {
-  // Try to overwrite ePMP entry 6 (ie: the otp lockout) by clearing its config
+  // Try to overwrite ePMP entry 0 (ie: the otp lockout) by clearing its config
   // bits.
-  CSR_CLEAR_BITS(CSR_REG_PMPCFG1, 0xff << 16);
-  CSR_WRITE(CSR_REG_PMPADDR6, 0);
+  CSR_CLEAR_BITS(CSR_REG_PMPCFG0, 0xff);
+  CSR_WRITE(CSR_REG_PMPADDR0, 0);
 
-  // The test rule in the BUILD file will look for the expected configuration
-  // for ePMP entry 10.
-  dbg_print_epmp();
+  // Read back ePMP entry 0 and make sure it hasn't changed to zero.
+  uint32_t pmpcfg0, pmpaddr0;
+  CSR_READ(CSR_REG_PMPCFG0, &pmpcfg0);
+  CSR_READ(CSR_REG_PMPADDR0, &pmpaddr0);
+  CHECK((pmpcfg0 & 0xFF) != 0);
+  CHECK(pmpaddr0 != 0);
   return true;
 }
