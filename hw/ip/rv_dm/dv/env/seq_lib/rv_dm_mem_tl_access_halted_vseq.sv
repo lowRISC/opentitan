85fae4ff9000debff7ef13329ca2aad0d95bc397
Basit Mehmood <basit.impare@gmail.com>
2024-01-18 15:07:57 +0500

    [RV_DM]rv_dm_mem_tl_access_halted_vseq
    Verify that writing to HALTED results in anyhalted and allhalted to be set.
    
    Signed-off-by: Basit Mehmood <basit.impare@gmail.com>

--- hw/ip/rv_dm/dv/env/seq_lib/rv_dm_mem_tl_access_halted_vseq.sv
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_mem_tl_access_halted_vseq.sv
@@ -16,17 +16,18 @@ class rv_dm_mem_tl_access_halted_vseq extends rv_dm_base_vseq;
   }
 
   task body();
-    uvm_reg_data_t rw_data;
+    uvm_reg_data_t w_data;
+    uvm_reg_data_t r_data;
     repeat ($urandom_range(1, 10)) begin
-      rw_data = $urandom_range(0, 1);
+      w_data = $urandom_range(0, 1);
       // Verify that writing to HALTED results in anyhalted and allhalted to be set.
       csr_wr(.ptr(jtag_dmi_ral.dmcontrol.haltreq), .value(1));
       cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
       csr_wr(.ptr(tl_mem_ral.halted), .value(0));
       cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
-      csr_rd(.ptr(jtag_dmi_ral.dmstatus), .value(rw_data));
-      `DV_CHECK_EQ(1, get_field_val(jtag_dmi_ral.dmstatus.anyhalted, rw_data))
-      `DV_CHECK_EQ(1, get_field_val(jtag_dmi_ral.dmstatus.allhalted, rw_data))
+      csr_rd(.ptr(jtag_dmi_ral.dmstatus), .value(r_data));
+      `DV_CHECK_EQ(1, get_field_val(jtag_dmi_ral.dmstatus.anyhalted, r_data))
+      `DV_CHECK_EQ(1, get_field_val(jtag_dmi_ral.dmstatus.allhalted, r_data))
       cfg.clk_rst_vif.wait_clks($urandom_range(1, 10));
     end
   endtask : body
