fa1aab42fc12b072a69a97a057b6217e03b386e5
Amaury Pouly <amaury.pouly@lowrisc.org>
2024-01-23 16:25:28 +0000

    [opentitantool] Rework stamping
    Allow opentitantool to be built with stamping disabled, and disable
    it by default. Update the `version` command to handle the case of
    missing information. The build time/date is removed since it adds
    no useful information.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/host/opentitantool/src/command/version.rs
+++ sw/host/opentitantool/src/command/version.rs
@@ -15,19 +15,29 @@ pub struct Version {}
 
 #[derive(Debug, serde::Serialize)]
 pub struct VersionResponse {
-    pub version: String,
-    pub revision: String,
-    pub clean: bool,
-    pub timestamp: i64,
+    pub version: Option<String>,
+    pub revision: Option<String>,
+    pub clean: Option<bool>,
+}
+
+fn parse_variable(content: &str) -> Option<String> {
+    // If stamping is disabled, the variable substition in stamp-env.txt
+    // will not happen and the content of the environment variable will be
+    // `{NAME_OF_VARIABLE}`. If we detect that this is the case, we return
+    // None, otherwise we return the content.
+    if content.starts_with('{') {
+        None
+    } else {
+        Some(content.to_string())
+    }
 }
 
 impl Default for VersionResponse {
     fn default() -> Self {
         VersionResponse {
-            version: std::env!("BUILD_GIT_VERSION").to_string(),
-            revision: std::env!("BUILD_SCM_REVISION").to_string(),
-            clean: std::env!("BUILD_SCM_STATUS") == "clean",
-            timestamp: std::env!("BUILD_TIMESTAMP").parse::<i64>().unwrap(),
+            version: parse_variable(std::env!("BUILD_GIT_VERSION")),
+            revision: parse_variable(std::env!("BUILD_SCM_REVISION")),
+            clean: parse_variable(std::env!("BUILD_SCM_STATUS")).map(|x| x == "clean"),
         }
     }
 }
