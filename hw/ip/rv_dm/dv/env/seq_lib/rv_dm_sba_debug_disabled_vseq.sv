d678965c53c856682e2b7364ad70cd837cd65037
Shahid Mehmood <shahid.impare@gmail.com>
2024-01-23 11:02:02 +0500

    [RV_DM] rv_dm_sba_debug_disabled_vseq
    The test ensures that the SBA interface is disabled when 'lc_hw_debug_en' is not set to true, ensuring no SBA TL accesses occur. This commit contains only test sequence as suggested to separate it from scoreboard.
    
    Signed-off-by: Shahid Mehmood <shahid.impare@gmail.com>

---
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_sba_debug_disabled_vseq.sv
@@ -0,0 +1,34 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class rv_dm_sba_debug_disabled_vseq extends rv_dm_sba_tl_access_vseq;
+  `uvm_object_utils(rv_dm_sba_debug_disabled_vseq)
+
+  `uvm_object_new
+
+  constraint lc_hw_debug_en_c {
+    lc_hw_debug_en == lc_ctrl_pkg::On;
+  }
+  constraint scanmode_c {
+    scanmode == prim_mubi_pkg::MuBi4False;
+  }
+
+  task sba_access();
+    csr_wr(.ptr(jtag_dmi_ral.dmcontrol.dmactive), .value(1));
+    req = sba_access_item::type_id::create("req");
+    randomize_req(req);
+    cfg.debugger.sba_access(req);
+  endtask
+
+  task body();
+    repeat ($urandom_range(1, 10)) begin
+      sba_access();
+      cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
+      cfg.rv_dm_vif.lc_hw_debug_en = get_rand_lc_tx_val(.t_weight(0), .f_weight(1),
+                                                        .other_weight(4));
+      sba_access();
+      `DV_CHECK_EQ(req.is_err, SbaErrNone)
+    end
+  endtask : body
+endclass  : rv_dm_sba_debug_disabled_vseq
