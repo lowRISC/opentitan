6f32148bceaacc21759d41e2de3f503559b7c43f
Amaury Pouly <amaury.pouly@lowrisc.org>
2024-01-24 13:46:15 +0000

    [sysrst_ctrl,sival] Enable the sysrst config to specify input pins
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>
    (cherry picked from commit cba80878bc9d2b69337518aae43e7372da2c5734)

--- sw/host/tests/chip/sysrst_ctrl/sysrst_ctrl.rs
+++ sw/host/tests/chip/sysrst_ctrl/sysrst_ctrl.rs
@@ -1,19 +1,19 @@
 // Copyright lowRISC contributors.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
-use anyhow::Result;
+use anyhow::{ensure, Result};
 
 use opentitanlib::app::TransportWrapper;
 use opentitanlib::io::gpio::{PinMode, PullMode};
 
 pub struct Config {
-    // List to the IO names of the transport, the order should match the pin
-    // values in `kTestExpected` on the device C code: the ith item corresponds
-    // to the i-th bit.
-    pub pins: Vec<&'static str>,
-    // If true, the pin will be setup in open drain mode (with a pullup), otherwise
+    // List of the output IO names of the transport.
+    pub output_pins: Vec<&'static str>,
+    // If true, the input pin will be setup in open drain mode (with a pullup), otherwise
     // in push-pull mode. The order must match the one in pins.
     pub open_drain: Vec<bool>,
+    // List of the input IO names of the transport.
+    pub input_pins: Vec<&'static str>,
 }
 
 /// Configure earlgrey and debugger pins.
@@ -21,9 +21,9 @@ pub fn setup_pins(transport: &TransportWrapper, config: &Config) -> Result<()> {
     log::info!("Configuring transport GPIOs");
     // Since the EC reset and flash WP pins are open drain, configure those hyperdebug pins as
     // open drain with pullup.
-    assert_eq!(config.pins.len(), config.open_drain.len());
-    for (pin,od) in std::iter::zip(&config.pins, &config.open_drain) {
-        log::info!("pin {}: {}", pin, od);
+    assert_eq!(config.output_pins.len(), config.open_drain.len());
+    for (pin,od) in std::iter::zip(&config.output_pins, &config.open_drain) {
+        log::info!("output pin {}: {}", pin, od);
         if *od {
             transport.gpio_pin(pin)?.set_mode(PinMode::OpenDrain)?;
             transport.gpio_pin(pin)?.set_pull_mode(PullMode::PullUp)?;
@@ -32,14 +32,18 @@ pub fn setup_pins(transport: &TransportWrapper, config: &Config) -> Result<()> {
             transport.gpio_pin(pin)?.set_mode(PinMode::PushPull)?;
         }
     }
+    for pin in &config.input_pins {
+        log::info!("input pin {}", pin);
+        transport.gpio_pin(pin)?.set_mode(PinMode::Input)?;
+    }
     Ok(())
 }
 
-/// Set pins: the i-th bit of `values` corresponds to the i-th entry of `config`.
+/// Set pins: the i-th bit of `values` corresponds to the i-th entry of `config.output_pins`.
 pub fn set_pins(transport: &TransportWrapper, config: &Config, values: u8) -> Result<()> {
     log::info!("Set pins to {:b}", values);
     // Set pins on the transport.
-    for (i, pin) in config.pins.iter().enumerate() {
+    for (i, pin) in config.output_pins.iter().enumerate() {
         transport
             .gpio_pin(pin)?
             .write(((values >> i) & 0b1) == 0b1)?;
