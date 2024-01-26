335ef15b74d565c31d3392a91f43329b9dcd45f4
Tim Trippel <ttrippel@google.com>
2024-01-25 16:05:47 -0800

    [demo] add example bitmap array to drive screen with OpenTitan
    This adds an example OpenTitan logo bitmap array to drive onto an LED
    screen over SPI.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>
    (cherry picked from commit 603c6d9311c22490ca8c60865f87dd90faaa6921)

---
+++ sw/device/examples/teacup_demos/data/bitmaps.h
@@ -0,0 +1,38 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_EXAMPLES_TEACUP_DEMOS_DATA_BITMAPS_H_
+#define OPENTITAN_SW_DEVICE_EXAMPLES_TEACUP_DEMOS_DATA_BITMAPS_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+/**
+ * A bitmap to draw on a screen.
+ */
+typedef struct screen_bitmap {
+  /**
+   * Number of rows in the bitmap.
+   */
+  const size_t num_rows;
+  /**
+   * Number of columns in the bitmap.
+   */
+  const size_t num_cols;
+  /**
+   * A 2D bitmap array.
+   */
+  const uint16_t *bitmap;
+  /**
+   * Fill color to surround image by.
+   */
+  const uint16_t fill_color;
+} screen_bitmap_t;
+
+/**
+ * Example bitmap(s).
+ */
+extern const screen_bitmap_t kOtLogoBitmap;
+
+#endif  // OPENTITAN_SW_DEVICE_EXAMPLES_TEACUP_DEMOS_DATA_BITMAPS_H_
