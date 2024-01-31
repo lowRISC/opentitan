8831c68cfbf7f25d24d4eb5216832ab66dbec014
Gary Guo <gary.guo@lowrisc.org>
2024-01-31 11:44:53 +0000

    [opentitanlib] add irscan/drscan method to OpenOcd struct.
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

--- sw/host/tests/chip/rv_dm/src/jtag.rs
+++ sw/host/tests/chip/rv_dm/src/jtag.rs
@@ -37,44 +37,44 @@ fn test_jtag(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
     assert_eq!(openocd.execute("init")?, "");
 
     // Read IDCODE register to ensure that it is expected.
-    assert_eq!(openocd.execute("irscan riscv.tap 0x1")?, "");
-    let idcode = u32::from_str_radix(&openocd.execute("drscan riscv.tap 32 0")?, 16)?;
+    openocd.irscan("riscv.tap", 1)?;
+    let idcode = openocd.drscan("riscv.tap", 32, 0)?;
     assert_eq!(idcode, RISCV_IDCODE);
 
     // Attempt to write IDCODE
-    assert_eq!(openocd.execute("irscan riscv.tap 0x1")?, "");
+    openocd.irscan("riscv.tap", 1)?;
     assert_eq!(
-        openocd.execute("drscan riscv.tap 64 0xdeadbeef")?,
-        format!("deadbeef{RISCV_IDCODE:x}")
+        openocd.drscan("riscv.tap", 64, 0xdeadbeef)?,
+        0xdeadbeef << 32 | RISCV_IDCODE as u64
     );
 
     // Read back IDCODE to ensure that the value remains
-    assert_eq!(openocd.execute("irscan riscv.tap 0x1")?, "");
-    let idcode = u32::from_str_radix(&openocd.execute("drscan riscv.tap 32 0")?, 16)?;
+    openocd.irscan("riscv.tap", 1)?;
+    let idcode = openocd.drscan("riscv.tap", 32, 0)?;
     assert_eq!(idcode, RISCV_IDCODE);
 
     // Check functionality of BYPASS
-    assert_eq!(openocd.execute("irscan riscv.tap 0")?, "");
-    assert_eq!(openocd.execute("drscan riscv.tap 1 0")?, "00");
-    let scan = u64::from_str_radix(&openocd.execute("drscan riscv.tap 33 0xdeadbeef")?, 16)?;
-    assert_eq!(scan, 0xdeadbeef << 1);
+    openocd.irscan("riscv.tap", 0)?;
+    assert_eq!(openocd.drscan("riscv.tap", 1, 0)?, 0);
+    let scan = openocd.drscan("riscv.tap", 33, 0xdeadbeef)?;
+    assert_eq!(scan, 0xdeadbeefu64 << 1);
 
     // Read DTMCS ensure value is as expected
-    assert_eq!(openocd.execute("irscan riscv.tap 0x10")?, "");
-    let value = u32::from_str_radix(&openocd.execute("drscan riscv.tap 32 0")?, 16)?;
+    openocd.irscan("riscv.tap", 0x10)?;
+    let value = openocd.drscan("riscv.tap", 32, 0)?;
     // DTMCS.version
     assert_eq!(value & 0xF, 1);
     // DTMCS.abits
     assert_eq!((value >> 4) & 0x3F, 7);
 
     // Write DTMCS and ensure it's unchanged
-    assert_eq!(openocd.execute("irscan riscv.tap 0x10")?, "");
+    openocd.irscan("riscv.tap", 0x10)?;
     assert_eq!(
-        openocd.execute("drscan riscv.tap 64 0xdeadbeef")?,
-        format!("deadbeef{:08x}", value)
+        openocd.drscan("riscv.tap", 64, 0xdeadbeef)?,
+        0xdeadbeef << 32 | value as u64
     );
-    assert_eq!(openocd.execute("irscan riscv.tap 0x10")?, "");
-    let new_value = u32::from_str_radix(&openocd.execute("drscan riscv.tap 32 0")?, 16)?;
+    openocd.irscan("riscv.tap", 0x10)?;
+    let new_value = openocd.drscan("riscv.tap", 32, 0)?;
     assert_eq!(new_value, value);
 
     openocd.shutdown()?;
