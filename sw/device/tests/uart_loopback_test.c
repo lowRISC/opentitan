357864129d4181086fe7207fbeabc76aa6502eb7
James Wainwright <james.wainwright@lowrisc.org>
2024-01-31 10:28:22 +0000

    [uart,sival] Add test for UART loopbacks
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
    (cherry picked from commit caf2001f08433996f9e18f9eef41360a272775dc)

---
+++ sw/device/tests/uart_loopback_test.c
@@ -0,0 +1,114 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/arch/device.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/dif/dif_uart.h"
+#include "sw/device/lib/runtime/hart.h"
+#include "sw/device/lib/testing/test_framework/check.h"
+#include "sw/device/lib/testing/test_framework/ottf_console.h"
+#include "sw/device/lib/testing/test_framework/ottf_main.h"
+#include "sw/device/lib/testing/test_framework/ottf_utils.h"
+#include "sw/device/lib/testing/uart_testutils.h"
+
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+
+static const uint8_t kSendData[] = "Loopback test!";
+static const uint32_t kBaseAddrs[4] = {
+    TOP_EARLGREY_UART0_BASE_ADDR,
+    TOP_EARLGREY_UART1_BASE_ADDR,
+    TOP_EARLGREY_UART2_BASE_ADDR,
+    TOP_EARLGREY_UART3_BASE_ADDR,
+};
+enum {
+  kTestTimeoutMicros = 1000000,
+};
+
+typedef enum test_phases {
+  kTestPhaseCfg,
+  kTestPhaseLine,
+  kTestPhaseSystem,
+  kTestPhaseDone,
+} test_phase_t;
+
+static volatile uint8_t uart_idx = 0xff;
+static volatile uint8_t test_phase = kTestPhaseCfg;
+
+static dif_uart_t uart;
+static dif_pinmux_t pinmux;
+
+OTTF_DEFINE_TEST_CONFIG(.enable_uart_flow_control = true);
+
+bool test_main(void) {
+  mmio_region_t base_addr;
+
+  // Wait for the testbench to set the `uart_idx`.
+  OTTF_WAIT_FOR(uart_idx != 0xff, kTestTimeoutMicros);
+
+  base_addr = mmio_region_from_addr(TOP_EARLGREY_PINMUX_AON_BASE_ADDR);
+  CHECK_DIF_OK(dif_pinmux_init(base_addr, &pinmux));
+
+  // If we're testing UART0 we need to move the console to UART1.
+  if (uart_idx == 0) {
+    CHECK_STATUS_OK(
+        uart_testutils_select_pinmux(&pinmux, 1, kUartPinmuxChannelConsole));
+    ottf_console_configure_uart(TOP_EARLGREY_UART1_BASE_ADDR);
+  }
+
+  // Attach the UART under test to the DUT channel.
+  CHECK_STATUS_OK(
+      uart_testutils_select_pinmux(&pinmux, uart_idx, kUartPinmuxChannelDut));
+
+  base_addr = mmio_region_from_addr(kBaseAddrs[uart_idx]);
+  CHECK_DIF_OK(dif_uart_init(base_addr, &uart));
+
+  CHECK_DIF_OK(dif_uart_configure(
+      &uart, (dif_uart_config_t){
+                 .baudrate = (uint32_t)kUartBaudrate,
+                 .clk_freq_hz = (uint32_t)kClockFreqPeripheralHz,
+                 .parity_enable = kDifToggleDisabled,
+                 .parity = kDifUartParityEven,
+                 .tx_enable = kDifToggleDisabled,
+                 .rx_enable = kDifToggleDisabled,
+             }));
+
+  LOG_INFO("UART%d configured", uart_idx);
+
+  // First, enable the line loopback which echoes whatever is received to it.
+  // Enable the TX line once it's enabled.
+  CHECK_DIF_OK(
+      dif_uart_loopback_set(&uart, kDifUartLoopbackLine, kDifToggleEnabled));
+  CHECK_DIF_OK(
+      dif_uart_set_enable(&uart, kDifUartDatapathTx, kDifToggleEnabled));
+
+  CHECK_DIF_OK(dif_uart_fifo_reset(&uart, kDifUartDatapathAll));
+
+  OTTF_WAIT_FOR(test_phase == kTestPhaseLine, kTestTimeoutMicros);
+  LOG_INFO("Testing the line loopback");
+
+  // Wait for the line test to be finished before configuring the UART for the
+  // system loopback.
+  OTTF_WAIT_FOR(test_phase == kTestPhaseSystem, kTestTimeoutMicros);
+  LOG_INFO("Testing the system loopback");
+
+  CHECK_DIF_OK(
+      dif_uart_loopback_set(&uart, kDifUartLoopbackSystem, kDifToggleEnabled));
+  CHECK_DIF_OK(
+      dif_uart_set_enable(&uart, kDifUartDatapathRx, kDifToggleEnabled));
+
+  CHECK_DIF_OK(dif_uart_fifo_reset(&uart, kDifUartDatapathAll));
+
+  for (int i = 0; i < sizeof(kSendData); ++i) {
+    CHECK_DIF_OK(dif_uart_byte_send_polled(&uart, kSendData[i]));
+
+    uint8_t receive_byte;
+    CHECK_DIF_OK(dif_uart_byte_receive_polled(&uart, &receive_byte));
+    CHECK(kSendData[i] == receive_byte, "expected %c, got %c", kSendData[i],
+          receive_byte);
+  }
+
+  test_phase = kTestPhaseDone;
+
+  return true;
+}
---
+++ sw/host/tests/chip/uart/src/uart_loopback.rs
@@ -0,0 +1,123 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::fs;
+use std::io::{BufReader, Read};
+use std::path::PathBuf;
+use std::time::Duration;
+
+use anyhow::{Context, Result};
+use clap::Parser;
+use object::read::File;
+
+use opentitanlib::app::TransportWrapper;
+use opentitanlib::execute_test;
+use opentitanlib::io::uart::Uart;
+use opentitanlib::test_utils;
+use opentitanlib::test_utils::init::InitializeTest;
+use opentitanlib::test_utils::mem::MemWriteReq;
+use opentitanlib::uart::console::UartConsole;
+
+#[derive(Debug, Parser)]
+struct Opts {
+    #[command(flatten)]
+    init: InitializeTest,
+
+    /// Console receive timeout.
+    #[arg(long, value_parser = humantime::parse_duration, default_value = "600s")]
+    timeout: Duration,
+
+    /// Path to the ELF file being tested on the device.
+    #[arg(long)]
+    firmware_elf: PathBuf,
+}
+
+struct TestData {
+    uart_idx: u8,
+    uart_idx_addr: u32,
+    test_phase_addr: u32,
+}
+
+#[repr(u8)]
+enum TestPhase {
+    _Cfg,
+    Line,
+    System,
+    _Done,
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    opts.init.init_logging();
+
+    let elf_file = fs::read(&opts.firmware_elf).context("failed to read ELF")?;
+    let object: File = File::parse(elf_file.as_ref()).context("failed to parse ELF")?;
+
+    let uart_idx_addr = test_utils::object::symbol_addr(&object, "uart_idx")?;
+    let test_phase_addr = test_utils::object::symbol_addr(&object, "test_phase")?;
+
+    let transport = opts.init.init_target()?;
+    let uart_console = transport.uart("console")?;
+
+    for uart_idx in 0..4 {
+        transport.reset_target(Duration::from_millis(500), true)?;
+
+        let test_data = TestData {
+            uart_idx,
+            uart_idx_addr,
+            test_phase_addr,
+        };
+
+        execute_test!(uart_loopback, &opts, &transport, &*uart_console, &test_data);
+    }
+
+    Ok(())
+}
+
+/// Send and receive data with a device's UART.
+fn uart_loopback(
+    opts: &Opts,
+    transport: &TransportWrapper,
+    console: &dyn Uart,
+    test_data: &TestData,
+) -> Result<()> {
+    const TEST_DATA: [u8; 14] = *b"Loopback test!";
+
+    let uart = transport.uart("dut")?;
+    std::thread::sleep(Duration::from_millis(100));
+    uart.clear_rx_buffer()?;
+
+    let TestData {
+        uart_idx,
+        uart_idx_addr,
+        test_phase_addr,
+    } = test_data;
+
+    UartConsole::wait_for(console, r"waiting for commands", opts.timeout)?;
+    MemWriteReq::execute(console, *uart_idx_addr, &[*uart_idx])?;
+
+    UartConsole::wait_for(console, r"waiting for commands", opts.timeout)?;
+    MemWriteReq::execute(console, *test_phase_addr, &[TestPhase::Line as u8])?;
+    log::info!("Starting line loopback test");
+
+    log::info!("Sending data...");
+    uart.write(&TEST_DATA).context("failed to send data")?;
+
+    log::info!("Reading data...");
+    let mut received_data = [0u8; TEST_DATA.len()];
+    let mut buf_reader = BufReader::new(&*uart);
+    buf_reader
+        .read_exact(&mut received_data)
+        .context("failed to read data")?;
+
+    assert_eq!(received_data, TEST_DATA);
+
+    UartConsole::wait_for(console, r"waiting for commands", opts.timeout)?;
+    MemWriteReq::execute(console, *test_phase_addr, &[TestPhase::System as u8])?;
+    log::info!("Starting system loopback test");
+
+    UartConsole::wait_for(console, r"PASS![^\r\n]*", opts.timeout)?;
+
+    Ok(())
+}
