4b736ab5d455b2016f144a93257015020afb9232
Ryan Torok <rtorok@opentitan.org>
2024-02-01 11:38:38 -0500

    [cryptotest] NIST HMAC test vector parser
    Signed-off-by: Ryan Torok <rtorok@opentitan.org>

---
+++ sw/host/cryptotest/testvectors/parsers/nist_cavp_hmac_parser.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+"""Parser for converting NIST CAVP Digital Signatures test vectors to JSON.
+
+"""
+
+import argparse
+import sys
+import json
+import jsonschema
+
+from cryptotest_util import parse_rsp
+
+# NIST vectors allow differently-sized MACs for each hash function
+# (https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Algorithm-Validation-Program/documents/mac/HMACVS.pdf).
+# However, the test vectors only include the longest length for each
+# hash (i.e. the entire hash is the MAC).
+LENGTH_TO_HASH = {
+    "20": "sha1",
+    "28": "sha224",
+    "32": "sha256",
+    "48": "sha384",
+    "64": "sha512",
+}
+
+
+def parse_testcases(args) -> None:
+    raw_testcases = parse_rsp(args.src)
+    test_cases = list()
+
+    # NIST splits the rsp files into sections based on tag length
+    # (which implies which hash function should be used)
+    for section_name in raw_testcases.keys():
+        _, mac_len = section_name.split("=")
+        hash_alg = LENGTH_TO_HASH[mac_len]
+        for test_vec in raw_testcases[section_name]:
+            test_case = {
+                "vendor": "nist",
+                "test_case_id": test_vec["Count"],
+                "algorithm": "hmac",
+                "hash_alg": hash_alg,
+                "key": list(bytes.fromhex(test_vec["Key"])),
+                "message": list(bytes.fromhex(test_vec["Msg"])),
+                "tag": list(bytes.fromhex(test_vec["Mac"])),
+                # All NIST HMAC vectors are expected to pass
+                "result": True,
+            }
+            test_cases.append(test_case)
+
+    json_filename = f"{args.dst}.json"
+    with open(json_filename, "w") as file:
+        json.dump(test_cases, file, indent=4)
+
+    # Validate generated JSON
+    with open(args.schema) as schema_file:
+        schema = json.load(schema_file)
+    jsonschema.validate(test_cases, schema)
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(
+        description="Parsing utility for NIST CAVP Digital Signatures test vectors.")
+
+    parser.add_argument(
+        "--src",
+        help="Source file to import."
+    )
+    parser.add_argument(
+        "--dst",
+        help="Destination of the output file."
+    )
+    parser.add_argument(
+        "--schema",
+        type = str,
+        help = "Test vector schema file"
+    )
+    args = parser.parse_args()
+    parse_testcases(args)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
