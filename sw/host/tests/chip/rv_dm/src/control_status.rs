c0ab4977e5bb3f4cece0603427cf2172d3506189
Gary Guo <gary.guo@lowrisc.org>
2024-02-01 15:33:23 +0000

    [sival, rv_dm] implement rv_dm_control_status test
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>
    (cherry picked from commit 7f1b862aa6ba7b6a903d83d662a428ae0166e471)

---
+++ sw/host/tests/chip/rv_dm/src/control_status.rs
@@ -0,0 +1,80 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::Result;
+use clap::Parser;
+
+use opentitanlib::app::TransportWrapper;
+use opentitanlib::debug::dmi::{consts, Dmi, DmiDebugger, OpenOcdDmi};
+use opentitanlib::execute_test;
+use opentitanlib::test_utils::init::InitializeTest;
+
+#[derive(Debug, Parser)]
+struct Opts {
+    #[command(flatten)]
+    init: InitializeTest,
+}
+
+// Needs to match util/openocd/target
+const RISCV_IDCODE: u32 = 0x10001cdf;
+
+fn test_control_status(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    transport.pin_strapping("PINMUX_TAP_RISCV")?.apply()?;
+    transport.reset_target(opts.init.bootstrap.options.reset_delay, true)?;
+
+    let mut openocd = opts.init.jtag_params.create(transport)?.into_raw()?;
+
+    // Configure OpenOCD to expect RISC-V tap and initialize JTAG.
+    assert_eq!(
+        openocd.execute(&format!(
+            "jtag newtap riscv tap -irlen 5 -expected-id {RISCV_IDCODE:#x}"
+        ))?,
+        ""
+    );
+    assert_eq!(openocd.execute("init")?, "");
+
+    let mut dmi = DmiDebugger::new(OpenOcdDmi::new(openocd, "riscv.tap")?);
+
+    // Check dmstatus indicates havereset for Ibex (power-on reset) and set ackhavereset to clear it.
+    let mut hart = dmi.select_hart(0)?;
+    let dmstatus = hart.dmstatus()?;
+    assert!(dmstatus & consts::DMSTATUS_ANYHAVERESET_MASK != 0);
+    hart.set_dmcontrol(consts::DMCONTROL_ACKHAVERESET_MASK)?;
+
+    // Write all 1s to hartsel and confirm readback.
+    let dmcontrol = 0x3ff << consts::DMCONTROL_HARTSELLO_SHIFT
+        | 0x3ff << consts::DMCONTROL_HARTSELHI_SHIFT
+        | consts::DMCONTROL_DMACTIVE_MASK;
+    dmi.dmi_write(consts::DMCONTROL, dmcontrol)?;
+    assert_eq!(dmi.dmi_read(consts::DMCONTROL)?, dmcontrol);
+
+    // Write 0 to hartsel and confirm readback.
+    let dmcontrol = 0 << consts::DMCONTROL_HARTSELLO_SHIFT
+        | 0 << consts::DMCONTROL_HARTSELHI_SHIFT
+        | consts::DMCONTROL_DMACTIVE_MASK;
+    dmi.dmi_write(consts::DMCONTROL, dmcontrol)?;
+    assert_eq!(dmi.dmi_read(consts::DMCONTROL)?, dmcontrol);
+
+    let mut hart = dmi.select_hart(0)?;
+    assert!(hart.state()?.running);
+
+    hart.set_halt_request(true)?;
+    hart.wait_halt()?;
+    hart.set_halt_request(false)?;
+
+    hart.set_resume_request(true)?;
+    hart.wait_resume()?;
+    hart.set_resume_request(false)?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    opts.init.init_logging();
+    let transport = opts.init.init_target()?;
+
+    execute_test!(test_control_status, &opts, &transport);
+
+    Ok(())
+}
