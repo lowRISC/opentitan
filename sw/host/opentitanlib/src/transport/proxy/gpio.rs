b6418ee3cb2624689f6ea6d5c411adb871835791
Jes B. Klinke <jbk@chromium.org>
2024-02-05 14:48:51 -0800

    [opentitantool] Proxy support for GPIO bitbanging
    Add proxy client and server side handling of the `GpioBitbanging` trait.
    
    Change-Id: Ib7fa3bedb65a9ab0e200563b9d29cd7a1767bb1e
    Signed-off-by: Jes B. Klinke <jbk@chromium.org>

--- sw/host/opentitanlib/src/transport/proxy/gpio.rs
+++ sw/host/opentitanlib/src/transport/proxy/gpio.rs
@@ -2,16 +2,18 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::{bail, Result};
+use anyhow::{bail, ensure, Result};
 use std::rc::Rc;
+use std::time::Duration;
 
 use super::ProxyError;
 use crate::io::gpio::{
-    ClockNature, GpioMonitoring, GpioPin, MonitoringReadResponse, MonitoringStartResponse, PinMode,
-    PullMode,
+    BitbangEntry, ClockNature, GpioBitbanging, GpioError, GpioMonitoring, GpioPin,
+    MonitoringReadResponse, MonitoringStartResponse, PinMode, PullMode,
 };
 use crate::proxy::protocol::{
-    GpioMonRequest, GpioMonResponse, GpioRequest, GpioResponse, Request, Response,
+    BitbangEntryRequest, BitbangEntryResponse, GpioBitRequest, GpioBitResponse, GpioMonRequest,
+    GpioMonResponse, GpioRequest, GpioResponse, Request, Response,
 };
 use crate::transport::proxy::{Inner, Proxy};
 
@@ -152,6 +154,89 @@ impl GpioMonitoring for GpioMonitoringImpl {
         })? {
             GpioMonResponse::Read { resp } => Ok(resp),
             _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+}
+
+pub struct GpioBitbangingImpl {
+    inner: Rc<Inner>,
+}
+
+impl GpioBitbangingImpl {
+    pub fn new(proxy: &Proxy) -> Result<Self> {
+        Ok(Self {
+            inner: Rc::clone(&proxy.inner),
+        })
+    }
+
+    // Convenience method for issuing GPIO bitbanging commands via proxy protocol.
+    fn execute_command(&self, command: GpioBitRequest) -> Result<GpioBitResponse> {
+        match self
+            .inner
+            .execute_command(Request::GpioBitbanging { command })?
+        {
+            Response::GpioBitbanging(resp) => Ok(resp),
+            _ => bail!(ProxyError::UnexpectedReply()),
+        }
+    }
+}
+
+impl GpioBitbanging for GpioBitbangingImpl {
+    fn run(
+        &self,
+        pins: &[&dyn GpioPin],
+        clock_tick: Duration,
+        waveform: &mut [BitbangEntry],
+    ) -> Result<()> {
+        let pins = pins
+            .iter()
+            .map(|p| p.get_internal_pin_name().unwrap().to_string())
+            .collect::<Vec<String>>();
+
+        let mut req: Vec<BitbangEntryRequest> = Vec::new();
+        for entry in waveform.iter() {
+            match entry {
+                BitbangEntry::Write(wbuf) => req.push(BitbangEntryRequest::Write {
+                    data: wbuf.to_vec(),
+                }),
+                BitbangEntry::Both(wbuf, rbuf) => {
+                    ensure!(
+                        rbuf.len() == wbuf.len(),
+                        GpioError::MismatchedDataLength(wbuf.len(), rbuf.len())
+                    );
+                    req.push(BitbangEntryRequest::Both {
+                        data: wbuf.to_vec(),
+                    })
+                }
+                BitbangEntry::Delay(ticks) => req.push(BitbangEntryRequest::Delay {
+                    clock_ticks: *ticks,
+                }),
+            }
+        }
+        match self.execute_command(GpioBitRequest::Run {
+            pins,
+            clock_ns: clock_tick.as_nanos() as u64,
+            entries: req,
+        })? {
+            GpioBitResponse::Run { entries: resp } => {
+                ensure!(
+                    resp.len() == waveform.len(),
+                    ProxyError::UnexpectedReply()
+                );
+                for pair in resp.iter().zip(waveform.iter_mut()) {
+                    match pair {
+                        (BitbangEntryResponse::Both { data }, BitbangEntry::Both(_, rbuf)) => {
+                            rbuf.clone_from_slice(data);
+                        }
+                        (BitbangEntryResponse::Write, BitbangEntry::Write(_)) => (),
+                        (BitbangEntryResponse::Delay, BitbangEntry::Delay(_)) => (),
+                        _ => bail!(ProxyError::UnexpectedReply()),
+                    }
+                }
+                Ok(())
+            }
+            // Insert the below line, when a second kind of bitbanging request is added:
+            // _ => bail!(ProxyError::UnexpectedReply()),
         }
     }
 }
