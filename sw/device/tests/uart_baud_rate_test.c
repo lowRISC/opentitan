a3053c97140da8a98bf97d0f13959e242b53bf08
James Wainwright <james.wainwright@lowrisc.org>
2024-02-05 10:52:55 +0000

    [sival,uart] Add test for UART baud rates
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

---
+++ sw/device/tests/uart_baud_rate_test.c
@@ -0,0 +1,125 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdint.h>
+
+#include "sw/device/lib/arch/device.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/dif/dif_pinmux.h"
+#include "sw/device/lib/dif/dif_uart.h"
+#include "sw/device/lib/runtime/hart.h"
+#include "sw/device/lib/testing/test_framework/check.h"
+#include "sw/device/lib/testing/test_framework/ottf_console.h"
+#include "sw/device/lib/testing/test_framework/ottf_main.h"
+#include "sw/device/lib/testing/test_framework/ottf_utils.h"
+#include "sw/device/lib/testing/uart_testutils.h"
+
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+
+static const uint8_t kSendData[] = "UART baud test!";
+static const uint32_t kBaseAddrs[4] = {
+    TOP_EARLGREY_UART0_BASE_ADDR,
+    TOP_EARLGREY_UART1_BASE_ADDR,
+    TOP_EARLGREY_UART2_BASE_ADDR,
+    TOP_EARLGREY_UART3_BASE_ADDR,
+};
+static uint32_t kBauds[7] = {
+    9600, 115200, 230400, 128000, 256000, 1000000, 1500000,
+};
+
+enum {
+  kTestTimeoutMillis = 500000,
+  // Number of Bauds to test from `kBauds`.
+  kBaudCountSilicon = 7,
+  // The two highest Bauds won't work at the clock speed we run the FPGAs at.
+  kBaudCountFpga = 5,
+};
+
+typedef enum test_phase {
+  kTestPhaseInit,
+  kTestPhaseCfg,
+  kTestPhaseSend,
+  kTestPhaseRecv,
+  kTestPhaseDone,
+} test_phase_t;
+
+static volatile uint8_t test_phase = kTestPhaseInit;
+static volatile uint8_t uart_idx = UINT8_MAX;
+static volatile uint32_t baud_rate = UINT32_MAX;
+
+static dif_uart_t uart;
+static dif_pinmux_t pinmux;
+
+OTTF_DEFINE_TEST_CONFIG(.enable_uart_flow_control = true);
+
+// Send all bytes in `kSendData`, and check that they are received via the
+// loopback mechanism.
+static status_t test_uart_baud(void) {
+  test_phase = kTestPhaseCfg;
+
+  CHECK_DIF_OK(dif_uart_configure(
+      &uart, (dif_uart_config_t){
+                 .baudrate = (uint32_t)baud_rate,
+                 .clk_freq_hz = (uint32_t)kClockFreqPeripheralHz,
+                 .parity_enable = kDifToggleDisabled,
+                 .parity = kDifUartParityEven,
+                 .tx_enable = kDifToggleEnabled,
+                 .rx_enable = kDifToggleEnabled,
+             }));
+
+  CHECK_DIF_OK(dif_uart_fifo_reset(&uart, kDifUartDatapathAll));
+
+  LOG_INFO("Configured UART%d with Baud rate %d", uart_idx, baud_rate);
+
+  OTTF_WAIT_FOR(test_phase == kTestPhaseSend, kTestTimeoutMillis);
+
+  LOG_INFO("Sending data...");
+  CHECK_DIF_OK(dif_uart_bytes_send(&uart, kSendData, sizeof(kSendData), NULL));
+  LOG_INFO("Data sent");
+
+  OTTF_WAIT_FOR(test_phase == kTestPhaseRecv, kTestTimeoutMillis);
+
+  LOG_INFO("Receiving data...");
+  uint8_t data[sizeof(kSendData)] = {0};
+  for (size_t i = 0; i < sizeof(data); ++i) {
+    CHECK_DIF_OK(dif_uart_byte_receive_polled(&uart, &data[i]));
+  }
+  CHECK_ARRAYS_EQ(data, kSendData, sizeof(kSendData));
+
+  test_phase = kTestPhaseDone;
+
+  return OK_STATUS();
+}
+
+bool test_main(void) {
+  OTTF_WAIT_FOR(uart_idx != 0xff, kTestTimeoutMillis);
+
+  mmio_region_t base_addr;
+
+  base_addr = mmio_region_from_addr(TOP_EARLGREY_PINMUX_AON_BASE_ADDR);
+  CHECK_DIF_OK(dif_pinmux_init(base_addr, &pinmux));
+
+  base_addr = mmio_region_from_addr(kBaseAddrs[uart_idx]);
+  CHECK_DIF_OK(dif_uart_init(base_addr, &uart));
+
+  if (uart_idx == 0) {
+    CHECK_STATUS_OK(
+        uart_testutils_select_pinmux(&pinmux, 1, kUartPinmuxChannelConsole));
+    ottf_console_configure_uart(TOP_EARLGREY_UART1_BASE_ADDR);
+  }
+
+  CHECK_STATUS_OK(
+      uart_testutils_select_pinmux(&pinmux, uart_idx, kUartPinmuxChannelDut));
+
+  size_t baud_count =
+      kDeviceType == kDeviceSilicon ? kBaudCountSilicon : kBaudCountFpga;
+
+  // Check every baud rate is sent and received okay.
+  status_t result = OK_STATUS();
+  for (size_t baud_idx = 0; baud_idx < baud_count; ++baud_idx) {
+    baud_rate = kBauds[baud_idx];
+    EXECUTE_TEST(result, test_uart_baud);
+  }
+  return status_ok(result);
+}
---
+++ sw/host/tests/chip/uart/src/uart_baud_rate.rs
@@ -0,0 +1,151 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::fs;
+use std::io::{BufReader, Read};
+use std::path::PathBuf;
+use std::time::Duration;
+
+use anyhow::{Context, Result};
+use clap::Parser;
+
+use opentitanlib::app::TransportWrapper;
+use opentitanlib::execute_test;
+use opentitanlib::io::uart::Uart;
+use opentitanlib::test_utils;
+use opentitanlib::test_utils::init::InitializeTest;
+use opentitanlib::test_utils::mem::{MemRead32Req, MemWriteReq};
+use opentitanlib::uart::console::UartConsole;
+
+#[derive(Debug, Parser)]
+struct Opts {
+    #[command(flatten)]
+    init: InitializeTest,
+
+    /// Console receive timeout.
+    #[arg(long, value_parser = humantime::parse_duration, default_value = "600s")]
+    timeout: Duration,
+
+    /// Path to the ELF file being tested on the device.
+    #[arg(long)]
+    firmware_elf: PathBuf,
+}
+
+struct TestData {
+    expected_data: Vec<u8>,
+    uart_idx: u8,
+    uart_idx_addr: u32,
+    baud_rate_addr: u32,
+    test_phase_addr: u32,
+}
+
+#[repr(u8)]
+enum TestPhase {
+    _Init,
+    _Cfg,
+    Send,
+    Recv,
+    _Done,
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    opts.init.init_logging();
+
+    let elf_file = fs::read(&opts.firmware_elf).context("failed to read ELF")?;
+    let object = object::File::parse(elf_file.as_ref()).context("failed to parse ELF")?;
+
+    let expected_data = test_utils::object::symbol_data(&object, "kSendData")?;
+
+    let uart_idx_addr = test_utils::object::symbol_addr(&object, "uart_idx")?;
+    let baud_rate_addr = test_utils::object::symbol_addr(&object, "baud_rate")?;
+    let test_phase_addr = test_utils::object::symbol_addr(&object, "test_phase")?;
+
+    let transport = opts.init.init_target()?;
+    let uart_console = transport.uart("console")?;
+
+    for uart_idx in 0..4 {
+        transport.reset_target(Duration::from_millis(500), true)?;
+
+        let test_data = TestData {
+            expected_data: expected_data.clone(),
+            uart_idx,
+            uart_idx_addr,
+            baud_rate_addr,
+            test_phase_addr,
+        };
+
+        execute_test!(
+            uart_baud_rate,
+            &opts,
+            &transport,
+            &*uart_console,
+            &test_data
+        );
+    }
+
+    Ok(())
+}
+
+/// Send and receive data with a device's UART.
+fn uart_baud_rate(
+    opts: &Opts,
+    transport: &TransportWrapper,
+    console: &dyn Uart,
+    test_data: &TestData,
+) -> Result<()> {
+    let TestData {
+        expected_data,
+        uart_idx,
+        uart_idx_addr,
+        baud_rate_addr,
+        test_phase_addr,
+    } = test_data;
+
+    let uart = transport.uart("dut")?;
+
+    UartConsole::wait_for(console, r"waiting for commands", opts.timeout)?;
+    MemWriteReq::execute(console, *uart_idx_addr, &[*uart_idx])?;
+
+    // Keep repeating the test for various Bauds until the device tells us it's
+    // `PASS`ed.
+    loop {
+        let msg = UartConsole::wait_for(
+            console,
+            r"PASS![^\r\n]*|Starting test[^\n]*\n",
+            opts.timeout,
+        )?;
+
+        if msg[0].as_str().contains("PASS") {
+            break;
+        }
+
+        // Read the configured baud rate and configure our side of the UART.
+        let baud_rate = MemRead32Req::execute(console, *baud_rate_addr)?;
+        uart.set_baudrate(baud_rate)?;
+        uart.clear_rx_buffer()?;
+
+        // Ask the device to send us some data.
+        MemWriteReq::execute(console, *test_phase_addr, &[TestPhase::Send as u8])?;
+        UartConsole::wait_for(console, r"Data sent[^\n]*\n", opts.timeout)?;
+
+        log::info!("Reading data...");
+
+        let mut data = vec![0u8; expected_data.len()];
+        let mut buf_reader = BufReader::new(&*uart);
+        buf_reader
+            .read_exact(&mut data)
+            .context("failed to read data")?;
+
+        assert_eq!(data, *expected_data);
+
+        // Ask the device to receive the data we send.
+        MemWriteReq::execute(console, *test_phase_addr, &[TestPhase::Recv as u8])?;
+
+        log::info!("Sending data...");
+        uart.write(expected_data).context("failed to send data")?;
+    }
+
+    Ok(())
+}
