1f3b922363ebe5cfaff5c68f1a14d914b9fcefb9
Amaury Pouly <amaury.pouly@lowrisc.org>
2024-02-06 15:40:06 +0000

    [sysrst_ctrl,sival] Rework testutils
    Previously, a unique function did three things: setup DIO,
    release the pins and set the pulse width to zero. Separete those
    functions into three distinct functions. The existing tests are
    correspondingly updated.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>
    (cherry picked from commit 532273292c5b36f5d7a9313cb9b967fb30274e46)

--- sw/device/lib/testing/sysrst_ctrl_testutils.c
+++ sw/device/lib/testing/sysrst_ctrl_testutils.c
@@ -4,13 +4,14 @@
 
 #include "sw/device/lib/testing/sysrst_ctrl_testutils.h"
 
+#include "sw/device/lib/base/math.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/test_framework/check.h"
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
-void setup_dio_pins(dif_pinmux_t *pinmux, dif_sysrst_ctrl_t *sysrst_ctrl) {
-  // Make sure the the DIO pins EC reset and flash WP are configured correctly:
+void sysrst_ctrl_testutils_setup_dio(dif_pinmux_t *pinmux) {
+  // Make sure that the DIO pins EC reset and flash WP are configured correctly
   // https://opentitan.org/book/hw/ip/sysrst_ctrl/doc/theory_of_operation.html#ec-and-power-on-reset
   // https://opentitan.org/book/hw/ip/sysrst_ctrl/doc/theory_of_operation.html#flash-write-protect-output
   //
@@ -39,30 +40,50 @@ void setup_dio_pins(dif_pinmux_t *pinmux, dif_sysrst_ctrl_t *sysrst_ctrl) {
   CHECK_DIF_OK(dif_pinmux_pad_write_attrs(
       pinmux, kTopEarlgreyDirectPadsSysrstCtrlAonFlashWpL, kDifPinmuxPadKindDio,
       in_attr, &out_attr));
+}
+
+void sysrst_ctrl_testutils_release_dio(dif_sysrst_ctrl_t *sysrst_ctrl,
+                                       bool release_ec, bool release_flash) {
+  // Make sure that the DIO pins EC reset and flash WP are released according to
+  // the documentation:
+  // https://opentitan.org/book/hw/ip/sysrst_ctrl/doc/theory_of_operation.html#ec-and-power-on-reset
+  // https://opentitan.org/book/hw/ip/sysrst_ctrl/doc/theory_of_operation.html#flash-write-protect-output
   // We also need to disable the output override mecanism (i.e. "release the
   // pin").
-  dif_sysrst_ctrl_pin_config_t cfg_ec_reset = {
-      .enabled = kDifToggleDisabled,
-      .allow_zero = true,
-      .allow_one = true,
-      .override_value = false,
-  };
-  CHECK_DIF_OK(dif_sysrst_ctrl_output_pin_override_configure(
-      sysrst_ctrl, kDifSysrstCtrlPinEcResetInOut, cfg_ec_reset));
+  if (release_ec) {
+    dif_sysrst_ctrl_pin_config_t cfg_ec_reset = {
+        .enabled = kDifToggleDisabled,
+        .allow_zero = true,
+        .allow_one = true,
+        .override_value = false,
+    };
+    CHECK_DIF_OK(dif_sysrst_ctrl_output_pin_override_configure(
+        sysrst_ctrl, kDifSysrstCtrlPinEcResetInOut, cfg_ec_reset));
+  }
   // Confusingly, the flash WP is different: the value of flash_wp_l_o defaults
   // to logic 0 when it is not explicitly driven via the override function.
   // Therefore to disable the driver we need to *enable* the override and set
   // to 1.
-  dif_sysrst_ctrl_pin_config_t cfg_flash_wp = {
-      .enabled = kDifToggleEnabled,
-      .allow_zero = false,
-      .allow_one = true,
-      .override_value = true,
-  };
-  CHECK_DIF_OK(dif_sysrst_ctrl_output_pin_override_configure(
-      sysrst_ctrl, kDifSysrstCtrlPinFlashWriteProtectInOut, cfg_flash_wp));
-  // Disable the EC reset debounce timer because it could interfere with
-  // reading the pins.
+  if (release_flash) {
+    dif_sysrst_ctrl_pin_config_t cfg_flash_wp = {
+        .enabled = kDifToggleEnabled,
+        .allow_zero = false,
+        .allow_one = true,
+        .override_value = true,
+    };
+    CHECK_DIF_OK(dif_sysrst_ctrl_output_pin_override_configure(
+        sysrst_ctrl, kDifSysrstCtrlPinFlashWriteProtectInOut, cfg_flash_wp));
+  }
+}
+
+void sysrst_ctrl_testutils_set_ec_rst_pulse_width(
+    dif_sysrst_ctrl_t *sysrst_ctrl, uint32_t pulse_us) {
+  // The register field counts in aon_clock ticks.
+  uint64_t ticks =
+      udiv64_slow((uint64_t)pulse_us * kClockFreqAonHz, 1000 * 1000, NULL);
+  // The register field is 16-bit wide.
+  OT_ASSERT_ENUM_VALUE(SYSRST_CTRL_EC_RST_CTL_EC_RST_PULSE_OFFSET, 0);
+  CHECK(ticks <= SYSRST_CTRL_EC_RST_CTL_EC_RST_PULSE_MASK);
   mmio_region_write32(sysrst_ctrl->base_addr, SYSRST_CTRL_EC_RST_CTL_REG_OFFSET,
-                      0);
+                      (uint16_t)ticks);
 }
--- sw/device/lib/testing/sysrst_ctrl_testutils.h
+++ sw/device/lib/testing/sysrst_ctrl_testutils.h
@@ -12,8 +12,7 @@
 
 /**
  * Setup the sysrst_ctrl direct IO (DIO) pins, that is flash write protect (WP)
- * and EC reset. Also disable the EC reset debounce timer and release the flash
- * WP so that the output is not overidden to low by the block.
+ * and EC reset.
  *
  * This function will try to setup the DIO pins as open drain. If this fails,
  * it will then try virtual open drain.
@@ -21,8 +20,34 @@
  * This function will panic on error.
  *
  * @param pinmux Pointer to an initialized pinmux DIF.
- * @param pinmux Pointer to an initialized sysrst_ctrl DIF.
  */
-void setup_dio_pins(dif_pinmux_t *pinmux, dif_sysrst_ctrl_t *sysrst_ctrl);
+void sysrst_ctrl_testutils_setup_dio(dif_pinmux_t *pinmux);
+
+/**
+ * Release the flash WP and EC reset so that the output is not overidden to low
+ * by the block.
+ *
+ * This function will panic on error.
+ *
+ * @param pinmux Pointer to an initialized sysrst_ctrl DIF.
+ * @param release_ec Whether to release the EC reset pin.
+ * @param release_flash Whether to release the flast write protect pin.
+ */
+void sysrst_ctrl_testutils_release_dio(dif_sysrst_ctrl_t *sysrst_ctrl,
+                                       bool release_ec, bool release_flash);
+
+/**
+ * Set the EC reset pulse width.
+ *
+ * The supplied pulse width is converted into a count of AON clock ticks.
+ * If the width is not an exact multiple of the AON clock period, the conversion
+ * rounds down. The function will panic if the converted pulse width does not
+ * fit into the RST_CTL register.
+ *
+ * @param pinmux Pointer to an initialized sysrst_ctrl DIF.
+ * @param pulse_us Pulse width in microseconds.
+ */
+void sysrst_ctrl_testutils_set_ec_rst_pulse_width(
+    dif_sysrst_ctrl_t *sysrst_ctrl, uint32_t pulse_us);
 
 #endif  // OPENTITAN_SW_DEVICE_LIB_TESTING_SYSRST_CTRL_TESTUTILS_H_
