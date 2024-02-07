b9f4e724adf9f588fa249df547b36607878cc63c
Chris Frantz <cfrantz@google.com>
2024-02-07 10:53:08 -0800

    [rom_ext, rescue] Add a firmware Xmodem implementation
    1. Create an Xmodem-CRC implementation for firmware.
    2. Bindgen the C implementation to rust for testing.
    3. Test the C implementation against the classic Xmodem implementation
       supplied by the `lrzsz` programs.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>
    (cherry picked from commit 791b521bf9b779040c82a9b89da8f55897815c10)

---
+++ sw/host/tests/xmodem/lrzsz_test.rs
@@ -0,0 +1,169 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// These test cases are nearly identical to those in
+// `//sw/host/opentitanlib/src/bootstrap/xmodem.rs`.  In fact, the test cases
+// check the same conditions as those in the aforementioned module.  The
+// differencees are:
+// - This module tests against the C implementation of xmodem used in firmware.
+// - The error codes herein refer to the errors returned by the C implementation.
+//
+// The xmodem tests depend on the lrzsz package which contains the classic
+// XMODEM/YMODEM/ZMODEM file transfer programs dating back to the 1980s and
+// 1990s.
+#[cfg(test)]
+mod test {
+    //use opentitanlib::io::uart::Uart;
+    use anyhow::Result;
+    //use std::io::{Read, Write};
+    use opentitanlib::util::testing::{ChildUart, TransferState};
+    use opentitanlib::util::tmpfilename;
+    use xmodem::XmodemFirmware;
+
+    #[rustfmt::skip]
+    const GETTYSBURG: &str =
+r#"Four score and seven years ago our fathers brought forth on this
+continent, a new nation, conceived in Liberty, and dedicated to the
+proposition that all men are created equal.
+Now we are engaged in a great civil war, testing whether that nation,
+or any nation so conceived and so dedicated, can long endure. We are met
+on a great battle-field of that war. We have come to dedicate a portion
+of that field, as a final resting place for those who here gave their
+lives that that nation might live. It is altogether fitting and proper
+that we should do this.
+But, in a larger sense, we can not dedicate -- we can not consecrate --
+we can not hallow -- this ground. The brave men, living and dead, who
+struggled here, have consecrated it, far above our poor power to add or
+detract. The world will little note, nor long remember what we say here,
+but it can never forget what they did here. It is for us the living,
+rather, to be dedicated here to the unfinished work which they who
+fought here have thus far so nobly advanced. It is rather for us to be
+here dedicated to the great task remaining before us -- that from these
+honored dead we take increased devotion to that cause for which they gave
+the last full measure of devotion -- that we here highly resolve that
+these dead shall not have died in vain -- that this nation, under God,
+shall have a new birth of freedom -- and that government of the people,
+by the people, for the people, shall not perish from the earth.
+Abraham Lincoln
+November 19, 1863
+"#;
+
+    #[test]
+    fn test_xmodem_send() -> Result<()> {
+        let _ = TransferState::default();
+        let filename = tmpfilename("test_xmodem_send");
+        let child = ChildUart::spawn(&["rx", "--with-crc", &filename])?;
+        let xmodem = XmodemFirmware::new();
+        let gettysburg = GETTYSBURG.as_bytes();
+        xmodem.send(&child, gettysburg)?;
+        assert!(child.wait()?.success());
+        let result = std::fs::read(&filename)?;
+        // The file should be a multiple of the block size.
+        assert_eq!(result.len() % 128, 0);
+        assert!(result.len() >= gettysburg.len());
+        assert_eq!(&result[..gettysburg.len()], gettysburg);
+        Ok(())
+    }
+
+    #[test]
+    fn test_xmodem_send_with_errors() -> Result<()> {
+        let filename = tmpfilename("test_xmodem_send_with_errors");
+        let child = ChildUart::spawn_corrupt(
+            &["rx", "--with-crc", &filename],
+            TransferState::default(),
+            TransferState::new(&[3, 1032]),
+        )?;
+        let xmodem = XmodemFirmware::new();
+        let gettysburg = GETTYSBURG.as_bytes();
+        let err = xmodem.send(&child, gettysburg);
+        assert!(err.is_err());
+        assert_eq!(err.unwrap_err().to_string(), "TooManyErrors");
+        Ok(())
+    }
+
+    #[test]
+    fn test_xmodem_checksum_mode() -> Result<()> {
+        let filename = tmpfilename("test_xmodem_checksum_mode");
+        let child = ChildUart::spawn(&["rx", &filename])?;
+        let xmodem = XmodemFirmware::new();
+        let gettysburg = GETTYSBURG.as_bytes();
+        let result = xmodem.send(&child, gettysburg);
+        assert_eq!(child.wait()?.success(), false);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().to_string(), "Protocol");
+        Ok(())
+    }
+
+    #[test]
+    fn test_xmodem_recv() -> Result<()> {
+        let filename = tmpfilename("test_xmodem_recv");
+        let gettysburg = GETTYSBURG.as_bytes();
+        std::fs::write(&filename, gettysburg)?;
+        let child = ChildUart::spawn(&["sx", &filename])?;
+        let xmodem = XmodemFirmware::new();
+        let mut result = Vec::new();
+        xmodem.receive(&child, &mut result)?;
+        assert!(child.wait()?.success());
+        // The received data should be a multiple of the block size.
+        assert_eq!(result.len() % 128, 0);
+        assert!(result.len() >= gettysburg.len());
+        assert_eq!(&result[..gettysburg.len()], gettysburg);
+        Ok(())
+    }
+
+    #[test]
+    fn test_xmodem1k_recv() -> Result<()> {
+        let filename = tmpfilename("test_xmodem1k_recv");
+        let gettysburg = GETTYSBURG.as_bytes();
+        std::fs::write(&filename, gettysburg)?;
+        let child = ChildUart::spawn(&["sx", "--1k", &filename])?;
+        let xmodem = XmodemFirmware::new();
+        let mut result = Vec::new();
+        xmodem.receive(&child, &mut result)?;
+        assert!(child.wait()?.success());
+        // The received data should be a multiple of the block size.
+        // Even though we're using 1K blocks, the lrzsz programs use
+        // shorter blocks for the last bit of the data.
+        assert_eq!(result.len() % 128, 0);
+        assert!(result.len() >= gettysburg.len());
+        assert_eq!(&result[..gettysburg.len()], gettysburg);
+        Ok(())
+    }
+
+    #[test]
+    fn test_xmodem_recv_with_errors() -> Result<()> {
+        let filename = tmpfilename("test_xmodem_recv_with_errors");
+        let gettysburg = GETTYSBURG.as_bytes();
+        std::fs::write(&filename, gettysburg)?;
+        let child = ChildUart::spawn_corrupt(
+            &["sx", &filename],
+            TransferState::new(&[3, 1032]),
+            TransferState::default(),
+        )?;
+        let xmodem = XmodemFirmware { max_errors: 2 };
+        let mut result = Vec::new();
+        let err = xmodem.receive(&child, &mut result);
+        assert!(err.is_err());
+        assert_eq!(err.unwrap_err().to_string(), "Crc");
+        Ok(())
+    }
+
+    #[test]
+    fn test_xmodem_recv_with_cancel() -> Result<()> {
+        let filename = tmpfilename("test_xmodem_recv_with_cancel");
+        let gettysburg = GETTYSBURG.as_bytes();
+        std::fs::write(&filename, gettysburg)?;
+        let child = ChildUart::spawn_corrupt(
+            &["sx", &filename],
+            TransferState::new(&[1, 1030]),
+            TransferState::default(),
+        )?;
+        let xmodem = XmodemFirmware { max_errors: 2 };
+        let mut result = Vec::new();
+        let err = xmodem.receive(&child, &mut result);
+        assert!(err.is_err());
+        assert_eq!(err.unwrap_err().to_string(), "Cancel");
+        Ok(())
+    }
+}
---
+++ sw/host/tests/xmodem/xmodem.rs
@@ -0,0 +1,138 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This module provides a convenience wrapper around the firmware's C
+// implementation of Xmodem so that its easy to write tests for that
+// implementation.
+
+use anyhow::{anyhow, Result};
+use std::io::Write;
+
+use opentitanlib::io::uart::Uart;
+use opentitanlib::with_unknown;
+
+use xmodem_testlib::*;
+
+with_unknown! {
+    pub enum XmodemResult: u32 {
+        Ok = rom_error_kErrorOk,
+        TimeoutStart = rom_error_kErrorXModemTimeoutStart,
+        TimeoutPacket = rom_error_kErrorXModemTimeoutPacket,
+        TimeoutData = rom_error_kErrorXModemTimeoutData,
+        TimeoutCrc = rom_error_kErrorXModemTimeoutCrc,
+        TimeoutAck = rom_error_kErrorXModemTimeoutAck,
+        Crc = rom_error_kErrorXModemCrc,
+        EndOfFile = rom_error_kErrorXModemEndOfFile,
+        Cancel = rom_error_kErrorXModemCancel,
+        Unknown = rom_error_kErrorXModemUnknown,
+        Protocol = rom_error_kErrorXModemProtocol,
+        TooManyErrors = rom_error_kErrorXModemTooManyErrors,
+    }
+}
+
+#[derive(Default)]
+pub struct XmodemFirmware {
+    // Only relevant to the `receive` function.
+    pub max_errors: usize,
+}
+
+impl XmodemFirmware {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn send(&self, uart: &dyn Uart, data: &[u8]) -> Result<()> {
+        let result = unsafe {
+            let io: *mut std::os::raw::c_void = std::mem::transmute(&uart as *const &dyn Uart);
+            let buf = data.as_ptr() as *const std::os::raw::c_void;
+            let len = data.len();
+            XmodemResult(xmodem_send(io, buf, len))
+        };
+        match result {
+            XmodemResult::Ok => Ok(()),
+            _ => Err(anyhow!("{}", result)),
+        }
+    }
+
+    pub fn receive(&self, uart: &dyn Uart, data: &mut impl Write) -> Result<()> {
+        unsafe {
+            let io: *mut std::os::raw::c_void = std::mem::transmute(&uart as *const &dyn Uart);
+            xmodem_recv_start(io);
+
+            let mut errors = 0;
+            let mut frame = 1u32;
+            let mut buf = [0u8; 1024];
+            let mut rxlen = 0usize;
+            let mut unknown_rx = 0u8;
+
+            loop {
+                let result = XmodemResult(xmodem_recv_frame(
+                    io,
+                    frame,
+                    buf.as_mut_ptr(),
+                    &mut rxlen as *mut usize,
+                    &mut unknown_rx as *mut u8,
+                ));
+
+                match result {
+                    XmodemResult::Ok => {
+                        data.write_all(&buf[..rxlen])?;
+                        xmodem_ack(io, true);
+                        frame += 1;
+                    }
+                    XmodemResult::Crc => {
+                        xmodem_ack(io, false);
+                        errors += 1;
+                        if errors >= self.max_errors {
+                            return Err(anyhow!("{}", result));
+                        }
+                    }
+                    XmodemResult::EndOfFile => {
+                        xmodem_ack(io, true);
+                        return Ok(());
+                    }
+                    _ => return Err(anyhow!("{}", result)),
+                }
+            }
+        }
+    }
+}
+
+// The xmodem_{read,write} functions provide the interface to the low-level C implementation to
+// interact with the `Uart` device provided to the `XmodemFirmware` struct.
+#[no_mangle]
+extern "C" fn xmodem_read(
+    iohandle: *mut std::os::raw::c_void,
+    data: *mut u8,
+    len: usize,
+    _timeout_ms: u32,
+) -> usize {
+    unsafe {
+        let iohandle = iohandle as *const &dyn Uart;
+        let uart: &dyn Uart = *iohandle;
+        let data = std::slice::from_raw_parts_mut(data, len);
+        match uart.read(data) {
+            Ok(n) => n,
+            Err(e) => {
+                eprintln!("xmodem_read: {e:?}");
+                0
+            }
+        }
+    }
+}
+
+#[no_mangle]
+extern "C" fn xmodem_write(iohandle: *mut std::os::raw::c_void, data: *const u8, len: usize) {
+    unsafe {
+        let iohandle = iohandle as *const &dyn Uart;
+        let uart: &dyn Uart = *iohandle;
+        let data = std::slice::from_raw_parts(data, len);
+        match uart.write(data) {
+            Ok(_) => {}
+            Err(e) => {
+                eprintln!("xmodem_write: {e:?}");
+            }
+        }
+    }
+}
