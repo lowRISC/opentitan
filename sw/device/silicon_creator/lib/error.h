af135c380ebc32c688142980e4a2f47b8bc16361
Chris Frantz <cfrantz@google.com>
2024-02-07 10:53:08 -0800

    [rom_ext, rescue] Add a firmware Xmodem implementation
    1. Create an Xmodem-CRC implementation for firmware.
    2. Bindgen the C implementation to rust for testing.
    3. Test the C implementation against the classic Xmodem implementation
       supplied by the `lrzsz` programs.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/device/silicon_creator/lib/error.h
+++ sw/device/silicon_creator/lib/error.h
@@ -50,6 +50,7 @@ enum module_ {
   kModuleRomExtInterrupt = MODULE_CODE('R', 'I'),
   kModuleAsn1 =            MODULE_CODE('A', '1'),
   kModuleRetRam =          MODULE_CODE('R', 'R'),
+  kModuleXModem =          MODULE_CODE('X', 'M'),
   // clang-format on
 };
 
@@ -154,6 +155,18 @@ enum module_ {
   X(kErrorBootSvcBadSlot,             ERROR_(2, kModuleBootSvc, kInvalidArgument)), \
   \
   X(kErrorRomExtBootFailed,           ERROR_(1, kModuleRomExt, kFailedPrecondition)), \
+  \
+  X(kErrorXModemTimeoutStart,         ERROR_(1, kModuleXModem, kDeadlineExceeded)), \
+  X(kErrorXModemTimeoutPacket,        ERROR_(2, kModuleXModem, kDeadlineExceeded)), \
+  X(kErrorXModemTimeoutData,          ERROR_(3, kModuleXModem, kDeadlineExceeded)), \
+  X(kErrorXModemTimeoutCrc,           ERROR_(4, kModuleXModem, kDeadlineExceeded)), \
+  X(kErrorXModemTimeoutAck,           ERROR_(5, kModuleXModem, kDeadlineExceeded)), \
+  X(kErrorXModemCrc,                  ERROR_(6, kModuleXModem, kDataLoss)), \
+  X(kErrorXModemEndOfFile,            ERROR_(7, kModuleXModem, kOutOfRange)), \
+  X(kErrorXModemCancel,               ERROR_(8, kModuleXModem, kCancelled)), \
+  X(kErrorXModemUnknown,              ERROR_(9, kModuleXModem, kUnknown)), \
+  X(kErrorXModemProtocol,             ERROR_(10, kModuleXModem, kInvalidArgument)), \
+  X(kErrorXModemTooManyErrors,        ERROR_(11, kModuleXModem, kFailedPrecondition)), \
   \
   /* The high-byte of kErrorInterrupt is modified with the interrupt cause */ \
   X(kErrorRomExtInterrupt,            ERROR_(0, kModuleRomExtInterrupt, kUnknown)), \
