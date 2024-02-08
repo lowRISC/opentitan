9168afe28323467dbcb34c220e578b3532b27029
Ryan Torok <rtorok@opentitan.org>
2024-02-08 21:11:33 +0000

    [cryptotest] add uJSON API for SHA2/3, SHAKE test harness
    Signed-off-by: Ryan Torok <rtorok@opentitan.org>

---
+++ sw/device/tests/crypto/cryptotest/json/hash_commands.h
@@ -0,0 +1,48 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_TESTS_CRYPTO_CRYPTOTEST_JSON_HASH_COMMANDS_H_
+#define OPENTITAN_SW_DEVICE_TESTS_CRYPTO_CRYPTOTEST_JSON_HASH_COMMANDS_H_
+#include "sw/device/lib/ujson/ujson_derive.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define HASH_CMD_MAX_MESSAGE_BYTES 17068
+#define HASH_CMD_MAX_DIGEST_BYTES 256
+
+// clang-format off
+
+#define HASH_ALGORITHM(_, value) \
+    value(_, Sha256) \
+    value(_, Sha384) \
+    value(_, Sha512) \
+    value(_, Sha3_256) \
+    value(_, Sha3_384) \
+    value(_, Sha3_512) \
+    value(_, Shake128) \
+    value(_, Shake256)
+UJSON_SERDE_ENUM(CryptotestHashAlgorithm, cryptotest_hash_algorithm_t, HASH_ALGORITHM);
+
+#define SHAKE_DIGEST_LENGTH(field, string) \
+    field(length, size_t)
+UJSON_SERDE_STRUCT(CryptotestHashShakeDigestLength, cryptotest_hash_shake_digest_length_t, SHAKE_DIGEST_LENGTH);
+
+#define HASH_MESSAGE(field, string) \
+    field(message, uint8_t, HASH_CMD_MAX_MESSAGE_BYTES) \
+    field(message_len, size_t)
+UJSON_SERDE_STRUCT(CryptotestHashMessage, cryptotest_hash_message_t, HASH_MESSAGE);
+
+#define HASH_OUTPUT(field, string) \
+    field(oneshot_digest, uint8_t, HASH_CMD_MAX_DIGEST_BYTES) \
+    field(stepwise_digest, uint8_t, HASH_CMD_MAX_DIGEST_BYTES) \
+    field(digest_len, size_t)
+UJSON_SERDE_STRUCT(CryptotestHashOutput, cryptotest_hash_output_t, HASH_OUTPUT);
+
+// clang-format on
+
+#ifdef __cplusplus
+}
+#endif
+#endif  // OPENTITAN_SW_DEVICE_TESTS_CRYPTO_CRYPTOTEST_JSON_HASH_COMMANDS_H_
