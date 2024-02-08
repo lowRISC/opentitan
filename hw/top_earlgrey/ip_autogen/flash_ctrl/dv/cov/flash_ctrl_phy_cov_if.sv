a5a0980b90b2b51c77e7df8046c632180fb2b456
Michael Schaffner <msf@opentitan.org>
2024-02-08 11:21:00 -0800

    [flash_ctrl/dv] Simplify cov interface and remove NoSameAddrRead_A
    Previously, NoSameAddrRead_A would sometimes fire incorrectly since the
    model of the read buffer is too simplistic in this interface. Modelling
    the read buffer accurately would require access to more internal signals
    from the design so that all corner cases (e.g. integrity errors) can be
    modelled accurately.
    
    The assertion is therefore removed, as it is not strictly required for
    the coverage interface to fulfill its purpose.
    
    Signed-off-by: Michael Schaffner <msf@opentitan.org>

--- hw/top_earlgrey/ip_autogen/flash_ctrl/dv/cov/flash_ctrl_phy_cov_if.sv
+++ hw/top_earlgrey/ip_autogen/flash_ctrl/dv/cov/flash_ctrl_phy_cov_if.sv
@@ -18,63 +18,49 @@ interface flash_ctrl_phy_cov_if
   input logic [15:0] addr
 );
 
-  bit                en_cov = 0;
-  typedef enum logic[1:0] {READ, PROG, ERASE, NONE} cmd_e;
-  cmd_e cur_cmd, cur_cmd_d, prv_cmd;
+  bit    en_cov = 0;
 
-  // read address for previous read command
-  logic [15:0] prv_rd_addr, prv_rd_addr_d;
-  // read address for current read command
-  logic [15:0] rd_addr, rd_addr_d;
   logic  any_req;
-  assign any_req = rd_req | prog_req | pg_erase_req | bk_erase_req;
+  assign any_req = rd_req || prog_req || pg_erase_req || bk_erase_req;
   logic  any_vld_req;
-  assign any_vld_req = any_req & ack;
+  assign any_vld_req = any_req && ack;
+
+  // Decode current command
+  typedef enum logic[1:0] {READ, PROG, ERASE, NONE} cmd_e;
+  cmd_e cur_cmd;
+  always_comb begin
+    cur_cmd = NONE;
+    if (any_vld_req) begin
+      if (rd_req) cur_cmd = READ;
+      else if (prog_req) cur_cmd = PROG;
+      else if (pg_erase_req || bk_erase_req) cur_cmd = ERASE;
+    end
+  end
+
+  // previous command
+  cmd_e prv_cmd_q;
+  always @(posedge clk_i) begin
+    if (!rst_ni) begin
+      prv_cmd_q <= NONE;
+    end else begin
+      if (any_vld_req) begin
+        prv_cmd_q <= cur_cmd;
+      end
+    end
+  end
 
   // command interval counter
   // couter will be saturated when it hits maxium
   bit [31:0] idle_cnt;
-
-  function static cmd_e get_cmd;
-    if (rd_req) return READ;
-    if (prog_req) return PROG;
-    if (pg_erase_req|bk_erase_req) return ERASE;
-  endfunction
-
-  assign cur_cmd = (any_vld_req)? get_cmd() : NONE;
-  assign rd_addr = (cur_cmd == READ)? addr : rd_addr_d;
-  assign prv_rd_addr = (cur_cmd != READ && cur_cmd_d == READ)? rd_addr : prv_rd_addr_d;
-
   always @(posedge clk_i) begin
-    if (~rst_ni) begin
-      cur_cmd_d <= NONE;
-      rd_addr_d <= 'h0;
-      prv_rd_addr_d <= 'h0;
-      prv_cmd <= NONE;
-    end else begin
-      cur_cmd_d <= cur_cmd;
-      rd_addr_d <= rd_addr;
-      prv_rd_addr_d <= prv_rd_addr;
-      prv_cmd <= (any_req & ack)? cur_cmd : prv_cmd;
-    end
-  end
-
-  always @(posedge clk_i) begin
-    if (~rst_ni| ~rd_buf_en) idle_cnt <= 0;
-    else idle_cnt <= (any_req & ack)? 0 :
+    if (!rst_ni || !rd_buf_en) idle_cnt <= 0;
+    else idle_cnt <= (any_vld_req)? 0 :
                      (idle_cnt == 32'hffff_ffff)? 32'hffff_ffff : idle_cnt + 32'h1;
   end
 
   // back to back read sequence
   logic b2b_read;
-  assign b2b_read = (cur_cmd == READ && prv_cmd == READ);
-
-
-  // Flash controller has read cache before the physical interface.
-  // The cache returns data for the read address recorded in the cache.
-  // Therefore, back to back same address read should not be shown on the physical interface.
-  `ASSERT(NoSameAddrRead_A,
-          cur_cmd == READ |-> prv_rd_addr != addr, clk_i,(~rst_ni | ~rd_buf_en))
+  assign b2b_read = (cur_cmd == READ && prv_cmd_q == READ);
 
   // Covergroup to sample read after any of {read,prog,erase} operations
   // Which was asked in #3353
