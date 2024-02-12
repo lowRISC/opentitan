bd1678da10ad852aa618db7ac1473ca3b0134ca1
Chris Frantz <cfrantz@google.com>
2024-02-12 12:49:29 -0800

    [opentitallib, rescue] Create a rescue client for opentitantool
    1. Implement firmware update, boot_log retrieval, boot_svc retrieval and
       sending selected boot_svc messages.
    2. Implement opentitantool commands for each of the client functions.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/host/opentitanlib/src/rescue/mod.rs
+++ sw/host/opentitanlib/src/rescue/mod.rs
@@ -2,4 +2,21 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+use thiserror::Error;
+
+pub mod boot_log;
+pub mod boot_svc;
+pub mod serial;
 pub mod xmodem;
+
+#[derive(Debug, Error)]
+pub enum RescueError {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error("bad mode: {0}")]
+    BadMode(String),
+    #[error("bad size: expected {0} bytes, but found {1}")]
+    BadSize(usize, usize),
+    #[error("invalid digest")]
+    InvalidDigest,
+}
