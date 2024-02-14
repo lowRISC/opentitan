ca5c527de866d74a4e49edbbcff073647cdb846e
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-02-14 11:49:01 +0100

    [pentest] Add FI Ibex RF tests
    This PR adds new penetration tests targeting the register file (RF)
    of Ibex:
    - ibex_fi_char_register_file: Test whether a fault can manipulate
      values in the RF
    - ibex_fi_char_register_file_read: Test whether a fault during
      reading the RF can manipulate values. This is related to #20715
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit bc64a22e885a9c0cbb9ddd5e23d170ccb6101405)

--- sw/device/tests/crypto/cryptotest/firmware/ibex_fi.h
+++ sw/device/tests/crypto/cryptotest/firmware/ibex_fi.h
@@ -13,6 +13,8 @@ status_t handle_ibex_fi_char_reg_op_loop(ujson_t *uj);
 status_t handle_ibex_fi_char_unrolled_mem_op_loop(ujson_t *uj);
 status_t handle_ibex_fi_char_unrolled_reg_op_loop(ujson_t *uj);
 status_t handle_ibex_fi_init_trigger(ujson_t *uj);
+status_t handle_ibex_fi_char_register_file(ujson_t *uj);
+status_t handle_ibex_fi_char_register_file_read(ujson_t *uj);
 status_t handle_ibex_fi(ujson_t *uj);
 
 #endif  // OPENTITAN_SW_DEVICE_TESTS_CRYPTO_CRYPTOTEST_FIRMWARE_IBEX_FI_H_
