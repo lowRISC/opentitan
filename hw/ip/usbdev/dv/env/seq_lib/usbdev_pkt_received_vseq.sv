ac378eb4ad7f4ff1e183ff021c10cb35064b95b8
Mubashir Saleem <mubashir.impare@gmail.com>
2024-02-19 16:08:09 +0500

    [usbdev] Usbdev_sequences_changes
    This commit contains changes that will clear regression failures of
    already merged sequences.
    
    Signed-off-by: Mubashir Saleem <mubashir.impare@gmail.com>

--- hw/ip/usbdev/dv/env/seq_lib/usbdev_pkt_received_vseq.sv
+++ hw/ip/usbdev/dv/env/seq_lib/usbdev_pkt_received_vseq.sv
@@ -8,55 +8,27 @@ class usbdev_pkt_received_vseq extends usbdev_base_vseq;
 
   `uvm_object_new
 
-  usb20_item     item;
-  RSP            rsp_item;
-  bit            pkt_received;
-  uvm_reg_data_t read_rxfifo;
-  uvm_reg_data_t intr_state;
-
   task body();
     // Configure transaction
-    configure_trans();
+    configure_out_trans();
     // Out token packet followed by a data packet
     call_token_seq(PidTypeOutToken);
-    cfg.clk_rst_vif.wait_clks(20);
+    cfg.clk_rst_vif.wait_clks(10);
     call_data_seq(PidTypeData0, .randomize_length(1'b1), .num_of_bytes(0));
-    get_response(rsp_item);
-    $cast(item, rsp_item);
-    get_out_response_from_device(item, PidTypeAck);
-    cfg.clk_rst_vif.wait_clks(20);
+    get_response(m_response_item);
+    $cast(m_usb20_item, m_response_item);
+    get_out_response_from_device(m_usb20_item, PidTypeAck);
+    cfg.clk_rst_vif.wait_clks(10);
     // Check transaction accuracy
     check_trans_accuracy();
   endtask
 
-  task configure_trans();
-    super.apply_reset("HARD");
-    super.dut_init("HARD");
-    cfg.clk_rst_vif.wait_clks(200);
-    clear_all_interrupts();
-    // Enable EP0 Out
-    csr_wr(.ptr(ral.ep_out_enable[0].enable[endp]), .value(1'b1));
-    csr_update(ral.ep_out_enable[0]);
-    // Enable rx out
-    ral.rxenable_out[0].out[endp].set(1'b1);
-    csr_update(ral.rxenable_out[0]);
-    // Set buffer
-    ral.avoutbuffer.buffer.set(out_buffer_id);
-    csr_update(ral.avoutbuffer);
-    // Enable pkt_received interrupt
-    ral.intr_enable.pkt_received.set(1'b1);
-    csr_update(ral.intr_enable);
-  endtask
-
   task check_trans_accuracy();
-    // Read rx_fifo reg
-    csr_rd(.ptr(ral.rxfifo), .value(read_rxfifo));
+    uvm_reg_data_t intr_state;
     // Read intr_state reg
     csr_rd(.ptr(ral.intr_state), .value(intr_state));
-    // Get pkt_received interrupt status
-    pkt_received = bit'(get_field_val(ral.intr_state.pkt_received, intr_state));
     // DV_CHECK on pkt_received interrupt
-    `DV_CHECK_EQ(pkt_received, 1);
+    `DV_CHECK_EQ(get_field_val(ral.intr_state.pkt_received, intr_state), 1)
   endtask
 
 endclass
