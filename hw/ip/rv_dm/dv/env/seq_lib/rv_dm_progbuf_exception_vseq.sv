d00a8c1e3d33a20e296b012a912d92cacb25e070
Basit Mehmood <basit.impare@gmail.com>
2024-02-20 10:29:28 +0500

    [RV_DM]rv_dm_progbuf_exception_vseq
    This PR contains an exception error case using program buffer.
    
    Signed-off-by: Basit Mehmood <basit.impare@gmail.com>

---
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_progbuf_exception_vseq.sv
@@ -0,0 +1,34 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class rv_dm_progbuf_exception_vseq extends rv_dm_base_vseq;
+
+  `uvm_object_utils(rv_dm_progbuf_exception_vseq)
+
+  `uvm_object_new
+
+  constraint lc_hw_debug_en_c {
+    lc_hw_debug_en == lc_ctrl_pkg::On;
+  }
+  constraint scanmode_c {
+    scanmode == prim_mubi_pkg::MuBi4False;
+  }
+  task body();
+    uvm_reg_data_t data;
+    uvm_reg_data_t r_data;
+    repeat ($urandom_range(1, 10)) begin
+    data = $urandom_range(0,31);
+      csr_wr(.ptr(jtag_dmi_ral.dmcontrol.haltreq), .value(1));
+      csr_wr(.ptr(tl_mem_ral.halted), .value(0));
+      // Access Register command with postexec bit set.
+      csr_wr(.ptr(jtag_dmi_ral.command), .value('h00250000));
+      csr_wr(.ptr(jtag_dmi_ral.progbuf[0]), .value(data));
+      csr_wr(.ptr(tl_mem_ral.exception), .value(data));
+      csr_rd(.ptr(jtag_dmi_ral.abstractcs), .value(r_data));
+      // Check that there is exception in command.
+      `DV_CHECK_EQ(3, get_field_val(jtag_dmi_ral.abstractcs.cmderr, r_data));
+    end
+  endtask
+
+endclass:rv_dm_progbuf_exception_vseq
