5bfed0a8b58ec7263cf7af8c6f48b12775ba0a10
Ryan Torok <rtorok@opentitan.org>
2024-02-21 18:45:51 +0000

    [cryptotest] add parser for manual hjson kmac tests
    Signed-off-by: Ryan Torok <rtorok@opentitan.org>
    (cherry picked from commit 4011eee8a9432d8297e23479c0bce22fd276d74b)

---
+++ sw/host/cryptotest/testvectors/parsers/hjson_kmac_parser.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+import argparse
+import json
+import logging
+import sys
+import jsonschema
+import hjson
+from cryptotest_util import str_to_byte_array
+
+
+def parse_test_vectors(raw_data):
+    test_vectors = []
+    count = 1
+    for test in raw_data:
+        if "cust_str" in test:
+            customization_string = str_to_byte_array(test["cust_str"])
+        else:
+            customization_string = []
+        test_vec = {
+            "vendor": "manual",
+            "test_case_id": count,
+            "algorithm": "kmac",
+            "mode": test["security_str"],
+            "key": str_to_byte_array(test["key"]),
+            "message": str_to_byte_array(test["input_msg"]),
+            "customization_string": customization_string,
+            "tag": str_to_byte_array(test["digest"]),
+        }
+        test_vectors.append(test_vec)
+        count += 1
+
+    return test_vectors
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src',
+                        metavar='FILE',
+                        type=argparse.FileType('r'),
+                        help='Read test vectors from this JSON file.')
+    parser.add_argument('--dst',
+                        metavar='FILE',
+                        type=argparse.FileType('w'),
+                        help='Write output to this file.')
+    parser.add_argument("--schema", type=str, help="Testvector schema file")
+    args = parser.parse_args()
+
+    testvecs = parse_test_vectors(hjson.loads(args.src.read()))
+
+    # Validate generated JSON
+    with open(args.schema) as schema_file:
+        schema = json.load(schema_file)
+    jsonschema.validate(testvecs, schema)
+
+    logging.info(f"Created {len(testvecs)} tests")
+    json.dump(testvecs, args.dst)
+    args.src.close()
+    args.dst.close()
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
