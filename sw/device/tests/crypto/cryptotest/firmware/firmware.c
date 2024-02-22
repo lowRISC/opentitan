593dd36b731518c63ec42ea51ccbd261226ed41e
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-02-22 17:55:55 +0000

    [pentest] Add OTBN FI characterization tests
    This PR adds the following OTBN tests that can be used for FI
    characterization:
    - otbn.fi.char.unrolled.reg.op.loop
    - otbn.fi.char.unrolled.dmem.op.loop
    - otbn.fi.char.hardware.reg.op.loop
    - otbn.fi.char.hardware.dmem.op.loop
    
    The host code is located in lowRISC/ot-sca#327
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/tests/crypto/cryptotest/firmware/firmware.c
+++ sw/device/tests/crypto/cryptotest/firmware/firmware.c
@@ -16,6 +16,7 @@
 #include "sw/device/tests/crypto/cryptotest/json/commands.h"
 #include "sw/device/tests/crypto/cryptotest/json/ibex_fi_commands.h"
 #include "sw/device/tests/crypto/cryptotest/json/kmac_sca_commands.h"
+#include "sw/device/tests/crypto/cryptotest/json/otbn_fi_commands.h"
 #include "sw/device/tests/crypto/cryptotest/json/prng_sca_commands.h"
 #include "sw/device/tests/crypto/cryptotest/json/sha3_sca_commands.h"
 #include "sw/device/tests/crypto/cryptotest/json/trigger_sca_commands.h"
@@ -25,6 +26,7 @@
 #include "aes_sca.h"
 #include "ibex_fi.h"
 #include "kmac_sca.h"
+#include "otbn_fi.h"
 #include "prng_sca.h"
 #include "sha3_sca.h"
 #include "trigger_sca.h"
@@ -47,6 +49,9 @@ status_t process_cmd(ujson_t *uj) {
         break;
       case kCryptotestCommandKmacSca:
         RESP_ERR(uj, handle_kmac_sca(uj));
+        break;
+      case kCryptotestCommandOtbnFi:
+        RESP_ERR(uj, handle_otbn_fi(uj));
         break;
       case kCryptotestCommandPrngSca:
         RESP_ERR(uj, handle_prng_sca(uj));
---
+++ sw/device/tests/crypto/cryptotest/firmware/otbn/otbn_char_hardware_dmem_op_loop.s
@@ -0,0 +1,27 @@
+/* Copyright lowRISC contributors. */
+/* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
+/* SPDX-License-Identifier: Apache-2.0 */
+/*
+    OBTN.CHAR.HARDWARE_DMEM_OP_LOOP FI Penetration Test
+*/
+.section .text.start
+
+    /* Load loop counter from dmem and set num it. of outer loop to 100. */
+    li x1, 100
+    la x3, lc
+    /* Increment loop counter in nested hardware loops. */
+    loop x1, 5
+      loopi 100, 3
+        lw x2, 0(x3)
+        addi x2, x2, 1
+        sw x2, 0(x3)
+      nop
+
+    ecall
+
+.data
+    /* Loop counter (lc) result. */
+    .balign 32
+    .globl lc
+    lc:
+    .zero 32
