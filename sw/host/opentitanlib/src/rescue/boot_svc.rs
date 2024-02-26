ffb3d8b00b8d39532e0826673a956c9f42533a7d
Chris Frantz <cfrantz@google.com>
2024-02-26 08:24:00 -0800

    [opentitanlib, boot_svc] Add ownership boot_svc structs
    Signed-off-by: Chris Frantz <cfrantz@google.com>
    (cherry picked from commit a3184701052b9ce379790a023467e91b8ce4753e)

--- sw/host/opentitanlib/src/rescue/boot_svc.rs
+++ sw/host/opentitanlib/src/rescue/boot_svc.rs
@@ -8,8 +8,10 @@ use serde::Serialize;
 use serde_annotate::Annotate;
 use sha2::{Digest, Sha256};
 use std::convert::TryFrom;
-use std::io::Write;
+use std::io::{Read, Write};
 
+use crate::chip::boolean::HardenedBool;
+use crate::crypto::ecdsa::{EcdsaPrivateKey, EcdsaPublicKey, EcdsaRawPublicKey, EcdsaRawSignature};
 use crate::rescue::RescueError;
 use crate::with_unknown;
 
@@ -26,6 +28,14 @@ with_unknown! {
         SlotB = 0xcd598a4a,
     }
 
+    pub enum UnlockMode: u32 [default = Self::Unknown] {
+        Unknown = 0,
+        Any = 0x00594e41,
+        Endorsed = 0x4f444e45,
+        Update = 0x00445055,
+        Abort = 0x54524241,
+    }
+
     pub enum BootSvcKind: u32 [default = Self::Unknown] {
         Unknown = 0,
         Empty = 0xb4594546,
@@ -35,6 +45,10 @@ with_unknown! {
         NextBl0SlotResponse = 0x657051be,
         PrimaryBl0SlotRequest = 0x3d6c47b8,
         PrimaryBl0SlotResponse = 0xf2a4a609,
+        UnlockOwnershipRequest = 0x51524e55,
+        UnlockOwnershipResponse = 0x53524e55,
+        ActivateOwnerRequest = 0x51524f41,
+        ActivateOwnerResponse = 0x53524f41,
     }
 }
 
@@ -86,6 +100,39 @@ pub struct PrimaryBl0SlotResponse {
     status: u32,
 }
 
+#[derive(Debug, Default, Serialize, Annotate)]
+pub struct UnlockOwnershipRequest {
+    unlock_mode: UnlockMode,
+    #[serde(with = "serde_bytes")]
+    #[annotate(format=hexdump)]
+    reserved: Vec<u8>,
+    #[annotate(format=hex)]
+    nonce: u64,
+    next_owner_key: EcdsaRawPublicKey,
+    signature: EcdsaRawSignature,
+}
+
+#[derive(Debug, Default, Serialize, Annotate)]
+pub struct UnlockOwnershipResponse {
+    status: u32,
+}
+
+#[derive(Debug, Default, Serialize, Annotate)]
+pub struct ActivateOwnerRequest {
+    primary_bl0_slot: BootDataSlot,
+    erase_previous: HardenedBool,
+    #[serde(with = "serde_bytes")]
+    #[annotate(format=hexdump)]
+    reserved: Vec<u8>,
+    nonce: u64,
+    signature: EcdsaRawSignature,
+}
+
+#[derive(Debug, Default, Serialize, Annotate)]
+pub struct ActivateOwnerResponse {
+    status: u32,
+}
+
 #[derive(Debug, Serialize, Annotate)]
 pub enum Message {
     Raw(
@@ -97,9 +144,13 @@ pub enum Message {
     MinBl0SecVerRequest(MinBl0SecVerRequest),
     NextBl0SlotRequest(NextBl0SlotRequest),
     PrimaryBl0SlotRequest(PrimaryBl0SlotRequest),
+    UnlockOwnershipRequest(UnlockOwnershipRequest),
+    ActivateOwnerRequest(ActivateOwnerRequest),
     MinBl0SecVerResponse(MinBl0SecVerResponse),
     NextBl0SlotResponse(NextBl0SlotResponse),
     PrimaryBl0SlotResponse(PrimaryBl0SlotResponse),
+    UnlockOwnershipResponse(UnlockOwnershipResponse),
+    ActivateOwnerResponse(ActivateOwnerResponse),
 }
 
 #[derive(Debug, Serialize, Annotate)]
@@ -140,6 +191,18 @@ impl TryFrom<&[u8]> for BootSvc {
             BootSvcKind::PrimaryBl0SlotResponse => {
                 Message::PrimaryBl0SlotResponse(TryFrom::try_from(buf)?)
             }
+            BootSvcKind::UnlockOwnershipRequest => {
+                Message::UnlockOwnershipRequest(TryFrom::try_from(buf)?)
+            }
+            BootSvcKind::UnlockOwnershipResponse => {
+                Message::UnlockOwnershipResponse(TryFrom::try_from(buf)?)
+            }
+            BootSvcKind::ActivateOwnerRequest => {
+                Message::ActivateOwnerRequest(TryFrom::try_from(buf)?)
+            }
+            BootSvcKind::ActivateOwnerResponse => {
+                Message::ActivateOwnerResponse(TryFrom::try_from(buf)?)
+            }
             _ => Message::Raw(buf.to_vec()),
         };
 
@@ -159,6 +222,10 @@ impl BootSvc {
             Message::NextBl0SlotResponse(m) => m.write(&mut data)?,
             Message::PrimaryBl0SlotRequest(m) => m.write(&mut data)?,
             Message::PrimaryBl0SlotResponse(m) => m.write(&mut data)?,
+            Message::UnlockOwnershipRequest(m) => m.write(&mut data)?,
+            Message::UnlockOwnershipResponse(m) => m.write(&mut data)?,
+            Message::ActivateOwnerRequest(m) => m.write(&mut data)?,
+            Message::ActivateOwnerResponse(m) => m.write(&mut data)?,
             Message::Raw(m) => data.extend_from_slice(m.as_slice()),
         };
         let mut digest = Sha256::digest(&data[Header::HASH_LEN..]);
@@ -358,6 +425,121 @@ impl PrimaryBl0SlotResponse {
     pub const SIZE: usize = 8;
     pub fn write(&self, dest: &mut impl Write) -> Result<()> {
         dest.write_u32::<LittleEndian>(u32::from(self.primary_bl0_slot))?;
+        dest.write_u32::<LittleEndian>(self.status)?;
+        Ok(())
+    }
+}
+
+impl TryFrom<&[u8]> for UnlockOwnershipRequest {
+    type Error = RescueError;
+    fn try_from(buf: &[u8]) -> std::result::Result<Self, Self::Error> {
+        let mut reader = std::io::Cursor::new(buf);
+        let mut val = Self::default();
+        val.unlock_mode = UnlockMode(reader.read_u32::<LittleEndian>()?);
+        val.reserved.resize(Self::RESERVED_SIZE, 0);
+        reader.read_exact(&mut val.reserved)?;
+        val.nonce = reader.read_u64::<LittleEndian>()?;
+        val.next_owner_key = EcdsaRawPublicKey::read(&mut reader).map_err(RescueError::Anyhow)?;
+        val.signature = EcdsaRawSignature::read(&mut reader).map_err(RescueError::Anyhow)?;
+        Ok(val)
+    }
+}
+impl UnlockOwnershipRequest {
+    pub const SIZE: usize = 8;
+    const RESERVED_SIZE: usize = 18 * std::mem::size_of::<u32>();
+    const SIGNATURE_OFFSET: usize = 148;
+    pub fn write(&self, dest: &mut impl Write) -> Result<()> {
+        dest.write_u32::<LittleEndian>(u32::from(self.unlock_mode))?;
+        for i in 0..Self::RESERVED_SIZE {
+            let p = self.reserved.get(i).unwrap_or(&0xFF);
+            dest.write_all(std::slice::from_ref(p))?;
+        }
+        dest.write_u64::<LittleEndian>(self.nonce)?;
+        self.next_owner_key.write(dest)?;
+        self.signature.write(dest)?;
+        Ok(())
+    }
+
+    pub fn set_next_owner_key(&mut self, key: &EcdsaPublicKey) -> Result<()> {
+        self.next_owner_key = key.to_raw();
+        Ok(())
+    }
+
+    pub fn sign(&mut self, key: &EcdsaPrivateKey) -> Result<()> {
+        let mut data = Vec::new();
+        self.write(&mut data)?;
+        self.signature = key.digest_and_sign(&data[..Self::SIGNATURE_OFFSET])?;
+        Ok(())
+    }
+}
+
+impl TryFrom<&[u8]> for UnlockOwnershipResponse {
+    type Error = RescueError;
+    fn try_from(buf: &[u8]) -> std::result::Result<Self, Self::Error> {
+        let mut reader = std::io::Cursor::new(buf);
+        Ok(UnlockOwnershipResponse {
+            status: reader.read_u32::<LittleEndian>()?,
+        })
+    }
+}
+impl UnlockOwnershipResponse {
+    pub const SIZE: usize = 4;
+    pub fn write(&self, dest: &mut impl Write) -> Result<()> {
+        dest.write_u32::<LittleEndian>(self.status)?;
+        Ok(())
+    }
+}
+
+impl TryFrom<&[u8]> for ActivateOwnerRequest {
+    type Error = RescueError;
+    fn try_from(buf: &[u8]) -> std::result::Result<Self, Self::Error> {
+        let mut reader = std::io::Cursor::new(buf);
+        let mut val = Self::default();
+        val.primary_bl0_slot = BootDataSlot(reader.read_u32::<LittleEndian>()?);
+        val.erase_previous = HardenedBool(reader.read_u32::<LittleEndian>()?);
+        val.reserved.resize(Self::RESERVED_SIZE, 0);
+        reader.read_exact(&mut val.reserved)?;
+        val.nonce = reader.read_u64::<LittleEndian>()?;
+        val.signature = EcdsaRawSignature::read(&mut reader).map_err(RescueError::Anyhow)?;
+        Ok(val)
+    }
+}
+impl ActivateOwnerRequest {
+    pub const SIZE: usize = 8;
+    const RESERVED_SIZE: usize = 33 * std::mem::size_of::<u32>();
+    const SIGNATURE_OFFSET: usize = 148;
+    pub fn write(&self, dest: &mut impl Write) -> Result<()> {
+        dest.write_u32::<LittleEndian>(u32::from(self.primary_bl0_slot))?;
+        dest.write_u32::<LittleEndian>(u32::from(self.erase_previous))?;
+        for i in 0..Self::RESERVED_SIZE {
+            let p = self.reserved.get(i).unwrap_or(&0xFF);
+            dest.write_all(std::slice::from_ref(p))?;
+        }
+        dest.write_u64::<LittleEndian>(self.nonce)?;
+        self.signature.write(dest)?;
+        Ok(())
+    }
+
+    pub fn sign(&mut self, key: &EcdsaPrivateKey) -> Result<()> {
+        let mut data = Vec::new();
+        self.write(&mut data)?;
+        self.signature = key.digest_and_sign(&data[..Self::SIGNATURE_OFFSET])?;
+        Ok(())
+    }
+}
+
+impl TryFrom<&[u8]> for ActivateOwnerResponse {
+    type Error = RescueError;
+    fn try_from(buf: &[u8]) -> std::result::Result<Self, Self::Error> {
+        let mut reader = std::io::Cursor::new(buf);
+        Ok(ActivateOwnerResponse {
+            status: reader.read_u32::<LittleEndian>()?,
+        })
+    }
+}
+impl ActivateOwnerResponse {
+    pub const SIZE: usize = 4;
+    pub fn write(&self, dest: &mut impl Write) -> Result<()> {
         dest.write_u32::<LittleEndian>(self.status)?;
         Ok(())
     }
