b35b960e3ca6d9850dc5c8fce48ecd90cc01bce2
Chris Frantz <cfrantz@google.com>
2024-02-28 14:23:45 -0800

    [rom_ext] Add a fake `no_owner_recovery` key
    Add an ECDSA key allowing silicon_creator to perform a recovery process
    for chips that get into a "no owner" state.
    
    This state _should_ be impossible to reach, but if it does happen, the
    chip will have a remediation process via a silicon_creator key.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ sw/device/silicon_creator/lib/ownership/keys/fake/no_owner_recovery_ecdsa_p256.h
@@ -0,0 +1,15 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_NO_OWNER_RECOVERY_ECDSA_P256_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_NO_OWNER_RECOVERY_ECDSA_P256_H_
+
+#define NO_OWNER_RECOVERY_ECDSA_P256                                        \
+  {                                                                         \
+    0x81401b6d, 0x21b2f288, 0x94022520, 0xe7af2be2, 0x52fd43d5, 0x51a5e029, \
+        0x307c3109, 0xb3f7b84f, 0x9ca09689, 0x43f77fd0, 0x84a2ca45,         \
+        0x731885f9, 0xcb248ef6, 0x9e91b14c, 0x08d28c0e, 0x4c0e3fd1,         \
+  }
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_NO_OWNER_RECOVERY_ECDSA_P256_H_
---
+++ sw/device/silicon_creator/lib/ownership/keys/fake/ownership_ecdsa_keys_fake.c
@@ -0,0 +1,12 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdint.h>
+
+#include "sw/device/silicon_creator/lib/ownership/keys/fake/no_owner_recovery_ecdsa_p256.h"
+#include "sw/device/silicon_creator/lib/ownership/ownership_key.h"
+
+const owner_key_t *const kNoOwnerRecoveryKey = &(const owner_key_t){
+    .key = NO_OWNER_RECOVERY_ECDSA_P256,
+};
