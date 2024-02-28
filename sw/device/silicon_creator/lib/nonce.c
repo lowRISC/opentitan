c3bea29c997fb70aa4ed2322585242fe9138c611
Chris Frantz <cfrantz@google.com>
2024-02-28 13:52:12 -0800

    [silicon_creator] Nonce library
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ sw/device/silicon_creator/lib/nonce.c
@@ -0,0 +1,14 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/silicon_creator/lib/nonce.h"
+
+#include "sw/device/silicon_creator/lib/drivers/rnd.h"
+
+void nonce_new(nonce_t *nonce) {
+  nonce->value[0] = rnd_uint32();
+  nonce->value[1] = rnd_uint32();
+}
+
+extern bool nonce_equal(nonce_t *a, nonce_t *b);
