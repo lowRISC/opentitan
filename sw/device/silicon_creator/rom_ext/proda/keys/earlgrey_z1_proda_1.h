d1468a6b0176d1d20345b8cce005e022fea141fc
Miguel Osorio <miguelosorio@google.com>
2024-02-29 21:48:38 +0000

    [silicon] Add proda verification key.
    This commit adds an RSA verification key to be used with the proda SKU.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

---
+++ sw/device/silicon_creator/rom_ext/proda/keys/earlgrey_z1_proda_1.h
@@ -0,0 +1,39 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXT_PRODA_KEYS_EARLGREY_Z1_PRODA_1_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXT_PRODA_KEYS_EARLGREY_Z1_PRODA_1_H_
+
+#define EARLGREY_Z1_PRODA_1                                             \
+  {                                                                     \
+    .n =                                                                \
+        {{                                                              \
+            0xd1e0eea9, 0x95888c8d, 0x4339eec6, 0x342d81eb, 0xe730bdf8, \
+            0x4c03fc20, 0x4902bb45, 0x9af4a05f, 0xaae4f4a1, 0xec130b1d, \
+            0x18734294, 0x97983c69, 0x0927b16e, 0x083726c0, 0x2629d1e0, \
+            0x91a3ec8e, 0x35b604aa, 0xfa75712f, 0x8761134a, 0xd4c38c5f, \
+            0x53c48290, 0x906eb62d, 0x440972b3, 0x4f19c0e6, 0x6d195fb9, \
+            0x7456b39c, 0xdb9230fd, 0x3f93bb67, 0x96cc1c9a, 0x6afa6465, \
+            0x6697bd14, 0x215e83ce, 0xcf85d638, 0xa4c20d22, 0xb71b27ac, \
+            0x8850f917, 0x1e09590b, 0x222c7ee9, 0x1ce82241, 0x69e106b9, \
+            0xe6d3729e, 0xbfa0b71a, 0xeed505ea, 0xafc41ca6, 0x0e613044, \
+            0x1a6b2273, 0xa6e62ee1, 0x15769006, 0xe2938900, 0x705e9719, \
+            0x6cac5ca1, 0xe59a6da9, 0xb6e74216, 0xab73fff8, 0x556b73f7, \
+            0x3065bc53, 0xc3f8c3d5, 0x62b01003, 0x7ad71ec8, 0x0cd64a9b, \
+            0xb5eaac52, 0x1fca350d, 0x9cfd8851, 0x391a9def, 0xf34718fb, \
+            0xd41e9212, 0x8a0a3cc9, 0xda6371d4, 0x5c8afe9d, 0x0cd98e28, \
+            0xbff08562, 0x7f51a741, 0xd9834551, 0x1ce43659, 0xe7d7a5b4, \
+            0x9d5d7aa8, 0x6f41061a, 0x5dd0d043, 0x898ab826, 0xde821647, \
+            0xa5fcf750, 0x43960c3d, 0x3b4987ca, 0x967155ef, 0x1ae03dde, \
+            0xe72595ca, 0x20123b68, 0x47ba4459, 0x028da097, 0x5dae361b, \
+            0x296585d5, 0xd62f8aa4, 0x68105e36, 0x048bd433, 0x3cf2f35e, \
+            0xccdc8a6d,                                                 \
+        }},                                                             \
+    .n0_inv = {                                                         \
+        0xa0fe6a67, 0xd8e9a8fb, 0x5e4e4b03, 0xcb549524,                 \
+        0x4f241a62, 0x2196628c, 0x8c0fb8a8, 0x0e917b43,                 \
+    },                                                                  \
+  }
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXT_PRODA_KEYS_EARLGREY_Z1_PRODA_1_H_
