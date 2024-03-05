57cc26a53aff7b06013f111aa48a838c85d04bfb
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-03-05 15:45:01 +0000

    [usbdev,dv] Sort sequences (mostly) alphabetically
    There is no functional change here: we're just listing the sequences
    alphabetically in usbdev_vseq_list.sv and in usbdev_sim_cfg.hjson.
    This avoids an annoying situation where every PR is trying to add
    something at the bottom of the list and thus conflicts.
    
    Now, we can insert a new PR at its point in the alphabetical list and
    should hopefully have simple merges most of the time.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/usbdev/dv/env/seq_lib/usbdev_vseq_list.sv
+++ hw/ip/usbdev/dv/env/seq_lib/usbdev_vseq_list.sv
@@ -4,19 +4,23 @@
 
 `include "usbdev_base_vseq.sv"
 `include "usbdev_common_vseq.sv"
-`include "usbdev_csr_test_vseq.sv"
+
 `include "usbdev_smoke_vseq.sv"
-`include "usbdev_pkt_received_vseq.sv"
+
 `include "usbdev_av_buffer_vseq.sv"
-`include "usbdev_setup_trans_ignored_vseq.sv"
-`include "usbdev_pkt_sent_vseq.sv"
-`include "usbdev_nak_trans_vseq.sv"
+`include "usbdev_csr_test_vseq.sv"
 `include "usbdev_enable_vseq.sv"
+`include "usbdev_fifo_rst_vseq.sv"
 `include "usbdev_in_trans_vseq.sv"
-`include "usbdev_random_length_out_transaction_vseq.sv"
-`include "usbdev_min_length_out_transaction_vseq.sv"
-`include "usbdev_max_length_out_transaction_vseq.sv"
+`include "usbdev_nak_trans_vseq.sv"
 `include "usbdev_out_stall_vseq.sv"
 `include "usbdev_out_trans_nak_vseq.sv"
-`include "usbdev_fifo_rst_vseq.sv"
 `include "usbdev_phy_pins_sense_vseq.sv"
+`include "usbdev_pkt_received_vseq.sv"
+`include "usbdev_pkt_sent_vseq.sv"
+`include "usbdev_random_length_out_transaction_vseq.sv"
+`include "usbdev_setup_trans_ignored_vseq.sv"
+
+// These depend on usbdev_random_length_out_transaction, so need to come after it.
+`include "usbdev_max_length_out_transaction_vseq.sv"
+`include "usbdev_min_length_out_transaction_vseq.sv"
