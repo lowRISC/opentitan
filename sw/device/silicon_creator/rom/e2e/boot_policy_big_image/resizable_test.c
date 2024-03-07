0a26aac7c83151325a0e0169235138336fe20401
Tim Trippel <ttrippel@google.com>
2024-03-07 15:56:24 -0800

    [rom_e2e] add e2e test for big ROM_EXT images
    This tests the ability to load a ROM_EXT image that is larger than the
    64k ROM_EXT slot to facilitate provisioning flows.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

---
+++ sw/device/silicon_creator/rom/e2e/boot_policy_big_image/resizable_test.c
@@ -0,0 +1,32 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdbool.h>
+
+#include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/testing/test_framework/check.h"
+#include "sw/device/lib/testing/test_framework/ottf_main.h"
+#include "sw/device/silicon_creator/lib/manifest_def.h"
+#include "sw/device/silicon_creator/rom/boot_policy_ptrs.h"
+
+OTTF_DEFINE_TEST_CONFIG();
+
+#ifdef ARRAY_SIZE
+static const unsigned char arr[ARRAY_SIZE] = {1, 2, 3};
+#endif
+
+bool test_main(void) {
+#ifdef ARRAY_SIZE
+  for (size_t i = 0; i < ARRAY_SIZE; i += (ARRAY_SIZE / 4)) {
+    LOG_INFO("arr[%d] = %d", i, arr[i]);
+  }
+#endif
+
+  // Check image of the expected size.
+  const manifest_t *manifest = boot_policy_manifest_a_get();
+  LOG_INFO("Image Length (bytes) = %d", manifest->length);
+  CHECK(manifest->length > CHIP_ROM_EXT_SIZE_MAX);
+  CHECK(manifest->length <= (CHIP_ROM_EXT_SIZE_MAX + CHIP_BL0_SIZE_MAX));
+  return true;
+}
