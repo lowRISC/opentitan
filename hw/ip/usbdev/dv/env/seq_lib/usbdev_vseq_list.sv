e97544a8ba355a85975b57b19d3ce602842c833c
Mubashir <mubashir.impare@gmail.com>
2024-03-08 10:29:49 -0800

    [usbdev] usbdev_out_iso_seq_pr
    Adds usbdev_out_iso_sequence.
    
    Signed-off-by: Mubashir <mubashir.impare@gmail.com>

--- hw/ip/usbdev/dv/env/seq_lib/usbdev_vseq_list.sv
+++ hw/ip/usbdev/dv/env/seq_lib/usbdev_vseq_list.sv
@@ -15,6 +15,7 @@
 `include "usbdev_in_trans_vseq.sv"
 `include "usbdev_in_iso_vseq.sv"
 `include "usbdev_nak_trans_vseq.sv"
+`include "usbdev_out_iso_vseq.sv"
 `include "usbdev_out_stall_vseq.sv"
 `include "usbdev_out_trans_nak_vseq.sv"
 `include "usbdev_phy_pins_sense_vseq.sv"
