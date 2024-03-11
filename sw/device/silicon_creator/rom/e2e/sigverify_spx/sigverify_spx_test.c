3d21fe76c51cb4333b1a2abd589473cbb52d0833
Miguel Osorio <miguelosorio@google.com>
2024-03-11 03:13:49 +0000

    [rom e2e] Upgrade sigverify_spx tests
    Upgrades the `rom_e2e_sigverify_spx` test suite to use the new
    `opentitan_test` Bazel macro.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

---
+++ sw/device/silicon_creator/rom/e2e/sigverify_spx/sigverify_spx_test.c
@@ -0,0 +1,27 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdbool.h>
+
+#include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/testing/test_framework/ottf_main.h"
+#include "sw/device/silicon_creator/lib/drivers/lifecycle.h"
+#include "sw/device/silicon_creator/lib/drivers/otp.h"
+#include "sw/device/silicon_creator/lib/manifest_def.h"
+#include "sw/device/silicon_creator/lib/sigverify/spx_verify.h"
+
+#include "otp_ctrl_regs.h"
+
+OTTF_DEFINE_TEST_CONFIG();
+
+bool test_main(void) {
+  LOG_INFO(
+      "spx_en=0x%08x, spx_en_otp=0x%08x, spx_key_en=0x%08x%08x",
+      sigverify_spx_verify_enabled(lifecycle_state_get()),
+      otp_read32(OTP_CTRL_PARAM_CREATOR_SW_CFG_SIGVERIFY_SPX_EN_OFFSET),
+      otp_read32(OTP_CTRL_PARAM_CREATOR_SW_CFG_SIGVERIFY_SPX_KEY_EN_OFFSET +
+                 sizeof(uint32_t)),
+      otp_read32(OTP_CTRL_PARAM_CREATOR_SW_CFG_SIGVERIFY_SPX_KEY_EN_OFFSET));
+  return true;
+}
