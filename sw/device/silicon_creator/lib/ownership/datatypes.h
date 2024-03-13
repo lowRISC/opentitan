d20279803b6b475fab8e320e541cc400f7f5095a
Chris Frantz <cfrantz@google.com>
2024-03-13 13:37:14 -0700

    [ownership] Owner structs for firmware
    1. Add structs to represent owner application keys, flash region
       configuration, flash info configuration and rescue configuration.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>
    (cherry picked from commit 2837f5601ac856b1b72b17405dd6e6b645b2e9d2)

--- sw/device/silicon_creator/lib/ownership/datatypes.h
+++ sw/device/silicon_creator/lib/ownership/datatypes.h
@@ -8,6 +8,9 @@
 #include <stdint.h>
 
 #include "sw/device/lib/base/macros.h"
+#include "sw/device/silicon_creator/lib/sigverify/ecdsa_p256_key.h"
+#include "sw/device/silicon_creator/lib/sigverify/rsa_key.h"
+#include "sw/device/silicon_creator/lib/sigverify/spx_key.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -40,6 +43,30 @@ typedef enum ownership_state {
   /* Locked None: any bit pattern not listed above. */
   kOwnershipStateLockedNone = 0,
 } ownership_state_t;
+
+typedef enum ownership_key_alg {
+  /** Key algorithm RSA: `RSA3` */
+  kOwnershipKeyAlgRsa = 0x33415352,
+  /** Key algorithm ECDSA P-256: `P256` */
+  kOwnershipKeyAlgEcdsaP256 = 0x36353250,
+  /** Key algorithm SPX+: `SPX+` */
+  kOwnershipKeyAlgSpx = 0x2b585053,
+  /** Key algorithm SPX+q20: `Sq20` */
+  kOwnershipKeyAlgSpxq20 = 0x30327153,
+} ownership_key_alg_t;
+
+typedef enum tlv_tag {
+  /** Owner struct: `OWNR`. */
+  kTlvTagOwner = 0x524e574f,
+  /** Application Key: `APPK`. */
+  kTlvTagApplicationKey = 0x4b505041,
+  /** Flash Configuration: `FLSH`. */
+  kTlvTagFlashConfig = 0x48534c46,
+  /** Flash INFO configuration: `INFO`. */
+  kTlvTagInfoConfig = 0x4f464e49,
+  /** Rescue Configuration: `RESQ`. */
+  kTlvTagRescueConfig = 0x51534552,
+} tlv_tag_t;
 
 typedef struct tlv_header {
   uint32_t tag;
@@ -99,6 +126,159 @@ OT_ASSERT_MEMBER_OFFSET(owner_block_t, data, 224);
 OT_ASSERT_MEMBER_OFFSET(owner_block_t, signature, 1952);
 OT_ASSERT_MEMBER_OFFSET(owner_block_t, seal, 2016);
 OT_ASSERT_SIZE(owner_block_t, 2048);
+
+/**
+ * The owner application domain designates an application key
+ * as one of Test, Dev or Prod.
+ */
+typedef enum owner_app_domain {
+  /** Test domain: `test` */
+  kOwnerAppDomainTest = 0x74736574,
+  /** Dev domain: `dev_` */
+  kOwnerAppDomainDev = 0x5f766564,
+  /** Prod domain: `prod` */
+  kOwnerAppDomainProd = 0x646f7270,
+} owner_app_domain_t;
+/**
+ * The owner application key encodes keys for verifying the owner's application
+ * firmware.
+ */
+typedef struct owner_application_key {
+  /**
+   * Header identifying this struct.
+   * tag: `APPK`.
+   * length: 48 + sizeof(key).
+   */
+  tlv_header_t header;
+  /** Key algorithm.  One of ECDSA, SPX+ or SPXq20. */
+  uint32_t key_alg;
+  /** Key domain.  Recognized values: PROD, DEV, TEST */
+  uint32_t key_domain;
+  /** Key diversifier.
+   *
+   * This value is concatenated to key_domain to create an 8 word
+   * diversification constant to be programmed into the keymgr.
+   */
+  uint32_t key_diversifier[7];
+  /** Usage constraint must match manifest header's constraint */
+  uint32_t usage_constraint;
+  /** Key material.  Varies by algorithm type. */
+  union {
+    uint32_t id;
+    sigverify_rsa_key_t rsa;
+    sigverify_spx_key_t spx;
+    sigverify_ecdsa_p256_buffer_t ecdsa;
+  } data;
+} owner_application_key_t;
+
+OT_ASSERT_MEMBER_OFFSET(owner_application_key_t, header, 0);
+OT_ASSERT_MEMBER_OFFSET(owner_application_key_t, key_alg, 8);
+OT_ASSERT_MEMBER_OFFSET(owner_application_key_t, key_domain, 12);
+OT_ASSERT_MEMBER_OFFSET(owner_application_key_t, key_diversifier, 16);
+OT_ASSERT_MEMBER_OFFSET(owner_application_key_t, usage_constraint, 44);
+OT_ASSERT_MEMBER_OFFSET(owner_application_key_t, data, 48);
+OT_ASSERT_SIZE(owner_application_key_t, 464);
+
+typedef enum owner_flash_property {
+  kOwnerFlashPropertyRead = 0x00000001,
+  kOwnerFlashPropertyProgram = 0x00000002,
+  kOwnerFlashPropertyErase = 0x00000004,
+  kOwnerFlashPropertyScramble = 0x00000008,
+  kOwnerFlashPropertyEcc = 0x00000010,
+  kOwnerFlashPropertyHighEndurance = 0x00000020,
+  kOwnerFlashPropertyProtectWhenPrimary = 0x40000000,
+  kOwnerFlashPropertyLock = 0x80000000,
+} owner_flash_property_t;
+
+/**
+ * The owner flash region describes a region of flash and its configuration
+ * properties (ie: ECC, Scrambling, High Endurance, etc).
+ */
+typedef struct owner_flash_region {
+  /** The start of the region, in flash pages. */
+  uint16_t start;
+  /** The size of the region, in flash pages. */
+  uint16_t size;
+  /** The properties of the flash region. */
+  uint32_t properties;
+} owner_flash_region_t;
+OT_ASSERT_SIZE(owner_flash_region_t, 8);
+
+/**
+ * The owner flash config is a collection of owner region configuration items.
+ */
+typedef struct owner_flash_config {
+  /**
+   * Header identifiying this struct.
+   * tag: `FLSH`.
+   * length: 8 + 8 * length(config).
+   */
+  tlv_header_t header;
+  /** A list of flash region configurations. */
+  owner_flash_region_t config[];
+} owner_flash_config_t;
+OT_ASSERT_MEMBER_OFFSET(owner_flash_config_t, header, 0);
+OT_ASSERT_MEMBER_OFFSET(owner_flash_config_t, config, 8);
+OT_ASSERT_SIZE(owner_flash_config_t, 8);
+
+/**
+ * The owner info page describes an INFO page in flash and its configuration
+ * properties (ie: ECC, Scrambling, High Endurance, etc).
+ */
+typedef struct owner_info_page {
+  /** The bank where the info page is located. */
+  uint8_t bank;
+  /** The info page number. */
+  uint8_t page;
+  uint16_t _pad;
+  /** The properties of the info page. */
+  uint32_t properties;
+} owner_info_page_t;
+OT_ASSERT_SIZE(owner_info_page_t, 8);
+
+typedef struct owner_flash_info_config {
+  /**
+   * Header identifiying this struct.
+   * tag: `INFO`.
+   * length: 8 + 8 * length(config).
+   */
+  tlv_header_t header;
+  /** A list of flash info page configurations. */
+  owner_info_page_t config[];
+} owner_flash_info_config_t;
+OT_ASSERT_MEMBER_OFFSET(owner_flash_info_config_t, header, 0);
+OT_ASSERT_MEMBER_OFFSET(owner_flash_info_config_t, config, 8);
+OT_ASSERT_SIZE(owner_flash_info_config_t, 8);
+
+/**
+ * The owner rescue configuration describes how the rescue protocol should
+ * behave when invoked in the ROM_EXT.
+ */
+typedef struct owner_rescue_config {
+  /**
+   * Header identifiying this struct.
+   * tag: `RSCU`.
+   * length: 16 + sizeof(command_allow).
+   */
+  tlv_header_t header;
+  /** The rescue type.  Currently, only `XMDM` is supported. */
+  uint32_t rescue_type;
+  /** The start offset of the rescue region in flash (in pages). */
+  uint16_t start;
+  /** The size of the rescue region in flash (in pages). */
+  uint16_t size;
+  /** An allowlist of rescue and boot_svc commands that may be invoked by the
+   * rescue protocol.  The commands are identified by their 4-byte tags (tag
+   * identifiers between rescue commands and boot_svc commands are unique).
+   */
+  uint32_t command_allow[];
+} owner_rescue_config_t;
+OT_ASSERT_MEMBER_OFFSET(owner_rescue_config_t, header, 0);
+OT_ASSERT_MEMBER_OFFSET(owner_rescue_config_t, rescue_type, 8);
+OT_ASSERT_MEMBER_OFFSET(owner_rescue_config_t, start, 12);
+OT_ASSERT_MEMBER_OFFSET(owner_rescue_config_t, size, 14);
+OT_ASSERT_MEMBER_OFFSET(owner_rescue_config_t, command_allow, 16);
+OT_ASSERT_SIZE(owner_rescue_config_t, 16);
 
 #ifdef __cplusplus
 }  // extern "C"
