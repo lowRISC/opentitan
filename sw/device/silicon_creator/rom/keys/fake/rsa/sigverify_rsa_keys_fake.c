659206a2448b4bbaba393945d272daf56c9dcdec
Miguel Osorio <miguelosorio@google.com>
2024-03-15 22:40:39 +0000

    [rom] Reduce number of fake RSA keys.
    Reduce number of fake RSA keys down to three: 1 TEST, 1 DEV and 1 PROD.
    This is to save ROM space in preparation for A1 changes.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- sw/device/silicon_creator/rom/keys/fake/rsa/sigverify_rsa_keys_fake.c
+++ sw/device/silicon_creator/rom/keys/fake/rsa/sigverify_rsa_keys_fake.c
@@ -4,12 +4,8 @@
 
 #include "sw/device/lib/base/macros.h"
 #include "sw/device/silicon_creator/rom/keys/fake/rsa/dev_key_0_rsa_3072_exp_f4.h"
-#include "sw/device/silicon_creator/rom/keys/fake/rsa/dev_key_1_rsa_3072_exp_f4.h"
 #include "sw/device/silicon_creator/rom/keys/fake/rsa/prod_key_0_rsa_3072_exp_f4.h"
-#include "sw/device/silicon_creator/rom/keys/fake/rsa/prod_key_1_rsa_3072_exp_f4.h"
-#include "sw/device/silicon_creator/rom/keys/fake/rsa/prod_key_2_rsa_3072_exp_f4.h"
 #include "sw/device/silicon_creator/rom/keys/fake/rsa/test_key_0_rsa_3072_exp_f4.h"
-#include "sw/device/silicon_creator/rom/keys/fake/rsa/test_key_1_rsa_3072_exp_f4.h"
 #include "sw/device/silicon_creator/rom/sigverify_keys_rsa.h"
 
 #include "otp_ctrl_regs.h"
@@ -18,7 +14,7 @@
  * Number of RSA public keys.
  */
 enum {
-  kSigverifyRsaKeysCnt_ = 7,
+  kSigverifyRsaKeysCnt_ = 3,
 };
 const size_t kSigverifyRsaKeysCnt = kSigverifyRsaKeysCnt_;
 
@@ -28,7 +24,7 @@ const size_t kSigverifyRsaKeysCnt = kSigverifyRsaKeysCnt_;
  * This must be coprime with and less than `kSigverifyNumRsaKeys`.
  * Note: Step size is not applicable when `kSigverifyNumRsaKeys` is 1.
  */
-const size_t kSigverifyRsaKeysStep = 5;
+const size_t kSigverifyRsaKeysStep = 1;
 
 /**
  * Fake public keys for signature verification in tests.
@@ -47,13 +43,6 @@ const sigverify_rom_rsa_key_t kSigverifyRsaKeys[kSigverifyRsaKeysCnt_] = {
     {
         .entry =
             {
-                .key = TEST_KEY_1_RSA_3072_EXP_F4,
-                .key_type = kSigverifyKeyTypeTest,
-            },
-    },
-    {
-        .entry =
-            {
                 .key = DEV_KEY_0_RSA_3072_EXP_F4,
                 .key_type = kSigverifyKeyTypeDev,
             },
@@ -61,28 +50,7 @@ const sigverify_rom_rsa_key_t kSigverifyRsaKeys[kSigverifyRsaKeysCnt_] = {
     {
         .entry =
             {
-                .key = DEV_KEY_1_RSA_3072_EXP_F4,
-                .key_type = kSigverifyKeyTypeDev,
-            },
-    },
-    {
-        .entry =
-            {
                 .key = PROD_KEY_0_RSA_3072_EXP_F4,
-                .key_type = kSigverifyKeyTypeProd,
-            },
-    },
-    {
-        .entry =
-            {
-                .key = PROD_KEY_1_RSA_3072_EXP_F4,
-                .key_type = kSigverifyKeyTypeProd,
-            },
-    },
-    {
-        .entry =
-            {
-                .key = PROD_KEY_2_RSA_3072_EXP_F4,
                 .key_type = kSigverifyKeyTypeProd,
             },
     },
