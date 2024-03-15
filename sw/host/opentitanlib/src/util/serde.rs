92e8da80ace44bedc31a37ab80060dd50142bfcb
Chris Frantz <cfrantz@google.com>
2024-03-15 13:05:34 -0700

    [opentitanlib] ECDSA/RSA/SPX raw key helper functions
    1. Add a `Raw` struct type for each of the key types that is both serde
       serializable and convertible to/from binary form.
    2. Add `TryFrom` conversions between the native key types and the `Raw`
       structs.
    3. Implement `FromStr` conversions that load the Raw key material from a
       file.
    4. Add a serde helper to allow serde deserialization to use the
       `FromStr` trait to initialize structs based either on their `FromStr`
       conversion or a json key-value map.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>
    (cherry picked from commit 96f12253f261048c2ce426c35164324caeefac03)

---
+++ sw/host/opentitanlib/src/util/serde.rs
@@ -0,0 +1,72 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::fmt;
+use std::marker::PhantomData;
+use std::str::FromStr;
+
+use serde::de::{self, MapAccess, Visitor};
+use serde::{Deserialize, Deserializer};
+
+/// Deserialize a type T from either a string or struct by forwarding
+/// string forms to `FromStr`.
+///
+/// The use-case for this is to allow specifying key material in ownership
+/// configuration files either directly or by filename:
+/// ```
+/// key: {
+///   Ecdsa: "some/path/to/key.pub.der"
+/// }
+///
+/// key: {
+///   Ecdsa: {
+///     x: "...",
+///     y: "..."
+///   }
+/// }
+/// ```
+
+// This function was taken nearly verbatim from the serde documentation.
+// The example in the serde documentation constrains the `FromStr` error
+// type to `Void`; we constrain to any type implementing std::error::Error.
+pub fn string_or_struct<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+where
+    T: Deserialize<'de> + FromStr,
+    <T as FromStr>::Err: std::error::Error,
+    D: Deserializer<'de>,
+{
+    // This is a Visitor that forwards string types to T's `FromStr` impl and
+    // forwards map types to T's `Deserialize` impl. The `PhantomData` is to
+    // keep the compiler from complaining about T being an unused generic type
+    // parameter. We need T in order to know the Value type for the Visitor
+    // impl.
+    struct StringOrStruct<T>(PhantomData<fn() -> T>);
+
+    impl<'de, T> Visitor<'de> for StringOrStruct<T>
+    where
+        T: Deserialize<'de> + FromStr,
+        <T as FromStr>::Err: std::error::Error,
+    {
+        type Value = T;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("string or map")
+        }
+
+        fn visit_str<E>(self, value: &str) -> Result<T, E>
+        where
+            E: de::Error,
+        {
+            FromStr::from_str(value).map_err(|e| E::custom(format!("{e:?}")))
+        }
+
+        fn visit_map<M>(self, map: M) -> Result<T, M::Error>
+        where
+            M: MapAccess<'de>,
+        {
+            Deserialize::deserialize(de::value::MapAccessDeserializer::new(map))
+        }
+    }
+    deserializer.deserialize_any(StringOrStruct(PhantomData))
+}
