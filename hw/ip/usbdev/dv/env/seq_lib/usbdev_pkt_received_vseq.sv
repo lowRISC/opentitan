50fccddaec444c827ec1484daac8a736dc93c431
Adrian Lees <a.lees@lowrisc.org>
2024-03-18 15:19:54 +0000

    [dv,usbdev] Fix usbev_pkt_received and usbdev_enable
    The 'pkt_received' interrupt is now of type Status so it is
    asserted only whilst the RX FIFO still contains the packet
    description.
    Check the properties and content of the received packet in
    each sequence.
    Commute the 4n constraint on the length of the received packets,
    pending more thorough checking.
    Keep the 'recover_orig_data' function local to the affected
    sequences until such time as we can eliminate/reduce the
    reordering being performed.
    
    Signed-off-by: Adrian Lees <a.lees@lowrisc.org>

--- hw/ip/usbdev/dv/env/seq_lib/usbdev_pkt_received_vseq.sv
+++ hw/ip/usbdev/dv/env/seq_lib/usbdev_pkt_received_vseq.sv
@@ -9,8 +9,15 @@ class usbdev_pkt_received_vseq extends usbdev_base_vseq;
   `uvm_object_new
 
   task body();
+    // Expected data content of packet
+    byte unsigned exp_data[];
+
     // Configure transaction
     configure_out_trans();
+    // Enable pkt_received interrupt
+    ral.intr_enable.pkt_received.set(1'b1);
+    csr_update(ral.intr_enable);
+
     // Out token packet followed by a data packet
     call_token_seq(PidTypeOutToken);
     cfg.clk_rst_vif.wait_clks(10);
@@ -18,17 +25,18 @@ class usbdev_pkt_received_vseq extends usbdev_base_vseq;
     get_response(m_response_item);
     $cast(m_usb20_item, m_response_item);
     get_out_response_from_device(m_usb20_item, PidTypeAck);
-    cfg.clk_rst_vif.wait_clks(10);
-    // Check transaction accuracy
-    check_trans_accuracy();
+
+    recover_orig_data(m_data_pkt.data, exp_data);
+    // Check that the USB device received a packet with the expected properties.
+    check_pkt_received(endp, 0, out_buffer_id, exp_data);
   endtask
 
-  task check_trans_accuracy();
-    uvm_reg_data_t intr_state;
-    // Read intr_state reg
-    csr_rd(.ptr(ral.intr_state), .value(intr_state));
-    // DV_CHECK on pkt_received interrupt
-    `DV_CHECK_EQ(get_field_val(ral.intr_state.pkt_received, intr_state), 1)
-  endtask
+  // TODO: Presently the act of sending a data packet, destructively modifies it!
+  // Restore the data packet to its original state. This just bit-reverses each byte
+  // within the input array.
+  function recover_orig_data(input byte unsigned in[], output byte unsigned out[]);
+    out = {<<8{in}};  // Reverse the order of the bytes
+    out = {<<{out}};  // Bit-reverse everything
+  endfunction
 
 endclass
