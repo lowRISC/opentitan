d276190077a24c45a33e569c5aa038b49997d862
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-03-20 16:04:38 +0100

    [pentest] Add FI Ibex tests
    This commit adds the following FI penetrationt test that can be
    used to characterize Ibex:
    - ibex.char.flash_read
    - ibex.char.flash_write
    - ibex.char.sram_read
    - ibex.char.sram_write
    - ibex.char.unconditional_branch
    - ibex.char.conditional_branch
    
    The host code is located in lowRISC/ot-sca#346
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit a0938d3fe11a13d48735628a113447beae4bdfbb)

--- sw/device/tests/crypto/cryptotest/json/ibex_fi_commands.h
+++ sw/device/tests/crypto/cryptotest/json/ibex_fi_commands.h
@@ -18,13 +18,25 @@ extern "C" {
     value(_, CharUnrolledMemOpLoop) \
     value(_, CharMemOpLoop) \
     value(_, CharRegisterFile) \
-    value(_, CharRegisterFileRead)
+    value(_, CharRegisterFileRead) \
+    value(_, CharCondBranch) \
+    value(_, CharUncondBranch) \
+    value(_, CharSramWrite) \
+    value(_, CharSramRead) \
+    value(_, CharFlashWrite) \
+    value(_, CharFlashRead)
 UJSON_SERDE_ENUM(IbexFiSubcommand, ibex_fi_subcommand_t, IBEXFI_SUBCOMMAND);
 
 #define IBEXFI_TEST_RESULT(field, string) \
     field(result, uint32_t) \
     field(err_status, uint32_t)
 UJSON_SERDE_STRUCT(IbexFiTestResult, ibex_fi_test_result_t, IBEXFI_TEST_RESULT);
+
+#define IBEXFI_TEST_RESULT_MULT(field, string) \
+    field(result1, uint32_t) \
+    field(result2, uint32_t) \
+    field(err_status, uint32_t)
+UJSON_SERDE_STRUCT(IbexFiTestResultMult, ibex_fi_test_result_mult_t, IBEXFI_TEST_RESULT_MULT);
 
 #define IBEXFI_LOOP_COUNTER_OUTPUT(field, string) \
     field(loop_counter, uint32_t) \
