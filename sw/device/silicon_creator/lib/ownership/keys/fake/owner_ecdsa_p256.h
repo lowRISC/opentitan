131f72fcfe25912282d35ee983877a0bd4968256
Chris Frantz <cfrantz@google.com>
2024-03-25 10:00:58 -0700

    [ownership] Add fake owner keys
    The fake owner keys include:
    - An owner key.
    - An activate key.
    - An unlock key.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>
    (cherry picked from commit 56ae8ffb089ac783ce933323a6f69f9cc79d0b28)
    (cherry picked from commit bf276fb986d0ce9c90a6aa2b0774e46aa104a254)

---
+++ sw/device/silicon_creator/lib/ownership/keys/fake/owner_ecdsa_p256.h
@@ -0,0 +1,15 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_OWNER_ECDSA_P256_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_OWNER_ECDSA_P256_H_
+
+#define OWNER_ECDSA_P256                                                    \
+  {                                                                         \
+    0x8e3dcb50, 0x0036bafe, 0xe9ca771f, 0xbce13f2c, 0x8246d17a, 0x5949d6b1, \
+        0x3a624c28, 0x7c5d8d08, 0xd664e861, 0xd07b1bf5, 0x6f437b27,         \
+        0x21696187, 0x123b6637, 0xee962e67, 0xaec24bf5, 0x529422ab,         \
+  }
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_OWNER_ECDSA_P256_H_
