5b644a7a10128810b7a0ba4da3863ced55c39522
Mubashir <mubashir.impare@gmail.com>
2024-03-26 03:21:26 -0700

    [usbdev,dv] usbdev phy_config_usb_ref_disable sequence
    Added usbdev_phy_config_usb_ref_disable sequence to evaluate the functionality of the device's usb_ref_disable feature. Once it's set to disabled, the device will refrain from asserting the ref_value_output signal for synchronization after receiving the Start of Frame (SoF) packet.
    
    Signed-off-by: Mubashir <mubashir.impare@gmail.com>

---
+++ hw/ip/usbdev/dv/env/seq_lib/usbdev_phy_config_usb_ref_disable_vseq.sv
@@ -0,0 +1,27 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class usbdev_phy_config_usb_ref_disable_vseq extends usbdev_base_vseq;
+  `uvm_object_utils(usbdev_phy_config_usb_ref_disable_vseq)
+
+  `uvm_object_new
+
+  task body();
+    configure_out_trans();
+    // Tx Start of Frame (SoF) packet to the device.
+    // The device will receive the SoF and in response, it will assert the usb_ref_val_o signal.
+    call_sof_seq(PidTypeSofToken);
+    inter_packet_delay();
+    // Verify that the device has asserted the usb_ref_val_o signal after receiving the SoF packet.
+    `DV_CHECK_EQ(cfg.m_usb20_agent_cfg.bif.usb_ref_val_o, 1)
+    // De-assert usb_ref_disable signal from phy_config register.
+    csr_wr(.ptr(ral.phy_config.usb_ref_disable), .value(1'b1));
+    // Check that usb_ref_val_o is zero.
+    `DV_CHECK_EQ(cfg.m_usb20_agent_cfg.bif.usb_ref_val_o, 0)
+    call_sof_seq(PidTypeSofToken);
+    // Confirm that after setting usb_ref_disable to 1,
+    // the device will not assert the synchronous usb_ref_val_o signal.
+    `DV_CHECK_EQ(cfg.m_usb20_agent_cfg.bif.usb_ref_val_o, 0)
+  endtask
+endclass
--- hw/ip/usbdev/dv/env/seq_lib/usbdev_vseq_list.sv
+++ hw/ip/usbdev/dv/env/seq_lib/usbdev_vseq_list.sv
@@ -17,6 +17,7 @@
 `include "usbdev_nak_trans_vseq.sv"
 `include "usbdev_out_stall_vseq.sv"
 `include "usbdev_out_trans_nak_vseq.sv"
+`include "usbdev_phy_config_usb_ref_disable_vseq.sv"
 `include "usbdev_phy_pins_sense_vseq.sv"
 `include "usbdev_pkt_buffer_vseq.sv"
 `include "usbdev_pkt_received_vseq.sv"
