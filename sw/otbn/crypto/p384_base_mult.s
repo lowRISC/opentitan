e0abfc9a58fa0d2a36986035d7f369bb3622de4d
Moritz Wettermann <moritz.wettermann@gi-de.com>
2024-03-26 11:07:04 +0100

    [crypto] Change P-384 pointer handling in base multiplication
    This commit removes dptr_<x> variables and adapts the code to function
    without it.
    
    Signed-off-by: Moritz Wettermann <moritz.wettermann@gi-de.com>

--- sw/otbn/crypto/p384_base_mult.s
+++ sw/otbn/crypto/p384_base_mult.s
@@ -19,12 +19,12 @@
  * Sets up context and calls the internal scalar multiplication routine.
  * This routine runs in constant time.
  *
- * @param[in]  dmem[0]: dptr_d0, pointer to location in dmem containing
- *                      1st private key share d0
- * @param[in]  dmem[4]: dptr_d1, pointer to location in dmem containing
- *                      2nd private key share d1
- * @param[in]  dmem[20]: dptr_x, pointer to result buffer for x-coordinate
- * @param[in]  dmem[24]: dptr_y, pointer to result buffer for y-coordinate
+ * @param[in]       x17: dptr_d0, pointer to location in dmem containing
+ *                                1st private key share d0
+ * @param[in]       x19: dptr_d1, pointer to location in dmem containing
+ *                                2nd private key share d1
+ * @param[out]  dmem[x]: x-coordinate in dmem
+ * @param[out]  dmem[y]: y-coordinate in dmem
  *
  * 384-bit quantities have to be provided in dmem in little-endian format,
  * 512 bit aligned, with the highest 128 bit set to zero.
@@ -44,14 +44,6 @@ p384_base_mult:
 
   /* set dmem pointer to y-coordinate of base point */
   la        x21, p384_gy
-
-  /* set dmem pointer to 1st scalar share d0 */
-  la        x17, dptr_d0
-  lw        x17, 0(x17)
-
-  /* set dmem pointer to 2nd scalar share d1 */
-  la        x19, dptr_d1
-  lw        x19, 0(x19)
 
   /* set dmem pointer to domain parameter b */
   la        x28, p384_b
@@ -84,12 +76,10 @@ p384_base_mult:
   jal       x1, proj_to_affine_p384
 
   /* set dmem pointer to point x-coordinate */
-  la        x20, dptr_x
-  lw        x20, 0(x20)
+  la        x20, x
 
   /* set dmem pointer to point y-coordinate */
-  la        x21, dptr_y
-  lw        x21, 0(x21)
+  la        x21, y
 
   /* store result in dmem */
   li        x2, 25
@@ -100,46 +90,22 @@ p384_base_mult:
 
   ret
 
-/* pointers and scratchpad memory */
+/* variables and scratchpad memory */
 .section .data
 
 .balign 32
 
-  /* pointer to k0 (dptr_k0) */
-.globl dptr_k0
-.weak dptr_k0
-dptr_k0:
-  .zero 4
+/* buffer for x-coordinate */
+.globl x
+.weak x
+x:
+  .zero 64
 
-/* pointer to k1 (dptr_k1) */
-.globl dptr_k1
-.weak dptr_k1
-dptr_k1:
-  .zero 4
-
-/* pointer to d0 (dptr_d0) */
-.globl dptr_d0
-.weak dptr_d0
-dptr_d0:
-  .zero 4
-
-/* pointer to d1 (dptr_d1) */
-.globl dptr_d1
-.weak dptr_d1
-dptr_d1:
-  .zero 4
-
-/* pointer to X (dptr_x) */
-.globl dptr_x
-.weak dptr_x
-dptr_x:
-  .zero 4
-
-/* pointer to Y (dptr_y) */
-.globl dptr_y
-.weak dptr_y
-dptr_y:
-  .zero 4
+/* buffer for y-coordinate */
+.globl y
+.weak y
+y:
+  .zero 64
 
 /* 704 bytes of scratchpad memory */
 .balign 32
