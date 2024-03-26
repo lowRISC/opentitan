f7964298f7fc8bf4891029c1e08cbb73b4851da4
James Wainwright <james.wainwright@lowrisc.org>
2024-03-26 16:07:01 +0000

    [spdx] Add SPDX-License-Identifiers for third party files
    These files were vendored in from other projects. Adding the SPDX lines
    makes it possible for automated tools to know what license they are
    distributed under without trying to parse the license text.
    
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- third_party/coremark/top_earlgrey/core_portme.h
+++ third_party/coremark/top_earlgrey/core_portme.h
@@ -2,6 +2,7 @@
 #define OPENTITAN_THIRD_PARTY_COREMARK_TOP_EARLGREY_CORE_PORTME_H_
 /*
 Copyright 2018 Embedded Microprocessor Benchmark Consortium (EEMBC)
+SPDX-License-Identifier: Apache-2.0
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
