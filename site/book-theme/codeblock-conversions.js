808506e32f602a8ee164efbd662caf19f6ed49b5
Hugo McNally <hugo.mcnally@gmail.com>
2024-03-29 20:02:14 +0000

    [doc] Added mermaid support and removed wavejson preprocessor
    Mermaid is a markup for drawing diagrams.
    This commit adds a renderer of the markup to the docs.
    
    wavejson codeblocks are now converted to WaveDrom scripts by embedded
    javascript.
    
    Signed-off-by: Hugo McNally <hugo.mcnally@gmail.com>

---
+++ site/book-theme/codeblock-conversions.js
@@ -0,0 +1,25 @@
+// Copyright lowRISC contributors.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+function convert_codeblocks() {
+  let replace_codeblock = (query, template_fn) => {
+    for (let instance of document.querySelectorAll(query)) {
+      let new_node = template_fn();
+      new_node.innerHTML = instance.innerHTML;
+      instance.parentElement.replaceWith(new_node);
+    };
+  };
+  replace_codeblock(".language-mermaid", () => {
+    let template = document.createElement("pre");
+    template.classList.add("mermaid");
+    return template;
+  });
+  replace_codeblock(".language-wavejson", () => {
+    let template = document.createElement("script");
+    template.type = "WaveDrom";
+    return template;
+  });
+}
+convert_codeblocks();
+window.addEventListener('load', WaveDrom.ProcessAll)
