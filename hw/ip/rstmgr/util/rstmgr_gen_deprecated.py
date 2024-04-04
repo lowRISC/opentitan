9f752f988a98f3fb16d395b3c3db8b98965fc783
Michael Munday <mike.munday@lowrisc.org>
2024-04-04 22:15:45 +0100

    Fix python lint issues
    Signed-off-by: Michael Munday <mike.munday@lowrisc.org>

--- hw/ip/rstmgr/util/rstmgr_gen_deprecated.py
+++ hw/ip/rstmgr/util/rstmgr_gen_deprecated.py
@@ -5,17 +5,14 @@
 r"""Reset Manager Generator
 """
 
-import argparse
 import logging as log
 import sys
-import subprocess
 from collections import OrderedDict
-from io import StringIO
 from pathlib import Path
 
 import hjson
-from mako import exceptions
 from mako.template import Template
+
 
 # Common header for generated files
 def main():
@@ -23,18 +20,18 @@ def main():
     current = Path(__file__).parent.absolute()
 
     hjson_tpl = Template(filename=str(current / '../data/rstmgr.hjson.tpl'))
-    rtl_tpl   = Template(filename=str(current / '../data/rstmgr.sv.tpl'))
-    pkg_tpl   = Template(filename=str(current / '../data/rstmgr_pkg.sv.tpl'))
+    rtl_tpl = Template(filename=str(current / '../data/rstmgr.sv.tpl'))
+    pkg_tpl = Template(filename=str(current / '../data/rstmgr_pkg.sv.tpl'))
 
     hjson_out = current / '../data/rstmgr.hjson'
-    rtl_out   = current / '../rtl/rstmgr.sv'
-    pkg_out   = current / '../rtl/rstmgr_pkg.sv'
+    rtl_out = current / '../rtl/rstmgr.sv'
+    pkg_out = current / '../rtl/rstmgr_pkg.sv'
 
-    cfgpath   = current / '../../../top_earlgrey/data/autogen/top_earlgrey.gen.hjson'
+    cfgpath = current / '../../../top_earlgrey/data/autogen/top_earlgrey.gen.hjson'
 
     try:
         with open(cfgpath, 'r') as cfg:
-            topcfg = hjson.load(cfg,use_decimal=True,object_pairs_hook=OrderedDict)
+            topcfg = hjson.load(cfg, use_decimal=True, object_pairs_hook=OrderedDict)
     except ValueError:
         log.error("{} not found".format(cfgpath))
         raise SystemExit(sys.exc_info()[1])
@@ -68,13 +65,13 @@ def main():
 
     # generate hjson
     hjson_out.write_text(
-         hjson_tpl.render(clks=clks,
-                          power_domains=topcfg['power']['domains'],
-                          num_rstreqs=n_rstreqs,
-                          sw_rsts=sw_rsts,
-                          output_rsts=output_rsts,
-                          leaf_rsts=leaf_rsts,
-                          export_rsts=topcfg['exported_rsts']))
+        hjson_tpl.render(clks=clks,
+                         power_domains=topcfg['power']['domains'],
+                         num_rstreqs=n_rstreqs,
+                         sw_rsts=sw_rsts,
+                         output_rsts=output_rsts,
+                         leaf_rsts=leaf_rsts,
+                         export_rsts=topcfg['exported_rsts']))
 
     # generate rtl package
     pkg_out.write_text(
@@ -88,13 +85,13 @@ def main():
 
     # generate top level
     rtl_out.write_text(
-         rtl_tpl.render(clks=clks,
-                        power_domains=topcfg['power']['domains'],
-                        num_rstreqs=n_rstreqs,
-                        sw_rsts=sw_rsts,
-                        output_rsts=output_rsts,
-                        leaf_rsts=leaf_rsts,
-                        export_rsts=topcfg['exported_rsts']))
+        rtl_tpl.render(clks=clks,
+                       power_domains=topcfg['power']['domains'],
+                       num_rstreqs=n_rstreqs,
+                       sw_rsts=sw_rsts,
+                       output_rsts=output_rsts,
+                       leaf_rsts=leaf_rsts,
+                       export_rsts=topcfg['exported_rsts']))
 
 
 if __name__ == "__main__":
