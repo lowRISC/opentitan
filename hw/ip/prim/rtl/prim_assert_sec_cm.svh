45ad84c34d9dac0b0abfea64306a43254325dfb1
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-04-09 15:58:37 +0100

    [prim,fpv] Tidy up and document some FPV macros
    No functional change. The only change to the code is that
    ASSERT_ERROR_TRIGGER_ALERT can be defined in terms of
    ASSERT_ERROR_TRIGGER_ERR, so I've done that to avoid the duplicated
    code.
    
    The main change is a few lines of documentation comment that explain
    exactly what the macros are actually doing.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/prim/rtl/prim_assert_sec_cm.svh
+++ hw/ip/prim/rtl/prim_assert_sec_cm.svh
@@ -9,24 +9,35 @@
 
 `define _SEC_CM_ALERT_MAX_CYC 30
 
-// Helper macros
-`define ASSERT_ERROR_TRIGGER_ALERT(NAME_, PRIM_HIER_, ALERT_, GATE_, MAX_CYCLES_, ERR_NAME_) \
-  `ASSERT(FpvSecCm``NAME_``, \
-          $rose(PRIM_HIER_.ERR_NAME_) && !(GATE_) \
-          |-> ##[0:MAX_CYCLES_] (ALERT_.alert_p)) \
-  `ifdef INC_ASSERT \
-  assign PRIM_HIER_.unused_assert_connected = 1'b1; \
-  `endif \
-  `ASSUME_FPV(``NAME_``TriggerAfterAlertInit_S, $stable(rst_ni) == 0 |-> \
-              PRIM_HIER_.ERR_NAME_ == 0 [*10])
-
+// When a named error signal rises, expect to see an associated error in at most MAX_CYCLES_ cycles.
+//
+// The NAME_ argument gets included in the name of the generated assertion, following an FpSecCm
+// prefix. The error signal should be at PRIM_HIER_.ERR_NAME_ and the posedge is ignored if GATE_ is
+// true.
+//
+// This macro drives a magic "unused_assert_connected" signal, which is used for a static check to
+// ensure the assertions are in place.
 `define ASSERT_ERROR_TRIGGER_ERR(NAME_, PRIM_HIER_, ERR_, GATE_, MAX_CYCLES_, ERR_NAME_, CLK_, RST_) \
-  `ASSERT(FpvSecCm``NAME_``, \
-          $rose(PRIM_HIER_.ERR_NAME_) && !(GATE_) \
-          |-> ##[0:MAX_CYCLES_] (ERR_), CLK_, RST_) \
-  `ifdef INC_ASSERT \
-  assign PRIM_HIER_.unused_assert_connected = 1'b1; \
+  `ASSERT(FpvSecCm``NAME_``,                                                                         \
+          $rose(PRIM_HIER_.ERR_NAME_) && !(GATE_) |-> ##[0:MAX_CYCLES_] (ERR_),                      \
+          CLK_, RST_)                                                                                \
+  `ifdef INC_ASSERT                                                                                  \
+    assign PRIM_HIER_.unused_assert_connected = 1'b1;                                                \
   `endif
+
+// When an error signal rises, expect to see the associated alert in at most MAX_CYCLE_ cycles.
+//
+// The NAME_, PRIM_HIER_, GATE_, MAX_CYCLES_ and ERR_NAME_ arguments are the same as for
+// `ASSERT_ERROR_TRIGGER_ERR. The ALERT_ argument is the name of the alert that we expect to be
+// asserted.
+//
+// This macro adds an assumption that says the named error signal will stay low for the first 10
+// cycles after reset.
+`define ASSERT_ERROR_TRIGGER_ALERT(NAME_, PRIM_HIER_, ALERT_, GATE_, MAX_CYCLES_, ERR_NAME_)    \
+  `ASSERT_ERROR_TRIGGER_ERR(NAME_, PRIM_HIER_, (ALERT_.alert_p), GATE_, MAX_CYCLES_, ERR_NAME_, \
+                            `ASSERT_DEFAULT_CLK, `ASSERT_DEFAULT_RST)                           \
+  `ASSUME_FPV(``NAME_``TriggerAfterAlertInit_S,                                                 \
+              $stable(rst_ni) == 0 |-> PRIM_HIER_.ERR_NAME_ == 0 [*10])
 
 // macros for security countermeasures that will trigger alert
 `define ASSERT_PRIM_COUNT_ERROR_TRIGGER_ALERT(NAME_, PRIM_HIER_, ALERT_, GATE_ = 0, MAX_CYCLES_ = `_SEC_CM_ALERT_MAX_CYC) \
