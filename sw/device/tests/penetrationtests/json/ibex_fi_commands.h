bc5491d3780d3a8881ae0becc363156b45dc425f
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-04-15 15:02:45 +0200

    [pentest] Add SRAM Write Static test
    This commit adds the ibex.fi.char.sram_write_static FI test to the
    codebase. The aim of this test is to fault one of the 64 consecutive
    SW instructions used to write a static value into SRAM. The host
    code is located in lowRISC/ot-sca#359
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/tests/penetrationtests/json/ibex_fi_commands.h
+++ sw/device/tests/penetrationtests/json/ibex_fi_commands.h
@@ -22,6 +22,7 @@ extern "C" {
     value(_, CharCondBranch) \
     value(_, CharUncondBranch) \
     value(_, CharSramWrite) \
+    value(_, CharSramWriteStatic) \
     value(_, CharSramRead) \
     value(_, CharSramStatic) \
     value(_, CharFlashWrite) \
