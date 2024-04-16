f1bd0be4dfd77982d99db15c0cd21aae70e8dbb3
Your Name <you@example.com>
2024-04-16 18:57:32 +0200

    Update also carfield idma test

--- sw/tests/carfield/idma_test/idma_test.c
+++ sw/tests/carfield/idma_test/idma_test.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -9,40 +10,53 @@
 #define L2_BASE        0x78000000
 #define L3_BASE        0x80000000
 
-#define IDMA_SRC_ADDR_OFFSET         0x00
-#define IDMA_DST_ADDR_OFFSET         0x04
-#define IDMA_LENGTH_OFFSET           0x08
-#define IDMA_NEXT_ID_OFFSET          0x20
-#define IDMA_REPS_2_OFFSET           0x18
+#define IDMA_SRC_ADDR_OFFSET         0x000000d8
+#define IDMA_DST_ADDR_OFFSET         0x000000d0
+#define IDMA_LENGTH_OFFSET           0x000000e0
+#define IDMA_NEXT_ID_OFFSET          0x00000044
+#define IDMA_REPS_2                  0x000000f8
+#define IDMA_REPS_3                  0x00000110
+#define IDMA_CONF                    0x00000000
+#define EOC                          0xc11c0018
 
 int main() {
 
   int volatile  * ptr;
   int buff,b;
   int err = 0;
-  for(int i = 0; i<100; i++){
+
+  for(int i = 0; i<1024; i++){
     ptr = (int *) TCDM_BASE + i*4;
     *ptr = i;
   }
+
   ptr = (int *) (IDMA_BASE + IDMA_SRC_ADDR_OFFSET);
   *ptr = TCDM_BASE;
   ptr = (int *) (IDMA_BASE + IDMA_DST_ADDR_OFFSET);
   *ptr = L2_BASE;
   ptr = (int *) (IDMA_BASE + IDMA_LENGTH_OFFSET);
   *ptr = 0x00000190;
+  ptr = (int *) (IDMA_BASE + IDMA_CONF);
+  *ptr = 0x3<<10;
+  ptr = (int *) (IDMA_BASE + IDMA_REPS_2);
+  *ptr = 0x00000001;
   ptr = (int *) (IDMA_BASE + IDMA_NEXT_ID_OFFSET);
   buff = *ptr;
-  for(int i = 0; i<100; i++){
+
+  for(int i = 0; i<1024; i++){
     ptr = (int *) L2_BASE + i;
+
     b = *ptr;
-    if(b!=i)
+    if(b=!i)
       err++;
   }
+
   if(err!=0){
-    ptr = (int *) 0xc11c0018;
+    ptr = (int *) EOC;
     *ptr = 0xFFFFFFFF;
-    while(1);
+    return -1;
   }
   else
     return 0;
+
 }
