f0a518b6bb9161d6f1af3ae3baab6b78c49608d8
Tim Trippel <ttrippel@google.com>
2024-04-18 11:18:12 -0700

    [dice] refactor data passed to cert build functions
    This refactors the DICE library to pass the ROM_EXT and Owner
    measurements directly into the cert build functions, rather than passing
    a ujson struct around that contained all measurements to make the DICE
    library reusable in the ROM_EXT for updating certificates.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>
    (cherry picked from commit 58f52133aff5a24857c7c1146f0e86271fbcc0a1)

--- sw/host/provisioning/ujson_lib/src/provisioning_command.rs
+++
@@ -1,6 +0,0 @@
-// Copyright lowRISC contributors (OpenTitan project).
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-
-// Bring in the auto-generated sources.
-include!(env!("provisioning_command"));
