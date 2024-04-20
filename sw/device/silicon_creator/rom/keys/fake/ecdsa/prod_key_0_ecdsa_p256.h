db1b3886f713d19a7fb4e12e5f62687e455a27d2
Miguel Osorio <miguelosorio@google.com>
2024-04-19 22:06:25 -0700

    [rom] Add ECDSA fake keys to OTP auth partition.
    Adds ECDSA keys to fake auth partition. Also adds header version of the
    public keys to simplify auditing of key values.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

---
+++ sw/device/silicon_creator/rom/keys/fake/ecdsa/prod_key_0_ecdsa_p256.h
@@ -0,0 +1,15 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_KEYS_FAKE_ECDSA_PROD_KEY_0_ECDSA_P256_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_KEYS_FAKE_ECDSA_PROD_KEY_0_ECDSA_P256_H_
+
+#define PROD_KEY_0_ECDSA_P256                                               \
+  {                                                                         \
+    0x9bf2dafd, 0x38daf96b, 0x0fa3a5a4, 0x28a525c1, 0x0eeeff00, 0x923499d9, \
+        0xe6557b33, 0x8277988c, 0x1ddda9bd, 0x0e3cd209, 0xef6d9014,         \
+        0x611a0325, 0x734ebd05, 0x0a5b3c2e, 0x866f12e0, 0x7e571d04,         \
+  }
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_KEYS_FAKE_ECDSA_PROD_KEY_0_ECDSA_P256_H_
