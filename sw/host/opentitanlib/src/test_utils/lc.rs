c1e517df52d20acbe09fa8a74c2f6255e4655f1e
Tim Trippel <ttrippel@google.com>
2024-04-22 13:51:26 -0700

    [manuf] increase reliability of lc read testutil
    The LC read test utility was not waiting for the lc_ctrl to be ready to
    receive transactions before reading the LC state. This increases the
    reliability of this utility, and test cases that use it, specifically,
    the `ft_provision` test case.
    
    Additionally, this move the `ft_provision` silicon exec_env to the
    `teacup` interface (a minor cleanup).
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/host/opentitanlib/src/test_utils/lc.rs
+++ sw/host/opentitanlib/src/test_utils/lc.rs
@@ -7,8 +7,9 @@ use std::time::Duration;
 use anyhow::Result;
 
 use crate::app::TransportWrapper;
-use crate::dif::lc_ctrl::{DifLcCtrlState, LcCtrlReg};
+use crate::dif::lc_ctrl::{DifLcCtrlState, LcCtrlReg, LcCtrlStatus};
 use crate::io::jtag::{JtagParams, JtagTap};
+use crate::test_utils::lc_transition::wait_for_status;
 
 pub fn read_lc_state(
     transport: &TransportWrapper,
@@ -22,6 +23,12 @@ pub fn read_lc_state(
     transport.pin_strapping("ROM_BOOTSTRAP")?.apply()?;
     transport.reset_target(reset_delay, true)?;
     let mut jtag = jtag_params.create(transport)?.connect(JtagTap::LcTap)?;
+    // We must wait for the lc_ctrl to initialize before the LC state is exposed.
+    wait_for_status(
+        &mut *jtag,
+        Duration::from_secs(1),
+        LcCtrlStatus::INITIALIZED,
+    )?;
     let raw_lc_state = jtag.read_lc_ctrl_reg(&LcCtrlReg::LcState)?;
     jtag.disconnect()?;
     transport.pin_strapping("PINMUX_TAP_LC")?.remove()?;
