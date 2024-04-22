4045c23a899941d5a669856269d01ebc0a11a8d4
Tim Trippel <ttrippel@google.com>
2024-04-22 12:25:21 -0700

    [manuf] provision TPM attestation key seeds in flash info
    This fixes #22623.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/device/silicon_creator/manuf/lib/flash_info_fields.c
+++ sw/device/silicon_creator/manuf/lib/flash_info_fields.c
@@ -83,6 +83,27 @@ const flash_info_field_t kFlashInfoFieldCdi1AttestationKeySeed = {
     .byte_offset = (2 * kAttestationSeedBytes),
 };
 
+const flash_info_field_t kFlashInfoFieldTpmEkAttestationKeySeed = {
+    .partition = 0,
+    .bank = 0,
+    .page = 4,
+    .byte_offset = (3 * kAttestationSeedBytes),
+};
+
+const flash_info_field_t kFlashInfoFieldTpmCekAttestationKeySeed = {
+    .partition = 0,
+    .bank = 0,
+    .page = 4,
+    .byte_offset = (4 * kAttestationSeedBytes),
+};
+
+const flash_info_field_t kFlashInfoFieldTpmCikAttestationKeySeed = {
+    .partition = 0,
+    .bank = 0,
+    .page = 4,
+    .byte_offset = (5 * kAttestationSeedBytes),
+};
+
 status_t manuf_flash_info_field_read(dif_flash_ctrl_state_t *flash_state,
                                      flash_info_field_t field,
                                      uint32_t *data_out, size_t num_words) {
