704e91fa25f3b021662b856ac5f1d90ba3ef347d
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-04-24 15:41:27 +0100

    [fpv,prim] Generalise from DecrNeverTrue to listing possible actions
    The existing DecrNeverTrue parameter was a nice way to avoid missing
    cover points when we knew we would never drive a particular
    action (using decr_en_i). It turns out that other instantiation sites
    might wire other actions to constant zero.
    
    Rather than add a parameter per action, this commit makes things a bit
    more general. The equivalent of passing DecrNeverTrue is to pass Clr |
    Set | Incr (and not Decr).
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/top_earlgrey/ip_autogen/alert_handler/rtl/alert_handler_esc_timer.sv
+++ hw/top_earlgrey/ip_autogen/alert_handler/rtl/alert_handler_esc_timer.sv
@@ -58,9 +58,11 @@ module alert_handler_esc_timer import alert_pkg::*; (
     // The alert handler behaves differently than other comportable IP. I.e., instead of sending out
     // an alert signal, this condition is handled internally in the alert handler.
     .EnableAlertTriggerSVA(0),
-    // Pass a parameter to disable some assertions that are unreachable because decr_en_i is tied to
-    // zero.
-    .DecrNeverTrue(1)
+    // Pass a parameter to disable coverage for some assertions that are unreachable because
+    // decr_en_i is tied to zero.
+    .PossibleActions(prim_count_pkg::Clr |
+                     prim_count_pkg::Set |
+                     prim_count_pkg::Incr)
   ) u_prim_count (
     .clk_i,
     .rst_ni,
