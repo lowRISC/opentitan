e53e9d63653a10315602d1ddacd39e8db6c13fca
Tim Trippel <ttrippel@google.com>
2024-04-26 14:05:22 -0700

    [rom] cleanup comments in `rom_verify` functions
    Cleans up `rom_verify()` comments to better explain what code is doing
    to ease auditing.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/device/silicon_creator/rom/rom.c
+++ sw/device/silicon_creator/rom/rom.c
@@ -237,8 +237,13 @@ static rom_error_t rom_init(void) {
 OT_WARN_UNUSED_RESULT
 static rom_error_t rom_verify(const manifest_t *manifest,
                               uint32_t *flash_exec) {
-  // Used for invalidating signatures if the security version of the manifest is
-  // smaller than the minimum required security version.
+  /**
+   * Check security version and manifest constraints.
+   *
+   * The poisoning work (`anti_rollback`) invalidates signatures if the security
+   * version of the manifest is smaller than the minimum required security
+   * version.
+   */
   const uint32_t extra_word = UINT32_MAX;
   const uint32_t *anti_rollback = NULL;
   size_t anti_rollback_len = 0;
@@ -247,22 +252,29 @@ static rom_error_t rom_verify(const manifest_t *manifest,
     anti_rollback = &extra_word;
     anti_rollback_len = sizeof(extra_word);
   }
-
-  HARDENED_RETURN_IF_ERROR(otbn_boot_app_load());
-
-  sigverify_otp_key_ctx_t sigverify_ctx;
-  HARDENED_RETURN_IF_ERROR(sigverify_otp_keys_init(&sigverify_ctx));
-
-  CFI_FUNC_COUNTER_INCREMENT(rom_counters, kCfiRomVerify, 1);
   *flash_exec = 0;
   HARDENED_RETURN_IF_ERROR(boot_policy_manifest_check(manifest, &boot_data));
 
+  /**
+   * Load OTBN boot services app.
+   *
+   * This will be reused by later boot stages.
+   */
+  HARDENED_RETURN_IF_ERROR(otbn_boot_app_load());
+  CFI_FUNC_COUNTER_INCREMENT(rom_counters, kCfiRomVerify, 1);
+
+  /**
+   * Load secure boot keys from OTP into RAM.
+   */
+  sigverify_otp_key_ctx_t sigverify_ctx;
+  HARDENED_RETURN_IF_ERROR(sigverify_otp_keys_init(&sigverify_ctx));
+  // ECDSA key.
   const sigverify_ecdsa_p256_buffer_t *ecdsa_key = NULL;
   HARDENED_RETURN_IF_ERROR(sigverify_ecdsa_p256_key_get(
       &sigverify_ctx,
       sigverify_ecdsa_p256_key_id_get(&manifest->ecdsa_public_key), lc_state,
       &ecdsa_key));
-
+  // SPX+ key.
   const sigverify_spx_key_t *spx_key = NULL;
   const sigverify_spx_signature_t *spx_signature = NULL;
   uint32_t sigverify_spx_en = sigverify_spx_verify_enabled(lc_state);
@@ -272,7 +284,6 @@ static rom_error_t rom_verify(const manifest_t *manifest,
     HARDENED_RETURN_IF_ERROR(sigverify_spx_key_get(
         &sigverify_ctx, sigverify_spx_key_id_get(&ext_spx_key->key), lc_state,
         &spx_key));
-
     const manifest_ext_spx_signature_t *ext_spx_signature;
     HARDENED_RETURN_IF_ERROR(
         manifest_ext_get_spx_signature(manifest, &ext_spx_signature));
@@ -281,35 +292,42 @@ static rom_error_t rom_verify(const manifest_t *manifest,
     HARDENED_CHECK_EQ(sigverify_spx_en, kSigverifySpxDisabledOtp);
   }
 
+  /**
+   * Measure ROM_EXT and portions of manifest via SHA256 digest.
+   */
+  // Initialize ROM_EXT measurement in .static_critical with garbage.
   memset(boot_measurements.rom_ext.data, (int)rnd_uint32(),
          sizeof(boot_measurements.rom_ext.data));
-
+  // Add anti-rollback poisoning word to measurement.
   hmac_sha256_init();
   hmac_sha256_update(anti_rollback, anti_rollback_len);
   HARDENED_CHECK_GE(manifest->security_version,
                     boot_data.min_security_version_rom_ext);
-
-  // Hash usage constraints.
+  // Add manifest usage constraints to the measurement.
   manifest_usage_constraints_t usage_constraints_from_hw;
   sigverify_usage_constraints_get(manifest->usage_constraints.selector_bits,
                                   &usage_constraints_from_hw);
   hmac_sha256_update(&usage_constraints_from_hw,
                      sizeof(usage_constraints_from_hw));
-  // Hash the remaining part of the image.
+  // Add remaining part of manifest / ROM_EXT image to the measurement.
   manifest_digest_region_t digest_region = manifest_digest_region_get(manifest);
-
+  // Add remaining part of manifest / ROM_EXT image to the measurement.
   hmac_sha256_update(digest_region.start, digest_region.length);
-  // Verify signature
   hmac_digest_t act_digest;
   hmac_sha256_final(&act_digest);
-
+  // Copy the ROM_EXT measurement to the .static_critical section.
   static_assert(sizeof(boot_measurements.rom_ext) == sizeof(act_digest),
                 "Unexpected ROM_EXT digest size.");
   memcpy(&boot_measurements.rom_ext, &act_digest,
          sizeof(boot_measurements.rom_ext));
 
   CFI_FUNC_COUNTER_INCREMENT(rom_counters, kCfiRomVerify, 2);
-  // Swap the order of signature verifications randomly.
+
+  /**
+   * Verify the RSA/SPX+ signatures of ROM_EXT.
+   *
+   * We swap the order of signature verifications randomly.
+   */
   *flash_exec = 0;
   if (rnd_uint32() < 0x80000000) {
     HARDENED_RETURN_IF_ERROR(sigverify_ecdsa_p256_verify(
