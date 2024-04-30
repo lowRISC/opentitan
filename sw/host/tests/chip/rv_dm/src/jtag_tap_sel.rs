dd040af6c7e4fd4850e3cedaca0b11314ee56229
Gary Guo <gary.guo@lowrisc.org>
2024-04-30 12:54:11 +0100

    [sival, rv_dm] improve jtag_tap_sel to support multiple lc states
    This test now tests PROD states and check in RMA state the tap is
    continuously sampled. This makes it cover chip_sw_tap_strap_sampling
    test.
    
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

--- sw/host/tests/chip/rv_dm/src/jtag_tap_sel.rs
+++ sw/host/tests/chip/rv_dm/src/jtag_tap_sel.rs
@@ -42,54 +42,103 @@ fn test_jtag_tap_sel(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
     let lc_state =
         DifLcCtrlState::from_redundant_encoding(jtag.read_lc_ctrl_reg(&LcCtrlReg::LcState)?)?;
 
+    // For test_unlocked* and RMA state, test that we can switch TAP without reset.
+    let (needs_reset, rv_dm_enabled) = match lc_state {
+        DifLcCtrlState::TestUnlocked0
+        | DifLcCtrlState::TestUnlocked1
+        | DifLcCtrlState::TestUnlocked2
+        | DifLcCtrlState::TestUnlocked3
+        | DifLcCtrlState::TestUnlocked4
+        | DifLcCtrlState::TestUnlocked5
+        | DifLcCtrlState::TestUnlocked6
+        | DifLcCtrlState::TestUnlocked7
+        | DifLcCtrlState::Rma => (false, true),
+        DifLcCtrlState::Dev => (true, true),
+        _ => (true, false),
+    };
+
     jtag.disconnect()?;
     transport.pin_strapping("PINMUX_TAP_LC")?.remove()?;
 
     // Test RISC-V TAP
     transport.pin_strapping("PINMUX_TAP_RISCV")?.apply()?;
-    transport.reset_target(opts.init.bootstrap.options.reset_delay, true)?;
+    if needs_reset {
+        transport.reset_target(opts.init.bootstrap.options.reset_delay, true)?;
+    }
 
-    let mut jtag = opts
+    let jtag = opts
         .init
         .jtag_params
         .create(transport)?
-        .connect(JtagTap::RiscvTap)?;
-    let mut lc_state_rv = 0;
-    jtag.read_memory32(
-        top_earlgrey::LC_CTRL_BASE_ADDR as u32 + LcCtrlReg::LcState as u32,
-        core::slice::from_mut(&mut lc_state_rv),
-    )?;
-    let lc_state_rv = DifLcCtrlState::from_redundant_encoding(lc_state_rv)?;
-    assert_eq!(lc_state, lc_state_rv);
+        .connect(JtagTap::RiscvTap);
 
-    jtag.disconnect()?;
+    assert_eq!(rv_dm_enabled, jtag.is_ok());
+
+    if let Ok(mut jtag) = jtag {
+        let mut lc_state_rv = 0;
+        jtag.read_memory32(
+            top_earlgrey::LC_CTRL_BASE_ADDR as u32 + LcCtrlReg::LcState as u32,
+            core::slice::from_mut(&mut lc_state_rv),
+        )?;
+        let lc_state_rv = DifLcCtrlState::from_redundant_encoding(lc_state_rv)?;
+        assert_eq!(lc_state, lc_state_rv);
+        jtag.disconnect()?;
+    }
+
     transport.pin_strapping("PINMUX_TAP_RISCV")?.remove()?;
 
     // Test DFT TAP
     transport.pin_strapping("PINMUX_TAP_DFT")?.apply()?;
-    transport.reset_target(opts.init.bootstrap.options.reset_delay, true)?;
+    if needs_reset {
+        transport.reset_target(opts.init.bootstrap.options.reset_delay, true)?;
+    }
 
-    // For DFT test we need to do raw OpenOCD command.
+    // If RV-DM is disabled, strapping DFT tap will result in LC TAP being selected.
+    if !rv_dm_enabled {
+        opts.init
+            .jtag_params
+            .create(transport)?
+            .connect(JtagTap::LcTap)?
+            .disconnect()?;
+    } else {
+        // For DFT test we need to do raw OpenOCD command.
+        let mut openocd = opts.init.jtag_params.create(transport)?.into_raw()?;
+        // Perform JTAG initialisation and capture the result.
+        let init_result = openocd.execute("capture \"jtag init\"")?;
+
+        static ID_REGEX: Lazy<Regex> =
+            Lazy::new(|| Regex::new(r"tap/device found: 0x([0-9A-Fa-f]+) \(").unwrap());
+        let idcode = if init_result.contains("JTAG scan chain interrogation failed") {
+            None
+        } else {
+            Some(u32::from_str_radix(
+                ID_REGEX
+                    .captures(&init_result)
+                    .context("Failed to parse IDCODE from OpenOCD output")?
+                    .get(1)
+                    .unwrap()
+                    .as_str(),
+                16,
+            )?)
+        };
+
+        // For everything other than test_unlocked* and RMA states, the DFT TAP should not exist.
+        let expected_idcode = if needs_reset { opts.dft_idcode } else { None };
+        assert_eq!(idcode, expected_idcode);
+        openocd.shutdown()?;
+    }
+
+    transport.pin_strapping("PINMUX_TAP_DFT")?.remove()?;
+    if needs_reset {
+        transport.reset_target(opts.init.bootstrap.options.reset_delay, true)?;
+    }
+
+    // Now check that there's no JTAG TAP present.
     let mut openocd = opts.init.jtag_params.create(transport)?.into_raw()?;
     // Perform JTAG initialisation and capture the result.
     let init_result = openocd.execute("capture \"jtag init\"")?;
-
-    static ID_REGEX: Lazy<Regex> =
-        Lazy::new(|| Regex::new(r"tap/device found: 0x([0-9A-Fa-f]+) \(").unwrap());
-    let idcode = if init_result.contains("JTAG scan chain interrogation failed") {
-        None
-    } else {
-        Some(u32::from_str_radix(
-            ID_REGEX
-                .captures(&init_result)
-                .context("Failed to parse IDCODE from OpenOCD output")?
-                .get(1)
-                .unwrap()
-                .as_str(),
-            16,
-        )?)
-    };
-    assert_eq!(idcode, opts.dft_idcode);
+    assert!(init_result.contains("JTAG scan chain interrogation failed"));
+    openocd.shutdown()?;
 
     Ok(())
 }
