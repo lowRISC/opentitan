a5e0142acda09e30cbff48c5720bc7b54c77ac65
Vadim Bendebury <vbendeb@chromium.org>
2024-05-01 12:18:29 -0700

    [personalization] unify certificate templates
    Synchronize TPM and UDS certificates to point to the same CA Issuer
    values. Add TPM certificates to the lists of generated code components
    where appropriate. Update the UDS certificate sizes to reflect the
    larger Issuer section.
    
    Tested by successful 'openssl verify ...' ran after the entire
    patchset is merged.
    
    Signed-off-by: Vadim Bendebury <vbendeb@chromium.org>

--- sw/device/lib/testing/json/provisioning_data.h
+++ sw/device/lib/testing/json/provisioning_data.h
@@ -99,16 +99,22 @@ UJSON_SERDE_STRUCT(ManufCertgenInputs, \
  * DICE certificates exported during personalization.
  */
 // clang-format off
-#define STRUCT_MANUF_DICE_CERTS(field, string) \
-    field(uds_tbs_certificate, uint8_t, 714) \
+#define STRUCT_MANUF_PERSO_CERTS(field, string) \
+    field(uds_tbs_certificate, uint8_t, 880) \
     field(uds_tbs_certificate_size, size_t) \
     field(cdi_0_certificate, uint8_t, 582) \
     field(cdi_0_certificate_size, size_t) \
     field(cdi_1_certificate, uint8_t, 631) \
-    field(cdi_1_certificate_size, size_t)
-UJSON_SERDE_STRUCT(ManufDiceCerts, \
-                   manuf_dice_certs_t, \
-                   STRUCT_MANUF_DICE_CERTS);
+    field(cdi_1_certificate_size, size_t) \
+    field(tpm_ek_tbs_certificate, uint8_t, 714) \
+    field(tpm_ek_tbs_certificate_size, size_t) \
+    field(tpm_cek_tbs_certificate, uint8_t, 714) \
+    field(tpm_cek_tbs_certificate_size, size_t) \
+    field(tpm_cik_tbs_certificate, uint8_t, 714) \
+    field(tpm_cik_tbs_certificate_size, size_t)
+UJSON_SERDE_STRUCT(ManufCerts, \
+                   manuf_certs_t, \
+                   STRUCT_MANUF_PERSO_CERTS);
 // clang-format on
 
 /**
@@ -116,8 +122,14 @@ UJSON_SERDE_STRUCT(ManufDiceCerts, \
  */
 // clang-format off
 #define STRUCT_MANUF_ENDORSED_CERTS(field, string) \
-    field(uds_certificate, uint8_t, 805) \
-    field(uds_certificate_size, size_t)
+    field(uds_certificate, uint8_t, 870) \
+    field(uds_certificate_size, size_t) \
+    field(tpm_ek_certificate, uint8_t, 714) \
+    field(tpm_ek_certificate_size, size_t) \
+    field(tpm_cek_certificate, uint8_t, 714) \
+    field(tpm_cek_certificate_size, size_t) \
+    field(tpm_cik_certificate, uint8_t, 714) \
+    field(tpm_cik_certificate_size, size_t)
 UJSON_SERDE_STRUCT(ManufEndorsedCerts, \
                    manuf_endorsed_certs_t, \
                    STRUCT_MANUF_ENDORSED_CERTS);
