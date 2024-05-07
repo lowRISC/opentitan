64f8a0a26ce67841e6a0cfc70db916199501ff71
Chris Frantz <cfrantz@google.com>
2024-05-06 17:50:05 -0700

    [rom_ext_e2e] More ownership transfer tests
    The tests added by this change are all simple variations on
    the ownership transfer test added in #24419.  They involve verifying the
    different modes and error conditions (e.g. using the wrong key).
    
    Adds the following tests:
    - `bad_unlock_test`; Fixes #24466
    - `bad_activate_test`; Fixes #24467
    - `bad_owner_block_test`; Fixes #24468
    - `bad_app_key_test`; Fixes #24469
    - `transfer_endorsed_test`; Fixes #24470
    - `bad_endorsee_test`; Fixes #24471
    - `locked_update_test`; Fixes #24472
    - `bad_locked_update_test` & `bad_locked_update_no_exec_test`; Fixes #24473
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>
    (cherry picked from commit 4d520bd7efa5399f54cafe756bcc08e58d20b23e)

--- sw/host/tests/ownership/transfer_test.rs
+++ sw/host/tests/ownership/transfer_test.rs
@@ -3,12 +3,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #![allow(clippy::bool_assert_comparison)]
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
+use regex::Regex;
 use std::path::PathBuf;
 use std::rc::Rc;
 use std::time::Duration;
 
+use opentitanlib::app::TransportWrapper;
+use opentitanlib::chip::boot_svc::UnlockMode;
+use opentitanlib::chip::rom_error::RomError;
 use opentitanlib::rescue::serial::RescueSerial;
 use opentitanlib::test_utils::init::InitializeTest;
 use opentitanlib::uart::console::UartConsole;
@@ -23,62 +27,134 @@ struct Opts {
     timeout: Duration,
     #[arg(long, help = "Unlock private key (ECDSA P256)")]
     unlock_key: PathBuf,
+    #[arg(long, help = "Activate private key (ECDSA P256)")]
+    activate_key: Option<PathBuf>,
     #[arg(long, help = "Next Owner private key (ECDSA P256)")]
     next_owner_key: PathBuf,
+    #[arg(long, help = "Next Owner public key (ECDSA P256)")]
+    next_owner_key_pub: Option<PathBuf>,
     #[arg(long, help = "Next Owner activate private key (ECDSA P256)")]
     next_activate_key: PathBuf,
     #[arg(long, help = "Next Owner unlock private key (ECDSA P256)")]
     next_unlock_key: PathBuf,
     #[arg(long, help = "Next Owner's application public key (RSA3K)")]
     next_application_key: PathBuf,
+
+    #[arg(long, default_value_t = false, action = clap::ArgAction::Set, help = "Corrupt owner block signature")]
+    corrupt_owner_block_signature: bool,
+    #[arg(long, default_value_t = true, action = clap::ArgAction::Set, help = "Check the firmware boot in dual-owner mode")]
+    dual_owner_boot_check: bool,
+
+    #[arg(long, default_value = "Any", help = "Mode of the unlock operation")]
+    unlock_mode: UnlockMode,
+    #[arg(long, help = "Expected error condition")]
+    expected_error: Option<String>,
 }
 
-fn main() -> Result<()> {
-    let opts = Opts::parse();
-    opts.init.init_logging();
-
-    let transport = opts.init.init_target()?;
+fn transfer_test(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
     let uart = transport.uart("console")?;
     let rescue = RescueSerial::new(Rc::clone(&uart));
 
-    let data = transfer_lib::get_boot_log(&transport, &rescue)?;
-    transfer_lib::ownership_unlock_any(&transport, &rescue, data.rom_ext_nonce, &opts.unlock_key)?;
+    log::info!("###### Get Boot Log (1/2) ######");
+    let data = transfer_lib::get_boot_log(transport, &rescue)?;
+    log::info!("###### Ownership Unlock ######");
+    transfer_lib::ownership_unlock(
+        transport,
+        &rescue,
+        opts.unlock_mode,
+        data.rom_ext_nonce,
+        &opts.unlock_key,
+        if opts.unlock_mode == UnlockMode::Endorsed {
+            opts.next_owner_key_pub.as_deref()
+        } else {
+            None
+        },
+    )?;
 
+    log::info!("###### Upload Owner Block ######");
     transfer_lib::create_owner(
-        &transport,
+        transport,
         &rescue,
         &opts.next_owner_key,
         &opts.next_activate_key,
         &opts.next_unlock_key,
         &opts.next_application_key,
+        opts.corrupt_owner_block_signature,
     )?;
 
-    // At this point, the device should be unlocked and should have accepted the owner
-    // configuration.  Owner code should run and report the state as `UANY`.
-    transport.reset_target(Duration::from_millis(50), /*clear_uart=*/ true)?;
-    let capture = UartConsole::wait_for(
-        &*uart,
-        r"(?msR)ownership_state = UANY$.*ownership_transfers = (\d+)$.*PASS!$",
-        opts.timeout,
-    )?;
-    let transfers0 = capture[1].parse::<u32>()?;
+    let mut transfers0 = 0;
+    if opts.dual_owner_boot_check {
+        log::info!("###### Boot in Dual-Owner Mode ######");
+        // At this point, the device should be unlocked and should have accepted the owner
+        // configuration.  Owner code should run and report the ownership state.
+        transport.reset_target(Duration::from_millis(50), /*clear_uart=*/ true)?;
+        let capture = UartConsole::wait_for(
+            &*uart,
+            r"(?msR)ownership_state = (\w+)$.*ownership_transfers = (\d+)$.*PASS!$|BFV:([0-9A-Fa-f]{8})$",
+            opts.timeout,
+        )?;
+        if capture[0].starts_with("BFV") {
+            return RomError(u32::from_str_radix(&capture[3], 16)?).into();
+        }
+        match opts.unlock_mode {
+            UnlockMode::Any => assert_eq!(capture[1], "UANY"),
+            UnlockMode::Endorsed => assert_eq!(capture[1], "UEND"),
+            UnlockMode::Update => assert_eq!(capture[1], "LUPD"),
+            _ => return Err(anyhow!("Unexpected ownership state: {}", capture[1])),
+        }
+        transfers0 = capture[2].parse::<u32>()?;
+    }
 
-    let data = transfer_lib::get_boot_log(&transport, &rescue)?;
+    log::info!("###### Get Boot Log (2/2) ######");
+    let data = transfer_lib::get_boot_log(transport, &rescue)?;
+
+    log::info!("###### Ownership Activate Block ######");
     transfer_lib::ownership_activate(
-        &transport,
+        transport,
         &rescue,
         data.rom_ext_nonce,
-        &opts.next_activate_key,
+        opts.activate_key
+            .as_deref()
+            .unwrap_or(&opts.next_activate_key),
     )?;
 
+    log::info!("###### Boot After Transfer Complete ######");
     // After the activate command, the device should report the ownership state as `OWND`.
     transport.reset_target(Duration::from_millis(50), /*clear_uart=*/ true)?;
     let capture = UartConsole::wait_for(
         &*uart,
-        r"(?msR)ownership_state = OWND$.*ownership_transfers = (\d+)$.*PASS!$",
+        r"(?msR)ownership_state = (\w+)$.*ownership_transfers = (\d+)$.*PASS!$|BFV:([0-9A-Fa-f]{8})$",
         opts.timeout,
     )?;
-    let transfers1 = capture[1].parse::<u32>()?;
+    if capture[0].starts_with("BFV") {
+        return RomError(u32::from_str_radix(&capture[3], 16)?).into();
+    }
+    assert_eq!(capture[1], "OWND");
+    let transfers1 = capture[2].parse::<u32>()?;
     assert_eq!(transfers0 + 1, transfers1);
     Ok(())
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    opts.init.init_logging();
+    let transport = opts.init.init_target()?;
+
+    let result = transfer_test(&opts, &transport);
+    if let Some(error) = &opts.expected_error {
+        match result {
+            Ok(_) => Err(anyhow!("Ok when expecting {error:?}")),
+            Err(e) => {
+                let re = Regex::new(error).expect("regex");
+                if re.is_match(&e.to_string()) {
+                    log::info!("Got expected error code: {e}");
+                    Ok(())
+                } else {
+                    Err(anyhow!("Expected {error:?} but got {e}"))
+                }
+            }
+        }
+    } else {
+        result
+    }
 }
