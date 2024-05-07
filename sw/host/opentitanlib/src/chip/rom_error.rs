54dcdfddfe4b01edada1b9deb01a7adfc6484423
Chris Frantz <cfrantz@google.com>
2024-05-06 17:50:05 -0700

    [rom_ext_e2e] More ownership transfer tests
    The tests added by this change are all simple variations on
    the ownership transfer test added in #24419.  They involve verifying the
    different modes and error conditions (e.g. using the wrong key).
    
    Adds the following tests:
    - `bad_unlock_test`; Fixes #24466
    - `bad_activate_test`; Fixes #24467
    - `bad_owner_block_test`; Fixes #24468
    - `bad_app_key_test`; Fixes #24469
    - `transfer_endorsed_test`; Fixes #24470
    - `bad_endorsee_test`; Fixes #24471
    - `locked_update_test`; Fixes #24472
    - `bad_locked_update_test` & `bad_locked_update_no_exec_test`; Fixes #24473
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>
    (cherry picked from commit 4d520bd7efa5399f54cafe756bcc08e58d20b23e)

--- sw/host/opentitanlib/src/chip/rom_error.rs
+++ sw/host/opentitanlib/src/chip/rom_error.rs
@@ -3,5 +3,28 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::with_unknown;
+use std::error::Error;
 
 include!(env!("rom_error_enum"));
+
+impl Error for RomError {}
+
+impl From<RomError> for Result<(), RomError> {
+    fn from(error: RomError) -> Self {
+        if error == RomError::Ok {
+            Ok(())
+        } else {
+            Err(error)
+        }
+    }
+}
+
+impl From<RomError> for Result<(), anyhow::Error> {
+    fn from(error: RomError) -> Self {
+        if error == RomError::Ok {
+            Ok(())
+        } else {
+            Err(error.into())
+        }
+    }
+}
