0e7f40ba5d7855ea5005bc7de8f873094fc5c198
Douglas Reis <doreis@lowrisc.org>
2024-05-13 16:14:28 +0100

    [sival, spi_host] Fix pmod tests on teacup board
    The teacup board inverts the spi_host1 pins sd0 and sd1.
    This commit adds the pin mapping by platform so that the test can select
    the platform during runtime.
    
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- sw/device/lib/testing/spi_host_testutils.c
+++ sw/device/lib/testing/spi_host_testutils.c
@@ -5,9 +5,112 @@
 #include "sw/device/lib/testing/spi_host_testutils.h"
 
 #include "sw/device/lib/dif/dif_pinmux.h"
+#include "sw/device/lib/testing/pinmux_testutils.h"
 #include "sw/device/lib/testing/test_framework/check.h"
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+
+/**
+ * Define an spi pinmux configuration.
+ */
+typedef struct spi_host1_pinmux_pins {
+  pinmux_testutils_mio_pin_t clk;
+  pinmux_testutils_mio_pin_t sd0;
+  pinmux_testutils_mio_pin_t sd1;
+  pinmux_testutils_mio_pin_t sd2;
+  pinmux_testutils_mio_pin_t sd3;
+} spi_host1_pinmux_pins_t;
+
+/**
+ * This table store spi host 1 pin mappings of different platforms.
+ * This is used to connect spi host 1 to mio pins based on the platform.
+ */
+static const spi_host1_pinmux_pins_t kSpiHost1PinmuxMap[] = {
+    [kSpiPinmuxPlatformIdCw310] =
+        {
+            .clk =
+                {
+                    .insel = kTopEarlgreyPinmuxInselConstantZero,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa3,
+                },
+            .sd0 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa5,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa5,
+                },
+            .sd1 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa4,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa4,
+                },
+            .sd2 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa8,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa8,
+                },
+            .sd3 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa7,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa7,
+                },
+        },
+    [kSpiPinmuxPlatformIdCw340] =
+        {
+            .clk =
+                {
+                    .insel = kTopEarlgreyPinmuxInselConstantZero,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa3,
+                },
+            .sd0 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa5,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa5,
+                },
+            .sd1 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa4,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa4,
+                },
+            .sd2 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa8,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa8,
+                },
+            .sd3 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa7,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa7,
+                },
+        },
+    [kSpiPinmuxPlatformIdTeacup] =
+        {
+            .clk =
+                {
+                    .insel = kTopEarlgreyPinmuxInselConstantZero,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa3,
+                },
+            .sd0 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa4,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa4,
+                },
+            .sd1 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa5,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa5,
+                },
+            .sd2 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa8,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa8,
+                },
+            .sd3 =
+                {
+                    .insel = kTopEarlgreyPinmuxInselIoa7,
+                    .mio_out = kTopEarlgreyPinmuxMioOutIoa7,
+                },
+        },
+};
 
 // `extern` declarations to give the inline functions in the
 // corresponding header a link location.
@@ -25,33 +128,37 @@ status_t spi_host_testutils_flush(dif_spi_host_t *spi_host) {
 }
 
 status_t spi_host1_pinmux_connect_to_bob(const dif_pinmux_t *pinmux,
-                                         dif_pinmux_index_t csb_outsel) {
+                                         dif_pinmux_index_t csb_outsel,
+                                         spi_pinmux_platform_id_t platform_id) {
+  TRY_CHECK(platform_id < kSpiPinmuxPlatformIdCount);
+  const spi_host1_pinmux_pins_t *platform = &kSpiHost1PinmuxMap[platform_id];
+
   // CSB.
   TRY(dif_pinmux_output_select(pinmux, csb_outsel,
                                kTopEarlgreyPinmuxOutselSpiHost1Csb));
   // SCLK.
-  TRY(dif_pinmux_output_select(pinmux, kTopEarlgreyPinmuxMioOutIoa3,
+  TRY(dif_pinmux_output_select(pinmux, platform->clk.mio_out,
                                kTopEarlgreyPinmuxOutselSpiHost1Sck));
   // SD0.
   TRY(dif_pinmux_input_select(pinmux, kTopEarlgreyPinmuxPeripheralInSpiHost1Sd0,
-                              kTopEarlgreyPinmuxInselIoa5));
-  TRY(dif_pinmux_output_select(pinmux, kTopEarlgreyPinmuxMioOutIoa5,
+                              platform->sd0.insel));
+  TRY(dif_pinmux_output_select(pinmux, platform->sd0.mio_out,
                                kTopEarlgreyPinmuxOutselSpiHost1Sd0));
 
   // SD1.
   TRY(dif_pinmux_input_select(pinmux, kTopEarlgreyPinmuxPeripheralInSpiHost1Sd1,
-                              kTopEarlgreyPinmuxInselIoa4));
-  TRY(dif_pinmux_output_select(pinmux, kTopEarlgreyPinmuxMioOutIoa4,
+                              platform->sd1.insel));
+  TRY(dif_pinmux_output_select(pinmux, platform->sd1.mio_out,
                                kTopEarlgreyPinmuxOutselSpiHost1Sd1));
   // SD2.
   TRY(dif_pinmux_input_select(pinmux, kTopEarlgreyPinmuxPeripheralInSpiHost1Sd2,
-                              kTopEarlgreyPinmuxInselIoa8));
-  TRY(dif_pinmux_output_select(pinmux, kTopEarlgreyPinmuxMioOutIoa8,
+                              platform->sd2.insel));
+  TRY(dif_pinmux_output_select(pinmux, platform->sd2.mio_out,
                                kTopEarlgreyPinmuxOutselSpiHost1Sd2));
   // SD3.
   TRY(dif_pinmux_input_select(pinmux, kTopEarlgreyPinmuxPeripheralInSpiHost1Sd3,
-                              kTopEarlgreyPinmuxInselIoa7));
-  TRY(dif_pinmux_output_select(pinmux, kTopEarlgreyPinmuxMioOutIoa7,
+                              platform->sd3.insel));
+  TRY(dif_pinmux_output_select(pinmux, platform->sd3.mio_out,
                                kTopEarlgreyPinmuxOutselSpiHost1Sd3));
   return OK_STATUS();
 }
