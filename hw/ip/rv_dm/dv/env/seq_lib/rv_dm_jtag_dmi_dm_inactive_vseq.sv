a182fcef27866fb502d8cceb810bdf76cfa3ef42
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-05-14 15:57:40 +0100

    [rv_dm,dv] Rewrite jtag_dmi_dm_inactive_vseq to be more general
    This is essentially the same as the code that existed before, but with
    a few of slight changes:
    
      - We now randomise the choice of register to write
    
      - We might try to read the register after enabling the module again
    
      - We no longer stick random waits between DMI operations: I don't
        think they acheive anything and they just make the code harder to
        read and slightly slower to run.
    
    Hopefully the behaviour of the test is a bit more obvious now as well.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rv_dm/dv/env/seq_lib/rv_dm_jtag_dmi_dm_inactive_vseq.sv
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_jtag_dmi_dm_inactive_vseq.sv
@@ -12,29 +12,46 @@ class rv_dm_jtag_dmi_dm_inactive_vseq extends rv_dm_common_vseq;
     lc_hw_debug_en == lc_ctrl_pkg::On;
   }
 
-  task body();
-    uvm_reg_data_t wdata;
+  // Write random data to the given register
+  task randomise_register(input uvm_object ptr);
+    uvm_reg_data_t wdata = $urandom_range(0, 1000);
+    csr_wr(.ptr(ptr), .value(wdata));
+  endtask
+
+  // Check that the given register has its reset value
+  task check_rst_value(input uvm_object ptr);
+    uvm_reg as_reg;
     uvm_reg_data_t rdata;
-    wdata=$urandom_range(0,31);
-    csr_wr(.ptr(jtag_dmi_ral.dmcontrol.dmactive), .value(0), .blocking(1), .predict(1));
-    cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
-    csr_wr(.ptr(jtag_dmi_ral.abstractdata[0]), .value(wdata));
-    cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
-    csr_wr(.ptr(jtag_dmi_ral.progbuf[0]), .value(wdata));
-    cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
-    csr_wr(.ptr(jtag_dmi_ral.abstractdata[1]), .value(wdata));
-    cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
-    csr_wr(.ptr(jtag_dmi_ral.progbuf[1]), .value(wdata));
-    cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
-    csr_rd(.ptr(jtag_dmi_ral.abstractdata[0]), .value(rdata));
-    `DV_CHECK_EQ(rdata, jtag_dmi_ral.abstractdata[0].get_reset());
-    csr_rd(.ptr(jtag_dmi_ral.progbuf[0]), .value(rdata));
-    `DV_CHECK_EQ(rdata, jtag_dmi_ral.progbuf[0].get_reset());
-    cfg.clk_rst_vif.wait_clks($urandom_range(0, 1000));
-    csr_rd(.ptr(jtag_dmi_ral.abstractdata[1]), .value(rdata));
-    `DV_CHECK_EQ(rdata, jtag_dmi_ral.abstractdata[1].get_reset());
-    csr_rd(.ptr(jtag_dmi_ral.progbuf[1]), .value(rdata));
-    `DV_CHECK_EQ(rdata, jtag_dmi_ral.progbuf[1].get_reset());
+
+    `DV_CHECK_FATAL($cast(as_reg, ptr));
+
+    csr_rd(.ptr(ptr), .value(rdata));
+    `DV_CHECK_EQ(rdata, as_reg.get_reset());
+  endtask
+
+  task body();
+    uvm_object ptrs[] = {jtag_dmi_ral.abstractdata[0],
+                         jtag_dmi_ral.abstractdata[1],
+                         jtag_dmi_ral.progbuf[0],
+                         jtag_dmi_ral.progbuf[1]};
+    bit is_active = 1'b0;
+
+    // Start by setting dmactive=0, which should put the debug module into a reset state.
+    csr_wr(.ptr(jtag_dmi_ral.dmcontrol.dmactive), .value(is_active), .blocking(1), .predict(1));
+
+    repeat (20) begin
+      uvm_object ptr = ptrs[$urandom_range(ptrs.size() - 1)];
+
+      randcase
+        10 * !is_active: randomise_register(.ptr(ptr));
+        10:              check_rst_value(.ptr(ptr));
+        1: begin
+          is_active = !is_active;
+          csr_wr(.ptr(jtag_dmi_ral.dmcontrol.dmactive),
+                 .value(is_active), .blocking(1), .predict(1));
+        end
+      endcase
+    end
   endtask
 
 endclass : rv_dm_jtag_dmi_dm_inactive_vseq
