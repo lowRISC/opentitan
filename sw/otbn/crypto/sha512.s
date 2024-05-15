916c6b43dc6c797d8ad6a59ca4fb2fbe3340e8e6
James Wainwright <james.wainwright@lowrisc.org>
2024-05-15 11:57:03 +0100

    [spdx] Add SPDX-License-Identifiers dcrypto files
    The `LicenseRef-...` SPDX headers are the canonical way of referring to
    custom license files. These are required because `dcrypto` uses a
    modified version of the BSD-3-Clause license that explicitly names
    Google instead of being generic.
    
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- sw/otbn/crypto/sha512.s
+++ sw/otbn/crypto/sha512.s
@@ -2,6 +2,7 @@
  * Copyright 2016 The Chromium OS Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE.dcrypto file.
+ * SPDX-License-Identifier: LicenseRef-BSD-3-Clause-dcrypto
  */
 
 /**
