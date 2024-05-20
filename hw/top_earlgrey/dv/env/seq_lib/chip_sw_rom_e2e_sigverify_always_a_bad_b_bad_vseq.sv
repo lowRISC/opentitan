131d999a856ff67de0e3cd9dbfd8ff7753746be8
Miguel Osorio <miguelosorio@google.com>
2024-05-20 21:18:38 +0000

    [dv] Fix e2e sigverify_always tests.
    Update the expected ROM error to ROM_BFV_BAD_ECDSA_SIGNATURE.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_sigverify_always_a_bad_b_bad_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_rom_e2e_sigverify_always_a_bad_b_bad_vseq.sv
@@ -14,7 +14,7 @@ class chip_sw_rom_e2e_sigverify_always_a_bad_b_bad_vseq extends
     connect_rom_uart_agent();
     lc_state = cfg.mem_bkdr_util_h[Otp].otp_read_lc_partition_state();
     check_uart_output_msg(
-      $sformatf("BFV:%0s\x0d\nLCV:%0s\x0d\n", ROM_BFV_BAD_RSA_SIGNATURE,
+      $sformatf("BFV:%0s\x0d\nLCV:%0s\x0d\n", ROM_BFV_BAD_ECDSA_SIGNATURE,
       lc_state_2_rom_lcv[lc_state]));
     disconnect_rom_uart_agent();
     rom_e2e_test_boot_fault_success();
