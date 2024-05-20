5dfe9651e403b1d8e9fa4a5ebc233a7a758ca470
Harry Callahan <hcallahan@lowrisc.org>
2024-05-20 01:46:45 +0100

    [i2c,dv] Add i2c_target_nack_acqfull vseq
    Add a directed testcase that allows the ACQFIFO to become full to stimulate
    the acq_stretch and the DUT-Target automatic NACK-generation.
    
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>

--- hw/ip/i2c/dv/env/seq_lib/i2c_vseq_list.sv
+++ hw/ip/i2c/dv/env/seq_lib/i2c_vseq_list.sv
@@ -39,3 +39,4 @@
 `include "i2c_target_fifo_watermarks_acq_vseq.sv"
 `include "i2c_target_fifo_watermarks_tx_vseq.sv"
 `include "i2c_target_tx_stretch_ctrl_vseq.sv"
+`include "i2c_target_nack_acqfull_vseq.sv"
