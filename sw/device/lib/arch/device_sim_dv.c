9fd9970809499b4d37500af3aa3a79c9afa9ba8e
Chris Frantz <cfrantz@google.com>
2024-05-20 14:42:47 -0700

    [arch] Precalculate several UART rates
    Precalculate the UART NCO for the "extended standard" rates of 230400,
    460800, 921600, 1.33M and 1.5M bps.  If a rate cannot be encoded in
    the 16-bit NCO field, then set the result of the calculation to zero to
    indicate the rate is not supported.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- sw/device/lib/arch/device_sim_dv.c
+++ sw/device/lib/arch/device_sim_dv.c
@@ -37,6 +37,19 @@ const uint64_t kUartBaudrate = 1 * 1000 * 1000;  // 1Mbps
 const uint32_t kUartNCOValue =
     CALCULATE_UART_NCO(kUartBaudrate, kClockFreqPeripheralHz);
 
+const uint32_t kUartBaud115K =
+    CALCULATE_UART_NCO(115200, kClockFreqPeripheralHz);
+const uint32_t kUartBaud230K =
+    CALCULATE_UART_NCO(115200 * 2, kClockFreqPeripheralHz);
+const uint32_t kUartBaud460K =
+    CALCULATE_UART_NCO(115200 * 4, kClockFreqPeripheralHz);
+const uint32_t kUartBaud921K =
+    CALCULATE_UART_NCO(115200 * 8, kClockFreqPeripheralHz);
+const uint32_t kUartBaud1M33 =
+    CALCULATE_UART_NCO(1333333, kClockFreqPeripheralHz);
+const uint32_t kUartBaud1M50 =
+    CALCULATE_UART_NCO(1500000, kClockFreqPeripheralHz);
+
 const uint32_t kUartTxFifoCpuCycles =
     CALCULATE_UART_TX_FIFO_CPU_CYCLES(kUartBaudrate, kClockFreqCpuHz);
 
