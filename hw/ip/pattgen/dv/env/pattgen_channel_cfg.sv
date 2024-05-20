e83691d9252ae9f79003eb2b949972c9237db6f1
Andreas Kurth <adk@lowrisc.org>
2024-05-20 13:47:35 +0000

    [pattgen/dv] Add vseq to test inactive level feature
    The feature is not yet supported in the scoreboard, so the scoreboard
    gets disabled.  This limitation is tracked in a linked issue.  A manual
    inspection of waves showed that the main use case works as intended,
    though.
    
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- hw/ip/pattgen/dv/env/pattgen_channel_cfg.sv
+++ hw/ip/pattgen/dv/env/pattgen_channel_cfg.sv
@@ -11,10 +11,18 @@ class pattgen_channel_cfg extends uvm_object;
   bit                enable;
 
   rand bit           polarity;
+  rand bit           inactive_level_pcl;
+  rand bit           inactive_level_pda;
   rand bit [31:0]    prediv;
   rand bit [63:0]    data;
   rand bit [5:0]     len;
   rand bit [9:0]     reps;
+
+  // TODO(#23219): Remove this when the scoreboard supports the inactive_level feature.
+  constraint inactive_level_disabled_c {
+    inactive_level_pcl == 1'b0;
+    inactive_level_pda == 1'b0;
+  }
 
   virtual function void reset_channel_config(string kind = "");
     start    = 1'b0;
@@ -32,16 +40,18 @@ class pattgen_channel_cfg extends uvm_object;
   virtual function string convert2string();
       string str;
 
-      str = {str, $sformatf("  start     %b\n",  start)};
-      str = {str, $sformatf("  stop      %b\n",  stop)};
-      str = {str, $sformatf("  enable    %b\n",  enable)};
-      str = {str, $sformatf("  polarity  %b\n",  polarity)};
-      str = {str, $sformatf("  prediv    %0d\n", prediv)};
-      str = {str, $sformatf("  len       %0d\n", len)};
-      str = {str, $sformatf("  reps      %0d\n", reps)};
-      str = {str, $sformatf("  data[0]   %0d\n", data[31:0])};
-      str = {str, $sformatf("  data[1]   %0d\n", data[63:32])};
-      str = {str, $sformatf("  patt_len  %0d",   (len + 1) * (reps + 1))};
+      str = {str, $sformatf("  start              %b\n",  start)};
+      str = {str, $sformatf("  stop               %b\n",  stop)};
+      str = {str, $sformatf("  enable             %b\n",  enable)};
+      str = {str, $sformatf("  polarity           %b\n",  polarity)};
+      str = {str, $sformatf("  inactive_level_pcl %b\n",  inactive_level_pcl)};
+      str = {str, $sformatf("  inactive_level_pda %b\n",  inactive_level_pda)};
+      str = {str, $sformatf("  prediv             %0d\n", prediv)};
+      str = {str, $sformatf("  len                %0d\n", len)};
+      str = {str, $sformatf("  reps               %0d\n", reps)};
+      str = {str, $sformatf("  data[0]            %0d\n", data[31:0])};
+      str = {str, $sformatf("  data[1]            %0d\n", data[63:32])};
+      str = {str, $sformatf("  patt_len           %0d",   (len + 1) * (reps + 1))};
       return str;
   endfunction : convert2string
 
