0aa654780b68dfae4abeb5110810287da679937b
Michael Tempelmeier <michael.tempelmeier@gi-de.com>
2024-05-21 15:49:05 +0200

    [ascon] add TB vectors for comportable IP
    Signed-off-by: Michael Tempelmeier <michael.tempelmeier@gi-de.com>

--- hw/ip/ascon/pre_dv/ascon_tb/rtl/ascon_tl_ul_stim.sv
+++ hw/ip/ascon/pre_dv/ascon_tb/rtl/ascon_tl_ul_stim.sv
@@ -10,8 +10,8 @@ module ascon_tl_ul_stim
   import ascon_reg_pkg::*;
   import ascon_tl_ul_stim_pkg::*;
 #(
-  localparam int NumStimulus = 9*4,
-  localparam int NumResponses = 2*4
+  localparam int NumStimulus = 13*4 + 4*2 + 1,
+  localparam int NumResponses = 3*4
 )
 (
   input  clk_i,
@@ -32,45 +32,101 @@ module ascon_tl_ul_stim
   tl_d2h_t response[NumResponses];
 
   initial begin
+    // The idea of this test case is to have human readable (DEADBEEF, CAFEF00D, etc) test vectors,
+    // that are easy to trace troughout the design to catch logical errors (endianness,
+    // clock delays, etc.) during development.
     stimulus = '{
       // NONCE
-      put_full_data(32'hDEADBEEF, ASCON_NONCE_SHARE0_0_OFFSET),
+      put_full_data(32'h0, ASCON_NONCE_SHARE0_0_OFFSET),
       put_full_data(32'h0, ASCON_NONCE_SHARE0_1_OFFSET),
-      put_full_data(32'h01234567, ASCON_NONCE_SHARE0_2_OFFSET),
+      put_full_data(32'hEFBEADDE, ASCON_NONCE_SHARE0_2_OFFSET),
       put_full_data(32'h0, ASCON_NONCE_SHARE0_3_OFFSET),
 
       put_full_data(32'h0, ASCON_NONCE_SHARE1_0_OFFSET),
-      put_full_data(32'hCAFECAFE, ASCON_NONCE_SHARE1_1_OFFSET),
+      put_full_data(32'h0, ASCON_NONCE_SHARE1_1_OFFSET),
       put_full_data(32'h0, ASCON_NONCE_SHARE1_2_OFFSET),
-      put_full_data(32'h89ABCDEF, ASCON_NONCE_SHARE1_3_OFFSET),
+      put_full_data(32'h0DF0FECA, ASCON_NONCE_SHARE1_3_OFFSET),
 
       // KEY
-      put_full_data(32'hDEADBEEF, ASCON_KEY_SHARE0_0_OFFSET),
+      put_full_data(32'hEFBEADDE, ASCON_KEY_SHARE0_0_OFFSET),
       put_full_data(32'h0, ASCON_KEY_SHARE0_1_OFFSET),
-      put_full_data(32'h01234567, ASCON_KEY_SHARE0_2_OFFSET),
+      put_full_data(32'h0, ASCON_KEY_SHARE0_2_OFFSET),
       put_full_data(32'h0, ASCON_KEY_SHARE0_3_OFFSET),
 
       put_full_data(32'h0, ASCON_KEY_SHARE1_0_OFFSET),
-      put_full_data(32'hCAFECAFE, ASCON_KEY_SHARE1_1_OFFSET),
+      put_full_data(32'h0DF0FECA, ASCON_KEY_SHARE1_1_OFFSET),
       put_full_data(32'h0, ASCON_KEY_SHARE1_2_OFFSET),
-      put_full_data(32'h89ABCDEF, ASCON_KEY_SHARE1_3_OFFSET),
+      put_full_data(32'h0, ASCON_KEY_SHARE1_3_OFFSET),
 
-      // DATA
-      put_full_data(32'hDEADBEEF, ASCON_DATA_IN_SHARE0_0_OFFSET),
+      //Varaint+ Operation:
+      // 5'b01_001 :  Ascon-128 Enc
+      put_full_data(32'h00000009, ASCON_CTRL_SHADOWED_OFFSET),
+      put_full_data(32'h00000009, ASCON_CTRL_SHADOWED_OFFSET),
+
+      // BLock Info
+      // 3 bits reserved, 5 bits valid_bytes, 12 bits last, 12 bist start (ignored)
+      // Full 64 bit block AD, last
+      put_full_data(32'h08969000, ASCON_BLOCK_CTRL_SHADOWED_OFFSET),
+      put_full_data(32'h08969000, ASCON_BLOCK_CTRL_SHADOWED_OFFSET),
+
+      // AD
+      put_full_data(32'h78563412, ASCON_DATA_IN_SHARE0_0_OFFSET),
       put_full_data(32'h0, ASCON_DATA_IN_SHARE0_1_OFFSET),
-      put_full_data(32'h01234567, ASCON_DATA_IN_SHARE0_2_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE0_2_OFFSET),
       put_full_data(32'h0, ASCON_DATA_IN_SHARE0_3_OFFSET),
 
       put_full_data(32'h0, ASCON_DATA_IN_SHARE1_0_OFFSET),
-      put_full_data(32'hCAFECAFE, ASCON_DATA_IN_SHARE1_1_OFFSET),
+      put_full_data(32'hF0DEBC9A, ASCON_DATA_IN_SHARE1_1_OFFSET),
       put_full_data(32'h0, ASCON_DATA_IN_SHARE1_2_OFFSET),
-      put_full_data(32'h89ABCDEF, ASCON_DATA_IN_SHARE1_3_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE1_3_OFFSET),
+
+      // Trigger start
+      put_full_data(32'h00000001, ASCON_TRIGGER_OFFSET),
+
+      // BLock Info
+      // 3 bits reserved, 5 bits valid_bytes, 12 bits last, 12 bist start (ignored)
+      // Full 64 bit block msg, last
+      put_full_data(32'h08999000, ASCON_BLOCK_CTRL_SHADOWED_OFFSET),
+      put_full_data(32'h08999000, ASCON_BLOCK_CTRL_SHADOWED_OFFSET),
+
+      // MSG
+      put_full_data(32'hEFBEADDE, ASCON_DATA_IN_SHARE0_0_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE0_1_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE0_2_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE0_3_OFFSET),
+
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE1_0_OFFSET),
+      put_full_data(32'h0DF0FECA, ASCON_DATA_IN_SHARE1_1_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE1_2_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE1_3_OFFSET),
 
       // TAG
-      put_full_data(32'hDEADBEEF, ASCON_TAG_IN_0_OFFSET),
-      put_full_data(32'h0, ASCON_TAG_IN_1_OFFSET),
-      put_full_data(32'h01234567, ASCON_TAG_IN_2_OFFSET),
-      put_full_data(32'h0, ASCON_TAG_IN_3_OFFSET),
+      //put_full_data(32'hDEADBEEF, ASCON_TAG_IN_0_OFFSET),
+      //put_full_data(32'h0, ASCON_TAG_IN_1_OFFSET),
+      //put_full_data(32'h01234567, ASCON_TAG_IN_2_OFFSET),
+      //put_full_data(32'h0, ASCON_TAG_IN_3_OFFSET),
+
+      // GET MSG
+      get_full_data(ASCON_MSG_OUT_0_OFFSET),
+      get_full_data(ASCON_MSG_OUT_1_OFFSET),
+      get_full_data(ASCON_MSG_OUT_2_OFFSET),
+      get_full_data(ASCON_MSG_OUT_3_OFFSET),
+
+      // BLock Info
+      // 3 bits reserved, 5 bits valid_bytes, 12 bits last, 12 bist start (ignored)
+      // Full 64 bit block msg, last
+      put_full_data(32'h02699000, ASCON_BLOCK_CTRL_SHADOWED_OFFSET),
+      put_full_data(32'h02699000, ASCON_BLOCK_CTRL_SHADOWED_OFFSET),
+            // DATA
+      put_full_data(32'hF0F0F0F0, ASCON_DATA_IN_SHARE0_0_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE0_1_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE0_2_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE0_3_OFFSET),
+
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE1_0_OFFSET),
+      put_full_data(32'hE0E0E0E0, ASCON_DATA_IN_SHARE1_1_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE1_2_OFFSET),
+      put_full_data(32'h0, ASCON_DATA_IN_SHARE1_3_OFFSET),
 
       // GET MSG
       get_full_data(ASCON_MSG_OUT_0_OFFSET),
@@ -87,16 +143,22 @@ module ascon_tl_ul_stim
 
     response = '{
       // MESG
-      expect_full_data(32'hDEADBEEF),
-      expect_full_data(32'hCAFECAFE),
-      expect_full_data(32'h01234567),
-      expect_full_data(32'h89ABCDEF),
+      expect_full_data(32'h057F25B4),
+      expect_full_data(32'hA9481060),
+      expect_full_data(32'h00000000),
+      expect_full_data(32'h00000000),
+
+      // MESG
+      expect_full_data(32'h00005A47),
+      expect_full_data(32'h00000000),
+      expect_full_data(32'h00000000),
+      expect_full_data(32'h00000000),
 
       // TAG
-      expect_full_data(32'hDEADBEEF),
-      expect_full_data(32'h0),
-      expect_full_data(32'h01234567),
-      expect_full_data(32'h0)
+      expect_full_data(32'h71151ABF),
+      expect_full_data(32'h95BD8BDB),
+      expect_full_data(32'h8387BE47),
+      expect_full_data(32'h79927DD5)
     };
   end
 
@@ -147,7 +209,7 @@ module ascon_tl_ul_stim
     end
   end
 
-  assign done_o = (response_counter == NumResponses-1)
-                  && (stimulus_counter == NumStimulus) ? 1'b1 : 1'b0;
+  assign done_o = (response_counter >= NumResponses-1)
+                  && (stimulus_counter >= NumStimulus) ? 1'b1 : 1'b0;
 
 endmodule
