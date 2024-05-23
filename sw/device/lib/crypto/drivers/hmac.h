b4589743d1b563f6abfaf2e479d95280d9c5fa05
Fatih Balli <fatihballi@google.com>
2024-05-23 21:13:20 +0000

    [hmac, driver] Bug fixes
    Follow-up improvements on HMAC driver:
    
    * Remove duplicate HMAC-specific structs
    and use generic `*word32_buf_t` structs instead.
    * Add placeholder oneshot HMAC call, which will be
    later replaced with more efficient oneshot
    implementation that does not suspend HMAC during
    update calls.
    * Use `key_swap` option and avoid swapping key
    endianness in SW.
    
    Signed-off-by: Fatih Balli <fatihballi@google.com>

--- sw/device/lib/crypto/drivers/hmac.h
+++ sw/device/lib/crypto/drivers/hmac.h
@@ -30,71 +30,46 @@ enum {
   kHmacSha512DigestBits = 512,
   kHmacSha512DigestBytes = kHmacSha512DigestBits / 8,
   kHmacSha512DigestWords = kHmacSha512DigestBytes / sizeof(uint32_t),
-  /* Maximum digest size supported by HW natively in bits and words
+  /* Internal block size for SHA-256/HMAC-256 in bits, bytes and word
      respectively. */
-  kHmacMaxDigestBits = 512,
-  kHmacMaxDigestWords = kHmacMaxDigestBits / sizeof(uint32_t) / 8,
+  kHmacSha256BlockBits = 2 * kHmacSha256DigestBits,
+  kHmacSha256BlockBytes = kHmacSha256BlockBits / 8,
+  kHmacSha256BlockWords = kHmacSha256BlockBytes / sizeof(uint32_t),
+  /* Internal block size for SHA-384/512 and HMAC-384/512 in bits, bytes and
+     word respectively. */
+  kHmacSha512BlockBits = 2 * kHmacSha512DigestBits,
+  kHmacSha512BlockBytes = kHmacSha512BlockBits / 8,
+  kHmacSha512BlockWords = kHmacSha512BlockBytes / sizeof(uint32_t),
+  /* Maximum digest size supported by HW natively in bits, bytes and words
+     respectively. */
+  kHmacMaxDigestBits = kHmacSha512DigestBits,
+  kHmacMaxDigestBytes = kHmacMaxDigestBits / 8,
+  kHmacMaxDigestWords = kHmacMaxDigestBytes / sizeof(uint32_t),
   /* Maximum internal block size bits, bytes and words respectively. */
-  kHmacMaxBlockBits = 1024,
+  kHmacMaxBlockBits = kHmacSha512BlockBits,
   kHmacMaxBlockBytes = kHmacMaxBlockBits / 8,
   kHmacMaxBlockWords = kHmacMaxBlockBytes / sizeof(uint32_t),
 };
-
-/* Supported key sizes from HWIP in bytes. */
-enum {
-  /* 128 bit key. */
-  kHmacKey128Bytes = 128 / 8,
-  /* 256 bit key. */
-  kHmacKey256Bytes = 256 / 8,
-  /* 384 bit key. */
-  kHmacKey384Bytes = 384 / 8,
-  /* 512 bit key. */
-  kHmacKey512Bytes = 512 / 8,
-  /* 1024 bit key. */
-  kHmacKey1024Bytes = 1024 / 8,
-  /* Number of words for maximum key size supported by HW natively. */
-  kHmacMaxKeyWords = kHmacKey1024Bytes / sizeof(uint32_t),
-};
-
-/**
- * A typed representation of the SHA-2/HMAC digests.
- */
-typedef struct hmac_digest {
-  uint32_t digest[kHmacMaxDigestWords];
-  // Length of `digest` in bytes.
-  size_t len;
-} hmac_digest_t;
-
-/**
- * A typed representation of an HMAC key. HWIP supports 128, 256, 384, 512, 1024
- * bit keys natively.
- */
-typedef struct hmac_key {
-  uint32_t key[kHmacMaxKeyWords];
-  // Length of `key` in bytes.
-  size_t len;
-} hmac_key_t;
 
 /**
  * A context struct maintained for streaming operations.
  */
 typedef struct hmac_ctx {
-  // Back up cfg register, except SHA bit.
+  // A copy of `CFG` register used during resumption.
   uint32_t cfg_reg;
-  // Need to keep some extra info around to reconfigure HW every time.
-  uint32_t key[kHmacMaxKeyWords];
-  // Length of `key` in words.
-  // key_len == 0 is used to infer that this is SHA-2 operation.
-  size_t key_len;
-  // The internal block size of HMAC/SHA2 in bytes.
-  size_t msg_block_len;
-  size_t digest_len;
+  // A copy of `KEY` to be used during start or resumption.
+  uint32_t key[kHmacMaxBlockWords];
+  // key_wordlen == 0 is used to infer that this is SHA-2 operation.
+  size_t key_wordlen;
+  // The internal (message) block size of SHA-2 for this operation.
+  size_t msg_block_bytelen;
+  size_t digest_wordlen;
   // The following fields are saved and restored during streaming.
   uint32_t H[kHmacMaxDigestWords];
   uint32_t lower;
   uint32_t upper;
-  // The following are flags exclusively used by the driver to make whether
-  // or not the driver needs to pass the incoming requests to HMAC IP.
+  // The following flags are used exclusively by the driver to determine whether
+  // or not the driver needs to pass the incoming requests to HMAC HWIP.
   uint32_t hw_started;
   uint8_t partial_block[kHmacMaxBlockBytes];
   // The number of valid bytes in `partial_block`.
@@ -119,32 +94,38 @@ typedef enum hmac_mode {
 /**
  * Initializes the context `ctx` according to given `hmac_mode` and `key`.
  *
- * This function does not invoke HMAC HWIP operation, but instead prepares `ctx`
- * with necessary data for the streaming operations to be called later:
+ * This function does not invoke a HMAC HWIP operation, but instead prepares
+ * `ctx` with necessary data for the streaming operations to be called later:
+ *
  * i) Prepare CFG register value (with the exception of `sha_en` bit, #23014)
  * and store it into `ctx`. This value is repetitively loaded into HWIP during
  * future `hmac_update` and `hmac_final` calls.
- *
  * ii) Copy given key and its length into `ctx` if the operation is HMAC.
  * iii) Initialize `hw_started` flag to false which indicates whether the very
  * first HWIP operation is executed or not. This helps decide whether start or
- * continue operation needs to be issues to HWIP later.
+ * continue operation needs to be issues to HMAC HWIP later.
  * iv) Compute and store message block length and digest len fields to `ctx`.
  *
- * For SHA-2 operation, `key` must be set to NULL.
- * For HMAC operations, `key` must point to a valid `hmac_key_t` struct where
- * `key->len` corresponds to either one of the natively supported key lengths:
- * {128, 256, 384, 512, 1024} bits. `key->key` should have sufficient number of
- * words for the matching key length.
+ * For SHA-2 operation, it must be that `key = NULL` and `key_wordlen = 0`.
+ * For HMAC operations, `key` must point to a key buffer that contains the
+ * processed key denoted as k0 in FIPS 198-1, Section 4. Therefore, the size of
+ * the key must match the internal block size of the underlying hash function.
+ * In other words, it means that:
+ *   For HMAC-256, `key_wordlen = 64` with `key` referring to the value of k0,
+ * which consists of exactly 64 words.
+ *   For HMAC-384/512, `key_wordlen = 128`, with `key` referring to the value
+ * of k0, which consists of exactly 128 words.
  *
  * @param[out] ctx Initialized context object for SHA2/HMAC-SHA2 operations.
  * @param hmac_mode Specifies the mode among SHA2-256/384/512, HMAC-256/384/512.
- * @param key HMAC key. The key to be used with HMAC calls.
- * @return Error status.
+ * @param key Processed HMAC key.
+ * @param key_wordlen The length of HMAC key, which must match the internal
+ * block size of the underlying hash function.
+ * @return Result of the operation.
  */
 OT_WARN_UNUSED_RESULT
 status_t hmac_init(hmac_ctx_t *ctx, const hmac_mode_t hmac_mode,
-                   const hmac_key_t *key);
+                   const uint32_t *key, size_t key_wordlen);
 
 /**
  * Updates the state of `ctx` with given additional messsage bytes.
@@ -168,7 +149,7 @@ status_t hmac_init(hmac_ctx_t *ctx, const hmac_mode_t hmac_mode,
  * @param ctx Context object referring to a particular SHA-2/HMAC stream.
  * @param data Incoming message bytes to be processed into the stream.
  * @param len size of the `data` buffer in bytes.
- * @return Error status.
+ * @return Result of the operation.
  */
 OT_WARN_UNUSED_RESULT
 status_t hmac_update(hmac_ctx_t *ctx, const uint8_t *data, size_t len);
@@ -184,12 +165,53 @@ status_t hmac_update(hmac_ctx_t *ctx, const uint8_t *data, size_t len);
  * invoked at HWIP to conclude SHA-2/HMAC operation and produce the digest/tag.
  * The result is read from HWIP into `digest` and the state of HWIP is cleared.
  *
+ * `digest` should point to a sufficiently large buffer to accommodate
+ * the resulting digest. `digest_wordlen` must match the digest length implied
+ * by the mode used during the initialization of `ctx`, otherwise an error is
+ * returned.
+ *
  * @param ctx Context object referring to a particular stream.
  * @param[out] digest The digest value to be returned.
- * @return Error status.
+ * @param digest_wordlen The length of the digest in words.
+ * @return Result of the operation.
  */
 OT_WARN_UNUSED_RESULT
-status_t hmac_final(hmac_ctx_t *ctx, hmac_digest_t *digest);
+status_t hmac_final(hmac_ctx_t *ctx, uint32_t *digest, size_t digest_wordlen);
+
+/**
+ * One-shot SHA-2/HMAC call.
+ *
+ * This function uses `hmac_mode` to determine whether to run SHA-2 or HMAC
+ * including the digest size. See `hmac_mode_t` for possible values.
+ *
+ * For SHA-2 operation, it must be that `key = NULL` and `key_wordlen = 0`.
+ * For HMAC operations, `key` must point to a key buffer that contains the
+ * processed key denoted as k0 in FIPS 198-1, Section 4. Therefore, the size of
+ * the key must match the internal block size of the underlying hash function.
+ * In other words, it means that:
+ *   For HMAC-256, `key_wordlen = 64` with `key` referring to the value of k0,
+ * which consists of exactly 64 words.
+ *   For HMAC-384/512, `key_wordlen = 128`, with `key` referring to the value
+ * of k0, which consists of exactly 128 words.
+ *
+ * `digest` should point to a sufficiently large buffer to accommodate
+ * the resulting digest. `digest_wordlen` must match the digest length implied
+ * by the mode used during the initialization of `ctx`, otherwise an error is
+ * returned.
+ *
+ * @param[out] ctx Initialized context object for SHA2/HMAC-SHA2 operations.
+ * @param hmac_mode Specifies the mode among SHA2-256/384/512, HMAC-256/384/512.
+ * @param key Processed HMAC key.
+ * @param key_wordlen The length of HMAC key, which must match the internal
+ * block size of the underlying hash function.
+ * @param[out] digest The digest value to be returned.
+ * @param digest_wordlen The length of the digest in words.
+ * @return Result of the operation.
+ */
+OT_WARN_UNUSED_RESULT
+status_t hmac(const hmac_mode_t hmac_mode, const uint32_t *key,
+              size_t key_wordlen, const uint8_t *data, size_t len,
+              uint32_t *digest, size_t digest_wordlen);
 
 #ifdef __cplusplus
 }
