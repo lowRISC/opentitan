lowRISC/opentitan

commit f70443076e0b41748249584b903b419fb2d6caa1

[otbn,dv] Factor out a wait_for_run_completion task in base vseq
No big change, but this will now be usable by subclasses of the base
vseq.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/otbn/dv/uvm/env/seq_lib/otbn_base_vseq.sv
@@ -473,6 +473,21 @@ class otbn_base_vseq extends cip_base_vseq #(
     running_ = 1'b0;
   endtask
 
+  // Wait for OTBN to finish, either by polling or by waiting on the interrupt pins
+  task wait_for_run_completion(input bit verbosity);
+    if (_pick_use_interrupt()) begin
+      `uvm_info(`gfn, "\n\t ----| Waiting for OTBN to finish (interrupt)", verbosity)
+      wait_for_interrupt();
+    end else begin
+      uvm_reg_data_t run_status;
+
+      `uvm_info(`gfn, "\n\t ----| Waiting for OTBN to finish (polling)", verbosity)
+      csr_utils_pkg::csr_rd(.ptr(ral.status), .value(run_status), .backdoor(1));
+      csr_utils_pkg::csr_spinwait(.ptr(ral.status), .exp_data(run_status),
+                                  .compare_op(CompareOpNe));
+    end
+  endtask
+
   // The guts of the run_otbn task. Writes to the CMD register to start OTBN and polls the status
   // register until completion. On reset, this returns immediately.
   protected task _run_otbn_cmd(logic [7:0] cmd_i);
@@ -496,15 +511,7 @@ class otbn_base_vseq extends cip_base_vseq #(
 
     csr_utils_pkg::csr_wr(ral.cmd, cmd_i);
 
-    // Wait for OTBN to finish, either by polling or by waiting on the interrupt pins
-    if (_pick_use_interrupt()) begin
-      `uvm_info(`gfn, "\n\t ----| Waiting for OTBN to finish (interrupt)", UVM_MEDIUM)
-      wait_for_interrupt();
-    end else begin
-      `uvm_info(`gfn, "\n\t ----| Waiting for OTBN to finish (polling)", UVM_MEDIUM)
-      csr_utils_pkg::csr_spinwait(.ptr(ral.status), .exp_data(exp_data_status),
-                                  .compare_op(CompareOpNe));
-    end
+    wait_for_run_completion(UVM_MEDIUM);
 
     if (cfg.clk_rst_vif.rst_n) begin
       if (exp_data_status == otbn_pkg::StatusBusyExecute) begin

commit 27534caa1166cd566fea1ecd79634f8a4e336ba7

[otbn,dv] Fix wait_for_run_completion
I actually broke this logic with commit f70443076e0. Before that
commit, we waited until the status was something different from the
"run command" that we expected to be running. In this commit, I
changed the logic so that it snooped the current status and waited
until the status had changed.

Of course, I might do the snooping too late and then we end up waiting
until the status is no longer idle. That times out... This commit
changes things so that we wait until we're Idle or Locked (so no
longer running one of the three commands).

The timeout (that already existed in the polling case) is now
explicitly encoded here, and applies to the interrupt case as well.
I've made it 1ms pretty much arbitrarily. We can always bump it up if
it is causing things to fail.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/ip/otbn/dv/uvm/env/seq_lib/otbn_base_vseq.sv
@@ -474,18 +474,36 @@ class otbn_base_vseq extends cip_base_vseq #(
   endtask
 
   // Wait for OTBN to finish, either by polling or by waiting on the interrupt pins
+  //
+  // When polling, this waits up to 1ms before failing with a timeout.
   task wait_for_run_completion(input bit verbosity);
-    if (_pick_use_interrupt()) begin
-      `uvm_info(`gfn, "\n\t ----| Waiting for OTBN to finish (interrupt)", verbosity)
-      wait_for_interrupt();
-    end else begin
-      uvm_reg_data_t run_status;
+    fork begin : isolation_fork
+      fork
+        begin
+          `DV_WAIT_TIMEOUT(1_000_000, ,
+                           "Timed out waiting for OTBN run to complete by polling status")
+        end
+        begin
+          if (_pick_use_interrupt()) begin
+            `uvm_info(`gfn, "\n\t ----| Waiting for OTBN to finish (interrupt)", verbosity)
+            wait_for_interrupt();
+          end else begin
+            uvm_reg_data_t run_status;
 
-      `uvm_info(`gfn, "\n\t ----| Waiting for OTBN to finish (polling)", verbosity)
-      csr_utils_pkg::csr_rd(.ptr(ral.status), .value(run_status), .backdoor(1));
-      csr_utils_pkg::csr_spinwait(.ptr(ral.status), .exp_data(run_status),
-                                  .compare_op(CompareOpNe));
-    end
+            `uvm_info(`gfn, "\n\t ----| Waiting for OTBN to finish (polling)", verbosity)
+            while (cfg.clk_rst_vif.rst_n) begin
+              uvm_reg_data_t rdata;
+              csr_rd(.ptr(ral.status), .value(rdata), .blocking(1));
+              if (rdata inside {otbn_pkg::StatusIdle, otbn_pkg::StatusLocked}) begin
+                `uvm_info(`gfn, $sformatf("Seen end of OTBN run (status is %0x)", rdata), UVM_HIGH)
+                break;
+              end
+            end
+          end
+        end
+      join_any
+      disable fork;
+    end join
   endtask
 
   // The guts of the run_otbn task. Writes to the CMD register to start OTBN and polls the status