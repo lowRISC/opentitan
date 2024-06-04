a34fb727d5474a9e4c429cac84a69b6ff00fa19e
Antonio Martinez Zambrana <amz@lowrisc.org>
2024-06-04 08:35:29 +0100

    [dv, spi_host] Temporary commit - only there to get SVA to fire
    This commit will be removed. It's only there to have a way to
    reproduce the failure.
    The assertion is taken care of in a different branch and is tied to
    the RTL via bind rather than living within the RTL.
    
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/ip/spi_host/rtl/spi_host.sv
+++ hw/ip/spi_host/rtl/spi_host.sv
@@ -43,6 +43,111 @@ module spi_host
 
   import spi_host_cmd_pkg::*;
 
+  // Note: this check is carried out via bind. The SVa is copied here in order to reproduce failures
+  //  in this branch, This commit will be dropped
+`ifndef SYNTHESIS
+  import uvm_pkg::*;
+ `include "uvm_macros.svh"
+
+  // Check to ensure cio_sd_o[i] stays stable for a whole clock cycle
+  // Truth table:
+  // ---------------------------------
+  // CPHA | CPOL | posedge | negedge |
+  // ---------------------------------
+  // 0    | 0    | 1       | 0       | -> XNOR(cpha, cpol): drive negedge, sample posedge
+  // 0    | 1    | 0       | 1       | -> XOR(cpha, cpol): drive posedge, sample negedge
+  // 1    | 0    | 0       | 1       | -> XOR(cpha, cpol): drive posedge, sample negedge
+  // 1    | 1    | 1       | 0       | -> XNOR(cpha, cpol): drive negedge, sample posedge
+  // ---------------------------------
+
+
+  //Driven in pos-edge, sampled in neg-edge
+  reg                      sampled_negedge_enable;
+  //Driven in neg-edge, sampled in pos-edge
+  reg                      sampled_posedge_enable;
+  logic                    csbs;
+  // Current design support NumCS=1 only
+  assign csbs = &cio_csb_o;
+
+  //HIGH when driven on pos-edge, sampled on the negedge
+  assign sampled_negedge_enable = !csbs & (configopts.cpha.q ^ configopts.cpol.q);
+  assign sampled_posedge_enable = !csbs & !sampled_negedge_enable;
+
+  for (genvar i = 0; i < 4; i++) begin : g_cio_signal_stable_sva
+    logic neg_value, pos_value;
+    event check_posedge, check_negedge;
+    initial begin
+      forever begin
+        @(negedge csbs);
+        //Initialising the neg/pos_value signals here, so they are ready for the first SPI cycle
+        pos_value <= cio_sd_i[i];
+        neg_value <= cio_sd_i[i];
+        if (sampled_posedge_enable) begin
+          @(negedge cio_sck_o);
+          pos_value <= cio_sd_i[i];
+        end
+        if (sampled_negedge_enable) begin
+          @(posedge cio_sck_o);
+          neg_value <= cio_sd_i[i];
+        end
+      end
+    end
+
+  always @(negedge cio_sck_o or negedge rst_ni) begin
+    if (!rst_ni) begin
+      neg_value <= 0;
+    end else if (!csbs) begin
+      neg_value <= cio_sd_i[i];
+      if (sampled_negedge_enable) begin
+        // TODO: remove IF below when passthrough mode is handled thorougly
+        // SPI-host block level TB drives gibberish to test passthru, which
+        // causes the assertion to fire. Until passthrough is tested via driving
+        // sensible SPI data is best to disable this SVA
+        if (!passthrough_i.passthrough_en)
+          -> check_negedge;
+      end
+    end
+  end
+
+  always @(posedge cio_sck_o or negedge rst_ni) begin
+    if (!rst_ni) begin
+      pos_value <= 0;
+    end else if (!csbs) begin
+      pos_value <= cio_sd_i[i];
+      if (sampled_posedge_enable) begin
+        // TODO: remove IF below when passthrough mode is handled thorougly
+        // SPI-host block level TB drives gibberish to test passthru, which
+        // causes the assertion to fire. Until passthrough is tested via driving
+        // sensible SPI data is best to disable this SVA
+        if (!passthrough_i.passthrough_en)
+          -> check_posedge;
+      end
+    end
+  end // always @ (posedge cio_sck_o or negedge rst_ni)
+
+  NEGEDGE_SAME_VALUE_CHECK_P: assert property (@(check_negedge) pos_value == neg_value) begin
+    `uvm_info("NEGEDGE_SAME_VALUE_CHECK_P", $sformatf("[i=%0d] - ASSERTION PASSED %t",i, $time),
+              UVM_DEBUG);
+  end
+  else begin
+    `uvm_error("NEGEDGE_SAME_VALUE_CHECK_P", {$sformatf("[i=%0d] - ASSERTION FAILED",i), $sformatf(
+                                              " pos_value (0x%0x) != neg_value (0x%0x) - time=%t",
+                                              pos_value, neg_value, $time)})
+  end
+  POSEDGE_SAME_VALUE_CHECK_P: assert property (@(check_posedge) pos_value == neg_value) begin
+    `uvm_info("POSEDGE_SAME_VALUE_CHECK_P", $sformatf("[i=%0d] - ASSERTION PASSED %t",i, $time),
+              UVM_DEBUG);
+  end
+  else begin
+    `uvm_error("POSEDGE_SAME_VALUE_CHECK_P", {$sformatf("[i=%0d] - ASSERTION FAILED",i), $sformatf(
+                                             " pos_value (0x%0x) != neg_value (0x%0x) - time=%t",
+                                             pos_value, neg_value, $time)})
+  end
+end
+
+`endif
+
+  
   spi_host_reg2hw_t reg2hw;
   spi_host_hw2reg_t hw2reg;
 
