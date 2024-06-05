601cc797051e9201639cad06d5a7a32478f3479b
James Wainwright <james.wainwright@lowrisc.org>
2024-06-05 14:45:29 +0100

    [sw] Add ujson command for configuring pinmux attrs
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- sw/host/tests/chip/gpio_intr/src/main.rs
+++ sw/host/tests/chip/gpio_intr/src/main.rs
@@ -216,7 +216,7 @@ fn test_gpio_outputs(opts: &Opts, transport: &TransportWrapper, uart: &dyn Uart)
     let config = CONFIG
         .get(opts.init.backend_opts.interface.as_str())
         .expect("interface");
-    PinmuxConfig::configure(uart, Some(&config.input), Some(&config.output))?;
+    PinmuxConfig::configure(uart, Some(&config.input), Some(&config.output), None)?;
 
     log::info!("Configuring debugger GPIOs as inputs");
     // The outputs (with respect to pinmux config) correspond to the input pins on the debug board.
@@ -290,7 +290,7 @@ fn test_gpio_inputs(opts: &Opts, transport: &TransportWrapper, uart: &dyn Uart) 
     let config = CONFIG
         .get(opts.init.backend_opts.interface.as_str())
         .expect("interface");
-    PinmuxConfig::configure(uart, Some(&config.input), None)?;
+    PinmuxConfig::configure(uart, Some(&config.input), None, None)?;
 
     log::info!("Configuring debugger GPIOs as outputs");
     // The inputs (with respect to pinmux config) correspond to the output pins on the debug board.
