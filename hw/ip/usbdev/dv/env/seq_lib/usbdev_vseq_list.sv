f1d9073c61389799dd9015bc58e753e1bad9330a
Adrian Lees <a.lees@lowrisc.org>
2024-06-10 17:22:11 +0100

    [dv,usbdev] Tx Oscillator test mode sequence
    Add sequence for testing the 'Tx Oscillator test mode' in which the
    DUT will drive out a repeated J/K sequence so that its 48MHz clock
    frequency may be (indirectly) measured.
    
    Signed-off-by: Adrian Lees <a.lees@lowrisc.org>

--- hw/ip/usbdev/dv/env/seq_lib/usbdev_vseq_list.sv
+++ hw/ip/usbdev/dv/env/seq_lib/usbdev_vseq_list.sv
@@ -33,6 +33,7 @@
 `include "usbdev_phy_config_usb_ref_disable_vseq.sv"
 `include "usbdev_phy_pins_sense_vseq.sv"
 `include "usbdev_phy_config_eop_single_bit_handling_vseq.sv"
+`include "usbdev_phy_config_tx_osc_test_mode_vseq.sv"
 `include "usbdev_pkt_buffer_vseq.sv"
 `include "usbdev_pkt_received_vseq.sv"
 `include "usbdev_pkt_sent_vseq.sv"
