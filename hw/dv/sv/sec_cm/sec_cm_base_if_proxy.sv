f8ce8a15b107778d055da1143d08393c6b6a1ddb
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-06-11 16:28:32 +0100

    [dv] Make it possible to disable FI tests at a particular interface
    This will be used by followup commits (which have explanatory notes).
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/dv/sv/sec_cm/sec_cm_base_if_proxy.sv
+++ hw/dv/sv/sec_cm/sec_cm_base_if_proxy.sv
@@ -7,6 +7,15 @@ virtual class sec_cm_base_if_proxy extends uvm_object;
   sec_cm_type_e sec_cm_type;
   string path;
 
+  // If this bit is set then fault injection sequences will skip this proxy. Code that sets the
+  // fi_disabled flag should have a detailed comment explaining why it is needed.
+  bit fi_disabled = 1'b0;
+
+  function void disable_fi();
+    `uvm_info(`gfn, $sformatf("Disabling fault injection at %0s", path), UVM_MEDIUM)
+    fi_disabled = 1'b1;
+  endfunction
+
   `uvm_object_new
 
   pure virtual task automatic inject_fault();
