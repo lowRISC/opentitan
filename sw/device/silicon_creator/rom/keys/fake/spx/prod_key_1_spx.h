aca619ba270269950b192eda39ac3e318c1b83f6
Jade Philipoom <jadep@zerorisc.com>
2024-06-17 17:28:40 +0200

    [host] Switch host-side SPHINCS+ to SHA2 params and update keys.
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>

--- sw/device/silicon_creator/rom/keys/fake/spx/prod_key_1_spx.h
+++ sw/device/silicon_creator/rom/keys/fake/spx/prod_key_1_spx.h
@@ -8,13 +8,13 @@
 #define PROD_KEY_1_SPX \
   {                    \
     .data = {          \
-      0xd89bea85,      \
-      0x5c4cee6e,      \
-      0x42d832c8,      \
-      0x4f58d90d,      \
-      0x4bb9d020,      \
-      0x4615c2ee,      \
-      0xd5a7158e,      \
-      0xd7aa1829,      \
+      0xad63458a,      \
+      0x496271fa,      \
+      0x8c9a5579,      \
+      0x1f5a1a8b,      \
+      0x3ee242a2,      \
+      0x33ae5f9a,      \
+      0x0356fbb0,      \
+      0x4d83c8d1,      \
     }
 #endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_KEYS_FAKE_SPX_PROD_KEY_1_SPX_H_
