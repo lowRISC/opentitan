9043a65cd20a74eb2120c8e4952df146ea160fb0
Harry Callahan <hcallahan@lowrisc.org>
2024-06-17 15:51:45 +0100

    [i2c,dv] Add i2c_target_nack_acqfull_addr vseq
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>

--- hw/ip/i2c/dv/env/seq_lib/i2c_vseq_list.sv
+++ hw/ip/i2c/dv/env/seq_lib/i2c_vseq_list.sv
@@ -41,4 +41,5 @@
 `include "i2c_target_tx_stretch_ctrl_vseq.sv"
 `include "i2c_target_smbus_maxlen_vseq.sv"
 `include "i2c_target_nack_acqfull_vseq.sv"
+`include "i2c_target_nack_acqfull_addr_vseq.sv"
 `include "i2c_target_nack_txstretch_vseq.sv"
