1adf01cc053fd2c3908169d1bd03a8aeda816ca2
James Wainwright <james.wainwright@lowrisc.org>
2024-06-17 15:53:24 +0100

    [sw] Refactor pinmux config chunking
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- sw/host/opentitanlib/src/test_utils/pinmux_config.rs
+++ sw/host/opentitanlib/src/test_utils/pinmux_config.rs
@@ -2,9 +2,11 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::Result;
+use std::cmp;
 use std::collections::HashMap;
 use std::time::Duration;
+
+use anyhow::Result;
 
 use crate::chip::autogen::earlgrey::{PinmuxInsel, PinmuxMioOut, PinmuxOutsel, PinmuxPeripheralIn};
 use crate::io::uart::Uart;
@@ -16,20 +18,8 @@ use crate::test_utils::status::Status;
 use crate::chip::autogen::earlgrey::ujson_alias::*;
 include!(env!("pinmux_config"));
 
-impl Default for PinmuxConfig {
-    fn default() -> Self {
-        Self {
-            input: PinmuxInputSelection {
-                peripheral: Default::default(),
-                selector: Default::default(),
-            },
-            output: PinmuxOutputSelection {
-                mio: Default::default(),
-                selector: Default::default(),
-            },
-        }
-    }
-}
+/// Capacity of a single configuration message.
+const CONFIG_CAP: usize = 16;
 
 impl PinmuxConfig {
     pub fn configure(
@@ -41,40 +31,43 @@ impl PinmuxConfig {
         // and output configurations.  We'll take the whole config and
         // chunk it into as many PinmuxConfig commands as necessary.
 
-        let len = std::cmp::max(
-            inputs.map(|h| h.len()).unwrap_or(0),
-            outputs.map(|h| h.len()).unwrap_or(0),
-        );
+        let mut inputs: Vec<_> = inputs
+            .into_iter()
+            .flat_map(HashMap::iter)
+            .map(|(&key, &value)| (key, value))
+            .collect();
+        let mut outputs: Vec<_> = outputs
+            .into_iter()
+            .flat_map(HashMap::iter)
+            .map(|(&key, &value)| (key, value))
+            .collect();
 
-        let df_ik = PinmuxPeripheralIn::default();
-        let df_iv = PinmuxInsel::default();
-        let df_ok = PinmuxMioOut::default();
-        let df_ov = PinmuxOutsel::default();
+        let len = cmp::max(inputs.len(), outputs.len())
+            .next_multiple_of(CONFIG_CAP);
 
-        let mut inputs = inputs.map(|h| h.iter());
-        let mut outputs = outputs.map(|h| h.iter());
-        let mut i = 0;
-        while i < len {
-            let mut config = Self::default();
-            for _ in 0..config.input.peripheral.capacity() {
-                let (ik, iv) = inputs
-                    .as_mut()
-                    .and_then(|i| i.next())
-                    .unwrap_or((&df_ik, &df_iv));
-                let (ok, ov) = outputs
-                    .as_mut()
-                    .and_then(|i| i.next())
-                    .unwrap_or((&df_ok, &df_ov));
-                config.input.peripheral.push(*ik);
-                config.input.selector.push(*iv);
-                config.output.mio.push(*ok);
-                config.output.selector.push(*ov);
-                i += 1;
-            }
+        inputs.resize_with(len, Default::default);
+        outputs.resize_with(len, Default::default);
+
+        while !inputs.is_empty() && !outputs.is_empty() {
+            let (in_peripherals, in_selectors) = inputs.drain(..CONFIG_CAP).unzip();
+            let (out_mios, out_selectors) = outputs.drain(..CONFIG_CAP).unzip();
+
+            let config = PinmuxConfig {
+                input: PinmuxInputSelection {
+                    peripheral: in_peripherals,
+                    selector: in_selectors,
+                },
+                output: PinmuxOutputSelection {
+                    mio: out_mios,
+                    selector: out_selectors,
+                },
+            };
+
             TestCommand::PinmuxConfig.send(uart)?;
             config.send(uart)?;
             Status::recv(uart, Duration::from_secs(300), false)?;
         }
+
         Ok(())
     }
 }
