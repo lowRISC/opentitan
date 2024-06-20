4eca6e585f2f65f16653423f3a150516109a76af
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-06-20 17:31:28 +0100

    [rv_dm,dv] Properly implement rv_dm_cmderr_not_supported_vseq
    The existing implementation seems to be rather confused: I don't think
    that the testpoint has anything to do with other random DMI registers.
    I wonder whether the original author was mixed up with
    rv_dm_cmderr_busy_vseq (which *is* related to other register
    accesses).
    
    Simplify things dramatically and tidy up in general.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rv_dm/dv/env/seq_lib/rv_dm_cmderr_not_supported_vseq.sv
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_cmderr_not_supported_vseq.sv
@@ -10,13 +10,50 @@ class rv_dm_cmderr_not_supported_vseq extends rv_dm_base_vseq;
   constraint lc_hw_debug_en_c {
     lc_hw_debug_en == lc_ctrl_pkg::On;
   }
+
+  // Generate an abstract command that will trigger a "not supported" error because it's cmdtype is
+  // unknown.
+  function command_t gen_bogus_abstract_cmd();
+    command_t cmd;
+    `DV_CHECK_STD_RANDOMIZE_FATAL(cmd)
+
+    if (cmd.cmdtype inside {AccessRegister, QuickAccess, AccessMemory}) begin
+      cmd.cmdtype = cmd_e'(cmd.cmdtype + 3);
+    end
+
+    return cmd;
+  endfunction
+
+  // Check that the cmderr field in abstractcs is as expected
+  task check_cmderr(cmderr_e cmderr_exp);
+    abstractcs_t abstractcs;
+    read_abstractcs(abstractcs);
+    `DV_CHECK_EQ(abstractcs.cmderr, cmderr_exp);
+  endtask
+
   task body();
-    write_chk(.ptr(jtag_dmi_ral.progbuf[0]),.cmderr(2),.command(32'h10231000));
-    write_chk(.ptr(jtag_dmi_ral.command),.cmderr(2),.command(32'h10231000));
-    write_chk(.ptr(jtag_dmi_ral.abstractauto),.cmderr(2),.command(32'h10231000));
-    write_chk(.ptr(jtag_dmi_ral.progbuf[0]),.cmderr(2),.command(32'h20231000));
-    write_chk(.ptr(jtag_dmi_ral.command),.cmderr(2),.command(32'h20231000));
-    write_chk(.ptr(jtag_dmi_ral.abstractauto),.cmderr(2),.command(32'h20231000));
+    // Check that there isn't currently any command error (if there is, it will be sticky and we
+    // won't be able to change it to CmdErrNotSupported)
+    check_cmderr(CmdErrNone);
+
+    // Tell the debug module to request a halt and tell it that we're halted (so that we can accept
+    // the abstract command)
+    request_halt();
+
+    // Ask to start a (bogus) abstract command. This won't actually start, but should cause a
+    // command error flag.
+    csr_wr(.ptr(jtag_dmi_ral.command), .value(gen_bogus_abstract_cmd()));
+
+    // Check that the cmderr field of the abstractcs register is now CmdErrNotSupported
+    check_cmderr(CmdErrNotSupported);
+
+    // Clear the cmderr field. This has access W1C and is 3 bits wide, so writing 3'b111 should
+    // clear all the bits.
+    csr_wr(.ptr(jtag_dmi_ral.abstractcs.cmderr), .value(7));
+
+    // Check that we have managed to clear the command error (to leave things clean for following
+    // sequences)
+    check_cmderr(CmdErrNone);
   endtask
 
 endclass : rv_dm_cmderr_not_supported_vseq
