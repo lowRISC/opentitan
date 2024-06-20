38a493acad66d5bc401344c8cdcd24a5f89abbf3
Adrian Lees <a.lees@lowrisc.org>
2024-06-20 23:14:58 +0100

    [dv,usbdev] Refine RX CRC sequence
    Test both invalid CRC5 in token packets and invalid CRC16 in
    data packets.
    Improve test description linguistically and to reflect the
    fact that there is no CRC on handshake packets.
    
    Signed-off-by: Adrian Lees <a.lees@lowrisc.org>

--- hw/ip/usbdev/dv/env/seq_lib/usbdev_rx_crc_err_vseq.sv
+++ hw/ip/usbdev/dv/env/seq_lib/usbdev_rx_crc_err_vseq.sv
@@ -8,16 +8,28 @@ class usbdev_rx_crc_err_vseq extends usbdev_base_vseq;
   `uvm_object_new
 
   task body();
-    // Configure transaction
+    // Configure transaction.
     configure_out_trans(ep_default);
 
-    // Enable rx_crc_err interrupt
+    // Enable rx_crc_err interrupt.
     csr_wr(.ptr(ral.intr_enable.rx_crc_err), .value(1'b1));
 
-    // Out Token packet with corrupted CRC5
-    inject_bad_token_crc5 = 1'b1;
-    send_token_packet(ep_default, PidTypeOutToken);
-    inject_bad_token_crc5 = 1'b0;
+    randcase
+      // Out Token packet with corrupted CRC5.
+      1: begin
+        `uvm_info(`gfn, "Sending OUT token packet with corrupted CRC5", UVM_LOW)
+        inject_bad_token_crc5 = 1'b1;
+        send_token_packet(ep_default, PidTypeOutToken);
+        inject_bad_token_crc5 = 1'b0;
+      end
+      // Out Data packet with corrupted CRC16.
+      1: begin
+        `uvm_info(`gfn, "Sending OUT DATA packet with corrupted CRC16", UVM_LOW)
+        inject_bad_data_crc16 = 1'b1;
+        send_prnd_out_packet(ep_default, PidTypeData0);
+        inject_bad_data_crc16 = 1'b0;
+      end
+    endcase
 
     // Wait a little while for the interrupt signal to become asserted.
     for (int i = 0; i < 16; i++) begin
