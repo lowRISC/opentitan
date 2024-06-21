6104e70f7f64e00ddfd1b2b07eba5aac3546db82
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-06-21 11:53:03 +0100

    [rv_dm,dv] Implement sba_debug_disabled testpoint
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

---
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_sba_debug_disabled_vseq.sv
@@ -0,0 +1,25 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class rv_dm_sba_debug_disabled_vseq extends rv_dm_base_vseq;
+  `uvm_object_utils(rv_dm_sba_debug_disabled_vseq)
+  `uvm_object_new
+
+  // This overrides a constraint in the base vseq: we want to ensure that debug is *not* enabled for
+  // this vseq.
+  constraint debug_enabled_c {
+    lc_hw_debug_en != lc_ctrl_pkg::On;
+  }
+
+  task body();
+    sba_tl_device_seq_start();
+    repeat (10) begin
+      sba_access_item sba_req = sba_access_item::type_id::create("sba_req");
+      `DV_CHECK_RANDOMIZE_FATAL(sba_req);
+      cfg.debugger.sba_access(sba_req);
+    end
+    sba_tl_device_seq_stop();
+  endtask
+
+endclass
