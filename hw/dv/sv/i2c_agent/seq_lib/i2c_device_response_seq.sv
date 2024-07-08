c86c23844f186f51bdf03cb92bce2d19d9882d7b
Harry Callahan <hcallahan@lowrisc.org>
2024-07-08 15:50:45 +0100

    [i2c,dv] Re-implement i2c_device_response_seq using xfer-item inputs
    The changes in #23911 broke this sequence, as we are fundamentally receiving
    feedback data in a different format from the i2c_monitor. This commit builds
    upon the previous changes to the i2c_base_seq.sv to implement the
    response/autoresponder functionality using the new hook-based framework.
    
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>

--- hw/dv/sv/i2c_agent/seq_lib/i2c_device_response_seq.sv
+++ hw/dv/sv/i2c_agent/seq_lib/i2c_device_response_seq.sv
@@ -2,60 +2,104 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+// Agent-Target sequence which accepts all write-transfer data and returns it in read-transfers.
+// aka. 'autoresponder'
+// - Write transfers to any address are accepted, and data is stored in a queue such that response
+//   data to reads is returned in the same order it was written to that address.
+//
 class i2c_device_response_seq extends i2c_base_seq;
+
+  ///////////////
+  // VARIABLES //
+  ///////////////
+
+  typedef bit [7:0] data_q[$];
+
+  // Only 7-bit addresses are supported
+  data_q response_mem[2**Addr7BitMode]; // One queue per 7-bit address.
+
   `uvm_object_utils(i2c_device_response_seq)
   `uvm_object_new
 
-  REQ w_q[256][$];
+  /////////////
+  // METHODS //
+  /////////////
 
-  protected virtual task get_dev_req(output REQ req);
+
+  virtual task body();
     fork
-      begin: isolation_thread
-        fork
-          begin
-            REQ item;
-            p_sequencer.req_analysis_fifo.get(item);
-            `downcast(req, item)
-          end
-          wait (stop);
-        join_any
-        #0;
-        disable fork;
-      end
-    join
-  endtask
-
-  task send_device_mode_txn();
-    // get seq for agent running in Device mode
-    fork
-      forever begin
-        REQ req;
-        get_dev_req(req);
-        if (req != null) begin
-          `uvm_info(`gfn, $sformatf("bus_op:%s drv_type:%s data_q:%p",req.bus_op.name,
-                                    req.drv_type.name, req.data_q), UVM_HIGH)
-          if (req.bus_op == BusOpWrite && req.drv_type == DevAck && req.data_q.size() > 0) begin
-            w_q[req.addr].push_back(req);
-          end else if (req.bus_op == BusOpRead && req.drv_type == RdData) begin
-            if (w_q[req.addr].size() == 0) begin
-              `uvm_fatal(`gfn, $sformatf("Read requested on empty Write queue"))
-            end else begin
-              rsp = w_q[req.addr].pop_front();
-              req.rdata = rsp.wdata;
-            end
-          end
-          start_item(req);
-          finish_item(req);
+      // The base_seq body behaviour for Target operations captures in-progress transfers from
+      // the monitor, then calls a number of hooks methods where we may wish to drive traffic or
+      // capture data. For this sequence, we just need to override the hooks to achieve the
+      // intended autoresponder function.
+      super.body();
+      // Spawn some background processes that run the entire length of the sequence
+      fork
+        // This task clears out the response memory upon each reset assertion
+        forever begin
+          @(negedge cfg.vif.rst_ni);
+          foreach (response_mem[i]) response_mem[i].delete();
         end
-        if (stop) break;
-      end // forever begin
-      forever begin
-        @(negedge cfg.vif.rst_ni);
-        foreach (w_q[i]) begin
-          w_q[i].delete();
+        // The cfg field 'got_stop' is used in block-level DV to prevent new stimulus from being
+        // generated before the previous transaction has completed. When used at chip-level, when
+        // the block-level sequences are not running, we need to handle this field differently.
+        // Simply clear this field here whenever it is set.
+        // TODO(#) Remove this hacky-hack, hopefully along with the cfg.got_stop field altogether.
+        forever begin
+          wait(cfg.got_stop == 1);
+          cfg.got_stop = 0;
         end
-      end
+      join_none
     join
-  endtask // send_device_mode
+  endtask : body
 
-endclass // i2c_device_response_seq
+
+  // We don't need to hook the address-byte ACK here (drive_addr_byte_ack()), as the base class
+  // will ACK to accept all transfers by default, which matches the behaviour we want for this
+  // sequence.
+
+
+  // This method hooks the end of every data byte in an in-progress WRITE transfer.
+  // Store the data byte into the local memory, for recall by the next read operation.
+  virtual task drive_write_byte_ack();
+    bit[Addr7BitMode-1:0] xfer_addr = inp_xfer.addr[Addr7BitMode-1:0];
+
+    response_mem[xfer_addr].push_back(inp_xfer.data_q[$]);
+    `uvm_info(`gfn, $sformatf(
+      "Autoresponder sequence storing the following write : addr:8'h%2x byte:8'h%2x",
+      xfer_addr, inp_xfer.data_q[$]), UVM_DEBUG)
+
+    // Drive the ACK to this write byte.
+    // This sequence will always ACK all write bytes.
+    super.drive_write_byte_ack();
+  endtask : drive_write_byte_ack
+
+
+  // This method hooks the start of every data byte in an inprogress READ transfer.
+  // Fetch any read data from the local memory, and drive the data byte with this value.
+  virtual task drive_read_byte();
+    bit[Addr7BitMode-1:0] xfer_addr = inp_xfer.addr[Addr7BitMode-1:0];
+    bit [7:0]             rdata;
+
+    if (response_mem[xfer_addr].size() == 0) begin
+      // We haven't previously seen a write transfer at this address. There is no data to return.
+      `uvm_fatal(`gfn, $sformatf("Read requested, but write-queue at this address is empty!"))
+    end
+
+    // Fetch the data written to this address, and use it to populate the drive item.
+    rdata = response_mem[xfer_addr].pop_front();
+
+    `uvm_info(`gfn, $sformatf(
+      "Autoresponder sequence transmitting based byte based on read : addr:8'h%2x byte:8'h%2x",
+      xfer_addr, rdata), UVM_DEBUG)
+
+    // Drive the data byte
+    `uvm_create_obj(REQ, req);
+    start_item(req);
+    req.drv_type = RdData;
+    req.rdata = rdata;
+    finish_item(req);
+  endtask : drive_read_byte
+
+
+endclass : i2c_device_response_seq
