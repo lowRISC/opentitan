63b333211cf4db8aa03524cd706168f0bfb5b51e
Douglas Reis <doreis@lowrisc.org>
2024-07-10 17:42:29 +0100

    [ottlib, test_utils] Refactor the i2c bitbanging
    Move the type Bit to the mod.rs to be used by other bitbangging libs.
    
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>
    (cherry picked from commit 69f6dcb06d815e3fce151c032d9467fc4a016b38)

--- sw/host/opentitanlib/src/test_utils/bitbanging/mod.rs
+++ sw/host/opentitanlib/src/test_utils/bitbanging/mod.rs
@@ -3,3 +3,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub mod i2c;
+
+#[repr(u8)]
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Bit {
+    Low = 0,
+    High = 1,
+}
+
+impl From<u8> for Bit {
+    fn from(val: u8) -> Self {
+        match val {
+            0x00 => Self::Low,
+            _ => Self::High,
+        }
+    }
+}
