5c04d62f34689671db9f151b8177d5fb9cf25aba
Harry Callahan <hcallahan@lowrisc.org>
2024-07-13 19:05:35 +0100

    [i2c,dv] Fixup failures in the 'i2c_host_fifo_full_vseq' test
    This test just needed some small tweaks to align stimulus with the new
    abstractions. I also gave it a cleanup while I was here.
    
    Note. There was an icebox comment (#18979) that I have removed while here. In
    essence, it described a more directed testcase that just checked the fifo_full
    status bit when in response to overfilling and then emptying the fifo.
    
    I think this is not required for the following reasons:
    1. This sort of directed testcase mostly just re-verifies the hookup of the
       standard fifo status signals, which have been verified at a primative level to
       be correct in isolation, and get test coverage across many blocks in OpenTitan.
    2. Instead of a directed testcase, we should just model the fifo(s) in the
       i2c_reference_model(), and use that to predict levels/status signals. This way
       we get checking across all possible stimulus, and re-use of the checking in any
       environment where the i2c_env components can be instantiated (one day at
       top-level too).
    3. We shouldn't encourage directed tests when modelling the behaviour in a
       predictor is not that hard. With the recent changes to standardize the i2c_item
       as an TLM-representation of an i2c-transfer, this really shouldn't be difficult to
       model. I think the inconsistent DV architecture made this difficult in the past,
       and under those circumstances a directed tests certainly would have been quicker
       to implement.
    
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>

--- hw/ip/i2c/dv/env/seq_lib/i2c_host_fifo_full_vseq.sv
+++ hw/ip/i2c/dv/env/seq_lib/i2c_host_fifo_full_vseq.sv
@@ -8,6 +8,10 @@ class i2c_host_fifo_full_vseq extends i2c_rx_tx_vseq;
   `uvm_object_utils(i2c_host_fifo_full_vseq)
   `uvm_object_new
 
+  /////////////////
+  // CONSTRAINTS //
+  /////////////////
+
   // fast write data to fmt_fifo to quickly trigger fmt_threshold interrupt
   constraint fmt_fifo_access_dly_c { fmt_fifo_access_dly == 0;}
   // fast read data from rd_fifo after it is full in order to quickly finish simulation
@@ -23,6 +27,11 @@ class i2c_host_fifo_full_vseq extends i2c_rx_tx_vseq;
   // read transaction length is equal to rx_fifo
   constraint num_rd_bytes_c { num_rd_bytes == I2C_RX_FIFO_DEPTH; }
 
+  /////////////
+  // METHODS //
+  /////////////
+
+
   virtual task pre_start();
     // hold reading rx_fifo to ensure rx_fifo gets full
     super.pre_start();
@@ -30,55 +39,61 @@ class i2c_host_fifo_full_vseq extends i2c_rx_tx_vseq;
     print_seq_cfg_vars("pre-start");
   endtask : pre_start
 
+
   virtual task body();
-    bit check_fifo_full = 1'b1;
+    bit do_interrupt = 1'b1;
+    bit do_check_fifo_status = 1'b1;
     initialization();
     `uvm_info(`gfn, "\n--> start of i2c_host_fifo_full_vseq", UVM_DEBUG)
     fork
+      // Every-cycle, check the fifo status signals are consistent with the reported levels.
       begin
-        while (!cfg.under_reset && check_fifo_full) process_fifo_full_status();
+        while (!cfg.under_reset && do_check_fifo_status) begin
+          @(posedge cfg.clk_rst_vif.clk);
+          check_fifo_status(); // Backdoor-read and check all fifo status signals. (0-time)
+        end
       end
+      // Process CONTROLLER-Mode interrupts while the stimulus is being generated.
       begin
-        host_send_trans(num_trans);
-        check_fifo_full = 1'b0; // gracefully stop process_fifo_full_status()
+        while (!cfg.under_reset && do_interrupt) process_interrupts();
+      end
+      // Generate test stimulus transactions
+      begin
+        host_send_trans(num_trans, .trans_type(WriteOnly));
+        do_check_fifo_status = 1'b0; // gracefully stop check_fifo_status() loop
+        do_interrupt = 1'b0; // gracefully stop process_interrupts() loop
       end
     join
     `uvm_info(`gfn, "\n--> end of i2c_host_fifo_full_vseq", UVM_DEBUG)
   endtask : body
 
-  // ICEBOX(#18979): weicai suggested corner tests: send N + 1 items (N is fifo depth), configure agent
-  // to slow read mode (not able to completely handle one item before N+1 items are written),
-  // then check fifo_full bit is
-  //   - not asserted when N items are written to fifo (fifo_lvl = N-1)
-  //   - asserted once writing one more item to fifo (fifo_lvl = N)
-  //   - clear interrupt, finish processing all the items and check interrupt not asserted
 
-  task process_fifo_full_status();
+  // Backdoor-read the DUT-CONTROLLER fifo status registers, and check that the full and empty
+  // flags are consistent with the reported levels and the known size of the fifos.
+  //
+  task check_fifo_status();
+
     bit fmt_full, rx_full, fmt_empty, rx_empty;
     bit [TL_DW-1:0] status, fmt_lvl, rx_lvl;
 
-    @(posedge cfg.clk_rst_vif.clk);
+    // Backdoor-read all of the CONTROLLER-Mode fifo status registers.
+    csr_rd(.ptr(ral.host_fifo_status.fmtlvl), .value(fmt_lvl), .backdoor(1'b1));
+    csr_rd(.ptr(ral.host_fifo_status.rxlvl), .value(rx_lvl), .backdoor(1'b1));
     csr_rd(.ptr(ral.status), .value(status), .backdoor(1'b1));
     fmt_full  = bit'(get_field_val(ral.status.fmtfull, status));
     fmt_empty = bit'(get_field_val(ral.status.fmtempty, status));
     rx_full   = bit'(get_field_val(ral.status.rxfull, status));
     rx_empty  = bit'(get_field_val(ral.status.rxempty, status));
-    csr_rd(.ptr(ral.host_fifo_status.fmtlvl), .value(fmt_lvl), .backdoor(1'b1));
-    csr_rd(.ptr(ral.host_fifo_status.rxlvl), .value(rx_lvl), .backdoor(1'b1));
-    if (!cfg.under_reset) begin
-      if (fmt_full)  begin
-        `DV_CHECK_EQ(fmt_lvl, I2C_FMT_FIFO_DEPTH);
-      end else begin
-        `DV_CHECK_LT(fmt_lvl, I2C_FMT_FIFO_DEPTH);
-      end
-      if (rx_full)   begin
-        `DV_CHECK_EQ(rx_lvl, I2C_RX_FIFO_DEPTH);
-      end else begin
-        `DV_CHECK_LT(rx_lvl, I2C_RX_FIFO_DEPTH);
-      end
-      if (fmt_empty) `DV_CHECK_EQ(fmt_lvl, 0);
-      if (rx_empty)  `DV_CHECK_EQ(rx_lvl, 0);
-    end
-  endtask : process_fifo_full_status
+
+    // Check the reported levels and status bits are consistent with the known fifo sizes.
+    if (fmt_full)  `DV_CHECK_EQ(fmt_lvl, I2C_FMT_FIFO_DEPTH)
+    else           `DV_CHECK_LT(fmt_lvl, I2C_FMT_FIFO_DEPTH)
+    if (rx_full)   `DV_CHECK_EQ(rx_lvl,  I2C_RX_FIFO_DEPTH)
+    else           `DV_CHECK_LT(rx_lvl,  I2C_RX_FIFO_DEPTH)
+    if (fmt_empty) `DV_CHECK_EQ(fmt_lvl, 0)
+    if (rx_empty)  `DV_CHECK_EQ(rx_lvl,  0)
+
+  endtask : check_fifo_status
+
 
 endclass : i2c_host_fifo_full_vseq
