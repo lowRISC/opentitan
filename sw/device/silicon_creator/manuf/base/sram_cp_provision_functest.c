e1be684f276fa5e03e6d51d97a820779c6b4b435
Tim Trippel <ttrippel@google.com>
2024-07-17 11:19:45 -0700

    [manuf] move provisioning fw to `base` directory
    The provisioning firmware in this repo is a baseline reference firmware
    and should be named as such. This partially addresses #23426, and
    enables testing provisioning extensions in pre-silicon.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/device/silicon_creator/manuf/base/sram_cp_provision_functest.c
+++ sw/device/silicon_creator/manuf/base/sram_cp_provision_functest.c
@@ -19,10 +19,10 @@
 #include "sw/device/lib/testing/test_framework/ottf_console.h"
 #include "sw/device/lib/testing/test_framework/ottf_test_config.h"
 #include "sw/device/lib/testing/test_framework/ujson_ottf.h"
+#include "sw/device/silicon_creator/manuf/base/flash_info_permissions.h"
 #include "sw/device/silicon_creator/manuf/lib/flash_info_fields.h"
 #include "sw/device/silicon_creator/manuf/lib/individualize.h"
 #include "sw/device/silicon_creator/manuf/lib/otp_fields.h"
-#include "sw/device/silicon_creator/manuf/skus/earlgrey_a0/sival_bringup/flash_info_permissions.h"
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 #include "otp_ctrl_regs.h"  // Generated.
