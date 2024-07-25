1c1186f0dd2f748a313b6f4ebefa0a2b251d9894
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-07-25 17:32:35 +0100

    [rv_dm,dv] Massively simplify rv_dm_cmderr_exception_vseq
    This gets rid of a magic hex constant (when I "decompiled it", this
    turned out to be an abstract command to write to x0) and replaces it
    with another arbitrary command (reading x0).
    
    It also reworks the sequence by adding comments and making it a bit
    clearer what's going on. We also remove the repetition of the test:
    there's no real reason to believe that running it 10 times will make
    any difference...
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rv_dm/dv/env/seq_lib/rv_dm_cmderr_exception_vseq.sv
+++ hw/ip/rv_dm/dv/env/seq_lib/rv_dm_cmderr_exception_vseq.sv
@@ -2,16 +2,33 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// command error exception test vseq
 class rv_dm_cmderr_exception_vseq extends rv_dm_base_vseq;
   `uvm_object_utils(rv_dm_cmderr_exception_vseq)
-
   `uvm_object_new
 
   task body();
-    // Verify that the cmderr should be AbstractCmdErrException,
-    // if an excepton occurred while executing the command.
-    write_chk(.ptr(tl_mem_ral.exception), .cmderr(AbstractCmdErrException), .command(32'h00231000));
+    uvm_status_e wr_status;
+
+    // Make sure that cmderr isn't currently set
+    clear_cmderr();
+
+    // Tell the debug module to request a halt and tell it that we're halted (so that we can accept
+    // the abstract command)
+    request_halt();
+
+    // Start an abstract command executing
+    csr_wr(.ptr(jtag_dmi_ral.command), .value(gen_read_register_cmd(0)));
+
+    // Now tell rv_dm that there has been an exception while it was executing the command (by
+    // writing over TL to the exception register)
+    tl_mem_ral.exception.write(.status(wr_status), .value(1'b1));
+    `DV_CHECK_EQ(wr_status, UVM_IS_OK)
+
+    // This should have been reflected in the cmderr field of abstractcs.
+    check_cmderr(CmdErrorException);
+
+    // Clear cmderr again (to avoid leaving things in a mess for any following sequence)
+    clear_cmderr();
   endtask : body
 
 endclass : rv_dm_cmderr_exception_vseq
