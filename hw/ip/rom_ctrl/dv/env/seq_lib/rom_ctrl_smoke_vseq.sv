7815b70af545f4047def3e1c33c0ad1391bb4022
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-07-25 15:48:02 +0100

    [rom_ctrl,dv] Halve smoke test(!)
    This matches the behaviour described in the testplan but this time
    chooses randomly whether KMAC sends us the value given as an expected
    digest in the ROM.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rom_ctrl/dv/env/seq_lib/rom_ctrl_smoke_vseq.sv
+++ hw/ip/rom_ctrl/dv/env/seq_lib/rom_ctrl_smoke_vseq.sv
@@ -2,18 +2,20 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// smoke test vseq
 class rom_ctrl_smoke_vseq extends rom_ctrl_base_vseq;
   `uvm_object_utils(rom_ctrl_smoke_vseq)
-
   `uvm_object_new
 
   task body();
+    bit send_expected = $urandom_range(0, 1);
+
+    // Tell the KMAC app agent whether generate the digest that was expected in the ROM.
+    configure_kmac_digest(send_expected);
+
+    // Queue up some memory operations. These will block until the rom check completes.
     do_rand_ops($urandom_range(20, 50));
-    read_digest_regs();
-    dut_init();
-    configure_kmac_digest(1'b1);
-    do_rand_ops($urandom_range(20, 50));
+
+    // Read all the digest and exp_digest registers, checking they match the values we expect.
     read_digest_regs();
   endtask : body
 
