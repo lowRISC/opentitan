1ddc30435eae657044e5c65524b03e81b2394548
Guillermo Maturana <maturana@opentitan.org>
2024-07-26 17:16:24 +0000

    [dv,flash_ctrl] Fix word sweep tests
    These tests rely on read_flash and prog_flash to increment the addresses
    in their flash_op_t argument precisely by the words accessed, but it is
    better to have the caller take care of it. The previous behavior caused
    some failures in flash_ctrl_write_word_sweep.
    
    This is a cherry pick of commit 16cdd48323236e64bbea0fa759149b1f199ae48f
    to branch earlgrey_1.0.0.
    
    Signed-off-by: Guillermo Maturana <maturana@opentitan.org>

--- hw/ip_templates/flash_ctrl/dv/env/seq_lib/flash_ctrl_read_word_sweep_vseq.sv
+++ hw/ip_templates/flash_ctrl/dv/env/seq_lib/flash_ctrl_read_word_sweep_vseq.sv
@@ -17,47 +17,50 @@ class flash_ctrl_read_word_sweep_vseq extends flash_ctrl_otf_base_vseq;
   // It begs a better way to handle this, like just stopping the loop below.
   constraint partition_c {rand_op.partition == FlashPartData;}
 
+  localparam int InitializedMemBytes = 1024;
+
   virtual task body();
     flash_op_t ctrl;
     addr_t start_addr, max_addr;
+    addr_t addr;
 
     int num, bank;
     int mywd;
 
     `DV_CHECK_RANDOMIZE_FATAL(this)
 
+    start_addr = rand_op.addr;
+    max_addr = start_addr + InitializedMemBytes - 1;
     // It may be better to initialize the data right before each individual read.
     begin
-      addr_t addr;
-      addr = rand_op.addr;
+      addr_t addr = start_addr;
       addr[2] = 1'b0;
       `uvm_info("read_word_sweep", $sformatf(
-                "Initializing data from addr:0x%x to:0x%x", addr, addr + 256 * 4 - 1),
+                "Initializing data from addr:0x%x to:0x%x", addr, addr + InitializedMemBytes - 1),
                 UVM_MEDIUM)
-       for (int w = 0; w < 256; w += 2) begin
+       for (; addr <= max_addr; addr += 8) begin
          cfg.update_otf_mem_read_zone(rand_op.partition, rand_op.addr[OTFBankId], addr);
-         addr += 8;
        end
     end
 
     ctrl = rand_op;
     num = 1;
     mywd = 1;
-    start_addr = rand_op.addr;
-    max_addr = start_addr;
+    addr = start_addr;
     repeat(20) begin
-      addr_t end_addr = ctrl.addr + num * mywd - 1;
-      if (end_addr > max_addr) max_addr = end_addr;
+      addr_t end_addr = addr + num * mywd * 4 - 1;
+      if (end_addr > max_addr) break;
+      ctrl.addr = addr;
+      ctrl.otf_addr = addr;
       print_flash_op(ctrl, UVM_MEDIUM);
       `uvm_info("read_word_sweep", $sformatf(
                 "Reading from addr:0x%x, otf_addr:0x%x, num:%0d, wd:%0d",
                 ctrl.addr, ctrl.otf_addr, num, mywd),
                 UVM_MEDIUM)
       read_flash(ctrl, bank, num, mywd);
+      addr += num * mywd * 4;
       mywd = (mywd % 16) + 1;
     end
-    `uvm_info(`gfn, $sformatf("Total test address span: min 0x%x, max 0x%x", start_addr, max_addr),
-              UVM_MEDIUM)
   endtask
 
 endclass : flash_ctrl_read_word_sweep_vseq
