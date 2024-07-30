0f31b7b5dfcf8bef959056d42ac4a4ba6510617b
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-07-30 17:03:05 +0200

    [sw/pentest] Return device ID for SCA tests
    This commit uses the SCA lib function to return the device ID for
    SCA test. By returning the device ID, it is easier to differentiate
    which chip/fpga was tested.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit fa86845)

--- sw/device/tests/penetrationtests/firmware/sca/aes_sca.c
+++ sw/device/tests/penetrationtests/firmware/sca/aes_sca.c
@@ -920,6 +920,11 @@ status_t handle_aes_sca_init(ujson_t *uj) {
   // measurements.
   sca_configure_cpu();
 
+  // Read device ID and return to host.
+  penetrationtest_device_id_t uj_output;
+  TRY(sca_read_device_id(uj_output.device_id));
+  RESP_OK(ujson_serialize_penetrationtest_device_id_t, uj, &uj_output);
+
   return OK_STATUS(0);
 }
 
