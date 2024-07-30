f5728cf5508110247dc35185779452d72c1a6c9c
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-07-30 16:54:44 +0200

    [sw/pentest] Move OTBN FI function documentation into .h file
    To comply with the OpenTitan C style guide, this commit
    moves the OTBN FI function documentation from the .c into the .h
    file.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit 50b66fa)

--- sw/device/tests/penetrationtests/firmware/fi/otbn_fi.c
+++ sw/device/tests/penetrationtests/firmware/fi/otbn_fi.c
@@ -24,11 +24,6 @@ static dif_rv_core_ibex_t rv_core_ibex;
 
 static dif_otbn_t otbn;
 
-/**
- * Reat the error bits of the OTBN accelerator.
- *
- * @returns Error bits.
- */
 status_t read_otbn_err_bits(dif_otbn_err_bits_t *err_bits) {
   dif_otbn_t otbn;
   TRY(dif_otbn_init(mmio_region_from_addr(TOP_EARLGREY_OTBN_BASE_ADDR), &otbn));
@@ -36,20 +31,6 @@ status_t read_otbn_err_bits(dif_otbn_err_bits_t *err_bits) {
   return OK_STATUS(0);
 }
 
-/**
- * otbn.fi.char.hardware.dmem.op.loop command handler.
- *
- * This FI penetration tests executes the following instructions on OTBN:
- * - Initialize register x3=0
- * - Perform 10000 x3 = x3 + 1 additions using hardware loop instructions.
- *   Load loop counter from memory and write back after increment.
- * - Return the value over UART.
- *
- * Faults are injected during the trigger_high & trigger_low.
- * It needs to be ensured that the compiler does not optimize this code.
- *
- * @param uj The received uJSON data.
- */
 status_t handle_otbn_fi_char_hardware_dmem_op_loop(ujson_t *uj) {
   // Clear registered alerts in alert handler.
   sca_registered_alerts_t reg_alerts = sca_get_triggered_alerts();
@@ -94,19 +75,6 @@ status_t handle_otbn_fi_char_hardware_dmem_op_loop(ujson_t *uj) {
   return OK_STATUS(0);
 }
 
-/**
- * otbn.fi.char.hardware.reg.op.loop command handler.
- *
- * This FI penetration tests executes the following instructions on OTBN:
- * - Initialize register x3=0
- * - Perform 10000 x3 = x3 + 1 additions using hardware loop instructions
- * - Return the value over UART.
- *
- * Faults are injected during the trigger_high & trigger_low.
- * It needs to be ensured that the compiler does not optimize this code.
- *
- * @param uj The received uJSON data.
- */
 status_t handle_otbn_fi_char_hardware_reg_op_loop(ujson_t *uj) {
   // Clear registered alerts in alert handler.
   sca_registered_alerts_t reg_alerts = sca_get_triggered_alerts();
@@ -151,21 +119,6 @@ status_t handle_otbn_fi_char_hardware_reg_op_loop(ujson_t *uj) {
   return OK_STATUS(0);
 }
 
-/**
- * otbn.fi.char.unrolled.dmem.op.loop command handler.
- *
- * This FI penetration tests executes the following instructions on OTBN:
- * - Perform 100 times:
- *  - Load loop counter from memory
- *  - Increment loop counter
- *  - Store back to memory
- * - Return the value over UART.
- *
- * Faults are injected during the trigger_high & trigger_low.
- * It needs to be ensured that the compiler does not optimize this code.
- *
- * @param uj The received uJSON data.
- */
 status_t handle_otbn_fi_char_unrolled_dmem_op_loop(ujson_t *uj) {
   // Clear registered alerts in alert handler.
   sca_registered_alerts_t reg_alerts = sca_get_triggered_alerts();
@@ -210,19 +163,6 @@ status_t handle_otbn_fi_char_unrolled_dmem_op_loop(ujson_t *uj) {
   return OK_STATUS(0);
 }
 
-/**
- * otbn.char.unrolled.reg.op.loop command handler.
- *
- * This FI penetration tests executes the following instructions on OTBN:
- * - Initialize register x2=0
- * - Perform 100 x2 = x2 + 1 additions
- * - Return the value over UART.
- *
- * Faults are injected during the trigger_high & trigger_low.
- * It needs to be ensured that the compiler does not optimize this code.
- *
- * @param uj The received uJSON data.
- */
 status_t handle_otbn_fi_char_unrolled_reg_op_loop(ujson_t *uj) {
   // Clear registered alerts in alert handler.
   sca_registered_alerts_t reg_alerts = sca_get_triggered_alerts();
@@ -267,15 +207,6 @@ status_t handle_otbn_fi_char_unrolled_reg_op_loop(ujson_t *uj) {
   return OK_STATUS(0);
 }
 
-/**
- * Initializes the OTBN FI test.
- *
- * Setup the trigger and alert handler. Disable dummy instructions and the
- * iCache.
- *
- * @param uj An initialized uJSON context.
- * @return OK or error.
- */
 status_t handle_otbn_fi_init(ujson_t *uj) {
   // Configure the entropy complex for OTBN. Set the reseed interval to max
   // to avoid a non-constant trigger window.
@@ -310,13 +241,6 @@ status_t handle_otbn_fi_init(ujson_t *uj) {
   return OK_STATUS();
 }
 
-/**
- * OTBN FI command handler.
- *
- * Command handler for the OTBN FI command.
- *
- * @param uj The received uJSON data.
- */
 status_t handle_otbn_fi(ujson_t *uj) {
   otbn_fi_subcommand_t cmd;
   TRY(ujson_deserialize_otbn_fi_subcommand_t(uj, &cmd));
