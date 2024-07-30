d0538a947b6cdda56d96c9c7ff7835112dd9a070
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-07-30 15:59:44 +0200

    [sw/pentest] Update SCA lib
    This extends the SCA lib that is used by the penetration testing
    framework by providing the following functions:
    - Configure max. reseed interval for entropy complex
    - Return device ID
    - Alert handler configuration and return triggered alerts
    
    These functions will be used in an upcoming commit.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit b27ed86)

--- sw/device/tests/penetrationtests/firmware/lib/sca_lib.h
+++ sw/device/tests/penetrationtests/firmware/lib/sca_lib.h
@@ -5,6 +5,59 @@
 #ifndef OPENTITAN_SW_DEVICE_TESTS_PENETRATIONTESTS_FIRMWARE_LIB_SCA_LIB_H_
 #define OPENTITAN_SW_DEVICE_TESTS_PENETRATIONTESTS_FIRMWARE_LIB_SCA_LIB_H_
 
+#include "sw/device/lib/ujson/ujson.h"
+#include "sw/device/tests/penetrationtests/json/sca_lib_commands.h"
+
+typedef struct sca_registered_alerts {
+  uint32_t alerts[3];
+} sca_registered_alerts_t;
+
+/**
+ * Configures the entropy complex for OTBN tests.
+ *
+ * Similar to entropy_testutils_auto_mode_init(), this function inits the
+ * entropy complex. However, in comparison to the function available in the
+ * testutils, this function maximizes the reseed intervall to 0xffffffff.
+ * This is necessary to guarantee a fixed trigger window for OTBN tests.
+ *
+ * @return OK or error.
+ */
+status_t sca_configure_entropy_source_max_reseed_interval(void);
+
+/**
+ * Returns the registered alerts.
+ *
+ * If a fault injection triggered an alert, this function returns the alert ID
+ * back to the host. Afterwards, the alert is cleared.
+ *
+ * @return A struct containing which of the alerts were triggered during the
+ * test.
+ */
+sca_registered_alerts_t sca_get_triggered_alerts(void);
+
+/**
+ * Configures the alert handler for FI.
+ *
+ * Register all alerts and let them escalate to Phase0 only.
+ */
+void sca_configure_alert_handler(void);
+
+/**
+ * Reads the device ID from the LC.
+ *
+ * Can be used to categorize different SCA and FI runs.
+ *
+ * @param device_id A buffer available to store the device id.
+ * @return OK or error.
+ */
+status_t sca_read_device_id(uint32_t device_id[]);
+
+/**
+ * Configures CPU for SCA and FI penetration tests.
+ *
+ * This function disables the iCache and the dummy instructions using the
+ * CPU Control and Status Register (cpuctrlsts).
+ */
 void sca_configure_cpu(void);
 
 #endif  // OPENTITAN_SW_DEVICE_TESTS_PENETRATIONTESTS_FIRMWARE_LIB_SCA_LIB_H_
