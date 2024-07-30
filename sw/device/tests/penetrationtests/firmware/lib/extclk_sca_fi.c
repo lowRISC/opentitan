24d63c0aaf3a42fa9aebebb05b70359af960be5b
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-07-30 16:00:53 +0200

    [sw/pentest] Rename extclk id to penetrationtest
    This commit renames the uJSON data returned by the extclk helper functions
    from cryptotest to penetrationtest.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit f51e36f)

--- sw/device/tests/penetrationtests/firmware/lib/extclk_sca_fi.c
+++ sw/device/tests/penetrationtests/firmware/lib/extclk_sca_fi.c
@@ -32,8 +32,8 @@ static bool did_extclk_settle(const dif_clkmgr_t *clkmgr) {
 }
 
 status_t handle_extclk_sca_fi_configure(ujson_t *uj) {
-  cryptotest_extclk_sca_fi_cfg_t uj_data;
-  TRY(ujson_deserialize_cryptotest_extclk_sca_fi_cfg_t(uj, &uj_data));
+  penetrationtest_extclk_sca_fi_cfg_t uj_data;
+  TRY(ujson_deserialize_penetrationtest_extclk_sca_fi_cfg_t(uj, &uj_data));
 
   TRY(dif_clkmgr_init(mmio_region_from_addr(TOP_EARLGREY_CLKMGR_AON_BASE_ADDR),
                       &clkmgr));
