9c454f79a694aadc5c432cbc90b35141896cdbb9
Tim Trippel <ttrippel@google.com>
2024-08-06 11:28:24 -0700

    [dice] move cert key ID pair type
    This moves the cert key ID type from the DICE lib to the cert lib to
    enable using the type when generating non-DICE keys destined for
    exposing in a certificate (e.g., when generating TPM keys).
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>
    (cherry picked from commit 776309841249cc90e7576e4e8670214bbfdcf673)

--- sw/device/silicon_creator/lib/dice.h
+++ sw/device/silicon_creator/lib/dice.h
@@ -9,6 +9,7 @@
 
 #include "sw/device/lib/testing/json/provisioning_data.h"
 #include "sw/device/silicon_creator/lib/attestation.h"
+#include "sw/device/silicon_creator/lib/cert/cert.h"
 #include "sw/device/silicon_creator/lib/drivers/hmac.h"
 #include "sw/device/silicon_creator/lib/drivers/keymgr.h"
 #include "sw/device/silicon_creator/lib/error.h"
@@ -38,21 +39,6 @@ extern const sc_keymgr_ecc_key_t kDiceKeyTpmCek;
 extern const sc_keymgr_ecc_key_t kDiceKeyTpmCik;
 
 /**
- * A set of public key IDs required to generate an X.509 certificate.
- */
-typedef struct dice_cert_key_id_pair {
-  /**
-   * Pointer to SHA256 digest of the public key matching the private key used to
-   * endorse the certificate.
-   */
-  hmac_digest_t *endorsement;
-  /**
-   * Pointer to SHA256 digest of the public key the certificate is created for.
-   */
-  hmac_digest_t *cert;
-} dice_cert_key_id_pair_t;
-
-/**
  * Generates the UDS attestation keypair and (unendorsed) X.509 TBS certificate.
  *
  * @param key_ids Pointer to the (current and endorsement) public key IDs.
@@ -64,7 +50,7 @@ typedef struct dice_cert_key_id_pair {
  * @return The result of the operation.
  */
 OT_WARN_UNUSED_RESULT
-rom_error_t dice_uds_tbs_cert_build(dice_cert_key_id_pair_t *key_ids,
+rom_error_t dice_uds_tbs_cert_build(cert_key_id_pair_t *key_ids,
                                     ecdsa_p256_public_key_t *uds_pubkey,
                                     uint8_t *tbs_cert, size_t *tbs_cert_size);
 
@@ -84,7 +70,7 @@ rom_error_t dice_uds_tbs_cert_build(dice_cert_key_id_pair_t *key_ids,
 OT_WARN_UNUSED_RESULT
 rom_error_t dice_cdi_0_cert_build(hmac_digest_t *rom_ext_measurement,
                                   uint32_t rom_ext_security_version,
-                                  dice_cert_key_id_pair_t *key_ids,
+                                  cert_key_id_pair_t *key_ids,
                                   ecdsa_p256_public_key_t *cdi_0_pubkey,
                                   uint8_t *cert, size_t *cert_size);
 
@@ -106,7 +92,7 @@ OT_WARN_UNUSED_RESULT
 rom_error_t dice_cdi_1_cert_build(hmac_digest_t *owner_measurement,
                                   hmac_digest_t *owner_manifest_measurement,
                                   uint32_t owner_security_version,
-                                  dice_cert_key_id_pair_t *key_ids,
+                                  cert_key_id_pair_t *key_ids,
                                   ecdsa_p256_public_key_t *cdi_1_pubkey,
                                   uint8_t *cert, size_t *cert_size);
 
@@ -122,7 +108,7 @@ rom_error_t dice_cdi_1_cert_build(hmac_digest_t *owner_measurement,
  * @return The result of the operation.
  */
 OT_WARN_UNUSED_RESULT
-rom_error_t dice_tpm_ek_tbs_cert_build(dice_cert_key_id_pair_t *key_ids,
+rom_error_t dice_tpm_ek_tbs_cert_build(cert_key_id_pair_t *key_ids,
                                        ecdsa_p256_public_key_t *tpm_ek_pubkey,
                                        uint8_t *tbs_cert,
                                        size_t *tbs_cert_size);
@@ -139,7 +125,7 @@ rom_error_t dice_tpm_ek_tbs_cert_build(dice_cert_key_id_pair_t *key_ids,
  * @return The result of the operation.
  */
 OT_WARN_UNUSED_RESULT
-rom_error_t dice_tpm_cek_tbs_cert_build(dice_cert_key_id_pair_t *key_ids,
+rom_error_t dice_tpm_cek_tbs_cert_build(cert_key_id_pair_t *key_ids,
                                         ecdsa_p256_public_key_t *tpm_cek_pubkey,
                                         uint8_t *tbs_cert,
                                         size_t *tbs_cert_size);
@@ -156,7 +142,7 @@ rom_error_t dice_tpm_cek_tbs_cert_build(dice_cert_key_id_pair_t *key_ids,
  * @return The result of the operation.
  */
 OT_WARN_UNUSED_RESULT
-rom_error_t dice_tpm_cik_tbs_cert_build(dice_cert_key_id_pair_t *key_ids,
+rom_error_t dice_tpm_cik_tbs_cert_build(cert_key_id_pair_t *key_ids,
                                         ecdsa_p256_public_key_t *tpm_cik_pubkey,
                                         uint8_t *tbs_cert,
                                         size_t *tbs_cert_size);
