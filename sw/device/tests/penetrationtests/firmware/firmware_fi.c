ccff24efa14f2bc6fca9664d82b7d35aa18f9511
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-08-09 15:25:19 +0200

    [pentest] Add RNG FI tests
    This commit pulls over the following tests from the pentest_edn
    branch of nasahlpa/opentitan that has been used for the penetration testing:
    - rng_fi_entropy_src_bias
    - rng_fi_firmware_override
    - rng_fi_edn_bias
    - rng_fi_edn_resp_ack
    - csrng_bias
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit b4d17c9)

--- sw/device/tests/penetrationtests/firmware/firmware_fi.c
+++ sw/device/tests/penetrationtests/firmware/firmware_fi.c
@@ -14,10 +14,12 @@
 #include "sw/device/tests/penetrationtests/json/commands.h"
 #include "sw/device/tests/penetrationtests/json/ibex_fi_commands.h"
 #include "sw/device/tests/penetrationtests/json/otbn_fi_commands.h"
+#include "sw/device/tests/penetrationtests/json/rng_fi_commands.h"
 
 // Include handlers
 #include "fi/ibex_fi.h"
 #include "fi/otbn_fi.h"
+#include "fi/rng_fi.h"
 #include "lib/extclk_sca_fi.h"
 
 OTTF_DEFINE_TEST_CONFIG(.enable_uart_flow_control = true);
@@ -35,6 +37,9 @@ status_t process_cmd(ujson_t *uj) {
         break;
       case kPenetrationtestCommandOtbnFi:
         RESP_ERR(uj, handle_otbn_fi(uj));
+        break;
+      case kPenetrationtestCommandRngFi:
+        RESP_ERR(uj, handle_rng_fi(uj));
         break;
       default:
         LOG_ERROR("Unrecognized command: %d", cmd);
