dbab1ec6da9381fd2bd3fcfb382ec8beda2ed01f
Guillermo Maturana <maturana@opentitan.org>
2024-08-12 23:28:09 +0000

    [dv,pwrmgr] Remove remaining TODOs
    Also mark ALL_TODOS_RESOLVED and a few others in the V3 checklist:
    - X_PROP_ANALYSIS_COMPLETED: no issues shown in compilation xprop.log
    - FPV_ASSERTIONS_PROVEN_AT_V3, FPV_CODE_COVERAGE_AT_100, and
      FPV_COI_COVERAGE_AT_100 do not apply to pwrmgr
    - TB_LINT_COMPLETE: no issues shown in dashboards
    
    Signed-off-by: Guillermo Maturana <maturana@opentitan.org>

--- hw/top_earlgrey/ip_autogen/pwrmgr/dv/env/seq_lib/pwrmgr_escalation_timeout_vseq.sv
+++ hw/top_earlgrey/ip_autogen/pwrmgr/dv/env/seq_lib/pwrmgr_escalation_timeout_vseq.sv
@@ -25,16 +25,23 @@ class pwrmgr_escalation_timeout_vseq extends pwrmgr_base_vseq;
   int trans_cnt = 0;
   constraint num_trans_c {num_trans inside {[1 : 5]};}
 
+  // This stops the escalation clock for a certain number of cycles, and
+  // checks a reset occurs or not, depending on expect_reset.
   task check_stopped_esc_clk(int stop_cycles, bit expect_reset);
     fork
       begin
-        `uvm_info(`gfn, $sformatf("Stopping escalation clock for %0d cycles", stop_cycles),
+        `uvm_info(`gfn, $sformatf(
+                  "Stopping escalation clock for %0d cycles %s expecting reset",
+                  stop_cycles, expect_reset ? "" : "not "),
                   UVM_MEDIUM)
         cfg.esc_clk_rst_vif.stop_clk();
-        cfg.clk_rst_vif.wait_clks(stop_cycles);
-        `uvm_info(`gfn, "Restarting escalation clock", UVM_MEDIUM)
-        cfg.esc_clk_rst_vif.start_clk();
-        cfg.esc_clk_rst_vif.wait_clks(4000);
+        // The clock will be restarted while handling a reset, so no need to restart it here.
+        if (!expect_reset) begin
+          cfg.clk_rst_vif.wait_clks(stop_cycles);
+          `uvm_info(`gfn, "Restarting escalation clock", UVM_MEDIUM)
+          cfg.esc_clk_rst_vif.start_clk();
+          cfg.esc_clk_rst_vif.wait_clks(4000);
+        end
       end
       begin
         if (expect_reset) begin
@@ -66,7 +73,7 @@ class pwrmgr_escalation_timeout_vseq extends pwrmgr_base_vseq;
     wait_for_fast_fsm(FastFsmActive);
     cfg.slow_clk_rst_vif.set_freq_mhz(1);
     cfg.esc_clk_rst_vif.wait_clks(200);
-    // The timeout is not predictable for two reasons:
+    // The timeout is not accurately predictable for two reasons:
     // - The initial count for the timeout can be from 0 to 7, which means the timeout could
     //   happen between 121 and 128 cycles after the clock.
     // - The timeout has a req-ack synchronizer which has some randomness due to the phase.
@@ -75,9 +82,7 @@ class pwrmgr_escalation_timeout_vseq extends pwrmgr_base_vseq;
     check_stopped_esc_clk(118, 1'b0);
     check_stopped_esc_clk(2000, 1'b1);
     wait_for_fast_fsm(FastFsmActive);
-    // This should generate a reset but it doesn't so the test will fail.
-    // TODO(lowrisc/opentitan#20516): Enable this test when this is fixed.
-    // check_stopped_esc_clk(136, 1'b1);
+    check_stopped_esc_clk(136, 1'b1);
   endtask : body
 
 endclass
