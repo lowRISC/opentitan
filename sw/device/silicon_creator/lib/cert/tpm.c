cd1b428547f1bf5ba54aebad7e68ecfff4094ae4
Vadim Bendebury <vbendeb@google.com>
2024-08-12 17:58:21 -0700

    [tpm_personalize] drop CEK and CIK certificates generation
    These certificates are a ChromeOS TPM extension, let's keep their
    generation in cros extension.
    
    Tested by running bazel test --test_output=streamed \
     //sw/device/silicon_creator/manuf/base:ft_provision_tpm_ext_fpga_hyper310_rom_with_fake_keys
    
    Observed proper generation of USD, CDI_0, CDI_1, and TPM_EK
    certificates.
    
    Signed-off-by: Vadim Bendebury <vbendeb@google.com>

--- sw/device/silicon_creator/lib/cert/tpm.c
+++ sw/device/silicon_creator/lib/cert/tpm.c
@@ -8,9 +8,7 @@
 
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/silicon_creator/lib/cert/cert.h"
-#include "sw/device/silicon_creator/lib/cert/tpm_cek.h"  // Generated.
-#include "sw/device/silicon_creator/lib/cert/tpm_cik.h"  // Generated.
-#include "sw/device/silicon_creator/lib/cert/tpm_ek.h"   // Generated.
+#include "sw/device/silicon_creator/lib/cert/tpm_ek.h"  // Generated.
 #include "sw/device/silicon_creator/lib/drivers/keymgr.h"
 #include "sw/device/silicon_creator/lib/error.h"
 #include "sw/device/silicon_creator/manuf/lib/flash_info_fields.h"
@@ -29,51 +27,10 @@ const sc_keymgr_diversification_t kTpmEkKeymgrDiversifier = {
         },
     .version = 0,
 };
-const sc_keymgr_diversification_t kTpmCekKeymgrDiversifier = {
-    .salt =
-        {
-            0x8a5d086c,
-            0xcbe850b4,
-            0x9aeab7c0,
-            0x8faf44a4,
-            0xc5bf5663,
-            0x217359ab,
-            0xb42fe0fd,
-            0xd06ad674,
-        },
-    .version = 0,
-
-};
-const sc_keymgr_diversification_t kTpmCikKeymgrDiversifier = {
-    .salt =
-        {
-            0x9d664be2,
-            0x8a9739a9,
-            0xe6f815bd,
-            0x8940348b,
-            0x6ee241f7,
-            0xea5b14cd,
-            0x9e81908b,
-            0x15ff16f0,
-        },
-    .version = 0,
-};
 const sc_keymgr_ecc_key_t kTpmKeyEk = {
     .type = kScKeymgrKeyTypeSealing,
     .keygen_seed_idx = kFlashInfoFieldTpmEkKeySeedIdx,
     .keymgr_diversifier = &kTpmEkKeymgrDiversifier,
-    .required_keymgr_state = kScKeymgrStateOwnerKey,
-};
-const sc_keymgr_ecc_key_t kTpmKeyCek = {
-    .type = kScKeymgrKeyTypeSealing,
-    .keygen_seed_idx = kFlashInfoFieldTpmCekKeySeedIdx,
-    .keymgr_diversifier = &kTpmCekKeymgrDiversifier,
-    .required_keymgr_state = kScKeymgrStateOwnerKey,
-};
-const sc_keymgr_ecc_key_t kTpmKeyCik = {
-    .type = kScKeymgrKeyTypeSealing,
-    .keygen_seed_idx = kFlashInfoFieldTpmCikKeySeedIdx,
-    .keymgr_diversifier = &kTpmCikKeymgrDiversifier,
     .required_keymgr_state = kScKeymgrStateOwnerKey,
 };
 
@@ -100,48 +57,6 @@ rom_error_t tpm_ek_tbs_cert_build(cert_key_id_pair_t *key_ids,
 
   HARDENED_RETURN_IF_ERROR(
       tpm_ek_build_tbs(&tpm_ek_tbs_params, tpm_ek_tbs, tpm_ek_tbs_size));
-
-  return kErrorOk;
-}
-
-rom_error_t tpm_cek_tbs_cert_build(cert_key_id_pair_t *key_ids,
-                                   ecdsa_p256_public_key_t *tpm_cek_pubkey,
-                                   uint8_t *tpm_cek_tbs,
-                                   size_t *tpm_cek_tbs_size) {
-  tpm_cek_tbs_values_t tpm_cek_tbs_params = {
-      .auth_key_key_id = (unsigned char *)key_ids->endorsement,
-      .auth_key_key_id_size = kCertKeyIdSizeInBytes,
-      .tpm_cek_pub_key_ec_x = (unsigned char *)tpm_cek_pubkey->x,
-      .tpm_cek_pub_key_ec_x_size = kEcdsaP256PublicKeyCoordBytes,
-      .tpm_cek_pub_key_ec_y = (unsigned char *)tpm_cek_pubkey->y,
-      .tpm_cek_pub_key_ec_y_size = kEcdsaP256PublicKeyCoordBytes,
-      .tpm_cek_pub_key_id = (unsigned char *)key_ids->cert,
-      .tpm_cek_pub_key_id_size = kCertKeyIdSizeInBytes,
-  };
-
-  HARDENED_RETURN_IF_ERROR(
-      tpm_cek_build_tbs(&tpm_cek_tbs_params, tpm_cek_tbs, tpm_cek_tbs_size));
-
-  return kErrorOk;
-}
-
-rom_error_t tpm_cik_tbs_cert_build(cert_key_id_pair_t *key_ids,
-                                   ecdsa_p256_public_key_t *tpm_cik_pubkey,
-                                   uint8_t *tpm_cik_tbs,
-                                   size_t *tpm_cik_tbs_size) {
-  tpm_cik_tbs_values_t tpm_cik_tbs_params = {
-      .auth_key_key_id = (unsigned char *)key_ids->endorsement,
-      .auth_key_key_id_size = kCertKeyIdSizeInBytes,
-      .tpm_cik_pub_key_ec_x = (unsigned char *)tpm_cik_pubkey->x,
-      .tpm_cik_pub_key_ec_x_size = kEcdsaP256PublicKeyCoordBytes,
-      .tpm_cik_pub_key_ec_y = (unsigned char *)tpm_cik_pubkey->y,
-      .tpm_cik_pub_key_ec_y_size = kEcdsaP256PublicKeyCoordBytes,
-      .tpm_cik_pub_key_id = (unsigned char *)key_ids->cert,
-      .tpm_cik_pub_key_id_size = kCertKeyIdSizeInBytes,
-  };
-
-  HARDENED_RETURN_IF_ERROR(
-      tpm_cik_build_tbs(&tpm_cik_tbs_params, tpm_cik_tbs, tpm_cik_tbs_size));
 
   return kErrorOk;
 }
