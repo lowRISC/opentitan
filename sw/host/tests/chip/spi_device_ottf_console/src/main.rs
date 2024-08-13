2dc5a6e1f8482a17c86750a75ffb6b6184a50e4a
Anthony Chen <antchen@google.com>
2024-08-13 05:35:28 -0700

    [test] add example spi_device OTTF console test
    This tests the OTTF spi_device console, which uses the OT spi_device in
    flash mode, along with the flash read buffer and a lightweight framing
    protocol to implement a one-way (RX) console.
    
    Signed-off-by: Anthony Chen <antchen@google.com>
    (cherry picked from commit c01bbc7c43c042d4389f570081d046c99898e314)

---
+++ sw/host/tests/chip/spi_device_ottf_console/src/main.rs
@@ -0,0 +1,105 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use regex::Regex;
+use std::fs;
+use std::path::PathBuf;
+use std::time::Duration;
+
+use opentitanlib::app::TransportWrapper;
+use opentitanlib::console::spi::SpiConsoleDevice;
+use opentitanlib::execute_test;
+use opentitanlib::test_utils;
+use opentitanlib::test_utils::init::InitializeTest;
+use opentitanlib::uart::console::{ExitStatus, UartConsole};
+
+#[derive(Debug, Parser)]
+struct Opts {
+    #[command(flatten)]
+    init: InitializeTest,
+
+    /// Console receive timeout.
+    #[arg(long, value_parser = humantime::parse_duration, default_value = "20s")]
+    timeout: Duration,
+
+    /// Name of the SPI interface to connect to the OTTF console.
+    #[arg(long, default_value = "BOOTSTRAP")]
+    console_spi: String,
+
+    /// Path to the firmware's ELF file, for querying symbol addresses.
+    #[arg(value_name = "FIRMWARE_ELF")]
+    firmware_elf: PathBuf,
+}
+
+fn spi_device_console_test(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+    let mut console = UartConsole {
+        timeout: Some(opts.timeout),
+        exit_success: Some(Regex::new(r"PASS.*\n")?),
+        exit_failure: Some(Regex::new(r"(FAIL|FAULT).*\n")?),
+        newline: true,
+        ..Default::default()
+    };
+    let mut stdout = std::io::stdout();
+    let spi = transport.spi(&opts.console_spi)?;
+
+    let spi_console_device = SpiConsoleDevice::new(&*spi)?;
+    let _ = UartConsole::wait_for(&spi_console_device, r"Running [^\r\n]*", opts.timeout)?;
+
+    /* Load the ELF binary and get the expect data.*/
+    let elf_binary = fs::read(&opts.firmware_elf)?;
+    let object = object::File::parse(&*elf_binary)?;
+    let mut data = test_utils::object::symbol_data(&object, "kTestStr")?;
+    let mut data_str = std::str::from_utf8(&data)?.trim_matches(char::from(0));
+    _ = UartConsole::wait_for(&spi_console_device, data_str, opts.timeout)?;
+    data = test_utils::object::symbol_data(&object, "kTest64bDataStr")?;
+    data_str = std::str::from_utf8(&data)?.trim_matches(char::from(0));
+    _ = UartConsole::wait_for(&spi_console_device, data_str, opts.timeout)?;
+    data = test_utils::object::symbol_data(&object, "kTest256bDataStr")?;
+    data_str = std::str::from_utf8(&data)?.trim_matches(char::from(0));
+    _ = UartConsole::wait_for(&spi_console_device, data_str, opts.timeout)?;
+    data = test_utils::object::symbol_data(&object, "kTest1KbDataStr")?;
+    data_str = std::str::from_utf8(&data)?.trim_matches(char::from(0));
+    // 1KB data will be sent twice.
+    _ = UartConsole::wait_for(&spi_console_device, data_str, opts.timeout)?;
+    _ = UartConsole::wait_for(&spi_console_device, data_str, opts.timeout)?;
+    data = test_utils::object::symbol_data(&object, "kTest4KbDataStr")?;
+    data_str = std::str::from_utf8(&data)?.trim_matches(char::from(0));
+    _ = UartConsole::wait_for(&spi_console_device, data_str, opts.timeout)?;
+
+    let result = console.interact(&spi_console_device, None, Some(&mut stdout))?;
+    match result {
+        ExitStatus::None | ExitStatus::CtrlC => Ok(()),
+        ExitStatus::Timeout => {
+            if console.exit_success.is_some() {
+                Err(anyhow!("Console timeout exceeded"))
+            } else {
+                Ok(())
+            }
+        }
+        ExitStatus::ExitSuccess => {
+            log::info!(
+                "ExitSuccess({:?})",
+                console.captures(result).unwrap().get(0).unwrap().as_str()
+            );
+            Ok(())
+        }
+        ExitStatus::ExitFailure => {
+            log::info!(
+                "ExitFailure({:?})",
+                console.captures(result).unwrap().get(0).unwrap().as_str()
+            );
+            Err(anyhow!("Matched exit_failure expression"))
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+    opts.init.init_logging();
+    let transport = opts.init.init_target()?;
+    execute_test!(spi_device_console_test, &opts, &transport);
+    Ok(())
+}
