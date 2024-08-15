3613aaabff85c8619af718e64dec20312c227adb
Antonio Martinez Zambrana <amz@lowrisc.org>
2024-08-15 11:40:21 +0100

    [spi_host, dv] Forcing clkdivider values to be multiples of 16
    to allow for quicker simtime
    
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/ip/spi_host/dv/env/seq_lib/spi_host_upper_range_clkdiv_vseq.sv
+++ hw/ip/spi_host/dv/env/seq_lib/spi_host_upper_range_clkdiv_vseq.sv
@@ -20,13 +20,35 @@ class spi_host_upper_range_clkdiv_vseq extends spi_host_speed_vseq;
       max_range_value -> (spi_config_regs.clkdiv[i] == cfg.seq_cfg.host_spi_max_clkdiv);
       !max_range_value -> (spi_config_regs.clkdiv[i] inside {[cfg.seq_cfg.host_spi_middle_clkdiv :
                                                               cfg.seq_cfg.host_spi_max_clkdiv]});
+
+      // In order to minimise sim-time the idle/lead/trail delays are all set to 0.
+      foreach (spi_config_regs.csnlead[i]) {
+        spi_config_regs.csnlead[i] == 0;
+      }
+      foreach (spi_config_regs.csntrail[i]) {
+        spi_config_regs.csntrail[i] == 0;
+      }
+      foreach (spi_config_regs.csnidle[i]) {
+        spi_config_regs.csnidle[i] == 0 ;
+      }
     }
   }
+
+`ifdef FAST_PRESCALER
+  constraint spi_config_regs_faster_clkdiv_c {
+    // Only multiples of 16 to quicken up the clock - this constraint should be part of the clkdiv
+    // VSEQ
+    // TODO: disable/adjust csnlead/idle/trail checks when this constraint is available.
+    //       currently those checks are part of a PR not yet in master.
+    foreach(spi_config_regs.clkdiv[i])
+      spi_config_regs.clkdiv[i][3:0] == 0;
+  }
+`endif
 
   constraint num_trans_c {
     // Really low number of TXN generated to avoid lenghty simulation time
     // since we already have a very slow SPI clock
-    num_trans inside {[1 : 3]};
+    num_trans inside {[1 : 2]};
   }
 
   function void pre_randomize();
