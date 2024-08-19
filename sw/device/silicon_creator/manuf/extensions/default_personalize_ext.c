d32e5c7455d9baf9b06082176d35fa2d2094663f
Tommy Chiu <tommychiu@google.com>
2024-08-19 16:56:54 +0800

    [manuf] Update pre-cert-endorsment perso extension hook
    Add one more parameter "dif_flash_ctrl_state_t*" to the perso extension
    function for flash access.
    
    Signed-off-by: Tommy Chiu <tommychiu@google.com>
    (cherry picked from commit 90c257e07534e6bb1fd686b12a7c0e13ff3287da)

--- sw/device/silicon_creator/manuf/extensions/default_personalize_ext.c
+++ sw/device/silicon_creator/manuf/extensions/default_personalize_ext.c
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "sw/device/lib/dif/dif_flash_ctrl.h"
 #include "sw/device/lib/testing/json/provisioning_data.h"
 #include "sw/device/lib/testing/test_framework/status.h"
 #include "sw/device/lib/testing/test_framework/ujson_ottf.h"
@@ -9,7 +10,8 @@
 
 status_t personalize_extension_pre_cert_endorse(
     ujson_t *uj, manuf_certgen_inputs_t *certgen_inputs,
-    manuf_certs_t *tbs_certs, cert_flash_info_layout_t *cert_flash_layout) {
+    manuf_certs_t *tbs_certs, cert_flash_info_layout_t *cert_flash_layout,
+    dif_flash_ctrl_state_t *flash_ctrl_handle) {
   return OK_STATUS();
 }
 
