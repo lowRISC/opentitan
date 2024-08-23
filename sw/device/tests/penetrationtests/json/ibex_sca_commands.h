a668385b593c7d5a5912ee6a36bf521bde55d5cc
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-08-23 11:39:44 +0200

    [pentest] Update and add new Ibex SCA tests
    During the penetration testing, new OTBN FI tests and existing ones have been
    improved. As this work happened in a fork, this PR brings these changes upstream.
    @aewag co-authored these changes.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit f5294e3)

--- sw/device/tests/penetrationtests/json/ibex_sca_commands.h
+++ sw/device/tests/penetrationtests/json/ibex_sca_commands.h
@@ -13,15 +13,26 @@ extern "C" {
 
 #define IBEXSCA_SUBCOMMAND(_, value) \
     value(_, Init) \
+    value(_, KeySideloading) \
     value(_, RFRead) \
+    value(_, RFReadBatchFvsr) \
+    value(_, RFReadBatchRandom) \
     value(_, RFWrite) \
+    value(_, RFWriteBatchFvsr) \
+    value(_, RFWriteBatchRandom) \
     value(_, TLRead) \
+    value(_, TLReadBatchFvsr) \
+    value(_, TLReadBatchFvsrFixAddress) \
+    value(_, TLReadBatchRandom) \
+    value(_, TLReadBatchRandomFixAddress) \
     value(_, TLWrite) \
-    value(_, KeySideloading)
+    value(_, TLWriteBatchFvsr) \
+    value(_, TLWriteBatchFvsrFixAddress) \
+    value(_, TLWriteBatchRandom) \
+    value(_, TLWriteBatchRandomFixAddress)
 UJSON_SERDE_ENUM(IbexScaSubcommand, ibex_sca_subcommand_t, IBEXSCA_SUBCOMMAND);
 
 #define IBEXSCA_TEST_DATA(field, string) \
-    field(num_iterations, uint32_t) \
     field(data, uint32_t, 8)
 UJSON_SERDE_STRUCT(IbexScaTestData, ibex_sca_test_data_t, IBEXSCA_TEST_DATA);
 
@@ -34,9 +45,18 @@ UJSON_SERDE_STRUCT(IbexScaSalt, ibex_sca_salt_t, IBEXSCA_SALT);
     field(share1, uint32_t, 8)
 UJSON_SERDE_STRUCT(IbexScaKey, ibex_sca_key_t, IBEXSCA_KEY);
 
+#define IBEXSCA_TEST_FVSR(field, string) \
+    field(num_iterations, uint32_t) \
+    field(fixed_data, uint32_t)
+UJSON_SERDE_STRUCT(IbexScaTestFvsr, ibex_sca_test_fvsr_t, IBEXSCA_TEST_FVSR);
+
 #define IBEXSCA_RESULT(field, string) \
     field(result, uint32_t)
 UJSON_SERDE_STRUCT(IbexScaResult, ibex_sca_result_t, IBEXSCA_RESULT);
+
+#define IBEXSCA_BATCH(field, string) \
+    field(num_iterations, uint32_t)
+UJSON_SERDE_STRUCT(IbexScaBatch, ibex_sca_batch_t, IBEXSCA_BATCH);
 
 // clang-format on
 
