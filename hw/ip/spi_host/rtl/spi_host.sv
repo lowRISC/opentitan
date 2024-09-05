lowRISC/opentitan

commit 7eb53da738a770a4cc00153eb7179a2c2d9bdda9

[rtl, spi_host] spi_host_fsm fix
The RTL had a bug where with the right timing the write enable in the
command queue could be set "early" and hence cause `sd_en_o` to
change values mid-cycle.
The sd_en_o is now latched and only driven at the time the RTL is
meant to.

Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

hw/ip/spi_host/rtl/spi_host_fsm.sv
@@ -585,30 +585,61 @@ module spi_host_fsm
 
   assign csb_o = csb_q;
 
+  logic [3:0] sd_en_ff_d, sd_en_ff_q;
+  assign sd_en_ff_d = sd_en_o;
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      sd_en_ff_q <= 0;
+    end
+    //TODO: do we need an enable signal?
+    else begin
+      sd_en_ff_q <= sd_en_ff_d;
+    end
+  end
+  logic drive_posedge;
+  // CPOL / CPHASE truth table
+  // ---------------------------------------------
+  // CPHA | CPOL | Drive/sample                  |
+  // ---------------------------------------------
+  // 0    | 0    | drive negedge, sample posedge |
+  // 0    | 1    | drive posedge, sample negedge |
+  // 1    | 0    | drive posedge, sample negedge |
+  // 1    | 1    | drive negedge, sample posedge |
+  // ---------------------------------------------
+  assign drive_posedge = (cpol != cpha);
+
   always_comb begin
     if (&csb_o) begin
       sd_en_o[3:0] = 4'h0;
     end else begin
-      unique case (speed_o)
-        Standard: begin
-          // Observing 'last_bit' ensures we do not deassert the enable too early
-          sd_en_o[0]   = cmd_wr_en_q | cmd_wr_en_last_bit;
-          sd_en_o[1]   = 1'b0;
-          sd_en_o[3:2] = 2'b00;
-        end
-        Dual:     begin
-          sd_en_o[1:0] = {2{cmd_wr_en_q}};
-          sd_en_o[3:2] = 2'b00;
-        end
-        Quad:     begin
-          sd_en_o[3:0] = {4{cmd_wr_en_q}};
-        end
-        default: begin
-          // invalid speed
-          sd_en_o[3:0] = 4'h0;
-        end
-      endcase
-    end
+      // Only update the sd_en_o on the right clock transition. Since sck_o
+      // is generated from the sys clock we ensure we only drive the data on the
+      // right clock edge
+      if( (drive_posedge && sck_o) ||
+          (!drive_posedge && !sck_o) ) begin
+        unique case (speed_o)
+          Standard: begin
+            sd_en_o[0]   = cmd_wr_en_q | cmd_wr_en_last_bit;
+            sd_en_o[1]   = 1'b0;
+            sd_en_o[3:2] = 2'b00;
+          end
+          Dual:     begin
+            sd_en_o[1:0] = {2{cmd_wr_en_q}};
+            sd_en_o[3:2] = 2'b00;
+          end
+          Quad:     begin
+            sd_en_o[3:0] = {4{cmd_wr_en_q}};
+          end
+          default: begin
+            // invalid speed
+            sd_en_o[3:0] = 4'h0;
+          end
+        endcase
+      end
+      else begin
+        sd_en_o  = sd_en_ff_q;
+      end
+    end // else: !if(&csb_o)
   end
 
   //

commit 7ff3af33d237a0852c03955e6ad920d5351e8b51

[top_earlgrey,spi_host] Fix `cio_sd_en_o` for SPI Host 1
This partially reverts commit 7eb53da738a770a4cc00153eb7179a2c2d9bdda9,
which introduced a glitch on the `sd_en_o` output of `spi_host_fsm`, for
`u_spi_host1` of `top_earlgrey`.  Details are as follows:

- This commit adds a parameter, `UpdateSdEnOnSck`, to the `spi_host`
  module and its sub-modules down to `spi_host_fsm`.
  - If set to `1`, the RTL is the currently implemented one, where the
    logic driving `cio_sd_en_o` (in non-passthrough mode)
    combinationally depends on `cio_sck_o`.
  - If set to `0`, the RTL is the one before the commit mentioned above,
    where `cio_sd_en_o` does not combinationally depend on `cio_sck_o`.

- In `top_earlgrey`, the `UpdateSdEnOnSck` parameter is set to:
  - `1` for `u_spi_host0`, meaning that SPI Host 0 keeps the current RTL
    where glitches need to be prevented with timing fixes;
  - `0` for `u_spi_host1`, meaning that SPI Host 1 gets the previous
    RTL that should be glitch-free.

- The reason for the introduced difference between SPI Host 0 and SPI
  Host 1 is that a logic ECO is possible for SPI Host 1 while SPI Host 0
  has to be fixed with a timing ECO.

- The default value of the `UpdateSdEnOnSck` is `0`, meaning that
  `spi_host` block-level DV runs with the partially reverted RTL.
  Together with many regression runs over the past weeks that were run
  on the un-reverted RTL, this should result in broad coverage of both
  code paths.  The main `spi_host` top-level test,
  `chip_sw_spi_host_tx_rx`, randomly selects between SPI Host 0 and 1,
  so both are covered at the top level.

- The reversal inside `spi_host_fsm` is partial in order to minimize the
  netlist diff.  The `drive_posedge` signal as well as the four
  `sd_en_ff_q` flops are unused for `UpdateSdEnOnSck = 0`.

Signed-off-by: Andreas Kurth <adk@lowrisc.org>

hw/ip/spi_host/data/spi_host.hjson
@@ -73,6 +73,17 @@
       desc: "The size of the Cmd FIFO (one segment descriptor per entry)",
       type: "int",
       default: "4"
+    },
+    { name: "UpdateSdEnOnSck",
+      type: "bit",
+      desc: '''
+        Whether to update `cio_sd_en_o` on `cio_sck_o`.  If this parameter is `1'b1`, the logic
+        driving `cio_sd_en_o` (in non-passthrough mode) combinationally depends on `cio_sck_o`.
+        If this parameter is `1'b0`, `cio_sd_en_o` does not combinationally depend on `cio_sck_o`.
+      ''',
+      default: "1'b0",
+      local: "false",
+      expose: "true",
     }
   ],
   available_output_list: [

hw/ip/spi_host/rtl/spi_host.sv
@@ -11,7 +11,8 @@
 module spi_host
   import spi_host_reg_pkg::*;
 #(
-  parameter logic [NumAlerts-1:0] AlertAsyncOn = {NumAlerts{1'b1}}
+  parameter logic [NumAlerts-1:0] AlertAsyncOn = {NumAlerts{1'b1}},
+  parameter bit UpdateSdEnOnSck = 1'b0
 ) (
   input              clk_i,
   input              rst_ni,
@@ -434,7 +435,8 @@ module spi_host
   assign en_sw  = reg2hw.control.spien.q;
 
   spi_host_core #(
-    .NumCS(NumCS)
+    .NumCS(NumCS),
+    .UpdateSdEnOnSck(UpdateSdEnOnSck)
   ) u_spi_core (
     .clk_i,
     .rst_ni,

hw/ip/spi_host/rtl/spi_host_core.sv
@@ -6,7 +6,8 @@
 //
 
 module spi_host_core #(
-  parameter  int NumCS     = 1
+  parameter int NumCS = 1,
+  parameter bit UpdateSdEnOnSck = 1'b0
 ) (
   input                             clk_i,
   input                             rst_ni,
@@ -122,7 +123,8 @@ module spi_host_core #(
   );
 
   spi_host_fsm #(
-    .NumCS(NumCS)
+    .NumCS(NumCS),
+    .UpdateSdEnOnSck(UpdateSdEnOnSck)
   ) u_fsm (
     .clk_i,
     .rst_ni,

hw/ip/spi_host/rtl/spi_host_fsm.sv
@@ -8,7 +8,8 @@
 module spi_host_fsm
   import spi_host_cmd_pkg::*;
 #(
-  parameter  int NumCS = 1
+  parameter int NumCS = 1,
+  parameter bit UpdateSdEnOnSck = 1'b0
 ) (
   input                              clk_i,
   input                              rst_ni,
@@ -608,15 +609,46 @@ module spi_host_fsm
   // ---------------------------------------------
   assign drive_posedge = (cpol != cpha);
 
-  always_comb begin
-    if (&csb_o) begin
-      sd_en_o[3:0] = 4'h0;
-    end else begin
-      // Only update the sd_en_o on the right clock transition. Since sck_o
-      // is generated from the sys clock we ensure we only drive the data on the
-      // right clock edge
-      if( (drive_posedge && sck_o) ||
-          (!drive_posedge && !sck_o) ) begin
+  if (UpdateSdEnOnSck) begin : gen_update_sd_en_on_sck
+    always_comb begin
+      if (&csb_o) begin
+        sd_en_o[3:0] = 4'h0;
+      end else begin
+        // Only update the sd_en_o on the right clock transition. Since sck_o
+        // is generated from the sys clock we ensure we only drive the data on the
+        // right clock edge
+        if( (drive_posedge && sck_o) ||
+            (!drive_posedge && !sck_o) ) begin
+          unique case (speed_o)
+            Standard: begin
+              sd_en_o[0]   = cmd_wr_en_q | cmd_wr_en_last_bit;
+              sd_en_o[1]   = 1'b0;
+              sd_en_o[3:2] = 2'b00;
+            end
+            Dual:     begin
+              sd_en_o[1:0] = {2{cmd_wr_en_q}};
+              sd_en_o[3:2] = 2'b00;
+            end
+            Quad:     begin
+              sd_en_o[3:0] = {4{cmd_wr_en_q}};
+            end
+            default: begin
+              // invalid speed
+              sd_en_o[3:0] = 4'h0;
+            end
+          endcase
+        end
+        else begin
+          sd_en_o  = sd_en_ff_q;
+        end
+      end // else: !if(&csb_o)
+    end
+
+  end else begin : gen_no_update_sd_en_on_sck
+    always_comb begin
+      if (&csb_o) begin
+        sd_en_o[3:0] = 4'h0;
+      end else begin
         unique case (speed_o)
           Standard: begin
             sd_en_o[0]   = cmd_wr_en_q | cmd_wr_en_last_bit;
@@ -635,11 +667,15 @@ module spi_host_fsm
             sd_en_o[3:0] = 4'h0;
           end
         endcase
-      end
-      else begin
-        sd_en_o  = sd_en_ff_q;
-      end
-    end // else: !if(&csb_o)
+      end // else: !if(&csb_o)
+    end
+
+    logic unused_sd_en_ff_q;
+    assign unused_sd_en_ff = ^{sd_en_ff_d, sd_en_ff_q};
+
+    logic unused_drive_posedge;
+    assign unused_drive_posedge = drive_posedge;
+
   end
 
   //

hw/top_earlgrey/data/autogen/top_earlgrey.gen.hjson
@@ -2366,6 +2366,10 @@
           domain: "0"
         }
       }
+      param_decl:
+      {
+        UpdateSdEnOnSck: 1'b1
+      }
       clock_connections:
       {
         clk_i: clkmgr_aon_clocks.clk_io_peri
@@ -2374,8 +2378,23 @@
       [
         "0"
       ]
-      param_decl: {}
-      param_list: []
+      memory: {}
+      param_list:
+      [
+        {
+          name: UpdateSdEnOnSck
+          desc:
+            '''
+            Whether to update `cio_sd_en_o` on `cio_sck_o`.  If this parameter is `1'b1`, the logic
+            driving `cio_sd_en_o` (in non-passthrough mode) combinationally depends on `cio_sck_o`.
+            If this parameter is `1'b0`, `cio_sd_en_o` does not combinationally depend on `cio_sck_o`.
+            '''
+          type: bit
+          default: 1'b1
+          expose: "true"
+          name_top: SpiHost0UpdateSdEnOnSck
+        }
+      ]
       inter_signal_list:
       [
         {
@@ -2425,6 +2444,10 @@
           domain: "0"
         }
       }
+      param_decl:
+      {
+        UpdateSdEnOnSck: 1'b0
+      }
       clock_connections:
       {
         clk_i: clkmgr_aon_clocks.clk_io_div2_peri
@@ -2433,8 +2456,23 @@
       [
         "0"
       ]
-      param_decl: {}
-      param_list: []
+      memory: {}
+      param_list:
+      [
+        {
+          name: UpdateSdEnOnSck
+          desc:
+            '''
+            Whether to update `cio_sd_en_o` on `cio_sck_o`.  If this parameter is `1'b1`, the logic
+            driving `cio_sd_en_o` (in non-passthrough mode) combinationally depends on `cio_sck_o`.
+            If this parameter is `1'b0`, `cio_sd_en_o` does not combinationally depend on `cio_sck_o`.
+            '''
+          type: bit
+          default: 1'b0
+          expose: "true"
+          name_top: SpiHost1UpdateSdEnOnSck
+        }
+      ]
       inter_signal_list:
       [
         {

hw/top_earlgrey/data/top_earlgrey.hjson
@@ -330,6 +330,9 @@
       clock_group: "peri",
       reset_connections: {rst_ni: "spi_host0"},
       base_addr: "0x40300000",
+      param_decl: {
+        UpdateSdEnOnSck: "1'b1",
+      },
     },
     { name: "spi_host1",
       type: "spi_host",
@@ -337,6 +340,9 @@
       clock_group: "peri",
       reset_connections: {rst_ni: "spi_host1"},
       base_addr: "0x40310000",
+      param_decl: {
+        UpdateSdEnOnSck: "1'b0",
+      },
     },
     { name: "usbdev",
       type: "usbdev",

hw/top_earlgrey/rtl/autogen/top_earlgrey.sv
@@ -40,7 +40,9 @@ module top_earlgrey #(
   parameter logic [31:0] LcCtrlIdcodeValue = jtag_id_pkg::LC_CTRL_JTAG_IDCODE,
   // parameters for alert_handler
   // parameters for spi_host0
+  parameter bit SpiHost0UpdateSdEnOnSck = 1'b1,
   // parameters for spi_host1
+  parameter bit SpiHost1UpdateSdEnOnSck = 1'b0,
   // parameters for usbdev
   parameter bit UsbdevStub = 0,
   parameter int UsbdevRcvrWakeTimeUs = 100,
@@ -1577,7 +1579,8 @@ module top_earlgrey #(
       .rst_edn_ni (rstmgr_aon_resets.rst_lc_n[rstmgr_pkg::Domain0Sel])
   );
   spi_host #(
-    .AlertAsyncOn(alert_handler_reg_pkg::AsyncOn[19:19])
+    .AlertAsyncOn(alert_handler_reg_pkg::AsyncOn[19:19]),
+    .UpdateSdEnOnSck(SpiHost0UpdateSdEnOnSck)
   ) u_spi_host0 (
 
       // Input
@@ -1609,7 +1612,8 @@ module top_earlgrey #(
       .rst_ni (rstmgr_aon_resets.rst_spi_host0_n[rstmgr_pkg::Domain0Sel])
   );
   spi_host #(
-    .AlertAsyncOn(alert_handler_reg_pkg::AsyncOn[20:20])
+    .AlertAsyncOn(alert_handler_reg_pkg::AsyncOn[20:20]),
+    .UpdateSdEnOnSck(SpiHost1UpdateSdEnOnSck)
   ) u_spi_host1 (
 
       // Input