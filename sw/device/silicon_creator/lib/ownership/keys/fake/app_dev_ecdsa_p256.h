01c4d716b49e86307edcb8648f0f3283745e1791
Chris Frantz <cfrantz@google.com>
2024-09-10 16:32:40 -0700

    [rom_ext, ownership] Add fake ECDSA application keys
    Add `prod`, `dev` and `test` ECDSA keys for testing.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ sw/device/silicon_creator/lib/ownership/keys/fake/app_dev_ecdsa_p256.h
@@ -0,0 +1,15 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_APP_DEV_ECDSA_P256_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_APP_DEV_ECDSA_P256_H_
+
+#define APP_DEV_ECDSA_P256                                                  \
+  {                                                                         \
+    0xddd43a0c, 0x622265ee, 0xae5b66d4, 0x94c06bca, 0x310493ae, 0x4ef6f61e, \
+        0x7581813f, 0xac7b2ff9, 0xc5aa3b52, 0x07db1c51, 0x905148dc,         \
+        0xe5c458de, 0xebccfe4b, 0x317e5744, 0x863e76fd, 0xa08f1a90,         \
+  }
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_APP_DEV_ECDSA_P256_H_
