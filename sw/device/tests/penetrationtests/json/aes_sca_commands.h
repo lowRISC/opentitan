67a4b73a91f0fd4c6f1ef25ee798f1cfe205c694
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-09-11 17:17:14 +0200

    [pentest] Restructure AES SCA tests
    This commit consists of several cosmetic changes enhancing the
    code readabiliy:
    - Move function header description into header file to comply
      with coding standard
    - Remove break from switch-case
    - Reorder handlers alphabetically
    - Remove the cryptotest prefix
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit c3f4a4c)

--- sw/device/tests/penetrationtests/json/aes_sca_commands.h
+++ sw/device/tests/penetrationtests/json/aes_sca_commands.h
@@ -19,48 +19,48 @@ extern "C" {
 // AES SCA arguments
 
 #define AESSCA_SUBCOMMAND(_, value) \
-    value(_, Init) \
-    value(_, KeySet) \
-    value(_, SingleEncrypt) \
+    value(_, BatchAlternativeEncrypt) \
     value(_, BatchEncrypt) \
     value(_, BatchEncryptRandom) \
-    value(_, FvsrKeySet) \
-    value(_, FvsrKeyBatchGenerate) \
-    value(_, FvsrKeyBatchEncrypt) \
-    value(_, FvsrKeyStartBatchGenerate) \
+    value(_, BatchPlaintextSet) \
     value(_, FvsrDataBatchEncrypt) \
+    value(_, FvsrKeyBatchEncrypt) \
+    value(_, FvsrKeyBatchGenerate) \
+    value(_, FvsrKeySet) \
+    value(_, FvsrKeyStartBatchGenerate) \
+    value(_, Init) \
+    value(_, KeySet) \
     value(_, SeedLfsr) \
     value(_, SeedLfsrOrder) \
-    value(_, BatchAlternativeEncrypt) \
-    value(_, BatchPlaintextSet)
+    value(_, SingleEncrypt)
 UJSON_SERDE_ENUM(AesScaSubcommand, aes_sca_subcommand_t, AESSCA_SUBCOMMAND);
 
 #define AES_SCA_KEY(field, string) \
     field(key, uint8_t, AESSCA_CMD_MAX_KEY_BYTES) \
     field(key_length, size_t)
-UJSON_SERDE_STRUCT(CryptotestAesScaKey, cryptotest_aes_sca_key_t, AES_SCA_KEY);
+UJSON_SERDE_STRUCT(CryptotestAesScaKey, aes_sca_key_t, AES_SCA_KEY);
 
 #define AES_SCA_DATA(field, string) \
     field(data, uint8_t, AESSCA_CMD_MAX_DATA_BYTES)
-UJSON_SERDE_STRUCT(CryptotestAesScaData, cryptotest_aes_sca_data_t, AES_SCA_DATA);
+UJSON_SERDE_STRUCT(CryptotestAesScaData, aes_sca_data_t, AES_SCA_DATA);
 
 #define AES_SCA_TEXT(field, string) \
     field(text, uint8_t, AESSCA_CMD_MAX_DATA_BYTES) \
     field(text_length, size_t)
-UJSON_SERDE_STRUCT(CryptotestAesScaText, cryptotest_aes_sca_text_t, AES_SCA_TEXT);
+UJSON_SERDE_STRUCT(CryptotestAesScaText, aes_sca_text_t, AES_SCA_TEXT);
 
 #define AES_SCA_LFSR(field, string) \
     field(seed, uint8_t, AESSCA_CMD_MAX_LFSR_BYTES)
-UJSON_SERDE_STRUCT(CryptotestAesScaLfsr, cryptotest_aes_sca_lfsr_t, AES_SCA_LFSR);
+UJSON_SERDE_STRUCT(CryptotestAesScaLfsr, aes_sca_lfsr_t, AES_SCA_LFSR);
 
 #define AES_SCA_CIPHERTEXT(field, string) \
     field(ciphertext, uint8_t, AESSCA_CMD_MAX_MSG_BYTES) \
     field(ciphertext_length, uint32_t)
-UJSON_SERDE_STRUCT(CryptotestAesScaCiphertext, cryptotest_aes_sca_ciphertext_t, AES_SCA_CIPHERTEXT);
+UJSON_SERDE_STRUCT(CryptotestAesScaCiphertext, aes_sca_ciphertext_t, AES_SCA_CIPHERTEXT);
 
 #define AES_SCA_FPGA_MODE(field, string) \
     field(fpga_mode, uint8_t)
-UJSON_SERDE_STRUCT(CryptotestAesScaFpgaMode, cryptotest_aes_sca_fpga_mode_t, AES_SCA_FPGA_MODE);
+UJSON_SERDE_STRUCT(CryptotestAesScaFpgaMode, aes_sca_fpga_mode_t, AES_SCA_FPGA_MODE);
 // clang-format on
 
 #ifdef __cplusplus
