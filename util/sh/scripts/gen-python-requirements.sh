bf6e854eaf0e21e70ae19d6797c31ebd6a362ea1
Gary Guo <gary.guo@lowrisc.org>
2024-09-17 16:17:46 +0100

    [python] switch from pip-tools to uv
    `uv pip compile` is compatible with `pip-tools`, but much faster.
    Switch to uv also make us no longer have to depend on `pip`, which is
    considered as an unsafe dependency by `pip-tools`.
    
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

--- util/sh/scripts/gen-python-requirements.sh
+++ util/sh/scripts/gen-python-requirements.sh
@@ -12,8 +12,7 @@ PYTHON_REQS_OUT_FILE="$REPO_TOP/python-requirements.txt"
 
 # The below shellcode autogenerates the `python-requirements.txt`, with hashes,
 # and prepends the license and auto-generated banners at the top of the file.
-pip-compile \
-  --allow-unsafe \
+uv pip compile \
   --generate-hashes \
   --no-annotate \
   --no-header \
@@ -24,6 +23,3 @@ add_autogen_banner "$PYTHON_REQS_OUT_FILE" \
   "./util/sh/scripts/gen-python-requirements.sh"
 echo -e "#\n$(cat "$PYTHON_REQS_OUT_FILE")" > "$PYTHON_REQS_OUT_FILE"
 add_license_banner "$PYTHON_REQS_OUT_FILE"
-
-# opentitan.egg-info is generated as part of pip-compile. Clean it up.
-rm -rf $REPO_TOP/opentitan.egg-info
