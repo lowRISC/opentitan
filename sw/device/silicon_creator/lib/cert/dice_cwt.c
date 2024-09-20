a471a83a84bff76b4146bd2d5699ae15a4102ef2
Tommy Chiu <tommychiu@google.com>
2024-09-20 14:05:49 +0800

    [dice] Add CWT COSE_Key implementation for Uds_pub
    For dice_lib to support different formats including X509 and CWT(CBOR).
    
    Bug: 356532759
    Bug: lowRISC/opentitan:#24281
    Test: ft_provision_dice_cwt_fpga_cw340_rom_with_fake_keys
    
    Signed-off-by: Tommy Chiu <tommychiu@google.com>

--- sw/device/silicon_creator/lib/cert/dice_cwt.c
+++ sw/device/silicon_creator/lib/cert/dice_cwt.c
@@ -2,11 +2,10 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "sw/device/silicon_creator/lib/cert/dice.h"
-
 #include <stdint.h>
 
 #include "sw/device/lib/base/memory.h"
+#include "sw/device/silicon_creator/lib/cert/cbor.h"
 #include "sw/device/silicon_creator/lib/cert/cert.h"
 #include "sw/device/silicon_creator/lib/cert/dice.h"
 #include "sw/device/silicon_creator/lib/drivers/hmac.h"
@@ -17,6 +16,18 @@
 #include "sw/device/silicon_creator/manuf/lib/flash_info_fields.h"
 
 #include "otp_ctrl_regs.h"  // Generated.
+
+// Keys
+const int64_t kCoseKeyKtyLabel = 1;
+const int64_t kCoseKeyAlgLabel = 3;
+const int64_t kCoseEc2CrvLabel = -1;
+const int64_t kCoseEc2XLabel = -2;
+const int64_t kCoseEc2YLabel = -3;
+
+// Values
+const int64_t kCoseKeyAlgEcdsa256 = -7;
+const int64_t kCoseEc2CrvP256 = 1;
+const int64_t kCoseKeyKtyEc2 = 2;
 
 // UDS (Creator) attestation key diverisfier constants.
 // Note: versions are always set to 0 so these keys are always valid from the
@@ -85,10 +96,37 @@ const sc_keymgr_ecc_key_t kDiceKeyCdi1 = {
     .required_keymgr_state = kScKeymgrStateOwnerKey,
 };
 
-
+// PubKeyECDSA256 = {               ; COSE_Key [RFC9052 s7]
+//     1 : 2,                       ; Key type : EC2
+//     3 : AlgorithmES256,          ; Algorithm : ECDSA w/ SHA-256
+//     -1 : 1,                      ; Curve: P256
+//     -2 : bstr,                   ; X coordinate, big-endian
+//     -3 : bstr                    ; Y coordinate, big-endian
+// }
 rom_error_t dice_uds_tbs_cert_build(cert_key_id_pair_t *key_ids,
                                     ecdsa_p256_public_key_t *uds_pubkey,
                                     uint8_t *tbs_cert, size_t *tbs_cert_size) {
+  struct CborOut kCborOutHandle;
+
+  struct CborOut *pCborOut = &kCborOutHandle;
+
+  HARDENED_RETURN_IF_ERROR(
+      cbor_write_out_init(pCborOut, tbs_cert, *tbs_cert_size));
+  HARDENED_RETURN_IF_ERROR(cbor_map_init(pCborOut, 5));
+  HARDENED_RETURN_IF_ERROR(
+      cbor_write_pair_uint_uint(pCborOut, kCoseKeyKtyLabel, kCoseKeyKtyEc2));
+  HARDENED_RETURN_IF_ERROR(cbor_write_pair_uint_int(pCborOut, kCoseKeyAlgLabel,
+                                                    kCoseKeyAlgEcdsa256));
+  HARDENED_RETURN_IF_ERROR(
+      cbor_write_pair_int_uint(pCborOut, kCoseEc2CrvLabel, kCoseEc2CrvP256));
+  HARDENED_RETURN_IF_ERROR(cbor_write_pair_int_bytes(pCborOut, kCoseEc2XLabel,
+                                                     (uint8_t *)uds_pubkey->x,
+                                                     sizeof(uds_pubkey->x)));
+  HARDENED_RETURN_IF_ERROR(cbor_write_pair_int_bytes(pCborOut, kCoseEc2YLabel,
+                                                     (uint8_t *)uds_pubkey->y,
+                                                     sizeof(uds_pubkey->y)));
+  *tbs_cert_size = CborOutSize(pCborOut);
+
   return kErrorOk;
 }
 
