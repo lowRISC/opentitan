f6e66323305029f304731a3154ec8d0e31df4a3f
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-09-26 09:24:22 +0100

    [pinmux,dv] Use multireg_idx in pinmux.hjson
    This overrides the reset value of one copy of the mio_pad_attr
    register to reflect its differing reset value.
    
    We also change the "tags" for that register to turn its reset value
    test back on (since the config file now gives the reset value we
    expect).
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/top_earlgrey/ip_autogen/pinmux/rtl/pinmux.sv
+++ hw/top_earlgrey/ip_autogen/pinmux/rtl/pinmux.sv
@@ -689,4 +689,8 @@ module pinmux
   `ASSUME(PwrMgrStrapSampleOnce0_A, strap_en_i |=> !strap_en_i)
   `ASSUME(PwrMgrStrapSampleOnce1_A, $fell(strap_en_i) |-> always !strap_en_i)
 
+  // The index of tap_strap0 is known in the hjson-based machinery that renders this template. It
+  // also gets passed in from the call site with the TargetCfg parameter. Make sure they match.
+  initial `ASSERT_I(TapStrap0Idx_A, TargetCfg.tap_strap0_idx == 30)
+
 endmodule : pinmux
