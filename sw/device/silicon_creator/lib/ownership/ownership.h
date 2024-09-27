8e9a0ad91f949f4184aa7fa5fff9b19f9a9abac6
Chris Frantz <cfrantz@google.com>
2024-09-27 08:57:40 -0700

    [ownership] Add an `update_mode` to the configuration
    The ownership `update_mode` allows the owner to attenuate the power of
    the unlock key:
    - UpdateMode::Open - The unlock key has full unlock power.
    - UpdateMode::Self - The unlock key can only unlock the device for
      updates to the self-same owner.
    - UpdateMode::NewVersion - The device cannot unlock, but can accept a
      signed update from the self-same owner.
    
    1. Add an `unlock_mode` field to the owner config and the associated
       UpdateMode constants.
    2. Based on the UpdateMode, allow or deny the unlock command.
    3. When in the NewVersion mode, check for updates during locked owner
       initialization.
    4. Implement owner page lockdown based on the ownership state and update
       mode.
    5a. Test that updates are accepted in NewVersion mode.
    5b. Test that the permissions are set appropriately on the owner pages.
    
    Addresses: #24657
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>
    (cherry picked from commit 17815172b9e3b9051a9140d2b1f42011fe49b940)
    (cherry picked from commit 13a3bdc7eac90905ff8974d0236e7575d7cef1cd)

--- sw/device/silicon_creator/lib/ownership/ownership.h
+++ sw/device/silicon_creator/lib/ownership/ownership.h
@@ -20,12 +20,19 @@ rom_error_t ownership_init(boot_data_t *bootdata, owner_config_t *config,
 /**
  * Lockdown the flash configuration.
  *
- *
  * @param bootdata The current bootdata.
  * @param config The current owner configuration.
  * @return error state.
  */
 rom_error_t ownership_flash_lockdown(boot_data_t *bootdata,
                                      const owner_config_t *config);
+
+/**
+ * Lockdown the ownership info pages.
+ *
+ * @param bootdata The current bootdata.
+ * @param rescue Whether the ROM_EXT is in rescue mode.
+ */
+void ownership_pages_lockdown(boot_data_t *bootdata, hardened_bool_t rescue);
 
 #endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_OWNERSHIP_H_
