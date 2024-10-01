c81cdfbcaed74360495ae673ffde7480e84e868b
James Wainwright <james.wainwright@lowrisc.org>
2024-10-01 17:20:19 +0100

    [ci] Convert Azure errors to GHA errors
    This commit drops the upload of the Doxygen warning logs since this does
    not seem very useful. Doxygen can be run fairly quickly locally to find
    out the warnings.
    
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
    (cherry picked from commit e116ba687edffd1fdad5641e68f167c1af807ad0)

--- ci/scripts/check_bazel_target_names.py
+++ ci/scripts/check_bazel_target_names.py
@@ -10,15 +10,13 @@ import sys
 
 from lib.bazel_query import BazelQueryRunner
 
-AZURE_PIPELINES_ERROR = "##vso[task.logissue type=warning]"
 
 if __name__ == '__main__':
     bazel = BazelQueryRunner()
 
     results = list(bazel.find_targets_with_banned_chars())
     if results:
-        print(AZURE_PIPELINES_ERROR +
-              "Some target names have banned characters.")
+        print("::error::Some target names have banned characters.")
     for target, bad_chars in results:
         print("Target name contains banned characters {}: {}".format(
             bad_chars, target))
