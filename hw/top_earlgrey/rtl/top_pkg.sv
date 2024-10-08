684eff865c045ee08c3c73feb3691de788cc9a62
Robert Schilling <rschilling@rivosinc.com>
2024-10-08 20:43:42 +0200

    [tlul,rtl] Derive TLUL-rsvd width from TLUL a_user width
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/top_earlgrey/rtl/top_pkg.sv
+++ hw/top_earlgrey/rtl/top_pkg.sv
@@ -9,7 +9,7 @@ localparam int TL_AW=32;
 localparam int TL_DW=32;    // = TL_DBW * 8; TL_DBW must be a power-of-two
 localparam int TL_AIW=8;    // a_source, d_source
 localparam int TL_DIW=1;    // d_sink
-localparam int TL_AUW=21;   // a_user
+localparam int TL_AUW=23;   // a_user
 localparam int TL_DUW=14;   // d_user
 localparam int TL_DBW=(TL_DW>>3);
 localparam int TL_SZW=$clog2($clog2(TL_DBW)+1);
