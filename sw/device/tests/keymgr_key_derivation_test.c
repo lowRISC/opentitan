913c56f1cff3ac119343e78a3a98b9232dc7242c
Andrea Caforio <andrea.caforio@lowrisc.org>
2024-10-10 12:44:06 +0200

    [sival, keymgr] State-agnostic chip_sw_key_derivation_test
    keymgr_key_derivation_test_fpga_cw310_sival_rom_ext test was marked as
    broken in #21706. This commit fixes this and addresses #21500.
    
    The source of the bug is due to the nature of the ROM_EXT that runs
    through a CDI attestation flow advancing the key manager state to
    "OwnerRootKey" in the process. This was not accounted for in the test,
    which assumed an active "CreatorRootKey" state (this works for sim_dv
    tests but not with ROM_EXT). The solution to this issue lies in
    rendering the test state-agnostic, meaning it will execute its
    verifications independently of the current key manager state. This
    approach thus mimics the keymgr_sideload_{aes,kmac,otbn} tests which
    follow a similar strategy.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>

--- sw/device/tests/keymgr_key_derivation_test.c
+++ sw/device/tests/keymgr_key_derivation_test.c
@@ -7,43 +7,58 @@
 #include "sw/device/lib/testing/keymgr_testutils.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 
-
 OTTF_DEFINE_TEST_CONFIG();
 
 bool test_main(void) {
-  // Initialize keymgr and advance to CreatorRootKey state.
   dif_keymgr_t keymgr;
   dif_kmac_t kmac;
-  CHECK_STATUS_OK(keymgr_testutils_startup(&keymgr, &kmac));
 
-  CHECK_STATUS_OK(keymgr_testutils_advance_state(&keymgr, &kOwnerIntParams));
-  CHECK_STATUS_OK(keymgr_testutils_check_state(
-      &keymgr, kDifKeymgrStateOwnerIntermediateKey));
-  LOG_INFO("Keymgr entered OwnerIntKey State");
+  // Initialize keymgr and advance to the state specified by the loaded ROM.
+  CHECK_STATUS_OK(keymgr_testutils_initialize(&keymgr, &kmac));
+
+  const char *state_name;
+  CHECK_STATUS_OK(keymgr_testutils_state_string_get(&keymgr, &state_name));
+
+  LOG_INFO("Keymgr entered %s State", state_name);
 
   CHECK_STATUS_OK(keymgr_testutils_generate_identity(&keymgr));
-  LOG_INFO("Keymgr generated identity at OwnerIntKey State");
+  LOG_INFO("Keymgr generated identity at %s State", state_name);
 
+  dif_keymgr_versioned_key_params_t sideload_params = kKeyVersionedParams;
+
+  // Get the maximum key version supported by the keymgr in its current state.
+  uint32_t max_key_version;
   CHECK_STATUS_OK(
-      keymgr_testutils_generate_versioned_key(&keymgr, kKeyVersionedParams));
-  LOG_INFO("Keymgr generated SW output at OwnerIntKey State");
+      keymgr_testutils_max_key_version_get(&keymgr, &max_key_version));
+
+  if (sideload_params.version > max_key_version) {
+    LOG_INFO("Key version %d is greater than the maximum key version %d",
+             sideload_params.version, max_key_version);
+    LOG_INFO("Setting key version to the maximum key version %d",
+             max_key_version);
+    sideload_params.version = max_key_version;
+  }
+
+  // Generate sw versioned key.
+  CHECK_STATUS_OK(
+      keymgr_testutils_generate_versioned_key(&keymgr, sideload_params));
+  LOG_INFO("Keymgr generated SW output at %s State", state_name);
 
   // Generate sideload keys for 3 HW interfaces - Kmac, Aes, Otbn.
-  dif_keymgr_versioned_key_params_t sideload_params = kKeyVersionedParams;
   sideload_params.dest = kDifKeymgrVersionedKeyDestKmac;
   CHECK_STATUS_OK(
       keymgr_testutils_generate_versioned_key(&keymgr, sideload_params));
-  LOG_INFO("Keymgr generated HW output for Kmac at OwnerIntKey State");
+  LOG_INFO("Keymgr generated HW output for Kmac at %s State", state_name);
 
   sideload_params.dest = kDifKeymgrVersionedKeyDestAes;
   CHECK_STATUS_OK(
       keymgr_testutils_generate_versioned_key(&keymgr, sideload_params));
-  LOG_INFO("Keymgr generated HW output for Aes at OwnerIntKey State");
+  LOG_INFO("Keymgr generated HW output for Aes at %sState", state_name);
 
   sideload_params.dest = kDifKeymgrVersionedKeyDestOtbn;
   CHECK_STATUS_OK(
       keymgr_testutils_generate_versioned_key(&keymgr, sideload_params));
-  LOG_INFO("Keymgr generated HW output for Otbn at OwnerIntKey State");
+  LOG_INFO("Keymgr generated HW output for Otbn at %s State", state_name);
 
   CHECK_STATUS_OK(keymgr_testutils_disable(&keymgr));
   CHECK_STATUS_OK(
