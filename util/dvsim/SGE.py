1ad4d3703cdd70fb59b40f654b338813a7f716b9
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-10-10 15:55:53 +0100

    [dvsim] Add typing annotations to SGE.py
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- util/dvsim/SGE.py
+++ util/dvsim/SGE.py
@@ -11,6 +11,7 @@ import os
 import re
 import subprocess
 import time
+from typing import Dict, Iterator, List, Optional, Union
 
 #
 from qsubopts import qsubOptions
@@ -21,7 +22,7 @@ class _JobData:
     """
     Internal helper class to manage job data from qstat
     """
-    def __init__(self, qstat_job_line):
+    def __init__(self, qstat_job_line: str) -> None:
         # The format of the line goes like
         # job-ID prior name  user  state submit/start at queue slots ja-task-ID
 
@@ -61,7 +62,7 @@ class _JobData:
                 raise ValueError(f"Could not parse line: {qstat_job_line}")
 
         # Convert array indices ja_task_id into python list format
-        ja_task_id = []
+        ja_task_id: List[Union[int, str]] = []
         if self.ja_task_id is not None:
             for blob in self.ja_task_id.split(','):
                 # Parse data of form '193-349:1' or '1934'
@@ -75,7 +76,7 @@ class _JobData:
 
         self._ja_tasklist = ja_task_id
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         repr = ['{']
         for key, value in self.__dict__.items():
             if key[0] != '_':
@@ -89,23 +90,23 @@ class JobList:
     Internal helper class to manage job lists
     """
 
-    def __init__(self, qstat_output=None):
+    def __init__(self, qstat_output: bytes) -> None:
         self._joblist = []
-        for line in qstat_output.split('\n')[2:-1]:
-            self._joblist.append(_JobData(line))
+        for line in qstat_output.split(b'\n')[2:-1]:
+            self._joblist.append(_JobData(str(line)))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[_JobData]:
         for job in self._joblist:
             yield job
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '\n'.join([str(job) for job in self._joblist])
 
 
 class SGE:
     """External system call handler for Sun Grid Engine environment."""
 
-    def __init__(self, q=None, path='', ):
+    def __init__(self, q: Optional[str] = None, path: str = '') -> None:
 
         logger = logging.getLogger('SGE.__init__')
 
@@ -131,8 +132,12 @@ Queues detected: %s""", self.q)
         self.cmd_qsub = os.path.join(path, 'qsub')
         self.cmd_qstat = os.path.join(path, 'qstat')
 
-    def wait(self, jobid, interval=10, name=None, pbar=None,
-             pbar_mode=None):
+    def wait(self,
+             jobid: object,
+             interval: int = 10,
+             name: Optional[str] = None,
+             pbar: object = None,
+             pbar_mode: object = None) -> None:
         """Waits for job running on SGE Grid Engine environment to finish.
 
         If you are just waiting for one job, this becomes a dumb substitute for
@@ -156,15 +161,15 @@ Queues detected: %s""", self.q)
                 logger.error('Progress bar handling not implemented')
 
             dowait = False
-            for line in pout.split('\n'):
+            for line in pout.split(b'\n'):
                 t = line.split()
-                if len(t) >= 5 and t[0] == str(jobid):
+                if len(t) >= 5 and str(t[0]) == str(jobid):
                     # Find a line with useful info
-                    if re.search(t[4], 'qwrt'):
+                    if re.search(t[4], b'qwrt'):
                         # Job must be queued, running or being transferred
                         dowait = True
                         break
-                    if re.search(t[4], 'acuE'):
+                    if re.search(t[4], b'acuE'):
                         # Job or host in error state
                         logger.warning('Job %d in error state', str(jobid))
 
@@ -177,9 +182,18 @@ Queues detected: %s""", self.q)
                     logger.info("Time %s: waiting for job '%s' (jobid %s) to \
 finish", time.ctime(), name, str(jobid))
 
-    def submit(self, job, array=False, useenvironment=True, usecwd=True,
-               name=None, stdin=None, stdout=None, stderr=None,
-               joinstdouterr=True, nproc=1, wait=True, lammpi=True):
+    def submit(self, job: str,
+               array: Optional[List[str]] = None,
+               useenvironment: bool = True,
+               usecwd: bool = True,
+               name: Optional[str] = None,
+               stdin: Optional[str] = None,
+               stdout: Optional[str] = None,
+               stderr: Optional[str] = None,
+               joinstdouterr: bool = True,
+               nproc: int = 1,
+               wait: bool = True,
+               lammpi: bool = True) -> int:
         """
         Submits a job to SGE
         Returns jobid as a number
@@ -211,11 +225,10 @@ Stderr: %s", stdout, stderr)
             qsuboptslist.append('-cwd')
         if useenvironment:
             qsuboptslist.append('-V')
-        if array is not False:
+        if array is not None:
             try:
                 n = int(array[0])
             except IndexError:
-                n = int(array)
                 raise IndexError("List is empty!")
             except ValueError:
                 error_msg = "array[0] being an out of bounds access."
@@ -224,10 +237,8 @@ Stderr: %s", stdout, stderr)
             try:
                 m = int(array[1])
             except ValueError:
-                m = None
                 raise ValueError("Could not convert data to an integer.")
             except IndexError:
-                m = None
                 raise IndexError("array[1] being an out of bounds access.")
             try:
                 s = int(array[2])
@@ -266,7 +277,7 @@ Output was:
             logger.error(error_msg)
             raise IOError(error_msg)
 
-    def getuserjobs(self, user=pwd.getpwuid(os.getuid())[0]):
+    def getuserjobs(self, user: Optional[str] = None) -> List[_JobData]:
         """Returns a list of SGE jobids run by a specific user
 
         Inputs
@@ -274,20 +285,26 @@ Output was:
             qstat - path to qstat binary (Default = 'qstat')
         """
 
+        if user is None:
+            user = pwd.getpwuid(os.getuid())[0]
+
         p = subprocess.Popen(self.cmd_qstat + " -u " + user, shell=True,
                              stdout=subprocess.PIPE)
         qstat_output, _ = p.communicate()
         joblist = JobList(qstat_output)
         return [job for job in joblist if job.user == user]
 
-    def run_job(self, command, name='default', logfnm='default.log',
-                wait=True):
+    def run_job(self,
+                command: str,
+                name: str = 'default',
+                logfnm: str = 'default.log',
+                wait: bool = True) -> int:
         """Run job on SGE with piped logging."""
         jobid = self.submit(command, name=name, stdout=logfnm,
                             stderr=logfnm, wait=wait)
         return jobid
 
-    def get_queue_instance_status(self):
+    def get_queue_instance_status(self) -> List[Dict[str, object]]:
         """
         Get loads for each queue instance
         """
@@ -308,15 +325,19 @@ Output was:
         return data
 
 
-def _exec(command, print_to_screen=False, logfnm=None, stdin='',
-          print_command=False):
+def _exec(command: str,
+          print_to_screen: bool = False,
+          logfnm: Optional[str] = None,
+          stdin: str = '',
+          print_command: bool = False) -> str:
     """
     Runs command line using subprocess, optionally returning stdout
     """
-    def _call_cmd(command, stdin=''):
+    def _call_cmd(command: str, stdin: str = '') -> str:
         p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE,
                              stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
+                             stderr=subprocess.STDOUT,
+                             universal_newlines=True)
         output, _ = p.communicate(stdin)
         return output
 
