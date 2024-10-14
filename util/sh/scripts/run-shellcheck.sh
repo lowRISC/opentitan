16f6b2b6a20ea283bc308da5832cfc261d773902
James Wainwright <james.wainwright@lowrisc.org>
2024-10-14 16:32:42 +0100

    [sh] Fix shellcheck errors in run-shellcheck.sh
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- util/sh/scripts/run-shellcheck.sh
+++ util/sh/scripts/run-shellcheck.sh
@@ -19,11 +19,15 @@ else
     SHELLCHECK="./bazelisk.sh run @shellcheck//:shellcheck --"
 fi
 
-EXCLUDED_DIRS="-name third_party -o -name vendor -o -name build-site -o -name lowrisc_misc-linters"
+EXCLUDED_DIRS=(
+    "-name" "third_party" "-o"
+    "-name" "vendor" "-o"
+    "-name" "build-site" "-o"
+    "-name" "lowrisc_misc-linters"
+)
 # Get an array of all shell scripts to check using input redirection and
 # process substitution. For details on this syntax, see ShellCheck SC2046.
-# shellcheck disable=SC2046
 readarray -t shell_scripts < \
-    <(find . \( $EXCLUDED_DIRS \) -prune -o -name '*.sh' -print)
+    <(find . \( "${EXCLUDED_DIRS[@]}" \) -prune -o -name '*.sh' -print)
 
-$SHELLCHECK --severity=warning "${shell_scripts[@]}"
+"$SHELLCHECK" --severity=warning "${shell_scripts[@]}"
