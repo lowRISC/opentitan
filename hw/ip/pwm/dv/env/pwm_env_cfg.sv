50d6d2c4ed6b6f25943adceeb3fef077bfad7004
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-10-29 15:00:23 +0000

    [pwm,dv] Rewrite pwm_env_cfg using out-of-block declarations
    Since we were touching the code anyway, I've also changed the way that
    we randomise the core frequency (divided down from the bus frequency).
    The code looked like the scaling was configurable, but it is always
    enabled. It also re-randomised on every call of what looks like a
    getter (after the rest of the class had been randomised).
    
    This commit changes things so we randomise the scaling ratio (picking
    a value between 1/4 and 1) and then apply it with the call to
    get_clk_core_freq. This two-stage process is needed because we
    randomise the cfg object before connecting up the vif that determines
    the bus frequency.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    
    [pwm,dv] Remove unused en_random argument from get_clk_core_freq
    
    This is never supplied at the call sites (and I don't think it ever
    has been since it was defined in 2021!).
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/pwm/dv/env/pwm_env_cfg.sv
+++ hw/ip/pwm/dv/env/pwm_env_cfg.sv
@@ -8,48 +8,45 @@ class pwm_env_cfg extends cip_base_env_cfg #(.RAL_T(pwm_reg_block));
 
   `uvm_object_new
 
-    // configs
+  // configs
   pwm_monitor_cfg       m_pwm_monitor_cfg[PWM_NUM_CHANNELS];
 
   // virtual ifs
   virtual clk_rst_if    clk_rst_core_vif;
-  int                   core_clk_freq_mhz;
 
-  // ratio between bus_clk and core_clk (must be >= 1)
-  rand int clk_ratio;
-  constraint clk_ratio_c { clk_ratio inside {[1: 4]}; }
+  // A scaling used to convert from the clock frequency in clk_rst_vif (the bus clock) to the core
+  // clock. This is a ratio, but scaled so 1.0 is represented by 1024. The scaling is constrained to
+  // be between 25% and 100%.
+  rand int unsigned clk_scale;
+  constraint clk_scale_c { clk_scale inside {[256:1024]}; }
 
-  virtual function void initialize(bit [31:0] csr_base_addr = '1);
-    list_of_alerts = pwm_env_pkg::LIST_OF_ALERTS;
-    super.initialize(csr_base_addr);
+  // Method from dv_base_env_cfg. Construct RAL models and fill in monitor configs.
+  extern virtual function void initialize(bit [31:0] csr_base_addr = '1);
 
-    // create pwm_agent_cfg
-    foreach (m_pwm_monitor_cfg[i]) begin
-      m_pwm_monitor_cfg[i] = pwm_monitor_cfg::type_id::create($sformatf("m_pwm_monitor%0d_cfg", i));
-      m_pwm_monitor_cfg[i].if_mode = Device;
-      m_pwm_monitor_cfg[i].is_active = 0;
-      m_pwm_monitor_cfg[i].monitor_id = i;
-    end
-
-    // only support 1 outstanding TL items in tlul_adapter
-    m_tl_agent_cfg.max_outstanding_req = 1;
-  endfunction
-
-  // clk_core_freq_mhz is assigned by
-  // - a slower frequency in range [bus_clock*scale : bus_clock] if en_random is set (scale <= 1)
-  // - bus_clock frequency otherwise
-  virtual function int get_clk_core_freq(uint en_random = 1);
-    int clk_core_min, clk_core_max, clk_core_mhz;
-
-    if (en_random) begin
-      `DV_CHECK_MEMBER_RANDOMIZE_FATAL(clk_ratio)
-      `DV_CHECK_GE(clk_ratio, 1)
-      clk_core_max = clk_rst_vif.clk_freq_mhz;
-      clk_core_min = int'(clk_rst_vif.clk_freq_mhz / clk_ratio);
-      clk_core_mhz = $urandom_range(clk_core_min, clk_core_max);
-    end else begin
-      clk_core_mhz = clk_rst_vif.clk_freq_mhz;
-    end
-    return clk_core_mhz;
-  endfunction : get_clk_core_freq
+  // Return the scaled frequency in MHz for the core
+  extern virtual function int get_clk_core_freq();
 endclass : pwm_env_cfg
+
+function void pwm_env_cfg::initialize(bit [31:0] csr_base_addr = '1);
+  list_of_alerts = pwm_env_pkg::LIST_OF_ALERTS;
+  super.initialize(csr_base_addr);
+
+  // create pwm_agent_cfg
+  foreach (m_pwm_monitor_cfg[i]) begin
+    m_pwm_monitor_cfg[i] = pwm_monitor_cfg::type_id::create($sformatf("m_pwm_monitor%0d_cfg", i));
+    m_pwm_monitor_cfg[i].if_mode = Device;
+    m_pwm_monitor_cfg[i].is_active = 0;
+    m_pwm_monitor_cfg[i].monitor_id = i;
+  end
+
+  // only support 1 outstanding TL items in tlul_adapter
+  m_tl_agent_cfg.max_outstanding_req = 1;
+endfunction
+
+function int pwm_env_cfg::get_clk_core_freq();
+  real scaled = clk_rst_vif.clk_freq_mhz * clk_scale / 1024;
+  `DV_CHECK_FATAL(clk_rst_vif.clk_freq_mhz > 0)
+  `DV_CHECK_FATAL(scaled > 0)
+
+  return int'(scaled + 0.5);
+endfunction
