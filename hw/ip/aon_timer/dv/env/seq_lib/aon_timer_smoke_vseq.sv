0579befeb205049ef6de301d8b84f24ad0367468
Antonio Martinez Zambrana <amz@lowrisc.org>
2024-10-30 17:14:02 +0000

    [aon_timer, dv] Add extern keyword to methods/constraints
    Moving TB towards declaring functions and constraints as extern to
    enhance readability
    
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/ip/aon_timer/dv/env/seq_lib/aon_timer_smoke_vseq.sv
+++ hw/ip/aon_timer/dv/env/seq_lib/aon_timer_smoke_vseq.sv
@@ -5,27 +5,31 @@
 // smoke test vseq
 class aon_timer_smoke_vseq extends aon_timer_base_vseq;
   `uvm_object_utils(aon_timer_smoke_vseq)
-  `uvm_object_new
 
-  task body();
-
-    aon_timer_init();
-    smoke_configure();
-    wait_for_interrupt();
-    aon_timer_shutdown();
-
-  endtask : body
-
-  task smoke_configure();
-
-    `uvm_info(`gfn, "Enabling AON Timer. Writing 1 to WKUP_CTRL and WDOG_CTRL", UVM_HIGH)
-    csr_utils_pkg::csr_wr(ral.wkup_ctrl.enable, 1'b1);
-    wdog_ctrl_pause_in_sleep = $urandom_range(0, 1);
-    ral.wdog_ctrl.enable.set(1);
-    ral.wdog_ctrl.pause_in_sleep.set(wdog_ctrl_pause_in_sleep);
-    csr_update(ral.wdog_ctrl);
-    `uvm_info(`gfn, "\n\t Waiting for AON Timer to finish (interrupt)", UVM_HIGH)
-  endtask : smoke_configure
-
+  extern function new (string name="");
+  extern task body();
+  extern task smoke_configure();
 
 endclass : aon_timer_smoke_vseq
+
+function aon_timer_smoke_vseq::new (string name="");
+  super.new(name);
+endfunction : new
+
+task aon_timer_smoke_vseq::body();
+  aon_timer_init();
+  smoke_configure();
+  wait_for_interrupt();
+  aon_timer_shutdown();
+endtask : body
+
+task aon_timer_smoke_vseq::smoke_configure();
+
+  `uvm_info(`gfn, "Enabling AON Timer. Writing 1 to WKUP_CTRL and WDOG_CTRL", UVM_HIGH)
+  csr_utils_pkg::csr_wr(ral.wkup_ctrl.enable, 1'b1);
+  wdog_ctrl_pause_in_sleep = $urandom_range(0, 1);
+  ral.wdog_ctrl.enable.set(1);
+  ral.wdog_ctrl.pause_in_sleep.set(wdog_ctrl_pause_in_sleep);
+  csr_update(ral.wdog_ctrl);
+  `uvm_info(`gfn, "\n\t Waiting for AON Timer to finish (interrupt)", UVM_HIGH)
+endtask : smoke_configure
--- hw/ip/aon_timer/dv/env/seq_lib/aon_timer_stress_all_vseq.sv
+++ hw/ip/aon_timer/dv/env/seq_lib/aon_timer_stress_all_vseq.sv
@@ -5,41 +5,48 @@
 class aon_timer_stress_all_vseq extends aon_timer_base_vseq;
   `uvm_object_utils(aon_timer_stress_all_vseq)
 
-  `uvm_object_new
+  extern constraint num_trans_c;
 
-  constraint num_trans_c {
-    num_trans inside {[15:20]};
-  }
+  extern function new (string name="");
+  extern task body();
 
-  task body();
-    string seq_names[] = {"aon_timer_smoke_vseq",
-                          "aon_timer_prescaler_vseq",
-                          "aon_timer_jump_vseq",
-                          "aon_timer_common_vseq"};
+endclass : aon_timer_stress_all_vseq
 
-    for (int i = 1; i <= num_trans; i++) begin
-      uvm_sequence        seq;
-      aon_timer_base_vseq aon_timer_vseq;
-      uint                seq_idx = $urandom_range(0, seq_names.size - 1);
+constraint aon_timer_stress_all_vseq::num_trans_c {
+  num_trans inside {[15:20]};
+}
 
-      seq = create_seq_by_name(seq_names[seq_idx]);
-      `downcast(aon_timer_vseq, seq)
+function aon_timer_stress_all_vseq::new (string name="");
+  super.new(name);
+endfunction : new
 
-      // if upper seq disables do_apply_reset for this seq, then can't issue reset
-      // as upper seq may drive reset
-      if (do_apply_reset) aon_timer_vseq.do_apply_reset = $urandom_range(0, 1);
-      else                aon_timer_vseq.do_apply_reset = 0;
+task aon_timer_stress_all_vseq::body();
+  string seq_names[] = {"aon_timer_smoke_vseq",
+                        "aon_timer_prescaler_vseq",
+                        "aon_timer_jump_vseq",
+                        "aon_timer_common_vseq"};
 
-      aon_timer_vseq.set_sequencer(p_sequencer);
-      `DV_CHECK_RANDOMIZE_FATAL(aon_timer_vseq)
-      if (seq_names[seq_idx] == "aon_timer_common_vseq") begin
-        aon_timer_common_vseq common_vseq;
-        `downcast(common_vseq, aon_timer_vseq);
-        common_vseq.common_seq_type = "intr_test";
-      end
+  for (int i = 1; i <= num_trans; i++) begin
+    uvm_sequence        seq;
+    aon_timer_base_vseq aon_timer_vseq;
+    uint                seq_idx = $urandom_range(0, seq_names.size - 1);
 
-      aon_timer_vseq.start(p_sequencer);
+    seq = create_seq_by_name(seq_names[seq_idx]);
+    `downcast(aon_timer_vseq, seq)
+
+    // if upper seq disables do_apply_reset for this seq, then can't issue reset
+    // as upper seq may drive reset
+    if (do_apply_reset) aon_timer_vseq.do_apply_reset = $urandom_range(0, 1);
+    else                aon_timer_vseq.do_apply_reset = 0;
+
+    aon_timer_vseq.set_sequencer(p_sequencer);
+    `DV_CHECK_RANDOMIZE_FATAL(aon_timer_vseq)
+    if (seq_names[seq_idx] == "aon_timer_common_vseq") begin
+      aon_timer_common_vseq common_vseq;
+      `downcast(common_vseq, aon_timer_vseq);
+      common_vseq.common_seq_type = "intr_test";
     end
-  endtask : body
 
-endclass
+    aon_timer_vseq.start(p_sequencer);
+  end
+endtask : body
