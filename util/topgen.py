d54ec66bb7ea93bb57544de913fe1fcc1f7758f8
Guillermo Maturana <maturana@opentitan.org>
2024-11-01 18:24:23 +0000

    [ipgen,pinmux] Change topgen to ipgen pinmux and remove old files
    This deletes all hw/ip/pinmux and hw/top_earlgrey/ip/pinmux files
    since they are no longer used.
    
    Part of pinmux #8440
    
    Signed-off-by: Guillermo Maturana <maturana@opentitan.org>

--- util/topgen.py
+++ util/topgen.py
@@ -11,7 +11,6 @@ import sys
 import tempfile
 from collections import OrderedDict
 from copy import deepcopy
-from io import StringIO
 from itertools import chain
 from pathlib import Path
 from typing import Dict, List
@@ -255,119 +254,6 @@ def generate_regfile_from_path(hjson_path: Path,
 
 
 def generate_pinmux(top: Dict[str, object], out_path: Path) -> None:
-
-    topname = top["name"]
-    pinmux = top["pinmux"]
-
-    # Generation without pinmux and pinout configuration is not supported.
-    assert "pinmux" in top
-    assert "pinout" in top
-
-    # Get number of wakeup detectors
-    if "num_wkup_detect" in pinmux:
-        num_wkup_detect = pinmux["num_wkup_detect"]
-    else:
-        num_wkup_detect = 1
-
-    if num_wkup_detect <= 0:
-        # TODO: add support for no wakeup counter case
-        log.error("Topgen does currently not support generation of a top " +
-                  "without DIOs.")
-        return
-
-    if "wkup_cnt_width" in pinmux:
-        wkup_cnt_width = pinmux["wkup_cnt_width"]
-    else:
-        wkup_cnt_width = 8
-
-    if wkup_cnt_width <= 1:
-        log.error("Wakeup counter width must be greater equal 2.")
-        return
-
-    # MIO Pads
-    n_mio_pads = pinmux["io_counts"]["muxed"]["pads"]
-
-    # Total inputs/outputs
-    # Reuse the counts from the merge phase
-    n_mio_periph_in = (pinmux["io_counts"]["muxed"]["inouts"] +
-                       pinmux["io_counts"]["muxed"]["inputs"])
-    n_mio_periph_out = (pinmux["io_counts"]["muxed"]["inouts"] +
-                        pinmux["io_counts"]["muxed"]["outputs"])
-    n_dio_periph_in = (pinmux["io_counts"]["dedicated"]["inouts"] +
-                       pinmux["io_counts"]["dedicated"]["inputs"])
-    n_dio_periph_out = (pinmux["io_counts"]["dedicated"]["inouts"] +
-                        pinmux["io_counts"]["dedicated"]["outputs"])
-    n_dio_pads = (pinmux["io_counts"]["dedicated"]["inouts"] +
-                  pinmux["io_counts"]["dedicated"]["inputs"] +
-                  pinmux["io_counts"]["dedicated"]["outputs"])
-
-    # Generation with zero MIO/DIO pads is currently not supported.
-    assert (n_mio_pads > 0)
-    assert (n_dio_pads > 0)
-
-    log.info("Generating pinmux with following info from hjson:")
-    log.info("num_wkup_detect: %d" % num_wkup_detect)
-    log.info("wkup_cnt_width:  %d" % wkup_cnt_width)
-    log.info("n_mio_periph_in:  %d" % n_mio_periph_in)
-    log.info("n_mio_periph_out: %d" % n_mio_periph_out)
-    log.info("n_dio_periph_in:  %d" % n_dio_periph_in)
-    log.info("n_dio_periph_out: %d" % n_dio_periph_out)
-    log.info("n_dio_pads:       %d" % n_dio_pads)
-
-    # Target path
-    #   rtl: pinmux_reg_pkg.sv & pinmux_reg_top.sv
-    #   data: pinmux.hjson
-    spec_ip_path = out_path / "ip" / "pinmux"
-    rtl_path = spec_ip_path / "rtl" / "autogen"
-    rtl_path.mkdir(parents=True, exist_ok=True)
-    data_path = spec_ip_path / "data" / "autogen"
-    data_path.mkdir(parents=True, exist_ok=True)
-
-    # Template path
-    orig_ip_path = SRCTREE_TOP / "hw" / "ip" / "pinmux"
-    tpl_path = orig_ip_path / "data" / "pinmux.hjson.tpl"
-    original_rtl_path = orig_ip_path / "rtl"
-
-    # Generate register package and RTLs
-    top_name = f"top_{topname}"
-    gencmd = (
-        f"// util/topgen.py -t hw/{top_name}/data/{top_name}.hjson "
-        f"-o hw/{top_name}/\n\n")
-
-    hjson_gen_path = data_path / "pinmux.hjson"
-
-    out = StringIO()
-    with tpl_path.open(mode="r", encoding="UTF-8") as fin:
-        hjson_tpl = Template(fin.read())
-        try:
-            out = hjson_tpl.render(
-                n_mio_periph_in=n_mio_periph_in,
-                n_mio_periph_out=n_mio_periph_out,
-                n_mio_pads=n_mio_pads,
-                # each DIO has in, out and oe wires
-                # some of these have to be tied off in the
-                # top, depending on the type.
-                n_dio_periph_in=n_dio_pads,
-                n_dio_periph_out=n_dio_pads,
-                n_dio_pads=n_dio_pads,
-                n_wkup_detect=num_wkup_detect,
-                wkup_cnt_width=wkup_cnt_width)
-        except:  # noqa: E722
-            log.error(exceptions.text_error_template().render())
-        log.info("PINMUX HJSON: %s" % out)
-
-    if out == "":
-        log.error("Cannot generate pinmux HJSON")
-        return
-
-    with hjson_gen_path.open(mode="w", encoding="UTF-8") as fout:
-        fout.write(genhdr + gencmd + out)
-
-    # Generate reg file
-    generate_regfile_from_path(hjson_gen_path, rtl_path, original_rtl_path)
-
-
-def generate_ipgen_pinmux(top: Dict[str, object], out_path: Path) -> None:
     log.info("Generating pinmux with ipgen")
     topname = top["name"]
 
@@ -936,7 +822,6 @@ def _process_top(
 
     # Generate Pinmux
     generate_pinmux(completecfg, out_path)
-    generate_ipgen_pinmux(completecfg, out_path)
 
     # Generate Pwrmgr
     generate_pwrmgr(completecfg, out_path)
