0e1898fc556d6768630a3f744a73f42378054970
Tim Trippel <ttrippel@google.com>
2024-11-01 22:07:25 -0700

    [manuf] write certs to flash in perso LTV object format
    Currently, during ft_perso, the host writes the raw endorsed X.509
    certificates (in ASN.1 DER format) to flash_info pages.
    When it needs to access them later on, the certificate length is
    calculated by parsing the ASN.1 header (1 or 2 bytes).
    This design works for X509 but not CWT-CBOR since there's no such
    information in some CBOR types (map, array specifically).
    
    Fixes #24942.
    
    Co-authored-by: Tommy Chiu <tommychiu@google.com>
    Signed-off-by: Tim Trippel <ttrippel@google.com>
    (cherry picked from commit fc628e6cdaea7d4f63e94c2d7a9e29f19d3be5a5)

--- sw/device/silicon_creator/manuf/base/perso_tlv_data.c
+++ sw/device/silicon_creator/manuf/base/perso_tlv_data.c
@@ -7,10 +7,6 @@
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/silicon_creator/lib/cert/cert.h"
 
-/**
- * A helper function adding arbitrary amount of data to the body of the perso
- * blob, returns error if the passed in data would not fit.
- */
 status_t perso_tlv_push_to_blob(const void *data, size_t size,
                                 perso_blob_t *perso_blob) {
   size_t room = sizeof(perso_blob->body) - perso_blob->next_free;
@@ -23,68 +19,68 @@ status_t perso_tlv_push_to_blob(const void *data, size_t size,
   return OK_STATUS();
 }
 
-status_t perso_tlv_set_cert_block(const uint8_t *buf, size_t max_room,
-                                  perso_tlv_cert_block_t *block) {
+status_t perso_tlv_get_cert_obj(const uint8_t *buf, size_t ltv_buf_size,
+                                perso_tlv_cert_obj_t *obj) {
   perso_tlv_object_header_t objh;
+  perso_tlv_object_type_t obj_type;
   uint16_t obj_size;
-  perso_tlv_object_header_t obj_type;
 
-  memcpy(&objh, buf, sizeof(objh));
+  // Extract LTV object header, including: size and type.
+  obj->obj_p = buf;
+  memcpy(&objh, buf, sizeof(perso_tlv_object_header_t));
+  // Extract LTV object size.
   PERSO_TLV_GET_FIELD(Objh, Size, objh, &obj_size);
-
-  if (obj_size > max_room)
+  if (obj_size > ltv_buf_size)
     return INTERNAL();  // Something is really screwed up.
-
-  buf += sizeof(objh);
-  max_room -= sizeof(objh);
-  block->obj_size = obj_size;
+  obj->obj_size = obj_size;
+  // Extract LTV object type.
   PERSO_TLV_GET_FIELD(Objh, Type, objh, &obj_type);
-
   if (obj_type != kPersoObjectTypeX509Cert) {
     LOG_INFO("Skipping object of type %d", obj_type);
     return NOT_FOUND();
   }
+  buf += sizeof(perso_tlv_object_header_t);
+  ltv_buf_size -= sizeof(perso_tlv_object_header_t);
+
+  // If we made it this far, we found a certificate LTV object, so we will parse
+  // the object's header and metadata next.
 
   perso_tlv_cert_header_t crth;
   uint16_t wrapped_cert_size;
   uint16_t name_len;
 
-  // Let's retrieve cert wrapper header.
-  block->wrapped_cert_p = buf;
-  memcpy(&crth, buf, sizeof(crth));
-  max_room -= sizeof(crth);
-  buf += sizeof(crth);
-  PERSO_TLV_GET_FIELD(Crth, Size, crth, &wrapped_cert_size);
+  // Extract the certificate object header, including: certificate object and
+  // nameksizes, certificate name string, and pointer to the certificate body.
+  memcpy(&crth, buf, sizeof(perso_tlv_cert_header_t));
+  // Extract certificate name size.
   PERSO_TLV_GET_FIELD(Crth, NameSize, crth, &name_len);
-
-  const size_t min_header_size = sizeof(objh) + sizeof(crth);
-
-  // At least 8 bytes in the certificate body to get its size.
-  if ((wrapped_cert_size < (name_len + min_header_size + 8)) ||
-      (wrapped_cert_size > max_room))
+  // Extract wrapped certificate object size.
+  PERSO_TLV_GET_FIELD(Crth, Size, crth, &wrapped_cert_size);
+  // There are at least 4 bytes in an X.509 ASN.1 DER certificate: two bytes of
+  // header and two bytes of size data.
+  if ((wrapped_cert_size < (sizeof(perso_tlv_cert_header_t) + name_len + 4)) ||
+      (wrapped_cert_size > ltv_buf_size))
     return INTERNAL();  // Something is really screwed up.
-
-  memcpy(block->name, buf, name_len);
+  buf += sizeof(perso_tlv_cert_header_t);
+  ltv_buf_size -= sizeof(perso_tlv_cert_header_t);
+  // Extract certificate name string.
+  memcpy(obj->name, buf, name_len);
+  obj->name[name_len] = '\0';
   buf += name_len;
-  max_room -= name_len;
-  block->name[name_len] = '\0';
-
-  size_t cert_size;
-
-  cert_size = cert_x509_asn1_decode_size_header(buf);
-  if (cert_size > max_room)
-    return INTERNAL();
-
-  uint16_t wire_cert_size =
+  ltv_buf_size -= name_len;
+  // Set pointer to certificate body.
+  obj->cert_body_size =
       wrapped_cert_size - sizeof(perso_tlv_cert_header_t) - name_len;
+  obj->cert_body_p = buf;
 
-  if (cert_size != wire_cert_size) {
-    LOG_ERROR("Unexpected cert size %d instead of %d for cert %s", cert_size,
-              wire_cert_size, block->name);
+  // Sanity check on the certificate body size.
+  size_t decoded_cert_size =
+      cert_x509_asn1_decode_size_header(obj->cert_body_p);
+  if (decoded_cert_size != obj->cert_body_size) {
+    LOG_ERROR("Unexpected cert size %d instead of %d for cert %s",
+              decoded_cert_size, obj->cert_body_size, obj->name);
     return INTERNAL();
   }
-
-  block->wrapped_cert_size = wrapped_cert_size;
 
   return OK_STATUS();
 }
@@ -94,58 +90,41 @@ status_t perso_tlv_prepare_cert_for_shipping(const char *name,
                                              const void *cert_body,
                                              size_t cert_size,
                                              perso_blob_t *pb) {
-  /**
-   * * The certificate is laid out in the perso blob buffer as follows:
-   * - 16 bit object header
-   * - 16 bits cert wrapper header
-   * - Certificate name string
-   * - Cerificate data itself
-   *
-   * Note that both certificate and object headers'
-   * are 16 bit integers in big endian format.
-   *
-   *  d15                                         d0
-   * +-------------+--------------------------------+
-   * | 4 bit type  |   12 bits total object size    | <-- Object Header
-   * +-------------+--------------------------------+
-   * | name length |12 bits total cert payload size | <-- Cert Header
-   * +-------------+--------------------------------+
-   * |             cert name string                 |
-   * +----------------------------------------------+
-   * |                   cert                       |
-   * +----------------------------------------------+
-   */
   perso_tlv_object_header_t obj_header = 0;
   perso_tlv_cert_header_t cert_header = 0;
   size_t name_len;
   size_t obj_len;
   size_t wrapped_len;
 
-  // strlen() is not available.
+  // Compute the name length (strlen() is not available).
   name_len = 0;
   while (name[name_len])
     name_len++;
-
   if (name_len > kCrthNameSizeFieldMask)
     return OUT_OF_RANGE();
 
+  // Compute the wrapped certificate object (cert header + cert data) and perso
+  // LTV object sizes.
   wrapped_len = sizeof(perso_tlv_cert_header_t) + name_len + cert_size;
   obj_len = wrapped_len + sizeof(perso_tlv_object_header_t);
 
+  // Check there is enough room in the buffer to store the perso LTV object.
   if (obj_len > (sizeof(pb->body) - pb->next_free))
     return OUT_OF_RANGE();
 
+  // Setup the perso LTV object header.
   if (needs_endorsement) {
     PERSO_TLV_SET_FIELD(Objh, Type, obj_header, kPersoObjectTypeX509Tbs);
   } else {
     PERSO_TLV_SET_FIELD(Objh, Type, obj_header, kPersoObjectTypeX509Cert);
   }
-
   PERSO_TLV_SET_FIELD(Objh, Size, obj_header, obj_len);
 
+  // Setup the cert object header.
   PERSO_TLV_SET_FIELD(Crth, Size, cert_header, wrapped_len);
   PERSO_TLV_SET_FIELD(Crth, NameSize, cert_header, name_len);
 
+  // Push the entire cert perso LTV object to the buffer.
   TRY(perso_tlv_push_to_blob(&obj_header, sizeof(obj_header), pb));
   TRY(perso_tlv_push_to_blob(&cert_header, sizeof(cert_header), pb));
   TRY(perso_tlv_push_to_blob(name, name_len, pb));
