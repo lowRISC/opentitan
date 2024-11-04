fde3810b5b362ff461fce7d17c94b7529afcca37
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-11-04 14:25:29 +0000

    [pwm,dv] Use out-of-block declarations in pwm_common_vseq
    Also, delete a comment. I don't believe it was true even for the
    initial check-in of the code in 2021.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/pwm/dv/env/seq_lib/pwm_common_vseq.sv
+++ hw/ip/pwm/dv/env/seq_lib/pwm_common_vseq.sv
@@ -4,17 +4,21 @@
 
 class pwm_common_vseq extends pwm_base_vseq;
   `uvm_object_utils(pwm_common_vseq)
-  `uvm_object_new
+  extern function new (string name="");
 
-  constraint num_trans_c { num_trans inside {[1:2]}; }
+  extern constraint num_trans_c;
 
-  // for this vseq, $value$plusargs "+en_scb=0" is defined in pwm_sim_cfg.hjson
-  // disable pwm_monitor and pwm_scoreboard since they can not handle this test
-
-  virtual task body();
-    `uvm_info(`gfn, "\n--> start of pwm_common_vseq", UVM_DEBUG)
-    run_common_vseq_wrapper(num_trans);
-    `uvm_info(`gfn, "\n--> end of pwm_common_vseq", UVM_DEBUG)
-  endtask : body
-
+  extern task body();
 endclass : pwm_common_vseq
+
+function pwm_common_vseq::new (string name = "");
+  super.new(name);
+endfunction
+
+constraint pwm_common_vseq::num_trans_c {
+  num_trans inside {[1:2]};
+}
+
+task pwm_common_vseq::body();
+  run_common_vseq_wrapper(num_trans);
+endtask
