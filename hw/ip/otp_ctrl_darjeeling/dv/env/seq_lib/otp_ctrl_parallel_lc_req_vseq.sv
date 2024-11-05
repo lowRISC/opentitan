577d906d3049e1bebc2804e2ecb99a2c617c90ff
Robert Schilling <rschilling@rivosinc.com>
2024-11-05 16:30:48 +0100

    [hw,otp_ctrl_darjeeling] Hard fork of OTP Ctrl from integrated_dev
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

---
+++ hw/ip/otp_ctrl_darjeeling/dv/env/seq_lib/otp_ctrl_parallel_lc_req_vseq.sv
@@ -0,0 +1,40 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// Otp_ctrl_parallel_lc_vseq is developed to generate rand lc_otp req, which runs parallel with
+// normal DAI access sequences.
+class otp_ctrl_parallel_lc_req_vseq extends otp_ctrl_parallel_base_vseq;
+  `uvm_object_utils(otp_ctrl_parallel_lc_req_vseq)
+
+  `uvm_object_new
+
+  // Disable the default LC program request from otp_ctrl_smoke_vseq.
+  constraint lc_trans_c {
+    do_lc_trans == 0;
+  }
+
+  virtual task run_parallel_seq(ref bit base_vseq_done);
+    forever begin
+      if (base_vseq_done) return;
+
+      fork begin
+        if ($urandom_range(0, 1)) begin
+          wait_clk_or_reset($urandom_range(0, 500));
+          if (!base_vseq_done && !cfg.under_reset) begin
+            // Because LC program request is issued in parallel with DAI access, we disable
+            // interrupt check in this task.
+            // We only check interrupts when DAI access and LC program request are done.
+            req_lc_transition(0, lc_prog_blocking);
+          end
+        end
+      end join
+    end
+  endtask
+
+  virtual task post_start();
+    expect_fatal_alerts = 1;
+    super.post_start();
+  endtask
+
+endclass
---
+++ hw/ip/otp_ctrl_darjeeling/dv/env/seq_lib/otp_ctrl_test_access_vseq.sv
@@ -0,0 +1,34 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class otp_ctrl_test_access_vseq extends otp_ctrl_dai_lock_vseq;
+  `uvm_object_utils(otp_ctrl_test_access_vseq)
+
+  `uvm_object_new
+
+  virtual task dut_init(string reset_kind = "HARD");
+    super.dut_init(reset_kind);
+
+    // Drive dft_en pins to access the test_access memory.
+    cfg.otp_ctrl_vif.drive_lc_dft_en(lc_ctrl_pkg::On);
+
+    // Once turn on lc_dft_en regiser, will need some time to update the state register
+    // two clock cycles for lc_async mode, one clock cycle for driving dft_en.
+    if (cfg.en_dv_cdc) cfg.clk_rst_vif.wait_clks(4);
+    else               cfg.clk_rst_vif.wait_clks(3);
+  endtask
+
+  virtual task rand_drive_dft_en();
+    super.rand_drive_dft_en();
+    // 25% chance drive lc_dft_en to a random value.
+    if ($urandom_range(0, 3) == 3) begin
+      cfg.otp_ctrl_vif.drive_lc_dft_en(get_rand_lc_tx_val(
+                                       .t_weight(1), .f_weight(1), .other_weight(1)));
+      // Once turn on lc_dft_en regiser, will need some time to update the state register
+      // two clock cycles for lc_async mode, one clock cycle for driving dft_en.
+      if (cfg.en_dv_cdc) cfg.clk_rst_vif.wait_clks(4);
+      else               cfg.clk_rst_vif.wait_clks(3);
+    end
+  endtask
+endclass
