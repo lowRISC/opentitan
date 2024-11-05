094a0effe77d435ed5bbbcdb6523430f11ae45ef
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-11-05 09:45:01 +0000

    [pwm,dv] Use out-of-block declarations in pwm_rand_output_vseq
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/pwm/dv/env/seq_lib/pwm_rand_output_vseq.sv
+++ hw/ip/pwm/dv/env/seq_lib/pwm_rand_output_vseq.sv
@@ -5,57 +5,67 @@
 // This sequence randomizes configurations at the output.
 class pwm_rand_output_vseq extends pwm_base_vseq;
   `uvm_object_utils(pwm_rand_output_vseq)
-  `uvm_object_new
 
-  // variables
+  // A parameter used for pwm_param for each channel
   rand param_reg_t rand_reg_param;
+
+  // Enable and invert bits for each channel
   rand bit [PWM_NUM_CHANNELS-1:0] rand_chan;
   rand bit [PWM_NUM_CHANNELS-1:0] rand_invert;
+
+  // If true, this stops the clock in "low power" mode
   rand bit low_power;
 
-  // constraints
-  constraint rand_reg_param_c {
-   rand_reg_param.HtbtEn == 1'b1 -> rand_reg_param.BlinkEn == 1'b1;
-   rand_reg_param.RsvParam == 0;
-   rand_reg_param.PhaseDelay inside {[0:MAX_16]};
-  }
+  // Make sure to enable blink if the heartbeat is enabled
+  extern constraint htbt_implies_blink_c;
 
-  constraint low_power_c {
-    // 1 in 10, in low power mode
-    low_power dist {1'b1:/1, 1'b0:/9};
-  }
+  // Model low power mode 10% of the time.
+  extern constraint low_power_c;
 
-  virtual task body();
+  extern function new (string name="");
+  extern virtual task body();
+endclass
 
-    set_ch_enables(32'h0);
+constraint pwm_rand_output_vseq::htbt_implies_blink_c {
+  rand_reg_param.HtbtEn == 1'b1 -> rand_reg_param.BlinkEn == 1'b1;
+  rand_reg_param.RsvParam == 0;
+  rand_reg_param.PhaseDelay inside {[0:MAX_16]};
+}
 
-    rand_pwm_cfg_reg();
+constraint pwm_rand_output_vseq::low_power_c {
+  low_power dist {1'b1:/1, 1'b0:/9};
+}
 
-    // set random dc and params for all channels
-    for (uint i = 0; i < PWM_NUM_CHANNELS; i++) begin
-      param_reg_t pwm_param;
-      dc_blink_t blink, duty_cycle;
+function pwm_rand_output_vseq::new (string name = "");
+  super.new(name);
+endfunction
 
-      duty_cycle = rand_pwm_duty_cycle();
-      blink = rand_pwm_blink(duty_cycle);
+task pwm_rand_output_vseq::body();
+  set_ch_enables(32'h0);
+  rand_pwm_cfg_reg();
 
-      // phase delay of the PWM rising edge, in units of 2^(-16) PWM cycles
-      pwm_param.PhaseDelay = (rand_reg_param.PhaseDelay * (2**(-16)));
-      pwm_param.HtbtEn = rand_reg_param.HtbtEn;
-      pwm_param.BlinkEn = rand_reg_param.BlinkEn;
+  // Set random dc and params for all channels
+  for (uint i = 0; i < PWM_NUM_CHANNELS; i++) begin
+    param_reg_t pwm_param;
+    dc_blink_t blink, duty_cycle;
 
-      set_duty_cycle(i, .A(duty_cycle.A), .B(duty_cycle.B));
-      set_blink(i, .A(blink.A), .B(blink.B));
-      set_param(i, pwm_param);
-    end
+    duty_cycle = rand_pwm_duty_cycle();
+    blink = rand_pwm_blink(duty_cycle);
 
-    set_ch_invert(rand_invert);
-    set_ch_enables(rand_chan);
+    // phase delay of the PWM rising edge, in units of 2^(-16) PWM cycles
+    pwm_param.PhaseDelay = (rand_reg_param.PhaseDelay * (2**(-16)));
+    pwm_param.HtbtEn = rand_reg_param.HtbtEn;
+    pwm_param.BlinkEn = rand_reg_param.BlinkEn;
 
-    low_power_mode(low_power, NUM_CYCLES);
+    set_duty_cycle(i, .A(duty_cycle.A), .B(duty_cycle.B));
+    set_blink(i, .A(blink.A), .B(blink.B));
+    set_param(i, pwm_param);
+  end
 
-    shutdown_dut();
+  set_ch_invert(rand_invert);
+  set_ch_enables(rand_chan);
 
-  endtask : body
+  low_power_mode(low_power, NUM_CYCLES);
 
-endclass : pwm_rand_output_vseq
+  shutdown_dut();
+endtask
