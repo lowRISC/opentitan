aebd61172e5147200000b558571cf323fb38ca2a
Tommy Chiu <tommychiu@google.com>
2024-11-06 11:35:24 +0800

    [dice] Move dice key related seeds to a separate library
    Both X509 and CWT DICE implementations need these definitions. Move it
    out from the original source file for code reuse.
    
    Signed-off-by: Tommy Chiu <tommychiu@google.com>

--- sw/device/silicon_creator/lib/cert/dice_cwt.c
+++ sw/device/silicon_creator/lib/cert/dice_cwt.c
@@ -8,8 +8,8 @@
 #include "sw/device/silicon_creator/lib/cert/cbor.h"
 #include "sw/device/silicon_creator/lib/cert/cert.h"
 #include "sw/device/silicon_creator/lib/cert/dice.h"
+#include "sw/device/silicon_creator/lib/cert/dice_keys.h"
 #include "sw/device/silicon_creator/lib/drivers/hmac.h"
-#include "sw/device/silicon_creator/lib/drivers/keymgr.h"
 #include "sw/device/silicon_creator/lib/drivers/otp.h"
 #include "sw/device/silicon_creator/lib/error.h"
 #include "sw/device/silicon_creator/lib/sigverify/ecdsa_p256_key.h"
@@ -28,73 +28,6 @@ const int64_t kCoseEc2YLabel = -3;
 const int64_t kCoseKeyAlgEcdsa256 = -7;
 const int64_t kCoseEc2CrvP256 = 1;
 const int64_t kCoseKeyKtyEc2 = 2;
-
-// UDS (Creator) attestation key diverisfier constants.
-// Note: versions are always set to 0 so these keys are always valid from the
-// perspective of the keymgr hardware.
-const sc_keymgr_diversification_t kUdsKeymgrDiversifier = {
-    .salt =
-        {
-            0xabffa6a9,
-            0xc781f1ad,
-            0x4c1107ad,
-            0xf9210d85,
-            0x0931f555,
-            0x6c5aef5d,
-            0xb9ba4df0,
-            0x77b248d2,
-        },
-    .version = 0,
-};
-// CDI_0 (OwnerIntermediate) attestation key diverisfier constants.
-const sc_keymgr_diversification_t kCdi0KeymgrDiversifier = {
-    .salt =
-        {
-            0x3e5913c7,
-            0x41156f1d,
-            0x998ddb9f,
-            0xfa334191,
-            0x8a85380e,
-            0xba76ca1a,
-            0xdb17c4a7,
-            0xfb8852dc,
-        },
-    .version = 0,
-};
-// CDI_1 (Owner) attestation key diverisfier constants.
-const sc_keymgr_diversification_t kCdi1KeymgrDiversifier = {
-    .salt =
-        {
-            0x2d12c2e3,
-            0x6acc6876,
-            0x4bfb07ee,
-            0xc45fc414,
-            0x5d4fa9de,
-            0xf295b128,
-            0x50f49882,
-            0xbbdefa29,
-        },
-    .version = 0,
-};
-
-const sc_keymgr_ecc_key_t kDiceKeyUds = {
-    .type = kScKeymgrKeyTypeAttestation,
-    .keygen_seed_idx = kFlashInfoFieldUdsKeySeedIdx,
-    .keymgr_diversifier = &kUdsKeymgrDiversifier,
-    .required_keymgr_state = kScKeymgrStateCreatorRootKey,
-};
-const sc_keymgr_ecc_key_t kDiceKeyCdi0 = {
-    .type = kScKeymgrKeyTypeAttestation,
-    .keygen_seed_idx = kFlashInfoFieldCdi0KeySeedIdx,
-    .keymgr_diversifier = &kCdi0KeymgrDiversifier,
-    .required_keymgr_state = kScKeymgrStateOwnerIntermediateKey,
-};
-const sc_keymgr_ecc_key_t kDiceKeyCdi1 = {
-    .type = kScKeymgrKeyTypeAttestation,
-    .keygen_seed_idx = kFlashInfoFieldCdi1KeySeedIdx,
-    .keymgr_diversifier = &kCdi1KeymgrDiversifier,
-    .required_keymgr_state = kScKeymgrStateOwnerKey,
-};
 
 // PubKeyECDSA256 = {               ; COSE_Key [RFC9052 s7]
 //     1 : 2,                       ; Key type : EC2
