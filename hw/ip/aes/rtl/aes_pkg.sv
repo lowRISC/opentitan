86b57b5c3e8c5b21858aebdbe1d91b46dc246980
Pirmin Vogel <vogelpi@lowrisc.org>
2024-11-07 22:08:55 +0100

    [aes/rtl] Implement masked version of the GHASH block
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

--- hw/ip/aes/rtl/aes_pkg.sv
+++ hw/ip/aes/rtl/aes_pkg.sv
@@ -258,7 +258,7 @@ typedef struct packed {
   } aes_ctrl_e;
 
 // Encoding generated with:
-// $ ./util/design/sparse-fsm-encode.py -d 3 -m 5 -n 6 \
+// $ ./util/design/sparse-fsm-encode.py -d 3 -m 8 -n 6 \
 //     -s 31468618 --language=sv
 //
 // Hamming distance histogram:
@@ -266,23 +266,26 @@ typedef struct packed {
 //  0: --
 //  1: --
 //  2: --
-//  3: |||||||||||||||||||| (50.00%)
-//  4: |||||||||||||||| (40.00%)
-//  5: |||| (10.00%)
+//  3: |||||||||||||||||||| (57.14%)
+//  4: ||||||||||||||| (42.86%)
+//  5: --
 //  6: --
 //
 // Minimum Hamming distance: 3
-// Maximum Hamming distance: 5
+// Maximum Hamming distance: 4
 // Minimum Hamming weight: 1
 // Maximum Hamming weight: 5
 //
 localparam int GhashStateWidth = 6;
 typedef enum logic [GhashStateWidth-1:0] {
-  GHASH_IDLE  = 6'b110000,
-  GHASH_MULT  = 6'b001000,
-  GHASH_TAG   = 6'b000011,
-  GHASH_OUT   = 6'b011101,
-  GHASH_ERROR = 6'b111110
+  GHASH_IDLE                    = 6'b101001,
+  GHASH_MULT                    = 6'b010000,
+  GHASH_ADD_S                   = 6'b111110,
+  GHASH_OUT                     = 6'b011101,
+  GHASH_ERROR                   = 6'b110011,
+  GHASH_MASKED_INIT             = 6'b001010,
+  GHASH_MASKED_ADD_STATE_SHARES = 6'b000111,
+  GHASH_MASKED_ADD_CORR         = 6'b100100
 } aes_ghash_e;
 
 // Generic, sparse mux selector encodings
@@ -517,6 +520,14 @@ typedef enum logic [GHashInSelWidth-1:0] {
   GHASH_IN_S            = MUX3_SEL_2
 } ghash_in_sel_e;
 
+parameter int GHashAddInSelNum = 3;
+parameter int GHashAddInSelWidth = Mux3SelWidth;
+typedef enum logic [GHashAddInSelWidth-1:0] {
+  ADD_IN_GHASH_IN = MUX3_SEL_0,
+  ADD_IN_CORR_A   = MUX3_SEL_1,
+  ADD_IN_CORR_B   = MUX3_SEL_2
+} ghash_add_in_sel_e;
+
 parameter int GHashStateSelNum = 4;
 parameter int GHashStateSelWidth = Mux4SelWidth;
 typedef enum logic [GHashStateSelWidth-1:0] {
@@ -525,6 +536,14 @@ typedef enum logic [GHashStateSelWidth-1:0] {
   GHASH_STATE_ADD     = MUX4_SEL_2,
   GHASH_STATE_MULT    = MUX4_SEL_3
 } ghash_state_sel_e;
+
+parameter int GFMultS1SelNum = 3;
+parameter int GFMultS1SelWidth = Mux3SelWidth;
+typedef enum logic [GFMultS1SelWidth-1:0] {
+  MULT_IN_STATE0 = MUX3_SEL_0,
+  MULT_IN_STATE1 = MUX3_SEL_1,
+  MULT_IN_S1     = MUX3_SEL_2
+} gf_mult_in_sel_e;
 
 parameter int DataOutSelNum = 2;
 parameter int DataOutSelWidth = Mux2SelWidth;
