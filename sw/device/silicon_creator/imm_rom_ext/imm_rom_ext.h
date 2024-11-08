839348bf64a71b7e144643c83cd6745714f88602
Yi-Hsuan Deng <yhdeng@google.com>
2024-11-08 08:13:59 +0000

    [rom_ext] Jump to immutable section first at startup
    To minimize the divergence between SKUs that with and without immutable rom_ext
    feature enabled in ROM, we propose always linking the section to rom_ext, and
    call it first if it isn’t called by ROM.
    
    With this change, the boot flows will be always (almost) the same, while the
    OTP flag `IMMUTABLE_ROM_EXT_EN` basically only controls whether the
    immutability is enforced by ROM.
    
    SKUs without the immutability enforced enable should still link with the
    trunk/master imm_rom_ext section, but without the EN bit set in their OTP
    configuration.
    
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>
    (cherry picked from commit db977a570f24edd7b8a56d468b30330d7ff93945)

---
+++ sw/device/silicon_creator/imm_rom_ext/imm_rom_ext.h
@@ -0,0 +1,18 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_IMM_ROM_EXT_IMM_ROM_EXT_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_IMM_ROM_EXT_IMM_ROM_EXT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void imm_rom_ext_main(void);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_IMM_ROM_EXT_IMM_ROM_EXT_H_
