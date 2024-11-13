96fce7f78e43664dc2a1e83e40dbdd5447f737a4
Andrea Caforio <andrea.caforio@lowrisc.org>
2024-11-13 18:38:39 +0100

    [tlul,shim] Expand `pre_dv` to handle arbitrary test cases
    This commit generalizes the `pre_dv` testbench to arbitrary test cases
    and adds an array of tests covering both AES-GCM and its modes of
    operation. The process of adding/modify tests is now streamlined
    reducing the feedback loop.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>
    Co-authored-by: Pirmin Vogel <vogelpi@lowrisc.org>

---
+++ hw/ip/aes/pre_dv/aes_tlul_shim_tb/rtl/aes_tlul_shim_tb_c_dpi.sv
@@ -0,0 +1,165 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// This module serves as the interface to the `c_dpi` API by instantiating the function call as a
+// combinatorial block. The `c_dpi` output (data and tag) is flopped and the 32 least significant
+// bits of data and tag become visible at the module's output ports in the next clock cycle. The
+// flopped data and tag can be rotated by 32 positions to the right to make other bits visible.
+
+module aes_tlul_shim_tb_c_dpi
+  import aes_pkg::*;
+  import aes_tlul_shim_tb_pkg::*;
+  import aes_model_dpi_pkg::*;
+#(
+    parameter int ADLength = 0,
+    parameter int DataLength = 16
+) (
+   input logic clk_i,
+   input logic rst_ni,
+
+   input c_dpi_input_t c_dpi_input_i,
+
+   input logic c_dpi_load_i,         // Flop `c_dpi` output.
+   input logic c_dpi_rotate_data_i,  // Rotate data >> 32.
+   input logic c_dpi_rotate_tag_i,   // Rotate tag  >> 32.
+
+   output logic [31:0] c_dpi_data_o,
+   output logic [31:0] c_dpi_tag_o
+);
+
+  // Make the 32 least signification bits of data and tag visible at the output ports.
+
+  assign c_dpi_data_o = {c_dpi_data_q[3], c_dpi_data_q[2], c_dpi_data_q[1], c_dpi_data_q[0]};
+  assign c_dpi_tag_o  = c_dpi_tag_q[31:0];
+
+  // Flop the output data acquired from the `c_dpi` API in an unpacked array of at least 4 bytes.
+
+  logic [7:0] c_dpi_data   [max(4, DataLength)];
+  logic [7:0] c_dpi_data_d [max(4, DataLength)];
+  logic [7:0] c_dpi_data_q [max(4, DataLength)];
+
+  always_comb begin
+    c_dpi_data_d = c_dpi_data_q;
+    if (c_dpi_load_i) begin
+      c_dpi_data_d = c_dpi_data;
+    end else if (c_dpi_rotate_data_i) begin
+      for (int i = 0; i < DataLength; i++) begin
+        c_dpi_data_d[i] = c_dpi_data_q[(i + 4) % DataLength];
+      end
+    end
+  end
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      c_dpi_data_q <= '{default: '0};
+    end else begin
+      c_dpi_data_q <= c_dpi_data_d;
+    end
+  end
+
+  // Flop the output tag acquired from the `c_dpi` API in the encryption case. The `c_dpi` API does
+  // not output the tag in the decryption case, consequently it needs to be input by the testbench
+  // itself. Note that the byte ordering of the `c_dpi` and HW tag differ.
+
+  logic [127:0] c_dpi_tag;
+  logic [127:0] c_dpi_tag_d;
+  logic [127:0] c_dpi_tag_q;
+
+  always_comb begin
+    c_dpi_tag_d = c_dpi_tag_q;
+    if (c_dpi_load_i) begin
+      if (c_dpi_input_i.operation == AES_ENC) begin
+        c_dpi_tag_d = aes_transpose(c_dpi_tag);
+      end else begin
+        c_dpi_tag_d = c_dpi_input_i.tag;
+      end
+    end else if (c_dpi_rotate_tag_i) begin
+      c_dpi_tag_d = {c_dpi_tag_q[31:0], c_dpi_tag_q[127:32]};
+    end
+  end
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      c_dpi_tag_q <= '0;
+    end else begin
+      c_dpi_tag_q <= c_dpi_tag_d;
+    end
+  end
+
+  // As of version 4.210, the only way of transforming a packed array into an unpacked one (required
+  // by the `c_dpi`) in a Verilator simulation is by manually reassigning the signals.
+
+  logic [7:0] c_dpi_ad_unp   [max(1, ADLength)];
+  logic [7:0] c_dpi_data_unp [max(1, DataLength)];
+
+  always_comb begin
+    for (int i = 0; i < ADLength; i++) begin
+      c_dpi_ad_unp[i] = c_dpi_input_i.ad[i];
+    end
+    for (int i = 0; i < DataLength; i++) begin
+      c_dpi_data_unp[i] = c_dpi_input_i.data[i];
+    end
+  end
+
+  always_comb begin
+    if (ADLength == 0 && DataLength == 0) begin
+      c_dpi_aes_crypt_message(
+        1'b1,
+        c_dpi_input_i.operation[1],
+        c_dpi_input_i.mode,
+        c_dpi_input_i.iv,
+        c_dpi_input_i.key_length,
+        c_dpi_input_i.key,
+        '0,
+        '0,
+        c_dpi_input_i.tag,
+        c_dpi_data,
+        c_dpi_tag
+      );
+    end else if (ADLength == 0 && DataLength > 0) begin
+      c_dpi_aes_crypt_message(
+        1'b1,
+        c_dpi_input_i.operation[1],
+        c_dpi_input_i.mode,
+        c_dpi_input_i.iv,
+        c_dpi_input_i.key_length,
+        c_dpi_input_i.key,
+        c_dpi_data_unp,
+        '0,
+        c_dpi_input_i.tag,
+        c_dpi_data,
+        c_dpi_tag
+      );
+    end else if (ADLength > 0 && DataLength == 0) begin
+      c_dpi_aes_crypt_message(
+        1'b1,
+        c_dpi_input_i.operation[1],
+        c_dpi_input_i.mode,
+        c_dpi_input_i.iv,
+        c_dpi_input_i.key_length,
+        c_dpi_input_i.key,
+        '0,
+        c_dpi_ad_unp,
+        c_dpi_input_i.tag,
+        c_dpi_data,
+        c_dpi_tag
+      );
+    end else begin
+      c_dpi_aes_crypt_message(
+        1'b1,
+        c_dpi_input_i.operation[1],
+        c_dpi_input_i.mode,
+        c_dpi_input_i.iv,
+        c_dpi_input_i.key_length,
+        c_dpi_input_i.key,
+        c_dpi_data_unp,
+        c_dpi_ad_unp,
+        c_dpi_input_i.tag,
+        c_dpi_data,
+        c_dpi_tag
+      );
+    end
+  end
+
+endmodule
--- hw/ip/aes/pre_dv/aes_tlul_shim_tb/rtl/aes_tlul_shim_tb_pkg.sv
+++ hw/ip/aes/pre_dv/aes_tlul_shim_tb/rtl/aes_tlul_shim_tb_pkg.sv
@@ -5,11 +5,10 @@
 package aes_tlul_shim_tb_pkg;
 
   import tlul_pkg::*;
+  import aes_pkg::*;
+  import aes_reg_pkg::*;
 
-  parameter bit AES_MANUAL_OPERATION    = 0;
-  parameter bit AES_SIDELOAD            = 0;
-  parameter int AES_GCM_NUM_VALID_BYTES = 16;
-
+  // AES register fields offsets.
   parameter int AES_CTRL_OPERATION_OFFSET        = 0;
   parameter int AES_CTRL_MODE_OFFSET             = 2;
   parameter int AES_CTRL_KEY_LEN_OFFSET          = 8;
@@ -26,6 +25,36 @@ package aes_tlul_shim_tb_pkg;
   parameter int AES_STATUS_OUTPUT_VALID_OFFSET = 3;
   parameter int AES_STATUS_INPUT_READY_OFFSET  = 4;
 
+  parameter int AES_TRIGGER_START_OFFSET                = 0;
+  parameter int AES_TRIGGER_KEY_IV_DATA_IN_CLEAR_OFFSET = 1;
+  parameter int AES_TRIGGER_DATA_OUT_CLEAR_OFFSET       = 2;
+  parameter int AES_TRIGGER_PRNG_RESEED_OFFSET          = 3;
+
+  // Caliptra register offsets
+  parameter logic [11:0] CALIPTRA_NAME_0_OFFSET    = 12'h100;
+  parameter logic [11:0] CALIPTRA_NAME_1_OFFSET    = 12'h104;
+  parameter logic [11:0] CALIPTRA_VERSION_0_OFFSET = 12'h108;
+  parameter logic [11:0] CALIPTRA_VERSION_1_OFFSET = 12'h10c;
+
+  `include `REQUESTS_FILE
+
+  // Grouping of all signals required for a `c_dpi` API call.
+  typedef struct packed {
+    aes_op_e operation;
+    aes_mode_e mode;
+    key_len_e key_length;
+
+    logic [255:0] key;
+    logic [127:0] iv;
+    // Unfortunately, Verilator v4.210 does not allow unpacked arrays in structs, hence both AD and
+    // data need to be unpacked later.
+    logic [max(1, `AD_LENGTH)-1:0][7:0] ad;
+    logic [max(1, `DATA_LENGTH)-1:0][7:0] data;
+    logic [127:0] tag; // Only used in the GCM decryption case.
+   } c_dpi_input_t;
+
+  // Grouping of all signals required for a shim read/write request and to instrument the `c_dpi`
+  // API.
   typedef struct packed {
     logic                       dv;
     logic [top_pkg::TL_AW-1:0]  addr;
@@ -38,9 +67,13 @@ package aes_tlul_shim_tb_pkg;
     logic [top_pkg::TL_AIW-1:0] id;
 
     // Internal signal to verify status bits.
-    logic [top_pkg::TL_DW-1:0]  mask;
+    logic [top_pkg::TL_DW-1:0] mask;
+
+    // Internal signal: AD and PT inputs to the `c_dpi` model.
+    c_dpi_input_t c_dpi_input;
   } shim_request_t;
 
+  // write_request returns a filled out `shim_request_t` struct for a shim write request.
   function automatic shim_request_t write_request (logic [7:0] addr,
                                                    logic [top_pkg::TL_DW-1:0] wdata);
     shim_request_t req = '{
@@ -53,17 +86,18 @@ package aes_tlul_shim_tb_pkg;
       last: 1'b0,
       user: 32'(TL_A_USER_DEFAULT),
       id: '{default: '0},
-      mask: '{default: '0}
+      mask: '{default: '0},
+      c_dpi_input: '0
     };
     return req;
   endfunction
 
+  // read_request returns a filled out `shim_request_t` struct for a shim read request.
   function automatic shim_request_t read_request (logic [7:0] addr,
-                                                  logic [top_pkg::TL_DW-1:0] mask = '0,
-                                                  bit calyptra = 1'b0);
+                                                  logic [top_pkg::TL_DW-1:0] mask = '0);
     shim_request_t req = '{
       dv: 1'b1,
-      addr: {23'b0, calyptra, addr},
+      addr: 32'(addr),
       write: 1'b0,
       wdata: '{default: '0},
       wstrb: top_pkg::TL_DBW'(4'b1111),
@@ -71,9 +105,35 @@ package aes_tlul_shim_tb_pkg;
       last: 1'b0,
       user: 32'(TL_A_USER_DEFAULT),
       id: '{default: '0},
-      mask: mask
+      mask: mask,
+      c_dpi_input: '0
     };
     return req;
   endfunction
+
+  // read_caliptra returns a filled out `shim_request_t` struct for an internal Caliptra register.
+  function automatic shim_request_t read_caliptra (logic [11:0] addr);
+    shim_request_t req = '{
+      dv: 1'b1,
+      addr: 32'(addr),
+      default: '0
+    };
+    return req;
+  endfunction
+
+  // c_dpi_load assigns a `c_dpi_input_t` to an empty `shim_request_t` struct. This type of
+  // request is used to instrument the `c_dpi` API at the beginning of a test.
+  function automatic shim_request_t c_dpi_load (c_dpi_input_t c_dpi_input);
+    shim_request_t req = '0;
+    req.c_dpi_input = c_dpi_input;
+    return req;
+  endfunction
+
+  // Convenience helper function.
+  function automatic int unsigned max (int unsigned a, int unsigned b);
+    return (a > b) ? a : b;
+  endfunction
+
+  `REQUESTS
 
 endpackage
