f0d66b51f46173a38e4752cc1c964578bb534efd
Amaury Pouly <amaury.pouly@lowrisc.org>
2024-11-14 17:58:00 +0000

    [test_rom] Convert test_rom.c to devicetables
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/lib/testing/test_rom/test_rom.c
+++ sw/device/lib/testing/test_rom/test_rom.c
@@ -27,12 +27,21 @@
 #if !OT_IS_ENGLISH_BREAKFAST
 #include "sw/device/silicon_creator/lib/drivers/retention_sram.h"
 #endif
+#include "devicetables.h"
 #include "sw/device/silicon_creator/lib/manifest.h"
 #include "sw/device/silicon_creator/rom/bootstrap.h"
 
 #include "flash_ctrl_regs.h"
-#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"  // Generated.
 #include "otp_ctrl_regs.h"
+
+static const dt_pinmux_t *kPinmuxDt = &kDtPinmux[0];
+static const dt_rstmgr_t *kRstmgrDt = &kDtRstmgr[0];
+static const dt_flash_ctrl_t *kFlashCtrlDt = &kDtFlashCtrl[0];
+static const dt_uart_t *kUart0Dt = &kDtUart[0];
+static const dt_rv_core_ibex_t *kRvCoreIbexDt = &kDtRvCoreIbex[0];
+#if !OT_IS_ENGLISH_BREAKFAST
+static const dt_otp_ctrl_t *kOtpCtrlDt = &kDtOtpCtrl[0];
+#endif
 
 /* These symbols are defined in
  * `opentitan/sw/device/lib/testing/test_rom/test_rom.ld`, and describes the
@@ -74,9 +83,11 @@ OT_WEAK
 bool rom_test_main(void) {
 #if !OT_IS_ENGLISH_BREAKFAST
   // Check the otp to see if execute should start
-  uint32_t otp_val = abs_mmio_read32(
-      TOP_EARLGREY_OTP_CTRL_CORE_BASE_ADDR + OTP_CTRL_SW_CFG_WINDOW_REG_OFFSET +
-      OTP_CTRL_PARAM_CREATOR_SW_CFG_ROM_EXEC_EN_OFFSET);
+  const uint32_t otp_ctrl_base =
+      dt_otp_ctrl_reg_block(kOtpCtrlDt, kDtOtpCtrlRegBlockDefault);
+  uint32_t otp_val =
+      abs_mmio_read32(otp_ctrl_base + OTP_CTRL_SW_CFG_WINDOW_REG_OFFSET +
+                      OTP_CTRL_PARAM_CREATOR_SW_CFG_ROM_EXEC_EN_OFFSET);
 
   if (otp_val == 0) {
     test_status_set(kTestStatusInBootRomHalt);
@@ -87,9 +98,9 @@ bool rom_test_main(void) {
   // Initialize Ibex cpuctrl (contains icache / security feature enablements).
   uint32_t cpuctrl_csr;
   CSR_READ(CSR_REG_CPUCTRL, &cpuctrl_csr);
-  uint32_t cpuctrl_otp_val = abs_mmio_read32(
-      TOP_EARLGREY_OTP_CTRL_CORE_BASE_ADDR + OTP_CTRL_SW_CFG_WINDOW_REG_OFFSET +
-      OTP_CTRL_PARAM_CREATOR_SW_CFG_CPUCTRL_OFFSET);
+  uint32_t cpuctrl_otp_val =
+      abs_mmio_read32(otp_ctrl_base + OTP_CTRL_SW_CFG_WINDOW_REG_OFFSET +
+                      OTP_CTRL_PARAM_CREATOR_SW_CFG_CPUCTRL_OFFSET);
   cpuctrl_csr = bitfield_field32_write(
       cpuctrl_csr, (bitfield_field32_t){.mask = 0x3f, .index = 0},
       cpuctrl_otp_val);
@@ -100,23 +111,21 @@ bool rom_test_main(void) {
   sec_mmio_init();
 
   // Configure the pinmux.
-  CHECK_DIF_OK(dif_pinmux_init(
-      mmio_region_from_addr(TOP_EARLGREY_PINMUX_AON_BASE_ADDR), &pinmux));
+  CHECK_DIF_OK(dif_pinmux_init_from_dt(kPinmuxDt, &pinmux));
   pinmux_testutils_init(&pinmux);
 
-  CHECK_DIF_OK(dif_rstmgr_init(
-      mmio_region_from_addr(TOP_EARLGREY_RSTMGR_AON_BASE_ADDR), &rstmgr));
+  CHECK_DIF_OK(dif_rstmgr_init_from_dt(kRstmgrDt, &rstmgr));
 
   // Initialize the flash.
   CHECK_DIF_OK(dif_flash_ctrl_init_state(
-      &flash_ctrl,
-      mmio_region_from_addr(TOP_EARLGREY_FLASH_CTRL_CORE_BASE_ADDR)));
+      &flash_ctrl, mmio_region_from_addr(dt_flash_ctrl_reg_block(
+                       kFlashCtrlDt, kDtFlashCtrlRegBlockCore))));
   CHECK_DIF_OK(dif_flash_ctrl_start_controller_init(&flash_ctrl));
   CHECK_STATUS_OK(flash_ctrl_testutils_wait_for_init(&flash_ctrl));
 #if !OT_IS_ENGLISH_BREAKFAST
   // Check the otp to see if flash scramble should be enabled.
   otp_val = abs_mmio_read32(
-      TOP_EARLGREY_OTP_CTRL_CORE_BASE_ADDR + OTP_CTRL_SW_CFG_WINDOW_REG_OFFSET +
+      otp_ctrl_base + OTP_CTRL_SW_CFG_WINDOW_REG_OFFSET +
       OTP_CTRL_PARAM_CREATOR_SW_CFG_FLASH_DATA_DEFAULT_CFG_OFFSET);
   if (otp_val != 0) {
     dif_flash_ctrl_region_properties_t default_properties;
@@ -137,8 +146,7 @@ bool rom_test_main(void) {
 
   // Setup the UART for printing messages to the console.
   if (kDeviceType != kDeviceSimDV) {
-    CHECK_DIF_OK(dif_uart_init(
-        mmio_region_from_addr(TOP_EARLGREY_UART0_BASE_ADDR), &uart0));
+    CHECK_DIF_OK(dif_uart_init_from_dt(kUart0Dt, &uart0));
     CHECK(kUartBaudrate <= UINT32_MAX, "kUartBaudrate must fit in uint32_t");
     CHECK(kClockFreqPeripheralHz <= UINT32_MAX,
           "kClockFreqPeripheralHz must fit in uint32_t");
@@ -177,8 +185,7 @@ bool rom_test_main(void) {
   // Print the FPGA version-id.
   // This is guaranteed to be zero on all non-FPGA implementations.
   dif_rv_core_ibex_fpga_info_t fpga;
-  CHECK_DIF_OK(dif_rv_core_ibex_init(
-      mmio_region_from_addr(TOP_EARLGREY_RV_CORE_IBEX_CFG_BASE_ADDR), &ibex));
+  CHECK_DIF_OK(dif_rv_core_ibex_init_from_dt(kRvCoreIbexDt, &ibex));
   CHECK_DIF_OK(dif_rv_core_ibex_read_fpga_info(&ibex, &fpga));
   if (fpga != 0) {
     LOG_INFO("TestROM:%08x", fpga);
@@ -201,8 +208,8 @@ bool rom_test_main(void) {
       dif_flash_ctrl_set_exec_enablement(&flash_ctrl, kDifToggleEnabled));
 
   // Always select slot a and enable address translation if manifest says to.
-  const manifest_t *manifest =
-      (const manifest_t *)TOP_EARLGREY_EFLASH_BASE_ADDR;
+  const manifest_t *manifest = (const manifest_t *)dt_flash_ctrl_reg_block(
+      kFlashCtrlDt, kDtFlashCtrlRegBlockMem);
   uintptr_t entry_point = manifest_entry_point_get(manifest);
   if (manifest->address_translation == kHardenedBoolTrue) {
     dif_rv_core_ibex_addr_translation_mapping_t addr_map = {
