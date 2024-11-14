bdd66d16183ce35d6ca3e221af93be42eef0b234
Amaury Pouly <amaury.pouly@lowrisc.org>
2024-11-14 18:06:59 +0000

    [silicon_creator,lib,manifet] Generalize for multitop
    There are no DT structures to store the eflash size yet so use
    ifdef for now.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/silicon_creator/lib/manifest.c
+++ sw/device/silicon_creator/lib/manifest.c
@@ -6,7 +6,18 @@
 
 #include "sw/device/silicon_creator/lib/base/chip.h"
 
+#if defined(OPENTITAN_IS_ENGLISHBREAKFAST)
+#include "hw/top_englishbreakfast/sw/autogen/top_englishbreakfast.h"
+#define EFLASH_SIZE_BYES TOP_ENGLISHBREAKFAST_EFLASH_SIZE_BYTES
+#elif defined(OPENTITAN_IS_EARLGREY)
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+#define EFLASH_SIZE_BYES TOP_EARLGREY_EFLASH_SIZE_BYTES
+#elif defined(OPENTITAN_IS_DARJEELING)
+#include "hw/top_darjeeling/sw/autogen/top_darjeeling.h"
+#define EFLASH_SIZE_BYES TOP_DARJEELING_RAM_MAIN_SIZE_BYTES
+#else
+#error unsupported top
+#endif
 
 static_assert(CHIP_ROM_EXT_SIZE_MIN >= CHIP_MANIFEST_SIZE,
               "`CHIP_ROM_EXT_SIZE_MIN` is too small");
@@ -18,8 +29,8 @@ static_assert(CHIP_BL0_SIZE_MIN >= CHIP_MANIFEST_SIZE,
               "`CHIP_BL0_SIZE_MIN` is too small");
 static_assert(CHIP_BL0_SIZE_MAX >= CHIP_BL0_SIZE_MIN,
               "`CHIP_BL0_SIZE_MAX` is too small");
-static_assert(CHIP_BL0_SIZE_MAX <= ((TOP_EARLGREY_EFLASH_SIZE_BYTES / 2) -
-                                    CHIP_ROM_EXT_SIZE_MAX),
+static_assert(CHIP_BL0_SIZE_MAX <=
+                  ((EFLASH_SIZE_BYES / 2) - CHIP_ROM_EXT_SIZE_MAX),
               "`CHIP_BL0_SIZE_MAX` is too large");
 
 // Extern declarations for the inline functions in the manifest header.
