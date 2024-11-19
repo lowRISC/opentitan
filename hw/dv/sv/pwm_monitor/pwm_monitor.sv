c2daaa5d3753a65bea93456750e16a5fcceeac0b
Rupert Swarbrick <rswarbrick@lowrisc.org>
2024-11-19 14:15:21 +0000

    [pwm,dv] Teach pwm_monitor to handle resets and cfg.active properly
    The change in behaviour should only ever matter if we see things that
    look like transactions when we're under reset or the config doesn't
    think it is active. Practically speaking, this isn't going to happen,
    but the change should hopefully make the code easier to reason about.
    
    The change also adds some documentation comments to explain how the
    monitor is expected to work and restructures the logic to be a bit
    easier to follow.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/dv/sv/pwm_monitor/pwm_monitor.sv
+++ hw/dv/sv/pwm_monitor/pwm_monitor.sv
@@ -11,8 +11,15 @@ class pwm_monitor extends dv_base_monitor #(
   extern function new(string name = "", uvm_component parent = null);
   extern function void build_phase(uvm_phase phase);
 
-  // collect transactions forever - already forked in dv_base_monitor::run_phase
+  // Return true if we are currently in the active phase (the first half) of a transaction.
+  extern function bit is_active();
+
+  // Collect transactions forever whenever rst_n is high and cfg.active is true.
   extern protected task collect_trans();
+
+  // Collect a single transaction. This is called in a loop by collect_trans and is safe to kill at
+  // any time.
+  extern protected task collect_transaction();
 
   extern task monitor_ready_to_end();
 endclass
@@ -35,46 +42,64 @@ function void pwm_monitor::build_phase(uvm_phase phase);
   end
 endfunction
 
+function bit pwm_monitor::is_active();
+  // The active phase is the first part of the transaction. If invert is false, pwm is true for that
+  // time.
+  return cfg.vif.cb.pwm == !cfg.invert;
+endfunction
+
 task pwm_monitor::collect_trans();
-  uint count_cycles, active_cycles;
-  logic pwm_prev = 0;
-
-  wait(cfg.vif.rst_n);
   forever begin
-    if (!cfg.active) begin
-      wait (cfg.active);
-      count_cycles = 0;
-      active_cycles = 0;
-    end
+    wait(cfg.vif.rst_n && cfg.active);
+    fork begin : isolation_fork
+      fork
+        wait(!(cfg.vif.rst_n && cfg.active));
+        forever collect_transaction();
+      join_any
+      disable fork;
+    end join
+  end
+endtask
 
+task pwm_monitor::collect_transaction();
+  uint active_cycles = 0, inactive_cycles = 0;
+
+  // Wait until the pwm signal is the 'active' value (marking the start of the first phase of the
+  // transaction).
+  while (!is_active()) @(cfg.vif.cb);
+
+  // Measure the amount of time we are active measure how many cycles we were active.
+  while (is_active()) begin
+    active_cycles++;
     @(cfg.vif.cb);
-    count_cycles++;
-    if (cfg.vif.cb.pwm != pwm_prev) begin
-      `uvm_info(`gfn, $sformatf("Detected edge: %0b->%0b at %0d cycles (from last edge)",
-                                pwm_prev, cfg.vif.cb.pwm, count_cycles), UVM_HIGH)
-      pwm_prev = cfg.vif.cb.pwm;
-      if (cfg.vif.cb.pwm == cfg.invert) begin
-        // We got to the first (active) half duty cycle point. Save the count and restart.
-        active_cycles = count_cycles;
-      end else begin
-        uint phase_count;
-        pwm_item item = pwm_item::type_id::create("item");
-        item.invert       = cfg.invert;
-        item.monitor_id   = cfg.monitor_id;
-        item.active_cnt   = active_cycles;
-        item.inactive_cnt = count_cycles;
-        item.period       = count_cycles + active_cycles;
-        item.duty_cycle   = item.get_duty_cycle();
+  end
 
-        // Each PWM pulse cycle is divided into 2^DC_RESN+1 beats, per beat the 16-bit
-        // phase counter increments by 2^(16-DC_RESN-1)(modulo 65536)
-        phase_count = ((item.period / (2 ** (cfg.resolution + 1))) *
-                       (2 ** (16 - (cfg.resolution - 1))));
-        item.phase = (phase_count % 65536);
-        analysis_port.write(item);
-      end
-      count_cycles = 0;
-    end
+  // Finally, wait until the pwm signal becomes the 'active' value again, incrementing
+  // inactive_cycles to measure how many cycles we were inactive.
+  while (!is_active()) begin
+    inactive_cycles++;
+    @(cfg.vif.cb);
+  end
+
+  // At this point, we've just finished the first cycle of the active phase of the next transaction,
+  // which will be counted correctly if we start collect_transaction again before any more time
+  // elapses. Report the transaction that we have measured through the analysis port.
+  begin
+    uint phase_count;
+    pwm_item item = pwm_item::type_id::create("item");
+    item.invert       = cfg.invert;
+    item.monitor_id   = cfg.monitor_id;
+    item.active_cnt   = active_cycles;
+    item.inactive_cnt = inactive_cycles;
+    item.period       = inactive_cycles + active_cycles;
+    item.duty_cycle   = item.get_duty_cycle();
+
+    // Each PWM pulse cycle is divided into 2^DC_RESN+1 beats, per beat the 16-bit
+    // phase counter increments by 2^(16-DC_RESN-1)(modulo 65536)
+    phase_count = ((item.period / (2 ** (cfg.resolution + 1))) *
+                   (2 ** (16 - (cfg.resolution - 1))));
+    item.phase = (phase_count % 65536);
+    analysis_port.write(item);
   end
 endtask
 
