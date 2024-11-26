fbab54b9baaefe724a0fec446f59b90ef769adc4
Amaury Pouly <amaury.pouly@lowrisc.org>
2024-11-26 18:49:35 +0100

    [silicon_creator] Remove useless dependencies on boot_data
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/silicon_creator/lib/boot_log.h
+++ sw/device/silicon_creator/lib/boot_log.h
@@ -8,7 +8,6 @@
 #include <stdint.h>
 
 #include "sw/device/lib/base/macros.h"
-#include "sw/device/silicon_creator/lib/boot_data.h"
 #include "sw/device/silicon_creator/lib/chip_info.h"
 #include "sw/device/silicon_creator/lib/drivers/hmac.h"
 #include "sw/device/silicon_creator/lib/error.h"
