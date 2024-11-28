7b8c4b4c27883092f9962c884decd96b9d0e066a
Amaury Pouly <amaury.pouly@lowrisc.org>
2024-11-28 17:51:28 +0100

    [ottf,main] Port to DT
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/lib/testing/test_framework/ottf_main.c
+++ sw/device/lib/testing/test_framework/ottf_main.c
@@ -7,6 +7,7 @@
 #include <assert.h>
 #include <stddef.h>
 
+#include "devicetables.h"
 #include "external/freertos/include/FreeRTOS.h"
 #include "external/freertos/include/queue.h"
 #include "external/freertos/include/task.h"
@@ -27,9 +28,6 @@
 #include "sw/device/lib/testing/test_framework/ottf_test_config.h"
 #include "sw/device/lib/testing/test_framework/status.h"
 #include "sw/device/silicon_creator/lib/manifest_def.h"
-
-// TODO: make this toplevel agnostic.
-#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
 #define MODULE_ID MAKE_MODULE_ID('o', 't', 'm')
 
@@ -153,8 +151,7 @@ void _ottf_main(void) {
 
   // Clear reset reason register.
   dif_rstmgr_t rstmgr;
-  CHECK_DIF_OK(dif_rstmgr_init(
-      mmio_region_from_addr(TOP_EARLGREY_RSTMGR_AON_BASE_ADDR), &rstmgr));
+  CHECK_DIF_OK(dif_rstmgr_init_from_dt(&kDtRstmgr[0], &rstmgr));
   if (kOttfTestConfig.clear_reset_reason) {
     CHECK_DIF_OK(dif_rstmgr_reset_info_clear(&rstmgr));
   }
@@ -170,9 +167,7 @@ void _ottf_main(void) {
   // Initialize a global random number generator testutil context to provide
   // tests with a source of entropy for randomizing test behaviors.
   dif_rv_core_ibex_t rv_core_ibex;
-  CHECK_DIF_OK(dif_rv_core_ibex_init(
-      mmio_region_from_addr(TOP_EARLGREY_RV_CORE_IBEX_CFG_BASE_ADDR),
-      &rv_core_ibex));
+  CHECK_DIF_OK(dif_rv_core_ibex_init_from_dt(&kDtRvCoreIbex[0], &rv_core_ibex));
   rand_testutils_rng_ctx = rand_testutils_init(&rv_core_ibex);
 
   // Run the test.
