f2eaab34c5b166f80f4624f94b01dffd67252e6a
Amaury Pouly <amaury.pouly@lowrisc.org>
2024-11-28 17:58:55 +0100

    [silicon_creator,hmac] Port driver to DT
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/silicon_creator/lib/drivers/hmac.c
+++ sw/device/silicon_creator/lib/drivers/hmac.c
@@ -4,6 +4,7 @@
 
 #include "sw/device/silicon_creator/lib/drivers/hmac.h"
 
+#include "devicetables.h"
 #include "sw/device/lib/base/abs_mmio.h"
 #include "sw/device/lib/base/bitfield.h"
 #include "sw/device/lib/base/macros.h"
@@ -11,17 +12,20 @@
 #include "sw/device/silicon_creator/lib/error.h"
 
 #include "hmac_regs.h"  // Generated.
-#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+
+static const dt_hmac_t *kHmacDt = &kDtHmac[0];
+
+static inline uint32_t hmac_base(void) {
+  return dt_hmac_reg_block(kHmacDt, kDtHmacRegBlockCore);
+}
 
 void hmac_sha256_configure(bool big_endian_digest) {
   // Clear the config, stopping the SHA engine.
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CFG_REG_OFFSET, 0u);
+  abs_mmio_write32(hmac_base() + HMAC_CFG_REG_OFFSET, 0u);
 
   // Disable and clear interrupts. INTR_STATE register is rw1c.
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_INTR_ENABLE_REG_OFFSET,
-                   0u);
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_INTR_STATE_REG_OFFSET,
-                   UINT32_MAX);
+  abs_mmio_write32(hmac_base() + HMAC_INTR_ENABLE_REG_OFFSET, 0u);
+  abs_mmio_write32(hmac_base() + HMAC_INTR_STATE_REG_OFFSET, UINT32_MAX);
 
   uint32_t reg = 0;
   reg = bitfield_bit32_write(reg, HMAC_CFG_DIGEST_SWAP_BIT, big_endian_digest);
@@ -33,12 +37,12 @@ void hmac_sha256_configure(bool big_endian_digest) {
                                HMAC_CFG_DIGEST_SIZE_VALUE_SHA2_256);
   reg = bitfield_field32_write(reg, HMAC_CFG_KEY_LENGTH_FIELD,
                                HMAC_CFG_KEY_LENGTH_VALUE_KEY_256);
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CFG_REG_OFFSET, reg);
+  abs_mmio_write32(hmac_base() + HMAC_CFG_REG_OFFSET, reg);
 }
 
 inline void hmac_sha256_start(void) {
   uint32_t cmd = bitfield_bit32_write(0, HMAC_CMD_HASH_START_BIT, true);
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CMD_REG_OFFSET, cmd);
+  abs_mmio_write32(hmac_base() + HMAC_CMD_REG_OFFSET, cmd);
 }
 
 void hmac_sha256_update(const void *data, size_t len) {
@@ -46,34 +50,30 @@ void hmac_sha256_update(const void *data, size_t len) {
 
   // Individual byte writes are needed if the buffer isn't word aligned.
   for (; len != 0 && (uintptr_t)data_sent & 3; --len) {
-    abs_mmio_write8(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_MSG_FIFO_REG_OFFSET,
-                    *data_sent++);
+    abs_mmio_write8(hmac_base() + HMAC_MSG_FIFO_REG_OFFSET, *data_sent++);
   }
 
   for (; len >= sizeof(uint32_t); len -= sizeof(uint32_t)) {
     uint32_t data_aligned = read_32(data_sent);
-    abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_MSG_FIFO_REG_OFFSET,
-                     data_aligned);
+    abs_mmio_write32(hmac_base() + HMAC_MSG_FIFO_REG_OFFSET, data_aligned);
     data_sent += sizeof(uint32_t);
   }
 
   // Handle non-32bit aligned bytes at the end of the buffer.
   for (; len != 0; --len) {
-    abs_mmio_write8(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_MSG_FIFO_REG_OFFSET,
-                    *data_sent++);
+    abs_mmio_write8(hmac_base() + HMAC_MSG_FIFO_REG_OFFSET, *data_sent++);
   }
 }
 
 void hmac_sha256_update_words(const uint32_t *data, size_t len) {
   for (size_t i = 0; i < len; i++) {
-    abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_MSG_FIFO_REG_OFFSET,
-                     data[i]);
+    abs_mmio_write32(hmac_base() + HMAC_MSG_FIFO_REG_OFFSET, data[i]);
   }
 }
 
 inline void hmac_sha256_process(void) {
   uint32_t cmd = bitfield_bit32_write(0, HMAC_CMD_HASH_PROCESS_BIT, true);
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CMD_REG_OFFSET, cmd);
+  abs_mmio_write32(hmac_base() + HMAC_CMD_REG_OFFSET, cmd);
 }
 
 /**
@@ -84,19 +84,16 @@ inline void hmac_sha256_process(void) {
 static void wait_for_done(void) {
   uint32_t reg = 0;
   do {
-    reg = abs_mmio_read32(TOP_EARLGREY_HMAC_BASE_ADDR +
-                          HMAC_INTR_STATE_REG_OFFSET);
+    reg = abs_mmio_read32(hmac_base() + HMAC_INTR_STATE_REG_OFFSET);
   } while (!bitfield_bit32_read(reg, HMAC_INTR_STATE_HMAC_DONE_BIT));
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_INTR_STATE_REG_OFFSET,
-                   reg);
+  abs_mmio_write32(hmac_base() + HMAC_INTR_STATE_REG_OFFSET, reg);
 }
 
 void hmac_sha256_final_truncated(uint32_t *digest, size_t len) {
   wait_for_done();
 
   uint32_t result, incr;
-  uint32_t reg =
-      abs_mmio_read32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CFG_REG_OFFSET);
+  uint32_t reg = abs_mmio_read32(hmac_base() + HMAC_CFG_REG_OFFSET);
   if (bitfield_bit32_read(reg, HMAC_CFG_DIGEST_SWAP_BIT)) {
     // Big-endian output.
     result = HMAC_DIGEST_0_REG_OFFSET;
@@ -114,7 +111,7 @@ void hmac_sha256_final_truncated(uint32_t *digest, size_t len) {
   // just in case.
   len = len <= kHmacDigestNumWords ? len : kHmacDigestNumWords;
   for (uint32_t i = 0; i < len; ++i, result += incr) {
-    digest[i] = abs_mmio_read32(TOP_EARLGREY_HMAC_BASE_ADDR + result);
+    digest[i] = abs_mmio_read32(hmac_base() + result);
   }
 }
 
@@ -129,66 +126,61 @@ void hmac_sha256_save(hmac_context_t *ctx) {
   // Issue the STOP command to halt the operation and compute the intermediate
   // digest.
   uint32_t cmd = bitfield_bit32_write(0, HMAC_CMD_HASH_STOP_BIT, true);
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CMD_REG_OFFSET, cmd);
+  abs_mmio_write32(hmac_base() + HMAC_CMD_REG_OFFSET, cmd);
   wait_for_done();
 
   // Read the digest registers. Note that endianness does not matter here,
   // because we will simply restore the registers in the same order as we saved
   // them.
   for (uint32_t i = 0; i < kHmacDigestNumWords; i++) {
-    ctx->digest[i] =
-        abs_mmio_read32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_DIGEST_0_REG_OFFSET +
-                        i * sizeof(uint32_t));
+    ctx->digest[i] = abs_mmio_read32(hmac_base() + HMAC_DIGEST_0_REG_OFFSET +
+                                     i * sizeof(uint32_t));
   }
 
   // Read the message length registers.
-  ctx->msg_len_lower = abs_mmio_read32(TOP_EARLGREY_HMAC_BASE_ADDR +
-                                       HMAC_MSG_LENGTH_LOWER_REG_OFFSET);
-  ctx->msg_len_upper = abs_mmio_read32(TOP_EARLGREY_HMAC_BASE_ADDR +
-                                       HMAC_MSG_LENGTH_UPPER_REG_OFFSET);
+  ctx->msg_len_lower =
+      abs_mmio_read32(hmac_base() + HMAC_MSG_LENGTH_LOWER_REG_OFFSET);
+  ctx->msg_len_upper =
+      abs_mmio_read32(hmac_base() + HMAC_MSG_LENGTH_UPPER_REG_OFFSET);
 
   // Momentarily clear the `sha_en` bit, which clears the digest.
-  uint32_t cfg =
-      abs_mmio_read32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CFG_REG_OFFSET);
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CFG_REG_OFFSET,
+  uint32_t cfg = abs_mmio_read32(hmac_base() + HMAC_CFG_REG_OFFSET);
+  abs_mmio_write32(hmac_base() + HMAC_CFG_REG_OFFSET,
                    bitfield_bit32_write(cfg, HMAC_CFG_SHA_EN_BIT, false));
 
   // Restore the full original configuration.
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CFG_REG_OFFSET, cfg);
+  abs_mmio_write32(hmac_base() + HMAC_CFG_REG_OFFSET, cfg);
 }
 
 void hmac_sha256_restore(const hmac_context_t *ctx) {
   // Clear the `sha_en` bit to ensure the message length registers are
   // writeable. Leave the rest of the configuration unchanged.
-  uint32_t cfg =
-      abs_mmio_read32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CFG_REG_OFFSET);
+  uint32_t cfg = abs_mmio_read32(hmac_base() + HMAC_CFG_REG_OFFSET);
   cfg = bitfield_bit32_write(cfg, HMAC_CFG_SHA_EN_BIT, false);
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CFG_REG_OFFSET, cfg);
+  abs_mmio_write32(hmac_base() + HMAC_CFG_REG_OFFSET, cfg);
 
   // Write the digest registers. Note that endianness does not matter here,
   // because we will simply restore the registers in the same order as we saved
   // them.
   for (uint32_t i = 0; i < kHmacDigestNumWords; i++) {
-    abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_DIGEST_0_REG_OFFSET +
-                         i * sizeof(uint32_t),
-                     ctx->digest[i]);
+    abs_mmio_write32(
+        hmac_base() + HMAC_DIGEST_0_REG_OFFSET + i * sizeof(uint32_t),
+        ctx->digest[i]);
   }
 
   // Write the message length registers.
-  abs_mmio_write32(
-      TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_MSG_LENGTH_LOWER_REG_OFFSET,
-      ctx->msg_len_lower);
-  abs_mmio_write32(
-      TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_MSG_LENGTH_UPPER_REG_OFFSET,
-      ctx->msg_len_upper);
+  abs_mmio_write32(hmac_base() + HMAC_MSG_LENGTH_LOWER_REG_OFFSET,
+                   ctx->msg_len_lower);
+  abs_mmio_write32(hmac_base() + HMAC_MSG_LENGTH_UPPER_REG_OFFSET,
+                   ctx->msg_len_upper);
 
   // Re-enable the SHA engine.
   cfg = bitfield_bit32_write(cfg, HMAC_CFG_SHA_EN_BIT, true);
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CFG_REG_OFFSET, cfg);
+  abs_mmio_write32(hmac_base() + HMAC_CFG_REG_OFFSET, cfg);
 
   // Issue the CONTINUE command to restart the operation.
   uint32_t cmd = bitfield_bit32_write(0, HMAC_CMD_HASH_CONTINUE_BIT, true);
-  abs_mmio_write32(TOP_EARLGREY_HMAC_BASE_ADDR + HMAC_CMD_REG_OFFSET, cmd);
+  abs_mmio_write32(hmac_base() + HMAC_CMD_REG_OFFSET, cmd);
 }
 
 extern void hmac_sha256_init(void);
