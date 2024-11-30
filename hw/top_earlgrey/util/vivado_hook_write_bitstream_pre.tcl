2376681d5f01fabc0e8a2732fdd0647687fe88ce
Alexander Williams <awill@opentitan.org>
2024-11-30 08:28:54 -0800

    [bitstream] Collect MMI data into one file
    Reduce BUILD file boilerplate by placing all memory map info in a single
    file. Use a separate Processor element for each map, so the InstData
    attribute can be used as a key to identify the memory to be updated.
    This construction also permits multiple memories to be spliced in a
    single call to updatemem, though that is not implemented in this commit.
    
    Adjust MMI dump to use a regex for mem type and a dictionary for each
    memory's parameters. For the mem type, some memories in the FPGA can
    consist of both RAMB36 and RAMB18 types, depending on the width of the
    array. The regex allows more flexible memory cell detection.
    
    Rev up the bitstream manifest schema to v3 so the memories contained in
    the MMI file are described. Add a function to collect MMI data from v2
    cache entries and rewrite in the expected v3 format, with the MMI data
    in a single file.
    
    Finally, drop support for bitstream cache entries that do not contain a
    manifest. The tools to create a manifest have been in the repo for
    awhile, and as of this commit's creation, no such cache entries remain
    in the public cloud storage bucket.
    
    Signed-off-by: Alexander Williams <awill@opentitan.org>

--- hw/top_earlgrey/util/vivado_hook_write_bitstream_pre.tcl
+++ hw/top_earlgrey/util/vivado_hook_write_bitstream_pre.tcl
@@ -19,89 +19,96 @@ set_property BITSTREAM.CONFIG.USR_ACCESS TIMESTAMP [current_design]
 #
 # Args:
 #   filename:            Path to the output file.
-#   brams:               A list of BRAM cells.
-#   mem_type:            The BRAM type, e.g. "RAMB36".
-#   fake_word_width:     If non-zero, pretend that $brams covers
-#                        `fake_word_width` bits. Influences the values of the
-#                        MMI's <AddressSpace> and <DataWidth> tags.
-#   addr_end_multiplier: A coefficient applied to the address space. Influences
-#                        the values of the MMI's <AddressSpace> and
-#                        <AddressRange> tags.
+#   mem_info:            Dictionary of dictionaries with following properties for each (inner) value:
+#       brams:               A list of BRAM cells.
+#       mem_type_regex:      The BRAM type regex, dividing the mem type and site, e.g. {(RAMB\d+)_(\w+)}.
+#       fake_word_width:     If non-zero, pretend that $brams covers
+#                            `fake_word_width` bits. Influences the values of the
+#                            MMI's <AddressSpace> and <DataWidth> tags.
+#       addr_end_multiplier: A coefficient applied to the address space. Influences
+#                            the values of the MMI's <AddressSpace> and
+#                            <AddressRange> tags.
 #   designtask_count:    A number used for logging with `send_msg`.
-proc generate_mmi {filename brams mem_type fake_word_width addr_end_multiplier designtask_count} {
+proc generate_mmi {filename mem_infos designtask_count} {
     send_msg "${designtask_count}-1" INFO "Dumping MMI to ${filename}"
-
-    if {[llen $brams] == 0} {
-        send_msg "${designtask_count}-1" INFO "Cannot make MMI for zero BRAMs"
-        return
-    }
 
     set workroot [file dirname [info script]]
     set filepath "${workroot}/${filename}"
     set fileout [open $filepath "w"]
-
-    set fake_slice_width [expr $fake_word_width / [llen $brams]]
-
-    # Calculate the overall address space.
-    set space 0
-    foreach inst [lsort -dictionary $brams] {
-        set slice_begin [get_property ram_slice_begin [get_cells $inst]]
-        set slice_end [get_property ram_slice_end [get_cells $inst]]
-        if {$slice_begin eq {} || $slice_end eq {}} {
-            send_msg "${designtask_count}-2" ERROR "Extraction of ${filename} information failed."
-        }
-        set slice_width [expr {$slice_end - $slice_begin + 1}]
-        if {$slice_width < $fake_slice_width} {
-            set slice_end [expr {$slice_begin + $fake_slice_width - 1}]
-            set slice_width $fake_slice_width
-        }
-        set addr_begin [get_property ram_addr_begin [get_cells $inst]]
-        set addr_end [get_property ram_addr_end [get_cells $inst]]
-        if {$addr_begin eq {} || $addr_end eq {}} {
-            send_msg "${designtask_count}-3" ERROR "Extraction of ${filename} MMI information failed."
-        }
-
-        # Calculate total number of bits.
-        set space [expr {$space + ($addr_end - $addr_begin + 1) * $slice_width}]
-        set last_slice_width $slice_width
-    }
-    set space [expr {($space * $addr_end_multiplier / 8) - 1}]
-
-    # Generate the MMI.
+    set part [get_property PART [current_design]]
     puts $fileout "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
     puts $fileout "<MemInfo Version=\"1\" Minor=\"1\">"
-    puts $fileout "  <Processor Endianness=\"Little\" InstPath=\"dummy\">"
-    puts $fileout "  <AddressSpace Name=\"dummy_addrspace\" Begin=\"0\" End=\"$space\">"
-    puts $fileout "      <BusBlock>"
 
-    set loc_prefix "${mem_type}_"
+    dict for {id mem_info} $mem_infos {
+        dict with mem_info {
+            if {[llen $brams] == 0} {
+                send_msg "${designtask_count}-1" INFO "Cannot make MMI for zero BRAMs"
+                return
+            }
 
-    set part [get_property PART [current_design]]
-    foreach inst [lsort -dictionary $brams] {
-        set loc [get_property LOC [get_cells $inst]]
-        set loc [string trimleft $loc $loc_prefix]
-        set slice_begin [get_property ram_slice_begin [get_cells $inst]]
-        set slice_end [get_property ram_slice_end [get_cells $inst]]
-        set slice_width [expr {$slice_end - $slice_begin + 1}]
-        if {$slice_width < $fake_slice_width} {
-            set slice_end [expr {$slice_begin + $fake_slice_width - 1}]
-            set slice_width $fake_slice_width
+            set fake_slice_width [expr $fake_word_width / [llen $brams]]
+
+            # Calculate the overall address space.
+            set space 0
+            foreach inst [lsort -dictionary $brams] {
+                set slice_begin [get_property ram_slice_begin [get_cells $inst]]
+                set slice_end [get_property ram_slice_end [get_cells $inst]]
+                if {$slice_begin eq {} || $slice_end eq {}} {
+                    send_msg "${designtask_count}-2" ERROR "Extraction of ${filename} information failed."
+                }
+                set slice_width [expr {$slice_end - $slice_begin + 1}]
+                if {$slice_width < $fake_slice_width} {
+                    set slice_end [expr {$slice_begin + $fake_slice_width - 1}]
+                    set slice_width $fake_slice_width
+                }
+                set addr_begin [get_property ram_addr_begin [get_cells $inst]]
+                set addr_end [get_property ram_addr_end [get_cells $inst]]
+                if {$addr_begin eq {} || $addr_end eq {}} {
+                    send_msg "${designtask_count}-3" ERROR "Extraction of ${filename} MMI information failed."
+                }
+
+                # Calculate total number of bits.
+                set space [expr {$space + ($addr_end - $addr_begin + 1) * $slice_width}]
+                set last_slice_width $slice_width
+            }
+            set space [expr {($space * $addr_end_multiplier / 8) - 1}]
+
+            # Generate the MMI.
+            puts $fileout "  <Processor Endianness=\"Little\" InstPath=\"$id\">"
+            puts $fileout "    <AddressSpace Name=\"dummy_addrspace\" Begin=\"0\" End=\"$space\">"
+            puts $fileout "      <BusBlock>"
+
+            foreach inst [lsort -dictionary $brams] {
+                set loc [get_property LOC [get_cells $inst]]
+                set loc_matches [regexp $mem_type_regex $loc loc_match loc_prefix loc_suffix]
+                if {$loc_matches == 0} {
+                    send_msg "${designtask_count}-4" ERROR "Extraction of ${filename} mem location failed."
+                }
+                set slice_begin [get_property ram_slice_begin [get_cells $inst]]
+                set slice_end [get_property ram_slice_end [get_cells $inst]]
+                set slice_width [expr {$slice_end - $slice_begin + 1}]
+                if {$slice_width < $fake_slice_width} {
+                    set slice_end [expr {$slice_begin + $fake_slice_width - 1}]
+                    set slice_width $fake_slice_width
+                }
+                set addr_begin [get_property ram_addr_begin [get_cells $inst]]
+                set addr_end [get_property ram_addr_end [get_cells $inst]]
+                set addr_end [expr {($addr_end + 1) * $addr_end_multiplier - 1}]
+                puts $fileout "        <BitLane MemType=\"$loc_prefix\" Placement=\"$loc_suffix\">"
+                puts $fileout "          <DataWidth MSB=\"$slice_end\" LSB=\"$slice_begin\"/>"
+                puts $fileout "          <AddressRange Begin=\"$addr_begin\" End=\"$addr_end\"/>"
+                puts $fileout "          <Parity ON=\"false\" NumBits=\"0\"/>"
+                puts $fileout "        </BitLane>"
+            }
+            puts $fileout "      </BusBlock>"
+            puts $fileout "    </AddressSpace>"
+            puts $fileout "  </Processor>"
         }
-        set addr_begin [get_property ram_addr_begin [get_cells $inst]]
-        set addr_end [get_property ram_addr_end [get_cells $inst]]
-        set addr_end [expr {($addr_end + 1) * $addr_end_multiplier - 1}]
-        puts $fileout "        <BitLane MemType=\"$mem_type\" Placement=\"$loc\">"
-        puts $fileout "          <DataWidth MSB=\"$slice_end\" LSB=\"$slice_begin\"/>"
-        puts $fileout "          <AddressRange Begin=\"$addr_begin\" End=\"$addr_end\"/>"
-        puts $fileout "          <Parity ON=\"false\" NumBits=\"0\"/>"
-        puts $fileout "        </BitLane>"
     }
-    puts $fileout "      </BusBlock>"
-    puts $fileout "    </AddressSpace>"
-    puts $fileout "  </Processor>"
-    puts $fileout "<Config>"
-    puts $fileout "  <Option Name=\"Part\" Val=\"$part\"/>"
-    puts $fileout "</Config>"
+
+    puts $fileout "  <Config>"
+    puts $fileout "    <Option Name=\"Part\" Val=\"$part\"/>"
+    puts $fileout "  </Config>"
     puts $fileout "</MemInfo>"
     close $fileout
     send_msg "${designtask_count}-4" INFO "MMI dumped to ${filepath}"
@@ -161,6 +168,7 @@ switch ${fpga_family} {
     set bram_regex "BMEM.*.*"
   }
 }
+set mem_type_regex {(RAMB\d+)_(\w+)}
 
 # The scrambled Boot ROM is actually 39 bits wide, but we need to pretend that
 # it's 40 bits, or else we will be unable to encode our ROM data in a MEM file
@@ -177,7 +185,10 @@ switch ${fpga_family} {
 # A hack that works is to pretend the data width is actually 40 bits. Updatemem
 # seems to write that extra zero bit into the ether without complaint.
 set rom_brams [split [get_cells -hierarchical -filter " PRIMITIVE_TYPE =~ ${bram_regex} && NAME =~ *u_rom_ctrl*"] " "]
-generate_mmi "rom.mmi" $rom_brams "RAMB36" 40 1 1
+dict set memInfo rom brams $rom_brams
+dict set memInfo rom mem_type_regex $mem_type_regex
+dict set memInfo rom fake_word_width 40
+dict set memInfo rom addr_end_multiplier 1
 
 # OTP does not require faking the word width, but it has its own quirk. It seems
 # each 22-bit OTP word is followed by 15 zero words. The MMI's <AddressSpace>
@@ -185,7 +196,12 @@ generate_mmi "rom.mmi" $rom_brams "RAMB36" 40 1 1
 # that its data input overruns the address space. The workaround is to pretend
 # the address space is 16 times larger than we would normally compute.
 set otp_brams [split [get_cells -hierarchical -filter " PRIMITIVE_TYPE =~ ${bram_regex} && NAME =~ *u_otp_ctrl*"] " "]
-generate_mmi "otp.mmi" $otp_brams "RAMB18" 0 16 2
+dict set memInfo otp brams $otp_brams
+dict set memInfo otp mem_type_regex $mem_type_regex
+dict set memInfo otp fake_word_width 0
+dict set memInfo otp addr_end_multiplier 16
+
+generate_mmi "memories.mmi" $memInfo 1
 
 # For debugging purposes, dump the INIT_XX strings for ROM and OTP.
 dump_init_strings "rom_init_strings.txt" $rom_brams 3
