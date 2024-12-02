039583a82b54eb4add55a50a03b23da97a5903a9
Amaury Pouly <amaury.pouly@lowrisc.org>
2024-12-02 10:22:24 +0100

    [tests] Partially port rstmgr_sw_req_test to DT
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/tests/rstmgr_sw_req_test.c
+++ sw/device/tests/rstmgr_sw_req_test.c
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "devicetables.h"
 #include "sw/device/lib/base/mmio.h"
 #include "sw/device/lib/dif/dif_rstmgr.h"
 #include "sw/device/lib/runtime/log.h"
@@ -9,15 +10,12 @@
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 
-#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
-
 static dif_rstmgr_t rstmgr;
 
 OTTF_DEFINE_TEST_CONFIG();
 
 bool test_main(void) {
-  CHECK_DIF_OK(dif_rstmgr_init(
-      mmio_region_from_addr(TOP_EARLGREY_RSTMGR_AON_BASE_ADDR), &rstmgr));
+  CHECK_DIF_OK(dif_rstmgr_init_from_dt(&kDtRstmgr[0], &rstmgr));
 
   dif_rstmgr_reset_info_bitfield_t reason;
   reason = rstmgr_testutils_reason_get();
