37fc765a852e618a202a40d24a33659cfe449580
Kinza Qamar <kqzaman@lowrisc.org>
2024-12-02 17:18:09 +0000

    [rom_ctrl, dv] Conditional coverage hole inside the adapter
    ** There is a removal of an item (reqfifo_rvalid) from the conditional statement to
       deduct coverage hole 1011 for rom_ctrl .We can't see dvalid & !reqfifo_rvalid.
       Reason being if d_valid is true then reqfifo_rvalid must be true. Similarly, if
       reqfifo_rvalid is false, then we can't see d_valid high.
    
    ** An assertion is added for the d_valid -> reqfifo_rvalid to make sure that it
       can never be true for the adapter.
    
    Signed-off-by: Kinza Qamar <kqzaman@lowrisc.org>

--- hw/ip/tlul/rtl/tlul_adapter_sram.sv
+++ hw/ip/tlul/rtl/tlul_adapter_sram.sv
@@ -311,7 +311,7 @@ module tlul_adapter_sram
   end
 
   logic vld_rd_rsp;
-  assign vld_rd_rsp = d_valid & reqfifo_rvalid & rspfifo_rvalid & (reqfifo_rdata.op == OpRead);
+  assign vld_rd_rsp = d_valid & rspfifo_rvalid & (reqfifo_rdata.op == OpRead);
   // If the response data is not valid, we set it to an illegal blanking value which is determined
   // by whether the current transaction is an instruction fetch or a regular read operation.
   logic [top_pkg::TL_DW-1:0] error_blanking_data;
@@ -682,4 +682,6 @@ module tlul_adapter_sram
   `ASSERT_KNOWN(TlOutValidKnown_A, tl_o.d_valid)
   `ASSERT(TlOutKnownIfFifoKnown_A, !$isunknown(rspfifo_rdata) -> !$isunknown(tl_o))
 
+  // The definition of d_valid leads to the assertion below.
+  `ASSERT(DValidNeedsReqFifoRValid, d_valid -> reqfifo_rvalid)
 endmodule
