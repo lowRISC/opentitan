e4e088a6f8dcec3f598e117ce6ef168950cd710a
Tim Trippel <ttrippel@google.com>
2024-12-03 02:03:04 -0800

    [provisioning] the DIN fields should be parsed as decimal values
    The DIN fields should each be parsed as decimal values, even though they
    are reported as a hex string; there are just unused bits of each
    subfield.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/host/provisioning/orchestrator/src/util.py
+++ sw/host/provisioning/orchestrator/src/util.py
@@ -25,6 +25,15 @@ def format_hex(n, width=8):
     return format(n, f"#0{width + 2}x")  # + 2 to account for 0x chars
 
 
+def read_int_as_decimal_str(x: int) -> int:
+    """Interpret a number by reading its hexadecimal representation as a decimal number.
+
+    For example, `70` in hex is `0x46` and should be read as the decimal value 46.
+    This encoding is a manufacturing equipment constraint.
+    """
+    return int(hex(x)[2:])
+
+
 def confirm():
     """Get user confirmation to continue."""
     confirm = input("Type confirm to continue: ")
