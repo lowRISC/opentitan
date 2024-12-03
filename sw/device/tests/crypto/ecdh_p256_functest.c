32a792ee4d3747962806b74dfa4bff129c8ab5a0
Jade Philipoom <jadep@zerorisc.com>
2024-12-03 14:01:14 +0100

    [crypto] Separate P-256 and P-384 ECDH operations.
    Last part of separating the curves' operations. This one involved some churn in
    the ECDH cryptotest firmware, since it heavily relied on calling the same
    function for both curves.
    
    The curve parameters structs are also now removed.
    
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>
    (cherry picked from commit 1a4dd4e0d1598b3ee6d770c17c2b49842af716df)

--- sw/device/tests/crypto/ecdh_p256_functest.c
+++ sw/device/tests/crypto/ecdh_p256_functest.c
@@ -23,11 +23,6 @@ enum {
   kP256SharedKeyWords = kP256SharedKeyBytes / sizeof(uint32_t),
 };
 
-static const otcrypto_ecc_curve_t kCurveP256 = {
-    .curve_type = kOtcryptoEccCurveTypeNistP256,
-    .domain_parameter = NULL,
-};
-
 // Configuration for the private key.
 static const otcrypto_key_config_t kEcdhPrivateKeyConfig = {
     .version = kOtcryptoLibVersion1,
@@ -109,12 +104,12 @@ status_t key_exchange_test(void) {
   // Compute the shared secret from A's side of the computation (using A's
   // private key and B's public key).
   LOG_INFO("Generating shared secret (A)...");
-  TRY(otcrypto_ecdh(&private_keyA, &public_keyB, &kCurveP256, &shared_keyA));
+  TRY(otcrypto_ecdh_p256(&private_keyA, &public_keyB, &shared_keyA));
 
   // Compute the shared secret from B's side of the computation (using B's
   // private key and A's public key).
   LOG_INFO("Generating shared secret (B)...");
-  TRY(otcrypto_ecdh(&private_keyB, &public_keyA, &kCurveP256, &shared_keyB));
+  TRY(otcrypto_ecdh_p256(&private_keyB, &public_keyA, &shared_keyB));
 
   // Get pointers to individual shares of both shared keys.
   uint32_t *keyA0;
