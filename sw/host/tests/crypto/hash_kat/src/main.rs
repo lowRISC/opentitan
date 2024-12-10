40fed6efea47d84a41a16f01ef79e257775557df
James Wainwright <james.wainwright@lowrisc.org>
2024-12-10 15:33:35 +0000

    [rust] Reformat crypto test files
    These did not seem to be running under `rustfmt` pre-bzlmod somehow.
    
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
    (cherry picked from commit 645296ba4b17af27af572aaa34fa4567eccbc979)

--- sw/host/tests/crypto/hash_kat/src/main.rs
+++ sw/host/tests/crypto/hash_kat/src/main.rs
@@ -17,11 +17,11 @@ use cryptotest_commands::hash_commands::{
 };
 
 use opentitanlib::app::TransportWrapper;
+use opentitanlib::console::spi::SpiConsoleDevice;
 use opentitanlib::execute_test;
 use opentitanlib::test_utils::init::InitializeTest;
 use opentitanlib::test_utils::rpc::{ConsoleRecv, ConsoleSend};
 use opentitanlib::uart::console::UartConsole;
-use opentitanlib::console::spi::SpiConsoleDevice;
 
 #[derive(Debug, Parser)]
 struct Opts {
