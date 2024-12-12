76e7434fcc6ebb39e8d5a1069b858dbd3f1353dc
Pascal Nasahl <nasahlpa@lowrisc.org>
2024-12-12 06:18:52 +0100

    [pentest] Update EDN SCA tests
    This commit updates the EDN SCA tests to use asm instead of C
    code such that the SCA target is more accurate.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    Co-authored-by: Alexander Wagner <alexander.wagner@aisec.fraunhofer.de>
    (cherry picked from commit cb3defc058df2dd36e08bb3916378a1c088627ab)

--- sw/device/tests/penetrationtests/json/edn_sca_commands.h
+++ sw/device/tests/penetrationtests/json/edn_sca_commands.h
@@ -12,26 +12,14 @@ extern "C" {
 // clang-format off
 
 #define EDNSCA_SUBCOMMAND(_, value) \
+    value(_, Init) \
     value(_, BusData) \
-    value(_, BusDataBatchFvsr) \
-    value(_, BusDataBatchRandom) \
-    value(_, Init)
+    value(_, BusDataBatch)
 UJSON_SERDE_ENUM(EdnScaSubcommand, edn_sca_subcommand_t, EDNSCA_SUBCOMMAND);
 
 #define EDNSCA_RESULT(field, string) \
-    field(result, uint32_t)
+    field(rnd_data, uint32_t, 4)
 UJSON_SERDE_STRUCT(EdnScaResult, edn_sca_result_t, EDNSCA_RESULT);
-
-#define EDNSCA_SEED(field, string) \
-    field(init_seed, uint32_t, 12) \
-    field(reseed, uint32_t, 12)
-UJSON_SERDE_STRUCT(EdnScaSeed, edn_sca_seed_t, EDNSCA_SEED);
-
-#define EDNSCA_SEED_BATCH(field, string) \
-    field(init_seed, uint32_t, 12) \
-    field(reseed, uint32_t, 12) \
-    field(num_iterations, uint32_t)
-UJSON_SERDE_STRUCT(EdnScaSeedBatch, edn_sca_seed_batch_t, EDNSCA_SEED_BATCH);
 
 #define EDNSCA_BATCH(field, string) \
     field(num_iterations, uint32_t)
