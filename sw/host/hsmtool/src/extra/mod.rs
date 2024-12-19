a04764b721fdee133c21259947bc74afdcf1096c
Chris Frantz <cfrantz@google.com>
2024-12-18 16:44:00 -0800

    [hsmtool] Rename the spxef module
    Rename the `spxef` module to `extra::spxef` as we'll be adding more
    custom backends into the `extra` module.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>
    (cherry picked from commit 9603e674c9989da7e164a2e1ca21b662b2d810d1)

--- sw/host/hsmtool/src/extra/mod.rs
+++ sw/host/hsmtool/src/extra/mod.rs
@@ -2,9 +2,6 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-pub mod commands;
-pub mod error;
-pub mod extra;
-pub mod module;
-pub mod profile;
-pub mod util;
+mod spxef;
+
+pub use spxef::SpxEf;
