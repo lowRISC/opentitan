6711c47eedf0ceaddc3fc6b95203b7e605000f2f
Robert Schilling <rschilling@rivosinc.com>
2024-12-23 09:45:59 +0100

    [hw,ibex,rtl] Add DFT signals to prim_ram_generic_ram_1p
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/vendor/lowrisc_ibex/dv/uvm/icache/dv/prim_badbit/prim_badbit_ram_1p.sv
+++ hw/vendor/lowrisc_ibex/dv/uvm/icache/dv/prim_badbit/prim_badbit_ram_1p.sv
@@ -36,15 +36,16 @@ module prim_badbit_ram_1p #(
     .DataBitsPerMask(DataBitsPerMask),
     .MemInitFile    (MemInitFile)
   ) u_mem (
-    .clk_i(clk_i),
+    .clk_i    (clk_i),
 
-    .cfg_i  ('0),
-    .req_i  (req_i),
-    .write_i(write_i),
-    .addr_i (addr_i),
-    .wdata_i(wdata_i),
-    .wmask_i(wmask_i),
-    .rdata_o(sram_rdata)
+    .cfg_i    ('0),
+    .cfg_rsp_o(),
+    .req_i    (req_i),
+    .write_i  (write_i),
+    .addr_i   (addr_i),
+    .wdata_i  (wdata_i),
+    .wmask_i  (wmask_i),
+    .rdata_o  (sram_rdata)
   );
 
   // This module doesn't work with Verilator (because of the wired-or). Because we define the
