5e42bb265a8c6a35cfb7fd51181171cdcd573c45
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-01-02 13:37:48 +0000

    [sram_ctrl,dv] Add new exclusions for fifo_cnt
    As the depth of the FIFO is one, we can exclude a couple of
    uncovered conditions.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- hw/ip/sram_ctrl/dv/cov/sram_ctrl_manual_cov_excl.el
+++ hw/ip/sram_ctrl/dv/cov/sram_ctrl_manual_cov_excl.el
@@ -25,3 +25,23 @@ CHECKSUM: "2099741489 2073313596"
 INSTANCE: tb.dut.u_reg_regs.u_status_sram_alert.wr_en_data_arb
 ANNOTATION: "[UNR] all inputs are constant"
 Block 2 "1620753216" "assign wr_data = d;"
+
+// As the depth of this FIFO is 1, the following conditions cannot be met.
+CHECKSUM: "3215070453 33318353"
+INSTANCE: tb.dut.u_tlul_adapter_sram.u_sram_byte.gen_integ_handling.u_sync_fifo.gen_normal_fifo.u_fifo_cnt
+Condition 1 "2532211833" "(incr_wptr_i & (wptr_o == 1'((Depth - 1)))) 1 -1"
+Condition 7 "687544961" "(full_o ? (1'(Depth)) : ((wptr_wrap_msb == rptr_wrap_msb) ? ((1'(wptr_o) - 1'(rptr_o))) : (((1'(Depth) - 1'(rptr_o)) + 1'(wptr_o))))) 1 -1"
+Condition 3 "2597027294" "(incr_rptr_i & (rptr_o == 1'((Depth - 1)))) 1 -1"
+CHECKSUM: "3215070453 1827096802"
+INSTANCE: tb.dut.u_tlul_adapter_sram.u_sram_byte.gen_integ_handling.u_sync_fifo.gen_normal_fifo.u_fifo_cnt
+Branch 0 "721764659" "full_o" (0) "full_o 1,-"
+Branch 0 "721764659" "full_o" (2) "full_o 0,0"
+Branch 0 "721764659" "full_o" (1) "full_o 0,1"
+CHECKSUM: "3215070453 3446030929"
+INSTANCE: tb.dut.u_tlul_adapter_sram.u_sram_byte.gen_integ_handling.u_sync_fifo.gen_normal_fifo.u_fifo_cnt
+Block 19 "4019242409" "wptr_wrap_cnt_q <= (wptr_wrap_cnt_q + {{(WrapPtrW - 1) {1'b0}}, 1'b1});"
+Block 28 "1113085816" "rptr_wrap_cnt_q <= (rptr_wrap_cnt_q + {{(WrapPtrW - 1) {1'b0}}, 1'b1});"
+CHECKSUM: "3215070453 1827096802"
+INSTANCE: tb.dut.u_tlul_adapter_sram.u_sram_byte.gen_integ_handling.u_sync_fifo.gen_normal_fifo.u_fifo_cnt
+Branch 1 "2417346495" "(!rst_ni)" (3) "(!rst_ni) 0,0,0,1"
+Branch 2 "456961687" "(!rst_ni)" (3) "(!rst_ni) 0,0,0,1"
