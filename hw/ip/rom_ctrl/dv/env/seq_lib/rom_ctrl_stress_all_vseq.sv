e78012839880e381d3b25dc1925c53e12e0552f1
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-01-14 12:04:59 +0000

    [rom_ctrl,dv] Unconditionally reset in the stress all vseq
    rom_ctrl is a little unusual and it doesn't really make sense to run a
    vseq without a reset. Change things so that we don't do so.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rom_ctrl/dv/env/seq_lib/rom_ctrl_stress_all_vseq.sv
+++ hw/ip/rom_ctrl/dv/env/seq_lib/rom_ctrl_stress_all_vseq.sv
@@ -8,13 +8,27 @@ class rom_ctrl_stress_all_vseq extends rom_ctrl_base_vseq;
 
   `uvm_object_new
 
+  // If the upper sequence has disabled do_apply_reset for this sequence (probably because it's
+  // stress_all_with_rand_reset), we aren't going to be able to allow the child sequences we start
+  // to apply a reset either.
+  //
+  // But rom_ctrl is a bit unusual: the only way of testing it is to come out of a reset! So
+  // constrain num_trans to be 1 in this situation: we are presumably coming out of a reset now but
+  // won't be able to inject any more.
+  //
+  // If the upper sequence allows us to apply reset, we can run several transactions. Pick 3,
+  // allowing several sequences back-to-back.
   extern constraint num_trans_c;
-  extern task body();
 
+  extern task body();
 endclass
 
 constraint rom_ctrl_stress_all_vseq::num_trans_c {
-  num_trans inside {[3:6]};
+  if (!do_apply_reset) {
+    num_trans == 1;
+  } else {
+    num_trans == 3;
+  }
 }
 
 task rom_ctrl_stress_all_vseq::body();
@@ -27,10 +41,11 @@ task rom_ctrl_stress_all_vseq::body();
     seq = create_seq_by_name(seq_names[seq_idx]);
     `downcast(rom_ctrl_vseq, seq)
 
-    // if upper seq disables do_apply_reset for this seq, then can't issue reset
-    // as upper seq may drive reset
-    if (do_apply_reset) rom_ctrl_vseq.do_apply_reset = $urandom_range(0, 1);
-    else                rom_ctrl_vseq.do_apply_reset = 0;
+    // Tell the child sequence to start with a reset (because a rom_ctrl vseq only really makes
+    // sense after a reset) unless this is the first iteration, in which case we can use the reset
+    // that we just saw.
+    rom_ctrl_vseq.do_apply_reset = (i > 1);
+
     rom_ctrl_vseq.set_sequencer(p_sequencer);
     `uvm_info(`gfn, $sformatf("Running %s sequence", seq_names[seq_idx]), UVM_LOW)
     `DV_CHECK_RANDOMIZE_FATAL(rom_ctrl_vseq)
