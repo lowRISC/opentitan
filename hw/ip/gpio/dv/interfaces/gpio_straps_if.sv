2449eed3479f182260ebd5816842f9ffdb252d52
Marcelo Carvalho Faleiro de Almeida <marcelo.carvalho@lowrisc.org>
2025-01-14 15:56:15 +0000

    New testcase for the GPIO feature straps
    Removed the assumption StrapSampleOnce_A, that does not work
    for cases that sample_trigger never fell or if we have more than one
    reset operation happening during the simulation.
    - Fix the gpio_stress_all with random reset PR:25657 was taking into this
      commit.
    
    Signed-off-by: Marcelo Carvalho Faleiro de Almeida <marcelo.carvalho@lowrisc.org>

---
+++ hw/ip/gpio/dv/interfaces/gpio_straps_if.sv
@@ -0,0 +1,21 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+// interface : gpio_straps_if
+`ifndef GPIO_STRAPS_IF_SV
+`define GPIO_STRAPS_IF_SV
+
+import gpio_pkg::*;
+
+// Interface definition
+interface gpio_straps_if();
+
+  logic          strap_en;       // Signal to enable straps
+  gpio_straps_t  sampled_straps; // Sampled gpio_i snapshot data from GPIO (DUT)
+
+  modport port_in(input strap_en, output sampled_straps);
+  modport port_out(output strap_en, input sampled_straps);
+
+endinterface
+
+`endif // GPIO_STRAPS_IF_SV
