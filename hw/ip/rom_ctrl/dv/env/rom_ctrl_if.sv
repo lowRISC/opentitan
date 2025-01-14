78fdb1fb0ab27e5cf9dcf8bc4a6520e73f78b57a
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-01-14 16:43:52 +0000

    [rom_ctrl,dv] Properly wait for outputs to avoid race
    This avoids a rather confusing race in rom_ctrl_scoreboard where we
    were waiting for a change in pwrmgr_data or keymgr_data and then doing
    something with the result.
    
    Unfortunately, the structure that we waited on got updated by multiple
    processes and they happened to be scheduled with the scoreboard in the
    middle.
    
    Use a clocking block to sort things out.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rom_ctrl/dv/env/rom_ctrl_if.sv
+++ hw/ip/rom_ctrl/dv/env/rom_ctrl_if.sv
@@ -2,8 +2,16 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-interface rom_ctrl_if;
+// An interface that holds the output signals from rom_ctrl. These are all updated on the posedge of
+// the associated clock, so can be accessed through a clocking block.
+
+interface rom_ctrl_if (input logic clk_i, input logic rst_ni);
   rom_ctrl_pkg::pwrmgr_data_t pwrmgr_data;
   rom_ctrl_pkg::keymgr_data_t keymgr_data;
   rom_ctrl_pkg::fsm_state_e   checker_fsm_state;
+
+  clocking cb @(posedge clk_i);
+    input pwrmgr_data;
+    input keymgr_data;
+  endclocking
 endinterface
