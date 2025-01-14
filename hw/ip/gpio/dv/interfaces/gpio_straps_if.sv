b31acc6a3cb3a724c38bafa5dfae1e1f8842e8f4
Marcelo Carvalho Faleiro de Almeida <marcelo.carvalho@lowrisc.org>
2025-01-14 15:56:15 +0000

    New testcase for the GPIO feature straps
    - Fix the gpio_stress_all with random reset PR:25657 was taking into this
      commit.
    
    Signed-off-by: Marcelo Carvalho Faleiro de Almeida <marcelo.carvalho@lowrisc.org>

---
+++ hw/ip/gpio/dv/interfaces/gpio_straps_if.sv
@@ -0,0 +1,34 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+// interface : gpio_straps_if
+`ifndef GPIO_STRAPS_IF_SV
+`define GPIO_STRAPS_IF_SV
+
+import gpio_pkg::*;
+
+// Interface definition
+interface gpio_straps_if(input logic clk, input logic rst_n);
+
+  logic          strap_en;       // Signal to enable straps
+  logic          strap_en_qq;
+  logic          strap_en_q;
+  gpio_straps_t  sampled_straps; // Sampled gpio_i snapshot data from GPIO (DUT)
+
+  modport dut_if(input strap_en, output sampled_straps);
+  modport tb_if(output strap_en, input sampled_straps);
+
+
+    always_ff @(posedge clk or negedge rst_n) begin
+      if (!rst_n) begin
+          strap_en_q  <= 0;
+          strap_en_qq <= 0;
+      end else begin
+          strap_en_q  <= strap_en;
+          strap_en_qq <= strap_en_q;
+      end
+    end
+
+endinterface
+
+`endif // GPIO_STRAPS_IF_SV
