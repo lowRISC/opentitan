d1e4d2dd2cff9b78efacb907f08a65db834f0043
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-01-14 13:00:54 +0000

    [rom_ctrl,dv] Tweak rom_ctrl_kmac_err_chk_vseq to allow resets
    This is just changing the vseq slightly so that the body task exits
    quickly if it sees a reset.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rom_ctrl/dv/env/seq_lib/rom_ctrl_kmac_err_chk_vseq.sv
+++ hw/ip/rom_ctrl/dv/env/seq_lib/rom_ctrl_kmac_err_chk_vseq.sv
@@ -15,10 +15,21 @@ task rom_ctrl_kmac_err_chk_vseq::body();
   cfg.m_kmac_agent_cfg.error_rsp_pct = 100;
   expect_fatal_alerts = 1'b1;
 
-  wait(cfg.m_kmac_agent_cfg.vif.mon_cb.rsp_done);
+  // Wait until kmac has sent a result to rom_ctrl. We know it will contain an error, which should
+  // be reflected in an alert from rom_ctrl.
+  //
+  // Since this takes some time to wait for, exit early if we see a reset.
+  fork begin : isolation_fork
+    fork
+      wait(cfg.under_reset);
+      begin
+        wait(cfg.m_kmac_agent_cfg.vif.mon_cb.rsp_done);
+        wait_for_fatal_alert(.max_delay(0), .max_wait_cycle(7));
+        `DV_CHECK_EQ(cfg.rom_ctrl_vif.pwrmgr_data.done, MuBi4False)
+      end
+    join_any
+    disable fork;
+  end join
 
-  wait_for_fatal_alert(.max_delay(0), .max_wait_cycle(7));
-
-  `DV_CHECK_EQ(cfg.rom_ctrl_vif.pwrmgr_data.done, MuBi4False)
   cfg.m_kmac_agent_cfg.error_rsp_pct = 0;
 endtask : body
