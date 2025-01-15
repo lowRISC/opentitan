5125b12b4c7972c31080f2b2fed1519d5a7f6fba
Antonio Martinez Zambrana <amz@lowrisc.org>
2025-01-15 11:38:56 +0000

    [dv, aon_timer] Enabling the prediction mask for cip_scb comparison
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/ip/aon_timer/dv/env/aon_timer_env_cfg.sv
+++ hw/ip/aon_timer/dv/env/aon_timer_env_cfg.sv
@@ -17,6 +17,9 @@ class aon_timer_env_cfg extends cip_base_env_cfg #(.RAL_T(aon_timer_reg_block));
 
   extern function new (string name="");
   extern virtual function void initialize(bit [31:0] csr_base_addr = '1);
+  // Set the 'has_prediction' field flag for all the intr_state fields so the comparison
+  // on reads can be carried out in cip_base_scoreboard
+  extern function void set_intr_state_has_prediction();
 
   // Convenience wrapper function to avoid calling explictly uvm_hdl_read multiple times
   // It takes a path relative to the RTL and appends the RTL instance to it
@@ -24,9 +27,9 @@ class aon_timer_env_cfg extends cip_base_env_cfg #(.RAL_T(aon_timer_reg_block));
   // Waits for AON signal to become 'value'
   extern task wait_for_aon_signal(string path, bit value);
   // Waits for the WE to rise and fall after a TL-UL write access
-  // Does HDL reads to be in sync with when the values kick-in the RTL
+  // Does HDL reads to be in sync with when the values are visible in the RTL
   // It needs to be called the moment the TL-access occurs, otherwise the thread may hang if the WE
-  // has risen already
+  // has risen and dropped again already
   extern task wait_for_we_pulse(input string path);
 
 endclass : aon_timer_env_cfg
@@ -43,6 +46,7 @@ function void aon_timer_env_cfg::initialize(bit [31:0] csr_base_addr = '1);
 
   // set num_interrupts & num_alerts
   num_interrupts = ral.intr_state.get_n_used_bits();
+  set_intr_state_has_prediction();
 endfunction : initialize
 
 function bit aon_timer_env_cfg::hdl_read_bit(string path);
@@ -65,3 +69,18 @@ task aon_timer_env_cfg::wait_for_we_pulse(input string path);
   wait_for_aon_signal(path, 1);
   wait_for_aon_signal(path, 0);
 endtask
+
+function void aon_timer_env_cfg::set_intr_state_has_prediction();
+  foreach (ral_model_names[i]) begin
+    dv_base_reg regs_q[$];
+    ral_models[ral_model_names[i]].get_dv_base_regs(regs_q);
+    foreach (regs_q[j]) begin
+      if (!uvm_re_match("intr_state*", regs_q[j].get_name())) begin
+        dv_base_reg_field fields_q[$];
+        regs_q[j].get_dv_base_reg_fields(fields_q);
+        foreach (fields_q[k])// Setting all fields to be compared in the base scoreboard
+          fields_q[k].has_prediction = 1;
+      end
+    end
+  end
+endfunction
