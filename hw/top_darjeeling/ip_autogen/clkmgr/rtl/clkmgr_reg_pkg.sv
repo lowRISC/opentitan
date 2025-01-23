b40c16313be85cf876c37e7382ab10df528200fa
Robert Schilling <rschilling@rivosinc.com>
2025-01-23 16:28:31 +0100

    [Darjeeling] Configure clock settings:
    main: 1GHz
    io: 1GHz
    usb (not used): 1 GHz
    AON: 62.5 MHz
    
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/top_darjeeling/ip_autogen/clkmgr/rtl/clkmgr_reg_pkg.sv
+++ hw/top_darjeeling/ip_autogen/clkmgr/rtl/clkmgr_reg_pkg.sv
@@ -80,10 +80,10 @@ package clkmgr_reg_pkg;
 
   typedef struct packed {
     struct packed {
-      logic [7:0]  q;
+      logic [3:0]  q;
     } lo;
     struct packed {
-      logic [7:0]  q;
+      logic [3:0]  q;
     } hi;
   } clkmgr_reg2hw_io_div4_meas_ctrl_shadowed_reg_t;
 
@@ -93,10 +93,10 @@ package clkmgr_reg_pkg;
 
   typedef struct packed {
     struct packed {
-      logic [9:0] q;
+      logic [5:0]  q;
     } lo;
     struct packed {
-      logic [9:0] q;
+      logic [5:0]  q;
     } hi;
   } clkmgr_reg2hw_main_meas_ctrl_shadowed_reg_t;
 
@@ -106,10 +106,10 @@ package clkmgr_reg_pkg;
 
   typedef struct packed {
     struct packed {
-      logic [8:0]  q;
+      logic [5:0]  q;
     } lo;
     struct packed {
-      logic [8:0]  q;
+      logic [5:0]  q;
     } hi;
   } clkmgr_reg2hw_usb_meas_ctrl_shadowed_reg_t;
 
@@ -216,18 +216,18 @@ package clkmgr_reg_pkg;
 
   // Register -> HW type
   typedef struct packed {
-    clkmgr_reg2hw_alert_test_reg_t alert_test; // [92:89]
-    clkmgr_reg2hw_extclk_ctrl_reg_t extclk_ctrl; // [88:81]
-    clkmgr_reg2hw_jitter_enable_reg_t jitter_enable; // [80:77]
-    clkmgr_reg2hw_clk_enables_reg_t clk_enables; // [76:74]
-    clkmgr_reg2hw_clk_hints_reg_t clk_hints; // [73:70]
-    clkmgr_reg2hw_measure_ctrl_regwen_reg_t measure_ctrl_regwen; // [69:69]
-    clkmgr_reg2hw_io_div4_meas_ctrl_en_reg_t io_div4_meas_ctrl_en; // [68:65]
-    clkmgr_reg2hw_io_div4_meas_ctrl_shadowed_reg_t io_div4_meas_ctrl_shadowed; // [64:49]
-    clkmgr_reg2hw_main_meas_ctrl_en_reg_t main_meas_ctrl_en; // [48:45]
-    clkmgr_reg2hw_main_meas_ctrl_shadowed_reg_t main_meas_ctrl_shadowed; // [44:25]
-    clkmgr_reg2hw_usb_meas_ctrl_en_reg_t usb_meas_ctrl_en; // [24:21]
-    clkmgr_reg2hw_usb_meas_ctrl_shadowed_reg_t usb_meas_ctrl_shadowed; // [20:3]
+    clkmgr_reg2hw_alert_test_reg_t alert_test; // [70:67]
+    clkmgr_reg2hw_extclk_ctrl_reg_t extclk_ctrl; // [66:59]
+    clkmgr_reg2hw_jitter_enable_reg_t jitter_enable; // [58:55]
+    clkmgr_reg2hw_clk_enables_reg_t clk_enables; // [54:52]
+    clkmgr_reg2hw_clk_hints_reg_t clk_hints; // [51:48]
+    clkmgr_reg2hw_measure_ctrl_regwen_reg_t measure_ctrl_regwen; // [47:47]
+    clkmgr_reg2hw_io_div4_meas_ctrl_en_reg_t io_div4_meas_ctrl_en; // [46:43]
+    clkmgr_reg2hw_io_div4_meas_ctrl_shadowed_reg_t io_div4_meas_ctrl_shadowed; // [42:35]
+    clkmgr_reg2hw_main_meas_ctrl_en_reg_t main_meas_ctrl_en; // [34:31]
+    clkmgr_reg2hw_main_meas_ctrl_shadowed_reg_t main_meas_ctrl_shadowed; // [30:19]
+    clkmgr_reg2hw_usb_meas_ctrl_en_reg_t usb_meas_ctrl_en; // [18:15]
+    clkmgr_reg2hw_usb_meas_ctrl_shadowed_reg_t usb_meas_ctrl_shadowed; // [14:3]
     clkmgr_reg2hw_fatal_err_code_reg_t fatal_err_code; // [2:0]
   } clkmgr_reg2hw_t;
 
@@ -305,11 +305,11 @@ package clkmgr_reg_pkg;
     4'b 0001, // index[ 8] CLKMGR_CLK_HINTS_STATUS
     4'b 0001, // index[ 9] CLKMGR_MEASURE_CTRL_REGWEN
     4'b 0001, // index[10] CLKMGR_IO_DIV4_MEAS_CTRL_EN
-    4'b 0011, // index[11] CLKMGR_IO_DIV4_MEAS_CTRL_SHADOWED
+    4'b 0001, // index[11] CLKMGR_IO_DIV4_MEAS_CTRL_SHADOWED
     4'b 0001, // index[12] CLKMGR_MAIN_MEAS_CTRL_EN
-    4'b 0111, // index[13] CLKMGR_MAIN_MEAS_CTRL_SHADOWED
+    4'b 0011, // index[13] CLKMGR_MAIN_MEAS_CTRL_SHADOWED
     4'b 0001, // index[14] CLKMGR_USB_MEAS_CTRL_EN
-    4'b 0111, // index[15] CLKMGR_USB_MEAS_CTRL_SHADOWED
+    4'b 0011, // index[15] CLKMGR_USB_MEAS_CTRL_SHADOWED
     4'b 0001, // index[16] CLKMGR_RECOV_ERR_CODE
     4'b 0001  // index[17] CLKMGR_FATAL_ERR_CODE
   };
