af7342172c42260f3c051019cbbe31e35a7e9c64
Robert Schilling <rschilling@rivosinc.com>
2025-01-24 10:35:45 +0100

    [hw,i2c,rtl] Add ASSERT_KNOWN to new IP outputs
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/i2c/rtl/i2c.sv
+++ hw/ip/i2c/rtl/i2c.sv
@@ -175,6 +175,9 @@ module i2c
   `ASSERT_KNOWN(IntrAcqStretchKnownO_A, intr_acq_stretch_o)
   `ASSERT_KNOWN(IntrUnexpStopKnownO_A, intr_unexp_stop_o)
   `ASSERT_KNOWN(IntrHostTimeoutKnownO_A, intr_host_timeout_o)
+  `ASSERT_KNOWN(LsioTriggerKnown_A, lsio_trigger_o)
+  `ASSERT_KNOWN(RaclErrorKnown_A, racl_error_o)
+  `ASSERT_KNOWN(RaclErrorLogKnown_A, racl_error_log_o)
 
   // Alert assertions for reg_we onehot check
   `ASSERT_PRIM_REG_WE_ONEHOT_ERROR_TRIGGER_ALERT(RegWeOnehotCheck_A, u_reg, alert_tx_o[0])
