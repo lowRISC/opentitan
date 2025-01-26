63643e0331f0e3c835271521032a54acb090d248
Robert Schilling <rschilling@rivosinc.com>
2025-01-26 10:54:19 +0100

    [hw,tlul_adapter_reg_racl,rtl] Implement shim in front of adapter
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/tlul/rtl/tlul_adapter_reg_racl.sv
+++ hw/ip/tlul/rtl/tlul_adapter_reg_racl.sv
@@ -52,33 +52,8 @@ module tlul_adapter_reg_racl
   // This can be a write or read error.
   input                    error_i
 );
-  logic racl_read_allowed, racl_write_allowed, racl_error;
-  logic rd_req, wr_req;
-  logic [RegDw-1:0] rdata;
-
-  tlul_adapter_reg #(
-    .CmdIntgCheck       (CmdIntgCheck),
-    .EnableRspIntgGen   (EnableRspIntgGen),
-    .EnableDataIntgGen  (EnableDataIntgGen),
-    .RegAw              (RegAw),
-    .RegDw              (RegDw),
-    .AccessLatency      (AccessLatency)
-  ) tlul_adapter_reg (
-    .clk_i,
-    .rst_ni,
-    .tl_i,
-    .tl_o,
-    .en_ifetch_i,
-    .intg_error_o,
-    .re_o(rd_req),
-    .we_o(wr_req),
-    .addr_o,
-    .wdata_o,
-    .be_o,
-    .busy_i,
-    .rdata_i(rdata),
-    .error_i(racl_error)
-  );
+  tl_h2d_t tl_h2d_filtered;
+  tl_d2h_t tl_d2h_filtered;
 
   if (EnableRacl) begin : gen_racl_role_logic
     // Retrieve RACL role from user bits and one-hot encode that for the comparison bitmap
@@ -94,30 +69,62 @@ module tlul_adapter_reg_racl
       .out_o( racl_role_vec )
     );
 
+    logic rd_req, racl_read_allowed, racl_write_allowed;
+    assign rd_req             = tl_i.a_opcode == tlul_pkg::Get;
     assign racl_read_allowed  = (|(racl_policies_i[RaclPolicySelVec].read_perm  & racl_role_vec));
     assign racl_write_allowed = (|(racl_policies_i[RaclPolicySelVec].write_perm & racl_role_vec));
-    assign racl_error_o       = (rd_req & ~racl_read_allowed) | (wr_req & ~racl_write_allowed);
-    // RACL only generates error responeses if enabled
-    assign racl_error         = racl_error_o & RaclErrorRsp;
+    assign racl_error_o       = (rd_req & ~racl_read_allowed) | (~rd_req & ~racl_write_allowed);
+
+    tlul_request_loopback #(
+      .ErrorRsp(RaclErrorRsp)
+    ) u_loopback (
+      .clk_i,
+      .rst_ni,
+      .squash_req_i ( racl_error_o    ),
+      .tl_h2d_i     ( tl_i            ),
+      .tl_d2h_o     ( tl_o            ),
+      .tl_h2d_o     ( tl_h2d_filtered ),
+      .tl_d2h_i     ( tl_d2h_filtered )
+    );
+
     // Collect RACL error information
     assign racl_error_log_o.read_access = tl_i.a_opcode == tlul_pkg::Get;
     assign racl_error_log_o.racl_role   = racl_role;
     assign racl_error_log_o.ctn_uid     = top_racl_pkg::tlul_extract_ctn_uid_bits(tl_i.a_user.rsvd);
   end else begin : gen_no_racl_role_logic
-    assign racl_read_allowed  = 1'b1;
-    assign racl_write_allowed = 1'b1;
-    assign racl_error         = 1'b0;
-    assign racl_error_o       = 1'b0;
-    assign racl_error_log_o   = '0;
+    // Pass through and default assignments
+    assign tl_h2d_filtered  = tl_i;
+    assign tl_o             = tl_d2h_filtered;
+    assign racl_error_o     = 1'b0;
+    assign racl_error_log_o = '0;
   end
 
-  // Not all RACL policies are used, even if RACL is enabled
-  logic unused_policy_sel;
-  assign unused_policy_sel = ^racl_policies_i;
+  tlul_adapter_reg #(
+    .CmdIntgCheck       (CmdIntgCheck),
+    .EnableRspIntgGen   (EnableRspIntgGen),
+    .EnableDataIntgGen  (EnableDataIntgGen),
+    .RegAw              (RegAw),
+    .RegDw              (RegDw),
+    .AccessLatency      (AccessLatency)
+  ) tlul_adapter_reg (
+    .clk_i,
+    .rst_ni,
+    .tl_i(tl_h2d_filtered),
+    .tl_o(tl_d2h_filtered),
+    .en_ifetch_i,
+    .intg_error_o,
+    .re_o,
+    .we_o,
+    .addr_o,
+    .wdata_o,
+    .be_o,
+    .busy_i,
+    .rdata_i,
+    .error_i
+  );
 
-  assign we_o = wr_req & racl_write_allowed;
-  assign re_o = rd_req & racl_read_allowed;
-  // Mask read data in case of a RACL violation
-  assign rdata = racl_error_o ? '1 : rdata_i;
+  // Ensure that RACL signals are not undefined
+  `ASSERT_KNOWN(RaclAdapterRegErrorKnown_A, racl_error_o)
+  `ASSERT_KNOWN(RaclAdapterRegErrorLogKnown_A, racl_error_log_o)
 
 endmodule
