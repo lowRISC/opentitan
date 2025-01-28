89055bbe8dc472a5fce92eee72a68c331645ea1a
Alexander Williams <awill@opentitan.org>
2025-01-28 23:17:16 +0000

    [prim_xilinx,englishbreakfast] Remove ROM dependency
    Create prim_xilinx_rom that instantiates an xpm_memory_sprom when there
    is no memory initialization file provided, and ensure the macro is
    configured to remain after optimizations, so splicing is available.
    
    Change the englishbreakfast build to start with a ROM that is all zeroes
    and requires splicing to get a complete image.
    
    Signed-off-by: Alexander Williams <awill@opentitan.org>

--- hw/top_englishbreakfast/util/vivado_hook_opt_design_post.tcl
+++ hw/top_englishbreakfast/util/vivado_hook_opt_design_post.tcl
@@ -5,10 +5,7 @@
 # Hook to check BRAM implementation for Boot ROM. This is required for Boot ROM splicing.
 send_msg "Designcheck 2-1" INFO "Checking if Boot ROM is mapped to BRAM."
 
-if {[catch [get_cells -hierarchical -filter { NAME =~  "*u_rom_ctrl*u_rom*rdata_o_reg_0" && PRIMITIVE_TYPE =~ BMEM.*.* }]]\
- && [catch [get_cells -hierarchical -filter { NAME =~  "*u_rom_ctrl*u_rom*rdata_o_reg_1" && PRIMITIVE_TYPE =~ BMEM.*.* }]]\
- && [catch [get_cells -hierarchical -filter { NAME =~  "*u_rom_ctrl*u_rom*rdata_o_reg_2" && PRIMITIVE_TYPE =~ BMEM.*.* }]]\
- && [catch [get_cells -hierarchical -filter { NAME =~  "*u_rom_ctrl*u_rom*rdata_o_reg_3" && PRIMITIVE_TYPE =~ BMEM.*.* }]] } {
+if {[catch [get_cells -hierarchical -filter { NAME =~  "*u_rom_ctrl*u_rom*u_prim_rom*" && PRIMITIVE_TYPE =~ BMEM.*.* }]]} {
   send_msg "Designcheck 2-2" INFO "BRAM implementation found for Boot ROM."
 } else {
   send_msg "Designcheck 2-3" ERROR "BRAM implementation not found for Boot ROM."
