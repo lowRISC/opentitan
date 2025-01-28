ab02d87705c2db718ae12bb63c785ebeedf0a361
Robert Schilling <rschilling@rivosinc.com>
2025-01-28 09:20:20 -0800

    [membkdr] Provide a tile adapter to access internal rows
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip_templates/flash_ctrl/dv/env/flash_ctrl_bkdr_util.sv
+++ hw/ip_templates/flash_ctrl/dv/env/flash_ctrl_bkdr_util.sv
@@ -21,9 +21,10 @@ class flash_ctrl_bkdr_util extends mem_bkdr_util;
   // secded package.
   function new(string name = "", string path, int unsigned depth,
                longint unsigned n_bits, err_detection_e err_detection_scheme,
+               mem_bkdr_util_row_adapter row_adapter = null,
                int num_prince_rounds_half = 3,
                int extra_bits_per_subword = 0, int unsigned system_base_addr = 0);
-    super.new(name, path, depth, n_bits, err_detection_scheme, num_prince_rounds_half,
+    super.new(name, path, depth, n_bits, err_detection_scheme, row_adapter, num_prince_rounds_half,
               extra_bits_per_subword, system_base_addr);
     // Error detection scheme is assumed to be 76_68, otherwise, the overrides below won't work.
     `DV_CHECK_EQ_FATAL(err_detection_scheme, mem_bkdr_util_pkg::EccHamming_76_68)
