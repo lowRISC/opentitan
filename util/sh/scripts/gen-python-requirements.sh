998eeab7cb052441edd05cd3f48c0ac1d13c28f2
Tim Trippel <ttrippel@google.com>
2025-01-31 16:05:44 -0800

    [python] switch from pip-tools to uv
    This is a manual cherry-pick of 56766397c71221be93cf47524d031f8f27884737
    to the `earlgrey_1.0.0` branch. This was originally part of #24521.
    
    `uv pip compile` is compatible with `pip-tools`, but much faster.
    Switch to uv also make us no longer have to depend on `pip`, which is
    considered as an unsafe dependency by `pip-tools`.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- util/sh/scripts/gen-python-requirements.sh
+++ util/sh/scripts/gen-python-requirements.sh
@@ -12,7 +12,7 @@ PYTHON_REQS_OUT_FILE="$REPO_TOP/python-requirements.txt"
 
 # The below shellcode autogenerates the `python-requirements.txt`, with hashes,
 # and prepends the license and auto-generated banners at the top of the file.
-pip-compile \
+uv pip compile \
   --allow-unsafe \
   --generate-hashes \
   --no-annotate \
@@ -24,6 +24,3 @@ add_autogen_banner "$PYTHON_REQS_OUT_FILE" \
   "./util/sh/scripts/gen-python-requirements.sh"
 echo -e "#\n$(cat "$PYTHON_REQS_OUT_FILE")" > "$PYTHON_REQS_OUT_FILE"
 add_license_banner "$PYTHON_REQS_OUT_FILE"
-
-# opentitan.egg-info is generated as part of pip-compile. Clean it up.
-rm -rf $REPO_TOP/opentitan.egg-info
