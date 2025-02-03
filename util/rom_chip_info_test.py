eacff8c1af9e61c5b14dae44c16db1b3eb92c4c4
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-02-03 15:18:21 +0100

    [rom,chip_info] Mark `kChipInfo` as used so it is not discarded
    With LTO, the linker is more eager to remove it since it's not used
    anywhere.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- util/rom_chip_info_test.py
+++ util/rom_chip_info_test.py
@@ -27,6 +27,7 @@ class TestGenerateChipInfoCSource(unittest.TestCase):
 
 #include "sw/device/lib/base/macros.h"
 
+OT_USED
 OT_SECTION(".chip_info")
 const chip_info_t kChipInfo = {
   .scm_revision = (chip_info_scm_revision_t){
@@ -52,6 +53,7 @@ const chip_info_t kChipInfo = {
 
 #include "sw/device/lib/base/macros.h"
 
+OT_USED
 OT_SECTION(".chip_info")
 const chip_info_t kChipInfo = {
   .scm_revision = (chip_info_scm_revision_t){
