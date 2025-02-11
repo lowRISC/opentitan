ef6e1f7825b7a1b5a00662ec1bcd3981347d5b72
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-02-11 15:23:05 +0100

    [dtgen] Add support for extension
    Enhance `dtgen` to accept a new `--extension` argument to point
    to a python plugin. This plugin can be used to customize the DT
    in a controlled way by adding some fields and methods. The extension
    can be set through the HW desc 'extension' field (per-IP).
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- util/dttool.py
+++ util/dttool.py
@@ -7,15 +7,66 @@
 import argparse
 import logging
 import hjson
+import inspect
 from mako.template import Template
 from mako import exceptions
 from pathlib import Path
+from importlib.util import module_from_spec, spec_from_file_location
 
 from reggen.ip_block import IpBlock
-from dtgen.helper import TopHelper, IpHelper
+from dtgen.helper import TopHelper, IpHelper, Extension
 from topgen.lib import CArrayMapping, CEnum
 
 TOPGEN_TEMPLATE_PATH = Path(__file__).parents[1].resolve() / "util" / "dtgen"
+
+
+def load_extension(plugin_path: Path):
+    """Load an extension from a file."""
+    if not plugin_path.exists():
+        logging.error(f"Plugin file not found: {plugin_path}")
+        return None
+
+    spec = spec_from_file_location(name="dt_ext", location=plugin_path)
+
+    if spec is None:
+        logging.error(f"Module spec could not be loaded from {plugin_path}")
+        return None
+
+    if spec.loader is None:
+        logging.error(f"Module spec does not have a loader for {plugin_path}")
+        return None
+
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    return module
+
+
+def find_extension_class(ext_mod, cls):
+    if ext_mod is None:
+        return None
+
+    # Look for a class extended `cls`
+    ext_cls = None
+    for (name, obj) in inspect.getmembers(ext_mod):
+        if not inspect.isclass(obj):
+            continue
+        # Only consider classes defined in this module.
+        if obj.__module__ != ext_mod.__name__:
+            continue
+        if not issubclass(obj, cls):
+            continue
+        # Found one.
+        if ext_cls is not None:
+            logging.error(f"extension defines several extension classes: {ext_cls} and {obj}")
+            raise RuntimeError("invalid extension")
+        ext_cls = obj
+
+    if ext_cls is None:
+        logging.error("extension does not define any extension class")
+        raise RuntimeError("invalid extension")
+
+    return ext_cls
 
 
 def render_template(template_path: Path, rendered_path: Path,
@@ -82,6 +133,11 @@ def main():
         default = None,
         help="Specify the default register block node (for --gen-ip)"
     )
+    parser.add_argument(
+        "--extension",
+        type=Path,
+        help="Path to a DT extension (a python file)",
+    )
 
     args = parser.parse_args()
     outdir = Path(args.outdir) / "dt"
@@ -98,6 +154,13 @@ def main():
 
     with open(args.topgencfg) as handle:
         topcfg = hjson.load(handle, use_decimal=True)
+
+    if args.extension:
+        ext_mod = load_extension(args.extension)
+        if ext_mod is None:
+            raise RuntimeError("Could not load extension")
+    else:
+        ext_mod = None
 
     top_helper = TopHelper(topcfg, CEnum, CArrayMapping)
 
@@ -126,10 +189,13 @@ def main():
                     logging.warning(f"IP {ipname} has more than one register block node " +
                                     f"but no default was specified, will use {default_node}")
 
+            extension_cls = find_extension_class(ext_mod, Extension)
+
             # The instance name is 'top_{topname}_{ipname}'.
             ipconfig = name_to_ipconfig.get('top_{}_{}'.format(topcfg["name"], ipname), None)
 
-            helper = IpHelper(top_helper, ip, ipconfig, default_node, CEnum, CArrayMapping)
+            helper = IpHelper(top_helper, ip, ipconfig, default_node, CEnum, CArrayMapping,
+                              extension_cls)
 
             render_template(
                 TOPGEN_TEMPLATE_PATH / "dt_ip.h.tpl",
