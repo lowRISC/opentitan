de49430ea0da20c3fd433fc153b0d6dcfeb28054
Alex Jones <alex.jones@lowrisc.org>
2025-02-11 16:53:44 +0000

    [bazel] Add Darjeeling DT clock speeds
    Adds a basic DT definition for Darjeeling's clock speeds in the `sim_dv`
    environment, which are just the default clock speeds targeted by the
    Darjeeling top. A small amount of supporting infrastructure is added
    alongside this as this is the first dt clock definition to be made for
    Darjeeling.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

---
+++ hw/top_darjeeling/sw/dt/sim_dv.c
@@ -0,0 +1,23 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdint.h>
+
+#include "dt/dt_api.h"  // Generated
+
+static const uint32_t clock_freqs[kDtClockCount] = {
+    [kDtClockMain] = 1 * 1000 * 1000 * 1000,
+    [kDtClockIo] = 1 * 1000 * 1000 * 1000,
+    [kDtClockUsb] = 1 * 1000 * 1000 * 1000,
+    [kDtClockAon] = 625 * 100 * 1000,
+    [kDtClockIoDiv2] = 500 * 1000 * 1000,
+    [kDtClockIoDiv4] = 250 * 1000 * 1000,
+};
+
+uint32_t dt_clock_frequency(dt_clock_t clk) {
+  if (clk < kDtClockCount) {
+    return clock_freqs[clk];
+  }
+  return 0;
+}
