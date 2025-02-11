a0272ea60e69bc7ab79b071e46f4b957d3822a1a
Chris Frantz <cfrantz@google.com>
2025-02-11 13:53:32 -0800

    [testing] Add a test for parallel ECDSA/SPX verify
    Signed-off-by: Chris Frantz <cfrantz@google.com>

---
+++ sw/device/tests/sigverify_parallel_digest.h
@@ -0,0 +1,14 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_TESTS_SIGVERIFY_PARALLEL_DIGEST_H_
+#define OPENTITAN_SW_DEVICE_TESTS_SIGVERIFY_PARALLEL_DIGEST_H_
+
+// clang-format off
+const unsigned char sigverify_parallel_digest[] = {
+0xcd,0xde,0x27,0xe5,0xac,0x5c,0x8b,0xf1,0xc5,0x92,0xd4,0x22,0x94,0xe2,0x05,0xac, // 00000000 |..'..\....."....|
+0x3d,0xbb,0xbd,0x64,0xf6,0xf1,0x3c,0x8c,0x4c,0x38,0x95,0x81,0xc3,0xb4,0xbe,0xf5, // 00000010 |=..d..<.L8......|
+};
+// clang-format on
+#endif  // OPENTITAN_SW_DEVICE_TESTS_SIGVERIFY_PARALLEL_DIGEST_H_
