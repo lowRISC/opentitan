a67f611d12bec69cef0757118fc08ba8a55ff266
Jade Philipoom <jadep@zerorisc.com>
2025-02-13 10:48:56 +0100

    [crypto] Split SHA-2 and SHA-3 APIs.
    Code is only moved in this commit, not changed.
    
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>
    (cherry picked from commit c8c012d1c29063eadf47ffeecfe35bac14cb5007)

--- sw/device/lib/crypto/include/sha2.h
+++ sw/device/lib/crypto/include/sha2.h
@@ -70,46 +70,6 @@ otcrypto_status_t otcrypto_hash(otcrypto_const_byte_buf_t input_message,
                                 otcrypto_hash_digest_t digest);
 
 /**
- * Performs the SHAKE extendable output function (XOF) on input data.
- *
- * The caller should allocate space for the `digest` buffer and set the `mode`
- * and `len` fields.  The `mode` parameter must be
- * `kOtcryptoHashXofModeShake128` or `kOtcryptoHashXofModeShake256`; other
- * values will result in errors.
- *
- * @param input_message Input message for extendable output function.
- * @param[out] digest Output from the extendable output function.
- * @return Result of the xof operation.
- */
-otcrypto_status_t otcrypto_xof_shake(otcrypto_const_byte_buf_t input_message,
-                                     otcrypto_hash_digest_t digest);
-
-/**
- * Performs the CSHAKE extendable output function (XOF) on input data.
- *
- * The `function_name_string` is used by NIST to define functions based on
- * cSHAKE. When no function other than cSHAKE is desired; it can be empty. The
- * `customization_string` is used to define a variant of the cSHAKE function.
- * If no customization is desired it can be empty.
- *
- * The caller should allocate space for the `digest` buffer and set the `mode`
- * and `len` fields. The `mode` parameter must be
- * `kOtcryptoHashXofModeCshake128` or `kOtcryptoHashXofModeCshake256`; other
- * values will result in errors.
- *
- * @param input_message Input message for extendable output function.
- * @param function_name_string NIST Function name string.
- * @param customization_string Customization string for cSHAKE.
- * @param[out] digest Output from the extendable output function.
- * @return Result of the xof operation.
- */
-otcrypto_status_t otcrypto_xof_cshake(
-    otcrypto_const_byte_buf_t input_message,
-    otcrypto_const_byte_buf_t function_name_string,
-    otcrypto_const_byte_buf_t customization_string,
-    otcrypto_hash_digest_t digest);
-
-/**
  * Performs the INIT operation for a cryptographic hash function.
  *
  * Initializes the generic hash context. The required hash mode is selected
