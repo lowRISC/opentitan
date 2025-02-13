b1d4af429251ea39aed94837fa8d32782a2915d5
Martin Velay <mvelay@lowrisc.org>
2025-02-13 10:21:40 +0000

    [acrc, dv] Contract 'agent' instances into 'agt'
    - only to have shorter names
    /!\ The generated files are not functional yet, it doesn't even compile
    
    Signed-off-by: Martin Velay <mvelay@lowrisc.org>

--- hw/top_darjeeling/ip_autogen/ac_range_check/dv/tb/tb.sv
+++ hw/top_darjeeling/ip_autogen/ac_range_check/dv/tb/tb.sv
@@ -65,9 +65,9 @@ module tb;
     clk_rst_if.set_active();
     uvm_config_db#(virtual clk_rst_if)::set(null, "*.env", "clk_rst_vif", clk_rst_if);
     uvm_config_db#(intr_vif)::set(null, "*.env", "intr_vif", intr_if);
-    uvm_config_db#(virtual tl_if)::set(null, "*.env.tl_csr_agent*", "tl_csr_vif", tl_csr_if);
-    uvm_config_db#(virtual tl_if)::set(null, "*.env.tl_unfilt_agent*", "tl_unfilt_vif", tl_unfilt_if);
-    uvm_config_db#(virtual tl_if)::set(null, "*.env.tl_filt_agent*", "tl_filt_vif", tl_filt_if);
+    uvm_config_db#(virtual tl_if)::set(null, "*.env.tl_csr_agt*", "tl_csr_vif", tl_csr_if);
+    uvm_config_db#(virtual tl_if)::set(null, "*.env.tl_unfilt_agt*", "tl_unfilt_vif", tl_unfilt_if);
+    uvm_config_db#(virtual tl_if)::set(null, "*.env.tl_filt_agt*", "tl_filt_vif", tl_filt_if);
     $timeformat(-12, 0, " ps", 12);
     run_test();
   end
