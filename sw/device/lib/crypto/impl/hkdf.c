e8f3c85ceef4a3a6144b80f6d8fc9a7a11ff92a1
Jade Philipoom <jadep@zerorisc.com>
2025-02-13 10:51:17 +0100

    [crypto] Clean up after splitting SHA-2 and SHA-3.
    Also split API calls by digest length and revise the driver to remove internal
    switch statements and remove some exposed constants that do not match other
    interfaces.
    
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>

--- sw/device/lib/crypto/impl/hkdf.c
+++ sw/device/lib/crypto/impl/hkdf.c
@@ -9,8 +9,8 @@
 #include "sw/device/lib/crypto/impl/keyblob.h"
 #include "sw/device/lib/crypto/impl/status.h"
 #include "sw/device/lib/crypto/include/datatypes.h"
+#include "sw/device/lib/crypto/include/hkdf.h"
 #include "sw/device/lib/crypto/include/hmac.h"
-#include "sw/device/lib/crypto/include/kdf.h"
 
 // Module ID for status codes.
 #define MODULE_ID MAKE_MODULE_ID('h', 'k', 'd')
