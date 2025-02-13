c73c425fce6f641454931723a95cc279dccea4c5
Robert Schilling <rschilling@rivosinc.com>
2025-02-13 14:53:02 +0100

    [darjeeling] Move mbx6 to 0x1496000
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/top_darjeeling/sw/autogen/top_darjeeling_soc_mbx.h
+++ hw/top_darjeeling/sw/autogen/top_darjeeling_soc_mbx.h
@@ -140,7 +140,7 @@ extern "C" {
  * This should be used with #mmio_region_from_addr to access the memory-mapped
  * registers associated with the peripheral (usually via a DIF).
  */
-#define TOP_DARJEELING_SOC_MBX_MBX6_SOC_BASE_ADDR 0x1465600u
+#define TOP_DARJEELING_SOC_MBX_MBX6_SOC_BASE_ADDR 0x1496000u
 
 /**
  * Peripheral size for soc device on mbx6 in top darjeeling.
