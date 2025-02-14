c59e1ac55f4491127abc85bf6c2066f2af6c2d67
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-02-14 13:57:37 +0000

    [pattgen,dv] Use extern definitions in pattgen_env_cfg
    No functional change, but this adds some documentation comments and
    also sets list_of_alerts a bit earlier (in the constructor instead of
    initialize()).
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/pattgen/dv/env/pattgen_env_cfg.sv
+++ hw/ip/pattgen/dv/env/pattgen_env_cfg.sv
@@ -3,33 +3,39 @@
 // SPDX-License-Identifier: Apache-2.0
 
 class pattgen_env_cfg extends cip_base_env_cfg #(.RAL_T(pattgen_reg_block));
-  // drained time of phase_ready_to_end
+  // Drain time of phase_ready_to_end
   uint ok_to_end_delay_ns = 8000;
 
-  // pattgen_agent_cfg
+  // Configuration for the pattgen agent (stored in the environment as m_pattgen_agent).
   rand pattgen_agent_cfg m_pattgen_agent_cfg;
 
-  // seq cfg
+  // Configuration that applies to the virtual sequences that run in the environment
   pattgen_seq_cfg seq_cfg;
+
+  extern function new (string name="");
+
+  // Implements a function from dv_base_env_cfg. The base class version creates RAL models. This
+  // extension uses the type of the RAL model to set num_interrupts.
+  extern virtual function void initialize(bit [TL_AW-1:0] csr_base_addr = '1);
 
   `uvm_object_utils_begin(pattgen_env_cfg)
     `uvm_field_object(m_pattgen_agent_cfg, UVM_DEFAULT)
   `uvm_object_utils_end
+endclass
 
-  `uvm_object_new
+function pattgen_env_cfg::new (string name="");
+  super.new(name);
 
-  virtual function void initialize(bit [TL_AW-1:0] csr_base_addr = '1);
-    list_of_alerts = pattgen_env_pkg::LIST_OF_ALERTS;
-    super.initialize(csr_base_addr);
+  list_of_alerts = pattgen_env_pkg::LIST_OF_ALERTS;
 
-    // create pattgen_agent_cfg
-    m_pattgen_agent_cfg = pattgen_agent_cfg::type_id::create("m_pattgen_agent_cfg");
-    m_pattgen_agent_cfg.if_mode = Device; // setup agent in Device mode
+  m_pattgen_agent_cfg = pattgen_agent_cfg::type_id::create("m_pattgen_agent_cfg");
+  m_pattgen_agent_cfg.if_mode = Device; // setup agent in Device mode
 
-    // create the seq_cfg
-    seq_cfg = pattgen_seq_cfg::type_id::create("seq_cfg");
+  seq_cfg = pattgen_seq_cfg::type_id::create("seq_cfg");
+endfunction
 
-    // set num_interrupts & num_alerts
-    num_interrupts = ral.intr_state.get_n_used_bits();
-  endfunction
-endclass : pattgen_env_cfg
+function void pattgen_env_cfg::initialize(bit [TL_AW-1:0] csr_base_addr = '1);
+  super.initialize(csr_base_addr);
+
+  num_interrupts = ral.intr_state.get_n_used_bits();
+endfunction
