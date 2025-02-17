39571890207f9a1d26324222b993748dbf38e377
Miguel Osorio <miguelosorio@google.com>
2025-02-16 21:05:26 -0800

    [hsmtool] Add AES generate, import, export
    Add support for generating, importing and exporting AES keys. AES keys
    are required to support key wrap/unwrap flows.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    (cherry picked from commit e94777854c1cd6098c259b7d9b6446fc3b84db31)

---
+++ sw/host/hsmtool/src/commands/aes/export.rs
@@ -0,0 +1,56 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::Result;
+use cryptoki::session::Session;
+use serde::{Deserialize, Serialize};
+use std::any::Any;
+use std::path::PathBuf;
+
+use crate::commands::{BasicResult, Dispatch};
+use crate::error::HsmError;
+use crate::module::Module;
+use crate::util::helper;
+use crate::util::secret::Secret;
+use crate::util::wrap::Wrap;
+
+#[derive(clap::Args, Debug, Serialize, Deserialize)]
+pub struct Export {
+    #[arg(long)]
+    id: Option<String>,
+    #[arg(short, long)]
+    label: Option<String>,
+    /// Wrap the exported key using an RSA wrapping key.
+    #[arg(long)]
+    wrap: Option<String>,
+    #[arg(long, default_value = "rsa-pkcs-oaep", help=Wrap::HELP)]
+    wrap_mechanism: Wrap,
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
+#[typetag::serde(name = "aes-export")]
+impl Dispatch for Export {
+    fn run(
+        &self,
+        _context: &dyn Any,
+        _hsm: &Module,
+        session: Option<&Session>,
+    ) -> Result<Box<dyn erased_serde::Serialize>> {
+        let session = session.ok_or(HsmError::SessionRequired)?;
+        let attrs = helper::search_spec(self.id.as_deref(), self.label.as_deref())?;
+        let object = helper::find_one_object(session, attrs.as_slice())?;
+
+        let secret = Secret::Aes;
+        let key = if self.wrap.is_some() {
+            secret.wrap_key(session, object, self.wrap.as_deref(), &self.wrap_mechanism)?
+        } else {
+            secret.export(session, object)?
+        };
+        if let Some(output) = &self.output {
+            std::fs::write(output, key.as_slice())?;
+        }
+        Ok(Box::<BasicResult>::default())
+    }
+}
