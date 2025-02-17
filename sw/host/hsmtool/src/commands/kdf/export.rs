0c3875f6b9bcbe364177d8e222e232dbbdfad477
Miguel Osorio <miguelosorio@google.com>
2025-02-16 21:07:05 -0800

    [hsmtool] Add support for `kdf-generate`.
    This change introduces the `kdf generate` command, which can be used to
    generate kdf secrets of generic type.
    
    This change also refactors the `kdf` commands to adopt the functionality
    provided by the `hsmtool::util::secret` package.
    
    Signed-off-by: Miguel Osorio <miguelosorio@google.com>
    (cherry picked from commit 79191f94c7f042ce9c7e8976c619e28e7c2e9a29)

--- sw/host/hsmtool/src/commands/kdf/export.rs
+++ sw/host/hsmtool/src/commands/kdf/export.rs
@@ -14,8 +14,9 @@ use serde::{Deserialize, Serialize};
 use crate::commands::{BasicResult, Dispatch};
 use crate::error::HsmError;
 use crate::module::Module;
-use crate::util::attribute::{AttributeMap, AttributeType, KeyType, ObjectClass};
 use crate::util::helper;
+use crate::util::secret::Secret;
+use crate::util::wrap::Wrap;
 
 #[derive(clap::Args, Debug, Serialize, Deserialize)]
 pub struct Export {
@@ -26,61 +27,10 @@ pub struct Export {
     /// Wrap the exported key using a wrapping key.
     #[arg(long)]
     wrap: Option<String>,
+    #[arg(long, default_value = "rsa-pkcs-oaep", help=Wrap::HELP)]
+    wrap_mechanism: Wrap,
     #[arg(short, long)]
     output: Option<PathBuf>,
-}
-
-impl Export {
-    fn export(&self, session: &Session, object: ObjectHandle) -> Result<()> {
-        let map = AttributeMap::from_object(session, object)?;
-
-        let class: ObjectClass = map
-            .get(&AttributeType::Class)
-            .ok_or_else(|| HsmError::KeyError("Key does not have a class attribute".into()))?
-            .try_into()
-            .map_err(HsmError::AttributeError)?;
-        let key_type: KeyType = map
-            .get(&AttributeType::KeyType)
-            .ok_or_else(|| HsmError::KeyError("Key does not have a key type attribute".into()))?
-            .try_into()
-            .map_err(HsmError::AttributeError)?;
-        if class != ObjectClass::SecretKey || key_type != KeyType::GenericSecret {
-            Err(HsmError::KeyError("Key is not a secret key".into()))?;
-        }
-
-        let sensitive: bool = map
-            .get(&AttributeType::Sensitive)
-            .ok_or_else(|| HsmError::KeyError("Key does not have a sensitive attribute".into()))?
-            .try_into()
-            .map_err(HsmError::AttributeError)?;
-        if sensitive {
-            Err(HsmError::KeyError("Key is marked as sensitive".into()))?;
-        }
-
-        let extractable: bool = map
-            .get(&AttributeType::Extractable)
-            .ok_or_else(|| HsmError::KeyError("Key does not have an extractable attribute".into()))?
-            .try_into()
-            .map_err(HsmError::AttributeError)?;
-        if !extractable {
-            Err(HsmError::KeyError("Key is not extractable".into()))?;
-        }
-
-        let key: Vec<u8> = map
-            .get(&AttributeType::Value)
-            .ok_or_else(|| HsmError::KeyError("Key does not have a value attribute".into()))?
-            .try_into()
-            .map_err(HsmError::AttributeError)?;
-
-        if let Some(output) = &self.output {
-            fs::write(output, &key)?;
-        }
-        Ok(())
-    }
-
-    fn wrap_key(&self, _session: &Session, _object: ObjectHandle) -> Result<()> {
-        bail!("Kdf key export by wrapping is not supported yet!");
-    }
 }
 
 #[typetag::serde(name = "kdf-export")]
@@ -94,10 +44,15 @@ impl Dispatch for Export {
         let session = session.ok_or(HsmError::SessionRequired)?;
         let attrs = helper::search_spec(self.id.as_deref(), self.label.as_deref())?;
         let object = helper::find_one_object(session, attrs.as_slice())?;
-        if self.wrap.is_some() {
-            self.wrap_key(session, object)?;
+
+        let secret = Secret::GenericSecret;
+        let key = if self.wrap.is_some() {
+            secret.wrap_key(session, object, self.wrap.as_deref(), &self.wrap_mechanism)?
         } else {
-            self.export(session, object)?;
+            secret.export(session, object)?
+        };
+        if let Some(output) = &self.output {
+            fs::write(output, key.as_slice())?;
         }
         Ok(Box::<BasicResult>::default())
     }
