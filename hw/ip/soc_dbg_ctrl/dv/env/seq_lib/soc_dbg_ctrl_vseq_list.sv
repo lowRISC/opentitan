30c144f6ba8680e5b8a54dbd5449e192bcbf42d6
Martin Velay <mvelay@lowrisc.org>
2025-02-19 15:37:10 +0000

    [soc_dbg_ctrl,dv] Create initial DV environment
    - Use the following uvmdvgen command:
      `util/uvmdvgen/uvmdvgen.py soc_dbg_ctrl -e -c -hr -ha -eo
       hw/ip/soc_dbg_ctrl`
    - Adjust the generated files
    - Instanciate the DUT into the TB top module
    - Add 2nd TL agent for JTAG register access
    
    Signed-off-by: Martin Velay <mvelay@lowrisc.org>

---
+++ hw/ip/soc_dbg_ctrl/dv/env/seq_lib/soc_dbg_ctrl_vseq_list.sv
@@ -0,0 +1,7 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+`include "soc_dbg_ctrl_base_vseq.sv"
+`include "soc_dbg_ctrl_smoke_vseq.sv"
+`include "soc_dbg_ctrl_common_vseq.sv"
---
+++ hw/ip/soc_dbg_ctrl/dv/env/soc_dbg_ctrl_env_cfg.sv
@@ -0,0 +1,30 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class soc_dbg_ctrl_env_cfg extends cip_base_env_cfg #(.RAL_T(soc_dbg_ctrl_core_reg_block));
+  `uvm_object_utils(soc_dbg_ctrl_env_cfg)
+
+  string jtag_ral_name = "soc_dbg_ctrl_jtag_reg_block";
+
+  // Standard SV/UVM methods
+  extern function new(string name="");
+
+  // Class specific methods
+  extern function void initialize(bit [31:0] csr_base_addr = '1);
+endclass : soc_dbg_ctrl_env_cfg
+
+
+function soc_dbg_ctrl_env_cfg::new(string name="");
+  super.new(name);
+endfunction : new
+
+function void soc_dbg_ctrl_env_cfg::initialize(bit [31:0] csr_base_addr = '1);
+  list_of_alerts = soc_dbg_ctrl_env_pkg::LIST_OF_ALERTS;
+
+  // Set up second RAL model for JTAG registers
+  ral_model_names.push_back(jtag_ral_name);
+  clk_freqs_mhz[jtag_ral_name] = clk_freq_mhz;
+
+  super.initialize(csr_base_addr);
+endfunction : initialize
