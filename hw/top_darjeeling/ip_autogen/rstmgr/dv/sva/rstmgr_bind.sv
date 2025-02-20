7c0f7a81390a9faf0badcda4d16761e52b03ab0f
Guillermo Maturana <maturana@opentitan.org>
2025-02-20 16:56:09 +0000

    [ipgen,rstmgr] Make src clocks configurable
    This is to support tops that have different source clocks,
    like no USB source clock for darjeeling.
    Extend the support for ipgen deeper into rstmgr DV.
    - Generate the LIST_OF_LEAFS and LIST_OF_SHADOW_LEAFS
    - Add frequency info to the clks parameters to generate
      frequencies
    - Add clk to the sw_rst targets to generate bind of
      rstmgr_sw_rst_sva_if
    Specifically generate rstmgr tb.sv.
    
    Fixes #25321
    Fixes #19538
    
    Signed-off-by: Guillermo Maturana <maturana@opentitan.org>

--- hw/top_darjeeling/ip_autogen/rstmgr/dv/sva/rstmgr_bind.sv
+++ hw/top_darjeeling/ip_autogen/rstmgr/dv/sva/rstmgr_bind.sv
@@ -16,11 +16,10 @@ module rstmgr_bind;
   bind rstmgr rstmgr_cascading_sva_if rstmgr_cascading_sva_if (
     .clk_i,
     .clk_aon_i,
-    .clk_io_div4_i,
-    .clk_io_div2_i,
     .clk_io_i,
+    .clk_io_div2_i,
+    .clk_io_div4_i,
     .clk_main_i,
-    .clk_usb_i,
     .por_n_i,
     .scan_rst_ni,
     .scanmode_i,
@@ -58,33 +57,18 @@ module rstmgr_bind;
     .clk_i({
       clk_io_div4_i,
       clk_io_div4_i,
-      clk_io_div4_i,
-      clk_aon_i,
-      clk_usb_i,
-      clk_io_div2_i,
-      clk_io_i,
       clk_io_div4_i
     }),
     .rst_ni,
     .parent_rst_n(rst_sys_src_n[1]),
     .ctrl_ns(reg2hw.sw_rst_ctrl_n),
     .rst_ens({
-      rst_en_o.i2c2[1] == prim_mubi_pkg::MuBi4True,
-      rst_en_o.i2c1[1] == prim_mubi_pkg::MuBi4True,
       rst_en_o.i2c0[1] == prim_mubi_pkg::MuBi4True,
-      rst_en_o.usb_aon[1] == prim_mubi_pkg::MuBi4True,
-      rst_en_o.usb[1] == prim_mubi_pkg::MuBi4True,
-      rst_en_o.spi_host1[1] == prim_mubi_pkg::MuBi4True,
       rst_en_o.spi_host0[1] == prim_mubi_pkg::MuBi4True,
       rst_en_o.spi_device[1] == prim_mubi_pkg::MuBi4True
     }),
     .rst_ns({
-      resets_o.rst_i2c2_n[1],
-      resets_o.rst_i2c1_n[1],
       resets_o.rst_i2c0_n[1],
-      resets_o.rst_usb_aon_n[1],
-      resets_o.rst_usb_n[1],
-      resets_o.rst_spi_host1_n[1],
       resets_o.rst_spi_host0_n[1],
       resets_o.rst_spi_device_n[1]
     })
@@ -93,12 +77,11 @@ module rstmgr_bind;
   bind rstmgr rstmgr_rst_en_track_sva_if rstmgr_rst_en_track_sva_if (
     .resets_i(resets_o),
     .reset_en_i(rst_en_o),
-    .clk_aon_i(clk_aon_i),
-    .clk_io_div4_i(clk_io_div4_i),
-    .clk_main_i(clk_main_i),
-    .clk_io_i(clk_io_i),
-    .clk_io_div2_i(clk_io_div2_i),
-    .clk_usb_i(clk_usb_i),
+    .clk_aon_i,
+    .clk_io_i,
+    .clk_io_div2_i,
+    .clk_io_div4_i,
+    .clk_main_i,
     .rst_por_ni(rst_por_ni)
   );
 
