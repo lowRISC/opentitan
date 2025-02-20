9908a5cbd6f8c4c1388a1f43c1d1779f64c7e1aa
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-02-20 15:01:42 +0100

    Partiall revert "[sw/device] Delete math_builtins defined with UB"
    This partiall yreverts commit eb69eedf60fc5fdacf3ccfbb31a3e3027e59b1b0,
    restoring the implementation of the shifts plus a tiny tweak to avoid
    a compiler warning.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/lib/base/math_builtins.c
+++ sw/device/lib/base/math_builtins.c
@@ -32,8 +32,121 @@
 
 #include "sw/device/lib/base/macros.h"
 
+/**
+ * Helper type for fussing with the upper and lower halves of an i64.
+ *
+ * This is necessary to avoid accidentally calling into the shift polyfills
+ * inside of their implementations.
+ */
+typedef union u32x2 {
+  struct {
+    uint32_t lo;
+    union {
+      uint32_t hi;
+      int32_t hi_signed;
+    };
+  };
+  int64_t full;
+} u32x2_t;
+
+int64_t _ot_builtin_lshift_i64(int64_t val, int32_t shift) {
+  if (shift == 0) {
+    return val;
+  }
+
+  u32x2_t word = {.full = val};
+
+  // When doing a big shift, we have
+  //
+  // aaaa'aaaa'bbbb'bbbb
+  // bbbb'bb00'0000'0000
+  if (shift >= 32) {
+    word.hi = word.lo << (shift - 32);
+    word.lo = 0;
+    return word.full;
+  }
+
+  // When doing a small shift, we have
+  //
+  // aaaa'aaaa'bbbb'bbbb
+  // aaaa'aabb'bbbb'bb00
+  word.hi <<= shift;
+  word.hi |= word.lo >> (32 - shift);
+  word.lo <<= shift;
+  return word.full;
+}
+
+int64_t _ot_builtin_rshift_i64(int64_t val, int32_t shift) {
+  if (shift == 0) {
+    return val;
+  }
+
+  u32x2_t word = {.full = val};
+
+  // When doing a big shift, we have
+  //
+  // aaaa'aaaa'bbbb'bbbb
+  // 0000'0000'00aa'aaaa
+  if (shift >= 32) {
+    word.lo = word.hi >> (shift - 32);
+    word.hi = 0;
+    return word.full;
+  }
+
+  // When doing a small shift, we have
+  //
+  // aaaa'aaaa'bbbb'bbbb
+  // 00aa'aaaa'aabb'bbbb
+  word.lo >>= shift;
+  word.lo |= word.hi << (32 - shift);
+  word.hi >>= shift;
+
+  return word.full;
+}
+
+int64_t _ot_builtin_ashift_i64(int64_t val, int32_t shift) {
+  if (shift == 0) {
+    return val;
+  }
+
+  u32x2_t word = {.full = val};
+
+  // When doing a big shift, we have
+  //
+  // aaaa'aaaa'bbbb'bbbb
+  // ssss'ssss'ssaa'aaaa
+  //
+  // where s is sign bits.
+  if (shift >= 32) {
+    word.lo = (uint32_t)(word.hi_signed >> (shift - 32));
+    word.hi_signed >>= 31;
+    return word.full;
+  }
+
+  // When doing a small shift, we have
+  //
+  // aaaa'aaaa'bbbb'bbbb
+  // ssaa'aaaa'aabb'bbbb
+  //
+  // where s is sign bits.
+  word.lo >>= shift;
+  word.lo |= word.hi << (32 - shift);
+  word.hi_signed >>= shift;
+
+  return word.full;
+}
+
 // Linker aliases for libgcc symbols.
 #ifdef OT_PLATFORM_RV32
+OT_WEAK
+OT_ALIAS("_ot_builtin_lshift_i64")
+int64_t __ashldi3(int64_t val, int32_t shift);
+OT_WEAK
+OT_ALIAS("_ot_builtin_rshift_i64")
+int64_t __lshrdi3(int64_t val, int32_t shift);
+OT_WEAK
+OT_ALIAS("_ot_builtin_ashift_i64")
+int64_t __ashrdi3(int64_t val, int32_t shift);
 
 extern noreturn void
 _ot_builtin_div64_intentionally_not_implemented_see_pull_11451(void);
