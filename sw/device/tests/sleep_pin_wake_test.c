7714a043ef4d6e7bba8bb73512fa3854a87be7db
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-02-21 16:13:44 +0000

    [sw,tests] Mark several variables as used
    With LTO, they are optimized away but the test harness is expecting
    to find a symbol for them.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/tests/sleep_pin_wake_test.c
+++ sw/device/tests/sleep_pin_wake_test.c
@@ -51,9 +51,12 @@ static pwrmgr_isr_ctx_t pwrmgr_isr_ctx = {
 // Preserve wakeup_detector_selected over multiple resets
 OT_SET_BSS_SECTION(".non_volatile_scratch", uint32_t wakeup_detector_idx;)
 
+OT_USED
 OT_SECTION(".data") int8_t sival_mio_pad = -1;  // -1 means not assigned yet.
+OT_USED
 OT_SECTION(".data")
 int8_t sival_wakeup_detector_idx = -1;  // -1 means not assigned yet.
+OT_USED
 OT_SECTION(".data") bool sival_ready_to_sleep = 0;
 
 /**
