b6ee7f7258c41ea6c4532b5ceb3e0a955a7ae692
Marcelo Carvalho Faleiro de Almeida <marcelo.carvalho@lowrisc.org>
2025-02-26 17:34:59 +0000

    Included functions to dv_base_virtual_sequencer
    Functions register/load to create a specialized sequencer
    to run a sub-sequence into the main virtual sequences.
    
    Signed-off-by: Marcelo Carvalho Faleiro de Almeida <marcelo.carvalho@lowrisc.org>

--- hw/dv/sv/dv_lib/dv_base_virtual_sequencer.sv
+++ hw/dv/sv/dv_lib/dv_base_virtual_sequencer.sv
@@ -9,6 +9,24 @@ class dv_base_virtual_sequencer #(type CFG_T = dv_base_env_cfg,
   CFG_T         cfg;
   COV_T         cov;
 
-  `uvm_component_new
+  function new(string name = "dv_base_virtual_sequencer", uvm_component parent);
+    super.new(name, parent);
+  endfunction
 
+  // Dynamic associative array to store sub-sequencers
+  uvm_sequencer_base sub_sequencers[string];
+
+  // Method to dynamically register a sub-sequencer
+  function void register_sequencer(string name, uvm_sequencer_base sequencer);
+    // Check if the sub_sequencer name does not exists before register it.
+    `DV_CHECK_FATAL(!sub_sequencers.exists(name))
+    sub_sequencers[name] = sequencer;
+  endfunction
+
+  // Method to retrieve a sequencer by name
+  function uvm_sequencer_base get_sequencer(string name);
+    // Check if the sub_sequencer name is registered before return it.
+    `DV_CHECK_FATAL(sub_sequencers.exists(name))
+    return sub_sequencers[name];
+  endfunction
 endclass
