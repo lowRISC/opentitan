64e9ed49da9c1a84853ac3013f0ef3591418c9c6
Chris Frantz <cfrantz@google.com>
2025-03-08 18:08:31 -0800

    [ownership] Create fake SPX keys for testing
    1. Create SPX keys for owner, unlock and activate for both the `fake`
       and `dummy` owner.
    2. Create a test_owner configuration that includes the SPX keys in a
       hybrid key mode.
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>
    (cherry picked from commit 421664873c1d5d0feb4861b388356966b58f987a)

---
+++ sw/device/silicon_creator/lib/ownership/keys/fake/owner_spx.h
@@ -0,0 +1,22 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_OWNER_SPX_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_OWNER_SPX_H_
+
+#define OWNER_SPX \
+  {               \
+    .data = {     \
+      2060225938, \
+      2906778233, \
+      1213194299, \
+      3760349346, \
+      3237819324, \
+      3174437600, \
+      577795037,  \
+      2762028488  \
+    }             \
+  }
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_OWNERSHIP_KEYS_FAKE_OWNER_SPX_H_
