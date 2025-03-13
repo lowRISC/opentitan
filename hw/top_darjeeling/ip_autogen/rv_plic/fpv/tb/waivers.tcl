5d07801299872369d05c161e20ef2c3df7bca4b2
Kinza Qamar <kqzaman@lowrisc.org>
2025-03-13 10:44:57 +0000

    [rv_plic, fpv] Added an assertion for err_o precondition
    The assertion FPVSecCmRegWeOnehotCheck in rv_plic.sv has a precondition
    which requires err_o in u_reg.*.u_prim_onehot_check true. But this is
    not possible. To know the reasoning refer waivers.tcl in
    hw/ip_templates/rv_plic/fpv/tb.
    
    Signed-off-by: Kinza Qamar <kqzaman@lowrisc.org>

--- hw/top_darjeeling/ip_autogen/rv_plic/fpv/tb/waivers.tcl
+++ hw/top_darjeeling/ip_autogen/rv_plic/fpv/tb/waivers.tcl
@@ -45,3 +45,10 @@ cover -disable -regexp ".*\.u_prim_reg_we_check.u_prim_onehot_check.Onehot0Check
 # To support that the precondition related to assertion Onehot0Check_A can never happen i.e.
 # !$onehot0(oh_i), an assertion to make sure that oh_i is always onehot or zero.
 assert -name InpAlwaysOnehot0_A {$onehot0(dut.u_reg.u_prim_reg_we_check.u_prim_onehot_check.oh_i)}
+
+# It is unlikely to get the err_o for rv_plic. There is a precondition related to the assertion
+# FpvSecCmRegWeOnehotCheck which is enabled if err_o is high. The intention to add this assertion
+# is to make sure that err_o never happens as we waived off the precondition in waivers.tcl.
+# Refer to waivers.tcl for the detailed argument as why this is not possible.
+assert -name RvPlicRegNoOneHot&EnError \
+  {dut.u_reg.u_prim_reg_we_check.u_prim_onehot_check.err_o == 0}
