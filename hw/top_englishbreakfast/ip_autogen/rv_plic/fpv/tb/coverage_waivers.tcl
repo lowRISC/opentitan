e58236e074d77ebfc95329f22d393c8f38b7012d
Kinza Qamar <kqzaman@lowrisc.org>
2025-03-13 09:51:40 +0000

    [rv_plic, fpv] No parasitic state in alert sender FSM while doing FPV
    Signed-off-by: Kinza Qamar <kqzaman@lowrisc.org>

--- hw/top_englishbreakfast/ip_autogen/rv_plic/fpv/tb/coverage_waivers.tcl
+++ hw/top_englishbreakfast/ip_autogen/rv_plic/fpv/tb/coverage_waivers.tcl
@@ -9,3 +9,11 @@ check_cov -waiver -add -start_line 34 -end_line 34 -type {branch} -instance {dut
 # to support the waiver for the statement above. The statement is a ternary assignment and the
 # conditional part of it is le_i which is always false.
 assert -name InterruptsAreLevelTriggered {!$rose(dut.u_gateway.le_i)}
+
+# This line is a default case statement which could be executed if a parasitic state has been
+# injected to the state register of u_prim_alert_sender.
+check_cov -waiver -add -start_line 249 -end_line 249 -type {branch} -instance {dut.gen_alert_tx[0].u_prim_alert_sender} -comment {No parasitic state injection while doing FPV}
+
+# The intention to add this assertion is to make sure that while doing FPV there is not a
+# possibility to inject parasitic state to the state register of alert sender FSM.
+assert -name AlertSenderNoParasiticState_A {dut.gen_alert_tx[0].u_prim_alert_sender.state_q <= 6}
