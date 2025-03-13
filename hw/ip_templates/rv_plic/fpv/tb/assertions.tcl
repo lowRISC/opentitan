05793ab9a633429d1a440eaeeee8bfe30f1216bc
Kinza Qamar <kqzaman@lowrisc.org>
2025-03-13 10:44:57 +0000

    [rv_plic, fpv] Added an assertion for err_o precondition
    The assertion FPVSecCmRegWeOnehotCheck in rv_plic.sv has a precondition
    which requires err_o in u_reg.*.u_prim_onehot_check true. But this is
    not possible. To know the reasoning refer waivers.tcl in
    hw/ip_templates/rv_plic/fpv/tb.

--- hw/ip_templates/rv_plic/fpv/tb/assertions.tcl
+++ hw/ip_templates/rv_plic/fpv/tb/assertions.tcl
@@ -6,3 +6,10 @@
 # possibility to inject parasitic state to the state variable of alert sender FSM.
 assert -name RvPlicAlertSenderNoParasiticState \
   {dut.gen_alert_tx[0].u_prim_alert_sender.state_q <= 6}
+
+# It is unlikely to get the err_o for rv_plic. There is a precondition related to the assertion
+# FpvSecCmRegWeOnehotCheck which is enabled if err_o is high. The intention to add this assertion
+# is to make sure that err_o never happens as we waived off the precondition in waivers.tcl.
+# Refer to waivers.tcl for the detailed argument as why this is not possible.
+assert -name RvPlicRegNoOneHot&EnError \
+  {dut.u_reg.u_prim_reg_we_check.u_prim_onehot_check.err_o == 0}
