971a6a5aa8fc28a0998df7edea0db1036ea18bc9
Robert Schilling <rschilling@rivosinc.com>
2025-03-17 15:36:06 +0100

    [hw,tlul_adapter_*_racl,rtl] Don't factor a_ready in squash decission
    We don't need to wait for the device to decide squashing the request.
    We can do that without knowing that. This breaks a combinational loop
    where in the user of the adapter has a dependency on req_o on the gnt_i
    signal.
    
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/tlul/rtl/tlul_adapter_reg_racl.sv
+++ hw/ip/tlul/rtl/tlul_adapter_reg_racl.sv
@@ -69,11 +69,10 @@ module tlul_adapter_reg_racl
       .out_o( racl_role_vec )
     );
 
-    logic req, rd_req, wr_req, racl_read_allowed, racl_write_allowed;
-    assign req                = tl_i.a_valid & tl_o.a_ready;
-    assign rd_req             = req & (tl_i.a_opcode == tlul_pkg::Get);
-    assign wr_req             = req & (tl_i.a_opcode == tlul_pkg::PutFullData |
-                                       tl_i.a_opcode == tlul_pkg::PutPartialData);
+    logic rd_req, wr_req, racl_read_allowed, racl_write_allowed;
+    assign rd_req             = tl_i.a_valid & (tl_i.a_opcode == tlul_pkg::Get);
+    assign wr_req             = tl_i.a_valid & (tl_i.a_opcode == tlul_pkg::PutFullData |
+                                                tl_i.a_opcode == tlul_pkg::PutPartialData);
     assign racl_read_allowed  = (|(racl_policies_i[RaclPolicySelVec].read_perm  & racl_role_vec));
     assign racl_write_allowed = (|(racl_policies_i[RaclPolicySelVec].write_perm & racl_role_vec));
     assign racl_error_o.valid = (rd_req & ~racl_read_allowed) | (wr_req & ~racl_write_allowed);
