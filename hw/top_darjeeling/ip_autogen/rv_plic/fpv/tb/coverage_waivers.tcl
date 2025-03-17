23fccd2efe569daf80a87abe38130a5ad4e4e95b
Kinza Qamar <kqzaman@lowrisc.org>
2025-03-17 11:32:26 +0000

    [rv_plic, fpv] Added assertions to support the precondition waivers
    Signed-off-by: Kinza Qamar <kqzaman@lowrisc.org>

--- hw/top_darjeeling/ip_autogen/rv_plic/fpv/tb/coverage_waivers.tcl
+++ hw/top_darjeeling/ip_autogen/rv_plic/fpv/tb/coverage_waivers.tcl
@@ -4,3 +4,8 @@
 
 # The statement is a deadcode as le_i is always level triggered.
 check_cov -waiver -add -start_line 34 -end_line 34 -type {branch} -instance {dut.u_gateway} -comment {le_i is tied to a parameter which is 0}
+
+# All the interrupts are level triggered thus le_i is always 0. The reason to add this assertion is
+# to support the waiver for the statement above. The statement is a ternary assignment and the
+# conditional part of it is le_i which is always false.
+assert -name InterruptsAreLevelTriggered {!$rose(dut.u_gateway.le_i)}
