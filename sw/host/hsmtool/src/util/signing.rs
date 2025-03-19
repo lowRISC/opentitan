04052f4d5b776aa522cb350dfa1eec23b4b1697b
Gary Guo <gary.guo@lowrisc.org>
2025-03-19 15:57:16 +0000

    [rust] fix `literal_string_with_formatting_args` clippy error
    These are supposed to be formatting strings but are used directly with
    literals. This is detected by clippy 1.85.
    
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

--- sw/host/hsmtool/src/util/signing.rs
+++ sw/host/hsmtool/src/util/signing.rs
@@ -91,7 +91,7 @@ impl SignData {
                 // Data is a slice of plaintext: hash.
                 SignData::Slice(a, b) => Self::data_plain_text(&input[*a..*b], false),
             },
-            _ => Err(HsmError::Unsupported("SignData prepare for {keytype:?}".into()).into()),
+            _ => Err(HsmError::Unsupported(format!("SignData prepare for {keytype:?}")).into()),
         }
     }
 
@@ -158,7 +158,7 @@ impl SignData {
                 SignData::Raw => Ok(Mechanism::Ecdsa),
                 SignData::Slice(_, _) => Ok(Mechanism::Ecdsa),
             },
-            _ => Err(HsmError::Unsupported("No mechanism for {keytype:?}".into()).into()),
+            _ => Err(HsmError::Unsupported(format!("No mechanism for {keytype:?}")).into()),
         }
     }
 
