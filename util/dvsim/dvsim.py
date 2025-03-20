bb8b8c65d0d33ba94997f3256631a66fcb5f4bd3
Martin Velay <mvelay@lowrisc.org>
2025-03-20 09:55:42 +0000

    [dvsim] Add -fs option for --fixed-seed
    - Add possibility to invoke "-fs" instead of "--fixed-seed" as a
      command line argument.
    
    Signed-off-by: Martin Velay <mvelay@lowrisc.org>
    (cherry picked from commit cdd111e20cd3127d4a9c060b2ac78ad99c915d15)

--- util/dvsim/dvsim.py
+++ util/dvsim/dvsim.py
@@ -534,6 +534,7 @@ def parse_args():
                              'in the order they are passed.'))
 
     seedg.add_argument("--fixed-seed",
+                       "-fs",
                        type=int,
                        metavar='S',
                        help=('Run all items with the seed S. This implies '
