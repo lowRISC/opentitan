8f123514ec3c760130fbd17353f58aa5efdba918
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-03-20 17:11:30 +0000

    [racl_ctrl,dv] Very simple initial smoke sequence
    This is rather trivial: it just writes to some policy registers and
    checks that the values are reflected in the output.
    
    It does *not* yet check the error log behaviour, but it is a (very)
    minimal test for the block.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/racl_ctrl/dv/env/seq_lib/racl_ctrl_base_vseq.sv
+++ hw/ip/racl_ctrl/dv/env/seq_lib/racl_ctrl_base_vseq.sv
@@ -2,32 +2,17 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-class racl_ctrl_base_vseq extends cip_base_vseq #(
-    .RAL_T               (racl_ctrl_reg_block),
-    .CFG_T               (racl_ctrl_env_cfg),
-    .COV_T               (racl_ctrl_env_cov),
-    .VIRTUAL_SEQUENCER_T (racl_ctrl_virtual_sequencer)
-  );
+class racl_ctrl_base_vseq
+  extends cip_base_vseq #(.RAL_T               (racl_ctrl_reg_block),
+                          .CFG_T               (racl_ctrl_env_cfg),
+                          .COV_T               (racl_ctrl_env_cov),
+                          .VIRTUAL_SEQUENCER_T (racl_ctrl_virtual_sequencer));
+
   `uvm_object_utils(racl_ctrl_base_vseq)
 
-  // various knobs to enable certain routines
-  bit do_racl_ctrl_init = 1'b1;
+  extern function new (string name="");
+endclass
 
-  `uvm_object_new
-
-  virtual task dut_init(string reset_kind = "HARD");
-    super.dut_init();
-    if (do_racl_ctrl_init) racl_ctrl_init();
-  endtask
-
-  virtual task dut_shutdown();
-    // check for pending racl_ctrl operations and wait for them to complete
-    // TODO
-  endtask
-
-  // setup basic racl_ctrl features
-  virtual task racl_ctrl_init();
-    `uvm_error(`gfn, "FIXME")
-  endtask
-
-endclass : racl_ctrl_base_vseq
+function racl_ctrl_base_vseq::new (string name="");
+  super.new(name);
+endfunction
