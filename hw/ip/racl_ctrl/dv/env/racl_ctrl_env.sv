a169e3e1e77c33e8ba904bf26e6face617a511bf
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-03-21 12:52:35 +0000

    [racl_ctrl,dv] Use out-of-block definitions in racl_ctrl_env
    No functional change, but I'm dropping the connect_phase
    definition (because we aren't just about to use it).
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/racl_ctrl/dv/env/racl_ctrl_env.sv
+++ hw/ip/racl_ctrl/dv/env/racl_ctrl_env.sv
@@ -2,22 +2,20 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-class racl_ctrl_env extends cip_base_env #(
-    .CFG_T              (racl_ctrl_env_cfg),
-    .COV_T              (racl_ctrl_env_cov),
-    .VIRTUAL_SEQUENCER_T(racl_ctrl_virtual_sequencer),
-    .SCOREBOARD_T       (racl_ctrl_scoreboard)
-  );
+class racl_ctrl_env extends cip_base_env #(.CFG_T              (racl_ctrl_env_cfg),
+                                           .COV_T              (racl_ctrl_env_cov),
+                                           .VIRTUAL_SEQUENCER_T(racl_ctrl_virtual_sequencer),
+                                           .SCOREBOARD_T       (racl_ctrl_scoreboard));
   `uvm_component_utils(racl_ctrl_env)
 
-  `uvm_component_new
-
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-  endfunction
-
-  function void connect_phase(uvm_phase phase);
-    super.connect_phase(phase);
-  endfunction
-
+  extern function new (string name="", uvm_component parent=null);
+  extern function void build_phase(uvm_phase phase);
 endclass
+
+function racl_ctrl_env::new (string name="", uvm_component parent=null);
+  super.new(name, parent);
+endfunction
+
+function void racl_ctrl_env::build_phase(uvm_phase phase);
+  super.build_phase(phase);
+endfunction
