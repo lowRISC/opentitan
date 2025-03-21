318dbbda87358828bf9ea131816dd188f72361a7
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-03-21 10:30:57 +0100

    [pentest] Return rstmgr dump to host
    This commit configures the reset manager in the pentest framework to
    collect reset and alert reasons. By introducing a new pentest command
    this information can be returned to the host after a reset happened.
    
    Note that the reset and alert reason is not saved when performing a
    POR.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/tests/penetrationtests/firmware/firmware_sca.c
+++ sw/device/tests/penetrationtests/firmware/firmware_sca.c
@@ -18,12 +18,14 @@
 #include "sw/device/tests/penetrationtests/json/ibex_sca_commands.h"
 #include "sw/device/tests/penetrationtests/json/kmac_sca_commands.h"
 #include "sw/device/tests/penetrationtests/json/otbn_sca_commands.h"
+#include "sw/device/tests/penetrationtests/json/pentest_lib_commands.h"
 #include "sw/device/tests/penetrationtests/json/prng_sca_commands.h"
 #include "sw/device/tests/penetrationtests/json/sha3_sca_commands.h"
 #include "sw/device/tests/penetrationtests/json/trigger_sca_commands.h"
 
 // Include handlers
 #include "lib/extclk_sca_fi.h"
+#include "lib/pentest_lib.h"
 #include "sca/aes_sca.h"
 #include "sca/edn_sca.h"
 #include "sca/hmac_sca.h"
@@ -43,6 +45,9 @@ status_t process_cmd(ujson_t *uj) {
     switch (cmd) {
       case kPenetrationtestCommandAesSca:
         RESP_ERR(uj, handle_aes_sca(uj));
+        break;
+      case kPenetrationtestCommandAlertInfo:
+        RESP_ERR(uj, pentest_read_rstmgr_alert_info(uj));
         break;
       case kPenetrationtestCommandEdnSca:
         RESP_ERR(uj, handle_edn_sca(uj));
