9b16547698f54287971be918ebcd45cc7388f5ef
Kinza Qamar <kqzaman@lowrisc.org>
2025-03-24 16:18:49 +0000

    [rv_pic, fpv] Removed the coverage waiver and its supporting assertion
    The piece of code is meaningless after #26559 landed.
    
    Signed-off-by: Kinza Qamar <kqzaman@lowrisc.org>

--- hw/top_earlgrey/ip_autogen/rv_plic/fpv/tb/coverage_waivers.tcl
+++ hw/top_earlgrey/ip_autogen/rv_plic/fpv/tb/coverage_waivers.tcl
@@ -2,14 +2,6 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-# The statement is a deadcode as le_i is always level triggered.
-check_cov -waiver -add -start_line 34 -end_line 34 -type {branch} -instance {dut.u_gateway} -comment {le_i is tied to a parameter which is 0}
-
-# All the interrupts are level triggered thus le_i is always 0. The reason to add this assertion is
-# to support the waiver for the statement above. The statement is a ternary assignment and the
-# conditional part of it is le_i which is always false.
-assert -name InterruptsAreLevelTriggered {!$rose(dut.u_gateway.le_i)}
-
 # This line is a default case statement which could be executed if a parasitic state has been
 # injected to the state register of u_prim_alert_sender.
 check_cov -waiver -add -start_line 249 -end_line 249 -type {branch} -instance {dut.gen_alert_tx[0].u_prim_alert_sender} -comment {No parasitic state injection while doing FPV}
