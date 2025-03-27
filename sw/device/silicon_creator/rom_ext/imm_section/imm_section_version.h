5abc0319fb453b05a5452a0eefde8b6762d08d79
Yi-Hsuan Deng <yhdeng@google.com>
2025-03-27 01:51:28 +0000

    [imm_section] Add version string to the compiled binary
    This commit stores a version string in the compiled immutable section, and
    prints it at the mutable stage to make it easier to debug issues between
    imm_section releases.
    
    Since the imm_section does not have a manifest, and the entry point must at
    the beginning, the version is stored at the last 8 bytes of the immutable
    section as a null-terminated printable string.
    
    Change-Id: I6c669349ab6700cf91e75355fb1f80328c2507f5
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

---
+++ sw/device/silicon_creator/rom_ext/imm_section/imm_section_version.h
@@ -0,0 +1,61 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXT_IMM_SECTION_IMM_SECTION_VERSION_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXT_IMM_SECTION_IMM_SECTION_VERSION_H_
+
+#include <stdint.h>
+
+#include "sw/device/lib/base/macros.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * The version struct is stored at the last 32 bytes of imm_section.
+ */
+enum {
+  kImmVersionSize = 32,
+  kImmVersionMagic = 0x43534d49,
+};
+
+/**
+ * Version info append at the end of built imm_section.
+ */
+typedef struct imm_section_version {
+  /**
+   * Magic Identifier 0x43534d49 `IMSC`.
+   */
+  uint32_t magic;
+  /**
+   * Major version (breaking changes).
+   *
+   * The version is defined in the bazel file:
+   *   sw/device/silicon_creator/rom_ext/imm_section/defs.bzl
+   */
+  uint32_t major;
+  /**
+   * Minor version (non-breaking changes).
+   *
+   * The version is defined in the bazel file:
+   *   sw/device/silicon_creator/rom_ext/imm_section/defs.bzl
+   */
+  uint32_t minor;
+  /**
+   * Reserved space for future update.
+   */
+  uint8_t reserved[20];
+} imm_section_version_t;
+
+OT_ASSERT_MEMBER_OFFSET(imm_section_version_t, magic, 0);
+OT_ASSERT_MEMBER_OFFSET(imm_section_version_t, major, 4);
+OT_ASSERT_MEMBER_OFFSET(imm_section_version_t, minor, 8);
+OT_ASSERT_SIZE(imm_section_version_t, 32);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_ROM_EXT_IMM_SECTION_IMM_SECTION_VERSION_H_
