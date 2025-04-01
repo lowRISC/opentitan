9ec77c03291c8dc6aa4b73a6b83fe9862b692935
Antonio Martinez Zambrana <amz@lowrisc.org>
2025-04-01 20:09:59 +0100

    [dv, alert_esc_agent] tasks switched to extern in alert_receiver_base_seq
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/dv/sv/alert_esc_agent/seq_lib/alert_receiver_base_seq.sv
+++ hw/dv/sv/alert_esc_agent/seq_lib/alert_receiver_base_seq.sv
@@ -11,25 +11,30 @@ class alert_receiver_base_seq extends dv_base_seq #(
 
   `uvm_object_utils(alert_receiver_base_seq)
 
-  `uvm_object_new
-
   rand bit r_alert_ping_send;
   rand bit r_alert_rsp;
 
-  task body();
-    `uvm_info(`gfn, $sformatf("starting alert receiver transfer"), UVM_HIGH)
-    req = alert_esc_seq_item::type_id::create("req");
-    start_item(req);
-    `DV_CHECK_RANDOMIZE_WITH_FATAL(req,
-        r_alert_ping_send == local::r_alert_ping_send;
-        r_alert_rsp       == local::r_alert_rsp;
-        int_err           == 0; // This agent do not support alert_receiver int_err
-    )
-    `uvm_info(`gfn, $sformatf("seq_item: ping_send=%0b alert_rsp=%0b int_err=%0b",
-                              req.r_alert_ping_send, req.r_alert_rsp, req.int_err), UVM_MEDIUM)
-    finish_item(req);
-    get_response(rsp);
-    `uvm_info(`gfn, "alert receiver transfer done", UVM_HIGH)
-  endtask : body
+  extern function new (string name = "");
+  extern task body();
 
 endclass : alert_receiver_base_seq
+
+function alert_receiver_base_seq::new (string name = "");
+  super.new(name);
+endfunction : new
+
+task alert_receiver_base_seq::body();
+  `uvm_info(`gfn, $sformatf("starting alert receiver transfer"), UVM_HIGH)
+  req = alert_esc_seq_item::type_id::create("req");
+  start_item(req);
+  `DV_CHECK_RANDOMIZE_WITH_FATAL(req,
+                                 r_alert_ping_send == local::r_alert_ping_send;
+                                 r_alert_rsp       == local::r_alert_rsp;
+                                 int_err           == 0; // This agent do not support alert_receiver int_err
+                                 )
+  `uvm_info(`gfn, $sformatf("seq_item: ping_send=%0b alert_rsp=%0b int_err=%0b",
+                            req.r_alert_ping_send, req.r_alert_rsp, req.int_err), UVM_MEDIUM)
+  finish_item(req);
+  get_response(rsp);
+  `uvm_info(`gfn, "alert receiver transfer done", UVM_HIGH)
+endtask : body
