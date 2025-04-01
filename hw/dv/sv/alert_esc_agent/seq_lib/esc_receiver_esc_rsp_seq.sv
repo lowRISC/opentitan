5ad1cab161dbed2481d6fddc426d3ceac848ee25
Antonio Martinez Zambrana <amz@lowrisc.org>
2025-04-01 20:07:20 +0100

    [dv, alert_esc_agent] tasks switched to extern in esc_receiver_esc_rsp_seq
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/dv/sv/alert_esc_agent/seq_lib/esc_receiver_esc_rsp_seq.sv
+++ hw/dv/sv/alert_esc_agent/seq_lib/esc_receiver_esc_rsp_seq.sv
@@ -6,10 +6,17 @@
 class esc_receiver_esc_rsp_seq extends esc_receiver_base_seq;
 
   `uvm_object_utils(esc_receiver_esc_rsp_seq)
-  `uvm_object_new
 
-  constraint esc_receiver_esc_rsp_seq_c {
-    r_esc_rsp == 1;
-  }
+  extern constraint esc_receiver_esc_rsp_seq_c;
+
+  extern function new (string name = "");
 
 endclass : esc_receiver_esc_rsp_seq
+
+constraint esc_receiver_esc_rsp_seq::esc_receiver_esc_rsp_seq_c {
+  r_esc_rsp == 1;
+}
+
+function esc_receiver_esc_rsp_seq::new (string name = "");
+  super.new(name);
+endfunction : new
