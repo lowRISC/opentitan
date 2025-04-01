0b41c5c3ecc3e6469f1f1128f0a01fa0a67c626b
Antonio Martinez Zambrana <amz@lowrisc.org>
2025-04-01 20:09:39 +0100

    [dv, alert_esc_agent] tasks switched to extern in alest_receiver_alert_rsp_seq
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/dv/sv/alert_esc_agent/seq_lib/alert_receiver_alert_rsp_seq.sv
+++ hw/dv/sv/alert_esc_agent/seq_lib/alert_receiver_alert_rsp_seq.sv
@@ -6,46 +6,55 @@
 class alert_receiver_alert_rsp_seq extends alert_receiver_base_seq;
 
   `uvm_object_utils(alert_receiver_alert_rsp_seq)
-  `uvm_object_new
 
-  constraint alert_receiver_alert_rsp_seq_c {
-    r_alert_ping_send == 0;
-    r_alert_rsp       == 1;
-  }
+  extern constraint alert_receiver_alert_rsp_seq_c;
 
-  virtual task body();
-    if (cfg.start_default_rsp_seq) begin
-      default_rsp_thread();
-    end else begin
-      super.body();
-    end
-  endtask
-
+  extern function new (string name = "");
+  extern virtual task body();
   // Sends alert rsps back to host.
-  virtual task default_rsp_thread();
-    alert_esc_seq_item req_q[$];
-    fork
-      forever begin : get_req
-        p_sequencer.req_analysis_fifo.get(req);
-        if (req.alert_esc_type == AlertEscSigTrans) req_q.push_back(req);
-      end : get_req
-      forever begin : send_rsp
-        if (cfg.in_reset) begin
-          req_q.delete();
-          wait (!cfg.in_reset);
-        end
-        wait (req_q.size());
-        rsp = req_q.pop_front();
-        start_item(rsp);
-        `DV_CHECK_RANDOMIZE_WITH_FATAL(req,
-            r_alert_ping_send == 0;
-            r_alert_rsp       == 1;
-            int_err           == 0;
-        )
-        finish_item(rsp);
-        get_response(rsp);
-      end : send_rsp
-    join
-  endtask
+  extern virtual task default_rsp_thread();
 
 endclass : alert_receiver_alert_rsp_seq
+
+constraint alert_receiver_alert_rsp_seq::alert_receiver_alert_rsp_seq_c {
+  r_alert_ping_send == 0;
+  r_alert_rsp       == 1;
+}
+
+function alert_receiver_alert_rsp_seq::new (string name = "");
+  super.new(name);
+endfunction : new
+
+task alert_receiver_alert_rsp_seq::body();
+  if (cfg.start_default_rsp_seq) begin
+    default_rsp_thread();
+  end else begin
+    super.body();
+  end
+endtask : body
+
+task alert_receiver_alert_rsp_seq::default_rsp_thread();
+  alert_esc_seq_item req_q[$];
+  fork
+    forever begin : get_req
+      p_sequencer.req_analysis_fifo.get(req);
+      if (req.alert_esc_type == AlertEscSigTrans) req_q.push_back(req);
+    end : get_req
+    forever begin : send_rsp
+      if (cfg.in_reset) begin
+        req_q.delete();
+        wait (!cfg.in_reset);
+      end
+      wait (req_q.size());
+      rsp = req_q.pop_front();
+      start_item(rsp);
+      `DV_CHECK_RANDOMIZE_WITH_FATAL(req,
+                                     r_alert_ping_send == 0;
+                                     r_alert_rsp       == 1;
+                                     int_err           == 0;
+                                     )
+      finish_item(rsp);
+      get_response(rsp);
+    end : send_rsp
+  join
+endtask : default_rsp_thread
