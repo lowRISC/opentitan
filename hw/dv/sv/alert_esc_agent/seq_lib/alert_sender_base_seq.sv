cfbb003c9b2953b58cde4f66db0cb080fb0a64f5
Antonio Martinez Zambrana <amz@lowrisc.org>
2025-04-01 20:10:41 +0100

    [dv, alert_esc_agent] tasks switched to extern in alert_sender_base_seq
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/dv/sv/alert_esc_agent/seq_lib/alert_sender_base_seq.sv
+++ hw/dv/sv/alert_esc_agent/seq_lib/alert_sender_base_seq.sv
@@ -10,28 +10,34 @@ class alert_sender_base_seq extends dv_base_seq #(
   );
 
   `uvm_object_utils(alert_sender_base_seq)
-  `uvm_object_new
 
   rand bit s_alert_send;
   rand bit s_alert_ping_rsp;
   rand bit int_err;
   rand bit ping_timeout;
 
-  virtual task body();
-    `uvm_info(`gfn, $sformatf("starting alert sender transfer"), UVM_HIGH)
-    req = alert_esc_seq_item::type_id::create("req");
-    start_item(req);
-    `DV_CHECK_RANDOMIZE_WITH_FATAL(req,
-        s_alert_send     == local::s_alert_send;
-        s_alert_ping_rsp == local::s_alert_ping_rsp;
-        int_err          == local::int_err;
-        ping_timeout     == local::ping_timeout;
-    )
-    `uvm_info(`gfn, $sformatf("seq_item: send_alert=%0b, ping_rsp=%0b, int_err=%0b",
-        req.s_alert_send, req.s_alert_ping_rsp, req.int_err), UVM_MEDIUM)
-    finish_item(req);
-    get_response(rsp);
-    `uvm_info(`gfn, "alert sender transfer done", UVM_HIGH)
-  endtask : body
+  extern function new (string name = "");
+  extern virtual task body();
 
 endclass : alert_sender_base_seq
+
+function alert_sender_base_seq::new (string name = "");
+  super.new(name);
+endfunction : new
+
+task alert_sender_base_seq::body();
+  `uvm_info(`gfn, $sformatf("starting alert sender transfer"), UVM_HIGH)
+  req = alert_esc_seq_item::type_id::create("req");
+  start_item(req);
+  `DV_CHECK_RANDOMIZE_WITH_FATAL(req,
+                                 s_alert_send     == local::s_alert_send;
+                                 s_alert_ping_rsp == local::s_alert_ping_rsp;
+                                 int_err          == local::int_err;
+                                 ping_timeout     == local::ping_timeout;
+                                 )
+  `uvm_info(`gfn, $sformatf("seq_item: send_alert=%0b, ping_rsp=%0b, int_err=%0b",
+                            req.s_alert_send, req.s_alert_ping_rsp, req.int_err), UVM_MEDIUM)
+  finish_item(req);
+  get_response(rsp);
+  `uvm_info(`gfn, "alert sender transfer done", UVM_HIGH)
+endtask : body
