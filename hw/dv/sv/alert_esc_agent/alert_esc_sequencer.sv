d7fa3b7924ef1a8c54e705306a29e16d13b68cfe
Antonio Martinez Zambrana <amz@lowrisc.org>
2025-04-01 20:08:36 +0100

    [dv, alert_esc_agent] tasks switched to extern in alest_esc_sequencer
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/dv/sv/alert_esc_agent/alert_esc_sequencer.sv
+++ hw/dv/sv/alert_esc_agent/alert_esc_sequencer.sv
@@ -5,6 +5,10 @@
 class alert_esc_sequencer extends dv_base_sequencer#(alert_esc_seq_item, alert_esc_agent_cfg);
   `uvm_component_utils(alert_esc_sequencer)
 
-  `uvm_component_new
+  extern function new (string name="", uvm_component parent=null);
 
-endclass
+endclass : alert_esc_sequencer
+
+function alert_esc_sequencer::new (string name="", uvm_component parent=null);
+  super.new(name, parent);
+endfunction : new
