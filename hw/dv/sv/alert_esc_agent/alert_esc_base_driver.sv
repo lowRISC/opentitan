0ac4706aa07d747041ebe3ec7ad15c59a13d62c9
Antonio Martinez Zambrana <amz@lowrisc.org>
2025-04-01 20:08:06 +0100

    [dv, alert_esc_agent] tasks switched to extern in alest_esc_base_driver
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/dv/sv/alert_esc_agent/alert_esc_base_driver.sv
+++ hw/dv/sv/alert_esc_agent/alert_esc_base_driver.sv
@@ -12,38 +12,45 @@ class alert_esc_base_driver extends dv_base_driver#(alert_esc_seq_item, alert_es
 
   `uvm_component_utils(alert_esc_base_driver)
 
-  `uvm_component_new
-
+  extern function new (string name="", uvm_component parent=null);
   // drive trans received from sequencer
-  virtual task get_and_drive();
-    fork
-      get_req();
-      drive_req();
-    join_none
-  endtask
+  extern virtual task get_and_drive();
+  extern virtual task drive_req();
+  extern virtual task get_req();
 
-  virtual task drive_req();
-    `uvm_fatal(`gfn, "this is implemented as pure virtual task - please extend")
-  endtask
+endclass : alert_esc_base_driver
 
-  virtual task get_req();
-    @(posedge cfg.vif.rst_n);
-    forever begin
-      alert_esc_seq_item req_clone;
-      seq_item_port.get(req);
-      `downcast(req_clone, req.clone());
-      req_clone.set_id_info(req);
-      // receiver mode
-      if (req.r_alert_ping_send) r_alert_ping_send_q.push_back(req_clone);
-      if (req.r_alert_rsp)       r_alert_rsp_q.push_back(req_clone);
-      if (req.r_esc_rsp)         r_esc_rsp_q.push_back(req_clone);
-      // sender mode
-      if (req.s_alert_send)      s_alert_send_q.push_back(req_clone);
-      if (req.s_alert_ping_rsp)  s_alert_ping_rsp_q.push_back(req_clone);
-      `uvm_info(`gfn, $sformatf({"Driver received item (after pushing): req.r_alert_ping_send=%0d",
-                                 " | req.r_alert_rsp=%0d | req.r_esc_rsp=%0d"},
-                                 req.r_alert_ping_send, req.r_alert_rsp, req.r_esc_rsp), UVM_DEBUG)
-    end
-  endtask : get_req
+function alert_esc_base_driver::new (string name="", uvm_component parent=null);
+  super.new(name, parent);
+endfunction : new
 
-endclass
+task alert_esc_base_driver::get_and_drive();
+  fork
+    get_req();
+    drive_req();
+  join_none
+endtask : get_and_drive
+
+task alert_esc_base_driver::drive_req();
+  `uvm_fatal(`gfn, "this is implemented as pure virtual task - please extend")
+endtask : drive_req
+
+task alert_esc_base_driver::get_req();
+  @(posedge cfg.vif.rst_n);
+  forever begin
+    alert_esc_seq_item req_clone;
+    seq_item_port.get(req);
+    `downcast(req_clone, req.clone());
+    req_clone.set_id_info(req);
+    // receiver mode
+    if (req.r_alert_ping_send) r_alert_ping_send_q.push_back(req_clone);
+    if (req.r_alert_rsp)       r_alert_rsp_q.push_back(req_clone);
+    if (req.r_esc_rsp)         r_esc_rsp_q.push_back(req_clone);
+    // sender mode
+    if (req.s_alert_send)      s_alert_send_q.push_back(req_clone);
+    if (req.s_alert_ping_rsp)  s_alert_ping_rsp_q.push_back(req_clone);
+    `uvm_info(`gfn, $sformatf({"Driver received item (after pushing): req.r_alert_ping_send=%0d",
+                               " | req.r_alert_rsp=%0d | req.r_esc_rsp=%0d"},
+                              req.r_alert_ping_send, req.r_alert_rsp, req.r_esc_rsp), UVM_DEBUG)
+  end
+endtask : get_req
