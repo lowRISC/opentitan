e1350619c68e33ac758cebaff5a2eb84b1f44860
David Schrammel <davidschrammel@rivosinc.com>
2025-04-02 07:53:05 +0200

    [sw] Extract the SPI device and UART dependencies from the print module
    Not all tops have a UART and/or a SPI device. Without separating the
    runtime/print module into a core part, and two SPI device and UART
    dependencies, those tops can not even log into memory.
    
    Co-authored-by: Samuel Ortiz <sameo@rivosinc.com>
    Signed-off-by: David Schrammel <davidschrammel@rivosinc.com>

---
+++ sw/device/lib/runtime/print_spi.c
@@ -0,0 +1,17 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "sw/device/lib/base/macros.h"
+#include "sw/device/lib/base/memory.h"
+#include "sw/device/lib/base/status.h"
+#include "sw/device/lib/dif/dif_spi_device.h"
+#include "sw/device/lib/dif/dif_uart.h"
+#include "sw/device/lib/runtime/print.h"
+
+#include "spi_device_regs.h"  // Generated.
