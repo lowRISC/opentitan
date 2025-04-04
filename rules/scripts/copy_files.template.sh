a3727d87e4e05c5c09150f4ead38ac77b26893ef
Chris Frantz <cfrantz@google.com>
2025-04-04 09:40:08 -0700

    [signing] Fix file copy automation
    Update the file copying rule to operate in the root of the opentitan
    repo and be able to copy files into an externally attached repo (e.g.
    the provisioning skus repo).
    
    Signed-off-by: Chris Frantz <cfrantz@google.com>
    (cherry picked from commit 8f9e9644b18674710966f75276b1e5b118a55384)

--- rules/scripts/copy_files.template.sh
+++ rules/scripts/copy_files.template.sh
@@ -7,13 +7,29 @@
 DEST="__DEST__"
 FILES=(__FILES__)
 
-if [[ ! -z "${__WORKSPACE__+is_set}" ]]; then
-    cd ${__WORKSPACE__} || exit 1
+# Change directory into the root of the project.
+if [[ ! -z "${BUILD_WORKSPACE_DIRECTORY+is_set}" ]]; then
+    cd ${BUILD_WORKSPACE_DIRECTORY} || exit 1
 else
+    echo "BUILD_WORKSPACE_DIRECTORY was not set."
+    echo "This rule should be executable and invoked with 'bazel run'."
+    exit 1
+fi
+
+# Since we may be building resources in an externally attached workspace,
+# determine the real path to that workspace.
+if [[ -z "${__WORKSPACE__+is_set}" ]]; then
     echo "__WORKSPACE__ was not set."
     exit 1
 fi
 
+WORKSPACE="$(realpath "${__WORKSPACE__}")"
+if [[ ! -d "$WORKSPACE" ]]; then
+    echo "$WORKSPACE isn't a directory."
+    exit 1
+fi
+
+# Copy the files from the source (e.g. bazel-out) to the destination.
 for f in "${FILES[@]}"; do
-  cp --no-preserve=mode "$f" "$DEST"
+  cp --no-preserve=mode "$f" "$WORKSPACE/$DEST"
 done
