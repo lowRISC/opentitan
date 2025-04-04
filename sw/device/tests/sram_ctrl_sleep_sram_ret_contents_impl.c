61484225e23d8cf08c02f1dc5c4a3ceef5db0d7c
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-04-04 10:02:38 +0000

    [tests] Port sram_ctrl_sleep_sram_ret_contents_impl to DT
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/tests/sram_ctrl_sleep_sram_ret_contents_impl.c
+++ sw/device/tests/sram_ctrl_sleep_sram_ret_contents_impl.c
@@ -18,33 +18,29 @@
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/silicon_creator/lib/drivers/retention_sram.h"
 
-#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
-#include "sw/device/lib/testing/autogen/isr_testutils.h"
-
 enum {
-  /**
-   * Retention SRAM start address (inclusive).
-   */
-  kRetSramBaseAddr = TOP_EARLGREY_SRAM_CTRL_RET_AON_RAM_BASE_ADDR,
-
-  kRetSramOwnerAddr = kRetSramBaseAddr + offsetof(retention_sram_t, owner),
-  kRetRamLastAddr =
-      kRetSramBaseAddr + TOP_EARLGREY_SRAM_CTRL_RET_AON_RAM_SIZE_BYTES - 1,
-
   kTestBufferSizeWords = 16,
+  kPlicTarget = 0,
 };
 
 static dif_rv_plic_t rv_plic;
 static dif_aon_timer_t aon_timer;
 static dif_pwrmgr_t pwrmgr;
 static dif_rstmgr_t rstmgr;
-static plic_isr_ctx_t plic_ctx = {.rv_plic = &rv_plic,
-                                  .hart_id = kTopEarlgreyPlicTargetIbex0};
-static pwrmgr_isr_ctx_t pwrmgr_isr_ctx = {
-    .pwrmgr = &pwrmgr,
-    .plic_pwrmgr_start_irq_id = kTopEarlgreyPlicIrqIdPwrmgrAonWakeup,
-    .expected_irq = kDifPwrmgrIrqWakeup,
-    .is_only_irq = true};
+
+static const dt_rstmgr_t kRstmgrDt = 0;
+static_assert(kDtRstmgrCount == 1, "this test expects a rstmgr");
+static const dt_pwrmgr_t kPwrmgrDt = 0;
+static_assert(kDtPwrmgrCount == 1, "this library expects exactly one pwrmgr");
+static const dt_aon_timer_t kAonTimerDt = 0;
+static_assert(kDtAonTimerCount == 1,
+              "this library expects exactly one aon_timer");
+static const dt_rv_plic_t kRvPlicDt = 0;
+static_assert(kDtRvPlicCount >= 1, "this test expects at least one rv_plic");
+
+static const dt_sram_ctrl_t kRetSramCtrlDt = kDtSramCtrlRetAon;
+
+static dif_pwrmgr_request_sources_t aon_timer_wakeup_sources;
 
 // Random data to read/write to/from retention SRAM.
 const uint32_t kTestData[kTestBufferSizeWords] = {
@@ -59,15 +55,19 @@ typedef struct {
 } check_config_t;
 
 static void retention_sram_check(check_config_t config) {
+  uintptr_t ret_sram_owner_addr =
+      dt_sram_ctrl_reg_block(kRetSramCtrlDt, kDtSramCtrlRegBlockRam) +
+      offsetof(retention_sram_t, owner);
+
   if (config.do_write) {
     sram_ctrl_testutils_write(
-        kRetSramOwnerAddr,
+        ret_sram_owner_addr,
         (sram_ctrl_testutils_data_t){.words = kTestData,
                                      .len = kTestBufferSizeWords});
   }
 
   uint32_t tmp_buffer[kTestBufferSizeWords];
-  memcpy(tmp_buffer, (uint8_t *)kRetSramOwnerAddr, sizeof(tmp_buffer));
+  memcpy(tmp_buffer, (uint8_t *)ret_sram_owner_addr, sizeof(tmp_buffer));
 
   if (config.is_equal) {
     CHECK_ARRAYS_EQ(tmp_buffer, kTestData, kTestBufferSizeWords);
@@ -87,17 +87,16 @@ void ottf_internal_isr(uint32_t *exc_info) {}
 /**
  * Override external interrupt handler to handle the normal sleep IRQ.
  */
-void ottf_external_isr(uint32_t *exc_info) {
-  dif_pwrmgr_irq_t irq_id;
-  top_earlgrey_plic_peripheral_t peripheral;
-
-  isr_testutils_pwrmgr_isr(plic_ctx, pwrmgr_isr_ctx, &peripheral, &irq_id);
-
-  LOG_INFO("Receive irq in normal sleep");
-  // Check that both the peripheral and the irq id are correct.
-  CHECK(peripheral == kTopEarlgreyPlicPeripheralPwrmgrAon,
-        "IRQ peripheral: %d is incorrect", peripheral);
-  CHECK(irq_id == kDifPwrmgrIrqWakeup, "IRQ ID: %d is incorrect", irq_id);
+bool ottf_handle_irq(uint32_t *exc_info, dt_instance_id_t devid,
+                     dif_rv_plic_irq_id_t irq_id) {
+  if (devid == dt_pwrmgr_instance_id(kPwrmgrDt) &&
+      irq_id == dt_pwrmgr_irq_to_plic_id(kPwrmgrDt, kDtPwrmgrIrqWakeup)) {
+    LOG_INFO("Receive irq in normal sleep");
+    CHECK_DIF_OK(dif_pwrmgr_irq_acknowledge(&pwrmgr, kDtPwrmgrIrqWakeup));
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // test these 2 cases:
@@ -110,14 +109,14 @@ void test_ret_sram_in_normal_sleep(void) {
   // set up wakeup timer
   CHECK_STATUS_OK(aon_timer_testutils_wakeup_config(&aon_timer, 20));
   // Enable all the AON interrupts used in this test.
-  rv_plic_testutils_irq_range_enable(&rv_plic, kTopEarlgreyPlicTargetIbex0,
-                                     kTopEarlgreyPlicIrqIdPwrmgrAonWakeup,
-                                     kTopEarlgreyPlicIrqIdPwrmgrAonWakeup);
+  dif_rv_plic_irq_id_t irq_id =
+      dt_pwrmgr_irq_to_plic_id(kPwrmgrDt, kDtPwrmgrIrqWakeup);
+  rv_plic_testutils_irq_range_enable(&rv_plic, kPlicTarget, irq_id, irq_id);
   CHECK_DIF_OK(dif_pwrmgr_irq_set_enabled(&pwrmgr, 0, kDifToggleEnabled));
 
   // Normal sleep.
   CHECK_STATUS_OK(pwrmgr_testutils_enable_low_power(
-      &pwrmgr, /*wakeups=*/kDifPwrmgrWakeupRequestSourceFive,
+      &pwrmgr, /*wakeups=*/aon_timer_wakeup_sources,
       /*domain_config=*/kDifPwrmgrDomainOptionCoreClockInLowPower |
           kDifPwrmgrDomainOptionUsbClockInActivePower |
           kDifPwrmgrDomainOptionMainPowerInLowPower));
@@ -137,8 +136,8 @@ void enter_deep_sleep(void) {
   // set up wakeup timer
   CHECK_STATUS_OK(aon_timer_testutils_wakeup_config(&aon_timer, 20));
   // Deep sleep.
-  CHECK_STATUS_OK(pwrmgr_testutils_enable_low_power(
-      &pwrmgr, kDifPwrmgrWakeupRequestSourceFive, 0));
+  CHECK_STATUS_OK(
+      pwrmgr_testutils_enable_low_power(&pwrmgr, aon_timer_wakeup_sources, 0));
 
   // Enter low power mode.
   LOG_INFO("Issue WFI to enter deep sleep");
@@ -169,14 +168,14 @@ bool execute_sram_ctrl_sleep_ret_sram_contents_test(bool scramble) {
   irq_global_ctrl(true);
   irq_external_ctrl(true);
 
-  mmio_region_t addr = mmio_region_from_addr(TOP_EARLGREY_PWRMGR_AON_BASE_ADDR);
-  CHECK_DIF_OK(dif_pwrmgr_init(addr, &pwrmgr));
-  addr = mmio_region_from_addr(TOP_EARLGREY_RSTMGR_AON_BASE_ADDR);
-  CHECK_DIF_OK(dif_rstmgr_init(addr, &rstmgr));
-  addr = mmio_region_from_addr(TOP_EARLGREY_AON_TIMER_AON_BASE_ADDR);
-  CHECK_DIF_OK(dif_aon_timer_init(addr, &aon_timer));
-  addr = mmio_region_from_addr(TOP_EARLGREY_RV_PLIC_BASE_ADDR);
-  CHECK_DIF_OK(dif_rv_plic_init(addr, &rv_plic));
+  CHECK_DIF_OK(dif_pwrmgr_init_from_dt(kPwrmgrDt, &pwrmgr));
+  CHECK_DIF_OK(dif_rstmgr_init_from_dt(kRstmgrDt, &rstmgr));
+  CHECK_DIF_OK(dif_aon_timer_init_from_dt(kAonTimerDt, &aon_timer));
+  CHECK_DIF_OK(dif_rv_plic_init_from_dt(kRvPlicDt, &rv_plic));
+
+  CHECK_DIF_OK(dif_pwrmgr_find_request_source(
+      &pwrmgr, kDifPwrmgrReqTypeWakeup, dt_aon_timer_instance_id(kAonTimerDt),
+      kDtAonTimerWakeupWkupReq, &aon_timer_wakeup_sources));
 
   dif_rstmgr_reset_info_bitfield_t rstmgr_reset_info;
   rstmgr_reset_info = rstmgr_testutils_reason_get();
@@ -190,9 +189,7 @@ bool execute_sram_ctrl_sleep_ret_sram_contents_test(bool scramble) {
 
     if (scramble) {
       dif_sram_ctrl_t ret_sram;
-      addr =
-          mmio_region_from_addr(TOP_EARLGREY_SRAM_CTRL_RET_AON_REGS_BASE_ADDR);
-      CHECK_DIF_OK(dif_sram_ctrl_init(addr, &ret_sram));
+      CHECK_DIF_OK(dif_sram_ctrl_init_from_dt(kRetSramCtrlDt, &ret_sram));
       LOG_INFO("Wiping ret_sram...");
       CHECK_STATUS_OK(sram_ctrl_testutils_wipe(&ret_sram));
       LOG_INFO("Scrambling ret_sram...");
@@ -205,7 +202,7 @@ bool execute_sram_ctrl_sleep_ret_sram_contents_test(bool scramble) {
     LOG_INFO("Wake up from deep sleep");
 
     CHECK(UNWRAP(pwrmgr_testutils_is_wakeup_reason(
-              &pwrmgr, kDifPwrmgrWakeupRequestSourceFive)) == true);
+              &pwrmgr, aon_timer_wakeup_sources)) == true);
     // data preserved
     retention_sram_check((check_config_t){.do_write = false, .is_equal = true});
 
