df31f820e74dc9271fc52fe920c228cc2024fe02
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-04-06 18:23:22 +0100

    [rom_ctrl,dv] Use new rom_ctrl_fsm_vif, not the uvm_hdl_* functions
    Switch to using a bound-in interface to access the internals of the
    design.
    
    This also lets us get rid of the checker_fsm_state value from
    rom_ctrl_if (since we can now spot it from somewhere more local) and
    tidy up the wait_while_reading_low() task in rom_ctrl_common_vseq.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rom_ctrl/dv/env/rom_ctrl_if.sv
+++ hw/ip/rom_ctrl/dv/env/rom_ctrl_if.sv
@@ -8,7 +8,6 @@
 interface rom_ctrl_if (input logic clk_i, input logic rst_ni);
   rom_ctrl_pkg::pwrmgr_data_t pwrmgr_data;
   rom_ctrl_pkg::keymgr_data_t keymgr_data;
-  rom_ctrl_pkg::fsm_state_e   checker_fsm_state;
 
   clocking cb @(posedge clk_i);
     input pwrmgr_data;
