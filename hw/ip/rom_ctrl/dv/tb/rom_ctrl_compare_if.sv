ff3015991be7c0c6dc02ef2518522e7fdffe94e4
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-04-06 13:05:36 +0100

    [rom_ctrl,dv] Tidy up the way we try to corrupt the compare module
    I'm not really convinced that the original author knew what we were
    stimulating and testing. And the "CompareCtrConsistencyDone" branch
    didn't really check the right thing.
    
    Change things so that they make a bit more sense and add lots of
    comments to explain what is going on.
    
    Since we are touching the way that signals in the counter module get
    forced, switch to using the new rom_ctrl_compare_if interface (which
    lets us get rid of some uvm_hdl_* calls).
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rom_ctrl/dv/tb/rom_ctrl_compare_if.sv
+++ hw/ip/rom_ctrl/dv/tb/rom_ctrl_compare_if.sv
@@ -8,4 +8,55 @@
 
 interface rom_ctrl_compare_if ();
 
+  // Return whether the current FSM state is Waiting
+  function automatic logic is_waiting();
+    return u_compare.state_q == u_compare.Waiting;
+  endfunction
+
+  // Return the width of the addresses used in the compare module
+  function automatic int unsigned get_AW();
+    return u_compare.AW;
+  endfunction
+
+  // Return the top address used in the compare module (one less than the number of words in a
+  // digest)
+  function automatic int unsigned get_last_addr();
+    return u_compare.LastAddr;
+  endfunction
+
+  // Force addr_q to the given value for a cycle, releasing the signal and returning on the next
+  // negedge.
+  task static force_addr_q(int unsigned value);
+    force u_compare.addr_q = value;
+    @(negedge u_compare.clk_i);
+    release u_compare.addr_q;
+  endtask
+
+  // Override the FSM with an invalid state for a cycle, releasing the signal and returning on the
+  // next negedge.
+  task static splat_fsm_state();
+    // Setting state_d directly is a bit fiddly because the enum type isn't in scope. But we know
+    // that the valid states are separated by a hamming distance of at least 3, so we can just
+    // invert one of the bits of the signal for a cycle and will know that we're setting an invalid
+    // value.
+    force u_compare.state_d[0] = ~u_compare.state_d[0];
+    @(negedge u_compare.clk_i);
+    release u_compare.state_d[0];
+  endtask
+
+  // Override the next FSM state for a single cycle to be Done, releasing the signal and returning
+  // on the next negedge.
+  task static set_fsm_done();
+    force u_compare.state_d = u_compare.Done;
+    @(negedge u_compare.clk_i);
+    release u_compare.state_d;
+  endtask
+
+  // Wait until addr_q becomes the given value, then return on the next negedge (mainly so that it's
+  // easy to see what's going on in the waveforms)
+  task automatic wait_addr_q(int unsigned value);
+    wait(u_compare.addr_q == value);
+    @(negedge u_compare.clk_i);
+  endtask
+
 endinterface
