dffa43a2e39108cddeabced2b849fe2a843aa140
Tim Trippel <ttrippel@google.com>
2025-04-07 14:26:43 -0700

    [opentitanlib] add GPIO indicator pin to SpiConsoleDevice
    Currently the SpiConsoleDevice polls the DUT's SPI device buffer for
    data that it wants to transmit out of the device to the host. To support
    less noisy, non-polling environments, this adds an indicator GPIO pin
    that can alternatively be sampled to indicate when data is ready to be
    transmitted out of the device.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/host/tests/crypto/ecdsa_kat/src/main.rs
+++ sw/host/tests/crypto/ecdsa_kat/src/main.rs
@@ -511,7 +511,7 @@ fn run_ecdsa_testcase(
 
 fn test_ecdsa(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
     let spi = transport.spi("BOOTSTRAP")?;
-    let spi_console_device = SpiConsoleDevice::new(&*spi)?;
+    let spi_console_device = SpiConsoleDevice::new(&*spi, None)?;
     let _ = UartConsole::wait_for(&spi_console_device, r"Running [^\r\n]*", opts.timeout)?;
 
     let mut test_counter = 0u32;
