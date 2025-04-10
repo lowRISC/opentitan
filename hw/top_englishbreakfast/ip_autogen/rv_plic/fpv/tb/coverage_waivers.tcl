6ec970c6e7a04f7c67fb87b04bff8b504ffe9a39
Kinza Qamar <kqzaman@lowrisc.org>
2025-04-10 14:00:41 +0100

    [rv_plic, fpv] Statement coverage affected by parasitic state
    This is the same code waived in #26613 as dead branch but it still
    appears as dead statement. Removing the -type argument will waive
    the code for all types of coverages
    
    Signed-off-by: Kinza Qamar <kqzaman@lowrisc.org>

--- hw/top_englishbreakfast/ip_autogen/rv_plic/fpv/tb/coverage_waivers.tcl
+++ hw/top_englishbreakfast/ip_autogen/rv_plic/fpv/tb/coverage_waivers.tcl
@@ -4,7 +4,7 @@
 
 # This line is a default case statement which could be executed if a parasitic state has been
 # injected to the state register of u_prim_alert_sender.
-check_cov -waiver -add -start_line 249 -end_line 249 -type {branch} -instance {dut.gen_alert_tx[0].u_prim_alert_sender} -comment {No parasitic state injection while doing FPV}
+check_cov -waiver -add -start_line 249 -end_line 249 -instance {dut.gen_alert_tx[0].u_prim_alert_sender} -comment {No parasitic state injection while doing FPV}
 
 # The intention to add this assertion is to make sure that while doing FPV there is not a
 # possibility to inject parasitic state to the state register of alert sender FSM.
