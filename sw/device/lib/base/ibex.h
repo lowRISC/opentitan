c2e9f8f02332c84c8ff7118dd269dc0aa4cfa4ef
h-filali <h.filali98@gmail.com>
2025-04-14 08:07:09 +0000

    [sw/libbase] Move ibex driver from cryptolib to the base lib
    This commit moves the ibex driver from cryptolib to the base lib
    and removes any cryptolib dependencies from the ibex driver.
    
    Signed-off-by: h-filali <h.filali98@gmail.com>

--- sw/device/lib/base/ibex.h
+++ sw/device/lib/base/ibex.h
@@ -1,14 +1,14 @@
 // Copyright lowRISC contributors (OpenTitan project).
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
-#ifndef OPENTITAN_SW_DEVICE_LIB_CRYPTO_DRIVERS_IBEX_H_
-#define OPENTITAN_SW_DEVICE_LIB_CRYPTO_DRIVERS_IBEX_H_
+#ifndef OPENTITAN_SW_DEVICE_LIB_BASE_IBEX_H_
+#define OPENTITAN_SW_DEVICE_LIB_BASE_IBEX_H_
 
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
-#include "sw/device/lib/crypto/impl/status.h"
+#include "sw/device/lib/base/status.h"
 
 /**
  * Wait for random data in the RND_DATA CSR to be valid.
@@ -39,4 +39,4 @@ status_t ibex_rnd_data_read(uint32_t *rnd_data);
  */
 status_t ibex_rnd_status_read(uint32_t *rnd_status);
 
-#endif  // OPENTITAN_SW_DEVICE_LIB_CRYPTO_DRIVERS_IBEX_H_
+#endif  // OPENTITAN_SW_DEVICE_LIB_BASE_IBEX_H_
