47374bd4983e853fc677bb44bf00d049e1122295
h-filali <h.filali98@gmail.com>
2025-04-14 08:07:09 +0000

    [sw/libbase] Move ibex driver from cryptolib to the base lib
    This commit moves the ibex driver from cryptolib to the base lib
    and removes any cryptolib dependencies from the ibex driver.
    
    Signed-off-by: h-filali <h.filali98@gmail.com>

--- sw/device/lib/base/ibex.c
+++ sw/device/lib/base/ibex.c
@@ -2,12 +2,12 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include "sw/device/lib/crypto/drivers/ibex.h"
+#include "sw/device/lib/base/ibex.h"
 
 #include "sw/device/lib/base/abs_mmio.h"
 #include "sw/device/lib/base/bitfield.h"
 #include "sw/device/lib/base/csr.h"
-#include "sw/device/lib/crypto/impl/status.h"
+#include "sw/device/lib/base/status.h"
 #include "sw/device/silicon_creator/lib/base/sec_mmio.h"
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
@@ -21,17 +21,17 @@ status_t ibex_wait_rnd_valid(void) {
   while (true) {
     uint32_t reg = abs_mmio_read32(kBase + RV_CORE_IBEX_RND_STATUS_REG_OFFSET);
     if (bitfield_bit32_read(reg, RV_CORE_IBEX_RND_STATUS_RND_DATA_VALID_BIT)) {
-      return OTCRYPTO_OK;
+      return OK_STATUS();
     }
   }
 }
 
 status_t ibex_rnd_status_read(uint32_t *rnd_status) {
   *rnd_status = abs_mmio_read32(kBase + RV_CORE_IBEX_RND_STATUS_REG_OFFSET);
-  return OTCRYPTO_OK;
+  return OK_STATUS();
 }
 
 status_t ibex_rnd_data_read(uint32_t *rnd_data) {
   *rnd_data = abs_mmio_read32(kBase + RV_CORE_IBEX_RND_DATA_REG_OFFSET);
-  return OTCRYPTO_OK;
+  return OK_STATUS();
 }
