7bb65086a358be39a2ad4beedc17b708ca807326
Tim Trippel <ttrippel@google.com>
2025-04-15 11:39:28 -0700

    [individualize] use SPI console GPIO TX-indicator pin
    This updates the individualization firmware to use the SPI console GPIO
    TX-indicator pin to reduce the amount of traffic on the SPI console to
    simplify ATE integrations.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>

--- sw/host/provisioning/ft_lib/src/lib.rs
+++ sw/host/provisioning/ft_lib/src/lib.rs
@@ -18,6 +18,7 @@ use ft_ext_lib::ft_ext;
 use opentitanlib::app::TransportWrapper;
 use opentitanlib::console::spi::SpiConsoleDevice;
 use opentitanlib::dif::lc_ctrl::{DifLcCtrlState, LcCtrlReg};
+use opentitanlib::io::gpio::{PinMode, PullMode};
 use opentitanlib::io::jtag::{JtagParams, JtagTap};
 use opentitanlib::test_utils::init::InitializeTest;
 use opentitanlib::test_utils::lc_transition::trigger_lc_transition;
@@ -86,10 +87,17 @@ pub fn run_sram_ft_individualize(
     reset_delay: Duration,
     sram_program: &SramProgramParams,
     ft_individualize_data_in: &ManufFtIndividualizeData,
-    spi_console: &SpiConsoleDevice,
+    console_spi: &String,
     timeout: Duration,
     ujson_payloads: &mut UjsonPayloads,
 ) -> Result<()> {
+    // Setup the SPI console with the GPIO TX indicator pin.
+    let spi = transport.spi(console_spi)?;
+    let device_console_tx_ready_pin = &transport.gpio_pin("IOA5")?;
+    device_console_tx_ready_pin.set_mode(PinMode::Input)?;
+    device_console_tx_ready_pin.set_pull_mode(PullMode::None)?;
+    let spi_console = SpiConsoleDevice::new(&*spi, Some(device_console_tx_ready_pin))?;
+
     // Set CPU TAP straps, reset, and connect to the JTAG interface.
     transport.pin_strapping("PINMUX_TAP_RISCV")?.apply()?;
     transport.reset_target(reset_delay, true)?;
@@ -114,7 +122,7 @@ pub fn run_sram_ft_individualize(
 
     // Wait for SRAM program to complete execution.
     let _ = UartConsole::wait_for(
-        spi_console,
+        &spi_console,
         r"Waiting for FT SRAM provisioning data ...",
         timeout,
     )?;
@@ -122,11 +130,11 @@ pub fn run_sram_ft_individualize(
     // Inject provisioning data into the device.
     ujson_payloads.dut_in.insert(
         "FT_INDIVIDUALIZE_DATA_IN".to_string(),
-        ft_individualize_data_in.send(spi_console)?,
+        ft_individualize_data_in.send(&spi_console)?,
     );
 
     // Wait for provisioning operations to complete.
-    let _ = UartConsole::wait_for(spi_console, r"FT SRAM provisioning done.", timeout)?;
+    let _ = UartConsole::wait_for(&spi_console, r"FT SRAM provisioning done.", timeout)?;
 
     Ok(())
 }
