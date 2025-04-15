46bd005332801f8a01da78e436c3c7553fa00ae6
Antonio Martinez Zambrana <amz@lowrisc.org>
2025-04-15 16:31:46 +0100

    [dv, spi_agent] adding host analysis port to sequencer
    This way the sequences that are used as a test can look directly look into data
    sampled at the monitor, instead of duplicating the monitoring the monitor does
    in the driver.
    
    The sequencer will populate the queue `hot_mon_item_q` every time the monitor
    writes and item into the analysis port
    
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/dv/sv/spi_agent/spi_sequencer.sv
+++ hw/dv/sv/spi_agent/spi_sequencer.sv
@@ -5,5 +5,22 @@
 class spi_sequencer extends dv_base_sequencer#(spi_item, spi_agent_cfg, spi_item);
   `uvm_component_utils(spi_sequencer)
 
-  `uvm_component_new
+  // Analysis imp to receive items from host AP in monitor
+  uvm_analysis_imp#(spi_item, spi_sequencer) host_mon_analysis_imp;
+
+  // Holds up to 1 item at a time sent by the monitor
+  spi_item host_mon_item_q[$];
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+    host_mon_analysis_imp = new("mon_analysis_imp", this);
+  endfunction : new
+
+  virtual function void write(spi_item item);
+    `uvm_info(`gfn, $sformatf("Received host item from monitor: \n%0s", item.sprint), UVM_DEBUG)
+    // Clear the queue then push the item
+    host_mon_item_q = {};
+    host_mon_item_q.push_back(item);
+  endfunction
+
 endclass : spi_sequencer
