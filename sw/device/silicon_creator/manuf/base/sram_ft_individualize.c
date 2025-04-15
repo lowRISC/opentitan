e6719148491d3285d4ee0c2c19a205e0c27db758
Tim Trippel <ttrippel@google.com>
2025-04-15 11:51:36 -0700

    [individualize] remove SRAM execution done print
    When SRAM programs complete execution they set the stack pointer to a
    magic value. We can detect this magic value instead of requiring the
    host to wait until a message is printed over the console. This eliminate
    more unneeded traffic on the SPI console.
    
    Signed-off-by: Tim Trippel <ttrippel@google.com>
    (cherry picked from commit 828c74470ee954ba59d41f3f65baed82cddc8527)

--- sw/device/silicon_creator/manuf/base/sram_ft_individualize.c
+++ sw/device/silicon_creator/manuf/base/sram_ft_individualize.c
@@ -167,8 +167,6 @@ static status_t provision(ujson_t *uj) {
   TRY(manuf_individualize_device_owner_sw_cfg(&otp_ctrl));
   TRY(manuf_individualize_device_creator_sw_cfg(&otp_ctrl, &flash_ctrl_state));
 
-  base_printf("FT SRAM provisioning done.\r\n");
-
   return OK_STATUS();
 }
 
