zerorisc/expo

[RACL] Fix RACL ranges.
A previous commit changed RACL ranges from a parameter to a signal,
however the topgen templates were not updated.

This commit fixes RACL ranges up such that they are working again with
topgen:

* RACL range support was added to bus_interfaces.py such that the
  template can know which interface uses RACL ranges.

* The parameter RaclPolicySelRangesRamNum was removed from the ip hjson
  and its name aligned with what the template previously generated.
  The template now also generates the necessary default value if RACL
  ranges are disabled.

* toplevel_racl.tpl was split into two files to allow generating the
  necessary parameters and signals:
  toplevel_racl_parameters.tpl and toplevel_racl_signals.tpl

* The toplevel_racl_pg.sv.tpl has been updated to only emit RACL range
  parameters for interfaces that support it. And the format was slightly
  adjusted to not generate lines that are too long.

Signed-off-by: David Schrammel <davidschrammel@rivosinc.com>

util/topgen/templates/toplevel_racl_signals.tpl
@@ -1,0 +1,21 @@
+## Copyright lowRISC contributors (OpenTitan project).
+## Licensed under the Apache License, Version 2.0, see LICENSE for details.
+## SPDX-License-Identifier: Apache-2.0
+<%page args="m, top, block"/>\
+% for if_name in (x.get('name') for x in block.bus_interfaces.as_dicts()):
+%   if if_name and block.bus_interfaces.racl_range_support.get(if_name):
+<%
+      mappings        = m.get('racl_mappings', {}).get(if_name, {})
+      range_mapping   = mappings.get('range_mapping', [])
+      racl_group      = mappings.get('racl_group')
+      group_suffix    = f"_{racl_group.upper()}" if racl_group and racl_group != "Null" else ""
+      if_suffix       = f"_{if_name.upper()}" if if_name else ""
+      policy_sel_name = f"{m['name'].upper()}{group_suffix}{if_suffix}"
+%>\
+%     if len(range_mapping) > 0:
+      .racl_policy_sel_ranges_${if_name}_i(RACL_POLICY_SEL_RANGES_${policy_sel_name}),
+%     else:
+      .racl_policy_sel_ranges_${if_name}_i(top_racl_pkg::RACL_RANGE_T_DEFAULT),
+%     endif
+%   endif
+% endfor

lowRISC/opentitan

util/topgen/templates/toplevel_racl_signals.tpl
@@ -1,0 +1,21 @@
+## Copyright lowRISC contributors (OpenTitan project).
+## Licensed under the Apache License, Version 2.0, see LICENSE for details.
+## SPDX-License-Identifier: Apache-2.0
+<%page args="module, top, block"/>\
+% for if_name in (x.get('name') for x in block.bus_interfaces.as_dicts()):
+%   if if_name and block.bus_interfaces.racl_range_support.get(if_name):
+<%
+      mappings        = module.get('racl_mappings', {}).get(if_name, {})
+      range_mapping   = mappings.get('range_mapping', [])
+      racl_group      = mappings.get('racl_group')
+      group_suffix    = f"_{racl_group.upper()}" if racl_group and racl_group != "Null" else ""
+      if_suffix       = f"_{if_name.upper()}" if if_name else ""
+      policy_sel_name = f"{module['name'].upper()}{group_suffix}{if_suffix}"
+%>\
+%     if len(range_mapping) > 0:
+      .racl_policy_sel_ranges_${if_name}_i(RACL_POLICY_SEL_RANGES_${policy_sel_name}),
+%     else:
+      .racl_policy_sel_ranges_${if_name}_i(top_racl_pkg::RACL_RANGE_T_DEFAULT),
+%     endif
+%   endif
+% endfor