6ea7bd55c541cf2fbf414983e8ec4354818d8ffa
Antonio Martinez Zambrana <amz@lowrisc.org>
2025-04-19 14:38:04 +0100

    [dv, ip_templates] flash_ctrl_erase_suspend_vseq correct foreach syntax
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/ip_templates/flash_ctrl/dv/env/seq_lib/flash_ctrl_erase_suspend_vseq.sv
+++ hw/ip_templates/flash_ctrl/dv/env/seq_lib/flash_ctrl_erase_suspend_vseq.sv
@@ -120,7 +120,7 @@ class flash_ctrl_erase_suspend_vseq extends flash_ctrl_base_vseq;
 
       mp_info_pages[i][j].size() == flash_ctrl_pkg::InfoTypeSize[j];
 
-      foreach (mp_info_pages[i][j][k]) {
+      foreach (mp_info_pages[i, j, k]) {
 
         mp_info_pages[i][j][k].en == MuBi4True;
 
