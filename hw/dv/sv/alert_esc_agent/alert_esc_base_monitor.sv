47893f99d2cbb9339b922fc709651f7d9066d8c7
Antonio Martinez Zambrana <amz@lowrisc.org>
2025-04-21 10:12:39 +0100

    [dv, alert_esc_base_monitor] call super. in run_phase
    Signed-off-by: Antonio Martinez Zambrana <amz@lowrisc.org>

--- hw/dv/sv/alert_esc_agent/alert_esc_base_monitor.sv
+++ hw/dv/sv/alert_esc_agent/alert_esc_base_monitor.sv
@@ -38,6 +38,7 @@ function void alert_esc_base_monitor::build_phase(uvm_phase phase);
 endfunction : build_phase
 
 task alert_esc_base_monitor::run_phase(uvm_phase phase);
+  super.run_phase(phase);
   wait_for_reset_done();
 endtask : run_phase
 
