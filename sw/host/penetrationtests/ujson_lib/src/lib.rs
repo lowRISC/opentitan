7a7e8689ff2f1dc0f60e3f4e0df5a02d19d8aaec
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-04-24 16:15:30 +0200

    [pentest] Add test framework for IbexFi
    This commit enables automatic testing of the IbexFi
    tests of the pentest framework.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit 6383e6f)

---
+++ sw/host/penetrationtests/ujson_lib/src/lib.rs
@@ -0,0 +1,6 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+pub mod commands;
+pub mod fi_ibex_commands;
+pub mod pentest_lib_commands;
