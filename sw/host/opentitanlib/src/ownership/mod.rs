0a30693b62673e89187785f99354d90c627747b5
Anthony Chen <antchen@google.com>
2025-04-25 20:31:50 +0800

    [opentitantool] Add `ownership detached-signature` command
    This introduces a new `ownership detached-signature` subcommand to
    `opentitantool`. This command allows users to generate a detached
    signature binary from the existing raw signature files
    
    Signed-off-by: Anthony Chen <antchen@google.com>

--- sw/host/opentitanlib/src/ownership/mod.rs
+++ sw/host/opentitanlib/src/ownership/mod.rs
@@ -17,7 +17,9 @@ pub use application_key::{ApplicationKeyDomain, OwnerApplicationKey};
 pub use flash::{FlashFlags, OwnerFlashConfig, OwnerFlashRegion};
 pub use flash_info::{OwnerFlashInfoConfig, OwnerInfoPage};
 pub use isfb::OwnerIsfbConfig;
-pub use misc::{HybridRawPublicKey, KeyMaterial, OwnershipKeyAlg, TlvHeader, TlvTag};
+pub use misc::{
+    DetachedSignatureCommand, HybridRawPublicKey, KeyMaterial, OwnershipKeyAlg, TlvHeader, TlvTag,
+};
 pub use owner::{OwnerBlock, OwnerConfigItem, SramExecMode};
 pub use rescue::{CommandTag, OwnerRescueConfig, RescueProtocol};
 pub use signature::DetachedSignature;
