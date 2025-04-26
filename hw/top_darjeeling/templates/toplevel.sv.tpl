zerorisc/expo

[RACL] Fix RACL ranges.
A previous commit changed RACL ranges from a parameter to a signal,
however the topgen templates were not updated.

This commit fixes RACL ranges up such that they are working again with
topgen:

* RACL range support was added to bus_interfaces.py such that the
  template can know which interface uses RACL ranges.

* The parameter RaclPolicySelRangesRamNum was removed from the ip hjson
  and its name aligned with what the template previously generated.
  The template now also generates the necessary default value if RACL
  ranges are disabled.

* toplevel_racl.tpl was split into two files to allow generating the
  necessary parameters and signals:
  toplevel_racl_parameters.tpl and toplevel_racl_signals.tpl

* The toplevel_racl_pg.sv.tpl has been updated to only emit RACL range
  parameters for interfaces that support it. And the format was slightly
  adjusted to not generate lines that are too long.

Signed-off-by: David Schrammel <davidschrammel@rivosinc.com>

hw/top_darjeeling/templates/toplevel.sv.tpl
@@ -494,7 +494,7 @@ max_intrwidth = (max(len(x.name) for x in block.interrupts)
 %>\
   % if m["param_list"] or block.alerts:
   ${m["type"]} #(
-<%include file="/toplevel_racl.tpl" args="m=m,top=top"/>\
+<%include file="/toplevel_racl_parameters.tpl" args="m=m,top=top,block=block"/>\
   % if block.alerts:
 <%
 w = len(block.alerts)
@@ -567,6 +567,7 @@ slice = f"{lo+w-1}:{lo}"
       .alert_rx_i  ( alert_rx[${slice}] ),
       % endif
     % endif
+<%include file="/toplevel_racl_signals.tpl" args="m=m,top=top,block=block"/>\
     ## TODO: Inter-module Connection
     % if m.get('inter_signal_list'):
 

hw/top_earlgrey/templates/toplevel.sv.tpl
@@ -507,7 +507,7 @@ max_intrwidth = (max(len(x.name) for x in block.interrupts)
 %>\
   % if m["param_list"] or block.alerts:
   ${m["type"]} #(
-<%include file="/toplevel_racl.tpl" args="m=m,top=top"/>\
+<%include file="/toplevel_racl_parameters.tpl" args="m=m,top=top,block=block"/>\
   % if block.alerts:
 <%
 w = len(block.alerts)
@@ -580,6 +580,7 @@ slice = f"{lo+w-1}:{lo}"
       .alert_rx_i  ( alert_rx[${slice}] ),
       % endif
     % endif
+<%include file="/toplevel_racl_signals.tpl" args="m=m,top=top,block=block"/>\
     ## TODO: Inter-module Connection
     % if m.get('inter_signal_list'):
 

lowRISC/opentitan

hw/top_darjeeling/templates/toplevel.sv.tpl
@@ -494,7 +494,7 @@ max_intrwidth = (max(len(x.name) for x in block.interrupts)
 %>\
   % if m["param_list"] or block.alerts:
   ${m["type"]} #(
-<%include file="/toplevel_racl.tpl" args="m=m,top=top"/>\
+<%include file="/toplevel_racl_parameters.tpl" args="module=m,top=top,block=block"/>\
   % if block.alerts:
 <%
 w = len(block.alerts)
@@ -567,6 +567,7 @@ slice = f"{lo+w-1}:{lo}"
       .alert_rx_i  ( alert_rx[${slice}] ),
       % endif
     % endif
+<%include file="/toplevel_racl_signals.tpl" args="module=m,top=top,block=block"/>\
     ## TODO: Inter-module Connection
     % if m.get('inter_signal_list'):
 

hw/top_earlgrey/templates/toplevel.sv.tpl
@@ -507,7 +507,7 @@ max_intrwidth = (max(len(x.name) for x in block.interrupts)
 %>\
   % if m["param_list"] or block.alerts:
   ${m["type"]} #(
-<%include file="/toplevel_racl.tpl" args="m=m,top=top"/>\
+<%include file="/toplevel_racl_parameters.tpl" args="module=m,top=top,block=block"/>\
   % if block.alerts:
 <%
 w = len(block.alerts)
@@ -580,6 +580,7 @@ slice = f"{lo+w-1}:{lo}"
       .alert_rx_i  ( alert_rx[${slice}] ),
       % endif
     % endif
+<%include file="/toplevel_racl_signals.tpl" args="module=m,top=top,block=block"/>\
     ## TODO: Inter-module Connection
     % if m.get('inter_signal_list'):
 