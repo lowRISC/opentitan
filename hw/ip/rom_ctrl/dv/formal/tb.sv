2ccfeb4733eb8caf112166c305a699dc1e78c907
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-04-29 16:02:15 +0100

    [rom_ctrl,fpv] Reword some prim_count stability checks
    Detailed notes in the testbench and TCL code.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rom_ctrl/dv/formal/tb.sv
+++ hw/ip/rom_ctrl/dv/formal/tb.sv
@@ -53,4 +53,20 @@ module tb
           dut.u_reg_regs.reg_we_check ->
           dut.u_reg_regs.reg_addr == rom_ctrl_reg_pkg::ROM_CTRL_ALERT_TEST_OFFSET)
 
+  // The prim_count module has logic to saturate if asked to count past its maximum (so long as that
+  // maximum is 2^n-1 where n is the width of the counter). The rom_ctrl_compare module that uses
+  // prim_count to run through the digests doesn't use that feature: in fact, it only passes
+  // incr_en_i when not at the maximum.
+  //
+  // One advantage of this is that the digest length needn't be a power of two. A disadvantage is
+  // that we will never ask for an increment that would saturate. As a result, the preconditions for
+  // UpCntIncrStable_A and DnCntIncrStable_A are both impossible. Their coverage properties are
+  // disabled in verify.tcl but these assertions check that the gating in rom_ctrl_compare really
+  // does work as expected.
+  `ASSERT(CompareCountNoSaturateUp_A,
+          dut.gen_fsm_scramble_enabled.u_checker_fsm.u_compare.u_prim_count_addr.incr_en_i ->
+          !&dut.gen_fsm_scramble_enabled.u_checker_fsm.u_compare.u_prim_count_addr.cnt_o)
+  `ASSERT(CompareCountNoSaturateDn_A,
+          dut.gen_fsm_scramble_enabled.u_checker_fsm.u_compare.u_prim_count_addr.incr_en_i ->
+          |dut.gen_fsm_scramble_enabled.u_checker_fsm.u_compare.u_prim_count_addr.cnt_q[1])
 endmodule
