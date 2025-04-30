b74100ea13c0e2d238e66b0aa4f8b9c0031832e3
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-04-30 10:42:03 +0200

    [pentest] Add test framework for CryptoFi
    This commit enables automatic testing of all the CryptoFi tests.
    The responses are compared to the golden responses stored in
    sw/host/penetrationtests/testvectors/data.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/host/tests/penetrationtests/fi_crypto/src/main.rs
+++ sw/host/tests/penetrationtests/fi_crypto/src/main.rs
@@ -11,7 +11,7 @@ use clap::Parser;
 use serde::Deserialize;
 
 use pentest_commands::commands::PenetrationtestCommand;
-use pentest_commands::fi_ibex_commands::IbexFiSubcommand;
+use pentest_commands::fi_crypto_commands::CryptoFiSubcommand;
 
 use opentitanlib::app::TransportWrapper;
 use opentitanlib::execute_test;
@@ -30,16 +30,17 @@ struct Opts {
     timeout: Duration,
 
     #[arg(long, num_args = 1..)]
-    fi_ibex_json: Vec<String>,
+    fi_crypto_json: Vec<String>,
 }
 
 #[derive(Debug, Deserialize)]
-struct FiIbexTestCase {
+struct FiCryptoTestCase {
     test_case_id: usize,
     command: String,
-    // Input only needed for the "Init" subcommand.
     #[serde(default)]
     input: String,
+    #[serde(default)]
+    mode: String,
     expected_output: String,
 }
 
@@ -51,13 +52,15 @@ fn filter_response(response: serde_json::Value) -> serde_json::Map<String, serde
     map.remove("err_status");
     // Device ID is different for each device.
     map.remove("device_id");
-    // Register file dump could change.
-    map.remove("registers");
+    // Shares can be random.
+    map.remove("share0");
+    map.remove("share1");
+
     map
 }
 
-fn run_fi_ibex_testcase(
-    test_case: &FiIbexTestCase,
+fn run_fi_crypto_testcase(
+    test_case: &FiCryptoTestCase,
     opts: &Opts,
     uart: &dyn Uart,
     fail_counter: &mut u32,
@@ -67,17 +70,23 @@ fn run_fi_ibex_testcase(
         test_case.test_case_id,
         test_case.command
     );
-    PenetrationtestCommand::IbexFi.send(uart)?;
+    PenetrationtestCommand::CryptoFi.send(uart)?;
 
     // Send test subcommand.
-    IbexFiSubcommand::from_str(test_case.command.as_str())
-        .context("unsupported Ibex FI subcommand")?
+    CryptoFiSubcommand::from_str(test_case.command.as_str())
+        .context("unsupported Crypto FI subcommand")?
         .send(uart)?;
 
     // Check if we need to send an input.
     if !test_case.input.is_empty() {
         let input: serde_json::Value = serde_json::from_str(test_case.input.as_str()).unwrap();
         input.send(uart)?;
+    }
+
+    // Check if we need to send a mode.
+    if !test_case.mode.is_empty() {
+        let mode: serde_json::Value = serde_json::from_str(test_case.mode.as_str()).unwrap();
+        mode.send(uart)?;
     }
 
     // Get test output & filter.
@@ -104,21 +113,21 @@ fn run_fi_ibex_testcase(
     Ok(())
 }
 
-fn test_fi_ibex(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
+fn test_fi_crypto(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
     let uart = transport.uart("console")?;
     uart.set_flow_control(true)?;
     let _ = UartConsole::wait_for(&*uart, r"Running [^\r\n]*", opts.timeout)?;
 
     let mut test_counter = 0u32;
     let mut fail_counter = 0u32;
-    let test_vector_files = &opts.fi_ibex_json;
+    let test_vector_files = &opts.fi_crypto_json;
     for file in test_vector_files {
         let raw_json = fs::read_to_string(file)?;
-        let fi_ibex_tests: Vec<FiIbexTestCase> = serde_json::from_str(&raw_json)?;
-        for fi_ibex_test in &fi_ibex_tests {
+        let fi_crypto_tests: Vec<FiCryptoTestCase> = serde_json::from_str(&raw_json)?;
+        for fi_crypto_test in &fi_crypto_tests {
             test_counter += 1;
             log::info!("Test counter: {}", test_counter);
-            run_fi_ibex_testcase(fi_ibex_test, opts, &*uart, &mut fail_counter)?;
+            run_fi_crypto_testcase(fi_crypto_test, opts, &*uart, &mut fail_counter)?;
         }
     }
     assert_eq!(
@@ -134,6 +143,6 @@ fn main() -> Result<()> {
     opts.init.init_logging();
 
     let transport = opts.init.init_target()?;
-    execute_test!(test_fi_ibex, &opts, &transport);
+    execute_test!(test_fi_crypto, &opts, &transport);
     Ok(())
 }
