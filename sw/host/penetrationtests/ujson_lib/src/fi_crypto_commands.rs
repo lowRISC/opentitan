4ef143fcbb8d57b618d9a5dd64d2ef919f2a8b6c
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-04-30 10:42:03 +0200

    [pentest] Add test framework for CryptoFi
    This commit enables automatic testing of all the CryptoFi tests.
    The responses are compared to the golden responses stored in
    sw/host/penetrationtests/testvectors/data.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/host/penetrationtests/ujson_lib/src/fi_crypto_commands.rs
+++ sw/host/penetrationtests/ujson_lib/src/fi_crypto_commands.rs
@@ -1,7 +1,4 @@
 // Copyright lowRISC contributors (OpenTitan project).
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
-pub mod commands;
-pub mod fi_crypto_commands;
-pub mod fi_ibex_commands;
-pub mod pentest_lib_commands;
+include!(env!("fi_crypto_commands_loc"));
