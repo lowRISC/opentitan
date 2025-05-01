51efadd6026281f0ff0c85659a8a69e14f3b39f6
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-05-01 09:02:08 +0000

    [test] Remove hardcoded constant from clkmgr_off_peri_test
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/tests/clkmgr_off_peri_test.c
+++ sw/device/tests/clkmgr_off_peri_test.c
@@ -97,9 +97,12 @@ static void test_gateable_clocks_off(const dif_clkmgr_t *clkmgr,
   CHECK_DIF_OK(
       dif_clkmgr_gateable_clock_set_enabled(clkmgr, clock, kDifToggleEnabled));
   // Enable watchdog bite reset.
-  CHECK_DIF_OK(dif_pwrmgr_set_request_sources(pwrmgr, kDifPwrmgrReqTypeReset,
-                                              kDifPwrmgrResetRequestSourceTwo,
-                                              kDifToggleEnabled));
+  dif_pwrmgr_request_sources_t reset_sources;
+  CHECK_DIF_OK(dif_pwrmgr_find_request_source(
+      pwrmgr, kDifPwrmgrReqTypeReset, dt_aon_timer_instance_id(kDtAonTimerAon),
+      kDtAonTimerResetReqAonTimer, &reset_sources));
+  CHECK_DIF_OK(dif_pwrmgr_set_request_sources(
+      pwrmgr, kDifPwrmgrReqTypeReset, reset_sources, kDifToggleEnabled));
   LOG_INFO("Testing peripheral clock %d, with unit %s", clock,
            peri_context[clock].peripheral_name);
 
