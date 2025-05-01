33627fb72944674623b25d112a0986686f01b339
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-05-01 13:13:34 +0200

    [pentest] Add test framework for OtbnSca
    This commit enables automatic testing of all the OtbnSca tests.
    The responses are compared to the golden responses stored in
    sw/host/penetrationtests/testvectors/data.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit 6c53d6c)

--- sw/device/tests/penetrationtests/firmware/sca/otbn_sca.c
+++ sw/device/tests/penetrationtests/firmware/sca/otbn_sca.c
@@ -217,6 +217,8 @@ static status_t p256_ecdsa_sign(const uint32_t *msg,
 status_t handle_otbn_sca_ecdsa_p256_sign(ujson_t *uj) {
   // Get masks off or on.
   penetrationtest_otbn_sca_en_masks_t uj_data_masks;
+  TRY(ujson_deserialize_penetrationtest_otbn_sca_en_masks_t(uj,
+                                                            &uj_data_masks));
 
   // Get message and key.
   penetrationtest_otbn_sca_ecdsa_p256_sign_t uj_data;
@@ -631,7 +633,6 @@ status_t handle_otbn_sca_rsa512_decrypt(ujson_t *uj) {
   // Get RSA256 parameters.
   penetrationtest_otbn_sca_rsa512_dec_t uj_data;
   TRY(ujson_deserialize_penetrationtest_otbn_sca_rsa512_dec_t(uj, &uj_data));
-
   otbn_load_app(kOtbnAppRsa);
 
   uint32_t mode = 2;  // Decrypt.
@@ -641,8 +642,8 @@ status_t handle_otbn_sca_rsa512_decrypt(ujson_t *uj) {
   // Write data into OTBN DMEM.
   TRY(dif_otbn_dmem_write(&otbn, kOtbnVarRsaMode, &mode, sizeof(mode)));
   TRY(dif_otbn_dmem_write(&otbn, kOtbnVarRsaNLimbs, &n_limbs, sizeof(n_limbs)));
-  TRY(dif_otbn_dmem_write(&otbn, kOtbnVarRsaModulus, uj_data.mod,
-                          sizeof(uj_data.mod)));
+  TRY(dif_otbn_dmem_write(&otbn, kOtbnVarRsaModulus, uj_data.modu,
+                          sizeof(uj_data.modu)));
   TRY(dif_otbn_dmem_write(&otbn, kOtbnVarRsaExp, uj_data.exp,
                           sizeof(uj_data.exp)));
   TRY(dif_otbn_dmem_write(&otbn, kOtbnVarRsaInOut, uj_data.msg,
