98e193ca6d8a73128cd135c67bfbce5dba8bae6e
Martin Velay <mvelay@lowrisc.org>
2025-05-05 11:49:24 +0000

    [soc_dbg_ctrl,dv] Fix JTAG CSR assert
    - Fix typo for soc_dbg_ctrl_jtag_csr_assert_fpv binding module
    
    Signed-off-by: Martin Velay <mvelay@lowrisc.org>

--- hw/ip/soc_dbg_ctrl/dv/sva/soc_dbg_ctrl_bind.sv
+++ hw/ip/soc_dbg_ctrl/dv/sva/soc_dbg_ctrl_bind.sv
@@ -15,8 +15,8 @@ module soc_dbg_ctrl_bind;
   bind soc_dbg_ctrl soc_dbg_ctrl_core_csr_assert_fpv soc_dbg_ctrl_core_csr_assert (
     .clk_i,
     .rst_ni,
-    .h2d    (core_tl_i),
-    .d2h    (core_tl_o)
+    .h2d  (core_tl_i),
+    .d2h  (core_tl_o)
   );
 
   bind soc_dbg_ctrl tlul_assert #(
@@ -31,7 +31,7 @@ module soc_dbg_ctrl_bind;
   bind soc_dbg_ctrl soc_dbg_ctrl_jtag_csr_assert_fpv soc_dbg_ctrl_jtag_csr_assert (
     .clk_i,
     .rst_ni,
-    .h2d    (core_tl_i),
-    .d2h    (core_tl_o)
+    .h2d  (jtag_tl_i),
+    .d2h  (jtag_tl_o)
   );
 endmodule : soc_dbg_ctrl_bind
