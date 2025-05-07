1507a824e7ca1d386c820432f59e7fb076e6c93d
Jade Philipoom <jadep@zerorisc.com>
2025-05-07 12:20:38 +0200

    [crypto] Move the RND driver back to cryptolib.
    After discussion in SW WG, we decided to keep a small RND driver in cryptolib
    and pass the read32 function as a pointer to hardening primitives from
    lib/base, to preserve hardware independence in lib/base while still avoiding
    dependencies from lib/base to cryptolib.
    
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>
    (cherry picked from commit 635ef8068ef557c730627dbf9bee8390164ec83c)

---
+++ sw/device/lib/crypto/drivers/rv_core_ibex.c
@@ -0,0 +1,33 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/crypto/drivers/rv_core_ibex.h"
+
+#include "sw/device/lib/base/abs_mmio.h"
+#include "sw/device/lib/base/bitfield.h"
+
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+#include "rv_core_ibex_regs.h"
+
+enum {
+  kBaseAddr = TOP_EARLGREY_RV_CORE_IBEX_CFG_BASE_ADDR,
+};
+
+/**
+ * Blocks until data is ready in the RND register.
+ */
+static void wait_rnd_valid(void) {
+  while (true) {
+    uint32_t reg =
+        abs_mmio_read32(kBaseAddr + RV_CORE_IBEX_RND_STATUS_REG_OFFSET);
+    if (bitfield_bit32_read(reg, RV_CORE_IBEX_RND_STATUS_RND_DATA_VALID_BIT)) {
+      return;
+    }
+  }
+}
+
+uint32_t ibex_rnd32_read(void) {
+  wait_rnd_valid();
+  return abs_mmio_read32(kBaseAddr + RV_CORE_IBEX_RND_DATA_REG_OFFSET);
+}
