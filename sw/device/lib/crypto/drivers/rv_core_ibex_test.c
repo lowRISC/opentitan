54352727137c1d3fad19044bd5620aa54e3a6f2e
Jade Philipoom <jadep@zerorisc.com>
2025-05-07 12:20:38 +0200

    [crypto] Move the RND driver back to cryptolib.
    After discussion in SW WG, we decided to keep a small RND driver in cryptolib
    and pass the read32 function as a pointer to hardening primitives from
    lib/base, to preserve hardware independence in lib/base while still avoiding
    dependencies from lib/base to cryptolib.
    
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>
    (cherry picked from commit 635ef8068ef557c730627dbf9bee8390164ec83c)

--- sw/device/lib/crypto/drivers/rv_core_ibex_test.c
+++ sw/device/lib/crypto/drivers/rv_core_ibex_test.c
@@ -1,15 +1,13 @@
 // Copyright lowRISC contributors (OpenTitan project).
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
-#include "sw/device/lib/base/ibex.h"
 
-#include "sw/device/lib/base/bitfield.h"
-#include "sw/device/lib/base/csr.h"
+#include "sw/device/lib/crypto/drivers/rv_core_ibex.h"
+
 #include "sw/device/lib/base/status.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
-#include "sw/device/silicon_creator/lib/base/sec_mmio.h"
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 #include "rv_core_ibex_regs.h"
@@ -25,22 +23,16 @@ static status_t ibex_entropy_test(void) {
   uint32_t rnd_data[2];
 
   // Read the initial value of the RND_DATA CSR.
-  TRY(ibex_rnd_data_read(rnd_data));
-  // Wait for RND_DATA to be valid again and check if RND_STATUS is as expected.
-  TRY(ibex_wait_rnd_valid());
-  TRY(ibex_rnd_status_read(&rnd_status));
-  TRY_CHECK(bitfield_bit32_read(rnd_status,
-                                RV_CORE_IBEX_RND_STATUS_RND_DATA_VALID_BIT));
+  uint32_t rnd_data0 = ibex_rnd32_read();
   // Read RND_DATA again and check if it changed.
-  TRY(ibex_rnd_data_read(&rnd_data[1]));
-  TRY_CHECK(rnd_data[0] != rnd_data[1]);
+  uint32_t rnd_data1 = ibex_rnd32_read();
+  TRY_CHECK(rnd_data0 != rnd_data1);
 
   return OK_STATUS();
 }
 
 bool test_main(void) {
   status_t result = OK_STATUS();
-  // Test RND_DATA and RND_STATUS CSR access.
   EXECUTE_TEST(result, ibex_entropy_test);
   return status_ok(result);
 }
