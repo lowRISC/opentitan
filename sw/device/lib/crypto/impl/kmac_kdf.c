583d8c4a776cb18db1278bc125f15fcda4c1420d
Jade Philipoom <jadep@zerorisc.com>
2025-05-19 16:55:03 +0200

    [crypto] Pass the blinded key to KMAC-KDF by reference.
    Most algorithms in cryptolib already do this, and it's necessary to update
    checksums.
    
    Signed-off-by: Jade Philipoom <jadep@zerorisc.com>

--- sw/device/lib/crypto/impl/kmac_kdf.c
+++ sw/device/lib/crypto/impl/kmac_kdf.c
@@ -15,12 +15,12 @@
 #define MODULE_ID MAKE_MODULE_ID('k', 'k', 'd')
 
 otcrypto_status_t otcrypto_kmac_kdf(
-    const otcrypto_blinded_key_t key_derivation_key,
+    otcrypto_blinded_key_t *key_derivation_key,
     const otcrypto_const_byte_buf_t label,
     const otcrypto_const_byte_buf_t context,
     otcrypto_blinded_key_t *output_key_material) {
   // Check NULL pointers.
-  if (key_derivation_key.keyblob == NULL || output_key_material == NULL ||
+  if (key_derivation_key->keyblob == NULL || output_key_material == NULL ||
       output_key_material->keyblob == NULL) {
     return OTCRYPTO_BAD_ARGS;
   }
@@ -41,43 +41,44 @@ otcrypto_status_t otcrypto_kmac_kdf(
   }
 
   // Check the private key checksum.
-  if (integrity_blinded_key_check(&key_derivation_key) != kHardenedBoolTrue) {
+  if (integrity_blinded_key_check(key_derivation_key) != kHardenedBoolTrue) {
     return OTCRYPTO_BAD_ARGS;
   }
 
   // Check `key_len` is supported by KMAC HWIP.
   // The set of supported key sizes is {128, 192, 256, 384, 512}.
-  HARDENED_TRY(kmac_key_length_check(key_derivation_key.config.key_length));
+  HARDENED_TRY(kmac_key_length_check(key_derivation_key->config.key_length));
 
   kmac_blinded_key_t kmac_key = {
       .share0 = NULL,
       .share1 = NULL,
-      .hw_backed = key_derivation_key.config.hw_backed,
-      .len = key_derivation_key.config.key_length,
+      .hw_backed = key_derivation_key->config.hw_backed,
+      .len = key_derivation_key->config.key_length,
   };
 
   // Validate key length of `key_derivation_key`.
-  if (key_derivation_key.config.hw_backed == kHardenedBoolTrue) {
+  if (key_derivation_key->config.hw_backed == kHardenedBoolTrue) {
     // Check that 1) key size matches sideload port size, 2) keyblob length
     // matches diversification length.
-    if (keyblob_share_num_words(key_derivation_key.config) * sizeof(uint32_t) !=
+    if (keyblob_share_num_words(key_derivation_key->config) *
+            sizeof(uint32_t) !=
         kKmacSideloadKeyLength / 8) {
       return OTCRYPTO_BAD_ARGS;
     }
     // Configure keymgr with diversification input and then generate the
     // sideload key.
     keymgr_diversification_t diversification;
-    // Diversification call also checks that `key_derivation_key.keyblob_length`
-    // is 8 words long.
-    HARDENED_TRY(keyblob_to_keymgr_diversification(&key_derivation_key,
+    // Diversification call also checks that
+    // `key_derivation_key->keyblob_length` is 8 words long.
+    HARDENED_TRY(keyblob_to_keymgr_diversification(key_derivation_key,
                                                    &diversification));
     HARDENED_TRY(keymgr_generate_key_kmac(diversification));
-  } else if (key_derivation_key.config.hw_backed == kHardenedBoolFalse) {
-    if (key_derivation_key.keyblob_length !=
-        keyblob_num_words(key_derivation_key.config) * sizeof(uint32_t)) {
+  } else if (key_derivation_key->config.hw_backed == kHardenedBoolFalse) {
+    if (key_derivation_key->keyblob_length !=
+        keyblob_num_words(key_derivation_key->config) * sizeof(uint32_t)) {
       return OTCRYPTO_BAD_ARGS;
     }
-    HARDENED_TRY(keyblob_to_shares(&key_derivation_key, &kmac_key.share0,
+    HARDENED_TRY(keyblob_to_shares(key_derivation_key, &kmac_key.share0,
                                    &kmac_key.share1));
   } else {
     return OTCRYPTO_BAD_ARGS;
@@ -100,9 +101,9 @@ otcrypto_status_t otcrypto_kmac_kdf(
     return OTCRYPTO_BAD_ARGS;
   }
 
-  switch (launder32(key_derivation_key.config.key_mode)) {
+  switch (launder32(key_derivation_key->config.key_mode)) {
     case kOtcryptoKeyModeKdfKmac128: {
-      HARDENED_CHECK_EQ(key_derivation_key.config.key_mode,
+      HARDENED_CHECK_EQ(key_derivation_key->config.key_mode,
                         kOtcryptoKeyModeKdfKmac128);
       // No need to further check key size against security level because
       // `kmac_key_length_check` ensures that the key is at least 128-bit.
@@ -113,11 +114,11 @@ otcrypto_status_t otcrypto_kmac_kdf(
       break;
     }
     case kOtcryptoKeyModeKdfKmac256: {
-      HARDENED_CHECK_EQ(key_derivation_key.config.key_mode,
+      HARDENED_CHECK_EQ(key_derivation_key->config.key_mode,
                         kOtcryptoKeyModeKdfKmac256);
       // Check that key size matches the security strength. It should be at
       // least 256-bit.
-      if (key_derivation_key.config.key_length < 256 / 8) {
+      if (key_derivation_key->config.key_length < 256 / 8) {
         return OTCRYPTO_BAD_ARGS;
       }
       HARDENED_TRY(kmac_kmac_256(
