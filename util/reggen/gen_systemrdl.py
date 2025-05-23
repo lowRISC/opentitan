8cb5c85d40264991e638e87b320a5592b6970e45
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-05-23 14:18:40 +0100

    [reggen] Wire up the RDLCompiler/RDLImporter stuff
    This doesn't actually do anything useful yet (the generated file just
    contains an empty address map!) but it *does* correctly connect up the
    rather complicated infrastructure.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- util/reggen/gen_systemrdl.py
+++ util/reggen/gen_systemrdl.py
@@ -5,8 +5,28 @@
 
 '''Generate a SystemRDL description of the block'''
 
+import os
+
 from reggen.ip_block import IpBlock
 
+from systemrdl.node import AddrmapNode
+from systemrdl.compiler import RDLEnvironment
+from systemrdl.importer import RDLImporter, RDLCompiler
+from peakrdl_systemrdl import exporter
+
+
 def gen(block: IpBlock, outdir: str) -> int:
-    print('gen_systemrdl')
+    out_file = os.path.join(outdir or '', 'reg.rdl')
+
+    comp = RDLCompiler()
+    imp = RDLImporter(comp)
+    imp.default_src_ref = None
+    exp = exporter.SystemRDLExporter()
+
+    empty_env = RDLEnvironment({})
+    node = AddrmapNode(block.to_systemrdl(imp),
+                       empty_env,
+                       None)
+
+    exp.export(node, out_file)
     return 0
