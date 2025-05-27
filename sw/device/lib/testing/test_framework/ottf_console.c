83f822858b642a39d02b9e596d2f52607035044a
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-05-27 14:27:25 +0000

    [ottf,console] Rename two variables
    These variables will become non-static so it's important to give
    them a name that won't clash.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/lib/testing/test_framework/ottf_console.c
+++ sw/device/lib/testing/test_framework/ottf_console.c
@@ -49,8 +49,8 @@ enum {
 };
 
 // Potential DIF handles for OTTF console communication.
-static dif_gpio_t gpio;
-static dif_pinmux_t pinmux;
+static dif_gpio_t ottf_console_gpio;
+static dif_pinmux_t ottf_console_pinmux;
 static dif_spi_device_handle_t ottf_console_spi_device;
 static dif_uart_t ottf_console_uart;
 
@@ -272,21 +272,21 @@ void ottf_console_configure_spi_device(uintptr_t base_addr) {
   // Setup TX GPIO if requested.
   if (kOttfTestConfig.console_tx_indicator.enable) {
     CHECK_DIF_OK(dif_gpio_init(
-        mmio_region_from_addr(TOP_EARLGREY_GPIO_BASE_ADDR), &gpio));
+        mmio_region_from_addr(TOP_EARLGREY_GPIO_BASE_ADDR), &ottf_console_gpio));
     CHECK_DIF_OK(dif_pinmux_init(
-        mmio_region_from_addr(TOP_EARLGREY_PINMUX_AON_BASE_ADDR), &pinmux));
+        mmio_region_from_addr(TOP_EARLGREY_PINMUX_AON_BASE_ADDR), &ottf_console_pinmux));
     CHECK_DIF_OK(dif_pinmux_output_select(
-        &pinmux, kOttfTestConfig.console_tx_indicator.spi_console_tx_ready_mio,
+        &ottf_console_pinmux, kOttfTestConfig.console_tx_indicator.spi_console_tx_ready_mio,
         kTopEarlgreyPinmuxOutselGpioGpio0 +
             kOttfTestConfig.console_tx_indicator.spi_console_tx_ready_gpio));
     CHECK_DIF_OK(dif_gpio_write(
-        &gpio, kOttfTestConfig.console_tx_indicator.spi_console_tx_ready_gpio,
+        &ottf_console_gpio, kOttfTestConfig.console_tx_indicator.spi_console_tx_ready_gpio,
         false));
     CHECK_DIF_OK(dif_gpio_output_set_enabled(
-        &gpio, kOttfTestConfig.console_tx_indicator.spi_console_tx_ready_gpio,
+        &ottf_console_gpio, kOttfTestConfig.console_tx_indicator.spi_console_tx_ready_gpio,
         true));
     base_spi_device_set_gpio_tx_indicator(
-        &gpio, kOttfTestConfig.console_tx_indicator.spi_console_tx_ready_gpio);
+        &ottf_console_gpio, kOttfTestConfig.console_tx_indicator.spi_console_tx_ready_gpio);
     spi_device_clear_flash_buffer(&ottf_console_spi_device);
   } else {
     spi_device_wait_for_sync(&ottf_console_spi_device);
