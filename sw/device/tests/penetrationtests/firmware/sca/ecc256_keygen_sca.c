e16c2949d0addda126531900226fc4bd98981355
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-05-28 14:10:42 +0200

    [pentest] Generate full keypair for ECDSA SCA tests
    Previously, only the private key was generated by the ECDSA SCA
    test. Switch to full keypair to enhance the testing.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit 95e945e)

--- sw/device/tests/penetrationtests/firmware/sca/ecc256_keygen_sca.c
+++ sw/device/tests/penetrationtests/firmware/sca/ecc256_keygen_sca.c
@@ -229,8 +229,7 @@ status_t handle_otbn_sca_ecc256_ecdsa_keygen_fvsr_key_batch(ujson_t *uj) {
   }
 
   for (size_t i = 0; i < num_traces; ++i) {
-    TRY(p256_run_keygen(kEcc256ModePrivateKeyOnly, batch_share0[i],
-                        batch_share1[i]));
+    TRY(p256_run_keygen(kEcc256ModeKeypair, batch_share0[i], batch_share1[i]));
 
     // Read results.
     TRY(otbn_dmem_read(kEcc256SeedNumWords, kOtbnVarD0, d0_batch));
@@ -298,8 +297,7 @@ status_t handle_otbn_sca_ecc256_ecdsa_keygen_fvsr_seed_batch(ujson_t *uj) {
   }
 
   for (size_t i = 0; i < num_traces; ++i) {
-    TRY(p256_run_keygen(kEcc256ModePrivateKeyOnly, batch_share0[i],
-                        batch_share1[i]));
+    TRY(p256_run_keygen(kEcc256ModeKeypair, batch_share0[i], batch_share1[i]));
 
     // Read results.
     TRY(otbn_dmem_read(kEcc256SeedNumWords, kOtbnVarD0, d0_batch));
