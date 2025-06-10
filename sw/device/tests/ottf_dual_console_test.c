00ae5872be7f6b5b9e42d072df076e3894a4d9fa
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-06-10 08:22:20 +0000

    [sw,tests] Cleanup various nits in the OTTF dual console test
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>
    (cherry picked from commit 275a955ef2caed70cb698d8a287043051524b2c2)

--- sw/device/tests/ottf_dual_console_test.c
+++ sw/device/tests/ottf_dual_console_test.c
@@ -13,7 +13,9 @@
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
 /**
- * Tests ...
+ * Test the ability to have several independent consoles. The main
+ * console is connected to the "console" UART and a secondary console
+ * is created which is connected to the "dut" UART.
  */
 
 OTTF_DEFINE_TEST_CONFIG();
@@ -24,7 +26,7 @@ static ottf_console_t debug_console;
 bool test_main(void) {
   CHECK_DIF_OK(dif_pinmux_init(
       mmio_region_from_addr(TOP_EARLGREY_PINMUX_AON_BASE_ADDR), &pinmux));
-  CHECK_STATUS_OK(uart_testutils_select_pinmux(&pinmux, /* UART */ 1,
+  CHECK_STATUS_OK(uart_testutils_select_pinmux(&pinmux, /* uart= */ 1,
                                                kUartPinmuxChannelDut));
   ottf_console_configure_uart(&debug_console, TOP_EARLGREY_UART1_BASE_ADDR);
   LOG_INFO("Main UART console");
