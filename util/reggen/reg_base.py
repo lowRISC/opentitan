608055f6d3d7f73d73ba9ccc5c60dc9367f54126
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-06-10 16:14:35 +0100

    [reggen] Use a dataclass for RegBase
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- util/reggen/reg_base.py
+++ util/reggen/reg_base.py
@@ -2,60 +2,50 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import List, Optional
+from dataclasses import dataclass
 
 from reggen.clocking import ClockingItem
 from reggen.field import Field
 
 
+@dataclass
 class RegBase:
     '''An abstract class inherited by Register and MultiRegister
 
     This represents a block of one or more registers with a base address.
-
-    Instance variables:
-
-      name       The name of the register/multiregister
-
-      offset     The offset or address of the register. For a MultiRegister,
-                 this gives the address of the first concrete register in the
-                 expansion.
-
-      async_name The name of an asynchronous clock used for the data backing
-                 the register/multiregister.
-
-      async_clk  The clocking item named by async_name
-
-      sync_name  The name of a synchronous clock used for the data backing the
-                 register/multiregister. Unlike an asynchronous clock, there is
-                 no CDC needed between the main clock and the clock used by the
-                 register.
-
-      sync_clk   The clocking item named by sync_name
-
-      alias_target The name of an analogous register/multiregister that we are
-                   going to override. This is used when building closed source
-                   components which override a generic register/multiregister
-                   implementation with extra fields/flags etc.
     '''
+    name: str
+    '''The name of the register/multiregister'''
 
-    def __init__(self,
-                 name: str,
-                 offset: int,
-                 async_name: Optional[str], async_clk: Optional[ClockingItem],
-                 sync_name: Optional[str], sync_clk: Optional[ClockingItem],
-                 alias_target: Optional[str]):
-        self.name = name
-        self.offset = offset
+    offset: int
+    '''The offset or address of the register. For a MultiRegister, this gives
+    the address of the first concrete register in the expansion.'''
 
-        self.async_name = async_name
-        self.async_clk = async_clk
-        self.sync_name = sync_name
-        self.sync_clk = sync_clk
+    async_name: str | None
+    '''The name of an asynchronous clock used for the data backing the
+    register/multiregister.'''
 
-        self.alias_target = alias_target
+    async_clk: ClockingItem | None
+    '''The clocking item named by async_name'''
 
-    def get_n_bits(self, bittype: List[str]) -> int:
+    sync_name: str | None
+    '''The name of a synchronous clock used for the data backing the
+    register/multiregister. Unlike an asynchronous clock, there is no CDC
+    needed between the main clock and the clock used by the register.'''
+
+    sync_clk: ClockingItem | None
+    '''The clocking item named by sync_name'''
+
+    alias_target: str | None
+    '''The name of an analogous register/multiregister that we are going to
+    override. This is used when building closed source components which
+    override a generic register/multiregister implementation with extra
+    fields/flags etc.'''
+
+    def __hash__(self) -> int:
+        return hash((self.offset, self.name))
+
+    def get_n_bits(self, bittype: list[str]) -> int:
         '''Get the size of this register / these registers in bits
 
         See Field.get_n_bits() for the precise meaning of bittype.
@@ -63,7 +53,7 @@ class RegBase:
         '''
         raise NotImplementedError()
 
-    def get_field_list(self) -> List[Field]:
+    def get_field_list(self) -> list[Field]:
         '''Get an ordered list of the fields in the register(s)
 
         Registers are ordered from low to high address. Within a register,
