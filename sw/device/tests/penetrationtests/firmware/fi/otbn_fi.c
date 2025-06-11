27e63e7938a1dbacf8f2f4b433c120eeb816d04d
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-06-11 08:19:11 +0200

    [pentest] Streamline iCache and dummy instr enable/disable
    Similar to SRAM readback and jittery clock, switch to explicit enable/
    disable for the iCache and dummy instruction enable/disable feature
    in the pentest lib.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit 59882993e7deb76c43a2e7c28f5981e841b4b2c9)

--- sw/device/tests/penetrationtests/firmware/fi/otbn_fi.c
+++ sw/device/tests/penetrationtests/firmware/fi/otbn_fi.c
@@ -1254,11 +1254,12 @@ status_t handle_otbn_fi_init(ujson_t *uj) {
   // Configure the CPU for the pentest.
   penetrationtest_device_info_t uj_output;
   TRY(pentest_configure_cpu(
-      uj_data.icache_disable, uj_data.dummy_instr_disable,
-      uj_data.enable_jittery_clock, uj_data.enable_sram_readback,
-      &uj_output.clock_jitter_locked, &uj_output.clock_jitter_en,
-      &uj_output.sram_main_readback_locked, &uj_output.sram_ret_readback_locked,
-      &uj_output.sram_main_readback_en, &uj_output.sram_ret_readback_en));
+      uj_data.enable_icache, &uj_output.icache_en, uj_data.enable_dummy_instr,
+      &uj_output.dummy_instr_en, uj_data.enable_jittery_clock,
+      uj_data.enable_sram_readback, &uj_output.clock_jitter_locked,
+      &uj_output.clock_jitter_en, &uj_output.sram_main_readback_locked,
+      &uj_output.sram_ret_readback_locked, &uj_output.sram_main_readback_en,
+      &uj_output.sram_ret_readback_en));
 
   // The load integrity, key sideloading, and char_mem tests get initialized at
   // the first run.
