4c73bfc134bba746d00de00f9f0c5e9470616c8a
Siemen Dhooghe <sdhooghe@google.com>
2025-06-12 22:53:47 +0200

    [pentest] Extend pentest lib
    This commit extends the pentest lib with the following changes:
    
    - Add the CSR config to add dummy instructions.
    - Add the variable to enable or disable the alerts from the sensor
      control.
    - Add functions to read out the sensor ctrl and the alert handler, and
      integrate this functionality to the test framework.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>
    Co-authored-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/host/tests/penetrationtests/fi_crypto/src/main.rs
+++ sw/host/tests/penetrationtests/fi_crypto/src/main.rs
@@ -42,6 +42,10 @@ struct FiCryptoTestCase {
     input: String,
     #[serde(default)]
     mode: String,
+    #[serde(default)]
+    sensors: String,
+    #[serde(default)]
+    alerts: String,
     expected_output: Vec<String>,
 }
 
@@ -85,6 +89,18 @@ fn run_fi_crypto_testcase(
     if !test_case.mode.is_empty() {
         let mode: serde_json::Value = serde_json::from_str(test_case.mode.as_str()).unwrap();
         mode.send(uart)?;
+    }
+
+    // Check if we need to send sensor info.
+    if !test_case.sensors.is_empty() {
+        let sensors: serde_json::Value = serde_json::from_str(test_case.sensors.as_str()).unwrap();
+        sensors.send(uart)?;
+    }
+
+    // Check if we need to send alert info.
+    if !test_case.alerts.is_empty() {
+        let alerts: serde_json::Value = serde_json::from_str(test_case.alerts.as_str()).unwrap();
+        alerts.send(uart)?;
     }
 
     // Check test outputs
