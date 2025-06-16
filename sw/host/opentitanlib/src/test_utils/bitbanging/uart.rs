617f92a3c661ba04bcb9893555a6751d6cf705e6
Alex Jones <alex.jones@lowrisc.org>
2025-06-16 22:14:05 +0100

    [opentitanlib, testutils] Add UART bitbanging decoder interface
    Adds a utility for decoding UART transmissions from a series of
    monitored GPIO samples (in the form of a uniformly sampled waveform,
    sampled at the correct baud rate). As before this is kept isolated from
    opentitanlib so as to allow easy re-use elsewhere.
    
    When decoding we consider a break condition to be any logical low
    transmission of equal length to or longer than a UART frame (start bit +
    data bits + optional parity + stop bits). We represent broken decoding
    states with an output error that records the parsed data & parity bit
    for reference.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- sw/host/opentitanlib/src/test_utils/bitbanging/uart.rs
+++ sw/host/opentitanlib/src/test_utils/bitbanging/uart.rs
@@ -3,7 +3,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::{bail, Result};
+use serde::{Deserialize, Serialize};
 use serialport::Parity;
+use thiserror::Error;
 
 /// UART frame configuration to use for UART bitbanging. Assumes LSB first.
 #[derive(Clone, Debug)]
@@ -53,7 +55,14 @@ impl UartBitbangConfig {
 #[derive(Debug, PartialEq)]
 pub enum UartTransfer {
     // Assumes <= 8 data bits.
-    Byte { data: u8 },
+    Byte {
+        data: u8,
+    },
+    Broken {
+        data: u8,
+        parity: Option<bool>,
+        error: UartTransferDecodeError,
+    },
     Break,
 }
 
@@ -118,18 +127,212 @@ impl<const TX: u8> UartBitbangEncoder<TX> {
 
     /// Encode a UART transmission (data / break) into UART bitbanging samples,
     // to be used on the TX pin.
-    pub fn encode_transfer(&self, transfer: &UartTransfer, samples: &mut Vec<u8>) {
+    pub fn encode_transfer(&self, transfer: &UartTransfer, samples: &mut Vec<u8>) -> Result<()> {
         match *transfer {
+            UartTransfer::Broken { .. } => bail!("Cannot encode a broken UART transfer"),
             UartTransfer::Break => self.encode_break(samples),
             UartTransfer::Byte { data } => self.encode_character(data, samples),
         }
+        Ok(())
     }
 
     /// Helper function to encode multiple UART transfers into UART bitbanging
     /// samples in one call.
-    pub fn encode_transfers(&self, transfers: &[UartTransfer], samples: &mut Vec<u8>) {
+    pub fn encode_transfers(
+        &self,
+        transfers: &[UartTransfer],
+        samples: &mut Vec<u8>,
+    ) -> Result<()> {
         for transfer in transfers {
-            self.encode_transfer(transfer, samples);
+            self.encode_transfer(transfer, samples)?;
         }
+        Ok(())
+    }
+}
+
+#[derive(Error, Debug, PartialEq, Serialize, Deserialize)]
+pub enum UartTransferDecodeError {
+    #[error("Computed parity does not match expected parity")]
+    ParityMismatch,
+    #[error("Stop was not held high for the full stop time")]
+    InvalidStop,
+    #[error(
+        "RX held low too long for a valid transmission, but not long enough for a break condition"
+    )]
+    InvalidBreak,
+}
+
+/// Possible states for the decoder state machine to be in.
+#[derive(Debug, PartialEq)]
+enum DecodingState {
+    Idle,
+    Data { bits: u8 },
+    Parity,
+    Stop { data: u8, bits: u8 },
+    Break { bits: u32 },
+}
+
+/// A decoder for decoding UART samples. `RX` is the bit in the input sample
+/// bitfield to use for RX transmissions.
+pub struct UartBitbangDecoder<const RX: u8> {
+    pub config: UartBitbangConfig,
+    state: DecodingState,
+    parsed_data: u8,
+    parsed_parity: Option<bool>,
+}
+
+impl<const RX: u8> UartBitbangDecoder<RX> {
+    pub fn new(config: UartBitbangConfig) -> Self {
+        Self {
+            config,
+            state: DecodingState::Idle,
+            parsed_data: 0x00,
+            parsed_parity: None,
+        }
+    }
+
+    /// A helper to change the parity of the decoder
+    pub fn set_parity(&mut self, parity: Parity) {
+        self.config.parity = parity;
+    }
+
+    // A helper to construct a `Broken` UART transfer symbol
+    fn broken(&self, error: UartTransferDecodeError) -> UartTransfer {
+        UartTransfer::Broken {
+            data: self.parsed_data,
+            parity: self.parsed_parity,
+            error,
+        }
+    }
+
+    /// Finishes decoding the current UART transfer.
+    fn get_decoded_character(&mut self) -> Result<UartTransfer> {
+        // Detect valid & invalid break conditions
+        if let DecodingState::Break { bits } = self.state {
+            if bits < self.config.break_bit_time() {
+                return Ok(self.broken(UartTransferDecodeError::InvalidBreak));
+            }
+            return Ok(UartTransfer::Break);
+        }
+
+        // Detect we've fully stopped, and handle invalid stop signals.
+        if let DecodingState::Stop { data, bits } = self.state {
+            if bits != self.config.stop_bits {
+                bail!("`get_decoded_character` called before reading all stop bits");
+            }
+            if data.count_ones() as u8 != bits {
+                return Ok(self.broken(UartTransferDecodeError::InvalidStop));
+            }
+        } else {
+            bail!("`get_decoded_character` called before the end of a transmission");
+        }
+
+        // If configured to use parity, check and report single parity errors.
+        if self.config.parity != Parity::None {
+            let mut decoded_parity = (self.parsed_data.count_ones() % 2) != 0;
+            if let Some(parity_bit) = self.parsed_parity {
+                decoded_parity ^= parity_bit;
+            }
+            let expected_parity = self.config.parity != Parity::Even;
+            if expected_parity != decoded_parity {
+                return Ok(self.broken(UartTransferDecodeError::ParityMismatch));
+            }
+        }
+
+        Ok(UartTransfer::Byte {
+            data: self.parsed_data,
+        })
+    }
+
+    /// Given a sampled waveform (where bit RX is the UART RX), advance the
+    /// UART decoder state based on the contents of the sample. If the sample
+    /// is the final stop bit, return the decoded UART transfer.
+    pub fn decode_sample(&mut self, sample: u8) -> Result<Option<UartTransfer>> {
+        let rx = (sample >> RX) & 0x1;
+        match self.state {
+            DecodingState::Idle => {
+                if rx == 0 {
+                    self.parsed_data = 0x00;
+                    self.state = DecodingState::Data { bits: 0 };
+                }
+            }
+            DecodingState::Data { mut bits } => {
+                self.parsed_data |= rx << bits;
+                bits += 1;
+                if bits >= self.config.data_bits {
+                    if self.config.parity == Parity::None {
+                        self.state = DecodingState::Stop {
+                            data: 0x00,
+                            bits: 0,
+                        };
+                    } else {
+                        self.state = DecodingState::Parity;
+                    }
+                } else {
+                    self.state = DecodingState::Data { bits };
+                }
+            }
+            DecodingState::Parity => {
+                self.parsed_parity = Some(rx != 0);
+                self.state = DecodingState::Stop {
+                    data: 0x00,
+                    bits: 0,
+                };
+            }
+            DecodingState::Stop { mut data, mut bits } => {
+                data |= rx << bits;
+                bits += 1;
+                self.state = DecodingState::Stop { data, bits };
+                if bits >= self.config.stop_bits {
+                    if self.parsed_data != 0x00 || self.parsed_parity == Some(true) || data != 0x00
+                    {
+                        let decoded = self.get_decoded_character()?;
+                        self.state = DecodingState::Idle;
+                        return Ok(Some(decoded));
+                    }
+                    let mut break_bits = 1 + self.config.data_bits + bits;
+                    if self.config.parity != Parity::None {
+                        break_bits += 1;
+                    }
+                    self.state = DecodingState::Break {
+                        bits: break_bits as u32,
+                    };
+                }
+            }
+            DecodingState::Break { bits } => {
+                if rx != 0 {
+                    let decoded = self.get_decoded_character()?;
+                    self.state = DecodingState::Idle;
+                    return Ok(Some(decoded));
+                }
+                self.state = DecodingState::Break { bits: bits + 1 };
+            }
+        }
+        Ok(None)
+    }
+
+    /// A helper function to decode a sequence of UART waveform samples,
+    /// advancing the decoder based on the contents. If the sample contains
+    /// any final stop bits, the decoded UART transfers are returned.
+    pub fn decode_samples(&mut self, samples: &Vec<u8>) -> Result<Vec<UartTransfer>> {
+        let mut transfers = vec![];
+        for sample in samples {
+            if let Some(transfer) = self.decode_sample(*sample)? {
+                transfers.push(transfer);
+            }
+        }
+        Ok(transfers)
+    }
+
+    /// Check if the decoder is currently in the idle state or not
+    pub fn is_idle(&self) -> bool {
+        self.state == DecodingState::Idle
+    }
+
+    /// Reset the state of decoder
+    pub fn reset(&mut self) {
+        self.state = DecodingState::Idle;
+        self.parsed_data = 0x00;
+        self.parsed_parity = None;
     }
 }
