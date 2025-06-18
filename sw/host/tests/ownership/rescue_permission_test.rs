1acb3d9ff582931e97b852496cb78fe763c54ada
Shih-Han Chang <shihhanchang@google.com>
2025-06-18 08:58:40 +0800

    [sw,test] Add detached ECDSA-only unlock request test
    This change adds a new ownership transfer test, `unlock_detached_sig_test`,
    to verify the detached ECDSA-only signature unlock request functionality.
    
    Signed-off-by: Shih-Han Chang <shihhanchang@google.com>
    (cherry picked from commit 1056c3b363ebf8f7a3425d41c491e142b83ac05b)
    Change-Id: Ibde6cd6dcb19b6065bb483f22de163c69ec22134
    (cherry picked from commit bffcba7bfe3778cde660a0214303f0aac02a5155)

--- sw/host/tests/ownership/rescue_permission_test.rs
+++ sw/host/tests/ownership/rescue_permission_test.rs
@@ -76,6 +76,7 @@ fn rescue_permission_test(opts: &Opts, transport: &TransportWrapper) -> Result<(
             None
         },
         None,
+        false,
     )?;
 
     log::info!("###### Get Boot Log (2/2) ######");
