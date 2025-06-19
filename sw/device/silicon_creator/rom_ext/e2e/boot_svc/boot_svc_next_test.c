9d62efc1e5521c8ab52e7ba30a9b9048c044281a
Yi-Hsuan Deng <yhdeng@google.com>
2025-06-19 10:08:57 +0800

    [sw,e2e] Add more boot_svc next boot slot test sequences
    This change adds more E2E tests for `boot_svc_next_boot_bl0_slot` to increase
    test coverage.
    
    The newly added E2E tests check the following boot sequences.
      - A -> B -> A (primary=N/A, next=B)
      - A -> A -> B (primary=B, next=A)
      - A -> B -> B (primary=B, next=N/A)
    
    Change-Id: I6ca37e23feae890db48e209ea3316422c5a3a448
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

--- sw/device/silicon_creator/rom_ext/e2e/boot_svc/boot_svc_next_test.c
+++ sw/device/silicon_creator/rom_ext/e2e/boot_svc/boot_svc_next_test.c
@@ -13,35 +13,72 @@
 
 OTTF_DEFINE_TEST_CONFIG();
 
-static status_t initialize(retention_sram_t *retram, boot_svc_retram_t *state) {
+static status_t next(retention_sram_t *retram, uint32_t primary_slot,
+                     uint32_t next_slot) {
   boot_svc_msg_t msg = {0};
-  boot_svc_next_boot_bl0_slot_req_init(
-      /*primary_slot=*/kBootSlotUnspecified,
-      /*next_slot=*/kBootSlotB, &msg.next_boot_bl0_slot_req);
+  boot_svc_next_boot_bl0_slot_req_init(primary_slot, next_slot,
+                                       &msg.next_boot_bl0_slot_req);
   retram->creator.boot_svc_msg = msg;
-  state->state = kBootSvcTestStateNextSideB;
   rstmgr_reset();
   return INTERNAL();
 }
 
-static status_t check_side_b(retention_sram_t *retram,
-                             boot_svc_retram_t *state) {
+static status_t initialize(retention_sram_t *retram, boot_svc_retram_t *state) {
+  state->state = kBootSvcTestStateNextSideB;
+
+#if defined(TEST_SEQUENCE_ABA)
+  TRY(next(retram, kBootSlotUnspecified, kBootSlotB));
+#elif defined(TEST_SEQUENCE_AAB)
+  TRY(next(retram, kBootSlotB, kBootSlotA));
+#elif defined(TEST_SEQUENCE_ABB)
+  TRY(next(retram, kBootSlotB, kBootSlotUnspecified));
+#else
+#error "test sequence is not defined"
+#endif
+
+  return INTERNAL();
+}
+
+static status_t check_side_next(retention_sram_t *retram,
+                                boot_svc_retram_t *state) {
+  state->state = kBootSvcTestStateReturnSideA;
+
   boot_svc_msg_t msg = retram->creator.boot_svc_msg;
   TRY(boot_svc_header_check(&msg.header));
   TRY_CHECK(msg.header.type == kBootSvcNextBl0SlotResType);
   TRY_CHECK(msg.next_boot_bl0_slot_res.status == kErrorOk);
+
+#if defined(TEST_SEQUENCE_ABA)
   TRY_CHECK(state->current_side == 'B');
   TRY_CHECK(state->primary_side == 'A');
   TRY_CHECK(msg.next_boot_bl0_slot_res.primary_bl0_slot == kBootSlotA);
-  state->state = kBootSvcTestStateReturnSideA;
+#elif defined(TEST_SEQUENCE_AAB)
+  TRY_CHECK(state->current_side == 'A');
+  TRY_CHECK(state->primary_side == 'B');
+  TRY_CHECK(msg.next_boot_bl0_slot_res.primary_bl0_slot == kBootSlotB);
+#elif defined(TEST_SEQUENCE_ABB)
+  TRY_CHECK(state->current_side == 'B');
+  TRY_CHECK(state->primary_side == 'B');
+  TRY_CHECK(msg.next_boot_bl0_slot_res.primary_bl0_slot == kBootSlotB);
+  TRY(next(retram, kBootSlotA, kBootSlotB));
+#endif
+
   rstmgr_reset();
   return INTERNAL();
 }
 
-static status_t check_return_side_a(retention_sram_t *retram,
-                                    boot_svc_retram_t *state) {
+static status_t check_return_side_primary(retention_sram_t *retram,
+                                          boot_svc_retram_t *state) {
+#if defined(TEST_SEQUENCE_ABA)
   TRY_CHECK(state->current_side == 'A');
   TRY_CHECK(state->primary_side == 'A');
+#elif defined(TEST_SEQUENCE_AAB)
+  TRY_CHECK(state->current_side == 'B');
+  TRY_CHECK(state->primary_side == 'B');
+#elif defined(TEST_SEQUENCE_ABB)
+  TRY_CHECK(state->current_side == 'B');
+  TRY_CHECK(state->primary_side == 'A');
+#endif
   state->state = kBootSvcTestStateFinal;
   return OK_STATUS();
 }
@@ -59,10 +96,10 @@ static status_t next_bl0_slot_test(void) {
         TRY(initialize(retram, state));
         break;
       case kBootSvcTestStateNextSideB:
-        TRY(check_side_b(retram, state));
+        TRY(check_side_next(retram, state));
         break;
       case kBootSvcTestStateReturnSideA:
-        TRY(check_return_side_a(retram, state));
+        TRY(check_return_side_primary(retram, state));
         break;
       case kBootSvcTestStateFinal:
         LOG_INFO("FinalBootLog: %d:%s", state->boots, state->partition);
