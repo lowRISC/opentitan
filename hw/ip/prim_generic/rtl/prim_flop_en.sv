cf60587464be35dc63c80b838299fa2753c3364c
Andreas Kurth <adk@lowrisc.org>
2025-06-23 09:31:48 +0200

    [prim_flop_en] Add parameter for depth
    This allows to use `prim_flop_en` for pipelining.
    
    Signed-off-by: Andreas Kurth <adk@lowrisc.org>

--- hw/ip/prim_generic/rtl/prim_flop_en.sv
+++ hw/ip/prim_generic/rtl/prim_flop_en.sv
@@ -6,6 +6,12 @@
 
 module prim_flop_en #(
   parameter int               Width      = 1,
+  // Depth of the flop, used for pipelining. Values >= 1 lead to the corresponding number of flop
+  // stages, connected in series. The `en_i` input determines when the *first* flop stage gets
+  // updated, all other flop stages are updated in subsequent clock cycles as the data propagates
+  // through the stages (the `en_i` input doesn't need to be high for that propagation). Parameter
+  // values < 1 lead `q_o` to be directly connected to `d_i`.
+  parameter int               Depth = 1,
   parameter bit               EnSecBuf   = 0,
   parameter logic [Width-1:0] ResetValue = 0
 ) (
@@ -28,11 +34,31 @@ module prim_flop_en #(
     assign en = en_i;
   end
 
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      q_o <= ResetValue;
-    end else if (en) begin
-      q_o <= d_i;
+  if (Depth <= 0) begin : gen_fallthrough
+    assign q_o = d_i;
+
+  end else begin : gen_flops
+    logic [Depth-1:0][Width-1:0] d, q;
+
+    // Connect D of first flop to input.
+    assign d[0] = d_i;
+
+    // Connect Q of last flop to output.
+    assign q_o = q[Depth-1];
+
+    for (genvar i = 0; i < Depth; i++) begin : gen_depth
+      always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni) begin
+          q[i] <= ResetValue;
+        end else if (i > 0 || en) begin
+          q[i] <= d[i];
+        end
+      end
+
+      if (i > 0) begin : gen_connect_d
+        // Connect D of current flop to Q of previous flop.
+        assign d[i] = q[i-1];
+      end
     end
   end
 
