23973a89f7a2e91798a894d0d3755c444e1ffe6a
Elliot Baptist <elliot.baptist@lowrisc.org>
2025-06-25 10:04:41 +0100

    [tlul, prim] Fix typos in typos fix
    A couple of improvements to the first round of typo fixes.
    
    Signed-off-by: Elliot Baptist <elliot.baptist@lowrisc.org>

--- hw/ip/tlul/rtl/tlul_sram_byte.sv
+++ hw/ip/tlul/rtl/tlul_sram_byte.sv
@@ -216,7 +216,7 @@ module tlul_sram_byte import tlul_pkg::*; #(
 
     // If the readback feature is enabled, we assume that the write phase takes one extra cycle
     // due to the underlying scrambling mechanism. If this additional cycle is not needed anymore
-    // in the future (e.g. due to the removable of the scrambling mechanism), the readback does not
+    // in the future (e.g. due to the removal of the scrambling mechanism), the readback does not
     // need to be delayed by once cylce in the FSM below.
     `ASSERT(NoPendingWriteAfterWrite_A, wr_phase & mubi4_test_true_loose(rdback_en_q)
         |=> write_pending_i)
