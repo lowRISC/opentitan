dfb47943412e48b32bce6f80a9d61ea94a6bf357
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-06-30 13:56:00 +0200

    [pentest] Integrate DRBG CL into CryptoLibFi & CryptoLibSca stub
    This commit adds a call to the CL DRBG implementation using
    a wrapper into the CryptoLibFi and CryptoLibSca DRBG stub.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/tests/penetrationtests/firmware/fi/cryptolib_fi_sym_impl.c
+++ sw/device/tests/penetrationtests/firmware/fi/cryptolib_fi_sym_impl.c
@@ -10,8 +10,10 @@
 #include "sw/device/lib/crypto/impl/keyblob.h"
 #include "sw/device/lib/crypto/include/aes.h"
 #include "sw/device/lib/crypto/include/datatypes.h"
+#include "sw/device/lib/crypto/include/drbg.h"
 #include "sw/device/lib/crypto/include/hmac.h"
 #include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ujson_ottf.h"
 #include "sw/device/tests/penetrationtests/firmware/lib/cryptolib_sym.h"
 #include "sw/device/tests/penetrationtests/firmware/lib/pentest_lib.h"
@@ -133,6 +135,60 @@ status_t cryptolib_fi_aes_impl(cryptolib_fi_sym_aes_in_t uj_input,
   uj_output->cfg = 0;
   memset(uj_output->data, 0, AES_CMD_MAX_MSG_BYTES);
   memcpy(uj_output->data, output_buf, uj_output->data_len);
+
+  return OK_STATUS();
+}
+
+status_t cryptolib_fi_drbg_impl(cryptolib_fi_sym_drbg_in_t uj_input,
+                                cryptolib_fi_sym_drbg_out_t *uj_output) {
+  // Entropy buffer used for the instantiation of the DRBG.
+  uint8_t entropy_buf[uj_input.entropy_len];
+  memcpy(entropy_buf, uj_input.entropy, uj_input.entropy_len);
+
+  otcrypto_const_byte_buf_t entropy = {
+      .len = uj_input.entropy_len,
+      .data = entropy_buf,
+  };
+
+  // Trigger window 0.
+  if (uj_input.trigger == 0) {
+    pentest_set_trigger_high();
+  }
+  TRY(otcrypto_drbg_instantiate(entropy));
+  if (uj_input.trigger == 0) {
+    pentest_set_trigger_low();
+  }
+
+  // Nonce buffer used for the generate command of the DRBG.
+  uint8_t nonce_buf[uj_input.nonce_len];
+  memcpy(nonce_buf, uj_input.nonce, uj_input.nonce_len);
+
+  otcrypto_const_byte_buf_t nonce = {
+      .len = uj_input.nonce_len,
+      .data = nonce_buf,
+  };
+
+  // Buffer for the output entropy data.
+  uint32_t output_data[uj_input.entropy_len];
+  otcrypto_word32_buf_t output = {
+      .data = output_data,
+      .len = ARRAYSIZE(output_data),
+  };
+
+  // Trigger window 0.
+  if (uj_input.trigger == 1) {
+    pentest_set_trigger_high();
+  }
+  TRY(otcrypto_drbg_generate(nonce, output));
+  if (uj_input.trigger == 1) {
+    pentest_set_trigger_low();
+  }
+
+  // Return data back to host.
+  uj_output->data_len = uj_input.entropy_len;
+  uj_output->cfg = 0;
+  memset(uj_output->data, 0, DRBG_CMD_MAX_OUTPUT_BYTES);
+  memcpy(uj_output->data, output_data, uj_output->data_len);
 
   return OK_STATUS();
 }
