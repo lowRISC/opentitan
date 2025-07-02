1230e8e30c856e602248112fa1e20ee22149ba45
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-07-02 14:27:40 +0100

    [fpv] Make sequence chains compile much faster
    If you have a sequence that looks like "A ##1 B ##1 C ##1 D", Jasper
    turns out to take quite a long time to compile it. This is rather
    annoying, and you can actually hit a timeout.
    
    It seems that the Cadence engineers realised that they could do things
    more efficiently and put it behind a flag (presumably because the
    change pessimizes some other example).
    
    Ask for the new behaviour, provided we are on a version of Jasper that
    supports it.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/formal/tools/jaspergold/fpv.tcl
+++ hw/formal/tools/jaspergold/fpv.tcl
@@ -52,13 +52,24 @@ if {$env(TASK) == "FpvSecCm"} {
     -f [glob *.scr]
 }
 
+# Chains of successive A ##1 B ##1 C ... in sequences can take a long time to elaborate. There is a
+# new flag to elaborate called -optimize_implication_assert which improves this, but it only appears
+# from Jasper 2023.06. Call the get_version function to figure out whether we've got the right
+# version.
+if {[regexp "^202\(\[4-9\]\|3.0\[69\]\)" [get_version]]} {
+  set oia_arg "-optimize_implication_assert"
+} else {
+  set oia_arg ""
+}
+
 if {$env(DUT_TOP) == "prim_count_tb"} {
   elaborate -top $env(DUT_TOP) \
             -enable_sva_isunknown \
             -disable_auto_bbox \
-            -param ResetValue $ResetValue
+            -param ResetValue $ResetValue \
+            ${oia_arg}
 } else {
-  elaborate -top $env(DUT_TOP) -enable_sva_isunknown -disable_auto_bbox
+  elaborate -top $env(DUT_TOP) -enable_sva_isunknown -disable_auto_bbox ${oia_arg}
 }
 
 set stopat [regexp -all -inline {[^\s\']+} $env(STOPATS)]
