ffcf4bf6f6c3f5897ab8a5696fcaabe5e25e7fa1
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-07-02 10:07:11 +0200

    [pentest] Integrate P384-VERIFY CL into CryptoLibFi stub
    This commit adds a call to the CL P384-VERIFY implementation using
    a wrapper into the CryptoLibFi P384-VERIFY stub.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/tests/penetrationtests/firmware/fi/cryptolib_fi_asym_impl.h
+++ sw/device/tests/penetrationtests/firmware/fi/cryptolib_fi_asym_impl.h
@@ -63,4 +63,15 @@ status_t cryptolib_fi_p384_sign_impl(
     cryptolib_fi_asym_p384_sign_in_t uj_input,
     cryptolib_fi_asym_p384_sign_out_t *uj_output);
 
+/**
+ * Wrapper to P384 Verify cryptolib implementation.
+ *
+ * @param uj_input An initialized uJSON context.
+ * @param uj_output An initialized uJSON context.
+ * @return OK or error.
+ */
+status_t cryptolib_fi_p384_verify_impl(
+    cryptolib_fi_asym_p384_verify_in_t uj_input,
+    cryptolib_fi_asym_p384_verify_out_t *uj_output);
+
 #endif  // OPENTITAN_SW_DEVICE_TESTS_PENETRATIONTESTS_FIRMWARE_FI_CRYPTOLIB_FI_ASYM_IMPL_H_
