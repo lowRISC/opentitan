becac2f3596c285a0a1e514acecaf9db21b1f5d6
Alex Jones <alex.jones@lowrisc.org>
2025-07-02 21:06:52 +0100

    [opentitanlib, testutils] Add UART bitbanging tests
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- sw/host/opentitanlib/src/test_utils/bitbanging/uart.rs
+++ sw/host/opentitanlib/src/test_utils/bitbanging/uart.rs
@@ -383,3 +383,276 @@ impl<const RX: u8> UartBitbangDecoder<RX> {
         self.state = DecodingState::Idle;
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn compare_decoded_result(received: &[UartTransfer], expected: &[u8]) -> Result<()> {
+        assert_eq!(received.len(), expected.len());
+        for (transfer, expected) in received.iter().zip(expected.iter()) {
+            match transfer {
+                UartTransfer::Byte { data } => {
+                    assert_eq!(data, expected);
+                }
+                _ => bail!("Only expected to decode bytes"),
+            }
+        }
+        Ok(())
+    }
+
+    fn uart_encode_decode(config: UartBitbangConfig, message: Option<&[u8]>) -> Result<()> {
+        let encoder = UartBitbangEncoder::<0>::new(config.clone());
+        let mut decoder = UartBitbangDecoder::<0>::new(config);
+        let msg = message.unwrap_or(b"Hello, this is a simple UART test message.");
+        let mut samples = Vec::new();
+        encoder.encode_characters(msg, &mut samples);
+        assert!(!samples.is_empty());
+        let decoded = decoder
+            .decode_samples(&samples)
+            .expect("Should have decoded the bitbanged message");
+        assert!(decoder.is_idle());
+        compare_decoded_result(&decoded, msg)
+    }
+
+    #[test]
+    fn smoke() -> Result<()> {
+        // Encode and decode some test strings
+        let config = UartBitbangConfig::new(8, UartStopBits::Stop2, 1, Parity::None)?;
+        uart_encode_decode(config.clone(), None)?;
+        uart_encode_decode(config.clone(), Some(b"abc def ghi jkl"))?;
+        uart_encode_decode(config.clone(), Some(b"12345"))?;
+
+        // Check bitbang encoding against a known sample.
+        let encoder = UartBitbangEncoder::<0>::new(config);
+        let bytes = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF];
+        let mut samples = Vec::new();
+        encoder.encode_characters(&bytes, &mut samples);
+        let expected = [
+            0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0,
+            1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1,
+            0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1,
+            1,
+        ];
+        assert_eq!(samples, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn bit_indexes() -> Result<()> {
+        // Check we can use different bit indexes correctly
+        let config = UartBitbangConfig::new(8, UartStopBits::Stop2, 1, Parity::None)?;
+        let encoder_1 = UartBitbangEncoder::<1>::new(config.clone());
+        let encoder_5 = UartBitbangEncoder::<5>::new(config.clone());
+        let mut decoder_1 = UartBitbangDecoder::<1>::new(config.clone());
+        let mut decoder_5 = UartBitbangDecoder::<5>::new(config.clone());
+        let msg = b"UART bit test message";
+        let mut samples_1 = Vec::new();
+        let mut samples_5 = Vec::new();
+        encoder_1.encode_characters(msg, &mut samples_1);
+        encoder_5.encode_characters(msg, &mut samples_5);
+        assert_eq!(samples_1.len(), samples_5.len());
+        let samples = samples_1.iter().zip(samples_5.iter());
+        for (sample_1, sample_5) in samples {
+            assert!((sample_1 >> 1) & 0x1 == (sample_5 >> 5) & 0x1);
+            assert_eq!(sample_1 & !(0x1 << 1), 0x00);
+            assert_eq!(sample_5 & !(0x1 << 5), 0x00);
+        }
+        let decoded_1 = decoder_1
+            .decode_samples(&samples_1)
+            .expect("Should have decoded the bitbanged message");
+        assert!(decoder_1.is_idle());
+        let decoded_5 = decoder_5
+            .decode_samples(&samples_5)
+            .expect("Should have decoded the bitbanged message");
+        assert!(decoder_5.is_idle());
+        compare_decoded_result(&decoded_1, msg)?;
+        compare_decoded_result(&decoded_5, msg)
+    }
+
+    #[test]
+    fn data_bits() -> Result<()> {
+        // Check invalid configurations
+        assert!(UartBitbangConfig::new(4, UartStopBits::Stop2, 1, Parity::None).is_err());
+        assert!(UartBitbangConfig::new(9, UartStopBits::Stop2, 1, Parity::None).is_err());
+        // Check all valid configurations of 5-8 data bits
+        let test_msg = b"data_bits TEST STRING";
+        for data_bits in 5..=8 {
+            let data_mask = ((0x1u16 << data_bits) - 1) as u8;
+            let msg = test_msg.iter().map(|b| b & data_mask).collect::<Vec<_>>();
+            uart_encode_decode(
+                UartBitbangConfig::new(data_bits, UartStopBits::Stop2, 1, Parity::None)?,
+                Some(&msg),
+            )?;
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn stop_bits() -> Result<()> {
+        // Check invalid configurations
+        assert!(UartBitbangConfig::new(8, UartStopBits::Stop1_5, 1, Parity::None).is_err());
+        // Check valid configurations
+        for stop_bits in [UartStopBits::Stop1, UartStopBits::Stop2] {
+            uart_encode_decode(
+                UartBitbangConfig::new(8, stop_bits, 1, Parity::None)?,
+                Some(b"hello from the `stop_bits()` test!"),
+            )?;
+        }
+        // Check stop bits are being applied correctly
+        let mut samples = Vec::new();
+        UartBitbangEncoder::<0>::new(UartBitbangConfig::new(
+            8,
+            UartStopBits::Stop1,
+            1,
+            Parity::None,
+        )?)
+        .encode_character(0xA5, &mut samples);
+        assert_eq!(&samples, &[0, 1, 0, 1, 0, 0, 1, 0, 1, 1]);
+        samples.clear();
+        UartBitbangEncoder::<0>::new(UartBitbangConfig::new(
+            8,
+            UartStopBits::Stop2,
+            1,
+            Parity::None,
+        )?)
+        .encode_character(0xA5, &mut samples);
+        assert_eq!(&samples, &[0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1]);
+        // Check error on incorrect number of stop bits
+        assert_eq!(
+            UartBitbangDecoder::<0>::new(UartBitbangConfig::new(
+                8,
+                UartStopBits::Stop2,
+                1,
+                Parity::None
+            )?)
+            .decode_samples(&vec![0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0])?,
+            vec![UartTransfer::Broken {
+                data: 0xA5,
+                parity: None,
+                error: UartTransferDecodeError::InvalidStop,
+            }]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn parity_bits() -> Result<()> {
+        let tests = [
+            (Parity::None, vec![0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1]),
+            (Parity::Odd, vec![0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1]),
+            (Parity::Even, vec![0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1]),
+        ];
+        for (parity, expected) in tests {
+            // Basic encode/decode smoke test for the parity
+            uart_encode_decode(
+                UartBitbangConfig::new(8, UartStopBits::Stop2, 1, parity)?,
+                Some(b"this string is used for testing parity"),
+            )?;
+            // Check the parity bit is calculated correctly
+            let config = UartBitbangConfig::new(8, UartStopBits::Stop2, 1, parity)?;
+            let mut samples = Vec::new();
+            UartBitbangEncoder::<0>::new(config.clone()).encode_character(0xA5, &mut samples);
+            assert_eq!(&samples, &expected);
+            // Check single parity errors are caught by the decoder.
+            if parity == Parity::None {
+                continue;
+            }
+            let mut decoder = UartBitbangDecoder::<0>::new(config.clone());
+            let mut invalid_samples = expected.clone();
+            // Introduce an error on the parity bit
+            invalid_samples[9] = !invalid_samples[9] & 0x01;
+            assert_eq!(
+                decoder.decode_samples(&invalid_samples)?,
+                vec![UartTransfer::Broken {
+                    data: 0xA5,
+                    parity: Some(invalid_samples[9] != 0),
+                    error: UartTransferDecodeError::ParityMismatch,
+                }]
+            );
+            decoder.reset();
+            // Introduce an error on a single data bit
+            for data_bit in 0..8 {
+                invalid_samples = expected.clone();
+                invalid_samples[1 + data_bit] = !expected[1 + data_bit] & 0x01;
+                assert_eq!(
+                    decoder.decode_samples(&invalid_samples)?,
+                    vec![UartTransfer::Broken {
+                        data: 0xA5 ^ (0x1 << data_bit),
+                        parity: Some(expected[9] != 0),
+                        error: UartTransferDecodeError::ParityMismatch,
+                    }]
+                );
+                decoder.reset();
+            }
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn breaks() -> Result<()> {
+        // Encode/decode tests for a variety of break times
+        let break_transfer = [
+            UartTransfer::Byte { data: 0x12 },
+            UartTransfer::Byte { data: 0x34 },
+            UartTransfer::Break,
+            UartTransfer::Byte { data: 0x56 },
+            UartTransfer::Byte { data: 0x78 },
+        ];
+        for break_cycles in 1..=5 {
+            let config =
+                UartBitbangConfig::new(8, UartStopBits::Stop2, break_cycles, Parity::None)?;
+            let encoder = UartBitbangEncoder::<0>::new(config.clone());
+            let mut decoder = UartBitbangDecoder::<0>::new(config.clone());
+            let mut samples = Vec::new();
+            encoder.encode_transfers(&break_transfer, &mut samples)?;
+            assert!(
+                samples.len() > (config.bit_time_per_frame() as usize) * (break_cycles as usize)
+            );
+            let decoded = decoder
+                .decode_samples(&samples)
+                .expect("Should have decoded the bitbanged transmission");
+            assert!(decoder.is_idle());
+            assert_eq!(&break_transfer, &decoded[..]);
+        }
+        // Check error on incorrect break time when decoding
+        assert_eq!(
+            UartBitbangDecoder::<0>::new(UartBitbangConfig::new(
+                8,
+                UartStopBits::Stop2,
+                2,
+                Parity::None
+            )?)
+            .decode_samples(&vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])?,
+            vec![UartTransfer::Broken {
+                data: 0x00,
+                parity: None,
+                error: UartTransferDecodeError::InvalidBreak,
+            }]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn partial_transfers() -> Result<()> {
+        // Check that the UART decoder is stateful, and correctly handles
+        // input samples of partial UART transfers.
+        let mut decoder = UartBitbangDecoder::<0>::new(UartBitbangConfig::new(
+            8,
+            UartStopBits::Stop2,
+            1,
+            Parity::None,
+        )?);
+        assert!(decoder.is_idle());
+        for sample in [0, 1, 0, 1, 0, 0, 1, 0, 1, 1] {
+            assert!(decoder.decode_samples(&vec![sample])?.is_empty());
+            assert!(!decoder.is_idle());
+        }
+        assert_eq!(
+            decoder.decode_samples(&vec![1])?,
+            vec![UartTransfer::Byte { data: 0xA5 }]
+        );
+        assert!(decoder.is_idle());
+        Ok(())
+    }
+}
