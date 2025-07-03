5f91fe94e564f63e90427337cb013f23071aa5c2
Alex Jones <alex.jones@lowrisc.org>
2025-07-03 14:51:14 +0100

    [opentitanlib, testutils] Parameterize bits per word in SPI bitbanging
    The current SPI bitbanging decoding functionality only supports reading
    SPI words that are bytes (8 bits long). Parameterize the `bits_per_word`
    configuration value to allow an arbitrary number of bits to be read.
    
    Words are read as consecutive bytes, where word sizes that are not
    evenly divisible into bytes (e.g. 12 bits) are such that the last byte
    will store the remainder of bits in its MSB,
    e.g. masks of [0b11111111, 0b1110000] for an 11-bit word.
    
    Signed-off-by: Alex Jones <alex.jones@lowrisc.org>

--- sw/host/tests/chip/spi_device/src/spi_host_config_test.rs
+++ sw/host/tests/chip/spi_device/src/spi_host_config_test.rs
@@ -105,6 +105,7 @@ fn spi_host_config_test(
         cpol: ctx.cpol == 1,
         cpha: ctx.cpha == 1,
         data_mode: test_utils::bitbanging::spi::SpiDataMode::Single,
+        bits_per_word: 8,
     };
     let decoded = decoder.run(samples.to_owned())?;
     assert_eq!(
