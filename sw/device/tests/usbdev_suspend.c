11d16aa8e5e2e6755737000dc02ee5499336d4ad
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-07-03 14:25:02 +0000

    [usb,tests] Enable USB logging
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/tests/usbdev_suspend.c
+++ sw/device/tests/usbdev_suspend.c
@@ -1816,7 +1816,7 @@ bool usbdev_suspend_test(usbdev_suspend_phase_t init_phase,
       host_resumes = true;
       host_resets = true;
       host_disconnects = true;
-      verbose = false;
+      verbose = true;
       break;
 
     default:
@@ -1835,7 +1835,7 @@ bool usbdev_suspend_test(usbdev_suspend_phase_t init_phase,
 
       // Presently, the FPGA build is expected to be observed/monitored by a
       // developer, so verbose reporting is appropriate.
-      verbose = false;  // true;
+      verbose = true;  // true;
       break;
   }
 
