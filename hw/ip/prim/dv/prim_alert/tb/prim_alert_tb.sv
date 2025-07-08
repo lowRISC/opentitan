4065c04673557a747f8f985022da95165337f39d
Robert Schilling <rschilling@rivosinc.com>
2025-07-08 09:21:37 +0200

    [hw,prim_alert,dv] Add 3 cycles clock skew to the testbench config
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/prim/dv/prim_alert/tb/prim_alert_tb.sv
+++ hw/ip/prim/dv/prim_alert/tb/prim_alert_tb.sv
@@ -33,6 +33,11 @@ module prim_alert_tb;
   `else
     localparam bit IsFatal = 0;
   `endif
+  `ifdef IS_3_CYCLE_SKEW
+    localparam int unsigned SkewCycles = 3;
+  `else
+    localparam int unsigned SkewCycles = 1;
+  `endif
 
   localparam time ClkPeriod  = 10_000;
   localparam int  WaitCycle = IsAsync ? 3 : 1;
@@ -85,7 +90,7 @@ module prim_alert_tb;
   prim_mubi_pkg::mubi4_t     init_trig = prim_mubi_pkg::MuBi4False;
   prim_alert_sender #(
     .AsyncOn(IsAsync),
-    .SkewCycles(1),
+    .SkewCycles(SkewCycles),
     .IsFatal(IsFatal)
   ) i_alert_sender (
     .clk_i(clk),
@@ -100,7 +105,7 @@ module prim_alert_tb;
 
   prim_alert_receiver #(
     .AsyncOn(IsAsync),
-    .SkewCycles(1)
+    .SkewCycles(SkewCycles)
   ) i_alert_receiver (
     .clk_i(clk),
     .rst_ni(rst_n),
