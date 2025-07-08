2eb86f086c18d3c6b70560ee12fbc995eeaa33f5
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-07-08 19:17:50 +0100

    [alert,fpv] Add missing definitions of SkewCycles in FPV tbs
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/prim/fpv/tb/prim_alert_rxtx_async_fatal_tb.sv
+++ hw/ip/prim/fpv/tb/prim_alert_rxtx_async_fatal_tb.sv
@@ -37,6 +37,8 @@ module prim_alert_rxtx_async_fatal_tb
   localparam bit AsyncOn = 1'b1;
   localparam bit IsFatal = 1'b1;
 
+  localparam int unsigned SkewCycles = 1;
+
   logic ping_pd;
   logic ping_nd;
   logic ack_pd;
@@ -71,9 +73,9 @@ module prim_alert_rxtx_async_fatal_tb
   assign alert_tx_in.alert_n = alert_nq[alert_skew_i[1]] ^ alert_err_ni;
 
   prim_alert_sender #(
-    .AsyncOn    ( AsyncOn ),
-    .SkewCycles ( 1       ),
-    .IsFatal    ( IsFatal )
+    .AsyncOn    ( AsyncOn    ),
+    .SkewCycles ( SkewCycles ),
+    .IsFatal    ( IsFatal    )
   ) i_prim_alert_sender (
     .clk_i,
     .rst_ni,
