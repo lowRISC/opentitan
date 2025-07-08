9b45d395d4440e7d7cd6c3c2a0c628fa95297f6f
Hakim Filali <hfilali@lowrisc.org>
2025-07-08 14:45:19 +0200

    [crypto,sw] Add is on curve check to base point mult
    This commit adds a is on curve check to the base point
    multiplication after the multiplication is done. This
    validates that the product is still on the curve and
    acts as a fault injection countermeasure.
    
    Signed-off-by: Hakim Filali <hfilali@lowrisc.org>

--- sw/otbn/crypto/p384_base_mult.s
+++ sw/otbn/crypto/p384_base_mult.s
@@ -16,6 +16,80 @@
  *               G is the base point of curve P-384, and
  *               d is a 384-bit scalar.
  *
+ * This routine calls the base point multiplication routine.
+ * Furthermore, this routine does an is on curve check on the result.
+ *
+ * @param[in]  dmem[d0]: 1st private key share d0 in dmem
+ * @param[in]  dmem[d1]: 2nd private key share d1 in dmem
+ * @param[out]  dmem[x]: x-coordinate in dmem
+ * @param[out]  dmem[y]: y-coordinate in dmem
+ *
+ * 384-bit quantities have to be provided in dmem in little-endian format,
+ * 512 bit aligned, with the highest 128 bit set to zero.
+ *
+ * Flags: When leaving this subroutine, the M, L and Z flags of FG0 correspond
+ *        to the computed affine y-coordinate.
+ *
+ * clobbered registers: x2, x3, x9 to x13, x17 to x23, x26 to x30
+ *                      w0 to w30
+ * clobbered flag groups: FG0
+ */
+.globl p384_base_mult_checked
+p384_base_mult_checked:
+  jal       x1, p384_base_mult
+
+  /* load left and right hand side output
+     addresses for the is on curve check */
+  la        x22, rhs
+  la        x23, lhs
+
+  /* load domain parameter p (modulus)
+     [w13, w12] = p = dmem[p384_p] */
+  li        x2, 12
+  la        x3, p384_p
+  bn.lid    x2++, 0(x3)
+  bn.lid    x2++, 32(x3)
+
+  /* call curve point test routine in P-384 lib */
+  jal       x1, p384_isoncurve
+
+  /* Load both sides of the equation.
+       [w7, w6] <= dmem[rhs]
+       [w5, w4] <= dmem[lhs] */
+  li        x2, 6
+  bn.lid    x2++, 0(x22)
+  bn.lid    x2, 32(x22)
+  li        x2, 4
+  bn.lid    x2++, 0(x23)
+  bn.lid    x2, 32(x23)
+
+  /* Compare the two sides of the equation.
+       FG0.Z <= (y^2) mod p == (x^2 + ax + b) mod p */
+  bn.sub    w0, w4, w6
+  bn.subb   w1, w5, w7
+
+  bn.cmp    w0, w31
+
+  /* Fail if FG0.Z is false. */
+  jal       x1, trigger_fault_if_fg0_not_z
+
+  bn.cmp    w1, w31
+
+  /* Fail if FG0.Z is false. */
+  jal       x1, trigger_fault_if_fg0_not_z
+
+  ret
+
+
+/**
+ * Externally callable routine for P-384 base point multiplication
+ *
+ * returns Q = d (*) G
+ *         where Q is a resulting valid P-384 curve point in affine
+ *                   coordinates,
+ *               G is the base point of curve P-384, and
+ *               d is a 384-bit scalar.
+ *
  * Sets up context and calls the internal scalar multiplication routine.
  * This routine runs in constant time.
  *
@@ -121,6 +195,16 @@ x:
 .globl y
 .weak y
 y:
+  .zero 64
+
+/* buffer for right side result of Weierstrass equation */
+.globl rhs
+rhs:
+  .zero 64
+
+/* buffer for left side result of Weierstrass equation */
+.globl lhs
+lhs:
   .zero 64
 
 /* 704 bytes of scratchpad memory */
