ce79a5464bb2e1ce217f2f6d64fb081bd045b34c
Douglas Reis <doreis@lowrisc.org>
2025-07-09 11:14:39 +0100

    [reggen, systemrdl] Refactor, move exporter away from reg_block.py
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- util/reggen/systemrdl_exporter.py
+++ util/reggen/systemrdl_exporter.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 from typing import TextIO
 
 from reggen.ip_block import IpBlock
+from reggen.reg_block import RegBlock
 from reggen.exporter import Exporter
 
 from systemrdl import RDLCompiler  # type: ignore[attr-defined]
@@ -16,6 +17,31 @@ from systemrdl.messages import FileSourceRef  # type: ignore[attr-defined]
 from systemrdl.importer import RDLImporter
 from systemrdl.component import Addrmap
 from peakrdl_systemrdl import exporter
+
+
+@dataclass
+class RegBlock2Systemrdl:
+    inner: RegBlock
+    importer: RDLImporter
+
+    def export(self) -> Addrmap | None:
+        nonempty = False
+
+        name = self.inner.name or "none"
+        rdl_addrmap_t = self.importer.create_addrmap_definition(name)
+        rdl_addrmap = self.importer.instantiate_addrmap(rdl_addrmap_t, name, 0)
+
+        # registers and multiregs
+        for name, flat_reg in self.inner.name_to_flat_reg.items():
+            nonempty = True
+            self.importer.add_child(rdl_addrmap, flat_reg.to_systemrdl(self.importer))
+
+        # windows
+        for window in self.inner.windows:
+            nonempty = True
+            self.importer.add_child(rdl_addrmap, window.to_systemrdl(self.importer))
+
+        return rdl_addrmap if nonempty else None
 
 
 @dataclass
@@ -29,7 +55,7 @@ class IpBlock2Systemrdl:
         rdl_addrmap = self.importer.create_addrmap_definition(self.inner.name)
 
         for rb in self.inner.reg_blocks.values():
-            rdl_rb = rb.to_systemrdl(self.importer)
+            rdl_rb = RegBlock2Systemrdl(rb, self.importer).export()
 
             # Skip empty interfaces
             if rdl_rb is None:
