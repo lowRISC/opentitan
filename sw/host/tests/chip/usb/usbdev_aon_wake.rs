c286f6540a93873e451cebcec39591b7ac31d78e
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-07-10 12:29:49 +0000

    [usb,tests] Document USB permissions and make test point to the doc
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/host/tests/chip/usb/usbdev_aon_wake.rs
+++ sw/host/tests/chip/usb/usbdev_aon_wake.rs
@@ -95,7 +95,8 @@ fn usbdev_aon_wake(opts: &Opts, transport: &TransportWrapper, uart: &dyn Uart) -
 
     // Next, we suspend the device by directly accessing the parent hub.
     let _ = UartConsole::wait_for(uart, r"configured, waiting for suspend", opts.timeout)?;
-    let hub = UsbHub::from_device(&parent).context("for this test, you need to make sure that the program has sufficient permissions to access the hub")?;
+    let hub = UsbHub::from_device(&parent).context("for this test, you need to make sure that the program has sufficient permissions to access the hub\n
+        See sw/host/tests/chip/usb/README.md for more information")?;
     log::info!("suspend device");
     hub.op(UsbHubOp::Suspend, port, Duration::from_millis(1000), !opts.usb.relaxed_hub_op)?;
     let _ = UartConsole::wait_for(uart, r"suspended, waiting for", opts.timeout)?;
