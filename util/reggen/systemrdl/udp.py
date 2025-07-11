3a0b21dc9a32e8855c1d5947583d78c98b1bcfd3
Douglas Reis <doreis@lowrisc.org>
2025-07-11 11:55:45 +0100

    [reggen, systemrdl] Add Hwre as a custom property (UDP)
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- util/reggen/systemrdl/udp.py
+++ util/reggen/systemrdl/udp.py
@@ -7,7 +7,7 @@ from typing import Any
 from systemrdl.udp import UDPDefinition
 from systemrdl import RDLCompiler  # type: ignore[attr-defined]
 from systemrdl.node import Node
-from systemrdl.component import Field
+from systemrdl.component import Reg
 
 
 class UDPBoolean(UDPDefinition):
@@ -19,6 +19,11 @@ class UDPBoolean(UDPDefinition):
             self.msg.error("The type of {self.name} must be bool.", self.get_src_ref(node))
 
 
+class Hwre(UDPBoolean):
+    name = "Hwre"
+    valid_components = {Reg}
+
+
 OPENTITAN_UDPS = Path(__file__).parent / "udp.rdl"
 
 
@@ -26,4 +31,5 @@ def register_udps(compiler: RDLCompiler) -> None:
     """
     Register opentitan specific UDPs (User Defined Properties)
     """
+    compiler.register_udp(Hwre)
     compiler.compile_file(OPENTITAN_UDPS)
