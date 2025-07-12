3cb8809fec81f83be7f30d9460a6e8e3b58eb9fa
Douglas Reis <doreis@lowrisc.org>
2025-07-12 13:22:28 +0100

    [util, testplantool] Create a tool to manipulate the testsplans
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

---
+++ util/testplantool/README.md
@@ -0,0 +1,9 @@
+# Testplantool
+
+A tool to manipulate and extract information from the testplans.
+
+## Exporting a csv for the top earlgrey:
+```sh
+python util/testplantool/testplantool.py export-csv \
+hw/top_earlgrey/data/chip_testplan.hjson /tmp/earlgrey.csv
+```
---
+++ util/testplantool/testplanlib/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright lowRISC contributors (OpenTitan project).
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+from .lib import Testplan
+
+__all__ = [
+    "Testplan",
+]
