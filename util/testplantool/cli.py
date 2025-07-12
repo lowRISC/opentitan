5e6203bce1dbf749cd95857588097f73a59c371e
Douglas Reis <doreis@lowrisc.org>
2025-07-12 15:01:48 +0100

    [util, testplantool] Add subcommand to query testpoints
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- util/testplantool/cli.py
+++ util/testplantool/cli.py
@@ -5,7 +5,7 @@
 
 import click
 from pathlib import Path
-from testplanlib import Testplan
+from testplanlib.lib import Testplan
 
 
 @click.group()
@@ -28,3 +28,43 @@ def main():
 def export_csv(input_file: str, out: str):
     Testplan.from_top(Path(input_file)).csv(Path(out))
     print("Successfully finished!")
+
+
+@main.command()
+@click.argument(
+    "input_file",
+    type=click.Path(writable=True),
+    # help="The top tesplan.hjson.",
+)
+@click.option(
+    "--name",
+    "-n",
+    default=".*",
+    type=str,
+    # help="name regex",
+)
+@click.option(
+    "--stage",
+    "-s",
+    default=".*",
+    type=str,
+    # help="dv stage regex",
+)
+@click.option(
+    "--si-stage",
+    "-i",
+    default=".*",
+    type=str,
+    # help="SiVal stage regex",
+)
+@click.option(
+    "--lc-state",
+    "-l",
+    default=".*",
+    type=str,
+    # help="life cycle regex",
+)
+def query(input_file: str, name: str, stage: str, si_stage: str, lc_state: str):
+    tp = Testplan.from_top(Path(input_file))
+    tp = tp.filter_testpoints(name=name, stage=stage, si_stage=si_stage, lc_state=lc_state)
+    tp.debug()
