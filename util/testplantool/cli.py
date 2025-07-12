d9abb3dec14436611ebf71403532fb17c57a1d3e
Douglas Reis <doreis@lowrisc.org>
2025-07-12 15:01:48 +0100

    [util, testplantool] Add subcommand to query testpoints
    Signed-off-by: Douglas Reis <doreis@lowrisc.org>

--- util/testplantool/cli.py
+++ util/testplantool/cli.py
@@ -30,3 +30,53 @@ def export_csv(input_file: str, out: str):
     """
     Testplan.from_top(Path(input_file)).csv(Path(out))
     print("Successfully finished!")
+
+
+@main.command()
+@click.argument(
+    "input_file",
+    type=click.Path(writable=True),
+)
+@click.option(
+    "--name",
+    "-n",
+    default=".*",
+    type=str,
+)
+@click.option(
+    "--stage",
+    "-s",
+    default=".*",
+    type=str,
+)
+@click.option(
+    "--si-stage",
+    "-i",
+    default=".*",
+    type=str,
+)
+@click.option(
+    "--lc-state",
+    "-l",
+    default=".*",
+    type=str,
+)
+@click.option(
+    "--fields",
+    "-f",
+    default=None,
+    type=str,
+)
+def query(input_file: str, name: str, stage: str, si_stage: str, lc_state: str, fields: str | None):
+    """Query testpoints from INPUT_FILE.hjson with filters.
+
+    INPUT_FILE is the top testplan.hjson.
+    NAME is an optional regex filter applied to the testpoint name.
+    STAGE is an optional regex filter applied to the testpoint stage.
+    SI-STAGE is an optional regex filter applied to the testpoint si-stage.
+    LC-STATE is an optional regex filter applied to the testpoint LC-STATE.
+    FIELDS is an optional comma separated list of fields that should be in the output.
+    """
+    tp = Testplan.from_top(Path(input_file))
+    tp = tp.filter_testpoints(name=name, stage=stage, si_stage=si_stage, lc_state=lc_state)
+    tp.debug()
