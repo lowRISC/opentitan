93ba05da4e2bfe2970d5b09f39451e2b21e86394
Siemen Dhooghe <sdhooghe@google.com>
2025-07-17 11:27:34 +0200

    [pentest] Add data_ind_timing option
    Add the option to set the data_ind_timing with all tests via the init
    function.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>
    (cherry picked from commit 97b9f25c0d4fa35c536a79c5e2a33a91ab9a9131)

--- sw/device/tests/penetrationtests/firmware/sca/hmac_sca.c
+++ sw/device/tests/penetrationtests/firmware/sca/hmac_sca.c
@@ -117,7 +117,8 @@ status_t handle_hmac_pentest_init(ujson_t *uj) {
       uj_cpuctrl_data.enable_sram_readback, &uj_output.clock_jitter_locked,
       &uj_output.clock_jitter_en, &uj_output.sram_main_readback_locked,
       &uj_output.sram_ret_readback_locked, &uj_output.sram_main_readback_en,
-      &uj_output.sram_ret_readback_en));
+      &uj_output.sram_ret_readback_en, uj_cpuctrl_data.enable_data_ind_timing,
+      &uj_output.data_ind_timing_en));
 
   // Enable the HMAC module and disable unused IP blocks to improve
   // SCA measurements.
