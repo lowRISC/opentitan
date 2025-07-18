3de50ff90b1e3dcb5282f62b69272ba53f30b919
Pirmin Vogel <vogelpi@lowrisc.org>
2025-07-18 12:02:29 +0200

    [pre_syn] Align Yosys synthesis setups with latest prim changes
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

--- hw/ip/kmac/pre_syn/syn_yosys.sh
+++ hw/ip/kmac/pre_syn/syn_yosys.sh
@@ -72,13 +72,15 @@ OT_DEP_SOURCES=(
     "$LR_SYNTH_SRC_DIR"/../prim/rtl/prim_onehot_check.sv
     "$LR_SYNTH_SRC_DIR"/../prim/rtl/prim_mubi4_sender.sv
     "$LR_SYNTH_SRC_DIR"/../prim/rtl/prim_fifo_sync_cnt.sv
-    "$LR_SYNTH_SRC_DIR"/../prim_generic/rtl/prim_generic_flop.sv
-    "$LR_SYNTH_SRC_DIR"/../prim_generic/rtl/prim_generic_flop_2sync.sv
-    "$LR_SYNTH_SRC_DIR"/../prim_xilinx/rtl/prim_xilinx_flop.sv
-    "$LR_SYNTH_SRC_DIR"/../prim_xilinx/rtl/prim_xilinx_flop_en.sv
-    "$LR_SYNTH_SRC_DIR"/../prim_xilinx/rtl/prim_xilinx_buf.sv
-    "$LR_SYNTH_SRC_DIR"/../prim_xilinx/rtl/prim_xilinx_xor2.sv
-    "$LR_SYNTH_SRC_DIR"/../prim_xilinx/rtl/prim_xilinx_xnor2.sv
+    "$LR_SYNTH_SRC_DIR"/../prim/rtl/prim_sec_anchor_buf.sv
+    "$LR_SYNTH_SRC_DIR"/../prim/rtl/prim_sec_anchor_flop.sv
+    "$LR_SYNTH_SRC_DIR"/../prim_generic/rtl/prim_flop_2sync.sv
+    "$LR_SYNTH_SRC_DIR"/../prim_xilinx/rtl/prim_flop.sv
+    "$LR_SYNTH_SRC_DIR"/../prim_xilinx/rtl/prim_flop_en.sv
+    "$LR_SYNTH_SRC_DIR"/../prim_xilinx/rtl/prim_and2.sv
+    "$LR_SYNTH_SRC_DIR"/../prim_xilinx/rtl/prim_buf.sv
+    "$LR_SYNTH_SRC_DIR"/../prim_xilinx/rtl/prim_xor2.sv
+    "$LR_SYNTH_SRC_DIR"/../prim_xilinx/rtl/prim_xnor2.sv
     "$LR_SYNTH_SRC_DIR"/../tlul/rtl/tlul_adapter_sram.sv
     "$LR_SYNTH_SRC_DIR"/../tlul/rtl/tlul_sram_byte.sv
     "$LR_SYNTH_SRC_DIR"/../tlul/rtl/tlul_socket_1n.sv
@@ -124,16 +126,6 @@ for file in "${OT_DEP_SOURCES[@]}"; do
         $file \
         > $LR_SYNTH_OUT_DIR/generated/${module}.v
 
-    # Make sure auto-generated primitives are resolved to generic or Xilinx-specific primitives
-    # where available.
-    sed -i 's/prim_flop/prim_xilinx_flop/g'              $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_xilinx_flop_2sync/prim_generic_flop_2sync/g' \
-        $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_sec_anchor_flop/prim_xilinx_flop/g'   $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_buf/prim_xilinx_buf/g'                $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_sec_anchor_buf/prim_xilinx_buf/g'     $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_xor2/prim_xilinx_xor2/g'              $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_xnor2/prim_xilinx_xnor2/g'            $LR_SYNTH_OUT_DIR/generated/${module}.v
 done
 
 # Rename the prim_sparse_fsm_flop module. For some reason, sv2v decides to append a suffix.
@@ -157,17 +149,6 @@ for file in "$LR_SYNTH_SRC_DIR"/rtl/*.sv; do
         $file \
         > $LR_SYNTH_OUT_DIR/generated/${module}.v
 
-    # Make sure auto-generated primitives are resolved to generic or Xilinx-specific primitives
-    # where available.
-    sed -i 's/prim_flop/prim_xilinx_flop/g'              $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_xilinx_flop_2sync/prim_generic_flop_2sync/g' \
-        $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_sec_anchor_flop/prim_xilinx_flop/g'   $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_buf/prim_xilinx_buf/g'                $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_sec_anchor_buf/prim_xilinx_buf/g'     $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_xor2/prim_xilinx_xor2/g'              $LR_SYNTH_OUT_DIR/generated/${module}.v
-    sed -i 's/prim_xnor2/prim_xilinx_xnor2/g'            $LR_SYNTH_OUT_DIR/generated/${module}.v
-
     # Rename prim_sparse_fsm_flop instances. For some reason, sv2v decides to append a suffix.
     sed -i 's/prim_sparse_fsm_flop_.*/prim_sparse_fsm_flop \#(/g' \
         $LR_SYNTH_OUT_DIR/generated/${module}.v
@@ -178,6 +159,7 @@ for file in "$LR_SYNTH_SRC_DIR"/rtl/*.sv; do
     sed -i '/\.StateEnumT_StateWidth(.*/d' $LR_SYNTH_OUT_DIR/generated/${module}.v
     sed -i '/\.StateEnumT_StateWidthPad(.*/d' $LR_SYNTH_OUT_DIR/generated/${module}.v
     sed -i '/\.StateEnumT_sha3_pkg.*(.*/d' $LR_SYNTH_OUT_DIR/generated/${module}.v
+    sed -i '/\.StateEnumT_kmac_pkg.*(.*/d' $LR_SYNTH_OUT_DIR/generated/${module}.v
 done
 
 #-------------------------------------------------------------------------
