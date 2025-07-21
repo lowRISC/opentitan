53a3800a532416f2335ae9889eedb57ca10ef063
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-07-21 16:17:10 +0200

    [crypto] Add security_config_check function
    This function checks, whether jittery clock, the dummy instructions,
    and the data independent timing is enabled.
    
    Addresses lowRISC/opentitan#27646
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit 099047ab5d6492acd35cc37d800fbde3ef2f6246)

--- sw/device/lib/crypto/impl/aes_gcm.c
+++ sw/device/lib/crypto/impl/aes_gcm.c
@@ -16,6 +16,7 @@
 #include "sw/device/lib/crypto/impl/aes_gcm/ghash.h"
 #include "sw/device/lib/crypto/impl/integrity.h"
 #include "sw/device/lib/crypto/impl/keyblob.h"
+#include "sw/device/lib/crypto/impl/security_config.h"
 #include "sw/device/lib/crypto/impl/status.h"
 #include "sw/device/lib/crypto/include/datatypes.h"
 
@@ -391,6 +392,9 @@ otcrypto_status_t otcrypto_aes_gcm_encrypt_init(
   if (key == NULL || key->keyblob == NULL || iv.data == NULL || ctx == NULL) {
     return OTCRYPTO_BAD_ARGS;
   }
+
+  // Check the security config of the device.
+  HARDENED_TRY(security_config_check(key->config.security_level));
 
   // Ensure entropy complex is initialized.
   HARDENED_TRY(entropy_complex_check());
--- sw/device/lib/crypto/impl/ecc_p384.c
+++ sw/device/lib/crypto/impl/ecc_p384.c
@@ -10,6 +10,7 @@
 #include "sw/device/lib/crypto/impl/ecc/p384.h"
 #include "sw/device/lib/crypto/impl/integrity.h"
 #include "sw/device/lib/crypto/impl/keyblob.h"
+#include "sw/device/lib/crypto/impl/security_config.h"
 #include "sw/device/lib/crypto/include/datatypes.h"
 
 // Module ID for status codes.
@@ -112,6 +113,9 @@ otcrypto_status_t otcrypto_ecdsa_p384_keygen_async_start(
   if (private_key == NULL || private_key->keyblob == NULL) {
     return OTCRYPTO_BAD_ARGS;
   }
+
+  // Check the security config of the device.
+  HARDENED_TRY(security_config_check(private_key->config.security_level));
 
   // Check the key mode.
   if (private_key->config.key_mode != kOtcryptoKeyModeEcdsaP384) {
@@ -288,6 +292,9 @@ otcrypto_status_t otcrypto_ecdsa_p384_sign_async_start(
       message_digest.data == NULL) {
     return OTCRYPTO_BAD_ARGS;
   }
+
+  // Check the security config of the device.
+  HARDENED_TRY(security_config_check(private_key->config.security_level));
 
   // Check that the entropy complex is initialized.
   HARDENED_TRY(entropy_complex_check());
