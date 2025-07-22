a9418ff48d239323cadc6d3fd1dfcf0b512cfa1f
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-07-22 09:31:58 +0200

    [crypto] Add security checks to CL
    When using CL, the device should be configured securely. This
    commit adds security config checks for each CL call. When
    security_level > low, the execution aborts if the device is
    not properly configured.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/lib/crypto/impl/rsa.c
+++ sw/device/lib/crypto/impl/rsa.c
@@ -11,6 +11,7 @@
 #include "sw/device/lib/crypto/impl/rsa/rsa_encryption.h"
 #include "sw/device/lib/crypto/impl/rsa/rsa_keygen.h"
 #include "sw/device/lib/crypto/impl/rsa/rsa_signature.h"
+#include "sw/device/lib/crypto/impl/security_config.h"
 #include "sw/device/lib/crypto/impl/status.h"
 #include "sw/device/lib/crypto/include/datatypes.h"
 
@@ -654,6 +655,9 @@ otcrypto_status_t otcrypto_rsa_sign_async_start(
     return OTCRYPTO_BAD_ARGS;
   }
 
+  // Check the security config of the device.
+  HARDENED_TRY(security_config_check(key->config.security_level));
+
   // Check that the entropy complex is initialized.
   HARDENED_TRY(entropy_complex_check());
 
@@ -931,6 +935,9 @@ otcrypto_status_t otcrypto_rsa_decrypt_async_start(
       ciphertext.data == NULL) {
     return OTCRYPTO_BAD_ARGS;
   }
+
+  // Check the security config of the device.
+  HARDENED_TRY(security_config_check(key->config.security_level));
 
   // Check that the entropy complex is initialized.
   HARDENED_TRY(entropy_complex_check());
