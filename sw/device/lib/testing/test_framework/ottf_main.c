7538a58d7f37f4cdb34291c0f5477b882cae2c89
James Wainwright <james.wainwright@lowrisc.org>
2025-07-23 16:35:24 +0100

    [test] Add log to OTTF that alert catcher is enabled
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- sw/device/lib/testing/test_framework/ottf_main.c
+++ sw/device/lib/testing/test_framework/ottf_main.c
@@ -183,6 +183,7 @@ void _ottf_main(void) {
 
 #if !OT_IS_ENGLISH_BREAKFAST
   if (!kOttfTestConfig.ignore_alerts) {
+    LOG_INFO("Enabling OTTF alert catcher");
     CHECK_STATUS_OK(ottf_alerts_enable_all());
   }
 #endif  // !OT_IS_ENGLISH_BREAKFAST
