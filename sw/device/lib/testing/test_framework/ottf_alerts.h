a10af2ce9322e2dc2f054ccfb1fd2e5ec62ece75
James Wainwright <james.wainwright@lowrisc.org>
2025-07-24 13:52:40 +0100

    [ottf] Allow alerts to be expected and checked
    Wrapping a code section with `ottf_alerts_expect_alert_{start,finish}`
    will cause OTTF to catch and then expect an alert to have fired. It is
    the responsibility of the test to ensure enough time has passed before
    `_finish` is called for the alert to have been caught.
    
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
    (cherry picked from commit da5842ec33e632d2ee8a497cc0436b046607c270)

--- sw/device/lib/testing/test_framework/ottf_alerts.h
+++ sw/device/lib/testing/test_framework/ottf_alerts.h
@@ -7,6 +7,7 @@
 
 #include "sw/device/lib/base/macros.h"
 #include "sw/device/lib/base/status.h"
+#include "sw/device/lib/dif/dif_alert_handler.h"
 #include "sw/device/lib/dif/dif_rv_plic.h"
 
 /**
@@ -22,6 +23,31 @@
  */
 OT_WARN_UNUSED_RESULT
 status_t ottf_alerts_enable_all(void);
+/**
+ * Record for the OTTF alert catcher that the given alert is expected.
+ *
+ * When this alert is caught, OTTF will not fault but will remember that
+ * the alert fired. Pair this with `ottf_alerts_expect_alert_finish` to
+ * confirm that the expected alert was indeed caught.
+ *
+ * @param alert The alert that is expected.
+ */
+OT_WARN_UNUSED_RESULT
+status_t ottf_alerts_expect_alert_start(dif_alert_handler_alert_t alert);
+
+/**
+ * Finish expecting an alert and confirm that OTTF caught it firing.
+ *
+ * Must be called after `ottf_alerts_expect_alert_start`. If the alert
+ * was not caught while it was expected, an error result will be returned.
+
+ * The alert will no longer be expected after this call, and OTTF will
+ * forget that it was caught.
+ *
+ * @param alert The alert that was expected.
+ */
+OT_WARN_UNUSED_RESULT
+status_t ottf_alerts_expect_alert_finish(dif_alert_handler_alert_t alert);
 
 /**
  * Check whether the OTTF alert catcher should handle a given IRQ.
