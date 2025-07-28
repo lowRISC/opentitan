e203ba13f19d3e26744bfa1130fe7adcec695ae1
Hakim Filali <hfilali@lowrisc.org>
2025-07-28 09:36:20 +0000

    [crypto/sw] Enable cryptolib map file analysis
    This PR contains necessary updates to allow for building the map
    file of the whole crypto lib and then use the map file to hjson
    python script to create a visual representation of the map file
    for easier analysis of the memory usage.
    
    Co-authored-by: Chris Frantz <cfrantz@google.com>
    Signed-off-by: Hakim Filali <hfilali@lowrisc.org>
    (cherry picked from commit  65c402fde4b2264aed8e35f6a3553c9028b0ea1b)

--- sw/device/tests/crypto/otcrypto_export_size.c
+++ sw/device/tests/crypto/otcrypto_export_size.c
@@ -5,12 +5,4 @@
 #include "sw/device/tests/crypto/otcrypto_interface.h"
 
 // Simple main function so the linker doesn't discard the inerface struct.
-int main(void) {
-  uint32_t digest_data[8];
-  otcrypto_hash_digest_t digest = {.data = digest_data, .len = 8};
-  otcrypto.sha2_256((otcrypto_const_byte_buf_t){.data = NULL, .len = 0},
-                    &digest);
-
-  // SHA256('') is a constant value; this will always be 0.
-  return digest_data[0] & 1;
-}
+void bare_metal_main(void) { (void)otcrypto; }
