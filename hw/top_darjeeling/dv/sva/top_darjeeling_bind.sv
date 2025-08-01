7de53d4b4ee6866a1e7b5deab29424ae0066e6ec
Kinza Qamar <kqzaman@lowrisc.org>
2025-08-01 15:04:57 +0100

    [chip, dv] Cleanups following #16503
    The PR #16503 removed pwrmgr_rstmgr_sva_if from top_earlgrey_bind
    and moved some assertions to pwrmgr_rstreqs_sva_if. This commit
    cleaned the affected vseqs.
    
    Signed-off-by: Kinza Qamar <kqzaman@lowrisc.org>

--- hw/top_darjeeling/dv/sva/top_darjeeling_bind.sv
+++ hw/top_darjeeling/dv/sva/top_darjeeling_bind.sv
@@ -17,5 +17,24 @@ module top_darjeeling_bind;
     .main_pd_n(u_pwrmgr_aon.pwr_ast_o.main_pd_n),
     .main_pok(u_pwrmgr_aon.pwr_ast_i.main_pok)
   );
+
+  bind top_darjeeling pwrmgr_rstreqs_sva_if pwrmgr_rstreqs_sva_if (
+    .clk_i(u_pwrmgr_aon.clk_i),
+    .rst_ni(u_pwrmgr_aon.rst_ni),
+    .clk_slow_i(u_pwrmgr_aon.clk_slow_i),
+    .rst_slow_ni(u_pwrmgr_aon.rst_slow_ni),
+    // Input resets.
+    .rstreqs_i(u_pwrmgr_aon.rstreqs_i),
+    .reset_en(u_pwrmgr_aon.reg2hw.reset_en),
+    .sw_rst_req_i(prim_mubi_pkg::mubi4_test_true_strict(u_pwrmgr_aon.sw_rst_req_i)),
+    .main_rst_req_i(!u_pwrmgr_aon.rst_main_ni),
+    .esc_rst_req_i(u_pwrmgr_aon.esc_rst_req_q),
+    .ndm_rst_req_i(u_pwrmgr_aon.ndmreset_req_i),
+    // The outputs from pwrmgr.
+    .rstreqs(u_pwrmgr_aon.pwr_rst_o.rstreqs),
+    .main_pd_n(u_pwrmgr_aon.pwr_ast_o.main_pd_n),
+    .reset_cause(u_pwrmgr_aon.pwr_rst_o.reset_cause)
+  );
 `endif
+
 endmodule
