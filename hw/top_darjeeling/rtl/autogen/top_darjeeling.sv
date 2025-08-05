5a0764a1d3030643a0af03be51a5f5ed7757250d
Pirmin Vogel <vogelpi@lowrisc.org>
2025-08-05 18:54:50 +0200

    [darjeeling,rtl] Enable flop between ROM and KMAC in Darjeeling
    Co-Authored-by: Rupert Swarbrick <rswarbrick@lowrisc.org>
    Signed-off-by: Pirmin Vogel <vogelpi@lowrisc.org>

--- hw/top_darjeeling/rtl/autogen/top_darjeeling.sv
+++ hw/top_darjeeling/rtl/autogen/top_darjeeling.sv
@@ -339,9 +339,9 @@ module top_darjeeling #(
   // local parameters for sram_ctrl_mbox
   localparam int SramCtrlMboxOutstanding = 2;
   // local parameters for rom_ctrl0
-  localparam bit RomCtrl0FlopToKmac = 1'b0;
+  localparam bit RomCtrl0FlopToKmac = 1'b1;
   // local parameters for rom_ctrl1
-  localparam bit RomCtrl1FlopToKmac = 1'b0;
+  localparam bit RomCtrl1FlopToKmac = 1'b1;
   // local parameters for racl_ctrl
   localparam int RaclCtrlNumSubscribingIps = 11;
 
