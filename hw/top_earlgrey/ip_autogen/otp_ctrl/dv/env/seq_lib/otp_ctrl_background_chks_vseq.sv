fa54332cb20ea23dda9c1f72c3c14e339f02d4d6
Guillermo Maturana <matute@zerorisc.com>
2025-08-08 21:28:37 +0000

    [dv,otp_ctrl] Fix otp_ctrl_background_chks
    Configure the timeout via CSRs on the last check. Before this change
    this test failed because the timeout was set after the checks were done.
    
    Signed-off-by: Guillermo Maturana <matute@zerorisc.com>

--- hw/top_earlgrey/ip_autogen/otp_ctrl/dv/env/seq_lib/otp_ctrl_background_chks_vseq.sv
+++ hw/top_earlgrey/ip_autogen/otp_ctrl/dv/env/seq_lib/otp_ctrl_background_chks_vseq.sv
@@ -1,4 +1,5 @@
 // Copyright lowRISC contributors (OpenTitan project).
+// Copyright zeroRISC Inc.
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
@@ -33,22 +34,26 @@ class otp_ctrl_background_chks_vseq extends otp_ctrl_dai_lock_vseq;
       // Write background check
       if (trigger_chks[0]) csr_wr(ral.integrity_check_period,   check_period);
       if (trigger_chks[1]) csr_wr(ral.consistency_check_period, check_period);
-      `uvm_info(`gfn, $sformatf("trigger background check %0h", trigger_chks), UVM_LOW)
+      `uvm_info(`gfn, $sformatf("trigger background check 0x%0h", trigger_chks), UVM_LOW)
 
       cfg.en_scb = 0;
       // According to spec, check period will append an 'hFF from the LSF. Add 10 cycle buffers for
       // register updates.
       check_wait_cycles = (check_period + 1) << 8 + 10;
 
-      // Wait for first check done
-      repeat($countones(trigger_chks)) begin
+      // Wait for first check done if there are multiple checks.
+      if ($countones(trigger_chks) > 1) begin
         csr_spinwait(.ptr(ral.status.check_pending), .exp_data(1),
                      .timeout_ns(cfg.clk_rst_vif.clk_period_ps / 1000 * check_wait_cycles));
 
         csr_spinwait(.ptr(ral.status.check_pending), .exp_data(0));
       end
 
-      // Configure timeout settings to trigger check error
+      // Wait for the last check to start to make the test more robust.
+      csr_spinwait(.ptr(ral.status.check_pending), .exp_data(1),
+                   .timeout_ns(cfg.clk_rst_vif.clk_period_ps / 1000 * check_wait_cycles));
+      // Configure the check timeout CSR so that the test will not complete before the timer
+      // count-down is done, which should cause a fatal alert.
       csr_wr(ral.check_timeout, $urandom_range(1, 5));
       `uvm_info(`gfn, "trigger check timeout error", UVM_LOW)
 
@@ -58,7 +63,6 @@ class otp_ctrl_background_chks_vseq extends otp_ctrl_dai_lock_vseq;
          cfg.clk_rst_vif.wait_clks(check_wait_cycles);,
          $sformatf("Timeout waiting for alert %0s", "fatal_check_error"))
       check_fatal_alert_nonblocking("fatal_check_error");
-
       cfg.clk_rst_vif.wait_clks($urandom_range(50, 1000));
       csr_rd_check(.ptr(ral.status.timeout_error), .compare_value(1));
     end
