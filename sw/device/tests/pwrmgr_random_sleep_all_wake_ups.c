e28030bce17bbd90bac6375540888a99a62c9a78
James Wainwright <james.wainwright@lowrisc.org>
2025-08-08 16:36:45 +0100

    [test] Ignore all alerts in sensor_ctrl tests
    On wakeup, the sensor ctrl triggers the recoverable alert. We don't have
    time to disable it in `test_main`, so just disable OTTF's alert catching
    all together. These tests handle alerts themselves anyway.
    
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
    (cherry picked from commit d7d0536325bfc7bd7db84c29488d1e642629c9ab)

--- sw/device/tests/pwrmgr_random_sleep_all_wake_ups.c
+++ sw/device/tests/pwrmgr_random_sleep_all_wake_ups.c
@@ -23,7 +23,10 @@
   This is tracked by a retention sram counter, given there are resets involved.
  */
 
-OTTF_DEFINE_TEST_CONFIG();
+// Test handles its own alerts.
+// `sensor_ctrl_aon_recov_alert` fires immediately on wake-up which doesn't
+// give us enough time to disable it specifically.
+OTTF_DEFINE_TEST_CONFIG(.catch_alerts = false);
 
 /**
  * Clean up pwrmgr wakeup reason register for the next round.
