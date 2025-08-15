cc83cd8a94803c8d40033447709190525768801d
Hakim Filali <hfilali@lowrisc.org>
2025-08-15 13:35:02 +0000

    [sw,cryptolib] Update execution time checks for OTBN
    Signed-off-by: Hakim Filali <hfilali@lowrisc.org>

--- sw/device/lib/crypto/impl/ecc/p384.c
+++ sw/device/lib/crypto/impl/ecc/p384.c
@@ -91,12 +91,12 @@ enum {
   /*
    * The expected instruction counts for constant time functions.
    */
-  kModeKeygenInsCnt = 1444128,
-  kModeKeygenSideloadInsCnt = 1444042,
-  kModeEcdhInsCnt = 1454936,
-  kModeEcdhSideloadInsCnt = 1455075,
-  kModeEcdsaSignInsCnt = 1505346,
-  kModeEcdsaSignSideloadInsCnt = 1505485,
+  kModeKeygenInsCnt = 1444125,
+  kModeKeygenSideloadInsCnt = 1444039,
+  kModeEcdhInsCnt = 1455729,
+  kModeEcdhSideloadInsCnt = 1455868,
+  kModeEcdsaSignInsCnt = 1505653,
+  kModeEcdsaSignSideloadInsCnt = 1505792,
 };
 
 static status_t p384_masked_scalar_write(const p384_masked_scalar_t *src,
