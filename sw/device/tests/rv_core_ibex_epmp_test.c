3573f1d6f0f26b806b6aceb7853d46dd6a1bcd9d
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-08-15 10:51:14 +0000

    [sw] Use OTTF console init functions instead of base_uart_dev
    Several tests already rely on the OTTF, so instead of manually
    initializing the UART0 and reinventing the wheel, just use the
    console code. Also enable the CW340 for several tests.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/tests/rv_core_ibex_epmp_test.c
+++ sw/device/tests/rv_core_ibex_epmp_test.c
@@ -8,9 +8,9 @@
 #include "sw/device/lib/runtime/ibex.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/runtime/pmp.h"
-#include "sw/device/lib/runtime/print.h"
 #include "sw/device/lib/testing/pinmux_testutils.h"
 #include "sw/device/lib/testing/test_framework/check.h"
+#include "sw/device/lib/testing/test_framework/ottf_console.h"
 #include "sw/device/lib/testing/test_framework/ottf_test_config.h"
 #include "sw/device/lib/testing/test_framework/status.h"
 #include "sw/device/silicon_creator/lib/dbg_print.h"
@@ -39,7 +39,6 @@ extern void (*finish_test)(void);
 /**
  * Diff handles
  */
-static dif_uart_t uart0;
 static dif_pinmux_t pinmux;
 
 /**
@@ -269,24 +268,10 @@ static void setup_uart(void) {
   // Initialise DIF handles
   CHECK_DIF_OK(dif_pinmux_init(
       mmio_region_from_addr(TOP_EARLGREY_PINMUX_AON_BASE_ADDR), &pinmux));
-  CHECK_DIF_OK(dif_uart_init(
-      mmio_region_from_addr(TOP_EARLGREY_UART0_BASE_ADDR), &uart0));
 
   // Initialise UART console.
   pinmux_testutils_init(&pinmux);
-  CHECK(kUartBaudrate <= UINT32_MAX, "kUartBaudrate must fit in uint32_t");
-  CHECK(kClockFreqPeripheralHz <= UINT32_MAX,
-        "kClockFreqPeripheralHz must fit in uint32_t");
-  CHECK_DIF_OK(dif_uart_configure(
-      &uart0, (dif_uart_config_t){
-                  .baudrate = (uint32_t)kUartBaudrate,
-                  .clk_freq_hz = (uint32_t)kClockFreqPeripheralHz,
-                  .parity_enable = kDifToggleDisabled,
-                  .parity = kDifUartParityEven,
-                  .tx_enable = kDifToggleEnabled,
-                  .rx_enable = kDifToggleEnabled,
-              }));
-  base_uart_stdout(&uart0);
+  ottf_console_init();
 }
 
 /**
