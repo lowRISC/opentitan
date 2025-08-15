edbc1ea8cda50750afebe93de16905cee11725ea
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-08-15 10:51:14 +0000

    [manuf,tests] Use OTTF console init functions
    These tests already rely on part of the OTTF, so instead of manually
    initializing the UART0, just use the console code.
    
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/silicon_creator/manuf/tests/sram_empty_functest.c
+++ sw/device/silicon_creator/manuf/tests/sram_empty_functest.c
@@ -4,12 +4,10 @@
 
 #include <stdint.h>
 
-#include "sw/device/lib/arch/device.h"
-#include "sw/device/lib/dif/dif_uart.h"
 #include "sw/device/lib/runtime/log.h"
-#include "sw/device/lib/runtime/print.h"
 #include "sw/device/lib/testing/pinmux_testutils.h"
 #include "sw/device/lib/testing/test_framework/check.h"
+#include "sw/device/lib/testing/test_framework/ottf_console.h"
 #include "sw/device/lib/testing/test_framework/ottf_test_config.h"
 #include "sw/device/lib/testing/test_framework/status.h"
 
@@ -17,29 +15,14 @@
 
 OTTF_DEFINE_TEST_CONFIG();
 
-static dif_uart_t uart;
-static dif_pinmux_t pinmux;
-
 bool test_main(void) {
-  // Initialize UART console.
+  // Initialize the pinmux (will setup UART0 pins).
+  dif_pinmux_t pinmux;
   CHECK_DIF_OK(dif_pinmux_init(
       mmio_region_from_addr(TOP_EARLGREY_PINMUX_AON_BASE_ADDR), &pinmux));
-  CHECK_DIF_OK(dif_uart_init(
-      mmio_region_from_addr(TOP_EARLGREY_UART0_BASE_ADDR), &uart));
   pinmux_testutils_init(&pinmux);
-  CHECK(kUartBaudrate <= UINT32_MAX, "kUartBaudrate must fit in uint32_t");
-  CHECK(kClockFreqPeripheralHz <= UINT32_MAX,
-        "kClockFreqPeripheralHz must fit in uint32_t");
-  CHECK_DIF_OK(dif_uart_configure(
-      &uart, (dif_uart_config_t){
-                 .baudrate = (uint32_t)kUartBaudrate,
-                 .clk_freq_hz = (uint32_t)kClockFreqPeripheralHz,
-                 .parity_enable = kDifToggleDisabled,
-                 .parity = kDifUartParityEven,
-                 .tx_enable = kDifToggleEnabled,
-                 .rx_enable = kDifToggleEnabled,
-             }));
-  base_uart_stdout(&uart);
+  // Initialize UART console.
+  ottf_console_init();
 
   LOG_INFO("Hello OpenTitan! We are executing from SRAM.");
 
