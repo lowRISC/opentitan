994f460cf730c0d3631175b613c4d7c25a594358
Adrian Lees <a.lees@lowrisc.org>
2025-08-18 17:07:22 +0100

    [dv,mbx] Remove unused class
    Signed-off-by: Adrian Lees <a.lees@lowrisc.org>

--- hw/ip/mbx/dv/env/seq_lib/mbx_vseq_list.sv
+++ hw/ip/mbx/dv/env/seq_lib/mbx_vseq_list.sv
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-`include "mbx_tl_reg_seq.sv"
 `include "mbx_tl_device_seq.sv"
 `include "mbx_base_vseq.sv"
 `include "mbx_common_vseq.sv"
