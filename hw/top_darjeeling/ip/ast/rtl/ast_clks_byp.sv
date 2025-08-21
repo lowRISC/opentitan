lowRISC/opentitan

commit 3a7a4aa782243b319f02c6feb55588b521cffb6c

[darjeeling,cleanup] Fix typos in comments/docs
Signed-off-by: Elliot Baptist <elliot.baptist@lowrisc.org>

hw/top_darjeeling/ip/ast/rtl/ast_clks_byp.sv
@@ -32,7 +32,7 @@ module ast_clks_byp (
 `endif // of AST_BYPASS_CLK
   input prim_mubi_pkg::mubi4_t io_clk_byp_req_i,    // External IO clock mux for OTP bootstrap
   input prim_mubi_pkg::mubi4_t all_clk_byp_req_i,   // External all clock mux override
-  input prim_mubi_pkg::mubi4_t ext_freq_is_96m_i,   // External Clock Frequecy is 96MHz (else 48MHz)
+  input prim_mubi_pkg::mubi4_t ext_freq_is_96m_i,   // External Clock Frequency is 96MHz (else 48MHz)
   output prim_mubi_pkg::mubi4_t io_clk_byp_ack_o,   // Switch IO clock to External clock
   output prim_mubi_pkg::mubi4_t all_clk_byp_ack_o,  // Switch all clocks to External clock
   output logic clk_src_sys_o,               // SYS Source Clock
@@ -149,7 +149,7 @@ prim_flop_2sync #(
 
 assign rst_aon_exda_n = scan_mode_i ? scan_reset_ni : rst_aon_n_exda;
 
-// External USB & AON clocks genaration
+// External USB & AON clocks generation
 ////////////////////////////////////////
 `ifndef AST_BYPASS_CLK
 logic clk_src_ext_usb, ext_freq_is_96m, ext_freq_is_96m_sync;
@@ -201,10 +201,10 @@ assign clk_ext_aon = clk_ext_aon_i;
 
 
 ////////////////////////////////////////
-// Deep-Sleep/Enables Gators
+// Deep-Sleep/Enables Gates
 ////////////////////////////////////////
 
-// Deep-Sleep Sync to External clcok
+// Deep-Sleep Sync to External clock
 ////////////////////////////////////////
 logic deep_sleep, deep_sleep_n;
 
@@ -543,7 +543,7 @@ prim_buf u_rst_clk_ext_aon (
   .out_o ( rst_clk_ext_aon_n )
 );
 
-// rst_aon_n deasset to io clock
+// rst_aon_n deassert to io clock
 ////////////////////////////////////////
 logic rst_aon_n_ioda, rst_aon_ioda_n;
 
@@ -747,7 +747,7 @@ prim_mubi4_sender #(
 logic io_clk_byp_is_48m_src, io_clk_byp_is_48m;
 
 // Oscillator source is always 96MHz.
-// External Bypass source is assume to be 96MHz until it is ebabled as 48MHz
+// External Bypass source is assume to be 96MHz until it is enabled as 48MHz
 always_ff @( posedge clk_aon, negedge rst_aon_n ) begin
   if ( !rst_aon_n ) begin
     io_clk_byp_is_48m_src <= 1'b0;

hw/top_darjeeling/data/chip_testplan.hjson
@@ -157,7 +157,7 @@
 
             - Enable upload related interrupts and configure the spi_device in passthrough mode.
             - Configure a command slot to enable upload for a flash program/erase command.
-            - Excecute two parallel threads:
+            - Execute two parallel threads:
               1. SPI host agent.
                 - Send this command via an upstream SPI host agent, then the agent keeps sending
                 read_status to poll the busy bit.
@@ -348,7 +348,7 @@
       desc: '''Verify the retention logic in pinmux that is activated during deep sleep.
 
             - Pick a pin (such as GPIO0) and enable it in output mode. Set a known value to it (0 or
-              1) and verify the correctless of the value on the chip IO.
+              1) and verify the correctness of the value on the chip IO.
             - Program the pin's retention value during deep sleep to be opposite of the active power
               value programmed in the previous step.
             - Reuse an existing deep sleep / low power wake up test, such as
@@ -366,7 +366,7 @@
       name: chip_sw_mbx_smoke
       desc: '''Verify the mailbox integrations with a basic smoketest
 
-            - Random-cyclicly select a mailbox to perform a req-rsp transaction with
+            - Random-cyclically select a mailbox to perform a req-rsp transaction with
             - TB(requester) writes a message into the imbx, then awaits a response
             - sw(responder) awaits the request, reads it then replies with a modified version
               of the original message
@@ -472,7 +472,7 @@
       desc: '''Verify non-debug reset request initiated from RV_DM when the chip is awake.
 
             - Program some CSRs / mem that are under life cycle reset tree and system reset tree.
-            - Configure RV_DM to send NDM reset request to reset sytem reset tree.
+            - Configure RV_DM to send NDM reset request to reset system reset tree.
             - While NDM reset is ongoing, ensure the RV_DM debug module registers can still be
               accessed.
             - Read the programmed CSRs / mem to ensure that everything under system reset tree is
@@ -611,7 +611,7 @@
               bark interrupt is fired.
             - Program the alert handler to escalate on alerts upto phase 2 (i.e. reset) but the
               phase 1 (i.e. wipe secrets) should occur and last during the time the wdog is
-              programed to bark and bite.
+              programmed to bark and bite.
             - Trigger an alert to cause an escalation condition before the bark signal asserts.
             - After the reset ensure that the reset cause was due to the escalation to prove that
               the wdog was disabled.
@@ -967,7 +967,7 @@
             This verifies ALL reset sources.
             - 7 resets are generated randomly with deep sleeps
             - POR (HW PAD) reset, SW POR, wdog timer reset, esc rst, SW req
-            - esc reset is followd by normal mode because it does not work with sleep mode
+            - esc reset is followed by normal mode because it does not work with sleep mode
             '''
       stage: V2
       tests: ["chip_sw_pwrmgr_deep_sleep_all_reset_reqs"]
@@ -1331,7 +1331,7 @@
       desc: '''Verify alert_handler's ping mechanism works correctly during sleep and wake up.
 
             There are two scenarios to check:
-            - Configure alert_handler's ping timeout register to a reasonble value that won't cause
+            - Configure alert_handler's ping timeout register to a reasonable value that won't cause
               ping timeout in normal cases.
               Then randomly enter and exit normal or deep sleep modes.
               Check that no local alerts triggered in alert_handler.
@@ -1895,7 +1895,7 @@
             TODO: system level scenario: have all entropy sources request entropy in the same test
             one after to show boot to post boot load, cycling all entropy blocks off and on again.
             Ensure there are no deadlocks and everything works as expected.
-            X'ref'ed with each IP test that requsts entropy from EDN.
+            X'ref'ed with each IP test that requests entropy from EDN.
             '''
       stage: V2
       tests: ["chip_sw_edn_entropy_reqs",
@@ -1908,7 +1908,7 @@
       desc: '''Verify the keymgr advances to all states and generate identity / SW output.
 
             - In the SW test, write fixed value to OTP for root_key and write creator and owner
-              seeds in flash. And then roboot the chip.
+              seeds in flash. And then reboot the chip.
             - In the SV sequence, backdoor read Device ID and ROM digest through CSRs.
             - For HardwareRevisionSecret, use the constant values in design.
             - Configure the keymgr and advance to `CreatorRootKey` and `OwnerIntermediateKey`.
@@ -1928,9 +1928,9 @@
             - Note: there are 3 ways of calculating the expected digest for comparison. Any of them
               is acceptable.
               - Use SW to calculate that, and it will also exercise the Ibex.
-              - SW sends all the keys through CSRs to KMAC to generate the degist data.
+              - SW sends all the keys through CSRs to KMAC to generate the digest data.
               - DV calls C functions to generate and backdoor load to a specific memory location
-                for SW. (Adpot this approach.)
+                for SW. (Adopt this approach.)
 
             X-ref'ed with kmac test.
             '''
@@ -2016,9 +2016,9 @@
 
             - SW initializes the entropy subsystem to generate randomness.
             - SW loads an OTBN app that executes instructions to read the RND bits.
-            - The OTBN app ensures that the values when read consequtively do not match, and its not
-              all 0s or all 1s, as a basic measure to ensure that the entropy subsystem is returning
-              some data.
+            - The OTBN app ensures that the values when read consecutively do not match, and it's
+              not all 0s or all 1s, as a basic measure to ensure that the entropy subsystem is
+              returning some data.
             '''
       stage: V2
       tests: ["chip_sw_otbn_randomness"]
@@ -2654,7 +2654,7 @@
       name: chip_sw_rv_core_ibex_fault_dump
       desc: '''Verify the functionality of the ibex fault dump.
 
-               - Purposely create an ibex exception during execution through reads to an ummapped
+               - Purposely create an ibex exception during execution through reads to an unmapped
                  address.
                - Ensure the rstmgr fault dump correctly captures the related addresses to the
                  exception.
@@ -2714,7 +2714,7 @@
             1. Implement test in the ROM stage itself
             2. Implement test in the flash stage, using test ROM
             3. Implement test in the flash stage, using production ROM
-            4. Enable external maufacturer hooks in existing tests developed in the open source
+            4. Enable external manufacturer hooks in existing tests developed in the open source
             5. Enable concurrent threads in tests
             '''
       stage: V1
@@ -2852,7 +2852,7 @@
     }
     {
       name: chip_sw_lc_walkthrough
-      desc: '''Walk through the life cycle stages from RAW state and reseting the chip each time.
+      desc: '''Walk through the life cycle stages from RAW state and resetting the chip each time.
 
              - Pre-load OTP image with RAW lc_state.
              - Initiate the LC transition to one of the test unlock state.
@@ -2893,7 +2893,7 @@
 
             This test characterizes the DUT initial state before RAW unlock operation, and
             switches to external clock via clkmgr to ensure the system clock can be switched
-            right after transition into test_unlcoked0 state. The test also verifies that
+            right after transition into test_unlocked0 state. The test also verifies that
             AST is not initialized while CPU is executing code.
 
              - Pre-load OTP image with RAW lc_state.
@@ -2928,7 +2928,7 @@
       The test should set a GPIO (mapped to the IOA2 pin) to high while the power
       state of interest is active.
 
-      Blocks / functionality to run simulatenously in this test:
+      Blocks / functionality to run simultaneously in this test:
 
         - The ADC is continuously sampling new data
         - Staggered activation of OTBN, aes, KMAC/HMAC.
@@ -2992,7 +2992,7 @@
       - Upon successful boot strap, ROM jumps to the newly programmed image and de-scrambles the
         instructions.
       - In the test program, check whether the OTP partition containing the scramble seeds is
-        locked. Also check that the unscrambled value progarmmed into flash isolated partition
+        locked. Also check that the unscrambled value programmed into flash isolated partition
         can be correctly read back when the region is set to scramble disable.
       - If either of the above checks is incorrect, return error.
 

hw/top_darjeeling/data/otp/otp_ctrl_img_owner_sw_cfg.hjson
@@ -37,14 +37,14 @@
                 },
                 {
                     name: "OWNER_SW_CFG_ROM_ALERT_ESCALATION"
-                    // Set the esclation policies to kAlertEscalateNone.
+                    // Set the escalation policies to kAlertEscalateNone.
                     // See `alert_escalate_t`
                     // in sw/device/silicon_creator/lib/drivers/alert.h
                     value: "0xd1d1d1d1",
                 },
                 {
                     name: "OWNER_SW_CFG_ROM_ALERT_CLASSIFICATION"
-                    // Set the classifiactions to kAlertClassX.
+                    // Set the classifications to kAlertClassX.
                     // See `alert_class_t`
                     // in sw/device/silicon_creator/lib/drivers/alert.h
                     value: [
@@ -152,7 +152,7 @@
                 },
                 {
                     name: "OWNER_SW_CFG_ROM_LOCAL_ALERT_CLASSIFICATION"
-                    // Set the classifiactions to kAlertClassX.
+                    // Set the classifications to kAlertClassX.
                     // See `alert_class_t`
                     // in sw/device/silicon_creator/lib/drivers/alert.h
                     value: [

hw/top_darjeeling/data/otp/otp_ctrl_img_test_locked1.hjson
@@ -19,7 +19,7 @@
                 {
                     name: "CREATOR_SW_CFG_ROM_EXEC_EN",
                     // ROM execution is enabled if this item is set to a
-                    // mnon-zero value.
+                    // non-zero value.
                     value: "0xffffffff",
                 },
             ],

hw/top_darjeeling/data/otp/otp_ctrl_mmap.hjson
@@ -823,7 +823,7 @@
                 {
                     name: "ROM_PATCH_DATA",
                     size: "9192"
-                    desc: ''' Fully SW managed patition
+                    desc: ''' Fully SW managed partition
                     ROM patch section
                     signed patch(es)
                     Each patch Contains header, signature, patch match/redirect configuration and patch code
@@ -1063,7 +1063,7 @@
                 // life cycle controller to ensure that the counter is always written
                 // before any state update. I.e., the life cycle controller
                 // already splits the counter and state updates into two
-                // supsequent requests through the LCI, where the first request
+                // subsequent requests through the LCI, where the first request
                 // only contains the updated transition counter, and the second
                 // request the updated transition counter and state.
                 {

hw/top_darjeeling/data/racl/racl.hjson
@@ -5,10 +5,10 @@
 {
   // error_response controls whether to return TLUL error on RACL errors
   error_response: true
-  // The CTN UID is transfered via the TLUL reserved user bits: rsvd[ctn_uid_bit_msb:ctn_uid_bit_lsb]
+  // The CTN UID is transferred via the TLUL reserved user bits: rsvd[ctn_uid_bit_msb:ctn_uid_bit_lsb]
   ctn_uid_bit_lsb: 0
   ctn_uid_bit_msb: 4
-  // The RACL role is transfered via the TLUL reserved user bits: rsvd[role_bit_msb:role_bit_lsb]
+  // The RACL role is transferred via the TLUL reserved user bits: rsvd[role_bit_msb:role_bit_lsb]
   role_bit_lsb: 5
   role_bit_msb: 8
   roles: {

hw/top_darjeeling/data/top_darjeeling.hjson
@@ -42,7 +42,7 @@
 
   // This is the clock data structure of the design.
   // The hier path refers to the clock reference path (struct / port)
-  //   - The top/ext desgination follows the same scheme as inter-module
+  //   - The top/ext designation follows the same scheme as inter-module
   // The src key indicates the raw clock sources in the design
   // The groups key indicates the various clock groupings in the design
   clocks: {
@@ -56,7 +56,7 @@
     // Clock Source attributes
     // name: Name of group.
     // aon:  Whether the clock is free running all the time.
-    //       If it is, the clock is not hanlded by clkmgr.
+    //       If it is, the clock is not handled by clkmgr.
     // freq: Absolute frequency of clk in Hz
     // ref: indicates the clock is used as a reference for measurement.
     srcs: [
@@ -68,7 +68,7 @@
     // Derived clock source attributes
     // name: Name of group.
     // aon:  Whether the clock is free running all the time.
-    //       If it is, the clock is not hanlded by clkmgr.
+    //       If it is, the clock is not handled by clkmgr.
     // freq: Absolute frequency of clk in Hz
     // src:  From which clock source is the clock derived
     // div:  Ratio between derived clock and source clock
@@ -113,7 +113,7 @@
 
   // This is the reset data structure of the design.
   // The hier path refers to the reset reference path (struct / port)
-  //   - The top/ext desgination follows the same scheme as inter-module
+  //   - The top/ext designation follows the same scheme as inter-module
   // The node key represents all the known resets in the design
   resets: {
     // Discrete OpenTitan does not need to wait for external resets
@@ -283,7 +283,7 @@
      //   reggen_only: Similar to reggen_top, but are not instantiated in the top level.
   module: [
     { name: "uart0",    // instance name
-      type: "uart",     // Must be matched to the ip name in `ip.hson` (_reg, _cfg permitted)
+      type: "uart",     // Must be matched to the ip name in `ip.hjson` (_reg, _cfg permitted)
                         // and `hw/ip/{type}`
 
       // clock connections defines the port to top level clock connection
@@ -1850,7 +1850,7 @@
 
       pinmux: {
         // Special signal positions. Each entry in the list below creates a
-        // target-specific pad position parameter in the  chiplevel hierarchy
+        // target-specific pad position parameter in the chiplevel hierarchy
         // that can be used to parameterize certain IPs like the pinmux or
         // padring. This is mainly used to define the pad positions of special
         // test and DFT signals such as the JTAG signals and the TAP and DFT

hw/top_darjeeling/dv/chip_sim_cfg.hjson
@@ -102,7 +102,7 @@
       name: vcs_xprop_cfg_file
       value: "{top_dv_path}/vcs_xprop.cfg"
     }
-    // The jtag agent requires the data and bytenable widths to be increased.
+    // The jtag agent requires the data and byte enable widths to be increased.
     {
       name: tl_dw
       value: 64
@@ -635,7 +635,7 @@
       uvm_test_seq: chip_sw_base_vseq
       sw_images: ["//sw/device/tests/sim_dv:otp_ctrl_lc_signals_test:6:new_rules"]
       en_run_modes: ["sw_test_mode_test_rom"],
-      // Use the image as basis, but clear provitioning state of the SECRET2 and SECRET3
+      // Use the image as basis, but clear provisioning state of the SECRET2 and SECRET3
       // partitions so that the test can make front-door accesses to those partitions.
       run_opts: ["+use_otp_image=OtpTypeLcStTestUnlocked0", "+otp_clear_secret2=1",
                  "+otp_clear_secret3=1"]
@@ -645,7 +645,7 @@
       uvm_test_seq: chip_sw_base_vseq
       sw_images: ["//sw/device/tests/sim_dv:otp_ctrl_lc_signals_test:6:new_rules"]
       en_run_modes: ["sw_test_mode_test_rom"],
-      // Use the image as basis, but clear provitioning state of the SECRET2 and SECRET3
+      // Use the image as basis, but clear provisioning state of the SECRET2 and SECRET3
       // partitions so that the test can make front-door accesses to those partitions.
       run_opts: ["+use_otp_image=OtpTypeLcStDev", "+otp_clear_secret2=1", "+otp_clear_secret3=1",
                  "+sw_test_timeout_ns=100_000_000"]
@@ -655,7 +655,7 @@
       uvm_test_seq: chip_sw_base_vseq
       sw_images: ["//sw/device/tests/sim_dv:otp_ctrl_lc_signals_test:6:new_rules"]
       en_run_modes: ["sw_test_mode_test_rom"],
-      // Use the image as basis, but clear provitioning state of the SECRET2 and SECRET3
+      // Use the image as basis, but clear provisioning state of the SECRET2 and SECRET3
       // partitions so that the test can make front-door accesses to those partitions.
       run_opts: ["+use_otp_image=OtpTypeLcStProd", "+otp_clear_secret2=1", "+otp_clear_secret3=1",
                  "+sw_test_timeout_ns=100_000_000"]
@@ -665,7 +665,7 @@
       uvm_test_seq: chip_sw_base_vseq
       sw_images: ["//sw/device/tests/sim_dv:otp_ctrl_lc_signals_test:6:new_rules"]
       en_run_modes: ["sw_test_mode_test_rom"],
-      // Use the image as basis, but clear provitioning state of the SECRET2 and SECRET3
+      // Use the image as basis, but clear provisioning state of the SECRET2 and SECRET3
       // partitions so that the test can make front-door accesses to those partitions.
       run_opts: ["+use_otp_image=OtpTypeLcStRma", "+otp_clear_secret2=1", "+otp_clear_secret3=1",
                  "+sw_test_timeout_ns=100_000_000"]

commit 5f2b2ba8fdafae1fbbc4905e5f56a48c767a7fbe

[darjeeling] Revert typo fix causing lint failure
Looks like on of the typo fixes in commit
3a7a4aa782243b319f02c6feb55588b521cffb6c
caused a lint failure by pushing the line length over the limit.

Quick fix of reverting the word back to its previous spelling.

Signed-off-by: Elliot Baptist <elliot.baptist@lowrisc.org>

hw/top_darjeeling/ip/ast/rtl/ast_clks_byp.sv
@@ -32,7 +32,7 @@ module ast_clks_byp (
 `endif // of AST_BYPASS_CLK
   input prim_mubi_pkg::mubi4_t io_clk_byp_req_i,    // External IO clock mux for OTP bootstrap
   input prim_mubi_pkg::mubi4_t all_clk_byp_req_i,   // External all clock mux override
-  input prim_mubi_pkg::mubi4_t ext_freq_is_96m_i,   // External Clock Frequency is 96MHz (else 48MHz)
+  input prim_mubi_pkg::mubi4_t ext_freq_is_96m_i,   // External Clock Frequecy is 96MHz (else 48MHz)
   output prim_mubi_pkg::mubi4_t io_clk_byp_ack_o,   // Switch IO clock to External clock
   output prim_mubi_pkg::mubi4_t all_clk_byp_ack_o,  // Switch all clocks to External clock
   output logic clk_src_sys_o,               // SYS Source Clock