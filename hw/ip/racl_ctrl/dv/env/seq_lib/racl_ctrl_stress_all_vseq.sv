5f985f93a14e62ce41d2a3d1b915998d5410c7d8
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-08-25 15:57:13 +0100

    [racl_ctrl,dv] Avoid resets in parts of stress_all_with_rand_reset
    The surrounding code can get a little confused if it applies a reset
    at the same time as a subsequence does. Don't do that.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/racl_ctrl/dv/env/seq_lib/racl_ctrl_stress_all_vseq.sv
+++ hw/ip/racl_ctrl/dv/env/seq_lib/racl_ctrl_stress_all_vseq.sv
@@ -44,6 +44,11 @@ task racl_ctrl_stress_all_vseq::run_child(string child_vseq_name);
   racl_ctrl_base_vseq child;
   `downcast(child, create_seq_by_name(child_vseq_name))
   child.set_sequencer(p_sequencer);
+
+  // Decide whether the sub-sequence will apply a reset. If the current sequence is configured not
+  // to apply a reset, don't do so in the child.
+  child.do_apply_reset = do_apply_reset ? $urandom_range(0, 1) : 0;
+
   `DV_CHECK_RANDOMIZE_FATAL(child)
   child.start(p_sequencer);
 endtask
