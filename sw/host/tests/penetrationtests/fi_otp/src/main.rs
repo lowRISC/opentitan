86cea5ff500ca4f1d3c43e5271437d376f14bfc7
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-08-25 10:47:34 +0200

    [pentest] earlgrey_1.0.0 specific changes
    As the master and earlgrey_1.0.0 branch diverged over time, this
    commit applies a couple of fixes that are required such that the
    pentest framework again works on this branch.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (commit is original to earlgrey_1.0.0)

--- sw/host/tests/penetrationtests/fi_otp/src/main.rs
+++ sw/host/tests/penetrationtests/fi_otp/src/main.rs
@@ -141,7 +141,12 @@ fn run_fi_otp_testcase(
     if !test_case.flaky_expected_output.is_empty() {
         for exp_output in test_case.flaky_expected_output.iter() {
             // Get test output & filter.
-            let output = serde_json::Value::recv(uart, opts.timeout, false)?;
+            let output = serde_json::Value::recv(
+                uart,
+                opts.timeout,
+                /*quiet=*/ false,
+                /*skip_crc=*/ false,
+            )?;
             // Only check non empty JSON responses.
             if output.as_object().is_some() {
                 let output_received = filter_response(output.clone());
