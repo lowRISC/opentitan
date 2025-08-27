05f3d396120ef694b0839ed03d04978e47cecd9c
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-08-27 12:28:23 +0200

    [crypto] Check aes_key_t checksum for AES-GCM
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit 6e77c087cc69bbfbb35ab97999ff7991c10da9ac)

--- sw/device/lib/crypto/include/aes_gcm.h
+++ sw/device/lib/crypto/include/aes_gcm.h
@@ -39,7 +39,7 @@ typedef enum otcrypto_aes_gcm_tag_len {
  * change.
  */
 typedef struct otcrypto_aes_gcm_context {
-  uint32_t data[98];
+  uint32_t data[100];
 } otcrypto_aes_gcm_context_t;
 
 /**
