ec8d23471bf8cd428d1a75f690f3c227528faff3
Samuel Ortiz <sameo@rivosinc.com>
2025-08-28 15:58:43 +0200

    [sw,silicon_creator] Multi-top OTP driver
    * Convert EG base addresses to DT generated ones
    * Use the OTP ctrl DT defined OTP partition access API
    
    Fixes #28071
    
    Signed-off-by: Samuel Ortiz <sameo@rivosinc.com>

--- sw/device/silicon_creator/lib/drivers/mock_otp.h
+++ sw/device/silicon_creator/lib/drivers/mock_otp.h
@@ -6,7 +6,6 @@
 #define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_DRIVERS_MOCK_OTP_H_
 
 #include "sw/device/lib/base/global_mock.h"
-#include "sw/device/silicon_creator/lib/drivers/otp.h"
 #include "sw/device/silicon_creator/testing/rom_test.h"
 
 namespace rom_test {
