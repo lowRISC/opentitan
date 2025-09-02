6f4b5d1d3783cd5affb859fefb7973126afd4400
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-09-02 12:59:32 +0100

    [racl_ctrl,dv] Improve print methods for racl_error_log_item
    This was just to make debugging easier for me when tracking something
    down, but the clearer prints are probably worth having.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/racl_ctrl/dv/env/racl_error_log_agent/racl_error_log_vec_item.sv
+++ hw/ip/racl_ctrl/dv/env/racl_error_log_agent/racl_error_log_vec_item.sv
@@ -24,6 +24,7 @@ class racl_error_log_vec_item extends uvm_sequence_item;
 
   extern function new (string name="");
   extern function void post_randomize();
+  extern function void do_print(uvm_printer printer);
 endclass
 
 function racl_error_log_vec_item::new(string name="");
@@ -37,5 +38,11 @@ function void racl_error_log_vec_item::post_randomize();
     if (!errors[sub_idx].randomize()) begin
       `uvm_error(`gfn, $sformatf("Failed to randomize errors[%d]", sub_idx));
     end
+  end
+endfunction
+
+function void racl_error_log_vec_item::do_print(uvm_printer printer);
+  foreach (errors[i]) begin
+    printer.print_object($sformatf("errors[%0d]", i), errors[i]);
   end
 endfunction
