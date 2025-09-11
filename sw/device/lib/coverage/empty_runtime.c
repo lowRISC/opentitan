f9c46c12593c0bb1b1c9890ae9d0f924569d970d
Yi-Hsuan Deng <yhdeng@google.com>
2025-09-12 02:53:10 +0800

    [sw,cov] Add default weak coverage runtime
    This change introduces a default, "no-op" coverage runtime.
    By implementing the coverage API as weak symbols, other libraries can
    invoke these OpenTitan coverage functions without causing failures in
    unit tests that do not have a OpenTitan coverage runtime.
    
    Change-Id: If9f66e1eb837ca34733e8aedaddbfca17818a99c
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>
    (cherry picked from commit 38111b8dff76bf303f76b80f39f1845501db12f8)

---
+++ sw/device/lib/coverage/empty_runtime.c
@@ -0,0 +1,10 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/base/macros.h"
+
+OT_WEAK void coverage_transport_init(void) {}
+OT_WEAK void coverage_init(void) {}
+OT_WEAK void coverage_report(void) {}
+OT_WEAK void coverage_invalidate(void) {}
