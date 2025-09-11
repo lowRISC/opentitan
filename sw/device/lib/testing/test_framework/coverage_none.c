b1db997e95d92978c25b164af4002bb904491128
Yi-Hsuan Deng <yhdeng@google.com>
2025-09-11 19:00:18 +0800

    [sw,cov] Cleanup old coverage runtime
    This change removes code for the old LLVM compiler_rt based coverage runtime on
    the device, which will be replaced by a in-house custom runtime developed for
    OpenTitan.
    
    Change-Id: Ic35f61ad941b64f54602a78f1aaa90a61c0aa98a
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

--- sw/device/lib/testing/test_framework/coverage_none.c
+++
@@ -1,9 +0,0 @@
-// Copyright lowRISC contributors (OpenTitan project).
-// Licensed under the Apache License, Version 2.0, see LICENSE for details.
-// SPDX-License-Identifier: Apache-2.0
-
-#include "sw/device/lib/testing/test_framework/coverage.h"
-
-// This NOP function gets linked in when coverage is disabled. See
-// `test_coverage_llvm.c` for its actual definition when coverage is enabled.
-void coverage_send_buffer(void) {}
