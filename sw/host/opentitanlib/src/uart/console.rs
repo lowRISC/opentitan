ce1358decbaea2211a85db5d3f8c610057d3e5f3
Yi-Hsuan Deng <yhdeng@google.com>
2025-09-11 19:54:04 +0800

    [sw,cov] Save coverage report in opentitantool UartConsole
    This change adds a feature to opentitantool's `UartConsole` that allows saving
    device-side coverage reports.
    
    The new implementation parses coverage data embedded in the console output,
    validates it using CRC, and then saves it to a `.xprofraw` file.
    
    This is part of Bazel coverage integration to collect reports for tests running
    directly on the OpenTitan device. It follows LLVM's convention for saving to the
    path specified by the `LLVM_PROFILE_FILE` environment variable, which Bazel will
    set.
    
    Additionally, a new `wait_for_coverage` function is introduced, allowing
    `opentitantool` to block until coverage data is received or skipped.
    
    Change-Id: I682655aeeb3c16228036ad58fb41187f7eb932cb
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

--- sw/host/opentitanlib/src/uart/console.rs
+++ sw/host/opentitanlib/src/uart/console.rs
@@ -3,7 +3,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::{anyhow, bail, Result};
+use crc::{Crc, CRC_32_ISO_HDLC};
 use mio::{Events, Interest, Poll, Token};
+use rand::Rng;
 use regex::{Captures, Regex};
 use std::fs::File;
 use std::io::{ErrorKind, Read, Write};
@@ -13,6 +15,10 @@ use std::time::{Duration, Instant, SystemTime};
 
 use crate::io::console::{ConsoleDevice, ConsoleError};
 use crate::util::file;
+
+const COVERAGE_START_ANCHOR: &str = "== COVERAGE PROFILE START ==\r\n";
+const COVERAGE_END_ANCHOR: &str = "== COVERAGE PROFILE END ==\r\n";
+const COVERAGE_SKIP_ANCHOR: &str = "== COVERAGE PROFILE SKIP ==\r\n";
 
 #[derive(Default)]
 pub struct UartConsole {
@@ -26,6 +32,8 @@ pub struct UartConsole {
     pub newline: bool,
     pub carriage_return: bool,
     pub break_en: bool,
+    pub alt_buffer_enabled: bool,
+    pub alt_buffer: String,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -92,21 +100,8 @@ impl UartConsole {
         // HACK(nbdd0121): do a nonblocking read because the UART buffer may still have data in it.
         // If we wait for mio event now, we might be blocking forever.
         while self.uart_read(device, Duration::from_millis(0), &mut stdout)? {
-            if self
-                .exit_success
-                .as_ref()
-                .map(|rx| rx.is_match(&self.buffer))
-                == Some(true)
-            {
-                return Ok(ExitStatus::ExitSuccess);
-            }
-            if self
-                .exit_failure
-                .as_ref()
-                .map(|rx| rx.is_match(&self.buffer))
-                == Some(true)
-            {
-                return Ok(ExitStatus::ExitFailure);
+            if let Some(exit_status) = self.process_buffer()? {
+                return Ok(exit_status);
             }
         }
 
@@ -155,21 +150,8 @@ impl UartConsole {
                     // `mio` convention demands that we keep reading until a read returns zero
                     // bytes, otherwise next `poll()` is not guaranteed to notice more data.
                     while self.uart_read(device, Duration::from_millis(1), &mut stdout)? {
-                        if self
-                            .exit_success
-                            .as_ref()
-                            .map(|rx| rx.is_match(&self.buffer))
-                            == Some(true)
-                        {
-                            return Ok(ExitStatus::ExitSuccess);
-                        }
-                        if self
-                            .exit_failure
-                            .as_ref()
-                            .map(|rx| rx.is_match(&self.buffer))
-                            == Some(true)
-                        {
-                            return Ok(ExitStatus::ExitFailure);
+                        if let Some(exit_status) = self.process_buffer()? {
+                            return Ok(exit_status);
                         }
                     }
                 }
@@ -182,12 +164,100 @@ impl UartConsole {
         Token(TOKEN_COUNTER.fetch_add(1, Ordering::Relaxed))
     }
 
+    fn get_active_buffer(&self) -> &str {
+        if self.alt_buffer_enabled {
+            &self.alt_buffer
+        } else {
+            &self.buffer
+        }
+    }
+
     // Maintain a buffer for the exit regexes to match against.
     fn append_buffer(&mut self, data: &[u8]) {
-        self.buffer.push_str(&String::from_utf8_lossy(data));
-        while self.buffer.len() > UartConsole::BUFFER_LEN {
-            self.buffer.remove(0);
+        let data = &String::from_utf8_lossy(data);
+        if self.alt_buffer_enabled {
+            self.alt_buffer.push_str(data);
+        } else {
+            self.buffer.push_str(data);
+            while self.buffer.len() > UartConsole::BUFFER_LEN {
+                self.buffer.remove(0);
+            }
         }
+    }
+
+    fn process_coverage(&mut self) -> Result<()> {
+        let response = hex::decode(&self.alt_buffer)?;
+        if response.is_empty() {
+            bail!("Got empty coverage");
+        }
+        if response.len() < 4 {
+            bail!("Coverage from device is too short");
+        }
+
+        let (response, crc) = response.split_at(response.len() - 4);
+        let crc = u32::from_le_bytes(crc.try_into()?);
+        let actual = Crc::<u32>::new(&CRC_32_ISO_HDLC).checksum(response);
+        if crc != actual {
+            bail!("Coverage corrupted: crc = {crc:08x}, actual = {actual:08x}");
+        }
+
+        let path = std::env::var("LLVM_PROFILE_FILE").unwrap_or("./default.profraw".to_owned());
+        let path = path.replace("%h", "test.on.device");
+        let path = path.replace("%p", &rand::thread_rng().gen::<u32>().to_string());
+        let path = path.replace("%m", "0");
+        let path = path.replace(".profraw", ".xprofraw");
+
+        println!("Saving coverage profile to {path}");
+        std::fs::write(path, response)?;
+
+        Ok(())
+    }
+
+    fn process_exit_regex(&self) -> Result<Option<ExitStatus>> {
+        let active_buffer = self.get_active_buffer();
+
+        if self
+            .exit_success
+            .as_ref()
+            .map(|rx| rx.is_match(active_buffer))
+            == Some(true)
+        {
+            return Ok(Some(ExitStatus::ExitSuccess));
+        }
+        if self
+            .exit_failure
+            .as_ref()
+            .map(|rx| rx.is_match(active_buffer))
+            == Some(true)
+        {
+            return Ok(Some(ExitStatus::ExitFailure));
+        }
+        Ok(None)
+    }
+
+    fn process_buffer(&mut self) -> Result<Option<ExitStatus>> {
+        let result = self.process_exit_regex();
+
+        // Handle coverage transport
+        if self.buffer.ends_with(COVERAGE_START_ANCHOR) {
+            self.buffer
+                .truncate(self.buffer.len() - COVERAGE_START_ANCHOR.len());
+            self.alt_buffer_enabled = true;
+            self.alt_buffer.clear();
+        }
+        if self.alt_buffer.ends_with(COVERAGE_END_ANCHOR) {
+            self.alt_buffer
+                .truncate(self.alt_buffer.len() - COVERAGE_END_ANCHOR.len());
+            self.process_coverage()?;
+            self.alt_buffer_enabled = false;
+            self.alt_buffer.clear();
+        }
+        if self.buffer.ends_with(COVERAGE_SKIP_ANCHOR) {
+            self.buffer
+                .truncate(self.buffer.len() - COVERAGE_SKIP_ANCHOR.len());
+        }
+
+        result
     }
 
     // Read from the console device and process the data read.
@@ -205,30 +275,32 @@ impl UartConsole {
         if len == 0 {
             return Ok(false);
         }
-        for i in 0..len {
-            if self.timestamp && self.newline {
-                let t = humantime::format_rfc3339_millis(SystemTime::now());
+        if !self.alt_buffer_enabled {
+            for i in 0..len {
+                if self.timestamp && self.newline {
+                    let t = humantime::format_rfc3339_millis(SystemTime::now());
+                    stdout.as_mut().map_or(Ok(()), |out| {
+                        out.write_fmt(format_args!("[{}  console]", t))
+                    })?;
+                    self.newline = false;
+                }
+                self.newline = buf[i] == b'\n';
                 stdout.as_mut().map_or(Ok(()), |out| {
-                    out.write_fmt(format_args!("[{}  console]", t))
+                    out.write_all(if self.newline && !self.carriage_return {
+                        b"\r\n"
+                    } else {
+                        &buf[i..i + 1]
+                    })
                 })?;
-                self.newline = false;
+                self.carriage_return = buf[i] == b'\r';
             }
-            self.newline = buf[i] == b'\n';
-            stdout.as_mut().map_or(Ok(()), |out| {
-                out.write_all(if self.newline && !self.carriage_return {
-                    b"\r\n"
-                } else {
-                    &buf[i..i + 1]
-                })
-            })?;
-            self.carriage_return = buf[i] == b'\r';
-        }
-        stdout.as_mut().map_or(Ok(()), |out| out.flush())?;
+            stdout.as_mut().map_or(Ok(()), |out| out.flush())?;
 
-        // If we're logging, save it to the logfile.
-        self.logfile
-            .as_mut()
-            .map_or(Ok(()), |f| f.write_all(&buf[..len]))?;
+            // If we're logging, save it to the logfile.
+            self.logfile
+                .as_mut()
+                .map_or(Ok(()), |f| f.write_all(&buf[..len]))?;
+        }
         self.append_buffer(&buf[..len]);
         Ok(true)
     }
@@ -300,35 +372,23 @@ impl UartConsole {
 
         // Check for input on the uart.
         self.uart_read(device, Duration::from_millis(10), stdout)?;
-        if self
-            .exit_success
-            .as_ref()
-            .map(|rx| rx.is_match(&self.buffer))
-            == Some(true)
-        {
-            return Ok(ExitStatus::ExitSuccess);
-        }
-        if self
-            .exit_failure
-            .as_ref()
-            .map(|rx| rx.is_match(&self.buffer))
-            == Some(true)
-        {
-            return Ok(ExitStatus::ExitFailure);
+        if let Some(exit_status) = self.process_buffer()? {
+            return Ok(exit_status);
         }
         self.process_input(device, stdin)
     }
 
     pub fn captures(&self, status: ExitStatus) -> Option<Captures> {
+        let active_buffer = self.get_active_buffer();
         match status {
             ExitStatus::ExitSuccess => self
                 .exit_success
                 .as_ref()
-                .and_then(|rx| rx.captures(&self.buffer)),
+                .and_then(|rx| rx.captures(active_buffer)),
             ExitStatus::ExitFailure => self
                 .exit_failure
                 .as_ref()
-                .and_then(|rx| rx.captures(&self.buffer)),
+                .and_then(|rx| rx.captures(active_buffer)),
             _ => None,
         }
     }
@@ -349,12 +409,13 @@ impl UartConsole {
         println!();
         match result {
             ExitStatus::ExitSuccess => {
-                let caps = console.captures(ExitStatus::ExitSuccess).expect("capture");
                 let mut vec = Vec::new();
-                for c in caps.iter() {
-                    match c {
-                        None => vec.push(String::new()),
-                        _ => vec.push(c.unwrap().as_str().to_owned()),
+                if let Some(caps) = console.captures(ExitStatus::ExitSuccess) {
+                    for c in caps.iter() {
+                        match c {
+                            None => vec.push(String::new()),
+                            _ => vec.push(c.unwrap().as_str().to_owned()),
+                        }
                     }
                 }
                 Ok(vec)
@@ -362,5 +423,16 @@ impl UartConsole {
             ExitStatus::Timeout => Err(ConsoleError::GenericError("Timed Out".into()).into()),
             _ => Err(anyhow!("Impossible result: {:?}", result)),
         }
+    }
+
+    pub fn wait_for_coverage<T>(device: &T, timeout: Duration) -> Result<()>
+    where
+        T: ConsoleDevice + ?Sized,
+    {
+        let anchor =
+            regex::escape(COVERAGE_END_ANCHOR) + "|" + &regex::escape(COVERAGE_SKIP_ANCHOR);
+
+        Self::wait_for(device, &anchor, timeout)?;
+        Ok(())
     }
 }
