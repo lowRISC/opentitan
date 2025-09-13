bf1555d6b2398f39686f95bc3885c10ef94b0f2a
Harry Callahan <hcallahan@lowrisc.org>
2025-09-13 20:58:50 +0100

    [dv] Cleanup prints in sim.mk
    Signed-off-by: Harry Callahan <hcallahan@lowrisc.org>
    (cherry picked from commit 6437f96a8b658f6008ac2f2144584f38c30b6c05)

--- hw/dv/tools/dvsim/sim.mk
+++ hw/dv/tools/dvsim/sim.mk
@@ -15,7 +15,7 @@ all: build run
 build: build_result
 
 pre_build:
-	@echo "[make]: pre_build"
+	@echo -e "\n[make]: pre_build"
 	mkdir -p ${build_dir}
 ifneq (${pre_build_cmds},)
 	# pre_build_cmds are likely changing the in-tree sources. We hence use FLOCK
@@ -26,51 +26,51 @@ ifneq (${pre_build_cmds},)
 endif
 
 gen_sv_flist: pre_build
-	@echo "[make]: gen_sv_flist"
+	@echo -e "\n[make]: gen_sv_flist"
 ifneq (${sv_flist_gen_cmd},)
 	cd ${build_dir} && ${sv_flist_gen_cmd} ${sv_flist_gen_opts}
 endif
 
 do_build: gen_sv_flist
-	@echo "[make]: build"
+	@echo -e "\n[make]: build"
 	cd ${sv_flist_gen_dir} && ${build_cmd} ${build_opts}
 
 post_build: do_build
-	@echo "[make]: post_build"
+	@echo -e "\n[make]: post_build"
 ifneq (${post_build_cmds},)
 	cd ${build_dir} && ${post_build_cmds}
 endif
 
 build_result: post_build
-	@echo "[make]: build_result"
+	@echo -e "\n[make]: build_result"
 
 run: run_result
 
 pre_run:
-	@echo "[make]: pre_run"
+	@echo -e "\n[make]: pre_run"
 	mkdir -p ${run_dir}
 ifneq (${pre_run_cmds},)
 	cd ${run_dir} && ${pre_run_cmds}
 endif
 
 sw_build: pre_run
-	@echo "[make]: sw_build"
+	@echo -e "\n[make]: sw_build"
 ifneq (${sw_images},)
 	cd ${proj_root} && ${sw_build_cmd} --build-seed=${build_seed}
 endif
 
 simulate: sw_build
-	@echo "[make]: simulate"
+	@echo -e "\n[make]: simulate"
 	cd ${run_dir} && ${run_cmd} ${run_opts}
 
 post_run: simulate
-	@echo "[make]: post_run"
+	@echo -e "\n[make]: post_run"
 ifneq (${post_run_cmds},)
 	cd ${run_dir} && ${post_run_cmds}
 endif
 
 run_result: post_run
-	@echo "[make]: run_result"
+	@echo -e "\n[make]: run_result"
 
 #######################
 ## Load waves target ##
@@ -82,19 +82,20 @@ debug_waves:
 ## coverage rated targets ##
 ############################
 cov_unr_build: gen_sv_flist
-	@echo "[make]: cov_unr_build"
+	@echo -e "\n[make]: cov_unr_build"
 	cd ${sv_flist_gen_dir} && ${cov_unr_build_cmd} ${cov_unr_build_opts}
 
 cov_unr_vcs: cov_unr_build
-	@echo "[make]: cov_unr"
+	@echo -e "\n[make]: cov_unr"
 	cd ${sv_flist_gen_dir} && ${cov_unr_run_cmd} ${cov_unr_run_opts}
 
 cov_unr_xcelium:
-	@echo "[make]: cov_unr"
+	@echo -e "\n[make]: cov_unr"
 	mkdir -p ${cov_unr_dir}
 	cd ${cov_unr_dir} && ${cov_unr_run_cmd} ${cov_unr_run_opts}
 
 cov_unr_merge:
+	@echo -e "\n[make]: cov_unr_merge"
 	cd ${cov_unr_dir} && ${job_prefix} ${cov_merge_cmd} -init ${cov_unr_dir}/jgproject/sessionLogs/session_0/unr_imc_coverage_merge.cmd
 
 ifeq (${SIMULATOR}, xcelium)
@@ -105,17 +106,17 @@ endif
 
 # Merge coverage if there are multiple builds.
 cov_merge:
-	@echo "[make]: cov_merge"
+	@echo -e "\n[make]: cov_merge"
 	${job_prefix} ${cov_merge_cmd} ${cov_merge_opts}
 
 # Generate coverage reports.
 cov_report:
-	@echo "[make]: cov_report"
+	@echo -e "\n[make]: cov_report"
 	${cov_report_cmd} ${cov_report_opts}
 
 # Open coverage tool to review and create report or exclusion file.
 cov_analyze:
-	@echo "[make]: cov_analyze"
+	@echo -e "\n[make]: cov_analyze"
 	${cov_analyze_cmd} ${cov_analyze_opts}
 
 .PHONY: build \
