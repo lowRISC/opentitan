5afcc9f1181cd716c9695c26f405a1d6127496ea
Robert Schilling <rschilling@rivosinc.com>
2025-09-15 10:13:01 +0200

    [hw,otbn,rtl] Add missing includes of prim_assert.h
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/otbn/rtl/otbn_loop_controller.sv
+++ hw/ip/otbn/rtl/otbn_loop_controller.sv
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+`include "prim_assert.sv"
+
 module otbn_loop_controller
   import otbn_pkg::*;
 #(
