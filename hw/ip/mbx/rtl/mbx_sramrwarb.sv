4b39d701f37f984c74b41e63215d5844844123c5
Robert Schilling <rschilling@rivosinc.com>
2025-09-15 09:58:20 +0200

    [hw,mbx,rtl] Add missing includes of prim_assert.h
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/mbx/rtl/mbx_sramrwarb.sv
+++ hw/ip/mbx/rtl/mbx_sramrwarb.sv
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+`include "prim_assert.sv"
+
 module mbx_sramrwarb
   import  tlul_pkg::*;
 #(
