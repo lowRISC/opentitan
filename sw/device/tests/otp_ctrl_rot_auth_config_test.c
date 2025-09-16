716ef6097ecfd8c2f1fddebc66d251053aa09956
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-09-16 13:29:23 +0200

    [test] Remove unused code in otp_ctrl_rot_auth_config_test
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- sw/device/tests/otp_ctrl_rot_auth_config_test.c
+++ sw/device/tests/otp_ctrl_rot_auth_config_test.c
@@ -2,27 +2,20 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-#include <stdint.h>
-
 #include "sw/device/lib/arch/device.h"
-#include "sw/device/lib/base/abs_mmio.h"
 #include "sw/device/lib/dif/dif_otp_ctrl.h"
-#include "sw/device/lib/runtime/ibex.h"
-#include "sw/device/lib/runtime/log.h"
-#include "sw/device/lib/runtime/print.h"
 #include "sw/device/lib/testing/otp_ctrl_testutils.h"
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ottf_test_config.h"
 #include "sw/device/silicon_creator/manuf/lib/individualize.h"
 #include "sw/device/silicon_creator/manuf/lib/individualize_sw_cfg.h"
-#include "sw/device/silicon_creator/manuf/lib/otp_fields.h"
 
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 
 OTTF_DEFINE_TEST_CONFIG();
 
 bool test_main(void) {
-  static dif_otp_ctrl_t otp_ctrl;
+  dif_otp_ctrl_t otp_ctrl;
   CHECK_DIF_OK(dif_otp_ctrl_init(
       mmio_region_from_addr(TOP_EARLGREY_OTP_CTRL_CORE_BASE_ADDR), &otp_ctrl));
   CHECK_STATUS_OK(
