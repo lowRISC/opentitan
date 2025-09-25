afac4f4cfc3433f786b891fbc7998c1edb8793c6
Yi-Hsuan Deng <yhdeng@google.com>
2025-09-25 16:47:48 +0800

    [e2e,cov] Add on-device UART coverage runtime
    This change adds the on-device coverage runtime for the device to report the
    coverage information via UART.
    
    The runtime has two versions:
    - `uart_runtime_real`: the real implementation that collects and sends
      coverage information.
    - `uart_runtime_skip`: a dummy implementation that just prints a message
      to indicate the coverage is skipped.
    
    The `uart_runtime` alias switches between these two implementations based on
    the build configuration.
    
    The new `util/uart_hex.py` utility converts hex strings to `uart_write_imm`
    calls.
    
    Change-Id: I081d634b4b347c66189ba5fe00fa4fd211737f2e
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>
    (cherry picked from commit 317d02e18804e51bd1af9c0aad89685f04708291)

---
+++ sw/device/lib/coverage/uart_runtime.c
@@ -0,0 +1,38 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/arch/device.h"
+#include "sw/device/lib/coverage/printer.h"
+#include "sw/device/silicon_creator/lib/dbg_print.h"
+#include "sw/device/silicon_creator/lib/drivers/pinmux.h"
+#include "sw/device/silicon_creator/lib/drivers/uart.h"
+
+void coverage_transport_init(void) {
+  pinmux_init_uart0_tx();
+  uart_init(kUartNCOValue);
+
+  dbg_puts("COVERAGE:UART\r\n");
+  while (!uart_tx_idle()) {
+  }
+}
+
+void coverage_report(void) {
+  if (coverage_is_valid()) {
+    dbg_puts("== COVERAGE PROFILE START ==\r\n");
+    coverage_printer_run();
+    dbg_puts("== COVERAGE PROFILE END ==\r\n");
+  } else {
+    dbg_puts("== COVERAGE PROFILE INVALID ==\r\n");
+  }
+
+  // Wait until the report is sent.
+  while (!uart_tx_idle()) {
+  }
+}
+
+void coverage_printer_sink(const void *data, size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    uart_write_hex(((uint8_t *)data)[i], 1, 0);
+  }
+}
