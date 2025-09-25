317d02e18804e51bd1af9c0aad89685f04708291
Yi-Hsuan Deng <yhdeng@google.com>
2025-09-25 16:47:48 +0800

    [e2e,cov] Add on-device UART coverage runtime
    This change adds the on-device coverage runtime for the device to report the
    coverage information via UART.
    
    The runtime has two versions:
    - `uart_runtime_real`: the real implementation that collects and sends
      coverage information.
    - `uart_runtime_skip`: a dummy implementation that just prints a message
      to indicate the coverage is skipped.
    
    The `uart_runtime` alias switches between these two implementations based on
    the build configuration.
    
    The new `util/uart_hex.py` utility converts hex strings to `uart_write_imm`
    calls.
    
    Change-Id: I081d634b4b347c66189ba5fe00fa4fd211737f2e
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

---
+++ sw/device/lib/coverage/uart_runtime_skip.c
@@ -0,0 +1,29 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/arch/device.h"
+#include "sw/device/silicon_creator/lib/dbg_print.h"
+#include "sw/device/silicon_creator/lib/drivers/pinmux.h"
+#include "sw/device/silicon_creator/lib/drivers/uart.h"
+
+void coverage_transport_init(void) {
+  pinmux_init_uart0_tx();
+  uart_init(kUartNCOValue);
+
+  dbg_puts("COV_SKIP:UART\r\n");
+  while (!uart_tx_idle()) {
+  }
+}
+
+void coverage_init(void) {}
+
+void coverage_report(void) {
+  dbg_puts("== COVERAGE PROFILE SKIP ==\r\n");
+
+  // Wait until the report is sent.
+  while (!uart_tx_idle()) {
+  }
+}
+
+void coverage_invalidate(void) {}
