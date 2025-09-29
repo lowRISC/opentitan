cd9ab96c0a3c08acf569a07feb67f7ad677636e0
Siemen Dhooghe <sdhooghe@google.com>
2025-09-29 05:59:14 -0700

    [crypto] Add checksum to ECC masked scalar and shared key
    Add checksums to the internal structures for p256 and p384 shared key and masked scalar. Populate those checksums and verify them for fault injection resistance.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>

--- sw/device/tests/penetrationtests/firmware/fi/cryptolib_fi_asym_impl.c
+++ sw/device/tests/penetrationtests/firmware/fi/cryptolib_fi_asym_impl.c
@@ -671,7 +671,7 @@ status_t cryptolib_fi_p256_sign_impl(
   p256_masked_scalar_t private_key_masked;
   otcrypto_blinded_key_t private_key = {
       .config = kP256PrivateKeyConfig,
-      .keyblob_length = sizeof(private_key_masked),
+      .keyblob_length = kP256MaskedScalarTotalShareBytes,
       .keyblob = (uint32_t *)&private_key_masked,
   };
   memset(private_key_masked.share0, 0, kP256MaskedScalarShareBytes);
@@ -897,7 +897,7 @@ status_t cryptolib_fi_p384_sign_impl(
   p384_masked_scalar_t private_key_masked;
   otcrypto_blinded_key_t private_key = {
       .config = kP384PrivateKeyConfig,
-      .keyblob_length = sizeof(private_key_masked),
+      .keyblob_length = kP384MaskedScalarTotalShareBytes,
       .keyblob = (uint32_t *)&private_key_masked,
   };
   memset(private_key_masked.share0, 0, kP384MaskedScalarShareBytes);
