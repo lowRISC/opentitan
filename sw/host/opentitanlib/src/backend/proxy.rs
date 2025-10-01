ec94e22cf3b1def766acdc08ab3d9b380ac224f7
Gary Guo <gary.guo@lowrisc.org>
2025-10-01 12:26:53 +0100

    [opentitanlib] use `inventory` for backend registration
    This allows distributed registration of backends. This change would allow
    the backend to be spread into individual crates without having to be tightly
    coupled with the rest.
    
    clap is updated to 4.5.47+ which implements `Args` for `()`.
    
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

--- sw/host/opentitanlib/src/backend/proxy.rs
+++ sw/host/opentitanlib/src/backend/proxy.rs
@@ -5,6 +5,7 @@
 use anyhow::Result;
 use clap::Args;
 
+use super::{Backend, BackendOpts, define_interface};
 use crate::transport::Transport;
 use crate::transport::proxy::Proxy;
 
@@ -16,6 +17,14 @@ pub struct ProxyOpts {
     port: u16,
 }
 
-pub fn create(args: &ProxyOpts) -> Result<Box<dyn Transport>> {
-    Ok(Box::new(Proxy::open(args.proxy.as_deref(), args.port)?))
+struct ProxyBackend;
+
+impl Backend for ProxyBackend {
+    type Opts = ProxyOpts;
+
+    fn create_transport(_: &BackendOpts, args: &ProxyOpts) -> Result<Box<dyn Transport>> {
+        Ok(Box::new(Proxy::open(args.proxy.as_deref(), args.port)?))
+    }
 }
+
+define_interface!("proxy", ProxyBackend);
