a67cf6a1c9c5de1c21dde673cd472aceb7e6d8aa
Yi-Hsuan Deng <yhdeng@google.com>
2025-10-01 23:56:10 +0800

    [sw,cov] Add asm macro to mark covered code
    This change adds `COVERAGE_ASM_*_MARK` which can be used to mark
    a coverage region as hit from assembly.
    
    Change-Id: I5a884c4cfee8b1558ea35f8a113cff2de36496f7
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

--- sw/device/lib/coverage/api_asm.h
+++ sw/device/lib/coverage/api_asm.h
@@ -23,11 +23,26 @@
  */
 #define COVERAGE_ASM_TRANSPORT_INIT() call coverage_transport_init;
 
+/**
+ * Marks a specific coverage point as hit.
+ *
+ * This function is no-op in non-coverage builds.
+ *
+ * @param kTemp A temporary register that will be clobbered.
+ * @param kIndex The index of the counter to mark.
+ */
+#define COVERAGE_ASM_AUTOGEN_MARK(kTemp, kIndex) \
+  lui kTemp, % hi(.L__asm_profc + kIndex);       \
+  sb zero, % lo(.L__asm_profc + kIndex)(kTemp)
+
 #else  // OT_COVERAGE_INSTRUMENTED
 
 #define COVERAGE_ASM_INIT(...)
 #define COVERAGE_ASM_TRANSPORT_INIT(...)
+#define COVERAGE_ASM_AUTOGEN_MARK(...)
 
 #endif  // OT_COVERAGE_INSTRUMENTED
+
+#define COVERAGE_ASM_MANUAL_MARK COVERAGE_ASM_AUTOGEN_MARK
 
 #endif  // OPENTITAN_SW_DEVICE_LIB_COVERAGE_API_ASM_H_
