dacf03bd756de3a730617504c0947bd0fd54399f
Hakim Filali <hfilali@lowrisc.org>
2025-10-06 11:33:18 +0200

    [sw,cryptolib] Only do instr count checks for OTBN status code OK
    This commit moves down the instruction count checks for the ECC
    ECDH finalize functions to after the OTBN status response check.
    This is necessary since when a user input error occurs, OTBN doesnt
    escalate to a fatal fault but returns a status code other than OK.
    In this case the instruction count is not as expected and should not
    be checked.
    
    Signed-off-by: Hakim Filali <hfilali@lowrisc.org>
    (cherry picked from commit 40efc39999d32f38f6c1ba7c24e1b71b4ce5ec23)

--- sw/device/lib/crypto/impl/ecc/p256.c
+++ sw/device/lib/crypto/impl/ecc/p256.c
@@ -381,6 +381,15 @@ status_t p256_ecdh_finalize(p256_ecdh_shared_key_t *shared_key) {
   }
   HARDENED_CHECK_EQ(ok, kHardenedBoolTrue);
 
+  // OTBN returned the status code OK, so check for the expected instr. count.
+  uint32_t ins_cnt;
+  ins_cnt = otbn_instruction_count_get();
+  if (launder32(ins_cnt) == kModeEcdhSideloadInsCnt) {
+    HARDENED_CHECK_EQ(ins_cnt, kModeEcdhSideloadInsCnt);
+  } else {
+    HARDENED_CHECK_EQ(ins_cnt, kModeEcdhInsCnt);
+  }
+
   // Read the shares of the key from OTBN dmem (at vars x and y).
   HARDENED_TRY_WIPE_DMEM(
       otbn_dmem_read(kP256CoordWords, kOtbnVarX, shared_key->share0));
