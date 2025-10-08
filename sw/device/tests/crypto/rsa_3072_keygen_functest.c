346e7aae6ee8b34ceff2701717562fa251369d42
Andrea Caforio <andrea.caforio@lowrisc.org>
2025-10-08 09:51:35 +0200

    [crypto/rsa] Hardened RSA key generation
    This commit is a complete rewrite of the RSA key generation OTBN
    implementation in an effort to render it more secure. It consists of
    the following changes:
    
      1. Use the previously hardened `modexp` routine as the center piece
      of the Fermat and Miller-Rabin primality tests. The sequence of
      Fermat and Miller-Rabin tests replaces the previous trial divisions
      followed by a Miller-Rabin sequence.
    
      2. Mask the computation of the GCD during the derivation of the
      secret exponent d.
    
      3. Merge RSA sign/enc and key generation into a single hardened OTBN
      app (`run_rsa`). Move the orthogonal RSA key from cofactor routine
      into a separate app (`run_rsa_key_from_cofactor`). This merge
      results in some cosmetic changes in the cryptolib C API.
    
      4. Adapt all the existing OTBN assembler tests to the new RSA key
      generation algorithm.
    
    The implementation is compliant with FIPS 186-5.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>
    (cherry picked from commit 5c640ea8b5a2bc84679a4137803ab4735377ff9b)

--- sw/device/tests/crypto/rsa_3072_keygen_functest.c
+++ sw/device/tests/crypto/rsa_3072_keygen_functest.c
@@ -74,7 +74,7 @@ status_t keygen_then_sign_test(void) {
   // ensuring that the most significant half is nonzero.
   bool d_large_enough = false;
   for (size_t i = kRsa3072NumWords / 2; i < kRsa3072NumWords; i++) {
-    if (sk->d0.data[i] != 0) {
+    if ((sk->d0.data[i] ^ sk->d1.data[i]) != 0) {
       d_large_enough = true;
     }
   }
