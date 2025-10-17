4aacefb9c10b0782fcc353e6785103c7d38446a8
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-10-17 15:34:56 +0200

    [cryptotest] Test RSA verify using wycheproof vectors
    This commit extends the cryptotest framework to test the RSA verify
    function using the wycheproof test vectors.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/tests/crypto/cryptotest/json/commands.h
+++ sw/device/tests/crypto/cryptotest/json/commands.h
@@ -20,6 +20,7 @@ extern "C" {
     value(_, Hash) \
     value(_, Hmac) \
     value(_, Kmac) \
+    value(_, Rsa) \
     value(_, SphincsPlus)
 UJSON_SERDE_ENUM(CryptotestCommand, cryptotest_cmd_t, COMMAND);
 
