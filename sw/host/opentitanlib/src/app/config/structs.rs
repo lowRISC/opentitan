707f01ae59c2683dcf79a99e019a16bbad9b808e
James Wainwright <james.wainwright@lowrisc.org>
2025-10-17 16:18:40 +0100

    [otlib] Allow reset delay to be configured in files
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
    (cherry picked from commit f8ba7c7b4fd82a3a10c01fabe8d1ef374d0a54c7)

--- sw/host/opentitanlib/src/app/config/structs.rs
+++ sw/host/opentitanlib/src/app/config/structs.rs
@@ -4,11 +4,13 @@
 
 //! Schema for configuration files, exact encoding json/xml to be worked out.
 
+use std::collections::HashMap;
+use std::time::Duration;
+
 use crate::io::gpio::{PinMode, PullMode};
 use crate::io::spi::TransferMode;
 
 use serde::Deserialize;
-use std::collections::HashMap;
 
 /// Configuration of a particular GPIO pin.
 #[derive(Deserialize, Clone, Debug)]
@@ -162,6 +164,10 @@ pub struct ConfigurationFile {
     /// configuration files), in order for it to make sense to use this file.
     #[serde(default)]
     pub requires: HashMap<String, String>,
+    /// Duration of time to assert reset and wait after de-asserting reset.
+    #[serde(default)]
+    #[serde(with = "humantime_serde")]
+    pub reset_delay: Option<Duration>,
     /// List of GPIO pin configurations.
     #[serde(default)]
     pub pins: Vec<PinConfiguration>,
