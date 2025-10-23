6b732f2905ab44f6b4d801e13a24ce53a62443bd
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-10-23 12:44:13 +0200

    [crypto] Cryptolib versioning
    This commit adds a versioning concept to the cryptolib.
    cryptolib_build_info_t contains:
    - released - is the CL version released or not?
    - version - pinned version of the CL
    - scm_revision_low/high - truncated commit hash of
      sw/device/lib/crypto
    
    This is an automatic cherry-pick of #28550 to branch earlgrey_1.0.0.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

---
+++ util/cryptolib_build_info.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# Copyright lowRISC contributors (OpenTitan project).
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+"""Generates cryptolib_build_info.c for cryptolib build."""
+
+import argparse
+import logging as log
+from pathlib import Path
+
+import version_file
+
+
+def generate_build_info_c_source(scm_revision: int) -> str:
+    """Return the contents of a C source file that defines `kCryptoLibBuildInfo`.
+
+    Args:
+        scm_revision: SHA1 sum identifying the current SCM revision.
+    """
+    SHA1_BYTE_CNT = 20
+    assert 0 < scm_revision < 2**(SHA1_BYTE_CNT * 8)
+    scm_rev_byte_be = scm_revision.to_bytes(SHA1_BYTE_CNT, "big")
+    scm_revision_high = int.from_bytes(scm_rev_byte_be[0:4], "big")
+    scm_revision_low = int.from_bytes(scm_rev_byte_be[4:8], "big")
+
+    return f"""
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// --------- W A R N I N G: A U T O - G E N E R A T E D   C O D E !! ---------//
+
+#include "sw/device/lib/crypto/drivers/cryptolib_build_info.h"
+
+#include "sw/device/lib/crypto/include/datatypes.h"
+
+OT_USED
+const cryptolib_build_info_t kCryptoLibBuildInfo = {{
+  .scm_revision = (cryptolib_build_info_scm_revision_t){{
+    .scm_revision_low = {scm_revision_low:#010x},
+    .scm_revision_high = {scm_revision_high:#010x},
+  }},
+  .version = (uint32_t)kCryptoLibVersion,
+  .released = (bool)kCryptoLibReleased,
+}};
+"""
+
+
+def read_version_file(version_info_path, default_version) -> int:
+    """
+    Search for the scm revision variable and interprets
+    the contents as a big-endian hex literal. Return an error
+    if the revision cannot be found.
+    """
+
+    version_info = version_file.VersionInformation(version_info_path)
+    version = version_info.cryptolib_scm_revision(default_version)
+    return int(version, base=16)
+
+
+def write_source_file(outdir: Path, contents: str) -> Path:
+    """Creates cryptolib_build_info.c in `outdir`. Returns the path to the new file."""
+
+    source_out_path = outdir / "cryptolib_build_info.c"
+
+    outdir.mkdir(parents=True, exist_ok=True)
+    with source_out_path.open(mode='w', encoding='utf-8') as fout:
+        fout.write(contents)
+
+    return source_out_path
+
+
+def main():
+    log.basicConfig(format="%(levelname)s: %(message)s")
+
+    parser = argparse.ArgumentParser(prog="cryptolib_build_info")
+    parser.add_argument('--outdir',
+                        '-o',
+                        required=True,
+                        type=Path,
+                        help='Output Directory')
+    parser.add_argument('--ot_version_file',
+                        type=Path,
+                        help='Path to a file with the OpenTitan Version')
+    parser.add_argument('--default_version',
+                        type=str,
+                        help='Version to use if the version file does not indicate a version')
+
+    args = parser.parse_args()
+
+    # Extract version stamp from file
+    version = read_version_file(args.ot_version_file, args.default_version)
+    log.info("Version: %x" % (version, ))
+
+    generated_source = generate_build_info_c_source(version)
+    out_path = write_source_file(args.outdir, generated_source)
+    log.info("Generated new source file: %s" % (out_path))
+
+
+if __name__ == "__main__":
+    main()
