b776b4fca50298b2bf82edde2ed181d09ad56a07
Hakim Filali <hfilali@lowrisc.org>
2025-10-27 14:24:34 +0100

    [sw,cryptolib] Fix trigger_fault_if_fg-1 functions
    The trigger_fault_if_fg0 functions had a bug where we never
    trigger a fault. This new implementation should now correctly
    trigger the fault.
    
    For the case where no fault is triggered, we load address 0 into
    w31. For the error case we try to load address 0 into
    w39 (which doesn't exist), which triggers a ILLEGAL_INSN error.
    
    Signed-off-by: Hakim Filali <hfilali@lowrisc.org>

--- sw/otbn/crypto/p256_shared_key.s
+++ sw/otbn/crypto/p256_shared_key.s
@@ -89,7 +89,7 @@ p256_shared_key:
      The check fails if both sides are not equal.
      FG0.Z <= (y^2) mod p == (x^2 + ax + b) mod p */
   bn.cmp   w18, w19
-  jal      x1, trigger_fault_if_fg0_not_z
+  jal      x1, trigger_fault_if_fg0_z
 
   /* Arithmetic masking:
    1. Generate a random mask
