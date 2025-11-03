62a40ba1a68fa13bf006dfbb76557eeabd12ab1b
Siemen Dhooghe <sdhooghe@google.com>
2025-11-03 03:55:07 -0500

    [fisim/crypto] Add instruction skip simulator
    Add a script to load the pentest framework, connect to OpenOCD, and connect to GDB.
    Generate a trace file of a called function.
    Use the trace file to insert instruction skips in order to simulate fault attacks and test countermeasures.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>

---
+++ sw/host/penetrationtests/python/util/gdb_controller.py
@@ -0,0 +1,207 @@
+# Copyright lowRISC contributors (OpenTitan project).
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+from subprocess import PIPE, Popen, TimeoutExpired
+import select
+import os
+import time
+import re
+import signal
+
+
+class GDBController:
+    def __init__(self, gdb_path, gdb_port=3333, remote_host="localhost", elf_file=None):
+        self.remote_host = remote_host
+        self.gdb_port = gdb_port
+        self.gdb_path = gdb_path
+        gdb_command = [
+            gdb_path,
+            # "--interpreter=mi",
+            "-ex",
+            f"target remote {remote_host}:{gdb_port}",
+        ]
+        if elf_file:
+            gdb_command.append(elf_file)
+
+        try:
+            self.gdb_process = Popen(gdb_command, stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=0)
+
+            # Flush the output from GDB
+            self.dump_output()
+            # Start clean
+            self.send_command("delete breakpoints", timeout=10)
+            # Need to flush again from the breakpoints
+            self.dump_output()
+
+            # Set number of breakpoints
+            self.n_brkp = 1
+        except Exception:
+            self.close_gdb()
+            raise
+
+    def read_output(self, print_errors=False, timeout=0.05):
+        if not self.gdb_process:
+            return ""
+
+        output = ""
+
+        readable_pipes = []
+        if self.gdb_process.stdout:
+            readable_pipes.append(self.gdb_process.stdout.fileno())
+        if self.gdb_process.stderr:
+            readable_pipes.append(self.gdb_process.stderr.fileno())
+
+        try:
+            readable, _, _ = select.select(readable_pipes, [], [], timeout)
+
+            for fd in readable:
+                if fd == self.gdb_process.stdout.fileno():
+                    data = os.read(fd, 4096).decode("utf-8", errors="ignore")
+                    output += data
+                elif fd == self.gdb_process.stderr.fileno():
+                    # GDB uses stderr for logging/errors
+                    err_data = os.read(fd, 4096).decode("utf-8", errors="ignore")
+                    if print_errors:
+                        print(f"GDB Stderr: {err_data}")
+        except Exception as e:
+            print(f"Error reading GDB output: {e}")
+
+        return output
+
+    def dump_output(self, timeout=0.05):
+        self.read_output(timeout=timeout)
+
+    def send_command(self, mi_command, timeout=0.05, check_response=True):
+        if not self.gdb_process or not self.gdb_process.stdin:
+            raise RuntimeError("GDB process not started or stdin not available.")
+
+        command_line = mi_command.strip() + "\n"
+
+        self.gdb_process.stdin.write(command_line.encode("utf-8"))
+        # Aria: After sending the command let's wait for a while till the command is
+        # processed on the receiving end
+        time.sleep(0.1)
+
+        self.gdb_process.stdin.flush()
+
+        if check_response:
+            start_time = time.time()
+            response = ""
+            while True:
+                response += self.read_output()
+
+                if response.strip().endswith("(gdb)") or (
+                    "^done" in response and response.strip().endswith("=")
+                ):
+                    break
+
+                if time.time() - start_time > timeout:
+                    raise TimeoutError(
+                        f"GDB timed out after {timeout}s. Current output: {response}, {mi_command}"
+                    )
+
+            # To debug you can print this output to see GDB's response
+            return response
+        else:
+            return None
+
+    def reset_target(self, reset_delay=0.005):
+        self.send_command("monitor reset run", check_response=False)
+        time.sleep(reset_delay)
+        self.dump_output()
+
+    def close_gdb(self):
+        if not self.gdb_process or self.gdb_process.poll() is not None:
+            return
+
+        self.dump_output()
+        self.gdb_process.send_signal(signal.SIGINT)
+        try:
+            self.gdb_process.communicate(timeout=1)
+        except TimeoutExpired:
+            self.gdb_process.kill()
+            self.gdb_process.communicate()
+        finally:
+            self.gdb_process = None
+
+    def get_program_counter(self):
+        gdb_command = "p $pc"
+
+        try:
+            response = self.send_command(gdb_command, timeout=0.5)
+            pc_pattern = re.compile(r"0x([0-9a-fA-F]+)")
+            match = pc_pattern.search(response)
+            if match:
+                return "0x" + match.group(1).strip()
+            if "No symbol " in response or "Undefined command" in response:
+                raise RuntimeError(f"GDB returned an error: {response}")
+
+        except Exception:
+            return None
+
+    def setup_pc_trace(self, file_name, trace_start_addr, trace_end_addr):
+        self.n_brkp = 1
+        self.send_command(f"set logging file {file_name}")
+        self.send_command("set logging overwrite on")
+        self.send_command("set pagination off")
+        self.send_command("set logging enabled on")
+        traceloop_definition = f"""\
+        define traceloop
+            while 1
+                if $pc=={trace_end_addr}
+                    printf "PC trace complete.\\n"
+                    return
+                end
+                printf "PC: 0x%x\\n", $pc
+                stepi
+            end
+        end
+        """
+        self.send_command(traceloop_definition)
+        self.send_command(f"tb *({trace_start_addr})")
+        commands_definition = "commands 1\ntraceloop\nend"
+        self.send_command(commands_definition)
+        self.n_brkp += 1
+
+    def parse_pc_trace_file(self, file_path):
+        pc_list = []
+        pc_pattern = re.compile(r"PC: (0x[0-9a-fA-F]+)")
+
+        try:
+            with open(file_path, "r") as f:
+                for line in f:
+                    match = pc_pattern.search(line)
+                    if match:
+                        pc_list.append(match.group(1))
+        except FileNotFoundError:
+            print(f"Error: Trace file not found at {file_path}")
+        except Exception as e:
+            print(f"Error reading or parsing trace file: {e}")
+
+        return pc_list
+
+    def apply_instruction_skip(self, pc_address, next_pc_address, count):
+        skip_commands = f"commands {self.n_brkp}\n"
+        skip_commands += f"set $pc={next_pc_address}\n"
+        skip_commands += 'printf "instruction skip applied\\n"\n'
+        skip_commands += "c\n"
+        skip_commands += "end"
+
+        self.send_command(f"tb *({pc_address})")
+        if count > 1:
+            ignore_amount = count - 1
+            self.send_command(f"ignore {self.n_brkp} {ignore_amount}")
+        self.send_command(skip_commands)
+        self.n_brkp += 1
+
+    def add_observation(self, observations):
+        for addr, log_message in observations.items():
+            obs_command = f"commands {self.n_brkp}\n"
+            obs_command += f'printf "fisim_result: {log_message} \\n"\n'
+            obs_command += "c\n"
+            obs_command += "end"
+
+            self.send_command(f"tb *({addr})")
+            self.send_command(obs_command)
+            self.n_brkp += 1
--- sw/host/penetrationtests/python/util/hyperdebug.py
+++ sw/host/penetrationtests/python/util/hyperdebug.py
@@ -3,84 +3,85 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import time
-from subprocess import PIPE, Popen
-from typing import Optional
-
+from subprocess import Popen, run, PIPE, CalledProcessError, TimeoutExpired
+import select
+import signal
 import serial
+import sys
+import os
+import socket
 from serial.tools.list_ports import comports
 
 
-class HyperDebug():
+class HyperDebug:
     """Class for the FPGA or Chip connected via a hyperdebug.
     Initializes OpenTitan with the provided firmware & provides helper functions.
     """
-    def __init__(self, opentitantool, fw_bin, bitstream, tool_args):
+
+    def __init__(
+        self,
+        opentitantool,
+        fw_bin,
+        bitstream,
+        tool_args,
+        openocd,
+        openocd_chip_config,
+        openocd_design_config,
+    ):
         self.opentitantool = opentitantool
         self.fw_bin = fw_bin
         self.bitstream = bitstream
         self.tool_args = tool_args
+        self.openocd = openocd
+        self.openocd_chip_config = openocd_chip_config
+        self.openocd_design_config = openocd_design_config
+        self.openocd_process = None
 
-    def initialize_target(self):
+    def initialize_target(self, print_output=True):
         # Programming the bitstream via the opentitantool seems to block
         # communication, programming it twice seems to solve the problem
-        self.program_bitstream(self.bitstream)
-        self.program_bitstream(self.bitstream)
+        self.program_bitstream(self.bitstream, print_output=print_output)
+        self.program_bitstream(self.bitstream, print_output=print_output)
 
-        self.flash_target(self.fw_bin)
+        self.flash_target(self.fw_bin, print_output=print_output)
 
-    def program_bitstream(self, bitstream, program_delay=2):
-        if bitstream:
-            bitstream_process = Popen(
-                [
-                    self.opentitantool,
-                ] +
-                self.tool_args +
-                [
-                    "--exec",
-                    "transport init",
-                    "--exec",
-                    "fpga load-bitstream " + bitstream,
-                    "no-op",
-                ],
-                stderr=PIPE,
-            )
-            stderr = bitstream_process.communicate()
-            rc = bitstream_process.returncode
-            if rc != 0:
-                raise RuntimeError("Error: Failed to program the bitstream.")
-                return 0
-            else:
-                stderr_str = stderr[1].decode('utf-8')
-                if "Skip loading bitstream" not in stderr_str:
-                    time.sleep(program_delay)
-                print(f"Info: FPGA programmed with {bitstream}.")
-        return 1
+        if self.openocd:
+            self.start_openocd(print_output=print_output)
 
-    def flash_target(self, firmware, boot_delay=2):
-        flash_process = Popen(
-            [
-                self.opentitantool,
-            ] +
-            self.tool_args +
-            [
-                "--exec",
-                "transport init",
-                "--exec",
-                "bootstrap " + firmware,
-                "no-op",
-            ],
-            stdout=PIPE, stderr=PIPE
+    def program_bitstream(self, bitstream, program_delay=2, print_output=True):
+        if not bitstream:
+            return
+
+        command = (
+            [self.opentitantool]
+            + self.tool_args
+            + ["--exec", "transport init", "--exec", f"fpga load-bitstream {bitstream}", "no-op"]
         )
-        flash_process.communicate()
-        rc = flash_process.returncode
-        if rc != 0:
-            raise RuntimeError("Error: Failed to flash chip.")
-            return 0
-        else:
-            # Wait until chip finished booting.
+        try:
+            result = run(command, check=True, capture_output=True, text=True)
+            if "Skip loading bitstream" not in result.stderr:
+                time.sleep(program_delay)
+            if print_output:
+                print(f"Info: FPGA programmed with {bitstream}.")
+        except CalledProcessError as e:
+            print(f"Error: Failed to program the bitstream.\nStderr: {e.stderr}", file=sys.stderr)
+            raise
+
+    def flash_target(self, firmware, boot_delay=2, print_output=True):
+        command = (
+            [self.opentitantool]
+            + self.tool_args
+            + ["--exec", "transport init", "--exec", f"bootstrap {firmware}", "no-op"]
+        )
+        try:
+            run(command, check=True, capture_output=True)
+            # Wait until chip finishes booting.
             time.sleep(boot_delay)
-            print(f"Info: Chip flashed with {firmware}.")
-            return 1
+            if print_output:
+                print(f"Info: Chip flashed with {firmware}.")
+        except CalledProcessError as e:
+            print(f"Error: Failed to flash chip.\nStderr: {e.stderr}", file=sys.stderr)
+            raise
 
     def init_communication(self, port, baudrate):
         """Open the communication interface.
@@ -96,6 +97,117 @@ class HyperDebug():
         com_interface.timeout = 1
         return com_interface
 
+    def start_openocd(self, startup_delay=4, print_output=True):
+        self.close_openocd()
+        # We set up OpenOCD with the following default ports
+        # 6666 for tcl connections
+        # 4444 for telnet connections
+        # 3333 for gdb connections
+        # You can adapt those ports, e.g., via adding the config: -c "telnet_port 4444"
+        OPENOCD_COMMANDS = "adapter speed 500; transport select jtag; reset_config trst_only"
+
+        command = [
+            self.openocd,
+            "-f",
+            self.openocd_chip_config,
+            "-c",
+            OPENOCD_COMMANDS,
+            "-f",
+            self.openocd_design_config,
+        ]
+
+        if print_output:
+            print("Starting OpenOCD", flush=True)
+
+        self.openocd_process = Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+        # OpenOCD provides its status data via the error output
+        openocd_stderr = self.openocd_process.stderr.fileno()
+        os.set_blocking(openocd_stderr, False)
+        # Wait until the openocd is set up (this process will remain in the background)
+        time.sleep(startup_delay)
+        readable, _, _ = select.select([openocd_stderr], [], [], 0.1)
+        if readable:
+            try:
+                data_after_wait = os.read(openocd_stderr, 1024 * 1024).decode(
+                    "utf-8", errors="ignore"
+                )
+                if print_output:
+                    print(data_after_wait, flush=True)
+                if "Error:" in data_after_wait:
+                    print("Error detected in starting openocd")
+
+            except BlockingIOError:
+                print("Error reading the openocd output")
+                pass
+
+    def close_openocd(self):
+        if not self.openocd_process or self.openocd_process.poll() is not None:
+            return
+
+        self.openocd_process.send_signal(signal.SIGINT)
+        try:
+            self.openocd_process.communicate(timeout=10)
+        except TimeoutExpired:
+            self.openocd_process.kill()
+            self.openocd_process.communicate()
+        finally:
+            self.openocd_process = None
+
+    def read_openocd(self):
+        if self.openocd_process:
+            openocd_stderr = self.openocd_process.stderr.fileno()
+            readable, _, _ = select.select([openocd_stderr], [], [], 0.1)
+            if readable:
+                try:
+                    data = os.read(openocd_stderr, 1024 * 1024).decode("utf-8", errors="ignore")
+                    return data
+
+                except BlockingIOError:
+                    print("Error reading the OpenOCD output")
+                    pass
+        return None
+
+    def send_openocd_command(self, command, timeout=1.0, port=6666):
+        HOST = "127.0.0.1"
+
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(timeout)
+            s.connect((HOST, port))
+        except socket.error as e:
+            print(f"Error connecting to OpenOCD on port {port}: {e}", flush=True)
+            return f"ERROR: Connection failed: {e}"
+
+        full_command = f"{command}\x1a"
+        try:
+            s.sendall(full_command.encode("utf-8"))
+
+            response = b""
+            while True:
+                chunk = s.recv(4096)
+                if not chunk:
+                    break
+                response += chunk
+                if b"\x00" in chunk:
+                    break
+
+        except socket.timeout:
+            print(f"Warning: Command '{command}' timed out after {timeout}s.", flush=True)
+        except socket.error as e:
+            print(f"Error sending/receiving data: {e}", flush=True)
+            s.close()
+            return f"ERROR: Send/Receive failed: {e}"
+
+        s.close()
+
+        decoded_response = response.decode("utf-8", errors="ignore").strip()
+
+        if decoded_response.startswith(command):
+            decoded_response = decoded_response[len(command):].strip()
+
+        return decoded_response.strip()
+
     def find_target_port(self):
         for port in comports():
             if "UART2" in port.description and "HyperDebug" in port.description:
@@ -103,14 +215,12 @@ class HyperDebug():
         print("Target not found!")
         return None
 
-    def reset_target(self, com_reset: Optional[bool] = False, reset_delay=0.005):
+    def reset_target(self, com_reset=False, reset_delay=0.005):
         """Resets the target."""
-        reset_process = Popen(
-            [
-                self.opentitantool,
-            ] +
-            self.tool_args +
-            [
+        reset_process = (
+            [self.opentitantool]
+            + self.tool_args
+            + [
                 "--exec",
                 "transport init",
                 "--exec",
@@ -118,14 +228,13 @@ class HyperDebug():
                 "--exec",
                 "gpio write RESET true",
                 "no-op",
-            ],
-            stdout=PIPE, stderr=PIPE
+            ]
         )
-        reset_process.communicate()
-        rc = reset_process.returncode
-        if rc != 0:
-            raise RuntimeError("Error: Failed to reset chip.")
-        else:
+        try:
+            run(reset_process, check=True, capture_output=True, text=True)
             time.sleep(reset_delay)
+        except CalledProcessError:
+            print("Error: Failed to reset chip.")
+            raise
         if com_reset:
             self.com_interface = self.init_communication(None, 115200)
