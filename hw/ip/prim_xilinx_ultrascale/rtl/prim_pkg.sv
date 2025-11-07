0c7074fbb4af9b4bb951d6e59caae7df1f9f0118
Robert Schilling <rschilling@rivosinc.com>
2025-11-07 15:09:02 +0100

    [hw,prim_pkg,dv] Add a paramter to the prim_pkg for the minimum CDC delay
    Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

--- hw/ip/prim_xilinx_ultrascale/rtl/prim_pkg.sv
+++ hw/ip/prim_xilinx_ultrascale/rtl/prim_pkg.sv
@@ -7,4 +7,11 @@ package prim_pkg;
   // The name of the technology implementation.
   parameter PrimTechName = "XilinxUltrascale";
 
+  // The minimum delay in cycles it can take a signal to propagate through a CDC prim_2sync flop.
+  parameter int CdcMinDelayCycles = 1;
+
+  function automatic int get_cdc_min_delay_cycles();
+    return CdcMinDelayCycles;
+  endfunction
+
 endpackage // prim_pkg
