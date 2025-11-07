ebafc68d4af740ef3e39ac9db9525826d3e76908
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-11-07 12:57:47 +0000

    [racl_ctrl,dv] Avoid templated type in vseq / scoreboard
    This type and its name are generated with a template, but we don't
    care about that in either the vseq or the scoreboard.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/racl_ctrl/dv/env/racl_ctrl_scoreboard.sv
+++ hw/ip/racl_ctrl/dv/env/racl_ctrl_scoreboard.sv
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 class racl_ctrl_scoreboard extends cip_base_scoreboard #(.CFG_T(racl_ctrl_env_cfg),
-                                                         .RAL_T(racl_ctrl_reg_block),
+                                                         .RAL_T(dv_base_reg_block),
                                                          .COV_T(racl_ctrl_env_cov));
   `uvm_component_utils(racl_ctrl_scoreboard)
 
