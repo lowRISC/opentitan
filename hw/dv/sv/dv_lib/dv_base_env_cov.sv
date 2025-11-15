9ee9212c994af47c706ae5520e789f3289e9b251
Rupert Swarbrick <rswarbrick@lowrisc.org>
2025-11-15 20:28:36 +0000

    [dv] Tiny tidy-up in dv_base_env_cov
    Add a documentation comment and inline the slightly confusing
    uvm_component_new macro.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/dv/sv/dv_lib/dv_base_env_cov.sv
+++ hw/dv/sv/dv_lib/dv_base_env_cov.sv
@@ -2,11 +2,15 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+// A base class for coverage collection. This base class only really contains the CFG_T handle, that
+// points to the config for the environment in use.
+
 class dv_base_env_cov #(type CFG_T = dv_base_env_cfg) extends uvm_component;
   `uvm_component_param_utils(dv_base_env_cov #(CFG_T))
 
   CFG_T cfg;
 
-  `uvm_component_new
-
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
 endclass
