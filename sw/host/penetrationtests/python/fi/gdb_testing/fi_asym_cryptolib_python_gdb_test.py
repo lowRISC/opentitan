90636e652e8fd890107a81dc0ee0a7cea9638352
Siemen Dhooghe <sdhooghe@google.com>
2025-11-15 11:35:53 -0500

    [fisim] Add RSA PSS pad test
    Add the same test for RSA verify but with pss padding.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>

--- sw/host/penetrationtests/python/fi/gdb_testing/fi_asym_cryptolib_python_gdb_test.py
+++ sw/host/penetrationtests/python/fi/gdb_testing/fi_asym_cryptolib_python_gdb_test.py
@@ -1072,8 +1072,8 @@ class AsymCryptolibFiSim(unittest.TestCase):
                 self.assertEqual(successful_faults, 0)
                 self.assertEqual(started, True)
 
-    def test_rsa_verify(self):
-        print("Starting the rsa verify test")
+    def test_rsa_pkcs1v15_verify(self):
+        print("Starting the rsa pkcs1v15 verify test")
         # Preparing the input for an invalid signature
         key = RSA.generate(2048)
         public_exponent = key.e
@@ -1092,14 +1092,295 @@ class AsymCryptolibFiSim(unittest.TestCase):
         cfg = 0
         trigger = 0x4
         hashing = 0
+        # pkcs1v15
         padding = 0
 
         # Directory for the trace log files
-        pc_trace_file = os.path.join(log_dir, "rsa_verify_pc_trace.log")
+        pc_trace_file = os.path.join(log_dir, "rsa_verify_pkcs1v15_pc_trace.log")
         # Directory for the output results
-        test_results_file = os.path.join(log_dir, "rsa_verify_test_results.log")
+        test_results_file = os.path.join(log_dir, "rsa_verify_pkcs1v15_test_results.log")
         # Directory for the the log of the campaign
-        campaign_file = os.path.join(log_dir, "rsa_verify_test_campaign.log")
+        campaign_file = os.path.join(log_dir, "rsa_verify_pkcs1v15_test_campaign.log")
+
+        successful_faults = 0
+        total_attacks = 0
+
+        def trigger_testos_init(print_output=True):
+            # Initializing the testOS (setting up the alerts and accelerators)
+            (device_id, _, _, _, _, _, _) = asymfi.init(
+                alert_config=common_library.no_escalation_alert_config
+            )
+            if print_output:
+                print("Output from init ", device_id)
+
+        def read_testos_output():
+            # Read the output from the operation
+            response = target.read_response(max_tries=1000)
+            return response
+
+        gdb = None
+        started = False
+        with open(test_results_file, "w") as test_results, open(campaign_file, "w") as campaign:
+            print(f"Switching terminal output to {campaign_file}", flush=True)
+            sys.stdout = campaign
+            try:
+                # Program the bitstream, flash the target, and set up OpenOCD
+                target.initialize_target()
+
+                # Initialize the testOS
+                trigger_testos_init()
+
+                # Connect to GDB
+                gdb = GDBController(gdb_path=GDB_PATH, gdb_port=GDB_PORT, elf_file=elf_path)
+
+                # Provide the marker name and extract the start and end address from the dis file
+                function_name = "PENTEST_MARKER_RSA_VERIFY"
+                # Gives back an array of hits where the function is called
+                trace_address = parser.get_marker_addresses(function_name)
+                print("Start and stop addresses of ", function_name, ": ", trace_address)
+
+                crash_observation_address = parser.get_function_start_address(
+                    "ottf_exception_handler"
+                )
+
+                # Start the tracing
+                # We set a short timeout to detect whether GDB has connected properly
+                # and a long timeout for the entire tracing
+                initial_timeout = 10
+                total_timeout = 60 * 60 * 5
+
+                gdb.setup_pc_trace(pc_trace_file, trace_address[0], trace_address[1])
+                gdb.send_command("c", check_response=False)
+
+                # Trigger the rsa verify from the testOS (we do not read its output)
+                asymfi.handle_rsa_verify(
+                    data,
+                    data_len,
+                    public_exponent,
+                    n,
+                    n_len,
+                    sig,
+                    sig_len,
+                    padding,
+                    hashing,
+                    cfg,
+                    trigger,
+                )
+
+                start_time = time.time()
+                initial_timeout_stopped = False
+                total_timeout_stopped = False
+
+                # Run the tracing to get the trace log
+                # Sometimes the tracing fails due to race conditions,
+                # we have a quick initial timeout to catch this
+                while time.time() - start_time < initial_timeout:
+                    output = gdb.read_output()
+                    if "breakpoint 1, " in output:
+                        initial_timeout_stopped = True
+                        break
+                if not initial_timeout_stopped:
+                    print("No initial break point found, can be a misfire, try again")
+                    sys.exit(1)
+                while time.time() - start_time < total_timeout:
+                    output = gdb.read_output()
+                    if "PC trace complete" in output:
+                        print("\nTrace complete")
+                        total_timeout_stopped = True
+                        break
+                if not total_timeout_stopped:
+                    print("Final tracing timeout reached")
+                    sys.exit(1)
+
+                # Parse and truncate the trace log to get all PCs in a list
+                pc_list = gdb.parse_pc_trace_file(pc_trace_file)
+                # Get the unique PCs and annotate their occurence count
+                pc_count_dict = Counter(pc_list)
+                if len(pc_count_dict) <= 0:
+                    print("Found no tracing, stopping")
+                    sys.exit(1)
+                print("Tracing has a total of", len(pc_count_dict), "unique PCs", flush=True)
+
+                # Reset the target, flush the output, and close gdb
+                gdb.close_gdb()
+                target.close_openocd()
+                target.initialize_target()
+                trigger_testos_init()
+                target.dump_all()
+                gdb = GDBController(gdb_path=GDB_PATH, gdb_port=GDB_PORT, elf_file=elf_path)
+
+                started = True
+                for pc, count in pc_count_dict.items():
+                    for i_count in range(min(MAX_SKIPS_PER_LOOP, count)):
+                        print("-" * 80)
+                        print("Applying instruction skip in ", pc, "occurence", i_count)
+                        print("-" * 80)
+
+                        crash_observation = "crash detected"
+
+                        try:
+                            # The observation points
+                            observations = {
+                                # Crash check
+                                crash_observation_address: f"{crash_observation}",
+                            }
+                            gdb.add_observation(observations)
+
+                            gdb.apply_instruction_skip(
+                                pc, parser.parse_next_instruction(pc), i_count
+                            )
+                            gdb.send_command("c", check_response=False)
+
+                            # The instruction skip loop
+                            asymfi.handle_rsa_verify(
+                                data,
+                                data_len,
+                                public_exponent,
+                                n,
+                                n_len,
+                                sig,
+                                sig_len,
+                                padding,
+                                hashing,
+                                cfg,
+                                trigger,
+                            )
+                            testos_response = read_testos_output()
+
+                            gdb_response = gdb.read_output()
+                            if "instruction skip applied" in gdb_response:
+                                total_attacks += 1
+
+                                if crash_observation in gdb_response:
+                                    print("Crash detected, resetting", flush=True)
+                                    gdb.close_gdb()
+                                    gdb = GDBController(
+                                        gdb_path=GDB_PATH, gdb_port=GDB_PORT, elf_file=elf_path
+                                    )
+                                    gdb.reset_target()
+                                    target.dump_all()
+                                    trigger_testos_init(print_output=False)
+                                    # Reset again
+                                    gdb.close_gdb()
+                                    gdb = GDBController(
+                                        gdb_path=GDB_PATH, gdb_port=GDB_PORT, elf_file=elf_path
+                                    )
+                                else:
+                                    testos_response_json = json.loads(testos_response)
+                                    print("Output:", testos_response_json, flush=True)
+                                    verification_result = testos_response_json["result"]
+                                    verification_status = testos_response_json["status"]
+                                    if verification_result and (verification_status == 0):
+                                        successful_faults += 1
+                                        print("-" * 80)
+                                        print("Successful FI attack!")
+                                        print("Location:", pc, "iteration", i_count)
+                                        print(gdb_response)
+                                        print("Response:", testos_response_json)
+                                        print("-" * 80)
+                                        test_results.write(
+                                            f"{pc}, {i_count}: {testos_response_json}\n"
+                                        )
+                                    # Reset GDB by closing and opening again
+                                    gdb.close_gdb()
+                                    gdb = GDBController(
+                                        gdb_path=GDB_PATH, gdb_port=GDB_PORT, elf_file=elf_path
+                                    )
+                                    # We do not need to reset the target since it returned an output
+                            else:
+                                print("No break point found, something went wrong", flush=True)
+                                gdb.close_gdb()
+                                target.close_openocd()
+                                target.initialize_target()
+                                trigger_testos_init()
+                                target.dump_all()
+                                gdb = GDBController(
+                                    gdb_path=GDB_PATH, gdb_port=GDB_PORT, elf_file=elf_path
+                                )
+
+                        except json.JSONDecodeError:
+                            print(
+                                "Error: JSON decoding failed. Invalid response format", flush=True
+                            )
+                            gdb.close_gdb()
+                            gdb = GDBController(
+                                gdb_path=GDB_PATH, gdb_port=GDB_PORT, elf_file=elf_path
+                            )
+                            gdb.reset_target()
+                            target.dump_all()
+                            trigger_testos_init(print_output=False)
+                            # Reset again
+                            gdb.close_gdb()
+                            gdb = GDBController(
+                                gdb_path=GDB_PATH, gdb_port=GDB_PORT, elf_file=elf_path
+                            )
+                            gdb.dump_output()
+                            target.dump_all()
+
+                        except TimeoutError as e:
+                            print("Timeout error, retrying", flush=True)
+                            print(e, flush=True)
+                            gdb.close_gdb()
+                            target.close_openocd()
+                            target.initialize_target()
+                            trigger_testos_init()
+                            target.dump_all()
+                            gdb = GDBController(
+                                gdb_path=GDB_PATH, gdb_port=GDB_PORT, elf_file=elf_path
+                            )
+
+            finally:
+                print("-" * 80)
+                print("Trace data is logged in ", pc_trace_file)
+                print("The campaign is logged in ", campaign_file)
+                print("Instruction skip results are logged in ", test_results_file)
+                print(f"Total attacks {total_attacks}, successful attacks {successful_faults}")
+                print("You can find the dissassembly in ", dis_path)
+                # Close the OpenOCD and GDB connection at the end
+                if gdb:
+                    gdb.close_gdb()
+                target.close_openocd()
+                test_results.write(
+                    f"Total attacks {total_attacks}, successful attacks {successful_faults}\n"
+                )
+                sys.stdout = original_stdout
+                print("Trace data is logged in ", pc_trace_file)
+                print("The campaign is logged in ", campaign_file)
+                print("Instruction skip results are logged in ", test_results_file)
+                print(f"Total attacks {total_attacks}, successful attacks {successful_faults}")
+                print("You can find the dissassembly in ", dis_path)
+                self.assertEqual(successful_faults, 0)
+                self.assertEqual(started, True)
+
+    def test_rsa_pss_verify(self):
+        print("Starting the rsa pss verify test")
+        # Preparing the input for an invalid signature
+        key = RSA.generate(2048)
+        public_exponent = key.e
+        n = [x for x in (key.n).to_bytes(256, "little")]
+        n_len = 256
+        data_len = 13
+        data = [i for i in range(data_len)]
+        h = SHA256.new(bytes(data))
+        signer = pkcs1_15.new(key)
+        signature = signer.sign(h)
+        sig = [x for x in signature]
+        # Corrupt the signature for FiSim Testing
+        sig[0] ^= 0x1
+        sig.reverse()
+        sig_len = len(sig)
+        cfg = 0
+        trigger = 0x4
+        hashing = 0
+        # pss
+        padding = 1
+
+        # Directory for the trace log files
+        pc_trace_file = os.path.join(log_dir, "rsa_verify_pss_pc_trace.log")
+        # Directory for the output results
+        test_results_file = os.path.join(log_dir, "rsa_verify_pss_test_results.log")
+        # Directory for the the log of the campaign
+        campaign_file = os.path.join(log_dir, "rsa_verify_pss_test_campaign.log")
 
         successful_faults = 0
         total_attacks = 0
