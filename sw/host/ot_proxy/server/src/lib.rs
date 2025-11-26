aa548942070dc9c481d699b317afa9cab331d1d6
Gary Guo <gary.guo@lowrisc.org>
2025-11-26 16:18:13 +0000

    [opentitanproxy] use proper async in `Connection` handling in proxy server
    The previous code is a direct translation from mio to tokio. This is now
    refactored to proper async.
    
    Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

--- sw/host/ot_proxy/server/src/lib.rs
+++ sw/host/ot_proxy/server/src/lib.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::net::SocketAddr;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use anyhow::{Result, bail};
 use tokio::net::TcpListener;
@@ -20,7 +20,7 @@ mod socket_server;
 /// Interface for handlers of protocol messages, responding to each message with a single
 /// instance of the same protocol message.
 pub trait CommandHandler<Msg> {
-    fn execute_cmd(&mut self, conn: &Arc<Mutex<Connection>>, msg: &Msg) -> Result<Msg>;
+    fn execute_cmd(&mut self, conn: &Arc<Connection>, msg: &Msg) -> Result<Msg>;
 }
 
 /// This is the main entry point for the session proxy.  This struct will either bind on a
