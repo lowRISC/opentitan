8e65fedf00c54cd297952d24e6ce24a1171050ac
Douglas Reis <doreis@lowrisc.org>
2025-11-26 16:17:06 +0000

    [systemrdl] Add new udps for be used in signals

--- util/reggen/systemrdl/udp.py
+++ util/reggen/systemrdl/udp.py
@@ -7,7 +7,7 @@ from typing import Any
 from systemrdl.udp import UDPDefinition
 from systemrdl import RDLCompiler  # type: ignore[attr-defined]
 from systemrdl.node import Node
-from systemrdl.component import Reg, Mem
+from systemrdl.component import Reg, Mem, Signal
 
 
 class UDPBoolean(UDPDefinition):
@@ -39,6 +39,52 @@ class IntegrityBypass(UDPBoolean):
     valid_components = {Mem}
 
 
+class SigType(UDPDefinition):
+    default_assignment = None
+    name = "sigtype"
+    valid_components = {Signal}
+    valid_type = "SigType"
+
+    enum_map = {
+        "None": 0,
+        "Interrupt": 1,  # Signal is an interrupt
+        "Alert": 2,  # Signal is an alert
+        "InterModReqRsp": 3,  # Signal is an inter module, with type=req_rsp
+        "InterModReq": 4,  # Signal is an inter module, with type=uni and act=req
+        "InterModRecv": 5,  # Signal is an inter module, with type=uni and act=recv
+        "Output": 6,  # Signal is an output
+        "Input": 7,  # Signal is an input
+        "Inout": 8,  # Signal is input and/or output
+    }
+
+    def validate(self, node: Node, value: Any) -> None:
+        if value not in self.enum_map:
+            self.msg.error(
+                f"Invalid sigtype '{value}'. Expected one of {list(self.enum_map.keys())}"
+            )
+
+
+class InterModStruct(UDPDefinition):
+    valid_type = str
+    default_assignment = "logic"
+    name = "inter_mod_struct"
+    valid_components = {Signal}
+
+    def validate(self, node: Node, value: Any) -> None:
+        if not isinstance(value, str):
+            self.msg.error("The type of {self.name} must be string.", self.get_src_ref(node))
+
+
+class InterModPackage(UDPDefinition):
+    valid_type = str
+    name = "inter_mod_package"
+    valid_components = {Signal}
+
+    def validate(self, node: Node, value: Any) -> None:
+        if not isinstance(value, str):
+            self.msg.error("The type of {self.name} must be string.", self.get_src_ref(node))
+
+
 OPENTITAN_UDPS = Path(__file__).parent / "udp.rdl"
 
 
@@ -50,5 +96,9 @@ def register_udps(compiler: RDLCompiler) -> Path:
     compiler.register_udp(Shadowed)
     compiler.register_udp(AsyncClk)
     compiler.register_udp(IntegrityBypass)
+    # TODO: figure out how to declare the 'valid_types' attribute when it is a user-defined enum.
+    # compiler.register_udp(SigType)
+    compiler.register_udp(InterModStruct)
+    compiler.register_udp(InterModPackage)
     compiler.compile_file(OPENTITAN_UDPS)
     return OPENTITAN_UDPS
