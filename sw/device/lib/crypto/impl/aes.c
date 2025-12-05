dadecd8fd5349bc92d9ca9ad1d8397c198809964
Pascal Nasahl <nasahlpa@lowrisc.org>
2025-12-05 09:51:13 +0100

    [crypto] Move `security_config_check()` to public API
    This commit moves the `security_config_check()` function
    to the cryptolib public API. With this, callers of the CL also can
    make sure that the device is in a state allowing a secure execution
    of CL.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit 1cbf2983023b294c6d1197e0a916ed03197d2e2b)

--- sw/device/lib/crypto/impl/aes.c
+++ sw/device/lib/crypto/impl/aes.c
@@ -13,9 +13,9 @@
 #include "sw/device/lib/crypto/drivers/keymgr.h"
 #include "sw/device/lib/crypto/impl/integrity.h"
 #include "sw/device/lib/crypto/impl/keyblob.h"
-#include "sw/device/lib/crypto/impl/security_config.h"
 #include "sw/device/lib/crypto/impl/status.h"
 #include "sw/device/lib/crypto/include/datatypes.h"
+#include "sw/device/lib/crypto/include/security_config.h"
 
 // Module ID for status codes.
 #define MODULE_ID MAKE_MODULE_ID('a', 'e', 's')
@@ -301,7 +301,7 @@ static otcrypto_status_t otcrypto_aes_impl(
   }
 
   // Check the security config of the device.
-  HARDENED_TRY(security_config_check(key->config.security_level));
+  HARDENED_TRY(otcrypto_security_config_check(key->config.security_level));
 
   // Ensure the entropy complex is initialized.
   HARDENED_TRY(entropy_complex_check());
