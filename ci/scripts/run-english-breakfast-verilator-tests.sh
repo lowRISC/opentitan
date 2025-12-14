f0639390ae6dc4f869288e2172edbc95008b3c62
Amaury Pouly <amaury.pouly@lowrisc.org>
2025-12-14 11:54:20 +0100

    [ci] Use a more bazel-based test flow for englishbreakfast
    Signed-off-by: Amaury Pouly <amaury.pouly@lowrisc.org>

--- ci/scripts/run-english-breakfast-verilator-tests.sh
+++ ci/scripts/run-english-breakfast-verilator-tests.sh
@@ -9,33 +9,19 @@
 
 set -e
 
-. util/build_consts.sh
+# Increase the test_timeout due to slow performance on CI
 
-# Cleaning is necessary for the find commands below to work correctly
-./bazelisk.sh clean
-
-# Build the modified EB software.
-./hw/top_englishbreakfast/util/prepare_sw.py -b
-
-# Build some other dependencies.
-./bazelisk.sh build  \
-    --copt=-DOT_IS_ENGLISH_BREAKFAST_REDUCED_SUPPORT_FOR_INTERNAL_USE_ONLY_ \
-    --features=-rv32_bitmanip \
-    //sw/host/opentitantool //hw/top_earlgrey/data/otp:img_rma
-
-# Run the one test.
-# This needs to be run outside the bazel sandbox, so we do not use `bazel run`
-#
-# NOTE: we specify `-type f` in the find commands to avoid finding any
-# additional symlinks bazel might have prepared when building the test targets.
-bazel-bin/sw/host/opentitantool/opentitantool \
-    --rcfile="" \
-    --logging=info \
-    --interface=verilator \
-    --verilator-bin=$BIN_DIR/hw/top_englishbreakfast/Vchip_englishbreakfast_verilator \
-    --verilator-rom="$(find bazel-out/* -type f -name 'test_rom_sim_verilator.32.vmem')" \
-    --verilator-flash="$(find bazel-out/* -type f -name 'aes_smoketest_sim_verilator.64.vmem')" \
-    console \
-    --exit-failure="(FAIL|FAULT).*\n" \
-    --exit-success="PASS.*\n" \
-    --timeout=3600s
+./bazelisk.sh test \
+    --build_tests_only=true \
+    --test_timeout=2400,2400,3600,-1 \
+    --local_test_jobs=8 \
+    --local_resources=cpu=8 \
+    --test_tag_filters=verilator,-broken \
+    --test_output=errors \
+    --//hw:verilator_options=--threads,1 \
+    --//hw:make_options=-j,8 \
+    --//hw/top=englishbreakfast \
+    //sw/device/tests:aes_smoketest_sim_verilator \
+    //sw/device/tests:uart_smoketest_sim_verilator \
+    //sw/device/tests:crt_test_sim_verilator \
+    //sw/device/tests:flash_ctrl_test_sim_verilator
