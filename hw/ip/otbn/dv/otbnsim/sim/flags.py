3d18194e525998fa3ec6398bd66b3aa1ae01bd80
Hakim Filali <hfilali@lowrisc.org>
2026-01-26 14:44:34 +0100

    [sw,otbnsim] Generalize WSRs and CSRs into one ISPR class
    This commit changes the WSR and DumbWSR classes to be more
    generic. This allows code sharing between the WSR classes
    and potential new classes with different register size but
    the same functionality.
    
    Signed-off-by: Hakim Filali <hfilali@lowrisc.org>

--- hw/ip/otbn/dv/otbnsim/sim/flags.py
+++ hw/ip/otbn/dv/otbnsim/sim/flags.py
@@ -102,33 +102,33 @@ class FlagReg:
 
 class FlagGroups:
     def __init__(self) -> None:
-        self._groups = {0: FlagReg(False, False, False, False),
-                        1: FlagReg(False, False, False, False)}
+        self.groups = {0: FlagReg(False, False, False, False),
+                       1: FlagReg(False, False, False, False)}
         # Have any flags changed?
         self._dirty = False
 
     def __getitem__(self, key: int) -> FlagReg:
         assert 0 <= key <= 1
-        return self._groups[key]
+        return self.groups[key]
 
     def __setitem__(self, key: int, value: FlagReg) -> None:
         assert 0 <= key <= 1
         self._dirty = True
-        self._groups[key].set_flags(value)
+        self.groups[key].set_flags(value)
 
     def changes(self) -> List[TraceFlags]:
-        return self._groups[0].changes(0) + self._groups[1].changes(1)
+        return self.groups[0].changes(0) + self.groups[1].changes(1)
 
     def commit(self) -> None:
         if self._dirty:
-            self._groups[0].commit()
-            self._groups[1].commit()
+            self.groups[0].commit()
+            self.groups[1].commit()
         self._dirty = False
 
     def abort(self) -> None:
         if self._dirty:
-            self._groups[0].abort()
-            self._groups[1].abort()
+            self.groups[0].abort()
+            self.groups[1].abort()
         self._dirty = False
 
     def read_unsigned(self) -> int:
@@ -137,8 +137,8 @@ class FlagGroups:
         Format is defined in FlagReg, with group 0 as LSB.
 
         '''
-        return ((self._groups[1].read_unsigned() << 4) |
-                (self._groups[0].read_unsigned() << 0))
+        return ((self.groups[1].read_unsigned() << 4) |
+                (self.groups[0].read_unsigned() << 0))
 
     def write_unsigned(self, value: int) -> None:
         '''Set the flag groups with an unsigned value, ignoring unused bits
@@ -149,5 +149,5 @@ class FlagGroups:
         assert 0 <= value
         mask4 = (1 << 4) - 1
         self._dirty = True
-        self._groups[0].write_unsigned((value >> 0) & mask4)
-        self._groups[1].write_unsigned((value >> 4) & mask4)
+        self.groups[0].write_unsigned((value >> 0) & mask4)
+        self.groups[1].write_unsigned((value >> 4) & mask4)
