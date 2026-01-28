7e489ba2322ff8cdc285cae0eeee25ae3187b768
Willy Zhang <willyzhang@google.com>
2026-01-27 22:00:54 -0800

    [hsmtool] Add MLDSA support
    Adds commands to generate, export, sign, and verify using the MLDSA
    algorithm (defaulting to ML-DSA-87). Includes support for exporting
    CSRs with the correct ML-DSA-87 OID.
    
    Signed-off-by: Willy Zhang <willyzhang@google.com>

---
+++ sw/host/hsmtool/src/commands/mldsa/import.rs
@@ -0,0 +1,153 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::{Result, anyhow};
+use cryptoki::object::ObjectHandle;
+use cryptoki::session::Session;
+use serde::{Deserialize, Serialize};
+use std::any::Any;
+use std::fs;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+use crate::commands::{BasicResult, Dispatch};
+use crate::error::HsmError;
+use crate::module::Module;
+use crate::util::attribute::{AttrData, AttributeMap, AttributeType};
+use crate::util::helper;
+use crate::util::wrap::{Wrap, WrapPrivateKey};
+
+#[derive(clap::Args, Debug, Serialize, Deserialize)]
+pub struct Import {
+    #[arg(long)]
+    id: Option<String>,
+    #[arg(short, long)]
+    label: Option<String>,
+    /// Import a private key.
+    #[arg(long)]
+    private: bool,
+    /// Unwrap the imported key with a wrapping key.
+    #[arg(long)]
+    unwrap: Option<String>,
+    // Wrapping key mechanism. Required when unwrap is specified.
+    #[arg(long, default_value = "aes-key-wrap-pad")]
+    unwrap_mechanism: Option<WrapPrivateKey>,
+    /// Template for creating the public key.
+    #[arg(long)]
+    public_template: Option<AttributeMap>,
+    /// Template for creating the private key.
+    #[arg(long)]
+    private_template: Option<AttributeMap>,
+    filename: PathBuf,
+}
+
+impl Import {
+    const PUBLIC_TEMPLATE: &str = r#"{
+        "CKA_CLASS": "CKO_PUBLIC_KEY",
+        "CKA_KEY_TYPE": "CKK_MLDSA",
+        "CKA_TOKEN": true,
+        "CKA_VERIFY": true
+    }"#;
+
+    const PRIVATE_TEMPLATE: &str = r#"{
+        "CKA_CLASS": "CKO_PRIVATE_KEY",
+        "CKA_KEY_TYPE": "CKK_MLDSA",
+        "CKA_TOKEN": true,
+        "CKA_PRIVATE": true,
+        "CKA_SENSITIVE": true,
+        "CKA_SIGN": true
+    }"#;
+
+    fn import(&self, session: &Session, id: AttrData, label: AttrData) -> Result<ObjectHandle> {
+        let data = fs::read(&self.filename)?;
+        let key_value = if let Ok((_label, bytes)) = pem_rfc7468::decode_vec(&data) {
+             bytes
+        } else {
+             // Assume DER/Raw
+             data
+        };
+
+        let mut template = if self.private {
+            AttributeMap::from_str(Self::PRIVATE_TEMPLATE).expect("error in PRIVATE_TEMPLATE")
+        } else {
+            AttributeMap::from_str(Self::PUBLIC_TEMPLATE).expect("error in PUBLIC_TEMPLATE")
+        };
+
+        template.insert(AttributeType::Id, id);
+        template.insert(AttributeType::Label, label);
+        template.insert(AttributeType::Value, AttrData::from(key_value.as_slice()));
+
+        if self.private {
+            if let Some(tpl) = &self.private_template {
+                template.merge(tpl.clone());
+            }
+        } else if let Some(tpl) = &self.public_template {
+            template.merge(tpl.clone());
+        }
+
+        log::info!(
+            "template = {}",
+            serde_json::to_string_pretty(&template)?
+        );
+
+        let template_vec = template.to_vec()?;
+        Ok(session.create_object(&template_vec)?)
+    }
+
+    fn unwrap_key(&self, session: &Session, id: AttrData, label: AttrData) -> Result<ObjectHandle> {
+        let wrapper: Wrap = self
+            .unwrap_mechanism
+            .ok_or(anyhow!("unwrap_mechanism is required when unwrap is specified"))?
+            .into();
+        
+        let mut template = if self.private {
+            AttributeMap::from_str(Self::PRIVATE_TEMPLATE).expect("error in PRIVATE_TEMPLATE")
+        } else {
+            AttributeMap::from_str(Self::PUBLIC_TEMPLATE).expect("error in PUBLIC_TEMPLATE")
+        };
+        template.insert(AttributeType::Id, id);
+        template.insert(AttributeType::Label, label);
+
+        if self.private {
+            if let Some(tpl) = &self.private_template {
+                template.merge(tpl.clone());
+            }
+        } else if let Some(tpl) = &self.public_template {
+            template.merge(tpl.clone());
+        }
+
+        let wrapped_data = fs::read(&self.filename)?;
+        wrapper.unwrap(session, &wrapped_data, self.unwrap.as_deref(), &template)
+    }
+}
+
+#[typetag::serde(name = "mldsa-import")]
+impl Dispatch for Import {
+    fn run(
+        &self,
+        _context: &dyn Any,
+        _hsm: &Module,
+        session: Option<&Session>,
+    ) -> Result<Box<dyn erased_serde::Serialize>> {
+        let session = session.ok_or(HsmError::SessionRequired)?;
+        helper::no_object_exists(session, self.id.as_deref(), self.label.as_deref())?;
+        let id = AttrData::Str(self.id.as_ref().cloned().unwrap_or_else(helper::random_id));
+        let label = AttrData::Str(self.label.as_ref().cloned().unwrap_or_default());
+        
+        let result = Box::new(BasicResult {
+            success: true,
+            id: id.clone(),
+            label: label.clone(),
+            value: None,
+            error: None,
+        });
+
+        if self.unwrap.is_some() {
+            self.unwrap_key(session, id, label)?;
+        } else {
+            self.import(session, id, label)?;
+        }
+        Ok(result)
+    }
+}
---
+++ sw/host/hsmtool/src/commands/mldsa/verify.rs
@@ -0,0 +1,74 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use anyhow::{Result, anyhow};
+use cryptoki::object::Attribute;
+use cryptoki::session::Session;
+use serde::{Deserialize, Serialize};
+use std::any::Any;
+use std::fs;
+use std::ops::Range;
+use std::path::PathBuf;
+
+use crate::commands::{BasicResult, Dispatch};
+use crate::error::HsmError;
+use crate::module::Module;
+use crate::util::attribute::KeyType;
+use crate::util::helper;
+use crate::util::signing::SignData;
+
+#[derive(clap::Args, Debug, Serialize, Deserialize)]
+pub struct Verify {
+    #[arg(long)]
+    id: Option<String>,
+    #[arg(short, long)]
+    label: Option<String>,
+    #[arg(short, long, default_value = "sha256-hash", help=SignData::HELP)]
+    format: SignData,
+    /// Reverse the result (for little-endian targets).
+    #[arg(short = 'r', long)]
+    little_endian: bool,
+    /// The signature is at the given byte range of the input file.
+    #[arg(short, long, value_parser=helper::parse_range, conflicts_with="signature")]
+    signature_at: Option<Range<usize>>,
+    input: PathBuf,
+    #[arg(conflicts_with = "signature_at")]
+    signature: Option<PathBuf>,
+}
+
+#[typetag::serde(name = "mldsa-verify")]
+impl Dispatch for Verify {
+    fn run(
+        &self,
+        _context: &dyn Any,
+        _hsm: &Module,
+        session: Option<&Session>,
+    ) -> Result<Box<dyn erased_serde::Serialize>> {
+        let session = session.ok_or(HsmError::SessionRequired)?;
+        let mut attrs = helper::search_spec(self.id.as_deref(), self.label.as_deref())?;
+        attrs.push(Attribute::KeyType(KeyType::MlDsa.try_into()?));
+        attrs.push(Attribute::Verify(true));
+        let object = helper::find_one_object(session, &attrs)?;
+
+        let data = fs::read(&self.input)?;
+        let data = self.format.prepare(KeyType::MlDsa, &data)?;
+        let mechanism = self.format.mechanism(KeyType::MlDsa)?;
+        let mut signature = if let Some(filename) = &self.signature {
+            fs::read(filename)?
+        } else if let Some(range) = &self.signature_at {
+            let input = fs::read(&self.input)?;
+            input
+                .get(range.clone())
+                .ok_or_else(|| anyhow!("Invalid range on input file: {range:?}"))?
+                .to_vec()
+        } else {
+            unreachable!();
+        };
+        if self.little_endian {
+            signature.reverse();
+        }
+        session.verify(&mechanism, object, &data, &signature)?;
+        Ok(Box::<BasicResult>::default())
+    }
+}
