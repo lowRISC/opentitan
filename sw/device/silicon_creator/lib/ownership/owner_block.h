2e55ad99e93a660b7ba5c8374bf4569de0a33069
Yi-Hsuan Deng <yhdeng@google.com>
2026-02-02 15:28:58 +0800

    [ownership] Use empty config in parser instead of default config
    The `owner_block_parse` function should start with an empty configuration
    rather than the default configuration due to its duplication item checks.
    
    This change introduces `owner_config_empty` which initializes the
    `owner_config_t` struct with safe, bogus values, and makes the weak
    `owner_config_default` call it to maintain consistent initialization.
    
    Change-Id: I7ff281331c53760a0fbc28173b15af8960627b07
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

--- sw/device/silicon_creator/lib/ownership/owner_block.h
+++ sw/device/silicon_creator/lib/ownership/owner_block.h
@@ -87,13 +87,20 @@ hardened_bool_t owner_block_newversion_mode(void);
 hardened_bool_t owner_block_page1_valid_for_transfer(boot_data_t *bootdata);
 
 /**
- * Initialize the owner config with default values.
+ * Clear all settings in the owner config.
  *
  * The sram_exec mode is set to DisabledLocked and the three configuration
  * pointers are set to kHardenedBoolFalse.
  *
- * The default implementation is weak and can be overridden for SKU-specific
- * defaults.
+ * @param config A pointer to a config struct holding pointers to config items.
+ */
+void owner_config_clear(owner_config_t *config);
+
+/**
+ * Initialize the owner config with default fallback values.
+ *
+ * The default implementation calls `owner_config_clear`, and is weak so it can
+ * be overridden for SKU-specific defaults.
  *
  * @param config A pointer to a config struct holding pointers to config items.
  */
