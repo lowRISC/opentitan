c5c5db5630b2c8542a83438aac5b431156ffdd61
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-02-15 17:41:13 +0000

    [lc_ctrl,assert] Express LcInitDoneSticky_A more comfortably
    This is equivalent, but rather easier to read (and avoids generating a
    pointless coverpoint).
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/lc_ctrl/rtl/lc_ctrl.sv
+++ hw/ip/lc_ctrl/rtl/lc_ctrl.sv
@@ -876,9 +876,7 @@ module lc_ctrl
   `ASSERT_KNOWN(HwRevKnown_A,           hw_rev_o                   )
 
   `ASSERT(LcInitDoneSticky_A,
-      lc_tx_test_true_strict(lc_init_done_o)
-      |=>
-      ##1 !$fell(lc_tx_test_true_strict(lc_init_done_o)))
+          ##1 !$fell(lc_tx_test_true_strict(lc_init_done_o)))
 
   // Alert assertions for sparse FSMs.
   `ASSERT_PRIM_FSM_ERROR_TRIGGER_ALERT(CtrlLcFsmCheck_A,
