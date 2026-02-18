ef07088f5bf5495c496f1446fc7b9a6ac9488db5
Andrea Caforio <andrea.caforio@lowrisc.org>
2026-02-18 15:19:08 +0100

    [crypto] ML-DSA-87: forward number-theoretic transform
    This is an implementation of the forward number-theoretic transform
    over the polynomial ring Z_q[X] / (X^256 + 1) using the 512-th root of
    unity 1753.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>

---
+++ sw/otbn/crypto/mldsa87/tests/mldsa87_ntt_test.s
@@ -0,0 +1,55 @@
+/* Copyright lowRISC contributors (OpenTitan project). */
+/* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+/* Test that the forward NTT is correct. */
+
+.section .text.start
+
+main:
+  /* Setup stack pointer and all-zero WDR. */
+  la x31, _stack
+  bn.xor w31, w31, w31
+
+  /* Load the parameters into the MOD WSR. */
+  la x2, _params
+  bn.lid x0, 0(x2)
+  bn.wsrw MOD, w0
+
+  /* Out-of-place NTT. */
+  la x2, _ntt_x
+  la x3, _ntt_y
+  jal x1, ntt
+
+  /* In-place NTT. */
+  la x2, _ntt_x
+  la x3, _ntt_x
+  jal x1, ntt
+
+  ecall
+
+.data
+.balign 32
+
+/*
+ * q  = 8380417 = 2^23 - 2^13 + 1 (ML-DSA modulus)
+ * mu = -q^-1 mod R (Montgomery constant)
+ * f  = 256^-1 * R mod q (INTT divisor in Montgomery domain)
+ */
+_params:
+.word 0x007fe001 /* q */
+.word 0xfc7fdfff /* mu */
+.word 0x00003ffe /* f */
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+
+_ntt_x:
+.zero 1024
+_ntt_y:
+.zero 1024
+
+_stack:
+.zero 4
