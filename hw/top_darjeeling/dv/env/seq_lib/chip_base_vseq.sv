ff1fd3e8b2c8e70868b96ef0af30d961f0ea11ee
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-02-28 14:06:34 +0000

    [chip,dv] Move do_creator_sw_cfg_ast_cfg out of chip_env_cfg
    This flag controls the behaviour of the vseq (when configuring the
    environment). As such, it doesn't really belong in the environment
    config.
    
    Move it to the vseq that uses it, and move the $value$plusargs call to
    chip_base_test (we shouldn't be adding *more* calls to plusargs in the
    sequence itself).
    
    One extra change is that the darjeeling version of the base vseq now
    sets a valid mubi4 value either way. This was implemented for earlgrey
    in commit 051b3a54d54, but it appears the snapshot for integration-dev
    was taken before that.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/top_darjeeling/dv/env/seq_lib/chip_base_vseq.sv
+++ hw/top_darjeeling/dv/env/seq_lib/chip_base_vseq.sv
@@ -28,6 +28,9 @@ class chip_base_vseq #(
   // You have to set this knob before or within dut_init task
   bit early_cpu_init = 0;
 
+  // Should the AST actually be programmed?
+  bit do_creator_sw_cfg_ast_cfg = 1;
+
   `uvm_object_new
 
   virtual function void set_handles();
@@ -326,11 +329,9 @@ class chip_base_vseq #(
 
   // Initialize the OTP creator SW cfg region with AST configuration data.
   virtual function void initialize_otp_creator_sw_cfg_ast_cfg();
-    // The knob controls whether the AST is actually programmed.
-    if (cfg.do_creator_sw_cfg_ast_cfg) begin
-      cfg.mem_bkdr_util_h[Otp].write32(otp_ctrl_reg_pkg::CreatorSwCfgAstInitEnOffset,
-                                       prim_mubi_pkg::MuBi4True);
-    end
+    cfg.mem_bkdr_util_h[Otp].write32(otp_ctrl_reg_pkg::CreatorSwCfgAstInitEnOffset,
+                                     (do_creator_sw_cfg_ast_cfg ?
+                                      prim_mubi_pkg::MuBi4True : prim_mubi_pkg::MuBi4False));
 
     // Ensure that the allocated size of the AST cfg region in OTP is equal to the number of AST
     // registers to be programmed.
