9e09e7c0dfb4ee45cc0511328a21f8933d1c6528
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-03-01 14:40:11 +0000

    [dv] Make subclasses' versions of predict_tl_err protected
    It turns out that Xcelium spits out a warning message if you don't:
    apparently I should have been a little more careful when I made the
    function protected in ff023656fdf.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rv_dm/dv/env/rv_dm_scoreboard.sv
+++ hw/ip/rv_dm/dv/env/rv_dm_scoreboard.sv
@@ -434,7 +434,7 @@ class rv_dm_scoreboard extends cip_base_scoreboard #(
   // enabled, any TL request will respond with an error. This is what we expect, but the code in
   // cip_base_scoreboard fails because we're responding with an error to an access that would
   // otherwise be perfectly reasonable.
-  function bit predict_tl_err(tl_seq_item item, tl_channels_e channel, string ral_name);
+  protected function bit predict_tl_err(tl_seq_item item, tl_channels_e channel, string ral_name);
     if (!cfg.tl_err_prediction) begin
       return item.d_error;
     end
