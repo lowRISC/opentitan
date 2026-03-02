54324e1b002f6d2526da5caa0f2c9e6eb5ebe81d
Yi-Hsuan Deng <yhdeng@google.com>
2026-03-02 21:37:17 +0800

    [cov,cryptotest] Add Quit command to firmware and host tests
    This change adds a 'Quit' command to the cryptotest command set and
    updates the firmware and host test harnesses to use it. This allows
    host tests to signal to the device that they are finished, ensuring the
    device shutdown gracefully, which is essential for transferring coverage
    reports back to the host.
    
    Change-Id: I89a7097d649157e35c791dbd04053415cb84fe88
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>

--- sw/host/tests/crypto/aes_gcm_nist_kat/src/main.rs
+++ sw/host/tests/crypto/aes_gcm_nist_kat/src/main.rs
@@ -157,6 +157,8 @@ fn test_aes_gcm(opts: &Opts, transport: &TransportWrapper) -> Result<()> {
             run_aes_gcm_testcase(aes_gcm_test, opts, &spi_console_device)?;
         }
     }
+    CryptotestCommand::Quit.send(&spi_console_device)?;
+    let _ = UartConsole::wait_for(&spi_console_device, r"PASS!|FAIL!", opts.timeout * 10)?;
     Ok(())
 }
 
