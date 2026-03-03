8f8fdb18fcd6da2af30677fc48323e04d4456cc3
Amaury Pouly <amaury.pouly@opentitan.org>
2026-03-03 18:40:27 +0100

    WIP

--- ci/scripts/schedule_fpga_jobs.py
+++ ci/scripts/schedule_fpga_jobs.py
@@ -48,7 +48,7 @@ class TestDatabase:
         the `run_in_ci` tag, all the other tests which are not marked as `run_in_ci`
         will be removed from the suite.
         """
-        selected_tests = set()
+        selected_tests = []
 
         for (suite, tests) in self.tests_by_suite.items():
             discard_non_run_in_ci = False
@@ -56,7 +56,7 @@ class TestDatabase:
                 # Once we have schedule a non-run_in_ci job in the suite,
                 # others non-run_in_ci are not elligible anymore.
                 if ('run_in_ci' in test["tags"] or not discard_non_run_in_ci) and filter_fn(test):
-                    selected_tests.add(test["label"])
+                    selected_tests.append(test)
                     if self.explain:
                         print("{} was selected".format(test["label"]))
                     if 'run_in_ci' not in test["tags"]:
@@ -66,9 +66,10 @@ class TestDatabase:
                         discard_non_run_in_ci = True
             # Update the test suite:
             new_tests = []
+            selected_labels = set([t["label"] for t in selected_tests])
             for test in tests:
                 # Do not keep selected tests.
-                if test["label"] in selected_tests:
+                if test["label"] in selected_labels:
                     continue
                 # If asked to, do not keep non-run_in_ci tests.
                 if 'run_in_ci' in test["tags"] or not discard_non_run_in_ci:
@@ -79,7 +80,7 @@ class TestDatabase:
             tests.clear()
             tests.extend(new_tests)
 
-        return sorted(list(selected_tests))
+        return selected_tests
 
 FPGAS = {
     "cw340": {
@@ -110,6 +111,47 @@ JOBS = [
     {"name": "TestROM", "tags": ["cw340_test_rom"], "fpga": "cw340", "id": "test_rom"},
     {"name": "TestROM", "tags": ["cw310_test_rom"], "fpga": "cw310", "id": "test_rom"},
 ]
+
+# Average time it takes to run a test,
+# assuming that the bitstream is already loaded.
+AVERAGE_TEST_RUNTIME_SECS = 5
+# Time it takes to load a bitstream, keyed by fpga.
+BITSTREAM_LOAD_TIME_SECS = {
+    'cw310': 10,
+    'cw340': 20,
+}
+# Maximum job length in minutes.
+MAX_JOB_LENGTH_MIN = 30
+
+def estimated_test_runtime(fpga, test):
+    """
+    Return estimatd runtime in seconds.
+    """
+    # If the tag contains the `changes_otp` flag, then the bitstream will have to be cleared
+    # at the end, so we count this in the running time of this test.
+    load_time = BITSTREAM_LOAD_TIME_SECS[fpga] if 'changes_otp' in test['tags'] else 0
+    return AVERAGE_TEST_RUNTIME_SECS + load_time
+
+
+def split_job(fpga, tests, max_job_length_min):
+    jobs = []
+    cur_job = []
+    cur_job_time = 0
+    for test in tests:
+        test_time = estimated_test_runtime(fpga, test)
+        # If we go above the limit, commit current job and start a new one.
+        if cur_job_time + test_time > max_job_length_min * 60:
+            jobs.append(cur_job)
+            cur_job = []
+            cur_job_time = 0
+        # Add test to current job
+        cur_job.append(test)
+        cur_job_time += test_time
+    # Commit job if non-empty
+    if cur_job:
+        jobs.append(cur_job)
+    return jobs
+
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -154,17 +196,22 @@ def main():
     jobs = []
     for job in JOBS:
         fpga = FPGAS[job["fpga"]]
-        id = "{}_{}".format(job["fpga"], job["id"])
         tests = test_db.schedule_tests(lambda test: all(tag in test["tags"] for tag in job["tags"]))
-        (args.out_dir / f"{id}.txt").write_text("\n".join(tests))
-        jobs.append({
-            "name": "{} {} Tests".format(fpga["human_name"], job["name"]),
-            "board": fpga["ci_board"],
-            "bitstream": fpga["ci_bitstream"],
-            "interface": fpga["ci_interface"],
-            "id": id,
-            "target_file": f"{id}.txt",
-        })
+        tests = split_job(job["fpga"], tests, MAX_JOB_LENGTH_MIN)
+        for (part, job_tests) in enumerate(tests, start=1):
+            suffix = "" if len(tests) == 1 else f"_{part}"
+            id = "{}_{}{}".format(job["fpga"], job["id"], suffix)
+            human_suffix = "" if len(tests) == 1 else " ({} / {})".format(part, len(tests))
+            job_tests = [t["label"] for t in job_tests]
+            (args.out_dir / f"{id}.txt").write_text("\n".join(job_tests))
+            jobs.append({
+                "name": "{} {} Tests{}".format(fpga["human_name"], job["name"], human_suffix),
+                "board": fpga["ci_board"],
+                "bitstream": fpga["ci_bitstream"],
+                "interface": fpga["ci_interface"],
+                "id": id,
+                "target_file": f"{id}.txt",
+            })
     # Make sure that there are no FPGA jobs left
     rem_fpga_tests = test_db.schedule_tests(lambda test: "fpga" in test["tags"])
     if rem_fpga_tests:
