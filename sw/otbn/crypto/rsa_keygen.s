2c19600d03cf199fba51a3f0d932d85437f1b941
Andrea Caforio <andrea.caforio@lowrisc.org>
2026-03-04 13:07:26 +0100

    [crypto] RSA: Refactor the mod F4 routine
    1. Move the `relprime_f4_tests` routine to `gcd.s` and rename it
       `mod_f4`. Simply return the result of x mod F4 instead of
        performing a zero check.
    2. Turn the former x mod F4 portion, for x < 2^35, into a callable
       routine `mod_f4_35`.
    3. Perform the gcd(e, p - 1) and gcd(e, q - 1) checks on the masked
       values p - 1  and q - 1 during the derivation of the secret key.
    4. Replace the old hardcoded unit tests with a randomized suite for
       different parameter sets.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>
    (cherry picked from commit 5745527bd8461c4508a1b03d9be92d784c760c83)

--- sw/otbn/crypto/rsa_keygen.s
+++ sw/otbn/crypto/rsa_keygen.s
@@ -39,7 +39,6 @@
 .globl gen_d
 
 # Exposed for testing only.
-.globl relprime_f4_test
 .globl distance_test
 
 /**
@@ -136,12 +135,32 @@ gen_d:
   li x31, 2
   jal x1, bignum_mul
 
+  # Check that gcd(g * (p - 1), F4) == 1.
+  la x16, RR
+  addi x31, x30, 2
+  jal x1, mod_f4
+
+  bn.cmp w22, w31, FG0
+  csrrs x16, FG0, x0
+  andi x16, x16, 0x8
+  bne x16, x0, rsa_keygen
+
   # dmem[rsa_h] <= (q - 1) * h
   la  x10, rsa_q
   la  x11, rsa_h
   la  x12, work_buf
   li  x31, 2
   jal x1, bignum_mul
+
+  # Check that gcd(h * (q - 1), F4) == 1.
+  la x16, work_buf
+  addi x31, x30, 2
+  jal x1, mod_f4
+
+  bn.cmp w22, w31, FG0
+  csrrs x16, FG0, x0
+  andi x16, x16, 0x8
+  bne x16, x0, rsa_keygen
 
   # Compute gcd(g * (p - 1), h * (q - 1)).
   # dmem[RR] <= 0.
@@ -321,21 +340,6 @@ _gen_p_body:
   la x16, rsa_n
   jal x1, gen_prime_candidate
 
-  # Temporarily calculate p - 1 for the relprime F4 test.
-  la      x16, rsa_n
-  li      x8, 2
-  bn.lid  x8, 0(x16)
-  bn.subi w2, w2, 1
-  bn.sid  x8, 0(x16)
-
-  jal x1, relprime_f4_test
-  beq x2, x0, _gen_p_loop
-
-  # Restore p after successful relprime F4 test.
-  bn.lid  x8, 0(x16)
-  bn.addi w2, w2, 1
-  bn.sid  x8, 0(x16)
-
   jal x1, fermat_test
   beq x2, x0, _gen_p_loop
 
@@ -402,21 +406,6 @@ _gen_q_body_inner:
   # Distance test
   jal x1, distance_test
   beq x2, x0, _gen_q_body_inner
-
-  # Temporarily calculate q - 1 for the relprime F4 test.
-  la      x16, rsa_n
-  li      x8, 2
-  bn.lid  x8, 0(x16)
-  bn.subi w2, w2, 1
-  bn.sid  x8, 0(x16)
-
-  jal x1, relprime_f4_test
-  beq x2, x0, _gen_q_loop
-
-  # Restore p after successful relprime F4 test.
-  bn.lid  x8, 0(x16)
-  bn.addi w2, w2, 1
-  bn.sid  x8, 0(x16)
 
   jal x1, fermat_test
   beq x2, x0, _gen_q_loop
@@ -553,180 +542,6 @@ distance_test:
   andi  x2, x2, 8
   srli  x2, x2, 3
   xori  x2, x2, 1
-  ret
-
-/**
- * Check if a large number is relatively prime to 65537 (aka F4).
- *
- * Returns a nonzero value if GCD(x,65537) == 1, and 0 otherwise
- *
- * A naive implementation would simply check if GCD(x, F4) == 1, However, we
- * can simplify the check for relative primality using a few helpful facts
- * about F4 specifically:
- *   1. It is prime.
- *   2. It has the special form (2^16 + 1).
- *
- * Because F4 is prime, checking if a number x is relatively prime to F4 means
- * simply checking if x is a direct multiple of F4; if (x % F4) != 0, then x is
- * relatively prime to F4. This means that instead of computing GCD, we can use
- * basic modular arithmetic.
- *
- * Here, the special form of F4, fact (2), comes in handy. Since 2^32 mod F4 =
- * 1, we can use `fold_bignum` to bring the number down to 35 bits cheaply.
- *
- * Since 2^16 is equivalent to -1 modulo F4, we can express the resulting
- * number in base-2^16 and simplify as follows:
- *   x = x0 + 2^16 * x1 + 2^32 * x2
- *   x \equiv x0 + (-1) * x1 + (-1)^2 * x2
- *   x \equiv x0 - x1 + x2 (mod F4)
- *
- * Flags: Flags have no meaning beyond the scope of this subroutine.
- *
- * @param[in]  x16: dptr_x, pointer to first limb of x in dmem
- * @param[in]  x30: n, number of 256-bit limbs for x
- * @param[in]  w31: all-zero
- * @param[out]  x2: result, 1 if x is relatively prime to F4 else 0
- *
- * clobbered registers: x2, w22, w23
- * clobbered flag groups: FG0
- */
-relprime_f4_test:
-  # Load F4 into the modulus register for later.
-  # MOD <= 2^16 + 1
-  bn.addi w22, w31, 1
-  bn.add  w22, w22, w22 << 16
-  bn.wsrw MOD, w22
-
-  # Generate a 256-bit mask.
-  # w24 <= 2^256 - 1
-  bn.not w24, w31
-
-  # Fold the bignum to get a 35-bit number r such that r mod F4 = x mod F4.
-  # w23 <= r
-  jal x1, fold_bignum
-
-  # Isolate the lower 16 bits of the 35-bit working sum.
-  # w22 <= w23[15:0]
-  bn.and w22, w23, w24 >> 240
-
-  # Add the lower 16 bits of the sum to the highest 3 bits to get a 17-bit
-  # result.
-  # w22 <= w22 + (w23 >> 32)
-  bn.add w22, w22, w23 >> 32
-
-  # The sum from the previous addition is at most 2^16 - 1 + 2^3 - 1 < 2 * F4,
-  # so a modular addition with zero is sufficient to fully reduce.
-  # w22 <= w22 mod F4
-  bn.addm w22, w22, w31
-
-  # Isolate the subtraction term.
-  # w23 <= w23[31:16]
-  bn.rshi w23, w23, w31 >> 32
-  bn.rshi w23, w31, w23 >> 240
-
-  # Final subtraction modulo F4.
-  # w22 <= (w22 - w23) mod F4 = x mod F4
-  bn.subm w22, w22, w23
-
-  # Return x2 = 1 if w22 != 0.
-  bn.cmp w22, w31, FG0
-  csrrs  x2, FG0, x0
-  andi   x2, x2, 8
-  srli   x2, x2, 3
-  li     x3, 1
-  xor    x2, x2, x3
-
-  ret
-
-/**
- * Partially reduce a value modulo m such that 2^32 mod m == 1.
- *
- * Returns r such that r mod m = x mod m and r < 2^35.
- *
- * Can be used to speed up modular reduction on certain numbers, such as 3, 5,
- * 17, and 65537.
- *
- * Because we know 2^32 mod m is 1, it follows that in general 2^(32*k) for any
- * k are all 1 modulo m. This includes 2^256, so when we receive the input as
- * a bignum in 256-bit limbs, we can simply all the limbs together to get an
- * equivalent number modulo m:
- *  x = x[0] + 2^256 * x[1] + 2^512 * x[2] + ...
- *  x \equiv x[0] + x[1] + x[2] + ... (mod F4)
- *
- * From there, we can essentially use the same trick to bisect the number into
- * 128-bit, 64-bit, and 32-bit chunks and add these together to get an
- * equivalent number modulo m. This operation is visually sort of like folding
- * the number over itself repeatedly, which is where the function gets its
- * name.
- *
- * Flags: Flags have no meaning beyond the scope of this subroutine.
- *
- * @param[in]  x16: dptr_x, pointer to first limb of x in dmem
- * @param[in]  x30: plen, number of 256-bit limbs for x
- * @param[in]  w24: constant, 2^256 - 1
- * @param[in]  w31: all-zero
- * @param[out] w23: r, result
- *
- * clobbered registers: x2, w22, w23
- * clobbered flag groups: FG0
- */
-fold_bignum:
-  # Initialize constants for loop.
-  li x22, 22
-
-  # Copy input pointer.
-  addi x2, x16, 0
-
-  # Initialize sum to zero and clear FG0.C.
-  # w23 <= 0
-  # FG0.C <= 0
-  bn.addi w23, w31, 0
-
-  # Iterate through the limbs of x and add them together.
-  # Loop invariants for iteration i (i=0..n-1):
-  #   x2 = dptr_x + i*32
-  #   x22 = 22
-  #   (w23 + FG0.C) \equiv x[0] + x[1] + ... + x[i-1] (mod m)
-  #
-  loop x30, 2
-    # Load the next limb.
-    # w22 <= x[i]
-    bn.lid x22, 0(x2++)
-
-    # Accumulate the new limb, incorporating the carry bit from the previous
-    # round if there was one (this works because 2^256 \equiv 1 mod m).
-    # w23 <= (w23 + x[i] + FG0.C) mod 2^256
-    # FG0.C <= (w23 + x[i] + FG0.C) / 2^256
-    bn.addc w23, w23, w22
-
-  # Isolate the lower 128 bits of the sum.
-  # w22 <= w23[127:0]
-  bn.and w22, w23, w24 >> 128
-
-  # Add the two 128-bit halves of the sum, plus the carry from the last round
-  # of the sum computation. The sum is now up to 129 bits.
-  # w23 <= (w22 + (w23 >> 128) + FG0.C)
-  bn.addc w23, w22, w23 >> 128
-
-  # Isolate the lower 64 bits of the sum.
-  # w22 <= w23[63:0]
-  bn.and w22, w23, w24 >> 192
-
-  # Add the two halves of the sum (technically 64 and 65 bits). A carry was
-  # not possible in the previous addition since the value is too small. The
-  # value is now up to 66 bits.
-  # w23 <= (w22 + (w23 >> 64))
-  bn.add w23, w22, w23 >> 64
-
-  # Isolate the lower 32 bits of the sum.
-  # w22 <= w23[31:0]
-  bn.and w22, w23, w24 >> 224
-
-  # Add the two halves of the sum (technically 32 and 34 bits). A carry was
-  # not possible in the previous addition since the value is too small.
-  # w23 <= (w22 + (w23 >> 32))
-  bn.add w23, w22, w23 >> 32
-
   ret
 
 /**
