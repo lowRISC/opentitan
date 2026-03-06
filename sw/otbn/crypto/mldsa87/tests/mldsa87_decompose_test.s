c3562e547ee9d820bbbfc259c3c4b111a6844483
Andrea Caforio <andrea.caforio@lowrisc.org>
2026-03-06 17:10:08 +0100

    [crypto] ML-DSA-87: Polynomial decomposition
    The decomposition step is a key operation in both sign and verify as
    the decomposition of the commitment vector yields the challenge
    polynomial.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>

---
+++ sw/otbn/crypto/mldsa87/tests/mldsa87_decompose_test.s
@@ -0,0 +1,45 @@
+/* Copyright lowRISC contributors (OpenTitan project). */
+/* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+/* Test that a polynomial can be correctly decomposed. */
+
+.section .text.start
+
+main:
+  la x31, _stack
+  bn.xor w31, w31, w31
+
+  la x2, _params
+  bn.lid x0, 0(x2)
+  bn.wsrw MOD, w0
+
+  la x2, _decompose_w
+  la x3, _decompose_w0
+  la x4, _decompose_w1
+  jal x1, decompose
+
+  ecall
+
+.data
+.balign 32
+
+_decompose_w:
+.zero 1024
+_decompose_w0:
+.zero 1024
+_decompose_w1:
+.zero 1024
+
+_params:
+.word 0x007fe001 /* q */
+.word 0xfc7fdfff /* mu */
+.word 0x0000a3fa /* n^-1 * R^3 mod q */
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+
+_stack:
+.zero 4
