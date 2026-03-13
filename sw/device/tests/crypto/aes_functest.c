ac167425ddec1b5127f8620f51ae4c0593823418
Siemen Dhooghe <sdhooghe@google.com>
2026-03-13 13:46:38 +0100

    [crypto] Propagate OTCRYPTO_MAKE_BUF to otcrypto_byte_buf_t
    Use the OTCRYPTO_MAKE_BUF macro to every instantiation of a
    otcrypto_byte_buf_t.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>

--- sw/device/tests/crypto/aes_functest.c
+++ sw/device/tests/crypto/aes_functest.c
@@ -207,9 +207,10 @@ static status_t run_decrypt(const aes_test_t *test, bool streaming) {
   }
 
   // Decrypt the remaining input in one shot.
-  otcrypto_const_byte_buf_t ciphertext_buf = {.data = ciphertext, .len = len};
-  otcrypto_byte_buf_t recovered_plaintext_buf = {.data = recovered_plaintext,
-                                                 .len = len};
+  otcrypto_const_byte_buf_t ciphertext_buf =
+      OTCRYPTO_MAKE_BUF(otcrypto_const_byte_buf_t, ciphertext, len);
+  otcrypto_byte_buf_t recovered_plaintext_buf =
+      OTCRYPTO_MAKE_BUF(otcrypto_byte_buf_t, recovered_plaintext, len);
   TRY(otcrypto_aes(&key, iv, test->mode, kOtcryptoAesOperationDecrypt,
                    ciphertext_buf, test->padding, recovered_plaintext_buf));
 
