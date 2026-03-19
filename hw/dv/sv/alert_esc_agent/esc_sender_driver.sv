003d9f5ab7bd0524f2d6e6fa7d6fd4203ce8ab23
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-03-19 12:30:53 +0000

    [alert,dv] Avoid escalation drivers extending alert_esc_base_driver
    The two types of interface are pretty unrelated and the existing code
    had some strange corners. Make things more explicit and change
    esc_receiver_driver and esc_sender_driver to derive directly from
    dv_base_driver instead of alert_esc_base_driver.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/dv/sv/alert_esc_agent/esc_sender_driver.sv
+++ hw/dv/sv/alert_esc_agent/esc_sender_driver.sv
@@ -1,22 +1,17 @@
 // Copyright lowRISC contributors (OpenTitan project).
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
-//
 
-// ---------------------------------------------
-// Esc sender driver
-// ---------------------------------------------
-class esc_sender_driver extends alert_esc_base_driver;
+// Escalation sender driver
 
+class esc_sender_driver extends dv_base_driver#(alert_esc_seq_item, alert_esc_agent_cfg);
   `uvm_component_utils(esc_sender_driver)
-
 
   extern function new (string name, uvm_component parent);
   extern virtual task reset_signals();
   extern virtual task get_and_drive();
-  extern virtual task drive_esc();
-  extern virtual task do_reset();
 
+  extern local task do_reset();
 endclass : esc_sender_driver
 
 function esc_sender_driver::new (string name, uvm_component parent);
@@ -24,13 +19,13 @@ function esc_sender_driver::new (string name, uvm_component parent);
 endfunction : new
 
 task esc_sender_driver::reset_signals();
-  do_reset();
   forever begin
-    @(negedge cfg.vif.rst_n);
     under_reset = 1;
     do_reset();
     @(posedge cfg.vif.rst_n);
     under_reset = 0;
+
+    @(negedge cfg.vif.rst_n);
   end
 endtask : reset_signals
 
@@ -38,15 +33,8 @@ task esc_sender_driver::get_and_drive();
   // LC_CTRL uses virtual interface to directly drive escalation requests.
   // Other escalation handshakes are checked in prim_esc direct sequence.
   // So the following task is not implemented.
-  drive_esc();
-endtask : get_and_drive
-
-task esc_sender_driver::drive_esc();
-  // LC_CTRL uses virtual interface to directly drive escalation requests.
-  // Other escalation handshakes are checked in prim_esc direct sequence.
-  // So this task is not implemented.
   wait(!under_reset);
-endtask : drive_esc
+endtask : get_and_drive
 
 task esc_sender_driver::do_reset();
   cfg.vif.esc_tx_int.esc_p <= 1'b0;
