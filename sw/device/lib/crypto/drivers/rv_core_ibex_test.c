7fdb4335dee188332ea3e9b1d503d07de24b3258
Siemen Dhooghe <sdhooghe@google.com>
2026-03-22 12:58:00 +0100

    [crypto/test] Add negative tests for rv_core_ibex_test
    For coverage add negative tests for the rv_core_ibex and test the icache
    and security config functions.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>

--- sw/device/lib/crypto/drivers/rv_core_ibex_test.c
+++ sw/device/lib/crypto/drivers/rv_core_ibex_test.c
@@ -4,6 +4,7 @@
 
 #include "sw/device/lib/crypto/drivers/rv_core_ibex.h"
 
+#include "sw/device/lib/base/csr.h"
 #include "sw/device/lib/base/status.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/test_framework/check.h"
@@ -19,8 +20,7 @@ enum {
 OTTF_DEFINE_TEST_CONFIG();
 
 static status_t ibex_entropy_test(void) {
-  uint32_t rnd_status;
-  uint32_t rnd_data[2];
+  LOG_INFO("Running IBEX entropy test.");
 
   // Read the initial value of the RND_DATA CSR.
   uint32_t rnd_data0 = ibex_rnd32_read();
@@ -31,8 +31,74 @@ static status_t ibex_entropy_test(void) {
   return OK_STATUS();
 }
 
+static status_t ibex_security_config_test(void) {
+  LOG_INFO("Running IBEX security config test.");
+
+  uint32_t original_csr;
+  CSR_READ(CSR_REG_CPUCTRL, &original_csr);
+  CSR_SET_BITS(CSR_REG_CPUCTRL, 0x6);
+  uint32_t current_csr;
+  CSR_READ(CSR_REG_CPUCTRL, &current_csr);
+
+  if (((current_csr >> 1) & 0x3) == 0x3) {
+    // Check the positive path
+    TRY_CHECK(ibex_check_security_config() == kHardenedBoolTrue);
+
+    // Check the negative path
+    CSR_CLEAR_BITS(CSR_REG_CPUCTRL, 0x2);
+    TRY_CHECK(ibex_check_security_config() == kHardenedBoolFalse);
+  } else {
+    // Only check the negative path
+    TRY_CHECK(ibex_check_security_config() == kHardenedBoolFalse);
+  }
+
+  if (original_csr & 0x2) {
+    CSR_SET_BITS(CSR_REG_CPUCTRL, 0x2);
+  } else {
+    CSR_CLEAR_BITS(CSR_REG_CPUCTRL, 0x2);
+  }
+
+  if (original_csr & 0x4) {
+    CSR_SET_BITS(CSR_REG_CPUCTRL, 0x4);
+  } else {
+    CSR_CLEAR_BITS(CSR_REG_CPUCTRL, 0x4);
+  }
+
+  return OK_STATUS();
+}
+
+static status_t ibex_icache_test(void) {
+  LOG_INFO("Running IBEX iCache disable/restore test.");
+
+  hardened_bool_t icache_state_1;
+  hardened_bool_t icache_state_2;
+
+  // Test ibex_disable_icache where the icache was enabled
+  CSR_SET_BITS(CSR_REG_CPUCTRL, 1);
+  TRY(ibex_disable_icache(&icache_state_1));
+  TRY_CHECK(icache_state_1 == kHardenedBoolTrue);
+
+  // Test ibex_disable_icache where the icache was disabled
+  TRY(ibex_disable_icache(&icache_state_2));
+  TRY_CHECK(icache_state_2 == kHardenedBoolFalse);
+
+  // Test the restore
+  ibex_restore_icache(icache_state_2);
+  uint32_t csr;
+  CSR_READ(CSR_REG_CPUCTRL, &csr);
+  TRY_CHECK((csr & 1) == 0);
+  ibex_restore_icache(icache_state_1);
+  CSR_READ(CSR_REG_CPUCTRL, &csr);
+  TRY_CHECK((csr & 1) == 1);
+
+  return OK_STATUS();
+}
+
 bool test_main(void) {
   status_t result = OK_STATUS();
   EXECUTE_TEST(result, ibex_entropy_test);
+  EXECUTE_TEST(result, ibex_security_config_test);
+  EXECUTE_TEST(result, ibex_icache_test);
+
   return status_ok(result);
 }
