c0c44d352b45255f721a2906d04ef48c17ed45b6
Siemen Dhooghe <sdhooghe@google.com>
2026-03-29 20:06:04 +0200

    [crypto/alert] Add functions to read/init alerts
    Add alert drivers to read and init the sensors and alerts.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>

---
+++ sw/device/lib/crypto/drivers/alert.c
@@ -0,0 +1,85 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/crypto/drivers/alert.h"
+
+#include "sw/device/lib/base/abs_mmio.h"
+#include "sw/device/lib/base/bitfield.h"
+#include "sw/device/lib/base/hardened.h"
+#include "sw/device/lib/base/macros.h"
+#include "sw/device/lib/crypto/impl/status.h"
+
+#include "alert_handler_regs.h"  // Generated
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+#include "sensor_ctrl_regs.h"  // Generated.
+
+uint32_t read_alert_registers(void) {
+  // Read the sensors
+  uint32_t sensors_recov_alerts =
+      abs_mmio_read32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
+                      SENSOR_CTRL_RECOV_ALERT_REG_OFFSET);
+  uint32_t sensors_fatal_alerts =
+      abs_mmio_read32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
+                      SENSOR_CTRL_FATAL_ALERT_REG_OFFSET);
+  uint32_t combined_alerts = sensors_recov_alerts | sensors_fatal_alerts;
+
+  // Read out the alerts
+  for (uint32_t alert = 0; alert < ALERT_HANDLER_PARAM_N_ALERTS; alert++) {
+    uint32_t cause = abs_mmio_read32(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
+                                     (alert * sizeof(uint32_t)) +
+                                     ALERT_HANDLER_ALERT_CAUSE_0_REG_OFFSET);
+    combined_alerts |= cause;
+  }
+  // Read out the local alerts
+  for (uint32_t local_alert = 0; local_alert < ALERT_HANDLER_PARAM_N_LOC_ALERT;
+       local_alert++) {
+    uint32_t local_cause =
+        abs_mmio_read32(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
+                        (local_alert * sizeof(uint32_t)) +
+                        ALERT_HANDLER_LOC_ALERT_CAUSE_0_REG_OFFSET);
+    combined_alerts |= local_cause;
+  }
+  return combined_alerts;
+}
+
+status_t init_alert_registers(void) {
+  // Clear the sensors.
+  abs_mmio_write32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
+                       SENSOR_CTRL_RECOV_ALERT_REG_OFFSET,
+                   SENSOR_CTRL_RECOV_ALERT_REG_RESVAL);
+  abs_mmio_write32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
+                       SENSOR_CTRL_FATAL_ALERT_REG_OFFSET,
+                   SENSOR_CTRL_FATAL_ALERT_REG_RESVAL);
+  abs_mmio_write32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
+                       SENSOR_CTRL_ALERT_TEST_REG_OFFSET,
+                   SENSOR_CTRL_ALERT_TEST_REG_RESVAL);
+  abs_mmio_write32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
+                       SENSOR_CTRL_ALERT_TRIG_REG_OFFSET,
+                   SENSOR_CTRL_ALERT_TRIG_REG_RESVAL);
+  abs_mmio_write32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
+                       SENSOR_CTRL_INTR_STATE_REG_OFFSET,
+                   SENSOR_CTRL_INTR_STATE_REG_RESVAL);
+
+  // Clear the alerts.
+  for (uint32_t alert = 0; alert < ALERT_HANDLER_PARAM_N_ALERTS; alert++) {
+    abs_mmio_write32(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
+                         (alert * sizeof(uint32_t)) +
+                         ALERT_HANDLER_ALERT_CAUSE_0_REG_OFFSET,
+                     0x1);
+  }
+
+  // Clear the local alerts.
+  for (uint32_t local_alert = 0; local_alert < ALERT_HANDLER_PARAM_N_LOC_ALERT;
+       local_alert++) {
+    abs_mmio_write32(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
+                         (local_alert * sizeof(uint32_t)) +
+                         ALERT_HANDLER_LOC_ALERT_CAUSE_0_REG_OFFSET,
+                     0x1);
+  }
+
+  abs_mmio_write32(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
+                       ALERT_HANDLER_INTR_STATE_REG_OFFSET,
+                   0xffffffff);
+  return OTCRYPTO_OK;
+}
---
+++ sw/device/lib/crypto/drivers/alert.h
@@ -0,0 +1,29 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_LIB_CRYPTO_DRIVERS_ALERT_H_
+#define OPENTITAN_SW_DEVICE_LIB_CRYPTO_DRIVERS_ALERT_H_
+
+#include <stdint.h>
+
+#include "sw/device/lib/crypto/include/datatypes.h"
+
+/**
+ * Read the alert, the sensors, and the local alert registers.
+ *
+ * @return The OR of the alerts, sensors, and local alerts.
+ * Zero means no alerts were triggered.
+ */
+OT_WARN_UNUSED_RESULT
+uint32_t read_alert_registers(void);
+
+/**
+ * Clear the alert, the sensors, and the local alert registers.
+ *
+ * @return OK when the alerts are cleared.
+ */
+OT_WARN_UNUSED_RESULT
+status_t init_alert_registers(void);
+
+#endif  // OPENTITAN_SW_DEVICE_LIB_CRYPTO_DRIVERS_ALERT_H_
