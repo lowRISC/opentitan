381f42dff1125288ebb906c98fbc654168a6d14c
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-04-01 18:21:17 +0100

    [dv] Turn alert_esc_if.is_alert_handshaking into a cts assignment
    This doesn't make much difference to cip_base_seq, but it makes the
    wait in entropy_src_base_vseq a bit simpler: we can use the wait
    statement instead of a busy-loop.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/entropy_src/dv/env/seq_lib/entropy_src_base_vseq.sv
+++ hw/ip/entropy_src/dv/env/seq_lib/entropy_src_base_vseq.sv
@@ -287,9 +287,7 @@ class entropy_src_base_vseq extends cip_base_vseq #(
 
     `DV_SPINWAIT_EXIT(
       try_apply_base_configuration(newcfg, pause, completed);,
-      while (!cfg.m_alert_agent_cfgs["recov_alert"].vif.is_alert_handshaking()) begin
-         cfg.clk_rst_vif.wait_clks(1);
-      end
+      wait (cfg.m_alert_agent_cfgs["recov_alert"].vif.is_alert_handshaking);
       wait_no_outstanding_access();
     )
 
