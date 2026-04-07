2c459d8b15790e3d83ae83f5c3c3848293032f3a
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-04-07 19:09:53 +0100

    [sram_ctrl,dv] Massively shorten bijection vseq
    This sequence works by writing every word in memory and then reading
    it back again and by default it did so between 5 and 25 times. I guess
    the author intended to try out several different scrambling seeds, but
    there aren't really "corners" that we can get stuck in, so there's
    probably no need.
    
    Cut this down to just two scrambling seeds (and speed the test up by a
    factor of 7.5). Using two means that we'll still see any problems that
    appear after changing the seed.
    
    If we really wanted to see the same number of scrambling seeds, we
    could increase the reseed count, but I'm very unconvinced that this is
    necessary.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/sram_ctrl/dv/env/seq_lib/sram_ctrl_bijection_vseq.sv
+++ hw/ip/sram_ctrl/dv/env/seq_lib/sram_ctrl_bijection_vseq.sv
@@ -3,32 +3,40 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // This sequence tests the bijectivity of the SRAM address remapping.
-// To this, we iterate over every address in SRAM memory and write a unique value to it
-// (in this case we write the address to memory, e.g. write 0x0 to address 0x0,
-//  write 0x4 to address 0x4, etc...).
-// We then read out each address in memory and check that the read data is equal to the address
-// we read it from (this is done using the built-in checking capabilities of `tl_acess()` task.
 //
-// This helps us verify that the SRAM address scrambling does not lead to any address collisions,
-// e.g. addresses 0x4 and 0x8 do not both map to the same line in memory.
-// If there are any address collisions, then we will read out an incorrect value from memory.
+// To do this, use the following stimulus:
+//
+//  - Write a different value to every address in SRAM memory (using the address, so write 0x0 to
+//    address 0x0, write 0x4 to address 0x4, etc...).
+//
+//  - Read the value back from every address in SRAM memory. As well as any check from the
+//    scoreboard, the sequence knows the value it expects to read, so can check the value as well.
+//
+// The test helps us verify that the SRAM address scrambling does not lead to any address collisions
+// (e.g. addresses 0x4 and 0x8 do not both map to the same line in memory). If there are any address
+// collisions, then we will read out an incorrect value from memory.
+//
+// The sequence runs twice, requesting a new scrambling key between the two runs (to check that
+// nothing gets broken by switching scrambling key)
 class sram_ctrl_bijection_vseq extends sram_ctrl_smoke_vseq;
-
   `uvm_object_utils(sram_ctrl_bijection_vseq)
-  `uvm_object_new
 
-  constraint num_trans_c {
-    num_trans inside {[5 : 25]};
-  }
+  function new (string name="");
+    super.new(name);
+  endfunction
 
   virtual task body();
+    int unsigned sram_depth = cfg.sram_ctrl_bkdr_util_h.get_depth();
 
-    int sram_depth = cfg.sram_ctrl_bkdr_util_h.get_depth();
-
-    `uvm_info(`gfn, $sformatf("sram_depth: %0d", sram_depth), UVM_HIGH)
+    `uvm_info(get_name(),
+              $sformatf("Read-write bijection test with %0d iterations over SRAM with depth %0d.",
+                        num_trans, sram_depth),
+              UVM_LOW)
 
     for (int i = 0; i < num_trans; i++) begin
-      `uvm_info(`gfn, $sformatf("iteration: %0d", i), UVM_HIGH)
+      `uvm_info(get_name(),
+                $sformatf("Starting iteration %0d of %0d", i+1, num_trans),
+                UVM_LOW)
 
       // Write the corresponding address to each entry in the SRAM
       for (int j = 0; j < sram_depth; j++) begin
@@ -46,11 +54,13 @@ class sram_ctrl_bijection_vseq extends sram_ctrl_smoke_vseq;
 
       // request new scrambling key on all but the last iteration
       if (i != num_trans - 1) begin
+        `uvm_info(get_name(), "Rotating scrambling key", UVM_LOW)
         req_scr_key();
       end
-
     end
-
   endtask : body
 
+  // Only run two transactions: this will ensure we check a new scrambling key, but avoids spending
+  // too long in the (slow) sweep across the entire memory.
+  constraint num_trans_c { num_trans == 2; }
 endclass
