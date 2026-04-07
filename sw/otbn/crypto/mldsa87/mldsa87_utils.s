ae57d803bf5b5dd26939178787391526404c32de
Andrea Caforio <andrea.caforio@lowrisc.org>
2026-04-07 14:27:46 +0200

    [crypto] ML-DSA-87: Secure polynomial decomposition
    This commit includes the secure gadget to compute the polynomial
    decomposition as part of the ML-DSA sign.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>

--- sw/otbn/crypto/mldsa87/mldsa87_utils.s
+++ sw/otbn/crypto/mldsa87/mldsa87_utils.s
@@ -18,3 +18,43 @@ zeroize:
     bn.sid x22, 0(x20++)
     /* End of loop */
   ret
+
+/**
+ * Unmask n vectors of 8 Boolean-shared coefficients in-place.
+ *
+ * CAUTION: INSECURE! Only use for testing purposes.
+ *
+ * @param[in] x20: DMEM address of the first share.
+ * @param[in] x21: DMEM address of the second share.
+ * @param[in] x22: n, Number of 8-coefficients vectors to unmask.
+ */
+unmask_boolean:
+  addi x23, x0, 0
+  addi x24, x0, 1
+  loop x22, 4
+    bn.lid x23, 0(x20)
+    bn.lid x24, 0(x21++)
+    bn.xor w0, w0, w1
+    bn.sid x23, 0(x20++)
+    /* End of loop */
+  ret
+
+/**
+ * Unmask n vectors of 8 arithmetically shared coefficients in-place.
+ *
+ * CAUTION: INSECURE! Only use for testing purposes.
+ *
+ * @param[in] x20: DMEM address of the first share.
+ * @param[in] x21: DMEM address of the second share.
+ * @param[in] x22: n, Number of 8-coefficients vectors to unmask.
+ */
+unmask_arithmetic:
+  addi x23, x0, 0
+  addi x24, x0, 1
+  loop x22, 4
+    bn.lid x23, 0(x20)
+    bn.lid x24, 0(x21++)
+    bn.addvm.8s w0, w0, w1
+    bn.sid x23, 0(x20++)
+    /* End of loop */
+  ret
