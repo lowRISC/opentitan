2debde6d83b0548441da9412f2da2750387544b1
Siemen Dhooghe <sdhooghe@google.com>
2026-04-08 10:51:38 +0200

    [pentest] Connect trng init
    Connect the otcrypto_entropy_init function to the pentest framework.
    This function is currently only useful for fault injection testing.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>

--- sw/device/tests/penetrationtests/firmware/fi/cryptolib_fi_sym_impl.h
+++ sw/device/tests/penetrationtests/firmware/fi/cryptolib_fi_sym_impl.h
@@ -42,6 +42,17 @@ status_t cryptolib_fi_drbg_reseed_impl(
     cryptolib_fi_sym_drbg_reseed_out_t *uj_output);
 
 /**
+ * Wrapper to TRNG init cryptolib implementation.
+ *
+ * @param uj_input An initialized uJSON context.
+ * @param uj_output An initialized uJSON context.
+ * @return OK or error.
+ */
+status_t cryptolib_fi_trng_init_impl(
+    cryptolib_fi_sym_trng_init_in_t uj_input,
+    cryptolib_fi_sym_trng_init_out_t *uj_output);
+
+/**
  * Wrapper to AES-GCM cryptolib implementation.
  *
  * @param uj_input An initialized uJSON context.
