d83cdb04f2e3ff5031d90aa23fa9445ef8184ad5
martin-velay <mvelay@lowrisc.org>
2026-04-09 18:00:40 +0200

    [script] Remove double entry of exclusion
    - the line exists twice in this script, so remove one of them.
    
    Signed-off-by: martin-velay <mvelay@lowrisc.org>

--- hw/dv/tools/xcelium/common_cov_excl.tcl
+++ hw/dv/tools/xcelium/common_cov_excl.tcl
@@ -9,4 +9,3 @@ exclude -inst $::env(DUT_TOP) -toggle '*tl_o.d_sink' -comment "\[UNR\] Based on 
 exclude -inst $::env(DUT_TOP) -toggle '*tl_o.d_user.rsp_intg'\[6\] -comment "\[UNR\] Due to the ECC logics"
 exclude -inst $::env(DUT_TOP).gen_alert_tx*.u_prim_alert_sender -toggle '*alert*.ping_*' -comment "\[LOW_RISK\] Verified in prim_alert_receiver TB."
 exclude -inst $::env(DUT_TOP).*_chk.u_chk -toggle 'data_i[56:43]' -comment "\[UNR\] unused inputs at prim_secded_* in reg_top and mem."
-exclude -inst $::env(DUT_TOP).gen_alert_tx*.u_prim_alert_sender -toggle '*alert*.ping_*' -comment "\[LOW_RISK\] Verified in prim_alert_receiver TB."
