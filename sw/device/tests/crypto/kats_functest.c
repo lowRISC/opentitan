5063e4e0f78b8cdb2dd55664413521aeb5231716
Siemen Dhooghe <sdhooghe@google.com>
2026-04-11 20:18:17 +0200

    [crypto] Add kats
    Add a new file with the run_kats function. This is for FIPS
    certification to run health tests on the API calls of the library.
    Populate the function with several kats such as sha2, ecc, rsa, aes
    already.
    
    with thanks to Bastien Gillon <bgillon@google.com>.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>
    (cherry picked from commit 6ee47519e1c0589b1ed26f75fdb9143712ab58f6)

---
+++ sw/device/tests/crypto/kats_functest.c
@@ -0,0 +1,19 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/crypto/impl/kats.h"
+#include "sw/device/lib/crypto/include/entropy_src.h"
+#include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/testing/test_framework/check.h"
+#include "sw/device/lib/testing/test_framework/ottf_main.h"
+
+OTTF_DEFINE_TEST_CONFIG();
+
+bool test_main(void) {
+  CHECK_STATUS_OK(otcrypto_entropy_init());
+
+  CHECK_STATUS_OK(run_kats(OTCRYPTO_KAT_ALL_TESTS));
+
+  return true;
+}
