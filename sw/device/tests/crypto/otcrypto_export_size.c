a4497ad878b0f5fbd3faae6dc4cc6628077b5f96
Siemen Dhooghe <sdhooghe@google.com>
2026-04-13 23:39:49 +0200

    [crypto/tests] Add crt sections to export size
    Add the crt_section_copy and crt_section_clear to otcrypto_export_size
    such that it is possible to use the otcrypto_export_size as the baseline
    for coverage reporting.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>
    (cherry picked from commit 8755f094a9fdbc8fbbfd0a208f0b65620055e242)

--- sw/device/tests/crypto/otcrypto_export_size.c
+++ sw/device/tests/crypto/otcrypto_export_size.c
@@ -4,5 +4,11 @@
 
 #include "sw/device/tests/crypto/otcrypto_interface.h"
 
+// Only here for coverage reporting.
+// Satisfy bare_metal_start.S so the linker doesn't crash.
+// These don't need to actually initialize RAM.
+void crt_section_copy(void) {}
+void crt_section_clear(void) {}
+
 // Simple main function so the linker doesn't discard the inerface struct.
 void bare_metal_main(void) { (void)otcrypto; }
