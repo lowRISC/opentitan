0673d37bc1b621a7cffbe27eb719e853cf7010b3
Andrea Caforio <andrea.caforio@lowrisc.org>
2026-04-15 16:12:09 +0200

    [crypto] ML-DSA-87: Polynomial decoding of T0 polynomials
    T0 polynomials are a public part of the secret key and need to be
    decoded to the canonical polynomial representation.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>

---
+++ sw/otbn/crypto/mldsa87/sign/tests/mldsa87_sign_decode_t0_test.s
@@ -0,0 +1,42 @@
+/* Copyright lowRISC contributors (OpenTitan project). */
+/* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+/* Test that we can correctly decode a T0 polynomial. */
+
+.section .text.start
+
+main:
+  la x31, _stack
+  bn.xor w31, w31, w31
+
+  la x2, _params
+  bn.lid x0, 0(x2)
+  bn.wsrw MOD, w0
+
+  la x2, _decode_t0_t0_enc
+  la x3, _decode_t0_t0_dec
+  jal x1, decode_t0
+
+  ecall
+
+.data
+.balign 32
+
+_decode_t0_t0_enc:
+.zero 1024
+_decode_t0_t0_dec:
+.zero 1024
+
+_params:
+.word 0x007fe001 /* q */
+.word 0xfc7fdfff /* mu */
+.word 0x0000a3fa /* n^-1 * R^3 mod q */
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+
+_stack:
+.zero 256
