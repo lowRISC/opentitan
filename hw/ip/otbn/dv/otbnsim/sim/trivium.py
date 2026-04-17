66f0ed265d323633853d2f5f4260af5f2f9c401d
Pascal Etterli <pascal.etterli@lowrisc.org>
2026-04-17 15:56:18 +0200

    [otbnsim] Optimize the state update function of `trivium.py`
    Instead of modeling each bit separately and store all bits in a list, we model the Trivium/Bivium
    state with 3/2 regular integer values. This allows to perform multiple updates at once leading to
    better execution times.
    
    Co-authored-by: Thomas Benz <tbenz@lowrisc.org>
    Signed-off-by: Thomas Benz <tbenz@lowrisc.org>
    Signed-off-by: Pascal Etterli <pascal.etterli@lowrisc.org>

--- hw/ip/otbn/dv/otbnsim/sim/trivium.py
+++ hw/ip/otbn/dv/otbnsim/sim/trivium.py
@@ -4,7 +4,7 @@
 
 import math
 from enum import IntEnum
-from typing import List
+from typing import Optional, Tuple, Union
 
 
 class SeedType(IntEnum):
@@ -34,20 +34,6 @@ class CipherType(IntEnum):
     # primitives can be used to instantiate a PRNG.
     TRIVIUM = 0
     BIVIUM = 1
-
-
-def i2b(i: int, n: int) -> List[int]:
-    """Convert a little endian integer `i` to a `n`-bit array with the LSB at
-    idx 0. The resulting bit array is padded with 0s if log2(i) < `n` until its
-    size is `n` bits. Regardless of `n` there will always be at least
-    ceil(log2(i)) bits"""
-    return [int(b) for b in bin(i)[2:].zfill(n)][::-1]
-
-
-def b2i(b: List[int]) -> int:
-    """Convert a bit array `b` with the LSB at idx 0 to a little endian
-    integer."""
-    return int("".join(str(d) for d in b[::-1]), 2)
 
 
 class Trivium:
@@ -83,19 +69,17 @@ class Trivium:
     TRIVIUM_LAST_PART_SEED_SIZE = 32
     BIVIUM_LAST_PART_SEED_SIZE = 17
 
-    # Initial state after reset (see `prim_trivium_pkg.sv`).
-    TRIVIUM_INIT_SEED = i2b(
-        0x758A442031E1C4616EA343EC153282A30C132B5723C5A4CF4743B3C7C32D580F74F1713A,
-        TRIVIUM_STATE_SIZE,
-    )
-    BIVIUM_INIT_SEED = TRIVIUM_INIT_SEED[0:BIVIUM_STATE_SIZE]
+    # Initial state after reset as a little-endian integer (bit j = state[j]).
+    # See `prim_trivium_pkg.sv`.
+    TRIVIUM_INIT_SEED = 0x758A442031E1C4616EA343EC153282A30C132B5723C5A4CF4743B3C7C32D580F74F1713A
+    BIVIUM_INIT_SEED = TRIVIUM_INIT_SEED & ((1 << BIVIUM_STATE_SIZE) - 1)
 
     def __init__(
         self,
         cipher_type: CipherType,
         seed_type: SeedType,
         output_width: int,
-        init_seed: List[int] = [],
+        init_seed: Optional[int] = None,
     ):
         """The cipher is defined by its cipher type (see `CipherType`), its
         seed type (see `SeedType`), the output (keystream) width and an
@@ -103,49 +87,50 @@ class Trivium:
 
         if cipher_type == CipherType.TRIVIUM:
             self.state_size = self.TRIVIUM_STATE_SIZE
-            self.update_func = self._trivium_update
             self.last_part_seed_size = self.TRIVIUM_LAST_PART_SEED_SIZE
-            self.state = self.TRIVIUM_INIT_SEED[:]
-
         elif cipher_type == CipherType.BIVIUM:
             self.state_size = self.BIVIUM_STATE_SIZE
-            self.update_func = self._bivium_update
             self.last_part_seed_size = self.BIVIUM_LAST_PART_SEED_SIZE
-            self.state = self.BIVIUM_INIT_SEED[:]
-
         else:
-            raise ValueError("unknown cipher type: {cipher_type}")
+            raise ValueError(f"unknown cipher type: {cipher_type}")
 
-        if init_seed != []:
-            if len(init_seed) != self.state_size:
+        if init_seed is not None:
+            if not (0 <= init_seed < (1 << self.state_size)):
                 raise ValueError(
-                    f"{cipher_type.name} init seed must be the same size as the state"
+                    f"{cipher_type.name} init seed must fit in {self.state_size} bits"
                 )
-            self.state = init_seed
+            seed_int = init_seed
+        elif cipher_type == CipherType.TRIVIUM:
+            seed_int = self.TRIVIUM_INIT_SEED
+        else:
+            seed_int = self.BIVIUM_INIT_SEED
+
+        # Cipher state is a tuple of Python integers using reversed-bit storage
+        # (bit N-1-j of reg = state[j]). This allows updates to run as pure integer arithmetic.
+        self.state = self._int_to_regs(seed_int, cipher_type)
 
         # Depending on cipher and seed type, a different number of seed rounds
         # have to be run.
         if seed_type == SeedType.KEY_IV:
             self.seed_rounds = math.ceil(4 * self.state_size / output_width)
-            self.seed_counter = 0
         elif seed_type == SeedType.STATE_FULL:
             self.seed_rounds = 1
-            self.seed_counter = 0
         elif seed_type == SeedType.STATE_PARTIAL:
             self.seed_rounds = math.ceil(self.state_size / self.PART_SEED_SIZE)
-            self.seed_counter = 0
         else:
-            raise ValueError("unknown seed type: {seed_type}")
+            raise ValueError(f"unknown seed type: {seed_type}")
 
+        self.seed_counter = 0
         self.cipher_type = cipher_type
         self.seed_type = seed_type
         self.output_width = output_width
 
-        # Scheduled state and seed for the current cycle interval.
-        self.next_state: List[int] = []
-        self.next_seed: List[int] = []
+        # Scheduled state and seed for the current cycle interval. Next seed can be a full state
+        # (multiple ints) or a partial seed (single int).
+        self.next_state: Optional[Tuple[int, ...]] = None
+        self.next_seed: Optional[Union[Tuple[int, ...], int]] = None
 
-        self.ks = [0] * output_width
+        self.ks: int = 0
 
     def update(self) -> None:
         """Run the state update function `OUTPUT_WIDTH`-many times and schedule
@@ -153,45 +138,39 @@ class Trivium:
         This will also generate the keystream that can be read via
         `keystream`."""
 
-        if self.next_state != []:
+        if self.next_state is not None:
             raise Exception("cannot update more than once per cycle interval")
 
-        self.next_state = self.state[:]
-        for i in range(self.output_width):
-            self.ks[i] = self.update_func(self.next_state)
+        self.next_state = self._update(*self.state)
 
-    def seed(self, seed: List[int]) -> None:
+    def seed(self, seed: int) -> None:
         """Schedule a new seed that depending on the seed type will be injected
         into the state at the end of the cycle interval."""
 
-        if self.next_seed != []:
+        if self.next_seed is not None:
             raise Exception("cannot seed more than once per cycle interval")
 
         if self.seed_type == SeedType.KEY_IV:
-            assert len(seed) == 160
-
-            key = seed[0:80]
-            iv = seed[80:160]
-
+            # seed encodes key in bits [0, 80) and IV in bits [80, 160).
+            assert 0 <= seed < (1 << 160)
+            key = seed & ((1 << 80) - 1)
+            iv = (seed >> 80) & ((1 << 80) - 1)
             if self.cipher_type == CipherType.TRIVIUM:
-                self.next_seed = (
-                    (key + [0] * 13) + (iv + [0] * 4) + ([0] * 108 + [1, 1, 1])
-                )
+                state_int = key | (iv << 93) | (7 << 285)
             else:
-                self.next_seed = (key + [0] * 13) + (iv + [0] * 4)
-
+                state_int = key | (iv << 93)
+            self.next_seed = self._int_to_regs(state_int, self.cipher_type)
             if self.seed_done():
                 self.seed_counter = 0
 
         elif self.seed_type == SeedType.STATE_FULL:
-            assert len(seed) == self.state_size
-            self.next_seed = seed
+            assert 0 <= seed < (1 << self.state_size)
+            self.next_seed = self._int_to_regs(seed, self.cipher_type)
             self.seed_counter = 0
 
         else:
-            assert len(seed) == self.PART_SEED_SIZE
+            assert 0 <= seed < (1 << self.PART_SEED_SIZE)
             self.next_seed = seed
-
             if self.seed_done():
                 self.seed_counter = 0
 
@@ -199,52 +178,39 @@ class Trivium:
         """Advance the state by one clock cycle. Depending on the
         scheduled state and seed this will alter the current state."""
 
-        if self.next_state == [] and self.next_seed == []:
-            # Do nothing when neither an update nor reseed is scheduled.
+        if self.next_state is None and self.next_seed is None:
             return
 
-        if self.seed_type == SeedType.KEY_IV:
+        if self.seed_type == SeedType.KEY_IV or self.seed_type == SeedType.STATE_FULL:
             # Seeding takes precedence over updating.
-            if self.next_seed != []:
+            if self.next_seed is not None:
+                assert isinstance(self.next_seed, tuple)
                 self.state = self.next_seed
-
-            elif self.next_state != []:
+                if self.seed_type == SeedType.STATE_FULL:
+                    self.seed_counter += 1
+            elif self.next_state is not None:
                 self.state = self.next_state
                 if not self.seed_done():
                     self.seed_counter += 1
-
-        elif self.seed_type == SeedType.STATE_FULL:
-            # Seeding takes precedence over updating.
-            if self.next_seed != []:
-                self.state = self.next_seed
-                self.seed_counter += 1
-
-            elif self.next_state != []:
-                self.state = self.next_state
-
         else:
-            # Update and seeding in the same cycle interval is allowed. In this
-            # case the state is first updated, then partially overwritten with
-            # the seed bits.
-            if self.next_state != []:
+            # STATE_PARTIAL: update and seeding in the same cycle interval is allowed.
+            if self.next_state is not None:
                 self.state = self.next_state
-            if self.next_seed != []:
-                if self.seed_counter == self.seed_rounds - 1:
-                    self.state[self.state_size - self.last_part_seed_size:] = (
-                        self.next_seed[: self.last_part_seed_size]
-                    )
-                else:
-                    self.state[
-                        self.PART_SEED_SIZE * self.seed_counter:
-                        self.PART_SEED_SIZE * (self.seed_counter + 1)
-                    ] = self.next_seed
-
+            if self.next_seed is not None:
+                start = (self.state_size - self.last_part_seed_size
+                         if self.seed_counter == self.seed_rounds - 1
+                         else self.PART_SEED_SIZE * self.seed_counter)
+                n = (self.last_part_seed_size
+                     if self.seed_counter == self.seed_rounds - 1
+                     else self.PART_SEED_SIZE)
+                assert isinstance(self.next_seed, int)
+                self._inject_partial_seed(self.next_seed, start, n)
                 self.seed_counter += 1
 
-        self.next_state = []
-        self.next_seed = []
+        self.next_state = None
+        self.next_seed = None
 
-    def keystream(self) -> List[int]:
+    def keystream(self) -> int:
         """Returns the generated keystream for the current cycle interval. Note
         that an `update` call is necessary to populate the keystream."""
         return self.ks
@@ -253,57 +219,122 @@ class Trivium:
         """Returns true if the seeding procedure has been completed."""
         return self.seed_rounds == self.seed_counter
 
-    def _trivium_update(self, state: List[int]) -> int:
-        mul_90_91 = state[90] & state[91]
-        add_65_92 = state[65] ^ state[92]
+    @staticmethod
+    def _int_to_regs(val: int, cipher_type: CipherType) -> Tuple[int, ...]:
+        """Convert a little-endian integer (bit j = state[j]) to a register
+        tuple using reversed-bit storage: bit (N-1-j) of reg = state[j]."""
+        reg1 = sum(((val >> j) & 1) << (92 - j) for j in range(93))
+        reg2 = sum(((val >> (93 + j)) & 1) << (83 - j) for j in range(84))
+        if cipher_type == CipherType.TRIVIUM:
+            reg3 = sum(((val >> (177 + j)) & 1) << (110 - j) for j in range(111))
+            return reg1, reg2, reg3
+        return reg1, reg2
 
-        mul_174_175 = state[174] & state[175]
-        add_161_176 = state[161] ^ state[176]
+    def _inject_partial_seed(self, val: int, start: int, n: int) -> None:
+        """Overwrite n state bits at flat offset `start` with the low n bits of val."""
+        regs = list(self.state)
+        for i in range(n):
+            b, pos = (val >> i) & 1, start + i
+            if pos < 93:
+                k = 92 - pos
+                regs[0] = (regs[0] & ~(1 << k)) | (b << k)
+            elif pos < 177:
+                k = 83 - (pos - 93)
+                regs[1] = (regs[1] & ~(1 << k)) | (b << k)
+            else:
+                k = 110 - (pos - 177)
+                regs[2] = (regs[2] & ~(1 << k)) | (b << k)
+        self.state = tuple(regs)
 
-        mul_285_286 = state[285] & state[286]
-        add_242_287 = state[242] ^ state[287]
+    def _update(self, *regs: int) -> Tuple[int, ...]:
+        """Advance the state by output_width cycles using integer arithmetic.
+        Updates self.ks and returns the new register state tuple."""
+        w = self.output_width
+        ks_int = 0
+        processed = 0
+        mask93 = (1 << 93) - 1
+        mask84 = (1 << 84) - 1
+        new_state: Tuple[int, ...]
 
-        t0 = state[68] ^ (mul_285_286 ^ add_242_287)
-        t1 = state[170] ^ (add_65_92 ^ mul_90_91)
-        t2 = state[263] ^ (mul_174_175 ^ add_161_176)
+        if self.cipher_type == CipherType.TRIVIUM:
+            reg1, reg2, reg3 = regs
+            mask111 = (1 << 111) - 1
+            while processed < w:
+                chunk = min(w - processed, 64)
+                mask = (1 << chunk) - 1
+                v65 = (reg1 >> 27) & mask
+                v68 = (reg1 >> 24) & mask
+                v90 = (reg1 >> 2) & mask
+                v91 = (reg1 >> 1) & mask
+                v92 = reg1 & mask
 
-        state[0:93] = [t0] + state[0:92]
-        state[93:177] = [t1] + state[93:176]
-        state[177:288] = [t2] + state[177:287]
+                v161 = (reg2 >> 15) & mask
+                v170 = (reg2 >> 6) & mask
+                v174 = (reg2 >> 2) & mask
+                v175 = (reg2 >> 1) & mask
+                v176 = reg2 & mask
 
-        return add_65_92 ^ add_161_176 ^ add_242_287
+                v242 = (reg3 >> 45) & mask
+                v263 = (reg3 >> 24) & mask
+                v285 = (reg3 >> 2) & mask
+                v286 = (reg3 >> 1) & mask
+                v287 = reg3 & mask
 
-    def _bivium_update(self, state: List[int]) -> int:
-        mul_90_91 = state[90] & state[91]
-        add_65_92 = state[65] ^ state[92]
+                t0 = (v68 ^ (v285 & v286) ^ v242 ^ v287) & mask
+                t1 = (v170 ^ (v65 ^ v92) ^ (v90 & v91)) & mask
+                t2 = (v263 ^ (v174 & v175) ^ v161 ^ v176) & mask
 
-        mul_174_175 = state[174] & state[175]
-        add_161_176 = state[161] ^ state[176]
+                ks_int |= ((v65 ^ v92 ^ v161 ^ v176 ^ v242 ^ v287) & mask) << processed
+                reg1 = ((reg1 >> chunk) | (t0 << (93 - chunk))) & mask93
+                reg2 = ((reg2 >> chunk) | (t1 << (84 - chunk))) & mask84
+                reg3 = ((reg3 >> chunk) | (t2 << (111 - chunk))) & mask111
+                processed += chunk
+            new_state = (reg1, reg2, reg3)
+        else:
+            reg1, reg2 = regs
+            while processed < w:
+                chunk = min(w - processed, 64)
+                mask = (1 << chunk) - 1
+                v65 = (reg1 >> 27) & mask
+                v68 = (reg1 >> 24) & mask
+                v90 = (reg1 >> 2) & mask
+                v91 = (reg1 >> 1) & mask
+                v92 = reg1 & mask
 
-        t0 = state[68] ^ (mul_174_175 ^ add_161_176)
-        t1 = state[170] ^ (add_65_92 ^ mul_90_91)
+                v161 = (reg2 >> 15) & mask
+                v170 = (reg2 >> 6) & mask
+                v174 = (reg2 >> 2) & mask
+                v175 = (reg2 >> 1) & mask
+                v176 = reg2 & mask
 
-        state[0:93] = [t0] + state[0:92]
-        state[93:177] = [t1] + state[93:176]
+                t0 = (v68 ^ (v174 & v175) ^ v161 ^ v176) & mask
+                t1 = (v170 ^ (v65 ^ v92) ^ (v90 & v91)) & mask
 
-        return add_65_92 ^ add_161_176
+                ks_int |= ((v65 ^ v92 ^ v161 ^ v176) & mask) << processed
+                reg1 = ((reg1 >> chunk) | (t0 << (93 - chunk))) & mask93
+                reg2 = ((reg2 >> chunk) | (t1 << (84 - chunk))) & mask84
+                processed += chunk
+            new_state = (reg1, reg2)
+
+        self.ks = ks_int
+        return new_state
 
 
 _OUTPUT_WIDTH = 64
 
 
-def check_keystream(trivium: Trivium, ref: List[int]) -> None:
+def check_keystream(trivium: Trivium, reference: int) -> None:
     """Draw keystream bits and compare them against a test vector."""
     assert trivium.seed_done()
 
-    # Draw (512 / 8)-many `_OUTPUT_WIDTH`-size words from the cipher.
-    keystream = []
-    for _ in range(_OUTPUT_WIDTH >> 3):
+    # Draw (_OUTPUT_WIDTH / 8)-many `_OUTPUT_WIDTH`-size words from the cipher.
+    ks_int = 0
+    for i in range(_OUTPUT_WIDTH >> 3):
         trivium.update()
         trivium.step()
-        keystream.extend(trivium.keystream())
+        ks_int |= trivium.keystream() << (i * _OUTPUT_WIDTH)
 
-    assert keystream == ref
+    assert ks_int == reference
 
 
 if __name__ == "__main__":
@@ -313,65 +344,52 @@ if __name__ == "__main__":
     # implementation and generate 512 bits of keystream.
 
     # AVR cryptolib: Set 1, vector 0
-    ref = i2b(
-        int(
-            """
-F980FC5474EFE87BB9626ACCCC20FF98
-807FCFCE928F6CE0EB21096115F5FBD2
-649AF249C24120550175C86414657BBB
-0D5420443AF18DAF9C7A0D73FF86EB38""".replace("\n", ""),
-            16,
-        ),
-        288,
-    )
+    # Bytewise in little endian order
+    stream_str = """
+38EB86FF730D7A9CAF8DF13A4420540D
+BB7B651464C87501552041C249F29A64
+D2FBF515610921EBE06C8F92CECF7F80
+98FF20CCCC6A62B97BE8EF7454FC80F9""".replace("\n", "")
+    stream = int.from_bytes(bytes.fromhex(stream_str), 'little')
 
     trivium = Trivium(CipherType.TRIVIUM, SeedType.KEY_IV, _OUTPUT_WIDTH)
 
-    key = i2b(0x01000000000000000000, 80)
-    iv = [0] * 80
+    # KEY_IV seed: key in bits [0, 80), IV in bits [80, 160).
+    key_iv_seed = 0x01000000000000000000  # key=0x010...0, IV=0
 
     # Reseed twice.
     for _ in range(2):
-        trivium.seed(key + iv)
+        trivium.seed(key_iv_seed)
         trivium.step()
 
         while not trivium.seed_done():
             trivium.update()
             trivium.step()
 
-        check_keystream(trivium, ref)
+        check_keystream(trivium, stream)
 
     # Full state seed:
     #
     # Use the state after the init rounds of the first test as a full state seed.
 
-    seed = i2b(
-        0xC7D7C89BCC06725B3D94718106F2A0656422AF1FA457B81F0D2516A9D565893A64C1E50E, 288
-    )
-
     trivium = Trivium(CipherType.TRIVIUM, SeedType.STATE_FULL, _OUTPUT_WIDTH)
 
     # Reseed twice.
     for _ in range(2):
-        trivium.seed(seed)
+        trivium.seed(
+            0xC7D7C89BCC06725B3D94718106F2A0656422AF1FA457B81F0D2516A9D565893A64C1E50E
+        )
         trivium.step()
 
-        check_keystream(trivium, ref)
+        check_keystream(trivium, stream)
 
     # Partial state seed
     #
     # Use the state after the init rounds of the first test as a partial state seed.
 
     seeds = [
-        i2b(0x64C1E50E, Trivium.PART_SEED_SIZE),
-        i2b(0xD565893A, Trivium.PART_SEED_SIZE),
-        i2b(0x0D2516A9, Trivium.PART_SEED_SIZE),
-        i2b(0xA457B81F, Trivium.PART_SEED_SIZE),
-        i2b(0x6422AF1F, Trivium.PART_SEED_SIZE),
-        i2b(0x06F2A065, Trivium.PART_SEED_SIZE),
-        i2b(0x3D947181, Trivium.PART_SEED_SIZE),
-        i2b(0xCC06725B, Trivium.PART_SEED_SIZE),
-        i2b(0xC7D7C89B, Trivium.PART_SEED_SIZE),
+        0x64C1E50E, 0xD565893A, 0x0D2516A9, 0xA457B81F, 0x6422AF1F,
+        0x06F2A065, 0x3D947181, 0xCC06725B, 0xC7D7C89B,
     ]
 
     trivium = Trivium(CipherType.TRIVIUM, SeedType.STATE_PARTIAL, _OUTPUT_WIDTH)
@@ -379,8 +397,8 @@ F980FC5474EFE87BB9626ACCCC20FF98
     # Reseed twice.
     for _ in range(2):
         # Need ceil(288/32) seed calls to fill every bit of the state.
-        for i in range(9):
-            trivium.seed(seeds[i])
+        for s in seeds:
+            trivium.seed(s)
             trivium.step()
 
-        check_keystream(trivium, ref)
+        check_keystream(trivium, stream)
