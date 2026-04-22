51504ad9e906bfeab7915ec4a354549a99d676c4
Michael Gautschi <mgautschi@lowrisc.org>
2026-04-22 15:08:53 +0200

    [rram_ctrl,rtl] add rram_ctrl memory protection
    The rram_ctrl_mp checks each transaction against programmable and fixed
    rules. These rules define which master (sw, host, hw-lcmgr, hw-otp) can
    access which partition of the RRAM.
    
    The rule selection is done based on the requested address, partition,
    and selected hardware interface.
    
    This module also determines if a request requires
    scrambling/descrambling, and if it is ECC protected.
    
    Signed-off-by: Michael Gautschi <mgautschi@lowrisc.org>

--- hw/ip/rram_ctrl/rtl/rram_ctrl_wr.sv
+++ hw/ip/rram_ctrl/rtl/rram_ctrl_wr.sv
@@ -92,7 +92,7 @@ module rram_ctrl_wr import rram_ctrl_pkg::*; (
   always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
       op_err_addr_o <= '0;
-    end else if (~|op_err_q && |op_err_d) begin
+    end else if ((op_err_q == 0) && (op_err_d != 0)) begin
       op_err_addr_o <= int_addr[BusAddrW-1:0];
     end
   end
