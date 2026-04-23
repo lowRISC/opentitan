lowRISC/opentitan

commit c3b9a23f10b8f0c410ebd032ab48bbb05a189193

[tl,dv] Remove pre_body specialisation in tl_device_seq
It's rather simpler to put the "thing to do before body" into... the
start of the body task!

The pre_body task also triggers warnings from lint tools because it
doesn't always get called. (It doesn't get called when the sequence is
started with `uvm_do: see the notes in the documentation for
uvm_sequence_base)

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/dv/sv/tl_agent/seq_lib/tl_device_seq.sv
@@ -41,15 +41,10 @@ class tl_device_seq #(type REQ = tl_seq_item, int unsigned AddrWidth = 32) exten
     };
   }
 
-  // In case this seq start in the middle of other body.
-  // If we set `stop` at the beginning of this body, then
-  // there is a risk to get the race when start and stop are called
-  // at the same time.
-  virtual task pre_body();
-    stop = 0;
-  endtask
-
   virtual task body();
+    // Clear the stop flag (which may have been set by seq_stop on the previous run of the sequence)
+    stop = 0;
+
     fork
       begin: isolation_thread
         fork
@@ -183,13 +178,6 @@ class tl_device_seq #(type REQ = tl_seq_item, int unsigned AddrWidth = 32) exten
 
   // Stop running this seq and wait until it has finished gracefully.
   virtual task seq_stop();
-    // We want to tell the sequence to stop by setting the stop flag. The current implementation of
-    // pre_body clears the stop signal. To avoid our stop flag getting cleared before it has any
-    // effect, wait until pre_body is out of the way.
-    if (get_sequence_state() inside {UVM_CREATED, UVM_PRE_START}) begin
-      wait_for_sequence_state(UVM_BODY);
-    end
-
     stop = 1'b1;
     wait_for_sequence_state(UVM_FINISHED);
   endtask

commit 0f4dc9747a5b24696f1bba640875cbfe84fb2722

[tl,dv] Clear the stop flag at the end, not the start, of body()
Before this change, there was a race condition in the rv_dm testbench.

That block's base virtual sequence creates a device
sequence (m_tl_sba_device_seq) in a task called
sba_tl_device_seq_start and then uses fork/join_none to start it in
the background. One sequence that derives from rv_dm_base_vseq is
rv_dm_sba_tl_access_vseq, which calls sba_tl_device_seq_stop at the
start of its body. The result is that you can end up with this
course of events:

  - rv_dm_base_vseq::dut_init calls sba_tl_device_seq_start, which
    starts an instance of tl_device_seq (whose body will run shortly).

  - The start of rv_dm_sba_tl_access_vseq::body calls
    sba_tl_device_seq_stop, which sets the stop flag in the device
    sequence and then waits for that sequence to run to completion.

  - The device sequence finally starts! At the start of
    tl_device_seq::body, it clears the stop flag.

  - The device sequence now runs until it sees the stop flag being
    set.

  - Hang.

The change in this commit moves the flag clearing to the end of the
sequence. Honestly, I don't think we ever run an instance of this
sequence multiple times, so we can probably just drop it. But this is
clearly safe too.

Initial debugging made me wonder whether this was caused by commit
c3b9a23, but I think the race is there either way.

This change fixes rv_dm_sba_tl_access, which is currently broken.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/dv/sv/tl_agent/seq_lib/tl_device_seq.sv
@@ -42,9 +42,6 @@ class tl_device_seq #(type REQ = tl_seq_item, int unsigned AddrWidth = 32) exten
   }
 
   virtual task body();
-    // Clear the stop flag (which may have been set by seq_stop on the previous run of the sequence)
-    stop = 0;
-
     fork
       begin: isolation_thread
         fork
@@ -56,6 +53,11 @@ class tl_device_seq #(type REQ = tl_seq_item, int unsigned AddrWidth = 32) exten
         disable fork;
       end
     join
+
+    // Clear the stop flag. It must be set (because collect_request_thread and send_response_thread
+    // don't stop otherwise). Clear it to allow a virtual sequence to start the same sequence object
+    // again if it wishes.
+    stop = 0;
   endtask
 
   // A blocking task that retrieves a request from the TLM fifo, unless the seq is stopped.