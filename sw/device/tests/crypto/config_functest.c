03c161e64cd61393b9227e66e415b124b9e68784
Siemen Dhooghe <sdhooghe@google.com>
2026-04-23 21:40:37 +0200

    [crypto/test] Improve config testing
    Add more tests for config including a negative test on
    otcrypto_security_config_check.
    The new test does not work on cw310, so we remove its exec env.
    Also remove the otcrypto_security_config_check functions from the
    pentest.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>
    (cherry picked from commit 6a7b6a17d56819600a68d495c730c7586b746c42)

--- sw/device/tests/crypto/config_functest.c
+++ sw/device/tests/crypto/config_functest.c
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "sw/device/lib/base/csr.h"
 #include "sw/device/lib/base/macros.h"
 #include "sw/device/lib/base/status.h"
 #include "sw/device/lib/crypto/impl/status.h"
@@ -10,6 +11,8 @@
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/test_framework/check.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
+
+#include "rv_core_ibex_regs.h"
 
 #define MODULE_ID MAKE_MODULE_ID('t', 's', 't')
 
@@ -49,12 +52,41 @@ static status_t test_multiple_inits(void) {
   return OK_STATUS();
 }
 
+static status_t test_security_config_check_negative(void) {
+  LOG_INFO("Testing negative security config check on boot state");
+  otcrypto_key_security_level_t sec_level = kOtcryptoKeySecurityLevelHigh;
+
+  otcrypto_status_t tamper_status = otcrypto_security_config_check(sec_level);
+  CHECK(tamper_status.value != OTCRYPTO_OK.value);
+
+  TRY(otcrypto_set_security_config(sec_level));
+
+  TRY(otcrypto_security_config_check(sec_level));
+
+  return OK_STATUS();
+}
+
+static status_t test_icache_disable_restore(void) {
+  LOG_INFO("Testing icache disable and restore");
+  hardened_bool_t icache_state;
+
+  TRY(otcrypto_disable_icache(&icache_state));
+
+  TRY(otcrypto_restore_icache(icache_state));
+
+  return OK_STATUS();
+}
+
 bool test_main(void) {
   status_t result = OK_STATUS();
 
+  // We run the config check first as the chip comes out of boot, so the config
+  // is not set yet.
+  EXECUTE_TEST(result, test_security_config_check_negative);
   EXECUTE_TEST(result, test_set_and_check_config);
   EXECUTE_TEST(result, test_init_and_exit);
   EXECUTE_TEST(result, test_multiple_inits);
+  EXECUTE_TEST(result, test_icache_disable_restore);
 
   return status_ok(result);
 }
