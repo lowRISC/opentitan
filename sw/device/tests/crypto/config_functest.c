5ec7895645e8bdae1400aa3943b43b159f6aa3ea
Siemen Dhooghe <sdhooghe@google.com>
2026-04-25 21:33:03 +0200

    [crypto/alert] Exit on class acumulators
    Reading all alert causes is costly in terms of cycles (due to CDC),
    instead, read class acumulators.
    In addition, remove the redundant second read of the alerts.
    Create a new function to clear the alerts (mention that it sets
    registers).
    Create a force alert test to verify the new functions and method.
    Also add a test that shows it is possible to lock the accumulators in
    which case the cryptolib stays locked.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>
    (cherry picked from commit 3aa560df4bdfd4573d3a39afc694cc4cf7e15234)
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/tests/crypto/config_functest.c
+++ sw/device/tests/crypto/config_functest.c
@@ -2,16 +2,22 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "sw/device/lib/base/abs_mmio.h"
 #include "sw/device/lib/base/csr.h"
 #include "sw/device/lib/base/macros.h"
 #include "sw/device/lib/base/status.h"
 #include "sw/device/lib/crypto/impl/status.h"
 #include "sw/device/lib/crypto/include/config.h"
 #include "sw/device/lib/crypto/include/datatypes.h"
+#include "sw/device/lib/runtime/irq.h"
 #include "sw/device/lib/runtime/log.h"
 #include "sw/device/lib/testing/test_framework/check.h"
+#include "sw/device/lib/testing/test_framework/ottf_alerts.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 
+#include "aes_regs.h"
+#include "alert_handler_regs.h"  // Generated
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
 #include "rv_core_ibex_regs.h"
 
 #define MODULE_ID MAKE_MODULE_ID('t', 's', 't')
@@ -77,6 +83,122 @@ static status_t test_icache_disable_restore(void) {
   return OK_STATUS();
 }
 
+static status_t test_alert_caught_by_eval_exit(void) {
+  LOG_INFO("Testing otcrypto_eval_exit with forced AES alert");
+
+  // Initialize crypto (this also clears alert accumulators)
+  TRY(otcrypto_init(kOtcryptoKeySecurityLevelHigh));
+
+  // OTTF has its own alert management system, we have to bypass it.
+  // Disable interrupts globally.
+  // This prevents the OTTF ISR from preempting us and clearing the accumulator.
+  irq_global_ctrl(false);
+
+  // Force a recoverable alert of the AES
+  uint32_t alert_test_reg =
+      bitfield_bit32_write(0, AES_ALERT_TEST_RECOV_CTRL_UPDATE_ERR_BIT, true);
+  mmio_region_write32(mmio_region_from_addr(TOP_EARLGREY_AES_BASE_ADDR),
+                      (ptrdiff_t)AES_ALERT_TEST_REG_OFFSET, alert_test_reg);
+
+  // Verify eval_exit catches the alert.
+  // Because interrupts are off, the accumulator is preserved for this check.
+  otcrypto_status_t exit_status = otcrypto_eval_exit(OTCRYPTO_OK);
+  CHECK(exit_status.value == OTCRYPTO_FATAL_ERR.value,
+        "otcrypto_eval_exit failed to catch the AES alert. Expected 0x%08x, "
+        "got 0x%08x",
+        OTCRYPTO_FATAL_ERR.value, exit_status.value);
+
+  // Use the cryptolib's helper call to clear the alerts.
+  TRY(otcrypto_clear_alerts());
+
+  // Verify that the eval_exit passes again.
+  TRY(otcrypto_eval_exit(OTCRYPTO_OK));
+
+  // - Cleanup
+  // The PLIC still has a pending interrupt latched from the alert. If we turn
+  // IRQs back on right now, the ISR will run, find no cause, and abort(). To
+  // exit safely, we tell the OTTF to expect an alert, and trigger a fresh one.
+
+  // Feed the OTTF ISR.
+  TRY(ottf_alerts_expect_alert_start(kTopEarlgreyAlertIdAesRecovCtrlUpdateErr));
+
+  // Provide a new alert.
+  mmio_region_write32(mmio_region_from_addr(TOP_EARLGREY_AES_BASE_ADDR),
+                      (ptrdiff_t)AES_ALERT_TEST_REG_OFFSET, alert_test_reg);
+
+  // The CPU jumps to the ISR immediately. The ISR sees the second alert, marks
+  // it as caught, clears the PLIC state, and returns.
+  irq_global_ctrl(true);
+
+  // Finish the OTTF's expectation.
+  TRY(ottf_alerts_expect_alert_finish(
+      kTopEarlgreyAlertIdAesRecovCtrlUpdateErr));
+
+  return OK_STATUS();
+}
+
+static status_t test_lock(void) {
+  LOG_INFO("Testing locking the regwen provides a bad status");
+  // Locking the accumulators causes the alert to no longer be clearable.
+  // The test is similar to test_alert_caught_by_eval_exit
+
+  TRY(otcrypto_init(kOtcryptoKeySecurityLevelHigh));
+  irq_global_ctrl(false);
+
+  // Lock the accumulators
+  abs_mmio_write32_shadowed(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
+                                ALERT_HANDLER_CLASSA_CLR_REGWEN_REG_OFFSET,
+                            0);
+  abs_mmio_write32_shadowed(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
+                                ALERT_HANDLER_CLASSB_CLR_REGWEN_REG_OFFSET,
+                            0);
+  abs_mmio_write32_shadowed(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
+                                ALERT_HANDLER_CLASSC_CLR_REGWEN_REG_OFFSET,
+                            0);
+  abs_mmio_write32_shadowed(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
+                                ALERT_HANDLER_CLASSD_CLR_REGWEN_REG_OFFSET,
+                            0);
+
+  // Recoverable alert
+  uint32_t alert_test_reg =
+      bitfield_bit32_write(0, AES_ALERT_TEST_RECOV_CTRL_UPDATE_ERR_BIT, true);
+  mmio_region_write32(mmio_region_from_addr(TOP_EARLGREY_AES_BASE_ADDR),
+                      (ptrdiff_t)AES_ALERT_TEST_REG_OFFSET, alert_test_reg);
+
+  // eval_exit catches the alert.
+  otcrypto_status_t exit_status = otcrypto_eval_exit(OTCRYPTO_OK);
+  CHECK(exit_status.value == OTCRYPTO_FATAL_ERR.value,
+        "otcrypto_eval_exit failed to catch the AES alert. Expected 0x%08x, "
+        "got 0x%08x",
+        OTCRYPTO_FATAL_ERR.value, exit_status.value);
+
+  // Use the cryptolib's helper call to clear the alerts.
+  // However, the function will return OTCRYPTO_FATAL_ERR as these accumulators
+  // are locked.
+  exit_status = otcrypto_clear_alerts();
+  CHECK(exit_status.value == OTCRYPTO_FATAL_ERR.value,
+        "otcrypto_clear_alerts failed to lock. Expected 0x%08x, "
+        "got 0x%08x",
+        OTCRYPTO_FATAL_ERR.value, exit_status.value);
+
+  // Since the accumulators are locked, the cryptolib remains locked.
+  exit_status = otcrypto_eval_exit(OTCRYPTO_OK);
+  CHECK(exit_status.value == OTCRYPTO_FATAL_ERR.value,
+        "otcrypto_eval_exit failed to lock. Expected 0x%08x, "
+        "got 0x%08x",
+        OTCRYPTO_FATAL_ERR.value, exit_status.value);
+
+  // Cleanup
+  TRY(ottf_alerts_expect_alert_start(kTopEarlgreyAlertIdAesRecovCtrlUpdateErr));
+  mmio_region_write32(mmio_region_from_addr(TOP_EARLGREY_AES_BASE_ADDR),
+                      (ptrdiff_t)AES_ALERT_TEST_REG_OFFSET, alert_test_reg);
+  irq_global_ctrl(true);
+  TRY(ottf_alerts_expect_alert_finish(
+      kTopEarlgreyAlertIdAesRecovCtrlUpdateErr));
+
+  return OK_STATUS();
+}
+
 bool test_main(void) {
   status_t result = OK_STATUS();
 
@@ -87,6 +209,10 @@ bool test_main(void) {
   EXECUTE_TEST(result, test_init_and_exit);
   EXECUTE_TEST(result, test_multiple_inits);
   EXECUTE_TEST(result, test_icache_disable_restore);
+  EXECUTE_TEST(result, test_alert_caught_by_eval_exit);
+  // Locks the accumulators making the cryptolib alerts no longer clearable
+  // Hence this test should be the last
+  EXECUTE_TEST(result, test_lock);
 
   return status_ok(result);
 }
