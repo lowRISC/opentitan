efb99eae5318cffebbc8d5776e5d2cc120a7d13f
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-04-30 18:26:20 +0100

    [dv] Re-order csr_aliasing_seq to use out-of-block definitions
    No functional change: this is just reordering lines in the file.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/dv/sv/csr_utils/csr_aliasing_seq.sv
+++ hw/dv/sv/csr_utils/csr_aliasing_seq.sv
@@ -20,121 +20,137 @@ class csr_aliasing_seq extends csr_base_seq;
   // was no aliasing).
   int unsigned m_chunk_size = 10;
 
-  function new (string name="");
-      super.new(name);
-  endfunction
+  extern function new (string name="");
+
+  extern virtual task body();
 
   // Write a random value to csr, after applying any write exclusion that has been defined for the
   // register.
-  local task randomize_register(uvm_reg csr);
-    uvm_reg_data_t wdata;
-
-    if (!std::randomize(wdata)) begin
-      `uvm_fatal(get_full_name(), "Failed to randomize wdata.")
-    end
-
-    wdata = get_csr_wdata_with_write_excl(csr, wdata, CsrAliasingTest);
-    csr_wr(.ptr(csr), .value(wdata), .predict(!external_checker));
-  endtask
+  extern local task randomize_register(uvm_reg csr);
 
   // Write random values to each register in the list, running the sequences in parallel for maximum
   // throughput.
-  local task randomize_registers(const ref uvm_reg registers[$]);
-    fork : isolation_fork begin
-      foreach (registers[i]) begin
-        automatic uvm_reg _reg = registers[i];
-        fork
-          randomize_register(_reg);
-        join_none
-      end
-      wait fork;
-    end join
-  endtask
+  extern local task randomize_registers(const ref uvm_reg registers[$]);
 
   // Read back csr using csr_rd_check and checking against the register model. Mask the comparison
   // using get_mask_excl_fields (to avoid checking the values of fields that are not modelled).
-  local task read_back_register(uvm_reg csr);
-    csr_rd_check(.ptr           (csr),
-                 .compare       (!external_checker),
-                 .compare_vs_ral(1'b1),
-                 .compare_mask  (get_mask_excl_fields(csr, CsrExclWriteCheck, CsrAliasingTest)));
-  endtask
+  extern local task read_back_register(uvm_reg csr);
 
   // Read back every register in the list, using csr_rd_check and checking against the register
   // model.
   //
   // Comparisons are masked using get_mask_excl_fields (to avoid checking the values of fields that
   // are not modelled).
-  local task read_back_registers(uvm_reg registers[$]);
-    fork : isolation_fork begin
-      foreach (registers[i]) begin
-        automatic uvm_reg _reg = registers[i];
-        fork
-          read_back_register(_reg);
-        join_none
-      end
-      wait fork;
-    end join
-  endtask
+  extern local task read_back_registers(uvm_reg registers[$]);
 
   // Write random values to each register in write_list, then read all the registers in read_list
-  local task test_chunk(uvm_reg write_list[$], uvm_reg read_list[$]);
-    randomize_registers(write_list);
-    read_back_registers(read_list);
-  endtask
+  extern local task test_chunk(uvm_reg write_list[$], uvm_reg read_list[$]);
 
   // Return a random sample from reg_queue with length sample_size (or the entire list if there
   // aren't that many items)
-  local function reg_queue_t sample_reg_list(int unsigned      sample_size,
-                                             const ref uvm_reg reg_queue[$]);
-    uvm_reg ret[$] = reg_queue;
-    ret.shuffle();
-    return (reg_queue.size() < sample_size) ? ret[0:sample_size - 1] : ret;
-  endfunction
+  extern local function reg_queue_t sample_reg_list(int unsigned      sample_size,
+                                                    const ref uvm_reg reg_queue[$]);
+endclass
 
-  virtual task body();
-    uvm_reg      total_list[$];
-    int unsigned num_chunks;
+function csr_aliasing_seq::new (string name="");
+    super.new(name);
+endfunction
 
-    foreach(test_csrs[i]) begin
-      if (!is_excl(test_csrs[i], CsrExclWrite, CsrAliasingTest)) begin
-        total_list.push_back(test_csrs[i]);
-      end else begin
-        `uvm_info(get_full_name(),
-                  $sformatf("Skipping register %0s because of CsrExclWrite exclusion",
-                            test_csrs[i].get_full_name()),
-                  UVM_MEDIUM)
-      end
+task csr_aliasing_seq::body();
+  uvm_reg      total_list[$];
+  int unsigned num_chunks;
+
+  foreach(test_csrs[i]) begin
+    if (!is_excl(test_csrs[i], CsrExclWrite, CsrAliasingTest)) begin
+      total_list.push_back(test_csrs[i]);
+    end else begin
+      `uvm_info(get_full_name(),
+                $sformatf("Skipping register %0s because of CsrExclWrite exclusion",
+                          test_csrs[i].get_full_name()),
+                UVM_MEDIUM)
     end
+  end
+
+  `uvm_info(get_full_name(),
+            $sformatf("Aliasing test with list of %0d registers (with a total of %0d visible)",
+                      test_csrs.size(),
+                      all_csrs.size()),
+            UVM_LOW)
+
+  num_chunks = (test_csrs.size() + m_chunk_size - 1) / m_chunk_size;
+
+  for (int unsigned i = 0; i < num_chunks; i++) begin
+    int unsigned start_idx = i * m_chunk_size;
+    int unsigned end_idx = start_idx + m_chunk_size;
+    uvm_reg write_regs[$];
+
+    if (end_idx >= test_csrs.size()) begin
+      end_idx = test_csrs.size() - 1;
+    end
+
+    write_regs = test_csrs[start_idx:end_idx];
 
     `uvm_info(get_full_name(),
-              $sformatf("Aliasing test with list of %0d registers (with a total of %0d visible)",
-                        test_csrs.size(),
-                        all_csrs.size()),
+              $sformatf("Testing aliasing with chunk %0d/%0d", i + 1, num_chunks),
               UVM_LOW)
-
-    num_chunks = (test_csrs.size() + m_chunk_size - 1) / m_chunk_size;
-
-    for (int unsigned i = 0; i < num_chunks; i++) begin
-      int unsigned start_idx = i * m_chunk_size;
-      int unsigned end_idx = start_idx + m_chunk_size;
-      uvm_reg write_regs[$];
-
-      if (end_idx >= test_csrs.size()) begin
-        end_idx = test_csrs.size() - 1;
-      end
-
-      write_regs = test_csrs[start_idx:end_idx];
-
-      `uvm_info(get_full_name(),
-                $sformatf("Testing aliasing with chunk %0d/%0d", i + 1, num_chunks),
-                UVM_LOW)
-      foreach (write_regs[j]) begin
-        `uvm_info(get_full_name(), $sformatf("  - %0s", write_regs[j].get_name()), UVM_MEDIUM)
-      end
-
-      test_chunk(write_regs, sample_reg_list(100, all_csrs));
+    foreach (write_regs[j]) begin
+      `uvm_info(get_full_name(), $sformatf("  - %0s", write_regs[j].get_name()), UVM_MEDIUM)
     end
-  endtask
 
-endclass
+    test_chunk(write_regs, sample_reg_list(100, all_csrs));
+  end
+endtask
+
+task csr_aliasing_seq::randomize_register(uvm_reg csr);
+  uvm_reg_data_t wdata;
+
+  if (!std::randomize(wdata)) begin
+    `uvm_fatal(get_full_name(), "Failed to randomize wdata.")
+  end
+
+  wdata = get_csr_wdata_with_write_excl(csr, wdata, CsrAliasingTest);
+  csr_wr(.ptr(csr), .value(wdata), .predict(!external_checker));
+endtask
+
+task csr_aliasing_seq::randomize_registers(const ref uvm_reg registers[$]);
+  fork : isolation_fork begin
+    foreach (registers[i]) begin
+      automatic uvm_reg _reg = registers[i];
+      fork
+        randomize_register(_reg);
+      join_none
+    end
+    wait fork;
+  end join
+endtask
+
+task csr_aliasing_seq::read_back_register(uvm_reg csr);
+  csr_rd_check(.ptr           (csr),
+               .compare       (!external_checker),
+               .compare_vs_ral(1'b1),
+               .compare_mask  (get_mask_excl_fields(csr, CsrExclWriteCheck, CsrAliasingTest)));
+endtask
+
+task csr_aliasing_seq::read_back_registers(uvm_reg registers[$]);
+  fork : isolation_fork begin
+    foreach (registers[i]) begin
+      automatic uvm_reg _reg = registers[i];
+      fork
+        read_back_register(_reg);
+      join_none
+    end
+    wait fork;
+  end join
+endtask
+
+task csr_aliasing_seq::test_chunk(uvm_reg write_list[$], uvm_reg read_list[$]);
+  randomize_registers(write_list);
+  read_back_registers(read_list);
+endtask
+
+function reg_queue_t csr_aliasing_seq::sample_reg_list(int unsigned      sample_size,
+                                                       const ref uvm_reg reg_queue[$]);
+  uvm_reg ret[$] = reg_queue;
+  ret.shuffle();
+  return (reg_queue.size() < sample_size) ? ret[0:sample_size - 1] : ret;
+endfunction
