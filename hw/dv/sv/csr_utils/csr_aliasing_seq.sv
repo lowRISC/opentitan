e5378f3b9ea3654cd6f697bed5f961b1bed8e3cc
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-04-30 15:36:49 +0100

    [dv] Speed up csr_aliasing_seq
    Historically, the sequence ran the following pseudocode:
    
      for (dest in registers) {
        dest <- random_value;
        for (src in registers) {
          read_and_check(src);
        }
      }
    
    This iterates over all registers, writing a value and then reads every
    register to check that it has changed in the way we expect. That loop
    is obviously quadratic in the number of registers, which is a problem
    if you have lots of registers(!)
    
    To try to work around that problem, the initial version of OpenTitan
    repository had two tweaks:
    
      - Instead of writing to each register (in the outer loop), we pick
        some segment of registers to write. This is actually done in
        csr_base_seq::set_csr_test_range and the size of the segment can
        be controlled by plusargs.
    
        Interestingly, this isn't actually used in the repository: it is
        controlled (in csr_base_seq) by plusargs +num_csr_chunks and
        +test_csr_chunk. They have been possible to supply since the start
        of the repository history, but are never actually used.
    
      - If there are lots of registers then, instead of reading each
        register (the inner loop), we actually pick up to 100 registers at
        random and read them instead.
    
    After this commit, the code works by writing "chunks" with multiple
    registers each time.
    
    To compare the expected run time, consider a testbench with lots of
    registers (a chip simulation for example). In this situation, the
    all_csrs queue is trimmed down to 100 registers.
    
    Before this change, a run with N registers in test_csrs would have
    100*N register operations (for each register, write it and then read
    100 randomly selected registers).
    
    After this change, the run works in N/10 chunks: write each register
    in the chunk and then read 100 registers. This takes (N/10) * 100 =
    10*N operations.
    
    If N is large, which will be the case for chip-level simulations, this
    will be about a factor of 10 quicker.
    
    Note that CSR exclusions make this a bit more fiddly to implement. To
    make sure that the chunks are evenly sized, the implementation starts
    by sampling the registers that are allowed to be
    written (regs_to_write) and the registers that are allowed to be
    read (regs_to_read). It then splits regs_to_write into chunks of size
    10 and picks random samples of 100 registers from regs_to_read.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/dv/sv/csr_utils/csr_aliasing_seq.sv
+++ hw/dv/sv/csr_utils/csr_aliasing_seq.sv
@@ -10,53 +10,146 @@
 class csr_aliasing_seq extends csr_base_seq;
   `uvm_object_utils(csr_aliasing_seq)
 
-  `uvm_object_new
+  // A queue of uvm_reg objects
+  typedef uvm_reg reg_queue_t[$];
+
+  // The size of "register chunk" to use in the test.
+  //
+  // The list of CSRs to test (test_csrs) is checked in chunks of this size (writing a random value
+  // to each register in a chunk, then reading back every register in the design to check that there
+  // was no aliasing).
+  int unsigned m_chunk_size = 10;
+
+  // The number of registers to read after writing a chunk of registers, looking for values that
+  // have changed unexpectedly.
+  int unsigned m_reads_per_chunk = 100;
+
+  function new (string name="");
+      super.new(name);
+  endfunction
+
+  // Write a random value to csr, after applying any write exclusion that has been defined for the
+  // register.
+  local task randomize_register(uvm_reg csr);
+    uvm_reg_data_t wdata;
+
+    if (!std::randomize(wdata)) begin
+      `uvm_fatal(get_full_name(), "Failed to randomize wdata.")
+    end
+
+    wdata = get_csr_wdata_with_write_excl(csr, wdata, CsrAliasingTest);
+    csr_wr(.ptr(csr), .value(wdata), .predict(!external_checker));
+  endtask
+
+  // Write random values to each register in the list, running the sequences in parallel for maximum
+  // throughput.
+  local task randomize_chunk(const ref uvm_reg registers[$]);
+    fork : isolation_fork begin
+      foreach (registers[i]) begin
+        automatic uvm_reg _reg = registers[i];
+        fork
+          randomize_register(_reg);
+        join_none
+      end
+      wait fork;
+    end join
+  endtask
+
+  // Read back csr using csr_rd_check and checking against the register model. Mask the comparison
+  // using get_mask_excl_fields (to avoid checking the values of fields that are not modelled).
+  local task read_back_register(uvm_reg csr);
+    csr_rd_check(.ptr           (csr),
+                 .compare       (!external_checker),
+                 .compare_vs_ral(1'b1),
+                 .compare_mask  (get_mask_excl_fields(csr, CsrExclWriteCheck, CsrAliasingTest)));
+  endtask
+
+  // Read back every register in the list, using csr_rd_check and checking against the register
+  // model.
+  //
+  // Comparisons are masked using get_mask_excl_fields (to avoid checking the values of fields that
+  // are not modelled).
+  local task read_back_chunk(uvm_reg registers[$]);
+    fork : isolation_fork begin
+      foreach (registers[i]) begin
+        automatic uvm_reg _reg = registers[i];
+        fork
+          read_back_register(_reg);
+        join_none
+      end
+      wait fork;
+    end join
+  endtask
+
+  // Write random values to each register in write_list, then read all the registers in read_list
+  local task test_chunk(uvm_reg write_list[$], uvm_reg read_list[$]);
+    randomize_chunk(write_list);
+    read_back_chunk(read_list);
+  endtask
+
+  // Return a random subset of reg_queue with m_reads_per_chunk registers (or the entire list if
+  // there aren't that many)
+  local function reg_queue_t sample_reg_list(const ref uvm_reg reg_queue[$]);
+    uvm_reg ret[$] = reg_queue;
+    ret.shuffle();
+    return (reg_queue.size() < m_reads_per_chunk) ? ret[0:m_reads_per_chunk - 1] : ret;
+  endfunction
 
   virtual task body();
+    uvm_reg      total_list[$];
+    int unsigned num_chunks;
+
+    // The subset of test_csrs consisting of registers that do not have the CsrExclWrite exclusion.
+    // This will be split into chunks of size m_chunk_size.
+    uvm_reg regs_to_write[$];
+
+    // The subset of test_csrs consisting of registers that do not have the CsrExclInitCheck or
+    // CsrExclWriteCheck exclusion. Since test_csrs is just a section of all_csrs, this list is
+    // probably much larger than regs_to_write.
+    //
+    // A random sample of m_reads_per_chunk of these registers will be read back for each chunk.
+    uvm_reg regs_to_read[$];
+
     foreach(test_csrs[i]) begin
-      uvm_reg_data_t wdata;
+      if (!is_excl(test_csrs[i], CsrExclWrite, CsrAliasingTest)) begin
+        regs_to_write.push_back(test_csrs[i]);
+      end
+    end
+    foreach (all_csrs[i]) begin
+      if (!(is_excl(test_csrs[i], CsrExclInitCheck, CsrAliasingTest) ||
+            is_excl(test_csrs[i], CsrExclWriteCheck, CsrAliasingTest))) begin
+        regs_to_read.push_back(test_csrs[i]);
+      end
+    end
 
-      // check if parent block or register is excluded
-      if (is_excl(test_csrs[i], CsrExclWrite, CsrAliasingTest)) begin
-        `uvm_info(`gtn, $sformatf("Skipping register %0s due to CsrExclWrite exclusion",
-                                  test_csrs[i].get_full_name()), UVM_MEDIUM)
-        continue;
+    `uvm_info(get_full_name(),
+              $sformatf({"Aliasing test with list of %0d writable and %0d readable registers ",
+                         "(from a group of %0d registers selected from the %0d that are visible)"},
+                        regs_to_write.size(), regs_to_read.size(),
+                        test_csrs.size(), all_csrs.size()),
+              UVM_LOW)
+
+    // The test will break the registers that get written into chunks, of size m_chunk_size.
+    num_chunks = (regs_to_write.size() + m_chunk_size - 1) / m_chunk_size;
+
+    for (int unsigned i = 0; i < num_chunks; i++) begin
+      // The [start_idx:end_idx] range is [i*m_chunk_size, (i+1)*m_chunk_size - 1], intersected with
+      // the set of valid indices for regs_to_write.
+      int unsigned start_idx = i * m_chunk_size;
+      int unsigned end_idx = start_idx + m_chunk_size - 1;
+
+      if (end_idx >= regs_to_write.size()) begin
+        end_idx = regs_to_write.size() - 1;
       end
 
-      `uvm_info(`gtn,
-                $sformatf("Verifying register aliasing for %0s (register %0d / %0d)",
-                          test_csrs[i].get_full_name(), i + 1, test_csrs.size()),
-                UVM_MEDIUM)
-
-      `DV_CHECK_STD_RANDOMIZE_FATAL(wdata)
-      wdata = get_csr_wdata_with_write_excl(test_csrs[i], wdata, CsrAliasingTest);
-      csr_wr(.ptr(test_csrs[i]), .value(wdata), .blocking(0), .predict(!external_checker));
-
-      all_csrs.shuffle();
-
-      // If all_csrs queue size is larger than 100, randomly pick 100 CSRs to avoid chip level test
-      // runtime too long.
-      if (all_csrs.size() > 100) all_csrs = all_csrs[0 : 99];
-
-      foreach (all_csrs[j]) begin
-        uvm_reg_data_t compare_mask;
-
-        // check if parent block or register is excluded
-        if (is_excl(all_csrs[j], CsrExclInitCheck, CsrAliasingTest) ||
-            is_excl(all_csrs[j], CsrExclWriteCheck, CsrAliasingTest)) begin
-          `uvm_info(`gtn, $sformatf("Skipping register %0s due to CsrExclInit/WriteCheck exclusion",
-                                    all_csrs[j].get_full_name()), UVM_HIGH)
-          continue;
-        end
-
-        compare_mask = get_mask_excl_fields(all_csrs[j], CsrExclWriteCheck, CsrAliasingTest);
-        csr_rd_check(.ptr           (all_csrs[j]),
-                     .blocking      (0),
-                     .compare       (!external_checker),
-                     .compare_vs_ral(1'b1),
-                     .compare_mask  (compare_mask));
+      `uvm_info(get_full_name(),
+                $sformatf("Testing aliasing with chunk %0d/%0d", i + 1, num_chunks),
+                UVM_LOW)
+      for (int unsigned j = start_idx; j <= end_idx; j++) begin
+        `uvm_info(get_full_name(), $sformatf("  - %0s", regs_to_write[j].get_name()), UVM_MEDIUM)
       end
-      wait_no_outstanding_access();
+
+      test_chunk(regs_to_write[start_idx:end_idx], sample_reg_list(regs_to_read));
     end
   endtask
 
