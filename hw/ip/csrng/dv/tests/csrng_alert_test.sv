64688473cd6cf588677c7c1c24251c88313f34d0
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-05-05 20:02:46 +0100

    [csrng,dv] Remove pointless double-randomization from tests
    The environment config gets randomized in dv_base_test::build_phase
    anyway.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/csrng/dv/tests/csrng_alert_test.sv
+++ hw/ip/csrng/dv/tests/csrng_alert_test.sv
@@ -15,9 +15,5 @@ class csrng_alert_test extends csrng_base_test;
     cfg.otp_en_cs_sw_app_read_inval_pct = 0;
     cfg.sw_app_enable_pct               = 100;
     cfg.use_invalid_mubi                = 1;
-
-    `DV_CHECK_RANDOMIZE_FATAL(cfg)
-
-    `uvm_info("csrng_intr_dbg", $sformatf("%s", cfg.convert2string()), UVM_LOW)
   endfunction
 endclass : csrng_alert_test
--- hw/ip/csrng/dv/tests/csrng_cmds_test.sv
+++ hw/ip/csrng/dv/tests/csrng_cmds_test.sv
@@ -38,8 +38,5 @@ class csrng_cmds_test extends csrng_base_test;
       cfg.m_edn_agent_cfg[i].min_genbits_rdy_dly = 0;
       cfg.m_edn_agent_cfg[i].max_genbits_rdy_dly = 70;
     end
-
-    `DV_CHECK_RANDOMIZE_FATAL(cfg)
-    `uvm_info(`gfn, $sformatf("%s", cfg.convert2string()), UVM_LOW)
   endfunction
 endclass : csrng_cmds_test
