a908beae188c8c2b19eafe46fef4b65f524cc519
Kinza Qamar <kqzaman@lowrisc.org>
2026-05-07 21:51:44 +0100

    [I2C, DV] Use urandom_range() to simplify randomizing Ack / Nack
    Signed-off-by: Kinza Qamar <kqzaman@lowrisc.org>

--- hw/dv/sv/i2c_agent/seq_lib/i2c_target_may_nack_seq.sv
+++ hw/dv/sv/i2c_agent/seq_lib/i2c_target_may_nack_seq.sv
@@ -23,24 +23,17 @@ class i2c_target_may_nack_seq extends i2c_base_seq;
   // METHODS //
   /////////////
 
-
-  virtual function void randomize_ack();
-    `DV_CHECK_STD_RANDOMIZE_FATAL(acknack);
-    case (acknack)
-      i2c_pkg::ACK:  req.drv_type = DevAck;
-      i2c_pkg::NACK: req.drv_type = DevNack;
-      default: `uvm_fatal(`gfn, "Shouldn't get here!")
-    endcase
-  endfunction : randomize_ack
-
+  // Return DevAck or DevNack with equal probability
+  local function drv_type_e get_ack_nack();
+    return ($urandom_range(0,1) ? DevAck : DevNack);
+  endfunction
 
   virtual task drive_addr_byte_ack();
     `uvm_create_obj(REQ, req);
     start_item(req);
 
-    // The base class / default behaviour is to ACK all address bytes.
-    // Here, choose randomly between ACK and NACK.
-    randomize_ack();
+    req.drv_type = get_ack_nack();
+
     if (req.drv_type == DevNack) `uvm_info(`gfn, "NACKing the address byte!", UVM_LOW)
 
     finish_item(req);
@@ -51,7 +44,8 @@ class i2c_target_may_nack_seq extends i2c_base_seq;
     `uvm_create_obj(REQ, req);
     start_item(req);
 
-    randomize_ack();
+    req.drv_type = get_ack_nack();
+
     if (req.drv_type == DevNack) `uvm_info(`gfn, "NACKing a data byte!", UVM_LOW)
 
     finish_item(req);
