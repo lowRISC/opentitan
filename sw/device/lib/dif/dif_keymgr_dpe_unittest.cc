5af475c3df0c65fa164891af4d2929945c0068dd
Raphael Roth <rroth@lowrisc.org>
2026-05-07 11:47:14 +0200

    [keymgr_dpe, dif] Add missing API calls
    Add three missing features to the dif of the keymgr_dpe.
    
    - The disable function to transition the keymgr_dpe into the disabled state
    - The configuration function to set the re-seeding interval of the keymgr_dpe
    - Any sideloaded key on the sideload interface can now be cleared by sw
    
    Add an empty `dif_keymgr_dpe_unittest.cc` file to avoid triggering error
    during the top integration, tracked in Issue #30609.
    
    Signed-off-by: Raphael Roth <rroth@lowrisc.org>

---
+++ sw/device/lib/dif/dif_keymgr_dpe_unittest.cc
@@ -0,0 +1,23 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "sw/device/lib/dif/dif_keymgr_dpe.h"
+
+#include <array>
+
+#include "gtest/gtest.h"
+#include "sw/device/lib/base/mmio.h"
+#include "sw/device/lib/base/mock_mmio.h"
+#include "sw/device/lib/dif/dif_base.h"
+#include "sw/device/lib/dif/dif_test_base.h"
+
+#include "hw/top/keymgr_dpe_regs.h"  // Generated
+
+namespace dif_keymgr_dpe_unittest {
+namespace {
+
+// TODO(#30609): Write these unittests
+
+}  // namespace
+}  // namespace dif_keymgr_dpe_unittest
