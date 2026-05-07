e41ae42578f71db33cc6a3373af22e2cab7dbfd6
Raphael Roth <rroth@lowrisc.org>
2026-05-07 12:07:27 +0200

    [keymgr_dpe, sw] Add first version of the silicon driver
    This commit introduces a first version of the keymgr_dpe sw in
    the silicon creator lib. It covers the basic keymgr_dpe features.
    
    Add source / header file **without** linking them in the
    appropriate *BUILD* file as some dependencies do not exist yet!
    
    Add an empty keymgr_dpe_unittest.cc file to avoid triggering error
    during the top integration, tracked in Issue #30609.
    
    Signed-off-by: Raphael Roth <rroth@lowrisc.org>

---
+++ sw/device/silicon_creator/lib/drivers/keymgr_dpe_unittest.cc
@@ -0,0 +1,24 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "hw/top/dt/keymgr_dpe.h"
+
+#include <array>
+#include <limits>
+
+#include "gtest/gtest.h"
+#include "sw/device/lib/base/mock_abs_mmio.h"
+#include "sw/device/silicon_creator/lib/base/mock_sec_mmio.h"
+#include "sw/device/silicon_creator/lib/drivers/keymgr_dpe.h"
+#include "sw/device/silicon_creator/lib/error.h"
+
+#include "hw/top/keymgr_dpe_regs.h"  // Generated.
+
+namespace keymgr_dpe_unittest {
+namespace {
+
+// TODO(#30609): write this unittest
+
+}  // namespace
+}  // namespace keymgr_dpe_unittest
