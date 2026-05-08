0a0d3dbe34cd51293091661ba847d3fee13c8ea3
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-05-08 13:26:50 +0100

    [alert,dv] Merge handshake completion covergroups
    These are identical, except for a single value. Pass that as an
    argument to the constructor instead.
    
    The fiddly bit with hjson files is that we have to merge
    alert_agent_basic_testplan and esc_agent_basic_testplan because they
    now each contain the same covergroup.
    
    Note that we'll get one instance of the covergroup per instance of the
    agent, so this won't spuriously merge together coverage data.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/dv/sv/alert_esc_agent/alert_esc_agent_cov.sv
+++ hw/dv/sv/alert_esc_agent/alert_esc_agent_cov.sv
@@ -2,33 +2,24 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-covergroup alert_handshake_complete_cg with function sample(alert_esc_trans_type_e trans,
-                                                            alert_handshake_e      status);
+// A general covergroup to track completion / alerts in an alert or escalation interface. The status
+// input to the sample() function should be an alert_handshake_e or esc_handshake_e enum value, but
+// the function takes an int unsigned (the encoding of the enum) to avoid needing to parameterise
+// the type.
+covergroup handshake_complete_cg (int unsigned complete_status)
+  with function sample(alert_esc_trans_type_e trans,
+                       int unsigned handshake_status);
   option.per_instance = 1;
 
-  cp_handshake_complete: coverpoint status {
-    bins complete = {AlertAckComplete};
+  cp_handshake_complete: coverpoint handshake_status {
+    bins complete = {complete_status};
   }
   cp_trans_type: coverpoint trans {
     bins alert_triggered = {AlertEscSigTrans};
   }
 
   alert_handshake_complete: cross cp_handshake_complete, cp_trans_type;
-endgroup : alert_handshake_complete_cg
-
-covergroup esc_handshake_complete_cg with function sample(alert_esc_trans_type_e trans,
-                                                          esc_handshake_e        status);
-  option.per_instance = 1;
-
-  cp_handshake_complete: coverpoint status {
-    bins complete = {EscRespComplete};
-  }
-  cp_trans_type: coverpoint trans {
-    bins esc_triggered = {AlertEscSigTrans};
-  }
-
-  esc_handshake_complete: cross cp_handshake_complete, cp_trans_type;
-endgroup : esc_handshake_complete_cg
+endgroup : handshake_complete_cg
 
 covergroup alert_esc_trans_cg with function sample(alert_esc_trans_type_e trans);
   option.per_instance = 1;
@@ -48,11 +39,10 @@ endgroup
 // alerts/escalations/pings have been completed.
 class alert_esc_agent_cov extends dv_base_agent_cov #(alert_esc_agent_cfg);
 
-  alert_handshake_complete_cg m_alert_handshake_complete_cg;
-  esc_handshake_complete_cg   m_esc_handshake_complete_cg;
-  alert_esc_trans_cg          m_alert_trans_cg;
-  alert_esc_trans_cg          m_esc_trans_cg;
-  alert_lpg_cg                m_alert_lpg_cg;
+  handshake_complete_cg m_handshake_complete_cg;
+  alert_esc_trans_cg    m_alert_trans_cg;
+  alert_esc_trans_cg    m_esc_trans_cg;
+  alert_lpg_cg          m_alert_lpg_cg;
 
   `uvm_component_utils(alert_esc_agent_cov)
 
@@ -74,6 +64,5 @@ function void alert_esc_agent_cov::build_phase(uvm_phase phase);
   if (cfg.en_lpg_cov && cfg.is_alert) begin
     m_alert_lpg_cg = new();
   end
-  if (cfg.is_alert)    m_alert_handshake_complete_cg = new();
-  else                 m_esc_handshake_complete_cg = new();
+  m_handshake_complete_cg = new(cfg.is_alert ? AlertAckComplete : EscRespComplete);
 endfunction : build_phase
