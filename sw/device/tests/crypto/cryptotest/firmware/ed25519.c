5d71b4fffd5a56c7e3586bba30526a688c303d11
Pascal Nasahl <nasahlpa@lowrisc.org>
2026-05-13 08:47:56 +0200

    [cryptotest] Add Ed25519 Sign to cryptotest firmware
    Return the signature as well as the public key.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/tests/crypto/cryptotest/firmware/ed25519.c
+++ sw/device/tests/crypto/cryptotest/firmware/ed25519.c
@@ -222,6 +222,82 @@ status_t handle_ed25519_sign_check(ujson_t *uj) {
   return OK_STATUS();
 }
 
+// Generates an Ed25519 signature for the provided message and private key.
+// Returns the signature and the derived public key.
+status_t handle_ed25519_sign(ujson_t *uj) {
+  cryptotest_ed25519_sign_mode_t uj_sign_mode;
+  cryptotest_ed25519_message_t uj_message;
+  cryptotest_ed25519_private_key_t uj_private_key;
+
+  TRY(ujson_deserialize_cryptotest_ed25519_sign_mode_t(uj, &uj_sign_mode));
+  TRY(ujson_deserialize_cryptotest_ed25519_message_t(uj, &uj_message));
+  TRY(ujson_deserialize_cryptotest_ed25519_private_key_t(uj, &uj_private_key));
+
+  otcrypto_eddsa_sign_mode_t sign_mode;
+  switch (uj_sign_mode) {
+    case kCryptotestEd25519SignModeEddsa:
+      sign_mode = kOtcryptoEddsaSignModeEddsa;
+      break;
+    case kCryptotestEd25519SignModeHashEddsa:
+      sign_mode = kOtcryptoEddsaSignModeHashEddsa;
+      break;
+    default:
+      LOG_ERROR("Unrecognized Ed25519 sign mode: %d", uj_sign_mode);
+      return INVALID_ARGUMENT();
+  }
+
+  if (uj_private_key.d0_len > ED25519_CMD_PRIVATE_KEY_SHARE_BYTES ||
+      uj_private_key.d1_len > ED25519_CMD_PRIVATE_KEY_SHARE_BYTES) {
+    LOG_ERROR(
+        "Ed25519 private key share too large (have d0 = %d d1 = %d, max = "
+        "%d bytes)",
+        uj_private_key.d0_len, uj_private_key.d1_len,
+        ED25519_CMD_PRIVATE_KEY_SHARE_BYTES);
+    return INVALID_ARGUMENT();
+  }
+
+  uint32_t keyblob[kEd25519MaskedScalarTotalShareWords] = {0};
+  memcpy(keyblob, uj_private_key.d0, uj_private_key.d0_len);
+  memcpy(keyblob + kEd25519MaskedScalarShareWords, uj_private_key.d1,
+         uj_private_key.d1_len);
+
+  otcrypto_blinded_key_t private_key = {
+      .config = kEd25519PrivateKeyConfig,
+      .keyblob_length = sizeof(keyblob),
+      .keyblob = keyblob,
+  };
+  private_key.checksum = otcrypto_integrity_blinded_checksum(&private_key);
+
+  uint32_t public_key_buf[kEd25519PublicKeyWords];
+  otcrypto_unblinded_key_t public_key = {
+      .key_mode = kOtcryptoKeyModeEd25519,
+      .key_length = ED25519_CMD_PUBLIC_KEY_BYTES,
+      .key = public_key_buf,
+  };
+  TRY(otcrypto_ed25519_public_key_from_private(&private_key, &public_key));
+
+  otcrypto_const_byte_buf_t message = OTCRYPTO_MAKE_BUF(
+      otcrypto_const_byte_buf_t, (const uint8_t *)uj_message.input,
+      uj_message.input_len);
+
+  uint32_t signature_data[kEd25519SignatureWords];
+  otcrypto_word32_buf_t signature = OTCRYPTO_MAKE_BUF(
+      otcrypto_word32_buf_t, signature_data, kEd25519SignatureWords);
+
+  TRY(otcrypto_ed25519_sign(&private_key, &message, sign_mode, &signature));
+
+  cryptotest_ed25519_sign_resp_t uj_output;
+  memset(uj_output.signature, 0, ED25519_CMD_SIGNATURE_BYTES);
+  memcpy(uj_output.signature, signature_data, ED25519_CMD_SIGNATURE_BYTES);
+  uj_output.signature_len = ED25519_CMD_SIGNATURE_BYTES;
+  memset(uj_output.public_key, 0, ED25519_CMD_PUBLIC_KEY_BYTES);
+  memcpy(uj_output.public_key, public_key_buf, ED25519_CMD_PUBLIC_KEY_BYTES);
+  uj_output.public_key_len = ED25519_CMD_PUBLIC_KEY_BYTES;
+
+  RESP_OK(ujson_serialize_cryptotest_ed25519_sign_resp_t, uj, &uj_output);
+  return OK_STATUS();
+}
+
 status_t handle_ed25519(ujson_t *uj) {
   cryptotest_ed25519_operation_t uj_op;
   TRY(ujson_deserialize_cryptotest_ed25519_operation_t(uj, &uj_op));
@@ -230,6 +306,8 @@ status_t handle_ed25519(ujson_t *uj) {
       return handle_ed25519_verify(uj);
     case kCryptotestEd25519OperationSignCheck:
       return handle_ed25519_sign_check(uj);
+    case kCryptotestEd25519OperationSign:
+      return handle_ed25519_sign(uj);
     default:
       LOG_ERROR("Unrecognized Ed25519 operation: %d", uj_op);
       return INVALID_ARGUMENT();
