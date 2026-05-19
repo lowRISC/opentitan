1d0c7220dd6dcd148ddcf855cf30b6f1ea68e571
Andrea Caforio <andrea.caforio@lowrisc.org>
2026-05-19 16:43:17 +0200

    [crypto] ML-DSA-87: Implementation of `power2round`
    This commits adds an implementation of the `power2round` function to
    decompose T polynomials into lower-bit and higher-bit polynomials T0
    and T1.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>

---
+++ sw/otbn/crypto/mldsa87/keygen/tests/mldsa87_keygen_power2round_test.s
@@ -0,0 +1,45 @@
+/* Copyright lowRISC contributors (OpenTitan project). */
+/* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+/* Verify that the we can correctly compute `power2round`. */
+
+.section .text.start
+
+main:
+  la x31, _stack
+  bn.xor w31, w31, w31
+
+  la x2, _params
+  bn.lid x0, 0(x2)
+  bn.wsrw MOD, w0
+
+  la x2, _power2round_t
+  la x3, _power2round_t0
+  la x4, _power2round_t1
+  jal x1, power2round
+
+  ecall
+
+.data
+.balign 32
+
+_power2round_t:
+.zero 1024
+_power2round_t0:
+.zero 1024
+_power2round_t1:
+.zero 1024
+
+_params:
+.word 0x007fe001 /* q */
+.word 0xfc7fdfff /* mu */
+.word 0x0000a3fa /* n^-1 * R^3 mod q */
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+
+_stack:
+.zero 256
