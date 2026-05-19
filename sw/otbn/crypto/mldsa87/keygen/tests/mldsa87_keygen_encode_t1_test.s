74d4d6895e0391ce17d8efce5b71583beedb9d6a
Andrea Caforio <andrea.caforio@lowrisc.org>
2026-05-19 14:46:15 +0200

    [crypto] ML-DSA-87: Encoding of T1 polynomials
    A new routine for the encoding of the public key polynomials T1 is
    introduced.
    
    Signed-off-by: Andrea Caforio <andrea.caforio@lowrisc.org>

---
+++ sw/otbn/crypto/mldsa87/keygen/tests/mldsa87_keygen_encode_t1_test.s
@@ -0,0 +1,42 @@
+/* Copyright lowRISC contributors (OpenTitan project). */
+/* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
+/* SPDX-License-Identifier: Apache-2.0 */
+
+/* Verify that the we can correctly encode a T1 polynomial. */
+
+.section .text.start
+
+main:
+  la x31, _stack
+  bn.xor w31, w31, w31
+
+  la x2, _params
+  bn.lid x0, 0(x2)
+  bn.wsrw MOD, w0
+
+  la x2, _encode_t1_t1_dec
+  la x3, _encode_t1_t1_enc
+  jal x1, encode_t1
+
+  ecall
+
+.data
+.balign 32
+
+_encode_t1_t1_dec:
+.zero 1024
+_encode_t1_t1_enc:
+.zero 320
+
+_params:
+.word 0x007fe001 /* q */
+.word 0xfc7fdfff /* mu */
+.word 0x0000a3fa /* n^-1 * R^3 mod q */
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+.word 0x00000000
+
+_stack:
+.zero 256
