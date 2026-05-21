5a5aeb06839e41d7dea99edb8b75a747125ad23c
Pascal Nasahl <nasahlpa@lowrisc.org>
2026-05-21 09:00:58 +0200

    [cryptotest] Split X25519 firmware into cmd handlers
    This enables us to add more new command handlers in the future.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>
    (cherry picked from commit 600d24c45d6f57aafd37b9b37d9a437c758d261d)

--- sw/device/tests/crypto/cryptotest/firmware/x25519.h
+++ sw/device/tests/crypto/cryptotest/firmware/x25519.h
@@ -8,6 +8,7 @@
 #include "sw/device/lib/base/status.h"
 #include "sw/device/lib/ujson/ujson.h"
 
+status_t handle_x25519_shared_secret_generation(ujson_t *uj);
 status_t handle_x25519(ujson_t *uj);
 
 #endif  // OPENTITAN_SW_DEVICE_TESTS_CRYPTO_CRYPTOTEST_FIRMWARE_X25519_H_
