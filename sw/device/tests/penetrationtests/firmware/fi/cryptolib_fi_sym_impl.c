b1cebe849bc3d3e035af338c62391659c0457275
Siemen Dhooghe <sdhooghe@google.com>
2026-05-21 14:35:32 +0200

    [pentest] Add masking
    Add the randomization of inputs in the pentest framework.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>
    (cherry picked from commit 2f1c710e950e26ec8531e5aa677a34bde9b627cb)

--- sw/device/tests/penetrationtests/firmware/fi/cryptolib_fi_sym_impl.c
+++ sw/device/tests/penetrationtests/firmware/fi/cryptolib_fi_sym_impl.c
@@ -4,6 +4,7 @@
 
 #include "sw/device/tests/penetrationtests/firmware/fi/cryptolib_fi_sym_impl.h"
 
+#include "sw/device/lib/base/hardened_memory.h"
 #include "sw/device/lib/base/memory.h"
 #include "sw/device/lib/base/status.h"
 #include "sw/device/lib/crypto/impl/status.h"
@@ -106,17 +107,11 @@ status_t cryptolib_fi_aes_impl(cryptolib_fi_sym_aes_in_t uj_input,
 
   uint32_t key_buf[kPentestAesMaxKeyWords];
   memset(key_buf, 0, AES_CMD_MAX_KEY_BYTES);
-  memcpy(key_buf, uj_input.key, uj_input.key_len);
 
   uint32_t aes_key_mask[kPentestAesMaxKeyWords];
   memset(aes_key_mask, 0, AES_CMD_MAX_KEY_BYTES);
-  for (size_t it = 0; it < kPentestAesMaxKeyWords; it++) {
-    aes_key_mask[it] = pentest_ibex_rnd32_read();
-  }
-
-  for (size_t i = 0; i < key_words; ++i) {
-    key_buf[i] ^= aes_key_mask[i];
-  }
+  hardened_memshred(aes_key_mask, kPentestAesMaxKeyWords);
+  hardened_xor((uint32_t *)uj_input.key, aes_key_mask, key_words, key_buf);
 
   otcrypto_const_word32_buf_t share0 =
       OTCRYPTO_MAKE_BUF(otcrypto_const_word32_buf_t, key_buf, key_words);
@@ -265,17 +260,11 @@ status_t cryptolib_fi_gcm_impl(cryptolib_fi_sym_gcm_in_t uj_input,
 
   uint32_t key_buf[kPentestAesMaxKeyWords];
   memset(key_buf, 0, AES_CMD_MAX_KEY_BYTES);
-  memcpy(key_buf, uj_input.key, uj_input.key_len);
 
   uint32_t aes_key_mask[kPentestAesMaxKeyWords];
   memset(aes_key_mask, 0, AES_CMD_MAX_KEY_BYTES);
-  for (size_t it = 0; it < kPentestAesMaxKeyWords; it++) {
-    aes_key_mask[it] = pentest_ibex_rnd32_read();
-  }
-
-  for (size_t i = 0; i < key_words; ++i) {
-    key_buf[i] ^= aes_key_mask[i];
-  }
+  hardened_memshred(aes_key_mask, kPentestAesMaxKeyWords);
+  hardened_xor((uint32_t *)uj_input.key, aes_key_mask, key_words, key_buf);
 
   otcrypto_const_word32_buf_t share0 =
       OTCRYPTO_MAKE_BUF(otcrypto_const_word32_buf_t, key_buf, key_words);
@@ -398,17 +387,11 @@ status_t cryptolib_fi_hmac_impl(cryptolib_fi_sym_hmac_in_t uj_input,
 
   uint32_t key_buf[kPentestHmacMaxKeyWords];
   memset(key_buf, 0, HMAC_CMD_MAX_KEY_BYTES);
-  memcpy(key_buf, uj_input.key, uj_input.key_len);
 
   uint32_t hmac_key_mask[kPentestHmacMaxKeyWords];
   memset(hmac_key_mask, 0, HMAC_CMD_MAX_KEY_BYTES);
-  for (size_t it = 0; it < kPentestHmacMaxKeyWords; it++) {
-    hmac_key_mask[it] = pentest_ibex_rnd32_read();
-  }
-
-  for (size_t i = 0; i < key_words; ++i) {
-    key_buf[i] ^= hmac_key_mask[i];
-  }
+  hardened_memshred(hmac_key_mask, kPentestHmacMaxKeyWords);
+  hardened_xor((uint32_t *)uj_input.key, hmac_key_mask, key_words, key_buf);
 
   otcrypto_const_word32_buf_t share0 =
       OTCRYPTO_MAKE_BUF(otcrypto_const_word32_buf_t, key_buf, key_words);
@@ -474,17 +457,11 @@ status_t cryptolib_fi_cmac_impl(cryptolib_fi_sym_cmac_in_t uj_input,
 
   uint32_t key_buf[kPentestAesMaxKeyWords];
   memset(key_buf, 0, AES_CMD_MAX_KEY_BYTES);
-  memcpy(key_buf, uj_input.key, uj_input.key_len);
 
   uint32_t cmac_key_mask[kPentestAesMaxKeyWords];
   memset(cmac_key_mask, 0, AES_CMD_MAX_KEY_BYTES);
-  for (size_t it = 0; it < kPentestAesMaxKeyWords; it++) {
-    cmac_key_mask[it] = pentest_ibex_rnd32_read();
-  }
-
-  for (size_t i = 0; i < key_words; ++i) {
-    key_buf[i] ^= cmac_key_mask[i];
-  }
+  hardened_memshred(cmac_key_mask, kPentestAesMaxKeyWords);
+  hardened_xor((uint32_t *)uj_input.key, cmac_key_mask, key_words, key_buf);
 
   otcrypto_const_word32_buf_t share0 =
       OTCRYPTO_MAKE_BUF(otcrypto_const_word32_buf_t, key_buf, key_words);
