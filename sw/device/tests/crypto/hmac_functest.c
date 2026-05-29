64b9ec914ce87f0b2fd2c81932a2ee75e22a50d5
Pascal Nasahl <nasahlpa@lowrisc.org>
2026-05-29 13:08:57 +0200

    [cryptotest] Fix integrity.h dependency
    It lives under sw/device/lib/crypto/include - fix the
    wrong paths in the cryptotests.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/tests/crypto/hmac_functest.c
+++ sw/device/tests/crypto/hmac_functest.c
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "sw/device/lib/crypto/drivers/entropy.h"
-#include "sw/device/lib/crypto/impl/integrity.h"
 #include "sw/device/lib/crypto/impl/keyblob.h"
 #include "sw/device/lib/crypto/impl/status.h"
 #include "sw/device/lib/crypto/include/datatypes.h"
