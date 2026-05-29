9f94b79be1abc368101df38b74311872e591b087
Pascal Nasahl <nasahlpa@lowrisc.org>
2026-05-29 14:31:53 +0200

    [crypto] Switch alert.c to read from correct registers
    The previous commit used the earlgrey_1.0.0 approach of accessing
    registers. Switch this to the master branch approach.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/lib/crypto/drivers/alert.c
+++ sw/device/lib/crypto/drivers/alert.c
@@ -4,82 +4,83 @@
 
 #include "sw/device/lib/crypto/drivers/alert.h"
 
-#include "sw/device/lib/base/abs_mmio.h"
 #include "sw/device/lib/base/bitfield.h"
 #include "sw/device/lib/base/hardened.h"
 #include "sw/device/lib/base/macros.h"
+#include "sw/device/lib/base/mmio.h"
 #include "sw/device/lib/crypto/impl/status.h"
 
-#include "alert_handler_regs.h"  // Generated
+#include "hw/top/alert_handler_regs.h"
+#include "hw/top/sensor_ctrl_regs.h"
 #include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
-#include "sensor_ctrl_regs.h"  // Generated.
 
 uint32_t read_alert_registers(void) {
+  mmio_region_t sensor_ctrl =
+      mmio_region_from_addr(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR);
+  mmio_region_t alert_handler =
+      mmio_region_from_addr(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR);
+
   // Read the sensors
   uint32_t sensors_recov_alerts =
-      abs_mmio_read32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
-                      SENSOR_CTRL_RECOV_ALERT_REG_OFFSET);
+      mmio_region_read32(sensor_ctrl, SENSOR_CTRL_RECOV_ALERT_REG_OFFSET);
   uint32_t sensors_fatal_alerts =
-      abs_mmio_read32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
-                      SENSOR_CTRL_FATAL_ALERT_REG_OFFSET);
+      mmio_region_read32(sensor_ctrl, SENSOR_CTRL_FATAL_ALERT_REG_OFFSET);
   uint32_t combined_alerts = sensors_recov_alerts | sensors_fatal_alerts;
 
   // Read out the alerts
   for (uint32_t alert = 0; alert < ALERT_HANDLER_PARAM_N_ALERTS; alert++) {
-    uint32_t cause = abs_mmio_read32(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
-                                     (alert * sizeof(uint32_t)) +
-                                     ALERT_HANDLER_ALERT_CAUSE_0_REG_OFFSET);
+    uint32_t cause = mmio_region_read32(
+        alert_handler, (ptrdiff_t)(alert * sizeof(uint32_t)) +
+                           ALERT_HANDLER_ALERT_CAUSE_0_REG_OFFSET);
     combined_alerts |= cause;
   }
   // Read out the local alerts
   for (uint32_t local_alert = 0; local_alert < ALERT_HANDLER_PARAM_N_LOC_ALERT;
        local_alert++) {
-    uint32_t local_cause =
-        abs_mmio_read32(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
-                        (local_alert * sizeof(uint32_t)) +
-                        ALERT_HANDLER_LOC_ALERT_CAUSE_0_REG_OFFSET);
+    uint32_t local_cause = mmio_region_read32(
+        alert_handler, (ptrdiff_t)(local_alert * sizeof(uint32_t)) +
+                           ALERT_HANDLER_LOC_ALERT_CAUSE_0_REG_OFFSET);
     combined_alerts |= local_cause;
   }
   return combined_alerts;
 }
 
 status_t init_alert_registers(void) {
+  mmio_region_t sensor_ctrl =
+      mmio_region_from_addr(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR);
+  mmio_region_t alert_handler =
+      mmio_region_from_addr(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR);
+
   // Clear the sensors.
-  abs_mmio_write32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
-                       SENSOR_CTRL_RECOV_ALERT_REG_OFFSET,
-                   SENSOR_CTRL_RECOV_ALERT_REG_RESVAL);
-  abs_mmio_write32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
-                       SENSOR_CTRL_FATAL_ALERT_REG_OFFSET,
-                   SENSOR_CTRL_FATAL_ALERT_REG_RESVAL);
-  abs_mmio_write32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
-                       SENSOR_CTRL_ALERT_TEST_REG_OFFSET,
-                   SENSOR_CTRL_ALERT_TEST_REG_RESVAL);
-  abs_mmio_write32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
-                       SENSOR_CTRL_ALERT_TRIG_REG_OFFSET,
-                   SENSOR_CTRL_ALERT_TRIG_REG_RESVAL);
-  abs_mmio_write32(TOP_EARLGREY_SENSOR_CTRL_AON_BASE_ADDR +
-                       SENSOR_CTRL_INTR_STATE_REG_OFFSET,
-                   SENSOR_CTRL_INTR_STATE_REG_RESVAL);
+  mmio_region_write32(sensor_ctrl, SENSOR_CTRL_RECOV_ALERT_REG_OFFSET,
+                      SENSOR_CTRL_RECOV_ALERT_REG_RESVAL);
+  mmio_region_write32(sensor_ctrl, SENSOR_CTRL_FATAL_ALERT_REG_OFFSET,
+                      SENSOR_CTRL_FATAL_ALERT_REG_RESVAL);
+  mmio_region_write32(sensor_ctrl, SENSOR_CTRL_ALERT_TEST_REG_OFFSET,
+                      SENSOR_CTRL_ALERT_TEST_REG_RESVAL);
+  mmio_region_write32(sensor_ctrl, SENSOR_CTRL_ALERT_TRIG_REG_OFFSET,
+                      SENSOR_CTRL_ALERT_TRIG_REG_RESVAL);
+  mmio_region_write32(sensor_ctrl, SENSOR_CTRL_INTR_STATE_REG_OFFSET,
+                      SENSOR_CTRL_INTR_STATE_REG_RESVAL);
 
   // Clear the alerts.
   for (uint32_t alert = 0; alert < ALERT_HANDLER_PARAM_N_ALERTS; alert++) {
-    abs_mmio_write32(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
-                         (alert * sizeof(uint32_t)) +
-                         ALERT_HANDLER_ALERT_CAUSE_0_REG_OFFSET,
-                     0x1);
+    mmio_region_write32(alert_handler,
+                        (ptrdiff_t)(alert * sizeof(uint32_t)) +
+                            ALERT_HANDLER_ALERT_CAUSE_0_REG_OFFSET,
+                        0x1);
   }
 
   // Clear the local alerts.
   for (uint32_t local_alert = 0; local_alert < ALERT_HANDLER_PARAM_N_LOC_ALERT;
        local_alert++) {
-    abs_mmio_write32(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
-                         (local_alert * sizeof(uint32_t)) +
-                         ALERT_HANDLER_LOC_ALERT_CAUSE_0_REG_OFFSET,
-                     0x1);
+    mmio_region_write32(alert_handler,
+                        (ptrdiff_t)(local_alert * sizeof(uint32_t)) +
+                            ALERT_HANDLER_LOC_ALERT_CAUSE_0_REG_OFFSET,
+                        0x1);
   }
 
-  abs_mmio_write32(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR +
-                       ALERT_HANDLER_INTR_STATE_REG_OFFSET,
-                   0xffffffff);
+  mmio_region_write32(alert_handler, ALERT_HANDLER_INTR_STATE_REG_OFFSET,
+                      0xffffffff);
   return OTCRYPTO_OK;
 }
