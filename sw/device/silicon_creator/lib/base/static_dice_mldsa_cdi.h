f1909b04afdcd5b7c41e73de38d144af3be8b221
Yi-Hsuan Deng <yhdeng@google.com>
2026-06-11 03:14:14 +0000

    [dice] Add static_dice_mldsa_cdi region
    Introduce a static BSS memory section `.static_dice.mldsa_cdi` to hold
    the post-quantum ML-DSA DICE chain certificates and public keys. This section
    is located at a fixed location in memory, immediately following the ECDSA CDI_0
    section in the `.static_dice` RAM block.
    
    This allows the post-quantum DICE chain data to be passed cleanly from
    imm_section to ROM_EXT.
    
    Specifically, this change:
    1. Defines the `static_dice_mldsa_cdi_t` struct (18984 bytes) and declares
       its member offsets in a new header `static_dice_mldsa_cdi.h`.
    2. Allocates the global symbol `static_dice_mldsa_cdi` in the new section
       `.static_dice.mldsa_cdi` in `static_dice_mldsa_cdi.c`.
    3. Updates `static_dice.ld` to support and assert on the 18984-byte ML-DSA
       DICE CDI region.
    4. Adds the corresponding BUILD targets and links the new sections into
       the OTTF test framework to prevent OTTF overwrite this region when linked
       with the static_dice_mldsa_cdi library.
    
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>
    Change-Id: I7fdf2b047b9148766e7b0c98ebe278476a6a6964

---
+++ sw/device/silicon_creator/lib/base/static_dice_mldsa_cdi.h
@@ -0,0 +1,31 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_BASE_STATIC_DICE_MLDSA_CDI_H_
+#define OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_BASE_STATIC_DICE_MLDSA_CDI_H_
+
+#include <stdint.h>
+
+#include "sw/device/lib/base/macros.h"
+
+// The post-quantum DICE chain information passed from imm_section to
+// ROM_EXT, and from ROM_EXT to the Owner image.
+typedef struct {
+  uint8_t uds_pub[2592];
+  uint8_t cdi_0_cert[8192];
+  uint32_t cdi_0_cert_size;
+  uint8_t cdi_1_cert[8192];
+  uint32_t cdi_1_cert_size;
+} static_dice_mldsa_cdi_t;
+
+OT_ASSERT_MEMBER_OFFSET(static_dice_mldsa_cdi_t, uds_pub, 0);
+OT_ASSERT_MEMBER_OFFSET(static_dice_mldsa_cdi_t, cdi_0_cert, 2592);
+OT_ASSERT_MEMBER_OFFSET(static_dice_mldsa_cdi_t, cdi_0_cert_size, 10784);
+OT_ASSERT_MEMBER_OFFSET(static_dice_mldsa_cdi_t, cdi_1_cert, 10788);
+OT_ASSERT_MEMBER_OFFSET(static_dice_mldsa_cdi_t, cdi_1_cert_size, 18980);
+OT_ASSERT_SIZE(static_dice_mldsa_cdi_t, 18984);
+
+extern static_dice_mldsa_cdi_t static_dice_mldsa_cdi;
+
+#endif  // OPENTITAN_SW_DEVICE_SILICON_CREATOR_LIB_BASE_STATIC_DICE_MLDSA_CDI_H_
