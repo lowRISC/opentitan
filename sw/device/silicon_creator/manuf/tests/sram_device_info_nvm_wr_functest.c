c997d6f64c2d4dbaab79fa8782af4e6f078c0963
Michael Gautschi <mgautschi@lowrisc.org>
2026-06-12 18:04:56 +0200

    [nvm_testutils,sw] use nvm_testutils in manuf/tests
    Signed-off-by: Michael Gautschi <mgautschi@lowrisc.org>

---
+++ sw/device/silicon_creator/manuf/tests/sram_device_info_nvm_wr_functest.c
@@ -0,0 +1,84 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdint.h>
+
+#include "sw/device/lib/arch/device.h"
+#include "sw/device/lib/dif/dif_lc_ctrl.h"
+#include "sw/device/lib/dif/dif_otp_ctrl.h"
+#include "sw/device/lib/dif/dif_uart.h"
+#include "sw/device/lib/runtime/log.h"
+#include "sw/device/lib/testing/nvm_testutils.h"
+#include "sw/device/lib/testing/otp_ctrl_testutils.h"
+#include "sw/device/lib/testing/pinmux_testutils.h"
+#include "sw/device/lib/testing/test_framework/check.h"
+#include "sw/device/lib/testing/test_framework/ottf_console.h"
+#include "sw/device/lib/testing/test_framework/ottf_test_config.h"
+#include "sw/device/silicon_creator/manuf/lib/individualize_sw_cfg.h"
+#include "sw/device/silicon_creator/manuf/lib/nvm_info_field.h"
+#include "sw/device/silicon_creator/manuf/tests/test_wafer_auth_secret.h"
+
+#include "hw/top/otp_ctrl_regs.h"  // Generated.
+#include "hw/top_earlgrey/sw/autogen/top_earlgrey.h"
+
+OTTF_DEFINE_TEST_CONFIG();
+
+static dif_otp_ctrl_t otp_ctrl;
+static dif_pinmux_t pinmux;
+static dif_lc_ctrl_t lc_ctrl;
+
+/**
+ * Initializes all DIF handles used in this SRAM program.
+ */
+static status_t peripheral_handles_init(void) {
+  TRY(dif_lc_ctrl_init(
+      mmio_region_from_addr(TOP_EARLGREY_LC_CTRL_REGS_BASE_ADDR), &lc_ctrl));
+  TRY(dif_otp_ctrl_init(
+      mmio_region_from_addr(TOP_EARLGREY_OTP_CTRL_CORE_BASE_ADDR), &otp_ctrl));
+  TRY(dif_pinmux_init(mmio_region_from_addr(TOP_EARLGREY_PINMUX_AON_BASE_ADDR),
+                      &pinmux));
+  return OK_STATUS();
+}
+
+bool test_main(void) {
+  CHECK_STATUS_OK(peripheral_handles_init());
+
+  // Initialize UART (for console, since we do not have the OTTF).
+  pinmux_testutils_init(&pinmux);
+  ottf_console_init();
+
+  LOG_INFO("Executing from SRAM.");
+
+  // Read LC state.
+  dif_lc_ctrl_state_t lc_state = kDifLcCtrlStateInvalid;
+  CHECK_DIF_OK(dif_lc_ctrl_get_state(&lc_ctrl, &lc_state));
+
+  switch (lc_state) {
+    case kDifLcCtrlStateTestUnlocked0:
+    case kDifLcCtrlStateTestUnlocked1:
+    case kDifLcCtrlStateTestUnlocked2:
+    case kDifLcCtrlStateTestUnlocked3:
+    case kDifLcCtrlStateTestUnlocked4:
+    case kDifLcCtrlStateTestUnlocked5:
+    case kDifLcCtrlStateTestUnlocked6:
+    case kDifLcCtrlStateTestUnlocked7:
+      LOG_INFO("Writing to the isolated NVM partition.");
+      CHECK_STATUS_OK(nvm_testutils_info_page_setup(
+          kNvmInfoFieldWaferAuthSecret.page, kPageReadWrite, kPagePlainCfg));
+      CHECK_STATUS_OK(nvm_testutils_write_info_page(
+          kNvmInfoFieldWaferAuthSecret.page,
+          kNvmInfoFieldWaferAuthSecret.byte_offset, kExpectedWaferAuthSecret,
+          kNvmInfoFieldWaferAuthSecretSizeIn32BitWords,
+          /*erase_before_write=*/true, /*readback=*/false));
+      LOG_INFO("Enabling ROM execution to enable bootstrap after reset.");
+      CHECK_STATUS_OK(manuf_individualize_device_creator_sw_cfg(&otp_ctrl));
+      CHECK_STATUS_OK(manuf_individualize_device_owner_sw_cfg(&otp_ctrl));
+      LOG_INFO("Done. Perform an LC transition and run NVM stage.");
+      break;
+    default:
+      return false;
+  }
+
+  return true;
+}
