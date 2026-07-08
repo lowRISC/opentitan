22da8ff46782b0fa715d4c4c7cba8bcbd0764378
Raphael Roth <rroth@lowrisc.org>
2026-07-08 11:37:30 +0200

    [keymgr_dpe, sw, dv] Introduce the `keymgr_dpe_key_derivation` test
    Replace `chip_sw_keymgr_key_derivation_vseq` with a `keymgr_dpe` variant
    and port the corresponding SW test. Disable the test for `darjeeling` due
    to differences in the boot_process.
    
    Signed-off-by: Raphael Roth <rroth@lowrisc.org>

--- hw/top_earlgrey/dv/env/seq_lib/chip_vseq_list.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_vseq_list.sv
@@ -58,7 +58,7 @@
 `include "chip_sw_spi_device_pinmux_sleep_retention_vseq.sv"
 `include "chip_sw_sleep_pin_wake_vseq.sv"
 `include "chip_sw_sleep_pin_retention_vseq.sv"
-//`include "chip_sw_keymgr_key_derivation_vseq.sv"
+`include "chip_sw_keymgr_dpe_key_derivation_vseq.sv"
 //`include "chip_sw_keymgr_sideload_kmac_vseq.sv"
 //`include "chip_sw_keymgr_sideload_aes_vseq.sv"
 `include "chip_sw_ast_clk_outputs_vseq.sv"
