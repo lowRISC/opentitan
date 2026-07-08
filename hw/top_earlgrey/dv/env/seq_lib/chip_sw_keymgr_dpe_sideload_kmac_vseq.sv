4b44950eb4c8ab8a0f4970e0e04708a8d50aa783
Raphael Roth <rroth@lowrisc.org>
2026-07-08 15:01:16 +0200

    [keymgr_dpe, sw, dv] Introduce the `keymgr_dpe_sideload_kmac` test
    Signed-off-by: Raphael Roth <rroth@lowrisc.org>

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_keymgr_dpe_sideload_kmac_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_keymgr_dpe_sideload_kmac_vseq.sv
@@ -2,8 +2,8 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-class chip_sw_keymgr_sideload_kmac_vseq extends chip_sw_keymgr_key_derivation_vseq;
-  `uvm_object_utils(chip_sw_keymgr_sideload_kmac_vseq)
+class chip_sw_keymgr_dpe_sideload_kmac_vseq extends chip_sw_keymgr_dpe_key_derivation_vseq;
+  `uvm_object_utils(chip_sw_keymgr_dpe_sideload_kmac_vseq)
 
   `uvm_object_new
 
@@ -14,13 +14,22 @@ class chip_sw_keymgr_sideload_kmac_vseq extends chip_sw_keymgr_key_derivation_vs
   localparam int DigestBytes = 32;
   localparam int KeyBytes = keymgr_pkg::KeyWidth / 8;
 
-  virtual task check_op_in_owner_int_state(bit [keymgr_pkg::KeyWidth-1:0] unmasked_key);
+  virtual task run_test_sequence(key_shares_t creator_key);
     bit [keymgr_pkg::KeyWidth-1:0] sideload_kmac_key;
     bit [7:0] sideload_key_arr[KeyBytes];
     bit [7:0] digest_arr[DigestBytes];
 
-    check_kmac_sideload(unmasked_key, sideload_kmac_key);
+    // Wait until the sideloaded key is generated
+    `DV_WAIT(cfg.sw_logger_vif.printed_log == "KeymgrDpe generated HW output for Kmac from the CreatorRootKey")
 
+    // Check if the generated key matches the expected key
+    check_generated_output(.key_shares(creator_key),
+                           .dest(keymgr_pkg::Kmac),
+                           .version(kVersionVersionedKey),
+                           .salt(kSaltVersionedKey));
+
+    // Fetch the generated key via backdoor from the HW!
+    sideload_kmac_key = get_unmasked_key(get_output(keymgr_pkg::Kmac));
     {<< byte {sideload_key_arr}} = sideload_kmac_key;
 
     c_dpi_kmac128(MsgArr, MessageBytes,
@@ -31,4 +40,4 @@ class chip_sw_keymgr_sideload_kmac_vseq extends chip_sw_keymgr_key_derivation_vs
     sw_symbol_backdoor_overwrite("sideload_digest_result", digest_arr);
   endtask
 
-endclass : chip_sw_keymgr_sideload_kmac_vseq
+endclass : chip_sw_keymgr_dpe_sideload_kmac_vseq
