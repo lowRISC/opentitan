9b1a4b9983535eee0f798bdefefd9ed03e8558d0
Michael Gautschi <mgautschi@lowrisc.org>
2026-07-09 18:13:38 +0200

    [pinmux_aon,rtl] Improve JTAG Muxing
    The three jtag request (lc_jtag, dft_jtag, rv_jtag) are now gated with
    an AND-gate instead of a combinatorial process. This eliminates the risk
    of a glitch on the jtag clock or reset line.
    
    Signed-off-by: Michael Gautschi <mgautschi@lowrisc.org>

--- hw/top_englishbreakfast/ip_autogen/pinmux/rtl/pinmux_jtag_buf.sv
+++ hw/top_englishbreakfast/ip_autogen/pinmux/rtl/pinmux_jtag_buf.sv
@@ -3,18 +3,29 @@
 // SPDX-License-Identifier: Apache-2.0
 
 module pinmux_jtag_buf (
+  // Gates TCK/TRSTN below. Holds this TAP's clock off and reset asserted when deselected.
+  input  logic                en_i,
   input  jtag_pkg::jtag_req_t req_i,
   output jtag_pkg::jtag_req_t req_o,
   input  jtag_pkg::jtag_rsp_t rsp_i,
   output jtag_pkg::jtag_rsp_t rsp_o
 );
 
+  logic tck_gated;
+  prim_and2 #(.Width(1)) u_prim_and2_tck (
+    .in0_i(en_i),
+    .in1_i(req_i.tck),
+    .out_o(tck_gated)
+  );
+  // This buffer is used in the constraints to define a generated clock
   prim_clock_buf prim_clock_buf_tck (
-    .clk_i(req_i.tck),
+    .clk_i(tck_gated),
     .clk_o(req_o.tck)
   );
-  prim_buf prim_buf_trst_n (
-    .in_i (req_i.trst_n),
+
+  prim_and2 #(.Width(1)) u_prim_and2_trst_n (
+    .in0_i(en_i),
+    .in1_i(req_i.trst_n),
     .out_o(req_o.trst_n)
   );
   prim_buf prim_buf_tms (
