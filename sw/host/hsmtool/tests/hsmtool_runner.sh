270d25d25f860c8250fe0d00153cc8df6f0bc31c
Luís Marques <luismarques@lowrisc.org>
2026-07-21 11:49:09 +0100

    [hsmtool] Ensure hsmtool test can access token dir
    Without this hook, SoftHSM2 sees the symlinks of its token directory,
    not the actual files, and cannot load the tokens.
    
    Signed-off-by: Luís Marques <luismarques@lowrisc.org>

--- sw/host/hsmtool/tests/hsmtool_runner.sh
+++ sw/host/hsmtool/tests/hsmtool_runner.sh
@@ -3,5 +3,8 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
+set -euo pipefail
+source sw/host/hsmtool/tests/test_lib.sh
+
 readonly HSMTOOL=sw/host/hsmtool/hsmtool
 ${HSMTOOL} "$@"
