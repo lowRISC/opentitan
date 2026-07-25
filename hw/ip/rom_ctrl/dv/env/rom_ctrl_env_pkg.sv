1ca39b6ad2f49bc98db8a55071cb0936bf140294
Rupert Swarbrick <rswarbrick@lowrisc.org>
2026-07-25 21:50:00 +0100

    [rom_ctrl,dv] Define a vseq to control how ROM checking gets skipped
    This is designed for use at chip level, where the test will be able to
    configure the virtual sequence to track reset events and to be able to
    force signals in the rom_ctrl FSM.
    
    Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

--- hw/ip/rom_ctrl/dv/env/rom_ctrl_env_pkg.sv
+++ hw/ip/rom_ctrl/dv/env/rom_ctrl_env_pkg.sv
@@ -66,6 +66,8 @@ package rom_ctrl_env_pkg;
   typedef uvm_sequencer #(rom_ctrl_kmac_rsp_force_item) rom_ctrl_kmac_rsp_force_sequencer_t;
   `include "seq_lib/rom_ctrl_override_digest_seq.svh"
 
+  `include "seq_lib/rom_ctrl_skip_middle_with_digest_vseq.svh"
+
   `include "rom_ctrl_env_cfg.sv"
   `include "rom_ctrl_env_cov.sv"
   `include "rom_ctrl_virtual_sequencer.sv"
