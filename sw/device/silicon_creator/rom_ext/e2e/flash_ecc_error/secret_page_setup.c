fc90d114bae728aff023f4a0d5609c69cc315ed0
Raphael Roth <rroth@lowrisc.org>
2026-08-03 14:30:26 +0200

    [keymgr_dpe, rom_ext] Use the `keymgr_dpe` testutils in secret_page_setup
    Signed-off-by: Raphael Roth <rroth@lowrisc.org>

--- sw/device/silicon_creator/rom_ext/e2e/flash_ecc_error/secret_page_setup.c
+++ sw/device/silicon_creator/rom_ext/e2e/flash_ecc_error/secret_page_setup.c
@@ -6,12 +6,12 @@
 #include <stdint.h>
 
 #include "sw/device/lib/base/status.h"
-#include "sw/device/lib/testing/keymgr_testutils.h"
+#include "sw/device/lib/testing/keymgr_dpe_testutils.h"
 #include "sw/device/lib/testing/test_framework/ottf_main.h"
 
 OTTF_DEFINE_TEST_CONFIG();
 
 bool test_main(void) {
-  status_t result = keymgr_testutils_init_nvm_then_reset();
+  status_t result = keymgr_dpe_testutils_init_nvm_then_reset();
   return status_ok(result);
 }
