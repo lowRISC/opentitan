cb3e7aea55038e23ba34654dafff4ef3040dab80
Siemen Dhooghe <sdhooghe@google.com>
2026-08-06 22:41:09 +0200

    [crypto] Make hash then sign/verify ECC and RSA calls
    FIPS does not allow pre-hash modes for ECC and RSA, hence we integrate
    the pure modes in the library.
    In order to save code size, we make a new file hash.c which implements a hash
    dispatch table. This is then used by ECC and RSA (which RSA already
    had).
    
    The RSA PCT changed to use the pure mode.
    
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>
    (cherry picked from commit efed577d52d1f192b35dfebf9287b7ab84a84802)

--- sw/device/tests/crypto/otcrypto_interface.c
+++ sw/device/tests/crypto/otcrypto_interface.c
@@ -184,7 +184,9 @@ volatile otcrypto_interface_t otcrypto = {
 
     // RSA signatures (blocking).
     .rsa_sign = &otcrypto_rsa_sign,
+    .rsa_hash_sign_verify = &otcrypto_rsa_hash_sign_verify,
     .rsa_verify = &otcrypto_rsa_verify,
+    .rsa_hash_verify = &otcrypto_rsa_hash_verify,
 
     // RSA signatures (async).
     .rsa_sign_async_start = &otcrypto_rsa_sign_async_start,
@@ -207,8 +209,10 @@ volatile otcrypto_interface_t otcrypto = {
     .ecdsa_p256_dice_keygen = &otcrypto_ecdsa_p256_dice_keygen,
     .ecdsa_p256_sign_config_k = &otcrypto_ecdsa_p256_sign_config_k,
     .ecdsa_p256_sign = &otcrypto_ecdsa_p256_sign,
+    .ecdsa_p256_hash_sign_verify = &otcrypto_ecdsa_p256_hash_sign_verify,
     .ecdsa_p256_sign_verify = &otcrypto_ecdsa_p256_sign_verify,
     .ecdsa_p256_verify = &otcrypto_ecdsa_p256_verify,
+    .ecdsa_p256_hash_verify = &otcrypto_ecdsa_p256_hash_verify,
 
     // ECDH P-256 (blocking).
     .ecdh_p256_keygen = &otcrypto_ecdh_p256_keygen,
@@ -254,9 +258,11 @@ volatile otcrypto_interface_t otcrypto = {
     // ECDSA P-384 (blocking).
     .ecdsa_p384_keygen = &otcrypto_ecdsa_p384_keygen,
     .ecdsa_p384_sign = &otcrypto_ecdsa_p384_sign,
+    .ecdsa_p384_hash_sign_verify = &otcrypto_ecdsa_p384_hash_sign_verify,
     .ecdsa_p384_sign_config_k = &otcrypto_ecdsa_p384_sign_config_k,
     .ecdsa_p384_sign_verify = &otcrypto_ecdsa_p384_sign_verify,
     .ecdsa_p384_verify = &otcrypto_ecdsa_p384_verify,
+    .ecdsa_p384_hash_verify = &otcrypto_ecdsa_p384_hash_verify,
 
     // ECDH P-384 (blocking).
     .ecdh_p384_keygen = &otcrypto_ecdh_p384_keygen,
