e5c95cecb474d71cdc39017ad2c22c7c2a2a4f07
Lovepreet Singh <lpsingh@google.com>
2026-08-10 14:44:42 -0700

    [dice,ot_certs] Add unit test with golden data for DICE UDS certificate generation
    I am planning to change `sw/device/silicon_creator/lib/cert/uds.hjson`
    to support MLDSA public keys and signatures. To make sure that those
    changes still allow existing EC-DSA P256 certificates to be generated
    and parsed without any behavior change, I am adding a unit test to
    generate TBS certificate and endorsed certificate using DICE UDS
    template, and compare it against data I captured from
    `//sw/host/provisioning/orchestrator/tests:e2e_emulation_cw340_test` at
    commit `f11678e9553c8e4e612a30a40c1de488965209de`
    
    I did not use firmware code in
    `//sw/device/silicon_creator/lib/cert:dice_cert_build` since one of its
    dependency `//sw/device/silicon_creator/lib:otbn_boot_services` does not
    build on the host platform
    
    Signed-off-by: Lovepreet Singh <lpsingh@google.com>

---
+++ sw/host/ot_certs/tests/uds_golden_tests.rs
@@ -0,0 +1,51 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+use core::include_bytes;
+
+use ot_certs::asn1::{der, x509};
+use ot_certs::template::Template;
+use ot_certs::template::subst::{Subst, SubstData};
+use ot_certs::x509::generate_certificate;
+
+const UDS_CERT_TEMPLATE: &str =
+    include_str!("../../../../sw/device/silicon_creator/lib/cert/uds.hjson");
+const GOLDEN_ECDSA_TBS_INPUT_DATA: &str = include_str!("uds_ecdsa_tbs_input_golden.json");
+const GOLDEN_ECDSA_TBS_OUTPUT_DATA: &[u8] = include_bytes!("uds_ecdsa_tbs_golden.der");
+const GOLDEN_ECDSA_ENDORSED_CERT_INPUT_DATA: &str =
+    include_str!("uds_ecdsa_endorsed_cert_input_golden.json");
+const GOLDEN_ECDSA_ENDORSED_CERT_OUTPUT_DATA: &[u8] =
+    include_bytes!("uds_ecdsa_endorsed_cert_golden.der");
+
+#[test]
+fn compare_uds_ecdsa_tbs() {
+    let uds_template = Template::from_hjson_str(UDS_CERT_TEMPLATE)
+        .expect("UDS certificate template must be parseable as HJSON");
+    let ecdsa_test_data = SubstData::from_json(GOLDEN_ECDSA_TBS_INPUT_DATA)
+        .expect("Golden data for ECDSA must be parseable as JSON");
+    let subst_template = uds_template
+        .subst(&ecdsa_test_data)
+        .expect("Template substitution for UDS ECDSA TBS must succeed");
+    let tbs = der::Der::generate(|builder| {
+        x509::X509::push_tbs_certificate(builder, &subst_template.certificate)
+    })
+    .expect("TBS UDS certificate generation to succeed");
+
+    assert_eq!(tbs, GOLDEN_ECDSA_TBS_OUTPUT_DATA);
+}
+
+#[test]
+fn compare_uds_ecdsa_endorsed_cert() {
+    let uds_template = Template::from_hjson_str(UDS_CERT_TEMPLATE)
+        .expect("UDS certificate template must be parseable as HJSON");
+    let ecdsa_test_data = SubstData::from_json(GOLDEN_ECDSA_ENDORSED_CERT_INPUT_DATA)
+        .expect("Golden data for ECDSA must be parseable as JSON");
+    let subst_template = uds_template
+        .subst(&ecdsa_test_data)
+        .expect("Template substitution for UDS ECDSA endorsed certificate must succeed");
+    let endorsed_cert = generate_certificate(&subst_template)
+        .expect("Certificate generation for UDS ECDSA certificate must succeed");
+
+    assert_eq!(endorsed_cert, GOLDEN_ECDSA_ENDORSED_CERT_OUTPUT_DATA);
+}
