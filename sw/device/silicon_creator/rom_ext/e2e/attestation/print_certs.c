762885a022bf3d20b4bdc21160190958f6f3f5fb
Yi-Hsuan Deng <yhdeng@google.com>
2026-08-13 11:37:18 +0000

    [perso_tlv_data] Support per-object version prefix in device TLV library and firmware
    Add 4-byte V1 version prefix (0x010004F0) handling to device perso_tlv_data.
    Update perso_tlv_get_cert_obj, perso_tlv_object_type, and perso_tlv_object_size
    to detect object version from buffer without external blob_version arguments.
    Update ft_personalize.c and test callers accordingly.
    
    TAG=agy
    CONV=f24c5c61-71d1-4fc3-aef0-af78d1125515
    
    Signed-off-by: Yi-Hsuan Deng <yhdeng@google.com>
    Change-Id: I0db63e5d5adf3fa35d9a95fd0bceb4296a6a6964

--- sw/device/silicon_creator/rom_ext/e2e/attestation/print_certs.c
+++ sw/device/silicon_creator/rom_ext/e2e/attestation/print_certs.c
@@ -49,8 +49,7 @@ static status_t print_cert(char *dest,
   size_t len = sizeof(data);
   while (true) {
     perso_tlv_cert_obj_t obj = {0};
-    rom_error_t err =
-        perso_tlv_get_cert_obj(data + offset, len, kPersoBlobVersionV0, &obj);
+    rom_error_t err = perso_tlv_get_cert_obj(data + offset, len, &obj);
     if (err != kErrorOk) {
       break;
     }
