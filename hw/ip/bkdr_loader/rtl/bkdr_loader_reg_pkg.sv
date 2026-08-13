780b335929cbc78faf9ec76d3d0c7d42e19ee4f9
Michael Gautschi <mgautschi@lowrisc.org>
2026-08-13 20:50:41 +0200

    [rtl,bkdr_loader] remove flash targets
    Flash has been removed for earlgrey. It can no longer be accessed by the
    bkdr loader.
    
    Signed-off-by: Michael Gautschi <mgautschi@lowrisc.org>

--- hw/ip/bkdr_loader/rtl/bkdr_loader_reg_pkg.sv
+++ hw/ip/bkdr_loader/rtl/bkdr_loader_reg_pkg.sv
@@ -7,7 +7,7 @@
 package bkdr_loader_reg_pkg;
 
   // Param list
-  parameter int unsigned NumBkdrTgts = 14;
+  parameter int unsigned NumBkdrTgts = 6;
   parameter int unsigned MaxWordWidthDiv32 = 8;
   parameter int unsigned TargetIdxWidth = 8;
 
@@ -15,7 +15,7 @@ package bkdr_loader_reg_pkg;
   parameter int RegsAw = 11;
 
   // Number of registers for every interface
-  parameter int NumRegsRegs = 78;
+  parameter int NumRegsRegs = 46;
 
   ///////////////////////////////////////////////
   // Typedefs for registers for regs interface //
@@ -119,26 +119,26 @@ package bkdr_loader_reg_pkg;
 
   // Register -> HW type for regs interface
   typedef struct packed {
-    bkdr_loader_reg2hw_control_reg_t control; // [1071:1055]
-    bkdr_loader_reg2hw_mission_mode_switch_delay_reg_t mission_mode_switch_delay; // [1054:1023]
-    bkdr_loader_reg2hw_read_data_mreg_t [7:0] read_data; // [1022:759]
-    bkdr_loader_reg2hw_write_data_mreg_t [7:0] write_data; // [758:495]
-    bkdr_loader_reg2hw_index_reg_t index; // [494:462]
-    bkdr_loader_reg2hw_hash_last_loaded_mreg_t [13:0] hash_last_loaded; // [461:0]
+    bkdr_loader_reg2hw_control_reg_t control; // [807:791]
+    bkdr_loader_reg2hw_mission_mode_switch_delay_reg_t mission_mode_switch_delay; // [790:759]
+    bkdr_loader_reg2hw_read_data_mreg_t [7:0] read_data; // [758:495]
+    bkdr_loader_reg2hw_write_data_mreg_t [7:0] write_data; // [494:231]
+    bkdr_loader_reg2hw_index_reg_t index; // [230:198]
+    bkdr_loader_reg2hw_hash_last_loaded_mreg_t [5:0] hash_last_loaded; // [197:0]
   } bkdr_loader_regs_reg2hw_t;
 
   // HW -> register type for regs interface
   typedef struct packed {
-    bkdr_loader_hw2reg_status_reg_t status; // [2148:2147]
-    bkdr_loader_hw2reg_control_reg_t control; // [2146:2145]
-    bkdr_loader_hw2reg_num_bkdr_targets_reg_t num_bkdr_targets; // [2144:2113]
-    bkdr_loader_hw2reg_usr_access_timestamp_reg_t usr_access_timestamp; // [2112:2081]
-    bkdr_loader_hw2reg_target_info_mreg_t [13:0] target_info; // [2080:1633]
-    bkdr_loader_hw2reg_width_info_mreg_t [13:0] width_info; // [1632:1185]
-    bkdr_loader_hw2reg_depth_info_mreg_t [13:0] depth_info; // [1184:737]
-    bkdr_loader_hw2reg_read_data_mreg_t [7:0] read_data; // [736:481]
-    bkdr_loader_hw2reg_index_reg_t index; // [480:448]
-    bkdr_loader_hw2reg_hash_last_loaded_mreg_t [13:0] hash_last_loaded; // [447:0]
+    bkdr_loader_hw2reg_status_reg_t status; // [1124:1123]
+    bkdr_loader_hw2reg_control_reg_t control; // [1122:1121]
+    bkdr_loader_hw2reg_num_bkdr_targets_reg_t num_bkdr_targets; // [1120:1089]
+    bkdr_loader_hw2reg_usr_access_timestamp_reg_t usr_access_timestamp; // [1088:1057]
+    bkdr_loader_hw2reg_target_info_mreg_t [5:0] target_info; // [1056:865]
+    bkdr_loader_hw2reg_width_info_mreg_t [5:0] width_info; // [864:673]
+    bkdr_loader_hw2reg_depth_info_mreg_t [5:0] depth_info; // [672:481]
+    bkdr_loader_hw2reg_read_data_mreg_t [7:0] read_data; // [480:225]
+    bkdr_loader_hw2reg_index_reg_t index; // [224:192]
+    bkdr_loader_hw2reg_hash_last_loaded_mreg_t [5:0] hash_last_loaded; // [191:0]
   } bkdr_loader_regs_hw2reg_t;
 
   // Register offsets for regs interface
@@ -153,42 +153,18 @@ package bkdr_loader_reg_pkg;
   parameter logic [RegsAw-1:0] BKDR_LOADER_TARGET_INFO_3_OFFSET = 11'h 10c;
   parameter logic [RegsAw-1:0] BKDR_LOADER_TARGET_INFO_4_OFFSET = 11'h 110;
   parameter logic [RegsAw-1:0] BKDR_LOADER_TARGET_INFO_5_OFFSET = 11'h 114;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_TARGET_INFO_6_OFFSET = 11'h 118;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_TARGET_INFO_7_OFFSET = 11'h 11c;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_TARGET_INFO_8_OFFSET = 11'h 120;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_TARGET_INFO_9_OFFSET = 11'h 124;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_TARGET_INFO_10_OFFSET = 11'h 128;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_TARGET_INFO_11_OFFSET = 11'h 12c;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_TARGET_INFO_12_OFFSET = 11'h 130;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_TARGET_INFO_13_OFFSET = 11'h 134;
   parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_0_OFFSET = 11'h 200;
   parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_1_OFFSET = 11'h 204;
   parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_2_OFFSET = 11'h 208;
   parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_3_OFFSET = 11'h 20c;
   parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_4_OFFSET = 11'h 210;
   parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_5_OFFSET = 11'h 214;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_6_OFFSET = 11'h 218;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_7_OFFSET = 11'h 21c;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_8_OFFSET = 11'h 220;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_9_OFFSET = 11'h 224;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_10_OFFSET = 11'h 228;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_11_OFFSET = 11'h 22c;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_12_OFFSET = 11'h 230;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_WIDTH_INFO_13_OFFSET = 11'h 234;
   parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_0_OFFSET = 11'h 300;
   parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_1_OFFSET = 11'h 304;
   parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_2_OFFSET = 11'h 308;
   parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_3_OFFSET = 11'h 30c;
   parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_4_OFFSET = 11'h 310;
   parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_5_OFFSET = 11'h 314;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_6_OFFSET = 11'h 318;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_7_OFFSET = 11'h 31c;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_8_OFFSET = 11'h 320;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_9_OFFSET = 11'h 324;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_10_OFFSET = 11'h 328;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_11_OFFSET = 11'h 32c;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_12_OFFSET = 11'h 330;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_DEPTH_INFO_13_OFFSET = 11'h 334;
   parameter logic [RegsAw-1:0] BKDR_LOADER_READ_DATA_0_OFFSET = 11'h 400;
   parameter logic [RegsAw-1:0] BKDR_LOADER_READ_DATA_1_OFFSET = 11'h 404;
   parameter logic [RegsAw-1:0] BKDR_LOADER_READ_DATA_2_OFFSET = 11'h 408;
@@ -212,14 +188,6 @@ package bkdr_loader_reg_pkg;
   parameter logic [RegsAw-1:0] BKDR_LOADER_HASH_LAST_LOADED_3_OFFSET = 11'h 70c;
   parameter logic [RegsAw-1:0] BKDR_LOADER_HASH_LAST_LOADED_4_OFFSET = 11'h 710;
   parameter logic [RegsAw-1:0] BKDR_LOADER_HASH_LAST_LOADED_5_OFFSET = 11'h 714;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_HASH_LAST_LOADED_6_OFFSET = 11'h 718;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_HASH_LAST_LOADED_7_OFFSET = 11'h 71c;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_HASH_LAST_LOADED_8_OFFSET = 11'h 720;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_HASH_LAST_LOADED_9_OFFSET = 11'h 724;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_HASH_LAST_LOADED_10_OFFSET = 11'h 728;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_HASH_LAST_LOADED_11_OFFSET = 11'h 72c;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_HASH_LAST_LOADED_12_OFFSET = 11'h 730;
-  parameter logic [RegsAw-1:0] BKDR_LOADER_HASH_LAST_LOADED_13_OFFSET = 11'h 734;
 
   // Reset values for hwext registers and their fields for regs interface
   parameter logic [1:0] BKDR_LOADER_STATUS_RESVAL = 2'h 0;
@@ -231,42 +199,18 @@ package bkdr_loader_reg_pkg;
   parameter logic [31:0] BKDR_LOADER_TARGET_INFO_3_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_TARGET_INFO_4_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_TARGET_INFO_5_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_TARGET_INFO_6_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_TARGET_INFO_7_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_TARGET_INFO_8_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_TARGET_INFO_9_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_TARGET_INFO_10_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_TARGET_INFO_11_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_TARGET_INFO_12_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_TARGET_INFO_13_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_0_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_1_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_2_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_3_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_4_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_5_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_6_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_7_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_8_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_9_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_10_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_11_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_12_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_WIDTH_INFO_13_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_0_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_1_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_2_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_3_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_4_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_5_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_6_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_7_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_8_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_9_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_10_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_11_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_12_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_DEPTH_INFO_13_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_READ_DATA_0_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_READ_DATA_0_VAL_0_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_READ_DATA_1_RESVAL = 32'h 0;
@@ -289,14 +233,6 @@ package bkdr_loader_reg_pkg;
   parameter logic [31:0] BKDR_LOADER_HASH_LAST_LOADED_3_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_HASH_LAST_LOADED_4_RESVAL = 32'h 0;
   parameter logic [31:0] BKDR_LOADER_HASH_LAST_LOADED_5_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_HASH_LAST_LOADED_6_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_HASH_LAST_LOADED_7_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_HASH_LAST_LOADED_8_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_HASH_LAST_LOADED_9_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_HASH_LAST_LOADED_10_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_HASH_LAST_LOADED_11_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_HASH_LAST_LOADED_12_RESVAL = 32'h 0;
-  parameter logic [31:0] BKDR_LOADER_HASH_LAST_LOADED_13_RESVAL = 32'h 0;
 
   // Register index for regs interface
   typedef enum int {
@@ -311,42 +247,18 @@ package bkdr_loader_reg_pkg;
     BKDR_LOADER_TARGET_INFO_3,
     BKDR_LOADER_TARGET_INFO_4,
     BKDR_LOADER_TARGET_INFO_5,
-    BKDR_LOADER_TARGET_INFO_6,
-    BKDR_LOADER_TARGET_INFO_7,
-    BKDR_LOADER_TARGET_INFO_8,
-    BKDR_LOADER_TARGET_INFO_9,
-    BKDR_LOADER_TARGET_INFO_10,
-    BKDR_LOADER_TARGET_INFO_11,
-    BKDR_LOADER_TARGET_INFO_12,
-    BKDR_LOADER_TARGET_INFO_13,
     BKDR_LOADER_WIDTH_INFO_0,
     BKDR_LOADER_WIDTH_INFO_1,
     BKDR_LOADER_WIDTH_INFO_2,
     BKDR_LOADER_WIDTH_INFO_3,
     BKDR_LOADER_WIDTH_INFO_4,
     BKDR_LOADER_WIDTH_INFO_5,
-    BKDR_LOADER_WIDTH_INFO_6,
-    BKDR_LOADER_WIDTH_INFO_7,
-    BKDR_LOADER_WIDTH_INFO_8,
-    BKDR_LOADER_WIDTH_INFO_9,
-    BKDR_LOADER_WIDTH_INFO_10,
-    BKDR_LOADER_WIDTH_INFO_11,
-    BKDR_LOADER_WIDTH_INFO_12,
-    BKDR_LOADER_WIDTH_INFO_13,
     BKDR_LOADER_DEPTH_INFO_0,
     BKDR_LOADER_DEPTH_INFO_1,
     BKDR_LOADER_DEPTH_INFO_2,
     BKDR_LOADER_DEPTH_INFO_3,
     BKDR_LOADER_DEPTH_INFO_4,
     BKDR_LOADER_DEPTH_INFO_5,
-    BKDR_LOADER_DEPTH_INFO_6,
-    BKDR_LOADER_DEPTH_INFO_7,
-    BKDR_LOADER_DEPTH_INFO_8,
-    BKDR_LOADER_DEPTH_INFO_9,
-    BKDR_LOADER_DEPTH_INFO_10,
-    BKDR_LOADER_DEPTH_INFO_11,
-    BKDR_LOADER_DEPTH_INFO_12,
-    BKDR_LOADER_DEPTH_INFO_13,
     BKDR_LOADER_READ_DATA_0,
     BKDR_LOADER_READ_DATA_1,
     BKDR_LOADER_READ_DATA_2,
@@ -369,19 +281,11 @@ package bkdr_loader_reg_pkg;
     BKDR_LOADER_HASH_LAST_LOADED_2,
     BKDR_LOADER_HASH_LAST_LOADED_3,
     BKDR_LOADER_HASH_LAST_LOADED_4,
-    BKDR_LOADER_HASH_LAST_LOADED_5,
-    BKDR_LOADER_HASH_LAST_LOADED_6,
-    BKDR_LOADER_HASH_LAST_LOADED_7,
-    BKDR_LOADER_HASH_LAST_LOADED_8,
-    BKDR_LOADER_HASH_LAST_LOADED_9,
-    BKDR_LOADER_HASH_LAST_LOADED_10,
-    BKDR_LOADER_HASH_LAST_LOADED_11,
-    BKDR_LOADER_HASH_LAST_LOADED_12,
-    BKDR_LOADER_HASH_LAST_LOADED_13
+    BKDR_LOADER_HASH_LAST_LOADED_5
   } bkdr_loader_regs_id_e;
 
   // Register width information to check illegal writes for regs interface
-  parameter logic [3:0] BKDR_LOADER_REGS_PERMIT [78] = '{
+  parameter logic [3:0] BKDR_LOADER_REGS_PERMIT [46] = '{
     4'b 0001, // index[ 0] BKDR_LOADER_STATUS
     4'b 0011, // index[ 1] BKDR_LOADER_CONTROL
     4'b 1111, // index[ 2] BKDR_LOADER_NUM_BKDR_TARGETS
@@ -393,73 +297,41 @@ package bkdr_loader_reg_pkg;
     4'b 1111, // index[ 8] BKDR_LOADER_TARGET_INFO_3
     4'b 1111, // index[ 9] BKDR_LOADER_TARGET_INFO_4
     4'b 1111, // index[10] BKDR_LOADER_TARGET_INFO_5
-    4'b 1111, // index[11] BKDR_LOADER_TARGET_INFO_6
-    4'b 1111, // index[12] BKDR_LOADER_TARGET_INFO_7
-    4'b 1111, // index[13] BKDR_LOADER_TARGET_INFO_8
-    4'b 1111, // index[14] BKDR_LOADER_TARGET_INFO_9
-    4'b 1111, // index[15] BKDR_LOADER_TARGET_INFO_10
-    4'b 1111, // index[16] BKDR_LOADER_TARGET_INFO_11
-    4'b 1111, // index[17] BKDR_LOADER_TARGET_INFO_12
-    4'b 1111, // index[18] BKDR_LOADER_TARGET_INFO_13
-    4'b 1111, // index[19] BKDR_LOADER_WIDTH_INFO_0
-    4'b 1111, // index[20] BKDR_LOADER_WIDTH_INFO_1
-    4'b 1111, // index[21] BKDR_LOADER_WIDTH_INFO_2
-    4'b 1111, // index[22] BKDR_LOADER_WIDTH_INFO_3
-    4'b 1111, // index[23] BKDR_LOADER_WIDTH_INFO_4
-    4'b 1111, // index[24] BKDR_LOADER_WIDTH_INFO_5
-    4'b 1111, // index[25] BKDR_LOADER_WIDTH_INFO_6
-    4'b 1111, // index[26] BKDR_LOADER_WIDTH_INFO_7
-    4'b 1111, // index[27] BKDR_LOADER_WIDTH_INFO_8
-    4'b 1111, // index[28] BKDR_LOADER_WIDTH_INFO_9
-    4'b 1111, // index[29] BKDR_LOADER_WIDTH_INFO_10
-    4'b 1111, // index[30] BKDR_LOADER_WIDTH_INFO_11
-    4'b 1111, // index[31] BKDR_LOADER_WIDTH_INFO_12
-    4'b 1111, // index[32] BKDR_LOADER_WIDTH_INFO_13
-    4'b 1111, // index[33] BKDR_LOADER_DEPTH_INFO_0
-    4'b 1111, // index[34] BKDR_LOADER_DEPTH_INFO_1
-    4'b 1111, // index[35] BKDR_LOADER_DEPTH_INFO_2
-    4'b 1111, // index[36] BKDR_LOADER_DEPTH_INFO_3
-    4'b 1111, // index[37] BKDR_LOADER_DEPTH_INFO_4
-    4'b 1111, // index[38] BKDR_LOADER_DEPTH_INFO_5
-    4'b 1111, // index[39] BKDR_LOADER_DEPTH_INFO_6
-    4'b 1111, // index[40] BKDR_LOADER_DEPTH_INFO_7
-    4'b 1111, // index[41] BKDR_LOADER_DEPTH_INFO_8
-    4'b 1111, // index[42] BKDR_LOADER_DEPTH_INFO_9
-    4'b 1111, // index[43] BKDR_LOADER_DEPTH_INFO_10
-    4'b 1111, // index[44] BKDR_LOADER_DEPTH_INFO_11
-    4'b 1111, // index[45] BKDR_LOADER_DEPTH_INFO_12
-    4'b 1111, // index[46] BKDR_LOADER_DEPTH_INFO_13
-    4'b 1111, // index[47] BKDR_LOADER_READ_DATA_0
-    4'b 1111, // index[48] BKDR_LOADER_READ_DATA_1
-    4'b 1111, // index[49] BKDR_LOADER_READ_DATA_2
-    4'b 1111, // index[50] BKDR_LOADER_READ_DATA_3
-    4'b 1111, // index[51] BKDR_LOADER_READ_DATA_4
-    4'b 1111, // index[52] BKDR_LOADER_READ_DATA_5
-    4'b 1111, // index[53] BKDR_LOADER_READ_DATA_6
-    4'b 1111, // index[54] BKDR_LOADER_READ_DATA_7
-    4'b 1111, // index[55] BKDR_LOADER_WRITE_DATA_0
-    4'b 1111, // index[56] BKDR_LOADER_WRITE_DATA_1
-    4'b 1111, // index[57] BKDR_LOADER_WRITE_DATA_2
-    4'b 1111, // index[58] BKDR_LOADER_WRITE_DATA_3
-    4'b 1111, // index[59] BKDR_LOADER_WRITE_DATA_4
-    4'b 1111, // index[60] BKDR_LOADER_WRITE_DATA_5
-    4'b 1111, // index[61] BKDR_LOADER_WRITE_DATA_6
-    4'b 1111, // index[62] BKDR_LOADER_WRITE_DATA_7
-    4'b 1111, // index[63] BKDR_LOADER_INDEX
-    4'b 1111, // index[64] BKDR_LOADER_HASH_LAST_LOADED_0
-    4'b 1111, // index[65] BKDR_LOADER_HASH_LAST_LOADED_1
-    4'b 1111, // index[66] BKDR_LOADER_HASH_LAST_LOADED_2
-    4'b 1111, // index[67] BKDR_LOADER_HASH_LAST_LOADED_3
-    4'b 1111, // index[68] BKDR_LOADER_HASH_LAST_LOADED_4
-    4'b 1111, // index[69] BKDR_LOADER_HASH_LAST_LOADED_5
-    4'b 1111, // index[70] BKDR_LOADER_HASH_LAST_LOADED_6
-    4'b 1111, // index[71] BKDR_LOADER_HASH_LAST_LOADED_7
-    4'b 1111, // index[72] BKDR_LOADER_HASH_LAST_LOADED_8
-    4'b 1111, // index[73] BKDR_LOADER_HASH_LAST_LOADED_9
-    4'b 1111, // index[74] BKDR_LOADER_HASH_LAST_LOADED_10
-    4'b 1111, // index[75] BKDR_LOADER_HASH_LAST_LOADED_11
-    4'b 1111, // index[76] BKDR_LOADER_HASH_LAST_LOADED_12
-    4'b 1111  // index[77] BKDR_LOADER_HASH_LAST_LOADED_13
+    4'b 1111, // index[11] BKDR_LOADER_WIDTH_INFO_0
+    4'b 1111, // index[12] BKDR_LOADER_WIDTH_INFO_1
+    4'b 1111, // index[13] BKDR_LOADER_WIDTH_INFO_2
+    4'b 1111, // index[14] BKDR_LOADER_WIDTH_INFO_3
+    4'b 1111, // index[15] BKDR_LOADER_WIDTH_INFO_4
+    4'b 1111, // index[16] BKDR_LOADER_WIDTH_INFO_5
+    4'b 1111, // index[17] BKDR_LOADER_DEPTH_INFO_0
+    4'b 1111, // index[18] BKDR_LOADER_DEPTH_INFO_1
+    4'b 1111, // index[19] BKDR_LOADER_DEPTH_INFO_2
+    4'b 1111, // index[20] BKDR_LOADER_DEPTH_INFO_3
+    4'b 1111, // index[21] BKDR_LOADER_DEPTH_INFO_4
+    4'b 1111, // index[22] BKDR_LOADER_DEPTH_INFO_5
+    4'b 1111, // index[23] BKDR_LOADER_READ_DATA_0
+    4'b 1111, // index[24] BKDR_LOADER_READ_DATA_1
+    4'b 1111, // index[25] BKDR_LOADER_READ_DATA_2
+    4'b 1111, // index[26] BKDR_LOADER_READ_DATA_3
+    4'b 1111, // index[27] BKDR_LOADER_READ_DATA_4
+    4'b 1111, // index[28] BKDR_LOADER_READ_DATA_5
+    4'b 1111, // index[29] BKDR_LOADER_READ_DATA_6
+    4'b 1111, // index[30] BKDR_LOADER_READ_DATA_7
+    4'b 1111, // index[31] BKDR_LOADER_WRITE_DATA_0
+    4'b 1111, // index[32] BKDR_LOADER_WRITE_DATA_1
+    4'b 1111, // index[33] BKDR_LOADER_WRITE_DATA_2
+    4'b 1111, // index[34] BKDR_LOADER_WRITE_DATA_3
+    4'b 1111, // index[35] BKDR_LOADER_WRITE_DATA_4
+    4'b 1111, // index[36] BKDR_LOADER_WRITE_DATA_5
+    4'b 1111, // index[37] BKDR_LOADER_WRITE_DATA_6
+    4'b 1111, // index[38] BKDR_LOADER_WRITE_DATA_7
+    4'b 1111, // index[39] BKDR_LOADER_INDEX
+    4'b 1111, // index[40] BKDR_LOADER_HASH_LAST_LOADED_0
+    4'b 1111, // index[41] BKDR_LOADER_HASH_LAST_LOADED_1
+    4'b 1111, // index[42] BKDR_LOADER_HASH_LAST_LOADED_2
+    4'b 1111, // index[43] BKDR_LOADER_HASH_LAST_LOADED_3
+    4'b 1111, // index[44] BKDR_LOADER_HASH_LAST_LOADED_4
+    4'b 1111  // index[45] BKDR_LOADER_HASH_LAST_LOADED_5
   };
 
 endpackage
