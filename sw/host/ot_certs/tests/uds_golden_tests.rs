c44d37741c9504d96d1723e8c95572af29f09911
Lovepreet Singh <lpsingh@google.com>
2026-08-18 18:15:36 -0700

    [ot_certs] Add unit tests with golden data for DICE ML-DSA 44 and ML-DSA 87 UDS certificates
    This commits finishes up a TODO item from a previous commit to add unit
    tests with golden data for generating DICE UDS certificates with ML-DSA
    44 and ML-DSA 87 public keys, and ML-DSA 87 signatures. The data for
    these golden tests was generated by running
    `//sw/host/provisioning/orchestrator/tests:e2e_multistage_emulation_dice_mldsa_cw340_test`
    target with bazel argument `--define mldsa_provisioning_test=true` after
    minor modifications to the perso firmware and tool to print measurements
    and TBS certificates.
    
    Tested by running
    `//sw/host/ot_certs:ot_certs_e2e_test_tests/uds_golden_tests_test`
    target locally.
    
    Signed-off-by: Lovepreet Singh <lpsingh@google.com>

--- sw/host/ot_certs/tests/uds_golden_tests.rs
+++ sw/host/ot_certs/tests/uds_golden_tests.rs
@@ -18,6 +18,20 @@ const GOLDEN_ECDSA_ENDORSED_CERT_INPUT_DATA: &str =
 const GOLDEN_ECDSA_ENDORSED_CERT_OUTPUT_DATA: &[u8] =
     include_bytes!("uds_ecdsa_endorsed_cert_golden.der");
 
+const GOLDEN_MLDSA44_TBS_INPUT_DATA: &str = include_str!("uds_mldsa44_tbs_input_golden.json");
+const GOLDEN_MLDSA44_TBS_OUTPUT_DATA: &[u8] = include_bytes!("uds_mldsa44_tbs_golden.der");
+const GOLDEN_MLDSA44_ENDORSED_CERT_INPUT_DATA: &str =
+    include_str!("uds_mldsa44_endorsed_cert_input_golden.json");
+const GOLDEN_MLDSA44_ENDORSED_CERT_OUTPUT_DATA: &[u8] =
+    include_bytes!("uds_mldsa44_endorsed_cert_golden.der");
+
+const GOLDEN_MLDSA87_TBS_INPUT_DATA: &str = include_str!("uds_mldsa87_tbs_input_golden.json");
+const GOLDEN_MLDSA87_TBS_OUTPUT_DATA: &[u8] = include_bytes!("uds_mldsa87_tbs_golden.der");
+const GOLDEN_MLDSA87_ENDORSED_CERT_INPUT_DATA: &str =
+    include_str!("uds_mldsa87_endorsed_cert_input_golden.json");
+const GOLDEN_MLDSA87_ENDORSED_CERT_OUTPUT_DATA: &[u8] =
+    include_bytes!("uds_mldsa87_endorsed_cert_golden.der");
+
 #[test]
 fn compare_uds_ecdsa_tbs() {
     let uds_template = Template::from_hjson_str(UDS_CERT_TEMPLATE)
@@ -50,4 +64,66 @@ fn compare_uds_ecdsa_endorsed_cert() {
     assert_eq!(endorsed_cert, GOLDEN_ECDSA_ENDORSED_CERT_OUTPUT_DATA);
 }
 
-// TODO: Add golden tests for MLDSA certificates once end-to-end provisioning test supports it
+#[test]
+fn compare_uds_mldsa44_tbs() {
+    let uds_template = Template::from_hjson_str(UDS_CERT_TEMPLATE)
+        .expect("UDS certificate template must be parseable as HJSON");
+    let mldsa_test_data = SubstData::from_json(GOLDEN_MLDSA44_TBS_INPUT_DATA)
+        .expect("Golden data for MLDSA44 must be parseable as JSON");
+    let subst_template = uds_template
+        .subst(&mldsa_test_data)
+        .expect("Template substitution for UDS MLDSA44 TBS must succeed");
+    let tbs = der::Der::generate(|builder| {
+        x509::X509::push_tbs_certificate(builder, &subst_template.certificate)
+    })
+    .expect("TBS UDS certificate generation to succeed");
+
+    assert_eq!(tbs, GOLDEN_MLDSA44_TBS_OUTPUT_DATA);
+}
+
+#[test]
+fn compare_uds_mldsa44_endorsed_cert() {
+    let uds_template = Template::from_hjson_str(UDS_CERT_TEMPLATE)
+        .expect("UDS certificate template must be parseable as HJSON");
+    let ecdsa_test_data = SubstData::from_json(GOLDEN_MLDSA44_ENDORSED_CERT_INPUT_DATA)
+        .expect("Golden data for MLDSA44 must be parseable as JSON");
+    let subst_template = uds_template
+        .subst(&ecdsa_test_data)
+        .expect("Template substitution for UDS MLDSA44 endorsed certificate must succeed");
+    let endorsed_cert = generate_certificate(&subst_template)
+        .expect("Certificate generation for UDS MLDSA44 certificate must succeed");
+
+    assert_eq!(endorsed_cert, GOLDEN_MLDSA44_ENDORSED_CERT_OUTPUT_DATA);
+}
+
+#[test]
+fn compare_uds_mldsa87_tbs() {
+    let uds_template = Template::from_hjson_str(UDS_CERT_TEMPLATE)
+        .expect("UDS certificate template must be parseable as HJSON");
+    let mldsa_test_data = SubstData::from_json(GOLDEN_MLDSA87_TBS_INPUT_DATA)
+        .expect("Golden data for MLDSA87 must be parseable as JSON");
+    let subst_template = uds_template
+        .subst(&mldsa_test_data)
+        .expect("Template substitution for UDS MLDSA87 TBS must succeed");
+    let tbs = der::Der::generate(|builder| {
+        x509::X509::push_tbs_certificate(builder, &subst_template.certificate)
+    })
+    .expect("TBS UDS certificate generation to succeed");
+
+    assert_eq!(tbs, GOLDEN_MLDSA87_TBS_OUTPUT_DATA);
+}
+
+#[test]
+fn compare_uds_mldsa87_endorsed_cert() {
+    let uds_template = Template::from_hjson_str(UDS_CERT_TEMPLATE)
+        .expect("UDS certificate template must be parseable as HJSON");
+    let ecdsa_test_data = SubstData::from_json(GOLDEN_MLDSA87_ENDORSED_CERT_INPUT_DATA)
+        .expect("Golden data for MLDSA87 must be parseable as JSON");
+    let subst_template = uds_template
+        .subst(&ecdsa_test_data)
+        .expect("Template substitution for UDS MLDSA87 endorsed certificate must succeed");
+    let endorsed_cert = generate_certificate(&subst_template)
+        .expect("Certificate generation for UDS MLDSA87 certificate must succeed");
+
+    assert_eq!(endorsed_cert, GOLDEN_MLDSA87_ENDORSED_CERT_OUTPUT_DATA);
+}
