09036b8f2ee1a0486c1dd70d11e145574c979792
Raphael Roth <rroth@lowrisc.org>
2026-08-21 17:07:16 +0200

    [keymgr_dpe, rtl] Migrate all parameter to `keymgr_dpe_pkg`
    All parameter are moved from the `keymgr_pkg` to the `keymgr_dpe_pkg`,
    while removing all outdated parameter. The `keymgr_dpe_pkg` will be
    cleaned up in a later commit.
    
    Signed-off-by: Raphael Roth <rroth@lowrisc.org>

--- hw/ip/keymgr_dpe/rtl/keymgr_dpe_err.sv
+++ hw/ip/keymgr_dpe/rtl/keymgr_dpe_err.sv
@@ -8,7 +8,7 @@
 `include "prim_assert.sv"
 
 module keymgr_dpe_err
-  import keymgr_pkg::*;
+  import keymgr_dpe_pkg::*;
   import keymgr_dpe_reg_pkg::*;
 (
   input clk_i,
--- hw/ip/keymgr_dpe/rtl/keymgr_dpe_pkg.sv
+++ hw/ip/keymgr_dpe/rtl/keymgr_dpe_pkg.sv
@@ -4,9 +4,6 @@
 
 package keymgr_dpe_pkg;
 
-  // Most of the parameters are directly reused from keymgr_pkg
-  import keymgr_pkg::*;
-
   // Chip Device ID
   parameter int DeviceIdWidth = 256;
   typedef logic [DeviceIdWidth-1:0] keymgr_dpe_device_id_t;
@@ -20,6 +17,16 @@ package keymgr_dpe_pkg;
     OpDpeDisable = 4,
     OpDpeLoadRootKey = 5
   } keymgr_dpe_ops_e;
+
+  // Enumeration for operation (keymgr)
+  typedef enum logic [2:0] {
+    OpAdvance = 0,
+    OpGenId = 1,
+    OpGenSwOut = 2,
+    OpGenHwOut = 3,
+    OpDisable = 4
+  } keymgr_ops_e;
+
 
   // Encoding generated with:
   // $ ./util/design/sparse-fsm-encode.py -d 5 -m 9 -n 10 \
@@ -75,6 +82,73 @@ package keymgr_dpe_pkg;
   // Parameter Key Width
   parameter int KeyMgrKeyWidth = 256;
 
+  // Parameter provided by the keymgr
+  parameter int SaltWidth = 32 * keymgr_dpe_reg_pkg::NumSaltReg;
+  parameter int KeyVersionWidth = 32;  // Key version length for individual DICE stage
+  parameter int KeyWidth = 256;
+  parameter int OtbnKeyWidth = 384;
+  parameter int KmacDataIfWidth = 64;  // KMAC interface data width
+  parameter int SwBindingWidth = 32 * keymgr_dpe_reg_pkg::NumSwBindingReg;
+  parameter int Shares = 2; // number of key shares
+
+  // These should be defined in another module's package
+  parameter int HealthStateWidth = 128;
+  parameter int DevIdWidth = 256;
+  parameter int MaxWidth = 256;
+
+  // Default Lfsr configurations
+  // These LFSR parameters have been generated with
+  // $ util/design/gen-lfsr-seed.py --width 64 --seed 691876113 --prefix ""
+  parameter int LfsrWidth = 64;
+  typedef logic [LfsrWidth-1:0] lfsr_seed_t;
+  typedef logic [LfsrWidth-1:0][$clog2(LfsrWidth)-1:0] lfsr_perm_t;
+  parameter lfsr_seed_t RndCnstLfsrSeedDefault = 64'h22d326255bd24320;
+  parameter lfsr_perm_t RndCnstLfsrPermDefault = {
+    128'h16108c9f9008aa37e5118d1ec1df64a7,
+    256'h24f3f1b73537f42d38383ee8f897286df81d49ab54b6bbbb666cbd1a16c41252
+  };
+
+  // Random permutation
+  parameter int RandWidth = LfsrWidth / 2;
+  typedef logic [RandWidth-1:0][$clog2(RandWidth)-1:0] rand_perm_t;
+  parameter rand_perm_t RndCnstRandPermDefault = {
+    160'h62089181d2a6be2ce145e2e27099ededbd7dceb0
+  };
+
+  // Default seeds
+  // These have been generated with the following command by incrementing the --seed argument
+  // for every seed.
+  // util/design/gen-lfsr-seed.py --width 256 --seed 7535190 --prefix ""
+  typedef logic [KeyWidth-1:0] seed_t;
+  parameter seed_t RndCnstRevisionSeedDefault =
+    256'h69802e51_bacf8874_e650d692_e3d8a646_2d3f158f_0bf7961d_d346f880_b4d52170;
+  parameter seed_t RndCnstSoftOutputSeedDefault =
+    256'h23cbe85e_62e39992_4ab8d6a6_acbf12f0_0ca6f488_63eaa428_ffb11b26_c5282b14;
+  parameter seed_t RndCnstHardOutputSeedDefault =
+    256'h877ef0b9_aceaefc5_693b1aa7_e43a7f3d_ee7d63b6_4e73e182_b49cbf87_2872f2c;
+
+  // Target based deriviation seeds
+  // These are used during the generation stages for sideload
+  parameter seed_t RndCnstNoneSeedDefault =
+    256'h6013ba4d_8d04fe4f_80cb0472_536c9679_08b82f1d_98a4e405_cb4680ec_3a4a8d7a;
+  parameter seed_t RndCnstAesSeedDefault =
+    256'he16a8fa9_5b613cd5_fb9ad23f_bd8347e1_64e45dac_5d08a41b_e83caa37_e03d9482;
+  parameter seed_t RndCnstKmacSeedDefault =
+    256'hc57f4c0b_b308e83f_3fc4bc63_d87dd67d_9071dc1c_e19484c8_3c94fb97_dd634369;
+  parameter seed_t RndCnstOtbnSeedDefault =
+    256'hcbcb4d2d_0abeb81b_ca7451ae_d1e2479d_ba13530a_d046b945_646aa127_bd4f6a38;
+
+  // Width calculations
+  // These are the largest calculations in use across all stages
+  parameter int IdDataWidth = KeyWidth;
+  // key version + salt + key ID + constant
+  parameter int GenDataWidth = 32 + SaltWidth + KeyWidth*2;
+  // Max Payload Width to derivation function
+  // see security strength description https://keccak.team/keccak.html
+  // The max width here is chosen arbitrarily to ensure we do not get out of hand.
+  // Since KMAC is a MAC operation, the data can be as long as we need.
+  parameter int KDFMaxWidth = 1984;
+
   // Input struct for secrets required by the keymgr dpe.
   typedef struct packed {
     logic [KeyMgrKeyWidth-1:0]  share0;
@@ -109,6 +183,37 @@ package keymgr_dpe_pkg;
     seed         : 256'hf5052c0f14782d8b066be9f49c0b2000d3643ff3723ea7db972f69cd3e2e3e68,
     seed_valid   : 1'b1
   };
+
+  // Key connection to various symmetric modules
+  typedef struct packed {
+    logic valid;
+    logic [Shares-1:0][KeyWidth-1:0] key;
+  } hw_key_req_t;
+
+  // Key connection to otbn
+  typedef struct packed {
+    logic valid;
+    logic [Shares-1:0][OtbnKeyWidth-1:0] key;
+  } otbn_key_req_t;
+
+  parameter hw_key_req_t HW_KEY_REQ_DEFAULT = '{
+    valid: 1'b0,
+    key: {Shares{KeyWidth'(32'hDEADBEEF)}}
+  };
+
+  parameter otbn_key_req_t OTBN_KEY_REQ_DEFAULT = '{
+    valid: 1'b0,
+    key: {Shares{OtbnKeyWidth'(32'hDEADBEEF)}}
+  };
+
+  // Enumeration for operation status
+  typedef enum logic [1:0] {
+    OpIdle = 0,
+    OpWip = 1,
+    OpDoneSuccess = 2,
+    OpDoneFail = 3
+  } keymgr_dpe_op_status_e;
+
 
   // Enumeration for boot stage. In the BootStageRuntime stage, there is no limit on the number of
   // advance calls.
@@ -153,5 +258,110 @@ package keymgr_dpe_pkg;
     KeymgrDpeEnDebug,
     KeymgrDpeEnLast
   } keymgr_lc_en_usage_e;
+
+  // Enumeration for sideload sel
+  typedef enum logic [1:0] {
+    None,
+    Aes,
+    Kmac,
+    Otbn
+  } keymgr_dpe_key_dest_e;
+
+  // Enumeration for actual key slot idx
+  typedef enum logic [1:0] {
+    AesIdx,
+    KmacIdx,
+    OtbnIdx,
+    LastIdx
+  } keymgr_dpe_sideload_slot_idx_e;
+
+  // keymgr_dpe has 4 categories of errors
+  // sync errors  - recoverable errors that happen during keymgr_dpe operation
+  // async errors - recoverable errors that happen asynchronously
+  // sync faults  - fatal errors that happen during keymgr_dpe operation
+  // async faults - fatal errors that happen asynchronously
+
+  typedef enum logic [1:0] {
+    SyncErrInvalidOp,
+    SyncErrInvalidIn,
+    SyncErrLastIdx
+  } keymgr_dpe_sync_error_e;
+
+  typedef enum logic [1:0] {
+    AsyncErrShadowUpdate,
+    AsyncErrLastIdx
+  } keymgr_dpe_async_error_e;
+
+  typedef enum logic [1:0] {
+    SyncFaultKmacOp,
+    SyncFaultKmacOut,
+    SyncFaultSideSel,
+    SyncFaultLastIdx
+  } keymgr_dpe_sync_fault_e;
+
+  typedef enum logic [3:0] {
+    AsyncFaultKmacCmd,
+    AsyncFaultKmacFsm,
+    AsyncFaultKmacDone,
+    AsyncFaultRegIntg,
+    AsyncFaultShadow,
+    AsyncFaultFsmIntg,
+    AsyncFaultFsmChk,
+    AsyncFaultCntErr,
+    AsyncFaultRCntErr,
+    AsyncFaultSideErr,
+    AsyncFaultKeyEcc,
+    AsyncFaultLastIdx
+  } keymgr_dpe_async_fault_e;
+
+  // Bit position of fault status
+  typedef enum logic [3:0] {
+    FaultKmacCmd,
+    FaultKmacFsm,
+    FaultKmacDone,
+    FaultKmacOp,
+    FaultKmacOut,
+    FaultRegIntg,
+    FaultShadow,
+    FaultCtrlFsm,
+    FaultCtrlFsmChk,
+    FaultCtrlCnt,
+    FaultReseedCnt,
+    FaultSideFsm,
+    FaultSideSel,
+    FaultKeyEcc,
+    FaultLastPos
+  } keymgr_dpe_fault_pos_e;
+
+  // Bit position of error code
+  // Error is encoded as 1 error per bit
+  typedef enum logic [2:0] {
+    ErrInvalidOp,
+    ErrInvalidIn,
+    ErrShadowUpdate,
+    ErrLastPos
+  } keymgr_dpe_err_pos_e;
+
+  typedef enum logic [2:0] {
+    SideLoadClrIdle,
+    SideLoadClrAes,
+    SideLoadClrKmac,
+    SideLoadClrOtbn
+  } keymgr_dpe_sideload_clr_e;
+
+  // perm_data
+  function automatic logic[RandWidth-1:0] perm_data (logic [RandWidth-1:0] data,
+    rand_perm_t perm_sel);
+
+    for (int k = 0; k < 32; k++) begin : gen_data_loop
+      perm_data[k] = data[perm_sel[k]];
+    end
+
+  endfunction
+
+  // checks for all 0's or all 1's of value
+  function automatic logic valid_data_chk (logic [KeyWidth-1:0] value);
+    return |value & ~&value;
+  endfunction
 
 endpackage : keymgr_dpe_pkg
