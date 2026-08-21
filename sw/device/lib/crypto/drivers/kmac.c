521937891a678b3b483b5ee1398a728d354650e4
Pascal Nasahl <nasahlpa@lowrisc.org>
2026-08-21 10:23:03 +0200

    [crypto] Fix shadow reg write in KMAC driver
    When writing to a shadow register, we should use the
    dedicated functions that write twice to the register
    such that the change gets commited.
    
    kmac_hwip_default_configure was anyway only used in
    tests so this was fine until now.
    
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- sw/device/lib/crypto/drivers/kmac.c
+++ sw/device/lib/crypto/drivers/kmac.c
@@ -315,7 +315,7 @@ status_t kmac_hwip_default_configure(void) {
   entropy_hash_threshold = bitfield_field32_write(
       entropy_hash_threshold,
       KMAC_ENTROPY_REFRESH_THRESHOLD_SHADOWED_THRESHOLD_FIELD, UINT32_MAX);
-  abs_mmio_write32(
+  abs_mmio_write32_shadowed(
       kKmacBaseAddr + KMAC_ENTROPY_REFRESH_THRESHOLD_SHADOWED_REG_OFFSET,
       entropy_hash_threshold);
 
