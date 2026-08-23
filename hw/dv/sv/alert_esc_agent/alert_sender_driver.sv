lowRISC/opentitan

commit efed93f30c7cb7fc1c51f04e44f1a0cf7c96f17e

[dv] Rationalise dv_base_driver reset tracking
The subclasses all implemented a reset_signals task (incorrectly in
two cases), which is a bit silly: each agent should be doing the same
thing. Move the forever loop into the base class and provide hooks
instead.

Also, get rid of the driver's under_reset flag entirely: we can just
use dv_base_agent_cfg::in_reset instead, which should make things a
bit more uniform.

Most of the changes across the different IPs' agents are simple
enough, but the alert_esc_agent was quite tricky! This commit rewrites
the alert receiver and sender drivers (which weren't trivial to switch
across to the new mechanism). I strongly suspect that they were full
of race conditions before, and the new implementations should be a bit
easier to reason about.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/dv/sv/alert_esc_agent/alert_sender_driver.sv
@@ -10,12 +10,16 @@ class alert_sender_driver extends alert_base_driver;
 
   `uvm_component_utils(alert_sender_driver)
 
+  // An event that can be triggered (by functions as well as tasks). This is monitored by
+  // maintain_alert_drive, which will call cfg.vif.force_alert with m_desired_alert_val.
+  uvm_event m_update_alert_val;
+  bit [1:0] m_desired_alert_val;
+
   extern function new (string name, uvm_component parent);
 
-  // Monitor resets and reset all driven signals when a reset occurs.
-  //
-  // An implementation of dv_base_driver::reset_signals.
-  extern virtual task reset_signals();
+  // This function gets called by dv_base_driver::reset_signals when the interface enters reset.
+  // Clear any alert signal.
+  extern virtual task on_enter_reset();
 
   // Drive sequence items for alerts and pings that have been retrieved by get_req (and therefore
   // have started) and have been pushed to the various queues. This also does the alert init
@@ -62,8 +66,9 @@ class alert_sender_driver extends alert_base_driver;
   // Set the alert pins to be (alert_p=1; alert_n=0) if enabled=1 and (alert_p=0; alert_n=1)
   // otherwise.
   //
-  // This task takes zero time (but needs to be a task because it uses non-blocking assignment)
-  extern local task set_alert(bit enabled);
+  // Because the alert signals are set through a clocking block (using cfg.vif.force_alert()), this
+  // works by setting m_desired_alert_val and triggering the m_update_alert_val event.
+  extern local function void set_alert(bit enabled);
 
   // Set the alert pins to 11 or 00 (depending on the high argument).
   //
@@ -84,62 +89,53 @@ class alert_sender_driver extends alert_base_driver;
   // This task exits early on reset or if cfg.en_alert_lpg is asserted (in which case, the receiver
   // won't reply to the init)
   extern local task do_alert_tx_init();
+
+  // This task runs forever, calling cfg.vif.set_alert() each time the m_update_alert_val event is
+  // triggered.
+  extern local task maintain_alert_drive();
 endclass : alert_sender_driver
 
 function alert_sender_driver::new (string name, uvm_component parent);
   super.new(name, parent);
+  m_update_alert_val = new("m_update_alert_val");
 endfunction : new
 
-task alert_sender_driver::reset_signals();
+task alert_sender_driver::on_enter_reset();
   set_alert(1'b0);
-  forever begin
-    wait(!cfg.in_reset);
-    wait(cfg.in_reset);
-    set_alert(1'b0);
-  end
 endtask
 
 task alert_sender_driver::drive_req();
-  forever begin
-    // Wait until we are out of reset. Until that happens, respond instantly to any sequence items
-    // that come in.
-    while (cfg.in_reset) begin
-      fork : isolation_fork begin
-        alert_esc_seq_item item;
-        fork
-          wait (!cfg.in_reset);
-          begin
-            wait(s_alert_send_q.size());
-            item = s_alert_send_q.pop_front();
-          end
-          begin
-            wait(s_alert_ping_rsp_q.size());
-            item = s_alert_ping_rsp_q.pop_front();
-          end
-        join_any
-        disable fork;
-        if (item != null) begin
-          alert_esc_seq_item rsp;
-          `downcast(rsp, item.clone());
-          rsp.set_id_info(item);
-          seq_item_port.put_response(rsp);
-        end
-      end join
-    end
-
-    while(!cfg.in_reset) begin
-      bit en_alert_lpg = cfg.en_alert_lpg;
-
-      // If LPG is not enabled, initialise the alert interface
-      if (!en_alert_lpg) begin
-        do_alert_tx_init();
+  fork
+    maintain_alert_drive();
+    forever begin
+      // Wait until we are out of reset. Until that happens, respond instantly to any sequence items
+      // that come in.
+      while (cfg.in_reset) begin
+        fork : isolation_fork begin
+          alert_esc_seq_item item;
+          fork
+            wait(!cfg.in_reset);
+            m_sender_requests.get(item);
+          join_any
+          disable fork;
+          if (item != null) seq_item_port.put_response(item);
+        end join
       end
 
-      // Drive any requests that we see. The task will exit on reset or if the LPG flag changes,
-      // letting us initialise the alert interface (if LPG is disabled) and continue.
-      drive_reqs_with_lpg_mode(en_alert_lpg);
+      while(!cfg.in_reset) begin
+        bit en_alert_lpg = cfg.en_alert_lpg;
+
+        // If LPG is not enabled, initialise the alert interface
+        if (!en_alert_lpg) begin
+          do_alert_tx_init();
+        end
+
+        // Drive any requests that we see. The task will exit on reset or if the LPG flag changes,
+        // letting us initialise the alert interface (if LPG is disabled) and continue.
+        drive_reqs_with_lpg_mode(en_alert_lpg);
+      end
     end
-  end
+  join
 endtask
 
 task alert_sender_driver::drive_reqs_with_lpg_mode(bit en_alert_lpg);
@@ -150,30 +146,22 @@ task alert_sender_driver::drive_reqs_with_lpg_mode(bit en_alert_lpg);
     // Pick an item to drive, but keep track of resets and any change to the LPG flag.
     fork : isolation_fork begin
       fork
-        wait (cfg.en_alert_lpg != en_alert_lpg);
-        wait (cfg.in_reset);
-        begin
-          wait(s_alert_send_q.size());
-          item = s_alert_send_q.pop_front();
-          is_alert = 1;
-        end
-        begin
-          wait(s_alert_ping_rsp_q.size());
-          item = s_alert_ping_rsp_q.pop_front();
-          is_ping = 1;
-        end
+        wait(cfg.en_alert_lpg != en_alert_lpg);
+        wait(cfg.in_reset);
+        m_sender_requests.get(item);
       join_any
       disable fork;
     end join
 
-    // Handle the alert or ping response. If there is no item, there has been a reset or the LPG
-    // flag has changed. Return from the task.
-    //
-    // The send_alert and send_ping_rsp tasks both exit early when they see a reset, but ignore any
-    // further changes to en_alert_lpg.
-    if (is_alert) send_item(1'b0, item);
-    else if (is_ping) send_item(1'b1, item);
-    else return;
+    // If there is no item, there has been a reset or the LPG flag has changed. Return from the
+    // task.
+    if (item == null) return;
+
+    // Handle the alert request or ping response. If the item requests both, send the ping response
+    // first and then the real alert.
+    if (item.s_alert_ping_rsp) send_item(1'b1, item);
+    if (item.s_alert_send) send_item(1'b0, item);
+    seq_item_port.put_response(item);
   end
 endtask
 
@@ -196,7 +184,6 @@ task alert_sender_driver::send_item(bit is_ping_rsp, alert_esc_seq_item item);
     join_any
     disable fork;
   end join
-
   `uvm_info(`gfn,
             $sformatf("finished sending sender item, alert_send=%0b, ping_rsp=%0b, int_err=%0b",
                       item.s_alert_send, item.s_alert_ping_rsp, item.int_err), UVM_HIGH)
@@ -257,10 +244,10 @@ task alert_sender_driver::random_drive_int_fail(int int_err_cyc);
   end
 endtask
 
-task alert_sender_driver::set_alert(bit enabled);
-  cfg.vif.sender_cb.alert_tx_int.alert_p <= enabled;
-  cfg.vif.sender_cb.alert_tx_int.alert_n <= !enabled;
-endtask
+function void alert_sender_driver::set_alert(bit enabled);
+  m_desired_alert_val = {enabled, !enabled};
+  m_update_alert_val.trigger();
+endfunction
 
 task alert_sender_driver::drive_invalid_alert(bit high);
   cfg.vif.sender_cb.alert_tx_int.alert_p <= high;
@@ -284,4 +271,12 @@ task alert_sender_driver::do_alert_tx_init();
     join_any
     disable fork;
   end join
+endtask
+
+task alert_sender_driver::maintain_alert_drive();
+  forever begin
+    m_update_alert_val.wait_ptrigger();
+    cfg.vif.force_alert(m_desired_alert_val[1], m_desired_alert_val[0]);
+    m_update_alert_val.reset();
+  end
 endtask

hw/dv/sv/alert_esc_agent/alert_base_driver.sv
@@ -5,10 +5,8 @@
 // Virtual base class used for alert_sender_driver and alert_receiver_driver
 
 virtual class alert_base_driver extends dv_base_driver#(alert_esc_seq_item, alert_esc_agent_cfg);
-  alert_esc_seq_item r_alert_ping_send_q[$];
-  alert_esc_seq_item r_alert_rsp_q[$];
-  alert_esc_seq_item s_alert_send_q[$];
-  alert_esc_seq_item s_alert_ping_rsp_q[$];
+  protected uvm_tlm_analysis_fifo #(alert_esc_seq_item) m_sender_requests;
+  protected uvm_tlm_analysis_fifo #(alert_esc_seq_item) m_receiver_requests;
 
   extern function new (string name, uvm_component parent);
 
@@ -28,6 +26,8 @@ endclass
 
 function alert_base_driver::new (string name, uvm_component parent);
   super.new(name, parent);
+  m_sender_requests = new("m_sender_requests", this);
+  m_receiver_requests = new("m_receiver_requests", this);
 endfunction : new
 
 task alert_base_driver::get_and_drive();
@@ -45,11 +45,9 @@ task alert_base_driver::get_req();
     `downcast(req_clone, req.clone());
     req_clone.set_id_info(req);
     // receiver mode
-    if (req.r_alert_ping_send) r_alert_ping_send_q.push_back(req_clone);
-    if (req.r_alert_rsp)       r_alert_rsp_q.push_back(req_clone);
+    if (req.r_alert_ping_send || req.r_alert_rsp) m_receiver_requests.write(req_clone);
     // sender mode
-    if (req.s_alert_send)      s_alert_send_q.push_back(req_clone);
-    if (req.s_alert_ping_rsp)  s_alert_ping_rsp_q.push_back(req_clone);
+    if (req.s_alert_send || req.s_alert_ping_rsp) m_sender_requests.write(req_clone);
 
     if (req.r_esc_rsp) begin
       `uvm_error(get_full_name(), "Alert driver cannot drive an esc item")

hw/dv/sv/alert_esc_agent/alert_esc_if.sv
@@ -183,6 +183,16 @@ interface alert_esc_if(input clk, input rst_n);
     end while (cycle_cnt > 1 || rst_n != 1'b1);
   endtask : wait_esc_ping
 
+  // Set the alert pins to the requested value (either 1;0 or 0;1) when the interface is active and
+  // configured to act as the alert sender.
+  //
+  // This uses a clocking drive to update the version of the signal in sender_cb (causing the update
+  // to have a lasting effect, and also to be synchronised with the next clock edge).
+  task automatic force_alert(bit alert_p, bit alert_n);
+    sender_cb.alert_tx_int.alert_p <= alert_p;
+    sender_cb.alert_tx_int.alert_n <= alert_n;
+  endtask
+
   // Return true if the current state is PingSt
   //
   // This rather trivial function is needed because we want to use the interface through a virtual

hw/dv/sv/alert_esc_agent/esc_receiver_driver.sv
@@ -15,11 +15,6 @@ class esc_receiver_driver extends dv_base_driver#(alert_esc_seq_item, alert_esc_
 
   extern function new (string name, uvm_component parent);
 
-  // This task runs forever calls do_reset at the start of every reset.
-  //
-  // Overridden from dv_base_driver.
-  extern virtual task reset_signals();
-
   // This task runs forever. It works by running rsp_escalator (which consumes and drives items from
   // seq_item_port) and esc_ping_detector (which responds to ping requests).
   //
@@ -56,22 +51,13 @@ class esc_receiver_driver extends dv_base_driver#(alert_esc_seq_item, alert_esc_
   extern virtual task wait_esc_complete();
   extern virtual task wait_esc();
   // Set the values driven through resp_p / resp_n to 0/1 and clear the is_ping flag
-  extern virtual task do_reset();
+  extern task on_enter_reset();
 
 endclass : esc_receiver_driver
 
 function esc_receiver_driver::new (string name, uvm_component parent);
   super.new(name, parent);
 endfunction : new
-
-task esc_receiver_driver::reset_signals();
-  do_reset();
-  forever begin
-    wait (cfg.in_reset);
-    do_reset();
-    wait (!cfg.in_reset);
-  end
-endtask : reset_signals
 
 task esc_receiver_driver::get_and_drive();
   fork
@@ -229,8 +215,8 @@ task esc_receiver_driver::wait_esc();
   while (cfg.vif.esc_tx.esc_p === 1'b0 && cfg.vif.esc_tx.esc_n === 1'b1) @(cfg.vif.receiver_cb);
 endtask : wait_esc
 
-task esc_receiver_driver::do_reset();
+task esc_receiver_driver::on_enter_reset();
   cfg.vif.esc_rx_int.resp_p <= 1'b0;
   cfg.vif.esc_rx_int.resp_n <= 1'b1;
   is_ping = 0;
-endtask : do_reset
+endtask : on_enter_reset

hw/dv/sv/alert_esc_agent/esc_sender_driver.sv
@@ -8,24 +8,13 @@ class esc_sender_driver extends dv_base_driver#(alert_esc_seq_item, alert_esc_ag
   `uvm_component_utils(esc_sender_driver)
 
   extern function new (string name, uvm_component parent);
-  extern virtual task reset_signals();
   extern virtual task get_and_drive();
-
-  extern local task reset_esc();
+  extern virtual task on_enter_reset();
 endclass : esc_sender_driver
 
 function esc_sender_driver::new (string name, uvm_component parent);
   super.new(name, parent);
 endfunction : new
-
-task esc_sender_driver::reset_signals();
-  wait(cfg.vif.rst_n !== 1'b1);
-  forever begin
-    reset_esc();
-    wait (!cfg.in_reset);
-    wait (cfg.in_reset);
-  end
-endtask : reset_signals
 
 task esc_sender_driver::get_and_drive();
   // LC_CTRL uses virtual interface to directly drive escalation requests.
@@ -34,7 +23,7 @@ task esc_sender_driver::get_and_drive();
   wait (!cfg.in_reset);
 endtask : get_and_drive
 
-task esc_sender_driver::reset_esc();
+task esc_sender_driver::on_enter_reset();
   cfg.vif.esc_tx_int.esc_p <= 1'b0;
   cfg.vif.esc_tx_int.esc_n <= 1'b1;
-endtask : reset_esc
+endtask

hw/dv/sv/csrng_agent/csrng_device_driver.sv
@@ -9,7 +9,7 @@ class csrng_device_driver extends csrng_driver;
   uint   cmd_ack_dly;
   bit    rsp_sts;
 
-  virtual task reset_signals();
+  task on_enter_reset();
     cfg.vif.cmd_rsp_int.csrng_rsp_ack <= 1'b0;
     cfg.vif.cmd_rsp_int.csrng_rsp_sts <= CMD_STS_SUCCESS;
   endtask
@@ -55,7 +55,7 @@ class csrng_device_driver extends csrng_driver;
         seq_item_port.item_done(rsp);
       end
 
-      // Write ack bit again in case the race condition with `reset_signals`.
+      // Write ack bit again to avoid a race with reset tracking
       if (cfg.in_reset) cfg.vif.device_cb.cmd_rsp_int.csrng_rsp_ack <= 1'b0;
 
     end

hw/dv/sv/csrng_agent/csrng_host_driver.sv
@@ -9,11 +9,6 @@ class csrng_host_driver extends csrng_driver;
   // Break down the data and send it to push_pull_host_driver through its sequencer
   csrng_sequencer   m_csrng_sequencer;
 
-  // reset signals
-  virtual task reset_signals();
-//    `uvm_fatal(`gtn, "FIXME")
-  endtask
-
   // drive trans received from sequencer
   virtual task get_and_drive();
     forever begin

commit b027acf83c59bc6d3053744f33390804c7b75167

[alert,dv] Fix double response in alert_sender_driver
This fixes a bug that I introduced in commit efed93f, where the
drive_reqs_with_lpg_mode task calls send_item to send an item and then
sends a response. Unfortunately, send_item also sends the response!

Drop the duplication and amend the task documentation comment to
explicitly say what's going on.

Signed-off-by: Rupert Swarbrick <rswarbrick@lowrisc.org>

hw/dv/sv/alert_esc_agent/alert_sender_driver.sv
@@ -31,8 +31,8 @@ class alert_sender_driver extends alert_base_driver;
   // Drive sequence items for alerts and pings until either a reset or a change of cfg.en_alert_lpg
   extern local task drive_reqs_with_lpg_mode(bit en_alert_lpg);
 
-  // Drive a single sequence item to send an alert, possibly responding to a ping. Once the item has
-  // been driven, mark it done in seq_item_port.
+  // Drive a single sequence item to send an alert, possibly responding to a ping. This task doesn't
+  // mark the item done in seq_item_port: the caller should do that.
   //
   // This exits immediately on a reset.
   extern local task send_item(bit is_ping_rsp, alert_seq_item item);
@@ -140,7 +140,7 @@ endtask
 
 task alert_sender_driver::drive_reqs_with_lpg_mode(bit en_alert_lpg);
   forever begin
-    alert_seq_item item;
+    alert_seq_item item, rsp;
 
     // Pick an item to drive, but keep track of resets and any change to the LPG flag.
     fork : isolation_fork begin
@@ -156,21 +156,21 @@ task alert_sender_driver::drive_reqs_with_lpg_mode(bit en_alert_lpg);
     // task.
     if (item == null) return;
 
+    `downcast(rsp, item.clone());
+    rsp.set_id_info(item);
+
     // Handle the alert request or ping response.
     case (item.m_txn_type)
-      alert_seq_item::AlertTxn: send_item(1'b0, item);
-      alert_seq_item::PingTxn:  send_item(1'b1, item);
+      alert_seq_item::AlertTxn: send_item(1'b0, rsp);
+      alert_seq_item::PingTxn:  send_item(1'b1, rsp);
       default: `uvm_fatal(get_full_name(), "Unknown txn type.")
     endcase
 
-    seq_item_port.put_response(item);
+    seq_item_port.put_response(rsp);
   end
 endtask
 
 task alert_sender_driver::send_item(bit is_ping_rsp, alert_seq_item item);
-  alert_seq_item rsp;
-  `downcast(rsp, item.clone());
-  rsp.set_id_info(item);
 
   `uvm_info(`gfn,
             $sformatf("starting to send sender item: %0s, int_err_cyc=%0b",
@@ -186,11 +186,10 @@ task alert_sender_driver::send_item(bit is_ping_rsp, alert_seq_item item);
     join_any
     disable fork;
   end join
-  `uvm_info(`gfn,
-            $sformatf("finished sending sender item: %0s, int_err_cyc=%0b",
-                      item.m_txn_type.name(), item.m_int_err_cyc), UVM_HIGH)
-
-  seq_item_port.put_response(rsp);
+  `uvm_info("driving_item",
+            $sformatf("finished sender item: %0s, m_int_err_cyc=%0b",
+                      item.m_txn_type.name(), item.m_int_err_cyc),
+            UVM_HIGH)
 endtask
 
 task alert_sender_driver::drive_alert_pins(alert_seq_item req);