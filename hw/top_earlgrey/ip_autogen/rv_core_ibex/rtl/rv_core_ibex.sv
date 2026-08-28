b2fb476fa2d7990fe8c6fee0d4d4c3dff875eeda
Samuel Riedel <sriedel@lowrisc.org>
2026-08-28 16:07:40 +0200

    [rv_core_ibex] Add missing countermeasures in documentation
     - Add countermeasure comments for mubi signals added recently
     - Add icache tweak infection countermeasure comment
     - Add rv_core_ibex address translation and TLUL conversion lockstep
       comparison countermeasure comment
     - Update comment of registerfile protection since the registerfile's
       ECC was split into two registerfiles
    
    Signed-off-by: Samuel Riedel <sriedel@lowrisc.org>

--- hw/top_earlgrey/ip_autogen/rv_core_ibex/rtl/rv_core_ibex.sv
+++ hw/top_earlgrey/ip_autogen/rv_core_ibex/rtl/rv_core_ibex.sv
@@ -460,11 +460,13 @@ module rv_core_ibex
   ibex_pkg::ibex_mubi_t mcounteren_writable_ibex;
   assign mcounteren_writable_mubi4 = prim_mubi_pkg::mubi4_t'(reg2hw.mcounteren_writable.q);
   // Convert the mubi4 to ibex_mubi. They are both four bit, but with different encodings.
+  // SEC_CM: MCOUNTEREN_WRITABLE.CTRL.MUBI
   assign mcounteren_writable_ibex = mcounteren_writable_mubi4 == prim_mubi_pkg::MuBi4True ?
                                     ibex_pkg::IbexMuBiOn : ibex_pkg::IbexMuBiOff;
   // Convert the mubi4 to ibex_mubi for the cheriot_enable signal. Invalid mubi inputs must not
   // accidentally map to a valid ibex_mubi value and switch modes, so the default arm produces an
   // invalid ibex_mubi value that the core can detect and escalate.
+  // SEC_CM: CHERIOT_ENABLE.CTRL.MUBI
   always_comb begin
     unique case (cheriot_ena_o)
       prim_mubi_pkg::MuBi4True:  cheriot_enable_ibex = ibex_pkg::IbexMuBiOn;
@@ -512,6 +514,7 @@ module rv_core_ibex
     // SEC_CM: PC.CTRL_FLOW.CONSISTENCY, CTRL_FLOW.UNPREDICTABLE, CORE.DATA_REG_SW.SCA
     // SEC_CM: EXCEPTION.CTRL_FLOW.GLOBAL_ESC, EXCEPTION.CTRL_FLOW.LOCAL_ESC
     // SEC_CM: DATA_REG_SW.INTEGRITY, DATA_REG_SW.GLITCH_DETECT
+    // SEC_CM: ICACHE.MEM.ADDR_INFECTION
     .SecureIbex                  ( SecureIbex               ),
     .LockstepOffset              ( LockstepOffset           ),
     .RndCnstLfsrSeed             ( RndCnstLfsrSeed          ),
@@ -845,6 +848,7 @@ module rv_core_ibex
     .spare_rsp_i (1'b0),
     .spare_rsp_o ());
 
+  // SEC_CM: CORED_TLUL.BUS.LC_GATED
   // Gate any pending requests on escalation
   tlul_lc_gate #(
     .Outstanding(NumOutstandingReqs),
@@ -1158,6 +1162,7 @@ module rv_core_ibex
   // fpga build info hook-up
   assign hw2reg.fpga_info.d = fpga_info_i;
 
+  // SEC_CM: TLUL_ADAPTER.LOGIC.SHADOW
   if (SecureIbex) begin : gen_tlul_lockstep
     /////////////////////////////////////////////////////
     // Shadow Core instruction and data region config. //
