bdfa76b08017f88658731c61e766854bb0031ea5
Michael Gautschi <mgautschi@lowrisc.org>
2026-09-03 16:04:31 +0200

    [rram_ctrl,dv] Add test package and SVA bind
    Adds rram_ctrl_base_test (the default UVM test) and its fusesoc core, and
    the SVA bind file that hooks the block's assertions into the testbench.
    
    Signed-off-by: Michael Gautschi <mgautschi@lowrisc.org>
    
    Co-authored-by: Martin Velay <mvelay@lowrisc.org>

---
+++ hw/ip/rram_ctrl/dv/tests/rram_ctrl_base_test.sv
@@ -0,0 +1,27 @@
+// Copyright lowRISC contributors (OpenTitan project).
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+class rram_ctrl_base_test extends cip_base_test #(
+    .CFG_T(rram_ctrl_env_cfg),
+    .ENV_T(rram_ctrl_env)
+  );
+
+  `uvm_component_utils(rram_ctrl_base_test)
+
+  // The base class dv_base_test creates the following instances:
+  //   - rram_ctrl_env_cfg: cfg
+  //   - rram_ctrl_env:     env
+
+  // The base class also looks up UVM_TEST_SEQ plusarg to create and run that seq in the run_phase.
+  // As such, nothing more needs to be done
+
+  // Standard SV/UVM methods
+  extern function new(string name="", uvm_component parent=null);
+
+endclass : rram_ctrl_base_test
+
+
+function rram_ctrl_base_test::new(string name="", uvm_component parent=null);
+  super.new(name, parent);
+endfunction : new
