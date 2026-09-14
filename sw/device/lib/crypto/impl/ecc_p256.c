48d304064221b6d5e38536634bb5fbd6c9c81d57
Siemen Dhooghe <sdhooghe@google.com>
2026-09-14 10:15:49 +0200

    [crypto] Shred ss after use
    Signed-off-by: Siemen Dhooghe <sdhooghe@google.com>

--- sw/device/lib/crypto/impl/ecc_p256.c
+++ sw/device/lib/crypto/impl/ecc_p256.c
@@ -903,6 +903,10 @@ otcrypto_status_t otcrypto_ecdh_p256_async_finalize(
   // Set the checksum.
   shared_secret->checksum = otcrypto_integrity_blinded_checksum(shared_secret);
 
+  // Shred ss.
+  HARDENED_TRY(hardened_memshred(ss.share0, ARRAYSIZE(ss.share0)));
+  HARDENED_TRY(hardened_memshred(ss.share1, ARRAYSIZE(ss.share1)));
+
   // Clear the OTBN sideload slot (in case the seed was sideloaded).
   return otcrypto_eval_exit(keymgr_sideload_clear_otbn());
 }
