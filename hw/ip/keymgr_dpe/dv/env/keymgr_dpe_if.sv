e4d56ac612cbcf2925da4eb1aea36ca9d236cbbe
Raphael Roth <rroth@lowrisc.org>
2026-09-17 10:43:53 +0200

    [keymgr_dpe, dv] Truncate `otbn` sideload key
    If the width of `kmac_digests_t` is wider than
    the `OtbnKeyWidth`, then the dv raises an error
    if the key is not properly truncated.
    
    Signed-off-by: Raphael Roth <rroth@lowrisc.org>

--- hw/ip/keymgr_dpe/dv/env/keymgr_dpe_if.sv
+++ hw/ip/keymgr_dpe/dv/env/keymgr_dpe_if.sv
@@ -281,8 +281,14 @@ interface keymgr_dpe_if(input clk, input rst_n);
       keymgr_dpe_pkg::keymgr_dpe_exposed_working_state_e state,
       keymgr_pkg::keymgr_key_dest_e dest = keymgr_pkg::Kmac
   );
-    keymgr_dpe_env_pkg::key_shares_t trun_key_shares = {key_shares[1][keymgr_pkg::KeyWidth-1:0],
-                                                    key_shares[0][keymgr_pkg::KeyWidth-1:0]};
+    keymgr_dpe_env_pkg::key_shares_t trun_key_shares = {
+        key_shares[1][keymgr_pkg::KeyWidth-1:0],
+        key_shares[0][keymgr_pkg::KeyWidth-1:0]
+      };
+    keymgr_dpe_env_pkg::otbn_key_shares_t otbn_key_shares = {
+        key_shares[1][keymgr_pkg::OtbnKeyWidth-1:0],
+        key_shares[0][keymgr_pkg::OtbnKeyWidth-1:0]
+      };
     case (dest)
       keymgr_pkg::Kmac: begin
         if (kmac_sideload_status != SideLoadClear) begin
@@ -301,7 +307,7 @@ interface keymgr_dpe_if(input clk, input rst_n);
       keymgr_pkg::Otbn: begin
         if (otbn_sideload_status != SideLoadClear) begin
           // only otbn uses full 384 bits digest data
-          otbn_key_exp         <= '{1'b1, key_shares};
+          otbn_key_exp         <= '{1'b1, otbn_key_shares};
           otbn_sideload_status <= SideLoadAvail;
         end
       end
