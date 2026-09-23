8cc8f9abf65d3557523c8a0904b254a302e147ef
Pascal Etterli <pascal.etterli@lowrisc.org>
2026-09-23 09:01:25 +0200

    [topgen] Handle intra-IP and inter-module signals for split IPs
    A split IP can have intra-IP connections which directly connect the two partitions. However, in the
    config a split IP is a single instance. So any regular inter-module signal would have the same start
    and end point, which is not supported by the current topgen.
    
    This commit thus introduces an intra-IP signal handling which uniquifies inter-module signals per
    partition. It does so by adding a partition qualifier to each inter-module signal of a split IP.
    This allows the regular inter-module signal handling to connect the two partitions properly.
    
    It also enables the partitions to use the same signal name, otherwise these would have to be unique.
    
    Non-split IPs use no qualifier.
    
    Co-authored-by: Florian Glaser <glaserf@lowrisc.org>
    Signed-off-by: Pascal Etterli <pascal.etterli@lowrisc.org>

--- util/topgen/lib.py
+++ util/topgen/lib.py
@@ -16,7 +16,7 @@ from basegen.lib import Name
 from basegen.typing import ConfigT, ParamsT
 from mako.template import Template
 from reggen.ip_block import IpBlock
-from reggen.lib import check_bool
+from reggen.lib import PART_PRIMARY, PART_SECONDARY, check_bool
 from version_file import VersionInformation
 
 # Ignore flake8 warning as the function is used in the template
@@ -601,6 +601,19 @@ def get_all_modules(top: ConfigT, domain: str = ""):
         return [m for m in top["module"] if m.get("domain") == domain]
     else:
         return top["module"]
+
+
+def get_domain_of_partition(module: ConfigT, partition: str = PART_PRIMARY,
+                            default: Optional[str] = None) -> Optional[str]:
+    '''Return the power domain of the given partition of a module instance.'''
+    if partition == PART_SECONDARY:
+        return module.get(secondary_key('domain'), default)
+    return module.get('domain', default)
+
+
+def get_domain_of_signal(module: Dict, sig: OrderedDict):
+    '''Power domain of a inter-module signal endpoint.'''
+    return get_domain_of_partition(module, sig.get('partition', PART_PRIMARY))
 
 
 # Template functions
